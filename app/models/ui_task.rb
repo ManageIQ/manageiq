9ad4aecd44bdb2cd45623e2c7f3bcfad6ca9c578
Jason Frey <jfrey@redhat.com>
2014-01-21 16:21:35 -0500

    Remove UiTask and UiTaskSet as they are no longer used.

--- app/models/ui_task.rb
+++
@@ -1,7 +0,0 @@
-class UiTask < ActiveRecord::Base
-  default_scope { where self.conditions_for_my_region_default_scope }
-
-  validates_presence_of     :area, :typ, :task, :name
-
-  acts_as_miq_set_member
-end
