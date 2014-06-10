25293b56306ae103f979a65fc23d0009e95598fd
Jason Dillaman <dillaman@redhat.com>
2014-06-10 04:33:26 -0400

    Add support for custom attributes for services
    Automation can now use the "custom_keys", "custom_set", and "custom_get"
    methods to manipulate the custom attributes on a service.  Any associated
    custom attributes are now visible on the service details page.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1108331

--- vmdb/app/models/service.rb
+++ vmdb/app/models/service.rb
@@ -14,6 +14,7 @@ class Service < ActiveRecord::Base
 
   include ServiceMixin
   include OwnershipMixin
+  include CustomAttributeMixin
 
   include_concern 'RetirementManagement'
   include_concern 'Aggregation'
