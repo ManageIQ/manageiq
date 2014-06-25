f11d18b7b563800f3029267428f4133d3234f834
Joe Rafaniello <jrafanie@redhat.com>
2014-06-24 22:15:48 -0400

    Add Build::GitCheckout to convert the VERSION file to git references.
    From the VERSION file, we can determine the branch or tag to we want to build from and then determine a consistent commit_sha for the build.

---
+++ build/git_checkout.rb
@@ -0,0 +1,37 @@
+require 'pathname'
+
+module Build
+  class GitCheckout
+    attr_reader :commit_sha, :branch_or_tag, :remote
+    def initialize(remote = "https://github.com/ManageIQ/manageiq.git")
+      @remote = remote
+      read_version_file
+    end
+
+    def commit_sha
+      long_sha =
+        if @branch_or_tag == "master"
+          ls_remote_sha("master")
+        else
+          ls_remote_sha(dereferenced_tag) || ls_remote_sha(@branch_or_tag)
+        end
+      long_sha.to_s[0, 10]
+    end
+
+    def ls_remote_sha(target)
+      `git ls-remote #{remote} #{target}`.split.first
+    end
+
+    private
+
+    def dereferenced_tag
+      "#{@branch_or_tag}^{}"
+    end
+
+    # TODO: Perhaps the contents of the VERSION should be passed in
+    def read_version_file
+      file = Pathname.new(File.expand_path(File.dirname(__FILE__)))
+      @branch_or_tag = File.read(file.join("../vmdb/VERSION")).strip
+    end
+  end
+end
