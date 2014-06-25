63824b7f8aef652c58f6eda800c544f8bc02bcd8
Joe Rafaniello <jrafanie@redhat.com>
2014-06-25 16:06:15 -0400

    Clients of this class should only need to read these variables.

--- build/kickstart_generator.rb
+++ build/kickstart_generator.rb
@@ -14,7 +14,7 @@ module Build
     KS_GEN_DIR = KS_DIR.join("generated").freeze
     KS_FILE    = Productization.file_for("kickstarts/base.ks.erb").freeze
 
-    attr_accessor :targets, :puddle
+    attr_reader :targets, :puddle
 
     def initialize(targets, puddle)
       @targets = targets
