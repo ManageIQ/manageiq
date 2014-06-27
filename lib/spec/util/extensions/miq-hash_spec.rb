9be23a14fc6fafda04a33987b642b5e2374f9982
Jason Frey <jfrey@redhat.com>
2014-06-27 12:42:45 -0400

    Add Hash#sort! and Hash#sort_by! core extensions.
    https://bugzilla.redhat.com/show_bug.cgi?id=1086015

--- lib/spec/util/extensions/miq-hash_spec.rb
+++ lib/spec/util/extensions/miq-hash_spec.rb
@@ -20,4 +20,24 @@ describe Hash do
     s2.should == s
     s2.sub_str.should == s.sub_str
   end
+
+  it "#sort!" do
+    h = {:x => 1, :b => 2, :y => 3, :a => 4}
+    h_id = h.object_id
+
+    h.sort!
+
+    h.keys.should      == [:a, :b, :x, :y]
+    h.object_id.should == h_id
+  end
+
+  it "#sort_by!" do
+    h = {:x => 1, :b => 2, :y => 3, :a => 4}
+    h_id = h.object_id
+
+    h.sort_by! { |k, _v| k }
+
+    h.keys.should      == [:a, :b, :x, :y]
+    h.object_id.should == h_id
+  end
 end
