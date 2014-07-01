bf6bea38c84e1b2429d9fd77e883f747f4c23397
Richard Oliveri <roliveri@redhat.com>
2014-07-01 15:47:29 -0400

    Added inventory collection for OpenStack cloud storage.
    Added collection of: volumes, volume snapshots, object store containers and object store objects.

---
+++ vmdb/db/migrate/20140612181021_update_cloud_volume_and_cloud_volume_snapshot.rb
@@ -0,0 +1,47 @@
+class UpdateCloudVolumeAndCloudVolumeSnapshot < ActiveRecord::Migration
+  def up
+    change_table :cloud_volumes do |t|
+      t.remove     :vm_id
+      t.remove     :device_name
+
+      t.string     :status
+      t.string     :description
+      t.string     :volume_type
+      t.boolean    :bootable
+      t.datetime   :creation_time
+
+      t.belongs_to :cloud_tenant
+    end
+
+    change_table :cloud_volume_snapshots do |t|
+      t.string   :status
+      t.datetime :creation_time
+      t.integer  :size, :limit => 8
+
+      t.belongs_to :cloud_tenant
+    end
+  end
+
+  def down
+    change_table :cloud_volumes do |t|
+      t.belongs_to :vm
+      t.string     :device_name
+
+      t.remove     :status
+      t.remove     :description
+      t.remove     :volume_type
+      t.remove     :bootable
+      t.remove     :creation_time
+
+      t.remove     :cloud_tenant_id
+    end
+
+    change_table :cloud_volume_snapshots do |t|
+      t.remove :status
+      t.remove :creation_time
+      t.remove :size
+
+      t.remove :cloud_tenant_id
+    end
+  end
+end
