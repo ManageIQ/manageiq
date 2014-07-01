8b7dd5aff7ff0a1b3dc445763f0b9b86c955fb74
Tina Fitzgerald <tfitzger@redhat.com>
2014-07-01 13:42:44 -0400

    Adjust MiqAeDatastore model counts.

--- vmdb/spec/automation/unit/miq_ae_import_spec.rb
+++ vmdb/spec/automation/unit/miq_ae_import_spec.rb
@@ -2,11 +2,11 @@ require "spec_helper"
 
 describe MiqAeDatastore do
   it "should test import database count values" do
-    MiqAeClass.count.should eql(54)
-    MiqAeField.count.should eql(496)
+    MiqAeClass.count.should eql(53)
+    MiqAeField.count.should eql(479)
     MiqAeInstance.count.should eql(222)
-    MiqAeNamespace.count.should eql(15)
-    MiqAeMethod.count.should eql(174)
+    MiqAeNamespace.count.should eql(14)
+    MiqAeMethod.count.should eql(169)
     MiqAeValue.count.should eql(1748)
   end
 end
