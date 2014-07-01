df6a41225dce4c3af492771b424992324fc492c2
Richard Oliveri <roliveri@redhat.com>
2014-07-01 16:23:14 -0400

    Rubocop cleanup.

--- vmdb/app/models/ems_refresh/parsers/openstack.rb
+++ vmdb/app/models/ems_refresh/parsers/openstack.rb
@@ -185,7 +185,7 @@ module EmsRefresh::Parsers
       collection.each { |item| process_collection_item(item, key, &block) }
     end
 
-    def process_collection_item(item, key, &block)
+    def process_collection_item(item, key)
       @data[key] ||= []
 
       uid, new_result = yield(item)
