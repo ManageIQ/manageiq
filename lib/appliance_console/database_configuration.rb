d3438ecbd3a5f27e7cae95f4d7aae55f9dbb18b4
Joe Rafaniello <jrafanie@redhat.com>
2014-07-11 12:45:32 -0400

    Split up each option value properly.
    "evm:db:region --" was previously intended as a single unescaped option key with no value.
    
    AwesomeSpawn was changed to escape keys in addition to values that it had done previously.
    This caused the command line to be built like this:
    script/rails runner script/rake evm:db:region\ -- --region 0
    
    "dvm:db:region" and "--" are now separate option values with no key.

--- lib/appliance_console/database_configuration.rb
+++ lib/appliance_console/database_configuration.rb
@@ -72,7 +72,7 @@ module ApplianceConsole
 
     def create_region
       log_and_feedback(__method__) do
-        LinuxAdmin.run!("script/rails runner script/rake", :params => {"evm:db:region --" => nil, "--region" => region}, :chdir => RAILS_ROOT)
+        LinuxAdmin.run!("script/rails runner script/rake", :params => ["evm:db:region", "--", {:region => region}], :chdir => RAILS_ROOT)
       end
     end
 
