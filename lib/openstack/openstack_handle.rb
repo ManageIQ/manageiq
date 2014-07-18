d53c0047fb5a80ac5937cd165dd5cf54d9b81fe6
Richard Oliveri <roliveri@redhat.com>
2014-07-18 16:37:16 -0400

    Added new openstack_handle.rb

---
+++ lib/openstack/openstack_handle.rb
@@ -0,0 +1,11 @@
+__lib_dir__ = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift __lib_dir__ unless $LOAD_PATH.include?(__lib_dir__)
+
+require 'openstack_handle/handle'
+require 'fog'
+require 'openstack_handle/compute_delegate'
+require 'openstack_handle/identity_delegate'
+require 'openstack_handle/network_delegate'
+require 'openstack_handle/image_delegate'
+require 'openstack_handle/volume_delegate'
+require 'openstack_handle/storage_delegate'
