f7adedbab5511832bc45586ed0de54ab5c0a6044
Tina Fitzgerald <tfitzger@redhat.com>
2014-07-28 14:48:08 -0400

    Service auto-approval change.
    Create new Service schema approval_type attribute, and changed
    approve_request method to condition auto_approval based on type.

--- vmdb/db/fixtures/ae_datastore/ManageIQ/Service/Provisioning/StateMachines/ServiceProvisionRequestApproval.class/__methods__/approve_request.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Service/Provisioning/StateMachines/ServiceProvisionRequestApproval.class/__methods__/approve_request.rb
@@ -3,5 +3,9 @@
 #
 
 # Auto-Approve request
-$evm.log("info", "AUTO-APPROVING")
-$evm.root["miq_request"].approve("admin", "Auto-Approved")
+$evm.log("info", "Checking for auto_approval")
+approval_type = $evm.object['approval_type'].downcase
+if approval_type == 'auto'
+  $evm.log("info", "AUTO-APPROVING")
+  $evm.root["miq_request"].approve("admin", "Auto-Approved")
+end
