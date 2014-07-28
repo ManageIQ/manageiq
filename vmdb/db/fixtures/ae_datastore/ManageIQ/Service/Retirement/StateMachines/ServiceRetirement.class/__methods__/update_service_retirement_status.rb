8f933d3395af62c35a19b034f6bf2cbea8256684
Tina Fitzgerald <tfitzger@redhat.com>
2014-07-28 17:00:57 -0400

    Modified class schema entries and methods for status_state change.
    Removed references to status_state on update_*_status method calls and
    modified methods to get new root attribute instead of input value.

--- vmdb/db/fixtures/ae_datastore/ManageIQ/Service/Retirement/StateMachines/ServiceRetirement.class/__methods__/update_service_retirement_status.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Service/Retirement/StateMachines/ServiceRetirement.class/__methods__/update_service_retirement_status.rb
@@ -14,8 +14,8 @@ step = $evm.current_object.current_field_name
 # Get status from input field status
 status = $evm.inputs['status']
 
-# Get status_state ['on_entry', 'on_exit', 'on_error'] from input field
-status_state = $evm.inputs['status_state']
+# Get status_state ['on_entry', 'on_exit', 'on_error']
+status_state = $evm.root['ae_status_state']
 
 $evm.log("info", "Server:<#{server.name}> Ae_Result:<#{$evm.root['ae_result']}> State:<#{state}> Step:<#{step}> Status_State:<#{status_state}> Status:<#{status}>")
 
