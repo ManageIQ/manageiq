dc2d8020cab8b9267c2a6a2eff56a42360de1a6b
Lucy Fu <lufu@redhat.com>
2014-08-21 15:12:18 -0400

    Add test cases to reproduce the issue
    where tag fields are not available in the service report.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1127779

--- vmdb/spec/models/miq_expression/model_details_spec.rb
+++ vmdb/spec/models/miq_expression/model_details_spec.rb
@@ -2,12 +2,25 @@ require "spec_helper"
 
 describe MiqExpression do
   describe ".model_details" do
-    context "with :typ=>tag" do
-      before do
-        cat = FactoryGirl.create(:classification, :description => "Auto Approve - Max CPU", :name => "prov_max_cpu", :single_value => true, :show => true, :parent_id => 0)
-        cat.add_entry(:description=>"1", :read_only=>"0", :syntax=>"string", :name=>"1", :example_text=>nil, :default=>true, :single_value=>"1")
-      end
+    before do
+      cat = FactoryGirl.create(:classification,
+                               :description  => "Auto Approve - Max CPU",
+                               :name         => "prov_max_cpu",
+                               :single_value => true,
+                               :show         => true,
+                               :parent_id    => 0
+                              )
+      cat.add_entry(:description  => "1",
+                    :read_only    => "0",
+                    :syntax       => "string",
+                    :name         => "1",
+                    :example_text => nil,
+                    :default      => true,
+                    :single_value => "1"
+                   )
+    end
 
+    context "with :typ=>tag" do
       it "VmInfra" do
         result = described_class.model_details("VmInfra", :typ=>"tag", :include_model=>true, :include_my_tags=>true, :userid=>"admin")
         result.select { |r| r.first == "Virtual Machine.My Company Tags : Auto Approve - Max CPU" }.should_not be_empty
@@ -47,6 +60,18 @@ describe MiqExpression do
       it "EmsCloud" do
         result = described_class.model_details("EmsCloud", :typ=>"tag", :include_model=>true, :include_my_tags=>true, :userid=>"admin")
         result.select { |r| r.first == "Cloud Provider.My Company Tags : Auto Approve - Max CPU" }.should_not be_empty
+      end
+    end
+
+    context "with :typ=>all" do
+      it "Vm" do
+        result = described_class.model_details("Vm", :typ => "all", :include_model => false, :include_tags => true)
+        result.select { |r| r.first == "My Company Tags : Auto Approve - Max CPU" }.should_not be_empty
+      end
+
+      it "Service" do
+        result = described_class.model_details("Service", :typ => "all", :include_model => false, :include_tags => true)
+        result.select { |r| r.first == "My Company Tags : Auto Approve - Max CPU" }.should_not be_empty
       end
     end
   end
