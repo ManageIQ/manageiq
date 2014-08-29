87029ad92faa8c6c4764321073debb57dbbdb794
John Kim <kim_john3@bah.com>
2014-08-29 17:45:39 +0000

    adding ability to handle exception instances (instead of alaways assuming that it's a class level object)

--- vmdb/app/helpers/api_helper/error_handler.rb
+++ vmdb/app/helpers/api_helper/error_handler.rb
@@ -15,24 +15,27 @@ module ApiHelper
     end
 
     def api_exception_type(type, e)
-      api_error(type, e.message, e.class.name, e.backtrace.join("\n"), Rack::Utils.status_code(type))
+      api_error(type, e.message, e, e.backtrace.join("\n"), Rack::Utils.status_code(type))
     end
 
     def api_error_type(type, message)
-      api_error(type, message, self.class.name, "", Rack::Utils.status_code(type))
+      api_error(type, message, self.class, "", Rack::Utils.status_code(type))
     end
 
     private
 
-    def api_error(kind, message, klass, backtrace, status)
+    def api_error(kind, message, e_obj, backtrace, status)
       err = {
         :kind    => kind,
         :message => message,
-        :klass   => klass
+        :klass   => e_obj.name || e_obj.class.name
       }
       # We don't want to return the stack trace, but only log it in case of an internal error
       api_log_error("#{message}\n\n#{backtrace}") if kind == :internal_server_error && !backtrace.empty?
 
+      if status == 405
+        response.headers['Allow'] = klass.allowed_methods
+      end
       render :json => {:error => err}, :status => status
     end
   end
