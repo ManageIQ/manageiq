dd1644591df84f226c6d5d15f62f0dd9bb12a8c0
John Kim <kim_john3@bah.com>
2014-09-12 21:45:39 +0000

    fixing typo in rates_query_resource method

--- vmdb/app/controllers/api_controller/chargebacks.rb
+++ vmdb/app/controllers/api_controller/chargebacks.rb
@@ -3,40 +3,8 @@ class ApiController
 
     def rates_query_resource(object)
       klass = collection_config[:rates][:klass].constantize
-      object ? klass.where(:chargaback_rate_id => object.id) : {}
+      object ? klass.where(:chargeback_rate_id => object.id) : {}
     end
 
-    #def update_chargebacks()
-      #api_log_info("here")
-      #render json: {}
-    #end
-
-    #def set_chargeback()
-    #end
-
-    #def get_assignments(type)
-      #ChargebackRate.get_assignments(type)
-    #end
-
-    #def build_rates_obj()
-      #rates = []
-      #cbrs = ChargebackRate.all()
-      #cbrs.each do |cbr|
-        #rate = cbr.attributes
-        #rate['rate_details'] = cbr.chargeback_rate_details
-        #rates.append(rate)
-      #end
-      #rates
-    #end
-
-    #def show_chargebacks()
-      #render json: {
-        #:rates => build_rates_obj(),
-        #:assignments => {
-          #:compute => get_assignments(:compute),
-          #:storage => get_assignments(:storage)
-        #}
-      #}
-    #end
   end
 end
