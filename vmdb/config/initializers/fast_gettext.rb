7c9682975cfe9d6002005ac1ed72a068d35f6be6
Jozef Zigmund <jzigmund@redhat.com>
2014-09-16 16:41:38 +0200

    Initializers/fast_gettext: Change dictionary filetype to PO

--- vmdb/config/initializers/fast_gettext.rb
+++ vmdb/config/initializers/fast_gettext.rb
@@ -1,3 +1,3 @@
-FastGettext.add_text_domain('manageiq', :path => "#{Rails.root}/config/locales", :type => :yaml)
+FastGettext.add_text_domain('manageiq', :path => Rails.root.join("config/locales"), :type => :po)
 FastGettext.default_available_locales = ['en']
 FastGettext.default_text_domain = 'manageiq'
