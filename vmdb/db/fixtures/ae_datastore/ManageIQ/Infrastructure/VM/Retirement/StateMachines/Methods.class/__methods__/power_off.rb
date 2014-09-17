7ea668c38e5f464a4042520a25ce53d7cfe9deee
Tina Fitzgerald <tfitzger@redhat.com>
2014-09-17 11:24:40 -0400

    Modify ManageIQ/Infrastructure/vm/retirement statemachine and method classes for combining delete and unregister provider.
    Class - remove unregisterfromprovider state.
    Remove instance display names.
    Rename delete_from_provider_check method to check_delete_from_provider
    Modified delete_from_provider method to have option for delete and
    unregister, added unregister code, and changed method criteria to
    check for vm archived or orphaned instead of host.nil.
    Modified Default instance to reflect provider changes.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1074532

--- vmdb/db/fixtures/ae_datastore/ManageIQ/Infrastructure/VM/Retirement/StateMachines/Methods.class/__methods__/power_off.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Infrastructure/VM/Retirement/StateMachines/Methods.class/__methods__/power_off.rb
@@ -1,10 +1,10 @@
 #
-# Description: This method powers-off the VM on the VC
+# Description: This method powers-off the VM on the provider
 #
 
 vm = $evm.root['vm']
 unless vm.nil? || vm.attributes['power_state'] == 'off'
   ems = vm.ext_management_system
-  $evm.log('info', "Powering Off VM <#{vm.name}> in VC <#{ems ? ems.name : nil}")
+  $evm.log('info', "Powering Off VM <#{vm.name}> in provider <#{ems.try(:name)}")
   vm.stop
 end
