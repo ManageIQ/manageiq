6c158fefabe1078e629ebfd2b1a86ebd4a7c2078
Lucy Fu <lufu@redhat.com>
2014-09-18 16:54:13 -0400

    Add virtual column used to CloudResourceQuota.
    So the column used will be exposed to automate automatically and available for reporting.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1142475

--- vmdb/lib/miq_automation_engine/service_models/miq_ae_service_cloud_resource_quota.rb
+++ vmdb/lib/miq_automation_engine/service_models/miq_ae_service_cloud_resource_quota.rb
@@ -2,6 +2,5 @@ module MiqAeMethodService
   class MiqAeServiceCloudResourceQuota < MiqAeServiceModelBase
     expose :ext_management_system, :association => true
     expose :cloud_tenant,          :association => true
-    expose :used
   end
 end
