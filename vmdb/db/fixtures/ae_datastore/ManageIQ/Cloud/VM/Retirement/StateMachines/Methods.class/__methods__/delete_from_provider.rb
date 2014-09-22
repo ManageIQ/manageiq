ddd7f0a038ab13f63269d5ab0d0f12ce1a486203
Tina Fitzgerald <tfitzger@redhat.com>
2014-09-22 15:19:38 -0400

    Modify ManageIQ/Cloud/vm/retirement statemachine and method classes for combining delete and unregister provider.
    Class - remove unregisterfromprovider state.
    Remove instance display names.
    Rename delete_from_provider_check method to check_delete_from_provider
    Modified delete_from_provider method to have option for delete and
    unregister, added unregister code, and changed method criteria to
    check for vm archived or orphaned instead of host.nil.
    Modified Default instance to reflect provider changes.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1169911

--- vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/delete_from_provider.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/delete_from_provider.rb
@@ -1,45 +1,30 @@
-###################################
 #
-# EVM Automate Method: delete_from_provider
+# Description: This method deletes the VM from the provider
 #
-# Notes: This method deletes the VM from the provider
-#
-###################################
-begin
-  @method = 'delete_from_provider'
-  $evm.log("info", "#{@method} - EVM Automate Method Started")
 
-  # Turn of verbose logging
-  @debug = true
+# Get vm from root object
+vm = $evm.root['vm']
+category = "lifecycle"
+tag = "retire_full"
 
-  # Get vm from root object
-  vm = $evm.root['vm']
-  category = "lifecycle"
-  tag = "retire_full"
+deletion_type = $evm.inputs['deletion_type'].downcase
+$evm.set_state_var('vm_deleted_from_provider', false)
 
-  miq_guid = /\w*MIQ\sGUID/i
-  if vm.v_annotation =~  miq_guid
-    vm_was_provisioned = true
+if vm
+  ems = vm.ext_management_system
+  case deletion_type
+  when "remove_from_disk"
+    if vm.miq_provision || vm.tagged_with?(category, tag)
+      $evm.log('info', "Deleting VM:<#{vm.name}> from provider:<#{ems.try(:name)}>")
+      vm.remove_from_disk
+      $evm.set_state_var('vm_deleted_from_provider', true)
+    end
+  when "unregister"
+    $evm.log('info', "Unregistering VM:<#{vm.name}> from provider:<#{ems.try(:name)}")
+    vm.unregister
+    $evm.set_state_var('vm_deleted_from_provider', true)
   else
-    vm_was_provisioned = false
+    $evm.log('info', "Unknown retirement type for VM:<#{vm.name}> from provider:<#{ems.try(:name)}")
+    exit MIQ_ABORT
   end
-
-  if vm && (vm_was_provisioned || vm.miq_provision || vm.tagged_with?(category, tag))
-    ems = vm.ext_management_system
-    $evm.log('info', "#{@method} - Deleting VM:<#{vm.name}> from EMS:<#{ems ? ems.name : nil}>") if @debug
-    vm.remove_from_disk
-  end
-
-  #
-  # Exit method
-  #
-  $evm.log("info", "#{@method} - EVM Automate Method Ended")
-  exit MIQ_OK
-
-  #
-  # Set Ruby rescue behavior
-  #
-rescue => err
-  $evm.log("error", "#{@method} - [#{err}]\n#{err.backtrace.join("\n")}")
-  exit MIQ_ABORT
 end
