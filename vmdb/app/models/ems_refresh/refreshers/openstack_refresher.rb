fbea080876829cdf0f68f0c1a9ec097e61efa3fa
Greg Blomquist <gblomqui@redhat.com>
2014-10-02 17:45:45 -0400

    Refactoring EMS refreshers' core logic into mixin
    There is a lot of logic copied between the EMS Refreshers.  This removes a lot
    fo code and logic duplication.
    
    Caveats:
    
    1. The VC Refresher is quite a bit different from the other refreshers, so it
       was largely left as is.
    2. The NonEmsRefresher wasn't touched, since it's not an EMS refresher
    
    Skipping CI for this rebase because the only update was a comment
    [skip ci]

--- vmdb/app/models/ems_refresh/refreshers/openstack_refresher.rb
+++ vmdb/app/models/ems_refresh/refreshers/openstack_refresher.rb
@@ -1,36 +1,13 @@
 module EmsRefresh::Refreshers
   class OpenstackRefresher < BaseRefresher
-    def refresh
-      log_header = "MIQ(#{self.class.name}.refresh)"
-      $log.info "#{log_header} Refreshing all targets..."
+    include EmsRefresherMixin
 
-      @targets_by_ems_id.each do |ems_id, targets|
-        # Get the ems object
-        ems = @ems_by_ems_id[ems_id]
+    def parse_inventory(ems, _targets)
+      EmsRefresh::Parsers::Openstack.ems_inv_to_hashes(ems, refresher_options)
+    end
 
-        log_ems_target = "#{log_header} EMS: [#{ems.name}], id: [#{ems.id}]"
-        $log.info "#{log_ems_target} Refreshing targets for EMS: [#{ems.name}], id: [#{ems.id}]..."
-        targets.each { |t| $log.info "#{log_ems_target}   #{t.class} [#{t.name}] id [#{t.id}]" }
-
-        start_time = Time.now
-
-        $log.debug "#{log_ems_target} Parsing Openstack inventory..."
-        hashes = EmsRefresh::Parsers::Openstack.ems_inv_to_hashes(ems, refresher_options)
-        $log.debug "#{log_ems_target} Parsing Openstack inventory...Completed in #{Time.now - start_time} seconds"
-        $log.debug "#{log_ems_target} inv hashes:\n#{hashes.pretty_inspect}" if DEBUG_TRACE
-        EmsRefresh.save_ems_inventory(ems, hashes)
-
-        # Do any post-operations for this EMS
-        [Vm].each do |klass|
-          if klass.respond_to?(:post_refresh_ems)
-            $log.info "#{log_ems_target} Performing post-refresh operations for #{klass} instances..."
-            klass.post_refresh_ems(ems_id, start_time)
-            $log.info "#{log_ems_target} Performing post-refresh operations for #{klass} instances...Complete"
-          end
-        end
-      end
-
-      $log.info "#{log_header} Refreshing all targets...Complete"
+    def post_process_refresh_classes
+      [Vm]
     end
   end
 end
