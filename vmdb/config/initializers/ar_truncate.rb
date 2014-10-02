4e97e3df4b2e6c84c33d8ac664cc28e187b04165
Aaron Patterson <aaron.patterson@gmail.com>
2014-10-02 17:39:47 +0200

    don't establish a connection for feature detection
    we should avoid bringing up a connection too early in the boot process,
    so just check the pg connection class directly

--- vmdb/config/initializers/ar_truncate.rb
+++ vmdb/config/initializers/ar_truncate.rb
@@ -1,7 +1,8 @@
-if ActiveRecord::Base.connection.respond_to? :truncate
+begin
+  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.instance_method(:truncate)
   puts "Rails is new enough to have truncate, please remove this: #{__FILE__}"
-else
-  class << ActiveRecord::Base.connection.class
+rescue NameError
+  class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
     def truncate(table_name, name = nil)
       execute("TRUNCATE TABLE #{quote_table_name(table_name)}", name)
     end
