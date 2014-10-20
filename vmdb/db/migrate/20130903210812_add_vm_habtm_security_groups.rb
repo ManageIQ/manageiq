f607f22c4d41b750fcd29d35f7f8cb0a76002a12
Aaron Patterson <aaron.patterson@gmail.com>
2014-10-20 15:24:31 -0700

    pass the column type for the join column to references
    this is the same API that is supported on master, so we can upgrade
    this more easily

--- vmdb/db/migrate/20130903210812_add_vm_habtm_security_groups.rb
+++ vmdb/db/migrate/20130903210812_add_vm_habtm_security_groups.rb
@@ -1,8 +1,8 @@
 class AddVmHabtmSecurityGroups < ActiveRecord::Migration
   def change
     create_table :security_groups_vms, :id => false do |t|
-      t.references :security_group
-      t.references :vm
+      t.references :security_group, :type => :bigint
+      t.references :vm, :type => :bigint
     end
   end
 end
--- vmdb/db/migrate/20130904193717_add_vm_habtm_key_pairs.rb
+++ vmdb/db/migrate/20130904193717_add_vm_habtm_key_pairs.rb
@@ -1,8 +1,8 @@
 class AddVmHabtmKeyPairs < ActiveRecord::Migration
   def change
     create_table :key_pairs_vms, :id => false do |t|
-      t.references :authentication
-      t.references :vm
+      t.references :authentication, :type => :bigint
+      t.references :vm, :type => :bigint
     end
   end
 end
