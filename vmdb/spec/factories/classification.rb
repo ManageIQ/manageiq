d8dda38a3f6f6158e86f6e33fc33051ffbbf9333
Jason Frey <jfrey@redhat.com>
2014-10-22 16:49:34 -0400

    Cleaned up factories for classifications and their tag children.

--- vmdb/spec/factories/classification.rb
+++ vmdb/spec/factories/classification.rb
@@ -2,24 +2,32 @@ FactoryGirl.define do
   factory :classification do
     sequence(:name)        { |n| "category_#{n}" }
     sequence(:description) { |n| "category #{n}" }
-    syntax    "string"
     parent_id 0
   end
+
+  factory :classification_tag, :class => :Classification do
+    sequence(:name)        { |n| "tag_#{n}" }
+    sequence(:description) { |n| "tag #{n}" }
+  end
+
+  #
+  # Classification categories
+  #
 
   factory :classification_cost_center, :parent => :classification do
     name        "cc"
     description "Cost Center"
   end
 
-  factory :classification_tag, :class => :Classification do
-    sequence(:name)        { |n| "tag_#{n}" }
-    sequence(:description) { |n| "tag #{n}" }
-    syntax "string"
-  end
+  #
+  # Classification categories with child tags
+  #
 
-  factory :classification_tag_cost_center, :parent => :classification_tag do
-    name        "001"
-    description "Cost Center 001"
-    parent      { FactoryGirl.create(:classification_cost_center) }
+  factory :classification_cost_center_with_tags, :parent => :classification_cost_center do
+    children {
+      [
+        FactoryGirl.create(:classification_tag, :name => "001", :description => "Cost Center 001"),
+      ]
+    }
   end
 end
