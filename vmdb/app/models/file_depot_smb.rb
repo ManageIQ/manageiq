6c66093074ce8cf5cfd905f1dfa1ddf1ebbaa9f6
Lucy Fu <lufu@redhat.com>
2014-10-23 12:31:54 -0400

    Move the Samba log depot validation code from LogFile.verify_log_depot_settings into FileDepotSmb.validate.
    https://bugzilla.redhat.com/show_bug.cgi?id=1151173

--- vmdb/app/models/file_depot_smb.rb
+++ vmdb/app/models/file_depot_smb.rb
@@ -2,4 +2,10 @@ class FileDepotSmb < FileDepot
   def self.uri_prefix
     "smb"
   end
+
+  def self.validate(settings)
+    res = MiqSmbSession.new(settings).verify
+    raise "Log Depot Settings validation failed with error: #{res.last}" unless res.first
+    res
+  end
 end
