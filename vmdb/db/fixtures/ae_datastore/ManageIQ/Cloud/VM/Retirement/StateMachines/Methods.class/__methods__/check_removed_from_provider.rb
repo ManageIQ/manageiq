d67505f52bfe1ce2cbb0946c5221bfbae6a0a293
Tina Fitzgerald <tfitzger@redhat.com>
2014-11-25 15:48:44 -0500

    Model changes - Cloud/infra retirement state machine name changes.
    Renamed deleted_from_provider and check_deleted_from_provider states,
    methods, instances and relationships.

--- vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/check_removed_from_provider.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/check_removed_from_provider.rb
@@ -1,12 +1,12 @@
 #
-# Description: This method checks to see if the VM has been deleted from the provider
+# Description: This method checks to see if the VM has been removed from the provider
 #
 
 vm = $evm.root['vm']
 
 $evm.root['ae_result'] = 'ok'
 
-if vm && $evm.get_state_var('vm_deleted_from_provider')
+if vm && $evm.get_state_var('vm_removed_from_provider')
   if vm.ext_management_system
     $evm.root['ae_result']         = 'retry'
     $evm.root['ae_retry_interval'] = '15.seconds'
--- vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/remove_from_provider.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/remove_from_provider.rb
@@ -1,5 +1,5 @@
 #
-# Description: This method deletes the VM from the provider
+# Description: This method removes the VM from the provider
 #
 
 # Get vm from root object
@@ -7,22 +7,22 @@ vm = $evm.root['vm']
 category = "lifecycle"
 tag = "retire_full"
 
-deletion_type = $evm.inputs['deletion_type'].downcase
-$evm.set_state_var('vm_deleted_from_provider', false)
+removal_type = $evm.inputs['removal_type'].downcase
+$evm.set_state_var('vm_removed_from_provider', false)
 
 if vm
   ems = vm.ext_management_system
-  case deletion_type
+  case removal_type
   when "remove_from_disk"
     if vm.miq_provision || vm.tagged_with?(category, tag)
-      $evm.log('info', "Deleting VM:<#{vm.name}> from provider:<#{ems.try(:name)}>")
-      vm.remove_from_disk
-      $evm.set_state_var('vm_deleted_from_provider', true)
+      $evm.log('info', "Removing VM:<#{vm.name}> from provider:<#{ems.try(:name)}>")
+      vm.remove_from_disk(false)
+      $evm.set_state_var('vm_removed_from_provider', true)
     end
   when "unregister"
     $evm.log('info', "Unregistering VM:<#{vm.name}> from provider:<#{ems.try(:name)}")
     vm.unregister
-    $evm.set_state_var('vm_deleted_from_provider', true)
+    $evm.set_state_var('vm_removed_from_provider', true)
   else
     $evm.log('info', "Unknown retirement type for VM:<#{vm.name}> from provider:<#{ems.try(:name)}")
     exit MIQ_ABORT
