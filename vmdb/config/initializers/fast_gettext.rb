4e420195f6ee21b59b917a867753f7eeaa4815ce
Martin Povolny <mpovolny@redhat.com>
2014-11-26 14:43:23 +0100

    Set locale based on browser preference.

--- vmdb/config/initializers/fast_gettext.rb
+++ vmdb/config/initializers/fast_gettext.rb
@@ -1,3 +1,3 @@
 FastGettext.add_text_domain('manageiq', :path => Rails.root.join("config/locales"), :type => :po)
-FastGettext.default_available_locales = ['en']
+FastGettext.default_available_locales = %w(en hu it nl)
 FastGettext.default_text_domain = 'manageiq'
