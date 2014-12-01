adba5b1163fa47c648509701f27dfcdc7769e2b0
Jozef Zigmund <jzigmund@redhat.com>
2014-12-01 15:21:43 +0100

    Fastgettext: Add Slovak to initializer

--- vmdb/config/initializers/fast_gettext.rb
+++ vmdb/config/initializers/fast_gettext.rb
@@ -1,3 +1,3 @@
 FastGettext.add_text_domain('manageiq', :path => Rails.root.join("config/locales"), :type => :po)
-FastGettext.default_available_locales = %w(en hu it nl)
+FastGettext.default_available_locales = %w(en hu it nl sk)
 FastGettext.default_text_domain = 'manageiq'
