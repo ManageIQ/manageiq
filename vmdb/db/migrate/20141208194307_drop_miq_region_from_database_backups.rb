e137ac5a7106b082df14eab0a799e2b41a20874c
Joe Rafaniello <jrafanie@redhat.com>
2014-12-10 15:44:11 -0500

    We need to re-add the index for the foreign key id in the down.
    To be symmetric, we should drop the index before we drop the column.

--- vmdb/db/migrate/20141208194307_drop_miq_region_from_database_backups.rb
+++ vmdb/db/migrate/20141208194307_drop_miq_region_from_database_backups.rb
@@ -1,9 +1,11 @@
 class DropMiqRegionFromDatabaseBackups < ActiveRecord::Migration
   def up
+    remove_index  :database_backups, :miq_region_id
     remove_column :database_backups, :miq_region_id
   end
 
   def down
     add_column :database_backups, :miq_region_id, :bigint
+    add_index  :database_backups, :miq_region_id
   end
 end
