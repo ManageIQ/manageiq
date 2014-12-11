2f80a78c3aefa34dc3f2918fe5ae6c5b734b0219
Lucy Fu <lufu@redhat.com>
2014-12-11 12:38:15 -0500

    Fix the log message about depot settings.
    Which is used by both log depot and DB backup.
    https://bugzilla.redhat.com/show_bug.cgi?id=1173213

--- vmdb/app/models/file_depot_smb.rb
+++ vmdb/app/models/file_depot_smb.rb
@@ -5,7 +5,7 @@ class FileDepotSmb < FileDepot
 
   def self.validate_settings(settings)
     res = MiqSmbSession.new(settings).verify
-    raise "Log Depot Settings validation failed with error: #{res.last}" unless res.first
+    raise "Depot Settings validation failed with error: #{res.last}" unless res.first
     res
   end
 end
