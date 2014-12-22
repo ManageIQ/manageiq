4511cac601dc80cb0c70b390eb8f2e9a50e4bd78
Joe Rafaniello <jrafanie@redhat.com>
2014-12-22 17:18:58 -0500

    Move specific setup out of the general LogManagement context.
    https://bugzilla.redhat.com/show_bug.cgi?id=1174855

--- vmdb/spec/models/miq_server/log_management_spec.rb
+++ vmdb/spec/models/miq_server/log_management_spec.rb
@@ -2,21 +2,21 @@ require "spec_helper"
 
 describe MiqServer do
   context "LogManagement" do
-    let(:depot)          { FactoryGirl.build(:file_depot, :uri => uri) { |d| d.save(:validate => false) } }
-    let(:new_depot_hash) { {:uri => "nfs://server.example.com", :username => "new_user", :password => "new_pass"} }
-    let(:uri)            { "smb://server/share" }
-
-    before do
-      _, @miq_server, @zone = EvmSpecHelper.create_guid_miq_server_zone
-      depot.update_authentication(:default => {:userid => "user", :password => "pass"})
-    end
-
     context "#get_log_depot_settings" do
       let(:depot_hash) do
         {:uri      => uri,
          :username => "user",
          :password => "pass",
          :name     => "File Depot"}
+      end
+
+      let(:depot)          { FactoryGirl.build(:file_depot, :uri => uri) { |d| d.save(:validate => false) } }
+      let(:new_depot_hash) { {:uri => "nfs://server.example.com", :username => "new_user", :password => "new_pass"} }
+      let(:uri)            { "smb://server/share" }
+
+      before do
+        _, @miq_server, @zone = EvmSpecHelper.create_guid_miq_server_zone
+        depot.update_authentication(:default => {:userid => "user", :password => "pass"})
       end
 
       it "set on miq_server" do
