d5ad9d288003044f0a076d73c56e960b87419b64
Brandon Dunne <bdunne@redhat.com>
2015-01-08 16:34:01 -0500

    [VmOrTemplate] Update generic status method specs based on feedback

--- vmdb/spec/factories/vm_or_template.rb
+++ vmdb/spec/factories/vm_or_template.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-  factory :vm do
+  factory :vm_or_template do
     sequence(:name) { |n| "vm_#{seq_padded_for_sorting(n)}" }
-    location        { |x| "#{x.name}/#{x.name}.xml" }
+    location        { |x| "#{x.name}/#{x.name}" }
     uid_ems         { MiqUUID.new_guid }
     vendor          "unknown"
     template        false
