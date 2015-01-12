0b5bf36350beed8cb083c2dd997987eab2202891
Erik Clarizio <eclarizi@redhat.com>
2015-01-12 12:52:03 -0800

    Add say_with_time for benchmarking conversion of options to columns

--- vmdb/db/migrate/20141126161823_convert_show_refresh_button_and_load_values_on_init_to_real_columns_for_dialog_fields.rb
+++ vmdb/db/migrate/20141126161823_convert_show_refresh_button_and_load_values_on_init_to_real_columns_for_dialog_fields.rb
@@ -8,18 +8,22 @@ class ConvertShowRefreshButtonAndLoadValuesOnInitToRealColumnsForDialogFields < 
     add_column :dialog_fields, :show_refresh_button, :boolean
     add_column :dialog_fields, :load_values_on_init, :boolean
 
-    DialogField.all.each do |dialog_field|
-      dialog_field.show_refresh_button = dialog_field.options.delete(:show_refresh_button)
-      dialog_field.load_values_on_init = dialog_field.options.delete(:load_values_on_init)
-      dialog_field.save
+    say_with_time("Converting options[:show_refresh_button] and options[:load_values_on_init] to column fields") do
+      DialogField.all.each do |dialog_field|
+        dialog_field.show_refresh_button = dialog_field.options.delete(:show_refresh_button)
+        dialog_field.load_values_on_init = dialog_field.options.delete(:load_values_on_init)
+        dialog_field.save
+      end
     end
   end
 
   def down
-    DialogField.all.each do |dialog_field|
-      dialog_field.options[:load_values_on_init] = dialog_field.load_values_on_init
-      dialog_field.options[:show_refresh_button] = dialog_field.show_refresh_button
-      dialog_field.save
+    say_with_time("Converting column fields show_refresh_button and load_values_on_init back to options") do
+      DialogField.all.each do |dialog_field|
+        dialog_field.options[:load_values_on_init] = dialog_field.load_values_on_init
+        dialog_field.options[:show_refresh_button] = dialog_field.show_refresh_button
+        dialog_field.save
+      end
     end
 
     remove_column :dialog_fields, :load_values_on_init
