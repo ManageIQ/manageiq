0baa69dff7aa28ddb785a68abaf7582bbc9b2d5a
Jason Frey <jfrey@redhat.com>
2015-01-16 18:07:01 -0500

    Add new C4 instance types for Amazon EC2.
    Also,
    - Fix minor things from other instance types
    - Added intel_avx2 attribute for the new C4 instance.
    
    See
    http://aws.amazon.com/ec2/instance-types
    http://docs.amazonwebservices.com/AWSEC2/latest/UserGuide/instance-types.html
    http://aws.amazon.com/ec2/previous-generation
    http://www.ec2instances.info/

--- vmdb/spec/models/ems_refresh/refreshers/ec2_refresher_other_region_spec.rb
+++ vmdb/spec/models/ems_refresh/refreshers/ec2_refresher_other_region_spec.rb
@@ -32,7 +32,7 @@ describe EmsRefresh::Refreshers::Ec2Refresher do
 
   def assert_table_counts
     ExtManagementSystem.count.should == 1
-    Flavor.count.should              == 38
+    Flavor.count.should              == 43
     AvailabilityZone.count.should    == 3
     FloatingIp.count.should          == 1
     AuthPrivateKey.count.should      == 2
@@ -61,7 +61,7 @@ describe EmsRefresh::Refreshers::Ec2Refresher do
       :uid_ems     => nil
     )
 
-    @ems.flavors.size.should            == 38
+    @ems.flavors.size.should            == 43
     @ems.availability_zones.size.should == 3
     @ems.floating_ips.size.should       == 1
     @ems.key_pairs.size.should          == 2
