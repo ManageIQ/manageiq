b09c33c77628d94990deac6d126ea7abd146e0ea
Joe Rafaniello <jrafanie@redhat.com>
2015-01-16 13:37:04 -0500

    Enumerable#inject -> #each_with_object

--- vmdb/app/models/miq_report/generator.rb
+++ vmdb/app/models/miq_report/generator.rb
@@ -670,9 +670,8 @@ module MiqReport::Generator
       if association == "categories" || association == "managed"
         association_objects = []
         assochash = {}
-        @descriptions_by_tag_id ||= Classification.where("parent_id != 0").inject({}) do |h, c|
+        @descriptions_by_tag_id ||= Classification.where("parent_id != 0").each_with_object({}) do |c, h|
           h[c.tag_id] = c.description
-          h
         end
 
         assoc_options[:only].each {|c|
