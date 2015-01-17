f5cf90d6ea153f2fbf41b5b10c1501a340070a22
Jason Frey <jfrey@redhat.com>
2015-01-17 01:09:00 -0500

    Change automation examples to use before(:suite) instead of before(:all)
    The usage of :all was a bug as :suite was the intention all along.
    This drops the test:automation suite from 32 seconds to 14 seconds.

--- vmdb/spec/support/automation_example_group.rb
+++ vmdb/spec/support/automation_example_group.rb
@@ -1,16 +1,27 @@
 module AutomationExampleGroup
   extend ActiveSupport::Concern
 
+  class << self
+    attr_accessor :fixtures_loaded
+  end
+
   included do
     metadata[:type] = :automation
 
-    before(:all) do
-      MiqAeDatastore.reset
-      MiqAeDatastore.reset_manageiq_domain
-    end
+    unless AutomationExampleGroup.fixtures_loaded
+      RSpec.configure do |config|
+        config.before(:suite) do
+          puts "** Resetting ManageIQ domain"
+          MiqAeDatastore.reset
+          MiqAeDatastore.reset_manageiq_domain
+        end
 
-    after(:all) do
-      MiqAeDatastore.reset
+        config.after(:suite) do
+          MiqAeDatastore.reset
+        end
+      end
+
+      AutomationExampleGroup.fixtures_loaded = true
     end
   end
 end
