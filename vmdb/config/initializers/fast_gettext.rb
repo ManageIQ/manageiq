d9af951ab7f2aa6d3efe95e40fe1f13e215765ca
Jozef Zigmund <jzigmund@redhat.com>
2015-01-26 17:44:41 +0100

    Initializers/fast_gettext: Fix the initialization of I18n.available_locales
    FastGettext is overwriting I18n.locale method that returns regional lang. codes
    as 'xx-XX' instead of 'xx_XX' as it's defined in I18n.available_locales

--- vmdb/config/initializers/fast_gettext.rb
+++ vmdb/config/initializers/fast_gettext.rb
@@ -2,7 +2,7 @@ def register_human_localenames
   original_locale = FastGettext.locale
   FastGettext.class.class_eval { attr_accessor :human_available_locales }
   FastGettext.human_available_locales = []
-  FastGettext.default_available_locales.each do |locale|
+  FastGettext.available_locales.each do |locale|
     FastGettext.locale = locale
     # TRANSLATORS: Provide locale name in native language (e.g. English, Deutsch or Português)
     human_locale = _("locale_name")
@@ -14,14 +14,21 @@ ensure
   FastGettext.locale = original_locale
 end
 
+def fix_i18n_available_locales
+  I18n.available_locales += FastGettext.available_locales.grep(/_/).map { |i| i.gsub("_", "-") }
+end
+
 locale_path = Rails.root.join("config/locales")
 
 FastGettext.add_text_domain('manageiq',
                             :path           => locale_path,
                             :type           => :po,
                             :report_warning => false)
-FastGettext.default_available_locales = Dir.entries(locale_path)
+FastGettext.available_locales = Dir.entries(locale_path)
   .select { |entry| (File.directory? File.join(locale_path, entry)) && entry != '.' && entry != '..' }
   .sort
+
+# temporary hack to fix a problem with locales including "_"
+fix_i18n_available_locales
 FastGettext.default_text_domain = 'manageiq'
 register_human_localenames
