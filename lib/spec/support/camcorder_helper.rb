cf17181ce5aef18cc2a0444fa17ad6a7d7960c63
Jason Frey <jfrey@redhat.com>
2015-01-30 14:10:18 -0500

    Cleanup camcorder helper.
    - Set the recordings_dir only once
    - Added ensure on the deintercept_all call.
    - Remove Camcorder references since we are already in the class

--- lib/spec/support/camcorder_helper.rb
+++ lib/spec/support/camcorder_helper.rb
@@ -1,42 +1,38 @@
-require 'openssl' # Required for 'Digest' in camcorder (< Ruby 2.1)?
 require 'camcorder'
 
+Camcorder.config.recordings_dir = File.join(LIB_ROOT, 'spec/recordings')
+
 module Camcorder
-  def self.recordings_dir
-    @record_dir ||= File.join(spec_dir, 'recordings')
-  end
-
-  def self.recording_for(id)
-    idyml = id + (id[-4..-1] == '.yml' ? '' : '.yml')
-    File.expand_path(File.join(recordings_dir, idyml))
-  end
-
-  def self.recorder_for(recording_id)
-    Camcorder::Recorder.new(recording_for(recording_id))
-  end
-
   def self.intercept(cls, *methods)
     @intercepted_constructors ||= []
+    raise "#{cls} already intercepted" if @intercepted_constructors.include?(cls)
     @intercepted_constructors << cls
 
-    Camcorder.intercept_constructor(cls) do
+    intercept_constructor(cls) do
       methods_with_side_effects(*methods)
     end
   end
 
-  def self.deintercept_all
-    @intercepted_constructors.each { |cls| Camcorder.deintercept_constructor(cls) }
-    @intercepted_constructors = []
-  end
-
-  def self.use_recording(recording)
-    Camcorder.config.recordings_dir = recordings_dir
-    Camcorder.default_recorder      = recorder_for(recording)
-
-    Camcorder.default_recorder.transaction do
-      yield
-
-      deintercept_all
+  def self.use_recording(name)
+    raise "no block given" unless block_given?
+    self.default_recorder = recorder_for(name)
+    default_recorder.transaction do
+      begin
+        yield
+      ensure
+        deintercept_all
+      end
     end
   end
+
+  def self.recorder_for(name)
+    Recorder.new(File.join(config.recordings_dir, "#{name}.yml"))
+  end
+  private_class_method :recorder_for
+
+  def self.deintercept_all
+    @intercepted_constructors.each { |cls| deintercept_constructor(cls) }
+    @intercepted_constructors = []
+  end
+  private_class_method :deintercept_all
 end
