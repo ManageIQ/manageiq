75c9d2810b5264e2097f7cb77d902ea6a1296d8f
Aparna Karve <akarve@redhat.com>
2015-02-03 14:07:40 -0800

    Folder restructuring and maintained the .html.haml extension for views

--- vmdb/app/assets/javascripts/application.js
+++ vmdb/app/assets/javascripts/application.js
@@ -4,13 +4,12 @@
 //= require directives/miqrequired
 //= require directives/checkchange
 //= require directives/repository/valid_unc_path
-//= require controllers/repository/repository_form_controller
-//= require cfme_application
-//= require miq_dynatree_replacement
-//= require controllers/schedule_form_controller
 //= require services/miq_service
 //= require services/timer_option_service
-//= require controllers/schedule_form_controller
+//= require controllers/repository/repository_form_controller
+//= require controllers/schedule/schedule_form_controller
+//= require cfme_application
+//= require miq_dynatree_replacement
 //= require dialog_import_export
 //= require widget_import_export
 //= require automate_import_export
