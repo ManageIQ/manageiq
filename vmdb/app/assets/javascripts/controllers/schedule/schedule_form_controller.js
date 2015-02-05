cf764762d2f07a931bf33267cdda24435733757a
Aparna Karve <akarve@redhat.com>
2015-02-05 13:33:22 -0800

    Added separate views for authorizing credentials
    These views can be reused in future Angular forms that need
    authorizing credentials

--- vmdb/app/assets/javascripts/controllers/schedule/schedule_form_controller.js
+++ vmdb/app/assets/javascripts/controllers/schedule/schedule_form_controller.js
@@ -282,5 +282,17 @@ cfmeAngularApplication.controller('scheduleFormController', ['$http', '$scope', 
     return value == null || value == '';
   };
 
+  $scope.validateFieldsValid = function() {
+    if($scope.angularForm != undefined &&
+       $scope.angularForm.depot_name.$valid &&
+       $scope.angularForm.uri.$valid &&
+       $scope.angularForm.log_userid.$valid &&
+       $scope.angularForm.log_password.$valid &&
+       $scope.angularForm.log_verify.$valid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   init();
 }]);
