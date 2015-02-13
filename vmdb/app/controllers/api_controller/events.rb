556d1670ee0bb7246636e3d8a2bd6deb7bfa01d6
Alberto Bellotti <abellott@redhat.com>
2015-02-13 10:19:10 -0500

    PR Review Changes
    - Simplifying valid condition check before calling events.
    - Optimized rspecs, using pluck vs collect, using match_array
    - Select/First -> detect simplification

--- vmdb/app/controllers/api_controller/events.rb
+++ vmdb/app/controllers/api_controller/events.rb
@@ -5,7 +5,7 @@ class ApiController
     #
 
     def events_query_resource(object)
-      return {} unless object && object.respond_to?(:events)
+      return {} unless object.respond_to?(:events)
       object.events
     end
   end
