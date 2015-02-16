0cac44ae0bed233b61e862476c189facccb8f744
Alissa Bonas <abonas@redhat.com>
2015-02-16 21:59:14 +0200

    add selectors persistence to kubernetes service

--- vmdb/app/models/container_service.rb
+++ vmdb/app/models/container_service.rb
@@ -6,4 +6,5 @@ class ContainerService < ActiveRecord::Base
   belongs_to  :ext_management_system, :foreign_key => "ems_id"
   has_many :container_groups
   has_many :labels, :class_name => CustomAttribute, :as => :resource, :conditions => {:section => "labels"}
+  has_many :selector_parts, :class_name => CustomAttribute, :as => :resource, :conditions => {:section => "selectors"}
 end
