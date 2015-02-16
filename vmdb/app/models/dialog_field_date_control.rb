401993c273baa2074c0e79e4a54e525e8795d54a
Erik Clarizio <eclarizi@redhat.com>
2015-02-16 08:31:37 -0800

    Add dynamic functionality for DialogFieldDateControl class
    https://trello.com/c/OdJj0Eme

--- vmdb/app/models/dialog_field_date_control.rb
+++ vmdb/app/models/dialog_field_date_control.rb
@@ -1,6 +1,11 @@
 class DialogFieldDateControl < DialogField
+  AUTOMATE_VALUE_FIELDS = %w(default_value show_past_dates)
 
   include TimezoneMixin
+
+  has_one :resource_action, :as => :resource, :dependent => :destroy
+
+  after_initialize :default_resource_action
 
   def show_past_dates
     self.options[:show_past_dates] || false
@@ -14,11 +19,39 @@ class DialogFieldDateControl < DialogField
     Date.parse(self.value).iso8601
   end
 
-  def default_time
-    with_current_user_timezone { Time.zone.now + 1.day }
+  def default_value
+    if dynamic
+      write_attribute(:default_value, values_from_automate)
+      read_attribute(:default_value)
+    else
+      default_time
+    end
   end
 
-  def default_value
-    default_time.strftime("%m/%d/%Y")
+  def normalize_automate_values(automate_hash)
+    self.class::AUTOMATE_VALUE_FIELDS.each do |key|
+      send("#{key}=", automate_hash[key]) if automate_hash.key?(key)
+    end
+
+    return default_time if automate_hash["default_value"].blank?
+    automate_hash["default_value"].to_s
+  end
+
+  def script_error_values
+    "<Script error>"
+  end
+
+  private
+
+  def default_resource_action
+    build_resource_action if resource_action.nil?
+  end
+
+  def default_time
+    with_current_user_timezone { Time.zone.now + 1.day }.strftime("%m/%d/%Y")
+  end
+
+  def values_from_automate
+    DynamicDialogFieldValueProcessor.values_from_automate(self)
   end
 end
--- vmdb/spec/models/dialog_field_date_control_spec.rb
+++ vmdb/spec/models/dialog_field_date_control_spec.rb
@@ -1,6 +1,99 @@
 require "spec_helper"
 
 describe DialogFieldDateControl do
+  describe "#default_value" do
+    let(:dialog_field) { described_class.new(:dynamic => dynamic) }
+
+    context "when the field is dynamic" do
+      let(:dynamic) { true }
+
+      before do
+        DynamicDialogFieldValueProcessor.stub(:values_from_automate).with(dialog_field).and_return("processor")
+      end
+
+      it "returns the values from the value processor" do
+        expect(dialog_field.default_value).to eq("processor")
+      end
+    end
+
+    context "when the field is not dynamic" do
+      let(:dynamic) { false }
+
+      before do
+        described_class.stub(:server_timezone).and_return("UTC")
+      end
+
+      it "returns tomorrow's date" do
+        Timecop.freeze(Time.new(2015, 1, 2)) do
+          expect(dialog_field.default_value).to eq("01/03/2015")
+        end
+      end
+    end
+  end
+
+  describe "#normalize_automate_values" do
+    let(:dialog_field) { described_class.new(:dynamic => true) }
+    let(:automate_hash) do
+      {
+        "default_value"   => default_value,
+        "show_past_dates" => true
+      }
+    end
+
+    shared_examples_for "DialogFieldDateControl#normalize_automate_values" do
+      before do
+        dialog_field.normalize_automate_values(automate_hash)
+      end
+
+      it "sets the show_past_dates" do
+        expect(dialog_field.show_past_dates).to be_true
+      end
+    end
+
+    context "when the automate hash has a default value" do
+      let(:default_value) { "01/02/2015" }
+
+      it_behaves_like "DialogFieldDateControl#normalize_automate_values"
+
+      it "sets the default value" do
+        dialog_field.normalize_automate_values(automate_hash)
+        expect(dialog_field.read_attribute(:default_value)).to eq("01/02/2015")
+      end
+
+      it "returns the default value" do
+        expect(dialog_field.normalize_automate_values(automate_hash)).to eq("01/02/2015")
+      end
+    end
+
+    context "when the automate hash does not have a default value" do
+      let(:default_value) { nil }
+
+      before do
+        described_class.stub(:server_timezone).and_return("UTC")
+      end
+
+      it_behaves_like "DialogFieldDateControl#normalize_automate_values"
+
+      it "sets the default value" do
+        dialog_field.normalize_automate_values(automate_hash)
+        expect(dialog_field.read_attribute(:default_value)).to eq(nil)
+      end
+
+      it "returns the initial values" do
+        Timecop.freeze(2015, 1, 2) do
+          expect(dialog_field.normalize_automate_values(automate_hash)).to eq("01/03/2015")
+        end
+      end
+    end
+  end
+
+  describe "#script_error_values" do
+    let(:dialog_field) { described_class.new }
+
+    it "returns a script error" do
+      expect(dialog_field.script_error_values).to eq("<Script error>")
+    end
+  end
 
   it "#automate_output_value" do
     subject.value = "08/07/2013"
