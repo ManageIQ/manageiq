358878fe629cafd21c11594326818d94c0754b60
Aaron Patterson <aaron.patterson@gmail.com>
2015-02-17 12:39:21 -0800

    rails 4 will rename indexes for us, so we may not need to rename

--- vmdb/db/migrate/20150202212058_fix_indexes_on_ems_events.rb
+++ vmdb/db/migrate/20150202212058_fix_indexes_on_ems_events.rb
@@ -1,11 +1,19 @@
 class FixIndexesOnEmsEvents < ActiveRecord::Migration
   def up
-    rename_index :ems_events, "index_ems_events_on_vm_id",      "index_ems_events_on_vm_or_template_id"
-    rename_index :ems_events, "index_ems_events_on_dest_vm_id", "index_ems_events_on_dest_vm_or_template_id"
+    if find_index_by_name(:ems_events, "index_ems_events_on_vm_id")
+      rename_index :ems_events, "index_ems_events_on_vm_id",      "index_ems_events_on_vm_or_template_id"
+    end
+    if find_index_by_name(:ems_events, "index_ems_events_on_dest_vm_id")
+      rename_index :ems_events, "index_ems_events_on_dest_vm_id", "index_ems_events_on_dest_vm_or_template_id"
+    end
   end
 
   def down
     rename_index :ems_events, "index_ems_events_on_vm_or_template_id",      "index_ems_events_on_vm_id"
     rename_index :ems_events, "index_ems_events_on_dest_vm_or_template_id", "index_ems_events_on_dest_vm_id"
+  end
+
+  def find_index_by_name(table, name)
+    indexes(table).find { |i| i.name == name }
   end
 end
