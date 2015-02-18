30813dc56ca87e732fdcbcc05285b4ac3b425b6f
Keenan Brock <kbrock@redhat.com>
2015-02-18 10:10:59 -0500

    ConfiguredSystemForeman#provider_object

--- vmdb/app/models/configured_system_foreman.rb
+++ vmdb/app/models/configured_system_foreman.rb
@@ -1,2 +1,13 @@
 class ConfiguredSystemForeman < ConfiguredSystem
+  include ProviderObjectMixin
+
+  def provider_object(connection = nil)
+    (connection || raw_connect).host(manager_ref)
+  end
+
+  private
+
+  def connection_source(options = {})
+    options[:connection_source] || configuration_manager
+  end
 end
