101ab13104ab777493bd450e9b57b0b9e726a675
Alexey Vasyukov <vasyukov@gmail.com>
2015-02-18 12:42:19 +0300

    Add moveIntoFolder and createVM methods into MiqVimFolder

--- lib/VMwareWebService/MiqVimFolder.rb
+++ lib/VMwareWebService/MiqVimFolder.rb
@@ -73,6 +73,37 @@ class MiqVimFolder
 	    waitForTask(taskMor)
 	end # def addStandaloneHost
 
+	# Places vCenterObject into current folder, removing it from other folders
+	def moveIntoFolder(vCenterObject)
+		oMor = nil
+		oMor = (vCenterObject.kind_of?(Hash) ? vCenterObject['MOR'] : vCenterObject) if vCenterObject
+		
+		$vim_log.info "MiqVimFolder(#{@invObj.server}, #{@invObj.username}).moveIntoFolder: calling moveIntoFolder_Task" if $vim_log
+		taskMor = @invObj.moveIntoFolder_Task(@fMor, oMor)
+		$vim_log.info "MiqVimFolder(#{@invObj.server}, #{@invObj.username}).moveIntoFolder: returned from moveIntoFolder_Task" if $vim_log
+		$vim_log.debug "MiqVimFolder::moveIntoFolder: taskMor = #{taskMor}" if $vim_log
+		waitForTask(taskMor)
+	end
+	
+	# Creates empty VM, that can be installed later from ISO or whatever
+	# 
+	# configSpec   - describes VM details, it should be filled in calling code since there are too many possible options
+	# pool         - VMware resource pool new VM will belong to
+	# host         - VMware host this VM should start on, it can be nil if pool is on DRS cluster or single-host cluster
+	#
+	# Returns MOR of new VM
+	def createVM(configSpec, pool, host=nil)
+		hMor = pMor = nil
+		hMor = (host.kind_of?(Hash) ? host['MOR'] : host) if host
+		pMor = (pool.kind_of?(Hash) ? pool['MOR'] : pool) if pool
+		
+		$vim_log.info "MiqVimFolder(#{@invObj.server}, #{@invObj.username}).createVM calling createVM_Task" if $vim_log
+		taskMor = @invObj.createVM_Task(@fMor, configSpec, pMor, hMor)
+		$vim_log.info "MiqVimFolder(#{@invObj.server}, #{@invObj.username}).createVM returned from createVM_Task" if $vim_log
+		$vim_log.debug "MiqVimFolder::createVM: taskMor = #{taskMor}" if $vim_log
+		waitForTask(taskMor)
+	end
+		
 	def createFolder(fname)
 		@invObj.createFolder(@fMor, fname)
 	end
