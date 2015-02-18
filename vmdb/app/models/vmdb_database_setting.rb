762290f7a77966f0b5fd3acbabbc824ad83de946
Aaron Patterson <aaron.patterson@gmail.com>
2015-02-18 10:14:51 -0800

    set the primary key on VmdbDatabaseSetting to nil
    there is no pk on this table, and when you call methods like `first` in
    rails 4, it will try to sort by the pk which doesn't exist in this case

--- vmdb/app/models/vmdb_database_setting.rb
+++ vmdb/app/models/vmdb_database_setting.rb
@@ -5,6 +5,7 @@ class VmdbDatabaseSetting < ActiveRecord::Base
   self.aar_columns = []
 
   self.table_name = 'pg_settings'
+  self.primary_key = nil
 
   virtual_belongs_to :vmdb_database
   virtual_column :description,      :type => :string
