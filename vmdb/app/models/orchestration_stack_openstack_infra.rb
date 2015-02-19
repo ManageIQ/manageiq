d10383393d8ca3636ec96ccc56ff2383c29ecf62
Ladislav Smola <lsmola@redhat.com>
2015-02-19 17:39:08 +0100

    Adding scaling action for OpenstackInfra stack
    Adding scaling action that uses Heat and Tuskar to call
    heat stack update for scaling Overcloud.
    
    Depends on: https://github.com/fog/fog/pull/3440

--- vmdb/app/models/orchestration_stack_openstack_infra.rb
+++ vmdb/app/models/orchestration_stack_openstack_infra.rb
@@ -1,2 +1,27 @@
 class OrchestrationStackOpenstackInfra < OrchestrationStack
+  def raw_update_stack(options)
+    planning_data = {}
+    ext_management_system.with_provider_connection(:service => "Planning") do |connection|
+      # Send new parameters to Tuskar and get updated plan
+      connection.plans.find_by_name(name).patch(options)
+      plan = connection.plans.find_by_name(name)
+
+      # Get all parameters needed for heat stack-update from Tuskar
+      planning_data = {
+        :stack_name       => name,
+        :template         => plan.master_template,
+        :environment      => plan.environment,
+        :files            => plan.provider_resource_templates,
+        :timeout_mins     => 60,
+        :disable_rollback => true
+      }
+    end
+
+    ext_management_system.with_provider_connection(:service => "Orchestration") do |connection|
+      # Update stack with updated planning data
+      stack = connection.stacks.get(name, ems_ref)
+      # Update stack with updated planning data
+      connection.update_stack(stack, planning_data)
+    end
+  end
 end
