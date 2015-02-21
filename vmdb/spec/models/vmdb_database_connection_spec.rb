d12323d047e82abb33fd6eaff9bd11b1c8daa069
Aaron Patterson <aaron.patterson@gmail.com>
2015-02-20 18:16:27 -0800

    add specs for VmdbDatabaseConnection

---
+++ vmdb/spec/models/vmdb_database_connection_spec.rb
@@ -0,0 +1,69 @@
+require "spec_helper"
+
+describe VmdbDatabaseConnection do
+  before :each do
+    @db = FactoryGirl.create(:vmdb_database)
+  end
+
+  it 'can find connections' do
+    connections = VmdbDatabaseConnection.all
+    expect(connections.length).to be > 0
+  end
+
+  it 'is blocked' do
+    locked_latch = ActiveSupport::Concurrency::Latch.new
+    continue_latch = ActiveSupport::Concurrency::Latch.new
+
+    get_lock = Thread.new do
+      VmdbDatabaseConnection.connection.transaction do
+        VmdbDatabaseConnection.connection.execute('LOCK users IN EXCLUSIVE MODE')
+        locked_latch.release
+        continue_latch.await
+      end
+    end
+
+    wait_for_lock = Thread.new do
+      VmdbDatabaseConnection.connection.transaction do
+        locked_latch.await # wait until `get_lock` has the lock
+        VmdbDatabaseConnection.connection.execute('LOCK users IN EXCLUSIVE MODE')
+      end
+    end
+
+    locked_latch.await # wait until `get_lock` has the lock
+    # spin until `wait_for_lock` is waiting to acquire the lock
+    loop { break if wait_for_lock.status == "sleep" }
+
+    connections = VmdbDatabaseConnection.all
+    continue_latch.release
+    get_lock.join
+    wait_for_lock.join
+
+    blocked_conn = connections.find { |conn| conn.blocked_by }
+    expect(blocked_conn).to be
+    blocked_by = connections.find { |conn| conn.spid == blocked_conn.blocked_by }
+    expect(blocked_by).to be
+    expect(blocked_conn.spid).not_to eq(blocked_by.spid)
+  end
+
+  [
+    :address,
+    :application,
+    :blocked_by,
+    :command,
+    :spid,
+    :task_state,
+    :wait_resource,
+    :wait_time,
+    :vmdb_database_id,
+    :vmdb_database,
+    :zone,
+    :miq_server,
+    :miq_worker,
+    :pid,
+  ].each do |field|
+    it "has a #{field}" do
+      setting = VmdbDatabaseConnection.all.first
+      expect(setting).to respond_to(field)
+    end
+  end
+end
