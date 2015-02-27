9830d69ea56e0931e7ee7a95c1b86942d4661849
Keenan Brock <kbrock@redhat.com>
2015-02-27 13:05:21 -0500

    foreman provisioning state machine

--- vmdb/app/models/configured_system_foreman.rb
+++ vmdb/app/models/configured_system_foreman.rb
@@ -5,6 +5,11 @@ class ConfiguredSystemForeman < ConfiguredSystem
     (connection || connection_source.raw_connect).host(manager_ref)
   end
 
+  # system is pending a build
+  def pending?
+    source.build_state == "pending"
+  end
+
   private
 
   def connection_source(options = {})
