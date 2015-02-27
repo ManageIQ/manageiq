474a36f9a7033c4a8c34424e2fbdd505db5c4f1e
Bill Wei <bilwei@redhat.com>
2015-02-27 11:37:02 -0500

    Add miq ae model for ServiceOrchestration

--- vmdb/lib/miq_automation_engine/service_models/miq_ae_service_service_template_orchestration.rb
+++ vmdb/lib/miq_automation_engine/service_models/miq_ae_service_service_template_orchestration.rb
@@ -1,8 +1,4 @@
 module MiqAeMethodService
   class MiqAeServiceServiceTemplateOrchestration < MiqAeServiceServiceTemplate
-    expose :deploy_orchestration_stack
-    expose :orchestration_stack_status
-    expose :convert_dialog_options
-    expose :stack_name
   end
 end
