e2a9a6dd6b699ba02d851f8bbbe56d1c0d7a7269
Martin Povolny <mpovolny@redhat.com>
2015-03-13 14:37:40 +0100

    Console: support for OpenStack VNC consoles.

--- vmdb/app/controllers/vm_common.rb
+++ vmdb/app/controllers/vm_common.rb
@@ -123,6 +123,16 @@ module VmCommon
     password, host_address, host_port, _proxy_address, _proxy_port, protocol, ssl = @sb[:html5]
     encrypt = get_vmdb_config.fetch_path(:server, :websocket_encrypt)
 
+    case protocol
+    when 'spice'     # spice, vnc - from rhevm
+      view = "vm_common/console_spice"
+    when nil, 'vnc'  # nil - from vmware
+      view = "vm_common/console_vnc"
+    when 'novnc_url' # from OpenStack
+      redirect_to host_address
+      return
+    end
+
     proxy_options = WsProxy.start(
       :host       => host_address,
       :host_port  => host_port,
@@ -131,13 +141,6 @@ module VmCommon
       :encrypt    => encrypt    # ssl on web client side
     )
     raise _("Console access failed: proxy errror") if proxy_options.nil?
-
-    view = case protocol   # spice, vnc - from rhevm
-           when 'spice'
-             "vm_common/console_spice"
-           when nil, 'vnc' # nil - from vmware
-             "vm_common/console_vnc"
-           end
 
     render :template => view,
            :layout   => false,
@@ -1444,7 +1447,10 @@ module VmCommon
         page.replace(:flash_msg_div, :partial => "layouts/flash_msg")
         page << "miqSparkle(false);"
       else # open a window to show a VNC or VMWare console
-        console_action = console_type == 'html5' ? 'launch_html5_console' : 'launch_vmware_console'
+        console_action = case console_type
+                         when 'html5' then 'launch_html5_console'
+                         else 'launch_vmware_console'
+                         end
         page << "miqSparkle(false);"
         page << "window.open('#{url_for :controller => controller_name, :action => console_action, :id => @record.id}');"
       end
