03fa483b8c34128469b3c5097c3a0d13bc58a389
Bill Wei <bilwei@redhat.com>
2015-03-18 17:57:43 -0400

    Add support for Heat orchestration provisionining

--- vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Provisioning/StateMachines/Methods.class/__methods__/provision.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Provisioning/StateMachines/Methods.class/__methods__/provision.rb
@@ -7,8 +7,10 @@ $evm.log("info", "Starting Orchestration Provisioning")
 service = $evm.root["service_template_provision_task"].destination
 
 begin
-  service.deploy_orchestration_stack
+  ems_ref = service.deploy_orchestration_stack
+  $evm.log("info", "Stack #{service.stack_name} with reference id (#{ems_ref}) is being created")
 rescue => err
   $evm.root['ae_result'] = 'error'
   $evm.root['ae_reason'] = err.message
+  $evm.log("error", "Stack #{service.stack_name} creation failed. Reason: #{err.message}")
 end
