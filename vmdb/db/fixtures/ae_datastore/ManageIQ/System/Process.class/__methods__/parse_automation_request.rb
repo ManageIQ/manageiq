7837ff0b3512f1bf63616dabc264a3725cc4f76d
Greg McCullough <gmccullo@redhat.com>
2015-03-18 09:34:07 -0400

    Automate models for Foreman

--- vmdb/db/fixtures/ae_datastore/ManageIQ/System/Process.class/__methods__/parse_automation_request.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/System/Process.class/__methods__/parse_automation_request.rb
@@ -9,6 +9,9 @@ cur['target_component'], cur['target_class'], cur['target_instance'] =
   when 'vm_retired'     then %w(VM   Lifecycle Retirement)
   when 'vm_migrate'     then %w(VM   Lifecycle Migrate)
   when 'host_provision' then %w(Host Lifecycle Provisioning)
+  when 'configured_system_provision'
+    $evm.root['ae_provider_category'] = 'infrastructure'
+    %w(Configured_System Lifecycle Provisioning)
   end
 
 $evm.log("info", "Request:<#{cur['request']}> Target Component:<#{cur['target_component']}> Target Class:<#{cur['target_class']}> Target Instance:<#{cur['target_instance']}>")
