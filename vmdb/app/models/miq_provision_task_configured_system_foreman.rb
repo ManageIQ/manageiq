7ef0f91baad2438db4d34d5a2607fb6ae3f35094
Brandon Dunne <bdunne@redhat.com>
2015-03-20 16:30:27 -0400

    Create an options helper for working with the options hashes

--- vmdb/app/models/miq_provision_task_configured_system_foreman.rb
+++ vmdb/app/models/miq_provision_task_configured_system_foreman.rb
@@ -1,4 +1,9 @@
 class MiqProvisionTaskConfiguredSystemForeman < MiqProvisionTask
   include_concern 'OperationsHelper'
+  include_concern 'OptionsHelper'
   include_concern 'StateMachine'
+
+  def model_class
+    ConfiguredSystemForeman
+  end
 end
