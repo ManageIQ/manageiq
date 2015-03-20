366614168f91c89f88680c98d1a317ab89b46f5f
Alberto Bellotti <abellott@redhat.com>
2015-03-20 13:49:07 -0400

    [api] rspec refactoring - spec/requests/api/versioning_spec.rb
    - Refactored spec/requests/api/versioning_spec.rb
    
    https://trello.com/c/p76x7lcu

--- vmdb/spec/requests/api/versioning_spec.rb
+++ vmdb/spec/requests/api/versioning_spec.rb
@@ -16,41 +16,48 @@ describe ApiController do
   end
 
   context "Versioning Queries" do
-
     it "test versioning query" do
-      basic_authorize @cfme[:user], @cfme[:password]
-      @success = run_get @cfme[:entrypoint]
-      expect(@success).to be_true
-      expect(@code).to eq(200)
-      expect(@result).to have_key("versions")
+      api_basic_authorize
+
+      run_get entrypoint_url
+
+      expect_single_resource_query
+      expect_result_to_have_keys(%w(name description version versions collections))
     end
 
     it "test query with a valid version" do
+      api_basic_authorize
+
       # Let's get the versions
-      basic_authorize @cfme[:user], @cfme[:password]
-      @success = run_get @cfme[:entrypoint]
-      expect(@success).to be_true
-      expect(@code).to eq(200)
-      expect(@result).to have_key("versions")
-      expect(@result["versions"]).to_not be_nil
-      # Let's get the first version
-      expect(@result["versions"][0]).to_not be_nil
-      ver = @result["versions"][0]
+      run_get entrypoint_url
+
+      expect_single_resource_query
+      expect_result_to_have_keys(%w(versions))
+
+      versions = @result["versions"]
+
+      # Let's get the first version identifier
+      expect(versions).to_not be_nil
+      expect(versions[0]).to_not be_nil
+
+      ver = versions[0]
       expect(ver).to have_key("href")
+
       ident = ver["href"].split("/").last
+
       # Let's try to access that version API URL
-      @success = run_get "#{@cfme[:entrypoint]}/#{ident}"
-      expect(@success).to be_true
-      expect(@code).to eq(200)
+      run_get "#{entrypoint_url}/#{ident}"
+
+      expect_single_resource_query
+      expect_result_to_have_keys(%w(name description version versions collections))
     end
 
     it "test query with an invalid version" do
-      basic_authorize @cfme[:user], @cfme[:password]
-      @success = run_get "#{@cfme[:entrypoint]}/v9999.9999"
-      expect(@success).to be_false
-      expect(@code).to eq(400)
+      api_basic_authorize
+
+      run_get "#{entrypoint_url}/v9999.9999"
+
+      expect_bad_request
     end
-
   end
-
 end
