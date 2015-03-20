ec97b4bf6af7afa14c962a88d75a50df50cf1404
Aaron Patterson <aaron.patterson@gmail.com>
2015-03-20 16:30:35 -0700

    don't set @_params ivar
    it is internal to Rails

--- vmdb/spec/controllers/cloud_tenant_controller_spec.rb
+++ vmdb/spec/controllers/cloud_tenant_controller_spec.rb
@@ -2,17 +2,21 @@ require "spec_helper"
 
 describe CloudTenantController do
   context "#button" do
+    before(:each) do
+      set_user_privileges
+      FactoryGirl.create(:vmdb_database)
+      EvmSpecHelper.create_guid_miq_server_zone
+    end
+
     it "when Instance Retire button is pressed" do
-      controller.instance_variable_set(:@_params, :pressed => "instance_retire")
       controller.should_receive(:retirevms).once
-      controller.button
+      post :button, :pressed => "instance_retire", :format => :js
       controller.send(:flash_errors?).should_not be_true
     end
 
     it "when Instance Tag is pressed" do
-      controller.instance_variable_set(:@_params, :pressed => "instance_tag")
       controller.should_receive(:tag).with(VmOrTemplate)
-      controller.button
+      post :button, :pressed => "instance_tag", :format => :js
       controller.send(:flash_errors?).should_not be_true
     end
   end
