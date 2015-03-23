a7be289ad98ccc1edec12ad10a23d8ea34e888c5
Keenan Brock <kbrock@redhat.com>
2015-03-23 19:09:22 -0400

    reduce forman id_lookup to 2 parameters makes more similar to id_lookups this makes it easier to default when a value is not found

--- vmdb/app/models/ems_refresh/parsers/foreman.rb
+++ vmdb/app/models/ems_refresh/parsers/foreman.rb
@@ -91,9 +91,9 @@ module EmsRefresh
             :manager_ref                    => profile["id"].to_s,
             :name                           => profile["name"],
             :description                    => profile["title"],
-            :operating_system_flavor_id     => id_lookup(indexes[:flavors], profile, "operatingsystem_id"),
-            :customization_script_medium_id => id_lookup(indexes[:media], profile, "medium_id"),
-            :customization_script_ptable_id => id_lookup(indexes[:ptables], profile, "ptable_id"),
+            :operating_system_flavor_id     => id_lookup(indexes[:flavors], profile["operatingsystem_id"]),
+            :customization_script_medium_id => id_lookup(indexes[:media], profile["medium_id"]),
+            :customization_script_ptable_id => id_lookup(indexes[:ptables], profile["ptable_id"]),
             :configuration_location_ids     => ids_lookup(indexes[:locations], profile["locations"]),
             :configuration_organization_ids => ids_lookup(indexes[:organizations], profile["organizations"]),
           }
@@ -106,16 +106,16 @@ module EmsRefresh
             :type                           => "ConfiguredSystemForeman",
             :manager_ref                    => cs["id"].to_s,
             :hostname                       => cs["name"],
-            :configuration_profile          => id_lookup(indexes[:profiles], cs, "hostgroup_id"),
-            :operating_system_flavor_id     => id_lookup(indexes[:flavors], cs, "operatingsystem_id"),
-            :customization_script_medium_id => id_lookup(indexes[:media], cs, "medium_id"),
-            :customization_script_ptable_id => id_lookup(indexes[:ptables], cs, "ptable_id"),
+            :configuration_profile          => id_lookup(indexes[:profiles], cs["hostgroup_id"]),
+            :operating_system_flavor_id     => id_lookup(indexes[:flavors], cs["operatingsystem_id"]),
+            :customization_script_medium_id => id_lookup(indexes[:media], cs["medium_id"]),
+            :customization_script_ptable_id => id_lookup(indexes[:ptables], cs["ptable_id"]),
             :last_checkin                   => cs["last_compile"],
             :build_state                    => cs["build"] ? "pending" : nil,
             :ipaddress                      => cs["ip"],
             :mac_address                    => cs["mac"],
-            :configuration_location_id      => id_lookup(indexes[:locations], cs, "location_id"),
-            :configuration_organization_id  => id_lookup(indexes[:organizations], cs, "organization_id"),
+            :configuration_location_id      => id_lookup(indexes[:locations], cs["location_id"]),
+            :configuration_organization_id  => id_lookup(indexes[:organizations], cs["organization_id"]),
           }
         end
       end
@@ -127,8 +127,7 @@ module EmsRefresh
         target
       end
 
-      def id_lookup(ids, record, id_key)
-        key = record[id_key]
+      def id_lookup(ids, key)
         return unless key
         ids[key.to_s].tap do |v|
           @needs_provisioning_refresh = true unless v
@@ -136,7 +135,7 @@ module EmsRefresh
       end
 
       def ids_lookup(ids, records, id_key = "id")
-        (records || []).collect { |record| id_lookup(ids, record, id_key) }.compact
+        (records || []).collect { |record| id_lookup(ids, record[id_key]) }.compact
       end
 
       def basic_hash(collection, type, extra_field = nil)
