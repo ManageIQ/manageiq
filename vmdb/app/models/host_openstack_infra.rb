1deaca60106d833fa14c7507460c8b90825ebacc
Ladislav Smola <lsmola@redhat.com>
2015-03-26 17:21:24 +0100

    Allowing host to connect to ssh using keypair
    Allowing host to connect to ssh using keypair. Only allowed
    for openstack infra, where the keypair is stored on Provider
    level.

--- vmdb/app/models/host_openstack_infra.rb
+++ vmdb/app/models/host_openstack_infra.rb
@@ -1,2 +1,20 @@
 class HostOpenstackInfra < Host
+  def ssh_users_and_passwords
+    # HostOpenstackInfra is using auth key set on ext_management_system level, not individual hosts
+    rl_user, auth_key = self.auth_user_keypair(:ssh_keypair)
+    rl_password = nil
+
+    # TODO(lsmola) make sudo user work. So it with be optional sudo password for private key auth, also test
+    # password-less sudo
+    su_user, su_password = nil, nil
+
+    return rl_user, rl_password, su_user, su_password, {:key_data => auth_key}
+  end
+
+  def auth_user_keypair(type = nil)
+    # HostOpenstackInfra is using auth key set on ext_management_system level, not individual hosts
+    cred = self.try(:ext_management_system).try(:authentication_best_fit, type)
+    return nil if cred.nil? || cred.userid.blank?
+    [cred.userid, cred.auth_key]
+  end
 end
