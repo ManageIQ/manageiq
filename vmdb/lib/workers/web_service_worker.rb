180afbe2bd98bc61d0976aedea9049ec09037ca4
Joe Rafaniello <jrafanie@redhat.com>
2015-03-27 15:43:15 -0400

    Add at_exit for thin based workers since thin traps interrupts.
    Because thin traps interrupts, SignalException/Interrupt exceptions are not raised
    to the UI and Web Service workers, causing them to exit without logging and updating
    it's worker status.  The latter causes the server to not restart the worker as quickly.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1205407

--- vmdb/lib/workers/web_service_worker.rb
+++ vmdb/lib/workers/web_service_worker.rb
@@ -3,15 +3,13 @@ require 'workers/worker_base'
 class WebServiceWorker < WorkerBase
   self.wait_for_worker_monitor = false
 
-  # Do NOT process the signal that we use to terminate Mongrel
-  def self.interrupt_signals
-    INTERRUPT_SIGNALS - ['TERM']
-  end
-
   def do_work
   end
 
   def do_before_work_loop
     @worker.release_db_connection
+
+    # Since thin traps interrupts, log that we're going away and update our worker row
+    at_exit { do_exit("Exit request received.") }
   end
 end
