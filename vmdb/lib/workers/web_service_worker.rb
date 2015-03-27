1d5ecf9154c57e6900a013bd8911459cb602573f
Joe Rafaniello <jrafanie@redhat.com>
2015-03-27 16:01:17 -0400

    Extract all of the commmon WS/UI worker script code to a mixin.
    https://bugzilla.redhat.com/show_bug.cgi?id=1205407

--- vmdb/lib/workers/web_service_worker.rb
+++ vmdb/lib/workers/web_service_worker.rb
@@ -1,15 +1,6 @@
 require 'workers/worker_base'
+require 'workers/mixins/web_server_worker_mixin'
 
 class WebServiceWorker < WorkerBase
-  self.wait_for_worker_monitor = false
-
-  def do_work
-  end
-
-  def do_before_work_loop
-    @worker.release_db_connection
-
-    # Since thin traps interrupts, log that we're going away and update our worker row
-    at_exit { do_exit("Exit request received.") }
-  end
+  include WebServerWorkerMixin
 end
