ee026f5ccf01f25dba907fdfeb6f31ee777d9529
Brandon Dunne <bdunne@redhat.com>
2015-03-27 15:47:26 -0400

    Show more detail about ConfiguredSystems to be provisioned

--- vmdb/app/models/configured_system_foreman.rb
+++ vmdb/app/models/configured_system_foreman.rb
@@ -6,6 +6,9 @@ class ConfiguredSystemForeman < ConfiguredSystem
   belongs_to :customization_script_medium
   belongs_to :customization_script_ptable
 
+  delegate :name, :to => :configuration_location,     :prefix => true, :allow_nil => true
+  delegate :name, :to => :configuration_organization, :prefix => true, :allow_nil => true
+
   def provider_object(connection = nil)
     (connection || connection_source.connect).host(manager_ref)
   end
