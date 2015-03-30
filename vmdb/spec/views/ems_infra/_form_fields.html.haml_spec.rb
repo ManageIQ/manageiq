6e7e93154759eed493019711f05b00f7ee92f38b
Jozef Zigmund <jzigmund@redhat.com>
2015-03-30 20:02:09 +0200

    Rspec/Views: Added examples to test displaying only Host Name

---
+++ vmdb/spec/views/ems_infra/_form_fields.html.haml_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe "rendering fields in ems_infra new/edit form" do
+  before(:each) do
+    @edit = {:new => {:emstype => "vm_ware"}}
+  end
+
+  it "displays Host Name" do
+    render :partial => "ems_infra/form_fields", :locals => {:url => ""}
+    expect(rendered).to match(/Host\ Name/)
+  end
+
+  it "doesn't display IP Address" do
+    render :partial => "ems_infra/form_fields", :locals => {:url => ""}
+    expect(rendered).not_to match(/\AIP\ Address/)
+  end
+end
