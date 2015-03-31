615176edbe2a00d18d482bd8c8a8b28365d6e1a4
Bill Wei <bilwei@redhat.com>
2015-03-31 11:43:41 -0400

    Use existing fixture to test template dialog generation

--- vmdb/spec/factories/orchestration_template.rb
+++ vmdb/spec/factories/orchestration_template.rb
@@ -17,15 +17,13 @@ FactoryGirl.define do
     stacks { [FactoryGirl.create(:orchestration_stack)] }
   end
 
-  factory :orchestration_template_with_content, :parent => :orchestration_template do
-    type 'OrchestrationTemplateCfn'
-    content '{'\
-            '"Description" : "Description",'\
-            '"Parameters" : {'\
-            '    "KeyName" : {'\
-            '      "Description" : "Description",'\
-            '      "Type" : "String",'\
-            '      "Default": "Description"'\
-            '}}}'
+  factory :orchestration_template_cfn_with_content, :parent => :orchestration_template_cfn do
+    content File.read('spec/fixtures/orchestration_templates/cfn_parameters.json')
+  end
+
+  factory :orchestration_template_hot_with_content,
+          :parent => :orchestration_template,
+          :class  => OrchestrationTemplateHot do
+    content File.read('spec/fixtures/orchestration_templates/hot_parameters.yml')
   end
 end
