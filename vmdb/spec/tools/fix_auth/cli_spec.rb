ec7485d40e1895f275d8ad6466994d92080d954a
Keenan Brock <kbrock@redhat.com>
2015-03-31 10:06:29 -0400

    fixauth pr feedback to make specs more clear
    - describe block to make cli more clear
    - got rid of "respect" in fix_auth_spec

--- vmdb/spec/tools/fix_auth/cli_spec.rb
+++ vmdb/spec/tools/fix_auth/cli_spec.rb
@@ -5,62 +5,64 @@ $LOAD_PATH << Rails.root.join("tools")
 require "fix_auth/cli"
 
 describe FixAuth::Cli do
-  it "should assign defaults" do
-    options = described_class.new.parse([], {})
-              .options.slice(:hostname, :username, :password, :hardcode, :databases)
-    expect(options).to eq(
-      :username  => "root",
-      :databases => %w(vmdb_production))
-  end
+  describe "#parse" do
+    it "should assign defaults" do
+      opts = described_class.new.parse([], {})
+             .options.slice(:hostname, :username, :password, :hardcode, :databases)
+      expect(opts).to eq(
+        :username  => "root",
+        :databases => %w(vmdb_production))
+    end
 
-  it "should pickup env variables" do
-    options = described_class.new.parse([], "PGUSER" => "envuser", "PGPASSWORD" => "envpass", "PGHOST" => "envhost")
-              .options.slice(:hostname, :username, :password, :hardcode, :databases)
-    expect(options).to eq(
-      :username  => "envuser",
-      :databases => %w(vmdb_production),
-      :password  => "envpass",
-      :hostname  => "envhost")
-  end
+    it "should pickup env variables" do
+      opts = described_class.new.parse([], "PGUSER" => "envuser", "PGPASSWORD" => "envpass", "PGHOST" => "envhost")
+             .options.slice(:hostname, :username, :password, :hardcode, :databases)
+      expect(opts).to eq(
+        :username  => "envuser",
+        :databases => %w(vmdb_production),
+        :password  => "envpass",
+        :hostname  => "envhost")
+    end
 
-  it "should parse database names" do
-    options = described_class.new.parse(%w(DB1 DB2))
-              .options.slice(:hostname, :username, :password, :hardcode, :databases)
-    expect(options).to eq(
-      :username  => "root",
-      :databases => %w(DB1 DB2))
-  end
+    it "should parse database names" do
+      opts = described_class.new.parse(%w(DB1 DB2))
+             .options.slice(:hostname, :username, :password, :hardcode, :databases)
+      expect(opts).to eq(
+        :username  => "root",
+        :databases => %w(DB1 DB2))
+    end
 
-  it "should parse hardcoded password" do
-    options = described_class.new.parse(%w(-P hardcoded))
-              .options.slice(:hostname, :username, :password, :hardcode, :databases)
-    expect(options).to eq(
-      :username  => "root",
-      :databases => %w(vmdb_production),
-      :hardcode  => "hardcoded")
-  end
+    it "should parse hardcoded password" do
+      opts = described_class.new.parse(%w(-P hardcoded))
+             .options.slice(:hostname, :username, :password, :hardcode, :databases)
+      expect(opts).to eq(
+        :username  => "root",
+        :databases => %w(vmdb_production),
+        :hardcode  => "hardcoded")
+    end
 
-  it "defaults to updating the database" do
-    options = described_class.new.parse(%w())
-              .options.slice(:db, :databaseyml, :key)
-    expect(options).to eq(:db => true)
-  end
+    it "defaults to updating the database" do
+      opts = described_class.new.parse(%w())
+             .options.slice(:db, :databaseyml, :key)
+      expect(opts).to eq(:db => true)
+    end
 
-  it "doesnt default to database if running another task" do
-    options = described_class.new.parse(%w(--databaseyml))
-              .options.slice(:db, :databaseyml, :key)
-    expect(options).to eq(:databaseyml => true)
-  end
+    it "doesnt default to database if running another task" do
+      opts = described_class.new.parse(%w(--databaseyml))
+             .options.slice(:db, :databaseyml, :key)
+      expect(opts).to eq(:databaseyml => true)
+    end
 
-  it "doesnt default to database if running another task 2" do
-    options = described_class.new.parse(%w(--key))
-              .options.slice(:db, :databaseyml, :key)
-    expect(options).to eq(:key => true)
-  end
+    it "doesnt default to database if running another task 2" do
+      opts = described_class.new.parse(%w(--key))
+             .options.slice(:db, :databaseyml, :key)
+      expect(opts).to eq(:key => true)
+    end
 
-  it "can run all 3 tasks" do
-    options = described_class.new.parse(%w(--key --db --databaseyml))
-              .options.slice(:db, :databaseyml, :key)
-    expect(options).to eq(:db => true, :databaseyml => true, :key => true)
+    it "can run all 3 tasks" do
+      opts = described_class.new.parse(%w(--key --db --databaseyml))
+             .options.slice(:db, :databaseyml, :key)
+      expect(opts).to eq(:db => true, :databaseyml => true, :key => true)
+    end
   end
 end
--- vmdb/spec/tools/fix_auth/fix_auth_spec.rb
+++ vmdb/spec/tools/fix_auth/fix_auth_spec.rb
@@ -7,13 +7,13 @@ require "fix_auth/models"
 
 describe FixAuth::FixAuth do
   describe "#fix_database_yml" do
-    it "respects --hardcode" do
+    it "supports --hardcode" do
       subject = described_class.new(:password => 'newpass', :root => '/', :databaseyml => true)
       expect(FixAuth::FixDatabaseYml).to receive(:run).with(:hardcode => 'newpass')
       subject.fix_database_yml
     end
 
-    it "respects --password" do
+    it "supports --password as an alias of --hardcode" do
       subject = described_class.new(:hardcode => 'newpass', :root => '/', :databaseyml => true)
       expect(FixAuth::FixDatabaseYml).to receive(:run).with(:hardcode => 'newpass')
       subject.fix_database_yml
