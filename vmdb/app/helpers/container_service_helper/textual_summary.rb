f6eb71f946e7107ded068701bc445e3837014b9e
Erin A Boyd <eboyd@redhat.com>
2015-03-31 13:45:59 -0600

    Update textual_summary.rb
    fixed copy/paste error

--- vmdb/app/helpers/container_service_helper/textual_summary.rb
+++ vmdb/app/helpers/container_service_helper/textual_summary.rb
@@ -4,7 +4,7 @@ module ContainerServiceHelper::TextualSummary
   #
 
   def textual_group_properties
-    itemsapp/helpers/vm_infra_helper/textual_summary.rb = %w(namespace name port creation_timestamp resource_version session_affinity portal_ip protocol container_port)
+    item = %w(namespace name port creation_timestamp resource_version session_affinity portal_ip protocol container_port)
     items.collect { |m| send("textual_#{m}") }.flatten.compact
   end
 
