154de3f79447d7fd6ff890c8e6f0fa363f8488d3
Erik Clarizio <eclarizi@redhat.com>
2015-03-31 00:52:05 -0700

    Serialize ResourceAction when exporting a DialogField
    https://bugzilla.redhat.com/show_bug.cgi?id=1204125

--- vmdb/spec/models/dialog_field_serializer_spec.rb
+++ vmdb/spec/models/dialog_field_serializer_spec.rb
@@ -1,42 +1,50 @@
 require "spec_helper"
 
 describe DialogFieldSerializer do
-  let(:dialog_field_serializer) { described_class.new }
-  let(:dialog_field) { DialogField.new(expected_serialized_values) }
-
-  let(:expected_serialized_values) do
-    {
-      "name"                    => "name",
-      "description"             => "description",
-      "type"                    => nil,
-      "data_type"               => "data_type",
-      "notes"                   => "notes",
-      "notes_display"           => "notes display",
-      "display"                 => "display",
-      "display_method"          => "display method",
-      "display_method_options"  => {"display method options" => true},
-      "dynamic"                 => false,
-      "required"                => false,
-      "required_method"         => "required method",
-      "required_method_options" => {"required method options" => true},
-      "show_refresh_button"     => false,
-      "default_value"           => "default value",
-      "values"                  => "values",
-      "values_method"           => "values method",
-      "values_method_options"   => {"values method options" => true},
-      "options"                 => {"options" => true},
-      "label"                   => "label",
-      "load_values_on_init"     => false,
-      "position"                => 1,
-      "validator_type"          => "validator_type",
-      "validator_rule"          => "validator_rule",
-      "reconfigurable"          => nil
-    }
-  end
+  let(:resource_action_serializer) { instance_double("ResourceActionSerializer") }
+  let(:dialog_field_serializer) { described_class.new(resource_action_serializer) }
 
   describe "#serialize" do
+    let(:dialog_field) { DialogFieldTextBox.new(expected_serialized_values.merge(:resource_action => resource_action)) }
+    let(:resource_action) { ResourceAction.new }
+    let(:expected_serialized_values) do
+      {
+        "name"                    => "name",
+        "description"             => "description",
+        "type"                    => "DialogFieldTextBox",
+        "data_type"               => "data_type",
+        "notes"                   => "notes",
+        "notes_display"           => "notes display",
+        "display"                 => "display",
+        "display_method"          => "display method",
+        "display_method_options"  => {"display method options" => true},
+        "dynamic"                 => false,
+        "required"                => false,
+        "required_method"         => "required method",
+        "required_method_options" => {"required method options" => true},
+        "show_refresh_button"     => false,
+        "default_value"           => "default value",
+        "values"                  => "values",
+        "values_method"           => "values method",
+        "values_method_options"   => {"values method options" => true},
+        "options"                 => {"options" => true},
+        "label"                   => "label",
+        "load_values_on_init"     => false,
+        "position"                => 1,
+        "validator_type"          => "validator_type",
+        "validator_rule"          => "validator_rule",
+        "reconfigurable"          => nil
+      }
+    end
+
+    before do
+      resource_action_serializer.stub(:serialize).with(resource_action).and_return("serialized resource action")
+    end
+
     it "serializes the dialog_field" do
-      dialog_field_serializer.serialize(dialog_field).should == expected_serialized_values
+      dialog_field_serializer.serialize(dialog_field).should == expected_serialized_values.merge(
+        "resource_action" => "serialized resource action"
+      )
     end
   end
 end
