dd707b8cb0f6569129ea3f96fd3b312470524a72
Martin Povolny <mpovolny@redhat.com>
2015-04-01 19:40:40 +0200

    Menu::Manager: implement some tests.

---
+++ vmdb/spec/presenters/menu/menu_spec_helper.rb
@@ -0,0 +1,37 @@
+module MenuSpecHelper
+  SECTION_DEF= <<EOF
+type: section
+name: Red Hat
+id: redhat
+before: svc
+section_type: big_iframe
+href: http://www.redhat.com
+EOF
+
+  ITEM_DEF= <<EOF
+type: item
+name: courses
+id: rht2
+feature: redhat_forum
+rbac:
+  feature: redhat_forum
+parent: redhat
+href: http://www.redhat.com/en/services/training/all-courses-exams
+item_type: big_iframe
+EOF
+
+  def create_temp_file(content)
+    temp_file  = Tempfile.new('snafu')
+    temp_file << content
+    temp_file.close
+    temp_file
+  end
+
+  def section_file
+    create_temp_file(SECTION_DEF)
+  end
+
+  def item_file
+    create_temp_file(ITEM_DEF)
+  end
+end
