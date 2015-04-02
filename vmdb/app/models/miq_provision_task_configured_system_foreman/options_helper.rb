76c0f77deff1a1bd60ab4a6a081dea0ab09e2a27
Brandon Dunne <bdunne@redhat.com>
2015-04-02 16:38:51 -0400

    Add other keys from dialog to update on the configured system

--- vmdb/app/models/miq_provision_task_configured_system_foreman/options_helper.rb
+++ vmdb/app/models/miq_provision_task_configured_system_foreman/options_helper.rb
@@ -11,9 +11,11 @@ module MiqProvisionTaskConfiguredSystemForeman::OptionsHelper
   end
 
   def prepare_provider_options
-    phase_context[:provider_options] = {
-      "hostgroup_id" => dest_configuration_profile.manager_ref,
-    }
+    h = {"hostgroup_id" => dest_configuration_profile.manager_ref}
+    h["name"]      = options[:hostname]                           if options[:hostname]
+    h["ip"]        = options[:ip_addr]                            if options[:ip_addr]
+    h["root_pass"] = MiqPassword.decrypt(options[:root_password]) if options[:root_password]
+    phase_context[:provider_options] = h
     dumpObj(phase_context[:provider_options], "MIQ(#{self.class.name}##{__method__}) Default Provider Options: ", $log, :info)
   end
 
