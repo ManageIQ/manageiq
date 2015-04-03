dc644b121dc134846383cbba23126cc4a4af0c74
Ladislav Smola <lsmola@redhat.com>
2015-04-03 14:12:37 +0200

    Fix tests for refresh using ssh fleecing
    Fix tests for refresh using ssh fleecing

--- vmdb/spec/models/ems_refresh/refreshers/openstack_infra_refresher_rhos_juno_spec.rb
+++ vmdb/spec/models/ems_refresh/refreshers/openstack_infra_refresher_rhos_juno_spec.rb
@@ -8,6 +8,11 @@ describe EmsRefresh::Refreshers::OpenstackInfraRefresher do
                               :ipaddress => "192.0.2.1", :port => 5000)
     @ems.update_authentication(
         :default => {:userid => "admin", :password => "a280e99820b2e01a42acc9535c81234842b45471"})
+
+    mock_object = double
+    mock_object.stub(:shell_exec).with("lscpu").and_return("Architecture:          x86_64\nCPU op-mode(s):        32-bit, 64-bit\nByte Order:            Little Endian\nCPU(s):                1\nOn-line CPU(s) list:   0\nThread(s) per core:    1\nCore(s) per socket:    1\nSocket(s):             1\nNUMA node(s):          1\nVendor ID:             GenuineIntel\nCPU family:            6\nModel:                 13\nModel name:            QEMU Virtual CPU version 2.1.2\nStepping:              3\nCPU MHz:               2399.998\nBogoMIPS:              4799.99\nHypervisor vendor:     KVM\nVirtualization type:   full\nL1d cache:             32K\nL1i cache:             32K\nL2 cache:              4096K\nNUMA node0 CPU(s):     0\n")
+    mock_object.stub(:shell_exec).with("dmidecode | grep -A8 'System Information'").and_return("System Information\n\tManufacturer: Red Hat\n\tProduct Name: KVM\n\tVersion: RHEL 7.1.0 PC (i440FX + PIIX, 1996)\n\tSerial Number: Not Specified\n\tUUID: 32B5BB93-364E-4D7D-A77C-CCBA193DB50B\n\tWake-up Type: Power Switch\n\tSKU Number: Not Specified\n\tFamily: Red Hat Enterprise Linux\n")
+    HostOpenstackInfra.any_instance.stub(:connect_ssh).and_yield(mock_object)
   end
 
   it "will perform a full refresh" do
@@ -93,10 +98,11 @@ describe EmsRefresh::Refreshers::OpenstackInfraRefresher do
     @host.should have_attributes(
       :ipmi_address     => nil,
       :vmm_vendor       => "RedHat",
-      :vmm_version      => nil,
+      :vmm_version      => "RHEL 7.1.0 PC (i440FX + PIIX, 1996)",
       :vmm_product      => "rhel (No hypervisor, Host Type is Controller)",
       :power_state      => "on",
-      :connection_state => "connected"
+      :connection_state => "connected",
+      :service_tag      => "Not Specified"
     )
 
     @host.operating_system.should have_attributes(
@@ -104,20 +110,20 @@ describe EmsRefresh::Refreshers::OpenstackInfraRefresher do
     )
 
     @host.hardware.should have_attributes(
-      :cpu_speed          => nil,
-      :cpu_type           => nil,
-      :manufacturer       => "",
-      :model              => "",
+      :cpu_speed          => 2399,
+      :cpu_type           => "QEMU Virtual CPU version 2.1.2",
+      :manufacturer       => "Red Hat",
+      :model              => "KVM",
       :memory_cpu         => 4096,  # MB
       :memory_console     => nil,
       :disk_capacity      => 40,
       :numvcpus           => 1,
-      :logical_cpus       => nil,
-      :cores_per_socket   => nil,
-      :guest_os           => nil,
-      :guest_os_full_name => nil,
+      :logical_cpus       => 1,
+      :cores_per_socket   => 1,
+      :guest_os           => "Red Hat Enterprise Linux",
+      :guest_os_full_name => "RHEL 7.1.0 PC (i440FX + PIIX, 1996)",
       :cpu_usage          => nil,
-      :memory_usage       => nil
+      :memory_usage       => nil,
     )
   end
 
