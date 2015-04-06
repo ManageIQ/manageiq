e11852db45175ee31375a840f7e5b4b996a7c3f7
Tina Fitzgerald <tfitzger@redhat.com>
2015-04-06 15:12:47 -0400

    Modified catalogiteminitialization method to use parsed data.
    Added call for dialogparser prior to catalogiteminitialization.

--- vmdb/db/fixtures/ae_datastore/ManageIQ/Service/Provisioning/StateMachines/Methods.class/__methods__/catalogiteminitialization.rb
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Service/Provisioning/StateMachines/Methods.class/__methods__/catalogiteminitialization.rb
@@ -1,126 +1,234 @@
+# CatalogItemInitialization.rb
 #
-# Description: Tag service and set provison options based on service dialog entries.
-# 1. Look for all Service Dialog Options in the service_template_provision_task.dialog_options
-#    (i.e. Dialog options that came from either a Catalog Bundle Service or a Service Dialog)
-# 2. Service dialog option keys that match the regular expression /^tag_\d*_(.*)/i
-#    (I.e. <tag>_<sequence_id>_variable,  tag_0_function, tag_1_environment) will be used to
-#    tag the destination Catalog Item Service and any subordinate miq_provision tasks
-# 3. The remaining Service Dialog Option keys are simply passed into the subordinate
-#    miq_provision object. I.e. option_0_vm_memory => 2048
+# Author: Kevin Morey <kmorey@redhat.com>
+# License: GPL v3
 #
-# Inputs: $evm.root['service_template_provision_task'].dialog_options
+# Description: This method Performs the following functions:
+# 1. YAML load the Service Dialog Options from @task.get_option(:parsed_dialog_options))
+# 2. Set the name of the service
+# 3. Set tags on the service
+# 5. Override miq_provision task with any options and tags
 #
+begin
+  def log_and_update_message(level, msg, update_message = false)
+    $evm.log(level, "#{msg}")
+    @task.message = msg if @task && (update_message || level == 'error')
+  end
 
-# Look for service dialog variables in the dialog options hash that start with "tag_[0-9]",
-def get_tags_hash(dialog_options)
-  # Setup regular expression for service dialog tags
-  tags_regex = /^dialog_tag_\d*_(.*)/
+  # Loop through all tags from the dialog and create the categories and tags automatically
+  def create_tags(category, single_value, tag)
+    # Convert to lower case and replace all non-word characters with underscores
+    category_name = category.to_s.downcase.gsub(/\W/, '_')
+    tag_name = tag.to_s.downcase.gsub(/\W/, '_')
+    # if the category exists else create it
+    unless $evm.execute('category_exists?', category_name)
+      log_and_update_message(:info, "Creating Category: {#{category_name} => #{category}}")
+      $evm.execute('category_create', :name         => category_name,
+                                      :single_value => single_value,
+                                      :description  => "#{category}")
+    end
+    # if the tag exists else create it
+    return if $evm.execute('tag_exists?', category_name, tag_name)
+    log_and_update_message(:info, "Creating tag: {#{tag_name} => #{tag}}")
+    $evm.execute('tag_create', category_name, :name => tag_name, :description => "#{tag}")
+  end
 
-  tags_hash = {}
-
-  # Loop through all of the tags and build an options_hash from them
-  dialog_options.each do |k, v|
-    if tags_regex =~ k
-      # Convert key to symbol
-      tag_category = Regexp.last_match[1].to_sym
-      tag_value = v.downcase
-
-      unless tag_value.blank?
-        $evm.log("info", "Adding category:<#{tag_category.inspect}> tag:<#{tag_value.inspect}> to tags_hash")
-        tags_hash[tag_category] = tag_value
+  def create_category_and_tags_if_necessary(dialog_tags_hash)
+    dialog_tags_hash.each do |category, tag|
+      Array.wrap(tag).each do |tag_entry|
+        create_tags(category, true, tag_entry)
       end
     end
   end
-  $evm.log("info", "Inspecting tags_hash:<#{tags_hash.inspect}>")
-  tags_hash
-end
 
-# Look for service dialog variables in the dialog options hash that start with "option_[0-9]",
-def get_options_hash(dialog_options)
-  # Setup regular expression for service dialog tags
-  options_regex = /^dialog_option_\d*_(.*)/
-  options_hash = {}
+  def override_service_name(dialog_options_hash)
+    log_and_update_message(:info, "Processing override_service_name...", true)
+    new_service_name = dialog_options_hash.fetch(:service_name, nil)
+    new_service_name = "#{@service.name}-#{Time.now.strftime('%Y%m%d-%H%M%S')}" if new_service_name.blank?
 
-  # Loop through all of the options and build an options_hash from them
-  dialog_options.each do |k, v|
-    if options_regex =~ k
-      option_key = Regexp.last_match[1].to_sym
-      option_value = v
+    log_and_update_message(:info, "Service name: #{new_service_name}")
+    @service.name = new_service_name
+    log_and_update_message(:info, "Processing override_service_name...Complete", true)
+  end
 
-      unless option_value.blank?
-        $evm.log("info", "Adding option_key:<#{option_key.inspect}> option_value:<#{option_value.inspect}> to options_hash")
-        options_hash[option_key] = option_value
-      end
+  def override_service_description(dialog_options_hash)
+    log_and_update_message(:info, "Processing override_service_description...", true)
+    new_service_description = dialog_options_hash.fetch(:service_description, nil)
+    return if new_service_description.blank?
+
+    log_and_update_message(:info, "Service description #{new_service_description}")
+    @service.description = new_service_description
+    log_and_update_message(:info, "Processing override_service_description...Complete", true)
+  end
+
+  def tag_service(dialog_tags_hash)
+    return if dialog_tags_hash.nil?
+
+    log_and_update_message(:info, "Processing tag service...", true)
+
+    dialog_tags_hash.each do |key, value|
+      log_and_update_message(:info, "Processing Tag Key: #{key.inspect}  value: #{value.inspect}")
+      next if value.blank?
+      get_service_tags(key.downcase, value)
+    end
+    log_and_update_message(:info, "Processing tag_service...Complete", true)
+  end
+
+  def get_service_tags(tag_category, tag_value)
+    Array.wrap(tag_value).each do |tag_entry|
+      assign_service_tag(tag_category, tag_entry)
+    end
+  end
+
+  def assign_service_tag(tag_category, tag_value)
+    $evm.log(:info, "Adding tag category: #{tag_category} tag: #{tag_value} to Service: #{@service.name}")
+    @service.tag_assign("#{tag_category}/#{tag_value}")
+  end
+
+  def get_vm_name(dialog_options_hash, prov)
+    log_and_update_message(:info, "Processing get_vm_name", true)
+    new_vm_name = dialog_options_hash.fetch(:vm_name, nil) || dialog_options_hash.fetch(:vm_target_name, nil)
+
+    new_vm_name = prov.get_option(:vm_target_name) if new_vm_name.blank?
+
+    dialog_options_hash[:vm_target_name] = new_vm_name
+    dialog_options_hash[:vm_target_hostname] = new_vm_name
+    dialog_options_hash[:vm_name] = new_vm_name
+    dialog_options_hash[:linux_host_name] = new_vm_name
+    log_and_update_message(:info, "Processing get_vm_name...Complete", true)
+  end
+
+  def service_item_dialog_values(dialogs_options_hash)
+    merged_options_hash = Hash.new { |h, k| h[k] = {} }
+    provision_index = determine_provision_index
+
+    if dialogs_options_hash[0].nil?
+      merged_options_hash = dialogs_options_hash[provision_index] || {}
     else
-      unless v.nil?
-        $evm.log("info", "Adding option:<#{k.to_sym.inspect}> value:<#{v.inspect}> to options_hash")
-        options_hash[k.to_sym] = v
-      end
+      merged_options_hash = dialogs_options_hash[0].merge(dialogs_options_hash[provision_index] || {})
+    end
+    merged_options_hash
+  end
+
+  def service_item_tag_values(dialogs_tags_hash)
+    merged_tags_hash         = Hash.new { |h, k| h[k] = {} }
+    provision_index = determine_provision_index
+
+    # merge dialog_tag_0 stuff with current build
+    if dialogs_tags_hash[0].nil?
+      merged_tags_hash = dialogs_tags_hash[provision_index] || {}
+    else
+      merged_tags_hash = dialogs_tags_hash[0].merge(dialogs_tags_hash[provision_index] || {})
+    end
+    merged_tags_hash
+  end
+
+  def determine_provision_index
+    service_resource = @task.service_resource
+    if service_resource
+      # Increment the provision_index number since the child resource starts with a zero
+      provision_index = service_resource.provision_index ? service_resource.provision_index + 1 : 0
+      log_and_update_message(:info, "Bundle --> Service name: #{@service.name}> provision_index: #{provision_index}")
+    else
+      provision_index = 1
+      log_and_update_message(:info, "Item --> Service name: #{@service.name}> provision_index: #{provision_index}")
+    end
+    provision_index
+  end
+
+  def add_provision_tag(key, value, prov)
+    log_and_update_message(:info, "Adding Tag: {#{key.inspect} => #{value.inspect}} to Provisioning id: #{prov.id}")
+    prov.add_tag(key.to_s.downcase.gsub(/\W/, '_'), value.to_s.downcase.gsub(/\W/, '_'))
+  end
+
+  def get_provision_tags(key, value, prov)
+    Array.wrap(value).each do |tag_entry|
+      add_provision_tag(key, tag_entry.downcase, prov)
     end
   end
-  $evm.log("info", "Inspecting options_hash:<#{options_hash.inspect}>")
-  options_hash
-end
 
-# Look in tags_hash for tags and tag the service
-def tag_service(service, tags_hash)
-  # Look for tags with a sequence_id of 0 to tag the destination Service
-  unless tags_hash.nil?
-    tags_hash.each do |k, v|
-      $evm.log("info", "Adding Tag:<#{k.inspect}/#{v.inspect}> to Service:<#{service.name}>")
-      service.tag_assign("#{k}/#{v}")
+  def tag_provision_task(dialog_tags_hash, prov)
+    dialog_tags_hash.each do |key, value|
+      get_provision_tags(key, value, prov)
     end
   end
+
+  def set_option_on_provision_task(dialog_options_hash, prov)
+    dialog_options_hash.each do |key, value|
+      log_and_update_message(:info, "Adding Option: {#{key} => #{value}} to Provisioning id: #{prov.id}")
+      prov.set_option(key.to_s.downcase.gsub(/\W/, '_'), value.to_s.downcase.gsub(/\W/, '_'))
+    end
+  end
+
+  def pass_dialog_values_to_provision_task(provision_task, dialog_options_hash, dialog_tags_hash)
+    provision_task.miq_request_tasks.each do |prov|
+      log_and_update_message(:info, "Grandchild Task: #{prov.id} Desc: #{prov.description} type: #{prov.source_type}")
+      get_vm_name(dialog_options_hash, prov)
+      tag_provision_task(dialog_tags_hash, prov)
+      set_option_on_provision_task(dialog_tags_hash, prov)
+    end
+  end
+
+  def pass_dialog_values_to_children(dialog_options_hash, dialog_tags_hash)
+    @task.miq_request_tasks.each do |t|
+      child_service = t.destination
+      log_and_update_message(:info, "Child Service: #{child_service.name}")
+      next if t.miq_request_tasks.nil?
+
+      pass_dialog_values_to_provision_task(t, dialog_options_hash, dialog_tags_hash)
+    end
+  end
+
+  def remove_service
+    log_and_update_message(:info, "Processing remove_service...", true)
+    if @service
+      log_and_update_message(:info, "Removing Service: #{@service.name} id: #{@service.id} due to failure")
+      @service.remove_from_vmdb
+    end
+    log_and_update_message(:info, "Processing remove_service...Complete", true)
+  end
+
+  def merge_dialog_information(dialog_options_hash, dialog_tags_hash)
+    merged_options_hash = service_item_dialog_values(dialog_options_hash)
+    merged_tags_hash = service_item_tag_values(dialog_tags_hash)
+
+    log_and_update_message(:info, "merged_options_hash: #{merged_options_hash.inspect}")
+    log_and_update_message(:info, "merged_tags_hash: #{merged_tags_hash.inspect}")
+    return merged_options_hash, merged_tags_hash
+  end
+
+  def parsed_dialog_information
+    dialog_options_hash = YAML.load(@task.get_option(:parsed_dialog_options))
+    dialog_tags_hash = YAML.load(@task.get_option(:parsed_dialog_tags))
+
+    if dialog_options_hash.blank? && dialog_tags_hash.blank?
+      log_and_update_message(:error, "Error loading dialog options")
+      exit MIQ_ABORT
+    end
+    merged_options_hash, merged_tags_hash = merge_dialog_information(dialog_options_hash, dialog_tags_hash)
+    return merged_options_hash, merged_tags_hash
+  end
+
+  @task = $evm.root['service_template_provision_task']
+
+  @service = @task.destination
+  log_and_update_message(:info, "Service: #{@service.name} Id: #{@service.id} Tasks: #{@task.miq_request_tasks.count}")
+
+  dialog_options_hash, dialog_tags_hash = parsed_dialog_information
+
+  override_service_name(dialog_options_hash)
+
+  override_service_description(dialog_options_hash)
+
+  create_category_and_tags_if_necessary(dialog_tags_hash)
+
+  tag_service(dialog_tags_hash)
+
+  pass_dialog_values_to_children(dialog_options_hash, dialog_tags_hash)
+
+rescue => err
+  log_and_update_message(:error, "[#{err}]\n#{err.backtrace.join("\n")}")
+  @task.finished("#{err}") if @task
+  remove_service if @service
+  exit MIQ_ABORT
 end
-
-# Get the task object from root
-service_template_provision_task = $evm.root['service_template_provision_task']
-
-# Get destination service object
-service = service_template_provision_task.destination
-$evm.log("info", "Detected Service:<#{service.name}> Id:<#{service.id}>")
-
-# Get dialog options from options hash
-# {:dialog=>{"option_0_myvar"=>"myprefix", "option_1_vservice_workers"=>"2", "tag_0_environment"=>"test", "tag_0_location"=>"paris",
-# "option_2_vm_memory"=>"2048", "option_0_vlan"=>"Internal", "option_1_cores_per_socket"=>"1"}}
-dialog_options = service_template_provision_task.dialog_options
-$evm.log("info", "Inspecting Dialog Options:<#{dialog_options.inspect}>")
-
-# Get tags_hash
-tags_hash = get_tags_hash(dialog_options)
-
-# Tag Service
-tag_service(service, tags_hash)
-
-# Get options_hash
-options_hash = get_options_hash(dialog_options)
-
-# Process Child Tasks
-service_template_provision_task.miq_request_tasks.each do |t|
-
-  # Process grandchildren service options
-  unless t.miq_request_tasks.nil?
-    grandchild_tasks = t.miq_request_tasks
-    grandchild_tasks.each do |gc|
-      $evm.log("info", "Detected Grandchild Task ID:<#{gc.id}> Description:<#{gc.description}> source type:<#{gc.source_type}>")
-
-      # If child task is provisioning then apply tags and options
-      if gc.source_type == "template"
-        unless tags_hash.nil?
-          tags_hash.each do |k, v|
-            $evm.log("info", "Adding Tag:<#{k.inspect}/#{v.inspect}> to Provisioning ID:<#{gc.id}>")
-            gc.add_tag(k, v)
-          end
-        end
-        unless options_hash.nil?
-          options_hash.each do |k, v|
-            $evm.log("info", "Adding Option:<{#{k.inspect} => #{v.inspect}}> to Provisioning ID:<#{gc.id}>")
-            gc.set_option(k, v)
-          end
-        end
-      else
-        $evm.log("info", "Invalid Source Type:<#{gc.source_type}>. Skipping task ID:<#{gc.id}>")
-      end # if gc.source_type
-    end # grandchild_tasks.each do
-  end # unless t.miq_request_tasks.nil?
-end # service_template_provision_task.miq_request_tasks.each do
