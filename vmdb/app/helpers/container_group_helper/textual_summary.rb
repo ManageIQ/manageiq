c8057c1f7b57a8853c779a827c9f283a687df446
Erin A Boyd <eboyd@redhat.com>
2015-04-09 15:13:19 -0600

    fixed timezone

--- vmdb/app/helpers/container_group_helper/textual_summary.rb
+++ vmdb/app/helpers/container_group_helper/textual_summary.rb
@@ -21,24 +21,12 @@ module ContainerGroupHelper::TextualSummary
     {:label => "Namespace", :value => @record.namespace}
   end
 
-  def textual_ems
-    ems = @record.ext_management_system
-    return nil if ems.nil?
-    label = ui_lookup(:table => "ems_container")
-    h = {:label => label, :image => "vendor-#{ems.image_name}", :value => ems.name}
-    if role_allows(:feature => "ems_container_show")
-      h[:title] = "Show parent #{label} '#{ems.name}'"
-      h[:link]  = url_for(:controller => 'ems_container', :action => 'show', :id => ems)
-    end
-    h
-  end
-
   def textual_name
     {:label => "Name", :value => @record.name}
   end
 
   def textual_creation_timestamp
-    {:label => "Creation Timestamp", :value => @record.creation_timestamp}
+    {:label => "Creation Timestamp", :value => format_timezone(@record.creation_timestamp)}
   end
 
   def textual_resource_version
@@ -51,5 +39,17 @@ module ContainerGroupHelper::TextualSummary
 
   def textual_dns_policy
     {:label => "DNS Policy", :value => @record.dns_policy}
+  end
+
+  def textual_ems
+    ems = @record.ext_management_system
+    return nil if ems.nil?
+    label = ui_lookup(:table => "ems_container")
+    h = {:label => label, :image => "vendor-#{ems.image_name}", :value => ems.name}
+    if role_allows(:feature => "ems_container_show")
+      h[:title] = "Show parent #{label} '#{ems.name}'"
+      h[:link]  = url_for(:controller => 'ems_container', :action => 'show', :id => ems)
+    end
+    h
   end
 end
