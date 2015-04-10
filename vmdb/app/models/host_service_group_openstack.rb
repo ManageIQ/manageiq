7b3af896b9601383ce26eb45efd94ae4203d3b2c
Ladislav Smola <lsmola@redhat.com>
2015-04-10 10:46:26 +0200

    Openstack Componenets methods based on state
    Openstack Componenets methods based on state. Since UI get_view
    method can't handle relations, we expose also condition for UI
    use for sublist.

--- vmdb/app/models/host_service_group_openstack.rb
+++ vmdb/app/models/host_service_group_openstack.rb
@@ -1,2 +1,19 @@
 class HostServiceGroupOpenstack < HostServiceGroup
+  def running_system_services_condition
+    # UI can't do arel relations, so I need to expose conditions
+    ["systemd_active= ? AND systemd_sub=?", 'active', 'running']
+  end
+
+  def failed_system_services_condition
+    # UI can't do arel relations, so I need to expose conditions
+    ["(systemd_active= ? OR systemd_sub=?)", 'failed', 'failed']
+  end
+
+  def running_system_services
+    system_services.where(running_system_services_condition)
+  end
+
+  def failed_system_services
+    system_services.where(failed_system_services_condition)
+  end
 end
