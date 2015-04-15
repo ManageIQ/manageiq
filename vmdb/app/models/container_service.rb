43297b297db4c2f91f44262fe2c73c55d0cc4f93
Matthew Draper <mdraper@redhat.com>
2015-04-15 09:54:09 +0930

    Yet more explicit join tables

--- vmdb/app/models/container_service.rb
+++ vmdb/app/models/container_service.rb
@@ -5,7 +5,7 @@ class ContainerService < ActiveRecord::Base
   # :labels, :selector, :protocol, :port, :container_port, :portal_ip, :session_affinity
 
   belongs_to  :ext_management_system, :foreign_key => "ems_id"
-  has_and_belongs_to_many :container_groups
+  has_and_belongs_to_many :container_groups, :join_table => :container_groups_container_services
   has_many :labels, :class_name => CustomAttribute, :as => :resource, :conditions => {:section => "labels"}
   has_many :selector_parts, :class_name => CustomAttribute, :as => :resource, :conditions => {:section => "selectors"}
 end
