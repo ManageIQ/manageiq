ed0b9acd39d190f0a8ebea2166b9ee9c5c749584
Alberto Bellotti <abellott@redhat.com>
2015-04-17 20:30:13 -0400

    Kerberos SSO with Invalid credentials spins login screen forever.
    Upon invalid kerberos SSO, the error javascript comes to us from outside
    the application /, namely proxy_pages/invalid_sso_credentials.js
    
    The javascript in that is no longer compatible with the later UI bits
    for flash messages.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1213014

--- system/COPY/var/www/html/proxy_pages/invalid_sso_credentials.js
+++ system/COPY/var/www/html/proxy_pages/invalid_sso_credentials.js
@@ -1,4 +1,23 @@
-Element.update("flash_div", "Error: Invalid Single Sign-On credentials")
-$("flash_div").show()
+var msg = "Invalid Single Sign-On credentials"
+$('#flash_msg_div').text("");
+
+var outerMost = $("<div id='flash_text_div' onclick=$('#flash_msg_div').text(''); title='Click to remove messages'>");
+var txt = $('<strong>' + msg + '</strong>');
+
+var outerBox = $('<div class="alert alert-danger">');
+var innerSpan = $('<span class="pficon-layered">');
+var icon1 = $('<span class="pficon pficon-error-octagon">');
+var icon2 = $('<span class="pficon pficon-warning-exclamation">');
+
+$(innerSpan).append(icon1);
+$(innerSpan).append(icon2);
+
+$(outerBox).append(innerSpan);
+$(outerBox).append(txt);
+$(outerMost).append(outerBox);
+$(outerMost).appendTo($("#flash_msg_div"));
+
+$('#flash_msg_div').show()
+
 miqSparkle(false)
 miqEnableLoginFields(true);
