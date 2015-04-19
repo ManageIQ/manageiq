32abadb2af4c55ac615c47125fde077d8e9dbdfa
Alissa Bonas <abonas@redhat.com>
2015-04-19 23:23:15 +0300

    add routes and projects to containers UI
    make routes/projects clickable for navigation on ose provider main page

--- vmdb/app/presenters/menu/default_menu.rb
+++ vmdb/app/presenters/menu/default_menu.rb
@@ -69,10 +69,12 @@ module Menu
       def container_menu_section
         Menu::Section.new(:cnt, "Containers", [
           Menu::Item.new('ems_container',     ui_lookup(:tables => 'ems_container'),     'ems_container',     {:feature => 'ems_container_show_list'},     '/ems_container'),
+          Menu::Item.new('container_project', ui_lookup(:tables => 'container_project'), 'container_project', {:feature => 'container_project_show_list'}, '/container_project'),
           Menu::Item.new('container_node',    ui_lookup(:tables => 'container_node'),    'container_node',    {:feature => 'container_node_show_list'},    '/container_node'),
           Menu::Item.new('container_group',   ui_lookup(:tables => 'container_group'),   'container_group',   {:feature => 'container_group_show_list'},   '/container_group'),
+          Menu::Item.new('container_route',   ui_lookup(:tables => 'container_route'),   'container_route',   {:feature => 'container_route_show_list'},   '/container_route'),
           Menu::Item.new('container_service', ui_lookup(:tables => 'container_service'), 'container_service', {:feature => 'container_service_show_list'}, '/container_service'),
-          Menu::Item.new('container',         ui_lookup(:tables => 'container'),         'containers',         {:feature => 'containers', :any => true},     '/container/explorer')
+          Menu::Item.new('container',         ui_lookup(:tables => 'container'),         'containers',        {:feature => 'containers', :any => true},     '/container/explorer')
         ])
       end
 
