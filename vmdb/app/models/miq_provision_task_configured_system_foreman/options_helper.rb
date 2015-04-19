f9e69e06be3c084b5109c70537e3f2dd5e5542b2
Brandon Dunne <bdunne@redhat.com>
2015-04-19 08:48:03 -0400

    Nil out OperatingSystem, Ptable and Medium on the ConfiguredSystem
    Provisioning will not use these values from the Hostgroup unless they
    are nilled out.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1213145

--- vmdb/app/models/miq_provision_task_configured_system_foreman/options_helper.rb
+++ vmdb/app/models/miq_provision_task_configured_system_foreman/options_helper.rb
@@ -4,12 +4,12 @@ module MiqProvisionTaskConfiguredSystemForeman::OptionsHelper
   end
 
   def merge_provider_options_from_automate
-    phase_context[:provider_options].merge!(get_option(:provider_options) || {}).delete_nils
+    phase_context[:provider_options].merge!(get_option(:provider_options) || {})
     dumpObj(phase_context[:provider_options], "MIQ(#{self.class.name}##{__method__}) Merged Provider Options: ", $log, :info, :protected => {:path => /root_pass/})
   end
 
   def prepare_provider_options
-    h = {"hostgroup_id" => dest_configuration_profile.manager_ref}
+    h = {"hostgroup_id" => dest_configuration_profile.manager_ref, "medium_id" => nil, "operatingsystem_id" => nil, "ptable_id" => nil}
     h["name"]      = options[:hostname]                           if options[:hostname]
     h["ip"]        = options[:ip_addr]                            if options[:ip_addr]
     h["root_pass"] = MiqPassword.decrypt(options[:root_password]) if options[:root_password]
