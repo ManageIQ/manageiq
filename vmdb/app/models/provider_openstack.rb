8481bd32f2d53a1dd3a60e8004de4dc4e932eff2
Ladislav Smola <lsmola@redhat.com>
2015-04-22 12:52:59 +0200

    Destroy parent provider when destroying EmsOpenstackInfra
    Destroy parent provider when destroying EmsOpenstackInfra, also
    nullify relations to cloud. Until we have some management of
    the Providers, this should be the way how it works.

--- vmdb/app/models/provider_openstack.rb
+++ vmdb/app/models/provider_openstack.rb
@@ -2,12 +2,11 @@ class ProviderOpenstack < Provider
   has_one :infra_ems,
           :foreign_key => "provider_id",
           :class_name  => "EmsOpenstackInfra",
-          :dependent   => :destroy,
           :autosave    => true
   has_many :cloud_ems,
            :foreign_key => "provider_id",
            :class_name  => "EmsOpenstack",
-           :dependent   => :destroy,
+           :dependent   => :nullify,
            :autosave    => true
 
   validates :name, :presence => true, :uniqueness => true
