f8c67a634d0a4a09cdc60acce4cdd4c8aa3f657c
Matthew Draper <mdraper@redhat.com>
2015-04-23 06:14:12 +0930

    Silently ignore archived VMs during refresh
    This restores behaviour that seems to have gradually faded between
    068485e33e7244299d8bbbd4496a50986ce4f23a (lost the comment) and
    3abfa1dd9e134667e2b58abbd20830762aaa250b (broke nil handling).
    
    But now we have tests!
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1211249

--- vmdb/spec/models/ems_refresh_spec.rb
+++ vmdb/spec/models/ems_refresh_spec.rb
@@ -64,4 +64,28 @@ describe EmsRefresh do
       described_class.get_ar_objects(pairs).should match_array([ems1, ems2])
     end
   end
+
+  context ".refresh" do
+    it "accepts VMs" do
+      ems = FactoryGirl.create(:ems_vmware, :name => "ems_vmware1")
+      vm1 = FactoryGirl.create(:vm_vmware, :name => "vm_vmware1", :ext_management_system => ems)
+      vm2 = FactoryGirl.create(:vm_vmware, :name => "vm_vmware2", :ext_management_system => ems)
+      EmsRefresh::Refreshers::VcRefresher.should_receive(:refresh).with([vm1, vm2])
+      EmsRefresh.refresh([
+        [vm1.class, vm1.id],
+        [vm2.class, vm2.id],
+      ])
+    end
+
+    it "ignores an EMS-less (archived) VM" do
+      ems = FactoryGirl.create(:ems_vmware, :name => "ems_vmware1")
+      vm1 = FactoryGirl.create(:vm_vmware, :name => "vm_vmware1", :ext_management_system => ems)
+      vm2 = FactoryGirl.create(:vm_vmware, :name => "vm_vmware2", :ext_management_system => nil)
+      EmsRefresh::Refreshers::VcRefresher.should_receive(:refresh).with([vm1])
+      EmsRefresh.refresh([
+        [vm1.class, vm1.id],
+        [vm2.class, vm2.id],
+      ])
+    end
+  end
 end
