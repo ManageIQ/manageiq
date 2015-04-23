b54871bc50b7306415d7bc7fbd972dca8e33fab2
Federico Simoncelli <fsimonce@redhat.com>
2015-04-23 15:04:57 -0400

    kubernetes: handle replication controller relationship
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

--- vmdb/app/models/container_group.rb
+++ vmdb/app/models/container_group.rb
@@ -10,6 +10,7 @@ class ContainerGroup < ActiveRecord::Base
   has_many :labels, :class_name => CustomAttribute, :as => :resource, :conditions => {:section => "labels"}
   belongs_to :container_node
   has_and_belongs_to_many :container_services
+  belongs_to :container_replicator
 
   # validates :restart_policy, :inclusion => { :in => %w(always onFailure never) }
   # validates :dns_policy, :inclusion => { :in => %w(ClusterFirst Default) }
