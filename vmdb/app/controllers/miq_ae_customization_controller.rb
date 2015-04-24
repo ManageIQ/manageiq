df1ebec42aba1b6e78f80b6c084b76c44fdd5b1e
Harpreet Kataria <hkataria@redhat.com>
2015-04-24 11:52:56 -0400

    Removed dhtmlxtree specific calls that aren't being used anymore
    - Removed deleteItem, saveOpenStates, loadOpenStates, loadJSONObject dhtmlxtree specific calls, that have either been replaced or no longer apply to dynatrees.
    
    #2714
    https://bugzilla.redhat.com/show_bug.cgi?id=1214776

--- vmdb/app/controllers/miq_ae_customization_controller.rb
+++ vmdb/app/controllers/miq_ae_customization_controller.rb
@@ -253,7 +253,6 @@ class MiqAeCustomizationController < ApplicationController
     handle_bottom_cell(presenter)
     setup_dialog_sample_buttons(nodetype, presenter)
     set_miq_record_id(presenter)
-    save_tree_states(presenter)
 
     render :js => presenter.to_html
   end
@@ -369,10 +368,6 @@ class MiqAeCustomizationController < ApplicationController
 
   def render_proc
     proc { |opts| render_to_string(opts) }
-  end
-
-  def save_tree_states(presenter)
-    presenter[:save_open_states_trees] += [:ab_tree, :old_dialogs_tree, :dialogs_tree, :dialog_edit_tree]
   end
 
   def set_miq_record_id(presenter)
