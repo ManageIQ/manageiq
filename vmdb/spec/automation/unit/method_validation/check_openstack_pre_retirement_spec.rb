f25bae2dbb5a8df93c2eb7e5cf9948a5ccc5a472
Matthew Draper <mdraper@redhat.com>
2015-04-29 04:11:43 +0930

    Revise OpenStack power state handling
    We should look at the state value, instead of the extended OS power
    status -- the latter seems to have much less precision.
    
    Most importantly, this allows us to correctly distinguish between off,
    suspended, and paused -- which each need a different "start" action.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1115557

--- vmdb/spec/automation/unit/method_validation/check_openstack_pre_retirement_spec.rb
+++ vmdb/spec/automation/unit/method_validation/check_openstack_pre_retirement_spec.rb
@@ -5,7 +5,7 @@ describe "openstack_check_pre_retirement Method Validation" do
     @zone = FactoryGirl.create(:zone)
     @ems  = FactoryGirl.create(:ems_vmware, :zone => @zone)
     @vm   = FactoryGirl.create(:vm_openstack,
-                               :name => "OOO",     :raw_power_state => "RUNNING",
+                               :name => "OOO",     :raw_power_state => "ACTIVE",
                                :ems_id => @ems.id, :registered => true)
     @ins  = "/Cloud/VM/Retirement/StateMachines/Methods/CheckPreRetirement"
   end
