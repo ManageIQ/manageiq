20f03b857e86eca6e4d08dda358f167c2f6e1081
Ladislav Smola <lsmola@redhat.com>
2015-04-29 10:56:26 +0200

    Add Provider hooks into OpenstackInfra tests
    Add Provider hooks into OpenstackInfra tests, making sure provider
    is being created and deleted according to Ems. And relation to
    cloud Ems is being nullified on provider destroy.

--- vmdb/spec/models/ems_openstack_infra_spec.rb
+++ vmdb/spec/models/ems_openstack_infra_spec.rb
@@ -44,4 +44,35 @@ describe EmsOpenstackInfra do
     end
   end
 
+  context "provider_hooks" do
+    before :each do
+      @ems = FactoryGirl.create(:ems_openstack_infra_with_authentication)
+    end
+
+    it "creates related ProviderOpenstack after creating EmsOpenstackInfra" do
+      @ems.provider.name.should equal @ems.name
+      @ems.provider.zone.should == @ems.zone
+      ProviderOpenstack.count.should equal 1
+    end
+
+    it "destroys related ProviderOpenstack after destroying EmsOpenstackInfra" do
+      ProviderOpenstack.count.should equal 1
+      @ems.destroy
+      ProviderOpenstack.count.should equal 0
+    end
+
+    it "related EmsOpenstack nullifies relation to ProviderOpenstack on EmsOpenstackInfra destroy" do
+      # add ems_cloud relation to @ems
+      @ems_cloud = FactoryGirl.create(:ems_openstack_with_authentication)
+      @ems.provider.cloud_ems << @ems_cloud
+
+      # compare they both use the same provider
+      @ems_cloud.provider.should == @ems.provider
+
+      # destroy ems and see the relation of ems_cloud to provider nullified
+      @ems.destroy
+      ProviderOpenstack.count.should equal 0
+      @ems_cloud.reload.provider.should be_nil
+    end
+  end
 end
