1acf75b6fd0662f48926c622abd3c2e67a364288
Keenan Brock <kbrock@redhat.com>
2015-05-06 16:45:48 -0400

    Mark foreman virtual columns as virtual
    This will provide them in reporting and allow
    them to be sorted
    
    Related to #2744

--- vmdb/app/models/configuration_profile.rb
+++ vmdb/app/models/configuration_profile.rb
@@ -15,9 +15,33 @@ class ConfigurationProfile < ActiveRecord::Base
   has_and_belongs_to_many :configuration_organizations, :join_table => :configuration_organizations_configuration_profiles
   has_and_belongs_to_many :configuration_tags
 
-  virtual_has_one :configuration_architecture, :class_name => 'ConfigurationArchitecture', :uses => :configuration_tags
+  delegate :name, :to => :configuration_architecture,    :prefix => true, :allow_nil => true
+  delegate :name, :to => :configuration_compute_profile, :prefix => true, :allow_nil => true
+  delegate :name, :to => :configuration_domain,          :prefix => true, :allow_nil => true
+  delegate :name, :to => :configuration_environment,     :prefix => true, :allow_nil => true
+  delegate :name, :to => :configuration_realm,           :prefix => true, :allow_nil => true
+  delegate :name, :to => :customization_script_medium,   :prefix => true, :allow_nil => true
+  delegate :name, :to => :customization_script_ptable,   :prefix => true, :allow_nil => true
+  delegate :name, :to => :operating_system_flavor,       :prefix => true, :allow_nil => true
 
   delegate :my_zone, :provider, :zone, :to => :manager
+
+  virtual_has_one :configuration_architecture,  :class_name => 'ConfigurationArchitecture', :uses => :configuration_tags
+  virtual_has_one :configuration_compute_profile, :class_name => 'ConfigurationProfile',    :uses => :configuration_tags
+  virtual_has_one :configuration_domain,          :class_name => 'ConfigurationDomain',     :uses => :configuration_tags
+  virtual_has_one :configuration_environment,    :class_name => 'ConfigurationEnvironment', :uses => :configuration_tags
+  virtual_has_one :configuration_realm,           :class_name => 'ConfigurationRealm',      :uses => :configuration_tags
+
+  virtual_column  :total_configured_systems,           :type => :integer
+  virtual_column  :my_zone,                            :type => :string
+  virtual_column  :configuration_architecture_name,    :type => :string
+  virtual_column  :configuration_compute_profile_name, :type => :string
+  virtual_column  :configuration_domain_name,          :type => :string
+  virtual_column  :configuration_environment_name,     :type => :string
+  virtual_column  :configuration_realm_name,           :type => :string
+  virtual_column  :customization_script_medium_name,   :type => :string
+  virtual_column  :customization_script_ptable_name,   :type => :string
+  virtual_column  :operating_system_flavor_name,       :type => :string
 
   def configuration_architecture
     tag_hash[ConfigurationArchitecture]
