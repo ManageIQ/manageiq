7b694a9c7a5d689df37bc1e1f46a214e42d01f9e
Bill Wei <bilwei@redhat.com>
2015-05-12 09:38:44 -0400

    Add tenant selection to orchestration provision dialog
    The selection dropbox is dynamic and points to automate by orchestration operations
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1218746

---
+++ vmdb/db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_tenants.rb
@@ -0,0 +1,25 @@
+#
+# Description: provide the dynamic list content from available tenants
+#
+tenant_list = {nil => "default"}
+service_template = $evm.root.attributes["service_template"]
+if service_template.orchestration_manager
+  service_template.orchestration_manager.cloud_tenants.each { |t| tenant_list[t.name] = t.name }
+end
+
+dialog_field = $evm.object
+
+# sort_by: value / description / none
+dialog_field["sort_by"] = "value"
+
+# sort_order: ascending / descending
+# dialog_field["sort_order"] = "ascending"
+
+# data_type: string / integer
+dialog_field["data_type"] = "string"
+
+# required: true / false
+# dialog_field["required"] = "true"
+
+dialog_field["values"] = tenant_list
+dialog_field["default_value"] = nil
