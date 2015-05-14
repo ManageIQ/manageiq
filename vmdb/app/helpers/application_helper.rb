919f81b94b6773bff9743acf6f957320c855e79a
Milan Zazrivec <mzazrivec@redhat.com>
2015-05-14 11:58:14 +0200

    Revert "Added curly braces in the if block."
    This reverts commit c82b2de54e904aa478521da9713be9f45f018cec.
    
    We need to properly initialize all required toolbars.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1220882

--- vmdb/app/helpers/application_helper.rb
+++ vmdb/app/helpers/application_helper.rb
@@ -1723,18 +1723,17 @@ module ApplicationHelper
   # Reload toolbars using new buttons object and xml
   def javascript_for_toolbar_reload(tb, buttons, xml)
     %Q{
-      if (miq_toolbars.#{tb} && miq_toolbars.#{tb}.obj){
+      if (miq_toolbars.#{tb} && miq_toolbars.#{tb}.obj)
         miq_toolbars.#{tb}.obj.unload();
 
-        window.#{tb} = new dhtmlXToolbarObject('#{tb}', 'miq_blue');
-        miq_toolbars['#{tb}'] = {
-          obj: window.#{tb},
-          buttons: #{buttons},
-          xml: "#{xml}"
-        };
+      window.#{tb} = new dhtmlXToolbarObject('#{tb}', 'miq_blue');
+      miq_toolbars['#{tb}'] = {
+        obj: window.#{tb},
+        buttons: #{buttons},
+        xml: "#{xml}"
+      };
 
-        miqInitToolbar(miq_toolbars['#{tb}']);
-      }
+      miqInitToolbar(miq_toolbars['#{tb}']);
     }
   end
 
