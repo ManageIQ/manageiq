f62e2edb3a85dde110aa3266d894134e6e827ad1
Bill Wei <bilwei@redhat.com>
2015-05-15 14:42:18 -0400

    Changes based on review comments

--- app/models/orchestration_stack_openstack.rb
+++ app/models/orchestration_stack_openstack.rb
@@ -1,8 +1,8 @@
 class OrchestrationStackOpenstack < OrchestrationStack
   def raw_update_stack(options)
-    options = {:service => "Orchestration"}
-    options.merge!(:tenant_name => cloud_tenant.name) if cloud_tenant
-    ext_management_system.with_provider_connection(options) do |connection|
+    connection_options = {:service => "Orchestration"}
+    connection_options.merge!(:tenant_name => cloud_tenant.name) if cloud_tenant
+    ext_management_system.with_provider_connection(connection_options) do |connection|
       connection.stacks.get(name, ems_ref).save(options)
     end
   end
