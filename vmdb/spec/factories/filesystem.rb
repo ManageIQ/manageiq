fbd0c0e4d9e84bb218de82b4293d1621f2d7f338
Ladislav Smola <lsmola@redhat.com>
2015-05-18 15:19:05 +0200

    Add tests for Filesystem contents_displayable method
    Add tests for Filesystem contents_displayable method

--- vmdb/spec/factories/filesystem.rb
+++ vmdb/spec/factories/filesystem.rb
@@ -2,4 +2,37 @@ FactoryGirl.define do
   factory :filesystem do
     sequence(:name)     { |n| "filesystem_#{seq_padded_for_sorting(n)}" }
   end
+
+  factory :filesystem_conf_file_ascii, :parent => :filesystem do
+    name "/etc/nova/nova.conf"
+    contents <<-EOT
+## NB: Unpolished config file
+## This config file was taken directly from the upstream repo, and tweaked just enough to work.
+## It has not been audited to ensure that everything present is either Heat controlled or a mandatory as-is setting.
+## Please submit patches for any setting that should be deleted or Heat-configurable. '"-"'
+##  https://git.openstack.org/cgit/openstack/tripleo-image-elements
+
+
+[DEFAULT]
+
+
+s3_host=192.0.2.10
+ec2_dmz_host=192.0.2.10
+ec2_url=http://192.0.2.10:8773/services/Cloud
+
+my_ip=192.0.2.13
+    EOT
+  end
+
+  factory :filesystem_conf_file_non_ascii, :parent => :filesystem_conf_file_ascii do
+    contents "abc\u{4242}"
+  end
+
+  factory :filesystem_binary_file, :parent => :filesystem do
+    name "periodical/utilities/blue_screen.exe"
+  end
+
+  factory :filesystem_txt_file, :parent => :filesystem do
+    name "periodical/utilities/blue_screen_description.txt"
+  end
 end
---
+++ vmdb/spec/models/filesystem_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+
+describe Filesystem do
+  context "#contents_displayable?" do
+    it "filesystem with missing name is not displayable" do
+      filesystem = FactoryGirl.create(:filesystem_conf_file_ascii)
+      filesystem.stub(:name).and_return(nil)
+
+      expect(filesystem.contents_displayable?).to be_false
+    end
+
+    it "non MIME .conf ascii file is displayable" do
+      filesystem = FactoryGirl.create(:filesystem_conf_file_ascii)
+
+      expect(filesystem.contents_displayable?).to be_true
+    end
+
+    it "non MIME .conf file, with non ascii characters is not displayable" do
+      filesystem = FactoryGirl.create(:filesystem_conf_file_non_ascii)
+
+      expect(filesystem.contents_displayable?).to be_false
+    end
+
+    it "non MIME .conf file, without content is not displayable" do
+      filesystem = FactoryGirl.create(:filesystem_conf_file_ascii)
+      filesystem.stub(:has_contents?).and_return(false)
+
+      expect(filesystem.contents_displayable?).to be_false
+    end
+
+    it "MIME .exe binary file is not displayable" do
+      filesystem = FactoryGirl.create(:filesystem_binary_file)
+
+      expect(filesystem.contents_displayable?).to be_false
+    end
+
+    it "MIME .txt non binary file is displayable" do
+      filesystem = FactoryGirl.create(:filesystem_txt_file)
+
+      expect(filesystem.contents_displayable?).to be_true
+    end
+  end
+end
