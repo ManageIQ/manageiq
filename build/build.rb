f4c6ebea1ff67f0e283831efcdcdc6ddc4550581
John Prause <jprause@redhat.com>
2015-05-21 14:51:40 -0400

    Add creation of MD5 and 256checksums

--- build/build.rb
+++ build/build.rb
@@ -4,6 +4,7 @@ require 'fileutils'
 require 'logger'
 require 'pathname'
 require 'yaml'
+require 'awesome_spawn'
 
 require_relative 'productization'
 require_relative 'kickstart_generator'
@@ -89,6 +90,8 @@ Dir.chdir("/root/src/imagefactory") do
     file_name = "#{name}-#{target}-#{build_label}-#{timestamp}-#{git_checkout.commit_sha}.#{FILE_TYPE[imgfac_target]}"
     destination = DESTINATION_DIRECTORY.join(file_name)
     $log.info `mv  #{source} #{destination}`
+    $log.info `cd #{DESTINATION_DIRECTORY}/; /usr/bin/md5sum * >> md5checksums\""`
+    $log.info `cd #{DESTINATION_DIRECTORY}/; /usr/bin/sha256sum * >> 256checksums\""`
   end
 end
 
