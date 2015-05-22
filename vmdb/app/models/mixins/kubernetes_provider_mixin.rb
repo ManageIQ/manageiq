c54c479acdff6751c656a8545ee70a72b3ddae34
Federico Simoncelli <fsimonce@redhat.com>
2015-05-22 09:36:24 -0400

    containers: separate kubernetes provider mixin
    This patch separates the kubernetes provider specifics from the
    container provider mixin into KubernetesProviderMixin.
    
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

---
+++ vmdb/app/models/mixins/kubernetes_provider_mixin.rb
@@ -0,0 +1,66 @@
+module KubernetesProviderMixin
+  extend ActiveSupport::Concern
+
+  include ContainerProviderMixin
+
+  included do
+    # TODO: support real authentication using certificates
+    before_validation :ensure_authentications_record
+  end
+
+  module ClassMethods
+    def raw_api_endpoint(hostname, port)
+      URI::HTTPS.build(:host => hostname, :port => port.presence.try(:to_i))
+    end
+
+    def kubernetes_connect(hostname, port, username, password)
+      require 'kubeclient'
+      api_endpoint = raw_api_endpoint(hostname, port)
+      kubeclient = Kubeclient::Client.new(api_endpoint, kubernetes_version)
+      # TODO: support real authentication using certificates
+      kubeclient.ssl_options(:verify_ssl => OpenSSL::SSL::VERIFY_NONE)
+      kubeclient.basic_auth(username, password) if username && password
+      kubeclient
+    end
+
+    def kubernetes_version
+      'v1beta3'
+    end
+  end
+
+  # UI methods for determining availability of fields
+  def supports_port?
+    true
+  end
+
+  def api_endpoint
+    self.class.raw_api_endpoint(hostname, port)
+  end
+
+  def connect(options = {})
+    hostname = options[:hostname] || address
+    port     = options[:port] || self.port
+    username = options[:user] || authentication_userid(options[:auth_type])
+    password = options[:pass] || authentication_password(options[:auth_type])
+    self.class.raw_connect(hostname, port, username, password, options[:service])
+  end
+
+  def verify_credentials(auth_type = nil, options = {})
+    options = options.merge(:auth_type => auth_type)
+
+    with_provider_connection(options, &:api_valid?)
+    rescue SocketError,
+           Errno::ECONNREFUSED,
+           RestClient::ResourceNotFound,
+           RestClient::InternalServerError => err
+      raise MiqException::MiqUnreachableError, err.message, err.backtrace
+    rescue RestClient::Unauthorized   => err
+      raise MiqException::MiqInvalidCredentialsError, err.message, err.backtrace
+  end
+
+  def ensure_authentications_record
+    # TODO: support real authentication using certificates
+    return if authentications.present?
+    update_authentication(:default => {:userid => "_", :save => false})
+  end
+end
