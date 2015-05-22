ca056ff6b9dd0b557228ffe676b80a46743b33a9
Martin Povolny <mpovolny@redhat.com>
2015-05-22 11:22:56 +0200

    TreeBuilder: fix x_get_tree_ems_kids.

--- vmdb/app/presenters/tree_builder_images.rb
+++ vmdb/app/presenters/tree_builder_images.rb
@@ -5,6 +5,16 @@ class TreeBuilderImages < TreeBuilder
     }
   end
 
+  def set_locals_for_render
+    locals = super
+    locals.merge!(
+      :tree_id   => "images_treebox",
+      :tree_name => "images_tree",
+      :id_prefix => "images_",
+      :autoload  => true
+    )
+  end
+
   def x_get_tree_roots(options)
     objects = rbac_filtered_objects(EmsCloud.order("lower(name)"), :match_via_descendants => "TemplateCloud")
     objects += [
@@ -14,6 +24,10 @@ class TreeBuilderImages < TreeBuilder
     count_only_or_objects(options[:count_only], objects, nil)
   end
 
-  include TreeBuilderCommon
+  def x_get_tree_ems_kids(object, options)
+    objects = rbac_filtered_objects(object.miq_templates.order("name"))
+    options[:count_only] ? objects.length : objects
+  end
+
   include TreeBuilderArchived
 end
--- vmdb/app/presenters/tree_builder_utilization.rb
+++ vmdb/app/presenters/tree_builder_utilization.rb
@@ -3,7 +3,25 @@ class TreeBuilderUtilization  < TreeBuilderRegion
 
   private
 
-  include TreeBuilderCommon
+  def x_get_tree_ems_kids(object, options)
+    ems_clusters        = rbac_filtered_objects(object.ems_clusters)
+    non_clustered_hosts = rbac_filtered_objects(object.non_clustered_hosts)
+
+    total = ems_clusters.count + non_clustered_hosts.count
+
+    return total if options[:count_only]
+    return [] if total == 0
+
+    click_to_open = _('Click to open')
+    [
+      {
+        :id    => "folder_c_xx-#{to_cid(object.id)}",
+        :text  => ui_lookup(:ems_cluster_types => "cluster"),
+        :image => "folder",
+        :tip   => "#{ui_lookup(:ems_cluster_types => "cluster")} (#{click_to_open})"
+      }
+    ]
+  end
 
   def x_get_tree_datacenter_kids(object, options)
     objects =
