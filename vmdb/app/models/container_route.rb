9894b3cd5b6ac251de954b425ea3e4c21bcb0976
Scott Creeley <screeley@redhat.com>
2015-06-03 13:17:27 -0400

    Add namespace and project backend persistence to container_projects

--- vmdb/app/models/container_route.rb
+++ vmdb/app/models/container_route.rb
@@ -3,5 +3,6 @@ class ContainerRoute < ActiveRecord::Base
   include ReportableMixin
 
   belongs_to :ext_management_system, :foreign_key => "ems_id"
+  belongs_to :container_project
   has_many :labels, -> { where(:section => "labels") }, :class_name => "CustomAttribute", :as => :resource
 end
--- vmdb/app/models/ems_refresh/parsers/openshift.rb
+++ vmdb/app/models/ems_refresh/parsers/openshift.rb
@@ -14,21 +14,22 @@ module EmsRefresh
       end
 
       def get_projects(inventory)
-        process_collection(inventory["project"], :container_projects) { |n| parse_project(n) }
+        inventory["project"].each { |item| parse_project(item) }
+
+        @data[:container_projects].each do |ns|
+          @data_index.store_path(:container_projects, :by_name, ns[:name], ns)
+        end
       end
 
-      def parse_project(project)
-        new_result = parse_base_item(project)
-        new_result.except!(:namespace)
-        new_result.merge!(
-            :labels       => parse_labels(project),
-            :display_name => project.displayName
-        )
-        new_result
+      def parse_project(project_item)
+        project = @data_index.fetch_path(:container_projects, :by_name, project_item.metadata.name)
+        project.merge!(:display_name => project_item.metadata.annotations.displayName) unless
+            project_item.metadata.annotations.nil?
       end
 
       def parse_route(route)
         new_result = parse_base_item(route)
+
         new_result.merge!(
             # TODO: persist tls
             :host_name    => route.host,
@@ -37,6 +38,9 @@ module EmsRefresh
             :service_name => route.serviceName,
             :path         => route.path
         )
+
+        new_result[:project] = @data_index.fetch_path(:container_projects, :by_name,
+                                                      route.metadata["table"][:namespace])
         new_result
       end
     end
