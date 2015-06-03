c89e457dfe296e5f3c1a9a83290a32deae573370
Brandon Dunne <bdunne@redhat.com>
2015-06-03 17:47:03 -0400

    Remove no longer used FileDepot#depot_hash reader and writer methods
    resolves #3038

--- vmdb/app/models/file_depot.rb
+++ vmdb/app/models/file_depot.rb
@@ -24,34 +24,7 @@ class FileDepot < ActiveRecord::Base
     false
   end
 
-  def depot_hash=(hsh = {})
-    deprecate_method(__method__, "attributes and authentications of the instance")
-    return if hsh == depot_hash
-    update_authentication(:default => {:userid   => hsh[:username],
-                                       :password => hsh[:password]})
-    update_attribute(:uri, hsh[:uri])
-    update_attribute(:name, hsh[:name])
-  end
-
-  def depot_hash
-    deprecate_method(__method__, "attributes and authentications of the instance")
-    {:username => authentication_userid,
-     :uri      => uri,
-     :password => authentication_password,
-     :name     => name}
-  end
-
   def upload_file(file)
     @file = file
-  end
-
-  private
-
-  def deprecate_method(method, instead)
-    unless Rails.env.production?
-      msg = "[DEPRECATION] #{method} method is deprecated.  Please use #{instead} instead.  At #{caller[1]}"
-      $log.warn msg
-      warn msg
-    end
   end
 end
