75b132e1472bc09067612eb6b98a71a748176973
Madhu Kanoor <mkanoor@redhat.com>
2015-06-08 12:08:47 -0400

    Specs for service template Automate methods
    Added specs for catalog_bundle_initialization,
    catalog_item_initialization & dialog_parser

---
+++ vmdb/spec/automation/unit/method_validation/catalog_bundle_initialization_spec.rb
@@ -0,0 +1,98 @@
+require "spec_helper"
+include ServiceTemplateHelper
+
+describe "CatalogBundleInitialization Automate Method" do
+  before do
+    @allowed_service_templates = %w(top vm_service1 vm_service2)
+    user_helper
+    build_small_environment
+    build_model
+  end
+
+  def create_request_and_tasks(dialog_options = {})
+    @request = build_service_template_request("top", @user.userid, dialog_options)
+    service_template_stubs
+    request_stubs
+    @request.create_request_tasks
+  end
+
+  def build_model
+    model = {"top"         => {:type          => 'composite',
+                               :children      => %w(vm_service1 vm_service2),
+                               :child_options => {'vm_service1' => {:provision_index => 0},
+                                                  'vm_service2' => {:provision_index => 1}}},
+             "vm_service1" => {:type    => 'atomic',
+                               :request => {:src_vm_id => @src_vm.id,
+                                            :number_of_vms => 1, :userid => @user.userid}
+                              },
+             "vm_service2" => {:type    => 'atomic',
+                               :request => {:src_vm_id => @src_vm.id,
+                                            :number_of_vms => 1, :userid => @user.userid}
+                              }
+            }
+    build_service_template_tree(model)
+  end
+
+  def run_automate_method
+    attrs = []
+    attrs << "ServiceTemplateProvisionTask::service_template_provision_task=#{@stp.id}"
+
+    MiqAeEngine.instantiate("/System/Request/Call_Instance_With_Message?" \
+                            "namespace=Service/Provisioning/StateMachines&class=Methods" \
+                            "&instance=CatalogBundleInitialization&" \
+                            "#{attrs.join('&')}")
+  end
+
+  def root_service_template_task
+    @request.miq_request_tasks.detect { |y| y.miq_request_task_id.nil? }
+  end
+
+  context "override" do
+    before do
+      @service_name = "Fred"
+      @service_description = "My Favorite Service"
+      @dialog_hash = {'dialog_option_1_service_name'        => 'one',
+                      'dialog_option_2_service_name'        => 'two',
+                      'dialog_option_0_service_name'        => @service_name,
+                      'dialog_option_0_service_description' => @service_description,
+                      'dialog_tag_0_tracker'                => 'gps',
+                      'dialog_tag_1_location'               => 'BOM',
+                      'dialog_tag_2_location'               => 'EWR'}
+      @parsed_dialog_options_hash = {1 => {:service_name => "one"},
+                                     2 => {:service_name => "two"},
+                                     0 => {:service_name        => @service_name,
+                                           :service_description => @service_description}}
+      @parsed_dialog_tags_hash = {1 => {:location => "BOM"},
+                                  2 => {:location => "EWR"},
+                                  0 => {:tracker => "gps"}}
+    end
+
+    def check_svc_attrs
+      @stp.reload
+      service = @stp.destination
+      service.description.should eql(@service_description)
+      service.name.should eql(@service_name)
+      service.tags[0].name.should eql('/managed/tracker/gps')
+    end
+
+    def process_stp(options)
+      create_request_and_tasks
+      @stp = root_service_template_task
+      @stp.options = @stp.options.merge(options)
+      @stp.save
+      run_automate_method
+    end
+
+    it "service name and description" do
+      parsed_options = {:parsed_dialog_options => @parsed_dialog_options_hash.to_yaml,
+                        :parsed_dialog_tags    => @parsed_dialog_tags_hash.to_yaml}
+      process_stp(parsed_options)
+      check_svc_attrs
+    end
+
+    it "backward compatibility" do
+      process_stp(:dialog => @dialog_hash)
+      check_svc_attrs
+    end
+  end
+end
---
+++ vmdb/spec/automation/unit/method_validation/catalog_item_initialization_spec.rb
@@ -0,0 +1,100 @@
+require "spec_helper"
+include ServiceTemplateHelper
+
+describe "CatalogItemInitialization Automate Method" do
+  before(:each) do
+    @allowed_service_templates = %w(top vm_service1 vm_service2)
+    user_helper
+    build_small_environment
+    build_model
+  end
+
+  def create_request_and_tasks(dialog_options = {})
+    @request = build_service_template_request("top", @user.userid, dialog_options)
+    service_template_stubs
+    request_stubs
+    @request.create_request_tasks
+  end
+
+  def build_model
+    model = {"top"         => {:type          => 'composite',
+                               :children      => %w(vm_service1 vm_service2),
+                               :child_options => {'vm_service1' => {:provision_index => 0},
+                                                  'vm_service2' => {:provision_index => 1}}},
+             "vm_service1" => {:type    => 'atomic',
+                               :request => {:src_vm_id => @src_vm.id,
+                                           :number_of_vms => 1, :userid => @user.userid}
+                             },
+             "vm_service2" => {:type    => 'atomic',
+                               :request => {:src_vm_id => @src_vm.id,
+                                           :number_of_vms => 1, :userid => @user.userid}
+                             }
+            }
+    build_service_template_tree(model)
+  end
+
+  def run_automate_method(stpt)
+    attrs = []
+    attrs << "ServiceTemplateProvisionTask::service_template_provision_task=#{stpt.id}" if stpt
+
+    MiqAeEngine.instantiate("/System/Request/Call_Instance_With_Message?" \
+                            "namespace=Service/Provisioning/StateMachines&class=Methods" \
+                            "&instance=CatalogItemInitialization&" \
+                            "#{attrs.join('&')}")
+  end
+
+  def root_service_template_task
+    @request.miq_request_tasks.detect { |y| y.miq_request_task_id.nil? }
+  end
+
+  context "options and tags" do
+    it "in provision request" do
+      parsed_dialog_options_hash = {1 => {:vm_target_name => "one"},
+                                    2 => {:vm_target_name => "two"},
+                                    0 => {:root_attr      => "fred"}}
+      parsed_dialog_tags_hash = {1 => {:location => "BOM"},
+                                 2 => {:location => "EWR"},
+                                 0 => {:tracker  => "gps"}}
+
+      create_request_and_tasks
+      stp = root_service_template_task
+      st1 = ServiceTemplate.where(:name => 'vm_service1').try(:first)
+      st2 = ServiceTemplate.where(:name => 'vm_service2').try(:first)
+      stp1 = stp.miq_request_tasks.detect { |x| x.source_id == st1.id }
+      stp2 = stp.miq_request_tasks.detect { |x| x.source_id == st2.id }
+
+      parsed_options = {:parsed_dialog_options => parsed_dialog_options_hash.to_yaml,
+                        :parsed_dialog_tags    => parsed_dialog_tags_hash.to_yaml}
+      required_options = {:vm_target_name => 'one', :root_attr => 'fred'}
+      required_tags    = {:tracker => 'gps', :location => 'bom'}
+      process_stp(stp1, parsed_options, required_options, required_tags)
+      required_options = {:vm_target_name => 'two', :root_attr => 'fred'}
+      required_tags    = {:tracker => 'gps', :location => 'ewr'}
+      process_stp(stp2, parsed_options, required_options, required_tags)
+    end
+
+    def process_stp(stp, parsed_options, required_options, required_tags)
+      stp.options = stp.options.merge(parsed_options)
+      stp.save
+      run_automate_method(stp)
+      stp.reload
+      request_task = stp.miq_request_tasks[0].miq_request_tasks[0]
+      check_vm_task(request_task, required_options, required_tags)
+    end
+
+    def check_vm_task(request_task, required_options, required_tags)
+      check_options(request_task, required_options)
+      check_tags(request_task, required_tags)
+    end
+
+    def check_options(request_task, required_options)
+      options = request_task.options
+      required_options.each { |k, v| options[k].should eql(v) }
+    end
+
+    def check_tags(request_task, required_tags)
+      tags = request_task.get_tags
+      required_tags.each { |k, v| tags[k].should eql(v) }
+    end
+  end
+end
