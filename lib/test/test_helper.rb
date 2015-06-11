b87fe36ea986242974249c99030e01df8658b298
Joe Rafaniello <jrafanie@redhat.com>
2015-06-11 11:56:07 -0400

    Fix 'Warning: you should require 'minitest/autorun' instead.'
    This caused lib test/unit / minitest tests to not run.

--- lib/test/test_helper.rb
+++ lib/test/test_helper.rb
@@ -1,5 +1,5 @@
 require_relative "../bundler_setup"
-require 'minitest/unit'
+require 'minitest/autorun'
 
 # Push the lib directory onto the load path
 $:.push(File.expand_path(File.join(File.dirname(__FILE__), '..')))
