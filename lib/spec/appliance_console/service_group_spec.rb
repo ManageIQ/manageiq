75e19bdd74259302acb9c3f413eb2f65ca835f21
Keenan Brock <kbrock@redhat.com>
2015-06-11 12:29:24 -0400

    In ServiceGroup specs, use let

--- lib/spec/appliance_console/service_group_spec.rb
+++ lib/spec/appliance_console/service_group_spec.rb
@@ -5,11 +5,9 @@ require "appliance_console/service_group"
 require "linux_admin"
 
 describe ApplianceConsole::ServiceGroup do
-  before do
-    @group = described_class.new
-    @common_services  = %w{evminit memcached miqtop evmserverd}
-    @postgres_service = %w{postgresql92-postgresql}
-  end
+  let(:group)             { described_class.new }
+  let(:common_services)   { %w(evminit memcached miqtop evmserverd) }
+  let(:postgres_service)  { %w(postgresql92-postgresql) }
 
   it "#postgresql?" do
     expect(described_class.new(:internal_postgresql => true).postgresql?).to be_true
@@ -18,60 +16,58 @@ describe ApplianceConsole::ServiceGroup do
   end
 
   context "postgresql" do
-    before do
-      @group.stub(:postgresql? => true)
-    end
+    let(:group) { described_class.new(:internal_postgresql => true) }
 
     it "#to_enable" do
-      expect(@group.to_enable).to eq(@common_services | @postgres_service)
+      expect(group.to_enable).to eq(common_services | postgres_service)
     end
 
     it "#to_start" do
-      expect(@group.to_start).to eq(@common_services)
+      expect(group.to_start).to eq(common_services)
     end
 
     it "#to_disable" do
-      expect(@group.to_disable).to eq([])
+      expect(group.to_disable).to eq([])
     end
 
     it "#to_stop" do
-      expect(@group.to_stop).to eq([])
+      expect(group.to_stop).to eq([])
     end
   end
 
   context "without postgresql" do
     it "#to_enable" do
-      expect(@group.to_enable).to eq(@common_services)
+      expect(group.to_enable).to eq(common_services)
     end
 
     it "#to_start" do
-      expect(@group.to_start).to eq(@common_services)
+      expect(group.to_start).to eq(common_services)
     end
 
     it "#to_disable" do
-      expect(@group.to_disable).to eq(@postgres_service)
+      expect(group.to_disable).to eq(postgres_service)
     end
 
     it "#to_stop" do
-      expect(@group.to_stop).to eq(@postgres_service)
+      expect(group.to_stop).to eq(postgres_service)
     end
   end
 
   shared_examples_for "service management" do |command|
     it "##{command}" do
       LinuxAdmin.should_receive(:run).with("chkconfig", :params => {"--add" => "miqtop"}) if command == "enable"
-      expected_calls = @group.send("to_#{command}")
+      expected_calls = group.send("to_#{command}")
 
       # Hack until LinuxAdmin::Service start handles detaching.
       if command == "start"
-        @group.should_receive(:start_command)
+        group.should_receive(:start_command)
       else
         expected_calls.each do |service|
           LinuxAdmin::Service.should_receive(:new).with(service).and_return(double(command => true))
         end
       end
 
-      @group.send(command)
+      group.send(command)
     end
   end
 
