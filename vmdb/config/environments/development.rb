e91c81b691add6f5be44b2185611fda1eed7881c
Keenan Brock <kbrock@redhat.com>
2015-06-12 18:59:29 -0400

    set eager_load for environments

--- vmdb/config/environments/development.rb
+++ vmdb/config/environments/development.rb
@@ -5,6 +5,7 @@ Vmdb::Application.configure do
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.eager_load = false
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
