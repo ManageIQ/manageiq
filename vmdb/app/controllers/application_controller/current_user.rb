38b356d95ad5468241733d1bba237c7eb43c600a
Keenan Brock <kbrock@redhat.com>
2015-06-17 20:16:48 -0400

    remove session[:role]

--- vmdb/app/controllers/application_controller/current_user.rb
+++ vmdb/app/controllers/application_controller/current_user.rb
@@ -30,7 +30,6 @@ module ApplicationController::CurrentUser
 
     role = db_group.try(:miq_user_role)
 
-    session[:role] = role.try(:id)
     # Build pre-sprint 69 role name if this is an EvmRole read_only role
     session[:userrole] = role.try(:read_only?) ? role.name.split("-").last : ""
   end
@@ -64,11 +63,6 @@ module ApplicationController::CurrentUser
     session[:group_description]
   end
   protected :current_group_description
-
-  def current_role
-    session[:role]
-  end
-  protected :current_role
 
   def current_userrole
     session[:userrole]
