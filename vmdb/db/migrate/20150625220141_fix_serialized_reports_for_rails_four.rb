4a523bf1c592d05edf418da90718e15592f31cc7
Gregg Tanzillo <gtanzill@redhat.com>
2015-06-26 09:33:35 -0400

    Issue #3208 - Combined migrations for miq_report_results and binary_blobs into one migration with a shared module containing the serialization methods.

--- vmdb/db/migrate/20150625220141_fix_serialized_reports_for_rails_four.rb
+++ vmdb/db/migrate/20150625220141_fix_serialized_reports_for_rails_four.rb
@@ -1,39 +1,51 @@
-class BinaryBlobFixSerializedReport < ActiveRecord::Migration
-  class BinaryBlob < ActiveRecord::Base
-    self.inheritance_column = :_type_disabled
-    has_many :binary_blob_parts, -> { order(:id) }
-
+class FixSerializedReportsForRailsFour < ActiveRecord::Migration
+  module Serializer
     YAML_ATTRS = [:table, :sub_table, :filter_summary, :extras, :ids, :scoped_association, :html_title, :file_name, :extras, :record_id,
                   :tl_times, :user_categories, :trend_data, :performance, :include_for_find, :report_run_time, :chart, :reserved]
 
-    def serialize_report_to_hash
-      val = binary
-
+    def serialize_report_to_hash(val)
       if val.include?("!ruby/object:MiqReport")
         val.sub!(/MiqReport/, 'Hash')
       else
-        raise "unexpected format of binary data encountered, '#{val.inspect}'"
+        raise "unexpected format of report attribute encountered, '#{val.inspect}'"
       end
-
       raw_hash = YAML.load(val)
+      raw_hash = raw_hash.last if raw_hash.kind_of?(Array) # MiqReport was serialized as an Array with 1 element in miq_report_results
       new_hash = YAML_ATTRS.each_with_object(raw_hash['attributes'].to_hash) { |k, h| h[k.to_s] = raw_hash[k.to_s] }
 
       YAML.dump(new_hash)
     end
 
-    def serialize_hash_to_report
-      val = binary
+    def serialize_hash_to_report(val, from)
       if val.starts_with?("---")
         new_hash = {'attributes' => {}}
         YAML.load(val).each do |k,v|
           YAML_ATTRS.include?(k.to_sym) ? new_hash[k.to_s] = v : new_hash['attributes'][k.to_s] = v
         end
 
-        YAML.dump(new_hash).sub(/---/, "--- !ruby/object:MiqReport")
+        if from == :miq_report_result
+          # MiqReport was serialized as an Array with 1 element in miq_report_results
+          YAML.dump([new_hash]).sub(/---\n- attributes:/, "---\n- !ruby/object:MiqReport\n  attributes:")
+        else
+          YAML.dump(new_hash).sub(/---/, "--- !ruby/object:MiqReport")
+        end
       else
         raise "unexpected format of report attribute encountered, '#{val.inspect}'"
       end
     end
+  end
+
+  class MiqReportResult < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+
+    include Serializer
+  end
+
+  class BinaryBlob < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+    has_many :binary_blob_parts, -> { order(:id) }
+
+    include Serializer
 
     def delete_binary
       self.md5 = self.size = self.part_size = nil
@@ -72,17 +84,29 @@ class BinaryBlobFixSerializedReport < ActiveRecord::Migration
   end
 
   def up
+    say_with_time("Converting MiqReportResult#report to a serialized hash") do
+      MiqReportResult.where('report IS NOT NULL').each do |rr|
+        rr.update_attribute(:report, rr.serialize_report_to_hash(rr.read_attribute(:report)))
+      end
+    end
+
     say_with_time("Converting BinaryBlob report results to a serialized hash") do
       BinaryBlob.where(:resource_type=>'MiqReportResult').each do |bb|
-        bb.binary = bb.serialize_report_to_hash
+        bb.binary = bb.serialize_report_to_hash(bb.binary)
       end
     end
   end
 
   def down
+    say_with_time("Converting MiqReportResult#report back to a serialized MiqReport") do
+      MiqReportResult.where('report IS NOT NULL').each do |rr|
+        rr.update_attribute(:report, rr.serialize_hash_to_report(rr.read_attribute(:report), :miq_report_result))
+      end
+    end
+
     say_with_time("Converting BinaryBlob report results back to a serialized MiqReport") do
       BinaryBlob.where(:resource_type=>'MiqReportResult').each do |bb|
-        bb.binary = bb.serialize_hash_to_report
+        bb.binary = bb.serialize_hash_to_report(bb.binary, :binary_blob)
       end
     end
   end
--- vmdb/spec/migrations/20150625220141_fix_serialized_reports_for_rails_four_spec.rb
+++ vmdb/spec/migrations/20150625220141_fix_serialized_reports_for_rails_four_spec.rb
@@ -1,16 +1,32 @@
 require "spec_helper"
-require Rails.root.join("db/migrate/20150623222927_binary_blob_fix_serialized_report.rb")
+require Rails.root.join("db/migrate/20150625220141_fix_serialized_reports_for_rails_four.rb")
 
-describe BinaryBlobFixSerializedReport do
+describe FixSerializedReportsForRailsFour do
+  let(:report_result_stub)  { migration_stub(:MiqReportResult) }
   let(:binary_blob)  { migration_stub(:BinaryBlob) }
   let(:data_dir) { File.join(Rails.root, 'spec/migrations/data', File.basename(__FILE__, '.rb')) }
 
   migration_context :up do
     before(:each) do
       @raw_report = File.read(File.join(data_dir, 'miq_report_obj.yaml'))
+      @raw_blob   = File.read(File.join(data_dir, 'binary_blob_obj.yaml'))
     end
 
     it "migrates existing reports serialized as MiqReport objects to Hashes" do
+      rr = report_result_stub.create!(
+          :db     => 'Chargeback',
+          :report => @raw_report
+      )
+
+      migrate
+
+      rr.reload
+
+      raw_report = YAML.load(rr.read_attribute(:report))
+      expect(raw_report).to be_a(Hash)
+    end
+
+    it "migrates existing binary blobs serialized as MiqReport objects to Hashes" do
       bb = binary_blob.create!(
           :resource_type => "MiqReportResult",
           :md5 => "b540c6aec8a7726c1154d71c06017150",
@@ -19,7 +35,7 @@ describe BinaryBlobFixSerializedReport do
           :name => "report_results",
           :data_type => "YAML"
       )
-      bb.binary = @raw_report
+      bb.binary = @raw_blob
 
       migrate
 
@@ -33,9 +49,25 @@ describe BinaryBlobFixSerializedReport do
   migration_context :down do
     before(:each) do
       @raw_report = File.read(File.join(data_dir, 'miq_report_hash.yaml'))
+      @raw_blob   = File.read(File.join(data_dir, 'binary_blob_hash.yaml'))
     end
 
     it "migrates existing reports serialized as Hashes objects to MiqReports" do
+      rr = report_result_stub.create!(
+          :db     => 'Chargeback',
+          :report => @raw_report
+      )
+
+      migrate
+
+      rr.reload
+
+      raw_report = YAML.load(rr.read_attribute(:report))
+      expect(raw_report).to be_a(Array)
+      expect(raw_report.first).to be_a(MiqReport)
+    end
+
+    it "migrates existing binary blobs serialized as Hashes objects to MiqReports" do
       bb = binary_blob.create!(
           :resource_type => "MiqReportResult",
           :md5 => "b540c6aec8a7726c1154d71c06017150",
@@ -44,7 +76,7 @@ describe BinaryBlobFixSerializedReport do
           :name => "report_results",
           :data_type => "YAML"
       )
-      bb.binary = @raw_report
+      bb.binary = @raw_blob
 
       migrate
 
