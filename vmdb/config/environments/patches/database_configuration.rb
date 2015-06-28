542429c5afe259b49abb7c43de203300593bcfa6
Matthew Draper <mdraper@redhat.com>
2015-06-28 12:52:16 +0930

    Add a spec to ensure our DB config patch is in place

--- vmdb/config/environments/patches/database_configuration.rb
+++ vmdb/config/environments/patches/database_configuration.rb
@@ -2,16 +2,8 @@
 # mode for security purposes.  Also, adds support to handle encrypted
 # password fields as strings without ERB.
 #
-# The original implementation is as follows:
-#
-#    def database_configuration
-#      require 'erb'
-#      YAML::load(ERB.new(IO.read(paths["config/database"].first)).result)
-#    end
 require 'rails/application/configuration'
 Rails::Application::Configuration.module_eval do
-  # sorry for the multi line {}
-  # block needs to be associated with module not prepend
   prepend Module.new {
     def database_configuration
       path = paths["config/database"].existent.first
