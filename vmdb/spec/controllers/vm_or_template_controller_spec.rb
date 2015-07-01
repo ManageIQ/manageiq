86e6953c28e93523cc76f13050f2c2d80f8f2dfd
Keenan Brock <kbrock@redhat.com>
2015-07-01 09:28:40 -0400

    let login_as setup users for specs

--- vmdb/spec/controllers/vm_or_template_controller_spec.rb
+++ vmdb/spec/controllers/vm_or_template_controller_spec.rb
@@ -118,8 +118,6 @@ describe VmOrTemplateController do
   context "skip or drop breadcrumb" do
     before do
       session[:settings] = {:views => {}, :perpage => {:list => 10}}
-      session[:userid] = User.current_user.userid
-      session[:eligible_groups] = []
       FactoryGirl.create(:vmdb_database)
       EvmSpecHelper.create_guid_miq_server_zone
       @vm_or_template = VmOrTemplate.create(:name     => "test_vm_or_template",
