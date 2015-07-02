ebd0a01a3c4988999b49a9ce86e28712cfa3a32d
Mooli Tayer <mtayer@redhat.com>
2015-07-02 15:33:50 +0300

    show count of related entities in Container Projects page.

--- app/models/container_project.rb
+++ app/models/container_project.rb
@@ -8,4 +8,25 @@ class ContainerProject < ActiveRecord::Base
   has_many :container_services
 
   has_many :labels, -> { where(:section => "labels") }, :class_name => "CustomAttribute", :as => :resource
+
+  virtual_column :groups_count,      :type => :integer
+  virtual_column :services_count,    :type => :integer
+  virtual_column :routes_count,      :type => :integer
+  virtual_column :replicators_count, :type => :integer
+
+  def groups_count
+    container_groups.size
+  end
+
+  def routes_count
+    container_routes.size
+  end
+
+  def replicators_count
+    container_replicators.size
+  end
+
+  def services_count
+    container_services.size
+  end
 end
