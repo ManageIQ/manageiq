754a0c99707455b44d3fc8f1c433f8b2155c3d09
Keenan Brock <kbrock@redhat.com>
2015-07-10 16:30:56 -0400

    fix_auth always cleanup all keys
    The --v2 option was an optimization to speed up fixing
    old encryption keys. It would only fix keys that had not been
    upgraded yet.
    
    Unfortunately it is confusing when users are trying to fix
    bad v2 keys. It also did not speed up the process that much.
    
    We have updated it across versions, so now the flag has
    different meanings on different versions. very confusing.
    
    This removes the flag.

--- spec/tools/fix_auth/auth_model_spec.rb
+++ spec/tools/fix_auth/auth_model_spec.rb
@@ -53,13 +53,7 @@ describe FixAuth::AuthModel do
 
     it "should build selection criteria (non selects)" do
       expect(subject.selection_criteria).to match(/OR/)
-      expect(subject.selection_criteria).to match(/password.*!~ 'v2.*OR.*auth_key.*!~ 'v2/)
-    end
-
-    it "should build selection criteria (non selects)" do
-      expect(subject.selection_criteria(true)).to match(/OR/)
-      expect(subject.selection_criteria(true)).not_to match(/password.*!~ 'v2.*OR.*auth_key.*!~ 'v2/)
-      expect(subject.selection_criteria(true)).to match(/password.*<>.*''.*OR.*auth_key.*<>.*''/)
+      expect(subject.selection_criteria).to match(/password.*<>.*''.*OR.*auth_key.*<>.*''/)
     end
 
     it "should not find empty records" do
@@ -69,13 +63,8 @@ describe FixAuth::AuthModel do
 
     it "should find records with encrypted passwords" do
       [v1, v2, leg, nls].each(&:save!)
-      expect(contenders).to include(v1.name, leg.name)
-      expect(contenders).not_to include(v2.name, nls.name)
-    end
-
-    it "finds records already encryped when requested" do
-      [v1, v2].each(&:save!)
-      expect(subject.contenders(true).collect(&:name)).to include(v2.name)
+      expect(contenders).to include(v1.name, leg.name, v2.name)
+      expect(contenders).not_to include(nls.name)
     end
 
     it "should find viable records among mixed mode records" do
