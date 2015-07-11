bba8ca8df7a013d8af5be99fdc313e3bcb4ae433
Gregg Tanzillo <gtanzill@redhat.com>
2015-07-11 07:54:05 -0400

    Made test that specifically tests the behavior of an AR query with includes with a polymorphic association and references pending until issue is addressed in ActiveRecord.
    https://bugzilla.redhat.com/show_bug.cgi?id=1238179

--- spec/models/metric_rollup_spec.rb
+++ spec/models/metric_rollup_spec.rb
@@ -3,10 +3,11 @@ require "spec_helper"
 describe MetricRollup do
   context "test" do
     it "should not raise an error when a polymorphic reflection is included and references are specified in a query" do
+      pending "until ActiveRecord is fixed"
       # TODO: A fix in ActiveRecord will make this test pass
-      # expect {
-      #   MetricRollup.where(:id=>1000000544893).includes(:resource=>{}, :time_profile=>{}).references(:time_profile=>{}).last
-      # }.not_to raise_error
+      expect {
+        MetricRollup.where(:id=>1000000544893).includes(:resource=>{}, :time_profile=>{}).references(:time_profile=>{}).last
+      }.not_to raise_error
 
       # TODO: Also, there is a bug that exists in only the manageiq repo and not rails that causes the error "ActiveRecord::ConfigurationError: nil"
       # TODO: instead of the expected "ActiveRecord::EagerLoadPolymorphicError" error.
