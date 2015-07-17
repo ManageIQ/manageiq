ff20567b2d3675d735e12a614fbdb7c82247ef6c
Oleg Barenboim <chessbyte@gmail.com>
2015-07-17 15:31:20 -0400

    Namespaced NtUtil inside NTFS module (ie. NTFS::NtUtil)

--- gems/pending/fs/ntfs/nt_util.rb
+++ gems/pending/fs/ntfs/nt_util.rb
@@ -1,44 +1,44 @@
 # encoding: US-ASCII
 
-module NtUtil
+module NTFS
+  module NtUtil
+    # Make a reference (upper two bytes are seq num, lower six are entry).
+    def NtUtil.MkRef(ref)
+      ref.divmod(2 ** 48)
+    end
+    
+    def NtUtil.gotBit?(flags, bit)
+  		(flags & bit) == bit
+  	end
+  	
+    # Process per-sector "fixups" that NTFS uses to detect corruption of
+    # multi-sector data structures, like MFT records.
+    def NtUtil.process_fixups(buf, fixup_offset, usa_offset, usa_count)
+      #
+      # The signature value we must look for is stored just before the fix-up array.
+      #
+      fu_sig = buf[usa_offset, 2].unpack('S')[0]
 
-  # Make a reference (upper two bytes are seq num, lower six are entry).
-  def NtUtil.MkRef(ref)
-    ref.divmod(2 ** 48)
-  end
-  
-  def NtUtil.gotBit?(flags, bit)
-		(flags & bit) == bit
-	end
-	
-  # Process per-sector "fixups" that NTFS uses to detect corruption of
-  # multi-sector data structures, like MFT records.
-  def NtUtil.process_fixups(buf, fixup_offset, usa_offset, usa_count)
-    #
-    # The signature value we must look for is stored just before the fix-up array.
-    #
-    fu_sig = buf[usa_offset, 2].unpack('S')[0]
+      #
+      # For each end-of-sector, check that the last two bytes equal the fixup signature.
+      # If so, replace them with original data stored in the update sequence array.
+      #
+      1.upto(usa_count - 1) do |i|
+        sig = buf[i * fixup_offset - 2, 2].unpack('S')[0]
+        raise "NTFS Fixup Error: fixup signature:<#{fu_sig}> does not match signature[#{i}]=<#{sig}> - consider running chkdsk" if sig != fu_sig
+        buf[i * fixup_offset - 2, 2] = buf[i * 2 + usa_offset, 2]
+      end
 
-    #
-    # For each end-of-sector, check that the last two bytes equal the fixup signature.
-    # If so, replace them with original data stored in the update sequence array.
-    #
-    1.upto(usa_count - 1) do |i|
-      sig = buf[i * fixup_offset - 2, 2].unpack('S')[0]
-      raise "NTFS Fixup Error: fixup signature:<#{fu_sig}> does not match signature[#{i}]=<#{sig}> - consider running chkdsk" if sig != fu_sig
-      buf[i * fixup_offset - 2, 2] = buf[i * 2 + usa_offset, 2]
+      return buf
     end
 
-    return buf
-  end
-
-  def NtUtil.validate_signature(signature, expected)
-    if signature != expected
-      raise "Uninitialized"   if signature == "\000\000\000\000"
-      raise "Bad Sector"      if signature == 'BAAD'
-      raise "Invalid Signature <#{signature}>"
+    def NtUtil.validate_signature(signature, expected)
+      if signature != expected
+        raise "Uninitialized"   if signature == "\000\000\000\000"
+        raise "Bad Sector"      if signature == 'BAAD'
+        raise "Invalid Signature <#{signature}>"
+      end
     end
-  end
 
+  end
 end
-
