65d159070eaa2f7c8071fb0171efcd7bb3b69f3e
Brandon Dunne <bdunne@redhat.com>
2015-07-24 20:19:07 -0400

    Remove extra methods in configure/set cpu, memory, etc in RHEV provision
    Refactor and update configuration tests
    Test more of #configure_container
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1238390

--- spec/models/manageiq/providers/redhat/infra_provider/provision/configuration_spec.rb
+++ spec/models/manageiq/providers/redhat/infra_provider/provision/configuration_spec.rb
@@ -2,57 +2,61 @@ require "spec_helper"
 require "ovirt"
 
 describe ManageIQ::Providers::Redhat::InfraManager::Provision::Configuration do
-  before do
-    ems       = FactoryGirl.create(:ems_redhat_with_authentication)
-    template  = FactoryGirl.create(:template_redhat, :ext_management_system => ems)
-    vm        = FactoryGirl.create(:vm_redhat)
-    options   = {:src_vm_id => template.id}
-    @task     = FactoryGirl.create(:miq_provision_redhat,
-                                   :source      => template,
-                                   :destination => vm,
-                                   :state       => 'pending',
-                                   :status      => 'Ok',
-                                   :options     => options)
-    @task.stub(:dest_cluster => FactoryGirl.create(:ems_cluster, :ext_management_system => ems))
+  let(:cust_template) { FactoryGirl.create(:customization_template_cloud_init, :script => '#some_script') }
+  let(:ems)           { FactoryGirl.create(:ems_redhat_with_authentication) }
+  let(:host)          { FactoryGirl.create(:host_redhat) }
+  let(:rhevm_vm)      { instance_double("Ovirt::Vm") }
+  let(:task)          { FactoryGirl.create(:miq_provision_redhat, :state => 'pending', :status => 'Ok', :options => {:src_vm_id => template.id}) }
+  let(:template)      { FactoryGirl.create(:template_redhat, :ext_management_system => ems) }
+  let(:vm)            { FactoryGirl.create(:vm_redhat) }
 
-    @rhevm_vm = double('rhevm_vm').as_null_object
-    @task.stub(:get_provider_destination => @rhevm_vm)
-  end
+  before { task.stub(:get_provider_destination => rhevm_vm) }
 
   context "#attach_floppy_payload" do
     it "should attach floppy if customization template provided" do
-      script = '#cloudinit'
-      template = FactoryGirl.create(:customization_template_cloud_init, :script => script)
+      task.options[:customization_template_id] = cust_template.id
 
-      @task.should_receive(:prepare_customization_template_substitution_options).and_return('key' => 'value')
+      expect(task).to     receive(:prepare_customization_template_substitution_options).and_return('key' => 'value')
+      expect(rhevm_vm).to receive(:attach_floppy).with(cust_template.default_filename => '#some_script')
 
-      @task.options[:customization_template_id] = template.id
-      @rhevm_vm.should_receive(:attach_floppy).with(template.default_filename => script)
-
-      @task.attach_floppy_payload
+      task.attach_floppy_payload
     end
   end
 
   context "#configure_cloud_init" do
     it "should configure cloudinit if customization template provided" do
-      script = '#cloudinit'
-      template = FactoryGirl.create(:customization_template_cloud_init, :script => script)
+      task.options[:customization_template_id] = cust_template.id
 
-      @task.should_receive(:prepare_customization_template_substitution_options).and_return('key' => 'value')
+      expect(task).to     receive(:prepare_customization_template_substitution_options).and_return('key' => 'value')
+      expect(rhevm_vm).to receive(:cloud_init=).with('#some_script')
 
-      @task.options[:customization_template_id] = template.id
-      @rhevm_vm.should_receive(:cloud_init=).with(script)
-
-      @task.configure_cloud_init
+      task.configure_cloud_init
     end
   end
 
   context "#configure_container" do
-    it "should configure the memory reserve if provided" do
-      memory_reserve_in_mb = 1024
-      @task.options[:memory_reserve] = memory_reserve_in_mb
-      @rhevm_vm.should_receive(:memory_reserve=).with(memory_reserve_in_mb.megabytes)
-      @task.configure_container
+    it "with options set" do
+      task.options[:cores_per_socket]  = 4
+      task.options[:dest_host]         = host.id
+      task.options[:memory_reserve]    = 1024
+      task.options[:number_of_sockets] = 2
+      task.options[:vm_description]    = "abc"
+      task.options[:vm_memory]         = 2048
+
+      expect(rhevm_vm).to receive(:description=).with("abc")
+      expect(rhevm_vm).to receive(:memory=).with(2147483648)
+      expect(rhevm_vm).to receive(:memory_reserve=).with(1073741824)
+      expect(rhevm_vm).to receive(:cpu_topology=).with(:cores => 4, :sockets => 2)
+      expect(rhevm_vm).to receive(:host_affinity=).with(host.ems_ref)
+
+      task.configure_container
+    end
+
+    it "without options" do
+      expect(rhevm_vm).to receive(:description=).with(nil)
+      expect(rhevm_vm).to receive(:cpu_topology=).with(:cores => 1, :sockets => 1)
+
+      task.configure_container
     end
   end
 end
