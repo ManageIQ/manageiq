6dac115d868920800e255ea04ff78dabc9597977
Madhu Kanoor <mkanoor@redhat.com>
2015-08-05 17:10:03 -0400

    Fixed rubocop warnings
    Rubocop cleanup
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1247664

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-vmware-infra_manager-vm.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-vmware-infra_manager-vm.rb
@@ -14,7 +14,7 @@ module MiqAeMethodService
     end
 
     def create_snapshot(name, desc = nil, memory = false)
-      snapshot_operation(:create_snapshot, {:name=>name, :description => desc, :memory => !!memory})
+      snapshot_operation(:create_snapshot, :name => name, :description => desc, :memory => !!memory)
     end
 
     def remove_all_snapshots
@@ -22,15 +22,15 @@ module MiqAeMethodService
     end
 
     def remove_snapshot(snapshot_id)
-      snapshot_operation(:remove_snapshot, {:snap_selected => snapshot_id})
+      snapshot_operation(:remove_snapshot, :snap_selected => snapshot_id)
     end
 
     def revert_to_snapshot(snapshot_id)
-      snapshot_operation(:revert_to_snapshot, {:snap_selected => snapshot_id})
+      snapshot_operation(:revert_to_snapshot, :snap_selected => snapshot_id)
     end
 
     def snapshot_operation(task, options = {})
-      options.merge!(:ids=>[self.id], :task=>task.to_s)
+      options.merge!(:ids => [id], :task => task.to_s)
       Vm.process_tasks(options)
     end
   end
