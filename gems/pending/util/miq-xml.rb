af8b420e89cbef70470cc6e9a1783e253eaf9ead
Daniel Berger <dberger@redhat.com>
2015-08-07 14:57:41 -0600

    Replace @@nokogiri_loaded with @rexml_parser class instance variable, and corresponding singleton method, and generally assume Nokogiri before REXML.

--- gems/pending/util/miq-xml.rb
+++ gems/pending/util/miq-xml.rb
@@ -6,35 +6,34 @@ require 'util/xml/xml_diff'
 require 'util/xml/xml_patch'
 
 class MiqXml
-  MIQ_XML_VERSION = 2.0 # Changed sub-xmls, added namespaces
+  MIQ_XML_VERSION = 3.0 # Use Nokogiri by default
 
-  @@defaultXmlType = :rexml  # REXML is always available so default it here.
+  DefaultXmlType = :nokogiri
 
-  # Set to true if nokogiri is set as parser.
-  @@nokogiri_loaded = false
+  @rexml_parser = false
 
-  def self.loadFile(filename, xmlClass = @@defaultXmlType)
+  def self.loadFile(filename, xmlClass = DefaultXmlType)
     self.xml_document(xmlClass).loadFile(filename)
   end
 
-  def self.load(data, xmlClass = @@defaultXmlType)
+  def self.load(data, xmlClass = DefaultXmlType)
     self.xml_document(xmlClass).load(data)
   end
 
-  def self.createDoc(rootName, rootAttrs = nil, version = MIQ_XML_VERSION, xmlClass = @@defaultXmlType)
+  def self.createDoc(rootName, rootAttrs = nil, version = MIQ_XML_VERSION, xmlClass = DefaultXmlType)
     self.xml_document(xmlClass).createDoc(rootName, rootAttrs, version)
   end
 
-  def self.newDoc(xmlClass = @@defaultXmlType)
+  def self.newDoc(xmlClass = DefaultXmlType)
     self.xml_document(xmlClass).newDoc()
   end
 
-  def self.decode(encodedText, xmlClass = @@defaultXmlType)
+  def self.decode(encodedText, xmlClass = DefaultXmlType)
     return self.xml_document(xmlClass).load(MIQEncode.decode(encodedText)) if encodedText
     self.newDoc()
   end
 
-  def self.newNode(data=nil, xmlClass = @@defaultXmlType)
+  def self.newNode(data=nil, xmlClass = DefaultXmlType)
     self.xml_document(xmlClass).newNode(data)
   end
 
@@ -43,12 +42,12 @@ class MiqXml
     begin
       case xmlClass
       when :rexml
+        @rexml_parser = true
         REXML::Document
       when :xmlhash
         XmlHash::Document
       when :nokogiri
         require 'util/xml/miq_nokogiri'
-        @@nokogiri_loaded = true
         Nokogiri::XML::Document
       else
         REXML::Document
@@ -59,24 +58,24 @@ class MiqXml
   end
 
   def self.isXmlElement?(handle)
-    return true if handle.is_a?(REXML::Element) || handle.is_a?(XmlHash::Element)
-    return true if @@nokogiri_loaded && handle.kind_of?(Nokogiri::XML::Node)
+    return true if handle.kind_of?(Nokogiri::XML::Node)
+    return true if rexml_parser? && handle.is_a?(REXML::Element) || handle.is_a?(XmlHash::Element)
     false
   end
 
   def self.isXmlDoc?(handle)
-    return true if handle.is_a?(REXML::Document) || handle.is_a?(XmlHash::Document)
-    return true if @@nokogiri_loaded && handle.is_a?(Nokogiri::XML::Document)
+    return true if handle.is_a?(Nokogiri::XML::Document)
+    return true if rexml_parser? && handle.is_a?(REXML::Document) || handle.is_a?(XmlHash::Document)
     false
   end
 
   def self.isXml?(handle)
-    return true if handle.is_a?(REXML::Element) || handle.is_a?(REXML::Document) || handle.is_a?(XmlHash::Element) || handle.is_a?(XmlHash::Document)
-    return true if @@nokogiri_loaded && (handle.is_a?(Nokogiri::XML::Element) || handle.is_a?(Nokogiri::XML::Document))
+    return true if (handle.is_a?(Nokogiri::XML::Element) || handle.is_a?(Nokogiri::XML::Document))
+    return true if rexml_parser? && handle.is_a?(REXML::Element) || handle.is_a?(REXML::Document) || handle.is_a?(XmlHash::Element) || handle.is_a?(XmlHash::Document)
     false
   end
 
-  def self.is_nokogiri_loaded?
-    @@nokogiri_loaded
+  def self.rexml_parser?
+    @rexml_parser
   end
 end
