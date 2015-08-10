43115a84b8ab2000e881e36cb5ba23c1263d3815
Ari Zellner <azellner@redhat.com>
2015-08-10 15:26:40 +0300

    added tags in UI for container images and container image registries

--- app/models/container_image.rb
+++ app/models/container_image.rb
@@ -4,4 +4,6 @@ class ContainerImage < ActiveRecord::Base
   belongs_to :container_image_registry
   belongs_to :ext_management_system, :foreign_key => "ems_id"
   has_many :containers
+
+  acts_as_miq_taggable
 end
--- app/models/container_image_registry.rb
+++ app/models/container_image_registry.rb
@@ -4,4 +4,6 @@ class ContainerImageRegistry < ActiveRecord::Base
   belongs_to :ext_management_system, :foreign_key => "ems_id"
   has_many :container_images, :dependent => :destroy # What about deleted registry but containers are still running
   has_many :containers, :through => :container_images
+
+  acts_as_miq_taggable
 end
