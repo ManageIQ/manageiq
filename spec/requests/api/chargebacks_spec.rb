24382beb8922a286de7d564e0332c568e65d7998
Tim Wade <hello@timjwade.com>
2015-08-13 10:41:02 -0700

    [wip] edit a chargeback rate detail with PATCH

--- spec/requests/api/chargebacks_spec.rb
+++ spec/requests/api/chargebacks_spec.rb
@@ -82,13 +82,24 @@ RSpec.describe "chargebacks API" do
     expect_request_success
   end
 
-  it "can edit a chargeback rate detail" do
+  it "can edit a chargeback rate detail through POST" do
     chargeback_rate_detail = FactoryGirl.create(:chargeback_rate_detail, :rate => 0)
 
     api_basic_authorize
     run_post rates_url(chargeback_rate_detail.id), gen_request(:edit, :rate => 0.02)
 
     expect(@result["rate"]).to eq("0.02")
+    expect_request_success
+    expect(chargeback_rate_detail.reload.rate).to eq("0.02")
+  end
+
+  it "can edit a chargeback rate detail through PATCH" do
+    chargeback_rate_detail = FactoryGirl.create(:chargeback_rate_detail, :rate => 0)
+
+    api_basic_authorize
+    run_patch rates_url(chargeback_rate_detail.id), [gen_request(:edit, :rate => 0.02)]
+
+    # expect(@result["rate"]).to eq("0.02")
     expect_request_success
     expect(chargeback_rate_detail.reload.rate).to eq("0.02")
   end
