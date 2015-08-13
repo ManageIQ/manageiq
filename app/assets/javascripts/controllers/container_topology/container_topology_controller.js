423540402263d027350175773c66dc3c410d87ea
Alissa Bonas <abonas@redhat.com>
2015-08-13 19:22:40 +0300

    Topology rewrite new rendering, sizes, colors
    Upgraded to topology widget v 0.0.17
    Implemented new rendering introduced by the new version.
    Added custom sizes per element and status colors to the graph.

--- app/assets/javascripts/controllers/container_topology/container_topology_controller.js
+++ app/assets/javascripts/controllers/container_topology/container_topology_controller.js
@@ -34,8 +34,77 @@ angular.module('topologyApp', ['kubernetesUI'])
 }])
 
 .run(function($rootScope) {
-    $rootScope.$on("render", function(ev, vertices) {
-        vertices.selectAll("title").text(function(d) { return "Name: " + d.item.metadata.name + "\nType: " + d.item.kind });
-        ev.preventDefault();
-    })
+        $rootScope.$on("render", function(ev, vertices, added) {
+            /*
+             * We are passed two selections of <g> elements:
+             * vertices: All the elements
+             * added: Just the ones that were added
+             */
+            added.attr("class", function(d) { return d.item.kind; });
+            added.append("circle").attr("r", function(d) { return getDimensions(d).r}).style("stroke", function(d) {
+                        switch (d.item.status) {
+                            case "OK":
+                            case "on":
+                            case "Ready":
+                            case "Running":
+                            case "Succeeded":
+                                return "#3F9C35";
+                            case "NotReady":
+                            case "Failed":
+                                return "#CC0000";
+                            case 'warning':
+                            case 'Pending':
+                                return "#EC7A08";
+                            case 'unknown':
+                                return "#bbb";
+                        }});
+            added.append("title");
+            added.append("image")
+                .attr("xlink:href",function(d) {
+                    var image_url = "/images/icons/new/";
+                    switch (d.item.kind) {
+                        case "Service":
+                        case "Node":
+                        case "Replicator":
+                            image_url += "container_" + d.item.kind.toLowerCase() + ".png";
+                            break;
+                        case "VM":
+                        case "Host":
+                        case "Container":
+                            image_url += d.item.kind.toLowerCase() + ".png";
+                            break;
+                        case "Pod":
+                            image_url += "container_group.png";
+                            break;
+                    }
+                    return image_url;
+                })
+                .attr("y", function(d) { return getDimensions(d).y})
+                .attr("x", function(d) { return getDimensions(d).x})
+                .attr("height", function(d) { return getDimensions(d).height})
+                .attr("width", function(d) { return getDimensions(d).width});
+
+
+            vertices.selectAll("title").text(function(d) { return "Name: " + d.item.name + "\nType: " + d.item.kind + "\nStatus: " + d.item.status });
+            //vertices.selectAll("use").classed("running", function(d) { return true; });
+
+            /* Don't do default rendering */
+            ev.preventDefault();
+        });
+
+        function getDimensions(d) {
+            switch (d.item.kind) {
+                case "Container" :
+                    return { x: -7, y: -7,height: 14, width: 14, r: 13};
+                case "Node" :
+                case "VM" :
+                case "Host" :
+                    return { x: -12, y: -12, height: 23, width: 23, r: 19};
+                default :
+                    return { x: -9, y: -9, height: 18, width: 18, r: 17};
+            }
+
+        }
+
+
 });
