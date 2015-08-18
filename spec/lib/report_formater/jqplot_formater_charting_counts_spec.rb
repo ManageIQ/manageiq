d622fc00a3d4503b9a16941e723a0bdc2b9eea03
Martin Povolny <mpovolny@redhat.com>
2015-08-18 17:51:41 +0200

    Charts: add a spec for count bases 2dim chart.

--- spec/lib/report_formater/jqplot_formater_charting_counts_spec.rb
+++ spec/lib/report_formater/jqplot_formater_charting_counts_spec.rb
@@ -1,7 +1,49 @@
 require 'spec_helper'
 
 describe ReportFormatter::JqplotFormatter do
-  context "build_reporting_chart_other" do
+  context '#build_reporting_chart_dim2' do
+    it 'builds a stacked chart' do
+      report = MiqReport.new(
+        :db        => "Vm",
+        :cols      => %w(os_image_name),
+        :include   => {"ext_management_system"=>{"columns"=>["name"]}},
+        :col_order => ["ext_management_system.name", "os_image_name"],
+        :headers   => ["Cloud/Infrastructure Provider Name", "OS Name"],
+        :order     => "Ascending",
+        :group     => nil,
+        :graph     => {:type=>"StackedBar", :mode=>"counts", :column=>nil, :count=>10, :other=>false},
+        :dims      => 2,
+        :col_options => {},
+        :rpt_options => {},
+        :sortby      => %w(ext_management_system.name os_image_name)
+      )
+
+      report.table = Ruport::Data::Table.new(
+        :column_names => %w(os_image_name ext_management_system.name id),
+        :data => [
+          %w(linux_centos MTC-RHEVM-3.0 10000000000012),
+          %w(linux_centos MTC-RHEVM-3.0 10000000000013),
+          %w(linux_redhat MTC-RHEVM-3.1 10000000000014),
+          %w(linux_centos MTC-RHEVM-3.1 10000000000015),
+        ]
+      )
+
+      expect_any_instance_of(described_class).to receive(:build_reporting_chart_dim2).once.and_call_original
+
+      ReportFormatter::ReportRenderer.render(Charting.format) do |e|
+        e.options.mri           = report
+        e.options.show_title    = true
+        e.options.graph_options = MiqReport.graph_options(600, 400)
+        e.options.theme         = 'miq'
+      end
+
+      expect(report.chart[:data]).to eq([[2, 1], [0, 1]])
+      expect(report.chart[:options][:seriesDefaults][:renderer]).to eq("jQuery.jqplot.BarRenderer")
+      expect(report.chart[:options][:series]).to eq([{:label=>"linux_centos"}, {:label=>"linux_redhat"}])
+    end
+  end
+
+  context "#build_reporting_chart_other" do
     it 'builds a pie chart' do
       report = MiqReport.new(
         :db          => "Host",
