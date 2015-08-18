8ca0bab5dec3914e84461a6e03780fbc47a27221
Keenan Brock <kbrock@redhat.com>
2015-08-17 21:30:31 -0400

    add migration spec for clear_tenant_seed

---
+++ spec/migrations/20150817213409_clear_tenant_seed_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+require Rails.root.join("db/migrate/20150817213409_clear_tenant_seed")
+
+describe ClearTenantSeed do
+  migration_context :up do
+    it "works with one tenants" do
+      tenant = ClearTenantSeed::Tenant.create(:name => 'My Company')
+      migrate
+      expect(tenant.reload.name).to be_nil
+    end
+  end
+end
