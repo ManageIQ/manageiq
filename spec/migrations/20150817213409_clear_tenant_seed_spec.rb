243e9c1250cae106a3952fe26d9897fcf6f821b4
Keenan Brock <kbrock@redhat.com>
2015-08-18 14:58:42 -0400

    update migration to use active record

--- spec/migrations/20150817213409_clear_tenant_seed_spec.rb
+++ spec/migrations/20150817213409_clear_tenant_seed_spec.rb
@@ -4,7 +4,7 @@ require Rails.root.join("db/migrate/20150817213409_clear_tenant_seed")
 describe ClearTenantSeed do
   migration_context :up do
     it "works with one tenants" do
-      tenant = ClearTenantSeed::Tenant.create(:name => 'My Company')
+      tenant = migration_stub(:Tenant).create(:name => 'My Company')
       migrate
       expect(tenant.reload.name).to be_nil
     end
