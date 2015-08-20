2a4a4164ec13d9793978691c20848f653567f717
Federico Simoncelli <fsimonce@redhat.com>
2015-08-20 18:51:37 -0400

    containers: add kubernetes images scanning
    This patch introduces the smart state analysis support for container
    images.
    
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

--- app/models/container_image.rb
+++ app/models/container_image.rb
@@ -1,10 +1,18 @@
 class ContainerImage < ActiveRecord::Base
   include ReportableMixin
+  include ScanningMixin
+
+  DOCKER_IMAGE_PREFIX = "docker://"
 
   belongs_to :container_image_registry
   belongs_to :ext_management_system, :foreign_key => "ems_id"
   has_many :containers
   has_many :guest_applications, :dependent => :destroy
+  has_one :computer_system, :as => :managed_entity, :dependent => :destroy
+  has_one :operating_system, :through => :computer_system
+
+  # Needed for scanning
+  delegate :my_zone, :to => :ext_management_system
 
   acts_as_miq_taggable
 
@@ -17,7 +25,31 @@ class ContainerImage < ActiveRecord::Base
     result
   end
 
-  def scan
-    raise 'Feature not implemented'
+  def operating_system=(value)
+    create_computer_system if computer_system.nil?
+    computer_system.operating_system = value
   end
+
+  def docker_id
+    if image_ref.start_with?(DOCKER_IMAGE_PREFIX)
+      return image_ref[DOCKER_IMAGE_PREFIX.length..-1]
+    end
+  end
+
+  # The guid is required by the smart analysis infrastructure
+  alias_method :guid, :docker_id
+
+  def scan
+    ext_management_system.scan_job_create(self.class.name, id)
+  end
+
+  def perform_metadata_scan(ost)
+    update!(:last_scan_attempt_on => Time.zone.now.utc)
+    miq_cnt_group = ext_management_system.scan_entity_create(ost.scanData)
+    # TODO: update smart state infrastructure with a better name
+    # than scan_via_miq_vm
+    scan_via_miq_vm(miq_cnt_group, ost)
+  end
+
+  alias_method :perform_metadata_sync, :sync_stashed_metadata
 end
--- spec/models/container_image_spec.rb
+++ spec/models/container_image_spec.rb
@@ -12,4 +12,21 @@ describe ContainerImage do
     image.container_image_registry = reg
     expect(image.full_name).to eq("docker.io:1234/fedora:v1")
   end
+
+  it "#docker_id" do
+    image = FactoryGirl.create(:container_image, :image_ref => "docker://id")
+    expect(image.docker_id).to eq("id")
+
+    image = FactoryGirl.create(:container_image, :image_ref => "rocket://id")
+    expect(image.docker_id).to eq(nil)
+  end
+
+  it "#operating_system=" do
+    image = FactoryGirl.create(:container_image)
+    expect(image.computer_system).to be_nil
+
+    image.operating_system = FactoryGirl.create(:operating_system)
+    expect(image.computer_system).not_to be_nil
+    expect(image.operating_system).to eq(image.computer_system.operating_system)
+  end
 end
