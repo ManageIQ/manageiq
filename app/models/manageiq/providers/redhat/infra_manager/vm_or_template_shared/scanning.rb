ce3291ca6e0f353eb2c16db0191228024585d337
Richard Oliveri <roliveri@redhat.com>
2015-08-24 17:07:40 -0400

    Fixes for various RHEVM fleecing issues.
    Fix for RHEV 3.5 - ovf file no longer on NFS share.
    Fix for NFS permission problem - uid changed when opening files on share.
    Fix for environments with both, NFS and LUN based storage.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1202895
    https://bugzilla.redhat.com/show_bug.cgi?id=1024033

--- app/models/manageiq/providers/redhat/infra_manager/vm_or_template_shared/scanning.rb
+++ app/models/manageiq/providers/redhat/infra_manager/vm_or_template_shared/scanning.rb
@@ -10,6 +10,7 @@ module ManageIQ::Providers::Redhat::InfraManager::VmOrTemplateShared::Scanning
     args1['ems']['connect'] = true if args1[:mount].blank?
 
     begin
+      $log.debug "perform_metadata_scan: vm_name = #{vm_name}"
       @vm_cfg_file = vm_name
       connect_to_ems(ost)
       miq_vm = MiqRhevmVm.new(@vm_cfg_file, ost)
@@ -20,7 +21,6 @@ module ManageIQ::Providers::Redhat::InfraManager::VmOrTemplateShared::Scanning
       raise
     ensure
       miq_vm.unmount if miq_vm
-      unmount_storage(@mount)
     end
   end
 
@@ -31,17 +31,9 @@ module ManageIQ::Providers::Redhat::InfraManager::VmOrTemplateShared::Scanning
   private
 
   # Moved from MIQExtract.rb
+  # TODO: Should this be in the ems?
   def connect_to_ems(ost)
     log_header = "MIQ(#{self.class.name}.#{__method__})"
-    @mount = storage_mounts(ost.taskid)
-    unless @mount.blank?
-      ost.nfs_mount = true
-      $rhevm_mount_root = @mount[:base_dir] # XXX $ ?
-      $log.info "#{log_header} Mounting storage for VM at <#{$rhevm_mount_root}>"
-      mount_storage(@mount)
-      @vm_cfg_file = File.join($rhevm_mount_root, @vm_cfg_file)
-      return
-    end
 
     # Check if we've been told explicitly not to connect to the ems
     # TODO: See vm_scan.rb: config_ems_list() - is this always false for RedHat?
@@ -91,78 +83,5 @@ module ManageIQ::Providers::Redhat::InfraManager::VmOrTemplateShared::Scanning
         raise err, msg, err.backtrace
       end
     end
-  end
-
-  # Moved from MIQExtract.rb
-  def mount_storage(mount_hash)
-    require 'util/mount/miq_nfs_session'
-    log_header = "MIQ(MIQExtract.mount_storage)"
-    $log.info "#{log_header} called"
-
-    begin
-      FileUtils.mkdir_p(mount_hash[:link_path]) unless File.directory?(mount_hash[:link_path])
-      mount_hash[:mount_points].each do |mnt|
-        $log.info "#{log_header} Creating mount point <#{mnt.inspect}>"
-        MiqNfsSession.new(mnt).connect
-      end
-      $log.info "#{log_header} - mount:\n#{`mount`}"
-      mount_hash[:symlinks].each do |link|
-        $log.info "#{log_header} Creating symlink <#{link.inspect}>"
-        File.symlink(link[:source], link[:target])
-      end
-    rescue
-      $log.error "#{log_header} Unable to mount all items from <#{mount_hash[:base_dir]}>"
-      unmount_storage(mount_hash)
-      raise $!
-    end
-  end
-
-  # Moved from MIQExtract.rb
-  def unmount_storage(mount_hash)
-    log_header = "MIQ(MIQExtract.unmount_storage)"
-    $log.debug "#{log_header}: mount_hash = #{mount_hash.class.name}"
-    return if mount_hash.blank?
-    begin
-      $log.warn "#{log_header} Unmount all items from <#{mount_hash[:base_dir]}>"
-      mount_hash[:mount_points].each { |mnt| MiqNfsSession.disconnect(mnt[:mount_point]) }
-      FileUtils.rm_rf(mount_hash[:base_dir])
-    rescue
-      $log.warn "#{log_header} Failed to unmount all items from <#{mount_hash[:base_dir]}>.  Reason: <#{$!}>"
-    end
-  end
-
-  # Helper method for VM scanning
-  # Moved from providers/redhat/infra_manager.rb
-  def storage_mounts(job_id = nil)
-    return nil unless storage && storage.store_type == "NFS"
-
-    datacenter = parent_datacenter
-    raise "VM <#{name}> is not attached to a Data-center" if datacenter.blank?
-
-    base_path = File.join('/mnt', 'vm', job_id)
-    base_rhevm_path = File.join(base_path, 'rhev', 'data-center')
-    mnt_path  = File.join(base_rhevm_path, 'mnt')
-    link_path = File.join(base_rhevm_path, datacenter.uid_ems)
-
-    # Find the storages we need to mount to access this VM
-    hms = storage.hosts.first.storages.detect { |s| s.master? && s.store_type == "NFS" }
-    storages = [storage, hms].uniq.compact
-
-    result = {:mount_points => [], :symlinks => [], :base_dir => base_path, :link_path => link_path}
-    storages.each do |s|
-      uri = s.location.gsub('//', ':/').strip
-      s_mnt_path = File.join(mnt_path, uri.gsub('/', '_'))
-      result[:mount_points] << mount_parms = {:uri => "nfs://#{uri}", :mount_point => s_mnt_path}
-
-      storage_guid = s.ems_ref.split('/').last
-      link_name = File.join(link_path, storage_guid)
-      link_mnt_path = File.join(mount_parms[:mount_point], storage_guid)
-
-      # Determine what symlinks we need to make
-      result[:symlinks] << {:source => link_mnt_path, :target => link_name}
-      result[:symlinks] << {:source => link_mnt_path, :target => File.join(link_path, 'mastersd')} if s.master?
-    end
-
-    result
   end
 end
