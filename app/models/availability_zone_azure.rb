6f1585ead410ddc2e8e565f92d7eb8dd45beee65
Bronagh Sorota <bsorota@redhat.com>
2015-08-24 13:16:07 -0400

    Post review changes Specs to test connectivity to Azure provider

---
+++ app/models/availability_zone_azure.rb
@@ -0,0 +1,6 @@
+class AvailabilityZoneAzure < ::AvailabilityZone
+  def provider_object(connection = nil)
+    connection ||= ext_management_system.connect
+    connection.availability_zones[ems_ref]
+  end
+end
--- app/models/ems_refresh/parsers/azure.rb
+++ app/models/ems_refresh/parsers/azure.rb
@@ -1,7 +1,9 @@
 module EmsRefresh
   module Parsers
     class Azure < ManageIQ::Providers::CloudManager::RefreshParser
-      VALID_LOCATION = /\w+/i
+      include Vmdb::Logging
+
+      VALID_LOCATION = /\w+/
 
       def self.ems_inv_to_hashes(ems, options = nil)
         new(ems, options).ems_inv_to_hashes
@@ -9,9 +11,9 @@ module EmsRefresh
 
       def initialize(ems, options = nil)
         @ems        = ems
-        ems.connect
-        @vmm        = ::Azure::Armrest::VirtualMachineManager.new
-        @asm        = ::Azure::Armrest::AvailabilitySetManager.new
+        config      = ems.connect
+        @vmm        = ::Azure::Armrest::VirtualMachineService.new(config)
+        @asm        = ::Azure::Armrest::AvailabilitySetService.new(config)
         @options    = options || {}
         @data       = {}
         @data_index = {}
@@ -20,12 +22,12 @@ module EmsRefresh
       def ems_inv_to_hashes
         log_header = "MIQ(#{self.class.name}.#{__method__}) Collecting data for EMS : [#{@ems.name}] id: [#{@ems.id}]"
 
-        $log.info("#{log_header}...")
+        _log.info("#{log_header}...")
         get_series
         get_availability_sets
-        get_vms
+        get_instances
         clean_up_extra_flavor_keys
-        $log.info("#{log_header}...Complete")
+        _log.info("#{log_header}...Complete")
 
         @data
       end
@@ -37,11 +39,12 @@ module EmsRefresh
         get_locations.each do |location|
           begin
             series << @vmm.series(location)
-          rescue RestClient::BadGateway
+          rescue RestClient::BadGateway, RestClient::GatewayTimeout, RestClient::BadRequest
             next
           end
         end
-        series = series.flatten!.uniq
+        series = series.flatten
+        series = series.uniq
         process_collection(series, :flavors) { |s| parse_series(s) }
       end
 
@@ -50,9 +53,9 @@ module EmsRefresh
         process_collection(a_zones, :availability_zones) { |az| parse_az(az) }
       end
 
-      def get_vms
-        vms = @vmm.get_vms
-        process_collection(vms, :vms) { |instance| parse_vm(instance) }
+      def get_instances
+        instances = @vmm.get_vms
+        process_collection(instances, :vms) { |instance| parse_instance(instance) }
       end
 
       def process_collection(collection, key)
@@ -70,7 +73,7 @@ module EmsRefresh
       def parse_series(s)
         name = uid = s['name']
         new_result = {
-          :type           => "SeriesAzure",
+          :type           => "FlavorAzure",
           :ems_ref        => uid,
           :name           => name,
           :cpus           => s['numberOfCores'], # where are the virtual CPUs??
@@ -78,7 +81,7 @@ module EmsRefresh
           :memory         => s['memoryInMB'].to_f,
 
           # Extra keys
-          :os_disk_size   => (s['osDiskSizeInMB'] * 1024),
+          :os_disk_size   => s['osDiskSizeInMB'] * 1024,
           :swap_disk_size => s['resourceDiskSizeInMB']
 
         }
@@ -86,44 +89,42 @@ module EmsRefresh
       end
 
       def parse_az(az)
-        name = az["name"]
-        id   = az["id"].downcase
+        id = az["id"].downcase
 
         new_result = {
-          :type    => EmsAzure::AvailabilityZone.name,
+          :type    => "AvailabilityZoneAzure",
           :ems_ref => id,
-          :name    => name,
+          :name    => az["name"],
         }
         return id, new_result
       end
 
-      def parse_vm(vm)
-        uid               = vm.fetch_path('resourceGroup') + "\\" + vm.fetch_path('name')
-        series_name       = vm.fetch_path('properties', 'hardwareProfile', 'vmSize')
-        az                = vm.fetch_path('properties', 'availabilitySet', 'id')
+      def parse_instance(instance)
+        uid               = instance.fetch_path('resourceGroup') + "\\" + instance.fetch_path('name')
+        series_name       = instance.fetch_path('properties', 'hardwareProfile', 'vmSize')
+        az                = instance.fetch_path('properties', 'availabilitySet', 'id')
         series            = @data_index.fetch_path(:flavors, series_name)
 
         new_result = {
           :type             => 'VmAzure',
           :uid_ems          => uid,
           :ems_ref          => uid,
-          :name             => vm.fetch_path('name'),
+          :name             => instance.fetch_path('name'),
           :vendor           => "Microsoft",
-          :raw_power_state  => vm["powerStatus"],
-          :operating_system => process_os(vm),
+          :raw_power_state  => instance["powerStatus"],
+          :operating_system => process_os(instance),
           :flavor           => series,
           :location         => uid,
           :hardware         => {
             :disks    => [], # Filled in later conditionally on flavor
             :networks => [], # Filled in later conditionally on what's available
-            # Do we need to set the guest_os??
           },
         }
         new_result[:availability_zone] = fetch_az(az) unless az.nil?
 
-        populate_hardware_hash_with_disks(new_result[:hardware][:disks], vm)
-        populate_hardware_hash_with_series(new_result[:hardware], vm, series)
-        populate_hardware_hash_with_networks(new_result[:hardware][:networks], vm)
+        populate_hardware_hash_with_disks(new_result[:hardware][:disks], instance)
+        populate_hardware_hash_with_series_attributes(new_result[:hardware], instance, series)
+        populate_hardware_hash_with_networks(new_result[:hardware][:networks], instance)
 
         return uid, new_result
       end
@@ -132,19 +133,19 @@ module EmsRefresh
         @data_index.fetch_path(:availability_zones, availability_zone.downcase)
       end
 
-      def process_os(vm)
+      def process_os(instance)
         {
-          :product_name => guest_os(vm)
+          :product_name => guest_os(instance)
         }
       end
 
-      def guest_os(vm)
-        image_reference = vm.fetch_path('properties', 'storageProfile', 'imageReference')
-        image_reference['offer'] + " "  + image_reference['sku'].gsub('-', ' ')
+      def guest_os(instance)
+        image_reference = instance.fetch_path('properties', 'storageProfile', 'imageReference')
+        image_reference['offer'] + " " + image_reference['sku'].gsub('-', ' ')
       end
 
-      def populate_hardware_hash_with_disks(hardware_disks_array, vm)
-        data_disks = vm.fetch_path('properties', 'storageProfile', 'dataDisks')
+      def populate_hardware_hash_with_disks(hardware_disks_array, instance)
+        data_disks = instance.fetch_path('properties', 'storageProfile', 'dataDisks')
         data_disks.each do |disk|
           disk_size      = disk['diskSizeGB'] * 1.gigabyte
           disk_name      = disk['name']
@@ -158,8 +159,8 @@ module EmsRefresh
         super(disks, size, name, location, "azure")
       end
 
-      def populate_hardware_hash_with_networks(hardware_networks_array, vm)
-        nics = vm.fetch_path('properties', 'networkProfile', 'networkInterfaces')
+      def populate_hardware_hash_with_networks(hardware_networks_array, instance)
+        nics = instance.fetch_path('properties', 'networkProfile', 'networkInterfaces')
 
         nics.each do |n|
           n['properties'].each do |n_prop|
@@ -176,23 +177,22 @@ module EmsRefresh
             hardware_networks_array <<
               private_network.merge(:description => "private") unless private_network.blank?
             hardware_networks_array <<
-              public_network.merge(:description => "public")   unless public_network.blank?
+              public_network.merge(:description => "public") unless public_network.blank?
           end
           hardware_networks_array.flatten!
         end
       end
 
-      def populate_hardware_hash_with_series(hardware_hash, vm, series)
+      def populate_hardware_hash_with_series_attributes(hardware_hash, instance, series)
         return if series.nil?
-        hardware_hash[:numvcpus]      = series[:cpus]
         hardware_hash[:logical_cpus]  = series[:cpus]
         hardware_hash[:memory_cpu]    = series[:memory] # MB
         hardware_hash[:disk_capacity] = series[:os_disk_size] + series[:swap_disk_size]
 
-        os_disk = vm.fetch_path('properties', 'storageProfile', 'osDisk')
+        os_disk = instance.fetch_path('properties', 'storageProfile', 'osDisk')
         sz      = series[:os_disk_size]
 
-        add_instance_disk(hardware_hash[:disks], sz,  os_disk['name'], os_disk['vhd']) unless sz.zero?
+        add_instance_disk(hardware_hash[:disks], sz, os_disk['name'], os_disk['vhd']) unless sz.zero?
 
         # No data availbale on swap disk? Called temp or resource disk.
       end
@@ -206,7 +206,7 @@ module EmsRefresh
 
       def get_locations
         @vmm.locations.collect do |location|
-          location.delete!(' ')
+          location = location.delete(' ')
           location.match(VALID_LOCATION).to_s
         end
       end
