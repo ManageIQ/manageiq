4c6cd98cc7de4a16d63f394d40e44314e1459b5e
Ofer Schreiber <oschreib@redhat.com>
2015-08-24 18:05:41 +0300

    Add Smart Analysis to Container Images
    Add the ability to scan container images from UI.

--- app/helpers/application_helper/toolbar_builder.rb
+++ app/helpers/application_helper/toolbar_builder.rb
@@ -482,9 +482,6 @@ class ApplicationHelper::ToolbarBuilder
     return true if %w(container_replicator_edit container_replicator_delete container_replicator_new).include?(id) &&
                    (@record.kind_of?(ContainerReplicator) || @record.nil?)
 
-    return true if %w(container_image_edit container_image_delete container_image_new).include?(id) &&
-                   (@record.kind_of?(ContainerImage) || @record.nil?)
-
     return true if %w(container_image_registry_edit container_image_registry_delete
                       container_image_registry_new).include?(id) &&
                    (@record.kind_of?(ContainerImageRegistry) || @record.nil?)
