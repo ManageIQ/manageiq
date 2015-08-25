ba03bcee495c323ade7d50c3b92b255e43b1bee8
Gregg Tanzillo <gtanzill@redhat.com>
2015-08-25 09:01:02 -0400

    Fixing broken test.
    https://trello.com/c/4SQCuzey

--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -81,18 +81,18 @@ describe Tenant do
 
   describe "#display_type" do
     before do
-      tenant  = FactoryGirl.create(:tenant, :parent => default_tenant)
-      project = FactoryGirl.create(:tenant, :parent => default_tenant, :divisible => false)
+      @tenant  = FactoryGirl.create(:tenant, :parent => default_tenant)
+      @project = FactoryGirl.create(:tenant, :parent => default_tenant, :divisible => false)
     end
 
     it "detects Tenant" do
-      expect(tenant.display_type).to eql  'Tenant'
-      expect(project.display_type).not_to eql  'Tenant'
+      expect(@tenant.display_type).to eql  'Tenant'
+      expect(@project.display_type).not_to eql  'Tenant'
     end
 
     it "detects Project" do
-      expect(project.display_type).to eql 'Project'
-      expect(tenant.display_type).not_to eql 'Project'
+      expect(@project.display_type).to eql 'Project'
+      expect(@tenant.display_type).not_to eql 'Project'
     end
   end
 
