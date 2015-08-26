21895ed84ac436ca4194d12044627eaed8e8fe56
Keenan Brock <kbrock@redhat.com>
2015-08-26 13:55:32 -0400

    stub_server_configuration: add merge_from_template
    some tests call merge_from_template_if_missing on configuration
    This stubs that out.

--- spec/support/configuration_helper.rb
+++ spec/support/configuration_helper.rb
@@ -1,5 +1,6 @@
 module ConfigurationHelper
   def stub_server_configuration(config, config_name = "vmdb")
-    allow(VMDB::Config).to receive(:new).with(config_name).and_return(double(:config => config))
+    configuration = double(:config => config, :merge_from_template_if_missing => nil)
+    allow(VMDB::Config).to receive(:new).with(config_name).and_return(configuration)
   end
 end
