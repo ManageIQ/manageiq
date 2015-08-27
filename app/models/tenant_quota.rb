27677fc30fc96917d133aab60fb82017ed810e10
Gregg Tanzillo <gtanzill@redhat.com>
2015-08-27 15:39:05 -0400

    Implemented methods to enable UI setting and getting tenant quotas.
    https://trello.com/c/75lCokIW

--- app/models/tenant_quota.rb
+++ app/models/tenant_quota.rb
@@ -1,3 +1,60 @@
 class TenantQuota < ActiveRecord::Base
   belongs_to :tenant
+
+  QUOTAS = {
+    :cpu_allocated => {
+      :unit   => :mhz,
+      :format => :mhz
+    },
+    :mem_allocated => {
+      :unit   => :bytes,
+      :format => :gigabytes_human
+    },
+    :storage_allocated => {
+      :unit   => :bytes,
+      :format => :gigabytes_human
+    },
+    :vms_allocated => {
+      :unit   => :fixnum,
+      :format => :general_number_precision_0
+    },
+    :templates_allocated => {
+      :unit   => :fixnum,
+      :format => :general_number_precision_0
+    }
+  }
+
+  NAMES = QUOTAS.stringify_keys
+
+  validates_inclusion_of :name, :in => NAMES
+  validates_presence_of  :unit, :value
+
+  def self.available
+    QUOTAS
+  end
+
+  def self.get(tenant)
+    where(:tenant => tenant).inject({}) do |h, q|
+      h[q.name.to_sym] = {:unit => q.unit, :value => q.value, :format => q.format}
+      h
+    end
+  end
+
+  def self.set(tenant, quotas)
+    quotas.each do |name, values|
+      q = where(:tenant => tenant, :name => name).last || new(values.merge(:tenant => tenant, :name => name))
+      q.unit ||= q.default_unit
+      q.update_attributes!(values)
+    end
+    # Deletes
+    destroy_all(:tenant => tenant, :name => (available.keys.sort - quotas.symbolize_keys.keys.sort))
+  end
+
+  def format
+    QUOTAS.fetch_path(name.to_sym, :format).to_s
+  end
+
+  def default_unit
+    QUOTAS.fetch_path(name.to_sym, :unit).to_s
+  end
 end
--- spec/models/tenant_quota_spec.rb
+++ spec/models/tenant_quota_spec.rb
@@ -1,5 +1,116 @@
 require 'spec_helper'
 
 describe TenantQuota do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:settings) { {} }
+  let(:tenant) { Tenant.new(:domain => 'x.com', :parent => default_tenant) }
+
+  let(:default_tenant) do
+    Tenant.seed
+    Tenant.default_tenant
+  end
+
+  let(:root_tenant) do
+    Tenant.seed
+    Tenant.root_tenant
+  end
+
+  context "validations" do
+    it "rejects invalid name" do
+      expect {
+        described_class.create!(:name => "XXX")
+      }.to raise_error
+    end
+
+    it "rejects missing value" do
+      expect {
+        described_class.create!(:name => :cpu_allocated, :unit => "mhz")
+      }.to raise_error
+    end
+
+    it "rejects missing unit" do
+      expect {
+        described_class.create!(:name => :cpu_allocated, :value => 4096)
+      }.to raise_error
+    end
+  end
+
+  describe ".set" do
+    it "adds new quotas" do
+      described_class.set(default_tenant, :cpu_allocated => {:value => 1024}, :vms_allocated => {:value => 20}, :mem_allocated => {:value => 4096})
+
+      default_tenant.reload
+
+      tq = default_tenant.tenant_quotas.order(:name)
+      expect(tq.length).to eql 3
+
+      tq_cpu = tq[0]
+      expect(tq_cpu.name).to eql "cpu_allocated"
+      expect(tq_cpu.unit).to eql "mhz"
+      expect(tq_cpu.format).to eql "mhz"
+      expect(tq_cpu.value).to eql 1024.0
+
+      tq_mem = tq[1]
+      expect(tq_mem.name).to eql "mem_allocated"
+      expect(tq_mem.unit).to eql "bytes"
+      expect(tq_mem.format).to eql "gigabytes_human"
+      expect(tq_mem.value).to eql 4096.0
+
+
+      tq_vms = tq[2]
+      expect(tq_vms.name).to eql "vms_allocated"
+      expect(tq_vms.unit).to eql "fixnum"
+      expect(tq_vms.format).to eql "general_number_precision_0"
+      expect(tq_vms.value).to eql 20.0
+    end
+
+    it "updates existing quotas" do
+      described_class.set(default_tenant, :vms_allocated => {:value => 20})
+
+      tq = described_class.last
+      expect(tq.value).to eql 20.0
+
+      described_class.set(default_tenant, :vms_allocated => {:value => 40})
+
+      tq.reload
+      expect(tq.value).to eql 40.0
+    end
+
+    it "deletes existing quotas" do
+      described_class.set(default_tenant, :cpu_allocated => {:value => 1024}, :vms_allocated => {:value => 20}, :mem_allocated => {:value => 4096})
+      default_tenant.reload
+
+      tq = default_tenant.tenant_quotas
+      expect(tq.length).to eql 3
+
+      described_class.set(default_tenant, :vms_allocated => {:value => 20}, :mem_allocated => {:value => 4096})
+      default_tenant.reload
+
+      tq = default_tenant.tenant_quotas
+      expect(tq.length).to eql 2
+      expect(tq.map(&:name).sort).to eql ['mem_allocated', 'vms_allocated']
+    end
+  end
+
+  describe ".get" do
+    it "gets existing quotas" do
+      described_class.set(default_tenant, :vms_allocated => {:value => 20}, :mem_allocated => {:value => 4096})
+
+      default_tenant.reload
+
+      expected =       {
+          :vms_allocated => {
+              :unit => "fixnum",
+              :value => 20.0,
+              :format => "general_number_precision_0"
+          },
+          :mem_allocated => {
+              :unit => "bytes",
+              :value => 4096.0,
+              :format => "gigabytes_human"
+          }
+      }
+
+      expect(described_class.get(default_tenant)).to eql expected
+    end
+  end
 end
