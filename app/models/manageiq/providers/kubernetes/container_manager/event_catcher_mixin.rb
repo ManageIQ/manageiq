d00ca21aa4a86c343fe6e51b40d5fbfd0f7bde84
dkorn <dkorn@redhat.com>
2015-08-27 16:35:31 +0300

    Moving container enabled events from config to event catcher

--- app/models/manageiq/providers/kubernetes/container_manager/event_catcher_mixin.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/event_catcher_mixin.rb
@@ -1,5 +1,9 @@
 module ManageIQ::Providers::Kubernetes::ContainerManager::EventCatcherMixin
   extend ActiveSupport::Concern
+  ENABLED_EVENTS = {
+    'Node' => %w(NodeReady NodeNotReady rebooted),
+    'Pod'  => %w(scheduled failedScheduling failedValidation HostPortConflict created failed started killing)
+  }
 
   def event_monitor_handle
     require 'kubernetes/events/kubernetes_event_monitor'
@@ -44,8 +48,11 @@ module ManageIQ::Providers::Kubernetes::ContainerManager::EventCatcherMixin
       :message   => event.object.message
     }
 
-    @enabled_events ||= worker_settings[:enabled_events]
-    supported_reasons = @enabled_events[event_data[:kind]] || []
+    unless event.object.involvedObject.fieldPath.nil?
+      event_data[:fieldpath] = event.object.involvedObject.fieldPath
+    end
+
+    supported_reasons = ENABLED_EVENTS[event_data[:kind]] || []
 
     unless supported_reasons.include?(event_data[:reason])
       _log.debug "#{log_prefix} Discarding event [#{event_data}]"
