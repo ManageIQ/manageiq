a5b501266b7ec297cd28fcfbf49c62fb620fdc8d
Keenan Brock <kbrock@redhat.com>
2015-08-26 22:13:11 -0400

    replace update_attribute with update_all

--- db/migrate/20150818181427_update_tenant_divisible_on_existing_rows.rb
+++ db/migrate/20150818181427_update_tenant_divisible_on_existing_rows.rb
@@ -1,7 +1,8 @@
 class UpdateTenantDivisibleOnExistingRows < ActiveRecord::Migration
   class Tenant < ActiveRecord::Base
   end
+
   def up
-    Tenant.where(:divisible => nil).each { |t| t.update_attribute(:divisible, true) }
+    Tenant.where(:divisible => nil).update_all(:divisible => true)
   end
 end
