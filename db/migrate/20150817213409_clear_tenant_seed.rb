4414394bb85aef718ad5c28c45c6bae3019c8d2c
Keenan Brock <kbrock@redhat.com>
2015-08-27 23:26:33 -0400

    tenant migrations: add say_with_time

--- db/migrate/20150817213409_clear_tenant_seed.rb
+++ db/migrate/20150817213409_clear_tenant_seed.rb
@@ -1,8 +1,11 @@
 class ClearTenantSeed < ActiveRecord::Migration
   class Tenant < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled # disable STI
   end
 
   def up
-    Tenant.update_all(:name => nil)
+    say_with_time("Setting root tenant to default to settings") do
+      Tenant.update_all(:name => nil)
+    end
   end
 end
--- db/migrate/20150818181427_update_tenant_divisible_on_existing_rows.rb
+++ db/migrate/20150818181427_update_tenant_divisible_on_existing_rows.rb
@@ -1,8 +1,11 @@
 class UpdateTenantDivisibleOnExistingRows < ActiveRecord::Migration
   class Tenant < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled # disable STI
   end
 
   def up
-    Tenant.where(:divisible => nil).update_all(:divisible => true)
+    say_with_time("marking root tenant divisible") do
+      Tenant.where(:divisible => nil).update_all(:divisible => true)
+    end
   end
 end
