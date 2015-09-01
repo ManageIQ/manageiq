5d6d7324366a88a021c7a6dab5f09eec23533f76
Federico Simoncelli <fsimonce@redhat.com>
2015-09-01 12:57:27 -0400

    containers: fix timeline displaying errors
    The addition of the topology widget has introduced a regression in
    displaying the timeline:
    
      FATAL -- : Error caught: [ActionView::MissingTemplate]
      Missing partial ems_container/_timeline, application/_timeline with
      {:locale=>[:en], :formats=>[:html], :variants=>[], :handlers=>[:erb,
       :builder, :raw, :ruby, :haml, :jbuilder, :rjs]}. Searched in:
      * "/home/simon/manageiq/app/views"
    
    This patch restore the correct beahvior of the timeline.
    
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

--- spec/controllers/ems_container_controller_spec.rb
+++ spec/controllers/ems_container_controller_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 describe EmsContainerController do
   before(:each) do
+    session[:settings] = {:views => {}}
     set_user_privileges
   end
 
@@ -13,6 +14,11 @@ describe EmsContainerController do
     expect(controller.stub(:edit)).to_not be_nil
   end
 
+  it "#show" do
+    ems = FactoryGirl.create(:ems_kubernetes)
+    get :show, :id => ems.id
 
+    expect(response.status).to eq(200)
+    expect(response).to render_template('ems_container/show')
   end
 end
