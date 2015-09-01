481e864df6e6eae06c19366452f628c5094f0117
Brandon Dunne <bdunne@redhat.com>
2015-09-01 12:35:29 -0400

    Rework spec to test mount point with and without base directory
    #connect shouldn't test for uniqueness

--- gems/pending/spec/util/mount/miq_generic_mount_session_spec.rb
+++ gems/pending/spec/util/mount/miq_generic_mount_session_spec.rb
@@ -2,26 +2,27 @@ require "spec_helper"
 require 'util/mount/miq_generic_mount_session'
 
 describe MiqGenericMountSession do
-  context "#connect" do
-    before do
-      MiqGenericMountSession.stub(:raw_disconnect)
-      @s1 = MiqGenericMountSession.new(:uri => '/tmp/abc', :mount_point => 'tmp')
-      @s2 = MiqGenericMountSession.new(:uri => '/tmp/abc', :mount_point => 'tmp')
+  it "#connect returns a string pointing to the mount point" do
+    described_class.stub(:raw_disconnect)
+    s = described_class.new(:uri => '/tmp/abc')
+    s.logger = Logger.new("/dev/null")
 
-      @s1.logger = Logger.new("/dev/null")
-      @s2.logger = Logger.new("/dev/null")
+    expect(s.connect).to match(/\A\/tmp\/miq_\d{8}-\d{5}-\w+\z/)
+
+    s.disconnect
+  end
+
+  context "#mount_share" do
+    it "without :mount_point uses default temp directory as a base" do
+      expect(described_class.new(:uri => '/tmp/abc').mount_share).to match(/\A\/tmp\/miq_\d{8}-\d{5}-\w+\z/)
     end
 
-    after do
-      @s1.disconnect
-      @s2.disconnect
+    it "with :mount_point uses specified directory as a base" do
+      expect(described_class.new(:uri => '/tmp/abc', :mount_point => "abc").mount_share).to match(/\Aabc\/miq_\d{8}-\d{5}-\w+\z/)
     end
 
     it "is unique" do
-      @s1.connect
-      @s2.connect
-
-      expect(@s1.mnt_point).to_not eq(@s2.mnt_point)
+      expect(described_class.new(:uri => '/tmp/abc').mount_share).to_not eq(described_class.new(:uri => '/tmp/abc').mount_share)
     end
   end
 end
