7f4fc60041d1fc6e130b29575d4fffd677b300e2
Brandon Dunne <bdunne@redhat.com>
2015-09-01 18:15:05 -0400

    Always return a chassis status
    Move #connected? test out of power ops context

--- gems/pending/spec/util/miq-ipmi_spec.rb
+++ gems/pending/spec/util/miq-ipmi_spec.rb
@@ -2,11 +2,14 @@ require "spec_helper"
 require "util/miq-ipmi"
 
 describe MiqIPMI do
+  let(:chassis_status_response) { {:result => "System Power         : off\nPower Overload       : false\nPower Interlock      : inactive\nMain Power Fault     : false\nPower Control Fault  : false\nPower Restore Policy : previous\nLast Power Event     : command\nChassis Intrusion    : inactive\nFront-Panel Lockout  : inactive\nDrive Fault          : false\nCooling/Fan Fault    : false\nSleep Button Disable : not allowed\nDiag Button Disable  : allowed\nReset Button Disable : not allowed\nPower Button Disable : allowed\nSleep Button Disabled: false\nDiag Button Disabled : true\nReset Button Disabled: false\nPower Button Disabled: false\n", :value => true} }
+
   subject { described_class.new("host", "user", "pass") }
+  before  { allow_any_instance_of(Rubyipmi::Ipmitool::Chassis).to receive(:status).and_return(chassis_status_response) }
+
+  it { expect(subject).to be_connected }
 
   it "#chassis_status" do
-    response = {:result => "System Power         : off\nPower Overload       : false\nPower Interlock      : inactive\nMain Power Fault     : false\nPower Control Fault  : false\nPower Restore Policy : previous\nLast Power Event     : command\nChassis Intrusion    : inactive\nFront-Panel Lockout  : inactive\nDrive Fault          : false\nCooling/Fan Fault    : false\nSleep Button Disable : not allowed\nDiag Button Disable  : allowed\nReset Button Disable : not allowed\nPower Button Disable : allowed\nSleep Button Disabled: false\nDiag Button Disabled : true\nReset Button Disabled: false\nPower Button Disabled: false\n", :value => true}
-
     result = {
       "System Power"          => "off",
       "Power Overload"        => "false",
@@ -29,13 +32,10 @@ describe MiqIPMI do
       "Power Button Disabled" => "false"
     }
 
-    expect_any_instance_of(Rubyipmi::Ipmitool::Chassis).to receive(:status).twice.and_return(response)
     expect(subject.chassis_status).to eq(result)
   end
 
   context "Power Ops" do
-    it { expect(subject).to be_connected }
-
     it "#power_state" do
       expect_any_instance_of(Rubyipmi::Ipmitool::Power).to receive(:status).and_return("off")
       expect(subject.power_state).to eq("off")
@@ -79,7 +79,6 @@ describe MiqIPMI do
 
   context "version 1.5" do
     before { described_class.stub(:is_2_0_available?).and_return(false) }
-    before { allow_any_instance_of(MiqIPMI).to receive(:chassis_status).and_return({}) }
 
     it "#interface_mode" do
       subject.interface_mode.should == "lan"
@@ -93,7 +92,6 @@ describe MiqIPMI do
 
   context "version 2.0" do
     before { described_class.stub(:is_2_0_available?).and_return(true) }
-    before { allow_any_instance_of(MiqIPMI).to receive(:chassis_status).and_return({}) }
 
     it "#interface_mode" do
       subject.interface_mode.should == "lanplus"
