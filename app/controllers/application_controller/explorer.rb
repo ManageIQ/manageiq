f0cb7e04c2ec72a273c0a3902f1d995111af4dde
Brandon Dunne <bdunne@redhat.com>
2015-09-02 16:09:00 -0400

    Delete unreachable code

--- app/controllers/application_controller/explorer.rb
+++ app/controllers/application_controller/explorer.rb
@@ -528,7 +528,6 @@ module ApplicationController::Explorer
   def x_get_tree_host_kids(object, options)
     if [:bottlenecks_tree, :utilization_tree].include?(x_active_tree)
       raise "ERROR: should not get here"
-      objects = Array.new
     else
       objects = rbac_filtered_objects(object.resource_pools).sort_by { |a| a.name.downcase }.delete_if(&:is_default)
       if object.default_resource_pool           # Go thru default RP VMs
