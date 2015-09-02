af758745d5e98b3f37f38bcf2e053f02af8ab03c
Federico Simoncelli <fsimonce@redhat.com>
2015-09-02 01:20:05 -0400

    containers: unify verify ssl mode configuration
    Signed-off-by: Federico Simoncelli <fsimonce@redhat.com>

--- app/models/manageiq/providers/openshift/container_manager.rb
+++ app/models/manageiq/providers/openshift/container_manager.rb
@@ -48,7 +48,7 @@ class ManageIQ::Providers::Openshift::ContainerManager < ManageIQ::Providers::Co
     api_endpoint = raw_api_endpoint(hostname, port)
     osclient = OpenshiftClient::Client.new(api_endpoint, api_version)
     # TODO: support real authentication using certificates
-    osclient.ssl_options(:verify_ssl => OpenSSL::SSL::VERIFY_NONE)
+    osclient.ssl_options(:verify_ssl => verify_ssl_mode)
     osclient.bearer_token(options[:bearer]) if options[:bearer]
     osclient
   end
