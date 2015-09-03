80cd058c738c619d619649263794fb481a5c3092
Brandon Dunne <bdunne@redhat.com>
2015-09-03 11:16:01 -0400

    Don't test Dir.tmpdir and Dir.mktmpdir

--- gems/pending/spec/util/mount/miq_generic_mount_session_spec.rb
+++ gems/pending/spec/util/mount/miq_generic_mount_session_spec.rb
@@ -1,31 +1,20 @@
 require "spec_helper"
-require "tmpdir"
 require "util/mount/miq_generic_mount_session"
 
 describe MiqGenericMountSession do
-  let(:prefix) { File.join(Dir.tmpdir, "miq_") }
-
   it "#connect returns a string pointing to the mount point" do
     described_class.stub(:raw_disconnect)
     s = described_class.new(:uri => '/tmp/abc')
     s.logger = Logger.new("/dev/null")
 
-    expect(s.connect).to match(%r(\A#{prefix}\d{8}-\d{5}-\w+\z))
+    result = s.connect
+    expect(result).to     be_kind_of(String)
+    expect(result).to_not be_blank
 
     s.disconnect
   end
 
-  context "#mount_share" do
-    it "without :mount_point uses default temp directory as a base" do
-      expect(described_class.new(:uri => '/tmp/abc').mount_share).to match(%r(\A#{prefix}\d{8}-\d{5}-\w+\z))
-    end
-
-    it "with :mount_point uses specified directory as a base" do
-      expect(described_class.new(:uri => '/tmp/abc', :mount_point => "xyz").mount_share).to match(%r(\Axyz/miq_\d{8}-\d{5}-\w+\z))
-    end
-
-    it "is unique" do
-      expect(described_class.new(:uri => '/tmp/abc').mount_share).to_not eq(described_class.new(:uri => '/tmp/abc').mount_share)
-    end
+  it "#mount_share is unique" do
+    expect(described_class.new(:uri => '/tmp/abc').mount_share).to_not eq(described_class.new(:uri => '/tmp/abc').mount_share)
   end
 end
