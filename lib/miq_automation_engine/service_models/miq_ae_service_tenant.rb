f9db2044205b650369b807f55603b7dbeeabd657
Madhu Kanoor <mkanoor@redhat.com>
2015-09-08 17:54:29 -0400

    Expose tenant_quotas from the tenant service object
    https://trello.com/c/agVzK69c
    
    Spec & service model code

--- lib/miq_automation_engine/service_models/miq_ae_service_tenant.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_tenant.rb
@@ -1,4 +1,5 @@
 module MiqAeMethodService
   class MiqAeServiceTenant < MiqAeServiceModelBase
+    expose :tenant_quotas,  :association => true
   end
 end
--- spec/lib/miq_automation_engine/service_methods/miq_ae_service_tenant_spec.rb
+++ spec/lib/miq_automation_engine/service_methods/miq_ae_service_tenant_spec.rb
@@ -5,12 +5,17 @@ module MiqAeServiceTenantSpec
     let(:settings) { {} }
     let(:tenant) { Tenant.create(:name => 'fred', :domain => 'a.b', :parent => root_tenant, :description => "Krueger") }
 
+    let(:cpu_quota) { TenantQuota.create(:name => "cpu_allocated", :unit => "int", :value => 2, :tenant_id => tenant.id) }
+    let(:storage_quota) { TenantQuota.create(:name => "storage_allocated", :unit => "GB", :value => 160, :tenant_id => tenant.id) }
+
     let(:root_tenant) do
       MiqRegion.seed
       Tenant.seed
       Tenant.root_tenant
     end
 
+    let(:st_cpu_quota) { MiqAeMethodService::MiqAeServiceTenantQuota.find(cpu_quota.id) }
+    let(:st_storage_quota) { MiqAeMethodService::MiqAeServiceTenantQuota.find(storage_quota.id) }
     let(:service_tenant) { MiqAeMethodService::MiqAeServiceTenant.find(tenant.id) }
 
     before do
@@ -27,6 +32,13 @@ module MiqAeServiceTenantSpec
 
     it "#description" do
       expect(service_tenant.description).to eq('Krueger')
+    end
+
+    it "#tenant_quotas" do
+      ids = []
+      ids << st_cpu_quota.id
+      ids << st_storage_quota.id
+      expect(service_tenant.tenant_quotas.collect(&:id)).to match_array(ids)
     end
   end
 end
