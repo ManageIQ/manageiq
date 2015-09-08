089f7f636483db3806effcd92cec986e0fca9800
Nick Carboni <ncarboni@redhat.com>
2015-09-08 09:23:01 -0400

    Added metrics_collector_queue_name method to base manager class

--- app/models/manageiq/providers/base_manager.rb
+++ app/models/manageiq/providers/base_manager.rb
@@ -1,5 +1,13 @@
 module ManageIQ::Providers
 class BaseManager < ExtManagementSystem
+  def self.metrics_collector_queue_name
+    self::MetricsCollectorWorker.default_queue_name
+  end
+
+  def metrics_collector_queue_name
+    self.class.metrics_collector_queue_name
+  end
+
   def ext_management_system
     self
   end
