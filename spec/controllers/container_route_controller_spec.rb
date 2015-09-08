22ae357f1733153d12a2471455227a94b1b8d091
Ari Zellner <azellner@redhat.com>
2015-09-08 12:28:25 +0300

    Fixes #4157 - Added spec for container_route_controller

---
+++ spec/controllers/container_route_controller_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+describe ContainerRouteController do
+  render_views
+  before(:each) do
+    set_user_privileges
+  end
+
+  it "renders index" do
+    get :index
+    expect(response.status).to eq(302)
+    response.should redirect_to(:action => 'show_list')
+  end
+
+  it "renders show screen" do
+    EvmSpecHelper.create_guid_miq_server_zone
+    ems = FactoryGirl.create(:ems_kubernetes)
+    container_route = ContainerRoute.create(:ext_management_system => ems, :name => "Test Route")
+    get :show, :id => container_route.id
+    expect(response.status).to eq(200)
+    expect(response.body).to_not be_empty
+    expect(assigns(:breadcrumbs)).to eq([:name => "Test Route (Summary)",
+                                         :url  => "/container_route/show/#{container_route.id}"])
+  end
+
+  it "renders show_list" do
+    session[:settings] = {:default_search => 'foo',
+                          :views          => {:containerroute => 'list'},
+                          :perpage        => {:list => 10}}
+
+    FactoryGirl.create(:vmdb_database)
+    EvmSpecHelper.create_guid_miq_server_zone
+
+    get :show_list
+    expect(response.status).to eq(200)
+    expect(response.body).to_not be_empty
+  end
+end
