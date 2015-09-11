ed7d857a54d1f9f89f12558f3a157b10f3e95297
Sonja Hall <hall_sonja@bah.com>
2015-09-11 13:55:18 -0400

    removed unnecessary code in list.state.js

--- spa_ui/self_service/client/app/states/services/list/list.state.js
+++ spa_ui/self_service/client/app/states/services/list/list.state.js
@@ -114,18 +114,7 @@
 
       return matches;
     }
-
-    //function matchesFilter(item, filter) {
-    //  var match = true;
-    //  if (filter.id === 'name') {
-    //    match = item.name === filter.value;
-    //  } else if (filter.id === 'id') {
-    //    match = Number(item.id) === Number(filter.value);
-    //  }
-    //  console.log(item.name, filter.value, item.name.toLowerCase().indexOf(filter.value.toLowerCase()))
-    //  return match;
-    //}
-
+    
     function matchesFilter(item, filter) {
       if ('name' === filter.id) {
         return item.name.toLowerCase().indexOf(filter.value.toLowerCase()) !== -1;
