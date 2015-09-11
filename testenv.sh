d899ae298029cfa08e79c7a381cc67b3b2676c34
amaurymuro <amaury.muro@gmail.com>
2015-09-11 11:45:24 +0200

    Edit test script

--- testenv.sh
+++ testenv.sh
@@ -1,7 +1,8 @@
 
 cd rhus-chargeback
-git pull gitlab
-bundle install --without qpid
+ls
+#bundle install --without qpid
+~/.rvm/gems/ruby-2.0.0-p643/bin/bundle install --without qpid
 cp config/database.pg.yml config/database.yml
 bin/rake db:migrate
 bin/rake evm:restart
