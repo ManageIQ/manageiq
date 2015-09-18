3edbb2479122539bd3d7f8541b6e52baaa911a4e
Tina Fitzgerald <tfitzger@redhat.com>
2015-09-18 13:33:08 -0400

    Expose tenants in the automate service models.

--- lib/miq_automation_engine/service_models/miq_ae_service_tenant.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_tenant.rb
@@ -4,5 +4,12 @@ module MiqAeMethodService
     expose :miq_requests,  :association => true
     expose :miq_request_tasks, :association => true
     expose :services, :association => true
+    expose :providers, :association => true
+    expose :ext_management_systems, :association => true
+    expose :vm_or_templates, :association => true
+    expose :service_templates, :association => true
+    expose :miq_groups, :association => true
+    expose :users, :association => true
+    expose :ae_domains, :association => true
   end
 end
