13c1a4087c7985652a91410e467c26c035a3e665
Zahi Akiva <zakiva@redhat.com>
2015-09-24 17:49:50 +0300

    Display OS information for container images

--- app/helpers/container_image_helper/textual_summary.rb
+++ app/helpers/container_image_helper/textual_summary.rb
@@ -5,7 +5,7 @@ module ContainerImageHelper
     #
 
     def textual_group_properties
-      %i(name tag id full_name)
+      %i(name tag id full_name os_distribution product_type product_name)
     end
 
     def textual_group_relationships
@@ -49,6 +49,21 @@ module ContainerImageHelper
 
     def textual_full_name
       {:label => "Full Name", :value => @record.full_name}
+    end
+
+    def textual_os_distribution
+      distribution = @record.operating_system.try(:distribution)
+      {:label => "Operating System Distribution", :value => distribution} if distribution
+    end
+
+    def textual_product_type
+      type = @record.operating_system.try(:product_type)
+      {:label => "Product Type", :value => type} if type
+    end
+
+    def textual_product_name
+      name = @record.operating_system.try(:product_name)
+      {:label => "Product Name", :value => name} if name
     end
   end
 end
