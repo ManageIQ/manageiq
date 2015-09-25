55c03dd03b4c2165f12d66f2b20c5e0049c6806f
Oleg Barenboim <chessbyte@gmail.com>
2015-09-25 12:31:30 -0400

    Added specs for FileDepot.depot_description_to_class

---
+++ spec/models/file_depot_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe FileDepot do
+  it ".depot_description_to_class" do
+    expect(described_class.depot_description_to_class("FTP")).to eq(FileDepotFtp)
+    expect(described_class.depot_description_to_class("NFS")).to eq(FileDepotNfs)
+    expect(described_class.depot_description_to_class("Samba")).to eq(FileDepotSmb)
+    expect(described_class.depot_description_to_class("Anonymous FTP")).to eq(FileDepotFtpAnonymous)
+  end
+end
