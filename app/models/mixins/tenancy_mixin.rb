50e61f7874db5bfabfa2f700c3f22146c8b10811
Gregg Tanzillo <gtanzill@redhat.com>
2015-09-25 13:14:07 -0400

    Prototype for assigning tenants to CIs based in priority
    Priority:
    - current user
    - owning group
    - parent EMS
    - root tenant
    
    https://trello.com/c/RDHTx89m

--- app/models/mixins/tenancy_mixin.rb
+++ app/models/mixins/tenancy_mixin.rb
@@ -1,6 +1,10 @@
 module TenancyMixin
   extend ActiveSupport::Concern
 
+  included do
+    before_validation :set_tenant
+  end
+
   module ClassMethods
     def scope_by_tenant?
       true
@@ -12,5 +16,22 @@ module TenancyMixin
 
       tenant.accessible_tenant_ids(strategy)
     end
+  end
+
+  def set_tenant
+    # Priority
+    #   Current user
+    #   Owning group
+    #   Parent EMS
+    #   Root tenant
+    self.tenant_id = if User.current_user
+                  User.current_user.miq_group.tenant_id
+                elsif respond_to?(:miq_group) && miq_group
+                  miq_group.tenant_id
+                elsif respond_to?(:ext_management_system) && ext_management_system
+                  ext_management_system.tenant_id
+                else
+                  Tenant.root_tenant.id
+                end
   end
 end
