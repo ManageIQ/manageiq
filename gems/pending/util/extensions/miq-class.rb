26bcfb92f26a08d62210c086b3f1d98bea28015c
Matthew Draper <mdraper@redhat.com>
2015-09-26 01:59:11 +0930

    Simplify Class extensions using existing methods

--- gems/pending/util/extensions/miq-class.rb
+++ gems/pending/util/extensions/miq-class.rb
@@ -1,26 +1,15 @@
 class Class
   def hierarchy
-    sc = self.superclass
-    return [self] if sc.nil?
-    return sc.hierarchy.unshift(self)
+    ancestors.grep(Class)
   end
 
-  def subclass_of?(c)
-    return false if self == c
-    hierarchy.include?(c)
-  end
+  alias subclass_of? <
   alias is_subclass_of? subclass_of?
 
-  def is_or_subclass_of?(c)
-    hierarchy.include?(c)
-  end
+  alias is_or_subclass_of? <=
 
-  def superclass_of?(c)
-    c.subclass_of?(self)
-  end
+  alias superclass_of? >
   alias is_superclass_of? superclass_of?
 
-  def is_or_superclass_of?(c)
-    c.is_or_subclass_of?(self)
-  end
+  alias is_or_superclass_of? >=
 end
