672f9cb967834caf008327cf70351a51e22e472a
Dennis Metzger <dmetzger@redhat.com>
2015-09-28 14:37:46 -0400

    fix to ensure virtual_has_many returns the result of add_virtual_reflection

--- lib/extensions/ar_virtual.rb
+++ lib/extensions/ar_virtual.rb
@@ -159,13 +159,13 @@ module VirtualFields
   end
 
   def virtual_has_many(name, options = {})
-    uses = options.delete :uses
-    reflection = ActiveRecord::Associations::Builder::HasMany.build(self, name, nil, options)
-    add_virtual_reflection(reflection, name, uses, options)
     define_method("#{name.to_s.singularize}_ids") do
       records = send(name)
       records.respond_to?(:ids) ? records.ids : records.collect(&:id)
     end
+    uses = options.delete :uses
+    reflection = ActiveRecord::Associations::Builder::HasMany.build(self, name, nil, options)
+    add_virtual_reflection(reflection, name, uses, options)
   end
 
   def virtual_belongs_to(name, options = {})
