7a3154541a20482f900444374b9f03a8af4944de
Keenan Brock <kbrock@redhat.com>
2015-09-29 19:29:05 -0400

    microsoft provision_workflow#create_request spec

--- spec/models/manageiq/providers/microsoft/infra_manager/provision_workflow_spec.rb
+++ spec/models/manageiq/providers/microsoft/infra_manager/provision_workflow_spec.rb
@@ -1,31 +1,69 @@
 require "spec_helper"
 
 describe ManageIQ::Providers::Microsoft::InfraManager::ProvisionWorkflow do
-  context "With a Valid Template," do
-    let(:admin)    { FactoryGirl.create(:user, :name => 'admin', :userid => 'admin') }
-    let(:provider) { FactoryGirl.create(:ems_microsoft) }
-    let(:template) { FactoryGirl.create(:template_microsoft, :name => "template", :ext_management_system => provider) }
+  include WorkflowSpecHelper
 
-    before do
-      ManageIQ::Providers::Microsoft::InfraManager::ProvisionWorkflow.any_instance.stub(:get_dialogs).and_return(:dialogs => {})
-      ManageIQ::Providers::Microsoft::InfraManager::ProvisionWorkflow.any_instance.stub(:update_field_visibility)
-    end
+  let(:admin)    { FactoryGirl.create(:user_with_group) }
+  let(:ems)      { FactoryGirl.create(:ems_microsoft) }
+  let(:template) { FactoryGirl.create(:template_microsoft, :name => "template", :ext_management_system => ems) }
 
-    it "pass platform attributes to automate" do
-      MiqAeEngine::MiqAeWorkspaceRuntime.should_receive(:instantiate)
-      MiqAeEngine.should_receive(:create_automation_object) do |name, attrs, _options|
-        name.should eq("REQUEST")
-        attrs.should have_attributes(
-          'request'                   => 'UI_PROVISION_INFO',
-          'message'                   => 'get_pre_dialog_name',
-          'dialog_input_request_type' => 'template',
-          'dialog_input_target_type'  => 'vm',
-          'platform_category'         => 'infra',
-          'platform'                  => 'microsoft'
-        )
-      end
+  before do
+    described_class.any_instance.stub(:update_field_visibility)
+  end
 
-      ManageIQ::Providers::Microsoft::InfraManager::ProvisionWorkflow.new({}, admin.userid)
+  it "pass platform attributes to automate" do
+    stub_dialog
+    assert_automate_dialog_lookup('infra', 'microsoft')
+
+    described_class.new({}, admin.userid)
+  end
+
+  describe "#make_request" do
+    let(:alt_user) { FactoryGirl.create(:user_with_group) }
+    it "creates and update a request" do
+      EvmSpecHelper.local_miq_server
+      stub_dialog(:get_pre_dialogs)
+      stub_dialog(:get_dialogs)
+
+      # if running_pre_dialog is set, it will run 'continue_request'
+      workflow = described_class.new(values = {:running_pre_dialog => false}, admin.userid)
+
+      expect(AuditEvent).to receive(:success).with(
+        :event        => "vm_provision_request_updated",
+        :target_class => "Vm",
+        :userid       => admin.userid,
+        :message      => "VM Provision requested by [#{admin.userid}] for VM:#{template.id}"
+      )
+
+      # creates a request
+      stub_get_next_vm_name
+
+      # the dialogs populate this
+      values.merge!(:src_vm_id => template.id, :vm_tags => [])
+
+      request = workflow.make_request(nil, values, admin.userid) # TODO: nil
+
+      expect(request).to be_valid
+      expect(request).to be_a_kind_of(MiqProvisionRequest)
+      expect(request.request_type).to eq("template")
+      expect(request.description).to eq("Provision from [#{template.name}] to [New VM]")
+      expect(request.requester).to eq(admin)
+      expect(request.userid).to eq(admin.userid)
+      expect(request.requester_name).to eq(admin.name)
+
+      # updates a request
+
+      stub_get_next_vm_name
+
+      workflow = described_class.new(values, alt_user.userid)
+
+      expect(AuditEvent).to receive(:success).with(
+        :event        => "vm_migrate_request_updated",
+        :target_class => "Vm",
+        :userid       => alt_user.userid,
+        :message      => "VM Provision request was successfully updated by [#{alt_user.userid}] for VM:#{template.id}"
+      )
+      workflow.make_request(request, values, alt_user.userid)
     end
   end
 end
