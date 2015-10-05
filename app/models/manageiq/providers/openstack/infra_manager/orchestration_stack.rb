6c829d4d5786049e96a16d804701e375a5c328a3
Ladislav Smola <lsmola@redhat.com>
2015-10-05 15:35:03 +0200

    Adding missing stack actions
    Adding missing stack actions, they were removed during the
    refactoring.
    
    Fixes issue
    https://github.com/ManageIQ/manageiq/issues/4671

--- app/models/manageiq/providers/openstack/infra_manager/orchestration_stack.rb
+++ app/models/manageiq/providers/openstack/infra_manager/orchestration_stack.rb
@@ -14,4 +14,31 @@ class ManageIQ::Providers::Openstack::InfraManager::OrchestrationStack < ::Orche
     # Update is possible only when in complete or failed state, otherwise API returns exception
     raw_status.first.end_with?("_COMPLETE", "_FAILED")
   end
+
+  def raw_delete_stack
+    options = {:service => "Orchestration"}
+    options.merge!(:tenant_name => cloud_tenant.name) if cloud_tenant
+    ext_management_system.with_provider_connection(options) do |service|
+      service.stacks.get(name, ems_ref).try(:delete)
+    end
+  rescue => err
+    _log.error "stack=[#{name}], error: #{err}"
+    raise MiqException::MiqOrchestrationDeleteError, err.to_s, err.backtrace
+  end
+
+  def raw_status
+    ems = ext_management_system
+    ems.with_provider_connection(:service => "Orchestration") do |service|
+      raw_stack = service.stacks.get(name, ems_ref)
+      raise MiqException::MiqOrchestrationStackNotExistError, "#{name} does not exist on #{ems.name}" unless raw_stack
+
+      # TODO(lsmola) implement Status class, like in Cloud Manager, or make it common superclass
+      [raw_stack.stack_status, raw_stack.stack_status_reason]
+    end
+  rescue MiqException::MiqOrchestrationStackNotExistError
+    raise
+  rescue => err
+    _log.error "stack=[#{name}], error: #{err}"
+    raise MiqException::MiqOrchestrationStatusError, err.to_s, err.backtrace
+  end
 end
