3450b31d33a9f562bfdf418b598cfab570e4b42f
Brandon Dunne <bdunne@redhat.com>
2015-10-06 13:13:29 -0400

    Refactor vm_reconfigure_request_spec
    Leverage FactoryGirl.build to prevent extra DB interactions
    Leverage let to delay creation of records and deduplicate creation of the same things

--- spec/models/vm_reconfigure_request_spec.rb
+++ spec/models/vm_reconfigure_request_spec.rb
@@ -1,36 +1,31 @@
 require "spec_helper"
 
 describe VmReconfigureRequest do
-  let(:admin) { FactoryGirl.create(:user, :userid => "tester") }
-  before do
-    _guid1, _server, @zone1 = EvmSpecHelper.create_guid_miq_server_zone
-    @request = FactoryGirl.create(:vm_reconfigure_request, :userid => admin.userid)
+  let(:admin)         { FactoryGirl.create(:user, :userid => "tester") }
+  let(:ems_vmware)    { FactoryGirl.create(:ems_vmware, :zone => zone2) }
+  let(:host_hardware) { FactoryGirl.build(:hardware, :logical_cpus => 40, :numvcpus => 10, :cores_per_socket => 4) }
+  let(:host)          { FactoryGirl.build(:host, :hardware => host_hardware) }
+  let(:request)       { FactoryGirl.create(:vm_reconfigure_request, :userid => admin.userid) }
+  let(:vm_hardware)   { FactoryGirl.build(:hardware, :virtual_hw_version => "07") }
+  let(:vm_redhat)     { FactoryGirl.create(:vm_redhat) }
+  let(:vm_vmware)     { FactoryGirl.create(:vm_vmware, :hardware => vm_hardware, :host => host) }
+  let(:zone2)         { FactoryGirl.create(:zone, :name => "zone_2") }
 
-    zone2  = FactoryGirl.create(:zone, :name => "zone_2")
-    FactoryGirl.create(:miq_server, :zone => zone2)
-    @vm = FactoryGirl.create(:vm_vmware, :ext_management_system => FactoryGirl.create(:ems_vmware, :zone => zone2))
-  end
+  before { _guid, _server, @zone1 = EvmSpecHelper.create_guid_miq_server_zone }
 
-  it("#my_role should be 'ems_operations'") { expect(@request.my_role).to eq('ems_operations') }
+  it("#my_role should be 'ems_operations'") { expect(request.my_role).to eq('ems_operations') }
 
-  describe '#my_zone' do
-    context 'with valid sources' do
-      before { @request.update_attributes(:options => {:src_ids => [@vm.id]}) }
+  context '#my_zone' do
+    it "with valid source should have the VM's zone, not the requests zone" do
+      vm_vmware.update_attributes(:ems_id => ems_vmware.id)
+      request.update_attributes(:options => {:src_ids => [vm_vmware.id]})
 
-      it "shoud be the same as VM's zone" do
-        expect(@request.my_zone).to eq(@vm.my_zone)
-      end
-
-      it "should not be the same as the request's zone" do
-        expect(@request.my_zone).not_to eq(@zone1.name)
-      end
+      expect(request.my_zone).to     eq(vm_vmware.my_zone)
+      expect(request.my_zone).not_to eq(@zone1.name)
     end
 
-    context "with no source" do
-      it "should be the same as the request's zone" do
-        @request.update_attributes(:options => {})
-        expect(@request.my_zone).to eq(@zone1.name)
-      end
+    it "with no source should be the same as the request's zone" do
+      expect(request.my_zone).to eq(@zone1.name)
     end
   end
 
@@ -43,23 +38,22 @@ describe VmReconfigureRequest do
         :event        => "vm_reconfigure_request_created",
         :target_class => "Vm",
         :userid       => admin.userid,
-        :message      => "VM Reconfigure requested by <#{admin.userid}> for Vm:#{[@vm.id].inspect}"
+        :message      => "VM Reconfigure requested by <#{admin.userid}> for Vm:#{[vm_vmware.id].inspect}"
       )
 
-      # creates a request
-      stub_get_next_vm_name
+      allow(MiqProvision).to receive(:get_next_vm_name).and_return("New VM")
 
       # the dialogs populate this
-      values = {:src_ids => [@vm.id]}
+      values = {:src_ids => [vm_vmware.id]}
 
       request = described_class.make_request(nil, values, admin.userid) # TODO: nil
 
-      expect(request).to be_valid
-      expect(request).to be_a_kind_of(described_class)
-      expect(request.request_type).to eq("vm_reconfigure")
-      expect(request.description).to eq("VM Reconfigure for: #{@vm.name} - ")
-      expect(request.requester).to eq(admin)
-      expect(request.userid).to eq(admin.userid)
+      expect(request).to                be_valid
+      expect(request).to                be_a_kind_of(described_class)
+      expect(request.request_type).to   eq("vm_reconfigure")
+      expect(request.description).to    eq("VM Reconfigure for: #{vm_vmware.name} - ")
+      expect(request.requester).to      eq(admin)
+      expect(request.userid).to         eq(admin.userid)
       expect(request.requester_name).to eq(admin.name)
 
       # updates a request
@@ -68,107 +62,66 @@ describe VmReconfigureRequest do
         :event        => "vm_reconfigure_request_updated",
         :target_class => "Vm",
         :userid       => alt_user.userid,
-        :message      => "VM Reconfigure request updated by <#{alt_user.userid}> for Vm:#{[@vm.id].inspect}"
+        :message      => "VM Reconfigure request updated by <#{alt_user.userid}> for Vm:#{[vm_vmware.id].inspect}"
       )
       described_class.make_request(request, values, alt_user.userid)
     end
-  end
-
-  def stub_get_next_vm_name(vm_name = "New VM")
-    allow(MiqProvision).to receive(:get_next_vm_name).and_return(vm_name)
   end
 
   context ".request_limits" do
     subject { described_class.request_limits(@options) }
 
     context "RHEV only" do
-      before do
-        @vm   = FactoryGirl.create(:vm_redhat)
-        @options = {:src_ids => [@vm.id]}
-      end
-
       it "single vm" do
-        expect(subject[:min__number_of_sockets]).to eq(1)
-        expect(subject[:max__number_of_sockets]).to eq(16)
-        expect(subject[:min__total_vcpus]).to eq(1)
-        expect(subject[:max__total_vcpus]).to eq(160)
-        expect(subject[:min__vm_memory]).to      eq(4)
-        expect(subject[:max__vm_memory]).to      eq(2.terabyte / 1.megabyte)
+        @options = {:src_ids => [vm_redhat.id]}
+
+        assert_rhev_cpu_and_memory_min_max
       end
 
       it "multiple vms" do
-        vm2   = FactoryGirl.create(:vm_redhat)
-        @options[:src_ids] << vm2.id
+        @options = {:src_ids => [vm_redhat.id, FactoryGirl.create(:vm_redhat).id]}
 
-        expect(subject[:min__number_of_sockets]).to eq(1)
-        expect(subject[:max__number_of_sockets]).to eq(16)
-        expect(subject[:min__total_vcpus]).to eq(1)
-        expect(subject[:max__total_vcpus]).to eq(160)
-        expect(subject[:min__vm_memory]).to      eq(4)
-        expect(subject[:max__vm_memory]).to      eq(2.terabyte / 1.megabyte)
+        assert_rhev_cpu_and_memory_min_max
       end
     end
 
     context "Vmware only" do
-      before do
-        @host = FactoryGirl.create(:host,
-                                   :hardware => FactoryGirl.create(:hardware,
-                                                                   :logical_cpus     => 40,
-                                                                   :numvcpus         => 10,
-                                                                   :cores_per_socket => 4,
-                                                                  )
-                                  )
-        @vm   = FactoryGirl.create(:vm_vmware, :host => @host, :hardware => FactoryGirl.create(:hardware, :virtual_hw_version => "07"))
-        @options = {:src_ids => [@vm.id]}
-      end
-
       it "single vm" do
-        expect(subject[:min__number_of_sockets]).to eq(1)
-        expect(subject[:max__number_of_sockets]).to eq(8)
-        expect(subject[:min__total_vcpus]).to eq(1)
-        expect(subject[:max__total_vcpus]).to eq(8)
-        expect(subject[:min__vm_memory]).to      eq(4)
-        expect(subject[:max__vm_memory]).to      eq(255.gigabyte / 1.megabyte)
+        @options = {:src_ids => [vm_vmware.id]}
+
+        assert_vmware_cpu_and_memory_min_max
       end
 
       it "multiple vms" do
-        host2 = FactoryGirl.create(:host,
-                                   :hardware => FactoryGirl.create(:hardware,
-                                                                   :logical_cpus     => 30,
-                                                                   :numvcpus         => 15,
-                                                                   :cores_per_socket => 2,
-                                                                  )
-                                  )
-        vm2   = FactoryGirl.create(:vm_vmware, :host => host2, :hardware => FactoryGirl.create(:hardware, :virtual_hw_version => "07"))
-        @options[:src_ids] << vm2.id
+        hardware = FactoryGirl.build(:hardware, :virtual_hw_version => "07")
+        @options = {:src_ids => [vm_vmware.id, FactoryGirl.create(:vm_vmware, :host => host, :hardware => hardware).id]}
 
-        expect(subject[:min__number_of_sockets]).to eq(1)
-        expect(subject[:max__number_of_sockets]).to eq(8)
-        expect(subject[:min__total_vcpus]).to eq(1)
-        expect(subject[:max__total_vcpus]).to eq(8)
-        expect(subject[:min__vm_memory]).to      eq(4)
-        expect(subject[:max__vm_memory]).to      eq(255.gigabyte / 1.megabyte)
+        assert_vmware_cpu_and_memory_min_max
       end
     end
 
     it "hybrid" do
-      vm1  = FactoryGirl.create(:vm_redhat)
-      host = FactoryGirl.create(:host,
-                                :hardware => FactoryGirl.create(:hardware,
-                                                                :logical_cpus     => 30,
-                                                                :numvcpus         => 15,
-                                                                :cores_per_socket => 2,
-                                                               )
-                               )
-      vm2  = FactoryGirl.create(:vm_vmware, :host => host, :hardware => FactoryGirl.create(:hardware, :virtual_hw_version => "07"))
-      @options = {:src_ids => [vm1.id, vm2.id]}
+      @options = {:src_ids => [vm_redhat.id, vm_vmware.id]}
 
-      expect(subject[:min__number_of_sockets]).to eq(1)
-      expect(subject[:max__number_of_sockets]).to eq(8)
-      expect(subject[:min__total_vcpus]).to eq(1)
-      expect(subject[:max__total_vcpus]).to eq(8)
-      expect(subject[:min__vm_memory]).to      eq(4)
-      expect(subject[:max__vm_memory]).to      eq(255.gigabyte / 1.megabyte)
+      assert_vmware_cpu_and_memory_min_max
     end
+  end
+
+  def assert_rhev_cpu_and_memory_min_max
+    expect(subject[:min__number_of_sockets]).to eq(1)
+    expect(subject[:max__number_of_sockets]).to eq(16)
+    expect(subject[:min__total_vcpus]).to       eq(1)
+    expect(subject[:max__total_vcpus]).to       eq(160)
+    expect(subject[:min__vm_memory]).to         eq(4)
+    expect(subject[:max__vm_memory]).to         eq(2.terabyte / 1.megabyte)
+  end
+
+  def assert_vmware_cpu_and_memory_min_max
+    expect(subject[:min__number_of_sockets]).to eq(1)
+    expect(subject[:max__number_of_sockets]).to eq(8)
+    expect(subject[:min__total_vcpus]).to       eq(1)
+    expect(subject[:max__total_vcpus]).to       eq(8)
+    expect(subject[:min__vm_memory]).to         eq(4)
+    expect(subject[:max__vm_memory]).to         eq(255.gigabyte / 1.megabyte)
   end
 end
