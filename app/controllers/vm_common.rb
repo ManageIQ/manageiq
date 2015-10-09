5bcbb137a2641e7c9c3ddcc388888733aea41339
Dávid Halász <dhalasz@redhat.com>
2015-10-09 19:48:01 +0200

    Fixed rendering when editing accords and compare/drift sidebars

--- app/controllers/vm_common.rb
+++ app/controllers/vm_common.rb
@@ -1488,6 +1488,9 @@ module VmCommon
       :delete_node => @delete_node,      # Remove a new node from the tree
     )
 
+    presenter[:set_visible_elements][:default_left_cell] = true
+    presenter[:set_visible_elements][:custom_left_cell] = false
+
     r = proc { |opts| render_to_string(opts) }
 
     add_ajax = false
@@ -1538,9 +1541,10 @@ module VmCommon
       add_ajax = true
 
       if ['compare', 'drift'].include?(@sb[:action])
-        presenter[:update_partials][:custom_left_cell_div] = r[
+        presenter[:update_partials][:custom_left_cell] = r[
           :partial => 'layouts/listnav/x_compare_sections', :locals => {:truncate_length => 23}]
-        presenter[:cell_a_view] = 'custom'
+        presenter[:set_visible_elements][:custom_left_cell] = true
+        presenter[:set_visible_elements][:default_left_cell] = false
       end
     elsif @sb[:action] || params[:display]
       partial_locals = {
