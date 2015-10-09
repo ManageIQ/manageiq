2db4877ddcd5d7c3b68a67a7b1acff7d16843af3
AllenBW <allen.b.wight@gmail.com>
2015-10-09 14:36:50 -0400

    adds toasts to service list and service details states
    proper functionality contingent on running the latest and greatest patternfly-angular, this commit employs the pfNotification service to add a basic toast (success and error) for all actions that can be performed to a service (retire, remove, edit)

--- spa_ui/self_service/client/app/components/retire-service-modal/retire-service-modal-service.factory.js
+++ spa_ui/self_service/client/app/components/retire-service-modal/retire-service-modal-service.factory.js
@@ -33,7 +33,7 @@
   }
 
   /** @ngInject */
-  function RetireServiceModalController(serviceDetails, $state, $modalInstance, CollectionsApi) {
+  function RetireServiceModalController(serviceDetails, $state, $modalInstance, CollectionsApi, Notifications) {
     var vm = this;
 
     vm.service = serviceDetails;
@@ -64,10 +64,13 @@
 
       function retireSuccess() {
         $modalInstance.close();
+        Notifications.success('Scheduling retirement for' + vm.service.name  + '.');
         $state.go($state.current, {}, {reload: true});
+        $state.go('services.list');
       }
 
       function retireFailure() {
+        Notifications.error('There was an error retiring this service.');
       }
     }
   }
