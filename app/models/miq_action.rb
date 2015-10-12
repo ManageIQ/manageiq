d7d040edec30b0f26f1c0f6c42104e8c0f1a79a2
Oleg Barenboim <chessbyte@gmail.com>
2015-10-12 14:47:48 +0300

    Addressed Brakeman's Command Injection warning in MiqAction model

--- app/models/miq_action.rb
+++ app/models/miq_action.rb
@@ -1,3 +1,5 @@
+require 'awesome_spawn'
+
 class MiqAction < ActiveRecord::Base
   default_scope { where conditions_for_my_region_default_scope }
 
@@ -474,14 +476,14 @@ class MiqAction < ActiveRecord::Base
 
     MiqPolicy.logger.info("MIQ(action_script): Executing: [#{filename}]")
     if File.extname(filename) == ".rb"
-      rails_cmd = MiqEnvironment::Command.rails_command
-      MiqPolicy.logger.info("MIQ(action_script): Eval:      [#{rails_cmd} runner #{fname} '#{rec.name}'}]")
-      result, _, status = Open3.capture3(rails_cmd, "runner", fname, "'#{rec.name}'")
+      runner_cmd = MiqEnvironment::Command.runner_command
+      MiqPolicy.logger.info("MIQ(action_script): Running: [#{runner_cmd} #{fname} '#{rec.name}'}]")
+      command_result = AwesomeSpawn.run(runner_cmd, :params => [fname, rec.name])
     else
-      MiqPolicy.logger.info("MIQ(action_script): Eval:      [#{fname}]")
-      result, _, status = Open3.capture3(fname)
+      MiqPolicy.logger.info("MIQ(action_script): Running: [#{fname}]")
+      command_result = AwesomeSpawn.run(fname)
     end
-    rc = status.exitstatus
+    rc = command_result.exit_status
     rc_verbose = RC_HASH[rc] || "Unknown RC: [#{rc}]"
 
     fd.delete
@@ -490,15 +492,15 @@ class MiqAction < ActiveRecord::Base
 
     case rc
     when 0
-      MiqPolicy.logger.info("MIQ(action_script): Result: #{result}, rc: #{rc_verbose}")
+      MiqPolicy.logger.info("MIQ(action_script): Result: #{command_result.output}, rc: #{rc_verbose}")
     when 4
-      MiqPolicy.logger.warn("MIQ(action_script): Result: #{result}, rc: #{rc_verbose}")
+      MiqPolicy.logger.warn("MIQ(action_script): Result: #{command_result.output}, rc: #{rc_verbose}")
     when 8
-      raise MiqException::StopAction, "Action script exited with rc=#{rc_verbose}"
+      raise MiqException::StopAction, "Action script exited with rc=#{rc_verbose}, error=#{command_result.error}"
     when 16
-      raise MiqException::AbortAction, "Action script exited with rc=#{rc_verbose}"
+      raise MiqException::AbortAction, "Action script exited with rc=#{rc_verbose}, error=#{command_result.error}"
     else
-      raise MiqException::UnknownActionRc, "Action script exited with rc=#{rc_verbose}"
+      raise MiqException::UnknownActionRc, "Action script exited with rc=#{rc_verbose}, error=#{command_result.error}"
     end
   end
 
