aebbc08700ebfdb9ac9403786c5283ca4537e61d
AllenBW <allen.b.wight@gmail.com>
2015-10-13 17:12:28 -0400

    adds provisioning dialog directive
    employed by marketplace and request details states this directive can both display and accept input while also displaying already selected values (input disabled)
    
    this directive takes three parameters:
    1. dialog (the dialog object)
    2. isDisabled (boolean, if the dialog form is to be used for display only) optional
    3. options (the options dialog object, if it exists, for indicating already selected values) optional

---
+++ spa_ui/self_service/client/app/components/dialog-content/dialog-content.directive.js
@@ -0,0 +1,46 @@
+(function() {
+  'use strict';
+
+  angular.module('app.components')
+    .directive('dialogContent', DialogContentDirective);
+
+  /** @ngInject */
+  function DialogContentDirective() {
+    var directive = {
+      restrict: 'AE',
+      replace: true,
+      scope: {
+        dialog: '=',
+        options: '=?',
+        inputDisabled: '=?'
+      },
+      link: link,
+      templateUrl: 'app/components/dialog-content/dialog-content.html',
+      controller: DialogContentController,
+      controllerAs: 'vm',
+      bindToController: true
+    };
+
+    return directive;
+
+    function link(scope, element, attrs, vm, transclude) {
+      vm.activate();
+    }
+
+    /** @ngInject */
+    function DialogContentController() {
+      var vm = this;
+      vm.parsedOptions = {};
+      vm.activate = activate;
+
+      function activate() {
+        if (vm.options) {
+          angular.forEach(vm.options, parseOptions);
+        }
+        function parseOptions(value, key) {
+          vm.parsedOptions[key.replace('dialog_', '')] = value;
+        }
+      }
+    }
+  }
+})();
