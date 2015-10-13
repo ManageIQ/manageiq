b51e6b5d72c792168e3117e2b3870f85ad18b913
Tamara Ledesma Rico <tamledric@gmail.com>
2015-10-13 11:44:52 +0200

    Add a reference to chargeback_rate_detail_currencies in chargeback_rate_details

---
+++ db/migrate/20151013104702_add_currencies_ref_to_chargeback_rate_detail.rb
@@ -0,0 +1,5 @@
+class AddCurrenciesRefToChargebackRateDetail < ActiveRecord::Migration
+  def change
+    add_reference :chargeback_rate_details, :chargeback_rate_detail_currency, foreign_key: true
+  end
+end
