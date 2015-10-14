6c44fd8c5abb75c739e485453a29a12cad97b1af
Martin Povolny <mpovolny@redhat.com>
2015-10-14 15:44:31 +0200

    Patternfly toolbars: add disabled image for buttons.

--- app/helpers/toolbar_helper.rb
+++ app/helpers/toolbar_helper.rb
@@ -8,15 +8,24 @@ module ToolbarHelper
     end
   end
 
+  def toolbar_image(props)
+    tag(:img,
+      :src => "/images/toolbars/#{props['img']}",
+      :style => 'margin-right: 5px; width: 15px',
+      'data-enabled'  => "/images/toolbars/#{props['img']}",
+      'data-disabled' => "/images/toolbars/#{props['imgdis']}",
+    )
+  end
+
   def toolbar_top_button_select(props)
     content_tag(:div, :class => 'btn-group dropdown') do
       out = []
       out << content_tag(:button, :type => "button", :class => "btn btn-default dropdown-toggle",
                                   'data-toggle' => "dropdown", :title => props['title'],
                                   'data-click' => props['id'], 'data-when' => props['onwhen']) do
-               (tag(:img, :src => "/images/toolbars/#{props['img']}", :style => 'margin-right: 5px; width: 15px') +
-               props['text'].to_s + "&nbsp;".html_safe +
-               tag(:span, :class => "caret")).html_safe
+               (toolbar_image(props) +
+                 props['text'].to_s + "&nbsp;".html_safe +
+                 tag(:span, :class => "caret")).html_safe
              end 
       out << content_tag(:ul, :class => 'dropdown-menu') do
                props[:items].collect do |button|
@@ -51,8 +60,8 @@ module ToolbarHelper
     #  "title"=>"Go to this item"},
     content_tag(:li, :title => button['title']) do
       content_tag(:a, :href => '#', 'data-click' => button['id'], 'data-when' => button['onwhen'] ) do
-        (tag(:img, :src => "/images/toolbars/#{button['img']}", :style => 'margin-right: 5px; width: 15px') +
-         button['text']).html_safe
+        (toolbar_image(button) +
+          button['text']).html_safe
       end
     end
   end
