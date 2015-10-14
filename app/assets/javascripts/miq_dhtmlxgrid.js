ManageIQ/manageiq

commit 2e6ce4ccd76a724495dc016613418302afbb430c

fix row clicking
I am going to write some notes to myself and future people.  The table
view that lists the cloud providers is actually rendered via the dhtmlx
JS libraries.  The link that is assembled via JavaScript in
`miqRowClick`, and is the combination of the "id" in XML tag for that
row, and the url which is generated by `view_to_url`.

Now, the *giant* problem is that the XML is generated using **Ruport**
objects rather than ActiveRecord objects.  This means that by the time
the XML is generated, we have no idea what AR types the objects are, and
we can't use Rails' polymorphic url helpers to calculate the links.

** HOW THIS SHOULD WORK **

We should be generating the links to each record on the server side, and
embedding that in the XML.  Then just extract the link from the XML and
use it in the JS (or we could generate a tags on the server side rather
than go through these XML hoops).

I haven't been able to figure out the XML hoops yet, so I added a giant
hack in the `view_to_url` method that just returns the right url for
this specific controller

app/assets/javascripts/miq_dhtmlxgrid.js
@@ -1,3 +1,16 @@
+// ES6 endsWith polyfill
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (position === undefined || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
+
 // Functions used by MIQ for the dhtmlxtree control
 
 // Function to pass ajax request to server, to remember tree states
@@ -13,6 +26,9 @@ function miqRowClick(row_id, cell_idx) {
     if (typeof row_url_ajax != "undefined" && row_url_ajax) {
       miqJqueryRequest(row_url + row_id, {beforeSend: true, complete: true});
     } else {
+      if (!row_url.endsWith("/")) {
+        row_url = row_url + "/";
+      }
       DoNav(row_url + row_id);
     }
   }

commit 18cfeae5f3a5553a68679512e7a1ef523bbc59ac

No need to add "/" at the end of url when url ends with "="
We should not add '/' to the end of url when url ends with '='. This was causing a malformed url to Parameter summary screen when viewing list of Parameters under Cloud Stack.
- Malformed URL: http://localhost:3000/orchestration_stack/parameters/10000000000005?show=/10r15
- Correct URL: http://localhost:3000/orchestration_stack/parameters/10000000000005?show=10r15
Issue was introduced in commit: 2e6ce4ccd76a724495dc016613418302afbb430c

https://bugzilla.redhat.com/show_bug.cgi?id=1271514

app/assets/javascripts/miq_dhtmlxgrid.js
@@ -26,7 +26,7 @@ function miqRowClick(row_id, cell_idx) {
     if (typeof row_url_ajax != "undefined" && row_url_ajax) {
       miqJqueryRequest(row_url + row_id, {beforeSend: true, complete: true});
     } else {
-      if (!row_url.endsWith("/")) {
+      if (!row_url.endsWith("/") && !row_url.endsWith("=")) {
         row_url = row_url + "/";
       }
       DoNav(row_url + row_id);