ddc4d2871bccef43a20551a5074867e940ac83af
Madhu Kanoor <mkanoor@redhat.com>
2015-10-14 12:22:42 -0400

    Removed monkey patch file no longer needed

--- spec/support/miq_ae_engine_monkeypatch.rb
+++
@@ -1,7 +0,0 @@
-module MiqAeEngine
-  def self.instantiate(uri, user = nil)
-    Tenant.seed
-    user ||= FactoryGirl.create(:user_with_group)
-    MiqAeWorkspaceRuntime.instantiate(uri, user)
-  end
-end
