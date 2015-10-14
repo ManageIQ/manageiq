1ba5433fdb47d7f15f5f489fb8ab48b08ea39a50
Keenan Brock <kbrock@redhat.com>
2015-10-13 21:37:30 -0400

    remove extra ws methods

--- app/models/miq_host_provision_workflow.rb
+++ app/models/miq_host_provision_workflow.rb
@@ -71,10 +71,6 @@ class MiqHostProvisionWorkflow < MiqRequestWorkflow
     @allowed_hosts_cache
   end
 
-  def allowed_ws_hosts(_options = {})
-    Host.where("mac_address is not NULL").select(&:ipmi_enabled)
-  end
-
   def allowed_ems(_options = {})
     result = {}
 
@@ -106,101 +102,5 @@ class MiqHostProvisionWorkflow < MiqRequestWorkflow
   # This is for summary screen display purposes only
   def update_selected_storage_names(values)
     values[:attached_ds_names] = Storage.where(:id => values[:attached_ds]).pluck(:name)
-  end
-
-  def ws_template_fields(_values, fields)
-    data = parse_ws_string(fields)
-    _log.info "data:<#{data.inspect}>"
-
-    name         =     data[:name].blank? ? nil : data[:name].downcase
-    mac_address  =     data[:mac_address].blank? ? nil : data[:mac_address].downcase
-    ipmi_address =     data[:ipmi_address].blank? ? nil : data[:ipmi_address].downcase
-
-    raise "No host search criteria values were passed.  input data:<#{data.inspect}>" if name.nil? && mac_address.nil? && ipmi_address.nil?
-
-    _log.info "Host Passed  : <#{name}> <#{mac_address}> <#{ipmi_address}>"
-    srcs = allowed_ws_hosts(:include_datacenter => true).find_all do |v|
-      _log.info "Host Detected: <#{v.name.downcase}> <#{v.mac_address}> <#{v.ipmi_address}>"
-      (name.nil? || name == v.name.downcase) && (mac_address.nil? || mac_address == v.mac_address.to_s.downcase) && (ipmi_address.nil? || ipmi_address == v.ipmi_address.to_s)
-    end
-    raise "Multiple source template were found from input data:<#{data.inspect}>" if srcs.length > 1
-    src = srcs.first
-
-    raise "No target host was found from input data:<#{data.inspect}>" if src.nil?
-    _log.info "Host Found: <#{src.name}> MAC:<#{src.mac_address}> IPMI:<#{src.ipmi_address}>"
-    src
-  end
-
-  def ws_host_fields(values, fields)
-    data = parse_ws_string(fields)
-
-    _log.info "data:<#{data.inspect}>"
-    ws_service_fields(values, fields, data)
-    ws_environment_fields(values, fields, data)
-    refresh_field_values(values)
-    ws_customize_fields(values, fields, data)
-    ws_schedule_fields(values, fields, data)
-
-    data.each { |k, v| _log.warn "Unprocessed key <#{k}> with value <#{v.inspect}>" }
-  end
-
-  def ws_service_fields(values, _fields, data)
-    return if (dlg_fields = get_ws_dialog_fields(dialog_name = :service)).nil?
-    dlg_keys = dlg_fields.keys
-
-    [[:pxe_server_id, :allowed_pxe_servers, [:name, :url], PxeServer], [:pxe_image_id, :allowed_pxe_images, [:name], nil]].each do |dlg_key, method, keys, klass|
-      result = ws_find_matching_ci(method, keys, data.delete(dlg_key), klass)
-      values[dlg_key] = result.kind_of?(Array) ? result : [result.id, result.name] unless result.nil?
-    end
-
-    data.keys.each { |key| set_ws_field_value(values, key, data, dialog_name, dlg_fields) if dlg_keys.include?(key) }
-  end
-
-  def ws_environment_fields(values, _fields, data)
-    return if (dlg_fields = get_ws_dialog_fields(dialog_name = :service)).nil?
-    dlg_keys = dlg_fields.keys
-
-    [[:placement_ems_name, :allowed_ems, [:name, :hostname, :ipaddress], ExtManagementSystem]].each do |dlg_key, method, keys, klass|
-      result = ws_find_matching_ci(method, keys, data.delete(dlg_key), klass)
-      values[dlg_key] = result.kind_of?(Array) ? result : [result.id, result.name] unless result.nil?
-    end
-
-    dlg_key = :placement_cluster_name
-    search_value = data.delete(dlg_key).to_s.downcase
-    values[dlg_key] = allowed_clusters.detect { |_idx, fq_name| fq_name.to_s.downcase == search_value }
-
-    data.keys.each { |key| set_ws_field_value(values, key, data, dialog_name, dlg_fields) if dlg_keys.include?(key) }
-  end
-
-  def ws_customize_fields(values, _fields, data)
-    return if (dlg_fields = get_ws_dialog_fields(dialog_name = :customize)).nil?
-    dlg_keys = dlg_fields.keys
-
-    [[:customization_template_id, :allowed_customization_templates, [:name], nil]].each do |dlg_key, method, keys, klass|
-      result = ws_find_matching_ci(method, keys, data.delete(dlg_key), klass)
-      values[dlg_key] = result.kind_of?(Array) ? result : [result.id, result.name] unless result.nil?
-    end
-
-    pwd_key = :root_password
-    root_pwd = data.delete(pwd_key)
-    unless root_pwd.blank?
-      values[pwd_key] = MiqPassword.try_encrypt(root_pwd)
-    end
-
-    data.keys.each { |key| set_ws_field_value(values, key, data, dialog_name, dlg_fields) if dlg_keys.include?(key) }
-  end
-
-  def ws_find_matching_ci(allowed_method, keys, match_str, klass)
-    return nil if match_str.blank?
-    match_str = match_str.to_s.downcase
-
-    send(allowed_method).detect do |item|
-      ci = item.kind_of?(Array) ? klass.find_by_id(item[0]) : item
-      keys.any? do |key|
-        value = ci.send(key).to_s.downcase
-        # _log.warn "<#{allowed_method}> - comparing <#{value}> to <#{match_str}>"
-        value.include?(match_str)
-      end
-    end
   end
 end # class MiqHostProvisionWorkflow
