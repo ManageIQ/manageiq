871b33083b44c6d5ff19f53ace9fcfb21ccfcf01
erundle <erundle@redhat.com>
2015-10-15 17:39:19 -0400

    Adding application wide loading indicator

--- spa_ui/self_service/client/app/app.module.js
+++ spa_ui/self_service/client/app/app.module.js
@@ -1,9 +1,25 @@
 (function() {
   'use strict';
-
   angular.module('app', [
-    'app.core',
+   'app.core',
     'app.config',
-    'app.states'
+    'app.states',
+    'ngProgress'
   ]);
+  angular.module('app').controller('AppController', ['$rootScope', '$scope', 'ngProgressFactory',
+   function($rootScope, $scope, ngProgressFactory) {
+      $scope.progressbar = ngProgressFactory.createInstance();
+      $scope.progressbar.setColor('#0088ce');
+      $scope.progressbar.setHeight('3px');
+      $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+        if (toState.resolve) {
+          $scope.progressbar.start();
+        }
+      });
+      $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        if (toState.resolve) {
+          $scope.progressbar.complete();
+        }
+      });
+    }]);
 })();
