c06a3ad159174a9fdd6d181ce45629660df92a25
Tina Fitzgerald <tfitzger@redhat.com>
2015-10-16 16:09:51 -0400

    Added tenant as default quota source and use tenant limits in place of model/tag limit values.

--- db/fixtures/ae_datastore/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/limits.rb
+++ db/fixtures/ae_datastore/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/limits.rb
@@ -1,6 +1,7 @@
 #
 # Description: Get quota values.
 #
+
 QUOTA_ATTRIBUTES = %w(storage vms cpu memory)
 
 def quota_values(model_attribute, tag_name)
@@ -11,6 +12,19 @@ def quota_values(model_attribute, tag_name)
   final_quota_value(tag_value, object_value)
 end
 
+def tenant_quota_values
+  tenant_values = {}
+  @source.tenant_quotas.each do |q|
+    value = q.value.to_i
+    name = q.name.chomp('_allocated')
+    name = 'memory' if name == 'mem'
+    tenant_values[name.to_sym] = (name == 'memory' || name == 'storage') ? bytes_to_megabytes(value) : value
+  end
+  $evm.log(:info, "Getting Tenant Quota Values for: #{tenant_values}")
+  $evm.root['quota_limit_max'] = tenant_values
+  $evm.root['quota_limit_warn'] = {:cpu => 0, :memory => 0, :storage => 0, :vms => 0}
+end
+
 def quota_model_value(model_attr)
   parent_model_value(model_attr)
 end
@@ -19,6 +33,10 @@ def final_quota_value(tag_quota_value, object_quota_value = 0)
   val = tag_quota_value.zero? ? object_quota_value : tag_quota_value
   $evm.log(:info, "Final Quota Value: #{val}")
   val
+end
+
+def bytes_to_megabytes(bytes)
+  bytes / 1024**2
 end
 
 def vmdb_object(object, id)
@@ -47,22 +65,26 @@ def determine_quota_value(model_attribute, tag_name)
 end
 
 def error(type)
-  msg = "Unable to calculate quota limits due to an error getting the Quota #{type}"
+  msg = "Unable to calculate requested #{type}, due to an error getting the #{type}"
   $evm.log(:error, " #{msg}")
   $evm.root['ae_result'] = 'error'
   raise msg
 end
 
+def quota_by_tenant?
+  $evm.root['quota_source_type'] == 'tenant'
+end
+
+def model_and_tag_quota_values
+  QUOTA_ATTRIBUTES.each do |i|
+    quota_max[i.to_sym] = quota_values("max_#{i}", "quota_max_#{i}".to_sym)
+    quota_warn[i.to_sym] = quota_values("warn_#{i}", "quota_warn_#{i}".to_sym)
+  end
+  $evm.root['quota_limit_max'] = quota_max
+  $evm.root['quota_limit_warn'] = quota_warn
+end
+
 @source = $evm.root['quota_source']
 error("source") if @source.nil?
 
-quota_max = {}
-quota_warn = {}
-
-QUOTA_ATTRIBUTES.each do |i|
-  quota_max[i.to_sym] = quota_values("max_#{i}", "quota_max_#{i}".to_sym)
-  quota_warn[i.to_sym] = quota_values("warn_#{i}", "quota_warn_#{i}".to_sym)
-end
-
-$evm.root['quota_limit_max'] = quota_max
-$evm.root['quota_limit_warn'] = quota_warn
+quota_by_tenant? ? tenant_quota_values : model_and_tag_quota_values
--- db/fixtures/ae_datastore/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/source.rb
+++ db/fixtures/ae_datastore/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/source.rb
@@ -5,6 +5,6 @@
 @miq_request = $evm.root['miq_request']
 $evm.log(:info, "Request: #{@miq_request.description} id: #{@miq_request.id} ")
 
-user = @miq_request.requester
-$evm.root['quota_source'] = user.current_group
-$evm.log(:info, "Setting Quota Source #{$evm.root['quota_source'].inspect}")
+$evm.root['quota_source'] = @miq_request.tenant
+$evm.root['quota_source_type'] = 'tenant'
+$evm.log(:info, "Setting Tenant as Quota Source #{$evm.root['quota_source'].inspect}")
