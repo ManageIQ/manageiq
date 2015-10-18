899519a7acf01903c61edefa1c04374503ef59e2
Zahi Akiva <zakiva@redhat.com>
2015-10-18 15:10:27 +0300

    Add persistent volume claims

--- app/models/persistent_volume_claim.rb
+++ app/models/persistent_volume_claim.rb
@@ -2,4 +2,8 @@ class PersistentVolumeClaim < ActiveRecord::Base
   belongs_to :ext_management_system, :foreign_key => "ems_id"
   has_many :container_volumes
   serialize :capacity, Hash
+
+  def persistent_volume
+    container_volumes.find_by_type('PersistentVolume')
+  end
 end
