0c07b203fe77145a72b50e6efad2cc78f10f13a5
Dávid Halász <dhalasz@redhat.com>
2015-10-19 12:57:48 +0200

    Added JS specs for initializing the toolbar

--- spec/javascripts/miq_application_spec.js
+++ spec/javascripts/miq_application_spec.js
@@ -1,9 +1,40 @@
 describe('miq_application.js', function() {
-  beforeEach(function () {
-    var html = '<div id="form_div"><textarea name="method_data">new line added\r\n\r\n</textarea></div>'
-    setFixtures(html);
+  describe('miqSerializeForm', function () {
+    beforeEach(function () {
+      var html = '<div id="form_div"><textarea name="method_data">new line added\r\n\r\n</textarea></div>'
+      setFixtures(html);
+    });
+
+    it('verify serialize method doesnt convert line feed value to windows line feed', function() {
+      expect(miqSerializeForm('form_div')).toEqual("method_data=new+line+added%0A%0A");
+    });
   });
-  it('verify serialize method doesnt convert line feed value to windows line feed', function() {
-    expect(miqSerializeForm('form_div')).toEqual("method_data=new+line+added%0A%0A");
+
+  describe('miqInitToolbars', function () {
+    beforeEach(function () {
+      var html  = '<div id="toolbar"><div class="btn-group"><button class="btn btn-default" id="first">Click me!</button></div><div class="btn-group"><button class="btn btn-default dropdown-toggle" id="second">Click me!</button><ul class="dropdown-menu"><li><a href="#" id="third">Click me!</a></li></ul></div></div>';
+      setFixtures(html);
+    });
+
+    it('initializes the onclick event on a regular toolbar button', function () {
+      spyOn(window, "miqToolbarOnClick");
+      miqInitToolbars();
+      $('#first').click();
+      expect(miqToolbarOnClick).toHaveBeenCalled();
+    });
+
+    it('not initializes an onclick event on a dropdown toolbar button', function () {
+      spyOn(window, "miqToolbarOnClick");
+      miqInitToolbars();
+      $('#second').click();
+      expect(miqToolbarOnClick).not.toHaveBeenCalled();
+    });
+
+    it('initializes the onclick event on a dropdown toolbar link', function () {
+      spyOn(window, "miqToolbarOnClick");
+      miqInitToolbars();
+      $('#third').click();
+      expect(miqToolbarOnClick).toHaveBeenCalled();
+    });
   });
 });
