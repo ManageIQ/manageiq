fcc3da8afb81f61ae4bc6594a202d533181e4886
Martin Hradil <mhradil@redhat.com>
2015-10-19 17:37:10 +0000

    jquery.observe_field - our changes
    this is just so we have a comparison what changed

--- app/assets/javascripts/jquery.observe_field.js
+++ app/assets/javascripts/jquery.observe_field.js
@@ -1,50 +1,45 @@
+// MIQ - Changes to work in our environment
+// Original source: https://github.com/splendeo/jquery.observe_field
+
 // jquery.observe_field.js
 
-
-(function( $ ){
+(function( $j ){
 
   jQuery.fn.observe_field = function(frequency, callback) {
+		var el = $j(this[0])
+		if (typeof el.data("events") == "undefined" || typeof el.data("events").click == "undefined"){
 
-    frequency = frequency * 1000; // translate to milliseconds
+			frequency = frequency * 1000; // translate to milliseconds
 
-    return this.each(function(){
-      var $this = $(this);
-      var prev = $this.val();
-      var prevChecked = $this.prop('checked');
+			return this.each(function(){
+				var $this = $j(this);
+				var prev = $this.val();
+				var ti;
 
-      var check = function() {
-        if(removed()){ // if removed clear the interval and don't fire the callback
-          if(ti) clearInterval(ti);
-          return;
-        }
+				var check = function() {
+					if(ti) clearInterval(ti);
+					var val = $this.val();
+					if(prev != val){
+						prev = val;
+						$this.map(callback); // invokes the callback on $this
+					}
+				};
 
-        var val = $this.val();
-        var checked = $this.prop('checked');
-        if(prev != val || checked != prevChecked ){
-          prev = val;
-          prevChecked = checked;
-          $this.map(callback); // invokes the callback on $this
-        }
-      };
+				var reset = function() {
+					if(ti) clearInterval(ti);
+					ti = setInterval(check, frequency);
+				};
 
-      var removed = function() {
-        return $this.closest('html').length == 0
-      };
+//				Commented the following so we don't have a timer constantly
+//				check();
+//				var ti = setInterval(check, frequency); // invoke check periodically
 
-      var reset = function() {
-        if(ti){
-          clearInterval(ti);
-          ti = setInterval(check, frequency);
-        }
-      };
-
-      check();
-      var ti = setInterval(check, frequency); // invoke check periodically
-
-      // reset counter after user interaction
-      $this.bind('keyup click mousemove', reset); //mousemove is for selects
-    });
+				// reset counter after user interaction
+	      $this.bind('keyup click mousemove', reset); //mousemove is for selects
+	    });
+		}
 
   };
 
 })( jQuery );
+
