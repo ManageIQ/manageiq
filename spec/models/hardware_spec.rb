3437766631aaface8889e73edb8e589c06999afe
Tim Wade <hello@timjwade.com>
2015-10-20 10:34:12 -0700

    Clean up hardware specs
    Put the expensive setup not needed in my tests in its own context, and
    delete one of my edge case tests since it doesn't add much value

--- spec/models/hardware_spec.rb
+++ spec/models/hardware_spec.rb
@@ -1,42 +1,43 @@
 require "spec_helper"
 
 describe Hardware do
-  before(:each) do
-    @vm_hw = FactoryGirl.create(:hardware)
-    @vm    = FactoryGirl.create(:vm_vmware, :hardware => @vm_hw)
+  context "integration" do
+    before(:each) do
+      @vm_hw = FactoryGirl.create(:hardware)
+      @vm    = FactoryGirl.create(:vm_vmware, :hardware => @vm_hw)
 
-    @template_hw = FactoryGirl.create(:hardware)
-    @template    = FactoryGirl.create(:template_vmware, :hardware => @template_hw)
+      @template_hw = FactoryGirl.create(:hardware)
+      @template    = FactoryGirl.create(:template_vmware, :hardware => @template_hw)
 
-    @host_hw = FactoryGirl.create(:hardware)
-    @host    = FactoryGirl.create(:host, :hardware => @host_hw)
+      @host_hw = FactoryGirl.create(:hardware)
+      @host    = FactoryGirl.create(:host, :hardware => @host_hw)
+    end
+
+    it "#vm_or_template" do
+      @vm_hw.vm_or_template.should == @vm
+      @template_hw.vm_or_template.should == @template
+      @host_hw.vm_or_template.should     be_nil
+    end
+
+    it "#vm" do
+      @vm_hw.vm.should == @vm
+      @template_hw.vm.should be_nil
+      @host_hw.vm.should     be_nil
+    end
+
+    it "#miq_template" do
+      @vm_hw.miq_template.should       be_nil
+      @template_hw.miq_template.should == @template
+      @host_hw.miq_template.should     be_nil
+    end
+
+    it "#host" do
+      @vm_hw.host.should       be_nil
+      @template_hw.host.should be_nil
+      @host_hw.host.should == @host
+    end
   end
 
-  it "#vm_or_template" do
-    @vm_hw.vm_or_template.should == @vm
-    @template_hw.vm_or_template.should == @template
-    @host_hw.vm_or_template.should     be_nil
-  end
-
-  it "#vm" do
-    @vm_hw.vm.should == @vm
-    @template_hw.vm.should be_nil
-    @host_hw.vm.should     be_nil
-  end
-
-  it "#miq_template" do
-    @vm_hw.miq_template.should       be_nil
-    @template_hw.miq_template.should == @template
-    @host_hw.miq_template.should     be_nil
-  end
-
-  it "#host" do
-    @vm_hw.host.should       be_nil
-    @template_hw.host.should be_nil
-    @host_hw.host.should == @host
-  end
-
-  # TODO: we probably want these to run without the above before block
   describe "#sockets" do
     it "returns 0 when logical cpus is nil" do
       hardware = FactoryGirl.build(:hardware, :logical_cpus => nil, :cores_per_socket => 1)
@@ -56,12 +57,6 @@ describe Hardware do
     it "returns logical_cpus / cores_per_socket" do
       hardware = FactoryGirl.build(:hardware, :logical_cpus => 8, :cores_per_socket => 4)
       expect(hardware.sockets).to eq(2)
-    end
-
-    it "floors the value when not a whole number" do
-      # TODO: not sure about this
-      hardware = FactoryGirl.build(:hardware, :logical_cpus => 5, :cores_per_socket => 4)
-      expect(hardware.sockets).to eq(1)
     end
   end
 end
