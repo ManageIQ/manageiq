0b633e7d8cb3ef5f888fda8b77cfeb31183514b8
amaurymuro <amaury.muro@gmail.com>
2015-10-21 16:59:25 +0200

    Recode from the pull request comments

--- spec/models/chargeback_rate_detail_measure_spec.rb
+++ spec/models/chargeback_rate_detail_measure_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe ChargebackRateDetailMeasure do
-
   it "has a valid factory" do
     FactoryGirl.create(:chargeback_rate_detail_measure_bytes).should be_valid
   end
@@ -25,7 +24,7 @@ describe ChargebackRateDetailMeasure do
   it "is invalid with a only one array units" do
     FactoryGirl.build(:chargeback_rate_detail_measure_bytes, :units => "{KB}").should_not be_valid
   end
-  
+
   it "is invalid with a units_display lenght diferent that the units lenght" do
     FactoryGirl.build(:chargeback_rate_detail_measure_bytes, :units => "{Bs,KBs,GBs}", :units_display => "{kbps,mbps}")
       .should_not be_valid
