0424f2785a1a422e5d8ff643b0b1c9459345cbc0
Bronagh Sorota <bsorota@redhat.com>
2015-10-21 12:19:06 -0400

    changed ems_id to bigint and addressed rubocop complaint.

--- db/migrate/20151019184531_create_resource_groups.rb
+++ db/migrate/20151019184531_create_resource_groups.rb
@@ -1,11 +1,11 @@
 class CreateResourceGroups < ActiveRecord::Migration
   def up
     create_table :resource_groups do |t|
-      t.string  :name
-      t.string  :ems_ref
-      t.integer :ems_id
-      t.string  :type
-      t.timestamps null: false
+      t.string :name
+      t.string :ems_ref
+      t.bigint :ems_id
+      t.string :type
+      t.timestamps :null => false
     end
   end
 
