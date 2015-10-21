23ece15ecdf94ce975fb3fcae758e7d27265354b
Viktar Basharymau <viktar.basharymau@gmail.com>
2015-10-21 13:18:12 +0300

    Use native CSV parser in MiqEventDefinitionSpec.seed

--- app/models/miq_event_definition_set.rb
+++ app/models/miq_event_definition_set.rb
@@ -3,20 +3,11 @@ class MiqEventDefinitionSet < ActiveRecord::Base
 
   default_scope { where conditions_for_my_region_default_scope }
 
-  FIXTURE_DIR = File.join(Rails.root, "db/fixtures")
+  FIXTURE_DIR = Rails.root.join("db/fixtures")
 
   def self.seed
-    fname = File.join(FIXTURE_DIR, "#{to_s.pluralize.underscore}.csv")
-    data  = File.read(fname).split("\n")
-    cols  = data.shift.split(",")
-
-    data.each do |s|
-      next if s =~ /^#.*$/ # skip commented lines
-
-      arr = s.split(",")
-
-      set = {}
-      cols.each_index { |i| set[cols[i].to_sym] = arr[i] }
+    CSV.foreach(FIXTURE_DIR.join("#{to_s.pluralize.underscore}.csv"), :headers => true, :skip_lines => /^#/) do |csv_row|
+      set = csv_row.to_hash.symbolize_keys
 
       rec = find_by_name(set[:name])
       if rec.nil?
