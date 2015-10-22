652c8eb29fab48960d176f4826a879151ed8b445
Brandon Dunne <bdunne@redhat.com>
2015-10-22 15:36:10 -0400

    Fix rubocop error "Syntax - dynamic constant assignment"

--- lib/vmdb/deprecation.rb
+++ lib/vmdb/deprecation.rb
@@ -1,5 +1,19 @@
-Vmdb::Deprecation ||= begin
-  deprecator = ActiveSupport::Deprecation.new("D-release", "ManageIQ")
-  deprecator.behavior = [:stderr, :log]
-  deprecator
+module Vmdb
+  class Deprecation
+    def self.instance
+      @instance ||= begin
+        deprecator = ActiveSupport::Deprecation.new("D-release", "ManageIQ")
+        deprecator.behavior = [:stderr, :log]
+        deprecator
+      end
+    end
+
+    def self.method_missing(method_name, *args, &block)
+      instance.respond_to?(method_name) ? instance.send(method_name, *args, &block) : super
+    end
+
+    def self.respond_to_missing?(method, _include_private = false)
+      instance.respond_to?(method)
+    end
+  end
 end
