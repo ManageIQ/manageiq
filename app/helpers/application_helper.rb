ec66ce2556b1ef7dd47812212ba2cf739f4a601a
Viktar Basharymau <viktar.basharymau@gmail.com>
2015-10-23 12:53:45 +0300

    Make vm_infra_explorer_accords_attributes more declarative

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1108,13 +1108,13 @@ module ApplicationHelper
 
   def vm_infra_explorer_accords_attributes(record)
     if role_allows(:feature => "vandt_accord") || role_allows(:feature => "vms_filter_accord")
-      attributes = {}
-      attributes[:link] = true
-      attributes[:controller] = "vm_infra"
-      attributes[:action] = "show"
-      attributes[:id] = record.id
+      {
+        :link => true,
+        :controller => "vm_infra",
+        :action => "show",
+        :id => record.id
+      }
     end
-    attributes
   end
 
   def service_workload_attributes(record)
