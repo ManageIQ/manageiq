a6a870af0358d1d7d3ef7bbf8932b0c25ac8a00f
Viktar Basharymau <viktar.basharymau@gmail.com>
2015-10-26 17:21:55 +0300

    Replace `time.iso8601.gsub(...)` by `time.strftime(...)`
    There is no point in generating a string that contains unwanted characters,
    and then replacing these characters. It is better to generate a well-formed string.

--- app/models/miq_server/log_management.rb
+++ app/models/miq_server/log_management.rb
@@ -253,7 +253,7 @@ module MiqServer::LogManagement
   end
 
   def base_zip_log_name
-    t = Time.now.utc.iso8601.gsub!(/:|\./, "_")
+    t = Time.now.utc.strftime('%FT%H_%M_%SZ'.freeze)
     # Name the file based on GUID and time.  GUID and Date/time of the request are as close to unique filename as we're going to get
     "App-#{guid}-#{t}"
   end
