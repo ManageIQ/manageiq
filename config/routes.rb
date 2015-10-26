7ebb81009addbe66cb2de3fde7a75a7fe51d1357
Martin Hradil <mhradil@redhat.com>
2015-10-26 12:50:48 +0000

    routes - fix formatting for container_image*

--- config/routes.rb
+++ config/routes.rb
@@ -417,23 +417,59 @@ Vmdb::Application.routes.draw do
     },
 
     :container_image          => {
-      :get  => %w(download_data edit index new show show_list tagging_edit tag_edit_form_field_changed),
-      :post => %w(button create dynamic_checkbox_refresh form_field_changed listnav_search_selected panel_control
-                  quick_search sections_field_changed show show_list update
-                  tagging_edit tag_edit_form_field_changed) +
-               adv_search_post +
-               exp_post +
-               save_post
+      :get  => %w(
+        download_data
+        edit
+        index
+        new
+        show
+        show_list
+        tagging_edit
+        tag_edit_form_field_changed
+      ),
+      :post => %w(
+        button
+        create
+        dynamic_checkbox_refresh
+        form_field_changed
+        listnav_search_selected
+        panel_control
+        quick_search
+        sections_field_changed
+        show
+        show_list
+        update
+        tagging_edit
+        tag_edit_form_field_changed
+      ) + adv_search_post + exp_post + save_post
     },
 
     :container_image_registry => {
-      :get  => %w(download_data edit index new show show_list tagging_edit tag_edit_form_field_changed),
-      :post => %w(button create dynamic_checkbox_refresh form_field_changed listnav_search_selected panel_control
-                  quick_search sections_field_changed show show_list update
-                  tagging_edit tag_edit_form_field_changed) +
-               adv_search_post +
-               exp_post +
-               save_post
+      :get  => %w(
+        download_data
+        edit
+        index
+        new
+        show
+        show_list
+        tagging_edit
+        tag_edit_form_field_changed
+      ),
+      :post => %w(
+        button
+        create
+        dynamic_checkbox_refresh
+        form_field_changed
+        listnav_search_selected
+        panel_control
+        quick_search
+        sections_field_changed
+        show
+        show_list
+        update
+        tagging_edit
+        tag_edit_form_field_changed
+      ) + adv_search_post + exp_post + save_post
     },
 
     :container_service        => {
