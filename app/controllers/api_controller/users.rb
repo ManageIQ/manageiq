f3a7a76a20a1cae5230a9cd798aad6a6f1143b40
Alberto Bellotti <abellott@redhat.com>
2015-10-26 14:27:01 -0400

    PR review changes

--- app/controllers/api_controller/users.rb
+++ app/controllers/api_controller/users.rb
@@ -1,8 +1,11 @@
 class ApiController
   module Users
     def update_users
-      if target_user_for_update_is_self?
-        @req[:action] = "edit"
+      aname = parse_action_name
+      if aname == "edit" && !api_user_role_allows?(aname) && update_target_is_api_user?
+        if json_body_resource.try(:keys) != %w(password)
+          raise BadRequestError, "Cannot update non-password attributes of the authenticated user resource"
+        end
         render_normal_update :users, update_collection(:users, @req[:c_id])
       else
         update_generic(:users)
@@ -11,7 +14,7 @@ class ApiController
 
     private
 
-    def target_user_for_update_is_self?
+    def update_target_is_api_user?
       @auth_user_obj.id == @req[:c_id].to_i
     end
   end
