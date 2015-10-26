919b5057879e884ed227341927660efa5d0d5b6f
Martin Hradil <mhradil@redhat.com>
2015-10-26 18:14:41 +0000

    fix tests

--- spec/views/layouts/list_grid.html.haml_spec.rb
+++ spec/views/layouts/list_grid.html.haml_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe "layouts/_list_grid.html.haml" do
   context "when showtype is 'performance'" do
     it "renders" do
-      view.stub(:options).and_return({})
-      view.stub(:js_options).and_return({})
+      view.stub(:options).and_return({:grid_hash => {:head => [], :rows => []}})
+      view.stub(:js_options).and_return({:row_url => '_none_'})
       record = EmsInfra.new(:id => 1)
       assign(:parent, record)
       render
