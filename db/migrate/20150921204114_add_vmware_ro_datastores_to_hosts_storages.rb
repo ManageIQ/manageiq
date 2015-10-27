5c45edba4a3149455d74044eb12c79918a27d7b9
Adam Grare <agrare@redhat.com>
2015-10-26 22:47:58 -0400

    Rename :hosts_storages table to :host_storages

--- db/migrate/20150921204114_add_vmware_ro_datastores_to_hosts_storages.rb
+++ db/migrate/20150921204114_add_vmware_ro_datastores_to_hosts_storages.rb
@@ -4,7 +4,8 @@ class AddVmwareRoDatastoresToHostsStorages < ActiveRecord::Migration
   end
 
   def up
-    add_column :hosts_storages, :read_only, :boolean
+    rename_table :hosts_storages, :host_storages
+    add_column :host_storages, :read_only, :boolean
 
     # Find the sequence_start value for our region, if the region is
     # 0 then start at 1
@@ -14,24 +15,25 @@ class AddVmwareRoDatastoresToHostsStorages < ActiveRecord::Migration
     # add_column ... :primary_key was adding ids to all existing rows before
     # the sequence_start could be set, so we have to create the sequence and
     # the primary keys manually
-    say_with_time("Add hosts_storages primary_key") do
+    say_with_time("Add host_storages primary_key") do
       # Add an auto-increment sequence to be used by the primary key column
-      execute "CREATE SEQUENCE hosts_storages_id_seq START #{seq_start_value}"
+      execute "CREATE SEQUENCE host_storages_id_seq START #{seq_start_value}"
 
       # Add a primary key column named id and use the previously created sequence
       # This will automatically fill in new primary keys for all existing rows
-      # in the hosts_storages table
-      execute "ALTER TABLE hosts_storages ADD COLUMN id BIGINT PRIMARY KEY "\
-              "NOT NULL DEFAULT NEXTVAL('hosts_storages_id_seq')"
+      # in the host_storages table
+      execute "ALTER TABLE host_storages ADD COLUMN id BIGINT PRIMARY KEY "\
+              "NOT NULL DEFAULT NEXTVAL('host_storages_id_seq')"
 
       # Now update the sequence to be owned by the pkey column so that when it
       # gets deleted the sequence also is deleted
-      execute "ALTER SEQUENCE hosts_storages_id_seq OWNED BY hosts_storages.id"
+      execute "ALTER SEQUENCE host_storages_id_seq OWNED BY host_storages.id"
     end
   end
 
   def down
-    remove_column :hosts_storages, :id
-    remove_column :hosts_storages, :read_only
+    remove_column :host_storages, :id
+    remove_column :host_storages, :read_only
+    rename_table :host_storages, :hosts_storages
   end
 end
