728217117b85f53e9cf313ac087ac715bf58e6a4
dkorn <dkorn@redhat.com>
2015-10-27 10:31:51 +0200

    Container replicator events db changes

--- app/models/container_replicator.rb
+++ app/models/container_replicator.rb
@@ -9,5 +9,19 @@ class ContainerReplicator < ActiveRecord::Base
   has_many :selector_parts, -> { where(:section => "selectors") }, :class_name => "CustomAttribute", :as => :resource, :dependent => :destroy
   has_many :container_nodes, -> { distinct }, :through => :container_groups
 
+  include EventMixin
+
   acts_as_miq_taggable
+
+  def event_where_clause(assoc = :ems_events)
+    case assoc.to_sym
+    when :ems_events
+      # TODO: improve relationship using the id
+      ["container_namespace = ? AND container_replicator_name = ? AND #{events_table_name(assoc)}.ems_id = ?",
+       container_project.name, name, ems_id]
+    when :policy_events
+      # TODO: implement policy events and its relationship
+      ["#{events_table_name(assoc)}.ems_id = ?", ems_id]
+    end
+  end
 end
