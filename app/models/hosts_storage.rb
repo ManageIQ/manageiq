eca78a96bc9744b0d13d86688ef12bdea79ee508
Adam Grare <agrare@redhat.com>
2015-10-28 09:51:21 -0400

    Rename HostStorage model to match new table name

--- app/models/hosts_storage.rb
+++
@@ -1,5 +0,0 @@
-class HostsStorage < ApplicationRecord
-  self.table_name = "host_storages"
-  belongs_to :host
-  belongs_to :storage
-end
--- spec/migrations/20150921204114_add_vmware_ro_datastores_to_hosts_storages_spec.rb
+++ spec/migrations/20150921204114_add_vmware_ro_datastores_to_hosts_storages_spec.rb
@@ -1,7 +1,7 @@
 require_migration
 
 describe AddVmwareRoDatastoresToHostsStorages do
-  let(:hosts_storages_stub) { migration_stub(:HostsStorage) }
+  let(:host_storages_stub) { migration_stub(:HostStorage) }
 
   migration_context :up do
     it "Adds ID in correct region" do
