d8c624fef32387235b4cd924501b509a7ca50e3f
Adam Grare <agrare@redhat.com>
2015-10-28 10:11:38 -0400

    Add spec tests for vmware_ro_datastores migration

--- spec/migrations/20150921204114_add_vmware_ro_datastores_to_hosts_storages_spec.rb
+++ spec/migrations/20150921204114_add_vmware_ro_datastores_to_hosts_storages_spec.rb
@@ -1,11 +1,38 @@
 require_migration
 
 describe AddVmwareRoDatastoresToHostsStorages do
+  class AddVmwareRoDatastoresToHostsStorages::HostsStorage < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled # disable STI
+    self.table_name = "hosts_storages"
+  end
+
   let(:host_storages_stub) { migration_stub(:HostStorage) }
+  let(:hosts_storages_stub) { migration_stub(:HostsStorage) }
 
   migration_context :up do
     it "Adds ID in correct region" do
+      seq_start = ActiveRecord::Base.rails_sequence_start
+      seq_start = 1 if seq_start == 0
+
+      h = hosts_storages_stub.create(:host_id => 1, :storage_id => 2)
       migrate
+      h = host_storages_stub.first
+
+      expect(h.id).to eq(seq_start)
+    end
+    it "Maintains host_id after rename" do
+      h = hosts_storages_stub.create(:host_id => 1, :storage_id => 2)
+      migrate
+      h = host_storages_stub.first
+
+      expect(h.host_id).to eq(1)
+    end
+    it "Maintains storage_id after rename" do
+      h = hosts_storages_stub.create(:host_id => 1, :storage_id => 2)
+      migrate
+      h = host_storages_stub.first
+
+      expect(h.storage_id).to eq(2)
     end
   end
 end
