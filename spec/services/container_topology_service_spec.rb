cb5f5b0c37a5db059e865a1b7710934f2bc32bad
Brandon Dunne <bdunne@redhat.com>
2015-10-31 09:21:57 -0400

    share common setup

--- spec/services/container_topology_service_spec.rb
+++ spec/services/container_topology_service_spec.rb
@@ -23,32 +23,24 @@ describe ContainerTopologyService do
       expect(container_topology_service.build_topology.keys).to match_array([:items, :kinds, :relations])
     end
 
+    let(:container) { Container.create(:name => "ruby-example", :ems_ref => "3572afee-3a41-11e5-a79a-001a4a231290_ruby-helloworld-database_openshift\n/mysql-55-centos7:latest", :state => 'running') }
+    let(:container_condition) { ContainerCondition.create(:name => 'Ready', :status => 'True') }
+    let(:container_def) { ContainerDefinition.create(:name => "ruby-example", :ems_ref => 'b6976f84-5184-11e5-950e-001a4a231290_ruby-helloworld_172.30.194.30:5000/test/origin-ruby-sample@sha256:0cd076c9beedb3b1f5cf3ba43da6b749038ae03f5886b10438556e36ec2a0dd9', :container => container) }
+    let(:container_node) { ContainerNode.create(:ext_management_system => ems_kube, :name => "127.0.0.1", :ems_ref => "905c90ba-3e00-11e5-a0d2-18037327aaeb", :container_conditions => [container_condition], :lives_on => vm_rhev) }
+    let(:ems_kube) { FactoryGirl.create(:ems_kubernetes, :name => "ems_kube") }
+    let(:ems_rhev) { FactoryGirl.create(:ems_redhat, :name => "ems_rhev") }
+    let(:vm_rhev) { FactoryGirl.create(:vm_redhat, :name => "vm1", :uid_ems => "558d9a08-7b13-11e5-8546-129aa6621998", :ext_management_system => ems_rhev) }
+
     it "topology contains the expected structure and content" do
-      ems_rhev = FactoryGirl.create(:ems_redhat, :name => "ems_rhev")
-      ems_kube = FactoryGirl.create(:ems_kubernetes, :name => "ems_kube")
-
       # vm and host test cross provider correlation to infra provider
-      vm_rhev = FactoryGirl.create(:vm_redhat, :name => "vm1",
-                                   :uid_ems => "558d9a08-7b13-11e5-8546-129aa6621998",
-                                   :ext_management_system => ems_rhev)
-      vm_rhev.stub(:power_state).and_return("on")
-
       hardware = FactoryGirl.create(:hardware, :cpu_sockets => 2, :cpu_cores_per_socket => 4, :cpu_total_cores => 8)
       host = FactoryGirl.create(:host, :name => "host1",
                                 :uid_ems => "abcd9a08-7b13-11e5-8546-129aa6621999",
                                 :ext_management_system => ems_rhev,
                                 :hardware => hardware)
-      vm_rhev.update_attribute(:host, host)
+      vm_rhev.update_attributes(:host => host, :raw_power_state => "up")
 
-      container_topology_service.stub(:retrieve_providers).and_return([ems_kube ])
-      container_condition = ContainerCondition.create(:name => 'Ready', :status => 'True')
-      container = Container.create(:name => "ruby-example", :ems_ref => '3572afee-3a41-11e5-a79a-001a4a231290_ruby-helloworld-database_openshift
-/mysql-55-centos7:latest', :state => 'running')
-      container_def = ContainerDefinition.create(:name => "ruby-example", :ems_ref => 'b6976f84-5184-11e5-950e-001a4a231290_ruby-helloworld_172.30.194.30:5000/test/origin-ruby-sample@sha256:0cd076c9beedb3b1f5cf3ba43da6b749038ae03f5886b10438556e36ec2a0dd9', :container => container)
-
-      container_node = ContainerNode.create(:ext_management_system => ems_kube,
-                                            :name => "127.0.0.1", :ems_ref => "905c90ba-3e00-11e5-a0d2-18037327aaeb",
-                                            :container_conditions => [container_condition], :lives_on => vm_rhev)
+      container_topology_service.stub(:retrieve_providers).and_return([ems_kube])
       container_replicator = ContainerReplicator.create(:ext_management_system => ems_kube,
                                                         :ems_ref => "8f8ca74c-3a41-11e5-a79a-001a4a231290",
                                                         :name => "replicator1")
@@ -106,23 +98,9 @@ describe ContainerTopologyService do
     end
 
     it "topology contains the expected structure when vm is off" do
-      ems_kube = FactoryGirl.create(:ems_kubernetes, :name => "ems_kube")
-      ems_rhev = FactoryGirl.create(:ems_redhat, :name => "ems_rhev")
       # vm and host test cross provider correlation to infra provider
-      vm_rhev2 = FactoryGirl.create(:vm_redhat, :name => "vm2", :uid_ems => "358d9a08-7b13-11e5-8546-129aa6621998",
-                                    :ext_management_system => ems_rhev, :raw_power_state => "down")
-      vm_rhev2.stub(:power_state).and_return("off")
-      container_topology_service.stub(:retrieve_providers).and_return([ems_kube ])
-      container_condition = ContainerCondition.create(:name => 'Ready', :status => 'True')
-      container = Container.create(:name => "ruby-example",
-                                   :ems_ref => '3572afee-3a41-11e5-a79a-001a4a231290_ruby-helloworld-database_openshift
-/mysql-55-centos7:latest', :state => 'running')
-      container_def = ContainerDefinition.create(:name => "ruby-example", :ems_ref => 'b6976f84-5184-11e5-950e-001a4a231290_ruby-helloworld_172.30.194.30:5000/test/origin-ruby-sample@sha256:0cd076c9beedb3b1f5cf3ba43da6b749038ae03f5886b10438556e36ec2a0dd9',
-                                                 :container => container)
-
-      container_node = ContainerNode.create(:ext_management_system => ems_kube, :name => "127.0.0.1",
-                                            :ems_ref => "905c90ba-3e00-11e5-a0d2-18037327aaeb",
-                                            :container_conditions => [container_condition], :lives_on => vm_rhev2)
+      vm_rhev.update_attributes(:raw_power_state => "down")
+      container_topology_service.stub(:retrieve_providers).and_return([ems_kube])
 
       container_group = ContainerGroup.create(:ext_management_system => ems_kube, :container_node => container_node,
                                               :name => "myPod", :ems_ref => "96c35ccd-3e00-11e5-a0d2-18037327aaeb",
@@ -148,15 +126,15 @@ describe ContainerTopologyService do
                                                                           :status => "Running", :kind => "Pod", :miq_id => container_group.id)
       topology[:items]["3572afee-3a41-11e5-a79a-001a4a231290_ruby-helloworld-database_openshift\n/mysql-55-centos7:latest"].should eql(:id => "3572afee-3a41-11e5-a79a-001a4a231290_ruby-helloworld-database_openshift\n/mysql-55-centos7:latest", :name => "ruby-example", :status => "Running", :kind => "Container",  :miq_id => container.id)
 
-      topology[:items]["358d9a08-7b13-11e5-8546-129aa6621998"].should eql(:id => "358d9a08-7b13-11e5-8546-129aa6621998",
-                                                                          :name => "vm2", :status => "Off",
-                                                                          :kind => "VM", :miq_id => vm_rhev2.id,
+      topology[:items]["558d9a08-7b13-11e5-8546-129aa6621998"].should eql(:id => "558d9a08-7b13-11e5-8546-129aa6621998",
+                                                                          :name => "vm1", :status => "Off",
+                                                                          :kind => "VM", :miq_id => vm_rhev.id,
                                                                           :provider => "ems_rhev")
 
       topology[:relations].should include(:source => "95e49048-3e00-11e5-a0d2-18037327aaeb",
                                           :target => "96c35ccd-3e00-11e5-a0d2-18037327aaeb")
       topology[:relations].should include(:source => "905c90ba-3e00-11e5-a0d2-18037327aaeb",
-                                          :target => "358d9a08-7b13-11e5-8546-129aa6621998")
+                                          :target => "558d9a08-7b13-11e5-8546-129aa6621998")
     end
   end
 end
