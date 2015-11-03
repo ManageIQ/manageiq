003aa2a86a1ca84a95b77d881521c6928069ecaa
Brandon Dunne <bdunne@redhat.com>
2015-11-03 10:01:45 -0500

    Move before block into proper context

--- spec/models/miq_server/configuration_management_spec.rb
+++ spec/models/miq_server/configuration_management_spec.rb
@@ -3,11 +3,11 @@ require "spec_helper"
 describe MiqServer do
   describe "ConfigurationManagement" do
     describe "#get_config" do
-      before do
-        _guid, @local_server, _zone = EvmSpecHelper.local_guid_miq_server_zone
-      end
-
       context "local server" do
+        before do
+          _guid, @local_server, _zone = EvmSpecHelper.local_guid_miq_server_zone
+        end
+
         it "with no changes in the database" do
           expect(@local_server.get_config("vmdb").config).to be_kind_of(Hash)
         end
