56beb9b8fb5032ec2d561d9a54ebcc8cad95d872
Dan Clarizio <dclarizi@redhat.com>
2015-11-04 08:12:25 -0800

    Merge pull request #5259 from himdel/bz1212470
    DialogRunner#dialog_get_form_vars - handle DateTime field input in random order

--- spec/controllers/application_controller/dialog_runner_spec.rb
+++ spec/controllers/application_controller/dialog_runner_spec.rb
@@ -61,4 +61,44 @@ describe CatalogController do
       expect(response.body).to eq({:values => "01/02/2015"}.to_json)
     end
   end
+
+  describe "#dialog_get_form_vars" do
+    include_context "valid session"
+
+    let(:dialog) { active_record_instance_double("Dialog") }
+    let(:wf) { double(:dialog => dialog) }
+    let(:dialog_field) { DialogFieldDateTimeControl.new }
+
+    let(:session) { {:edit => {:wf => wf}} }
+
+    before do
+      dialog.stub(:field).with("name").and_return(dialog_field)
+      dialog.stub(:field_name_exist?).with("name").and_return(true)
+      dialog.stub(:dialog_fields) { [Struct.new(:name, :type).new("name", "DialogFieldDateTimeControl")] }
+      wf.stub(:value).with("name") { dialog_field.value }
+      wf.stub(:set_value) { |_, val| dialog_field.instance_variable_set(:@value, val) }
+      Dialog.stub(:find_by_id).and_return(dialog)
+
+      dialog_field.instance_variable_set(:@value, "04/05/2015 14:52")
+      controller.instance_variable_set(:@edit, {:rec_id => nil, :wf => wf})
+    end
+
+    it "keeps hours and minutes when setting date" do
+      controller.stub(:params).and_return({'miq_date__name' => "11/13/2015"})
+      controller.send(:dialog_get_form_vars)
+      expect(dialog_field.value).to eq('11/13/2015 14:52')
+    end
+
+    it "keeps date and minutes when setting hours" do
+      controller.stub(:params).and_return({'start_hour' => "4"})
+      controller.send(:dialog_get_form_vars)
+      expect(dialog_field.value).to eq('04/05/2015 04:52')
+    end
+
+    it "keeps date and hourse when setting minutes" do
+      controller.stub(:params).and_return({'start_min' => "6"})
+      controller.send(:dialog_get_form_vars)
+      expect(dialog_field.value).to eq('04/05/2015 14:06')
+    end
+  end
 end
