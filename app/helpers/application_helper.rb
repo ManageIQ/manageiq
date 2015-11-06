67ac28ccf9b6075d781e6d23c65eb0934f5f71b4
Laura Galis <lgalis@redhat.com>
2015-11-06 16:03:58 -0500

    The pagination links need to account for restful paths
    https://bugzilla.redhat.com/show_bug.cgi?id=1277641

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1029,9 +1029,13 @@ module ApplicationHelper
   def update_paging_url_parms(action_url, parameter_to_update = {})
     url = update_query_string_params(parameter_to_update)
     action, an_id = action_url.split("/", 2)
-    url[:action] = action
-    url[:id] = an_id unless an_id.nil?
-    url_for(url)
+    if controller.send(:restful?) && action == 'show'
+      polymorphic_path(@record, url)
+    else
+      url[:action] = action
+      url[:id] = an_id unless an_id.nil?
+      url_for(url)
+    end
   end
 
   def update_query_string_params(update_this_param)
