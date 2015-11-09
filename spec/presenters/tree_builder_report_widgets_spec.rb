ddb1a7b26a36a2653bfc234c10769d728c9792f0
Brandon Dunne <bdunne@redhat.com>
2015-11-09 12:15:12 -0500

    Add test for TreeBuilderReportWidgets#x_get_tree_roots
    leverage subject

--- spec/presenters/tree_builder_report_widgets_spec.rb
+++ spec/presenters/tree_builder_report_widgets_spec.rb
@@ -1,12 +1,22 @@
 require "spec_helper"
 
 describe TreeBuilderReportWidgets do
+  subject { described_class.new("cb_rates_tree", "cb_rates", {}) }
+
+  it "#x_get_tree_roots" do
+    expect(subject.send(:x_get_tree_roots, false, nil)).to match_array([
+      {:id => "r",  :text => "Reports",   :image => "folder", :tip => "Reports"},
+      {:id => "c",  :text => "Charts",    :image => "folder", :tip => "Charts"},
+      {:id => "rf", :text => "RSS Feeds", :image => "folder", :tip => "RSS Feeds"},
+      {:id => "m",  :text => "Menus",     :image => "folder", :tip => "Menus"}
+    ])
+  end
+
   it "#x_get_tree_custom_kids" do
     widget1 = FactoryGirl.create(:miq_widget)
     widget2 = FactoryGirl.create(:miq_widget)
     FactoryGirl.create(:miq_widget, :content_type => "menu")
 
-    tree = described_class.new("cb_rates_tree", "cb_rates", {}).send(:x_get_tree_custom_kids, {:id => "-r"}, false, nil)
-    expect(tree).to match_array([widget1, widget2])
+    expect(subject.send(:x_get_tree_custom_kids, {:id => "-r"}, false, nil)).to match_array([widget1, widget2])
   end
 end
