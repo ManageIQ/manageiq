3b5049d498c1370e0df0a0476ce688be08f391c4
Brandon Dunne <bdunne@redhat.com>
2015-11-11 15:43:36 -0500

    Indicate that these specs are testing private methods
    [skip ci]

--- spec/presenters/tree_builder_report_widgets_spec.rb
+++ spec/presenters/tree_builder_report_widgets_spec.rb
@@ -3,11 +3,11 @@ require "spec_helper"
 describe TreeBuilderReportWidgets do
   subject { described_class.new("cb_rates_tree", "cb_rates", {}) }
 
-  it "#set_locals_for_render" do
+  it "#set_locals_for_render (private)" do
     expect(subject.send(:set_locals_for_render)).to have_attributes(:id_prefix => 'widgets_', :autoload => true)
   end
 
-  it "#x_get_tree_roots" do
+  it "#x_get_tree_roots (private)" do
     expect(subject.send(:x_get_tree_roots, false, nil)).to match_array([
       {:id => "r",  :text => "Reports",   :image => "folder", :tip => "Reports"},
       {:id => "c",  :text => "Charts",    :image => "folder", :tip => "Charts"},
@@ -16,7 +16,7 @@ describe TreeBuilderReportWidgets do
     ])
   end
 
-  it "#x_get_tree_custom_kids" do
+  it "#x_get_tree_custom_kids (private)" do
     widget1 = FactoryGirl.create(:miq_widget)
     widget2 = FactoryGirl.create(:miq_widget)
     FactoryGirl.create(:miq_widget, :content_type => "menu")
