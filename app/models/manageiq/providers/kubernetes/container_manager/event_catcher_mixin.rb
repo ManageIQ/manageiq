cf29f22619db8a8f8aab233b4dbb96555215823d
Joe VLcek <jvlcek@redhat.com>
2015-11-12 14:36:24 -0500

    Move event_monitor_running above comment block

--- app/models/manageiq/providers/kubernetes/container_manager/event_catcher_mixin.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/event_catcher_mixin.rb
@@ -26,10 +26,10 @@ module ManageIQ::Providers::Kubernetes::ContainerManager::EventCatcherMixin
 
   def monitor_events
     event_monitor_handle.start
+    event_monitor_running
     # TODO: since event_monitor_handle is returning only events that
     # are generated starting from this moment we need to pull the
     # entire # inventory to make sure that it's up-to-date.
-    event_monitor_running
     event_monitor_handle.each do |event|
       # Sleeping here is not necessary because the events are delivered
       # asynchronously when available.
