aaf0b0b4e3e0aa17c3c85b21eb7e644e2db83748
Daniel Berger <dberger@redhat.com>
2015-11-12 14:16:28 -0700

    Add network security group parsing support.
    Added specs for security groups and firewalls.
    
    Use service instances that have already been created.

--- spec/models/manageiq/providers/azure/cloud_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/azure/cloud_manager/refresher_spec.rb
@@ -5,12 +5,18 @@ describe ManageIQ::Providers::Azure::CloudManager do
   before(:each) do
     _guid, _server, zone = EvmSpecHelper.create_guid_miq_server_zone
     @ems = FactoryGirl.create(:ems_azure, :zone => zone, :provider_region => "eastus")
+
+    @userid   = "f895b5ef-3bb5-4366-8ce5-123456789012"
+    @passwd   = "5YcZ1lwRmNPWgo82X%2F1l97Fe4VaBGi%2B123456789012"
+    @tenantid = "a50f9983-d1a2-4a8d-be7d-123456789012"
+
     cred = {
-      :userid   => "f895b5ef-3bb5-4366-8ce5-123456789012",
-      :password => "5YcZ1lwRmNPWgo82X%2F1l97Fe4VaBGi%2B123456789012"
+      :userid   => @userid,
+      :password => @passwd,
     }
+
     @ems.authentications << FactoryGirl.create(:authentication, cred)
-    @ems.update_attributes(:azure_tenant_id => "a50f9983-d1a2-4a8d-be7d-123456789012")
+    @ems.update_attributes(:azure_tenant_id => @tenantid)
 
     # A true thread may fail the test with VCR
     allow(Thread).to receive(:new) do |*args, &block|
@@ -34,6 +40,7 @@ describe ManageIQ::Providers::Azure::CloudManager do
       assert_specific_az
       assert_specific_cloud_network
       assert_specific_flavor
+      assert_specific_security_group
       assert_specific_vm_powered_on
       assert_specific_vm_powered_off
       assert_specific_template
@@ -66,7 +73,7 @@ describe ManageIQ::Providers::Azure::CloudManager do
   def assert_ems
     expect(@ems).to have_attributes(
       :api_version => nil,
-      :uid_ems     => "a50f9983-d1a2-4a8d-be7d-123456789012"
+      :uid_ems     => @tenantid
     )
     expect(@ems.flavors.size).to eql(33)
     expect(@ems.availability_zones.size).to eql(1)
@@ -76,6 +83,30 @@ describe ManageIQ::Providers::Azure::CloudManager do
 
     expect(@ems.orchestration_stacks.size).to eql(7)
     expect(@ems.direct_orchestration_stacks.size).to eql(6)
+  end
+
+  def assert_specific_security_group
+    @sg = ManageIQ::Providers::Azure::CloudManager::SecurityGroup.where(:name => "Chef-Prod").first
+
+    @sg.should have_attributes(
+      :name        => "Chef-Prod",
+      :description => "Chef-Prod - Chef-Prod - eastus"
+    )
+
+    expected_firewall_rules = [
+      {:host_protocol => "TCP", :direction => "Inbound", :port => 22,  :end_port => 22,  :source_ip_range => "*"},
+      {:host_protocol => "TCP", :direction => "Inbound", :port => 80,  :end_port => 80,  :source_ip_range => "*"},
+      {:host_protocol => "TCP", :direction => "Inbound", :port => 443, :end_port => 443, :source_ip_range => "*"}
+    ]
+
+    @sg.firewall_rules.size.should == 3
+
+    @sg.firewall_rules
+      .order(:host_protocol, :direction, :port, :end_port, :source_ip_range, :source_security_group_id)
+      .zip(expected_firewall_rules)
+      .each do |actual, expected|
+        actual.should have_attributes(expected)
+      end
   end
 
   def assert_specific_flavor
