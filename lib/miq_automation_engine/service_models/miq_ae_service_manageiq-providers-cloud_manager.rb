e128c41cf0ccbb00f2a4b1663aa242bdd91035ea
Bill Wei <bilwei@redhat.com>
2015-11-12 16:20:32 -0500

    Add automate Available_Resource_Group method for dropdown box

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-cloud_manager.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-cloud_manager.rb
@@ -2,8 +2,8 @@ module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_CloudManager < MiqAeServiceExtManagementSystem
     expose :availability_zones,     :association => true
     expose :cloud_networks,         :association => true
-    expose :public_networks,  :association => true
-    expose :private_networks, :association => true
+    expose :public_networks,        :association => true
+    expose :private_networks,       :association => true
     expose :cloud_tenants,          :association => true
     expose :flavors,                :association => true
     expose :floating_ips,           :association => true
