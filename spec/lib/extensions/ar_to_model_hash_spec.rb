0e2238d92b92878181a4f700a5b825a10594db57
Joe Rafaniello <jrafanie@redhat.com>
2015-11-13 10:18:01 -0500

    Cleanup some specs

--- spec/lib/extensions/ar_to_model_hash_spec.rb
+++ spec/lib/extensions/ar_to_model_hash_spec.rb
@@ -5,6 +5,7 @@ describe ToModelHash do
     let(:test_disk_class)     { Class.new(ActiveRecord::Base) { self.table_name = "test_disks" } }
     let(:test_hardware_class) { Class.new(ActiveRecord::Base) { self.table_name = "test_hardwares" } }
     let(:test_vm_class)       { Class.new(ActiveRecord::Base) { self.table_name = "test_vms" } }
+    let(:fixed_options)       { test_vm_class.send(:to_model_hash_options_fixup, @test_to_model_hash_options) }
 
     before do
       ActiveRecord::Schema.define do
@@ -30,63 +31,76 @@ describe ToModelHash do
       test_vm_class.has_one          :test_hardware, :anonymous_class => test_hardware_class, :dependent => :destroy
     end
 
-    it "virtual_column" do
-      test_to_model_hash_options = {
-        "cols" => ["bitness"]
-      }
-
-      test_vm_class.virtual_column :bitness, :type => :integer, :uses => :test_hardware
-
-      options = test_vm_class.send(:to_model_hash_options_fixup, test_to_model_hash_options)
-      expect(test_vm_class.new.send(:to_model_hash_build_preload, options)).to eq [:bitness]
-    end
-
-    it "virtual_column and include association column" do
-      test_to_model_hash_options = {
-        "cols"    => ["bitness"],
-        "include" => {"test_hardware" => {"columns" => ["test_vm_id"]}}
-      }
-
-      test_vm_class.virtual_column :bitness, :type => :integer, :uses => :test_hardware
-
-      options = test_vm_class.send(:to_model_hash_options_fixup, test_to_model_hash_options)
-      expect(test_vm_class.new.send(:to_model_hash_build_preload, options)).to match_array [:bitness, :test_hardware]
-    end
-
-    it "virtual column matches included association column" do
-      test_to_model_hash_options = {
+    it "included column" do
+      @test_to_model_hash_options = {
         "include" => {"test_hardware" => {"columns" => ["bitness"]}}
       }
 
-      test_vm_class.virtual_column :bitness, :type => :integer, :uses => :test_hardware
-
-      options = test_vm_class.send(:to_model_hash_options_fixup, test_to_model_hash_options)
-      expect(test_vm_class.new.send(:to_model_hash_build_preload, options)).to eq [:test_hardware]
+      expect(test_vm_class.new.send(:to_model_hash_build_preload, fixed_options)).to eq [:test_hardware]
     end
 
-    it "virtual column on included association" do
-      test_to_model_hash_options = {
-        "include" => {"test_hardware" => {"columns" => ["num_disks"]}}
+    it "nested included columns" do
+      @test_to_model_hash_options = {
+        "include" => {
+          "test_hardware" => {
+            "columns" => ["bitness"],
+            "include" => {"test_disks" => {"columns" => ["num_disks"]}}
+          }
+        }
       }
 
-      test_hardware_class.virtual_column :num_disks, :type => :integer, :uses => :test_disks
-
-      options = test_vm_class.send(:to_model_hash_options_fixup, test_to_model_hash_options)
-      expect(test_vm_class.new.send(:to_model_hash_build_preload, options)).to match_array [{:test_hardware => [:num_disks]}]
+      expect(test_vm_class.new.send(:to_model_hash_build_preload, fixed_options)).to eq [{:test_hardware => [:test_disks]}]
     end
 
-    it "virtual and regular column includes from different associations" do
-      test_to_model_hash_options = {
-        "include" => [
-          {"test_hardware" => {"columns" => ["num_disks"]}},
-          {"test_disks"    => {"columns" => ["something"]}}
-        ]
-      }
+    context "virtual columns" do
+      it "virtual column on main table" do
+        @test_to_model_hash_options = {
+          "cols" => ["bitness"]
+        }
 
-      test_hardware_class.virtual_column :num_disks, :type => :integer, :uses => :test_disks
+        test_vm_class.virtual_column :bitness, :type => :integer, :uses => :test_hardware
+        expect(test_vm_class.new.send(:to_model_hash_build_preload, fixed_options)).to eq([:bitness])
+      end
 
-      options = test_vm_class.send(:to_model_hash_options_fixup, test_to_model_hash_options)
-      expect(test_vm_class.new.send(:to_model_hash_build_preload, options)).to match_array [{:test_hardware => [:num_disks]}]
+      it "virtual column and included column" do
+        @test_to_model_hash_options = {
+          "cols"    => ["bitness"],
+          "include" => {"test_hardware" => {"columns" => ["test_vm_id"]}}
+        }
+
+        test_vm_class.virtual_column :bitness, :type => :integer, :uses => :test_hardware
+        expect(test_vm_class.new.send(:to_model_hash_build_preload, fixed_options)).to match_array [:bitness, :test_hardware]
+      end
+
+      it "virtual column matches included association column" do
+        @test_to_model_hash_options = {
+          "include" => {"test_hardware" => {"columns" => ["bitness"]}}
+        }
+
+        test_vm_class.virtual_column :bitness, :type => :integer, :uses => :test_hardware
+        expect(test_vm_class.new.send(:to_model_hash_build_preload, fixed_options)).to eq [:test_hardware]
+      end
+
+      it "included association virtual column " do
+        @test_to_model_hash_options = {
+          "include" => {"test_hardware" => {"columns" => ["num_disks"]}}
+        }
+
+        test_hardware_class.virtual_column :num_disks, :type => :integer, :uses => :test_disks
+        expect(test_vm_class.new.send(:to_model_hash_build_preload, fixed_options)).to match_array [{:test_hardware => [:num_disks]}]
+      end
+
+      it "virtual and regular column included from different associations" do
+        @test_to_model_hash_options = {
+          "include" =>  {
+            "test_hardware" => {"columns" => ["num_disks"]},
+            "test_disks"    => {"columns" => ["something"]}
+          }
+        }
+
+        test_hardware_class.virtual_column :num_disks, :type => :integer, :uses => :test_disks
+        expect(test_vm_class.new.send(:to_model_hash_build_preload, fixed_options)).to match_array [{:test_hardware => [:num_disks]}]
+      end
     end
   end
 end
