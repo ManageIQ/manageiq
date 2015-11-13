dd61eee88fea4ca01802f03a44da3b2dc7b4761d
Michael Stack <michael.stack@gmail.com>
2015-11-13 14:53:35 -0600

    Add autofocus, and current users name
    - Using the 'autofocus' HTML5 attribute with directive fallback
    - After a successful login fire off a users query filtered to fetch the current users info
    - Only the current users name and email are currently being captured.

--- spa_ui/self_service/client/app/services/session.service.js
+++ spa_ui/self_service/client/app/services/session.service.js
@@ -8,14 +8,16 @@
   function SessionFactory($http, moment) {
     var model = {
       token: null,
-      expiresOn: moment().subtract(1, 'seconds')
+      expiresOn: moment().subtract(1, 'seconds'),
+      user: {}
     };
 
     var service = {
       current: model,
       create: create,
       destroy: destroy,
-      active: active
+      active: active,
+      currentUser: currentUser
     };
 
     destroy();
@@ -31,7 +33,16 @@
     function destroy() {
       model.token = null;
       model.expiresOn = moment().subtract(1, 'seconds');
+      model.user = {};
       delete $http.defaults.headers.common['X-Auth-Token'];
+    }
+
+    function currentUser(user) {
+      if (angular.isDefined(user)) {
+        model.user = user;
+      }
+
+      return model.user;
     }
 
     // Helpers
