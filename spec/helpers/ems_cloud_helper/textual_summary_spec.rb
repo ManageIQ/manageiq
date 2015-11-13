0a80ed9a586f74dea0207305ba4e6155abd5f9db
Brandon Dunne <bdunne@redhat.com>
2015-11-13 09:04:50 -0500

    Remove extra contexts and create a common one

--- spec/helpers/ems_cloud_helper/textual_summary_spec.rb
+++ spec/helpers/ems_cloud_helper/textual_summary_spec.rb
@@ -1,25 +1,23 @@
 require "spec_helper"
 
 describe EmsCloudHelper do
-  def role_allows(_)
-    true
-  end
+  context "#textual_instances and #textual_images" do
+    def role_allows(_)
+      true
+    end
 
-  before do
-    @ems = FactoryGirl.create(:ems_openstack, :zone => FactoryGirl.build(:zone))
-    controller.stub(:restful?).and_return(true)
-    controller.stub(:controller_name).and_return("ems_cloud")
-  end
+    before do
+      @ems = FactoryGirl.create(:ems_openstack, :zone => FactoryGirl.build(:zone))
+      controller.stub(:restful?).and_return(true)
+      controller.stub(:controller_name).and_return("ems_cloud")
+    end
 
-  context "textual_instances" do
     it "sets restful path for instances in summary for restful controllers" do
       FactoryGirl.create(:vm_openstack, :ems_id => @ems.id)
 
       expect(textual_instances[:link]).to eq("/ems_cloud/#{@ems.id}?display=instances")
     end
-  end
 
-  context "textual_images" do
     it "sets restful path for images in summary for restful controllers" do
       FactoryGirl.create(:template_cloud, :ems_id => @ems.id)
 
