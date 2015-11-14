b7ae432eef1f4137127cdb7fd3b7892035322dfa
Brandon Dunne <bdunne@redhat.com>
2015-11-14 07:29:25 -0500

    Remove extra references to self

--- spec/helpers/orchestration_stack_helper/textual_summary_spec.rb
+++ spec/helpers/orchestration_stack_helper/textual_summary_spec.rb
@@ -4,17 +4,17 @@ describe OrchestrationStackHelper::TextualSummary do
   before { @record = FactoryGirl.build(:orchestration_stack) }
 
   it "#textual_group_lifecycle should includes retirement_date" do
-    expect(self.textual_group_lifecycle).to include(:retirement_date)
+    expect(textual_group_lifecycle).to include(:retirement_date)
   end
 
   describe "#textual_retirement_date" do
     it "should returns 'Never' if nil values is passed" do
-      expect(self.textual_retirement_date).to include(:value => "Never")
+      expect(textual_retirement_date).to include(:value => "Never")
     end
 
     it "should returns date in %x format" do
       @record.retires_on = Date.new(2015, 11, 01)
-      expect(self.textual_retirement_date).to include(:value => "11/01/15")
+      expect(textual_retirement_date).to include(:value => "11/01/15")
     end
   end
 end
