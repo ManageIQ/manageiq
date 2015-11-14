39e48dcb3449932bba9edc6954e2bba20cf922bb
Brandon Dunne <bdunne@redhat.com>
2015-11-14 08:18:11 -0500

    Remove unnecessary contexts, clean up descriptions

--- spec/helpers/container_summary_helper.rb
+++ spec/helpers/container_summary_helper.rb
@@ -18,43 +18,37 @@ describe ContainerSummaryHelper do
 
   context ".textual_container_project" do
     subject { textual_container_project }
-    context "single relationship link allowed" do
-      it 'should return linked relationship' do
-        @user.stub(:role_allows?).and_return(true)
 
-        subject.keys.should be == REL_HASH_WITH_LINK
-        subject[:value].should be == container_project.name
-      end
+    it 'show link when role allows' do
+      @user.stub(:role_allows?).and_return(true)
+
+      subject.keys.should be == REL_HASH_WITH_LINK
+      subject[:value].should be == container_project.name
     end
 
-    context "single relationship link not allowed" do
-      it 'should not return linked relationship' do
-        @user.stub(:role_allows?).and_return(false)
+    it 'hide link when role does not allow' do
+      @user.stub(:role_allows?).and_return(false)
 
-        subject.keys.should be == REL_HASH_WITHOUT_LINK
-        subject[:value].should be == container_project.name
-      end
+      subject.keys.should be == REL_HASH_WITHOUT_LINK
+      subject[:value].should be == container_project.name
     end
   end
 
   context ".textual_containers" do
     subject { textual_containers }
-    context "multiple relationships link allowed" do
-      it 'should return linked relationship' do
-        @user.stub(:role_allows?).and_return(true)
 
-        subject.keys.should be == REL_HASH_WITH_LINK
-        subject[:value].should be == "2"
-      end
+    it 'show link when role allows' do
+      @user.stub(:role_allows?).and_return(true)
+
+      subject.keys.should be == REL_HASH_WITH_LINK
+      subject[:value].should be == "2"
     end
 
-    context "multiple relationships link not allowed" do
-      it 'should not return linked relationship' do
-        @user.stub(:role_allows?).and_return(false)
+    it 'hide link when role does not allow' do
+      @user.stub(:role_allows?).and_return(false)
 
-        subject.keys.should be == REL_HASH_WITHOUT_LINK
-        subject[:value].should be == "2"
-      end
+      subject.keys.should be == REL_HASH_WITHOUT_LINK
+      subject[:value].should be == "2"
     end
   end
 end
