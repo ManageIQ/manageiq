bfccaed4af9eb06e97857c5c4187e78aac7a51fa
Brandon Dunne <bdunne@redhat.com>
2015-11-14 07:05:43 -0500

    Describe the module, no need for extra context

--- spec/helpers/orchestration_stack_helper/textual_summary_spec.rb
+++ spec/helpers/orchestration_stack_helper/textual_summary_spec.rb
@@ -1,35 +1,33 @@
 require "spec_helper"
 
-describe OrchestrationStackHelper do
+describe OrchestrationStackHelper::TextualSummary do
   before :each do
     @record = FactoryGirl.create(:orchestration_stack)
   end
 
-  context "::TextualSummary" do
-    it "defines textual_group_lifecycle" do
-      expect(self).to respond_to(:textual_group_lifecycle)
+  it "defines textual_group_lifecycle" do
+    expect(self).to respond_to(:textual_group_lifecycle)
+  end
+
+  describe "#textual_group_lifecycle" do
+    it "should includes retirement_date" do
+      expect(self.textual_group_lifecycle).to include(:retirement_date)
+    end
+  end
+
+  it "defines textual_retirement_date" do
+    expect(self).to respond_to(:textual_retirement_date)
+  end
+
+  describe "#textual_retirement_date" do
+    it "should returns 'Never' if nil values is passed" do
+      @record.retires_on = nil
+      expect(self.textual_retirement_date).to include(:value => "Never")
     end
 
-    describe "#textual_group_lifecycle" do
-      it "should includes retirement_date" do
-        expect(self.textual_group_lifecycle).to include(:retirement_date)
-      end
-    end
-
-    it "defines textual_retirement_date" do
-      expect(self).to respond_to(:textual_retirement_date)
-    end
-
-    describe "#textual_retirement_date" do
-      it "should returns 'Never' if nil values is passed" do
-        @record.retires_on = nil
-        expect(self.textual_retirement_date).to include(:value => "Never")
-      end
-
-      it "should returns date in %x format" do
-        @record.retires_on = Date.new(2015, 11, 01)
-        expect(self.textual_retirement_date).to include(:value => "11/01/15")
-      end
+    it "should returns date in %x format" do
+      @record.retires_on = Date.new(2015, 11, 01)
+      expect(self.textual_retirement_date).to include(:value => "11/01/15")
     end
   end
 end
--- spec/helpers/vm_helper/textual_summary_spec.rb
+++ spec/helpers/vm_helper/textual_summary_spec.rb
@@ -1,11 +1,9 @@
 require "spec_helper"
 
-describe VmHelper do
-  context "::TextualSummary" do
-    it "#textual_server" do
-      server  = FactoryGirl.create(:miq_server)
-      @record = FactoryGirl.create(:vm_vmware, :miq_server => server)
-      expect(helper.textual_server).to eq("#{server.name} [#{server.id}]")
-    end
+describe VmHelper::TextualSummary do
+  it "#textual_server" do
+    server  = FactoryGirl.create(:miq_server)
+    @record = FactoryGirl.create(:vm_vmware, :miq_server => server)
+    expect(helper.textual_server).to eq("#{server.name} [#{server.id}]")
   end
 end
