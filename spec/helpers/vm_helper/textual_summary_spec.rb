1106fcab80b00db32bf1e8b6ffe2815f86761489
Brandon Dunne <bdunne@redhat.com>
2015-11-14 07:04:22 -0500

    No need for a before block when there is only one spec

--- spec/helpers/vm_helper/textual_summary_spec.rb
+++ spec/helpers/vm_helper/textual_summary_spec.rb
@@ -2,13 +2,10 @@ require "spec_helper"
 
 describe VmHelper do
   context "::TextualSummary" do
-    before(:each) do
-      guid, @server, zone = EvmSpecHelper.create_guid_miq_server_zone
-      @record = FactoryGirl.create(:vm_vmware, :miq_server => @server)
-    end
-
     it "#textual_server" do
-      expect(helper.textual_server).to eq("#{@server.name} [#{@server.id}]")
+      server  = FactoryGirl.create(:miq_server)
+      @record = FactoryGirl.create(:vm_vmware, :miq_server => server)
+      expect(helper.textual_server).to eq("#{server.name} [#{server.id}]")
     end
   end
 end
