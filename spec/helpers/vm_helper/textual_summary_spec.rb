c9398bd37ab5fb2558a7456c982477fb618470f7
Brandon Dunne <bdunne@redhat.com>
2015-11-14 07:10:59 -0500

    We don't actually need to save these, use :build instead
    - Build :miq_server with :id so that it is not nil

--- spec/helpers/vm_helper/textual_summary_spec.rb
+++ spec/helpers/vm_helper/textual_summary_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 describe VmHelper::TextualSummary do
   it "#textual_server" do
-    server  = FactoryGirl.create(:miq_server)
-    @record = FactoryGirl.create(:vm_vmware, :miq_server => server)
+    server  = FactoryGirl.build(:miq_server, :id => 99999)
+    @record = FactoryGirl.build(:vm_vmware, :miq_server => server)
     expect(helper.textual_server).to eq("#{server.name} [#{server.id}]")
   end
 end
