d580e21f9d7c45bca6ea704dd2e21532fec2b0a2
Brandon Dunne <bdunne@redhat.com>
2015-11-14 07:34:29 -0500

    Be more specific about what we're testing

--- spec/helpers/orchestration_stack_helper/textual_summary_spec.rb
+++ spec/helpers/orchestration_stack_helper/textual_summary_spec.rb
@@ -3,18 +3,18 @@ require "spec_helper"
 describe OrchestrationStackHelper::TextualSummary do
   before { @record = FactoryGirl.build(:orchestration_stack) }
 
-  it "#textual_group_lifecycle should includes retirement_date" do
-    expect(textual_group_lifecycle).to include(:retirement_date)
+  it "#textual_group_lifecycle includes retirement_date" do
+    expect(textual_group_lifecycle).to eq([:retirement_date])
   end
 
-  describe "#textual_retirement_date" do
-    it "should returns 'Never' if nil values is passed" do
-      expect(textual_retirement_date).to include(:value => "Never")
+  describe "#textual_retirement_date value" do
+    it "with no :retires_on returns 'Never'" do
+      expect(textual_retirement_date[:value]).to eq("Never")
     end
 
-    it "should returns date in %x format" do
+    it "with :retires_on returns date in %x format" do
       @record.retires_on = Date.new(2015, 11, 01)
-      expect(textual_retirement_date).to include(:value => "11/01/15")
+      expect(textual_retirement_date[:value]).to eq("11/01/15")
     end
   end
 end
