c6c4f0b7439796deb5a83f1a59ea7edf9a855259
Bill Wei <bilwei@redhat.com>
2015-11-16 17:07:45 -0500

    Add method Available_Images to populate image dropdown box

---
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_images.rb
@@ -0,0 +1,35 @@
+#
+# Description: provide the dynamic list content from available images
+#
+image_list = {}
+service_template = $evm.root.attributes["service_template"]
+if service_template.respond_to?(:orchestration_manager) && service_template.orchestration_manager
+  service_template.orchestration_manager.miq_templates.each do |img|
+    os = img.hardware.try(:guest_os) || "unknown"
+    image_list[img.uid_ems] = "#{os} | #{img.name}"
+  end
+end
+if image_list.empty?
+  image_list[nil] = "<None>"
+elsif image_list.size > 1
+  image_list[nil] = "<Choose>"
+else
+  default_value = image_list.first.first
+end
+
+dialog_field = $evm.object
+
+# sort_by: value / description / none
+dialog_field["sort_by"] = "description"
+
+# sort_order: ascending / descending
+dialog_field["sort_order"] = "ascending"
+
+# data_type: string / integer
+dialog_field["data_type"] = "string"
+
+# required: true / false
+dialog_field["required"] = "true"
+
+dialog_field["values"] = image_list
+dialog_field["default_value"] = default_value
