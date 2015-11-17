eb065d64e357b9f7215c292df7af7e05de909bbc
Mooli Tayer <mtayer@redhat.com>
2015-11-17 12:58:02 +0200

    kubernetes: hack 403 error on component statuses

--- app/models/ems_refresh/refreshers/container_refresher_mixin.rb
+++ app/models/ems_refresh/refreshers/container_refresher_mixin.rb
@@ -4,7 +4,17 @@ module EmsRefresh
       def fetch_entities(client, entities)
         h = {}
         entities.each do |entity|
-          h[entity.singularize] = client.send("get_" << entity)
+          begin
+            h[entity.singularize] = client.send("get_" << entity)
+          rescue KubeException => e
+            # Hack to handle
+            if entity == 'component_statuses' && e.error_code == 403
+              $log.error("Ignoring Exception during component_statuses refresh: #{e}")
+              h[entity.singularize] = {}
+            else
+              throw e
+            end
+          end
         end
         h
       end
