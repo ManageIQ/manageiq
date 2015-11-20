30342c3fbe4632bd1b71cc47ab5a929f5b6a662f
Jason Frey <jfrey@redhat.com>
2015-11-20 15:55:55 -0500

    Minor cleanup of string vs symbol in migrations for consistency.

--- db/migrate/20131021190044_remove_miq_license_models_and_table.rb
+++ db/migrate/20131021190044_remove_miq_license_models_and_table.rb
@@ -5,10 +5,10 @@ class RemoveMiqLicenseModelsAndTable < ActiveRecord::Migration
 
   def down
     create_table :miq_license_contents do |t|
-      t.text     "contents"
-      t.boolean  "active"
-      t.datetime "created_on"
-      t.datetime "updated_on"
+      t.text     :contents
+      t.boolean  :active
+      t.datetime :created_on
+      t.datetime :updated_on
     end
   end
 end
