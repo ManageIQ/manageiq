eb3564ae4748284b7a6aef20e008489342ba63fe
Libor Pichler <lpichler@redhat.com>
2015-11-20 16:02:08 +0100

    Allow enter infinity tenant quota
    https://bugzilla.redhat.com/show_bug.cgi?id=1278907
    It is allowed when parent tenant doesn't have defined related tenant quota

--- app/models/tenant_quota.rb
+++ app/models/tenant_quota.rb
@@ -139,8 +139,10 @@ class TenantQuota < ActiveRecord::Base
 
     # Check if the parent has enough quota available to give to the child
     parent_quota = tenant.parent.tenant_quotas.send(name).take
-    if parent_quota.nil? || parent_quota.available < diff
-      errors.add(name, "quota is over allocated, parent tenant does not have enough quota")
+    unless parent_quota.nil?
+      if parent_quota.available < diff
+        errors.add(name, "quota is over allocated, parent tenant does not have enough quota")
+      end
     end
   end
 end
