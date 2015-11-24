3dcd203fce17aae9168f6ebd875555677dc5f138
Ladislav Smola <lsmola@redhat.com>
2015-11-24 17:51:26 +0100

    Adding specs for SSA of network ports
    Testing that the parsed format is correct as well as the playalong
    with refresh of network ports.

--- spec/factories/cloud_subnet.rb
+++ spec/factories/cloud_subnet.rb
@@ -3,4 +3,6 @@ FactoryGirl.define do
     sequence(:name)    { |n| "cloud_subnet_#{seq_padded_for_sorting(n)}" }
     sequence(:ems_ref) { |n| "ems_ref_#{seq_padded_for_sorting(n)}" }
   end
+
+  factory :cloud_subnet_openstack_infra, :class => "ManageIQ::Providers::Openstack::InfraManager::CloudSubnet"
 end
