1920d2727a48ec739b7131a8952707ee4fb7935d
Brandon Dunne <bdunne@redhat.com>
2015-11-26 13:44:06 -0500

    Don't use method_missing :hammer:
    It was hiding that this is testing private methods

--- spec/helpers/application_helper/toolbar_chooser_spec.rb
+++ spec/helpers/application_helper/toolbar_chooser_spec.rb
@@ -2,16 +2,7 @@ require "spec_helper"
 
 describe ApplicationHelper do
   context "ToolbarChooser" do
-    def method_missing(sym, *args)
-      b = _toolbar_chooser
-      if b.respond_to?(sym, true)
-        b.send(sym, *args)
-      else
-        super
-      end
-    end
-
-    context "#center_toolbar_filename_classic" do
+    context "#center_toolbar_filename_classic (private)" do
       before do
         @view   = true
         @layout = "miq_request_vm"
@@ -19,34 +10,34 @@ describe ApplicationHelper do
 
       it "miq_request summary screen" do
         @lastaction = "show"
-        expect(center_toolbar_filename_classic).to eq("miq_request_center_tb")
+        expect(_toolbar_chooser.send(:center_toolbar_filename_classic)).to eq("miq_request_center_tb")
       end
 
       it "miq_request list screen" do
         @lastaction = "show_list"
-        expect(center_toolbar_filename_classic).to eq("miq_requests_center_tb")
+        expect(_toolbar_chooser.send(:center_toolbar_filename_classic)).to eq("miq_requests_center_tb")
       end
     end
 
     describe "generate explorer toolbar file names" do
-      context "#center_toolbar_filename_automate" do
+      context "#center_toolbar_filename_automate (private)" do
         before { @sb = {:active_tree => :ae_tree, :trees => {:ae_tree => {:tree => :ae_tree}}} }
 
         it "should return domains toolbar on root node" do
           x_node_set('root', :ae_tree)
-          expect(center_toolbar_filename_automate).to eq("miq_ae_domains_center_tb")
+          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_domains_center_tb")
         end
 
         it "should return namespaces toolbar on domain node" do
           n1 = FactoryGirl.create(:miq_ae_namespace, :name => 'ns1', :priority => 10)
           x_node_set("aen-#{n1.id}", :ae_tree)
-          expect(center_toolbar_filename_automate).to eq("miq_ae_domain_center_tb")
+          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_domain_center_tb")
         end
 
         it "should return namespace toolbar on namespace node" do
           n1 = FactoryGirl.create(:miq_ae_namespace, :parent => FactoryGirl.create(:miq_ae_domain))
           x_node_set("aen-#{n1.id}", :ae_tree)
-          expect(center_toolbar_filename_automate).to eq("miq_ae_namespace_center_tb")
+          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_namespace_center_tb")
         end
 
         it "should return tab specific toolbar on class node" do
@@ -55,16 +46,16 @@ describe ApplicationHelper do
           x_node_set("aec-#{c1.id}", :ae_tree)
 
           @sb[:active_tab] = "props"
-          expect(center_toolbar_filename_automate).to eq("miq_ae_class_center_tb")
+          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_class_center_tb")
 
           @sb[:active_tab] = "methods"
-          expect(center_toolbar_filename_automate).to eq("miq_ae_methods_center_tb")
+          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_methods_center_tb")
 
           @sb[:active_tab] = "schema"
-          expect(center_toolbar_filename_automate).to eq("miq_ae_fields_center_tb")
+          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_fields_center_tb")
 
           @sb[:active_tab] = ""
-          expect(center_toolbar_filename_automate).to eq("miq_ae_instances_center_tb")
+          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_instances_center_tb")
         end
       end
     end
