d8cf28612868fbef45c64f20d9d87c972328a899
Brandon Dunne <bdunne@redhat.com>
2015-11-27 08:11:19 -0500

    Move describe to the top since all tests appear to be described by it
    Collapse extra context

--- spec/helpers/application_helper/toolbar_chooser_spec.rb
+++ spec/helpers/application_helper/toolbar_chooser_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
-describe ApplicationHelper do
-  context "ToolbarChooser" do
+describe ApplicationHelper, "ToolbarChooser" do
+  describe "generate explorer toolbar file names" do
     context "#center_toolbar_filename_classic (private)" do
       before do
         @view   = true
@@ -19,44 +19,42 @@ describe ApplicationHelper do
       end
     end
 
-    describe "generate explorer toolbar file names" do
-      context "#center_toolbar_filename_automate (private)" do
-        before { @sb = {:active_tree => :ae_tree, :trees => {:ae_tree => {:tree => :ae_tree}}} }
+    context "#center_toolbar_filename_automate (private)" do
+      before { @sb = {:active_tree => :ae_tree, :trees => {:ae_tree => {:tree => :ae_tree}}} }
 
-        it "should return domains toolbar on root node" do
-          x_node_set('root', :ae_tree)
-          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_domains_center_tb")
-        end
+      it "should return domains toolbar on root node" do
+        x_node_set('root', :ae_tree)
+        expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_domains_center_tb")
+      end
 
-        it "should return namespaces toolbar on domain node" do
-          n1 = FactoryGirl.create(:miq_ae_namespace, :name => 'ns1', :priority => 10)
-          x_node_set("aen-#{n1.id}", :ae_tree)
-          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_domain_center_tb")
-        end
+      it "should return namespaces toolbar on domain node" do
+        n1 = FactoryGirl.create(:miq_ae_namespace, :name => 'ns1', :priority => 10)
+        x_node_set("aen-#{n1.id}", :ae_tree)
+        expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_domain_center_tb")
+      end
 
-        it "should return namespace toolbar on namespace node" do
-          n1 = FactoryGirl.create(:miq_ae_namespace, :parent => FactoryGirl.create(:miq_ae_domain))
-          x_node_set("aen-#{n1.id}", :ae_tree)
-          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_namespace_center_tb")
-        end
+      it "should return namespace toolbar on namespace node" do
+        n1 = FactoryGirl.create(:miq_ae_namespace, :parent => FactoryGirl.create(:miq_ae_domain))
+        x_node_set("aen-#{n1.id}", :ae_tree)
+        expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_namespace_center_tb")
+      end
 
-        it "should return tab specific toolbar on class node" do
-          n1 = FactoryGirl.create(:miq_ae_namespace, :parent => FactoryGirl.create(:miq_ae_domain))
-          c1 = FactoryGirl.create(:miq_ae_class, :namespace_id => n1.id, :name => "foo")
-          x_node_set("aec-#{c1.id}", :ae_tree)
+      it "should return tab specific toolbar on class node" do
+        n1 = FactoryGirl.create(:miq_ae_namespace, :parent => FactoryGirl.create(:miq_ae_domain))
+        c1 = FactoryGirl.create(:miq_ae_class, :namespace_id => n1.id, :name => "foo")
+        x_node_set("aec-#{c1.id}", :ae_tree)
 
-          @sb[:active_tab] = "props"
-          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_class_center_tb")
+        @sb[:active_tab] = "props"
+        expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_class_center_tb")
 
-          @sb[:active_tab] = "methods"
-          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_methods_center_tb")
+        @sb[:active_tab] = "methods"
+        expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_methods_center_tb")
 
-          @sb[:active_tab] = "schema"
-          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_fields_center_tb")
+        @sb[:active_tab] = "schema"
+        expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_fields_center_tb")
 
-          @sb[:active_tab] = ""
-          expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_instances_center_tb")
-        end
+        @sb[:active_tab] = ""
+        expect(_toolbar_chooser.send(:center_toolbar_filename_automate)).to eq("miq_ae_instances_center_tb")
       end
     end
   end
