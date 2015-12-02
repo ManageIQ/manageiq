0b910473e36aa6d3a0d47dd7819e1696431f9493
Alissa Bonas <abonas@redhat.com>
2015-12-02 22:52:07 +0200

    Make middleware manager connect more generic

--- app/models/manageiq/providers/hawkular/middleware_manager.rb
+++ app/models/manageiq/providers/hawkular/middleware_manager.rb
@@ -5,15 +5,22 @@ module ManageIQ::Providers
 
     include AuthenticationMixin
 
+    DEFAULT_PORT = 80
+    default_value_for :port, DEFAULT_PORT
     has_many :middleware_servers, :foreign_key => :ems_id
 
-    def self.raw_connect(hostname, username, password)
+    def verify_credentials(auth_type = nil, options = {})
+      auth_type ||= 'default'
+    end
+
+    def self.raw_connect(hostname, port, username, password)
       require 'hawkular_all'
-      ::Hawkular::Inventory::InventoryClient.new(hostname, :username => username, :password => password)
+      url = URI::HTTP.build(:host => hostname, :port => port.to_i, :path => "/hawkular/inventory").to_s
+      ::Hawkular::Inventory::InventoryClient.new(url, :username => username, :password => password)
     end
 
     def connect
-      @connection ||= self.class.raw_connect(hostname, *auth_user_pwd)
+      @connection ||= self.class.raw_connect(hostname, port, authentication_userid("default"), authentication_password("default"))
     end
 
     def connection
@@ -31,5 +38,20 @@ module ManageIQ::Providers
     def deployments(feed)
       connection.list_resources_for_type(feed, 'Deployment')
     end
+
+    # UI methods for determining availability of fields
+    def supports_port?
+      true
+    end
+
+    def self.ems_type
+      @ems_type ||= "hawkular".freeze
+    end
+
+    def self.description
+      @description ||= "Hawkular".freeze
+    end
+
+
   end
 end
