9bb171758e5b7040472d43a3c4d450ededa78a0f
Marek Aufart <maufart@redhat.com>
2015-12-03 16:08:44 +0100

    Add Openstack keypairs create/delete methods
    https://bugzilla.redhat.com/show_bug.cgi?id=1287064

--- app/models/manageiq/providers/openstack/cloud_manager/auth_key_pair.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/auth_key_pair.rb
@@ -1,2 +1,29 @@
 class ManageIQ::Providers::Openstack::CloudManager::AuthKeyPair < ManageIQ::Providers::CloudManager::AuthKeyPair
+  def self.raw_create_key_pair(ext_management_system, create_options)
+    connection_options = {:service => 'Compute'}
+    ext_management_system.with_provider_connection(connection_options) do |service|
+      service.key_pairs.create(create_options)
+    end
+  rescue => err
+    _log.error "keypair=[#{name}], error: #{err}"
+    raise MiqException::MiqOpenstackApiRequestError, err.to_s, err.backtrace
+  end
+
+  def self.validate_create_key_pair(ext_management_system)
+    validate_key_pair(ext_management_system)
+  end
+
+  def raw_delete_key_pair
+    connection_options = {:service => 'Compute'}
+    resource.with_provider_connection(connection_options) do |service|
+      service.delete_key_pair(name)
+    end
+  rescue => err
+    _log.error "keypair=[#{name}], error: #{err}"
+    raise MiqException::MiqOpenstackApiRequestError, err.to_s, err.backtrace
+  end
+
+  def validate_delete_key_pair
+    validate_key_pair
+  end
 end
