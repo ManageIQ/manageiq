931e218d2f9fe0e24ad7f6c053f24056adccf73f
Ladislav Smola <lsmola@redhat.com>
2015-12-04 17:09:30 +0100

    Refactoring of migrations tests for miq_template STI class
    Refactoring of migrations tests for miq_template STI class

--- spec/migrations/20151125081618_set_correct_sti_type_on_openstack_infra_miq_template_spec.rb
+++ spec/migrations/20151125081618_set_correct_sti_type_on_openstack_infra_miq_template_spec.rb
@@ -6,31 +6,31 @@ describe SetCorrectStiTypeOnOpenstackInfraMiqTemplate do
   let(:miq_template_stub) { migration_stub(:Vm) }
 
   EMS_ROW_ENTRIES = [
-    {:id => 1, :type => "ManageIQ::Providers::Openstack::InfraManager"},
-    {:id => 2, :type => "ManageIQ::Providers::Openstack::CloudManager"},
-    {:id => 3, :type => "ManageIQ::Providers::AnotherManager"}
+    {:type => "ManageIQ::Providers::Openstack::InfraManager"},
+    {:type => "ManageIQ::Providers::Openstack::CloudManager"},
+    {:type => "ManageIQ::Providers::AnotherManager"}
   ]
 
   ROW_ENTRIES = [{
-                   :ems_id   => 1,
+                   :ems      => EMS_ROW_ENTRIES[0],
                    :name     => "template_1",
                    :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
                    :type_out => 'ManageIQ::Providers::Openstack::InfraManager::Template'
                  },
                  {
-                   :ems_id   => 1,
+                   :ems      => EMS_ROW_ENTRIES[0],
                    :name     => "template_2",
                    :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
                    :type_out => 'ManageIQ::Providers::Openstack::InfraManager::Template'
                  },
                  {
-                   :ems_id   => 2,
+                   :ems      => EMS_ROW_ENTRIES[1],
                    :name     => "template_3",
                    :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
                    :type_out => 'ManageIQ::Providers::Openstack::CloudManager::Template'
                  },
                  {
-                   :ems_id   => 3,
+                   :ems      => EMS_ROW_ENTRIES[2],
                    :name     => "template_4",
                    :type_in  => 'ManageIQ::Providers::AnyManager::Template',
                    :type_out => 'ManageIQ::Providers::AnyManager::Template'
@@ -40,11 +40,11 @@ describe SetCorrectStiTypeOnOpenstackInfraMiqTemplate do
   migration_context :up do
     it "migrates a series of representative row" do
       EMS_ROW_ENTRIES.each do |x|
-        ext_management_system_stub.create!(:type => x[:type], :id => x[:id])
+        x[:ems] = ext_management_system_stub.create!(:type => x[:type])
       end
 
       ROW_ENTRIES.each do |x|
-        x[:miq_template] = miq_template_stub.create!(:type => x[:type_in], :ems_id => x[:ems_id], :name => x[:name])
+        x[:miq_template] = miq_template_stub.create!(:type => x[:type_in], :ems_id => x[:ems][:ems].id, :name => x[:name])
       end
 
       migrate
@@ -53,7 +53,7 @@ describe SetCorrectStiTypeOnOpenstackInfraMiqTemplate do
         expect(x[:miq_template].reload).to have_attributes(
                                       :type   => x[:type_out],
                                       :name   => x[:name],
-                                      :ems_id => x[:ems_id]
+                                      :ems_id => x[:ems][:ems].id
                                     )
       end
     end
@@ -62,11 +62,11 @@ describe SetCorrectStiTypeOnOpenstackInfraMiqTemplate do
   migration_context :down do
     it "migrates a series of representative row" do
       EMS_ROW_ENTRIES.each do |x|
-        ext_management_system_stub.create!(:type => x[:type], :id => x[:id])
+        x[:ems] = ext_management_system_stub.create!(:type => x[:type])
       end
 
       ROW_ENTRIES.each do |x|
-        x[:miq_template] = miq_template_stub.create!(:type => x[:type_out], :ems_id => x[:ems_id], :name => x[:name])
+        x[:miq_template] = miq_template_stub.create!(:type => x[:type_out], :ems_id => x[:ems][:ems].id, :name => x[:name])
       end
 
       migrate
@@ -75,7 +75,7 @@ describe SetCorrectStiTypeOnOpenstackInfraMiqTemplate do
         expect(x[:miq_template].reload).to have_attributes(
                                              :type   => x[:type_in],
                                              :name   => x[:name],
-                                             :ems_id => x[:ems_id]
+                                             :ems_id => x[:ems][:ems].id
         )
       end
     end
