bd503a1d5a4206053ca990575eeb1b5dc27bc09c
Martin Hradil <mhradil@redhat.com>
2015-12-07 18:22:44 +0000

    miqSpinner - compatibility fix
    the new spinner version would put the spinner at 0, 0 of spinner_div,
    fixed to be at 50%, 50%
    
    this change is not needed for the advanced search spinner, because the enclosing element has zero size

--- app/assets/javascripts/miq_application.js
+++ app/assets/javascripts/miq_application.js
@@ -1072,8 +1072,8 @@ function miqSpinner(status) {
         hwaccel: false, // Whether to use hardware acceleration
         className: 'miq-spinner', // The CSS class to assign to the spinner
         zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: 'auto', // Top position relative to parent in px
-        left: 'auto' // Left position relative to parent in px
+        top: '50%', // Top position relative to parent in px
+        left: '50%' // Left position relative to parent in px
       };
       ManageIQ.spinner.spinner = new Spinner(opts).spin($('#spinner_div')[0]);
     } else {
