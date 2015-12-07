d4678e50a12636cb9ae0fbf103a8bcdec42a9e2b
Libor Pichler <lpichler@redhat.com>
2015-12-07 15:30:38 +0100

    Confirming that passed miq_group_id is used and not user's miq_group_id
    Simulate situation user's miq_group_id can be changed during scheduling and
    generating report

--- spec/models/miq_report_result_spec.rb
+++ spec/models/miq_report_result_spec.rb
@@ -5,12 +5,12 @@ describe MiqReportResult do
     before(:each) do
       EvmSpecHelper.local_miq_server
 
-      user = FactoryGirl.create(:user_with_group)
+      @user = FactoryGirl.create(:user_with_group)
 
       5.times do |i|
         vm = FactoryGirl.build(:vm_vmware)
-        vm.evm_owner_id = user.id               if i > 2
-        vm.miq_group_id = user.current_group.id if vm.evm_owner_id || (i > 1)
+        vm.evm_owner_id = @user.id               if i > 2
+        vm.miq_group_id = @user.current_group.id if vm.evm_owner_id || (i > 1)
         vm.save
       end
 
@@ -35,6 +35,25 @@ describe MiqReportResult do
       report_result.report_results.kind_of?(MiqReport).should be_true
       report_result.report_results.table.should_not be_nil
       report_result.report_results.table.data.should_not be_nil
+    end
+
+    context "for miq_report_result is used different miq_group_id than user's current id" do
+      before(:each) do
+        MiqUserRole.seed
+        role = MiqUserRole.find_by_name("EvmRole-operator")
+        @miq_group = FactoryGirl.create(:miq_group, :miq_user_role => role, :description => "Group1")
+        MiqReport.seed_report(@name_of_report = "Vendor and Guest OS")
+      end
+
+      it "has passed miq_group_id and not user's miq_group_id(can be changed during scheduling and generating)" do
+        rpt = MiqReport.where(:name => @name_of_report).last
+        rpt.generate_table(:userid => "test")
+        report_result = rpt.build_create_results(:userid => "test", :miq_group_id => @miq_group.id) # passed group.id
+        report_result.reload
+
+        expect(@user.current_group_id).not_to eq(@miq_group.id)
+        expect(report_result.miq_group_id).to eq(@miq_group.id)
+      end
     end
   end
 
