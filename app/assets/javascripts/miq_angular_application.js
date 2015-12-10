8c918c641f6fccff157a01e356a6b9f47f1d6498
Martin Hradil <himdel@seznam.cz>
2015-12-10 15:39:55 +0000

    miq_bootstrap - wrapper around angular.bootstrap for ManageIQ
    ```
    perl -i -npe '
      s/angular\.bootstrap\((\$|jQuery)\((.*?)\),\s*\['\''ManageIQ'\''\].*;/miq_bootstrap($2);/
    ' `find app/views -type f`
    ```

--- app/assets/javascripts/miq_angular_application.js
+++ app/assets/javascripts/miq_angular_application.js
@@ -7,3 +7,9 @@ ManageIQ.angular.app = angular.module('ManageIQ', [
 ManageIQ.angular.app.config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.headers.common['X-CSRF-Token'] = jQuery('meta[name=csrf-token]').attr('content');
 }]);
+
+function miq_bootstrap(selector, app) {
+  app = app || 'ManageIQ';
+
+  return angular.bootstrap($(selector), [app], { strictDi: true });
+}
