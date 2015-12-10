11844097b630dce416d77ae9057124aecc458755
Martin Hradil <himdel@seznam.cz>
2015-12-10 15:27:01 +0000

    ManageIQ.angularApplication - now "ManageIQ" for app name and ManageIQ.angular.app for var name
    the whole thing made no sense as an angular module name,
    and we're using more angular-related globals so they should have their own namespace

--- app/assets/javascripts/miq_angular_application.js
+++ app/assets/javascripts/miq_angular_application.js
@@ -1,4 +1,9 @@
-ManageIQ.angularApplication = angular.module('ManageIQ.angularApplication', ['ui.bootstrap', 'patternfly', 'frapontillo.bootstrap-switch']);
-ManageIQ.angularApplication.config([ '$httpProvider', function ($httpProvider) {
+ManageIQ.angular.app = angular.module('ManageIQ', [
+  'ui.bootstrap',
+  'patternfly',
+  'frapontillo.bootstrap-switch',
+]);
+
+ManageIQ.angular.app.config(['$httpProvider', function($httpProvider) {
   $httpProvider.defaults.headers.common['X-CSRF-Token'] = jQuery('meta[name=csrf-token]').attr('content');
-} ]);
+}]);
