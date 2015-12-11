3743a6315c9ae0e8232c407309de78a2a852697b
chrisarcand <chris@chrisarcand.com>
2015-12-11 10:46:24 -0500

    Convert mailer specs to RSpec 2.99.2 syntax
    This conversion is done by Transpec 3.1.1 with the following command:
        transpec spec/mailers
    
    * 52 conversions
        from: obj.should
          to: expect(obj).to
    
    * 42 conversions
        from: == expected
          to: eq(expected)
    
    * 3 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 2 conversions
        from: =~ /pattern/
          to: match(/pattern/)
    
    * 2 conversions
        from: be_true
          to: be_truthy
    
    * 2 conversions
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    * 1 conversion
        from: lambda { }.should_not
          to: expect { }.not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/mailers/generic_mailer_spec.rb
+++ spec/mailers/generic_mailer_spec.rb
@@ -14,15 +14,15 @@ describe GenericMailer do
 
   it "call deliver_queue for generic_notification" do
     @args[:attachment] = [{:content_type => "text/plain", :filename => "generic_mailer_test.txt", :body => "generic_notification with text/plain attachment" * 10}]
-    BinaryBlob.count.should == 0
+    expect(BinaryBlob.count).to eq(0)
     GenericMailer.deliver_queue(:generic_notification, @args)
-    BinaryBlob.count.should == 1
-    MiqQueue.exists?(:method_name => 'deliver', :class_name => described_class, :role => 'notifier').should be_true
+    expect(BinaryBlob.count).to eq(1)
+    expect(MiqQueue.exists?(:method_name => 'deliver', :class_name => described_class, :role => 'notifier')).to be_truthy
   end
 
   it "call deliver_queue for automation_notification" do
     GenericMailer.deliver_queue(:automation_notification, @args)
-    MiqQueue.exists?(:method_name => 'deliver', :class_name => described_class, :role => 'notifier').should be_true
+    expect(MiqQueue.exists?(:method_name => 'deliver', :class_name => described_class, :role => 'notifier')).to be_truthy
   end
 
   context "delivery error" do
@@ -31,16 +31,16 @@ describe GenericMailer do
       # raises error when delivered
       msg = @args.merge(:to => 'me@bedrock.gov, you@bedrock.gov')
       notification = GenericMailer.generic_notification(msg)
-      notification.stub(:deliver_now).and_raise(Net::SMTPFatalError)
+      allow(notification).to receive(:deliver_now).and_raise(Net::SMTPFatalError)
 
       # send error msg first...
-      GenericMailer
-        .should_receive(:generic_notification)
+      expect(GenericMailer)
+        .to receive(:generic_notification)
         .and_return(notification)
 
       # ...after delegate to normal behaviour
-      GenericMailer
-        .should_receive(:generic_notification)
+      expect(GenericMailer)
+        .to receive(:generic_notification)
         .twice
         .and_call_original
 
@@ -48,9 +48,9 @@ describe GenericMailer do
       GenericMailer.deliver(:generic_notification)
 
       # ensure individual messages were sent
-      ActionMailer::Base.deliveries.size.should == 2
-      ActionMailer::Base.deliveries.first.to.should == ['me@bedrock.gov']
-      ActionMailer::Base.deliveries.last.to.should == ['you@bedrock.gov']
+      expect(ActionMailer::Base.deliveries.size).to eq(2)
+      expect(ActionMailer::Base.deliveries.first.to).to eq(['me@bedrock.gov'])
+      expect(ActionMailer::Base.deliveries.last.to).to eq(['you@bedrock.gov'])
     end
   end
 
@@ -58,23 +58,23 @@ describe GenericMailer do
     it "returns gracefully" do
       # generate message that will raise connection error when delivered
       notification = GenericMailer.generic_notification(@args)
-      notification.stub(:deliver_now).and_raise(Errno::ECONNREFUSED)
+      allow(notification).to receive(:deliver_now).and_raise(Errno::ECONNREFUSED)
 
       # generate error message on request
-      GenericMailer
-        .should_receive(:generic_notification)
+      expect(GenericMailer)
+        .to receive(:generic_notification)
         .and_return(notification)
 
       # send message
-      lambda do
+      expect do
         GenericMailer.deliver(:generic_notification)
-      end.should_not raise_error
+      end.not_to raise_error
     end
   end
 
   it "call deliver for generic_notification" do
     msg = GenericMailer.deliver(:generic_notification, @args)
-    ActionMailer::Base.deliveries.should == [msg]
+    expect(ActionMailer::Base.deliveries).to eq([msg])
   end
 
   it "call deliver for generic_notification without a 'from' address" do
@@ -82,96 +82,96 @@ describe GenericMailer do
     new_args = @args.dup
     new_args.delete(:from)
     msg = GenericMailer.deliver(:generic_notification, new_args)
-    msg.from.should == ["test@123.com"]
+    expect(msg.from).to eq(["test@123.com"])
   end
 
   it "call deliver for automation_notification" do
     msg = GenericMailer.deliver(:automation_notification, @args)
-    ActionMailer::Base.deliveries.should == [msg]
+    expect(ActionMailer::Base.deliveries).to eq([msg])
   end
 
   it "call deliver for automation_notification with attachment as BinaryBlob" do
     @args[:attachment] = [{:content_type => "application/txt", :filename => "generic_mailer_test.txt", :body => "automation_notification with attachment" * 2}]
-    BinaryBlob.count.should == 0
+    expect(BinaryBlob.count).to eq(0)
     @args[:attachment] = GenericMailer.attachment_to_blob(@args[:attachment])
-    BinaryBlob.count.should == 1
+    expect(BinaryBlob.count).to eq(1)
     msg = GenericMailer.deliver(:automation_notification, @args)
-    ActionMailer::Base.deliveries.should == [msg]
-    BinaryBlob.count.should == 0
+    expect(ActionMailer::Base.deliveries).to eq([msg])
+    expect(BinaryBlob.count).to eq(0)
   end
 
   it "call deliver for automation_notification, with attachment as BinaryBlob, and generate filenames" do
     @args[:attachment] = [{:content_type => "application/txt", :filename => nil, :body => "automation_notification with attachment" * 2}]
-    BinaryBlob.count.should == 0
+    expect(BinaryBlob.count).to eq(0)
     @args[:attachment] = GenericMailer.attachment_to_blob(@args[:attachment])
-    BinaryBlob.count.should == 1
+    expect(BinaryBlob.count).to eq(1)
     msg = GenericMailer.deliver(:automation_notification, @args)
-    ActionMailer::Base.deliveries.should == [msg]
-    BinaryBlob.count.should == 0
+    expect(ActionMailer::Base.deliveries).to eq([msg])
+    expect(BinaryBlob.count).to eq(0)
   end
 
   it "call blob_to_attachment and attachment_to_blob" do
     @args[:attachment] = [{:content_type => "application/txt", :filename => "generic_mailer_test.txt", :body => "maryhadalittlelamb" * 10}]
-    BinaryBlob.count.should == 0
+    expect(BinaryBlob.count).to eq(0)
     atob_attachment = GenericMailer.attachment_to_blob(@args[:attachment])
-    BinaryBlob.count.should == 1
+    expect(BinaryBlob.count).to eq(1)
     btoa_attachment = GenericMailer.blob_to_attachment(@args[:attachment])
-    btoa_attachment[0][:filename].should == "generic_mailer_test.txt"
-    @args[:attachment][0][:body].length.should == btoa_attachment[0][:body].length
-    BinaryBlob.count.should == 0
+    expect(btoa_attachment[0][:filename]).to eq("generic_mailer_test.txt")
+    expect(@args[:attachment][0][:body].length).to eq(btoa_attachment[0][:body].length)
+    expect(BinaryBlob.count).to eq(0)
   end
 
   it "call blob_to_attachment and attachment_to_blob and generate attachment filenames" do
     @args[:attachment] = [{:content_type => "application/txt", :filename => nil, :body => "maryhadalittlelamb" * 10},
                           {:content_type => "application/txt", :filename => nil, :body => "itsfleecewaswhiteassnow" * 10}]
-    @args[:attachment][0][:filename].should be_nil
-    BinaryBlob.count.should == 0
+    expect(@args[:attachment][0][:filename]).to be_nil
+    expect(BinaryBlob.count).to eq(0)
     atob_attachment = GenericMailer.attachment_to_blob(@args[:attachment])
-    BinaryBlob.count.should == 2
+    expect(BinaryBlob.count).to eq(2)
     btoa_attachment = GenericMailer.blob_to_attachment(@args[:attachment])
-    btoa_attachment[0][:filename].should == "evm_attachment_1"
-    btoa_attachment[1][:filename].should == "evm_attachment_2"
-    @args[:attachment][0][:body].length.should == btoa_attachment[0][:body].length
-    BinaryBlob.count.should == 0
+    expect(btoa_attachment[0][:filename]).to eq("evm_attachment_1")
+    expect(btoa_attachment[1][:filename]).to eq("evm_attachment_2")
+    expect(@args[:attachment][0][:body].length).to eq(btoa_attachment[0][:body].length)
+    expect(BinaryBlob.count).to eq(0)
   end
 
   it "call blob_to_attachment and attachment_to_blob and do not generate attachment filenames" do
     @args[:attachment] = [{:content_type => "application/txt", :filename => "maryhadalittlelamb.txt", :body => "maryhadalittlelamb" * 10},
                           {:content_type => "application/txt", :filename => "itsfleecewaswhiteassnow.txt", :body => "itsfleecewaswhiteassnow" * 10}]
-    BinaryBlob.count.should == 0
+    expect(BinaryBlob.count).to eq(0)
     atob_attachment = GenericMailer.attachment_to_blob(@args[:attachment])
-    BinaryBlob.count.should == 2
+    expect(BinaryBlob.count).to eq(2)
     btoa_attachment = GenericMailer.blob_to_attachment(@args[:attachment])
-    btoa_attachment[0][:filename].should == "maryhadalittlelamb.txt"
-    btoa_attachment[1][:filename].should == "itsfleecewaswhiteassnow.txt"
-    @args[:attachment][0][:body].length.should == btoa_attachment[0][:body].length
-    BinaryBlob.count.should == 0
+    expect(btoa_attachment[0][:filename]).to eq("maryhadalittlelamb.txt")
+    expect(btoa_attachment[1][:filename]).to eq("itsfleecewaswhiteassnow.txt")
+    expect(@args[:attachment][0][:body].length).to eq(btoa_attachment[0][:body].length)
+    expect(BinaryBlob.count).to eq(0)
   end
 
   it "call deliver for generic_notification with text/plain attachment" do
     @args[:attachment] = [{:content_type => "text/plain", :filename => "generic_mailer_test.txt", :body => "generic_notification with text/plain attachment" * 10}]
-    BinaryBlob.count.should == 0
+    expect(BinaryBlob.count).to eq(0)
     msg = GenericMailer.deliver(:generic_notification, @args)
-    ActionMailer::Base.deliveries.should == [msg]
-    BinaryBlob.count.should == 0
+    expect(ActionMailer::Base.deliveries).to eq([msg])
+    expect(BinaryBlob.count).to eq(0)
   end
 
   it "call deliver for generic_notification with text/html attachment" do
     @args[:attachment] = [{:content_type => "text/html", :filename => "generic_mailer_test.txt", :body => "generic_notification" * 10}]
     msg = GenericMailer.deliver(:generic_notification, @args)
-    ActionMailer::Base.deliveries.should == [msg]
+    expect(ActionMailer::Base.deliveries).to eq([msg])
   end
 
   it "call automation_notification directly" do
     @args[:attachment] = [{:content_type => "text/plain", :filename => "automation_filename.txt", :body => "automation_notification" * 10}]
-    GenericMailer.automation_notification(@args).message.should be_kind_of(Mail::Message)
-    ActionMailer::Base.deliveries.should be_empty
+    expect(GenericMailer.automation_notification(@args).message).to be_kind_of(Mail::Message)
+    expect(ActionMailer::Base.deliveries).to be_empty
   end
 
   it "call generic_notification directly" do
     @args[:attachment] = [{:content_type => "text/plain", :filename => "generic_filename.txt", :body => "generic_notification" * 10}]
-    GenericMailer.generic_notification(@args).message.should be_kind_of(Mail::Message)
-    ActionMailer::Base.deliveries.should be_empty
+    expect(GenericMailer.generic_notification(@args).message).to be_kind_of(Mail::Message)
+    expect(ActionMailer::Base.deliveries).to be_empty
   end
 
   it "policy_action_email" do
@@ -183,24 +183,24 @@ describe GenericMailer do
       :entity_name       => "My Vm"
     }
     mail = GenericMailer.policy_action_email(@args)
-    mail.parts.length.should == 2
-    mail.mime_type.should == "multipart/alternative"
-    mail.parts[0].mime_type.should == "text/plain"
-    mail.parts[0].body.should =~ /Do something on vm start policy/
-    mail.parts[1].mime_type.should == "text/html"
-    mail.parts[1].body.should =~ /<h3>[\s]*Alert Triggered[\s]*<\/h3>/
+    expect(mail.parts.length).to eq(2)
+    expect(mail.mime_type).to eq("multipart/alternative")
+    expect(mail.parts[0].mime_type).to eq("text/plain")
+    expect(mail.parts[0].body).to match(/Do something on vm start policy/)
+    expect(mail.parts[1].mime_type).to eq("text/html")
+    expect(mail.parts[1].body).to match(/<h3>[\s]*Alert Triggered[\s]*<\/h3>/)
   end
 
   describe "#test_mail" do
     it "should be called directly" do
       mail = GenericMailer.test_email(@args[:to], settings = {})
-      mail.subject.should start_with I18n.t("product.name")
+      expect(mail.subject).to start_with I18n.t("product.name")
     end
 
     it "should not change the input parameters" do
       settings = {:host => "localhost"}
       GenericMailer.test_email(@args[:to], settings)
-      settings[:host].should == "localhost"
+      expect(settings[:host]).to eq("localhost")
     end
   end
 end
