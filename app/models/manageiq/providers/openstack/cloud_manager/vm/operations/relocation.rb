f16c84f28a51750dc9bc4e354a7d71ca663f0ed3
Ladislav Smola <lsmola@redhat.com>
2015-12-17 13:04:20 +0100

    OpenStack adding evacuate action
    OpenStack adding evacuate action

--- app/models/manageiq/providers/openstack/cloud_manager/vm/operations/relocation.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/vm/operations/relocation.rb
@@ -10,14 +10,37 @@ module ManageIQ::Providers::Openstack::CloudManager::Vm::Operations::Relocation
     self.update_attributes!(:raw_power_state => "MIGRATING")
   end
 
+  def raw_evacuate(options = {})
+    hostname          = options[:hostname]
+    on_shared_storage = options[:on_shared_storage]
+    # on_shared_storage is required, by default we have Ceph shared storage, so set it to true
+    on_shared_storage = true if on_shared_storage.nil?
+    admin_password    = options[:admin_password]
+    with_provider_connection do |connection|
+      connection.evacuate_server(ems_ref, hostname, on_shared_storage, admin_password)
+    end
+    # Temporarily update state for quick UI response until refresh comes along
+    self.update_attributes!(:raw_power_state => "MIGRATING")
+  end
+
   def live_migrate(options = {})
     raw_live_migrate(options)
+  end
+
+  def evacuate(options = nil)
+    raw_evacuate(options)
   end
 
   def validate_live_migrate
     msg = validate_vm_control
     return {:available => msg[0], :message => msg[1]} unless msg.nil?
-    {:available => true,   :message => nil}
+    {:available => true, :message => nil}
+  end
+
+  def validate_evacuate
+    msg = validate_vm_control
+    return {:available => msg[0], :message => msg[1]} unless msg.nil?
+    {:available => true, :message => nil}
   end
 
   def validate_migrate
