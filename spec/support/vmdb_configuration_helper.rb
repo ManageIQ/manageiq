becf230af75b01a730547f7ae8b64a74f1689d57
chrisarcand <chris@chrisarcand.com>
2015-12-17 11:54:08 -0600

    .and_return {} is deprecated

--- spec/support/vmdb_configuration_helper.rb
+++ spec/support/vmdb_configuration_helper.rb
@@ -3,7 +3,7 @@ module VMDBConfigurationHelper
     configuration = double(:config                         => config,
                            :merge_from_template_if_missing => nil,
                            :merge_from_template            => nil)
-    allow(configuration).to receive(:fetch_with_fallback).and_return { |*arg| config.fetch_path(arg) }
-    allow(VMDB::Config).to receive(:new).with(config_name).and_return(configuration)
+    allow(configuration).to receive(:fetch_with_fallback) { |*args| config.fetch_path(args) }
+    allow(VMDB::Config).to receive(:new).with(config_name) { configuration }
   end
 end
