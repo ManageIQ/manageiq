640193ea4f7f7fb9f8f9045d82bea42810958d38
Keenan Brock <kbrock@redhat.com>
2015-12-21 13:30:22 -0500

    remove constant from rspec
    describe blocks are not a class, so these get
    defined in global.
    
    This tends not to be what we want to do

--- spec/migrations/20151125081618_set_correct_sti_type_on_openstack_infra_miq_template_spec.rb
+++ spec/migrations/20151125081618_set_correct_sti_type_on_openstack_infra_miq_template_spec.rb
@@ -5,51 +5,54 @@ describe SetCorrectStiTypeOnOpenstackInfraMiqTemplate do
   let(:ext_management_system_stub) { migration_stub(:ExtManagementSystem) }
   let(:miq_template_stub) { migration_stub(:Vm) }
 
-  EMS_ROW_ENTRIES = [
-    {:type => "ManageIQ::Providers::Openstack::InfraManager"},
-    {:type => "ManageIQ::Providers::Openstack::CloudManager"},
-    {:type => "ManageIQ::Providers::AnotherManager"}
-  ]
+  let(:ems_row_entries) do
+    [
+      {:type => "ManageIQ::Providers::Openstack::InfraManager"},
+      {:type => "ManageIQ::Providers::Openstack::CloudManager"},
+      {:type => "ManageIQ::Providers::AnotherManager"}
+    ]
+  end
 
-  ROW_ENTRIES = [{
-                   :ems      => EMS_ROW_ENTRIES[0],
-                   :name     => "template_1",
-                   :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
-                   :type_out => 'ManageIQ::Providers::Openstack::InfraManager::Template'
-                 },
-                 {
-                   :ems      => EMS_ROW_ENTRIES[0],
-                   :name     => "template_2",
-                   :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
-                   :type_out => 'ManageIQ::Providers::Openstack::InfraManager::Template'
-                 },
-                 {
-                   :ems      => EMS_ROW_ENTRIES[1],
-                   :name     => "template_3",
-                   :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
-                   :type_out => 'ManageIQ::Providers::Openstack::CloudManager::Template'
-                 },
-                 {
-                   :ems      => EMS_ROW_ENTRIES[2],
-                   :name     => "template_4",
-                   :type_in  => 'ManageIQ::Providers::AnyManager::Template',
-                   :type_out => 'ManageIQ::Providers::AnyManager::Template'
-                 },
-  ]
+  let(:row_entries) do
+   [{
+      :ems      => ems_row_entries[0],
+      :name     => "template_1",
+      :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
+      :type_out => 'ManageIQ::Providers::Openstack::InfraManager::Template'
+    },
+    {
+      :ems      => ems_row_entries[0],
+      :name     => "template_2",
+      :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
+      :type_out => 'ManageIQ::Providers::Openstack::InfraManager::Template'
+    },
+    {
+      :ems      => ems_row_entries[1],
+      :name     => "template_3",
+      :type_in  => 'ManageIQ::Providers::Openstack::CloudManager::Template',
+      :type_out => 'ManageIQ::Providers::Openstack::CloudManager::Template'
+    },
+    {
+      :ems      => ems_row_entries[2],
+      :name     => "template_4",
+      :type_in  => 'ManageIQ::Providers::AnyManager::Template',
+      :type_out => 'ManageIQ::Providers::AnyManager::Template'
+    }]
+  end
 
   migration_context :up do
     it "migrates a series of representative row" do
-      EMS_ROW_ENTRIES.each do |x|
+      ems_row_entries.each do |x|
         x[:ems] = ext_management_system_stub.create!(:type => x[:type])
       end
 
-      ROW_ENTRIES.each do |x|
+      row_entries.each do |x|
         x[:miq_template] = miq_template_stub.create!(:type => x[:type_in], :ems_id => x[:ems][:ems].id, :name => x[:name])
       end
 
       migrate
 
-      ROW_ENTRIES.each do |x|
+      row_entries.each do |x|
         expect(x[:miq_template].reload).to have_attributes(
                                       :type   => x[:type_out],
                                       :name   => x[:name],
@@ -61,17 +64,17 @@ describe SetCorrectStiTypeOnOpenstackInfraMiqTemplate do
 
   migration_context :down do
     it "migrates a series of representative row" do
-      EMS_ROW_ENTRIES.each do |x|
+      ems_row_entries.each do |x|
         x[:ems] = ext_management_system_stub.create!(:type => x[:type])
       end
 
-      ROW_ENTRIES.each do |x|
+      row_entries.each do |x|
         x[:miq_template] = miq_template_stub.create!(:type => x[:type_out], :ems_id => x[:ems][:ems].id, :name => x[:name])
       end
 
       migrate
 
-      ROW_ENTRIES.each do |x|
+      row_entries.each do |x|
         expect(x[:miq_template].reload).to have_attributes(
                                              :type   => x[:type_in],
                                              :name   => x[:name],
