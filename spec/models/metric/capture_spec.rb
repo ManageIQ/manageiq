3b990916c092f55b4a9494395a4afceb78ab6d35
Keenan Brock <kbrock@redhat.com>
2015-12-22 21:24:50 -0500

    extract common perf_capture

--- spec/models/metric/capture_spec.rb
+++ spec/models/metric/capture_spec.rb
@@ -65,4 +65,23 @@ describe Metric::Capture do
   context ".capture_threshold handles nil" do
     include_examples "captures a threshold", nil, nil
   end
+
+  context ".perf_capture_health_check" do
+    let(:miq_server) { EvmSpecHelper.local_miq_server }
+    let(:ems) { FactoryGirl.create(:ems_vmware, :zone => miq_server.zone) }
+    let(:vm) { FactoryGirl.create(:vm_perf, :ext_management_system => ems) }
+    let(:vm2) { FactoryGirl.create(:vm_perf, :ext_management_system => ems) }
+
+
+    it "should queue up realtime capture for vm" do
+      vm.perf_capture_realtime_now
+      vm2.perf_capture_realtime_now
+      expect(MiqQueue.count).to eq(2)
+
+      expect(Metric::Capture._log).to receive(:info).with(/2 "realtime" captures on the queue.*oldest:.*recent:/)
+      expect(Metric::Capture._log).to receive(:info).with(/0 "hourly" captures on the queue/)
+      expect(Metric::Capture._log).to receive(:info).with(/0 "historical" captures on the queue/)
+      described_class.perf_capture_health_check
+    end
+  end
 end
