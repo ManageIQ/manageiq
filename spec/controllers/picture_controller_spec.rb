b66479e581846461c682db7faee61f91394d9111
chrisarcand <chris@chrisarcand.com>
2015-12-23 14:47:26 -0600

    Remove `visit` in controller specs
    Using the capybara method `visit` in controller specs is deprecated.

--- spec/controllers/picture_controller_spec.rb
+++ spec/controllers/picture_controller_spec.rb
@@ -14,26 +14,26 @@ describe PictureController do
   end
 
   it 'can serve a picture directly from the database' do
-    visit "/pictures/#{picture.compressed_id}.#{picture.extension}"
-    expect(page.status_code).to eq(200)
+    get :show, :basename => "#{picture.compressed_id}.#{picture.extension}"
+    expect(response.status).to eq(200)
     expect(response.body).to eq(picture_content)
   end
 
   it 'can serve a picture directly from the database using the uncompressed id' do
-    visit "/pictures/#{picture.id}.#{picture.extension}"
-    expect(page.status_code).to eq(200)
+    get :show, :basename => "#{picture.compressed_id}.#{picture.extension}"
+    expect(response.status).to eq(200)
     expect(response.body).to eq(picture_content)
   end
 
   it "responds with a Not Found with pictures of incorrect extension" do
-    visit "/pictures/#{picture.compressed_id}.png"
-    expect(page.status_code).to eq(404)
+    get :show, :basename => "#{picture.compressed_id}.png"
+    expect(response.status).to eq(404)
     expect(response.body).to be_blank
   end
 
   it "responds with a Not Found with unknown pictures" do
-    visit "/pictures/bogusimage.gif"
-    expect(page.status_code).to eq(404)
+    get :show, :basename => "bogusimage.gif"
+    expect(response.status).to eq(404)
     expect(response.body).to be_blank
   end
 end
