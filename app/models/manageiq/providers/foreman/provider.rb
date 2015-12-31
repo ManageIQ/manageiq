4e6be206b9597cdda9bcb27263332e4d83816106
Julian Cheal <jcheal@redhat.com>
2015-12-31 12:47:14 +0000

    Fixed undefined method `resolve_verify_ssl_value` in Forman/provider.rb
    As resolve_verify_ssl_value is now a private method in Endpoint,
    providers need to call `verify_ssl=` instead.

--- app/models/manageiq/providers/foreman/provider.rb
+++ app/models/manageiq/providers/foreman/provider.rb
@@ -32,7 +32,7 @@ class ManageIQ::Providers::Foreman::Provider < ::Provider
     auth_type = options[:auth_type]
     raise "no credentials defined" if self.missing_credentials?(auth_type)
 
-    verify_ssl = resolve_verify_ssl_value(options[:verify_ssl]) || self.verify_ssl
+    verify_ssl = options[:verify_ssl] || self.verify_ssl
     base_url   = options[:url] || url
     username   = options[:username] || authentication_userid(auth_type)
     password   = options[:password] || authentication_password(auth_type)
