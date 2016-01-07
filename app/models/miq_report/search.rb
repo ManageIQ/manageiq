6c6cacedd66cadc9139d74363a3216e103ceea58
Roman Blanco <rblanco@redhat.com>
2016-01-07 16:20:22 +0100

    Optimalization of SQL query for loading target VMs

--- app/models/miq_report/search.rb
+++ app/models/miq_report/search.rb
@@ -101,8 +101,8 @@ module MiqReport::Search
     if options[:parent]
       targets = get_parent_targets(options[:parent], options[:association] || options[:parent_method])
       # filter out archived VMs
-      targets = targets.select { |target| !Vm.find_by(:id => target).archived? unless Vm.find_by(:id => target).nil? }
-      if targets.empty?
+      targets = targets.select { |target| !Vm.find_by(:id => target).try(:archived?) }
+      if targets.try(:empty?)
         search_results, attrs = [targets, {:auth_count => 0, :total_count => 0}]
       else
         search_results, attrs = Rbac.search(search_options.merge(:targets => targets))
