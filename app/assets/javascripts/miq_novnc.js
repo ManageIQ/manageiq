44c7b34a57e9fc49a90728f08432e605c33ef442
Keenan Brock <kbrock@redhat.com>
2016-01-07 08:49:31 -0500

    merge the novnc javascript together

--- app/assets/javascripts/miq_novnc.js
+++ app/assets/javascripts/miq_novnc.js
@@ -1,58 +1,57 @@
-"use strict";
-var rfb;
+//= require novnc-rails
 
-window.WEB_SOCKET_SWF_LOCATION = '/assets/noVNC/web-socket-js/WebSocketMain.swf';
+(function(){
+  "use strict";
+  var rfb;
 
-function sendCtrlAltDel() {
-  rfb.sendCtrlAltDel();
-  return false;
-}
+  window.WEB_SOCKET_SWF_LOCATION = '/assets/noVNC/web-socket-js/WebSocketMain.swf';
 
-function updateState(rfb, state, oldstate, msg) {
-  var level;
-  var s = $D('noVNC_status');
-  var sb = $D('noVNC_status');
-  var cad = $D('sendCtrlAltDelButton');
-  switch (state) {
-    case 'failed': level = "danger"; break;
-    case 'fatal': level = "danger"; break;
-    case 'normal': level = "success"; break;
-    case 'disconnected': level = "default"; break;
-    case 'loaded': level = "success"; break;
-    default: level = "warning"; break;
+  function sendCtrlAltDel() {
+    rfb.sendCtrlAltDel();
+    return false;
   }
 
-  cad.disabled = state !== "normal";
+  function updateState(rfb, state, oldstate, msg) {
+    var level;
+    var s = $D('noVNC_status');
+    var sb = $D('noVNC_status');
+    var cad = $D('sendCtrlAltDelButton');
+    switch (state) {
+      case 'failed': level = "danger"; break;
+      case 'fatal': level = "danger"; break;
+      case 'normal': level = "success"; break;
+      case 'disconnected': level = "default"; break;
+      case 'loaded': level = "success"; break;
+      default: level = "warning"; break;
+    }
 
-  if (typeof (msg) !== 'undefined') {
-    sb.setAttribute("class", "label label-" + level);
-    s.innerHTML = msg;
+    cad.disabled = state !== "normal";
+
+    if (typeof (msg) !== 'undefined') {
+      sb.setAttribute("class", "label label-" + level);
+      s.innerHTML = msg;
+    }
   }
-}
 
-$(function () {
-  $D('sendCtrlAltDelButton').style.display = "inline"; // FIXME
-  $D('sendCtrlAltDelButton').onclick = sendCtrlAltDel;
+  $(function () {
+    $D('sendCtrlAltDelButton').style.display = "inline"; // FIXME
+    $D('sendCtrlAltDelButton').onclick = sendCtrlAltDel;
 
-  var host = window.location.hostname;
-  var vnc_el = $('#vnc');
-  var port = vnc_el.attr('data-port');
-  var password = vnc_el.attr('data-password');
-  var encrypt_attr = vnc_el.attr('data-encrypt');
-  var encrypt = (encrypt_attr !== undefined) && (encrypt_attr !== 'false');
-  var path = "";
-  rfb = new RFB({
-    target: $D('noVNC_canvas'),
-    encrypt: encrypt,
-    true_color: true,
-    local_cursor: true,
-    shared: true,
-    view_only: false,
-    onUpdateState: updateState});
-  rfb.connect(host, port, password, path);
-  console.log("host: " + host);
-  console.log("port: " + port);
-  console.log("password: " + password);
-  console.log("encrypt: " + encrypt);
-  console.log("path: " + path);
-});
+    var host = window.location.hostname;
+    var vnc_el = $('#vnc');
+    var port = vnc_el.attr('data-port');
+    var password = vnc_el.attr('data-password');
+    var encrypt_attr = vnc_el.attr('data-encrypt');
+    var encrypt = (encrypt_attr !== undefined) && (encrypt_attr !== 'false');
+    var path = "";
+    rfb = new RFB({
+      target: $D('noVNC_canvas'),
+      encrypt: encrypt,
+      true_color: true,
+      local_cursor: true,
+      shared: true,
+      view_only: false,
+      onUpdateState: updateState});
+    rfb.connect(host, port, password, path);
+  });
+})();
