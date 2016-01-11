2e993620ba0c1e67e405306f778bed43ccaaf139
Harpreet Kataria <hkataria@redhat.com>
2016-01-11 13:09:33 -0500

    Moved JS code out of the views to a common JS function.

--- app/assets/javascripts/miq_application.js
+++ app/assets/javascripts/miq_application.js
@@ -1372,3 +1372,22 @@ function miqInitMainContent() {
 
   $('#main-content').css('height', 'calc(100% - ' + height + 'px)')
 }
+
+function miqHideSearchClearButton() {
+  // Hide the clear button if the search input is empty
+  $(".search-pf .has-clear .clear").each(function() {
+    if (!$(this).prev('.form-control').val()) {
+      $(this).hide();
+    }
+  });
+  // Show the clear button upon entering text in the search input
+  $(".search-pf .has-clear .form-control").keyup(function () {
+    var t = $(this);
+    t.next('button').toggle(Boolean(t.val()));
+  });
+  // Upon clicking the clear button, empty the entered text and hide the clear button
+  $(".search-pf .has-clear .clear").click(function () {
+    $(this).prev('.form-control').val('').focus();
+    $(this).hide();
+  });
+}
