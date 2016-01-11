db94012da7258b104f70c5b78bef6ea91a19f382
chrisarcand <chris@chrisarcand.com>
2016-01-11 15:52:18 -0500

    HACK: 'stub' dialog validation within factory
    Using rspec-mocks within factories is a bad, bad idea. Instead of
    rewriting a large part of the dialog spec, patch in a replacement.
    
    Removes .stub so we can disabled the old RSpec monkeypatch'd syntax.

--- spec/factories/dialog.rb
+++ spec/factories/dialog.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
   factory :dialog do
+    # HACK: This is required because we were previously depending on rspec-mocks'
+    # .stub monkeypatch here; the monkeypatch has since been removed and rspec-mocks
+    # should NOT be used within factories anyway.
+    # TODO: Rewrite dialog specs to properly mock this within the spec
     # skip validate_children callback for general dialog testing
-    before(:create) { |d| d.stub(:validate_children) }
+    to_create do |instance|
+      class << instance
+        def validate_children; true; end
+      end
+      instance.save!
+    end
   end
 end
