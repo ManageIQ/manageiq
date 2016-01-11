740820fa5ebaf69f983eb0a2c5c2e78baf37349e
chrisarcand <chris@chrisarcand.com>
2016-01-11 16:14:21 -0500

    Remove rspec/autorun require
    Requiring this is deprecated and unnecessary in RSpec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'application_helper'
 
-require 'rspec/autorun'
 require 'rspec/rails'
 require 'vcr'
 
