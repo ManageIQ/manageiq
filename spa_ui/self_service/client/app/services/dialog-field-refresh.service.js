10d0b959cd1f4535164e4c8c5a82cf9697c73001
Erik Clarizio <eclarizi@redhat.com>
2016-01-12 10:18:04 -0800

    Extract dialog field related functions to a DialogFieldRefresh service

---
+++ spa_ui/self_service/client/app/services/dialog-field-refresh.service.js
@@ -0,0 +1,111 @@
+(function() {
+  'use strict';
+
+  angular.module('app.services')
+    .factory('DialogFieldRefresh', DialogFieldRefreshFactory);
+
+  /** @ngInject */
+  function DialogFieldRefreshFactory(CollectionsApi, Notifications) {
+    var service = {
+      listenForAutoRefreshMessages: listenForAutoRefreshMessages,
+      refreshSingleDialogField: refreshSingleDialogField,
+      triggerAutoRefresh: triggerAutoRefresh
+    };
+
+    return service;
+
+    function listenForAutoRefreshMessages(allDialogFields, autoRefreshableDialogFields, url, serviceTemplateId) {
+      window.addEventListener('message', function(event) {
+        var dialogFieldsToRefresh = autoRefreshableDialogFields.filter(function(fieldName) {
+          if (event.data.fieldName !== fieldName) {
+            return fieldName;
+          }
+        });
+
+        refreshMultipleDialogFields(allDialogFields, dialogFieldsToRefresh, url, serviceTemplateId);
+      });
+    }
+
+    function refreshSingleDialogField(allDialogFields, dialogField, url, serviceTemplateId) {
+      function refreshSuccess(result) {
+        var resultObj = result.result[dialogField.name];
+
+        updateAttributesForDialogField(dialogField, resultObj);
+        dialogField.triggerAutoRefresh(dialogField);
+      }
+
+      function refreshFailure(result) {
+        Notifications.error('There was an error refreshing this dialog: ' + result);
+      }
+
+      fetchDialogFieldInfo(allDialogFields, [dialogField.name], url, serviceTemplateId, refreshSuccess, refreshFailure);
+    }
+
+    function triggerAutoRefresh(dialogField) {
+      if (dialogField.trigger_auto_refresh === true) {
+        parent.postMessage({fieldName: dialogField.name}, '*');
+      }
+    }
+
+    // Private
+
+    function refreshMultipleDialogFields(allDialogFields, fieldNamesToRefresh, url, serviceTemplateId) {
+      function refreshSuccess(result) {
+        angular.forEach(allDialogFields, function(dialogField) {
+          if (fieldNamesToRefresh.indexOf(dialogField.name) > -1) {
+            var resultObj = result.result[dialogField.name];
+            updateAttributesForDialogField(dialogField, resultObj);
+          }
+        });
+      }
+
+      function refreshFailure(result) {
+        Notifications.error('There was an error automatically refreshing dialogs' + result);
+      }
+
+      fetchDialogFieldInfo(allDialogFields, fieldNamesToRefresh, url, serviceTemplateId, refreshSuccess, refreshFailure);
+    }
+
+    function updateAttributesForDialogField(dialogField, newDialogField) {
+      copyDynamicAttributes(dialogField, newDialogField);
+
+      if (typeof(newDialogField.values) === 'object') {
+        dialogField.values = newDialogField.values;
+        dialogField.default_value = newDialogField.default_value;
+      } else {
+        dialogField.default_value = newDialogField.values;
+      }
+
+      function copyDynamicAttributes(current_dialog_field, new_dialog_field) {
+        current_dialog_field.data_type = new_dialog_field.data_type;
+        current_dialog_field.options   = new_dialog_field.options;
+        current_dialog_field.read_only = new_dialog_field.read_only;
+        current_dialog_field.required  = new_dialog_field.required;
+      }
+    }
+
+    function fetchDialogFieldInfo(allDialogFields, dialogFieldsToFetch, url, serviceTemplateId, successCallback, failureCallback) {
+      CollectionsApi.post(
+        url,
+        serviceTemplateId,
+        {},
+        JSON.stringify({
+          action: 'refresh_dialog_fields',
+          resource: {
+            dialog_fields: dialogFieldInfoToSend(allDialogFields),
+            fields: dialogFieldsToFetch
+          }
+        })
+      ).then(successCallback, failureCallback);
+    }
+
+    function dialogFieldInfoToSend(allDialogFields) {
+      var fieldValues = {};
+      angular.forEach(allDialogFields, function(dialogField) {
+        fieldValues[dialogField.name] = dialogField.default_value;
+      });
+
+      return fieldValues;
+    }
+  }
+})();
--- spa_ui/self_service/client/app/states/marketplace/details/details.state.js
+++ spa_ui/self_service/client/app/states/marketplace/details/details.state.js
@@ -40,7 +40,7 @@
   }
 
   /** @ngInject */
-  function StateController($state, CollectionsApi, dialogs, serviceTemplate, Notifications) {
+  function StateController($state, CollectionsApi, dialogs, serviceTemplate, Notifications, DialogFieldRefresh) {
     var vm = this;
 
     vm.title = 'Service Template Details';
@@ -53,15 +53,21 @@
     vm.submitDialog = submitDialog;
 
     var autoRefreshableDialogFields = [];
-    vm.dialogFields = [];
+    var allDialogFields = [];
 
     angular.forEach(vm.dialogs, function(dialog) {
       angular.forEach(dialog.dialog_tabs, function(dialogTab) {
         angular.forEach(dialogTab.dialog_groups, function(dialogGroup) {
           angular.forEach(dialogGroup.dialog_fields, function(dialogField) {
-            vm.dialogFields.push(dialogField);
-            dialogField.refreshSingleDialogField = refreshSingleDialogField;
-            dialogField.triggerAutoRefresh = triggerAutoRefresh;
+            allDialogFields.push(dialogField);
+            if (dialogField.default_value === '' && dialogField.values !== '') {
+              dialogField.default_value = dialogField.values;
+            }
+
+            dialogField.triggerAutoRefresh = function() {
+              DialogFieldRefresh.triggerAutoRefresh(dialogField);
+            };
+
             if (dialogField.auto_refresh === true) {
               autoRefreshableDialogFields.push(dialogField.name);
             }
@@ -70,31 +76,32 @@
       });
     });
 
-    listenForAutoRefreshMessages();
+    var dialogUrl = 'service_catalogs/' + serviceTemplate.service_template_catalog_id + '/service_templates';
 
-    function listenForAutoRefreshMessages(fromDialogFieldId, fromDialogFieldName) {
-      window.addEventListener('message', function(event) {
-        var dialogFieldsToRefresh = autoRefreshableDialogFields.filter(function(fieldName) {
-          if (event.data.fieldName !== fieldName) {
-            return fieldName;
-          }
-        });
+    angular.forEach(allDialogFields, function(dialogField) {
+      dialogField.refreshSingleDialogField = function() {
+        DialogFieldRefresh.refreshSingleDialogField(allDialogFields, dialogField, dialogUrl, vm.serviceTemplate.id);
+      };
+    });
 
-        refreshMultipleDialogFields(dialogFieldsToRefresh);
-      });
-    }
+    DialogFieldRefresh.listenForAutoRefreshMessages(
+      allDialogFields,
+      autoRefreshableDialogFields,
+      dialogUrl,
+      vm.serviceTemplate.id
+    );
 
     function submitDialog() {
       var dialogFieldData = {
         href: '/api/service_templates/' + serviceTemplate.id
       };
 
-      angular.forEach(vm.dialogFields, function(dialogField) {
+      angular.forEach(allDialogFields, function(dialogField) {
         dialogFieldData[dialogField.name] = dialogField.default_value;
       });
 
       CollectionsApi.post(
-        'service_catalogs/' + serviceTemplate.service_template_catalog_id + '/service_templates',
+        dialogUrl,
         serviceTemplate.id,
         {},
         JSON.stringify({action: 'order', resource: dialogFieldData})
@@ -108,91 +115,6 @@
       function submitFailure(result) {
         Notifications.error('There was an error submitting this request: ' + result);
       }
-    }
-
-    function refreshSingleDialogField(dialogField) {
-      function refreshSuccess(result) {
-        var resultObj = result.result[dialogField.name];
-
-        updateAttributesForDialogField(dialogField, resultObj);
-        dialogField.triggerAutoRefresh(dialogField);
-      }
-
-      function refreshFailure(result) {
-        Notifications.error('There was an error refreshing this dialog: ' + result);
-      }
-
-      fetchDialogFieldInfo([dialogField.name], refreshSuccess, refreshFailure);
-    }
-
-    function triggerAutoRefresh(dialogField) {
-      if (dialogField.trigger_auto_refresh === true) {
-        parent.postMessage({fieldName: dialogField.name}, '*');
-      }
-    }
-
-    function updateAttributesForDialogField(dialogField, newDialogField) {
-      copyDynamicAttributes(dialogField, newDialogField);
-
-      if (typeof(newDialogField.values) === 'object') {
-        dialogField.values = newDialogField.values;
-        dialogField.default_value = newDialogField.default_value;
-      } else {
-        dialogField.default_value = newDialogField.values;
-      }
-
-      function copyDynamicAttributes(current_dialog_field, new_dialog_field) {
-        current_dialog_field.data_type = new_dialog_field.data_type;
-        current_dialog_field.options   = new_dialog_field.options;
-        current_dialog_field.read_only = new_dialog_field.read_only;
-        current_dialog_field.required  = new_dialog_field.required;
-      }
-    }
-
-    function refreshMultipleDialogFields(fieldNamesToRefresh) {
-      function refreshSuccess(result) {
-        angular.forEach(vm.dialogFields, function(dialogField) {
-          if (fieldNamesToRefresh.indexOf(dialogField.name) > -1) {
-            var resultObj = result.result[dialogField.name];
-            updateAttributesForDialogField(dialogField, resultObj);
-          }
-        });
-      }
-
-      function refreshFailure(result) {
-        Notifications.error('There was an error automatically refreshing dialogs' + result);
-      }
-
-      var fieldValues = {};
-      angular.forEach(vm.dialogFields, function(dialogField) {
-        fieldValues[dialogField.name] = dialogField.value;
-      });
-
-      fetchDialogFieldInfo(fieldNamesToRefresh, refreshSuccess, refreshFailure);
-    }
-
-    function fetchDialogFieldInfo(dialogFieldsToFetch, successCallback, failureCallback) {
-      CollectionsApi.post(
-        'service_catalogs/' + serviceTemplate.service_template_catalog_id + '/service_templates',
-        serviceTemplate.id,
-        {},
-        JSON.stringify({
-          action: 'refresh_dialog_fields',
-          resource: {
-            dialog_fields: dialogFieldInfoToSend(),
-            fields: dialogFieldsToFetch
-          }
-        })
-      ).then(successCallback, failureCallback);
-    }
-
-    function dialogFieldInfoToSend() {
-      var fieldValues = {};
-      angular.forEach(vm.dialogFields, function(dialogField) {
-        fieldValues[dialogField.name] = dialogField.default_value;
-      });
-
-      return fieldValues;
     }
   }
 })();
