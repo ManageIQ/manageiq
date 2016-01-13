32a16ac47debb3d0df241553ba588c8feecfe884
Joshua Cornutt <jcornutt@gmail.com>
2016-01-13 00:10:39 -0500

    Fixed syntax issues

--- db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Provisioning/Naming.class/__methods__/vmname.rb
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Provisioning/Naming.class/__methods__/vmname.rb
@@ -1,3 +1,5 @@
+# / Cloud / VM / Provisioning / Naming / default (vmname)
+
 #
 # Description: This is the default naming scheme
 # 1. If VM Name was not chosen during dialog processing then use vm_prefix
@@ -6,15 +8,9 @@
 # 3. Add 3 digit suffix to vmname if more than one VM is being provisioned
 #
 
-$evm.log("info", "Detected vmdb_object_type:<#{$evm.root['vmdb_object_type']}>")
-prov = $evm.root['miq_provision_request'] || $evm.root['miq_provision'] || $evm.root['miq_provision_request_template']
-vm_name = prov.get_option(:vm_name).to_s.strip
-vm_name = vm_name == 'changeme' ? nil : vm_name
-
 # Returns the name prefix (preferences model over dialog) or nil
 def get_prefix(prov)
-  vm_name_prefix = prov.get_option(:vm_prefix).to_s.strip
-  return ($evm.object['vm_prefix'] || vm_name_prefix)
+  $evm.object['vm_prefix'] || prov.get_option(:vm_prefix).to_s.strip
 end
 
 # Returns the first 3 characters of the "environment" tag (or nil)
@@ -25,9 +21,13 @@ end
 
 # Returns the name suffix (provision number) or nil if provisioning only one
 def get_suffix(prov, condensed)
-  vm_num = prov.get_option(:number_of_vms)
-  return ("$n{3}" if vm_num > 1 || !condensed)
+  "$n{3}" if prov.get_option(:number_of_vms) > 1 || !condensed
 end
+
+$evm.log("info", "Detected vmdb_object_type:<#{$evm.root['vmdb_object_type']}>")
+prov = $evm.root['miq_provision_request'] || $evm.root['miq_provision'] || $evm.root['miq_provision_request_template']
+vm_name = prov.get_option(:vm_name).to_s.strip
+vm_name = vm_name == 'changeme' ? nil : vm_name
 
 # If the name is already specified, use it and exit
 if !vm_name.blank?
