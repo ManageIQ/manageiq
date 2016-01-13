d53a8e7c6630829f26e9ac729c3101d5b7af65f8
Jason Frey <jfrey@redhat.com>
2016-01-12 23:19:09 -0500

    Add decryption of passwords stored in Settings.

--- lib/vmdb/settings.rb
+++ lib/vmdb/settings.rb
@@ -8,7 +8,21 @@ module Vmdb
       ::Settings.add_source!(Rails.root.join("config/vmdb.tmpl.yml").to_s)
       ::Settings.add_source!(DatabaseSource.new)
       ::Settings.add_source!(Rails.root.join("config/settings.local.yml").to_s)
-      ::Settings.load!
+      reload!
+    end
+
+    def self.reload!
+      ::Settings.reload!
+      ::Settings.merge!(decrypted_password_fields)
+    end
+
+    def self.walk(settings = ::Settings, path = [], &block)
+      settings.each do |key, value|
+        new_path = path.dup << key
+        yield key, value, new_path, settings
+        walk(value, new_path, &block) if value.instance_of?(settings.class)
+      end
+      settings
     end
 
     private
@@ -20,6 +34,16 @@ module Vmdb
     def self.reset_settings_constant
       Kernel.send(:remove_const, ::Config.const_name) if Kernel.const_defined?(::Config.const_name)
       Kernel.const_set(::Config.const_name, ::Config::Options.new)
+    end
+
+    PASSWORD_FIELDS = %i(bind_pwd password amazon_secret).to_set.freeze
+
+    def self.decrypted_password_fields
+      hash = {}
+      walk do |key, value, path, _settings|
+        hash.store_path(path, MiqPassword.try_decrypt(value)) if PASSWORD_FIELDS.include?(key) && value.present?
+      end
+      hash
     end
   end
 end
