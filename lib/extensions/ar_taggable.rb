c56369c6c422c362eda4761528f571bf8586982b
Hanna Novikava <arnikasky@gmail.com>
2016-01-14 15:06:43 +0300

    Update ar_taggable.rb

--- lib/extensions/ar_taggable.rb
+++ lib/extensions/ar_taggable.rb
@@ -74,7 +74,7 @@ module ActsAsTaggable
           next
         end
 
-        results.to_a.delete_if { |obj| !ret.include?(obj) }
+        results.reject { |obj| !ret.include?(obj) }
         break if results.empty?
       end
       if limit
