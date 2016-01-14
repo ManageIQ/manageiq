a64cfca3623eb020d5330415f97e29c8e9c5f5cd
Joe Rafaniello <jrafanie@redhat.com>
2016-01-14 13:54:14 -0500

    Consolidate logic for rails server / debugger to one place.

--- lib/vmdb/initializer.rb
+++ lib/vmdb/initializer.rb
@@ -5,23 +5,14 @@ module Vmdb
 
       Vmdb::Loggers.apply_config
 
-      # For `rails server` invocation.  The server calls this before forking UI or Web Service Workers.
-      MiqUiWorker.preload if MiqEnvironment::Process.is_ui_worker_via_command_line?
-
       # When these classes are deserialized in ActiveRecord (e.g. EmsEvent, MiqQueue), they need to be preloaded
       require 'VimTypes'
 
-      ####################################################
-      # If UiWorker called in Development Mode
-      #   invoked via command line -- script/server
-      #   invoked via debugger     -- using rdebug-ide gem
-      #
-      # Then, set up VMDB as if called from MiqServer:
-      #   1. SEED the MUST-HAVE classes
-      #   2. Mark current server as started
-      #
-      ####################################################
+      # UiWorker called in Development Mode
+      #   * command line(rails server)
+      #   * debugger
       if MiqEnvironment::Process.is_ui_worker_via_command_line?
+        MiqUiWorker.preload
         EvmDatabase.seed_primordial
         MiqServer.my_server.starting_server_record
         MiqServer.my_server.update_attributes(:status => "started")
