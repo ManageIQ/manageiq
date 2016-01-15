0ce6330780cb0c3c8c294ff4aad157bc1bb3bfb5
Nick Carboni <ncarboni@redhat.com>
2016-01-15 16:08:45 -0500

    Add migration to add excluded tables and remove regional rows from global db
    Old regional rows currently in the global db for tables that are now
    excluded from replication should be removed.
    The migration also adds newly excluded tables to existing vmdb configurations.

---
+++ db/migrate/20160115142023_remove_replicated_rows_from_newly_excluded_tables.rb
@@ -0,0 +1,29 @@
+class RemoveReplicatedRowsFromNewlyExcludedTables < ActiveRecord::Migration
+  class MiqEventDefinition < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class ScanItem < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class Configuration < ActiveRecord::Base
+    serialize :settings, Vmdb::ConfigurationEncoder
+    self.inheritance_column = :_type_disabled
+  end
+
+  def up
+    say_with_time("Removing rows from newly excluded tables") do
+      region_cond = ActiveRecord::Base.region_to_conditions(Rails.root.join("REGION").read.to_i)
+      MiqEventDefinition.where.not(region_cond).delete_all
+      ScanItem.where.not(region_cond).delete_all
+    end
+
+    say_with_time("Adding tables to replication worker exclude list") do
+      Configuration.where(:typ => "vmdb").each do |c|
+        c.settings[:workers][:worker_base][:replication_worker][:replication][:exclude_tables] << MiqEventDefinition.table_name << ScanItem.table_name
+        c.save!
+      end
+    end
+  end
+end
---
+++ spec/migrations/20160115142023_remove_replicated_rows_from_newly_excluded_tables_spec.rb
@@ -0,0 +1,57 @@
+require "spec_helper"
+require_migration
+
+describe RemoveReplicatedRowsFromNewlyExcludedTables do
+  let(:event_def_stub) { migration_stub(:MiqEventDefinition) }
+  let(:scan_item_stub) { migration_stub(:ScanItem) }
+  let(:conf_stub)      { migration_stub(:Configuration) }
+
+  migration_context :up do
+    before do
+      path_double = double
+      allow(Rails).to receive(:root).and_return(path_double)
+      allow(path_double).to receive(:join).and_return(path_double)
+      allow(path_double).to receive(:read).and_return("99")
+    end
+
+    it "removes the rows from the tables" do
+      event_def_stub.create!(:id => 99_000_000_000_005)
+      event_def_stub.create!(:id => 5)
+      event_def_stub.create!(:id => 1_000_000_000_005)
+
+      scan_item_stub.create!(:id => 99_000_000_000_005)
+      scan_item_stub.create!(:id => 5)
+      scan_item_stub.create!(:id => 1_000_000_000_005)
+
+      migrate
+
+      expect(event_def_stub.count).to eq 1
+      expect(event_def_stub.first.id).to eq 99_000_000_000_005
+
+      expect(scan_item_stub.count).to eq 1
+      expect(scan_item_stub.first.id).to eq 99_000_000_000_005
+    end
+
+    it "adds newly excluded tables to the replication worker configuration" do
+      empty_settings = {
+        :workers => {
+          :worker_base => {
+            :replication_worker => {
+              :replication => {
+                :exclude_tables => []
+              }
+            }
+          }
+        }
+      }
+      config = conf_stub.create!(:typ => "vmdb", :settings => empty_settings)
+
+      migrate
+
+      config.reload
+      excludes = config.settings[:workers][:worker_base][:replication_worker][:replication][:exclude_tables]
+      expect(excludes).to include(event_def_stub.table_name)
+      expect(excludes).to include(scan_item_stub.table_name)
+    end
+  end
+end
