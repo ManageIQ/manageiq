adbbe923872c7723a2710ce0e42d623bc9ab6c48
Brandon Dunne <bdunne@redhat.com>
2016-01-18 12:24:48 -0500

    Consolidate Flavor and Template Factories, use naming from factory
    https://bugzilla.redhat.com/show_bug.cgi?id=1223316

--- spec/factories/security_group.rb
+++ spec/factories/security_group.rb
@@ -1,10 +1,8 @@
 FactoryGirl.define do
   factory :security_group do
+    sequence(:name) { |n| "security_group_#{seq_padded_for_sorting(n)}" }
   end
 
-  factory :security_group_amazon, :parent => :security_group, :class => "ManageIQ::Providers::Amazon::CloudManager::SecurityGroup" do
-  end
-
-  factory :security_group_openstack, :parent => :security_group, :class => "ManageIQ::Providers::Openstack::CloudManager::SecurityGroup" do
-  end
+  factory :security_group_amazon, :parent => :security_group, :class => "ManageIQ::Providers::Amazon::CloudManager::SecurityGroup"
+  factory :security_group_openstack, :parent => :security_group, :class => "ManageIQ::Providers::Openstack::CloudManager::SecurityGroup"
 end
