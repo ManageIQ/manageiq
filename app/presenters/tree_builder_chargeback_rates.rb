6b53dea735a94f8c29fa36f541a83576011e7688
amaurymuro <amaury.muro@gmail.com>
2016-01-18 15:47:17 +0100

    Change the rates tree. Merge Compute and Storage on one Rate

--- app/presenters/tree_builder_chargeback_rates.rb
+++ app/presenters/tree_builder_chargeback_rates.rb
@@ -14,29 +14,46 @@ class TreeBuilderChargebackRates < TreeBuilder
   end
 
   # Get root nodes count/array for explorer tree
-  def x_get_tree_roots(count_only, options)
-    # TODO: Common code in CharbackRate & ChargebackAssignments, need to move into module
-    case options[:type]
-    when :cb_assignments, :cb_rates
-      rate_types = ChargebackRate::VALID_CB_RATE_TYPES
+    def x_get_tree_roots(count_only, options)
+      # TODO: Common code in CharbackRate & ChargebackAssignments, need to move into module
+      case options[:type]
+      when :cb_assignments
+        rate_types = ChargebackRate::VALID_CB_RATE_TYPES
 
-      if count_only
-        rate_types.length
-      else
-        objects = []
-        rate_types.sort.each do |rtype|
-          img = rtype.downcase == "compute" ? "hardware-processor" : "hardware-disk"
-          objects.push(
-            :id    => rtype,
-            :text  => rtype,
-            :image => img,
-            :tip   => rtype
-          )
+        if count_only
+          rate_types.length
+        else
+          objects = []
+          rate_types.sort.each do |rtype|
+            img = rtype.downcase == "compute" ? "hardware-processor" : "hardware-disk"
+            objects.push(
+              :id    => rtype,
+              :text  => rtype,
+              :image => img,
+              :tip   => rtype
+            )
+          end
+          objects
         end
-        objects
+      when :cb_rates
+        # the rate accordion merge the compute rate and the storage rate in one unic rate grouping by description
+        rates = ChargebackRate.all
+        grouped_rates = rates.group_by(&:description)
+        if count_only
+          grouped_rates.length
+        else
+          objects = []
+          grouped_rates.sort.each do |description_group|
+            objects.push(
+              :id    => description_group[0],
+              :text  => description_group[0],
+              :image => 'chargeback_rate',
+            )
+          end
+          objects
+        end
       end
     end
-  end
 
   # Handle custom tree nodes (object is a Hash)
   def x_get_tree_custom_kids(object, count_only, _options)
