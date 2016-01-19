2a2da5d690a87ffb4cf635befbdd58758cf8105f
Ladislav Smola <lsmola@redhat.com>
2016-01-19 18:05:23 +0100

    Changing obfuscated address in tests
    The address 1.2.3.4 was conflicting with address in firewall
    rules.

--- spec/models/manageiq/providers/openstack/cloud_manager/refresher_rhos_juno_spec.rb
+++ spec/models/manageiq/providers/openstack/cloud_manager/refresher_rhos_juno_spec.rb
@@ -4,7 +4,7 @@ describe ManageIQ::Providers::Openstack::CloudManager::Refresher do
   include Openstack::RefreshSpecCommon
 
   before(:each) do
-    setup_ems('1.2.3.4', 'password_2WpEraURh')
+    setup_ems('11.22.33.44', 'password_2WpEraURh')
     @environment = :juno
   end
 
