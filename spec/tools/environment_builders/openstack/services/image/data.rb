a244e57a4f5534738138116b83196b5c10be6230
Ladislav Smola <lsmola@redhat.com>
2016-01-19 18:14:31 +0100

    Use pure cirros image for testing
    The previous image  was resulting into empty root disk, so extracting
    of aki and ari didn't work. We will build VCRs with cirros, so we
    can ssh to the VMs

--- spec/tools/environment_builders/openstack/services/image/data.rb
+++ spec/tools/environment_builders/openstack/services/image/data.rb
@@ -4,8 +4,19 @@ module Openstack
   module Services
     module Image
       class Data < ::Openstack::Services::BaseData
+        IMAGE_NAME = "EmsRefreshSpec-Image"
+
         def images
-          [{:name => "EmsRefreshSpec-Image"}]
+          [{:name =>IMAGE_NAME},
+           {:name      => "EmsRefreshSpec-Image-Private",
+            :is_public => false}
+           ]
+        end
+
+        def images_translate_table
+          {
+            :is_public => :publicly_available
+          }
         end
 
         def servers_snapshots(server_name = nil)
