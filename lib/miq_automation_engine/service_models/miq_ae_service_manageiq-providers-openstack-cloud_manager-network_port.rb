3b0e8ab8eaa7454483aadfd933f81c20941975e7
Ladislav Smola <lsmola@redhat.com>
2016-01-19 17:52:35 +0100

    Expose new methods of STI classes to automate
    Expose new methods of STI classes to automate

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-network_port.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-network_port.rb
@@ -1,4 +1,5 @@
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Openstack_CloudManager_NetworkPort < MiqAeServiceNetworkPort
+    expose :cloud_network,         :association => true
   end
 end
--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-infra_manager-network_port.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-infra_manager-network_port.rb
@@ -1,4 +1,5 @@
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Openstack_InfraManager_NetworkPort < MiqAeServiceNetworkPort
+    expose :cloud_network,         :association => true
   end
 end
