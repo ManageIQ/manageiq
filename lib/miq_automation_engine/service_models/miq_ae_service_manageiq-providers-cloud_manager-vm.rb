31eccb18b4a3ddfa54fd0853e86e08024a5fd868
Ladislav Smola <lsmola@redhat.com>
2016-01-19 17:52:35 +0100

    Expose new methods of STI classes to automate
    Expose new methods of STI classes to automate

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-cloud_manager-vm.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-cloud_manager-vm.rb
@@ -4,6 +4,9 @@ module MiqAeMethodService
     expose :flavor,            :association => true
     expose :cloud_network,     :association => true
     expose :cloud_subnet,      :association => true
+    expose :network_ports,     :association => true
+    expose :network_routers,   :association => true
+    expose :cloud_subnets,     :association => true
     expose :floating_ip,       :association => true
     expose :security_groups,   :association => true
     expose :key_pairs,         :association => true
--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-vm.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-vm.rb
@@ -1,4 +1,5 @@
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Openstack_CloudManager_Vm < MiqAeServiceVmCloud
+    expose :cloud_networks,      :association => true
   end
 end
