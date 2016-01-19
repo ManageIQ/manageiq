abe5ec7f63aa3d6a6508d373526b88dc4c79e3a2
Ladislav Smola <lsmola@redhat.com>
2016-01-19 17:54:25 +0100

    STI specific factories for cloud_network, cloud subnet and network_port
    STI specific factories for cloud_network, cloud subnet and network_port

--- spec/factories/network_port.rb
+++ spec/factories/network_port.rb
@@ -3,4 +3,6 @@ FactoryGirl.define do
     sequence(:name)    { |n| "cloud_network_#{seq_padded_for_sorting(n)}" }
     sequence(:ems_ref) { |n| "ems_ref_#{seq_padded_for_sorting(n)}" }
   end
+
+  factory :network_port_openstack, :class => "ManageIQ::Providers::Openstack::CloudManager::NetworkPort", :parent => :network_port
 end
