7f4152eb7de288edf614fef204d58cf7d37e785b
Aparna Karve <akarve@redhat.com>
2016-01-19 11:45:06 -0800

    Added angular route for /repository/new

--- app/assets/javascripts/miq_angular_configuration.js
+++ app/assets/javascripts/miq_angular_configuration.js
@@ -1,3 +1,20 @@
 ManageIQ.angularApplication.config([ '$httpProvider', '$stateProvider', '$urlRouterProvider', '$locationProvider', function ($httpProvider, $stateProvider, $urlRouterProvider, $locationProvider) {
   $httpProvider.defaults.headers.common['X-CSRF-Token'] = jQuery('meta[name=csrf-token]').attr('content');
+
+  /**
+   * Routes and States for Repository
+   */
+  $stateProvider
+    .state('new', {
+      url: '/repository/new',
+      templateUrl: 'new.html',
+      controller: 'repositoryFormController',
+      repositoryFormId: "new"
+    });
+
+  // default fall back route
+  //$urlRouterProvider.otherwise('/');
+
+  // enable HTML5 Mode for SEO
+  $locationProvider.html5Mode({enabled: true,requireBase: false});
 }]);
