483c2d0e07dd69d0d4a76a84c618b8bebe582eea
Keenan Brock <kbrock@redhat.com>
2016-01-21 16:21:44 -0500

    don't bother with REGION file from web app

--- tools/ci/before_install.sh
+++ tools/ci/before_install.sh
@@ -6,7 +6,6 @@ travis_retry gem install bundler -v ">= 1.11.1"
 if [[ -n "${GEM}" ]] ; then
   cd gems/${GEM}
 else
-  echo "1" > REGION
   cp certs/v2_key.dev certs/v2_key
   cp config/database.pg.yml config/database.yml
   psql -c "CREATE USER root SUPERUSER PASSWORD 'smartvm';" -U postgres
