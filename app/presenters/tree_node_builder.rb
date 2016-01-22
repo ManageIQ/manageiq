43539a6db0d9a8c34d9652fff6b6218ebc608ac0
Laura Galis <lgalis@redhat.com>
2016-01-22 16:50:11 -0500

    Only unescape non-nil strings

--- app/presenters/tree_node_builder.rb
+++ app/presenters/tree_node_builder.rb
@@ -151,7 +151,7 @@ class TreeNodeBuilder
   end
 
   def generic_node(text, image, tip = nil)
-    text = ERB::Util.html_escape(URI.unescape(text)) unless text.html_safe?
+    text = ERB::Util.html_escape(text ? URI.unescape(text) : text) unless text.html_safe?
     @node = {
       :key   => build_object_id,
       :title => text,
