4d39553006d32b73c9792d1372ac09e82960af8d
Martin Povolny <mpovolny@redhat.com>
2016-01-25 11:42:31 +0100

    Explorer presenter: update tests.
    Several methods that are covered by tests are now private.
    Call these using '.send' in spec.

--- spec/presenters/explorer_presenter_spec.rb
+++ spec/presenters/explorer_presenter_spec.rb
@@ -8,35 +8,35 @@ describe ExplorerPresenter do
 
     context "#replace_partial" do
       it 'returns proper JS' do
-        js_str = @presenter.replace_partial(@el, @content)
+        js_str = @presenter.send(:replace_partial, @el, @content)
         expect(js_str).to eq("$('##{@el}').replaceWith('#{escape_javascript(@content)}');")
       end
     end
 
     context "#update_partial" do
       it 'returns proper JS' do
-        js_str = @presenter.update_partial(@el, @content)
+        js_str = @presenter.send(:update_partial, @el, @content)
         expect(js_str).to eq("$('##{@el}').html('#{escape_javascript(@content)}');")
       end
     end
 
     context "#set_or_undef" do
       it 'return proper JS for nil' do
-        js_str = @presenter.set_or_undef("var1")
+        js_str = @presenter.send(:set_or_undef, "var1")
         expect(js_str).to eq('ManageIQ.record.var1 = null;')
       end
 
       it 'return proper JS for a random value' do
         random_value = 'xxx' + rand(10).to_s
         @presenter['var2'] = random_value
-        js_str = @presenter.set_or_undef("var2")
+        js_str = @presenter.send(:set_or_undef, "var2")
         expect(js_str).to eq("ManageIQ.record.var2 = '#{random_value}';")
       end
     end
 
     context "#ajax_action" do
       it 'returns JS to call miqAsyncAjax with proper url' do
-        js_str = @presenter.ajax_action(
+        js_str = @presenter.send(:ajax_action,
           :controller => 'foo',
           :action     => 'bar',
           :record_id  => 42,
@@ -49,7 +49,7 @@ describe ExplorerPresenter do
       it 'calls js_build_calendar' do
         @presenter[:build_calendar] = true
         expect(@presenter).to receive(:js_build_calendar)
-        @presenter.build_calendar
+        @presenter.send(:build_calendar)
       end
 
       it 'calls js_build_calendar with params' do
@@ -61,7 +61,7 @@ describe ExplorerPresenter do
         }.with_indifferent_access
         @presenter[:build_calendar] = obj
         expect(@presenter).to receive(:js_build_calendar).with(obj)
-        @presenter.build_calendar
+        @presenter.send(:build_calendar)
       end
     end
   end
