b1549d0353c32b7b8b331472fdecc7c58734c36e
Martin Hradil <mhradil@redhat.com>
2016-01-25 16:26:28 +0000

    miq_api - support login, logout, autorenew, token persistence
    should be complete now, just not used

--- app/assets/javascripts/miq_api.js
+++ app/assets/javascripts/miq_api.js
@@ -1,20 +1,91 @@
-function API() {
-  this.get = API.get;
-  this.post = API.post;
-}
+/* functions to use the API from our JS/Angular:
+ *
+ * API.get(url, options) - use API.get('/api'), returns promise
+ * API.post(url, data, options) - returns promise
+ * API.login(login, password) - performs initial authentication, saves token on success, returns promise
+ * API.logout() - clears login info, no return
+ * API.autorenew() - registers a 60second interval to query /api, returns a function to clear the interval
+ *
+ * can also be used from angular - depend on miq.api module and use the API service
+ * when used as an angular service, all the promises are $q, so work within the angular digest cycle
+ *
+ * the API token is persisted into sessionStorage
+ */
 
-API.get = function(url, options) {
-  return $.ajax(url, _.extend({
-    method: 'GET',
-  }, options || {}));
-}
+(function() {
+  function API() {
+  }
 
-API.post = function(url, data, options) {
-  return $.ajax(url, _.extend({
-    method: 'POST',
-    data: data,
-  }, options || {}));
-}
+  API.get = function(url, options) {
+    return $.ajax(url, _.extend({
+      method: 'GET',
+    }, process_options(options)));
+  };
+
+  API.post = function(url, data, options) {
+    return $.ajax(url, _.extend({
+      method: 'POST',
+      data: data,
+    }, process_options(options)));
+  };
+
+  var base64encode = window.btoa; // browser api
+
+  API.login = function(login, password) {
+    API.logout();
+
+    return API.get('/api/auth?requester_type=ui', {
+      headers: {
+        'Authorization': 'Basic ' + base64encode([login, password].join(':')),
+      },
+    })
+    .then(function(response) {
+      sessionStorage.miq_token = response.auth_token;
+    });
+  };
+
+  API.logout = function() {
+    delete sessionStorage.miq_token;
+  };
+
+  API.autorenew = function() {
+    var id = setInterval(function() {
+      API.get('/api')
+      .then(null, function() {
+        console.warn('API autorenew fail', arguments);
+        clearInterval(id);
+      });
+    }, 60 * 1000);
+
+    return function() {
+      clearInterval(id);
+    };
+  };
+
+  window.API = API;
+
+
+  function process_options(o) {
+    o = o || {};
+    delete o.type;
+    delete o.method;
+    delete o.url;
+    delete o.data;
+
+    if (o.skipTokenRenewal) {
+      o.headers = o.headers || {};
+      o.headers['X-Auth-Skip-Token-Renewal'] = 'true';
+    }
+
+    if (sessionStorage.miq_token) {
+      o.headers = o.headers || {};
+      o.headers['X-Auth-Token'] = sessionStorage.miq_token;
+    }
+
+    return o;
+  }
+})(window);
+
 
 angular.module('miq.api', [])
 .factory('API', ['$q', function($q) {
@@ -27,5 +98,8 @@ angular.module('miq.api', [])
   return {
     get: angularify(API.get),
     post: angularify(API.post),
+    login: angularify(API.login),
+    logout: API.logout,
+    autorenew: API.autorenew,
   };
 }]);
