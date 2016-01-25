5a97ddf1f1c3396fc4c322d9fc9a60396694759a
Tim Wade <hello@timjwade.com>
2016-01-25 15:02:44 -0800

    [api] Add Instances stop action
    Resolves https://trello.com/c/q7P31gGp (first item)

--- app/controllers/api_controller/instances.rb
+++ app/controllers/api_controller/instances.rb
@@ -10,6 +10,19 @@ class ApiController
       end
     end
 
+    def stop_resource_instances(type, id = nil, _data = nil)
+      raise BadRequestError, "Must specify an id for stopping a #{type} resource" unless id
+
+      api_action(type, id) do |klass|
+        instance = resource_search(id, type, klass)
+        api_log_info("Stopping #{instance_ident(instance)}")
+
+        result = validate_instance_for_action(instance, "stop")
+        result = stop_instance(instance) if result[:success]
+        result
+      end
+    end
+
     private
 
     def instance_ident(instance)
@@ -22,6 +35,19 @@ class ApiController
       action_result(true, desc, :task_id => task_id)
     rescue => err
       action_result(false, err.to_s)
+    end
+
+    def stop_instance(instance)
+      desc = "#{instance_ident(instance)} stopping"
+      task_id = queue_object_action(instance, desc, :method_name => "stop", :role => "ems_operations")
+      action_result(true, desc, :task_id => task_id)
+    rescue => err
+      action_result(false, err.to_s)
+    end
+
+    def validate_instance_for_action(instance, action)
+      validation = instance.send("validate_#{action}")
+      action_result(validation[:available], validation[:message].to_s)
     end
   end
 end
