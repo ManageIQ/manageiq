0b3e615fc5046d652b6598aa04b3ce5154c83465
Keenan Brock <kbrock@redhat.com>
2016-01-27 11:53:21 -0500

    rework hardware_spec

--- spec/models/hardware_spec.rb
+++ spec/models/hardware_spec.rb
@@ -1,36 +1,29 @@
 describe Hardware do
-  before(:each) do
-    @vm_hw = FactoryGirl.create(:hardware)
-    @vm    = FactoryGirl.create(:vm_vmware, :hardware => @vm_hw)
-
-    @template_hw = FactoryGirl.create(:hardware)
-    @template    = FactoryGirl.create(:template_vmware, :hardware => @template_hw)
-
-    @host_hw = FactoryGirl.create(:hardware)
-    @host    = FactoryGirl.create(:host, :hardware => @host_hw)
-  end
+  let(:vm) { FactoryGirl.create(:vm_vmware, :hardware => FactoryGirl.create(:hardware)) }
+  let(:template) { FactoryGirl.create(:template_vmware, :hardware => FactoryGirl.create(:hardware)) }
+  let(:host) { FactoryGirl.create(:host, :hardware => FactoryGirl.create(:hardware)) }
 
   it "#vm_or_template" do
-    expect(@vm_hw.vm_or_template).to eq(@vm)
-    expect(@template_hw.vm_or_template).to eq(@template)
-    expect(@host_hw.vm_or_template).to     be_nil
+    expect(vm.hardware.vm_or_template).to eq(vm)
+    expect(template.hardware.vm_or_template).to eq(template)
+    expect(host.hardware.vm_or_template).to     be_nil
   end
 
   it "#vm" do
-    expect(@vm_hw.vm).to eq(@vm)
-    expect(@template_hw.vm).to be_nil
-    expect(@host_hw.vm).to     be_nil
+    expect(vm.hardware.vm).to eq(vm)
+    expect(template.hardware.vm).to be_nil
+    expect(host.hardware.vm).to     be_nil
   end
 
   it "#miq_template" do
-    expect(@vm_hw.miq_template).to       be_nil
-    expect(@template_hw.miq_template).to eq(@template)
-    expect(@host_hw.miq_template).to     be_nil
+    expect(vm.hardware.miq_template).to       be_nil
+    expect(template.hardware.miq_template).to eq(template)
+    expect(host.hardware.miq_template).to     be_nil
   end
 
   it "#host" do
-    expect(@vm_hw.host).to       be_nil
-    expect(@template_hw.host).to be_nil
-    expect(@host_hw.host).to eq(@host)
+    expect(vm.hardware.host).to       be_nil
+    expect(template.hardware.host).to be_nil
+    expect(host.hardware.host).to eq(host)
   end
 end
