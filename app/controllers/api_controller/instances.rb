7794d4d3f77fb26b722d0eba47dd03aabca3b34e
Tim Wade <hello@timjwade.com>
2016-02-01 13:02:49 -0800

    [api] Add Instance pause action
    Resolves https://trello.com/c/q7P31gGp (third item)

--- app/controllers/api_controller/instances.rb
+++ app/controllers/api_controller/instances.rb
@@ -36,6 +36,19 @@ class ApiController
       end
     end
 
+    def pause_resource_instances(type, id = nil, _data = nil)
+      raise BadRequestError, "Must specify an id for pausing a #{type} resource" unless id
+
+      api_action(type, id) do |klass|
+        instance = resource_search(id, type, klass)
+        api_log_info("Pausing #{instance_ident(instance)}")
+
+        result = validate_instance_for_action(instance, "pause")
+        result = pause_instance(instance) if result[:success]
+        result
+      end
+    end
+
     private
 
     def instance_ident(instance)
@@ -61,6 +74,14 @@ class ApiController
     def start_instance(instance)
       desc = "#{instance_ident(instance)} starting"
       task_id = queue_object_action(instance, desc, :method_name => "start", :role => "ems_operations")
+      action_result(true, desc, :task_id => task_id)
+    rescue => err
+      action_result(false, err.to_s)
+    end
+
+    def pause_instance(instance)
+      desc = "#{instance_ident(instance)} pausing"
+      task_id = queue_object_action(instance, desc, :method_name => "pause", :role => "ems_operations")
       action_result(true, desc, :task_id => task_id)
     rescue => err
       action_result(false, err.to_s)
