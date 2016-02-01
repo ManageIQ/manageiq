158c74b4ae883d2e4bab646b7d6e4873fa24f75e
Brandon Dunne <bdunne@redhat.com>
2016-02-01 17:33:01 -0500

    Retry with sudo if needed for mount and unmount commands

--- gems/pending/spec/util/mount/miq_generic_mount_session_spec.rb
+++ gems/pending/spec/util/mount/miq_generic_mount_session_spec.rb
@@ -16,4 +16,12 @@ describe MiqGenericMountSession do
   it "#mount_share is unique" do
     expect(described_class.new(:uri => '/tmp/abc').mount_share).to_not eq(described_class.new(:uri => '/tmp/abc').mount_share)
   end
+
+  it ".runcmd will retry with sudo if needed" do
+    cmd = "abc"
+    expect(described_class).to receive(:`).once.with("#{cmd} 2>&1").and_return("mount: only root can do that\n")
+    expect(described_class).to receive(:`).with("sudo #{cmd} 2>&1").and_return("works with sudo")
+
+    described_class.runcmd(cmd)
+  end
 end
