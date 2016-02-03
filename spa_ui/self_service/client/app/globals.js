2a8d629b055ef6efb2a2e2b8192a7587ec15451b
Martin Hradil <mhradil@redhat.com>
2016-02-03 17:33:51 +0000

    SSUI - fix failing tests
    modify globals.js to only override the functions if they don't exist
    
    that way, the tests keep using the implementations from test-helpers/fake-i18n.js, which doesn't throw

--- spa_ui/self_service/client/app/globals.js
+++ spa_ui/self_service/client/app/globals.js
@@ -1,12 +1,16 @@
 // overriden from gettext.config once the initialization is done
-window.__ = function(str) {
-  throw new Error([
-    'Attempting to call gettext before the service was initialized.',
-    'Maybe you\'re calling it in the .config phase? ("' + str + '")',
-  ].join(' '));
-};
+if (!window.__) {
+  window.__ = function(str) {
+    throw new Error([
+      'Attempting to call gettext before the service was initialized.',
+      'Maybe you\'re calling it in the .config phase? ("' + str + '")',
+    ].join(' '));
+  };
+}
 
 // N_ is OK anywhere
-window.N_ = function(str) {
-  return str;
-};
+if (!window.N_) {
+  window.N_ = function(str) {
+    return str;
+  };
+}
