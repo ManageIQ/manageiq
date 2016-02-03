660b76ccd8cb33e2714385dbbb91242c704e087b
Jozef Zigmund <jzigmund@redhat.com>
2016-02-03 19:28:12 +0100

    Basic rspec test for CloudTenant#show

--- spec/controllers/cloud_tenant_controller_spec.rb
+++ spec/controllers/cloud_tenant_controller_spec.rb
@@ -70,4 +70,29 @@ describe CloudTenantController do
       expect(assigns(:edit)).to be_nil
     end
   end
+
+  describe "#show" do
+    before do
+      EvmSpecHelper.create_guid_miq_server_zone
+      @tenant = FactoryGirl.create(:cloud_tenant)
+      @user = FactoryGirl.create(:user)
+      login_as @user
+    end
+
+    subject do
+      get :show, :id => @tenant.id
+    end
+
+    context "respond_with" do
+      it { is_expected.to have_http_status 200 }
+
+      it { is_expected.not_to have_http_status 500 }
+    end
+
+    context "render listnav partial" do
+      render_views
+
+      it { is_expected.to render_template(:partial => "layouts/listnav/_cloud_tenant") }
+    end
+  end
 end
