0a4d20619dd2e36ea30d3f9996c07778f029b2ef
Marcel Hild <hild@b4mad.net>
2016-02-03 17:08:06 +0100

    reworked aws orchestration stack to work with sdk v2
    also rewrote the specs to use client_stubs

--- spec/models/manageiq/providers/amazon/cloud_manager/orchestration_stack_spec.rb
+++ spec/models/manageiq/providers/amazon/cloud_manager/orchestration_stack_spec.rb
@@ -1,68 +1,100 @@
+require_relative "../aws_helper"
+
 describe ManageIQ::Providers::Amazon::CloudManager::OrchestrationStack do
-  let(:ems) { FactoryGirl.create(:ems_amazon) }
+  let(:ems) { FactoryGirl.create(:ems_amazon_with_authentication) }
   let(:template) { FactoryGirl.create(:orchestration_template) }
   let(:orchestration_stack) do
     FactoryGirl.create(:orchestration_stack_amazon, :ext_management_system => ems, :name => 'test')
   end
 
-  let(:the_raw_stack) do
-    double.tap do |stack|
-      allow(stack).to receive(:stack_id).and_return('one_id')
-    end
-  end
-
-  let(:raw_stacks) do
-    double.tap do |stacks|
-      handle = double
-      allow(handle).to receive(:stacks).and_return(stacks)
-      allow(ems).to receive(:connect).and_return(handle)
-      allow(stacks).to receive(:[]).with(orchestration_stack.name).and_return(the_raw_stack)
-    end
-  end
-
-  before do
-    raw_stacks
-  end
-
   describe 'stack operations' do
-    context ".create_stack" do
-      it 'creates a stack' do
-        expect(raw_stacks).to receive(:create).and_return(the_raw_stack)
-
-        stack = OrchestrationStack.create_stack(ems, 'mystack', template, {})
-        expect(stack.class).to eq(described_class)
-        expect(stack.name).to eq('mystack')
-        expect(stack.ems_ref).to eq(the_raw_stack.stack_id)
+    context ".raw_create_stack" do
+      it "creates a stack" do
+        stubbed_responses = {
+            :cloudformation => {
+                :create_stack => [:stack_id => "stack_id"],
+                :describe_stacks => {
+                    :stacks =>
+                        [
+                            :stack_name => "mystack",
+                            :creation_time => 10.minutes.ago,
+                            :stack_status => 'CREATE_COMPLETE',
+                            :stack_id => "stack_id"
+                        ]
+                }
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          stack = OrchestrationStack.create_stack(ems, "mystack", template)
+          expect(stack.class).to eq(described_class)
+          expect(stack.name).to eq("mystack")
+          expect(stack.ems_ref).to eq("stack_id")
+        end
       end
 
       it 'catches errors from provider' do
-        expect(raw_stacks).to receive(:create).and_throw('bad request')
-
-        expect { OrchestrationStack.create_stack(ems, 'mystack', template, {}) }.to raise_error(MiqException::MiqOrchestrationProvisionError)
+        stubbed_responses = {
+            :cloudformation => {
+                :create_stack => "AlreadyExistsException"
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          expect do
+            OrchestrationStack.create_stack(ems, "mystack", template)
+          end.to raise_error(MiqException::MiqOrchestrationProvisionError)
+        end
       end
     end
 
     context "#update_stack" do
       it 'updates the stack' do
-        expect(the_raw_stack).to receive(:update)
-        orchestration_stack.update_stack(template, {})
+        stubbed_responses = {
+            :cloudformation => {
+                :update_stack => { :stack_id => "stack_id"}
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          expect(orchestration_stack.update_stack(template, {}).stack_id).to eq("stack_id")
+        end
       end
 
-      it 'catches errors from provider' do
-        expect(the_raw_stack).to receive(:update).and_throw('bad request')
-        expect { orchestration_stack.update_stack(template, {}) }.to raise_error(MiqException::MiqOrchestrationUpdateError)
+      it "catches errors from provider" do
+        stubbed_responses = {
+            :cloudformation => {
+                :update_stack => "ServiceError"
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          expect do
+            orchestration_stack.update_stack(template, {})
+          end.to raise_error(MiqException::MiqOrchestrationUpdateError)
+        end
       end
     end
 
     context "#delete_stack" do
-      it 'updates the stack' do
-        expect(the_raw_stack).to receive(:delete)
-        orchestration_stack.delete_stack
+      it "deletes the stack" do
+        stubbed_responses = {
+            :cloudformation => {
+                :delete_stack => {}
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          expect(orchestration_stack.delete_stack).to be_truthy
+        end
       end
 
       it 'catches errors from provider' do
-        expect(the_raw_stack).to receive(:delete).and_throw('bad request')
-        expect { orchestration_stack.delete_stack }.to raise_error(MiqException::MiqOrchestrationDeleteError)
+        stubbed_responses = {
+            :cloudformation => {
+                :delete_stack => "InsufficientCapabilitiesException"
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          expect do
+            orchestration_stack.delete_stack
+          end.to raise_error(MiqException::MiqOrchestrationDeleteError)
+        end
       end
     end
   end
@@ -70,27 +102,48 @@ describe ManageIQ::Providers::Amazon::CloudManager::OrchestrationStack do
   describe 'stack status' do
     context '#raw_status and #raw_exists' do
       it 'gets the stack status and reason' do
-        allow(the_raw_stack).to receive(:status).and_return('CREATE_COMPLETE')
-        allow(the_raw_stack).to receive(:status_reason).and_return('complete')
+        stubbed_responses = {
+            :cloudformation => {
+                :describe_stacks => {
+                    :stacks => [
+                        :stack_name => "StackName",
+                        :creation_time => 10.minutes.ago,
+                        :stack_status => 'CREATE_COMPLETE',
+                        :stack_status_reason => 'complete'
+                    ]
+                }
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          raw_status = orchestration_stack.raw_status
 
-        rstatus = orchestration_stack.raw_status
-        expect(rstatus).to have_attributes(:status => 'CREATE_COMPLETE', :reason => 'complete')
-
-        expect(orchestration_stack.raw_exists?).to be_truthy
+          expect(raw_status).to have_attributes(:status => 'CREATE_COMPLETE', :reason => 'complete')
+          expect(orchestration_stack.raw_exists?).to be_truthy
+        end
       end
 
       it 'parses error message to determine stack not exist' do
-        allow(raw_stacks).to receive(:[]).with(orchestration_stack.name).and_throw("Stack xxx does not exist")
-        expect { orchestration_stack.raw_status }.to raise_error(MiqException::MiqOrchestrationStackNotExistError)
-
-        expect(orchestration_stack.raw_exists?).to be_falsey
+        stubbed_responses = {
+            :cloudformation => {
+                :describe_stacks => Aws::CloudFormation::Errors::ValidationError.new(:no_context, "Stack with id stack_id does not exist")
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          expect { orchestration_stack.raw_status }.to raise_error(MiqException::MiqOrchestrationStackNotExistError)
+          expect(orchestration_stack.raw_exists?).to be_falsey
+        end
       end
 
       it 'catches errors from provider' do
-        allow(raw_stacks).to receive(:[]).with(orchestration_stack.name).and_throw("bad request")
-        expect { orchestration_stack.raw_status }.to raise_error(MiqException::MiqOrchestrationStatusError)
-
-        expect { orchestration_stack.raw_exists? }.to raise_error(MiqException::MiqOrchestrationStatusError)
+        stubbed_responses = {
+            :cloudformation => {
+                :describe_stacks => "ServiceError"
+            }
+        }
+        with_aws_stubbed(stubbed_responses) do
+          expect { orchestration_stack.raw_status }.to raise_error(MiqException::MiqOrchestrationStatusError)
+          expect { orchestration_stack.raw_exists? }.to raise_error(MiqException::MiqOrchestrationStatusError)
+        end
       end
     end
   end
