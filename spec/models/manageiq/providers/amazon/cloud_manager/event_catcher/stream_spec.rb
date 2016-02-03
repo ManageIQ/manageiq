386aa81b0cf6799bab79e0610df9aa79b770d5b2
Marcel Hild <hild@b4mad.net>
2016-02-03 16:24:34 +0100

    simplified aws event_catcher & specs

--- spec/models/manageiq/providers/amazon/cloud_manager/event_catcher/stream_spec.rb
+++ spec/models/manageiq/providers/amazon/cloud_manager/event_catcher/stream_spec.rb
@@ -2,7 +2,7 @@ require_relative "../../aws_helper"
 
 describe ManageIQ::Providers::Amazon::CloudManager::EventCatcher::Stream do
   subject do
-    ems = FactoryGirl.create(:ems_amazon_with_authentication, :name => "us-west-1", :provider_region => "us-west-1")
+    ems = FactoryGirl.create(:ems_amazon_with_authentication)
     described_class.new(ems)
   end
   let(:queue_url) { "https://sqs.eu-central-1.amazonaws.com/995412904407/the_queue_name" }
@@ -13,7 +13,7 @@ describe ManageIQ::Providers::Amazon::CloudManager::EventCatcher::Stream do
         with_aws_stubbed(:sqs => {:get_queue_url => {:queue_url => queue_url}}) do
           expect(subject).to receive(:sqs_get_queue_url).and_call_original
           expect(subject).not_to receive(:sqs_create_queue).and_call_original
-          expect(subject.find_or_create_queue).to eq(queue_url)
+          expect(subject.send(:find_or_create_queue)).to eq(queue_url)
         end
       end
     end
@@ -36,10 +36,11 @@ describe ManageIQ::Providers::Amazon::CloudManager::EventCatcher::Stream do
             expect(subject).to receive(:sqs_get_queue_url).and_call_original
             expect(subject).to receive(:sqs_create_queue).and_call_original
             expect(subject).to receive(:subscribe_topic_to_queue).and_call_original
-            expect(subject.find_or_create_queue).to eq(queue_url)
+            expect(subject.send(:find_or_create_queue)).to eq(queue_url)
           end
         end
       end
+
       context "and no topic present on aws" do
         it "raises ProviderUnreachable" do
           stubbed_responses = {
@@ -56,16 +57,29 @@ describe ManageIQ::Providers::Amazon::CloudManager::EventCatcher::Stream do
             expect(subject).to receive(:sqs_get_queue_url).and_call_original
             expect(subject).not_to receive(:sqs_create_queue).and_call_original
             expect(subject).not_to receive(:subscribe_topic_to_queue).and_call_original
-            expect { subject.find_or_create_queue }.to raise_exception(described_class::ProviderUnreachable)
+            expect { subject.send(:find_or_create_queue) }.to raise_exception(described_class::ProviderUnreachable)
           end
         end
       end
     end
   end
 
-  context "#collect_events" do
-    it "returns an event" do
-      message_body = File.read(File.join(File.dirname(__FILE__), "sns_message.json"))
+  context "#parse_event" do
+    let(:message) do
+      body = File.read(File.join(File.dirname(__FILE__), "sqs_message.json"))
+      Aws::SQS::Types::Message.new(:body => body, :message_id => 1)
+    end
+
+    it "parses a SNS Message" do
+      expect(subject.send(:parse_event, message)).to include('messageId'   => 1,
+                                                             "messageType" => "ConfigurationItemChangeNotification",
+                                                             "eventType"   => "AWS_EC2_Instance_UPDATE")
+    end
+  end
+
+  context "#poll" do
+    it "yields an event" do
+      message_body = File.read(File.join(File.dirname(__FILE__), "sqs_message.json"))
       stubbed_responses = {
         :sqs => {
           :get_queue_url   => {:queue_url => queue_url},
@@ -79,17 +93,21 @@ describe ManageIQ::Providers::Amazon::CloudManager::EventCatcher::Stream do
                 }
               ]
             },
-            {:messages => []} # second call returns no messages, this aborts the poller
+            # second message raises an exception
+            "Aws::SQS::Errors::ServiceError"
           ]
         }
       }
       with_aws_stubbed(stubbed_responses) do
-        subject.start
-        subject.each do |event|
-          parsed_message = JSON.parse(JSON.parse(message_body)["Message"])
-          expect(event['configurationItemDiff']).to eq(parsed_message['configurationItemDiff'])
-          subject.stop
-        end
+        allow(subject).to receive(:parse_event).and_return(message_body)
+        expect(subject)
+        polled_event = nil
+        expect do
+          subject.poll do |event|
+            polled_event = event
+          end
+        end.to raise_exception(described_class::ProviderUnreachable)
+        expect(polled_event).to eq(message_body)
       end
     end
   end
