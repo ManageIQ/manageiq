b50929e24fc3aa88d50e98c1d5f396d92ca5c002
Martin Hradil <mhradil@redhat.com>
2016-02-03 16:59:34 +0000

    SSUI - fix too long line

--- spa_ui/self_service/client/app/globals.js
+++ spa_ui/self_service/client/app/globals.js
@@ -1,6 +1,9 @@
 // overriden from gettext.config once the initialization is done
 window.__ = function(str) {
-  throw new Error('Attempting to call gettext before the service was initialized. Maybe you\'re doing it in the .config phase? ("' + str + '")');
+  throw new Error([
+    'Attempting to call gettext before the service was initialized.',
+    'Maybe you\'re calling it in the .config phase? ("' + str + '")',
+  ].join(' '));
 };
 
 // N_ is OK anywhere
