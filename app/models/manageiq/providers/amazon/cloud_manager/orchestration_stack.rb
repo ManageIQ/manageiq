b7005f9c5dae76c78eee5168cdb3f090fc0e2320
Marcel Hild <hild@b4mad.net>
2016-02-03 17:08:06 +0100

    reworked aws orchestration stack to work with sdk v2
    also rewrote the specs to use client_stubs

--- app/models/manageiq/providers/amazon/cloud_manager/orchestration_stack.rb
+++ app/models/manageiq/providers/amazon/cloud_manager/orchestration_stack.rb
@@ -3,7 +3,8 @@ class ManageIQ::Providers::Amazon::CloudManager::OrchestrationStack < ::Orchestr
 
   def self.raw_create_stack(orchestration_manager, stack_name, template, options = {})
     orchestration_manager.with_provider_connection(:service => "CloudFormation") do |service|
-      service.stacks.create(stack_name, template.content, options).stack_id
+      stack_options = options.merge(:stack_name => stack_name, :template_body => template.content)
+      service.create_stack(stack_options).stack_id
     end
   rescue => err
     _log.error "stack=[#{stack_name}], error: #{err}"
@@ -11,9 +12,9 @@ class ManageIQ::Providers::Amazon::CloudManager::OrchestrationStack < ::Orchestr
   end
 
   def raw_update_stack(template, options)
-    update_options = {:template => template.content}.merge(options.except(:disable_rollback, :timeout))
+    update_options = {:template_body => template.content}.merge(options.except(:disable_rollback, :timeout))
     ext_management_system.with_provider_connection(:service => "CloudFormation") do |service|
-      service.stacks[name].update(update_options)
+      service.stack(name).update(update_options)
     end
   rescue => err
     _log.error "stack=[#{name}], error: #{err}"
@@ -22,7 +23,7 @@ class ManageIQ::Providers::Amazon::CloudManager::OrchestrationStack < ::Orchestr
 
   def raw_delete_stack
     ext_management_system.with_provider_connection(:service => "CloudFormation") do |service|
-      service.stacks[name].try(:delete)
+      service.stack(name).try!(:delete)
     end
   rescue => err
     _log.error "stack=[#{name}], error: #{err}"
@@ -31,8 +32,8 @@ class ManageIQ::Providers::Amazon::CloudManager::OrchestrationStack < ::Orchestr
 
   def raw_status
     ext_management_system.with_provider_connection(:service => "CloudFormation") do |service|
-      raw_stack = service.stacks[name]
-      Status.new(raw_stack.status, raw_stack.status_reason)
+      raw_stack = service.stack(name)
+      Status.new(raw_stack.stack_status, raw_stack.stack_status_reason)
     end
   rescue => err
     if err.to_s =~ /[S|s]tack.+does not exist/
