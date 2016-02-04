1216507aa6ed0ece789b8c9839e233a91230d8b9
Tim Wade <hello@timjwade.com>
2016-02-04 15:22:59 -0800

    [api] Rename instances soft reboot -> reboot guest

--- spec/requests/api/instances_spec.rb
+++ spec/requests/api/instances_spec.rb
@@ -353,11 +353,11 @@ RSpec.describe "Instances API" do
     end
   end
 
-  describe "instance soft reboot action" do
+  describe "instance reboot guest action" do
     it "responds not found for an invalid instance" do
-      api_basic_authorize action_identifier(:instances, :soft_reboot)
+      api_basic_authorize action_identifier(:instances, :reboot_guest)
 
-      run_post(invalid_instance_url, gen_request(:soft_reboot))
+      run_post(invalid_instance_url, gen_request(:reboot_guest))
 
       expect_resource_not_found
     end
@@ -365,32 +365,32 @@ RSpec.describe "Instances API" do
     it "responds forbidden for an invalid instance without appropriate role" do
       api_basic_authorize
 
-      run_post(invalid_instance_url, gen_request(:soft_reboot))
+      run_post(invalid_instance_url, gen_request(:reboot_guest))
 
       expect_request_forbidden
     end
 
-    it "fails to soft reboot a powered off instance" do
-      api_basic_authorize action_identifier(:instances, :soft_reboot)
+    it "fails to reboot a powered off instance" do
+      api_basic_authorize action_identifier(:instances, :reboot_guest)
       update_raw_power_state("poweredOff", instance)
 
-      run_post(instance_url, gen_request(:soft_reboot))
+      run_post(instance_url, gen_request(:reboot_guest))
 
       expect_single_action_result(:success => false, :message => "is not powered on", :href => :instance_url)
     end
 
-    it "soft reboots a valid instance" do
-      api_basic_authorize action_identifier(:instances, :soft_reboot)
+    it "reboots a valid instance" do
+      api_basic_authorize action_identifier(:instances, :reboot_guest)
 
-      run_post(instance_url, gen_request(:soft_reboot))
+      run_post(instance_url, gen_request(:reboot_guest))
 
-      expect_single_action_result(:success => true, :message => "soft rebooting", :href => :instance_url, :task => true)
+      expect_single_action_result(:success => true, :message => "rebooting", :href => :instance_url, :task => true)
     end
 
-    it "soft reboots multiple valid instances" do
-      api_basic_authorize action_identifier(:instances, :soft_reboot)
+    it "reboots multiple valid instances" do
+      api_basic_authorize action_identifier(:instances, :reboot_guest)
 
-      run_post(instances_url, gen_request(:soft_reboot, nil, instance1_url, instance2_url))
+      run_post(instances_url, gen_request(:reboot_guest, nil, instance1_url, instance2_url))
 
       expect_multiple_action_result(2, :task => true)
       expect_result_resources_to_include_hrefs("results", :instances_list)
