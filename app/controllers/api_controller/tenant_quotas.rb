965ce8c931b430a380c6523cc7dfff0e88598a0e
alex-lairan <a.lairan@linkbynet.com>
2016-02-04 14:31:55 +0100

    Create API CRUD for quotas

---
+++ app/controllers/api_controller/tenant_quotas.rb
@@ -0,0 +1,47 @@
+class ApiController
+  INVALID_QUOTA_ATTRS = %w(id href tenant_id unit)
+
+  module TenantQuotas
+    #
+    # Tenant Quotas Subcollection Supporting Methods
+    #
+
+    def quotas_create_resource(object, type, id, data)
+      bad_attrs = data_includes_invalid_attrs(data)
+      if bad_attrs.present?
+        raise BadRequestError,
+              "Attribute(s) #{bad_attrs} should not be specified for creating a new tenant resource"
+      end
+
+      data['tenant_id'] = object.id
+      quota = collection_class(type).create(data)
+      if quota.invalid?
+        raise BadRequestError, "Failed to add a new tenant quota resource - #{quota.errors.full_messages.join(', ')}"
+      end
+      quota
+    end
+
+    def quotas_query_resource(object)
+      klass = collection_class(:quotas)
+      object ? klass.where(:tenant_id => object.id) : {}
+    end
+
+    def quotas_edit_resource(object, type, id, data)
+      bad_attrs = data_includes_invalid_attrs(data)
+      if bad_attrs.present?
+        raise BadRequestError, "Attributes #{bad_attrs} should not be specified for updating a quota resource"
+      end
+      edit_resource(type, id, data)
+    end
+
+    def delete_resource_quotas(object, type, id, data)
+      delete_resource(type, id, data)
+    end
+
+    private
+
+    def data_includes_invalid_attrs(data)
+      data.keys.select { |k| INVALID_QUOTA_ATTRS.include?(k) }.compact.join(", ") if data
+    end
+  end
+end
--- app/models/tenant_quota.rb
+++ app/models/tenant_quota.rb
@@ -36,7 +36,8 @@ class TenantQuota < ApplicationRecord
 
   NAMES = QUOTA_BASE.keys.map(&:to_s)
 
-  validates :name, :inclusion => {:in => NAMES}
+  validates :name, :inclusion => {:in => NAMES},
+                   :uniqueness => { scope: [:name, :tenant_id] , message: "should be unique per tenant" }
   validates :unit, :value, :presence => true
   validates :value, :numericality => {:greater_than => 0}
   validates :warn_value, :numericality => {:greater_than => 0}, :if => "warn_value.present?"
