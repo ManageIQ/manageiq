ed5a343252cbc01879ece0b2484af7a9b71cc8de
Harpreet Kataria <hkataria@redhat.com>
2016-02-08 17:20:10 -0500

    reset session[:changed] first time on Control/Simulation screen
    when coming from another screen sometimes session[:changed] was still set to true causing "Submit" button to be enabled when going to Control/Simulation screen first time causing it to blow up if user pressed submit button without making selections in the form.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1304288

---
+++ spec/controllers/miq_policy_controller/rsop_spec.rb
@@ -0,0 +1,28 @@
+include UiConstants
+
+describe MiqPolicyController do
+  render_views
+  before :each do
+    EvmSpecHelper.local_miq_server
+    set_user_privileges
+  end
+
+  context "#rsop" do
+    it "first time on RSOP screen, session[:changed] should be false" do
+      session[:changed] = true
+      controller.instance_variable_set(:@current_user,
+                                       FactoryGirl.create(:user,
+                                                          :name       => "foo",
+                                                          :miq_groups => [],
+                                                          :userid     => "foo"))
+      controller.instance_variable_set(:@sb, {})
+      allow(controller).to receive(:rsop_put_objects_in_sb)
+      allow(controller).to receive(:find_filtered)
+      allow(controller).to receive(:appliance_name)
+      get :rsop
+      expect(response.status).to eq(200)
+      expect(session[:changed]).to be_falsey
+      expect(response).to render_template('miq_policy/rsop')
+    end
+  end
+end
