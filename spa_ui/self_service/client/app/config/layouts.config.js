6e4d325a2ab1dde297419010ebace1747d3136e4
Aparna Karve <akarve@redhat.com>
2016-02-10 14:36:35 -0800

    Retrieve available product features from REST API

--- spa_ui/self_service/client/app/config/layouts.config.js
+++ spa_ui/self_service/client/app/config/layouts.config.js
@@ -25,9 +25,15 @@
   }
 
   /** @ngInject */
-  function enterApplication(Polling, NavCounts) {
+  function enterApplication(Polling, lodash, Session, NavCounts, Navigation) {
     // Application layout displays the navigation which might have items that require polling to update the counts
     angular.forEach(NavCounts.counts, updateCount);
+    angular.forEach(Navigation.items.primary, function(value, key) {
+      lodash.merge(value, Session.current.navFeatures[key]);
+    });
+    angular.forEach(Navigation.items.secondary, function(value, key) {
+      lodash.merge(value, Session.current.actionFeatures[key]);
+    });
 
     function updateCount(count, key) {
       count.func();
--- spa_ui/self_service/client/app/services/session.service.js
+++ spa_ui/self_service/client/app/services/session.service.js
@@ -5,7 +5,7 @@
     .factory('Session', SessionFactory);
 
   /** @ngInject */
-  function SessionFactory($http, moment, $sessionStorage, gettextCatalog) {
+  function SessionFactory($http, moment, $sessionStorage, gettextCatalog, lodash) {
     var model = {
       token: null,
       user: {}
@@ -38,9 +38,10 @@
     }
 
     function loadUser() {
-      return $http.get('/api')
+      return $http.get('/api?attributes=authorization')
         .then(function(response) {
           currentUser(response.data.identity);
+          setRBAC(response.data.authorization.product_features);
 
           var locale = response.data.settings && response.data.settings.locale;
           gettextCatalog.loadAndSet(locale);
@@ -55,11 +56,75 @@
       return model.user;
     }
 
+    function setRBAC(productFeatures) {
+      setRBACForActions(productFeatures);
+      setRBACForNavigation(productFeatures);
+    }
+
     // Helpers
 
     function active() {
       // may not be current, but if we have one, we'll rely on API 401ing if it's not
       return model.token;
+    }
+
+    function setRBACForNavigation(productFeatures) {
+      var features = {
+        dashboard: {show: entitledForDashboard(productFeatures)},
+        services: {show: entitledForServices(productFeatures)},
+        requests: {show: entitledForRequests(productFeatures)},
+        marketplace: {show: entitledForServiceCatalogs(productFeatures)}
+      };
+      model.navFeatures = features;
+
+      return model.navFeatures;
+    }
+
+    function setRBACForActions(productFeatures) {
+      var features = {
+        service_view: {show: angular.isDefined(productFeatures.service_view)},
+        service_edit: {show: angular.isDefined(productFeatures.service_edit)},
+        service_delete: {show: angular.isDefined(productFeatures.service_delete)},
+        service_reconfigure: {show: angular.isDefined(productFeatures.service_reconfigure)},
+        service_retire_now: {show: angular.isDefined(productFeatures.service_retire_now)}
+      };
+      model.actionFeatures = features;
+
+      return model.actionFeatures;
+    }
+
+    function entitledForServices(productFeatures) {
+      var serviceFeature = lodash.find(model.actionFeatures, function(o) {
+        return o.show === true;
+      });
+      
+      return angular.isDefined(serviceFeature);
+    }
+
+    function entitledForServiceCatalogs(productFeatures) {
+      if (angular.isDefined(productFeatures.svc_catalog_provision)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function entitledForRequests(productFeatures) {
+      if (angular.isDefined(productFeatures.miq_request_view)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function entitledForDashboard(productFeatures) {
+      if (entitledForServices(productFeatures) ||
+          entitledForRequests(productFeatures) ||
+          entitledForServiceCatalogs(productFeatures)) {
+        return true;
+      } else {
+        return false;
+      }
     }
   }
 })();
