0ea40ef5ae5222fd92212652ba38eb279813fa3c
dkorn <dkorn@redhat.com>
2016-02-10 12:03:42 +0200

    Fix Image Registries display_name string
    To maintain consistency with other container entities,
    this change pluralizes Image Registry display_name.
    
    This closes #6587

--- spec/controllers/container_image_registry_controller_spec.rb
+++ spec/controllers/container_image_registry_controller_spec.rb
@@ -18,7 +18,7 @@ describe ContainerImageRegistryController do
     get :show, :id => container_image_registry.id
     expect(response.status).to eq(200)
     expect(response.body).to_not be_empty
-    expect(assigns(:breadcrumbs)).to eq([{:name => "Container Image Registry",
+    expect(assigns(:breadcrumbs)).to eq([{:name => "Container Image Registries",
                                           :url  => "/container_image_registry/show_list?page=&refresh=y"},
                                          {:name => "Test Image Registry (Summary)",
                                           :url  => "/container_image_registry/show/#{container_image_registry.id}"}])
