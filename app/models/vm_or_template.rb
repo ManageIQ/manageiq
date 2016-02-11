d9ca96399eb48c1916afa0799a37c19b28fd96b4
Lucy Fu <lufu@redhat.com>
2016-02-11 09:40:18 -0500

    Remove the call to User#groups_include? which has been removed.
    Call User#miq_group_ids.include? directly which was called by User#groups_include?.

--- app/models/vm_or_template.rb
+++ app/models/vm_or_template.rb
@@ -1920,7 +1920,7 @@ class VmOrTemplate < ApplicationRecord
 
   def tenant_identity
     user = evm_owner
-    user = User.super_admin.tap { |u| u.current_group = miq_group } if user.nil? || !user.groups_include?(miq_group)
+    user = User.super_admin.tap { |u| u.current_group = miq_group } if user.nil? || !user.miq_group_ids.include?(miq_group_id)
     user
   end
 
