2ae420411502a80d4f770b18d270d61ded40fe2f
Jason Frey <jfrey@redhat.com>
2016-02-11 14:28:56 -0500

    Extract spa_ui/self_service into its own repo.
    https://github.com/ManageIQ/manageiq-ui-self_service

--- spa_ui/self_service/client/app/config/api.config.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('app.config')
-    .constant('API_BASE', location.protocol + '//' + location.host)
-    .constant('API_LOGIN', '')
-    .constant('API_PASSWORD', '');
-})();
--- spa_ui/self_service/client/test-helpers/fake-i18n.js
+++
@@ -1,2 +0,0 @@
-window.__ = function(x) { return x; };
-window.N_ = function(x) { return x; };
