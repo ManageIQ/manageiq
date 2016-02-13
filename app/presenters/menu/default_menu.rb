09e651f8436b35ab1de21d23116f6c58e98ea308
Sam Lucidi <slucidi@redhat.com>
2016-02-13 16:21:12 -0500

    Remove snapshots from main menu

--- app/presenters/menu/default_menu.rb
+++ app/presenters/menu/default_menu.rb
@@ -23,16 +23,15 @@ module Menu
 
       def clouds_menu_section
         Menu::Section.new(:clo, N_("Clouds"), [
-          Menu::Item.new('ems_cloud',             N_('Providers'),           'ems_cloud',                 {:feature => 'ems_cloud_show_list'},                     '/ems_cloud'),
-          Menu::Item.new('availability_zone',     N_('Availability Zones'),  'availability_zone',         {:feature => 'availability_zone_show_list'},             '/availability_zone'),
-          Menu::Item.new('cloud_tenant',          N_('Tenants'),             'cloud_tenant',              {:feature => 'cloud_tenant_show_list'},                  '/cloud_tenant'),
-          Menu::Item.new('cloud_volume',          N_('Volumes'),             'cloud_volume',              {:feature => 'cloud_volume_show_list'},                  '/cloud_volume'),
-          Menu::Item.new('flavor',                N_('Flavors'),             'flavor',                    {:feature => 'flavor_show_list'},                        '/flavor'),
-          Menu::Item.new('security_group',        N_('Security Groups'),     'security_group',            {:feature => 'security_group_show_list'},                '/security_group'),
-          Menu::Item.new('vm_cloud',              N_('Instances'),           'vm_cloud_explorer',         {:feature => 'vm_cloud_explorer_accords', :any => true}, '/vm_cloud/explorer'),
-          Menu::Item.new('orchestration_stack',   N_('Stacks'),              'orchestration_stack',       {:feature => 'orchestration_stack_show_list'},           '/orchestration_stack'),
-          Menu::Item.new('auth_key_pair_cloud',   N_('Key Pairs'),           'auth_key_pair_cloud',       {:feature => 'auth_key_pair_cloud_show_list'},           '/auth_key_pair_cloud'),
-          Menu::Item.new('cloud_volume_snapshot', N_('Snapshots'),           'cloud_volume_snapshot',     {:feature => 'cloud_volume_snapshot_show_list'},         '/cloud_volume_snapshot')
+          Menu::Item.new('ems_cloud',           N_('Providers'),           'ems_cloud',                 {:feature => 'ems_cloud_show_list'},                     '/ems_cloud'),
+          Menu::Item.new('availability_zone',   N_('Availability Zones'),  'availability_zone',         {:feature => 'availability_zone_show_list'},             '/availability_zone'),
+          Menu::Item.new('cloud_tenant',        N_('Tenants'),             'cloud_tenant',              {:feature => 'cloud_tenant_show_list'},                  '/cloud_tenant'),
+          Menu::Item.new('cloud_volume',        N_('Volumes'),             'cloud_volume',              {:feature => 'cloud_volume_show_list'},                  '/cloud_volume'),
+          Menu::Item.new('flavor',              N_('Flavors'),             'flavor',                    {:feature => 'flavor_show_list'},                        '/flavor'),
+          Menu::Item.new('security_group',      N_('Security Groups'),     'security_group',            {:feature => 'security_group_show_list'},                '/security_group'),
+          Menu::Item.new('vm_cloud',            N_('Instances'),           'vm_cloud_explorer',         {:feature => 'vm_cloud_explorer_accords', :any => true}, '/vm_cloud/explorer'),
+          Menu::Item.new('orchestration_stack', N_('Stacks'),              'orchestration_stack',       {:feature => 'orchestration_stack_show_list'},           '/orchestration_stack'),
+          Menu::Item.new('auth_key_pair_cloud', N_('Key Pairs'),           'auth_key_pair_cloud',       {:feature => 'auth_key_pair_cloud_show_list'},           '/auth_key_pair_cloud')
         ])
       end
 
