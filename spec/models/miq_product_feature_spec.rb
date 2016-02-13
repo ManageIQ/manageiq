892943bbd5937498333bb4f15784f891c906517f
Sam Lucidi <slucidi@redhat.com>
2016-02-13 16:21:12 -0500

    Remove snapshots from main menu

--- spec/models/miq_product_feature_spec.rb
+++ spec/models/miq_product_feature_spec.rb
@@ -2,7 +2,7 @@ require 'tmpdir'
 require 'pathname'
 
 describe MiqProductFeature do
-  let(:expected_feature_count) { 934 }
+  let(:expected_feature_count) { 938 }
 
   # - container_dashboard
   # - miq_report_widget_editor
