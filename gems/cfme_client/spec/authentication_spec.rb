7aa4ea9df0287481067b76a614ae46a8fa2a9cba
Jason Frey <jfrey@redhat.com>
2016-02-16 13:39:15 -0500

    Skip tests instead of passing them when we can't connect to the API.

--- gems/cfme_client/spec/authentication_spec.rb
+++ gems/cfme_client/spec/authentication_spec.rb
@@ -6,49 +6,49 @@ describe "CfmeClient Authentication" do
   context "Basic Authentication" do
     it "test basic authentication with bad credentials" do
       success = @cfme[:client].entrypoint(:user => "baduser", :password => "badpassword")
-      if test_api?
-        expect(success).to eq false
-        expect(@cfme[:client].code).to eq(401)
-      end
+      test_api?
+
+      expect(success).to eq false
+      expect(@cfme[:client].code).to eq(401)
     end
 
     it "test basic authentication with correct credentials" do
       success = @cfme[:client].entrypoint(:user => @cfme[:user], :password => @cfme[:password])
-      if test_api?
-        expect(success).to eq true
-        expect(@cfme[:client].code).to eq(200)
-      end
+      test_api?
+
+      expect(success).to eq true
+      expect(@cfme[:client].code).to eq(200)
     end
   end
 
   context "Token Based Authentication" do
     it "gets a token based identifier" do
       success = @cfme[:client].authenticate(:user => @cfme[:user], :password => @cfme[:password])
-      if test_api?
-        expect(success).to eq true
-        expect(@cfme[:client].result).to have_key("auth_token")
-        @cfme[:auth_token] = @cfme[:client].result["auth_token"]
-      end
+      test_api?
+
+      expect(success).to eq true
+      expect(@cfme[:client].result).to have_key("auth_token")
+      @cfme[:auth_token] = @cfme[:client].result["auth_token"]
     end
 
     it "authentication using a bad token" do
       success = @cfme[:client].entrypoint(:auth_token => "badtoken")
-      if test_api?
-        expect(success).to eq false
-        expect(@cfme[:client].code).to eq(401)
-      end
+      test_api?
+
+      expect(success).to eq false
+      expect(@cfme[:client].code).to eq(401)
     end
 
     it "authentication using a valid token" do
       success = @cfme[:client].authenticate(:user => @cfme[:user], :password => @cfme[:password])
-      if test_api?
-        expect(success).to eq true
-        expect(@cfme[:client].result).to have_key("auth_token")
-        @cfme[:auth_token] = @cfme[:client].result["auth_token"]
-        success = @cfme[:client].entrypoint(:auth_token => @cfme[:auth_token])
-        expect(success).to eq true
-        expect(@cfme[:client].code).to eq(200)
-      end
+      test_api?
+
+      expect(success).to eq true
+      expect(@cfme[:client].result).to have_key("auth_token")
+      @cfme[:auth_token] = @cfme[:client].result["auth_token"]
+      success = @cfme[:client].entrypoint(:auth_token => @cfme[:auth_token])
+      expect(success).to eq true
+      expect(@cfme[:client].code).to eq(200)
     end
   end
 end
--- gems/cfme_client/spec/versioning_spec.rb
+++ gems/cfme_client/spec/versioning_spec.rb
@@ -6,37 +6,37 @@ describe "CfmeClient Versioning" do
   context "Versioning Queries" do
     it "test versioning query" do
       success = @cfme[:client].entrypoint(:user => @cfme[:user], :password => @cfme[:password])
-      if test_api?
-        expect(success).to eq true
-        expect(@cfme[:client].code).to eq(200)
-        expect(@cfme[:client].result).to have_key("versions")
-        @cfme[:versions] = @cfme[:client].result["versions"]
-        expect(@cfme[:versions][0]).to_not be_nil
-        expect(@cfme[:versions][0]).to have_key("name")
-      end
+      test_api?
+
+      expect(success).to eq true
+      expect(@cfme[:client].code).to eq(200)
+      expect(@cfme[:client].result).to have_key("versions")
+      @cfme[:versions] = @cfme[:client].result["versions"]
+      expect(@cfme[:versions][0]).to_not be_nil
+      expect(@cfme[:versions][0]).to have_key("name")
     end
 
     it "test query with a valid version" do
       success = @cfme[:client].entrypoint(:user => @cfme[:user], :password => @cfme[:password])
-      if test_api?
-        expect(success).to eq true
-        expect(@cfme[:client].code).to eq(200)
-        expect(@cfme[:client].result).to have_key("versions")
-        @cfme[:versions] = @cfme[:client].result["versions"]
-        expect(@cfme[:versions][0]).to_not be_nil
-        expect(@cfme[:versions][0]).to have_key("name")
-        success = @cfme[:client].entrypoint(:version => @cfme[:versions][0]["name"])
-        expect(success).to eq true
-        expect(@cfme[:client].code).to eq(200)
-      end
+      test_api?
+
+      expect(success).to eq true
+      expect(@cfme[:client].code).to eq(200)
+      expect(@cfme[:client].result).to have_key("versions")
+      @cfme[:versions] = @cfme[:client].result["versions"]
+      expect(@cfme[:versions][0]).to_not be_nil
+      expect(@cfme[:versions][0]).to have_key("name")
+      success = @cfme[:client].entrypoint(:version => @cfme[:versions][0]["name"])
+      expect(success).to eq true
+      expect(@cfme[:client].code).to eq(200)
     end
 
     it "test query with an invalid version" do
       success = @cfme[:client].entrypoint(:user => @cfme[:user], :password => @cfme[:password], :version => "9999.9999")
-      if test_api?
-        expect(success).to eq false
-        expect(@cfme[:client].code).to eq(400)
-      end
+      test_api?
+
+      expect(success).to eq false
+      expect(@cfme[:client].code).to eq(400)
     end
   end
 end
