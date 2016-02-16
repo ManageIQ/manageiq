b40a68964c47e67950463c95ab2fffa2cdf357b0
Jason Frey <jfrey@redhat.com>
2016-02-16 13:30:11 -0500

    Upgrade to the latest RSpec.

--- gems/cfme_client/spec/authentication_spec.rb
+++ gems/cfme_client/spec/authentication_spec.rb
@@ -1,11 +1,3 @@
-#
-# Tests to exercise the CfmeClient
-#
-# Requires EVM to be running
-#
-
-require 'spec_helper'
-
 describe "CfmeClient Authentication" do
   before do
     @cfme = init_api
@@ -15,7 +7,7 @@ describe "CfmeClient Authentication" do
     it "test basic authentication with bad credentials" do
       success = @cfme[:client].entrypoint(:user => "baduser", :password => "badpassword")
       if test_api?
-        expect(success).to be_false
+        expect(success).to eq false
         expect(@cfme[:client].code).to eq(401)
       end
     end
@@ -23,7 +15,7 @@ describe "CfmeClient Authentication" do
     it "test basic authentication with correct credentials" do
       success = @cfme[:client].entrypoint(:user => @cfme[:user], :password => @cfme[:password])
       if test_api?
-        expect(success).to be_true
+        expect(success).to eq true
         expect(@cfme[:client].code).to eq(200)
       end
     end
@@ -33,7 +25,7 @@ describe "CfmeClient Authentication" do
     it "gets a token based identifier" do
       success = @cfme[:client].authenticate(:user => @cfme[:user], :password => @cfme[:password])
       if test_api?
-        expect(success).to be_true
+        expect(success).to eq true
         expect(@cfme[:client].result).to have_key("auth_token")
         @cfme[:auth_token] = @cfme[:client].result["auth_token"]
       end
@@ -42,7 +34,7 @@ describe "CfmeClient Authentication" do
     it "authentication using a bad token" do
       success = @cfme[:client].entrypoint(:auth_token => "badtoken")
       if test_api?
-        expect(success).to be_false
+        expect(success).to eq false
         expect(@cfme[:client].code).to eq(401)
       end
     end
@@ -50,11 +42,11 @@ describe "CfmeClient Authentication" do
     it "authentication using a valid token" do
       success = @cfme[:client].authenticate(:user => @cfme[:user], :password => @cfme[:password])
       if test_api?
-        expect(success).to be_true
+        expect(success).to eq true
         expect(@cfme[:client].result).to have_key("auth_token")
         @cfme[:auth_token] = @cfme[:client].result["auth_token"]
         success = @cfme[:client].entrypoint(:auth_token => @cfme[:auth_token])
-        expect(success).to be_true
+        expect(success).to eq true
         expect(@cfme[:client].code).to eq(200)
       end
     end
