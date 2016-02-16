1766240d59d7ffa2b954d67f118b29768c0f18e8
Jason Frey <jfrey@redhat.com>
2016-02-16 13:30:11 -0500

    Upgrade to the latest RSpec.

--- gems/cfme_client/spec/versioning_spec.rb
+++ gems/cfme_client/spec/versioning_spec.rb
@@ -1,13 +1,3 @@
-#
-# Tests to exercise the CfmeClient
-#
-# Requires EVM to be running
-#
-# Run as rspec spec
-#
-
-require 'spec_helper'
-
 describe "CfmeClient Versioning" do
   before do
     @cfme = init_api
@@ -17,11 +7,11 @@ describe "CfmeClient Versioning" do
     it "test versioning query" do
       success = @cfme[:client].entrypoint(:user => @cfme[:user], :password => @cfme[:password])
       if test_api?
-        expect(success).to be_true
+        expect(success).to eq true
         expect(@cfme[:client].code).to eq(200)
         expect(@cfme[:client].result).to have_key("versions")
         @cfme[:versions] = @cfme[:client].result["versions"]
-        @cfme[:versions][0].should_not be_nil
+        expect(@cfme[:versions][0]).to_not be_nil
         expect(@cfme[:versions][0]).to have_key("name")
       end
     end
@@ -29,14 +19,14 @@ describe "CfmeClient Versioning" do
     it "test query with a valid version" do
       success = @cfme[:client].entrypoint(:user => @cfme[:user], :password => @cfme[:password])
       if test_api?
-        expect(success).to be_true
+        expect(success).to eq true
         expect(@cfme[:client].code).to eq(200)
         expect(@cfme[:client].result).to have_key("versions")
         @cfme[:versions] = @cfme[:client].result["versions"]
-        @cfme[:versions][0].should_not be_nil
+        expect(@cfme[:versions][0]).to_not be_nil
         expect(@cfme[:versions][0]).to have_key("name")
         success = @cfme[:client].entrypoint(:version => @cfme[:versions][0]["name"])
-        expect(success).to be_true
+        expect(success).to eq true
         expect(@cfme[:client].code).to eq(200)
       end
     end
@@ -44,7 +34,7 @@ describe "CfmeClient Versioning" do
     it "test query with an invalid version" do
       success = @cfme[:client].entrypoint(:user => @cfme[:user], :password => @cfme[:password], :version => "9999.9999")
       if test_api?
-        expect(success).to be_false
+        expect(success).to eq false
         expect(@cfme[:client].code).to eq(400)
       end
     end
