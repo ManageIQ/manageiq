300599787ba76243b9b44f401cb3fe8c75799270
Bill Wei <bilwei@redhat.com>
2016-02-17 17:20:22 -0500

    Introduce new table cloud_databases and model

---
+++ app/models/cloud_database.rb
@@ -0,0 +1,10 @@
+class CloudDatabase < ApplicationRecord
+  include NewWithTypeStiMixin
+  include ReportableMixin
+
+  belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::CloudManager"
+  belongs_to :cloud_tenant
+  belongs_to :flavor
+
+  serialize :extra_attributes
+end
--- spec/models/manageiq/providers/cloud_manager_spec.rb
+++ spec/models/manageiq/providers/cloud_manager_spec.rb
@@ -22,4 +22,17 @@ describe EmsCloud do
                       ManageIQ::Providers::Google::CloudManager].collect(&:ems_type)
     expect(described_class.supported_types).to match_array(expected_types)
   end
+
+  context "#flavors" do
+    before do
+      @ems = FactoryGirl.create(:ems_cloud)
+      MyInstanceFlavor = Class.new(Flavor)
+      MyDatabaseFlavor = Class.new(DatabaseFlavor)
+      MyInstanceFlavor.create(:name => 'vm', :ext_management_system => @ems)
+      MyDatabaseFlavor.create(:name => 'db', :ext_management_system => @ems)
+    end
+
+    it { expect(@ems.flavors.first).not_to be_a DatabaseFlavor }
+    it { expect(@ems.database_flavors.first).to be_a DatabaseFlavor}
+  end
 end
