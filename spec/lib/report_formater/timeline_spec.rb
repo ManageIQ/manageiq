8004d4a2b51f8ee77a66b7787f3deffd353c595b
Brian McLaughlin <bmclaugh@redhat.com>
2016-02-18 15:49:06 -0500

    Spec tests for bubble_icon method
    https://bugzilla.redhat.com/show_bug.cgi?id=1280104

--- spec/lib/report_formater/timeline_spec.rb
+++ spec/lib/report_formater/timeline_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe ReportFormatter::ReportTimeline do
   context '#bubble_icon' do
     def stub_bottleneck_event(resource_type, ems_type = nil)
-      BottleneckEvent.create!(:resource_type => resource_type)
+      bottleneck_event = BottleneckEvent.create!(:resource_type => resource_type)
+      unless ems_type.nil?
+        ems = FactoryGirl.create(:ems_redhat)
+        allow(ems).to receive(:emstype).and_return(ems_type)
+        allow(bottleneck_event).to receive(:resource).and_return(ems)
+      end
+      bottleneck_event
     end
 
     it 'shows a generic icon for MiqEnterprise' do
@@ -16,6 +22,10 @@ describe ReportFormatter::ReportTimeline do
 
     it 'shows a generic icon for ExtManagementSystem' do
       expect(ReportFormatter::ReportTimeline.new.bubble_icon(stub_bottleneck_event(ExtManagementSystem))).to eq('ems')
+    end
+
+    it 'shows a Red Hat logo for RHEVM EMS' do
+      expect(ReportFormatter::ReportTimeline.new.bubble_icon(stub_bottleneck_event(ExtManagementSystem, 'rhevm'))).to eq('vendor-redhat')
     end
   end
 end
