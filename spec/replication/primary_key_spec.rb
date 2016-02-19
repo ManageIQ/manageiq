ce24a9f2e53547569722c8deec563c0846b9a78d
Nick Carboni <ncarboni@redhat.com>
2016-02-19 11:43:20 -0500

    Moved primary key check method to ar_dba

--- spec/replication/primary_key_spec.rb
+++ spec/replication/primary_key_spec.rb
@@ -5,18 +5,8 @@ describe "all tables" do
     no_pk = []
     connection.tables.each do |t|
       next if t == "schema_migrations"
-      no_pk << t unless primary_key?(t)
+      no_pk << t unless connection.primary_key?(t)
     end
     expect(no_pk.size).to eq(0), "No primary key found for: #{no_pk.join(", ")}"
-  end
-
-  def primary_key?(t)
-    connection.select_value(<<-SQL)
-      SELECT EXISTS(
-        SELECT 1
-        FROM pg_index
-        WHERE indrelid = '#{t}'::regclass AND indisprimary = true
-      )
-    SQL
   end
 end
