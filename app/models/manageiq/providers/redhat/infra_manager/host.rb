8a75041ed0f443b0a02146e49817a95a5cfedd0e
Šimon Lukašík <slukasik@redhat.com>
2016-02-21 20:53:29 +0100

    Cleans some rubocop
    These changes has been first proposed by @petrblaho. I am just rebasing
    them to the current master.

--- app/models/manageiq/providers/redhat/infra_manager/host.rb
+++ app/models/manageiq/providers/redhat/infra_manager/host.rb
@@ -5,9 +5,10 @@ class ManageIQ::Providers::Redhat::InfraManager::Host < ::Host
   end
 
   def verify_credentials(auth_type = nil, options = {})
-    raise MiqException::MiqHostError, "No credentials defined" if self.missing_credentials?(auth_type)
-    raise MiqException::MiqHostError, "Logon to platform [#{os_image_name}] not supported" if auth_type.to_s != 'ipmi' && os_image_name !~ /linux_*/
-
+    raise MiqException::MiqHostError, "No credentials defined" if missing_credentials?(auth_type)
+    if auth_type.to_s != 'ipmi' && os_image_name !~ /linux_*/
+      raise MiqException::MiqHostError, "Logon to platform [#{os_image_name}] not supported"
+    end
     case auth_type.to_s
     when 'ipmi' then verify_credentials_with_ipmi(auth_type)
     else
