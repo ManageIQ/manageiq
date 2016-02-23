cd6bb7d6cb933788d250ce0b91230551b422dcd7
Keenan Brock <kbrock@redhat.com>
2016-02-23 16:30:12 -0500

    Use MiqSearch.filtered for miq_provision workflow
    source_vm_rbac_filter was used in 2 ways.
    1. if passed an empty array, do a search, otherwise do a filter
    2. if passed an empty query, return nothing, otherwise do a filter
    
    This is changed to:
    
    1. if passed a Scope (with all) do a search, otherwise do a filter
    2. if passed an empty query, return nothing, otherwise do a filter
    
    No longer a special case, and these can collapse.
    No longer need to call empty? and causing a count(*) in the database
    The logging was pulling back all records just to log them. removed.
    
    Now, we are no longer passing in an empty target array into search and expecting
    results.

--- app/models/miq_provision_virt_workflow.rb
+++ app/models/miq_provision_virt_workflow.rb
@@ -517,10 +517,9 @@ class MiqProvisionVirtWorkflow < MiqProvisionWorkflow
     end
 
     rails_logger('allowed_templates', 0)
-    vms = []
+    vms = VmOrTemplate.all
     condition = allowed_template_condition
 
-    run_search = true
     unless options[:tag_filters].blank?
       tag_filters = options[:tag_filters].collect(&:to_s)
       selected_tags = (@values[:vm_tags].to_miq_a + @values[:pre_dialog_vm_tags].to_miq_a).uniq
@@ -540,16 +539,10 @@ class MiqProvisionVirtWorkflow < MiqProvisionWorkflow
       unless tag_conditions.blank?
         _log.info "Filtering VM templates with the following tag_filters: <#{tag_conditions.inspect}>"
         vms = MiqTemplate.find_tags_by_grouping(tag_conditions, :ns => "/managed", :conditions => condition)
-        if vms.blank?
-          _log.warn "tag_filters returned an empty VM template list.  Tag Filters used: <#{tag_conditions.inspect}>"
-          run_search = false
-        else
-          vms.each { |vm| _log.info "tag_filter template returned: <#{vm.id}:#{vm.name}>  GUID: <#{vm.guid}>  UID_EMS: <#{vm.uid_ems}>" }
-        end
       end
     end
 
-    allowed_templates_list = run_search ? source_vm_rbac_filter(vms, condition) : []
+    allowed_templates_list = source_vm_rbac_filter(vms, condition).to_a
     @allowed_templates_filter = filter_id
     @allowed_templates_tag_filters = @values[:vm_tags]
     rails_logger('allowed_templates', 1)
@@ -577,19 +570,8 @@ class MiqProvisionVirtWorkflow < MiqProvisionWorkflow
   end
 
   def source_vm_rbac_filter(vms, condition = nil)
-    filter_id = get_value(@values[:vm_filter]).to_i
-    search_options = {:user => @requester, :results_format => :objects}
-    search_options[:conditions] = condition unless condition.blank?
-    template_msg =  "User: <#{@requester.userid}>"
-    template_msg += " Role: <#{@requester.current_group.nil? ? "none" : @requester.current_group.miq_user_role.name}>  Group: <#{@requester.current_group.nil? ? "none" : @requester.current_group.description}>"
-    template_msg += "  VM Filter: <#{@values[:vm_filter].inspect}>"
-    template_msg += "  Passing inital template IDs: <#{vms.collect(&:id).inspect}>" unless vms.blank?
-    _log.info "Checking for allowed templates for #{template_msg}"
-    if vms.empty?
-      MiqSearch.search(filter_id, VmOrTemplate, search_options).first
-    else
-      MiqSearch.filtered(filter_id, VmOrTemplate, vms, search_options)
-    end
+    MiqSearch.filtered(get_value(@values[:vm_filter]).to_i, VmOrTemplate, vms,
+                       :user => @requester, :conditions => condition)
   end
 
   def allowed_provision_types(_options = {})
@@ -1030,28 +1012,11 @@ class MiqProvisionVirtWorkflow < MiqProvisionWorkflow
   end
 
   def ws_find_template_or_vm(_values, src_name, src_guid, ems_guid)
-    conditions = []
-    args       = []
-
-    unless src_guid.blank?
-      conditions << 'guid = ?'
-      args << src_guid
-    end
-
-    unless ems_guid.blank?
-      conditions << 'uid_ems = ?'
-      args << ems_guid
-    end
-
-    unless src_name.blank?
-      conditions << 'lower(name) = ?'
-      args << src_name
-    end
-
-    conditions = [conditions.join(" AND "), *args]
-    vms = VmOrTemplate.where(conditions)
-    vms = source_vm_rbac_filter(vms) unless vms.blank?
-    vms.first
+    conditions = {}
+    conditions[:guid] = src_guid unless src_guid.blank?
+    conditions[:uid_ems] = ems_guid unless ems_guid.blank?
+    conditions['lower(name)'] = src_name unless src_name.blank?
+    source_vm_rbac_filter(VmOrTemplate.where(conditions)).first
   end
 
   def ws_vm_fields(values, fields)
