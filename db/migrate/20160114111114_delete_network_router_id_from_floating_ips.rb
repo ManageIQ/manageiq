9c387fe16b13df86fc2d6f895ab6e4e334801ada
Ladislav Smola <lsmola@redhat.com>
2016-02-23 18:14:40 +0100

    Do up down migrating for remove_index
    Migration on down complains about order of remove_column
    and remove index

--- db/migrate/20160114111114_delete_network_router_id_from_floating_ips.rb
+++ db/migrate/20160114111114_delete_network_router_id_from_floating_ips.rb
@@ -1,6 +1,11 @@
 class DeleteNetworkRouterIdFromFloatingIps < ActiveRecord::Migration
-  def change
+  def up
     remove_index :floating_ips, :column => :network_router_id
     remove_column :floating_ips, :network_router_id, :bigint
+  end
+
+  def down
+    add_column :floating_ips, :network_router_id, :bigint
+    add_index :floating_ips, :network_router_id
   end
 end
