6f5350d3a4095436915079509f9803f72b0a00c0
Lucas Ponce <ponce.ballesteros@gmail.com>
2016-02-23 14:39:26 +0100

    Add a middleware manager spec

---
+++ spec/models/manageiq/providers/hawkular/middleware_manager_spec.rb
@@ -0,0 +1,9 @@
+describe ManageIQ::Providers::Hawkular::MiddlewareManager do
+  it ".ems_type" do
+    expect(described_class.ems_type).to eq('hawkular')
+  end
+
+  it ".description" do
+    expect(described_class.description).to eq('Hawkular')
+  end
+end
