34123724724fda191338103c3cfb45a930565bec
Chris Arcand <chris@chrisarcand.com>
2016-02-23 12:16:04 -0500

    Remove alias_method_chain from Process override

--- lib/extensions/ar_process.rb
+++ lib/extensions/ar_process.rb
@@ -1,7 +1,8 @@
 class << Process
-  def pid_with_safe
-    warn "Remove me: #{__FILE__}:#{__LINE__}.  Safe level 2-4 are no longer supported as of ruby 2.3!" if RUBY_VERSION >= "2.3"
-    $SAFE < 2 ? pid_without_safe : 0
-  end
-  alias_method_chain :pid, :safe
+  prepend(Module.new {
+    def pid
+      warn "Remove me: #{__FILE__}:#{__LINE__}.  Safe level 2-4 are no longer supported as of ruby 2.3!" if RUBY_VERSION >= "2.3"
+      $SAFE < 2 ? super : 0
+    end
+  })
 end
