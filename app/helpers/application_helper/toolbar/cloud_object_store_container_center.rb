81d9c7e36bf1d66efc1344db9e736a0d6c678258
Sam Lucidi <slucidi@redhat.com>
2016-02-25 16:44:01 -0500

    Convert object store toolbars to classes

---
+++ app/helpers/application_helper/toolbar/cloud_object_store_container_center.rb
@@ -0,0 +1,17 @@
+class ApplicationHelper::Toolbar::CloudObjectStoreContainerCenter < ApplicationHelper::Toolbar::Basic
+  button_group('cloud_object_store_container_policy', [
+    select(
+      :cloud_object_store_container_policy_choice,
+      'fa fa-shield fa-lg',
+      t = N_('Policy'),
+      t,
+      :items => [
+        button(
+          :cloud_object_store_container_tag,
+          'pficon pficon-edit fa-lg',
+          N_('Edit tags for this #{ui_lookup(:table=>"cloud_object_store_container")}'),
+          N_('Edit Tags'))
+      ]
+    )
+  ])
+end
