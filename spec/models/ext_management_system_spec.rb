d47b9439271c67f4dfada5654f771f4ced5680ee
Julian Cheal <jcheal@redhat.com>
2016-02-26 17:44:24 +0000

    Initial pass at `connection_by_role`
    This will become the main entry to adding all connection details to
    a Proivder, aka: hostname, port, authentication, etc

--- spec/models/ext_management_system_spec.rb
+++ spec/models/ext_management_system_spec.rb
@@ -97,14 +97,14 @@ describe ExtManagementSystem do
   context "with multiple endpoints" do
     it "will contain seperate ampq endpoint" do
       ems = FactoryGirl.build(:ems_openstack, :hostname => "example.org")
-      ems.hostname_with_role=({:role => "amqp", :hostname => "amqp.example.org"})
+      ems.connection_by_role=({:role => "amqp", :hostname => "amqp.example.org"})
       expect(ems.default_endpoint.hostname).to eq "example.org"
-      expect(ems.hostname_with_role("amqp")).to eq "amqp.example.org"
+      expect(ems.connection_by_role("amqp")).to eq "amqp.example.org"
     end
 
     it "will contain multiple endpoints" do
       ems = FactoryGirl.build(:ems_openstack, :hostname => "example.org")
-      ems.hostname_with_role=({:role => "amqp", :hostname => "amqp.example.org"})
+      ems.connection_by_role=({:role => "amqp", :hostname => "amqp.example.org"})
       expected_endpoints = ["example.org", "amqp.example.org"]
       expect(ems.hostnames).to match_array(expected_endpoints)
     end
