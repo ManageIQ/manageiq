3b1ea17983b54d852614e89fde6dd2d4e4b2c17d
Brandon Dunne <bdunne@redhat.com>
2016-02-26 11:04:15 -0500

    Extract finding an existing host to a method and add tests

---
+++ spec/models/ems_refresh/save_inventory_infra_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe EmsRefresh::SaveInventoryInfra do
+  let(:refresher) do
+    Class.new do
+      include EmsRefresh::SaveInventoryInfra
+
+      def _log
+        @logger ||= Vmdb.null_logger
+      end
+    end.new
+  end
+
+  context ".find_host" do
+    it "with ems_ref" do
+      FactoryGirl.create(:host, :ems_ref => "some_ems_ref")
+
+      expect(refresher.find_host({:ems_ref => "some_ems_ref"}, nil)).to be_kind_of(Host)
+    end
+
+    it "with ems_ref and ems_id" do
+      FactoryGirl.create(:host, :ems_ref => "some_ems_ref")
+      host_with_ems_id = FactoryGirl.create(:host, :ems_ref => "some_ems_ref_2", :ems_id => 1)
+
+      expect(refresher.find_host({:ems_ref => "some_ems_ref_2", :name => "name"}, nil)).to be_nil
+      expect(refresher.find_host({:ems_ref => "some_ems_ref_2", :name => "name"}, 1)).to   eq(host_with_ems_id)
+    end
+
+    it "with ems_ref, hostname and ipaddress" do
+      FactoryGirl.create(:host, :ems_ref => "some_ems_ref", :hostname => "my.hostname", :ipaddress => "192.168.1.1")
+      expected_host = FactoryGirl.create(:host, :ems_ref => "some_ems_ref", :hostname => "my.hostname", :ipaddress => "192.168.1.2")
+
+      expect(refresher.find_host(expected_host.slice(:ems_ref, :hostname, :ipaddress), nil)).to eq(expected_host)
+    end
+  end
+end
