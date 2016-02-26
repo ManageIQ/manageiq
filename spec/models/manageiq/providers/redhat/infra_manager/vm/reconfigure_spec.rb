5cc9dadbcc37c542ef8137a59016da128c9b123e
Lucy Fu <lufu@redhat.com>
2016-02-26 10:20:00 -0500

    Move method build_config_spec into its own file per provider.
    Move #build_config_spec out of VmReconfigureTask and into its own file under provider.

--- spec/models/manageiq/providers/redhat/infra_manager/vm/reconfigure_spec.rb
+++ spec/models/manageiq/providers/redhat/infra_manager/vm/reconfigure_spec.rb
@@ -20,4 +20,24 @@ describe ManageIQ::Providers::Redhat::InfraManager::Vm::Reconfigure do
   it "#max_memory_mb" do
     expect(vm.max_memory_mb).to eq(2.terabyte / 1.megabyte)
   end
+
+  context "#build_config_spec" do
+    before do
+      @options = {:vm_memory => '1024', :number_of_cpus => '8', :cores_per_socket => '2'}
+      @vm      = FactoryGirl.create(:vm_redhat, :hardware => FactoryGirl.create(:hardware))
+    end
+    subject { @vm.build_config_spec(@options) }
+
+    it "memoryMB" do
+      expect(subject["memoryMB"]).to eq(1024)
+    end
+
+    it "numCPUs" do
+      expect(subject["numCPUs"]).to eq(8)
+    end
+
+    it "numCoresPerSocket" do
+      expect(subject["numCoresPerSocket"]).to eq(2)
+    end
+  end
 end
