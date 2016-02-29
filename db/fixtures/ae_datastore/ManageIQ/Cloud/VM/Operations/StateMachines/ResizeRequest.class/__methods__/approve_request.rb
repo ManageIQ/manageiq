dc88d661ec0ad2d6635854e8e3234003e0e43b88
Marek Aufart <maufart@redhat.com>
2016-02-29 15:07:58 +0100

    [WIP] Add instance resize to automate
    Add state machine which can perform whole resize instance
    process (resize and confirm/revert when resize becomes ready).

---
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Operations/StateMachines/ResizeRequest.class/__methods__/approve_request.rb
@@ -0,0 +1,7 @@
+# Last step - confirm resize of instance
+
+vm = $evm.root['vm']
+
+vm.resize_confirm
+
+exit MIQ_OK
--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-vm.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-vm.rb
@@ -2,6 +2,14 @@ module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Openstack_CloudManager_Vm < MiqAeServiceVmCloud
     expose :cloud_networks, :association => true
 
+    expose :resize
+    expose :confirm_resize
+    expose :revert_resize
+
+    expose :validate_resize
+    expose :validate_resize_confirm
+    expose :validate_resize_revert
+
     def attach_volume(volume_id, device = nil, options = {})
       sync_or_async_ems_operation(options[:sync], "attach_volume", [volume_id, device])
     end
