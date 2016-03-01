040841941a6b7a9bf182c24d7eea7c354d291688
dkorn <dkorn@redhat.com>
2016-03-01 14:51:01 +0200

    OpenShift Deployment Automation
    This PR adds the Deployment class and its relevant methods to the
    automation engine.
    In it is the basic structure for the deployment state machine.

---
+++ db/fixtures/ae_datastore/Deployment/System/Request.class/__methods__/analyze_deployment.rb
@@ -0,0 +1,27 @@
+def analyze_deployment
+  $evm.root['state'] = "analyze_deployment"
+  $evm.log(:info, "********************** analyze deployment ***************************")
+  $evm.root['deployment_type'] = $evm.root['automation_task'].automation_request.options[:attrs][:deployment_type]
+
+  if $evm.root['deployment_type'] == "managed_provision"
+    $evm.root['ae_next_state'] = "provision"
+  else
+    $evm.root['ae_next_state'] = "check_ssh"
+    $evm.root['deployment_master'] = $evm.root['automation_task'].automation_request.options[:attrs][:deployment_master]
+    $evm.root['masters'] = $evm.root['automation_task'].automation_request.options[:attrs][:masters]
+    $evm.root['nodes'] = $evm.root['automation_task'].automation_request.options[:attrs][:nodes]
+  end
+
+  $evm.root['user'] = $evm.root['automation_task'].automation_request.options[:attrs][:username]
+  $evm.root['password'] = $evm.root['automation_task'].automation_request.options[:attrs][:password]
+  $evm.root['ssh_key_path'] = $evm.root['automation_task'].automation_request.options[:attrs][:ssh_key_path]
+  $evm.root['provider_name'] = $evm.root['automation_task'].automation_request.options[:attrs][:provider_name]
+  $evm.root['private_key'] = $evm.root['automation_task'].automation_request.options[:attrs][:ssh_private_key]
+
+  $evm.root['ae_result'] = "ok"
+  $evm.root['automation_task'].message = "deployment type - #{$evm.root['deployment_type']}"
+  $evm.log(:info, "State: #{$evm.root['state']} | Result: #{$evm.root['ae_result']} "\
+           "| Message: #{$evm.root['automation_task'].message}")
+end
+
+analyze_deployment
---
+++ db/fixtures/ae_datastore/Deployment/System/Request.class/__methods__/create_ansible_inventory.rb
@@ -0,0 +1,62 @@
+INVENTORY_FILE = 'inventory.yaml'
+
+def create_inventory_template(deployment_master, masters, nodes, user)
+  template = "
+[OSEv3:children]
+masters
+nodes
+
+[masters:vars]
+ansible_ssh_user=#{user}
+ansible_sudo=true
+deployment_type=origin
+#openshift_use_manageiq=True
+
+[nodes:vars]
+ansible_ssh_user=#{user}
+ansible_sudo=true
+deployment_type=origin
+#openshift_use_manageiq=True
+
+[masters]
+#{deployment_master} ansible_connection=local openshift_scheduleable=True
+#{masters.join("\n") unless masters.empty?}
+
+[nodes]
+#{nodes.join("\n")}
+  "
+
+  template
+end
+
+def replace_connecting_master_ip(nodes, master_ip)
+  nodes.each_with_index do |node, index|
+    if node.include? master_ip
+      nodes[index] = "#{master_ip}              ansible_connection=local"
+    end
+  end
+  nodes
+end
+
+def create_ansible_inventory_file
+  $evm.root['state'] = "create_ansible_inventory"
+  $evm.log(:info, "********************** creating ansible inventory file ***************************")
+  nodes = replace_connecting_master_ip($evm.root['nodes'], $evm.root['deployment_master'])
+  template = create_inventory_template($evm.root['deployment_master'], $evm.root['masters'], nodes, $evm.root['user'])
+
+  begin
+    File.open(INVENTORY_FILE, 'w') do |f|
+      f.write(template)
+    end
+    $evm.root['ae_result'] = "ok"
+    $evm.root['automation_task'].message = "successfully created ansible inventory file"
+  rescue StandardError => e
+    $evm.root['ae_result'] = "error"
+    $evm.root['automation_task'].message = "failed to create ansible inventory file: " + e
+  ensure
+    $evm.log(:info, "State: #{$evm.root['state']} | Result: #{$evm.root['ae_result']} "\
+             "| Message: #{$evm.root['automation_task'].message}")
+  end
+end
+
+create_ansible_inventory_file
