c9b568b1485e6c14e138dcb527e739489666c116
Joe Rafaniello <jrafanie@redhat.com>
2016-03-01 16:20:18 -0500

    Remove a protection around Process.pid in $SAFE 2+. :scissors:
    We no longer log a message with Process.pid while in $SAFE 2+.
    
    This was removed in commit: 9e5ba7dcf
    
    Note, $SAFE 2-4 are gone in ruby 2.3.0 so this would have to be removed anyway.

--- lib/extensions/ar_process.rb
+++
@@ -1,8 +0,0 @@
-class << Process
-  prepend(Module.new {
-    def pid
-      warn "Remove me: #{__FILE__}:#{__LINE__}.  Safe level 2-4 are no longer supported as of ruby 2.3!" if RUBY_VERSION >= "2.3"
-      $SAFE < 2 ? super : 0
-    end
-  })
-end
