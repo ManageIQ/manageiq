2399b7bc8ddc09bfe862cd9f773c16379eed2f88
Brandon Dunne <bdunne@redhat.com>
2016-03-01 18:01:07 -0500

    Remove redundant methods implemented in parent class

--- app/models/file_depot_ftp_anonymous.rb
+++ app/models/file_depot_ftp_anonymous.rb
@@ -1,12 +1,4 @@
 class FileDepotFtpAnonymous < FileDepotFtp
-  def self.requires_credentials?
-    true
-  end
-
-  def requires_support_case?
-    false
-  end
-
   def login_credentials
     ["anonymous", "anonymous"]
   end
