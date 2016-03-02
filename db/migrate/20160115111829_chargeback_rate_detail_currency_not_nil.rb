978ff5f37f61b0ef55cbe05d7822f7cadc0c7bcc
Tamara Ledesma Rico <tamledric@gmail.com>
2016-03-02 13:02:03 +0100

    Modify migration

--- db/migrate/20160115111829_chargeback_rate_detail_currency_not_nil.rb
+++ db/migrate/20160115111829_chargeback_rate_detail_currency_not_nil.rb
@@ -1,10 +1,22 @@
 class ChargebackRateDetailCurrencyNotNil < ActiveRecord::Migration
-  # Migration in order to put a currency by default in rates that were seeded or added by a user before the addition of currencies
+  # Migration in order to put a currency by default in rates that were added by a user before the addition of currencies
   class ChargebackRateDetail < ActiveRecord::Base
     self.inheritance_column = :_type_disabled # disable STI
   end
 
   def up
-    ChargebackRateDetail.where(:chargeback_rate_detail_currency_id => nil).update_all(:chargeback_rate_detail_currency_id => 0)
+    currency = ChargebackRateDetailCurrency.find_by_name("Dollars")
+    if currency
+      currency_id = currency.id
+    else
+      cbc = ChargebackRateDetailCurrency.create(:code        => "USD",
+                                                :name        => "Dollars",
+                                                :full_name   => "United States Dollars",
+                                                :symbol      => "$",
+                                                :unicode_hex => "36"
+                                               )
+      currency_id = cbc.id
+    end
+    ChargebackRateDetail.where(:chargeback_rate_detail_currency_id => nil).update_all(:chargeback_rate_detail_currency_id => currency_id)
   end
 end
