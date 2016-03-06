330e0c7308e9a5467b1e88a2253e0c878ddfd7c7
Aparna Karve <akarve@redhat.com>
2016-03-05 17:14:42 -0800

    Add i18n support

--- app/assets/javascripts/controllers/buttons/paging_div_button_group_controller.js
+++ app/assets/javascripts/controllers/buttons/paging_div_button_group_controller.js
@@ -9,14 +9,14 @@ ManageIQ.angular.app.controller('pagingDivButtonGroupController', ['$scope', 'mi
   }
 
   var saveButton = function() {
-    var disabledSaveHtml = '<button name="button" id="save_disabled" type="submit" class="btn btn-primary btn-disabled" ' +
-      'alt="Save changes" title="Save changes" ng-click="disabledClick($event)" style="cursor:not-allowed" ' +
-      'ng-show="!newRecord && !saveable(angularForm)">Save</button>';
+    var disabledSaveHtml = sprintf('<button name="button" id="save_disabled" type="submit" class="btn btn-primary btn-disabled" ' +
+      'alt=%s title=%s ng-click="disabledClick($event)" style="cursor:not-allowed" ' +
+      'ng-show="!newRecord && !saveable(angularForm)">%s</button>', __("Save changes"), __("Save changes"), __("Save"));
     var compiledDisabledSave = $compile(disabledSaveHtml)($scope);
 
-    var enabledSaveHtml = '<button name="button" id="save_enabled" type="submit" class="btn btn-primary ng-hide" ' +
-      'alt="Save changes" title="Save changes" ng-click="saveClicked($event, true)" ' +
-      'ng-show="!newRecord && saveable(angularForm)">Save</button>';
+    var enabledSaveHtml = sprintf('<button name="button" id="save_enabled" type="submit" class="btn btn-primary ng-hide" ' +
+      'alt=%s title=%s ng-click="saveClicked($event, true)" ' +
+      'ng-show="!newRecord && saveable(angularForm)">%s</button>', __("Save changes"), __("Save changes"), __("Save"));
     var compiledEnabledSave = $compile(enabledSaveHtml)($scope);
 
     if (angular.element(document.getElementById('save_disabled')).length == 0) {
@@ -29,10 +29,10 @@ ManageIQ.angular.app.controller('pagingDivButtonGroupController', ['$scope', 'mi
   }
 
   var resetButton = function() {
-    var resetHtml = '<button name="button" id="reset_enabled_diabled" type="submit" ' +
-      'class="btn btn-default btn-disabled" alt="Reset changes" title="Reset changes" ' +
+    var resetHtml = sprintf('<button name="button" id="reset_enabled_diabled" type="submit" ' +
+      'class="btn btn-default btn-disabled" alt=%s title=%s ' +
       'ng-class="{\'btn-disabled\': angularForm.$pristine}" ng-click="resetClicked()" ' +
-      'ng-disabled="angularForm.$pristine" ng-hide="newRecord" disabled="disabled">Reset</button>';
+      'ng-disabled="angularForm.$pristine" ng-hide="newRecord" disabled="disabled">%s</button>', __("Reset changes"), __("Reset changes"), __("Reset"));
     var compiledReset = $compile(resetHtml)($scope);
 
     if (angular.element(document.getElementById('reset_enabled_diabled')).length == 0) {
@@ -41,8 +41,8 @@ ManageIQ.angular.app.controller('pagingDivButtonGroupController', ['$scope', 'mi
   }
 
   var cancelButton = function() {
-    var cancelHtml = '<button name="button" id="cancel_enabled" type="submit" class="btn btn-default" alt="Cancel" ' +
-      'title="Cancel" ng-click="cancelClicked($event)">Cancel</button>';
+    var cancelHtml = sprintf('<button name="button" id="cancel_enabled" type="submit" class="btn btn-default" alt=%s ' +
+      'title=%s ng-click="cancelClicked($event)">%s</button>', __("Cancel"), __("Cancel"), __("Cancel"));
     var compiledCancel = $compile(cancelHtml)($scope);
 
     if (angular.element(document.getElementById('cancel_enabled')).length == 0) {
