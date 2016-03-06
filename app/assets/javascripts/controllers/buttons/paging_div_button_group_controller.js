01b860e06accf22520fe6d6ae170b3b78e6c98b5
Aparna Karve <akarve@redhat.com>
2016-03-05 16:20:32 -0800

    Refactor - created methods for buttons html

--- app/assets/javascripts/controllers/buttons/paging_div_button_group_controller.js
+++ app/assets/javascripts/controllers/buttons/paging_div_button_group_controller.js
@@ -1,5 +1,14 @@
 ManageIQ.angular.app.controller('pagingDivButtonGroupController', ['$scope', 'miqService', '$compile', function($scope, miqService, $compile) {
   var init = function() {
+    saveButton();
+    resetButton();
+    cancelButton();
+
+    $scope.saveable = miqService.saveable;
+    $scope.disabledClick = miqService.disabledClick;
+  }
+
+  var saveButton = function() {
     var disabledSaveHtml = '<button name="button" id="save_disabled" type="submit" class="btn btn-primary btn-disabled" ' +
       'alt="Save changes" title="Save changes" ng-click="disabledClick($event)" style="cursor:not-allowed" ' +
       'ng-show="!newRecord && !saveable(angularForm)">Save</button>';
@@ -10,16 +19,6 @@ ManageIQ.angular.app.controller('pagingDivButtonGroupController', ['$scope', 'mi
       'ng-show="!newRecord && saveable(angularForm)">Save</button>';
     var compiledEnabledSave = $compile(enabledSaveHtml)($scope);
 
-    var resetHtml = '<button name="button" id="reset_enabled_diabled" type="submit" ' +
-      'class="btn btn-default btn-disabled" alt="Reset changes" title="Reset changes" ' +
-      'ng-class="{\'btn-disabled\': angularForm.$pristine}" ng-click="resetClicked()" ' +
-      'ng-disabled="angularForm.$pristine" ng-hide="newRecord" disabled="disabled">Reset</button>';
-    var compiledReset = $compile(resetHtml)($scope);
-
-    var cancelHtml = '<button name="button" id="cancel_enabled" type="submit" class="btn btn-default" alt="Cancel" ' +
-      'title="Cancel" ng-click="cancelClicked($event)">Cancel</button>';
-    var compiledCancel = $compile(cancelHtml)($scope);
-
     if (angular.element(document.getElementById('save_disabled')).length == 0) {
       angular.element(document.getElementById('angular_paging_div_buttons')).append(compiledDisabledSave);
     }
@@ -27,18 +26,28 @@ ManageIQ.angular.app.controller('pagingDivButtonGroupController', ['$scope', 'mi
     if (angular.element(document.getElementById('save_enabled')).length == 0) {
       angular.element(document.getElementById('angular_paging_div_buttons')).append(compiledEnabledSave);
     }
+  }
+
+  var resetButton = function() {
+    var resetHtml = '<button name="button" id="reset_enabled_diabled" type="submit" ' +
+      'class="btn btn-default btn-disabled" alt="Reset changes" title="Reset changes" ' +
+      'ng-class="{\'btn-disabled\': angularForm.$pristine}" ng-click="resetClicked()" ' +
+      'ng-disabled="angularForm.$pristine" ng-hide="newRecord" disabled="disabled">Reset</button>';
+    var compiledReset = $compile(resetHtml)($scope);
 
     if (angular.element(document.getElementById('reset_enabled_diabled')).length == 0) {
       angular.element(document.getElementById('angular_paging_div_buttons')).append(compiledReset);
     }
+  }
+
+  var cancelButton = function() {
+    var cancelHtml = '<button name="button" id="cancel_enabled" type="submit" class="btn btn-default" alt="Cancel" ' +
+      'title="Cancel" ng-click="cancelClicked($event)">Cancel</button>';
+    var compiledCancel = $compile(cancelHtml)($scope);
 
     if (angular.element(document.getElementById('cancel_enabled')).length == 0) {
       angular.element(document.getElementById('angular_paging_div_buttons')).append(compiledCancel);
     }
-
-    $scope.saveable = miqService.saveable;
-    $scope.disabledClick = miqService.disabledClick;
   }
   init();
 }]);
-
