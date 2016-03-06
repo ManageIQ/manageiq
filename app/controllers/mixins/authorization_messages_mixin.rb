d27cc5a0f9a918063a9c6b7065292ca364ff7ddd
Šimon Lukašík <slukasik@redhat.com>
2016-03-06 14:13:22 +0100

    Refactor: Extract method: unauthorized_count

--- app/controllers/mixins/authorization_messages_mixin.rb
+++ app/controllers/mixins/authorization_messages_mixin.rb
@@ -2,12 +2,14 @@ module AuthorizationMessagesMixin
   private
 
   def notify_about_unauthorized_items(item, table)
-    if @view.extras[:total_count] && @view.extras[:auth_count] &&
-       @view.extras[:total_count] > @view.extras[:auth_count]
+    if unauthorized_count > 0
       @bottom_msg = _('* You are not authorized to view ') +
-                    pluralize(@view.extras[:total_count] - @view.extras[:auth_count],
-                              _("other %{items}") % {:items => item.singularize}) +
+                    pluralize(unauthorized_count, _("other %{items}") % {:items => item.singularize}) +
                     _(' on this ') + table
     end
+  end
+
+  def unauthorized_count
+    @view.extras[:total_count] && @view.extras[:auth_count] ? @view.extras[:total_count] - @view.extras[:auth_count] : 0
   end
 end
