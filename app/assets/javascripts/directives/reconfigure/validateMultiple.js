92b70c01be67acde3731c7603ff037bd4355570c
Laura Galis <lgalis@redhat.com>
2016-03-07 02:16:13 -0500

    Switch reconfig directive to parsers

--- app/assets/javascripts/directives/reconfigure/validateMultiple.js
+++ app/assets/javascripts/directives/reconfigure/validateMultiple.js
@@ -1,25 +1,28 @@
-ManageIQ.angular.app.directive('validateMultiple', function() {
+ManageIQ.angular.app.directive('validateMultiple', function(){
   return {
     require: 'ngModel',
     link: function(scope, elm, attrs, ctrl) {
-      var memtype = attrs.memtype;
-      var multiple = attrs.validateMultiple;
-      var minvalue = attrs.miqmin;
-      var maxvalue = attrs.miqmax;
-
-      ctrl.$validators.integer = function(modelValue, viewValue) {
-        if (ctrl.$isEmpty(modelValue)) {
-          return false;
-        } else{
-          var x = parseInt(viewValue, 10)
-          if(memtype == "GB")
-            x *= 1024;
-          if(x >= parseInt(minvalue, 10) && x <= parseInt(maxvalue, 10) && x % parseInt(multiple, 10) == 0) {
-            return true;
-          }
+      function multipleValidator(modelValue) {
+        var memtype = attrs.memtype;
+        var multiple = attrs.validateMultiple;
+        var minvalue = attrs.miqmin;
+        var maxvalue = attrs.miqmax;
+        var x = parseInt(modelValue, 10);
+        if(memtype == "GB")
+          x *= 1024;
+        if (x >= parseInt(minvalue) && x <= parseInt(maxvalue)) {
+          ctrl.$setValidity('inrange', true);
+        } else {
+          ctrl.$setValidity('inrange', false);
         }
-        return false;
-      };
+        if (x % parseInt(multiple, 10) == 0){
+          ctrl.$setValidity('notmultiple', true);
+        } else {
+          ctrl.$setValidity('notmultiple', false);
+        }
+        return modelValue;
+      }
+      ctrl.$parsers.push(multipleValidator);
     }
   };
 });
