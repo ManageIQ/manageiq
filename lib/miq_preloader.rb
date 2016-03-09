80a5ef85e7e9ead0837388d68e31f0972821fe21
Keenan Brock <kbrock@redhat.com>
2016-03-08 23:33:29 -0500

    Leverage ancestry for ServiceOrchestration#vms

--- lib/miq_preloader.rb
+++ lib/miq_preloader.rb
@@ -3,4 +3,14 @@ module MiqPreloader
     preloader = ActiveRecord::Associations::Preloader.new
     preloader.preload(records, associations)
   end
+
+  # it will load records and their associations, and return the children
+  #
+  # instead of N+1:
+  #   orchestration_stack.subtree.flat_map(&:direct_vms)
+  # use instead:
+  #   preload_and_map(orchestration_stack.subtree, :direct_vms)
+  def self.preload_and_map(records, association)
+    records.to_a.tap { |recs| MiqPreloader.preload(recs, association) }.flat_map(&association)
+  end
 end
