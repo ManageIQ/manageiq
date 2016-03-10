29a597afc2d6aeb87abab8132526d34a86eda6b5
Chris Arcand <chris@chrisarcand.com>
2016-03-09 19:10:50 -0500

    Fix ActionView helpers in MiqReport::Formatting
    These were broken since the monkey patching and alias_method_chains were
    removed from these methods in #6851
    
    Note using view helpers in this model is strongly discouraged, but this
    fixes the issue to how it worked before, anyway.

---
+++ spec/models/miq_report/formatting_spec.rb
@@ -0,0 +1,31 @@
+describe MiqReport, "::Formatting" do
+  subject { described_class.new(:db => "Vm") }
+
+  describe "#format_currency_with_delimiter" do
+    it "adds a prefix and suffix to NumberHelper#number_to_currency" do
+      expect(subject.format_currency_with_delimiter(1234567890.50, :prefix => "Front ", :suffix => " Back"))
+        .to eq("Front $1,234,567,890.50 Back")
+    end
+  end
+
+  describe "#format_number_with_delimiter" do
+    it "adds a prefix and suffix to NumberHelper#number_with_delimiter" do
+      expect(subject.format_number_with_delimiter(12345678, :prefix => "Front ", :suffix => " Back"))
+        .to eq("Front 12,345,678 Back")
+    end
+  end
+
+  describe "#format_mhz_to_human_size" do
+    it "adds a prefix and suffix to NumberHelper#mhz_to_human_size" do
+      expect(subject.format_mhz_to_human_size(123, :prefix => "Front ", :suffix => " Back"))
+        .to eq("Front 123 MHz Back")
+    end
+  end
+
+  describe "#format_bytes_to_human_size" do
+    it "adds a prefix and suffix to NumberHelper#number_to_human_size" do
+      expect(subject.format_bytes_to_human_size(123, :prefix => "Front ", :suffix => " Back"))
+        .to eq("Front 123 Bytes Back")
+    end
+  end
+end
