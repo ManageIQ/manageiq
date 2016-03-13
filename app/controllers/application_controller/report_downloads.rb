6da1eebbdec58453cb2d618f510455db33507673
Šimon Lukašík <isimluk@fedoraproject.org>
2016-03-13 15:33:32 +0100

    Refactor: Move render_report_data with dependencies to concern
    Then, make RENDER_TYPES private.

--- app/controllers/application_controller/report_downloads.rb
+++ app/controllers/application_controller/report_downloads.rb
@@ -44,7 +44,62 @@ module ApplicationController::ReportDownloads
     render_pdf
   end
 
+  # Render report in csv/txt/pdf format asynchronously
+  def render_report_data
+    render_type = RENDER_TYPES[params[:render_type]]
+    assert_privileges("render_report_#{render_type}")
+    unless params[:task_id] # First time thru, kick off the report generate task
+      if render_type
+        @sb[:render_type] = render_type
+        rr = MiqReportResult.find(session[:report_result_id]) # Get report task id from the session
+        task_id = rr.async_generate_result(@sb[:render_type], :userid     => session[:userid],
+                                                              :session_id => request.session_options[:id])
+        initiate_wait_for_task(:task_id => task_id)
+      end
+      return
+    end
+
+    miq_task = MiqTask.find(params[:task_id])
+    if miq_task.task_results.blank? || miq_task.status != "Ok" # Check to see if any results came back or status not Ok
+      add_flash(_("Report generation returned: Status [%{status}] Message [%{message}]") % {:status => miq_task.status, :message => miq_task.message}, :error)
+      render :update do |page|
+        page << "if (miqDomElementExists('flash_msg_div_report_list')){"
+        page.replace("flash_msg_div_report_list", :partial => "layouts/flash_msg",
+                                                  :locals  => {:div_num => "_report_list"})
+        page << "} else {"
+        page.replace("flash_msg_div", :partial => "layouts/flash_msg")
+        page << "}"
+        page << "miqSparkle(false);"
+      end
+    else
+      @sb[:render_rr_id] = miq_task.miq_report_result.id
+      render :update do |page|
+        page << "miqSparkle(false);"
+        page << "DoNav('#{url_for(:action => "send_report_data")}');"
+      end
+    end
+  end
+  alias_method :render_report_txt, :render_report_data
+  alias_method :render_report_csv, :render_report_data
+  alias_method :render_report_pdf, :render_report_data
+
+  # Send rendered report data
+  def send_report_data
+    if @sb[:render_rr_id]
+      rr = MiqReportResult.find(@sb[:render_rr_id])
+      filename = filename_timestamp(rr.report.title, 'export_filename')
+      disable_client_cache
+      generated_result = rr.get_generated_result(@sb[:render_type])
+      rr.destroy
+      send_data(generated_result,
+                :filename => "#{filename}.#{@sb[:render_type]}",
+                :type     => "application/#{@sb[:render_type]}")
+    end
+  end
+
   private
+
+  RENDER_TYPES = {'txt' => :txt, 'csv' => :csv, 'pdf' => :pdf}
 
   def report_for_rendering
     if session[:rpt_task_id]
