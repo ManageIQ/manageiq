b26eecdab1b784e90a7419b8f61f871f66979d31
Matthew Draper <mdraper@redhat.com>
2016-03-17 05:52:25 +1030

    Avoid deadlock due to our Dalli autoload "enhancement"

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -32,6 +32,23 @@ else
       :expire_after => 24.hours,
       :key          => "_vmdb_session",
     )
+
+    require 'rack/session/dalli'
+    Rack::Session::Dalli.prepend Module.new {
+      # As we monkey-patch marshal to support autoloading, Dalli can
+      # cause a load to occur. Consequently, we need to manage things
+      # carefully to prevent a deadlock between the Rails Interlock and
+      # Dalli's own exclusive lock.
+      def with_lock(*args)
+        ActiveSupport::Dependencies.interlock.permit_concurrent_loads do
+          super(*args) do
+            ActiveSupport::Dependencies.interlock.running do
+              yield
+            end
+          end
+        end
+      end
+    }
   end
 
   Vmdb::Application.config.session_store rails_store, session_options
