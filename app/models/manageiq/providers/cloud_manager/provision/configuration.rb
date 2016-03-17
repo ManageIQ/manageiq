89f15416abd142b06632584feb2345cb6695a139
alongoldboim <alongoldboim@gmail.com>
2016-03-17 11:16:30 +0200

    Fixes #7344

--- app/models/manageiq/providers/cloud_manager/provision/configuration.rb
+++ app/models/manageiq/providers/cloud_manager/provision/configuration.rb
@@ -2,6 +2,6 @@ module ManageIQ::Providers::CloudManager::Provision::Configuration
   def userdata_payload
     return nil unless customization_template
     options  = prepare_customization_template_substitution_options
-    customization_template.script_with_substitution(options)
+    Base64.encode64(customization_template.script_with_substitution(options))
   end
 end
