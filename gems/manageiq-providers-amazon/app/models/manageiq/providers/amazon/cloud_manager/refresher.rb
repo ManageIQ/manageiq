bc5db6cc0934f9ab8ea5bcb222b827a12896041a
Ladislav Smola <lsmola@redhat.com>
2016-03-24 17:14:54 +0100

    Hook Amazon NetworkManager refresh after CloudManager refresh
    Hook Amazon NetworkManager refresh after CloudManager refresh

--- gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/cloud_manager/refresher.rb
+++ gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/cloud_manager/refresher.rb
@@ -5,6 +5,13 @@ class ManageIQ::Providers::Amazon::CloudManager::Refresher < ManageIQ::Providers
     ::ManageIQ::Providers::Amazon::CloudManager::RefreshParser.ems_inv_to_hashes(ems, refresher_options)
   end
 
+  # TODO(lsmola) NetworkManager, remove this once we have a full representation of the NetworkManager.
+  # NetworkManager should refresh base on its own conditions
+  def save_inventory(ems, _targets, hashes)
+    EmsRefresh.save_ems_inventory(ems, hashes)
+    EmsRefresh.queue_refresh(ems.network_manager)
+  end
+
   def post_process_refresh_classes
     [::Vm]
   end
