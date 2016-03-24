55f213ff91be2b8b2a1c88d698374a7351c6a10d
Ladislav Smola <lsmola@redhat.com>
2016-03-24 18:09:09 +0100

    Amazon NetworkManager models factories
    Amazon NetworkManager models factories

--- spec/factories/floating_ip.rb
+++ spec/factories/floating_ip.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     sequence(:address) { |n| ip_from_seq(n) }
   end
 
-  factory :floating_ip_amazon, :parent => :floating_ip, :class => "ManageIQ::Providers::Amazon::CloudManager::FloatingIp" do
+  factory :floating_ip_amazon, :parent => :floating_ip, :class => "ManageIQ::Providers::Amazon::NetworkManager::FloatingIp" do
   end
 
   factory :floating_ip_openstack, :parent => :floating_ip, :class => "ManageIQ::Providers::Openstack::NetworkManager::FloatingIp" do
