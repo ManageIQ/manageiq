b5ef7df9ed357eec3ad5cdce6677e1a370f0eab9
Nick Carboni <ncarboni@redhat.com>
2016-03-24 16:28:06 -0400

    Alter replication primary key spec to check for "id" key name
    Also make the message more clear about why this is the case
    
    Amend primary key spec failure message

--- spec/replication/primary_key_spec.rb
+++ spec/replication/primary_key_spec.rb
@@ -1,12 +1,21 @@
 describe "all tables" do
   let(:connection) { ApplicationRecord.connection }
 
-  it "have a primary key" do
+  it "have a primary key called id" do
     no_pk = []
     connection.tables.each do |t|
-      next if t == "schema_migrations"
-      no_pk << t unless connection.primary_key?(t)
+      next if %w(schema_migrations ar_internal_metadata).include?(t)
+      no_pk << t unless connection.primary_keys(t) == ["id"]
     end
-    expect(no_pk.size).to eq(0), "No primary key found for: #{no_pk.join(", ")}"
+    expect(no_pk.size).to eq(0), <<-EOS.gsub!(/^ +/, "")
+      Primary key "id" not found for the following table(s):
+
+      #{no_pk.join("\n")}
+
+      All tables must have a primary key called "id"
+      Replication with pglogical requires all tables to have a primary key
+      We have chosen to use "id" rather than a composite key to avoid future migration
+      problems should a table need to move away from a composite key in the future.
+    EOS
   end
 end
