639ae6a5359c32f707bc647a985983b92edebcdb
Keenan Brock <kbrock@redhat.com>
2016-03-27 16:21:57 -0400

    lazy load AssignedServerRole in factory

--- spec/factories/assigned_server_role.rb
+++ spec/factories/assigned_server_role.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :assigned_server_role do
-    active          TRUE
-    priority        AssignedServerRole::HIGH_PRIORITY
+    active          { true }
+    priority        { AssignedServerRole::HIGH_PRIORITY }
   end
 end
