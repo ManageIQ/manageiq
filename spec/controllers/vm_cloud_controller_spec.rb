fc241e04ad5777da96ebd7fb9242d70e8be4dac8
Sam Lucidi <slucidi@redhat.com>
2016-03-28 17:27:41 -0400

    remove stray vm_protect test from resize ui test

--- spec/controllers/vm_cloud_controller_spec.rb
+++ spec/controllers/vm_cloud_controller_spec.rb
@@ -65,9 +65,6 @@ describe VmCloudController do
       post :explorer
       expect(response.status).to eq(200)
 
-      post :x_button, :params => {:pressed => 'vm_protect', :id => vm_openstack.id}
-      expect(response.status).to eq(200)
-
       allow(controller).to receive(:x_node).and_return("v-#{vm_openstack.compressed_id}")
 
       post :x_button, :params => {:pressed => 'instance_resize', :id => vm_openstack.id}
