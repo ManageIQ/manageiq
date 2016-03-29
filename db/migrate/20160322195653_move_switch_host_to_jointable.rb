4e4927276c88ffef061a122a3ee37a3e7b7368c4
James Wong <jwong@redhat.com>
2016-03-29 15:50:26 -0400

    Code review feedback implemented

--- db/migrate/20160322195653_move_switch_host_to_jointable.rb
+++ db/migrate/20160322195653_move_switch_host_to_jointable.rb
@@ -1,13 +1,11 @@
 class MoveSwitchHostToJointable < ActiveRecord::Migration[5.0]
   class Host < ActiveRecord::Base
     self.inheritance_column = :_type_disabled # disable STI
-    has_many :switches,
-             :class_name => 'MoveSwitchHostToJointable::Switch'
+    has_many :switches, :class_name => 'MoveSwitchHostToJointable::Switch'
   end
 
   class Switch < ActiveRecord::Base
-    belongs_to :host,
-               :class_name => 'MoveSwitchHostToJointable::Host'
+    belongs_to :host, :class_name => 'MoveSwitchHostToJointable::Host'
   end
 
   class HostSwitch < ActiveRecord::Base
@@ -15,7 +13,7 @@ class MoveSwitchHostToJointable < ActiveRecord::Migration[5.0]
 
   def up
     say_with_time('Populating host_switches table with (host.id, switch.id)') do
-      Host.all.each do |host|
+      Host.includes(:switches).find_each do |host|
         host.switches.each do |switch|
           HostSwitch.create!(:host_id => host.id, :switch_id => switch.id)
         end
