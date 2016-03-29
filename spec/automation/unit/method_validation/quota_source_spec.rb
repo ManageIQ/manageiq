bdd8102d1ed76a2162283da3f9eaf07c45836d2e
william fitzgerald <wfitzger@redhat.com>
2016-03-29 15:16:08 -0400

    Automate - Change consolidated quota schema to add quota source attribute
    Added quota_source_type attribute to QuotaStateMachines class
    
    Modified quota_source.rb to determine quota type
    
    Updated quota_source spec
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1319910

--- spec/automation/unit/method_validation/quota_source_spec.rb
+++ spec/automation/unit/method_validation/quota_source_spec.rb
@@ -1,21 +1,30 @@
 include QuotaHelper
 
 describe "Quota Validation" do
-  def run_automate_method(provision_request)
+  def run_automate_method(provision_request, type)
     attrs = []
     attrs << "MiqProvisionRequest::miq_provision_request=#{provision_request.id}&" \
            "MiqRequest::miq_request=#{provision_request.id}" if provision_request
     MiqAeEngine.instantiate("/ManageIQ/system/request/Call_Instance?namespace=System/CommonMethods&" \
-                            "class=QuotaMethods&instance=quota_source&#{attrs.join('&')}", @user)
+                            "class=QuotaMethods&instance=quota_source&" \
+                            "quota_source_type=#{type}&#{attrs.join('&')}", @user)
   end
 
   before do
     setup_model
   end
 
-  it "source" do
-    ws = run_automate_method(@miq_provision_request)
+  it "tenant source" do
+    ws = run_automate_method(@miq_provision_request, 'tenant')
     root = ws.root
     expect(root['quota_source']).to be_kind_of(MiqAeMethodService::MiqAeServiceTenant)
+    expect(root['quota_source_type']).to eq('tenant')
+  end
+
+  it "group source" do
+    ws = run_automate_method(@miq_provision_request, 'group')
+    root = ws.root
+    expect(root['quota_source']).to be_kind_of(MiqAeMethodService::MiqAeServiceMiqGroup)
+    expect(root['quota_source_type']).to eq('group')
   end
 end
