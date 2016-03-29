31322f3d3adc9516bc0a5333c22650adcd0895ee
James Wong <jwong@redhat.com>
2016-03-29 00:29:28 -0400

    Add down migration for HostSwitch

--- db/migrate/20160322205357_remove_host_id_from_switch.rb
+++ db/migrate/20160322205357_remove_host_id_from_switch.rb
@@ -1,5 +1,41 @@
 class RemoveHostIdFromSwitch < ActiveRecord::Migration[5.0]
-  def change
+  class Host < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled # disable STI
+    has_many :host_switches,
+             :dependent   => :destroy,
+             :class_name  => 'RemoveHostIdFromSwitch::HostSwitch'
+    has_many :switches,
+             :through => :host_switches,
+             :class_name  => 'RemoveHostIdFromSwitch::Switch'
+  end
+
+  class Switch < ActiveRecord::Base
+    has_many :host_switches,
+             :dependent   => :destroy,
+             :class_name  => 'RemoveHostIdFromSwitch::HostSwitch'
+    has_many :hosts,
+             :through => :host_switches,
+             :class_name => 'RemoveHostIdFromSwitch::Host'
+  end
+
+  class HostSwitch < ActiveRecord::Base
+    belongs_to :host,
+               :class_name  => 'RemoveHostIdFromSwitch::Host'
+    belongs_to :switch,
+               :class_name => 'RemoveHostIdFromSwitch::Switch'
+  end
+
+  def up
     remove_column :switches, :host_id, :bigint
+  end
+
+  def down
+    add_column :switches, :host_id, :bigint
+    say_with_time('Populating switches.host_id from host_switches table') do
+      Switch.all.each do |switch|
+        switch.host_id = switch.hosts[0].id
+        switch.save!
+      end
+    end
   end
 end
