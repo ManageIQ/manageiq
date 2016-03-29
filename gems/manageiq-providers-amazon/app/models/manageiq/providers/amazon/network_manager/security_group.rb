68be7571435669998ef528dd6d2654b48aef2113
Ladislav Smola <lsmola@redhat.com>
2016-03-29 18:39:20 +0200

    Amazon NetworkManager create fake network_port for old EC2 vms
    Amazon NetworkManager create fake network_port for old EC2 vms.
    The old EC2 instances don'have any nterfaces nor VPC, we need the
    interface for consistency in our models.

--- gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/network_manager/security_group.rb
+++ gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/network_manager/security_group.rb
@@ -1,2 +1,3 @@
 class ManageIQ::Providers::Amazon::NetworkManager::SecurityGroup < ::SecurityGroup
+  has_many :vms, :through => :network_ports, :source => :device, :source_type => 'VmOrTemplate'
 end
