618ba048ba1f78b1686b7d0f6fd09455d3b0154a
Martin Povolny <mpovolny@redhat.com>
2016-03-29 13:18:02 +0200

    Toolbar buttons: fix spec.

--- app/helpers/application_helper/button/history_item.rb
+++ app/helpers/application_helper/button/history_item.rb
@@ -10,7 +10,7 @@ class ApplicationHelper::Button::HistoryItem < ApplicationHelper::Button::Basic
   # buttons and these are then hidden one by one by the following code.
   # TODO: Generate the buttons from the history instead.
   def history_item_id
-    @history_item_id ||= self[:id].split("_").last.to_i
+    @history_item_id ||= self[:id].to_s.split("_").last.to_i
     @history_item_id
   end
 
