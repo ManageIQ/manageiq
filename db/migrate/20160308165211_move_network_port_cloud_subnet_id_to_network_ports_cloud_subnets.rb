89f6d011284f879a061a59951d92e20fac6256da
Ladislav Smola <lsmola@redhat.com>
2016-03-30 16:54:13 +0200

    Use find_each instead of all to be more memory friendly
    Use find_each instead of all to be more memory friendly

--- db/migrate/20160308165211_move_network_port_cloud_subnet_id_to_network_ports_cloud_subnets.rb
+++ db/migrate/20160308165211_move_network_port_cloud_subnet_id_to_network_ports_cloud_subnets.rb
@@ -14,7 +14,7 @@ class MoveNetworkPortCloudSubnetIdToNetworkPortsCloudSubnets < ActiveRecord::Mig
 
   def up
     # Move NetworkPort belongs_to :cloud_subnet to NetworkPort has_many :cloud_subnets, :through => :cloud_subnet_network_port
-    NetworkPort.all.each do |network_port|
+    NetworkPort.find_each do |network_port|
       CloudSubnetNetworkPort.create!(
         :cloud_subnet_id => network_port.cloud_subnet_id,
         :network_port_id => network_port.id)
@@ -23,7 +23,7 @@ class MoveNetworkPortCloudSubnetIdToNetworkPortsCloudSubnets < ActiveRecord::Mig
 
   def down
     # Move NetworkPort belongs_to :cloud_subnet to NetworkPort has_many :cloud_subnets, :through => :cloud_subnet_network_port
-    NetworkPort.all.each do |network_port|
+    NetworkPort.find_each do |network_port|
       cloud_subnet_network_port = CloudSubnetNetworkPort.find_by(:network_port_id => network_port.id)
       if cloud_subnet_network_port
         network_port.update_attributes!(:cloud_subnet_id => cloud_subnet_network_port.cloud_subnet_id)
