05132f29c7a527a4d094fa596b7a4a35b109fac7
Ladislav Smola <lsmola@redhat.com>
2016-03-30 09:52:00 +0200

    Simplify Amazon NetworkManager models structure
    Simplify Amazon NetworkManager models structure

--- gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/network_manager/cloud_subnet.rb
+++ gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/network_manager/cloud_subnet.rb
@@ -1,3 +1,2 @@
 class ManageIQ::Providers::Amazon::NetworkManager::CloudSubnet < ::CloudSubnet
-  belongs_to :network_router, :class_name => "ManageIQ::Providers::Amazon::NetworkManager::NetworkRouter"
 end
