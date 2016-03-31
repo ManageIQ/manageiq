6a2f81f5d67020b47ef9ccc4c3a61bcd3e664adb
Aparna Karve <akarve@redhat.com>
2016-03-31 09:38:56 -0700

    Refactor of ServiceData factory code

--- app/assets/javascripts/factories/service/service_data.js
+++ app/assets/javascripts/factories/service/service_data.js
@@ -1,10 +1,15 @@
 ManageIQ.angular.app.factory('serviceDataFactory', ['API', function(API) {
-  var factoryService = {
-    getServiceData: function(id) {
-      if(angular.isDefined(id)) {
-        return API.get('/api/services/' + id)
+  var urlBase = '/api/services';
+  var serviceDataFactory = {};
+
+  serviceDataFactory.getServiceData = function (id) {
+    if(angular.isDefined(id)) {
+      return API.get(urlBase + '/' + id).then(handleSuccess);
+
+      function handleSuccess(response) {
+        return response;
       }
     }
   };
-  return factoryService;
+  return serviceDataFactory;
 }]);
