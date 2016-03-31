db2d5711279c8adbd00f069f829d6bc153fdcad0
Adam Grare <agrare@redhat.com>
2016-03-31 14:47:33 -0400

    Add spec tests for best-fit with read-only storage

--- spec/automation/unit/method_validation/vmware_best_fit_least_utilized_spec.rb
+++ spec/automation/unit/method_validation/vmware_best_fit_least_utilized_spec.rb
@@ -23,12 +23,16 @@ describe "Vmware_best_fit_least_utilized" do
   context "Auto placement" do
     let(:storages) { 4.times.collect { |r| FactoryGirl.create(:storage, :free_space => 1000 * (r + 1)) } }
 
+    let(:ro_storage) { FactoryGirl.create(:storage, :free_space => 10000) }
+
     let(:vms) { 5.times.collect { FactoryGirl.create(:vm_vmware) } }
 
     # host1 has two small  storages and 2 vms
     # host2 has two larger storages and 3 vms
+    # host3 has one larger read-only datastore and one smaller writable datastore
     let(:host1) { FactoryGirl.create(:host_vmware, :storages => storages[0..1], :vms => vms[2..3], :ext_management_system => ems) }
     let(:host2) { FactoryGirl.create(:host_vmware, :storages => storages[0..1], :vms => vms[2..4], :ext_management_system => ems) }
+    let(:host3) { FactoryGirl.create(:host_vmware, :storages => [ro_storage, storages[2]], :vms => vms[2..4], :ext_management_system => ems) }
 
     let(:host_struct) do
       [MiqHashStruct.new(:id => host1.id, :evm_object_class => host1.class.base_class.name.to_sym),
@@ -40,6 +44,7 @@ describe "Vmware_best_fit_least_utilized" do
         host1.ems_cluster = ems_cluster
         host2.ems_cluster = ems_cluster
         datacenter.with_relationship_type("ems_metadata") { datacenter.add_child(ems_cluster) }
+        HostStorage.where(:host_id => host3.id, :storage_id => ro_storage.id).update(:read_only => true)
       end
 
       it "selects a host with fewer vms and a storage with more free space" do
@@ -50,6 +55,17 @@ describe "Vmware_best_fit_least_utilized" do
         miq_provision.reload
         expect(miq_provision.options[:placement_host_name]).to eq([host1.id, host1.name])
         expect(miq_provision.options[:placement_ds_name]).to   eq([host1.storages[1].id, host1.storages[1].name])
+      end
+
+      it "selects largest storage that is writable" do
+        allow_any_instance_of(MiqProvisionWorkflow).to receive(:allowed_hosts).and_return([host3])
+        allow_any_instance_of(MiqProvisionWorkflow).to receive(:allowed_storages).and_return(host3.storages)
+
+        ws.root
+        miq_provision.reload
+
+        # host3.storages[0] is larger but read-only, so it should select host3.storages[1]
+        expect(miq_provision.options[:placement_ds_name]).to eq([host3.storages[1].id, host3.storages[1].name])
       end
     end
 
