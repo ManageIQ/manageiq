5c14fc404d80ee55e5a1712b9da7fb80ff30cdc7
Yuri Rudman <yrudman@redhat.com>
2016-03-31 16:18:24 -0400

    Converted keys in settings to symbols in ‘Adding tables to replication worker exclude list’

--- db/migrate/20160115142023_remove_replicated_rows_from_newly_excluded_tables.rb
+++ db/migrate/20160115142023_remove_replicated_rows_from_newly_excluded_tables.rb
@@ -21,7 +21,7 @@ class RemoveReplicatedRowsFromNewlyExcludedTables < ActiveRecord::Migration
 
     say_with_time("Adding tables to replication worker exclude list") do
       Configuration.where(:typ => "vmdb").each do |c|
-        c.settings[:workers][:worker_base][:replication_worker][:replication][:exclude_tables] << MiqEventDefinition.table_name << ScanItem.table_name
+        c.settings.deep_symbolize_keys[:workers][:worker_base][:replication_worker][:replication][:exclude_tables] << MiqEventDefinition.table_name << ScanItem.table_name
         c.save!
       end
     end
