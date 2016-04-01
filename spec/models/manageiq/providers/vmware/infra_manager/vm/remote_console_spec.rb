b1eff2f2c8633e283ba009c7aea75b10cfb709a9
Dávid Halász <dhalasz@redhat.com>
2016-04-01 14:14:50 +0200

    Refactored specs for VMWare remote consoles

--- spec/models/manageiq/providers/vmware/infra_manager/vm/remote_console_spec.rb
+++ spec/models/manageiq/providers/vmware/infra_manager/vm/remote_console_spec.rb
@@ -1,192 +1,194 @@
 describe ManageIQ::Providers::Vmware::InfraManager::Vm::RemoteConsole do
-  context "#remote_console_acquire_ticket" do
-    before(:each) do
-      @vm = FactoryGirl.create(:vm_with_ref, :ext_management_system => @ems)
+  let(:ems) do
+    FactoryGirl.create(:ems_vmware,
+                       :hostname    => '192.168.252.16',
+                       :ipaddress   => '192.168.252.16',
+                       :api_version => '5.0',
+                       :uid_ems     => '2E1C1E82-BD83-4E54-9271-630C6DFAD4D1')
+  end
+  let(:vm) { FactoryGirl.create(:vm_with_ref, :ext_management_system => ems) }
+
+  context '#remote_console_acquire_ticket' do
+    it 'with :mks' do
+      expect(vm).to receive(:remote_console_mks_acquire_ticket).with(nil)
+      vm.remote_console_acquire_ticket(:mks)
     end
 
-    it "with :mks" do
-      expect(@vm).to receive(:remote_console_mks_acquire_ticket).with(nil)
-      @vm.remote_console_acquire_ticket(:mks)
+    it 'with :vmrc' do
+      expect(vm).to receive(:remote_console_vmrc_acquire_ticket).with(nil)
+      vm.remote_console_acquire_ticket(:vmrc)
     end
 
-    it "with :vmrc" do
-      expect(@vm).to receive(:remote_console_vmrc_acquire_ticket).with(nil)
-      @vm.remote_console_acquire_ticket(:vmrc)
-    end
-
-    context "with :vnc" do
-      it "without a proxy" do
-        expect(@vm).to receive(:remote_console_vnc_acquire_ticket).with(nil)
-        @vm.remote_console_acquire_ticket(:vnc)
+    context 'with :vnc' do
+      it 'without a proxy' do
+        expect(vm).to receive(:remote_console_vnc_acquire_ticket).with(nil)
+        vm.remote_console_acquire_ticket(:vnc)
       end
 
-      it "with a proxy" do
+      it 'with a proxy' do
         server = double("MiqServer")
-        expect(@vm).to receive(:remote_console_vnc_acquire_ticket).with(server)
-        @vm.remote_console_acquire_ticket(:vnc, server)
+        expect(vm).to receive(:remote_console_vnc_acquire_ticket).with(server)
+        vm.remote_console_acquire_ticket(:vnc, server)
       end
     end
   end
 
-  context "#remote_console_acquire_ticket_queue" do
-    before(:each) do
-      @vm = FactoryGirl.create(:vm_with_ref, :ext_management_system => @ems)
-      allow(@vm).to receive_messages(:my_zone => nil)
+  context '#remote_console_acquire_ticket_queue' do
+    let(:server) { double('MiqServer') }
 
-      @server = double("MiqServer")
-      allow(@server).to receive_messages(:my_zone => nil)
-      allow(MiqServer).to receive_messages(:my_server => @server)
+    before(:each) do
+      allow(vm).to receive_messages(:my_zone => nil)
+      allow(server).to receive_messages(:my_zone => nil)
+      allow(MiqServer).to receive_messages(:my_server => server)
     end
 
-    it "with :mks" do
-      @vm.remote_console_acquire_ticket_queue(:mks, "admin")
+    it 'with :mks' do
+      vm.remote_console_acquire_ticket_queue(:mks, 'admin')
 
       q_all = MiqQueue.all
       expect(q_all.length).to eq(1)
-      expect(q_all[0].method_name).to eq("remote_console_acquire_ticket")
+      expect(q_all[0].method_name).to eq('remote_console_acquire_ticket')
       expect(q_all[0].args).to eq([:mks, nil])
     end
 
-    it "with :vmrc" do
-      @vm.remote_console_acquire_ticket_queue(:vmrc, "admin")
+    it 'with :vmrc' do
+      vm.remote_console_acquire_ticket_queue(:vmrc, 'admin')
 
       q_all = MiqQueue.all
       expect(q_all.length).to eq(1)
-      expect(q_all[0].method_name).to eq("remote_console_acquire_ticket")
+      expect(q_all[0].method_name).to eq('remote_console_acquire_ticket')
       expect(q_all[0].args).to eq([:vmrc, nil])
     end
 
-    it "with :vnc" do
-      @vm.remote_console_acquire_ticket_queue(:vnc, "admin", 1234)
+    it 'with :vnc' do
+      vm.remote_console_acquire_ticket_queue(:vnc, 'admin', 1234)
 
       q_all = MiqQueue.all
       expect(q_all.length).to eq(1)
-      expect(q_all[0].method_name).to eq("remote_console_acquire_ticket")
+      expect(q_all[0].method_name).to eq('remote_console_acquire_ticket')
       expect(q_all[0].args).to eq([:vnc, 1234])
     end
   end
 
-  context "#remote_console_vmrc_acquire_ticket" do
-    before(:each) do
-      @ems = FactoryGirl.create(:ems_vmware, :hostname => "192.168.252.16", :ipaddress => "192.168.252.16", :api_version => "5.0", :uid_ems => "2E1C1E82-BD83-4E54-9271-630C6DFAD4D1")
-      @vm = FactoryGirl.create(:vm_with_ref, :ext_management_system => @ems)
-    end
-
-    it "normal case" do
+  context '#remote_console_vmrc_acquire_ticket' do
+    it 'normal case' do
       EvmSpecHelper.create_guid_miq_server_zone
-      @ems.update_attributes(:ipaddress => "192.168.252.14", :hostname => "192.168.252.14")
-      @ems.authentications = [FactoryGirl.create(:authentication, :userid => "dev1", :password => "dev1pass", :authtype => "default")]
+      ems.update_attributes(:ipaddress => '192.168.252.14', :hostname => '192.168.252.14')
+      auth = FactoryGirl.create(:authentication,
+                                :userid   => 'dev1',
+                                :password => 'dev1pass',
+                                :authtype => 'default')
+      ems.authentications = [auth]
       ticket = VCR.use_cassette(described_class.name.underscore) do
-        @vm.remote_console_vmrc_acquire_ticket
+        vm.remote_console_vmrc_acquire_ticket
       end
       expect(ticket).to match(/^[0-9\-A-Z]{40}$/)
     end
 
-    it "with vm off" do
-      @vm.update_attribute(:raw_power_state, "poweredOff")
-      expect { @vm.remote_console_vmrc_acquire_ticket }.to raise_error MiqException::RemoteConsoleNotSupportedError
+    it 'with vm off' do
+      vm.update_attribute(:raw_power_state, 'poweredOff')
+      expect { vm.remote_console_vmrc_acquire_ticket }.to raise_error MiqException::RemoteConsoleNotSupportedError
     end
 
-    it "with vm with no ems" do
-      @vm.ext_management_system = nil
-      @vm.save!
-      expect { @vm.remote_console_vmrc_acquire_ticket }.to raise_error MiqException::RemoteConsoleNotSupportedError
-    end
-  end
-
-  context "#validate_remote_console_vmrc_support" do
-    before(:each) do
-      @ems = FactoryGirl.create(:ems_vmware, :hostname => "192.168.252.16", :ipaddress => "192.168.252.16", :api_version => "5.0", :uid_ems => "2E1C1E82-BD83-4E54-9271-630C6DFAD4D1")
-      @vm = FactoryGirl.create(:vm_with_ref, :ext_management_system => @ems)
-    end
-
-    it "normal case" do
-      expect(@vm.validate_remote_console_vmrc_support).to be_truthy
-    end
-
-    it "with vm with no ems" do
-      @vm.ext_management_system = nil
-      @vm.save!
-      expect { @vm.validate_remote_console_vmrc_support }.to raise_error MiqException::RemoteConsoleNotSupportedError
-    end
-
-    it "with vm off" do
-      @vm.update_attribute(:raw_power_state, "poweredOff")
-      expect { @vm.validate_remote_console_vmrc_support }.to raise_error MiqException::RemoteConsoleNotSupportedError
-    end
-
-    it "on VC 4.0" do
-      @ems.update_attribute(:api_version, "4.0")
-      expect { @vm.validate_remote_console_vmrc_support }.to raise_error MiqException::RemoteConsoleNotSupportedError
+    it 'with vm with no ems' do
+      vm.ext_management_system = nil
+      vm.save!
+      expect { vm.remote_console_vmrc_acquire_ticket }.to raise_error MiqException::RemoteConsoleNotSupportedError
     end
   end
 
-  context "#remote_console_vnc_acquire_ticket" do
-    before(:each) do
-      @ems  = FactoryGirl.create(:ems_vmware)
-      @host = FactoryGirl.create(:host_vmware, :ext_management_system => @ems, :hostname => "192.168.252.4", :ipaddress => "192.168.252.4", :next_available_vnc_port => 5901)
-      @vm   = FactoryGirl.create(:vm_with_ref, :ext_management_system => @ems, :host => @host)
+  context '#validate_remote_console_vmrc_support' do
+    it 'normal case' do
+      expect(vm.validate_remote_console_vmrc_support).to be_truthy
     end
 
-    it "will set the attributes on the VC side" do
-      vim_vm = double("MiqVimVm")
+    it 'with vm with no ems' do
+      vm.ext_management_system = nil
+      vm.save!
+      expect { vm.validate_remote_console_vmrc_support }.to raise_error MiqException::RemoteConsoleNotSupportedError
+    end
+
+    it 'with vm off' do
+      vm.update_attribute(:raw_power_state, 'poweredOff')
+      expect { vm.validate_remote_console_vmrc_support }.to raise_error MiqException::RemoteConsoleNotSupportedError
+    end
+
+    it 'on VC 4.0' do
+      ems.update_attribute(:api_version, '4.0')
+      expect { vm.validate_remote_console_vmrc_support }.to raise_error MiqException::RemoteConsoleNotSupportedError
+    end
+  end
+
+  context '#remote_console_vnc_acquire_ticket' do
+    let(:ems) { FactoryGirl.create(:ems_vmware) }
+    let(:host) do
+      FactoryGirl.create(:host_vmware,
+                         :ext_management_system   => ems,
+                         :hostname                => '192.168.252.4',
+                         :ipaddress               => '192.168.252.4',
+                         :next_available_vnc_port => 5901)
+    end
+    let(:vm) { FactoryGirl.create(:vm_with_ref, :ext_management_system => ems, :host => host) }
+
+    it 'will set the attributes on the VC side' do
+      vim_vm = double('MiqVimVm')
       expect(vim_vm).to receive(:setRemoteDisplayVncAttributes) do |args|
         expect(args[:enabled]).to be_truthy
         expect(args[:port]).to eq(5901)
         expect(args[:password]).to match(%r{^[A-Za-z0-9+/]{8}$})
       end
-      allow(@vm).to receive(:with_provider_object).and_yield(vim_vm)
+      allow(vm).to receive(:with_provider_object).and_yield(vim_vm)
 
-      @vm.remote_console_vnc_acquire_ticket
+      vm.remote_console_vnc_acquire_ticket
     end
 
-    it "without a proxy miq_server" do
-      expect(@vm).to receive(:with_provider_object)
+    it 'without a proxy miq_server' do
+      expect(vm).to receive(:with_provider_object)
 
-      password, host_address, host_port, proxy_address, proxy_port = @vm.remote_console_vnc_acquire_ticket
+      password, host_address, host_port, proxy_address, proxy_port = vm.remote_console_vnc_acquire_ticket
 
       expect(password).to match(%r{^[A-Za-z0-9+/]{8}$})
-      expect(host_address).to eq("192.168.252.4")
+      expect(host_address).to eq('192.168.252.4')
       expect(host_port).to eq(5901)
       expect(proxy_address).to be_nil
       expect(proxy_port).to    be_nil
     end
 
-    context "with a proxy miq_server" do
-      it "with no proxy configured" do
-        server = double("MiqServer")
+    context 'with a proxy miq_server' do
+      it 'with no proxy configured' do
+        server = double('MiqServer')
         allow(server).to receive_message_chain(:get_config, :config => {:server => {:vnc_proxy_address => nil, :vnc_proxy_port => nil}})
-        expect(@vm).to receive(:with_provider_object)
+        expect(vm).to receive(:with_provider_object)
 
-        password, host_address, host_port, proxy_address, proxy_port = @vm.remote_console_vnc_acquire_ticket(server)
+        password, host_address, host_port, proxy_address, proxy_port = vm.remote_console_vnc_acquire_ticket(server)
 
         expect(password).to match(%r{^[A-Za-z0-9+/]{8}$})
-        expect(host_address).to eq("192.168.252.4")
+        expect(host_address).to eq('192.168.252.4')
         expect(host_port).to eq(5901)
         expect(proxy_address).to be_nil
         expect(proxy_port).to    be_nil
       end
 
-      it "with a proxy configured" do
-        server = double("MiqServer")
-        allow(server).to receive_message_chain(:get_config, :config => {:server => {:vnc_proxy_address => "1.2.3.4", :vnc_proxy_port => "5800"}}) # NOTE: Ports are actually stored as a String in the configuration
-        expect(@vm).to receive(:with_provider_object)
+      it 'with a proxy configured' do
+        server = double('MiqServer')
+        allow(server).to receive_message_chain(:get_config, :config => {:server => {:vnc_proxy_address => '1.2.3.4', :vnc_proxy_port => '5800'}}) # NOTE: Ports are actually stored as a String in the configuration
+        expect(vm).to receive(:with_provider_object)
 
-        password, host_address, host_port, proxy_address, proxy_port = @vm.remote_console_vnc_acquire_ticket(server)
+        password, host_address, host_port, proxy_address, proxy_port = vm.remote_console_vnc_acquire_ticket(server)
 
         expect(password).to match(%r{^[A-Za-z0-9+/]{8}$})
-        expect(host_address).to eq(@host.guid)
+        expect(host_address).to eq(host.guid)
         expect(host_port).to eq(5901)
-        expect(proxy_address).to eq("1.2.3.4")
+        expect(proxy_address).to eq('1.2.3.4')
         expect(proxy_port).to eq(5800)
       end
     end
 
-    it "will reclaim the port number from old VMs" do
+    it 'will reclaim the port number from old VMs' do
       allow_any_instance_of(ManageIQ::Providers::Vmware::InfraManager::Vm).to receive(:with_provider_object)
-      vm_old = FactoryGirl.create(:vm_with_ref, :host => @host, :vnc_port => 5901)
+      vm_old = FactoryGirl.create(:vm_with_ref, :host => host, :vnc_port => 5901)
 
-      @vm.remote_console_vnc_acquire_ticket
+      vm.remote_console_vnc_acquire_ticket
 
       vm_old.reload
       expect(vm_old.vnc_port).to be_nil
