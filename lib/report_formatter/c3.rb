c7b4aec29372f68fbc7c6fc3edfb759b7ed1346c
Martin Povolny <mpovolny@redhat.com>
2016-04-05 15:08:30 +0200

    C3: use number for series id.

--- lib/report_formatter/c3.rb
+++ lib/report_formatter/c3.rb
@@ -21,17 +21,16 @@ module ReportFormatter
     def add_series(label, data)
       @counter ||= 0
       @counter += 1
-      series_id = label + '_' + @counter.to_s
 
       if chart_is_2d?
-        mri.chart[:data][:columns] << [series_id, *data.map { |a| a[:value] }]
-        mri.chart[:data][:names][series_id] = label
+        mri.chart[:data][:columns] << [@counter, *data.map { |a| a[:value] }]
+        mri.chart[:data][:names][@counter] = label
       else
         mri.chart[:data][:columns] = data.collect { |a| [a[:tooltip], a[:value]] }
       end
 
       if chart_is_stacked?
-        mri.chart[:data][:groups][0] << series_id
+        mri.chart[:data][:groups][0] << @counter
       end
     end
 
