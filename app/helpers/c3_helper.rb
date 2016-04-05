a08ebfc7605b53ee74a86fcbfca7bd8d9828457c
Martin Povolny <mpovolny@redhat.com>
2016-04-05 15:13:55 +0200

    C3: replace broken sample chart with empty div.

--- app/helpers/c3_helper.rb
+++ app/helpers/c3_helper.rb
@@ -1,19 +1,4 @@
 module C3Helper
-  def c3chart_sample
-    content_tag(:div, '', :id => 'chart') +
-      javascript_tag(<<-EOJ)
-c3.generate({
-  bindto: '#chart',
-  data: {
-    columns: [
-      ['data1', 30, 200, 100, 400, 150, 250],
-      ['data2', 50, 20, 10, 40, 15, 25]
-    ]
-  }
-});
-EOJ
-  end
-
   def c3chart_remote(url, opts = {})
     chart_id = opts[:id] || ('chart' + rand(10**8).to_s)
 
