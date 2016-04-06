7cace4401b119550f34ead0778037e41358c613a
Erez Freiberger <efreiber@redhat.com>
2016-04-06 14:49:48 +0300

    small bugfix in save_inventory_container.rb

--- app/models/ems_refresh/save_inventory_container.rb
+++ app/models/ems_refresh/save_inventory_container.rb
@@ -332,7 +332,7 @@ module EmsRefresh::SaveInventoryContainer
     # the image, or stuck in Pending, or unable to fetch the image). Passing nil to
     # save_inventory_single is used to delete any pre-existing entity in containers,
     hash[:container_image_id] = hash[:container_image][:id] unless hash.nil?
-    hash[:ems_id] = container_definition[:ems_id]
+    hash[:ems_id] = container_definition[:ems_id] unless hash.nil?
     save_inventory_single(:container, container_definition, hash, [], :container_image, true)
   end
 
