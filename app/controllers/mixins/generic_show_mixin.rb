a5baaa81f3fe3902117f66d26ac6fee0097bee6d
Ladislav Smola <lsmola@redhat.com>
2016-04-12 16:23:32 +0200

    Generate controllers nested methods
    Generate controllers nested methods, when override is not
    specified

--- app/controllers/mixins/generic_show_mixin.rb
+++ app/controllers/mixins/generic_show_mixin.rb
@@ -20,7 +20,7 @@ module Mixins
         @showtype = "main"
         set_summary_pdf_data if ["download_pdf", "summary_only"].include?(@display)
       when *self.class.display_methods
-        nested_list_call(@display)
+        display_nested_list(@display)
       end
 
       # Came in from outside show_list partial
@@ -31,28 +31,38 @@ module Mixins
 
     def nested_list_method(display)
       methods = self.class.display_methods
-      # Converting to hash so brakeman doesn't domplain about using params directly
+      # Converting to hash so brakeman doesn't complain about using params directly
       methods.zip(methods).to_h[display]
     end
 
+    def nested_list_method_name(display)
+      "display_#{nested_list_method(display)}"
+    end
+
     def nested_list_call(display)
-      public_send("display_#{nested_list_method(display)}")
+      public_send(nested_list_method_name(display))
+    end
+
+    def display_nested_list(display)
+      respond_to?(nested_list_method_name(display).to_sym) ? nested_list_call(display) : display_nested_generic(display)
+    end
+
+    def display_nested_generic(display)
+      nested_list(display, display.camelize.singularize.constantize)
     end
 
     def display_instances
       nested_list("vm_cloud", ManageIQ::Providers::CloudManager::Vm)
     end
 
-    def display_cloud_subnets
-      nested_list("cloud_subnet", CloudSubnet)
-    end
-
-    def display_network_routers
-      nested_list("network_router", NetworkRouter)
+    def display_images
+      nested_list("template_cloud", ManageIQ::Providers::CloudManager::Template)
     end
 
     def nested_list(table_name, model)
       title = ui_lookup(:tables => table_name)
+      drop_breadcrumb(:name =>  _("%{name} (Summary)") % {:name => @record.name},
+                      :url  => "/#{self.class.table_name}/show/#{@record.id}")
       drop_breadcrumb(:name => _("%{name} (All %{title})") % {:name => @record.name, :title => title},
                       :url  => "/#{self.class.table_name}/show/#{@record.id}?display=#{@display}")
       @view, @pages = get_view(model, :parent => @record) # Get the records (into a view) and the paginator
