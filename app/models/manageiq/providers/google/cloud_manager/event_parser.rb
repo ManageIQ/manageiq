11753746c26ba67a3b31f4bdfc4222753188cdbf
Joe Selman <jsselman@google.com>
2016-04-13 01:05:28 +0000

    Add event catcher support for Google provider
    This handles the following four events:
    * VM Creation
    * VM Deletion
    * VM Start
    * VM Stop
    
    Closes #7902

--- app/models/manageiq/providers/google/cloud_manager/event_parser.rb
+++ app/models/manageiq/providers/google/cloud_manager/event_parser.rb
@@ -1,14 +1,19 @@
 module ManageIQ::Providers::Google::CloudManager::EventParser
+  extend ManageIQ::Providers::Google::EventCatcherMixin
+
   def self.event_to_hash(event, ems_id)
     log_header = "ems_id: [#{ems_id}] " unless ems_id.nil?
 
-    _log.debug { "#{log_header}event: [#{event["eventType"]}]" }
+    event_type = parse_event_type(event)
+    timestamp = event.fetch_path('metadata', 'timestamp')
+
+    _log.debug { "#{log_header}event: [#{event_type}]" }
 
     event_hash = {
-      :event_type => event["eventType"],
+      :event_type => event_type,
       :source     => "GOOGLE",
-      :message    => event["configurationItemDiff"],
-      :timestamp  => event["notificationCreationTime"],
+      :message    => event_type,
+      :timestamp  => timestamp,
       :full_data  => event,
       :ems_id     => ems_id
     }
