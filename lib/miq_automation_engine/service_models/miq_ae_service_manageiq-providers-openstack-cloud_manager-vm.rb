a1f919f0949840f0356e946664df76972e34a444
Marek Aufart <maufart@redhat.com>
2016-04-13 09:26:37 +0200

    Expose openstack instance resize methods to automate

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-vm.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-cloud_manager-vm.rb
@@ -2,6 +2,14 @@ module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Openstack_CloudManager_Vm < MiqAeServiceVmCloud
     expose :cloud_networks, :association => true
 
+    expose :resize,         :override_return => nil
+    expose :resize_confirm, :override_return => nil
+    expose :resize_revert,  :override_return => nil
+
+    expose :validate_resize
+    expose :validate_resize_confirm
+    expose :validate_resize_revert
+
     def attach_volume(volume_id, device = nil, options = {})
       sync_or_async_ems_operation(options[:sync], "attach_volume", [volume_id, device])
     end
