69aefb0ed3a26ea458b3a0a3a74a17581b5e9b2a
Ladislav Smola <lsmola@redhat.com>
2016-04-13 10:27:45 +0200

    Add NetworkTopology controller
    Add NetworkTopology controller

---
+++ app/controllers/network_topology_controller.rb
@@ -0,0 +1,38 @@
+class NetworkTopologyController < ApplicationController
+  before_action :check_privileges
+  before_action :get_session_data
+  after_action :cleanup_action
+  after_action :set_session_data
+
+  def show
+    # When navigated here without id, it means this is a general view for all providers (not for a specific provider)
+    # all previous navigation should not be displayed in breadcrumbs as the user could arrive from
+    # any other page in the application.
+    if params[:id].nil?
+      @breadcrumbs.clear
+    end
+    drop_breadcrumb(:name => _('Topology'), :url => '')
+  end
+
+  def index
+    redirect_to :action => 'show'
+  end
+
+  def data
+    render :json => {:data => generate_topology(params[:id])}
+  end
+
+  private
+
+  def get_session_data
+    @layout = "network_topology"
+  end
+
+  def set_session_data
+    session[:layout] = @layout
+  end
+
+  def generate_topology(provider_id)
+    NetworkTopologyService.new(provider_id).build_topology
+  end
+end
