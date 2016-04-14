0443ea810d19d8de90f1239e2bfd8f1198c4f5bc
Robin Knaur <rknaur@redhat.com>
2016-04-14 10:41:55 +0200

    Set higher priority to C3 charts

--- lib/charting/c3_charting.rb
+++ lib/charting/c3_charting.rb
@@ -6,7 +6,7 @@ class C3Charting < Charting
 
   # for Charting.detect_available_plugin
   def self.priority
-    0 # TODO: set to value > JqplotCharting.priority to take effect
+    2 # TODO: set to value > JqplotCharting.priority to take effect
   end
 
   # backend identifier
