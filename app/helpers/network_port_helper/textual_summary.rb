ee1524fda770b3d2bdc16497c7f7effa2ae15ec0
Ladislav Smola <lsmola@redhat.com>
2016-04-15 11:01:26 +0200

    Clean up network_port textual_summary and add missing cross-links
    Clean up network_port textual_summary and add missing cross-links

--- app/helpers/network_port_helper/textual_summary.rb
+++ app/helpers/network_port_helper/textual_summary.rb
@@ -8,7 +8,7 @@ module NetworkPortHelper::TextualSummary
   end
 
   def textual_group_relationships
-    %i(parent_ems_cloud ems_network instance cloud_tenant cloud_subnets)
+    %i(parent_ems_cloud ems_network cloud_tenant instance cloud_subnets floating_ips)
   end
 
   def textual_group_tags
@@ -35,23 +35,27 @@ module NetworkPortHelper::TextualSummary
   end
 
   def textual_parent_ems_cloud
-    textual_link(@record.ext_management_system.try(:parent_manager))
+    @record.ext_management_system.try(:parent_manager)
   end
 
   def textual_ems_network
-    textual_link(@record.ext_management_system)
+    @record.ext_management_system
   end
 
   def textual_instance
     # TODO(lsmola) Textual link is messed up here, it infers feature as vm_or_template_show, we need to fix that
-    textual_link(@record.device)
+    @record.device
   end
 
   def textual_cloud_tenant
-    textual_link(@record.cloud_tenant)
+    @record.cloud_tenant
   end
 
   def textual_cloud_subnets
-    textual_link(@record.cloud_subnets)
+    @record.cloud_subnets
+  end
+
+  def textual_floating_ips
+    @record.floating_ips
   end
 end
