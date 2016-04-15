e8fd3464daa5600782a13abbe64b221de0bc0201
Ladislav Smola <lsmola@redhat.com>
2016-04-15 11:03:34 +0200

    Clean up floating_ip textual_summary and add missing cross-links
    Clean up floating_ip textual_summary and add missing cross-links

--- app/helpers/floating_ip_helper/textual_summary.rb
+++ app/helpers/floating_ip_helper/textual_summary.rb
@@ -8,7 +8,7 @@ module FloatingIpHelper::TextualSummary
   end
 
   def textual_group_relationships
-    %i(parent_ems_cloud ems_network cloud_tenant instance)
+    %i(parent_ems_cloud ems_network cloud_tenant instance network_port)
   end
 
   def textual_group_tags
@@ -36,19 +36,23 @@ module FloatingIpHelper::TextualSummary
   end
 
   def textual_parent_ems_cloud
-    textual_link(@record.ext_management_system.try(:parent_manager))
+    @record.ext_management_system.try(:parent_manager)
   end
 
   def textual_ems_network
-    textual_link(@record.ext_management_system)
+    @record.ext_management_system
   end
 
   def textual_instance
     # TODO(lsmola) Textual link is messed up here, it infers feature as vm_or_template_show, we need to fix that
-    textual_link(@record.vm)
+    @record.vm
   end
 
   def textual_cloud_tenant
-    textual_link(@record.cloud_tenant)
+    @record.cloud_tenant
+  end
+
+  def textual_network_port
+    @record.network_port
   end
 end
