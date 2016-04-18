b5090d095357708192870e5716f2c7f229e667a8
Laura Galis <lgalis@redhat.com>
2016-04-18 11:45:53 -0400

    Update datastore clusters features and spec

--- spec/models/miq_product_feature_spec.rb
+++ spec/models/miq_product_feature_spec.rb
@@ -2,7 +2,7 @@ require 'tmpdir'
 require 'pathname'
 
 describe MiqProductFeature do
-  let(:expected_feature_count) { 1014 }
+  let(:expected_feature_count) { 1016 }
 
   # - container_dashboard
   # - miq_report_widget_editor
