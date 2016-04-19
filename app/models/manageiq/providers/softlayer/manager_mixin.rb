d2f06cb976e791c5f3a227650333b6a3a04deb66
Tomas Coufal <tcoufal@redhat.com>
2016-04-19 19:06:51 +0200

    Added SoftLayer discovery process
    Discovery scenario for SoftLayer means we want to add a separate
    provider for each Region

--- app/models/manageiq/providers/softlayer/manager_mixin.rb
+++ app/models/manageiq/providers/softlayer/manager_mixin.rb
@@ -1,9 +1,7 @@
-module ManageIQ::Providers::SoftLayer::ManagerMixin
+module ManageIQ.ems::SoftLayer::ManagerMixin
   extend ActiveSupport::Concern
 
   def connect(options = {})
-    require 'fog/softlayer'
-
     raise MiqException::MiqHostError, "No credentials defined" if missing_credentials?(options[:auth_type])
 
     client_id = options[:user] || authentication_userid(options[:auth_type])
@@ -16,7 +14,7 @@ module ManageIQ::Providers::SoftLayer::ManagerMixin
     connect(options)
 
     # Hit the SoftLayer servers to make sure authentication has
-    # been procced
+    # been proceed
     connection.regions.all
   rescue Excon::Errors::Unauthorized => err
     raise MiqException::MiqInvalidCredentialsError, err.message
@@ -52,25 +50,70 @@ module ManageIQ::Providers::SoftLayer::ManagerMixin
 
     # Discovery
 
+    # Process each region, add it as a new provider and queue refresh
     def discover(client_id, client_key)
+      new_ems = []
+
+      all_ems = includes(:authentications)
+      all_ems_names = all_ems.index_by(&:name)
+
+      known_ems = all_ems.select { |e| e.authentication_userid == client_id }
+      known_ems_regions = known_ems.index_by(&:provider_region)
+
+      compute = raw_connect(client_id, client_key)
+      network = raw_connect(client_id, client_key, :service => "network")
+
+      network.datacenters.each do |region|
+        next if known_ems_regions.include?(region.name)
+        next if servers_in_region(compute, region.name).empty?
+
+        new_ems << create_discovered_region(region, client_id, client_key, all_ems_names)
+      end
+
+      EmsRefresh.queue_refresh(new_ems) unless new_ems.blank?
+
+      new_ems
     end
 
     def discover_queue(_client_id, _client_key)
       MiqQueue.put(
         :class_name  => name,
         :method_name => "discover_from_queue",
-        :args        => [clientid, MiqPassword.encrypt(clientkey), azure_tenant_id, subscription]
+        :args        => [client_id, MiqPassword.encrypt(client_key)]
       )
     end
 
-    def vms_in_region(compute, region)
+    def servers_in_region(compute, region)
       compute.servers.all.select { |vm| vm.datacenter == region }
     end
 
-    def discover_from_queue(client_id, client_key, azure_tenant_id, subscription)
+    def discover_from_queue(client_id, client_key)
+      discover(client_id, MiqPassword.decrypt(client_key))
     end
 
-    def create_discovered_region(region_name, client_id, client_key)
+    def create_discovered_region(region_name, client_id, client_key, all_ems_names)
+      # TODO: Why is there all this sting gymnastics? Is it really needed?
+      name = "SoftLayer-#{region_name}"
+      name = "SoftLayer-#{region_name} #{clientid}" if all_ems_names.key?(name)
+
+      while all_ems_names.key?(name)
+        name_counter = name_counter.to_i + 1 if defined?(name_counter)
+        name = "SoftLayer-#{region_name} #{name_counter}"
+      end
+
+      # TODO: Is the uid_ems really needed here?
+      new_ems = create!(
+        :name            => name,
+        :provider_region => region_name,
+        :zone            => Zone.default_zone
+      )
+      new_ems.update_authentication(
+        :default => {
+          :userid   => client_id,
+          :password => client_key
+        }
+      )
+      new_ems
     end
   end
 end
