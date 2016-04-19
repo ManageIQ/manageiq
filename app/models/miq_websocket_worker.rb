9928f58cbc73185c828670991356a7849dd6a386
Dávid Halász <dhalasz@redhat.com>
2016-04-19 08:31:14 +0200

    Renamed missing cfme to manageiq in miq_websocket_worker

--- app/models/miq_websocket_worker.rb
+++ app/models/miq_websocket_worker.rb
@@ -4,8 +4,8 @@ class MiqWebsocketWorker < MiqWorker
   self.required_roles = ['websocket']
 
   RACK_APPLICATION           = WebsocketServer
-  BALANCE_MEMBER_CONFIG_FILE = '/etc/httpd/conf.d/cfme-balancer-websocket.conf'
-  REDIRECTS_CONFIG_FILE      = '/etc/httpd/conf.d/cfme-redirects-websocket'
+  BALANCE_MEMBER_CONFIG_FILE = '/etc/httpd/conf.d/manageiq-balancer-websocket.conf'
+  REDIRECTS_CONFIG_FILE      = '/etc/httpd/conf.d/manageiq-redirects-websocket'
   STARTING_PORT              = 5000
   PROTOCOL                   = 'ws'
   LB_METHOD                  = :busy
