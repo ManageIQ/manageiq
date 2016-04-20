621e0d9d5848eac53c422d9e58eb7022c3e17d53
sunnyve <sunny.verma@alcatel-lucent.com>
2016-04-20 19:05:54 -0400

    removed the TrailingWhitespace

--- db/migrate/20160419214316_add_nuage_fields_to_cloud_subnets.rb
+++ db/migrate/20160419214316_add_nuage_fields_to_cloud_subnets.rb
@@ -4,6 +4,5 @@ class AddNuageFieldsToCloudSubnets < ActiveRecord::Migration[5.0]
     add_column :cloud_subnets, :domain_id, :bigint
     add_column :cloud_subnets, :zone_id, :bigint
     add_column :cloud_subnets, :owner_id, :bigint
-    
   end
 end
