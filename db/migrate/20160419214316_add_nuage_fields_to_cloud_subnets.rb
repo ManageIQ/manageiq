07d83ac2b0ec70c8815720ca931b8156a2aee724
sunnyve <sunny.verma@alcatel-lucent.com>
2016-04-20 18:44:33 -0400

    Added nuage fields to cloud subnets

---
+++ db/migrate/20160419214316_add_nuage_fields_to_cloud_subnets.rb
@@ -0,0 +1,9 @@
+class AddNuageFieldsToCloudSubnets < ActiveRecord::Migration[5.0]
+  def change
+    add_column :cloud_subnets, :enterprise_name, :string
+    add_column :cloud_subnets, :domain_id, :bigint
+    add_column :cloud_subnets, :zone_id, :bigint
+    add_column :cloud_subnets, :owner_id, :bigint
+    
+  end
+end
