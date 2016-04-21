257bd934331625acee85b1fa3d492d65e6cfe968
Sam Lucidi <slucidi@redhat.com>
2016-04-21 18:03:38 -0400

    Change RSA key input to file upload
    https://bugzilla.redhat.com/show_bug.cgi?id=1326585

---
+++ app/assets/javascripts/directives/require_file.js
@@ -0,0 +1,24 @@
+ManageIQ.angular.app.directive('requireFile', ['$parse', function ($parse) {
+  return {
+    require: 'ngModel',
+    link:function(scope, el, attrs, ngModel) {
+      var modelGetter = $parse(attrs['ngModel']);
+      var modelSetter = modelGetter.assign;
+
+      //change event is fired when file is selected
+      el.bind('change', function () {
+        scope.$apply(function () {
+          var reader = new FileReader();
+          reader.onload = function (e) {
+            scope.$apply(function () {
+              modelSetter(scope, e.target.result);
+            });
+          };
+          if (el[0].files[0]) {
+            reader.readAsText(el[0].files[0]);
+          }
+        });
+      });
+    }
+  }
+}]);
