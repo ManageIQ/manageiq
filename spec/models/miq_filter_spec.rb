869b70f407fa00cd7604c4f5f9be0bf3d8461955
Keenan Brock <kbrock@redhat.com>
2016-04-21 13:02:55 -0400

    update filter specs to only load what is needed

--- spec/models/miq_filter_spec.rb
+++ spec/models/miq_filter_spec.rb
@@ -1,52 +1,52 @@
 describe MiqFilter do
   let(:ems)             { FactoryGirl.create(:ems_vmware, :name => 'ems') }
-  let(:ems_folder_path) { "/belongsto/ExtManagementSystem|#{ems.name}" }
-  let(:datacenter)      { FactoryGirl.create(:ems_folder, :name => "Datacenters") }
-  let(:mtc)             { FactoryGirl.create(:datacenter, :name => "MTC") }
+  let(:datacenter)      { FactoryGirl.create(:ems_folder, :name => "Datacenters").tap { |dc| dc.parent = ems } }
+  let(:mtc)             { FactoryGirl.create(:datacenter, :name => "MTC").tap { |mtc| mtc.parent = datacenter } }
   let(:ems_folder_path) { "/belongsto/ExtManagementSystem|#{ems.name}" }
   let(:mtc_folder_path) { "#{ems_folder_path}/EmsFolder|#{datacenter.name}/EmsFolder|#{mtc.name}" }
-  let(:host_folder)     { FactoryGirl.create(:ems_folder, :name => "host") }
-  let(:host_1)          { FactoryGirl.create(:host_vmware, :name => "Host_1", :ext_management_system => ems) }
-  let(:host_2)          { FactoryGirl.create(:host_vmware, :name => "Host_2", :ext_management_system => ems) }
+  let(:host_folder)     { FactoryGirl.create(:ems_folder, :name => "host").tap { |hf| hf.parent = mtc } }
+  let(:host_1)          { FactoryGirl.create(:host_vmware, :name => "Host_1", :ext_management_system => ems).tap { |h| h.parent = host_folder } }
+  let(:host_2)          { FactoryGirl.create(:host_vmware, :name => "Host_2", :ext_management_system => ems).tap { |h| h.parent = host_folder } }
   let(:host_3)          { FactoryGirl.create(:host_vmware, :name => "Host_3") }
 
   let(:mtc_folder_path_with_host_folder) { "#{mtc_folder_path}/EmsFolder|host" }
   let(:mtc_folder_path_with_host_1)      { "#{mtc_folder_path_with_host_folder}/Host|#{host_1.name}" }
 
-  before do
-    datacenter.parent = ems
-    mtc.parent = datacenter
-    host_folder.parent = mtc
-
-    host_1.parent = host_folder
-    host_2.parent = host_folder
-  end
-
   describe ".apply_belongsto_filters" do
+    def apply_belongsto_filters(*args)
+      MiqFilter.apply_belongsto_filters(*args)
+    end
+
     it "returns all parent's Host objects when most-specialized filter is the Host object" do
       input_objects = [host_1, host_2]
-      results = MiqFilter.apply_belongsto_filters(input_objects, [mtc_folder_path_with_host_1])
+      results = apply_belongsto_filters(input_objects, [mtc_folder_path_with_host_1])
       expect(results).to match_array(input_objects)
     end
 
     it "returns all Host objects when most-specialized filter is Host folder" do
       input_objects = [host_1, host_2]
-      results = MiqFilter.apply_belongsto_filters(input_objects, [mtc_folder_path_with_host_folder])
+      results = apply_belongsto_filters(input_objects, [mtc_folder_path_with_host_folder])
       expect(results).to match_array(input_objects)
     end
   end
 
   describe ".belongsto2object_list" do
-    it "converts belongs_to path to objects" do
-      objects_for_folder_path = {}
-      objects_for_folder_path[mtc_folder_path] = [ems, datacenter, mtc]
-      objects_for_folder_path[mtc_folder_path_with_host_folder] = [ems, datacenter, mtc, host_folder]
-      objects_for_folder_path[mtc_folder_path_with_host_1] = [ems, datacenter, mtc, host_folder, host_1]
+    def belongsto2object_list(*args)
+      MiqFilter.belongsto2object_list(*args)
+    end
 
-      objects_for_folder_path.each do |folder, expected_objects|
-        results = MiqFilter.belongsto2object_list(folder)
-        expect(results).to match_array(expected_objects)
-      end
+    it "converts met_folder_path" do
+      expect(belongsto2object_list(mtc_folder_path)).to match_array([ems, datacenter, mtc])
+    end
+
+    it "converts met_folder with host_folder" do
+      host_folder_object_path = [ems, datacenter, mtc, host_folder]
+      expect(belongsto2object_list(mtc_folder_path_with_host_folder)).to match_array(host_folder_object_path)
+    end
+
+    it "converts met_folder with host_1" do
+      host_object_path = [ems, datacenter, mtc, host_folder, host_1]
+      expect(belongsto2object_list(mtc_folder_path_with_host_1)).to match_array(host_object_path)
     end
   end
 end
