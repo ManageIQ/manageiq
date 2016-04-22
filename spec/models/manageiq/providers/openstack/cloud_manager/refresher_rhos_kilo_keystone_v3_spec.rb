7ec7413a8ccc6796c02eb58386632a0940ac500e
Ladislav Smola <lsmola@redhat.com>
2016-04-22 11:44:08 +0200

    Fix and re-enable tests for keystone v3
    Fix and re-enable tests for keystone v3

--- spec/models/manageiq/providers/openstack/cloud_manager/refresher_rhos_kilo_keystone_v3_spec.rb
+++ spec/models/manageiq/providers/openstack/cloud_manager/refresher_rhos_kilo_keystone_v3_spec.rb
@@ -4,35 +4,35 @@ describe ManageIQ::Providers::Openstack::CloudManager::Refresher do
   include Openstack::RefreshSpecCommon
 
   before(:each) do
-    setup_ems('11.22.33.44', 'password_2WpEraURh', 5000, "cloud_admin", "v3")
+    setup_ems('11.22.33.44', 'password_2WpEraURh', 5000, "admin", "v3", "default")
     @environment = :kilo_keystone_v3
   end
 
   it "will perform a full refresh against RHOS #{@environment}" do
     2.times do # Run twice to verify that a second run with existing data does not change anything
-      # TODO(lsmola) disabled until https://github.com/fog/fog/issues/3812 is fixed
-      # with_cassette(@environment, @ems) do
-      #   EmsRefresh.refresh(@ems)
-      # end
+      with_cassette(@environment, @ems) do
+        EmsRefresh.refresh(@ems)
+        EmsRefresh.refresh(@ems.network_manager)
+      end
 
-      # assert_common
+      assert_common
     end
   end
 
   context "when configured with skips" do
     before(:each) do
-      stub_server_configuration(
+      stub_settings(
         :ems_refresh => {:openstack => {:inventory_ignore => [:cloud_volumes, :cloud_volume_snapshots]}}
       )
     end
 
     it "will not parse the ignored items" do
-      # TODO(lsmola) disabled until https://github.com/fog/fog/issues/3812 is fixed
-      # with_cassette(@environment, @ems) do
-      #   EmsRefresh.refresh(@ems)
-      # end
+      with_cassette(@environment, @ems) do
+        EmsRefresh.refresh(@ems)
+        EmsRefresh.refresh(@ems.network_manager)
+      end
 
-      # assert_with_skips
+      assert_with_skips
     end
   end
 end
