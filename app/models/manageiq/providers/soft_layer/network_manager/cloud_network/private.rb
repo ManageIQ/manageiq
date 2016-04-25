42d15784e52b7d22c18ec8fe307cb0cd312b34d7
Tomas Coufal <tcoufal@redhat.com>
2016-04-25 19:48:31 +0200

    Finalization of NetworkManager for SoftLayer
    added NetworkRouter, NetworkPort and linked together

--- app/models/manageiq/providers/soft_layer/network_manager/cloud_network/private.rb
+++ app/models/manageiq/providers/soft_layer/network_manager/cloud_network/private.rb
@@ -1,8 +1,2 @@
 class ManageIQ::Providers::SoftLayer::NetworkManager::CloudNetwork::Private < ManageIQ::Providers::SoftLayer::NetworkManager::CloudNetwork
-  include CloudNetworkPrivateMixin
-
-  has_many :cloud_subnets, :class_name  => "ManageIQ::Providers::SoftLayer::NetworkManager::CloudSubnet",
-                           :foreign_key => :cloud_network_id
-  has_many :network_routers, :through => :cloud_subnets
-  has_many :public_networks, :through => :cloud_subnets
 end
--- app/models/manageiq/providers/soft_layer/network_manager/refresh_parser.rb
+++ app/models/manageiq/providers/soft_layer/network_manager/refresh_parser.rb
@@ -18,11 +18,12 @@ class ManageIQ::Providers::SoftLayer::NetworkManager::RefreshParser
   end
 
   def ems_inv_to_hashes
-    log_header = "Collecting data for EMS HERE!!!: [#{@ems.name}] id: [#{@ems.id}]"
+    log_header = "Collecting data for EMS: [#{@ems.name}] id: [#{@ems.id}]"
 
     _log.info("#{log_header}...")
     get_cloud_networks
     get_network_ports
+    get_network_routers
     _log.info("#{log_header}...Complete")
 
     @data
@@ -41,7 +42,7 @@ class ManageIQ::Providers::SoftLayer::NetworkManager::RefreshParser
   end
 
   def get_cloud_networks
-    networks = @network.networks.all.select {|n| n.datacenter.name == @ems.provider_region}
+    networks = @network.networks.all.select { |n| n.datacenter.name == @ems.provider_region }
     process_collection(networks, :cloud_networks) { |cloud_network| parse_cloud_network(cloud_network) }
   end
 
@@ -51,32 +52,52 @@ class ManageIQ::Providers::SoftLayer::NetworkManager::RefreshParser
   end
 
   def get_network_ports
+    instances = @compute.servers.all.select { |s| s.datacenter == @ems.provider_region }
 
+    instances.each do |instance|
+      ports = instance.network_components
+      process_collection(ports, :network_ports) { |port| parse_network_port(instance.id, port) }
+    end
+  end
+
+  def get_network_routers
+    networks = @data.fetch_path(:cloud_networks)
+    networks.each do |network|
+      uid, new_result = parse_network_router(network[:network_router])
+
+      @data[:network_routers] ||= []
+      @data[:network_routers] << new_result
+      router = @data_index.store_path(:network_routers, uid, new_result)
+
+      network[:cloud_subnets].each { |subnet| subnet[:network_router] = router }
+      network.delete(:network_router)
+    end
   end
 
   def parse_cloud_network(cloud_network)
-    uid = cloud_network.id
+    uid = cloud_network.id.to_s
 
     type_suffix = "::#{cloud_network.network_space.capitalize}"
 
     cloud_subnets = get_cloud_subnets(cloud_network).collect do |raw_subnet|
-      @data_index.fetch_path(:cloud_subnets, raw_subnet.id)
+      @data_index.fetch_path(:cloud_subnets, raw_subnet.id.to_s)
     end
 
     new_result = {
-      :type          => self.class.cloud_network_type + type_suffix,
-      :ems_ref       => cloud_network.id,
-      :name          => cloud_network.name,
-      :status        => "active",
-      :cidr          => nil,
-      :enabled       => true,
-      :cloud_subnets => cloud_subnets,
+      :type           => self.class.cloud_network_type + type_suffix,
+      :ems_ref        => uid,
+      :name           => cloud_network.name,
+      :status         => "active",
+      :cidr           => nil,
+      :enabled        => true,
+      :cloud_subnets  => cloud_subnets,
+      :network_router => cloud_network.router
     }
     return uid, new_result
   end
 
   def parse_cloud_subnet(subnet)
-    uid = subnet.id
+    uid = subnet.id.to_s
 
     new_result = {
       :type              => self.class.cloud_subnet_type,
@@ -87,6 +108,38 @@ class ManageIQ::Providers::SoftLayer::NetworkManager::RefreshParser
       :network_protocol  => "ipv#{subnet.ip_version}",
       :gateway           => subnet.gateway_ip,
       :availability_zone => @data_index.fetch_path(:availability_zones, 'default'),
+    }
+    return uid, new_result
+  end
+
+  def parse_network_port(device_ref, network_port)
+    uid = network_port.id.to_s
+    name = network_port.name.blank? ? network_port.mac_address : "#{network_port.name}#{network_port.port}"
+    subnet_id = @network.ips.by_address(network_port.primary_ip_address).subnet_id.to_s
+
+    new_result = {
+      :type                       => self.class.network_port_type,
+      :name                       => name,
+      :ems_ref                    => uid,
+      :status                     => network_port.status.downcase,
+      :mac_address                => network_port.mac_address,
+      :device_ref                 => device_ref,
+      :device                     => parent_manager_fetch_path(:vms, device_ref),
+      :fixed_ips                  => network_port.primary_ip_address,
+      :cloud_subnet_network_ports => [{
+        :address      => network_port.primary_ip_address,
+        :cloud_subnet => @data_index.fetch_path(:cloud_subnets, subnet_id)
+      }]
+    }
+    return uid, new_result
+  end
+
+  def parse_network_router(network_router)
+    uid = network_router["id"].to_s
+    new_result = {
+      :type    => self.class.network_router_type,
+      :name    => network_router["hostname"],
+      :ems_ref => uid
     }
     return uid, new_result
   end
