e660fd3974a3d89b8b7731826b76e96faa0fbc2d
lpichler <lpichler@redhat.com>
2016-04-25 03:24:18 +0200

    Add spec for chargeback rate for actions
    spec for action
    - renders edit form
    - removing and adding tiers thru button "add" and "remove"
    - save chargeback rate form when user is changing tiers(adding, removing and changing values)

--- spec/controllers/chargeback_controller_spec.rb
+++ spec/controllers/chargeback_controller_spec.rb
@@ -148,4 +148,138 @@ describe ChargebackController do
       expect(assigns(:edit)[:cb_assign][:cis]).to eq({})
     end
   end
+
+  context "chargeback rate form actions" do
+    # indexing inputs regard to database
+    # html inputs have names(and ids) in form "start_0_0", "finish_0_2"
+    # it means "start_[num_of_rate_detail]_[num_of_tier]"
+
+    # for example we have in database:
+    # chargeback_rate => [
+    #                     chargeback_rate_details_0 => [
+    #                                                 chargeback_tiers_0  => index of tier : 0_0
+    #                                                 chargeback_tiers_1  => index of tier : 0_1
+    #                                                 chargeback_tiers_2  => index of tier : 0_2
+    #                                                ]
+    #                     chargeback_rate_details_1 => [
+    #                                                 chargeback_tiers_0  => index of tier : 1_0
+    #                                                 chargeback_tiers_1  => index of tier : 1_1
+    #                                                 chargeback_tiers_2  => index of tier : 1_2
+    #                                                ]
+    # ]
+    #
+
+    render_views
+
+    let(:chargeback_rate) { FactoryGirl.create(:chargeback_rate_with_details) }
+
+    # this index represent first rate detail( "Allocated Memory in MB") chargeback_rate
+    let(:index_to_rate_type) { "0" }
+
+    before do
+      EvmSpecHelper.local_miq_server
+      allow_any_instance_of(described_class).to receive(:center_toolbar_filename).and_return("chargeback_center_tb")
+      seed_session_trees('chargeback', :cb_rates_tree, "xx-Compute_cr-#{controller.to_cid(chargeback_rate.id)}")
+    end
+
+    def expect_input(body, selector, value)
+      expect(body).to have_selector("input[value='#{value}']##{selector}")
+    end
+
+    def expect_rendered_tiers(body, tiers, order_of_rate_detail = 0)
+      tiers.each_with_index do |tier, index|
+        expect_input(body, "start_#{order_of_rate_detail}_#{index}", tier[:start])
+        finish_tier_value = tier[:finish] == Float::INFINITY ? "" : tier[:finish]
+        expect_input(body, "finish_#{order_of_rate_detail}_#{index}", finish_tier_value)
+      end
+    end
+
+    def change_form_value(field, value)
+      post :cb_rate_form_field_changed, :params => {:id => chargeback_rate.id, field => value}
+    end
+
+    it "renders edit form with correct values" do
+      post :x_button, :params => {:pressed => "chargeback_rates_edit", :id => chargeback_rate.id}
+      response_body = response.body.delete('\\')
+      expect(response).to render_template(:partial => 'chargeback/_cb_rate_edit')
+      expect(response).to render_template(:partial => 'chargeback/_cb_rate_edit_table')
+
+      expect_input(response_body, "description", "foo")
+
+      expect_rendered_tiers(response_body, [{:start => "0.0", :finish => "20.0"},
+                                            {:start => "20.0", :finish => "40.0"},
+                                            {:start => "40.0", :finish => Float::INFINITY}])
+
+      expect_rendered_tiers(response_body, [{:start => "0.0", :finish => Float::INFINITY}], 1)
+    end
+
+    it "removes requested tier line from edit from" do
+      post :x_button, :params => {:pressed => "chargeback_rates_edit", :id => chargeback_rate.id}
+      post :cb_tier_remove, :params => {:button => "remove", :index => "0-1"}
+
+      response_body = response.body.delete('\\').gsub('u003e', ">").gsub('u003c', "<")
+
+      expect(response).to render_template(:partial => 'chargeback/_cb_rate_edit_table')
+
+      expect_rendered_tiers(response_body, [{:start => "0.0", :finish => "20.0"},
+                                            {:start => "40.0", :finish => Float::INFINITY}])
+
+      expect_rendered_tiers(response_body, [{:start => "0.0", :finish => Float::INFINITY}], 1)
+    end
+
+    it "removes requested tier line and add 2 new tiers line from edit from" do
+      count_of_tiers = chargeback_rate.chargeback_rate_details[index_to_rate_type.to_i].chargeback_tiers.count
+      post :x_button, :params => {:pressed => "chargeback_rates_edit", :id => chargeback_rate.id}
+      post :cb_tier_remove, :params => {:button => "remove", :index => "#{index_to_rate_type}-1"}
+      post :cb_tier_add, :params => {:button => "add", :detail_index => index_to_rate_type}
+      post :cb_tier_add, :params => {:button => "add", :detail_index => index_to_rate_type}
+
+      response_body = response.body.delete('\\').gsub('u003e', ">").gsub('u003c', "<")
+
+      count_of_last_tier = count_of_tiers - 1 + 2 # one tier removed, two tiers added
+      selector = "#{index_to_rate_type}_#{count_of_last_tier - 1}"
+      expect_input(response_body, "start_#{selector}", "")
+      expect_input(response_body, "finish_#{selector}", "")
+    end
+
+    it "saves edited chargeback rate" do
+      post :x_button, :params => {:pressed => "chargeback_rates_edit", :id => chargeback_rate.id}
+
+      # remove second tier for rate detail; (values  {:start => "20.0", :finish => "40.0"})
+      post :cb_tier_remove, :params => {:button => "remove", :index => "#{index_to_rate_type}-1"}
+
+      # add new tier, new position is index_to_rate_type-1
+      post :cb_tier_add, :params => {:button => "add", :detail_index => index_to_rate_type}
+
+      # add new tier at, new position is index_to_rate_type-2
+      post :cb_tier_add, :params => {:button => "add", :detail_index => index_to_rate_type}
+
+      # after these actions we have for rate detail values:
+      # 0-0 :start => 0.0, :finish => 20.0
+      # 0-1 :start => 40.0, :finish => Infinity
+      # 0-2 :start => Infinity, :finish => Infinity
+      # 0-3 :start => Infinity, :finish => Infinity
+
+      # add values to newly added tiers to be valid
+      change_form_value(:start_0_1, "20.0")
+      change_form_value(:finish_0_1, "50.0")
+      change_form_value(:start_0_2, "50.0")
+      change_form_value(:finish_0_2, "70.0")
+      change_form_value(:start_0_3, "70.0")
+
+      # so after updating form values we have tiers with valid values
+      # 0-0 :start => 0.0, :finish => 30.0
+      # 0-1 :start => 30.0, :finish => 50.0
+      # 0-2 :start => 50.0, :finish => 70.0
+      # 0-3 :start => 70.0, :finish => Infinity
+
+      post :cb_rate_edit, :params => {:button => "save", :id => chargeback_rate.id}
+
+      rate_detail = ChargebackRate.find(chargeback_rate.id).chargeback_rate_details[index_to_rate_type.to_i]
+      expect(rate_detail.chargeback_tiers[0]).to have_attributes(:start => 0.0, :finish => 20.0)
+      expect(rate_detail.chargeback_tiers[1]).to have_attributes(:start => 20.0, :finish => 50.0)
+      expect(rate_detail.chargeback_tiers[2]).to have_attributes(:start => 50.0, :finish => 70.0)
+      expect(rate_detail.chargeback_tiers[3]).to have_attributes(:start => 70.0, :finish => Float::INFINITY)
+    end
+  end
 end
--- spec/factories/chargeback_rate.rb
+++ spec/factories/chargeback_rate.rb
@@ -2,5 +2,13 @@ FactoryGirl.define do
   factory :chargeback_rate do
     guid        { MiqUUID.new_guid }
     description 'foo'
+    rate_type 'Compute'
+  end
+
+  factory :chargeback_rate_with_details, :parent => :chargeback_rate do
+    after(:create) do |chargeback_rate|
+      chargeback_rate.chargeback_rate_details << FactoryGirl.create(:chargeback_rate_detail_memory_used_with_tiers)
+      chargeback_rate.chargeback_rate_details << FactoryGirl.create(:chargeback_rate_detail_memory_allocated_with_tiers)
+    end
   end
 end
