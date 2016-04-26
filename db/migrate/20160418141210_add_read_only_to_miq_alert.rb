3fd592abf17ee387eac4a3f2b1ca9d6bd63e42b9
lpichler <lpichler@redhat.com>
2016-04-26 11:23:34 +0200

    Add stub for model MiqAlert

--- db/migrate/20160418141210_add_read_only_to_miq_alert.rb
+++ db/migrate/20160418141210_add_read_only_to_miq_alert.rb
@@ -1,6 +1,9 @@
 class AddReadOnlyToMiqAlert < ActiveRecord::Migration[5.0]
+  class MiqAlert < ActiveRecord::Base; end
+
   def up
     add_column :miq_alerts, :read_only, :boolean
+
     say_with_time('Add read only parameter to MiqAlert with true for OOTB alerts') do
       guids = YAML.load_file(Rails.root.join("db/fixtures/miq_alerts.yml")).map { |h| h["guid"] || h[:guid] }
       MiqAlert.where(:guid => guids).update(:read_only => true)
