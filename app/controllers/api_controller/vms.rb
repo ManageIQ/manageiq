d10c168f5bab1f42c059af755efd76d60f4e9d6a
Martin Hradil <mhradil@redhat.com>
2016-04-28 08:20:25 +0000

    API - vms - use decorator_selection_for in vm_query_resource
    this allows us to add ?decorators=vms.decorator_name to the query and results in a decorator_name field being added to each vm, with the result of a particular decorator

--- app/controllers/api_controller/vms.rb
+++ app/controllers/api_controller/vms.rb
@@ -1,12 +1,29 @@
 class ApiController
   module Vms
     def vms_query_resource(object)
+      vms = object.try(:vms) || []
+
       vm_attrs = attribute_selection_for("vms")
-      vm_attrs.blank? ? object.try(:vms) : Array(object.try(:vms)).collect do |vm|
-        add_hash = vm_attrs.each_with_object({}) do |attr, hash|
+      vm_decorators = decorator_selection_for("vms")
+
+      return vms if vm_attrs.blank? && vm_decorators.blank?
+
+      vms.collect do |vm|
+        decorated = vm.decorator_class? ? vm.decorate : nil
+
+        attributes_hash = vm_attrs.each_with_object({}) do |attr, hash|
           hash[attr] = vm.public_send(attr.to_sym) if vm.respond_to?(attr.to_sym)
         end.compact
-        vm.as_json.merge(add_hash)
+
+        decorators_hash = vm_decorators.each_with_object({}) do |name, hash|
+          hash[name] = vm.decorate.public_send(name.to_sym) if decorated.respond_to?(name.to_sym)
+        end.compact unless decorated.nil?
+
+        conflictless_hash = attributes_hash.merge(decorators_hash || {}) do |key, _, _|
+          raise BadRequestError, "Requested both an attribute and a decorator of the same name: #{key}"
+        end
+
+        vm.as_json.merge(conflictless_hash)
       end
     end
 
