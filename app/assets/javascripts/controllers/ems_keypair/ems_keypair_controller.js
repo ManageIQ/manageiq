93cd9fe760476a7d27eca279311a22447df4db8d
Sam Lucidi <slucidi@redhat.com>
2016-04-28 19:50:15 -0400

    Reorganize emsKeypairController to minimize "scope soup"

--- app/assets/javascripts/controllers/ems_keypair/ems_keypair_controller.js
+++ app/assets/javascripts/controllers/ems_keypair/ems_keypair_controller.js
@@ -1,63 +1,67 @@
-ManageIQ.angular.app.controller('emsKeypairController', ['$http', '$scope', 'miqService', function($http, $scope, miqService) {
-  var init = function() {
-    $scope.bChangeStoredPrivateKey = undefined;
-    $scope.bCancelPrivateKeyChange = undefined;
+var EmsKeypairController = function($scope) {
+  var vm = this;
+  $scope.$on('resetClicked', function() {
+    vm.resetClicked();
+  });
 
-    $scope.$on('resetClicked', function(e) {
-      $scope.resetClicked();
-    });
-
-    $scope.$on('setNewRecord', function(event, args) {
-      if(args != undefined) {
-        $scope.newRecord = args.newRecord;
-      }
-      else {
-        $scope.newRecord = true;
-      }
-    });
-
-    $scope.$on('setUserId', function(event, args) {
-      if(args != undefined) {
-        $scope.modelCopy[args.userIdName] = args.userIdValue;
-      }
-    });
-
-    if ($scope.formId == 'new') {
-      $scope.newRecord = true;
+  $scope.$on('setNewRecord', function(event, args) {
+    if(args != undefined) {
+      vm.newRecord = args.newRecord;
     }
     else {
-      $scope.newRecord = false;
-      $scope.bChangeStoredPrivateKey = false;
-      $scope.bCancelPrivateKeyChange = false;
+      vm.newRecord = true;
     }
-  };
+  });
 
-  $scope.changeStoredPrivateKey = function() {
-    $scope.bChangeStoredPrivateKey = true;
-    $scope.bCancelPrivateKeyChange = false;
-    $scope.emsCommonModel.ssh_keypair_password = '';
-  };
-
-  $scope.cancelPrivateKeyChange = function() {
-    if($scope.bChangeStoredPrivateKey) {
-      $scope.bCancelPrivateKeyChange = true;
-      $scope.bChangeStoredPrivateKey = false;
-      $scope.emsCommonModel.ssh_keypair_password = '';
+  $scope.$on('setUserId', function(event, args) {
+    if(args != undefined) {
+      $scope.modelCopy[args.userIdName] = args.userIdValue;
     }
-  };
+  });
 
-  $scope.showVerifyPrivateKey = function(userid) {
-    return $scope.newRecord || (!$scope.showChangePrivateKeyLinks(userid)) || $scope.bChangeStoredPrivateKey;
-  };
+};
 
-  $scope.showChangePrivateKeyLinks = function(userid) {
-    return !$scope.newRecord && $scope.modelCopy[userid] != '';
-  };
+EmsKeypairController.prototype.initialize = function(model, formId) {
+  var vm = this;
+  vm.model = model;
+  vm.modelCopy = angular.copy(model);
+  vm.formId = formId;
+  vm.changeKey = undefined;
 
-  $scope.resetClicked = function() {
-    $scope.newRecord = false;
-    $scope.cancelPrivateKeyChange();
-  };
+  if (vm.formId == 'new') {
+    vm.newRecord = true;
+  } else {
+    vm.newRecord = false;
+    vm.changeKey = false;
+  }
+};
 
-  init();
-}]);
+EmsKeypairController.prototype.changeStoredPrivateKey = function() {
+  this.changeKey = true;
+  this.model.ssh_keypair_password = '';
+};
+
+EmsKeypairController.prototype.cancelPrivateKeyChange = function() {
+  if (this.changeKey) {
+    this.changeKey = false;
+    this.model.ssh_keypair_password = '';
+  }
+};
+
+EmsKeypairController.prototype.showVerifyPrivateKey = function(userid) {
+  return (this.newRecord
+          || !this.showChangePrivateKeyLinks(userid)
+          || this.changeKey);
+};
+
+EmsKeypairController.prototype.showChangePrivateKeyLinks = function(userid) {
+  return !this.newRecord && this.modelCopy[userid] != '';
+};
+
+EmsKeypairController.prototype.resetClicked = function() {
+  this.newRecord = false;
+  this.cancelPrivateKeyChange();
+};
+
+EmsKeypairController.$inject = ["$scope"];
+ManageIQ.angular.app.controller('emsKeypairController', EmsKeypairController);
