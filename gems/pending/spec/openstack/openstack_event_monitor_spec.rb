22534b7a5486a84cf1d92c5d8d4fabfd125f1333
Ladislav Smola <lsmola@redhat.com>
2016-04-28 19:07:53 +0200

    Fix test picking correct EventMonitor
    Fix test picking correct EventMonitor, also deletes tests for
    caching. We are not caching anymore since we are picking
    event monitor based on setting stored in DB. Before
    the EventMonitor was picked by trying event monitors one by one
    that could took minutes if some of them timeouted, so cache was
    needed.

--- gems/pending/spec/openstack/openstack_event_monitor_spec.rb
+++ gems/pending/spec/openstack/openstack_event_monitor_spec.rb
@@ -7,7 +7,8 @@ describe OpenstackEventMonitor do
     @topics = {"nova" => "nova_topic", "glance" => "glance_topic"}
     @receiver_options = {:capacity => 1, :duration => 1}
     @options = @receiver_options.merge(:topics => @topics)
-    @rabbit_host = {:hostname => "rabbit_host", :username => "rabbit_user", :password => "rabbit_pass"}
+    @rabbit_host = {:hostname => "rabbit_host", :username => "rabbit_user", :password => "rabbit_pass",
+                    :events_monitor => :amqp}
     @bad_host = {:hostname => "bad_host", :username => "bad_user", :password => "bad_pass"}
   end
 
@@ -16,18 +17,6 @@ describe OpenstackEventMonitor do
     allow(OpenstackRabbitEventMonitor).to receive(:test_connection).with(opts).and_return(false)
 
     expect(OpenstackEventMonitor.new(opts).class).to eq OpenstackNullEventMonitor
-  end
-
-  it "caches multiple event monitors for different keys" do
-    rabbit_options = @options.merge(@rabbit_host)
-    allow(OpenstackRabbitEventMonitor).to receive(:test_connection).with(rabbit_options).and_return(true)
-    rabbit_instance = OpenstackEventMonitor.new(rabbit_options)
-    expect(rabbit_instance.class).to eq OpenstackRabbitEventMonitor
-
-    # additionally, we should be able to access the event_monitor instance
-    # directly from the parent event_monitor
-    instance = OpenstackEventMonitor.new(rabbit_options)
-    expect(instance).to eq rabbit_instance
   end
 
   it "orders the event monitor plugins correctly" do
