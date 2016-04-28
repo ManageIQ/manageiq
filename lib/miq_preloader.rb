040a105e3bdb99ebd6d43cc25770447091d8c5be
Keenan Brock <kbrock@redhat.com>
2016-04-28 17:33:29 -0400

    preload_and_scope is preload to produce a scope
    instead of loading all records into memory, produce a scope that will
    resolve to that value. This lets a developer string multiple ones
    together and only create 1 query to fetch a deep chain. (or append other
    where clauses for the query)

--- lib/miq_preloader.rb
+++ lib/miq_preloader.rb
@@ -13,4 +13,22 @@ module MiqPreloader
   def self.preload_and_map(records, association)
     Array.wrap(records).tap { |recs| MiqPreloader.preload(recs, association) }.flat_map(&association)
   end
+
+  # @param records [ActiveRecord::Base, Array<ActiveRecord::Base>, Object, Array<Object>]
+  # @param association [String] an association on records
+  def self.preload_and_scope(records, association_name)
+    records = Array.wrap(records) unless records.kind_of?(Enumerable)
+    active_record_klass = records.respond_to?(:klass) ? records.klass : records.first.class
+    association = active_record_klass.reflect_on_association(association_name)
+
+    return preload_and_map(records, association_name) unless association
+
+    target_klass = association.klass
+    if (inverse_association = association.inverse_of)
+      target_klass.where(inverse_association.name.to_sym => records).where(association.scope)
+    else # assume it is a belongs_to
+      join_key = association.join_keys(target_klass)
+      target_klass.where(join_key.key.to_sym => records.select(join_key.foreign_key.to_sym))
+    end
+  end
 end
--- spec/lib/miq_preloader_spec.rb
+++ spec/lib/miq_preloader_spec.rb
@@ -49,4 +49,67 @@ describe MiqPreloader do
       MiqPreloader.preload_and_map(*args)
     end
   end
+
+  describe ".preload_and_scope" do
+    it "preloads (object).has_many" do
+      ems = FactoryGirl.create(:ems_infra)
+      FactoryGirl.create_list(:vm, 2, :ext_management_system => ems)
+
+      vms = nil
+      expect { vms = preload_and_scope(ems, :vms) }.to match_query_limit_of(0)
+      expect { expect(vms.count).to eq(2) }.to match_query_limit_of(1)
+    end
+
+    it "preloads (object.all).has_many" do
+      FactoryGirl.create_list(:vm, 2, :ext_management_system => FactoryGirl.create(:ems_infra))
+      FactoryGirl.create(:template, :ext_management_system => FactoryGirl.create(:ems_infra))
+
+      vms = nil
+      expect { vms = preload_and_scope(ExtManagementSystem.all, :vms_and_templates) }.to match_query_limit_of(0)
+      expect { expect(vms.count).to eq(3) }.to match_query_limit_of(1)
+    end
+
+    it "respects scopes (object.all).has_many {with scope}" do
+      FactoryGirl.create_list(:vm, 2, :ext_management_system => FactoryGirl.create(:ems_infra))
+      FactoryGirl.create(:template, :ext_management_system => FactoryGirl.create(:ems_infra))
+
+      expect { expect(preload_and_scope(ExtManagementSystem.all, :vms).count).to eq(2) }.to match_query_limit_of(1)
+    end
+
+    it "preloads (object.all).has_many.belongs_to" do
+      ems = FactoryGirl.create(:ems_infra)
+      FactoryGirl.create_list(:vm, 2,
+                              :ext_management_system => ems,
+                              :host                  => FactoryGirl.create(:host, :ext_management_system => ems))
+      FactoryGirl.create(:vm, :ext_management_system => ems,
+                              :host                  => FactoryGirl.create(:host, :ext_management_system => ems))
+
+      hosts = nil
+      vms = nil
+      expect { vms = preload_and_scope(ExtManagementSystem.all, :vms_and_templates) }.to match_query_limit_of(0)
+      expect { hosts = preload_and_scope(vms, :host) }.to match_query_limit_of(0)
+      expect { expect(hosts.count).to eq(2) }.to match_query_limit_of(1)
+    end
+
+    it "preloads (object.all).belongs_to.has_many" do
+      ems = FactoryGirl.create(:ems_infra)
+      host = FactoryGirl.create(:host, :ext_management_system => ems)
+      FactoryGirl.create_list(:vm, 2,
+                              :ext_management_system => ems,
+                              :host                  => host)
+      host = FactoryGirl.create(:host, :ext_management_system => ems)
+      FactoryGirl.create(:vm, :ext_management_system => ems,
+                              :host                  => host)
+
+      emses = nil
+      vms = nil
+      expect { emses = preload_and_scope(Host.all, :ext_management_system) }.to match_query_limit_of(0)
+      expect { vms = preload_and_scope(emses, :vms) }.to match_query_limit_of(0)
+      expect { expect(vms.count).to eq(3) }.to match_query_limit_of(1)
+    end
+
+    def preload_and_scope(*args)
+      MiqPreloader.preload_and_scope(*args)
+    end
+  end
 end
