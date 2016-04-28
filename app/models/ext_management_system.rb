73b4a3f80dd694f96eddcd1544c88d0b1a6e1afa
Ladislav Smola <lsmola@redhat.com>
2016-04-28 14:22:50 +0200

    Mark for delete endpoints that were not selected in the form
    Mark for delete endpoints that were not selected in the form,
    by marking for delete, the fields are not deleted when we
    validate fields, but only when we submit the form.

--- app/models/ext_management_system.rb
+++ app/models/ext_management_system.rb
@@ -274,6 +274,16 @@ class ExtManagementSystem < ApplicationRecord
     options.each do |option|
       add_connection_configuration_by_role(option)
     end
+
+    drop_unused_connections(options)
+  end
+
+  def drop_unused_connections(options)
+    chosen_endpoints   = options.map { |x| x.fetch_path(:endpoint, :role).try(:to_sym)}.compact.uniq
+    existing_endpoints = endpoints.pluck(:role).map(&:to_sym)
+    # Delete endpoint that were not picked
+    endpoints_for_deletion = existing_endpoints - chosen_endpoints
+    endpoints.select { |x| endpoints_for_deletion.include?(x.role.to_sym) }.each(&:mark_for_destruction)
   end
 
   def connection_configurations
@@ -304,6 +314,7 @@ class ExtManagementSystem < ApplicationRecord
         options[:authentication][:role] ||= "default"
       end
     end
+
     build_connection(options)
   end
 
