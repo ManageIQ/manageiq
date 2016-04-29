86c5e804f88728399379a8bf079c99fc2c59be4b
Šimon Lukašík <isimluk@fedoraproject.org>
2016-04-29 14:01:51 +0200

    Refactor: Extract method: TimelineMessage.text

--- lib/report_formatter/timeline_message.rb
+++ lib/report_formatter/timeline_message.rb
@@ -3,50 +3,50 @@ module ReportFormatter
     include CompressedIds
 
     def vm_name
-      "<a href=\"/vm/show/#{to_cid(@event.vm_or_template_id)}\">#{@text}</a>" if @event.vm_or_template_id
+      "<a href=\"/vm/show/#{to_cid(@event.vm_or_template_id)}\">#{text}</a>" if @event.vm_or_template_id
     end
 
     def src_vm_name
-      "<a href=\"/vm/show/#{to_cid(@event.src_vm_or_template.id)}\">#{@text}</a>" if @event.src_vm_or_template
+      "<a href=\"/vm/show/#{to_cid(@event.src_vm_or_template.id)}\">#{text}</a>" if @event.src_vm_or_template
     end
 
     def dest_vm_name
-      "<a href=\"/vm/show/#{to_cid(@event.dest_vm_or_template_id)}\">#{@text}</a>" if @event.dest_vm_or_template_id
+      "<a href=\"/vm/show/#{to_cid(@event.dest_vm_or_template_id)}\">#{text}</a>" if @event.dest_vm_or_template_id
     end
 
     def host_name
-      "<a href=\"/host/show/#{to_cid(@event.host_id)}\">#{@text}</a>" if @event.host_id
+      "<a href=\"/host/show/#{to_cid(@event.host_id)}\">#{text}</a>" if @event.host_id
     end
 
     def dest_host_name
-      "<a href=\"/host/show/#{to_cid(@event.dest_host_id)}\">#{@text}</a>" if @event.dest_host_id
+      "<a href=\"/host/show/#{to_cid(@event.dest_host_id)}\">#{text}</a>" if @event.dest_host_id
     end
 
     def ems_cluster_name
-      "<a href=\"/ems_cluster/show/#{to_cid(@event.ems_cluster_id)}\">#{@text}</a>" if @event.ems_cluster_id
+      "<a href=\"/ems_cluster/show/#{to_cid(@event.ems_cluster_id)}\">#{text}</a>" if @event.ems_cluster_id
     end
 
     def availability_zone_name
       if @event.availability_zone_id
-        "<a href=\"/availability_zone/show/#{to_cid(@event.availability_zone_id)}\">#{@text}</a>"
+        "<a href=\"/availability_zone/show/#{to_cid(@event.availability_zone_id)}\">#{text}</a>"
       end
     end
 
     def container_node_name
-      "<a href=\"/container_node/show/#{to_cid(@event.container_node_id)}\">#{@text}</a>" if @event.container_node_id
+      "<a href=\"/container_node/show/#{to_cid(@event.container_node_id)}\">#{text}</a>" if @event.container_node_id
     end
 
     def container_group_name
-      "<a href=\"/container_group/show/#{to_cid(@event.container_group_id)}\">#{@text}</a>" if @event.container_group_id
+      "<a href=\"/container_group/show/#{to_cid(@event.container_group_id)}\">#{text}</a>" if @event.container_group_id
     end
 
     def container_name
-      "<a href=\"/container/tree_select/?id=cnt-#{to_cid(@event.container_id)}\">#{@text}</a>" if @event.container_id
+      "<a href=\"/container/tree_select/?id=cnt-#{to_cid(@event.container_id)}\">#{text}</a>" if @event.container_id
     end
 
     def container_replicator_name
       if @event.container_replicator_id
-        "<a href=\"/container_replicator/show/#{to_cid(@event.container_replicator_id)}\">#{@text}</a>"
+        "<a href=\"/container_replicator/show/#{to_cid(@event.container_replicator_id)}\">#{text}</a>"
       end
     end
 
@@ -55,11 +55,11 @@ module ReportFormatter
         provider_id = @event.ext_management_system.id
         if @ems_cloud
           # restful route is used for cloud provider unlike infrastructure provider
-          "<a href=\"/ems_cloud/#{provider_id}\">#{@text}</a>"
+          "<a href=\"/ems_cloud/#{provider_id}\">#{text}</a>"
         elsif @ems_container
-          "<a href=\"/ems_container/#{to_cid(provider_id)}\">#{@text}</a>"
+          "<a href=\"/ems_container/#{to_cid(provider_id)}\">#{text}</a>"
         else
-          "<a href=\"/ems_infra/#{to_cid(provider_id)}\">#{@text}</a>"
+          "<a href=\"/ems_infra/#{to_cid(provider_id)}\">#{text}</a>"
         end
       end
     end
@@ -80,18 +80,26 @@ module ReportFormatter
     def set_parameters(column, row, event, flags, db)
       @event, @ems_cloud, @ems_container = event, flags[:ems_cloud], flags[:ems_container]
       @db   = db
-      @text = if row[column].kind_of?(Time) || TIMELINE_TIME_COLUMNS.include?(column)
-                format_timezone(Time.parse(row[column].to_s).utc, flags[:time_zone], "gtl")
-              else
-                row[column].to_s
-              end
+      @column = column
+      @row = row
+      @flags = flags
     end
 
     def message_html(column, row, event, flags, db)
       set_parameters(column, row, event, flags, db)
 
       field = column.tr('.', '_').to_sym
-      respond_to?(field) ? send(field).to_s : @text
+      respond_to?(field) ? send(field).to_s : text
+    end
+
+    private
+
+    def text
+      if @row[@column].kind_of?(Time) || TIMELINE_TIME_COLUMNS.include?(@column)
+        format_timezone(Time.parse(@row[@column].to_s).utc, @flags[:time_zone], "gtl")
+      else
+        @row[@column].to_s
+      end
     end
   end
 end
