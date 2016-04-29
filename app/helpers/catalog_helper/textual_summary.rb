5c2308ae398d90e6fe689712a6765d13357f1439
Zita Nemeckova <znemecko@redhat.com>
2016-04-29 08:42:17 +0200

    Specs added and rewrite code to look better

--- app/helpers/catalog_helper/textual_summary.rb
+++ app/helpers/catalog_helper/textual_summary.rb
@@ -1,24 +1,32 @@
 module CatalogHelper::TextualSummary
-  def textual_tags
-    label = _("%{name} Tags") % {:name => session[:customer_name]}
-    h = {:label => label}
-    if @record.tags.blank?
-      h[:image] = "smarttag"
-      h[:value] = _("No %{label} have been assigned") % {:label => label}
-    else
-      h[:value] = []
-      @record.tags.each do |tag|
-        values = tag.name.split('/')
-        p = h[:value].find { |x| x[:label] == values[2].humanize}
-        if p.present?
-          p[:value].push(values[3].humanize)
-        else
-          h[:value].push({:image => "smarttag", :label => values[2].humanize, :value => [values[3].humanize]})
-        end
+  def tags_from_record
+    tags = []
+    @record.tags.each do |tag|
+      values = tag.name.split('/')
+      p = tags.find { |x| x[:label] == values[2].humanize }
+      value = Classification.find_by(:tag_id => tag.id).description
+      if p.present?
+        p[:value].push(value)
+      else
+        name = Classification.find_by(:id => Classification.find_by(:tag_id => tag.id).parent_id).description
+        tags.push(:image => "smarttag", :label => name, :value => [value])
       end
     end
-    binding.pry
-    h
+    tags
+  end
+
+  def textual_tags
+    label = _("%{name} Tags") % {:name => session[:customer_name]}
+    tags = {:label => label}
+    if @record.tags.blank?
+      tags[:image] = "smarttag"
+      tags[:value] = _("No %{label} have been assigned") % {:label => label}
+    else
+      tags[:value] = tags_from_record
+      tags[:value].each { |value| value[:value].sort! }
+      tags[:value].sort_by!{ |x| x[:label] }
+    end
+    tags
   end
 
   def textual_group_smart_management
