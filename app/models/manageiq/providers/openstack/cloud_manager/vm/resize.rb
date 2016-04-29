edcdac67efa4d961db161f8aef259137bb28ee03
Marek Aufart <maufart@redhat.com>
2016-04-29 10:58:58 +0200

    Fix openstack cloud instance resize
    Issue #7899
    https://bugzilla.redhat.com/show_bug.cgi?id=1328083

--- app/models/manageiq/providers/openstack/cloud_manager/vm/resize.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/vm/resize.rb
@@ -4,10 +4,13 @@ module ManageIQ::Providers::Openstack::CloudManager::Vm::Resize
   end
 
   def raw_resize(new_flavor)
-    # TODO(maufart): check if a new flavor disk space is not smaller that the actual one?
     ext_management_system.with_provider_connection(compute_connection_options) do |service|
       service.resize_server(ems_ref, new_flavor.ems_ref)
     end
+    MiqQueue.put(:class_name  => self.class.name,
+                 :expires_on  => Time.now.utc + 4.hours,
+                 :instance_id => id,
+                 :method_name => "raw_resize_finish")
   rescue => err
     _log.error "vm=[#{name}], flavor=[#{new_flavor.name}], error: #{err}"
     raise MiqException::MiqOpenstackApiRequestError, err.to_s, err.backtrace
@@ -24,6 +27,11 @@ module ManageIQ::Providers::Openstack::CloudManager::Vm::Resize
   rescue => err
     _log.error "vm=[#{name}], error: #{err}"
     raise MiqException::MiqOpenstackApiRequestError, err.to_s, err.backtrace
+  end
+
+  def raw_resize_finish
+    raise MiqException::MiqQueueRetryLater.new(:deliver_on => Time.now.utc + 2.minutes) unless validate_resize_confirm
+    raw_resize_confirm
   end
 
   def validate_resize_revert
