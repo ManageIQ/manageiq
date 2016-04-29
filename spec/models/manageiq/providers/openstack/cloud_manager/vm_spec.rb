3eaf34baacb05f15eeda6dbb7230c3d5e30ea262
Marek Aufart <maufart@redhat.com>
2016-04-29 10:58:58 +0200

    Fix openstack cloud instance resize
    Issue #7899
    https://bugzilla.redhat.com/show_bug.cgi?id=1328083

--- spec/models/manageiq/providers/openstack/cloud_manager/vm_spec.rb
+++ spec/models/manageiq/providers/openstack/cloud_manager/vm_spec.rb
@@ -137,6 +137,7 @@ describe ManageIQ::Providers::Openstack::CloudManager::Vm do
       expect(vm.validate_resize).to be true
       expect(vm.validate_resize_confirm).to be false
       expect(service).to receive(:resize_server).with(vm.ems_ref, flavor.ems_ref)
+      expect(MiqQueue).to receive(:put)
       vm.resize(flavor)
     end
 
