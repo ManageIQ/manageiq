5df1c0a33f432adbfdece7cb6299cc6c0bd7de6d
Jirka Kremser <jkremser@redhat.com>
2016-05-02 18:24:29 +0200

    Adding datasource entities for the middleware provider + tests (mostly the model)

--- app/models/ems_refresh/save_inventory_middleware.rb
+++ app/models/ems_refresh/save_inventory_middleware.rb
@@ -2,7 +2,7 @@ module EmsRefresh::SaveInventoryMiddleware
   def save_ems_middleware_inventory(ems, hashes, target = nil)
     target = ems if target.nil?
 
-    child_keys = [:middleware_servers, :middleware_deployments]
+    child_keys = [:middleware_servers, :middleware_deployments, :middleware_datasources]
 
     # Save and link other subsections
     child_keys.each do |k|
@@ -45,5 +45,25 @@ module EmsRefresh::SaveInventoryMiddleware
     save_inventory_multi(ems.middleware_deployments, hashes, deletes, [:ems_ref], nil,
                          [:middleware_server])
     store_ids_for_new_records(ems.middleware_deployments, hashes, :ems_ref)
+  end
+
+  def save_middleware_datasources_inventory(ems, hashes, target = nil)
+    return if hashes.nil?
+    target = ems if target.nil?
+
+    ems.middleware_datasources(true)
+    deletes = if target.kind_of?(ExtManagementSystem)
+                :use_association
+              else
+                []
+              end
+
+    hashes.each do |h|
+      h[:server_id] = h.fetch_path(:middleware_server, :id)
+    end
+
+    save_inventory_multi(ems.middleware_datasources, hashes, deletes, [:ems_ref], nil,
+                         [:middleware_server])
+    store_ids_for_new_records(ems.middleware_datasources, hashes, :ems_ref)
   end
 end
---
+++ app/models/middleware_datasource.rb
@@ -0,0 +1,8 @@
+class MiddlewareDatasource < ApplicationRecord
+  include ReportableMixin
+
+  belongs_to :ext_management_system, :foreign_key => "ems_id"
+  belongs_to :middleware_server, :foreign_key => "server_id"
+  acts_as_miq_taggable
+  serialize :properties
+end
