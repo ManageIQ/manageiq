0aef3cba4ec65cabe9340b060a5f39fd00658e54
Greg Blomquist <blomquisg@gmail.com>
2016-05-02 17:27:49 -0400

    Merge pull request #8249 from durandom/auth_key_pair_validations
    auth keypair controller for new validation result
    (cherry picked from commit 4ee3610cb61e0af8475eb129d48b16042a8bb46f)

--- app/models/manageiq/providers/cloud_manager/auth_key_pair/operations.rb
+++ app/models/manageiq/providers/cloud_manager/auth_key_pair/operations.rb
@@ -15,17 +15,8 @@ module ManageIQ::Providers::CloudManager::AuthKeyPair::Operations
 
     private
 
-    # TODO(maufart): move validations to a separate place and make it more generic for better reuse?
     def validate_unsupported(message_prefix)
       self.class.validate_unsupported(message_prefix)
-    end
-
-    def validation_failed(operation, reason)
-      self.class.validation_failed(operation, reason)
-    end
-
-    def validate_key_pair
-      self.class.validate_key_pair(resource)
     end
   end
 
@@ -34,29 +25,13 @@ module ManageIQ::Providers::CloudManager::AuthKeyPair::Operations
       raise NotImplementedError, "raw_create_key_pair must be implemented in a subclass"
     end
 
-    def validate_create_key_pair(_ext_management_system, _options)
+    def validate_create_key_pair(_ext_management_system, _options = {})
       validate_unsupported(_("Create KeyPair Operation"))
-    end
-
-    # TODO(maufart): make it more generic or move to EMS (validate_resource?)
-    def validate_key_pair(ext_management_system)
-      if ext_management_system.nil?
-        return {:available => false,
-                :message   => _("The Keypair is not connected to an active %{table}") %
-                  {:table => ui_lookup(:table => "ext_management_system")}}
-      end
-      {:available => true, :message => nil}
     end
 
     def validate_unsupported(message_prefix)
       {:available => false,
        :message   => _("%{message} is not available for %{name}.") % {:message => message_prefix, :name => name}}
-    end
-
-    def validation_failed(operation, reason)
-      {:available => false,
-       :message   => _("Validation failed for #{name} operation %{operation}. %{reason}") % {:operation => operation,
-                                                                                             :reason    => reason}}
     end
   end
 end
