ed45eaaf237ea41c8703b0c97a1fd532236c8d8c
Laura Galis <lgalis@redhat.com>
2016-05-03 11:53:03 -0400

    Moved total_configured_systems to inventoryRootGroup, correct model title, remove unnecessary check for base in tree node builder

--- app/models/manageiq/providers/configuration_manager/inventory_group.rb
+++ app/models/manageiq/providers/configuration_manager/inventory_group.rb
@@ -1,9 +1,3 @@
 class ManageIQ::Providers::ConfigurationManager::InventoryGroup < EmsFolder
   belongs_to :manager, :foreign_key => "ems_id", :class_name => "ManageIQ::Providers::ConfigurationManager"
-
-  virtual_column :total_configured_systems, :type => :integer
-
-  def total_configured_systems
-    Rbac.filtered(ConfiguredSystem.where(:manager_id => ems_id, :inventory_root_group_id => id)).count
-  end
 end
