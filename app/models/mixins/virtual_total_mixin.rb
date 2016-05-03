a628d59d38cd5b67de02ebc1f150533d8176e184
Keenan Brock <kbrock@redhat.com>
2016-05-03 09:51:49 -0400

    fix virtual_total_mixin to not require table load

--- app/models/mixins/virtual_total_mixin.rb
+++ app/models/mixins/virtual_total_mixin.rb
@@ -21,14 +21,16 @@ module VirtualTotalMixin
 
       # allow this attribute to be sorted in the database
       if (reflection = reflect_on_association(relation))
-        foreign_table = reflection.klass.arel_table
-        local_key = reflection.active_record_primary_key
-        foreign_key = reflection.foreign_key
-        virtual_attribute name, :integer, :uses => relation, :arel => (lambda do |t|
+        arel = lambda do |t|
+          foreign_table = reflection.klass.arel_table
+          # need db access for the keys, so delaying all this lookup until call time
+          local_key = reflection.active_record_primary_key
+          foreign_key = reflection.foreign_key
           # assuming has_many
           Arel::Nodes::Grouping.new(foreign_table.project(Arel.star.count)
                                                  .where(t[local_key].eq(foreign_table[foreign_key])))
-        end)
+        end
+        virtual_attribute name, :integer, :uses => relation, :arel => arel
       else
         virtual_attribute name, :integer
       end
