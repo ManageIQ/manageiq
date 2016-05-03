6c867b97acaba93628aeb74ef06472202c9c6048
Lucy Fu <lufu@redhat.com>
2016-05-03 11:58:10 -0400

    There should be only private network and public network (no CloudNetowrk class) for Openstack.
    Public network is connected to private_networks via routers
    and private network is connected to public_networks via subnets and routers
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1332043

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-network_manager-cloud_network-private.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-openstack-network_manager-cloud_network-private.rb
@@ -1,4 +1,5 @@
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Openstack_NetworkManager_CloudNetwork_Private < MiqAeServiceManageIQ_Providers_Openstack_NetworkManager_CloudNetwork
+    expose :public_networks, :association => true
   end
 end
