0eeb387cb21f706b93877b744ad3b04b5d88f7aa
Ladislav Smola <lsmola@redhat.com>
2016-05-03 10:25:00 +0200

    Restart NetworkManager event_monitor on credentials change
    Restart NetworkManager event_monitor on credentials change,
    by safely checking if there is a network manager defined.

--- app/models/ext_management_system.rb
+++ app/models/ext_management_system.rb
@@ -598,6 +598,7 @@ class ExtManagementSystem < ApplicationRecord
     if event_monitor_class && !self.new_record? && self.credentials_changed?
       _log.info("EMS: [#{name}], Credentials have changed, stopping Event Monitor.  It will be restarted by the WorkerMonitor.")
       stop_event_monitor_queue
+      network_manager.stop_event_monitor_queue if respond_to?(:network_manager) && network_manager
     end
   end
 
