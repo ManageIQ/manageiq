3742664225abf8425003e3bde40adac7b502a4a8
Oleg Barenboim <chessbyte@gmail.com>
2016-05-04 00:37:12 -0500

    Merge pull request #8092 from romaintb/fix_arm_provisioning_without_secgroup
    fix arm provisioning if no security_group is selected
    (cherry picked from commit 78dc56cc2d3a8bb91f2116048e00610ab97619c2)

--- app/models/manageiq/providers/azure/cloud_manager/provision/cloning.rb
+++ app/models/manageiq/providers/azure/cloud_manager/provision/cloning.rb
@@ -96,32 +96,34 @@ module ManageIQ::Providers::Azure::CloudManager::Provision::Cloning
 
   def create_nic
     source.with_provider_connection do |azure|
-      nis = Azure::Armrest::Network::NetworkInterfaceService.new(azure)
-      ips = Azure::Armrest::Network::IpAddressService.new(azure)
-
-      ip = ips.create("#{dest_name}-publicIp", resource_group.name, :location => region)
-      network_options = {
-        :location   => region,
-        :properties => {
-          :networkSecurityGroup => {
-            :id => security_group.ems_ref
-          },
-          :ipConfigurations     => [
-            :name       => dest_name,
-            :properties => {
-              :subnet          => {
-                :id            => cloud_subnet.ems_ref
-              },
-              :publicIPAddress => {
-                :id => ip.id
-              },
-            }
-          ],
-        }
-      }
+      nis             = Azure::Armrest::Network::NetworkInterfaceService.new(azure)
+      ips             = Azure::Armrest::Network::IpAddressService.new(azure)
+      ip              = ips.create("#{dest_name}-publicIp", resource_group.name, :location => region)
+      network_options = build_nic_options(ip.id)
 
       return nis.create(dest_name, resource_group.name, network_options).id
     end
+  end
+
+  def build_nic_options(ip)
+    network_options = {
+      :location   => region,
+      :properties => {
+        :ipConfigurations => [
+          :name       => dest_name,
+          :properties => {
+            :subnet          => {
+              :id => cloud_subnet.ems_ref
+            },
+            :publicIPAddress => {
+              :id => ip
+            },
+          }
+        ],
+      }
+    }
+    network_options[:properties][:networkSecurityGroup] = {:id => security_group.ems_ref} if security_group
+    network_options
   end
 
   def start_clone(clone_options)
