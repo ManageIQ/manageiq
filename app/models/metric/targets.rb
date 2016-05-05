1bcb0f89507619cb5e81e7633e2928bb4fa4ce9b
Keenan Brock <kbrock@redhat.com>
2016-05-04 23:54:44 -0400

    metric::targets - merge 2 selects

--- app/models/metric/targets.rb
+++ app/models/metric/targets.rb
@@ -35,9 +35,10 @@ module Metric::Targets
 
     MiqPreloader.preload(zone.ems_clouds, :vms => [{:availability_zone => :tags}, :ext_management_system])
 
-    zone.ems_clouds.flat_map(&:vms)
-        .select { |vm| vm.availability_zone.nil? || vm.availability_zone && vm.availability_zone.perf_capture_enabled? }
-      .select { |vm| vm.ext_management_system && vm.state == 'on' }
+    zone.ems_clouds.flat_map(&:vms).select do |vm|
+      (vm.availability_zone.nil? || vm.availability_zone.perf_capture_enabled?) &&
+        vm.ext_management_system && vm.state == 'on'
+    end
   end
 
   def self.capture_container_targets(zone, _options)
