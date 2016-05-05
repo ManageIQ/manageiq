a44abc65af2a91f4b340158074d9363ff547799e
Alberto Gutierrez <aljesusg@gmail.com>
2016-05-05 12:01:34 +0200

    Added image in view

--- app/controllers/consumption_controller.rb
+++ app/controllers/consumption_controller.rb
@@ -2,6 +2,5 @@ class ConsumptionController < ApplicationController
   def show
     @layout     = "consumption"
     @showtype   = "consumption"
-    @message    = "Brace yourselves ... CONSUMPTION is coming !"
   end
 end
