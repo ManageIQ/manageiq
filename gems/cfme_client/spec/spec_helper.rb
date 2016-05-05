f3276941b1dc6c9ca42949e6684d9d72ae13c418
Alberto Bellotti <abellott@redhat.com>
2016-05-05 16:57:33 -0400

    [api] Removing the cfme_client and pulling the rest_api.rb CLI to tools/
    - Removing the gems cfme_client
    - Moving the only thing used from there which is the API CLI to the tools/ directory.
    - Versioned as 2.3.0-pre
    - Support decorators=
    - Fixing rubocop warnings in the rest_api.rb

--- gems/cfme_client/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-#
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-#
-Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require f }
-
-RSpec.configure do |config|
-end
