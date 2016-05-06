6da8c291913623c121885775632059dcdc2374fe
Milan Zazrivec <mzazrivec@redhat.com>
2016-05-06 10:22:39 +0200

    Use N_() instead of _() in button classes
    We don't need to use _() in button clases, since the strings
    will go through the _() routine once more in toolbar builder,
    so the gettext lookup would have to be done twice.
    
    Using N_() here is sufficient.

--- app/helpers/application_helper/button/old_dialogs_edit_delete.rb
+++ app/helpers/application_helper/button/old_dialogs_edit_delete.rb
@@ -4,9 +4,9 @@ class ApplicationHelper::Button::OldDialogsEditDelete < ApplicationHelper::Butto
     if @view_context.x_active_tree == :old_dialogs_tree && @record && @record[:default]
       self[:enabled] = false
       self[:title] = if self[:id] =~ /_edit/
-                       _('Default dialogs cannot be edited')
+                       N_('Default dialogs cannot be edited')
                      else
-                       _('Default dialogs cannot be removed from the VMDB')
+                       N_('Default dialogs cannot be removed from the VMDB')
                      end
     end
   end
