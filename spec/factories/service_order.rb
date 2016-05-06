f6ec923d7098c9dc7ed52dd1171c5a814d319890
Tim Wade <hello@timjwade.com>
2016-05-06 10:54:18 -0700

    Add a shopping cart factory

--- spec/factories/service_order.rb
+++ spec/factories/service_order.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory(:service_order) do
     name "service order"
     state "ordered"
+
+    factory(:shopping_cart) do
+      name "shopping cart"
+      state "cart"
+    end
   end
 end
