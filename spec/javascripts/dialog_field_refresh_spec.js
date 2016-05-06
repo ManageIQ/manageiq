2c7a36bdb276421b2409edd7037d01beaf0c5031
Martin Hradil <mhradil@redhat.com>
2016-05-06 18:45:49 +0000

    Fix "dialogFieldRefresh #initializeDialogSelectPicker ... has no expectations"
    Because the `done()` was called before that `expect`, the test would succeed but not test anything.
    
    Spec 'dialogFieldRefresh #initializeDialogSelectPicker triggers the auto refresh when the drop down changes' has no expectations.
    Spec 'dialogFieldRefresh #initializeDialogSelectPicker triggers autorefresh with "false" when triggerAutoRefresh arg is false' has no expectations.

--- spec/javascripts/dialog_field_refresh_spec.js
+++ spec/javascripts/dialog_field_refresh_spec.js
@@ -116,15 +116,13 @@ describe('dialogFieldRefresh', function() {
       expect(window.miqSelectPickerEvent).toHaveBeenCalledWith('fieldName', 'url', {callback: jasmine.any(Function)});
     });
 
-    it('triggers the auto refresh when the drop down changes', function(done) {
+    it('triggers the auto refresh when the drop down changes', function() {
       dialogFieldRefresh.initializeDialogSelectPicker(fieldName, fieldId, selectedValue, url);
-      done();
       expect(dialogFieldRefresh.triggerAutoRefresh).toHaveBeenCalledWith(fieldId, 'true');
     });
 
-    it('triggers autorefresh with "false" when triggerAutoRefresh arg is false', function(done) {
+    it('triggers autorefresh with "false" when triggerAutoRefresh arg is false', function() {
       dialogFieldRefresh.initializeDialogSelectPicker(fieldName, fieldId, selectedValue, url, 'false');
-      done();
       expect(dialogFieldRefresh.triggerAutoRefresh).toHaveBeenCalledWith(fieldId, 'false');
     });
   });
