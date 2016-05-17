5b1268268ce986986a0525c3203ab073047cef49
Joe Rafaniello <jrafanie@redhat.com>
2016-05-17 19:42:02 -0400

    Setup travis to use parallel_tests with 2 cores for vmdb suite
    It runs the parallel setup and the parallel vmdb suite:
    1) To setup 2 databases (1 per core):
    `bundle exec parallel_test -e "bundle exec rake test:vmdb:setup`
    
    2) test:vmdb_parallel - vmdb suite invoked through paralle_tests

--- tools/ci/before_script.sh
+++ tools/ci/before_script.sh
@@ -1,7 +1,11 @@
 set -v
 
 if [[ -n "$TEST_SUITE" ]]; then
-  bundle exec rake test:$TEST_SUITE:setup
+  if [[ -n "$PARALLEL" ]]; then
+    bundle exec parallel_test -e "bundle exec rake test:$TEST_SUITE:setup"
+  else
+    bundle exec rake test:$TEST_SUITE:setup
+  fi
 fi
 
 set +v
