ce7dbd5ec2f6faad4745547553fa291f21196b35
Zahi Akiva <zakiva@redhat.com>
2016-05-18 12:05:34 +0300

    Fix Container Replicator default view

--- app/helpers/ui_constants.rb
+++ app/helpers/ui_constants.rb
@@ -183,6 +183,7 @@ module UiConstants
       :containerservice                         => "list",
       :containerroute                           => "list",
       :containerproject                         => "list",
+      :containerreplicator                      => "list",
       :containerimage                           => "list",
       :containerimageregistry                   => "list",
       :persistentvolume                         => "list",
