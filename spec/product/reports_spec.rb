995a196a4819365ff1cfb6f5cc887a3175a9728d
Gregg Tanzillo <gtanzill@redhat.com>
2016-05-18 08:41:43 -0400

    Merge pull request #8678 from isimluk/spec-for-stock-miq-reports2
    Spec for col_order for stock MiqReports
    (cherry picked from commit 4f52e562020b250e40db05f73b9fd8cb80c75481)

--- spec/product/reports_spec.rb
+++ spec/product/reports_spec.rb
@@ -24,4 +24,32 @@ describe 'YAML reports' do
       expect(report.table).to be_kind_of(Ruport::Data::Table)
     end
   end
+
+  it 'defines headers that match col_order' do
+    report_yamls.each do |yaml|
+      report_data = YAML.load(File.open(yaml))
+      col_order = report_data['col_order'].length
+      headers = report_data['headers'].length
+      expect(headers).to eq(col_order)
+    end
+  end
+
+  it 'defines correct (existing) col_order columns' do
+    report_yamls.each do |yaml|
+      report_data = YAML.load(File.open(yaml))
+      cols = report_data['cols'] + collect_columns(report_data['include'])
+      dangling = report_data['col_order'].reject do |col|
+        cols.include?(col) || %w(max avg).include?(col.split('__')[-1])
+      end
+      expect(dangling).to eq([])
+    end
+  end
+
+  def collect_columns(include_hash)
+    return [] if include_hash.nil?
+    include_hash.inject([]) do |cols, (table_name, data)|
+      cols += data["columns"].collect { |col_name| "#{table_name}.#{col_name}" } if data['columns']
+      cols + collect_columns(data['include'])
+    end
+  end
 end
