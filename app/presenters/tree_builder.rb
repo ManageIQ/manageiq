01fcb1f94da9b4c30f3c2fecb352aa94580520b2
Laura Galis <lgalis@redhat.com>
2016-05-18 19:17:52 -0400

    Alignment and changes for clarity (cherry picked from commit f236f3e)

--- app/presenters/tree_builder.rb
+++ app/presenters/tree_builder.rb
@@ -8,7 +8,7 @@ class TreeBuilder
     # Catalog explorer trees
     when :configuration_manager_providers then TreeBuilderConfigurationManager
     when :cs_filter                       then TreeBuilderConfigurationManagerConfiguredSystems
-    when :configuration_scripts                then TreeBuilderConfigurationManagerConfigurationScripts
+    when :configuration_scripts           then TreeBuilderConfigurationManagerConfigurationScripts
 
     # Catalog explorer trees
     when :ot               then TreeBuilderOrchestrationTemplates
