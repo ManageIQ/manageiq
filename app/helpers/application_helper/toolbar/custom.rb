ac21752ab8f0ce911ffcab5e3e13e45b440e7ea4
Martin Povolny <mpovolny@redhat.com>
2016-05-18 11:28:51 +0200

    Toolbar custom content: user Renderer for render.

--- app/helpers/application_helper/toolbar/custom.rb
+++ app/helpers/application_helper/toolbar/custom.rb
@@ -1,6 +1,7 @@
 ApplicationHelper::Toolbar::Custom = Struct.new(:name, :args) do
   def render(view_context)
-    @content = view_context.render_to_string(:template => args[:partial])
+    # FIXME: assigns? locals? view_binding? instance_data?
+    @content = ApplicationController.renderer.render :partial => args[:partial]
   end
 
   attr_reader :content
