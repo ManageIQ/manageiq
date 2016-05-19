eda926376b625f73ac5ee5a6f86641ddd5c76eb9
Laura Galis <lgalis@redhat.com>
2016-05-19 18:21:00 -0400

    resolve conflicts - updated features spec

--- spec/models/miq_product_feature_spec.rb
+++ spec/models/miq_product_feature_spec.rb
@@ -2,7 +2,7 @@ require 'tmpdir'
 require 'pathname'
 
 describe MiqProductFeature do
-  let(:expected_feature_count) { 1035 }
+  let(:expected_feature_count) { 1039 }
 
   # - container_dashboard
   # - miq_report_widget_editor
