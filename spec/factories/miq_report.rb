d2766f7ddc2f2f7928e9a6f0019d495044776b51
lpichler <lpichler@redhat.com>
2016-05-19 16:39:43 +0200

    Spec for saved chargeback report results

--- spec/factories/miq_report.rb
+++ spec/factories/miq_report.rb
@@ -33,4 +33,15 @@ FactoryGirl.define do
     rpt_group       'Custom'
     association     :miq_group
   end
+
+  factory :miq_report_chargeback_with_results, :parent => :miq_report do
+    miq_report_results { [FactoryGirl.create(:miq_chargeback_report_result)] }
+    sequence(:name) { |n| "Test Report #{seq_padded_for_sorting(n)}" }
+    db              'ChargebackVm'
+    title           'some title'
+    rpt_type        'Default'
+    template_type   'report'
+    rpt_group       'Custom'
+    association     :miq_group
+  end
 end
