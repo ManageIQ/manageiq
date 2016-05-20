3b856cd5fd772ae4cca1bb123ceb806095295aee
Ladislav Smola <lsmola@redhat.com>
2016-05-20 10:18:46 +0200

    Add vnf and Vnfd models
    Add vnf and Vnfd models

---
+++ app/models/orchestration_template_vnfd.rb
@@ -0,0 +1,20 @@
+class OrchestrationTemplateVnfd < OrchestrationTemplate
+  def parameter_groups
+
+  end
+
+  def parameters
+
+  end
+
+  def self.eligible_manager_types
+    [ManageIQ::Providers::Openstack::CloudManager]
+  end
+
+  # return the parsing error message if not valid YAML; otherwise nil
+  def validate_format
+    YAML.parse(content) && nil if content
+  rescue Psych::SyntaxError => err
+    err.message
+  end
+end
