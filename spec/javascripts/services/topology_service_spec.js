0636b106abdf5e65d076b9f75419115b1eeec25b
Alissa Bonas <abonas@redhat.com>
2016-05-22 14:20:49 +0300

    Fix #8876 - navigation from topology to containers provider gets to error page

--- spec/javascripts/services/topology_service_spec.js
+++ spec/javascripts/services/topology_service_spec.js
@@ -22,7 +22,7 @@ describe('topologyService', function() {
     describe('the dbl click gets correct navigation url', function() {
       it('to entity pages', function() {
         var d = { id:"2",  item:{display_kind:"Openshift", kind:"ContainerManager", id:"2", miq_id:"37"}};
-        expect(testService.geturl(d)).toEqual("/ems_container/show/37");
+        expect(testService.geturl(d)).toEqual("/ems_container/37");
         d = { id:"3",  item:{display_kind:"Pod", kind:"ContainerGroup", id:"3", miq_id:"30"}};
         expect(testService.geturl(d)).toEqual("/container_group/show/30");
         d = { id:"4",  item:{display_kind:"VM", kind:"Vm", id:"4", miq_id:"25"}};
