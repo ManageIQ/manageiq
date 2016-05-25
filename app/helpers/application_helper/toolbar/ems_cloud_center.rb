d478e73ee6e00e45dbb028d4488cd4a2b3adc87a
Aparna Karve <akarve@redhat.com>
2016-05-25 12:00:24 -0700

    Add icons for the Authentication button

--- app/helpers/application_helper/toolbar/ems_cloud_center.rb
+++ app/helpers/application_helper/toolbar/ems_cloud_center.rb
@@ -74,13 +74,13 @@ class ApplicationHelper::Toolbar::EmsCloudCenter < ApplicationHelper::Toolbar::B
   button_group('ems_cloud_authentication', [
     select(
       :ems_cloud_authentication_choice,
-      'product fa-lg',
+      'fa fa-lock fa-lg',
       t = N_('Authentication'),
       t,
       :items => [
         button(
           :ems_cloud_recheck_auth_status,
-          'product fa-lg',
+          'fa fa-search fa-lg',
           N_('Re-check Authentication Status for this #{ui_lookup(:table=>"ems_cloud")}'),
           N_('Re-check Authentication Status'),
           :klass => ApplicationHelper::Button::EmsCloudRecheckAuthStatus),
