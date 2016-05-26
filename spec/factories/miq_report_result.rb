1debf0d4f0968dc349a649ca3be7c6dc4df7ad55
Gregg Tanzillo <gtanzill@redhat.com>
2016-05-26 16:31:38 -0400

    Merge pull request #8818 from lpichler/show_saved_chargeback_report_in_chargebacks
    Fix displaying saved chargeback reports in chargeback menu
    (cherry picked from commit c1be18b476ab3167376d493e8e6aff48c7113ebf)

--- spec/factories/miq_report_result.rb
+++ spec/factories/miq_report_result.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
   factory :miq_report_result do
   end
+
+  factory :miq_chargeback_report_result, :parent => :miq_report_result do
+    sequence(:name) { |n| "Test Report #{seq_padded_for_sorting(n)}" }
+    db "ChargebackVm"
+    report_source "Requested by user"
+  end
 end
