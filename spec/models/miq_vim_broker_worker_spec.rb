d5750de5d0967701467ca838748966daa6b8f1ff
Joe Rafaniello <jrafanie@redhat.com>
2016-05-27 16:09:37 -0400

    Collapse 'setup' code into the single example.

--- spec/models/miq_vim_broker_worker_spec.rb
+++ spec/models/miq_vim_broker_worker_spec.rb
@@ -1,12 +1,10 @@
 describe MiqVimBrokerWorker do
-  before(:each) do
+  it ".emses_to_monitor" do
     _guid, _server, @zone = EvmSpecHelper.create_guid_miq_server_zone
     FactoryGirl.create(:ems_vmware_with_authentication, :zone => @zone)
     FactoryGirl.create(:ems_vmware_with_authentication, :zone => @zone)
     allow_any_instance_of(ManageIQ::Providers::Vmware::InfraManager).to receive_messages(:authentication_status_ok? => true)
-  end
 
-  it ".emses_to_monitor" do
     expect(described_class.emses_to_monitor).to match_array @zone.ext_management_systems
   end
 end
