6d313bd9aa9269305b09e5aec780396018451811
Ladislav Smola <lsmola@redhat.com>
2016-05-31 17:40:52 +0200

    Allow to create ServiceOrchestration in automate
    Allow to create ServiceOrchestration in automate

--- lib/miq_automation_engine/service_models/miq_ae_service_service_orchestration.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_service_orchestration.rb
@@ -16,5 +16,13 @@ module MiqAeMethodService
     expose :orchestration_stack
     expose :build_stack_options_from_dialog
     expose :post_provision_configure
+
+    CREATE_ATTRIBUTES = [:name, :description]
+
+    def self.create(options = {})
+      attributes = options.symbolize_keys.slice(*CREATE_ATTRIBUTES)
+
+      ar_method { MiqAeServiceModelBase.wrap_results(ServiceOrchestration.create!(attributes)) }
+    end
   end
 end
