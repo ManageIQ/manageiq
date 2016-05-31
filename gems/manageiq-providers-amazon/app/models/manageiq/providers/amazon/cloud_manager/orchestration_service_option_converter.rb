632f688de75e4e57459999668c0b54a75aadc3c9
Bill Wei <bilwei@redhat.com>
2016-05-31 17:30:50 -0400

    change timeout to timeout_in_minutes due to sdk 2.0 upgrade
    https://bugzilla.redhat.com/show_bug.cgi?id=1340570

--- gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/cloud_manager/orchestration_service_option_converter.rb
+++ gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/cloud_manager/orchestration_service_option_converter.rb
@@ -3,7 +3,7 @@ class ManageIQ::Providers::Amazon::CloudManager::OrchestrationServiceOptionConve
     on_failure = @dialog_options['dialog_stack_onfailure']
     timeout = @dialog_options['dialog_stack_timeout']
     stack_options = {:parameters => stack_parameters, :disable_rollback => on_failure != 'ROLLBACK'}
-    stack_options[:timeout] = timeout.to_i unless timeout.blank?
+    stack_options[:timeout_in_minutes] = timeout.to_i unless timeout.blank?
 
     stack_options
   end
