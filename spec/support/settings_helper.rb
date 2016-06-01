0399959b80385fa05d07fbec2fed7d3d9a50b46c
Jason Frey <jfrey@redhat.com>
2016-06-01 11:13:36 -0400

    Avoid querying the database when asking for local config
    Fixes #9014
    https://bugzilla.redhat.com/show_bug.cgi?id=1337967

--- spec/support/settings_helper.rb
+++ spec/support/settings_helper.rb
@@ -4,6 +4,10 @@ def stub_settings(hash)
   allow(Vmdb::Settings).to receive(:for_resource) { settings }
 end
 
+def stub_local_settings(my_server)
+  stub_const("Settings", Vmdb::Settings.for_resource(my_server))
+end
+
 def stub_server_configuration(config, config_name = "vmdb")
   configuration = double(:config => config.deep_symbolize_keys)
   allow(VMDB::Config).to receive(:new).with(config_name) { configuration }
