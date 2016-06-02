ba456c7778fc7fddd838f8b847f2f108fa51dcad
Gregg Tanzillo <gtanzill@redhat.com>
2016-06-02 14:42:31 -0400

    Fixing broken chargeback_controller test
    -The test was originally broken when it was created in d2766f7ddc2f2f7928e9a6f but still passing because the names of the report and report result were the same.
    -It was comparing MiqReportResult#name to MiqReport#name
    -Also, changed the name in the factory for the miq_report_result so that it's different from the line in the miq_report factory and obvious

--- spec/factories/miq_report_result.rb
+++ spec/factories/miq_report_result.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   end
 
   factory :miq_chargeback_report_result, :parent => :miq_report_result do
-    sequence(:name) { |n| "Test Report #{seq_padded_for_sorting(n)}" }
+    sequence(:name) { |n| "Test Chargeback Report Result #{seq_padded_for_sorting(n)}" }
     db "ChargebackVm"
     report_source "Requested by user"
   end
