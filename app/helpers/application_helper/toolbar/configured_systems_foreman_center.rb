4874f1ef8b543e6938fcd6729d4ec7d13eed7f33
Brandon Dunne <bdunne@redhat.com>
2016-06-09 11:49:03 -0400

    Deduplicate lifecycle button group definition for configured_systems

--- app/helpers/application_helper/toolbar/configured_systems_foreman_center.rb
+++ app/helpers/application_helper/toolbar/configured_systems_foreman_center.rb
@@ -1,24 +1,5 @@
 class ApplicationHelper::Toolbar::ConfiguredSystemsForemanCenter < ApplicationHelper::Toolbar::Basic
-  button_group('provider_foreman_lifecycle', [
-    select(
-      :provider_foreman_lifecycle_choice,
-      'fa fa-recycle fa-lg',
-      t = N_('Lifecycle'),
-      t,
-      :enabled => true,
-      :items   => [
-        button(
-          :configured_system_provision,
-          'pficon pficon-add-circle-o fa-lg',
-          t = N_('Provision Configured Systems'),
-          t,
-          :url       => "provision",
-          :url_parms => "main_div",
-          :enabled   => false,
-          :onwhen    => "1+"),
-      ]
-    ),
-  ])
+  include ApplicationHelper::Toolbar::ConfiguredSystem::LifecycleMixin
   button_group('provider_foreman_policy', [
     select(
       :provider_foreman_policy_choice,
