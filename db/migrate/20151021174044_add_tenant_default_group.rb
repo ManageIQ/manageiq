d1a17a75c8193e3b17af677dd33bd21d406aafb1
Jason Frey <fryguy9@gmail.com>
2016-06-10 16:11:46 -0400

    Merge pull request #9177 from carbonin/reset_column_cache_after_adding_tenant_default_group
    Reset the column cache after adding default_miq_group_id to tenants
    (cherry picked from commit 5ba686298e40f9093da243c553262173ecd485ca)

--- db/migrate/20151021174044_add_tenant_default_group.rb
+++ db/migrate/20151021174044_add_tenant_default_group.rb
@@ -1,5 +1,8 @@
 class AddTenantDefaultGroup < ActiveRecord::Migration
+  class Tenant < ActiveRecord::Base; end
+
   def change
     add_column :tenants, :default_miq_group_id, :bigint
+    Tenant.reset_column_information
   end
 end
