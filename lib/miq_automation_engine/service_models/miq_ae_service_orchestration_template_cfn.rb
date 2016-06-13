402e2c60390c80e2a3180dc823104833e8851c40
Ladislav Smola <lsmola@redhat.com>
2016-06-13 15:22:07 +0200

    Allow to create CFN and HOT templates from automate
    Allow to create CFN and HOT templates from automate

--- lib/miq_automation_engine/service_models/miq_ae_service_orchestration_template_cfn.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_orchestration_template_cfn.rb
@@ -1,3 +1,11 @@
 module MiqAeMethodService
-  class MiqAeServiceOrchestrationTemplateCfn < MiqAeServiceOrchestrationTemplate; end
+  class MiqAeServiceOrchestrationTemplateCfn < MiqAeServiceOrchestrationTemplate
+    CREATE_ATTRIBUTES = [:name, :description, :content, :draft, :orderable].freeze
+
+    def self.create(options = {})
+      attributes = options.symbolize_keys.slice(*CREATE_ATTRIBUTES)
+
+      ar_method { MiqAeServiceOrchestrationTemplateCfn.wrap_results(OrchestrationTemplateCfn.create!(attributes)) }
+    end
+  end
 end
--- lib/miq_automation_engine/service_models/miq_ae_service_orchestration_template_hot.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_orchestration_template_hot.rb
@@ -1,3 +1,11 @@
 module MiqAeMethodService
-  class MiqAeServiceOrchestrationTemplateHot < MiqAeServiceOrchestrationTemplate; end
+  class MiqAeServiceOrchestrationTemplateHot < MiqAeServiceOrchestrationTemplate
+    CREATE_ATTRIBUTES = [:name, :description, :content, :draft, :orderable].freeze
+
+    def self.create(options = {})
+      attributes = options.symbolize_keys.slice(*CREATE_ATTRIBUTES)
+
+      ar_method { MiqAeServiceOrchestrationTemplateHot.wrap_results(OrchestrationTemplateHot.create!(attributes)) }
+    end
+  end
 end
