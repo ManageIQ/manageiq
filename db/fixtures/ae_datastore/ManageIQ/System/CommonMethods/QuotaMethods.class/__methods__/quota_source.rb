b086426c1b1dbf0cf6cc0fa3db574bf6b7ac4cae
william fitzgerald <wfitzger@redhat.com>
2016-06-14 11:48:54 -0400

    Automate - Quota enforcement for user fix
    Modified Log message to display quota source.
    
    Added new spec test for user quota.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1342180
    
    Modified log message to include 'source'.

--- db/fixtures/ae_datastore/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/quota_source.rb
+++ db/fixtures/ae_datastore/ManageIQ/System/CommonMethods/QuotaMethods.class/__methods__/quota_source.rb
@@ -5,8 +5,11 @@
 @miq_request = $evm.root['miq_request']
 $evm.root['quota_source_type'] = $evm.parent['quota_source_type'] || $evm.object['quota_source_type']
 
-if $evm.root['quota_source_type'].casecmp('group').zero?
+case $evm.root['quota_source_type'].downcase
+when 'group'
   $evm.root['quota_source'] = @miq_request.requester.current_group
+when 'user'
+  $evm.root['quota_source'] = @miq_request.requester
 else
   $evm.root['quota_source'] = @miq_request.tenant
   $evm.root['quota_source_type'] = 'tenant'
