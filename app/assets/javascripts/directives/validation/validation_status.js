18b18dd15046c89d4e81ab0d17f87a1733159cf7
Aparna Karve <akarve@redhat.com>
2016-06-15 12:35:19 -0700

    Mark the tab with the error indicator the moment error is detected

--- app/assets/javascripts/directives/validation/validation_status.js
+++ app/assets/javascripts/directives/validation/validation_status.js
@@ -1,4 +1,4 @@
-ManageIQ.angular.app.directive('validationStatus', function() {
+ManageIQ.angular.app.directive('validationStatus', ['$rootScope', function($rootScope) {
   return {
     require: 'ngModel',
     link: function (scope, elem, attrs, ctrl) {
@@ -6,9 +6,11 @@ ManageIQ.angular.app.directive('validationStatus', function() {
         if (angular.isDefined(viewValue) && viewValue === true) {
           scope.postValidationModelRegistry(attrs.prefix);
           return true;
+        } else {
+          $rootScope.$broadcast('setErrorOnTab', {tab: attrs.prefix});
+          return false;
         }
-        return false;
       };
     }
   }
-});
+}]);
