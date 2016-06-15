481c1f5e0fd056d8411c739314c1f6c27a758b12
Aparna Karve <akarve@redhat.com>
2016-06-15 10:51:25 -0700

    Style tabs, use directives to indicate Validation related errors

--- app/assets/javascripts/directives/validation/reset_validation_status.js
+++ app/assets/javascripts/directives/validation/reset_validation_status.js
@@ -1,20 +1,20 @@
-ManageIQ.angular.app.directive('resetValidationStatus', function() {
+ManageIQ.angular.app.directive('resetValidationStatus', ['$rootScope', function($rootScope) {
   return {
     require: 'ngModel',
     link: function (scope, elem, attrs, ctrl) {
       scope.$watch(attrs.ngModel, function() {
-        adjustValidationStatus(ctrl.$modelValue, scope, ctrl, attrs);
+        adjustValidationStatus(ctrl.$modelValue, scope, ctrl, attrs, $rootScope);
       });
 
       ctrl.$parsers.push(function(value) {
-        adjustValidationStatus(value, scope, ctrl, attrs);
+        adjustValidationStatus(value, scope, ctrl, attrs, $rootScope);
         return value;
       });
     }
   }
-});
+}]);
 
-var adjustValidationStatus = function(value, scope, ctrl, attrs) {
+var adjustValidationStatus = function(value, scope, ctrl, attrs, rootScope) {
   if(scope.checkAuthentication === true && angular.isDefined(scope.postValidationModel)) {
     var modelPostValidationObject = angular.copy(scope.postValidationModel[attrs.prefix]);
     delete modelPostValidationObject[ctrl.$name];
@@ -26,8 +26,10 @@ var adjustValidationStatus = function(value, scope, ctrl, attrs) {
 
     if (value == scope.postValidationModel[attrs.prefix][ctrl.$name] && _.isMatch(modelObject, modelPostValidationObject)) {
       scope[scope.model][attrs.resetValidationStatus] = true;
+      rootScope.$broadcast('clearErrorOnTab', {tab: attrs.prefix});
     } else {
       scope[scope.model][attrs.resetValidationStatus] = false;
+      rootScope.$broadcast('setErrorOnTab', {tab: attrs.prefix});
     }
   }
 };
