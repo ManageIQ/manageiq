5dd7d097ff214b1073d9f0252e2c997c7f85c4ce
Madhu Kanoor <mkanoor@redhat.com>
2016-06-16 13:13:05 -0400

    Use prepend namespace to do an additional lookup
    For provider specific domains check if there is
    prepend_namespace in the root object and if there is
    we do the first lookup by prepending the specified namespace
    to the path and doing a lookup before doing the normal lookup.
    A method would supply the prepend_namespace attribute.

--- lib/miq_automation_engine/engine/miq_ae_domain_search.rb
+++ lib/miq_automation_engine/engine/miq_ae_domain_search.rb
@@ -1,9 +1,11 @@
 module MiqAeEngine
   class MiqAeDomainSearch
+    attr_accessor :prepend_namespace
     def initialize
       @fqns_id_cache       = {}
       @fqns_id_class_cache = {}
       @partial_ns          = []
+      @prepend_namespace   = nil
     end
 
     def ae_user=(obj)
@@ -35,7 +37,9 @@ module MiqAeEngine
         end
       end
       @partial_ns << ns unless @partial_ns.include?(ns)
-      find_first_fq_domain(uri, ns, klass, instance, method)
+      updated_ns = find_first_fq_domain(uri, "#{@prepend_namespace}/#{ns}", klass, instance, method) if @prepend_namespace
+      updated_ns ||= find_first_fq_domain(uri, ns, klass, instance, method)
+      updated_ns || ns
     end
 
     def find_first_fq_domain(uri, ns, klass, instance, method)
@@ -45,12 +49,13 @@ module MiqAeEngine
       parts.unshift("")
       matching_domain = get_matching_domain(parts, klass, instance, method)
       matching_domain ||= get_matching_domain(parts, klass, MiqAeObject::MISSING_INSTANCE, method)
+      updated_ns = nil
       if matching_domain
-        parts[0]     = matching_domain
-        ns = parts.join('/')
-        $miq_ae_logger.info("Updated namespace [#{uri}  #{ns}]")
+        parts[0]   = matching_domain
+        updated_ns = parts.join('/')
+        $miq_ae_logger.info("Updated namespace [#{uri}  #{updated_ns}]")
       end
-      ns
+      updated_ns
     end
 
     def get_matching_domain(ns_parts, klass, instance, method)
