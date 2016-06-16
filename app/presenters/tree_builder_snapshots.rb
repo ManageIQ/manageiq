5b9ccd75222f7794f2b14c3fd07b372985a9fe6e
Zita Nemeckova <znemecko@redhat.com>
2016-06-16 15:15:45 +0200

    Add missing functionality

--- app/presenters/tree_builder_snapshots.rb
+++ app/presenters/tree_builder_snapshots.rb
@@ -1,15 +1,18 @@
 class TreeBuilderSnapshots < TreeBuilder
   has_kids_for Snapshot, [:x_get_tree_snapshot_kids]
 
-  def initialize(name, type, sandbox, build = true, root = nil)
+  attr_reader :selected_node
+
+  def initialize(name, type, sandbox, build = true, root = nil, selected_node = nil)
     @record = root
+    @selected_node = selected_node.present? ? id(selected_node) : nil
     super(name, type, sandbox, build)
   end
 
   private
 
   def tree_init_options(_tree_name)
-    {:full_ids => true, :lazy => false}
+    {:full_ids => true}
   end
 
   def set_locals_for_render
@@ -17,6 +20,7 @@ class TreeBuilderSnapshots < TreeBuilder
     locals.merge!(:id_prefix                   => "snap_",
                   :autoload                    => true,
                   :onclick                     => 'miqOnClickSnapshotTree',
+                  :exp_tree                    => true,
                   :open_close_all_on_dbl_click => true)
   end
 
@@ -26,11 +30,32 @@ class TreeBuilderSnapshots < TreeBuilder
 
   def x_get_tree_roots(count_only = false, _options)
     root_kid = @record.snapshots.present? ? [@record.snapshots.find { |x| x.parent_id.nil? }] : []
+    open_node("sn-#{to_cid(root_kid.first.id)}") if root_kid.present?
     count_only_or_objects(count_only, root_kid)
+  end
+
+  def parent_id(id)
+    Snapshot.find(id).parent_id
+  end
+
+  def id(kid_id)
+    id = kid_id
+    while parent_id(kid_id).present?
+      kid_id = parent_id(kid_id)
+      id = to_cid(id) if id.kind_of?(Fixnum)
+      id = "#{to_cid(kid_id)}_sn-#{id}"
+    end
+    "sn-#{id}"
   end
 
   def x_get_tree_snapshot_kids(parent, count_only)
     kids = parent.children.present? ? parent.children : []
+    if kids.present?
+      id = id(kids.first.id)
+      open_node(id)
+    end
+    # select last node if no node was selected
+    @selected_node = id(parent.id) if @selected_node.nil? && kids.empty?
     count_only_or_objects(count_only, kids)
   end
 end
