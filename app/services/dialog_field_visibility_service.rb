e6d1c6a5e10edfc79299200e00cfe1aae7387d3e
Erik Clarizio <eclarizi@redhat.com>
2016-06-17 10:11:32 -0700

    Add ServiceTemplateFieldsVisibilityService to DialogFieldVisibilityService

--- app/services/dialog_field_visibility_service.rb
+++ app/services/dialog_field_visibility_service.rb
@@ -2,14 +2,19 @@ class DialogFieldVisibilityService
   def initialize(
     auto_placement_visibility_service = AutoPlacementVisibilityService.new,
     number_of_vms_visibility_service = NumberOfVmsVisibilityService.new,
+    service_template_fields_visibility_service = ServiceTemplateFieldsVisibilityService.new,
   )
     @auto_placement_visibility_service = auto_placement_visibility_service
     @number_of_vms_visibility_service = number_of_vms_visibility_service
+    @service_template_fields_visibility_service = service_template_fields_visibility_service
   end
 
   def determine_visibility(options)
     field_names_to_hide = []
     field_names_to_show = []
+
+    visibility_hash = @service_template_fields_visibility_service.determine_visibility(options[:service_template_request])
+    field_names_to_hide += visibility_hash[:hide]
 
     visibility_hash = @auto_placement_visibility_service.determine_visibility(options[:auto_placement_enabled])
     field_names_to_hide += visibility_hash[:hide]
--- spec/services/dialog_field_visibility_service_spec.rb
+++ spec/services/dialog_field_visibility_service_spec.rb
@@ -6,6 +6,7 @@ describe DialogFieldVisibilityService do
       described_class.new(
         auto_placement_visibility_service,
         number_of_vms_visibility_service,
+        service_template_fields_visibility_service,
       )
     end
 
@@ -14,8 +15,12 @@ describe DialogFieldVisibilityService do
         :auto_placement_enabled          => auto_placement_enabled,
         :number_of_vms                   => number_of_vms,
         :platform                        => platform,
+        :service_template_request        => service_template_request,
       }
     end
+
+    let(:service_template_fields_visibility_service) { double("ServiceTemplateFieldsVisibilityService") }
+    let(:service_template_request) { "service_template_request" }
 
     let(:auto_placement_visibility_service) { double("AutoPlacementVisibilityService") }
     let(:auto_placement_enabled) { "auto_placement_enabled" }
@@ -25,6 +30,10 @@ describe DialogFieldVisibilityService do
     let(:platform) { "platform" }
 
     before do
+      allow(service_template_fields_visibility_service).
+        to receive(:determine_visibility).with(service_template_request).and_return(
+          {:hide => [:service_template_request_hide]}
+        )
       allow(auto_placement_visibility_service).
         to receive(:determine_visibility).with(auto_placement_enabled).and_return(
           {:hide => [:auto_hide], :show => [:auto_show]}
@@ -38,6 +47,7 @@ describe DialogFieldVisibilityService do
     it "adds the values to the field names to hide and show without duplicates or intersections" do
       expect(subject.determine_visibility(options)).to eq({
         :hide => [
+          :service_template_request_hide,
           :auto_hide,
           :number_hide
         ],
