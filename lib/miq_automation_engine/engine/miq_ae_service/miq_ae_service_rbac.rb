c212c0a7ba3a093163b59f939453cafd79614b15
Madhu Kanoor <mkanoor@redhat.com>
2016-06-20 09:50:48 -0400

    Changed to Rbac.filtered
    Changed to use Rbac.filtered instead of Rbac.search

--- lib/miq_automation_engine/engine/miq_ae_service/miq_ae_service_rbac.rb
+++ lib/miq_automation_engine/engine/miq_ae_service/miq_ae_service_rbac.rb
@@ -10,13 +10,13 @@ module MiqAeMethodService
 
       def all
         MiqAeService.set_current_user if MiqAeService.rbac_enabled?
-        objs = MiqAeService.rbac_enabled? ? Rbac.search(:class => model, :results_format => :objects).first : model.send(:all)
+        objs = MiqAeService.rbac_enabled? ? Rbac.filtered(model): model.send(:all)
         wrap_results(objs)
       end
 
       def count
         MiqAeService.set_current_user if MiqAeService.rbac_enabled?
-        MiqAeService.rbac_enabled? ? Rbac.search(:class => model).first.count : model.send(:count)
+        MiqAeService.rbac_enabled? ? Rbac.filtered(model).count : model.send(:count)
       end
 
       def first
@@ -32,7 +32,6 @@ module MiqAeMethodService
         ret = MiqAeService.rbac_enabled? ? Rbac.filtered(array) : array
         (objs.kind_of?(Array) || objs.kind_of?(ActiveRecord::Relation)) ? ret : ret.first
       end
-
     end
   end
 end
