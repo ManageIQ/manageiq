4ec779a983083800ea85c86f440853017feb7ca4
Ladislav Smola <lsmola@redhat.com>
2016-06-21 09:26:13 +0200

    Expose missing network relations to automate
    This relations will allow us to identify a floating IP from a
    certain subnet.

--- lib/miq_automation_engine/service_models/miq_ae_service_floating_ip.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_floating_ip.rb
@@ -3,5 +3,6 @@ module MiqAeMethodService
     expose :ext_management_system, :association => true
     expose :vm,                    :association => true
     expose :cloud_tenant,          :association => true
+    expose :network_port,          :association => true
   end
 end
