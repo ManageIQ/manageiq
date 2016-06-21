d925ef0a39bd826a497bcc95c0433fec402dc7b4
Jason Frey <fryguy9@gmail.com>
2016-06-21 17:31:31 -0400

    Merge pull request #8886 from durandom/supported_concern
    SupportsFeatureMixin
    (cherry picked from commit 80e2c93c270909aec168ae9293a10c0d37ceb2d0)

---
+++ app/models/mixins/supports_feature_mixin.rb
@@ -0,0 +1,114 @@
+module SupportsFeatureMixin
+  #
+  # Including this in a model gives you a DSL to make features supported or not
+  #
+  #   class Post
+  #     include SupportsFeatureMixin
+  #     supports :publish
+  #     supports_not :fake, "We keep it real"
+  #     supports :archive do
+  #       unsupported_reason_add(:archive) = "Its too good" if featured?
+  #     end
+  #   end
+  #
+  # To make a feature conditionally supported, pass a block to the +supports+ method.
+  # The block is evaluated in the context of the instance.
+  # If you call the private method +unsupported_reason_add+ whith the feature
+  # and a reason, then the feature will be unsupported and the reason will be
+  # accessible through
+  #
+  #   instance.unsupported_reason(:feature)
+  #
+  # The above allows you to call +supports_feature?+ methods on the Class and Instance:
+  #
+  #   Post.supports_publish?                       # => true
+  #   Post.new.supports_publish?                   # => true
+  #   Post.supports_fake?                          # => false
+  #   Post.supports_archive?                       # => true
+  #   Post.new(featured: true).supports_archive?   # => false
+  #
+  # To get a reason why a feature is unsupported use the +unsupported+ method
+  #
+  #   Post.unsupported_reason(:publish)                     # => nil
+  #   Post.unsupported_reason(:fake)                        # => "We keep it real"
+  #   Post.new(featured: true).unsupported_reason(:archive) # => "Its too good"
+  #
+  # If you include this concern in a Module that gets included by the Model
+  # you have to extend that model with +ActiveSupport::Concern+ and wrap the
+  # +supports+ calls in an +included+ block. This is also true for modules in between!
+  #
+  #   module Operations
+  #     extend ActiveSupport::Concern
+  #     module Power
+  #       extend ActiveSupport::Concern
+  #       included do
+  #         supports :operation
+  #       end
+  #     end
+  #   end
+  #
+  extend ActiveSupport::Concern
+
+  def unsupported_reason(feature)
+    public_send("supports_#{feature}?") unless unsupported.key?(feature)
+    unsupported[feature]
+  end
+
+  private
+
+  def unsupported_reason_add(feature, reason)
+    unsupported[feature] = reason
+  end
+
+  def unsupported
+    @unsupported ||= {}
+  end
+
+  class_methods do
+    def unsupported_reason(feature)
+      public_send("supports_#{feature}?") unless unsupported.key?(feature)
+      unsupported[feature]
+    end
+
+    def supports(feature, &block)
+      send(:define_supports_methods, feature, true, &block)
+    end
+
+    def supports_not(feature, reason)
+      send(:define_supports_methods, feature, false, reason)
+    end
+
+    private
+
+    def unsupported
+      # This is a class variable and it might be modified during runtime
+      # because we dont eager load all classes at boot time, so it needs to be thread safe
+      @unsupported ||= Concurrent::Hash.new
+    end
+
+    def unsupported_reason_add(feature, reason)
+      unsupported[feature] = reason
+    end
+
+    def define_supports_methods(feature, is_supported, reason = nil, &block)
+      method_name = "supports_#{feature}?"
+
+      define_method(method_name) do
+        unsupported.delete(feature)
+        if block_given?
+          instance_eval(&block)
+        else
+          unsupported[feature] = reason unless is_supported
+        end
+        !unsupported.key?(feature)
+      end
+
+      define_singleton_method(method_name) do
+        unsupported.delete(feature)
+        # TODO: durandom - make reason evaluate in class context, to e.g. include the name of a subclass (.to_proc?)
+        unsupported[feature] = reason unless is_supported
+        !unsupported.key?(feature)
+      end
+    end
+  end
+end
