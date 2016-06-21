62f70f062fd9a9ca52609de961509c3cb0692919
Madhu Kanoor <mkanoor@redhat.com>
2016-06-21 10:22:44 -0400

    PR Review comments

--- db/fixtures/ae_datastore/ManageIQ/ConfigurationManagement/AnsibleTower/Operations/StateMachines/Job.class/__methods__/launch_ansible_job.rb
+++ db/fixtures/ae_datastore/ManageIQ/ConfigurationManagement/AnsibleTower/Operations/StateMachines/Job.class/__methods__/launch_ansible_job.rb
@@ -35,25 +35,23 @@ class LaunchAnsibleJob
   end
 
   def object_vars(object, ext_vars)
-    key_list = object.attributes.keys.select { |k| k.start_with?('param', 'dialog_param') }
-    key_list.each_with_object(ext_vars) do |key, hash|
+    object.attributes.each_with_object(ext_vars) do |(key, value), hash|
       if key.start_with?('param')
-        match_data = ANSIBLE_VAR_REGEX.match(object[key])
+        match_data = ANSIBLE_VAR_REGEX.match(value)
         hash[match_data[1].strip] ||= match_data[2] if match_data
       else
         match_data = ANSIBLE_DIALOG_VAR_REGEX.match(key)
-        hash[match_data[1]] = object[key] if match_data
+        hash[match_data[1]] = value if match_data
       end
     end
   end
 
   def ansible_vars_from_options(ext_vars)
     options = @handle.root["miq_provision"].try(:options) || {}
-    keys = options.keys.map(&:to_s)
-    key_list = keys.select { |k| k.start_with?('dialog_param') }
-    key_list.each_with_object(ext_vars) do |key, hash|
+    options.each_with_object(ext_vars) do |(k, v), hash|
+      key = k.to_s
       match_data = ANSIBLE_DIALOG_VAR_REGEX.match(key)
-      hash[match_data[1]] = options[key.to_sym] if match_data
+      hash[match_data[1]] = v if match_data
     end
   end
 
