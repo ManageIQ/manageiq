70528a8e1a3a89ebfe233870308c272eb8f0b9e0
Jillian Tullo <jtullo@redhat.com>
2016-06-21 10:24:50 -0400

    removes unnecessary code to edit - will just use the generic methods; adds ability to delete

--- app/controllers/api_controller/virtual_templates.rb
+++ app/controllers/api_controller/virtual_templates.rb
@@ -1,14 +1,5 @@
 class ApiController
   module VirtualTemplates
-    def update_virtual_templates
-      aname = parse_action_name
-      if aname == 'edit' && !api_user_role_allows?(aname)
-        render_normal_update :virtual_templates, update_collection(:virtual_templates, @req[:c_id])
-      else
-        update_generic(:virtual_templates)
-      end
-    end
-
     def create_resource_virtual_templates(_type, _id, data)
       validate_vendor_present(data)
       template_name = 'ManageIQ::Providers::' + data['vendor'].capitalize + '::CloudManager::VirtualTemplate'
@@ -18,10 +9,6 @@ class ApiController
         raise BadRequestError, "Failed to create a new virtual template - #{template.errors.full_messages.join(', ')}"
       end
       template
-    end
-
-    def edit_resource_virtual_templates(type, id, data)
-      edit_resource(type, id, data)
     end
 
     private
