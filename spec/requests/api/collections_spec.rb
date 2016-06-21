cbb7ecba33ea5b5f6828614d677b9ff4302f5f87
Tim Wade <hello@timjwade.com>
2016-06-21 09:08:31 -0700

    Don't reparse the api.yml file in tests
    Even though the config is memoized in the api spec helper, it is only
    memoized for the running of a single test.
    
    Use the Api::Config instead

--- spec/requests/api/collections_spec.rb
+++ spec/requests/api/collections_spec.rb
@@ -9,7 +9,7 @@ describe ApiController do
   end
 
   def test_collection_query(collection, collection_url, klass, attr = :id)
-    if collection_config.fetch_path(collection, :collection_actions, :get)
+    if Api::Settings.collections.fetch_path(collection, :collection_actions, :get)
       api_basic_authorize collection_action_identifier(collection, :read, :get)
     else
       api_basic_authorize
