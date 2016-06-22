83f7cce1c59a646b0ff1d2cd966d34a21090fc1f
Brandon Dunne <bdunne@redhat.com>
2016-06-22 10:18:45 -0400

    Create a mixin for textual_osinfo to address Identical code

--- app/helpers/vm_helper/textual_summary.rb
+++ app/helpers/vm_helper/textual_summary.rb
@@ -1,4 +1,5 @@
 module VmHelper::TextualSummary
+  include TextualMixins::TextualOsInfo
   include TextualMixins::TextualPatches
   # TODO: Determine if DoNav + url_for + :title is the right way to do links, or should it be link_to with :title
 
@@ -164,21 +165,6 @@ module VmHelper::TextualSummary
 
   def textual_tools_status
     {:label => _("Platform Tools"), :value => (@record.tools_status.nil? ? _("N/A") : @record.tools_status)}
-  end
-
-  def textual_osinfo
-    h = {:label => _("Operating System")}
-    os = @record.operating_system.nil? ? nil : @record.operating_system.product_name
-    if os.blank?
-      h[:value] = _("Unknown")
-    else
-      h[:image] = "os-#{@record.os_image_name.downcase}"
-      h[:value] = os
-      h[:title] = _("Show OS container information")
-      h[:explorer] = true
-      h[:link]  = url_for(:action => 'show', :id => @record, :display => 'os_info')
-    end
-    h
   end
 
   def textual_cpu_affinity
