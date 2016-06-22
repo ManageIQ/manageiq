6bd0d0406f6d035d0e8d1e8e8fb20c4e5cb4240d
lpichler <lpichler@redhat.com>
2016-06-22 10:56:54 +0200

    Use shared examples 'column migration' for similar specs

--- spec/migrations/20150724030353_namespace_ems_foreman_spec.rb
+++ spec/migrations/20150724030353_namespace_ems_foreman_spec.rb
@@ -4,25 +4,12 @@ describe NamespaceEmsForeman do
   class NamespaceEmsForeman::ExtManagementSystem < ActiveRecord::Base
     self.inheritance_column = :_type_disabled # disable STI
   end
-  let(:ems_stub) { migration_stub(:ExtManagementSystem) }
 
   migration_context :up do
-    it "migrates a representative row" do
-      ems = ems_stub.create!(:type => "ConfigurationManagerForeman")
-
-      migrate
-
-      expect(ems.reload).to have_attributes(:type => "ManageIQ::Providers::Foreman::ConfigurationManager")
-    end
+    include_examples "column migration", :type, :ExtManagementSystem, described_class::NAME_MAP.first
   end
 
   migration_context :down do
-    it "migrates a representative row" do
-      ems = ems_stub.create!(:type => "ManageIQ::Providers::Foreman::ConfigurationManager")
-
-      migrate
-
-      expect(ems.reload).to have_attributes(:type => "ConfigurationManagerForeman")
-    end
+    include_examples "column migration", :type, :ExtManagementSystem, described_class::NAME_MAP.invert.first
   end
 end
