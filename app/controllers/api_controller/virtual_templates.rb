4d5a63fc708735aeed7c0e290ebb62ac0abfc284
Jillian Tullo <jtullo@redhat.com>
2016-06-23 13:38:13 -0400

    adding clarity to variable names

--- app/controllers/api_controller/virtual_templates.rb
+++ app/controllers/api_controller/virtual_templates.rb
@@ -3,12 +3,13 @@ class ApiController
     def create_resource_virtual_templates(type, _id, data)
       validate_vendor_present(data)
       klass = collection_class(type)
-      vt_type = type_from_vendor(klass, data['vendor'])
-      template = klass.create(data.merge(:type => vt_type))
-      if template.invalid?
-        raise BadRequestError, "Failed to create a new virtual template - #{template.errors.full_messages.join(', ')}"
+      virtual_template_type = type_from_vendor(klass, data['vendor'])
+      virtual_template = klass.create(data.merge(:type => virtual_template_type))
+      if virtual_template.invalid?
+        raise BadRequestError,
+              "Failed to create a new virtual template - #{virtual_template.errors.full_messages.join(', ')}"
       end
-      template
+      virtual_template
     end
 
     private
