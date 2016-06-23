b7681d35a1cefe232c24826e3b350da23816ae08
Andrey Veselov <aveselov@redhat.com>
2016-06-23 15:33:30 +0200

    Add routes for subnet topology

--- config/routes.rb
+++ config/routes.rb
@@ -815,6 +815,14 @@ Vmdb::Application.routes.draw do
         data
       )
     },
+
+    :subnet_topology         => {
+      :get => %w(
+        show
+        data
+      )
+    },
+
     :container_dashboard      => {
       :get => %w(
         show
