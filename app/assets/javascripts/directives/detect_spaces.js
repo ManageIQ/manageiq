8be60b347f0b71bec16a16c5325ac4d3f8272551
Aparna Karve <akarve@redhat.com>
2016-06-23 12:08:25 -0700

    Do not return false on the directive on an undefined viewValue

--- app/assets/javascripts/directives/detect_spaces.js
+++ app/assets/javascripts/directives/detect_spaces.js
@@ -2,8 +2,8 @@ ManageIQ.angular.app.directive('detectSpaces', function() {
   return {
     require: 'ngModel',
     link: function (_scope, _elem, _attrs, ctrl) {
-      ctrl.$validators.detectedSpaces = function (_modelValue, viewValue) {
-        if (angular.isDefined(viewValue) && !detectedSpaces(viewValue)) {
+      ctrl.$validators.detectedSpaces = function (modelValue, viewValue) {
+        if (!angular.isDefined(viewValue) || (angular.isDefined(viewValue) && !detectedSpaces(viewValue))) {
           return true;
         }
         return false;
