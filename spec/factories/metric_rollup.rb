fec261a36e91138686bec31208dee46c3b53a9a1
Yuri Rudman <yrudman@redhat.com>
2016-06-23 10:52:47 -0400

    added spec for MiqReport::Generator#generate on 'Host Memory Trend (last week)' report
    $ style
    
    $ typo

--- spec/factories/metric_rollup.rb
+++ spec/factories/metric_rollup.rb
@@ -27,7 +27,7 @@ FactoryGirl.define do
     capture_interval_name "hourly"
     resource_type         "Host"
   end
-  
+
   factory :metric_rollup_host_daily, :parent => :metric_rollup, :class => :MetricRollup do
     capture_interval_name "daily"
     resource_type         "Host"
---
+++ spec/models/miq_report/generator_spec.rb
@@ -0,0 +1,83 @@
+describe MiqReport::Generator do
+  before do
+    EvmSpecHelper.local_miq_server
+    @user = FactoryGirl.create(:user_with_group)
+    @time_profile_all = FactoryGirl.create(:time_profile_with_rollup, :tz => "UTC")
+    @host1 = FactoryGirl.create(:host)
+  end
+
+  describe "#generate" do
+    context "Memory Utilization Trends report (daily)" do
+      before :each do
+        @miq_report_profile_all = FactoryGirl.create(
+          :miq_report,
+          :db              => "VimPerformanceTrend",
+          :order           => "Ascending",
+          :sortby          => ["resource_name"],
+          :time_profile_id => @time_profile_all.id,
+          :db_options      => {:limit_col    => "max_derived_memory_available",
+                               :trend_col    => "derived_memory_used",
+                               :rpt_type     => "trend",
+                               :start_offset => 604_800,
+                               :end_offset   => 0,
+                               :trend_db     => "HostPerformance",
+                               :interval     => "daily",
+                               :target_pcts  => [70, 80, 90]})
+      end
+
+      it "returns one row for each host" do
+        used_mem_up = [700, 600, 500, 400]
+        @host2 = FactoryGirl.create(:host)
+        create_rollup(@host1, @time_profile_all, used_mem_up)
+        create_rollup(@host2, @time_profile_all, used_mem_up)
+        @miq_report_profile_all.generate_table(:userid => @user.userid)
+        expect(@miq_report_profile_all.table.data.size).to eq(2)
+      end
+
+      it "calculates 'UP' trend" do
+        used_mem_up = [700, 600, 500, 400]
+        create_rollup(@host1, @time_profile_all, used_mem_up)
+        @miq_report_profile_all.generate_table(:userid => @user.userid)
+        trend_direction = @miq_report_profile_all.table.data[0].data['direction_of_trend']
+        expect(trend_direction).to eq("Up")
+      end
+
+      it "calculates 'Down' trend" do
+        used_mem_down = [300, 400, 500, 600]
+        create_rollup(@host1, @time_profile_all, used_mem_down)
+        @miq_report_profile_all.generate_table(:userid => @user.userid)
+        trend_direction = @miq_report_profile_all.table.data[0].data['direction_of_trend']
+        expect(trend_direction).to eq("Down")
+      end
+
+      it "calculates 'Flat' trend" do
+        used_mem_flat = [302, 300, 300, 302]
+        create_rollup(@host1, @time_profile_all, used_mem_flat)
+        @miq_report_profile_all.generate_table(:userid => @user.userid)
+        trend_direction = @miq_report_profile_all.table.data[0].data['direction_of_trend']
+        expect(trend_direction).to eq("Flat")
+      end
+
+      it "calculates max and min trend values" do
+        used_mem_up = [700, 600, 500, 400]
+        create_rollup(@host1, @time_profile_all, used_mem_up)
+        @miq_report_profile_all.generate_table(:userid => @user.userid)
+        report_min = @miq_report_profile_all.table.data[0].data['min_trend_value']
+        report_max = @miq_report_profile_all.table.data[0].data['max_trend_value']
+        expect(report_min).to eq(used_mem_up.min)
+        expect(report_max).to eq(used_mem_up.max)
+      end
+    end
+
+    def create_rollup(host, profile, used_mem)
+      day_midnight = Time.zone.yesterday.beginning_of_day
+      used_mem.size.times do |i|
+        host.metric_rollups << FactoryGirl.create(:metric_rollup_host_daily,
+                                                  :timestamp                => day_midnight - i.day,
+                                                  :time_profile_id          => profile.id,
+                                                  :derived_memory_used      => used_mem[i],
+                                                  :derived_memory_available => 1400)
+      end
+    end
+  end
+end
