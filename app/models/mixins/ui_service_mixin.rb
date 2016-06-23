fc30786ba85f79dbeca11bb8f91182989d4a62ff
Jirka Kremser <jkremser@redhat.com>
2016-06-23 10:58:49 +0200

    Adding middleware datasource entity to the topology graph + tests

--- app/models/mixins/ui_service_mixin.rb
+++ app/models/mixins/ui_service_mixin.rb
@@ -1,19 +1,20 @@
 module UiServiceMixin
   def icons
     {
-      :ContainerReplicator => {:type => "glyph", :icon => "\uE624", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-replicator
-      :ContainerGroup      => {:type => "glyph", :icon => "\uF1B3", :fontfamily => "FontAwesome"},             # fa-cubes
-      :ContainerNode       => {:type => "glyph", :icon => "\uE621", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-container-node
-      :ContainerService    => {:type => "glyph", :icon => "\uE61E", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-service
-      :ContainerRoute      => {:type => "glyph", :icon => "\uE625", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-route
-      :Container           => {:type => "glyph", :icon => "\uF1B2", :fontfamily => "FontAwesome"},             # fa-cube
-      :Host                => {:type => "glyph", :icon => "\uE600", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-screen
-      :Vm                  => {:type => "glyph", :icon => "\uE90f", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-virtual-machine
-      :Kubernetes          => {:type => "image", :icon => provider_icon(:Kubernetes)},
-      :Openshift           => {:type => "image", :icon => provider_icon(:Openshift)},
-      :OpenshiftEnterprise => {:type => "image", :icon => provider_icon(:OpenshiftEnterprise)},
-      :Atomic              => {:type => "image", :icon => provider_icon(:Atomic)},
-      :AtomicEnterprise    => {:type => "image", :icon => provider_icon(:AtomicEnterprise)}
+      :ContainerReplicator  => {:type => "glyph", :icon => "\uE624", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-replicator
+      :ContainerGroup       => {:type => "glyph", :icon => "\uF1B3", :fontfamily => "FontAwesome"},             # fa-cubes
+      :ContainerNode        => {:type => "glyph", :icon => "\uE621", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-container-node
+      :ContainerService     => {:type => "glyph", :icon => "\uE61E", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-service
+      :ContainerRoute       => {:type => "glyph", :icon => "\uE625", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-route
+      :Container            => {:type => "glyph", :icon => "\uF1B2", :fontfamily => "FontAwesome"},             # fa-cube
+      :Host                 => {:type => "glyph", :icon => "\uE600", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-screen
+      :Vm                   => {:type => "glyph", :icon => "\uE90f", :fontfamily => "PatternFlyIcons-webfont"}, # pficon-virtual-machine
+      :MiddlewareDatasource => {:type => "glyph", :icon => "\uF1C0", :fontfamily => "FontAwesome"},             # fa-database
+      :Kubernetes           => {:type => "image", :icon => provider_icon(:Kubernetes)},
+      :Openshift            => {:type => "image", :icon => provider_icon(:Openshift)},
+      :OpenshiftEnterprise  => {:type => "image", :icon => provider_icon(:OpenshiftEnterprise)},
+      :Atomic               => {:type => "image", :icon => provider_icon(:Atomic)},
+      :AtomicEnterprise     => {:type => "image", :icon => provider_icon(:AtomicEnterprise)},
     }
   end
 
--- spec/javascripts/controllers/middleware/middleware_topology_controller_spec.js
+++ spec/javascripts/controllers/middleware/middleware_topology_controller_spec.js
@@ -3,39 +3,47 @@ describe('middlewareTopologyController', function () {
   var mock_data = getJSONFixture('middleware_topology_response.json');
 
   var mw_manager = {
-    id: "1", item: {
-      "name": "Hawkular",
+    id: "MiddlewareManager1", item: {
+      "name": "livingontheedge.hawkular.org",
       "kind": "MiddlewareManager",
       "miq_id": 1,
       "status": "Unknown",
       "display_kind": "Hawkular",
-      "icon": "vendor-hawkular",
-      "id": "1"
+      "icon": "vendor-hawkular"
     }
   };
 
   var mw_server = {
-    id: "/t;28026b36-8fe4-4332-84c8-524e173a68bf/f;91044f79-156e-4076-8056-ac3ac44ffff9/r;Local~~", item: {
+    id: "MiddlewareServer1", item: {
       "name": "Local",
       "kind": "MiddlewareServer",
-      "miq_id": 4,
+      "miq_id": 1,
       "status": "Unknown",
       "display_kind": "MiddlewareServer",
-      "icon": "vendor-hawkular",
-      "id": "Local~~"
+      "icon": "vendor-hawkular"
     }
   };
 
   var mw_deployment = {
-    id: "/t;28026b36-8fe4-4332-84c8-524e173a68bf/f;91044f79-156e-4076-8056-ac3ac44ffff9/r;Local~~/r;Local~%2Fdeployment%3Dhawkular-command-gateway-war.war",
+    id: "MiddlewareDeployment1",
     item: {
       "name": "hawkular-command-gateway-war.war",
       "kind": "MiddlewareDeployment",
-      "miq_id": 49,
+      "miq_id": 1,
       "status": "Unknown",
       "display_kind": "MiddlewareDeployment",
-      "icon": "middleware_deployment_war",
-      "id": "Local~/deployment=hawkular-command-gateway-war.war"
+      "icon": "middleware_deployment_war"
+    }
+  };
+
+  var mw_datasource = {
+    id: "MiddlewareDatasource1",
+    item: {
+      "name": "ExampleDS",
+      "kind": "MiddlewareDatasource",
+      "miq_id": 1,
+      "status": "Unknown",
+      "display_kind": "MiddlewareDatasource"
     }
   };
 
@@ -65,28 +73,42 @@ describe('middlewareTopologyController', function () {
     });
   });
 
+
   describe('kinds contains all expected mw kinds', function () {
     it('in all main objects', function () {
-      expect(Object.keys(scope.kinds).length).toBe(3);
+      expect(Object.keys(scope.kinds).length).toBe(4);
       expect(scope.kinds["MiddlewareManager"]).toBeDefined();
       expect(scope.kinds["MiddlewareServer"]).toBeDefined();
       expect(scope.kinds["MiddlewareDeployment"]).toBeDefined();
+      expect(scope.kinds["MiddlewareDatasource"]).toBeDefined();
     });
   });
 
+
   describe('the mw topology gets correct icons', function () {
     it('in graph elements', function () {
-      expect($controller.class_name(mw_manager)).toContain("vendor-hawkular");
-      expect($controller.class_name(mw_server)).toContain("vendor-hawkular");
-      expect($controller.class_name(mw_deployment)).toContain("middleware_deployment_war");
+      expect($controller.getIcon(mw_manager).icon).toContain("vendor-hawkular");
+      expect($controller.getIcon(mw_server).icon).toContain("vendor-hawkular");
+      expect($controller.getIcon(mw_deployment).icon).toContain("middleware_deployment_war");
+      expect($controller.getIcon(mw_datasource).fontfamily).toEqual("FontAwesome")
     });
   });
 
   describe('dimensions are returned correctly', function () {
     it('for all objects', function () {
-      expect($controller.getDimensions(mw_manager)).toEqual({x: -20, y: -20, height: 40, width: 40, r: 28});
-      expect($controller.getDimensions(mw_server)).toEqual({x: -12, y: -12, height: 23, width: 23, r: 19});
-      expect($controller.getDimensions(mw_deployment)).toEqual({x: -9, y: -9, height: 18, width: 18, r: 17});
+      expect($controller.getCircleDimensions(mw_manager)).toEqual({x: -20, y: -20, height: 40, width: 40, r: 28});
+      expect($controller.getCircleDimensions(mw_server)).toEqual({x: -12, y: -12, height: 23, width: 23, r: 19});
+      expect($controller.getCircleDimensions(mw_deployment)).toEqual({x: -9, y: -9, height: 18, width: 18, r: 17});
+      expect($controller.getCircleDimensions(mw_datasource)).toEqual({x: -9, y: -9, height: 18, width: 18, r: 17});
+    });
+  });
+
+  describe('icon types are returned correctly', function () {
+    it('for all objects', function () {
+      expect($controller.getIcon(mw_manager).type).toEqual("image");
+      expect($controller.getIcon(mw_server).type).toEqual("image");
+      expect($controller.getIcon(mw_deployment).type).toEqual("image");
+      expect($controller.getIcon(mw_datasource).type).toEqual("glyph");
     });
   });
 
