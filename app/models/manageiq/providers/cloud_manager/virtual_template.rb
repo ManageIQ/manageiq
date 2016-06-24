e9fc746ffcb315dedfaa9f336a46f765848a1797
Jillian Tullo <jtullo@redhat.com>
2016-06-24 13:47:02 -0400

    improving validation functions

--- app/models/manageiq/providers/cloud_manager/virtual_template.rb
+++ app/models/manageiq/providers/cloud_manager/virtual_template.rb
@@ -1,14 +1,16 @@
 class ManageIQ::Providers::CloudManager::VirtualTemplate < ::MiqTemplate
-  validate :single_template, :on => :create
+  validate :validate_single_template, :on => :create
   default_value_for :cloud, true
 
   TYPES = {
     :amazon => 'ManageIQ::Providers::Amazon::CloudManager::VirtualTemplate'
   }.freeze
 
-  def single_template
-    multiple = !type.constantize.where(:type => type).empty?
-    errors.add(:virtual_template, 'may only have one per type') if multiple
-    multiple
+  def single_template?
+    type.constantize.where(:type => type).empty?
+  end
+
+  def validate_single_template
+    errors.add(:virtual_template, 'may only have one per type') unless single_template?
   end
 end
