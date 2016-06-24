3abd2691854f2eea3329cc23c9367ec892f2218a
Jillian Tullo <jtullo@redhat.com>
2016-06-24 11:34:29 -0400

    updating specs to clarify name of factories.

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-amazon-cloud_manager-virtual_template.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-amazon-cloud_manager-virtual_template.rb
@@ -1,5 +1,4 @@
 module MiqAeMethodService
-  class MiqAeServiceManageIQ_Providers_Amazon_CloudManager_VirtualTemplate <
-      MiqAeServiceManageIQ_Providers_CloudManager_Template
+  class MiqAeServiceManageIQ_Providers_Amazon_CloudManager_VirtualTemplate < MiqAeServiceManageIQ_Providers_CloudManager_Template
   end
 end
--- spec/factories/virtual_template.rb
+++ spec/factories/virtual_template.rb
@@ -1,16 +1,16 @@
 FactoryGirl.define do
   factory :virtual_template, :class => 'ManageIQ::Providers::CloudManager::VirtualTemplate' do
-    vendor 'amazon'
-    name 'vt'
-    location 'us-west-1'
-    ems_ref 'i-12345'
-    availability_zone_id 0
-    cloud_subnet_id 1
-    cloud_network_id 2
-    type 'ManageIQ::Providers::Amazon::CloudManager::VirtualTemplate'
+    vendor                  'amazon'
+    name                    'vt'
+    location                'us-west-1'
+    ems_ref                 'i-12345'
+    availability_zone_id     0
+    cloud_subnet_id          1
+    cloud_network_id         2
+    type                     'ManageIQ::Providers::CloudManager::VirtualTemplate'
 
-    trait :generic do
-      type 'ManageIQ::Providers::CloudManager::VirtualTemplate'
+    trait :amazon do
+      type 'ManageIQ::Providers::Amazon::CloudManager::VirtualTemplate'
     end
   end
 end
