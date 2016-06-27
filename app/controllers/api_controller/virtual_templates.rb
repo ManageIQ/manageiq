c9eab2e8677d11b094f6e5e72c394692ed058322
Jillian Tullo <jtullo@redhat.com>
2016-06-27 08:28:26 -0400

    instead of deriving the type from the vendor, will just take in the type

--- app/controllers/api_controller/virtual_templates.rb
+++ app/controllers/api_controller/virtual_templates.rb
@@ -3,8 +3,7 @@ class ApiController
     def create_resource_virtual_templates(type, _id, data)
       validate_vendor_present(data)
       klass = collection_class(type)
-      virtual_template_type = type_from_vendor(klass, data['vendor'])
-      virtual_template = klass.create(data.merge(:type => virtual_template_type))
+      virtual_template = klass.create(data)
       if virtual_template.invalid?
         raise BadRequestError,
               "Failed to create a new virtual template - #{virtual_template.errors.full_messages.join(', ')}"
@@ -16,12 +15,6 @@ class ApiController
 
     def validate_vendor_present(data)
       raise BadRequestError, 'Must specify a vendor for creating a Virtual Template' unless data['vendor']
-    end
-
-    def type_from_vendor(klass, vendor)
-      type = klass::TYPES[vendor.to_sym]
-      raise BadRequestError, 'Must specify a supported type' unless type
-      type
     end
   end
 end
--- app/models/manageiq/providers/cloud_manager/virtual_template.rb
+++ app/models/manageiq/providers/cloud_manager/virtual_template.rb
@@ -2,10 +2,6 @@ class ManageIQ::Providers::CloudManager::VirtualTemplate < ::MiqTemplate
   validate :validate_single_template, :on => :create
   default_value_for :cloud, true
 
-  TYPES = {
-    :amazon => 'ManageIQ::Providers::Amazon::CloudManager::VirtualTemplate'
-  }.freeze
-
   def single_template?
     type.constantize.where(:type => type).empty?
   end
