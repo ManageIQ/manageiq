a3c6eb563c5204aabd8b9d15a2015454db710ea9
Greg McCullough <gmccullo@redhat.com>
2016-06-30 08:46:36 -0400

    Merge pull request #9478 from himdel/api-service-order-placed
    ServiceOrder#checkout* - save checkout date in placed_at
    (cherry picked from commit b464bf54f59ee6a3a197a1f701e56a90251f0e03)

--- app/models/service_order.rb
+++ app/models/service_order.rb
@@ -41,13 +41,15 @@ class ServiceOrder < ApplicationRecord
     raise "Invalid operation [checkout] for Service Order in state [#{state}]" if ordered?
     _log.info("Service Order checkout for service: #{name}")
     process_checkout(miq_requests)
-    update_attributes(:state => STATE_ORDERED)
+    update_attributes(:state     => STATE_ORDERED,
+                      :placed_at => Time.zone.now)
   end
 
   def checkout_immediately
     _log.info("Service Order checkout immediately for service: #{name}")
     process_checkout(miq_requests)
-    update_attributes(:state => STATE_ORDERED)
+    update_attributes(:state     => STATE_ORDERED,
+                      :placed_at => Time.zone.now)
   end
 
   def process_checkout(miq_requests)
