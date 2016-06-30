ba028319a2a0866c7058132b23dd68f46f01213a
Jillian Tullo <jtullo@redhat.com>
2016-06-30 13:49:11 -0400

    adds in check for href and id into controller request; updates tests

--- app/controllers/api_controller/virtual_templates.rb
+++ app/controllers/api_controller/virtual_templates.rb
@@ -1,9 +1,8 @@
 class ApiController
   module VirtualTemplates
     def create_resource_virtual_templates(type, _id, data)
-      validate_vendor_present(data)
-      klass = collection_class(type)
-      virtual_template = klass.create(data)
+      validate_data(data)
+      virtual_template = collection_class(type).create(data)
       if virtual_template.invalid?
         raise BadRequestError,
               "Failed to create a new virtual template - #{virtual_template.errors.full_messages.join(', ')}"
@@ -13,7 +12,10 @@ class ApiController
 
     private
 
-    def validate_vendor_present(data)
+    def validate_data(data)
+      if data.key?('id') || data.key?('href')
+        raise BadRequestError, 'Resource id or href should not be specified for creating a new virtual template'
+      end
       raise BadRequestError, 'Must specify a vendor for creating a Virtual Template' unless data['vendor']
     end
   end
