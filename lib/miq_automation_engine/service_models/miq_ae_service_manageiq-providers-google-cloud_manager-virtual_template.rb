ba3587dc2670bb097e1d43ab71ca2a8421f837b7
Jillian Tullo <jtullo@redhat.com>
2016-06-30 13:52:53 -0400

    disable rubocop Metrics/LineLength

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-google-cloud_manager-virtual_template.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-google-cloud_manager-virtual_template.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Metrics/LineLength
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Google_CloudManager_VirtualTemplate < MiqAeServiceManageIQ_Providers_CloudManager_Template
   end
