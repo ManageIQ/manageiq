aa4b59ac02e7c13e7cf9ef8381f6fcd2eeacca92
Šimon Lukašík <isimluk@fedoraproject.org>
2016-06-30 16:17:24 +0200

    Specs for /api/git_repositories

---
+++ spec/requests/api/git_repositories_spec.rb
@@ -0,0 +1,79 @@
+#
+# Rest API Request Tests - Git Repositories specs
+#
+# - Creating                              /api/git_repositories                 POST
+# - Creating a via action                 /api/git_repositories_url             action "create"
+#
+describe ApiController do
+  CREDENTIALS_ATTR = ApiController::GitRepositories::CREDENTIALS_ATTR
+  let(:expected_attributes) { %w(id name verify_ssl url) }
+  let(:credentials) do
+    {
+      'verify_ssl' => 0,
+      'userid'     => 'pink',
+      'password'   => 'unicorn'
+    }
+  end
+  let(:sample_git) do
+    {
+      'name' => 'Qilin',
+      'url'  => 'https://its.my/little/pony'
+    }
+  end
+  let(:sample_git_credentials) { sample_git.merge(CREDENTIALS_ATTR => credentials) }
+
+  describe 'Git repository creation' do
+    it 'rejects creation without appropriate role' do
+      api_basic_authorize
+      run_post(git_repositories_url, sample_git)
+
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'rejects creation with id specified' do
+      api_basic_authorize collection_action_identifier(:git_repositories, :create)
+      run_post(git_repositories_url, sample_git.merge('id' => 1))
+
+      expect_bad_request(/id or href should not be specified/i)
+    end
+
+    def expect_sample_git_creation_response
+      expect(response).to have_http_status(:ok)
+      expect_result_resources_to_include_keys('results', expected_attributes)
+      expect_results_to_match_hash('results', [sample_git])
+      expect(response_hash['results'].first['verify_ssl']).to eq(credentials['verify_ssl'])
+      git_id = response_hash['results'].first['id']
+      expect(GitRepository.exists?(git_id)).to be_truthy
+      git = GitRepository.find(git_id)
+      expect(git.authentications.size).to eq(1)
+      expect(git.authentications[0].authtype).to eq('password')
+      %w(userid password).each do |i|
+        expect(git.authentications[0].send(i)).to eq(credentials[i])
+      end
+    end
+
+    it 'supports single git repository creation' do
+      api_basic_authorize collection_action_identifier(:git_repositories, :create)
+      run_post(git_repositories_url, sample_git_credentials)
+      expect_sample_git_creation_response
+    end
+
+    it 'supports single git repository creation via action' do
+      api_basic_authorize collection_action_identifier(:git_repositories, :create)
+      run_post(git_repositories_url, gen_request(:create, sample_git_credentials))
+      expect_sample_git_creation_response
+    end
+    it 'supports single git repository creation without credentials' do
+      api_basic_authorize collection_action_identifier(:git_repositories, :create)
+      run_post(git_repositories_url, sample_git)
+      expect(response).to have_http_status(:ok)
+      expect_result_resources_to_include_keys('results', expected_attributes)
+      expect_results_to_match_hash('results', [sample_git])
+      expect(response_hash['results'].first['verify_ssl']).to eq(1)
+      git_id = response_hash['results'].first['id']
+      expect(GitRepository.exists?(git_id)).to be_truthy
+      git = GitRepository.find(git_id)
+      expect(git.authentications.size).to eq(0)
+    end
+  end
+end
