1e79b6a118ffdb1f5b0df5374e58206719bbdb13
Patrik Kománek <pkomanek@redhat.com>
2016-07-04 14:09:55 +0200

    Fixing sorting in Ansible Tower Configuration Manager
    Sorting broken by Total Configured Systems. There was missing virtual
    column total_configured_systems. Solved by adding virtual column to
    InventoryGroup model where Rbac searching happens.

--- app/models/manageiq/providers/configuration_manager/inventory_group.rb
+++ app/models/manageiq/providers/configuration_manager/inventory_group.rb
@@ -1,3 +1,9 @@
 class ManageIQ::Providers::ConfigurationManager::InventoryGroup < EmsFolder
   belongs_to :manager, :foreign_key => "ems_id", :class_name => "ManageIQ::Providers::ConfigurationManager"
+
+  virtual_column :total_configured_systems, :type => :integer
+
+  def total_configured_systems
+    Rbac.filtered(configured_systems).count
+  end
 end
