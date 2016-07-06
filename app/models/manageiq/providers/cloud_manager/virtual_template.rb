04d4f7dba7b07c028a72c5a611147465a95f78e2
Jillian Tullo <jtullo@redhat.com>
2016-07-06 10:03:17 -0400

    initial commit of updates to virtual template to create a request

--- app/models/manageiq/providers/cloud_manager/virtual_template.rb
+++ app/models/manageiq/providers/cloud_manager/virtual_template.rb
@@ -11,4 +11,27 @@ class ManageIQ::Providers::CloudManager::VirtualTemplate < ::MiqTemplate
   def validate_single_template
     errors.add(:virtual_template, _('may only have one per type')) unless single_template?
   end
+
+  # TODO: take in a name for the VM
+  def vm_fields
+    {
+      'placement_auto' => false,
+      'placement_availability_zone' => availability_zone_id,
+      'cloud_network'               => cloud_network_id,
+      'cloud_subnet'                => cloud_subnet_id,
+      'number_of_vms' => 1,
+      'retirement' => 0,
+      'vm_name' => 'arandomname',
+      'boot_disk_size' => "10.GB",
+      'instance_type' => flavor_id
+    }
+  end
+
+  def template_fields
+    {
+      'guid' => guid,
+      'name' => name,
+      'request_type' => 'template'.freeze
+     }
+  end
 end
