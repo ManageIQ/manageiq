8ab357067483cb436e33ef428292ecf3d6874a8b
Jillian Tullo <jtullo@redhat.com>
2016-07-07 11:21:17 -0400

    updating provision to be a subclass /provision

--- app/controllers/api_controller/virtual_template_provision.rb
+++ app/controllers/api_controller/virtual_template_provision.rb
@@ -1,12 +1,11 @@
 class ApiController
   module VirtualTemplateProvision
-    def create_resource_virtual_template_provision(type, _id, data)
-      virtual_template        = collection_class(type).find(data['template_id'])
+    def provision_create_resource(parent, _type, _id, data)
       version_str             = data['version'] || '1.1'.freeze
-      template_fields         = template_fields(virtual_template)
-      vm_fields               = vm_fields(virtual_template).merge('vm_name' => data['vm_name'])
+      template_fields         = template_fields(parent)
+      vm_fields               = vm_fields(parent).merge('vm_name' => data['vm_name'], 'number_of_vms' => data['number_of_vms'])
       requester               = data['requester']
-      tags                    = virtual_template.tags
+      tags                    = parent.tags
 
       MiqProvisionVirtWorkflow.from_ws(version_str, @auth_user_obj, template_fields, vm_fields, requester, tags,
                                        nil, nil, nil)
@@ -16,13 +15,9 @@ class ApiController
 
     def vm_fields(template)
       {
-        'placement_auto'              => false,
         'placement_availability_zone' => template.availability_zone_id,
         'cloud_network'               => template.cloud_network_id,
         'cloud_subnet'                => template.cloud_subnet_id,
-        'number_of_vms'               => 1,
-        'retirement'                  => 0,
-        'boot_disk_size'              => "10.GB",
         'instance_type'               => template.flavor_id
       }
     end
