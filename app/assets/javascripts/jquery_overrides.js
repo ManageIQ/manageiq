eb1fc98e89e55f5018d572ee0ac8ea38ad82e917
Martin Povolny <mpovolny@redhat.com>
2016-07-07 13:20:04 +0200

    ExplorerPresenter: Hook handling of GET requests too.
    GET requests after redirect from POST request where not handled
    properly.
    
    Example of such requests is table switching in Report editor.

--- app/assets/javascripts/jquery_overrides.js
+++ app/assets/javascripts/jquery_overrides.js
@@ -12,16 +12,29 @@ jQuery.evalLog = function (text) {
   return text;
 };
 
+jQuery.jsonPayload = function (text) {
+  var parsed_json = jQuery.parseJSON(text);
+  if (parsed_json['explorer']) {
+    return ManageIQ.explorer.process(parsed_json); // ExplorerPresenter payload
+  } else {
+    return text;
+  }
+};
+
 $.ajaxSetup({
+  accepts: {
+    json: "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript"
+  },
+  contents: {
+    json: /application\/json/
+  },
   converters: {
-    "text script": function(text) {
-      if (text.match(/^{/)) { // JSON payload
-        var parsed_json = jQuery.parseJSON(text);
-        if (parsed_json['explorer']) {
-          return ManageIQ.explorer.process(parsed_json); // ExplorerPresenter payload
-        } else {
-          return null;
-        }
+    "text json": function (text) {
+      return jQuery.jsonPayload(text);
+    },
+    "text script": function (text) {
+      if (text.match(/^{/)) {
+        return jQuery.jsonPayload(text);
       } else { // JavaScript payload
         jQuery.evalLog.call(this, text);
         return text;
