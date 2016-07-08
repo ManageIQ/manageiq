c04c1fcc870873d69c27405c78796aeec78089b5
Nick Carboni <ncarboni@redhat.com>
2016-07-08 14:44:26 -0400

    Remove replication worker

--- app/models/miq_server/worker_management/monitor/class_names.rb
+++ app/models/miq_server/worker_management/monitor/class_names.rb
@@ -55,7 +55,6 @@ module MiqServer::WorkerManagement::Monitor::ClassNames
     MiqGenericWorker
     MiqNetappRefreshWorker
     MiqPriorityWorker
-    MiqReplicationWorker
     MiqReportingWorker
     MiqScheduleWorker
     MiqSmartProxyWorker
@@ -84,7 +83,6 @@ module MiqServer::WorkerManagement::Monitor::ClassNames
     ManageIQ::Providers::Openstack::InfraManager::MetricsCollectorWorker
     MiqReportingWorker
     MiqSmartProxyWorker
-    MiqReplicationWorker
     MiqGenericWorker
     MiqEventHandler
     MiqSmisRefreshWorker
