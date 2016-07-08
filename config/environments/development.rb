2aace1924a41e4fec8d82b6da5e3d83d702a8629
Martin Povolny <mpovolny@redhat.com>
2016-07-08 10:00:18 +0200

    Add environment 'i18n' for i18n rake tasks.
    We need to allow eager load to collect model attributes and virtual
    columns for the catalog.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Vmdb::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.eager_load_paths = []
+
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
