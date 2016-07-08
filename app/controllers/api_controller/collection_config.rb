6a143514af821412b065fba04da28f59b018b788
Šimon Lukašík <isimluk@fedoraproject.org>
2016-07-08 15:00:07 +0200

    Refactor: Delegate local_uris_for_feature to CollectionConfig
    And name it names_for_feature as we later will have similar method
    returning name for given klass.

--- app/controllers/api_controller/collection_config.rb
+++ app/controllers/api_controller/collection_config.rb
@@ -4,5 +4,19 @@ class ApiController
       cspec = self[collection_name.to_sym]
       cspec && cspec[:options].include?(:custom_actions)
     end
+
+    def names_for_feature(product_feature_name)
+      names_for_features[product_feature_name]
+    end
+
+    private
+
+    def names_for_features
+      @names_for_features ||= each_with_object(Hash.new { |h, k| h[k] = [] }) do |(collection, cspec), result|
+        ident = cspec[:identifier]
+        next unless ident
+        result[ident] << collection
+      end
+    end
   end
 end
