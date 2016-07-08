50861dd6ad3d2605af720e60f66602ba23fd3f0c
Jillian Tullo <jtullo@redhat.com>
2016-07-08 09:06:36 -0400

    updates product feature spec

--- spec/models/miq_product_feature_spec.rb
+++ spec/models/miq_product_feature_spec.rb
@@ -2,7 +2,7 @@ require 'tmpdir'
 require 'pathname'
 
 describe MiqProductFeature do
-  let(:expected_feature_count) { 1060 }
+  let(:expected_feature_count) { 1062 }
 
   # - container_dashboard
   # - miq_report_widget_editor
