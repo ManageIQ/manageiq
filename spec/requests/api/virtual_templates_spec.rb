414c4d8f537ca8df2ba3e3baf57888281b9bc731
Jillian Tullo <jtullo@redhat.com>
2016-07-08 11:34:55 -0400

    updating specs to reflect changes

--- spec/requests/api/virtual_templates_spec.rb
+++ spec/requests/api/virtual_templates_spec.rb
@@ -222,4 +222,62 @@ RSpec.describe 'Virtual Template API' do
       expect(response).to have_http_status(:not_found)
     end
   end
+
+  context 'virtual template provision' do
+    let(:virtual_template) do
+      FactoryGirl.create(:virtual_template_google,
+                         :ems_id               => ems.id,
+                         :availability_zone_id => availability_zone.id,
+                         :cloud_network_id     => cloud_network.id,
+                         :flavor_id            => flavor.id)
+    end
+    let(:dialog) { FactoryGirl.create(:miq_dialog_provision) }
+    let(:request) do
+      {
+        'action'    => 'provision',
+        'vm_name'   => 'VirtualTemplate',
+        'requester' => {
+          'owner_first_name' => 'First',
+          'owner_last_name'  => 'Last',
+          'owner_email'      => 'email@email.com',
+          'request_notes'    => 'A Test Provision'
+        }
+      }
+    end
+    let(:request_url) { virtual_templates_url(virtual_template.id).to_s }
+
+    it 'creates an MiqProvisionRequest' do
+      api_basic_authorize collection_action_identifier(:virtual_templates, :provision)
+
+      dialog
+      run_post(request_url, request)
+
+      expect(response).to have_http_status(:ok)
+      task_id = response_hash['id']
+      expect(MiqProvisionRequest.exists?(task_id)).to be_truthy
+    end
+
+    it 'rejects requests without appropriate role' do
+      api_basic_authorize
+
+      run_post(request_url, request)
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'requires a requester' do
+      api_basic_authorize collection_action_identifier(:virtual_templates, :provision)
+
+      dialog
+      run_post(request_url, request.except('requester'))
+      expect_bad_request(/Requester required/)
+    end
+
+    it 'requires a VM name' do
+      api_basic_authorize collection_action_identifier(:virtual_templates, :provision)
+
+      dialog
+      run_post(request_url, request.except('vm_name'))
+      expect_bad_request(/VM name required/)
+    end
+  end
 end
