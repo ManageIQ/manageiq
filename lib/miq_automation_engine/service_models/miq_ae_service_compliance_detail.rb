3c16573a718c13b38e46abef23020dcb1248d405
jockey10 <shane.boulden@gmail.com>
2016-07-12 07:30:19 +1000

    Remove condition association

--- lib/miq_automation_engine/service_models/miq_ae_service_compliance_detail.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_compliance_detail.rb
@@ -1,7 +1,6 @@
 module MiqAeMethodService
   class MiqAeServiceComplianceDetail < MiqAeServiceModelBase
     expose :compliance, :association => true
-    expose :condition, :association => true
     expose :miq_policy, :association => true
   end
 end
