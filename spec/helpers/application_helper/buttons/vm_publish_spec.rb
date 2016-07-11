c548ebf9a7eb6c8c4af60321b086500f4ad7dafd
Roman Blanco <rblanco@redhat.com>
2016-07-11 15:49:22 +0200

    Specs for vm_publish toolbar button

---
+++ spec/helpers/application_helper/buttons/vm_publish_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+describe ApplicationHelper::Button::VmPublish do
+  describe '#skip?' do
+    it_behaves_like "when record is orphaned"
+    it_behaves_like "when record is archived"
+
+    context "when record is not cloneable and vendor is redhat" do
+      before do
+        @record = FactoryGirl.create(:vm_redhat)
+      end
+
+      it "will be skipped" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {'record' => @record}, {})
+        expect(button.skip?).to be_truthy
+      end
+    end
+
+    context "when record is not cloneable and vendor is redhat" do
+      before do
+        @record = FactoryGirl.create(:vm_vmware)
+        allow(@record).to receive(:archived?).and_return(false)
+      end
+
+      it "will not be skipped" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {'record' => @record}, {})
+        expect(button.skip?).to be_falsey
+      end
+    end
+  end
+end
