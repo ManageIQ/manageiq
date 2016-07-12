1908a74e4e3c5f2852e01287dd58251486b342cb
James Wong <jwong@redhat.com>
2016-07-12 15:52:59 -0400

    ext_management_system has many storage_profiles

--- app/models/storage_profile.rb
+++ app/models/storage_profile.rb
@@ -1,4 +1,5 @@
 class StorageProfile < ApplicationRecord
+  belongs_to :ext_management_system
   has_many :storage_profile_storages, :dependent  => :destroy
   has_many :storages,                 :through    => :storage_profile_storages
 end
