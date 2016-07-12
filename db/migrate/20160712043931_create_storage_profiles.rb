b4b95eece546e25b9411b5529890dff6f011d228
James Wong <jwong@redhat.com>
2016-07-12 01:48:19 -0400

    StorageProfile.uuid->ems_ref, add new tables to db/schema.yml
    Tables added to db/schema.yml: storage_profiles and storage_profile_storage

--- db/migrate/20160712043931_create_storage_profiles.rb
+++ db/migrate/20160712043931_create_storage_profiles.rb
@@ -2,7 +2,7 @@ class CreateStorageProfiles < ActiveRecord::Migration[5.0]
   def change
     create_table :storage_profiles do |t|
       t.string :name
-      t.string :uuid
+      t.string :ems_ref
       t.string :profile_type
 
       t.timestamps
