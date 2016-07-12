966d1555443ec48ae018d1f810d88738740540b8
Jillian Tullo <jtullo@redhat.com>
2016-07-12 14:30:19 -0400

    add arbitration controller API config info

--- spec/models/miq_product_feature_spec.rb
+++ spec/models/miq_product_feature_spec.rb
@@ -2,7 +2,7 @@ require 'tmpdir'
 require 'pathname'
 
 describe MiqProductFeature do
-  let(:expected_feature_count) { 1063 }
+  let(:expected_feature_count) { 1067 }
 
   # - container_dashboard
   # - miq_report_widget_editor
