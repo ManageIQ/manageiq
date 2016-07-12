0181683da9d8c1e9a4301858e437e2f04382e405
Roman Blanco <rblanco@redhat.com>
2016-07-12 11:04:22 +0200

    Specs for vm_stop toolbar button

---
+++ spec/helpers/application_helper/buttons/vm_stop_spec.rb
@@ -0,0 +1,60 @@
+require "spec_helper"
+
+describe ApplicationHelper::Button::VmStop do
+  describe '#skip?' do
+    context "when record is stopable" do
+      before do
+        @record = FactoryGirl.create(:vm_vmware)
+        allow(@record).to receive(:is_available?).with(:stop).and_return(true)
+      end
+
+      it "will not be skipped" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {'record' => @record}, {})
+        expect(button.skip?).to be_falsey
+      end
+    end
+
+    context "when record is not stopable" do
+      before do
+        @record = FactoryGirl.create(:vm_vmware)
+        allow(@record).to receive(:is_available?).with(:stop).and_return(false)
+      end
+
+      it "will be skipped" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {'record' => @record}, {})
+        expect(button.skip?).to be_truthy
+      end
+    end
+
+    context "when record has no error message" do
+      before do
+        @record = FactoryGirl.create(:vm_vmware)
+        allow(@record).to receive(:is_available_now_error_message).and_return(false)
+      end
+
+      it "will not be skipped" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {'record' => @record}, {})
+        expect(button.skip?).to be_falsey
+      end
+    end
+  end
+
+  describe '#disable?' do
+    context "when record has an error message" do
+      before do
+        @record = FactoryGirl.create(:vm_vmware)
+        message = "xx stop message"
+        allow(@record).to receive(:is_available_now_error_message).with(:stop).and_return(message)
+      end
+
+      it "disables the button and returns the stop error message" do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {'record' => @record}, {})
+        expect(button.disable?).to eq("xx stop message")
+      end
+    end
+  end
+end
