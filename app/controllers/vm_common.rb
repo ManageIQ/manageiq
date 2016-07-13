65a9ebc11a95ff44f97e0bb3620ff1492f2187ec
Zita Nemeckova <znemecko@redhat.com>
2016-07-13 10:49:49 +0200

    Correct mistake in warning in Policy Simulation

--- app/controllers/vm_common.rb
+++ app/controllers/vm_common.rb
@@ -626,7 +626,10 @@ module VmCommon
                                                               @policy_options)
     @edit = session[:edit] if session[:edit]
     if @edit && @edit[:explorer]
-      render_flash(_("No policies were selected for Policy Simulation."), :error) if session[:policies].empty?
+      if session[:policies].empty?
+        render_flash(_("No policies were selected for Policy Simulation."), :error)
+        return
+      end
       @in_a_form = true
       replace_right_cell
     else
