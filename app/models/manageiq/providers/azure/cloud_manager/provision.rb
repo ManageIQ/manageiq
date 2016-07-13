9a04902cec8558ef490a542f93610ae01d4043b1
Brandon Dunne <bdunne@redhat.com>
2016-07-13 17:38:45 -0400

    Don't Base64.encode64 by default, it doesn't apply to every provider
    https://bugzilla.redhat.com/show_bug.cgi?id=1355895

--- app/models/manageiq/providers/azure/cloud_manager/provision.rb
+++ app/models/manageiq/providers/azure/cloud_manager/provision.rb
@@ -1,4 +1,5 @@
 class ManageIQ::Providers::Azure::CloudManager::Provision < ManageIQ::Providers::CloudManager::Provision
   include_concern 'Cloning'
+  include_concern 'Configuration'
   include_concern 'OptionsHelper'
 end
