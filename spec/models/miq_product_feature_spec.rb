2dc728c2f838be981c6fed8d568104fae95fa2b6
Jillian Tullo <jtullo@redhat.com>
2016-07-13 10:16:15 -0400

    update feature spec conflict
    updating request to request_body
    
    update spec based on feedback
    
    updating formatting based off feedback
    
    fix arbitration default specs

--- spec/models/miq_product_feature_spec.rb
+++ spec/models/miq_product_feature_spec.rb
@@ -2,7 +2,7 @@ require 'tmpdir'
 require 'pathname'
 
 describe MiqProductFeature do
-  let(:expected_feature_count) { 1067 }
+  let(:expected_feature_count) { 1068 }
 
   # - container_dashboard
   # - miq_report_widget_editor
