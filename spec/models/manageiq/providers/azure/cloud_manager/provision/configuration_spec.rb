42582320062c6330c92a97931d8d624b24e4edda
Brandon Dunne <bdunne@redhat.com>
2016-07-13 17:38:45 -0400

    Don't Base64.encode64 by default, it doesn't apply to every provider
    https://bugzilla.redhat.com/show_bug.cgi?id=1355895

--- spec/models/manageiq/providers/azure/cloud_manager/provision/configuration_spec.rb
+++ spec/models/manageiq/providers/azure/cloud_manager/provision/configuration_spec.rb
@@ -1,11 +1,11 @@
-describe ManageIQ::Providers::CloudManager::Provision::Configuration do
+describe ManageIQ::Providers::Azure::CloudManager::Provision::Configuration do
   context 'Userdata payload checks' do
-    it "script is not encoded" do
+    it "script is Base64 encoded" do
       template  = FactoryGirl.build(:customization_template, :script => "#cloud-init")
-      provision = ManageIQ::Providers::CloudManager::Provision.new
+      provision = ManageIQ::Providers::Azure::CloudManager::Provision.new
       allow(provision).to receive(:customization_template).and_return(template)
       allow(provision).to receive(:post_install_callback_url).and_return("")
-      expect(provision.userdata_payload).to eq(template.script)
+      expect(Base64.decode64(provision.userdata_payload)).to eq(template.script)
     end
   end
 end
