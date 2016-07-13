dbdf8753586cd05aeac22a78dfcdcddd2e7dcf4f
Jillian Tullo <jtullo@redhat.com>
2016-07-13 10:16:15 -0400

    update feature spec conflict
    updating request to request_body
    
    update spec based on feedback
    
    updating formatting based off feedback
    
    fix arbitration default specs

--- spec/requests/api/arbitration_default_spec.rb
+++ spec/requests/api/arbitration_default_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'Arbitration Default API' do
 
   context 'arbitration defaults index' do
     it 'can list the arbitration defaults' do
-      FactoryGirl.create(:arbitration_default, :ems_id => ems.id)
+      FactoryGirl.create(:arbitration_default, :ext_management_system => ems)
 
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :read, :get)
       run_get(arbitration_defaults_url)
@@ -12,56 +12,54 @@ RSpec.describe 'Arbitration Default API' do
   end
 
   context 'arbitration defaults create' do
-    let(:request) do
+    let(:request_body) do
       {:ems_id => ems.id}
     end
 
     it 'rejects creation without appropriate role' do
       api_basic_authorize
 
-      run_post(arbitration_defaults_url, request)
+      run_post(arbitration_defaults_url, request_body)
       expect(response).to have_http_status(:forbidden)
     end
 
     it 'supports single arbitration_default creation' do
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :create)
-      run_post(arbitration_defaults_url, request)
 
+      expect do
+        run_post(arbitration_defaults_url, request_body)
+      end.to change(ArbitrationDefault, :count).by(1)
       expect(response).to have_http_status(:ok)
-
-      id = response_hash['results'].first['id']
-      expect(ArbitrationDefault.exists?(id)).to be_truthy
     end
 
     it 'supports arbitration_default creation via action' do
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :create)
-      run_post(arbitration_defaults_url, gen_request(:create, request))
 
+      expect do
+        run_post(arbitration_defaults_url, gen_request(:create, request_body))
+      end.to change(ArbitrationDefault, :count).by(1)
       expect(response).to have_http_status(:ok)
-
-      id = response_hash['results'].first['id']
-      expect(ArbitrationDefault.exists?(id)).to be_truthy
     end
 
     it 'rejects arbitration_default creation with duplicate ems' do
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :create)
 
-      FactoryGirl.create(:arbitration_default, :ems_id => ems.id)
-      run_post(arbitration_defaults_url, request)
+      FactoryGirl.create(:arbitration_default, :ext_management_system => ems)
+      run_post(arbitration_defaults_url, request_body)
 
       expect_bad_request(/Ext management system has already been taken/)
     end
 
     it 'rejects a request with an href' do
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :create)
-      run_post(arbitration_defaults_url, request.merge(:href => arbitration_defaults_url))
+      run_post(arbitration_defaults_url, request_body.merge(:href => arbitration_defaults_url))
 
       expect_bad_request(/Resource id or href should not be specified/)
     end
 
     it 'rejects a request with an id' do
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :create)
-      run_post(arbitration_defaults_url, request.merge(:id => 1))
+      run_post(arbitration_defaults_url, request_body.merge(:id => 1))
 
       expect_bad_request(/Resource id or href should not be specified/)
     end
@@ -69,12 +67,16 @@ RSpec.describe 'Arbitration Default API' do
 
   context 'arbitration defaults edit' do
     let(:cloud_subnet) { FactoryGirl.create(:cloud_subnet) }
-    let(:default) { FactoryGirl.create(:arbitration_default, :cloud_subnet_id => cloud_subnet.id, :ems_id => ems.id) }
+    let(:default) do
+      FactoryGirl.create(:arbitration_default,
+                         :cloud_subnet_id       => cloud_subnet.id,
+                         :ext_management_system => ems)
+    end
 
     it 'supports single arbitration_default edit' do
       subnet = FactoryGirl.create(:cloud_subnet)
-
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :edit)
+
       run_post(arbitration_defaults_url(default.id), gen_request(:edit, :cloud_subnet_id => subnet.id))
 
       expect(default.reload.cloud_subnet_id).to eq(subnet.id)
@@ -84,7 +86,7 @@ RSpec.describe 'Arbitration Default API' do
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :edit)
       subnet = FactoryGirl.create(:cloud_subnet)
       ext = FactoryGirl.create(:ext_management_system)
-      default_two = FactoryGirl.create(:arbitration_default, :ems_id => ext.id)
+      default_two = FactoryGirl.create(:arbitration_default, :ext_management_system => ext)
 
       default_id_1, default_id_2 = default.id, default_two.id
 
@@ -106,10 +108,11 @@ RSpec.describe 'Arbitration Default API' do
 
   context 'arbitration_defaults delete' do
     it 'supports single arbitration_default delete' do
-      arb_default = FactoryGirl.create(:arbitration_default, :ems_id => ems.id)
+      arb_default = FactoryGirl.create(:arbitration_default, :ext_management_system => ems)
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :delete)
 
       run_delete(arbitration_defaults_url(arb_default.id))
+
       expect(response).to have_http_status(:no_content)
       expect(ArbitrationDefault.exists?(arb_default.id)).to be_falsey
     end
@@ -118,13 +121,14 @@ RSpec.describe 'Arbitration Default API' do
       api_basic_authorize collection_action_identifier(:arbitration_defaults, :delete)
 
       ems_2 = FactoryGirl.create(:ext_management_system)
-      arb_default_1 = FactoryGirl.create(:arbitration_default, :ems_id => ems.id)
-      arb_default_2 = FactoryGirl.create(:arbitration_default, :ems_id => ems_2.id)
+      arb_default_1 = FactoryGirl.create(:arbitration_default, :ext_management_system => ems)
+      arb_default_2 = FactoryGirl.create(:arbitration_default, :ext_management_system => ems_2)
 
       arb_id_1, arb_id_2 = arb_default_1.id, arb_default_2.id
       arb_url_1, arb_url_2 = arbitration_defaults_url(arb_default_1.id), arbitration_defaults_url(arb_default_2.id)
 
       run_post(arbitration_defaults_url, gen_request(:delete, [{'href' => arb_url_1}, {'href' => arb_url_2}]))
+
       expect_multiple_action_result(2)
       expect_result_resources_to_include_hrefs('results', [arb_url_1, arb_url_2])
       expect(ArbitrationDefault.exists?(arb_id_1)).to be_falsey
