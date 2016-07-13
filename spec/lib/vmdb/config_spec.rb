17038ad5d99512e3853418a5e587c0417b307d09
Yuri Rudman <yrudman@redhat.com>
2016-07-13 18:42:12 -0400

    removed temporary hack (stub_my_sewrver_settings) from config_spec.rb

--- spec/lib/vmdb/config_spec.rb
+++ spec/lib/vmdb/config_spec.rb
@@ -12,15 +12,12 @@ describe VMDB::Config do
 
   context ".save_file" do
     it "normal" do
-      EvmSpecHelper.local_miq_server
-      stub_my_server_settings
-
+      resource = EvmSpecHelper.local_miq_server
       data = Settings.to_hash
       data.store_path(:api, :token_ttl, "1.day")
       data = data.to_yaml
 
-      expect(VMDB::Config.save_file(data)).to be true
-
+      expect(VMDB::Config.save_file(data, resource)).to be true
       expect(SettingsChange.count).to eq(1)
       expect(SettingsChange.first).to have_attributes(:key => '/api/token_ttl', :value => "1.day")
     end
@@ -34,12 +31,11 @@ describe VMDB::Config do
   end
 
   it "#save" do
-    EvmSpecHelper.local_miq_server
-    stub_my_server_settings
+    resource = EvmSpecHelper.local_miq_server
 
     config = VMDB::Config.new("vmdb")
     config.config.store_path(:api, :token_ttl, "1.day")
-    config.save
+    config.save(resource)
 
     expect(SettingsChange.count).to eq(1)
     expect(SettingsChange.first).to have_attributes(:key => '/api/token_ttl', :value => "1.day")
@@ -47,7 +43,6 @@ describe VMDB::Config do
 
   it "#set_worker_setting!" do
     EvmSpecHelper.local_miq_server
-    stub_my_server_settings
 
     config = VMDB::Config.new("vmdb")
     config.set_worker_setting!(:MiqEmsMetricsCollectorWorker, :memory_threshold, "1.terabyte")
@@ -55,14 +50,5 @@ describe VMDB::Config do
     fq_keys = [:workers, :worker_base, :queue_worker_base, :ems_metrics_collector_worker, :memory_threshold]
     v = config.config.fetch_path(fq_keys).to_i_with_method
     expect(v).to eq(1.terabyte)
-  end
-
-  # HACK: Temporary fix because zone name is stored in the configuration, but
-  #   when calling EvmSpecHelper.local_miq_server, it does not update the
-  #   corresponding settings.  enqueue_for_server expects that the zone is set,
-  #   and this can get messed up during Config activation during a change.
-  def stub_my_server_settings
-    server = MiqServer.my_server
-    allow(server).to receive(:enqueue_for_server)
   end
 end
