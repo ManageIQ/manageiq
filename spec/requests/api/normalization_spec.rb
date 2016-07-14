4352f7591b3fb531bc35a038c8307e17d49a04ea
Tim Wade <hello@timjwade.com>
2016-07-14 07:33:10 -0700

    Fix gen_time_attr_type_hash in ApiController
    When the API config was changed to be a `Config::Options`,
    `Api::Settings.collections` stopped responding properly to `each_value`
    - it now just returns `nil` and does not execute the block. Changing it
    to `each` fixes the issue.

---
+++ spec/requests/api/normalization_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe "Normalization of objects API" do
+  it "represents datetimes in ISO8601 format" do
+    api_basic_authorize action_identifier(:hosts, :read, :resource_actions, :get)
+    host = FactoryGirl.create(:host)
+
+    run_get(hosts_url(host.id))
+
+    expect(response_hash).to include("created_on" => host.created_on.iso8601)
+  end
+end
