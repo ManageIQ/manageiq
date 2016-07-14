4e4d7674b30678d9b82536d45770035055b84162
Alberto Bellotti <abellott@redhat.com>
2016-07-14 09:26:41 -0400

    PR Reviews
    - Added tests

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -59,6 +59,26 @@ RSpec.describe "users API" do
       expect(response).to have_http_status(:ok)
     end
 
+    it "can change the user's own email" do
+      api_basic_authorize action_identifier(:users, :edit)
+
+      expect do
+        run_post users_url(@user.id), gen_request(:edit, :email => "tom@cartoons.com")
+      end.to change { @user.reload.email }
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "can change the user's own settings" do
+      api_basic_authorize action_identifier(:users, :edit)
+
+      expect do
+        run_post users_url(@user.id), gen_request(:edit, :settings => {:cartoon => {:tom_jerry => 'y'}})
+      end.to change { @user.reload.settings }
+
+      expect(response).to have_http_status(:ok)
+    end
+
     it "will not allow the changing of attributes other than the password, email or settings" do
       api_basic_authorize
 
@@ -76,6 +96,17 @@ RSpec.describe "users API" do
       expect do
         run_post users_url(user.id), gen_request(:edit, :password => "new_password")
       end.not_to change { user.reload.password_digest }
+
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it "cannot change another user's settings" do
+      api_basic_authorize
+      user = FactoryGirl.create(:user, :settings => {:locale => "en"})
+
+      expect do
+        run_post users_url(user.id), gen_request(:edit, :settings => {:locale => "ja"})
+      end.not_to change { user.reload.settings }
 
       expect(response).to have_http_status(:forbidden)
     end
