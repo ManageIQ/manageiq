2677175a385fc8cd06190e88484974c4fd3ce5b9
Jillian Tullo <jtullo@redhat.com>
2016-07-14 15:44:53 -0400

    updates the API to be arbitration_profile, not arbitration_default
    fix rubocop error
    
    removing arbitration_default_spec

--- spec/requests/api/collections_spec.rb
+++ spec/requests/api/collections_spec.rb
@@ -245,10 +245,10 @@ describe ApiController do
       test_collection_query(:container_deployments, container_deployments_url, ContainerDeployment)
     end
 
-    it 'queries ArbitrationDefauls' do
+    it 'queries ArbitrationProfiles' do
       ems = FactoryGirl.create(:ext_management_system)
-      FactoryGirl.create(:arbitration_default, :ems_id => ems.id)
-      test_collection_query(:arbitration_defaults, arbitration_defaults_url, ArbitrationDefault)
+      FactoryGirl.create(:arbitration_profile, :ems_id => ems.id)
+      test_collection_query(:arbitration_profiles, arbitration_profiles_url, ArbitrationProfile)
     end
   end
 end
