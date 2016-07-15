bfb10e48dd52bc79bfe4f38c26c001b604acbd0e
Oleg Barenboim <chessbyte@gmail.com>
2016-07-15 10:02:00 -0400

    Merge pull request #9841 from durandom/rename_supports_retirement_to_retire
    rename supports_retirement? to supports_retire?
    (cherry picked from commit 11835bb2d522a320dce6f79aa692696cf683e77c)

--- app/models/vm/operations/lifecycle.rb
+++ app/models/vm/operations/lifecycle.rb
@@ -2,10 +2,8 @@ module Vm::Operations::Lifecycle
   extend ActiveSupport::Concern
 
   included do
-    supports :retirement do
-      if orphaned? || archived?
-        unsupported_reason_add :retirement, "VM orphaned or archived already"
-      end
+    supports :retire do
+      unsupported_reason_add(:retire, "VM orphaned or archived already") if orphaned? || archived?
     end
   end
 
@@ -21,11 +19,4 @@ module Vm::Operations::Lifecycle
     {:available => !(self.blank? || self.orphaned? || self.archived?), :message   => nil}
   end
 
-  def validate_retire
-    {:available => !(self.orphaned? || self.archived?), :message   => nil}
-  end
-
-  def validate_retire_now
-    {:available => !(self.orphaned? || self.archived?), :message   => nil}
-  end
 end
