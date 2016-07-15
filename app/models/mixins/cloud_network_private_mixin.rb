c0722c9d2056da7826aebaf59f43b9ba27aba874
Ladislav Smola <lsmola@redhat.com>
2016-07-15 14:41:57 +0200

    Remove unused public and private network mixins
    Remove unused public and private network mixins

--- app/models/mixins/cloud_network_private_mixin.rb
+++
@@ -1,9 +0,0 @@
-module CloudNetworkPrivateMixin
-  extend ActiveSupport::Concern
-
-  included do
-    has_many :vms, :through => :network_ports, :source => :device, :source_type => 'VmOrTemplate'
-    has_many :network_routers, -> { distinct }, :through => :cloud_subnets
-    has_many :public_networks, -> { distinct }, :through => :cloud_subnets
-  end
-end
--- app/models/mixins/cloud_network_public_mixin.rb
+++
@@ -1,9 +0,0 @@
-module CloudNetworkPublicMixin
-  extend ActiveSupport::Concern
-
-  included do
-    has_many :vms, :through => :network_routers
-    has_many :network_routers, :foreign_key => :cloud_network_id
-    has_many :private_networks, :through => :network_routers, :source => :cloud_networks
-  end
-end
