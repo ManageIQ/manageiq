343989af82c895d30c7ccfdcf25b17960ed57bea
Keenan Brock <kbrock@redhat.com>
2016-07-18 14:43:46 -0400

    find_targets_with_direct_rbac extract auth_count

--- spec/models/rbac_spec.rb
+++ spec/models/rbac_spec.rb
@@ -1052,7 +1052,7 @@ describe Rbac do
     it "works with no filters" do
       all_vms
       result = Rbac.find_targets_with_direct_rbac(Vm, {}, {}, nil, nil)
-      expect_counts(result, all_vms, all_vms.size)
+      expect(result).to match_array(all_vms)
     end
 
     # most of the functionality of search is channeled through find_options. including filters
@@ -1060,7 +1060,7 @@ describe Rbac do
     it "applies find_options[:conditions, :include]" do
       all_vms
       result = Rbac.find_targets_with_direct_rbac(Vm, {}, host_filter_find_options, nil, nil)
-      expect_counts(result, vms_match, 2)
+      expect(result).to match_array(vms_match)
     end
   end
 
