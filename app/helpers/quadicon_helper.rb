8a24dcfd78c96ae71c915e7eb5c5854393abcd82
Eric Hayes <eric@erichayes.net>
2016-07-18 14:25:21 -0700

    Account for Middleware Deployment quadicons Fixes #9892

--- app/helpers/quadicon_helper.rb
+++ app/helpers/quadicon_helper.rb
@@ -557,7 +557,7 @@ module QuadiconHelper
                    "100/#{item.class.to_s.underscore}.png"
                  end
                elsif item.respond_to?(:decorator_class?) && item.decorator_class?
-                 item.decorate.try(:fonticon) || item.decorate.try(:listicon_image)
+                 item.decorate.try(:listicon_image)
                else
                  "100/#{item.class.base_class.to_s.underscore}.png"
                end
