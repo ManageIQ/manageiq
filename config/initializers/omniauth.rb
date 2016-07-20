1af293c58fbe9be93e53cea4daa8938a8655b330
Oleg Barenboim <chessbyte@gmail.com>
2016-07-20 17:27:54 -0400

    Merge pull request #9961 from kbrock/omniauth_google
    only add middleware if have google
    (cherry picked from commit c13b3e7ff1259af794a77078ed50ec9504ea3b7e)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,10 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
-           :scope => 'https://www.googleapis.com/auth/compute,' \
-             'https://www.googleapis.com/auth/devstorage.read_only,' \
-             'https://www.googleapis.com/auth/logging.write,' \
-             'https://www.googleapis.com/auth/cloud-platform,' \
-             'email,profile'
+if ENV["GOOGLE_CLIENT_SECRET"]
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
+             :scope => 'https://www.googleapis.com/auth/compute,' \
+               'https://www.googleapis.com/auth/devstorage.read_only,' \
+               'https://www.googleapis.com/auth/logging.write,' \
+               'https://www.googleapis.com/auth/cloud-platform,' \
+               'email,profile'
+  end
 end
