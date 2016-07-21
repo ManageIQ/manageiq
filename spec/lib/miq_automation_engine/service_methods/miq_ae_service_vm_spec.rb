4c402818bc054ce53a977291a4c1da2a7cf9dfa5
Tina Fitzgerald <tfitzger@redhat.com>
2016-07-21 13:22:59 -0400

    Added extend_retires_on method for retirement mix and exposed method to automate. Added tests.

--- spec/lib/miq_automation_engine/service_methods/miq_ae_service_vm_spec.rb
+++ spec/lib/miq_automation_engine/service_methods/miq_ae_service_vm_spec.rb
@@ -149,17 +149,61 @@ module MiqAeServiceVmSpec
     end
 
     it "#retires_on - today" do
-      service_vm.retires_on = Date.today
-      vm.reload
+      vm.update_attributes(:retirement_last_warn => Date.today)
+      service_vm.retires_on = Time.zone.today
 
+      vm.reload
+      expect(vm.retirement_last_warn).to be_nil
       expect(vm.retirement_due?).to be_truthy
     end
 
     it "#retires_on - tomorrow" do
-      service_vm.retires_on = Date.today + 1
+      vm.update_attributes(
+        :retired              => true,
+        :retirement_last_warn => Time.zone.today,
+        :retirement_state     => "retiring"
+      )
+      service_vm.retires_on = Time.zone.today + 1
       vm.reload
 
-      expect(vm.retirement_due?).to be_falsey
+      expect(vm).to have_attributes(
+        :retirement_last_warn => nil,
+        :retired              => false,
+        :retirement_state     => nil,
+        :retirement_due?      => false
+      )
+    end
+
+    it "#extend_retires_on - no retirement date set" do
+      Timecop.freeze(Time.zone.today) do
+        extend_days = 7
+        service_vm.extend_retires_on(extend_days)
+        vm.reload
+
+        expect(vm.retires_on.day).to eq(Time.zone.today.day + extend_days)
+      end
+    end
+
+    it "#extend_retires_on - future retirement date set" do
+      Timecop.freeze(Time.zone.today) do
+        vm.update_attributes(
+          :retired              => true,
+          :retirement_last_warn => Time.zone.today,
+          :retirement_state     => "retiring"
+        )
+        future_retires_on = Time.zone.today + 30
+        service_vm.retires_on = future_retires_on
+        extend_days = 7
+        service_vm.extend_retires_on(extend_days, future_retires_on)
+        vm.reload
+
+        expect(vm).to have_attributes(
+          :retirement_last_warn => nil,
+          :retired              => false,
+          :retirement_state     => nil,
+          :retires_on           => future_retires_on + extend_days
+        )
+      end
     end
 
     it "#retirement_warn" do
