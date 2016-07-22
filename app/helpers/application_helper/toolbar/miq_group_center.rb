2829ffdb666f9bb6a6eac19d0a3090b2f120e48c
Martin Hradil <mhradil@redhat.com>
2016-07-22 11:19:30 +0000

    Fix %{:customer_name} typo in miq_group_center toolbar
    In Configuration > Access Control > Groups > *, the policy toolbar contains "Edit '%{:customer_name}' Tags for this Group"... clearly a typo..

--- app/helpers/application_helper/toolbar/miq_group_center.rb
+++ app/helpers/application_helper/toolbar/miq_group_center.rb
@@ -32,7 +32,7 @@ class ApplicationHelper::Toolbar::MiqGroupCenter < ApplicationHelper::Toolbar::B
           :rbac_group_tags_edit,
           'pficon pficon-edit fa-lg',
           t = proc do
-            _('Edit \'%{:customer_name}\' Tags for this Group') % {:customer_name => @view_context.session[:customer_name]}
+            _('Edit \'%{customer_name}\' Tags for this Group') % {:customer_name => @view_context.session[:customer_name]}
           end,
           t),
       ]
