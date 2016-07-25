7bb8fa933788601064569f3c359d2e63f8ce82fc
Oleg Barenboim <chessbyte@gmail.com>
2016-07-25 09:36:51 -0400

    Merge pull request #10028 from yaacov/sort-authentications-in-ems-summary-status
    Order the authentications display order by authtype
    (cherry picked from commit 4815022173ef07903dc98309878331d632954b80)

--- app/helpers/ems_container_helper/textual_summary.rb
+++ app/helpers/ems_container_helper/textual_summary.rb
@@ -25,7 +25,7 @@ module EmsContainerHelper::TextualSummary
     authentications = @ems.authentications
     return [{:label => _("Default Authentication"), :title => t = _("None"), :value => t}] if authentications.blank?
 
-    authentications.collect do |auth|
+    authentications.order(:authtype).collect do |auth|
       label =
         case auth.authtype
         when "default" then _("Default")
