b56adb2858b32496dff552847523e0ca56942869
Mike Thompson <mithomps@redhat.com>
2016-07-25 10:32:41 -0700

    Middleware server operations dialog: refactor http request out of controller and into stateless service.
    Fixes #9462. The controller should not have any responsibilities regarding the fetching of data. This will make testing much easier since we can now mock out the service and also test it individually. The service is pure and only knows about retrieving data (and no knowledge of html forms).  When we upgrade to Angular 2 having the services separate will make for a much easier migration especially since controllers and scope don't exist in Angular 2.

--- app/assets/javascripts/controllers/middleware_server/middleware_server_controller.js
+++ app/assets/javascripts/controllers/middleware_server/middleware_server_controller.js
@@ -1,56 +1,84 @@
-
-
 ManageIQ.angular.app.controller('middlewareServerController', MiddlewareServerController);
 
-MiddlewareServerController.$inject = ['$scope', '$http', 'miqService'];
+MiddlewareServerController.$inject = ['$scope', 'miqService', 'serverOpsService'];
 
-function MiddlewareServerController($scope, $http, miqService) {
+function MiddlewareServerController($scope, miqService, serverOpsService) {
   ManageIQ.angular.scope = $scope;
 
   $scope.showListener = function (args) {
     var operation = args.split(':')[1]; // format is 'operation:resume'
 
     $scope.paramsModel = {};
-    if(operation){
+    if (operation) {
       $scope.paramsModel.operation = operation;
-      $scope.paramsModel.operationTitle = makeOperationDisplayName(operation) + ' ' +_('Server');
+      $scope.paramsModel.operationTitle = makeOperationDisplayName(operation) + ' ' + _('Server');
       $scope.paramsModel.operationButtonName = makeOperationDisplayName(operation);
     }
     $scope.paramsModel.timeout = 10; // default timeout value
   };
 
-  $scope.runOperation = function (operation) {
+  $scope.runOperation = function () {
     miqService.sparkleOn();
 
-    var payload =  {
-      'id': angular.element('#mw_param_server_id').val(),
-      'timeout': $scope.paramsModel.timeout,
-      'operation': $scope.paramsModel.operation,
+    serverOpsService.runOperation(angular.element('#mw_param_server_id').val(),
+      $scope.paramsModel.operation,
+      $scope.paramsModel.timeout)
+      .then(function (response) {
+          miqService.miqFlash('success', response);
+        },
+        function (error) {
+          miqService.miqFlash('error', error);
+
+        }).finally(function () {
+
+      miqService.sparkleOff();
+    });
+
+  };
+
+  var makeOperationDisplayName = function (operation) {
+    return _.capitalize(operation);
+  }
+
+}
+
+ManageIQ.angular.app.service('serverOpsService', ServerOpsService);
+
+ServerOpsService.$inject = ['$http', '$q'];
+
+function ServerOpsService($http, $q) {
+  this.runOperation = function runOperation(id, operation, timeout) {
+    var errorMsg = _('Error running operation on this server.');
+    var deferred = $q.defer();
+    var payload = {
+      'id': id,
+      'operation': operation,
+      'timeout': timeout
     };
 
     $http.post('/middleware_server/run_operation', angular.toJson(payload))
       .then(
-        function(response) { // success
+        function (response) { // success
           var data = response.data;
 
-          if(data.status === 'ok'){
-            miqService.miqFlash('success', data.msg);
-          }else {
-            $log.error(data);
-            miqService.miqFlash('error', data.msg);
+          if (data.status === 'ok') {
+            deferred.resolve(data.msg);
+          } else {
+            deferred.reject(data.msg);
           }
         },
-        function() { // error
-          miqService.miqFlash('error', _('Error running operation on this server.'));
+        function () { // error
+          deferred.reject(errorMsg);
         })
-      .finally(function() {
+      .catch(function () {
+        deferred.reject(errorMsg);
+      })
+      .finally(function () {
         angular.element("#modal_param_div").modal('hide');
-        miqService.sparkleOff();
+        // we should already be resolved and promises can only fire once
+        deferred.resolve(data.msg);
       });
-  };
-
-  var makeOperationDisplayName = function(operation){
-    return  _.capitalize(operation);
+    return deferred.promise;
   }
-  
 }
+
