448aec9aa7f99a2e2e57c9464e551f4714ed1ee9
Joe Rafaniello <jrafanie@redhat.com>
2016-07-25 13:41:38 -0400

    Renice workers after they fork
    https://bugzilla.redhat.com/show_bug.cgi?id=1353700
    
    Before we changed from spawn to fork [1],
    we'd spawn the process with a nice prefix [2],
    which gave the kernel some hints for CPU scheduling
    of our processes.  A higher "nice" value for a process
    is nicer to other processes.
    
    This niceness wasn't brought over from spawn to fork.
    
    [1] https://github.com/ManageIQ/manageiq/pull/3593
    [2] b0caa423cd928f09e3287bb5141803b3360b5f89

--- app/models/miq_worker.rb
+++ app/models/miq_worker.rb
@@ -320,6 +320,7 @@ class MiqWorker < ApplicationRecord
   def self.after_fork
     close_pg_sockets_inherited_from_parent
     DRb.stop_service
+    renice(Process.pid)
   end
 
   # When we fork, the children inherits the parent's file descriptors
@@ -512,8 +513,8 @@ class MiqWorker < ApplicationRecord
                          end
   end
 
-  def self.nice_prefix
-    @nice_prefix ||= "nice -n #{nice_increment}"
+  def self.renice(pid)
+    AwesomeSpawn.run("renice", :params =>  {:n => nice_increment, :p => pid })
   end
 
   def self.nice_increment
