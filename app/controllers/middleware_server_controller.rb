85bd29b751057d9c215721528865fe4202ea7793
Mike Thompson <mithomps@redhat.com>
2016-07-26 14:34:44 -0700

    Middleware server operations: Refactor to reduce complexity and get rid of all Rubocop warnings.

--- app/controllers/middleware_server_controller.rb
+++ app/controllers/middleware_server_controller.rb
@@ -56,7 +56,7 @@ class MiddlewareServerController < ApplicationController
       :enabled      => params["enabled"],
       :runtime_name => params["runtimeName"]
     }
-    run_operation(OPERATIONS.fetch(:middleware_add_deployment), selected_servers)
+    run_server_operation(OPERATIONS.fetch(:middleware_add_deployment), selected_servers)
 
     render :update do |page|
       page << javascript_prologue
@@ -79,8 +79,8 @@ class MiddlewareServerController < ApplicationController
     if OPERATIONS.key?(selected_operation)
       selected_servers = identify_selected_entities
 
-      run_operation(OPERATIONS.fetch(selected_operation),
-                    selected_servers, "%{msg} initiated for selected server(s)")
+      run_server_operation(OPERATIONS.fetch(selected_operation), selected_servers)
+
       javascript_flash
     else
       super
@@ -88,45 +88,97 @@ class MiddlewareServerController < ApplicationController
   end
 
   def run_operation
-    selected_operation = ('middleware_server_' + params["operation"]).to_sym
+    selected_servers = identify_selected_servers
+    if selected_servers.nil?
+      render :json => {:status => :error, :msg => _("No Servers selected")}
+      return
+    end
 
-    if OPERATIONS.key?(selected_operation)
-      selected_servers = identify_selected_servers
-      operation_info = OPERATIONS.fetch(selected_operation)
-
-      if selected_servers.nil?
-        render :json => {:status => :error, :msg => _("No Servers selected")}
-        return
-      end
-
-      operation_triggered = false
-      selected_servers.split(/,/).each do |item|
-        mw_server = identify_record item
-
-        if mw_server.product == 'Hawkular' && operation_info.fetch(:skip)
-          skip_message = _("Not #{operation_info.fetch(:hawk)} the provider")
-          render :json => {:status => :ok, :msg => skip_message}
-        else
-          if operation_info.key? :param
-            name = operation_info.fetch(:param) # which currently evaluates to :timeout
-            val = params["timeout"]
-            trigger_mw_operation operation_info.fetch(:op), mw_server, name => val
-          else
-            trigger_mw_operation operation_info.fetch(:op), mw_server
-          end
-          operation_triggered = true
-        end
-      end
-      if operation_triggered
-        initiated_msg = _("#{operation_info.fetch(:msg)} initiated for selected server(s)")
-        render :json => {:status => :ok, :msg => initiated_msg}
-      end
+    operation = ('middleware_server_' + params["operation"]).to_sym
+    if OPERATIONS.key?(operation)
+      operation_info = OPERATIONS.fetch(operation)
+      run_server_param_operation(operation_info, selected_servers)
     else
-      msg = _("Unknown server operation: ") + selected_operation
-      render :json => {:status => :error, :msg => msg }
+      msg = _("Unknown server operation: ") + operation
+      render :json => {:status => :error, :msg => msg}
     end
   end
 
   private ############################
 
+  # Identify the selected servers. When we got the call from the
+  # single server page, we need to look at :id, otherwise from
+  # the list of servers we need to query :miq_grid_checks
+  def identify_selected_servers
+    items = params[:miq_grid_checks]
+    return items unless items.nil? || items.empty?
+
+    params[:id]
+  end
+
+  def run_server_param_operation(operation_info, mw_servers)
+    operation_triggered = false
+    mw_servers.split(/,/).each do |mw_server|
+      mw_server = identify_record mw_server
+
+      if mw_server.product == 'Hawkular' && operation_info.fetch(:skip)
+        skip_message = _("Not #{operation_info.fetch(:hawk)} the provider")
+        render :json => {:status => :ok, :msg => skip_message}
+      else
+        operation_triggered = trigger_param_operation(operation_info, mw_server, :param)
+      end
+      if operation_triggered
+        initiated_msg = _("#{operation_info.fetch(:msg)} initiated for selected server(s)")
+        render :json => {:status => :ok, :msg => initiated_msg}
+      end
+    end
+  end
+
+  def trigger_param_operation(operation_info, mw_server, op_param)
+    if operation_info.key? op_param
+      name = operation_info.fetch(op_param) # which currently evaluates to :timeout
+      val = params["timeout"]
+      trigger_mw_operation operation_info.fetch(:op), mw_server, name => val
+    else
+      trigger_mw_operation operation_info.fetch(:op), mw_server
+    end
+    true
+  end
+
+  def run_server_operation(operation_info, items)
+    if items.nil?
+      add_flash(_("No servers selected"))
+      return
+    end
+
+    operation_triggered = false
+    items.split(/,/).each do |item|
+      mw_server = identify_record item
+      if mw_server.product == 'Hawkular' && operation_info.fetch(:skip)
+        add_flash(_("Not %{hawkular_info} the provider") % {:hawkular_info => operation_info.fetch(:hawk)})
+      else
+        if operation_info.key? :param
+          # Fetch param from UI - > see #9462/#8079
+          name = operation_info.fetch(:param)
+          val = params.fetch name || 0 # Default until we can really get it from the UI ( #9462/#8079)
+          trigger_mw_operation operation_info.fetch(:op), mw_server, name => val
+        else
+          trigger_mw_operation operation_info.fetch(:op), mw_server
+        end
+        operation_triggered = true
+      end
+    end
+    add_flash(_("#{operation_info.fetch(:msg)} initiated for selected server(s)")) if operation_triggered
+  end
+
+  def trigger_mw_operation(operation, mw_server, params = nil)
+    mw_manager = mw_server.ext_management_system
+
+    op = mw_manager.public_method operation
+    if params
+      op.call(mw_server.ems_ref, params)
+    else
+      op.call mw_server.ems_ref
+    end
+  end
 end
