a3ea20b9d1ef191c213aa64e59b3801ec65798f3
Nick Carboni <ncarboni@redhat.com>
2016-07-27 14:30:53 -0400

    Add a down migration for the replication exclude table key change
    This will move the changes to the replication excludes back to
    the replication worker location.

--- spec/migrations/20160607141857_change_exclude_table_settings_key_spec.rb
+++ spec/migrations/20160607141857_change_exclude_table_settings_key_spec.rb
@@ -5,22 +5,37 @@ describe ChangeExcludeTableSettingsKey do
 
   migration_context :up do
     it "changes the key for the exclude tables" do
-      settings_change_stub.create!(
-        :resource_type => "MiqServer",
-        :key           => "/workers/worker_base/replication_worker/replication/exclude_tables",
-        :value         => %w(table1 table2 table3)
-      )
-
-      settings_change_stub.create!(
-        :resource_type => "MiqServer",
-        :key           => "/workers/worker_base/replication_worker/replication/exclude_tables",
-        :value         => %w(table1 table3)
-      )
+      insert_test_records(described_class::OLD_KEY)
 
       migrate
 
-      expect(settings_change_stub.where(:key => "/replication/exclude_tables").count).to eq 2
-      expect(settings_change_stub.where(:key => "/workers/worker_base/replication_worker/replication/exclude_tables").count).to eq 0
+      expect(settings_change_stub.where(:key => described_class::NEW_KEY).count).to eq 2
+      expect(settings_change_stub.where(:key => described_class::OLD_KEY).count).to eq 0
     end
+  end
+
+  migration_context :down do
+    it "changes the key for the exclude tables" do
+      insert_test_records(described_class::NEW_KEY)
+
+      migrate
+
+      expect(settings_change_stub.where(:key => described_class::OLD_KEY).count).to eq 2
+      expect(settings_change_stub.where(:key => described_class::NEW_KEY).count).to eq 0
+    end
+  end
+
+  def insert_test_records(key)
+    settings_change_stub.create!(
+      :resource_type => "MiqServer",
+      :key           => key,
+      :value         => %w(table1 table2 table3)
+    )
+
+    settings_change_stub.create!(
+      :resource_type => "MiqServer",
+      :key           => key,
+      :value         => %w(table1 table3)
+    )
   end
 end
