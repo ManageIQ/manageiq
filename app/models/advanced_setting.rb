2f6aca65fa623ece4ba1e1005453ef0f9cae1131
Jillian Tullo <jtullo@redhat.com>
2016-07-28 10:57:10 -0400

    camelCase to snake_case

--- app/models/advanced_setting.rb
+++ app/models/advanced_setting.rb
@@ -1,21 +1,21 @@
 class AdvancedSetting < ApplicationRecord
   belongs_to :resource, :polymorphic => true
 
-  def self.add_elements(parent, xmlNode)
-    add_missing_elements(parent, xmlNode, nil)
+  def self.add_elements(parent, xml_node)
+    add_missing_elements(parent, xml_node, nil)
   end
 
-  def self.add_missing_elements(parent, xmlNode, findPath)
-    hashes = xml_to_hashes(xmlNode, findPath)
+  def self.add_missing_elements(parent, xml_node, find_path)
+    hashes = xml_to_hashes(xml_node, find_path)
     EmsRefresh.save_advanced_settings_inventory(parent, hashes) if hashes
   end
 
-  def self.xml_to_hashes(xmlNode, findPath)
+  def self.xml_to_hashes(xml_node, find_path)
     result = []
-    if xmlNode.kind_of?(Array)
-      return xmlNode
+    if xml_node.kind_of?(Array)
+      return xml_node
     else
-      el = XmlFind.findElement(findPath, xmlNode.root)
+      el = XmlFind.findElement(find_path, xml_node.root)
       return nil unless MiqXml.isXmlElement?(el)
 
       el.each_element { |e| result << e.attributes.to_h }
