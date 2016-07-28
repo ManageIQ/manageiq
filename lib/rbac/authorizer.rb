0178170ad5c1f45f4cd2f6bb3c9c25dc4ef6a811
Chris Arcand <chris@chrisarcand.com>
2016-07-28 13:57:33 -0500

    Require feature argument in RBAC
    This is a slight API change - RBAC will require you to pass a feature
    argument. Before, you could technically pass an empty hash (or kwarg)
    from the front end and a simple log message would say you didn't pass
    one.
    
    Asking for permission to nothing is silly. So we're not allowing it.

--- lib/rbac/authorizer.rb
+++ lib/rbac/authorizer.rb
@@ -10,17 +10,12 @@ module Rbac
 
     end
 
-    def role_allows(user:, feature: nil, any: nil)
+    def role_allows(user:, feature:, any: nil)
       userid  = user.id
       role_id = user.miq_user_role.try(:id)
-      if feature
-        auth = any.present? ? user.role_allows_any?(:identifiers => [feature]) :
-          user.role_allows?(:identifier => feature)
-        _log.info("Auth #{auth ? "successful" : "failed"} for: userid [#{userid}], role id [#{role_id}], feature identifier [#{feature}]")
-      else
-        auth = false
-        _log.info("Auth #{auth ? "successful" : "failed"} for: userid [#{userid}], role id [#{role_id}], no main tab or feature passed to role_allows")
-      end
+      auth = any.present? ? user.role_allows_any?(:identifiers => [feature]) : user.role_allows?(:identifier => feature)
+      _log.info("Auth #{auth ? "successful" : "failed"} for: userid [#{userid}], role id [#{role_id}], feature identifier [#{feature}]")
+
       auth
     end
   end
