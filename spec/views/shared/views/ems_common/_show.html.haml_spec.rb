b417b1055cdf2d5fef2ed5ee61b75f44a15e9654
Martin Povolny <mpovolny@redhat.com>
2016-08-01 16:35:45 +0200

    Deduplicate ems*/show specs.

--- spec/views/shared/views/ems_common/_show.html.haml_spec.rb
+++ spec/views/shared/views/ems_common/_show.html.haml_spec.rb
@@ -1,30 +1,36 @@
 describe "shared/views/ems_common/show" do
-  let(:zone) { FactoryGirl.create(:zone) }
-  let(:ems_cloud) { FactoryGirl.create(:ems_openstack, :hostname => '1.1.1.1', :zone => zone) }
-  let(:action) { 'index' }
+  TestSetup = Struct.new(:ems_type, :helper)
+  [
+    TestSetup.new(:ems_openstack, EmsCloudHelper::TextualSummary),
+    TestSetup.new(:ems_vmware,    EmsInfraHelper::TextualSummary),
+  ].each do |setup|
+    let(:zone) { FactoryGirl.create(:zone) }
+    let(:ems) { FactoryGirl.create(setup.ems_type, :hostname => '1.1.1.1', :zone => zone) }
+    let(:action) { 'index' }
 
-  before do
-    view.extend EmsCloudHelper::TextualSummary
-    allow(MiqServer).to receive(:my_zone).and_return("default")
-    creds = {}
-    creds[:amqp] = {:userid => "amqp_user", :password => "amqp_password"}
-    ems_cloud.update_authentication(creds, :save => true)
-    ems_cloud.reload
-    assign(:ems, ems_cloud)
-    assign(:record, ems_cloud)
-    assign(:showtype, showtype)
-  end
-
-  context "when showtype is 'main'" do
-    let(:showtype) { "main" }
-    it "should not show '<Unknown> Credentials'" do
-      render
-      expect(rendered).not_to include('&lt;Unknown&gt; Credentials')
+    before do
+      view.extend setup.helper
+      allow(MiqServer).to receive(:my_zone).and_return("default")
+      creds = {}
+      creds[:amqp] = {:userid => "amqp_user", :password => "amqp_password"}
+      ems.update_authentication(creds, :save => true)
+      ems.reload
+      assign(:ems, ems)
+      assign(:record, ems)
+      assign(:showtype, showtype)
     end
 
-    it "should show 'AMQP Credentials'" do
-      render
-      expect(rendered).to include('AMQP Credentials')
+    context "when showtype is 'main'" do
+      let(:showtype) { "main" }
+      it "should not show '<Unknown> Credentials'" do
+        render
+        expect(rendered).not_to include('&lt;Unknown&gt; Credentials')
+      end
+
+      it "should show 'AMQP Credentials'" do
+        render
+        expect(rendered).to include('AMQP Credentials')
+      end
     end
   end
 end
