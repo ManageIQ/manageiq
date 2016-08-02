bb76489482de23b74e3a676b8e50bf8f62e24e3c
Robin Knaur <rknaur@redhat.com>
2016-08-02 15:48:59 +0200

    Instance Shelve Offload toolbar button

--- app/helpers/application_helper/toolbar/cloud/instance_operations_button_group_mixin.rb
+++ app/helpers/application_helper/toolbar/cloud/instance_operations_button_group_mixin.rb
@@ -53,7 +53,8 @@ module ApplicationHelper::Toolbar::Cloud::InstanceOperationsButtonGroupMixin
             N_('Shelve Offload this Instance'),
             N_('Shelve Offload'),
             :image   => "power_shelve_offload",
-            :confirm => N_("Shelve Offload this Instance?")),
+            :confirm => N_("Shelve Offload this Instance?"),
+            :klass   => ApplicationHelper::Button::InstanceShelveOffload),
           included_class.button(
             :instance_resume,
             nil,
