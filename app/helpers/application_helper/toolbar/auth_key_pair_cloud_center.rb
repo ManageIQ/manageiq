bb2d976af74484872bbc16a7f0a1322f7298dc78
Mike Dahlgren <dahlgren@gmail.com>
2016-08-03 09:18:36 -0500

    Simplify removal warning in auth_key_pair_cloud_center.rb

--- app/helpers/application_helper/toolbar/auth_key_pair_cloud_center.rb
+++ app/helpers/application_helper/toolbar/auth_key_pair_cloud_center.rb
@@ -12,7 +12,7 @@ class ApplicationHelper::Toolbar::AuthKeyPairCloudCenter < ApplicationHelper::To
           t = N_('Remove this Key Pair'),
           t,
           :url_parms => "&refresh=y",
-          :confirm   => N_("Warning: The selected Key Pair and ALL of its components will be permanently removed from the Virtual Management Database.  Are you sure you want to remove the selected Key Pair")),
+          :confirm   => N_("Warning: The selected Key Pair and ALL of its components will be permanently removed!")),
       ]
     ),
   ])
