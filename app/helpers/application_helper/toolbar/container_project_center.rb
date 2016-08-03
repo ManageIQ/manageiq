492b382d0ce0ecb4851dcedf4b2be534e29e9f73
Mike Dahlgren <dahlgren@gmail.com>
2016-08-03 09:40:57 -0500

    Simplify removal warning for container_project_center.rb

--- app/helpers/application_helper/toolbar/container_project_center.rb
+++ app/helpers/application_helper/toolbar/container_project_center.rb
@@ -18,7 +18,7 @@ class ApplicationHelper::Toolbar::ContainerProjectCenter < ApplicationHelper::To
           t = N_('Remove this Project from the VMDB'),
           t,
           :url_parms => "&refresh=y",
-          :confirm   => N_("Warning: This Project and ALL of its components will be permanently removed from the Virtual Management Database.  Are you sure you want to remove this Project?")),
+          :confirm   => N_("Warning: This Project and ALL of its components will be permanently removed!")),
       ]
     ),
   ])
