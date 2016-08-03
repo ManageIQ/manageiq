ffa74ec877ac2e23fab895bb17b30a6eee6475b4
Brandon Dunne <bdunne@redhat.com>
2016-08-03 12:24:53 -0400

    Describe the correct thing

--- spec/lib/miq_automation_engine/service_methods/miq_ae_service_manageiq-providers-openstack-cloud_manager-vm_spec.rb
+++ spec/lib/miq_automation_engine/service_methods/miq_ae_service_manageiq-providers-openstack-cloud_manager-vm_spec.rb
@@ -1,7 +1,7 @@
-module MiqAeServiceVmVmwareSpec
-  describe MiqAeMethodService::MiqAeServiceUser do
+module MiqAeServiceVmOpenstackSpec
+  describe MiqAeMethodService::MiqAeServiceManageIQ_Providers_Openstack_CloudManager_Vm do
     let(:vm)         { FactoryGirl.create(:vm_openstack) }
-    let(:service_vm) { MiqAeMethodService::MiqAeServiceManageIQ_Providers_Openstack_CloudManager_Vm.find(vm.id) }
+    let(:service_vm) { described_class.find(vm.id) }
 
     before do
       allow_any_instance_of(Vm).to receive(:my_zone).and_return('default')
