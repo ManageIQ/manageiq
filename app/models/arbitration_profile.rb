35a10b617e71e1768413ef9084a5fb1bf763c21b
Jillian Tullo <jtullo@redhat.com>
2016-08-03 11:58:00 -0400

    adding default scopes to arbitration profile and virtual templates

--- app/models/arbitration_profile.rb
+++ app/models/arbitration_profile.rb
@@ -1,4 +1,6 @@
 class ArbitrationProfile < ApplicationRecord
+  default_scope { where(:profile => true) }
+
   validates :ext_management_system, :presence => true
   validates :name, :presence => true
   validate :falsify_all_others, :if => :default_profile_changed?
--- app/models/manageiq/providers/cloud_manager/virtual_template.rb
+++ app/models/manageiq/providers/cloud_manager/virtual_template.rb
@@ -1,4 +1,6 @@
 class ManageIQ::Providers::CloudManager::VirtualTemplate < ArbitrationProfile
+  default_scope { where(:profile => false) }
+
   validates :ext_management_system, :presence => true
 
   default_value_for :profile, false
