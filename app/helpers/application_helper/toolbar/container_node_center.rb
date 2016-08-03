54ca264ebf6494b73ca7d735281cabbf07f684f2
Erez Freiberger <efreiber@redhat.com>
2016-08-03 09:02:43 +0300

    adding cockpit button to container node

--- app/helpers/application_helper/toolbar/container_node_center.rb
+++ app/helpers/application_helper/toolbar/container_node_center.rb
@@ -72,4 +72,13 @@ class ApplicationHelper::Toolbar::ContainerNodeCenter < ApplicationHelper::Toolb
       ]
     ),
   ])
+  button_group('container_cockpit_button_group', [
+    button(
+      :vm_cockpit,
+      nil,
+      N_('Open a new browser window with Cockpit for this Node.  This requires that Cockpit is pre-configured on the Node.'),
+      nil,
+      :image => "cockpit",
+      :url   => "launch_cockpit"),
+  ])
 end
