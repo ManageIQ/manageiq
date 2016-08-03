1ac4516c33e714d41e414e72d774284bc8a94df9
Chris Arcand <chris@chrisarcand.com>
2016-08-03 16:41:08 -0500

    Remove set_user_privileges :scissors:
    With the addition of #stub_user, this [very janky, barely works] method
    can be removed.

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -7,16 +7,6 @@ module ControllerSpecHelper
     end
   end
 
-  def set_user_privileges(user = FactoryGirl.create(:user_with_group))
-    allow(User).to receive(:server_timezone).and_return("UTC")
-    allow_any_instance_of(described_class).to receive(:set_user_time_zone)
-
-    # TODO: remove these stubs
-    allow(controller).to receive(:check_privileges).and_return(true)
-    login_as user
-    allow_any_instance_of(User).to receive(:role_allows?).and_return(true)
-  end
-
   def setup_zone
     EvmSpecHelper.create_guid_miq_server_zone
   end
