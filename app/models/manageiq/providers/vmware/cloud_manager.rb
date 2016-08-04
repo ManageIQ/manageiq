a4283a97a97ce17fa9d085e012188464ba5a0d4c
Miha Pleško <miha.plesko@xlab.si>
2016-08-04 13:49:22 +0200

    Add missing require statement
    This patch fixes this [PR](https://github.com/ManageIQ/manageiq/pull/10062)
    where OrchestrationStack module was not registered in automation system.
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- app/models/manageiq/providers/vmware/cloud_manager.rb
+++ app/models/manageiq/providers/vmware/cloud_manager.rb
@@ -1,4 +1,5 @@
 class ManageIQ::Providers::Vmware::CloudManager < ManageIQ::Providers::CloudManager
+  require_nested :OrchestrationStack
   require_nested :RefreshParser
   require_nested :RefreshWorker
   require_nested :Refresher
---
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-vmware-cloud_manager-orchestration_stack.rb
@@ -0,0 +1,4 @@
+module MiqAeMethodService
+  class MiqAeServiceManageIQ_Providers_Vmware_CloudManager_OrchestrationStack < MiqAeServiceManageIQ_Providers_CloudManager_OrchestrationStack
+  end
+end
