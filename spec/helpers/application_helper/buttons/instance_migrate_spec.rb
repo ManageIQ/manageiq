f5b9b7ca4ee4d1b8edbe677d0390e977bfa5c990
Brandon Dunne <bdunne@redhat.com>
2016-08-04 17:27:50 -0400

    Get test passing since stubbing was never updated for SupportsFeatureMixin

--- spec/helpers/application_helper/buttons/instance_migrate_spec.rb
+++ spec/helpers/application_helper/buttons/instance_migrate_spec.rb
@@ -1,18 +1,17 @@
 describe ApplicationHelper::Button::InstanceMigrate do
+  let(:migration_supported_vm)   { Class.new(VmOrTemplate) { supports(:live_migrate) }.new }
+  let(:migration_unsupported_vm) { Class.new(VmOrTemplate) { supports_not(:live_migrate, :reason => "unavailable") }.new }
+
   describe '#disabled?' do
     it "when the live migrate action is available then the button is not disabled" do
       view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(
-        view_context, {}, {"record" => object_double(VmCloud.new, :is_available? => true)}, {}
-      )
+      button = described_class.new(view_context, {}, {"record" => migration_supported_vm}, {})
       expect(button.disabled?).to be false
     end
 
     it "when the live migrate action is unavailable then the button is disabled" do
       view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(
-        view_context, {}, {"record" => object_double(VmCloud.new, :is_available? => false)}, {}
-      )
+      button = described_class.new(view_context, {}, {"record" => migration_unsupported_vm}, {})
       expect(button.disabled?).to be true
     end
   end
@@ -20,19 +19,14 @@ describe ApplicationHelper::Button::InstanceMigrate do
   describe '#calculate_properties' do
     it "when the live migrate action is unavailable the button has the error in the title" do
       view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(
-        view_context, {}, {"record" => object_double(
-          VmCloud.new, :is_available? => false, :is_available_now_error_message => "unavailable")}, {}
-      )
+      button = described_class.new(view_context, {}, {"record" => migration_unsupported_vm}, {})
       button.calculate_properties
       expect(button[:title]).to eq("unavailable")
     end
 
     it "when the live migrate is avaiable, the button has no error in the title" do
       view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(
-        view_context, {}, {"record" => object_double(VmCloud.new, :is_available? => true)}, {}
-      )
+      button = described_class.new(view_context, {}, {"record" => migration_supported_vm}, {})
       button.calculate_properties
       expect(button[:title]).to be nil
     end
