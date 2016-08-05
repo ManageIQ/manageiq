c466da162b4aff7bba81ea3e01d10660db11459f
Harpreet Kataria <hkataria@redhat.com>
2016-08-05 10:15:03 -0400

    Fixed rubocop warnings on toolbar files.
    Also fixed copy/pasted id of button_group

--- app/helpers/application_helper/toolbar/arbitration_profile_center.rb
+++ app/helpers/application_helper/toolbar/arbitration_profile_center.rb
@@ -1,26 +1,27 @@
 class ApplicationHelper::Toolbar::ArbitrationProfileCenter < ApplicationHelper::Toolbar::Basic
-  button_group('ems_cloud_vmdb', [
-    select(
-      :arbitration_profile_vmdb_choice,
-      'fa fa-cog fa-lg',
-      t = N_('Configuration'),
-      t,
-      :items => [
-        button(
-          :arbitration_profile_edit,
-          'pficon pficon-edit fa-lg',
-          t = N_('Edit this Arbitration Profile'),
-          t,
-          :association_id => true,
-          :full_path => "<%= edit_ems_cloud_path(@ems) %>"),
-        button(
-          :arbitration_profile_delete,
-          'pficon pficon-delete fa-lg',
-          t = N_('Remove this Arbitration Profile from the VMDB'),
-          t,
-          :association_id => true,
-          :confirm   => N_("Warning: This Arbitration Profile will be permanently removed from the Virtual Management Database.  Are you sure you want to remove this Arbitration Profile?")),
-      ]
-    ),
-  ])
+  button_group('arbitration_profile_vmdb', [
+                 select(
+                   :arbitration_profile_vmdb_choice,
+                   'fa fa-cog fa-lg',
+                   t = N_('Configuration'),
+                   t,
+                   :items => [
+                     button(
+                       :arbitration_profile_edit,
+                       'pficon pficon-edit fa-lg',
+                       t = N_('Edit this Arbitration Profile'),
+                       t,
+                       :association_id => true,
+                       :full_path      => "<%= edit_ems_cloud_path(@ems) %>"),
+                     button(
+                       :arbitration_profile_delete,
+                       'pficon pficon-delete fa-lg',
+                       t = N_('Remove this Arbitration Profile from the VMDB'),
+                       t,
+                       :association_id => true,
+                       :confirm        => N_("Warning: This Arbitration Profile will be permanently removed from the Virtual Management Database.  Are you sure you want to remove this Arbitration Profile?")),
+                   ]
+                 ),
+               ]
+              )
 end
