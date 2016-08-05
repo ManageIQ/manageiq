10ad5d4bdb077baf9b549821b2b9488113e5409f
Ladislav Smola <lsmola@redhat.com>
2016-08-05 16:45:43 +0200

    LBaaS load_balancer_pool model
    LBaaS load_balancer_pool model

---
+++ app/models/load_balancer_pool.rb
@@ -0,0 +1,9 @@
+class LoadBalancerPool < ApplicationRecord
+  belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::NetworkManager"
+  belongs_to :cloud_tenant
+  belongs_to :load_balancer
+
+  has_many :load_balancer_listener_pools
+  has_many :load_balancer_listeners, :through => :load_balancer_listener_pools
+  has_many :load_balancer_pool_members
+end
