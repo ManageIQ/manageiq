49e27bc34bb4faae8e332bdb6ed10c47f1e46a65
Jason Frey <fryguy9@gmail.com>
2016-08-05 11:37:35 -0400

    Merge pull request #10228 from bdunne/fix_spec_file_names
    Fix spec file names
    (cherry picked from commit d7fefd2b1c72345283dece2de0bc3016776e8dea)

--- spec/helpers/container_summary_helper_spec.rb
+++ spec/helpers/container_summary_helper_spec.rb
@@ -1,8 +1,8 @@
 describe ContainerSummaryHelper do
-  REL_HASH_WITH_LINK = [:label, :image, :value, :link, :title]
-  REL_HASH_WITHOUT_LINK = [:label, :image, :value]
+  let(:container_project)     { FactoryGirl.create(:container_project) }
+  let(:rel_hash_with_link)    { [:label, :image, :value, :link, :title] }
+  let(:rel_hash_without_link) { [:label, :image, :value] }
 
-  let(:container_project) { FactoryGirl.create(:container_project) }
   before do
     self.class.send(:include, ApplicationHelper)
 
@@ -17,14 +17,14 @@ describe ContainerSummaryHelper do
     it 'show link when role allows' do
       allow(@user).to receive(:role_allows?).and_return(true)
 
-      expect(subject.keys).to eq(REL_HASH_WITH_LINK)
+      expect(subject.keys).to eq(rel_hash_with_link)
       expect(subject[:value]).to eq(container_project.name)
     end
 
     it 'hide link when role does not allow' do
       allow(@user).to receive(:role_allows?).and_return(false)
 
-      expect(subject.keys).to eq(REL_HASH_WITHOUT_LINK)
+      expect(subject.keys).to eq(rel_hash_without_link)
       expect(subject[:value]).to eq(container_project.name)
     end
   end
@@ -36,14 +36,14 @@ describe ContainerSummaryHelper do
     it 'show link when role allows' do
       allow(@user).to receive(:role_allows?).and_return(true)
 
-      expect(subject.keys).to eq(REL_HASH_WITH_LINK)
+      expect(subject.keys).to eq(rel_hash_with_link)
       expect(subject[:value]).to eq("2")
     end
 
     it 'hide link when role does not allow' do
       allow(@user).to receive(:role_allows?).and_return(false)
 
-      expect(subject.keys).to eq(REL_HASH_WITHOUT_LINK)
+      expect(subject.keys).to eq(rel_hash_without_link)
       expect(subject[:value]).to eq("2")
     end
   end
