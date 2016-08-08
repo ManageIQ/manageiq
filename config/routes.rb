b340bcee4dcff745f6e9f3f9b69fd5ce82738b35
Jozef Zigmund <jzigmund@redhat.com>
2016-08-08 16:09:14 +0200

    Added optional adv_search routes for Network Routers

--- config/routes.rb
+++ config/routes.rb
@@ -1354,6 +1354,7 @@ Vmdb::Application.routes.draw do
       :post => %w(
         button
         quick_search
+        listnav_search_selected
         show
         show_list
         tag_edit_form_field_changed
@@ -1361,6 +1362,7 @@ Vmdb::Application.routes.draw do
       ) +
         adv_search_post +
         compare_post +
+        save_post +
         exp_post
     },
 
