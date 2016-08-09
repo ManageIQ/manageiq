b3ce6974f1d20b83d545f44c3fa116cc103d9618
Jillian Tullo <jtullo@redhat.com>
2016-08-09 14:46:58 -0400

    more cleanup

--- app/models/miq_arbitration.rb
+++ app/models/miq_arbitration.rb
@@ -1,23 +1,21 @@
 class MiqArbitration
-  # Will want to take in an MIQ Request
-  # highest to lowest priority
-  RULE_PRIORITIES = %w(Blueprint User).freeze
-
   def self.arbitrate_from_request(requester, blueprint)
     @requester = requester
     @blueprint = validate_blueprint(blueprint)
-    @rules = {
-      'Blueprint' => [],
-      'User'      => []
-    }
-    rules_init
     execute_rules
   end
 
   class << self
-    attr_reader :rules
+    attr_accessor :blueprint, :requester
 
     private
+
+    # Hardcoding rule priorities for now until system of
+    # setting up priorities is in place
+    RULE_PRIORITIES = {
+      'Blueprint' => 'blueprint',
+      'User'      => 'requester'
+    }.freeze
 
     def validate_blueprint(blueprint)
       User.with_user(@requester) do
@@ -28,27 +26,11 @@ class MiqArbitration
     end
 
     def execute_rules
-      RULE_PRIORITIES.each do |rule_name|
-        send("#{rule_name.downcase}_execute")
-      end
-    end
-
-    def blueprint_execute
-      rules['Blueprint'].each do |rule|
-        send(rule.operation, rule) if rule.expression.evaluate(@blueprint)
-      end
-    end
-
-    def user_execute
-      rules['User'].each do |rule|
-        send(rule.operation, rule) if rule.expression.evaluate(@requester)
-      end
-    end
-
-    def rules_init
-      ArbitrationRule.all.each do |rule|
-        expression = rule.expression.class_details
-        rules[expression].push(rule) if RULE_PRIORITIES.include?(expression)
+      RULE_PRIORITIES.each do |rule_name, attribute|
+        rules = ArbitrationRule.get_by_rule_class(rule_name)
+        rules.each do |rule|
+          send(rule.operation, rule) if rule.expression.evaluate(send(attribute))
+        end
       end
     end
 
