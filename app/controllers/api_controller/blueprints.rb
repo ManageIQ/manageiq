397e3284bac78d730c1fb0acde95552fb229b43d
Tim Wade <hello@timjwade.com>
2016-08-11 11:58:18 -0700

    Make bundle fields optional

--- app/controllers/api_controller/blueprints.rb
+++ app/controllers/api_controller/blueprints.rb
@@ -12,16 +12,19 @@ class ApiController
     private
 
     def create_bundle(blueprint, bundle)
-      service_catalog = ServiceTemplateCatalog.find(parse_id(bundle["service_catalog"], :service_catalogs))
-      service_dialog = Dialog.find(parse_id(bundle["service_dialog"], :service_dialogs))
-      service_templates = bundle["service_templates"].collect do |st|
+      service_catalog = if bundle["service_catalog"]
+                          ServiceTemplateCatalog.find(parse_id(bundle["service_catalog"], :service_catalogs))
+                        end
+      service_dialog = if bundle["service_dialog"]
+                         Dialog.find(parse_id(bundle["service_dialog"], :service_dialogs))
+                       end
+      service_templates = bundle.fetch("service_templates", []).collect do |st|
         ServiceTemplate.find(parse_id(st, :service_templates))
       end
       automate_entrypoints = bundle["automate_entrypoints"]
-      blueprint.create_bundle(service_templates,
-                              service_dialog,
-                              service_catalog,
-                              "entry_points" => automate_entrypoints)
+      options = {}
+      options["entry_points"] = automate_entrypoints if automate_entrypoints
+      blueprint.create_bundle(service_templates, service_dialog, service_catalog, options)
     rescue => e
       raise BadRequestError, "Couldn't create the bundle - #{e}"
     end
