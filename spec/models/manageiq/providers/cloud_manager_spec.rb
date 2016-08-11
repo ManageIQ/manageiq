acc51ce4486501cd6e5037aef65a9a5e7ecb6145
lpichler <lpichler@redhat.com>
2016-08-11 18:11:48 +0200

    Spec for ensuring that tenants have been created correctly
    and updated

--- spec/models/manageiq/providers/cloud_manager_spec.rb
+++ spec/models/manageiq/providers/cloud_manager_spec.rb
@@ -25,4 +25,51 @@ describe EmsCloud do
                       ManageIQ::Providers::Vmware::CloudManager].collect(&:ems_type)
     expect(described_class.supported_types).to match_array(expected_types)
   end
+
+  context "CloudTenant Mapping" do
+    describe "#sync_cloud_tenants_with_tenants" do
+      let(:ems_cloud)              { FactoryGirl.create(:ems_openstack) }
+      let(:name_of_created_tenant) { "#{described_class::OPENSTACK_CLOUD_PROVIDER} #{ems_cloud.name}" }
+      let!(:default_tenant)        { Tenant.seed }
+
+      it "creates tenant related to provider" do
+        ems_cloud.sync_cloud_tenants_with_tenants
+
+        ems_cloud.reload
+
+        expect(ems_cloud.source_tenant).not_to be_nil
+        expect(ems_cloud.source_tenant.name).to eq(name_of_created_tenant)
+        expect(ems_cloud.source_tenant.description).to eq(name_of_created_tenant)
+      end
+
+      it "creates only the one tenant per openstack provider when synchronization was executed multiple times" do
+        count_of_tenants = Tenant.count
+
+        ems_cloud.sync_cloud_tenants_with_tenants
+
+        expect(Tenant.count).to eq(count_of_tenants + 1)
+
+        ems_cloud.sync_cloud_tenants_with_tenants
+        ems_cloud.sync_cloud_tenants_with_tenants
+
+        expect(Tenant.count).to eq(count_of_tenants + 1)
+      end
+
+      it "name and description of tenant created by openstack is updated" do
+        ems_cloud.sync_cloud_tenants_with_tenants
+
+        ems_cloud.reload
+
+        ems_cloud.source_tenant.update_attributes(:name => "XXX", :description => "XXX")
+        ems_cloud.save
+
+        ems_cloud.sync_cloud_tenants_with_tenants
+
+        ems_cloud.reload
+
+        expect(ems_cloud.source_tenant.name).to eq(name_of_created_tenant)
+        expect(ems_cloud.source_tenant.description).to eq(name_of_created_tenant)
+      end
+    end
+  end
 end
