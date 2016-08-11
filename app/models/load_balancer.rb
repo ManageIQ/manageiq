3e5e6c5dd2fdc0c76c2808f58a180dac4d5b493d
Ladislav Smola <lsmola@redhat.com>
2016-08-11 15:09:54 +0200

    LBaaS enhance load_balancer model
    - load balancer has_many health checks assigned
    - add through relation for load_balancer_pools and
      load_balancer_pool_members
    - relation to main cloud entities VMs and Resource groups

--- app/models/load_balancer.rb
+++ app/models/load_balancer.rb
@@ -2,13 +2,17 @@ class LoadBalancer < ApplicationRecord
   belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::NetworkManager"
   belongs_to :cloud_tenant
 
+  has_many :load_balancer_health_checks
   has_many :load_balancer_listeners
-  has_many :load_balancer_pools
-  has_many :load_balancer_pool_members, :through => :load_balancer_pools
+  has_many :load_balancer_pools, :through => :load_balancer_listeners
+  has_many :load_balancer_pool_members, -> { distinct }, :through => :load_balancer_pools
 
   has_many :network_ports
   has_many :cloud_subnet_network_ports, :through => :network_ports
   has_many :cloud_subnets, :through => :cloud_subnet_network_ports
   has_many :floating_ips, :through => :network_ports
   has_many :security_groups, -> { distinct }, :through => :network_ports
+
+  has_many :vms, -> { distinct }, :through => :load_balancer_pool_members
+  has_many :resource_groups, -> { distinct }, :through => :load_balancer_pool_members
 end
