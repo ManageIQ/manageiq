d067b8c453bf9c3445bc7bdadeabeab04559c7ac
Martin Povolny <mpovolny@redhat.com>
2016-08-12 16:37:08 +0200

    Fix styling bug in menu editor.
    https://bugzilla.redhat.com/show_bug.cgi?id=1346765

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -768,7 +768,8 @@ class ReportController < ApplicationController
         end
         presenter.update(:main_div, r[:partial => partial])
         presenter[:element_updates][:menu1_legend] = {:legend => fieldset_title}
-        presenter.show(:menu_div1, :treeStatus).hide(:menu_div2, :flash_msg_div_menu_list)
+        presenter.show(:menu_div1).hide(:menu_div2, :flash_msg_div_menu_list)
+        presenter[:element_updates][:menu_roles_treebox] = {:class => 'disabled', :add => true}
         presenter[:element_updates][:folder_top]      = {:title => img_title_top}
         presenter[:element_updates][:folder_up]       = {:title => img_title_up}
         presenter[:element_updates][:folder_down]     = {:title => img_title_down}
@@ -783,19 +784,22 @@ class ReportController < ApplicationController
         unless @sb[:role_list_flag]
           # we dont need to show the overlay on first time load
           @sb[:role_list_flag] = true
-          presenter.show(:treeStatus)
+          presenter[:element_updates][:menu_roles_treebox] = {:class => 'disabled', :add => true}
         end
         presenter.hide(:menu_div1, :menu_div2).show(:menu_div3)
       end
     elsif nodetype == "menu_default" || nodetype == "menu_reset"
       presenter.update(:main_div, r[:partial => partial])
       presenter.replace(:menu_div1, r[:partial => "menu_form1", :locals => {:folders => @grid_folders}])
-      presenter.hide(:menu_div1, :menu_div2).show(:menu_div3).hide(:treeStatus)
+      presenter.hide(:menu_div1, :menu_div2).show(:menu_div3)
+      presenter[:element_updates][:menu_roles_treebox] = {:class => 'disabled', :remove => true}
+
       # set changed to true if menu has been set to default
       session[:changed] = @sb[:menu_default] ? true : (@edit[:new] != @edit[:current])
     elsif nodetype == "menu_edit_reports"
       presenter.replace(:flash_msg_div_menu_list, r[:partial => "layouts/flash_msg", :locals => {:div_num => "_menu_list"}]) if @flash_array
-      presenter.show(:menu_div1, :treeStatus)
+      presenter.show(:menu_div1)
+      presenter[:element_updates][:menu_roles_treebox] = {:class => 'disabled', :add => true}
       presenter.replace(:menu_div2, r[:partial => "menu_form2"])
       presenter.hide(:menu_div1, :menu_div3).show(:menu_div2)
     elsif nodetype == "menu_commit_reports"
@@ -816,7 +820,7 @@ class ReportController < ApplicationController
         else
           presenter.hide(:menu_div1, :menu_div3).show(:menu_div2)
         end
-        presenter.hide(:treeStatus)
+        presenter[:element_updates][:menu_roles_treebox] = {:class => 'disabled', :remove => true}
       end
     elsif nodetype == 'menu_commit_folders'
       # Hide flash_msg if it's being shown from New folder add event
@@ -831,13 +835,15 @@ class ReportController < ApplicationController
         presenter.show(:menu_div1).hide(:menu_div2, :menu_div3)
       else
         presenter.replace(:menu_roles_div, r[:partial => "role_list"])
-        presenter.hide(:menu_div1, :menu_div2).show(:menu_div3).hide(:treeStatus)
+        presenter.hide(:menu_div1, :menu_div2).show(:menu_div3)
+        presenter[:element_updates][:menu_roles_treebox] = {:class => 'disabled', :remove => true}
       end
       @sb[:tree_err] = false
     elsif nodetype == 'menu_discard_folders' || nodetype == 'menu_discard_reports'
       presenter.replace(:flash_msg_div_menu_list, r[:partial => 'layouts/flash_msg', :locals => {:div_num => '_menu_list'}])
       presenter.replace(:menu_div1,               r[:partial => 'menu_form1', :locals => {:folders => @grid_folders}])
-      presenter.hide(:menu_div1, :menu_div2).show(:menu_div3).hide(:treeStatus)
+      presenter.hide(:menu_div1, :menu_div2).show(:menu_div3)
+      presenter[:element_updates][:menu_roles_treebox] = {:class => 'disabled', :remove => true}
     end
 
     if x_active_tree == :roles_tree && x_node != "root"
