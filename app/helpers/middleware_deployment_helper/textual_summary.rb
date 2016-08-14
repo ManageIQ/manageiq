ed16fa066ae4e3d5bbc950655613ef16634a172b
Alissa Bonas <abonas@redhat.com>
2016-08-14 18:32:22 +0300

    Remove replicated code across middleware summary helpers

--- app/helpers/middleware_deployment_helper/textual_summary.rb
+++ app/helpers/middleware_deployment_helper/textual_summary.rb
@@ -1,5 +1,4 @@
 module MiddlewareDeploymentHelper::TextualSummary
-  include TextualMixins::TextualName
   #
   # Groups
   #
@@ -11,14 +10,6 @@ module MiddlewareDeploymentHelper::TextualSummary
   def textual_group_relationships
     # Order of items should be from parent to child
     %i(ems middleware_server)
-  end
-
-  def textual_group_smart_management
-    %i(tags)
-  end
-
-  def textual_nativeid
-    @record.nativeid
   end
 
   def textual_status
--- app/helpers/middleware_summary_helper.rb
+++ app/helpers/middleware_summary_helper.rb
@@ -1,6 +1,16 @@
 module MiddlewareSummaryHelper
+  include TextualMixins::TextualName
+
   def textual_ems
     textual_link(@record.ext_management_system)
+  end
+
+  def textual_nativeid
+    @record.nativeid
+  end
+
+  def textual_group_smart_management
+    %i(tags)
   end
 
   def textual_middleware_servers
