a3246460911ac6a8d15c34b8a22ab346e897a990
Šimon Lukašík <isimluk@fedoraproject.org>
2016-08-15 09:41:42 +0200

    Use pupulation-sd when sample size is 1
    There are two formulas for calculating standard deviation. One known
    as population standard deviation another known as sample standard
    deviation. The sample version returns NaN on samples of single data
    point. The population-sd of single data point equals to 0.

--- app/models/metric/long_term_averages.rb
+++ app/models/metric/long_term_averages.rb
@@ -62,7 +62,7 @@ module Metric::LongTermAverages
       Metric::Aggregation::Process.average(c, nil, results[:avg], counts)
 
       begin
-        results[:dev][c]  = vals[c].stddev
+        results[:dev][c] = vals[c].length == 1 ? 0 : vals[c].stddev
         raise StandardError, "result was NaN" if results[:dev][c].try(:nan?)
       rescue => err
         _log.warn("Unable to calculate standard deviation, '#{err.message}', values: #{vals[c].inspect}")
