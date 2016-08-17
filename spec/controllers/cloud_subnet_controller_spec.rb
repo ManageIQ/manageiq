04d869e3c0caa435eb230c44ac65ca5cc56a8c98
Ladislav Smola <lsmola@redhat.com>
2016-08-17 13:45:37 +0200

    Add a spec checking child subnets render under cloud_subnets
    Add a spec checking child subnets render under cloud_subnets

--- spec/controllers/cloud_subnet_controller_spec.rb
+++ spec/controllers/cloud_subnet_controller_spec.rb
@@ -44,6 +44,24 @@ describe CloudSubnetController do
 
           is_expected.to render_template(:partial => "layouts/listnav/_cloud_subnet")
         end
+
+        it "shows associated cloud_subnets" do
+          child_subnet = FactoryGirl.create(
+            "cloud_subnet_#{t}".to_sym, :name => "Child Cloud Subnet", :parent_cloud_subnet => @cloud_subnet)
+
+          controller.instance_variable_set(:@breadcrumbs, [])
+          get :show, :params => {:id => @cloud_subnet.id, :display => 'cloud_subnets'}
+          expect(response.status).to eq(200)
+          expect(response).to render_template('cloud_subnet/show')
+          expect(response).to render_template('layouts/listnav/_cloud_subnet')
+          expect(assigns(:breadcrumbs)).to eq([{:name => "#{@cloud_subnet.name} (All Cloud Subnets)",
+                                                :url  => "/cloud_subnet/show/#{@cloud_subnet.id}?display=cloud_subnets"}])
+          child_subnet_row = "miqRowClick(&#39;#{child_subnet.compressed_id}&#39;, &#39;/cloud_subnet/show/&#39;"
+          expect(response.body).to include(child_subnet_row)
+
+          # display needs to be saved to session for GTL pagination and such
+          expect(session[:cloud_subnet_display]).to eq('cloud_subnets')
+        end
       end
 
       describe "#test_toolbars" do
