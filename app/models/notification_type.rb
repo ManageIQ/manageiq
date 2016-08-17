6dc26e5b89c268898109aaa78fdf962966da9267
Šimon Lukašík <isimluk@fedoraproject.org>
2016-08-17 16:07:06 +0200

    Seed first notification types
    We will add much more notification types as we will need them.

--- app/models/notification_type.rb
+++ app/models/notification_type.rb
@@ -3,4 +3,22 @@ class NotificationType < ApplicationRecord
   validates :message, :presence => true
   validates :level, :inclusion => { :in => %w(success error warning info) }
   validates :audience, :inclusion => { :in => %w(user tenant global) }
+
+  def self.seed
+    seed_data.each do |t|
+      rec = find_by_name(t[:name])
+      t[:expires_in] = $1.to_i.send($2).to_i if t[:expires_in] =~ /^(\d+) (minutes?|hours?|days?)$/
+      if rec.nil?
+        create(t)
+      else
+        rec.update_attributes(t)
+        rec.save!
+      end
+    end
+  end
+
+  def self.seed_data
+    fixture_file = File.join(FIXTURE_DIR, 'notification_types.yml')
+    File.exist?(fixture_file) ? YAML.load_file(fixture_file) : []
+  end
 end
--- spec/models/notification_type_spec.rb
+++ spec/models/notification_type_spec.rb
@@ -1,3 +1,15 @@
 describe NotificationType, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.seed' do
+    subject { described_class.count }
+    context 'before the seed is run' do
+      it { is_expected.to be_zero }
+    end
+    context 'after the seed is run' do
+      before { described_class.seed }
+      it { is_expected.to be > 0 }
+      it 'can be run again without any effects' do
+        expect { described_class.seed }.not_to change(described_class, :count)
+      end
+    end
+  end
 end
