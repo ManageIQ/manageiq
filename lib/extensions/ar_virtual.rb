999d0c39a5ea4fe4c0db135f876fd6d17b584230
Keenan Brock <kbrock@redhat.com>
2016-08-18 10:55:05 -0400

    virtual_delegate: copy active support delegate

--- lib/extensions/ar_virtual.rb
+++ lib/extensions/ar_virtual.rb
@@ -70,7 +70,7 @@ module VirtualDelegates
       unless (to = options[:to])
         raise ArgumentError, 'Delegation needs an association. Supply an options hash with a :to key as the last argument (e.g. delegate :hello, to: :greeter).'
       end
-      delegate(*methods, options.except(:arel, :uses))
+      define_delegate(*methods, **options.except(:arel, :uses))
 
       # put method entry per method name.
       # This better supports reloading of the class and changing the definitions
@@ -105,6 +105,70 @@ module VirtualDelegates
       raise "unknown attribute #{to_model.name}##{col} referenced in #{name}" unless type
       arel = virtual_delegate_arel(col, to, to_model, to_ref)
       define_virtual_attribute method_name, type, :uses => (options[:uses] || to), :arel => arel
+    end
+
+    def define_delegate(*methods, to: nil, prefix: nil, allow_nil: nil)
+      unless to
+        raise ArgumentError, 'Delegation needs a target. Supply an options hash with a :to key as the last argument (e.g. delegate :hello, to: :greeter).'
+      end
+
+      if prefix == true && to =~ /^[^a-z_]/
+        raise ArgumentError, 'Can only automatically set the delegation prefix when delegating to a method.'
+      end
+
+      method_prefix = \
+        if prefix
+          "#{prefix == true ? to : prefix}_"
+        else
+          ''
+        end
+
+      location = caller_locations(1, 1).first
+      file, line = location.path, location.lineno
+
+      to = to.to_s
+      #to = "self.#{to}" if DELEGATION_RESERVED_METHOD_NAMES.include?(to)
+
+      methods.each do |method|
+        # Attribute writer methods only accept one argument. Makes sure []=
+        # methods still accept two arguments.
+        definition = (method =~ /[^\]]=$/) ? 'arg' : '*args, &block'
+
+        # The following generated method calls the target exactly once, storing
+        # the returned value in a dummy variable.
+        #
+        # Reason is twofold: On one hand doing less calls is in general better.
+        # On the other hand it could be that the target has side-effects,
+        # whereas conceptually, from the user point of view, the delegator should
+        # be doing one call.
+        if allow_nil
+          method_def = [
+            "def #{method_prefix}#{method}(#{definition})",
+            "_ = #{to}",
+            "if !_.nil? || nil.respond_to?(:#{method})",
+            "  _.#{method}(#{definition})",
+            "end",
+          "end"
+          ].join ';'
+        else
+          exception = %(raise DelegationError, "#{self}##{method_prefix}#{method} delegated to #{to}.#{method}, but #{to} is nil: \#{self.inspect}")
+
+          method_def = [
+            "def #{method_prefix}#{method}(#{definition})",
+            " _ = #{to}",
+            "  _.#{method}(#{definition})",
+            "rescue NoMethodError => e",
+            "  if _.nil? && e.name == :#{method}",
+            "    #{exception}",
+            "  else",
+            "    raise",
+            "  end",
+            "end"
+          ].join ';'
+        end
+
+        module_eval(method_def, file, line)
+      end
     end
 
     def virtual_delegate_name_prefix(prefix, to)
