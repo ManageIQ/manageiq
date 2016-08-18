ec39fa5ad9339c5528e32c78b56f9908f961984f
Richard Oliveri <oliveri.richard.github@gmail.com>
2016-08-18 16:46:16 -0400

    Merge pull request #10067 from enoodle/container_ssa_refactor_and_cancel_option
    Container ssa cancel option
    (cherry picked from commit af45d126aaaf3d71059171d9a951b705616a0adf)

--- app/models/manageiq/providers/kubernetes/container_manager/scanning/job.rb
+++ app/models/manageiq/providers/kubernetes/container_manager/scanning/job.rb
@@ -19,14 +19,18 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::Scanning::Job < Job
     {
       :initializing => {'initializing'     => 'waiting_to_start'},
       :start        => {'waiting_to_start' => 'pod_create'},
-      :pod_wait     => {'pod_create'       => 'waiting_to_scan'},
+      :pod_wait     => {'pod_create'       => 'waiting_to_scan',
+                        'waiting_to_scan'  => 'waiting_to_scan'},
       :analyze      => {'waiting_to_scan'  => 'scanning'},
       :data         => {'scanning'      => 'synchronizing',
                         'synchronizing' => 'synchronizing'},
       :cleanup      => {'synchronizing'    => 'pod_delete'},
       :abort_job    => {'*'                => 'aborting'},
+      :cancel_job   => {'*'                => 'canceling'},
+      :cancel       => {'*'                => 'canceling'},
       :finish       => {'pod_delete' => 'finished',
-                        'aborting'   => 'finished'},
+                        'aborting'   => 'finished',
+                        'canceling'  => 'finished'},
     }
   end
 
@@ -80,30 +84,23 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::Scanning::Job < Job
     }
 
     # TODO: move this to a more appropriate place (lib)
-    loop do
-      response = pod_health_poll(client, health_url, http_options)
+    response = pod_health_poll(client, health_url, http_options)
 
-      case response
-      when Net::HTTPOK
-        _log.info("pod #{pod_full_name} is ready and accessible")
-        break
-      when Net::HTTPServiceUnavailable
-        # TODO: check that the pod wasn't terminated (exit code)
-        # continue: pod is still not up and running
-      else
-        msg = "unknown access error to pod #{pod_full_name}: #{response}"
-        _log.info(msg)
-        return queue_signal(:abort_job, msg, "error")
-      end
-
-      # TODO: for recovery purposes it would be better if this
-      # method was short-lived instead of waiting for the pod to be
-      # available
+    case response
+    when Net::HTTPOK
+      _log.info("pod #{pod_full_name} is ready and accessible")
+      queue_signal(:analyze)
+    when Net::HTTPServiceUnavailable
+      # TODO: check that the pod wasn't terminated (exit code)
+      # continue: pod is still not up and running
       _log.info("pod #{pod_full_name} is not available")
-      sleep(POD_POLL_INTERVAL)
+      queue_signal(:pod_wait,
+                   :deliver_on => POD_POLL_INTERVAL.seconds.from_now.utc)
+    else
+      msg = "unknown access error to pod #{pod_full_name}: #{response}"
+      _log.info(msg)
+      queue_signal(:abort_job, msg, "error")
     end
-
-    queue_signal(:analyze)
   end
 
   def analyze
@@ -201,19 +198,33 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::Scanning::Job < Job
         # TODO: handle the cleanup at a later time
       end
     end
-    set_status('image analysis completed successfully', 'ok')
-
   ensure
-    args.empty? ? queue_signal(:finish) : process_abort(*args)
+    case self.state
+    when 'aborting' then process_abort(*args)
+    when 'canceling' then process_cancel(*args)
+    else queue_signal(:finish, 'image analysis completed successfully', 'ok')
+    end
   end
 
-  def finish(*_args)
+  def finish(*args)
     # exactly like job.dispatch_finish except for storage bits
     _log.info "Dispatch Status is 'finished'"
     update(:dispatch_status => "finished")
+    process_finished(*args)
   end
 
   alias_method :abort_job, :cleanup
+
+  def cancel(*_args)
+    _log.info "Job Canceling"
+    if self.state != "canceling" # ensure change of states
+      signal :cancel
+    else
+      unqueue_all_signals
+      queue_signal(:cancel_job)
+    end
+  end
+  alias_method :cancel_job, :cleanup
 
   def queue_callback(state, msg, _)
     if state == "timeout" && self.state != "aborting"
@@ -243,9 +254,8 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::Scanning::Job < Job
     )
   end
 
-  def queue_signal(*args)
-    MiqQueue.put_unless_exists(
-      :args        => args,
+  def queue_options
+    {
       :class_name  => "Job",
       :instance_id => id,
       :method_name => "signal",
@@ -253,7 +263,15 @@ class ManageIQ::Providers::Kubernetes::ContainerManager::Scanning::Job < Job
       :role        => "smartstate",
       :task_id     => guid,
       :zone        => zone
-    ) do |_msg, find_options|
+    }
+  end
+
+  def unqueue_all_signals
+    MiqQueue.unqueue(queue_options)
+  end
+
+  def queue_signal(*args, deliver_on: nil)
+    MiqQueue.put_unless_exists(**queue_options, :args => args, :deliver_on => deliver_on) do |_msg, find_options|
       find_options.merge(
         :miq_callback => {
           :class_name  => self.class.to_s,
