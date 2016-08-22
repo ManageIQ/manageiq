1a8e36d71aa33f1ce6ff782ad660682aac570d69
Šimon Lukašík <isimluk@fedoraproject.org>
2016-08-22 11:10:27 +0200

    Do not use &. notation just yet
    We need to support Ruby 2.2.2 notation.

--- app/helpers/application_helper/button/vm_policy.rb
+++ app/helpers/application_helper/button/vm_policy.rb
@@ -2,6 +2,6 @@ class ApplicationHelper::Button::VmPolicy < ApplicationHelper::Button::Basic
   needs_record
 
   def skip?
-    @record.host&.vmm_product.to_s.casecmp("workstation")
+    @record.host.try(:vmm_product).to_s.casecmp("workstation")
   end
 end
