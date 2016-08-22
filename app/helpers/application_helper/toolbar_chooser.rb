0f0c28e925be8a1e56849731f18b24e055177cbc
Hilda Stastna <hstastna@redhat.com>
2016-08-22 15:52:12 +0200

    Add missing policy buttons to Network Manager Relationship pages
    fixing https://bugzilla.redhat.com/show_bug.cgi?id=1347223
    
    Add missing policy buttons to some of the Relationship pages to enable
    tagging more items at once when managing a cloud provider in Compute ->
    -> Clouds -> Provider.

--- app/helpers/application_helper/toolbar_chooser.rb
+++ app/helpers/application_helper/toolbar_chooser.rb
@@ -435,7 +435,9 @@ class ApplicationHelper::ToolbarChooser
 
     # Original non vmx view code follows
     # toolbar buttons on sub-screens
-    to_display = %w(ems_clusters hosts resource_pools storages)
+    to_display = %w(availability_zones cloud_networks cloud_object_store_containers cloud_subnets
+                    cloud_tenants cloud_volumes ems_clusters flavors floating_ips hosts load_balancers
+                    network_ports network_routers orchestration_stacks resource_pools security_groups storages)
     to_display_center = %w(stack_orchestration_template topology)
     if @lastaction == 'show' && (@view || @display != 'main') && !@layout.starts_with?("miq_request")
       if @display == "vms" || @display == "all_vms"
