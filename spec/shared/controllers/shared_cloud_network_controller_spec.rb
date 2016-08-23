51e90c4065c1a5a5e60f5c06910bd5f52be58eb5
Ladislav Smola <lsmola@redhat.com>
2016-08-23 10:04:17 +0200

    Add Specs for cloud_network nested lists
    Add Specs for cloud_network nested lists

--- spec/shared/controllers/shared_cloud_network_controller_spec.rb
+++ spec/shared/controllers/shared_cloud_network_controller_spec.rb
@@ -1,3 +1,5 @@
+require Rails.root.join('spec/support/controller_spec_helper')
+
 shared_examples :cloud_network_controller_spec do |providers|
   include CompressedIds
 
@@ -10,7 +12,42 @@ shared_examples :cloud_network_controller_spec do |providers|
   providers.each do |t|
     context "for #{t}" do
       before :each do
-        @cloud_network = FactoryGirl.create("cloud_network_#{t}".to_sym, :name => "Cloud Network")
+        @provider       = FactoryGirl.create("ems_#{t}".to_sym,
+                                             :name => "Cloud Manager")
+        @security_group = FactoryGirl.create("security_group_#{t}".to_sym,
+                                             :ext_management_system => @provider.network_manager,
+                                             :name                  => 'Security Group')
+        @vm             = FactoryGirl.create("vm_#{t}".to_sym,
+                                             :name => "Instance")
+        if t == 'openstack'
+          @cloud_network        = FactoryGirl.create("cloud_network_private_#{t}".to_sym,
+                                                     :name => "Cloud Network")
+          @cloud_network_public = FactoryGirl.create("cloud_network_public_#{t}".to_sym,
+                                                     :name => "Cloud Network Public")
+        else
+          @cloud_network        = FactoryGirl.create("cloud_network_#{t}".to_sym,
+                                                     :name => "Cloud Network")
+          @cloud_network_public = nil
+        end
+
+        @network_router = FactoryGirl.create("network_router_#{t}".to_sym,
+                                             :cloud_network => @cloud_network_public,
+                                             :name          => "Network Router")
+
+        @cloud_subnet = FactoryGirl.create("cloud_subnet_#{t}".to_sym,
+                                           :network_router        => @network_router,
+                                           :cloud_network         => @cloud_network,
+                                           :ext_management_system => @provider.network_manager,
+                                           :name                  => "Cloud Subnet")
+
+        @floating_ip = FactoryGirl.create("floating_ip_#{t}".to_sym,
+                                          :ext_management_system => @provider.network_manager)
+        @vm.network_ports << @network_port = FactoryGirl.create("network_port_#{t}".to_sym,
+                                                                :name            => "eth0",
+                                                                :device          => @vm,
+                                                                :security_groups => [@security_group],
+                                                                :floating_ip     => @floating_ip)
+        FactoryGirl.create(:cloud_subnet_network_port, :cloud_subnet => @cloud_subnet, :network_port => @network_port)
       end
 
       describe "#show_list" do
@@ -43,6 +80,18 @@ shared_examples :cloud_network_controller_spec do |providers|
                                                 :url  => "/cloud_network/show/#{@cloud_network.id}"}])
 
           is_expected.to render_template(:partial => "layouts/listnav/_cloud_network")
+        end
+
+        it "show associated cloud_subnets" do
+          assert_nested_list(@cloud_network, [@cloud_subnet], 'cloud_subnets', 'All Cloud Subnets')
+        end
+
+        it "show associated network routers" do
+          assert_nested_list(@cloud_network, [@network_router], 'network_routers', 'All Network Routers')
+        end
+
+        it "show associated instances" do
+          assert_nested_list(@cloud_network, [@vm], 'instances', 'All Instances', child_path: 'vm_cloud')
         end
       end
 
