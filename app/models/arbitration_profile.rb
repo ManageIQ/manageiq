be3e9b8f486f81440f7c834e3d16aa6be3598e6e
Jillian Tullo <jtullo@redhat.com>
2016-08-24 08:22:21 -0400

    moving base model method to the top

--- app/models/arbitration_profile.rb
+++ app/models/arbitration_profile.rb
@@ -1,6 +1,10 @@
 class ArbitrationProfile < ArbitrationRecord
   default_scope { where(:profile => true) }
 
+  def self.base_model
+    ArbitrationProfile
+  end
+
   validates :ext_management_system, :presence => true
   validates :name, :presence => true
   validate :falsify_all_others, :if => :default_profile_changed?
@@ -11,9 +15,5 @@ class ArbitrationProfile < ArbitrationRecord
   # If a record is updated as the default, falsify others
   def falsify_all_others
     self.class.where(:default_profile => true).where.not(:id => id).update_all(:default_profile => false)
-  end
-
-  def self.base_model
-    ArbitrationProfile
   end
 end
