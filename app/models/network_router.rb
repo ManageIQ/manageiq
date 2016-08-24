048b256bf98beb5138396d7498258c869d1cedb5
Ladislav Smola <lsmola@redhat.com>
2016-08-24 14:14:44 +0200

    Clean up NetworkManager models relations to ext_management_system
    Clean up NetworkManager models relations to ext_management_system

--- app/models/network_router.rb
+++ app/models/network_router.rb
@@ -4,9 +4,7 @@ class NetworkRouter < ApplicationRecord
 
   acts_as_miq_taggable
 
-  # TODO(lsmola) NetworkManager, once all providers use network manager rename this to
-  # "ManageIQ::Providers::NetworkManager"
-  belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::BaseManager"
+  belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::NetworkManager"
   belongs_to :cloud_tenant
   belongs_to :network_group
   belongs_to :cloud_network
