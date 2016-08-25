667f422822bd0fb764b0601fb39edf92d9b67405
Jillian Tullo <jtullo@redhat.com>
2016-08-25 13:56:53 -0400

    allow for a blank service_dialog

--- app/controllers/manageiq/api/base_controller/blueprints.rb
+++ app/controllers/manageiq/api/base_controller/blueprints.rb
@@ -42,21 +42,32 @@ module ManageIQ
         def deserialize_bundle(bundle)
           options = {}
           if bundle["service_catalog"]
-            options["service_catalog"] =
-              if bundle["service_catalog"].blank?
-                nil
-              else
-                ServiceTemplateCatalog.find(parse_id(bundle["service_catalog"], :service_catalogs))
-              end
+            options["service_catalog"] = service_catalog_option(bundle)
           end
           if bundle["service_dialog"]
-            options[:service_dialog] = Dialog.find(parse_id(bundle["service_dialog"], :service_dialogs))
+            options[:service_dialog] = service_dialog_option(bundle)
           end
           options[:service_templates] = bundle.fetch("service_templates", []).collect do |st|
             ServiceTemplate.find(parse_id(st, :service_templates))
           end
           options[:entry_points] = bundle["automate_entrypoints"] if bundle["automate_entrypoints"]
           options
+        end
+
+        def service_catalog_option(bundle)
+          if bundle["service_catalog"].blank?
+            nil
+          else
+            ServiceTemplateCatalog.find(parse_id(bundle["service_catalog"], :service_catalogs))
+          end
+        end
+
+        def service_dialog_option(bundle)
+          if bundle["service_dialog"].blank?
+            nil
+          else
+            Dialog.find(parse_id(bundle["service_dialog"], :service_dialogs))
+          end
         end
       end
     end
