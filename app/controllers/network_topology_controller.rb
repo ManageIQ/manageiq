7ffdb65a9c4bf5ddf980b7623d5ef3d720a710c6
Ladislav Smola <lsmola@redhat.com>
2016-08-25 13:56:21 +0200

    Fix NetworkTopology breadcrumbs link
    Fix NetworkTopology breadcrumbs link
    
    Fixes BZ:
    https://bugzilla.redhat.com/show_bug.cgi?id=1347241

--- app/controllers/network_topology_controller.rb
+++ app/controllers/network_topology_controller.rb
@@ -5,6 +5,16 @@ class NetworkTopologyController < ApplicationController
   after_action :cleanup_action
   after_action :set_session_data
 
+  def show
+    # When navigated here without id, it means this is a general view for all providers (not for a specific provider)
+    # all previous navigation should not be displayed in breadcrumbs as the user could arrive from
+    # any other page in the application.
+    @breadcrumbs.clear if params[:id].nil?
+    drop_breadcrumb(:name => _('Topology'), :url => "/network_topology/show/#{params[:id]}")
+    @lastaction = 'show'
+    @display = @showtype = 'topology'
+  end
+
   private
 
   def get_session_data
