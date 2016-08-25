c5d0aa3f04866be1d552dbd613cbb2d93c6ac936
Julian Cheal <jcheal@redhat.com>
2016-08-25 14:21:54 +0100

    Added log warning if `default` proxy isn't available.
    On older applicances the `settings.yml` may not contain the new
    `default` proxy setting. So this will warn of that. Also works if
    one of the provider's proxy settings hasn't been added to the
    settings file.

--- lib/vmdb/util.rb
+++ lib/vmdb/util.rb
@@ -3,8 +3,14 @@ module VMDB
     def self.http_proxy_uri(proxy_config = :default)
       # TODO: (julian) This looks really messy. Need to make it look nicer.
       vmdb_proxy = VMDB::Config.new("vmdb").config[:http_proxy]
+
+      if vmdb_proxy[proxy_config].nil?
+        $log.warn("Could not find proxy setting for #{proxy_config}")
+      end
+
       return nil unless vmdb_proxy
       proxy = vmdb_proxy[proxy_config] || {}
+
       return nil unless proxy[:host]
       proxy = proxy.dup
 
