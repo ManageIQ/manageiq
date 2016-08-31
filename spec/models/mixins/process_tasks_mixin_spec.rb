4a5ae0bf1e68bdf62cd6a2a189c27a19d591f158
Nick Carboni <ncarboni@redhat.com>
2016-08-31 17:26:38 -0400

    Make tests for .process_tasks more generic

--- spec/models/mixins/process_tasks_mixin_spec.rb
+++ spec/models/mixins/process_tasks_mixin_spec.rb
@@ -1,39 +1,66 @@
 describe ProcessTasksMixin do
-  describe ".process_tasks" do
-    before(:each) do
-      @guid, @server, @zone = EvmSpecHelper.create_guid_miq_server_zone
-      @host = FactoryGirl.create(:host_vmware, :name => "test_host",    :hostname   => "test_host", :state => 'on')
-    end
-
-    it "deletes VM via call to MiqTask#queue_callback and verifies message" do
-      @vm1 = FactoryGirl.create(:vm_vmware, :host => @host, :name => "VM-mini1")
-
-      @vm1.class.process_tasks(:task => "destroy", :userid => "system", :ids => [@vm1.id])
-
-      expect(MiqQueue.count).to eq(1)
-      @msg1 = MiqQueue.first
-      status, message, result = @msg1.deliver
-
-      expect(@msg1.state).to eq("ready")
-      expect(@msg1.class_name).to eq("ManageIQ::Providers::Vmware::InfraManager::Vm")
-      @msg1.args.each do |h|
-        expect(h[:task]).to eq("destroy")
-        expect(h[:ids]).to eq([@vm1.id])
-        expect(h[:userid]).to eq("system")
+  let(:test_class) do
+    Class.new(ApplicationRecord) do
+      include ProcessTasksMixin
+      def self.name
+        "ProcessTasksMixinTestClass"
       end
 
-      @msg1.destroy
-      expect(MiqQueue.count).to eq(1)
-      @msg2 = MiqQueue.first
-      status, message, result = @msg2.deliver
-      expect_any_instance_of(MiqTask).to receive(:queue_callback).with("Finished", status, message, result)
-      @msg2.delivered(status, message, result)
+      def test_method
+        "a test"
+      end
+    end
+  end
+
+  describe ".process_tasks" do
+    context "class responds to refresh_ems" do
+      before do
+        test_class.define_singleton_method(:refresh_ems, ->(_ids) { "refresh that ems" })
+      end
+
+      it "calls .refresh_ems and raises an audit event" do
+        ids = [1, 2, 3]
+        expect(test_class).to receive(:refresh_ems).with(ids)
+        expect(AuditEvent).to receive(:success)
+        test_class.process_tasks(:task => "refresh_ems", :ids => ids)
+      end
     end
 
-    it "deletes VM via call to MiqTask#queue_callback and successfully saves object image via YAML.dump" do
-      @vm2 = FactoryGirl.create(:vm_vmware, :host => @host, :name => "VM-mini2")
-      @vm2.destroy
-      expect { YAML.dump(@vm2) }.not_to raise_error
+    it "queues a message for the specified task" do
+      EvmSpecHelper.create_guid_miq_server_zone
+      test_class.process_tasks(:task => "test_method", :ids => [5], :userid => "admin")
+
+      message = MiqQueue.first
+
+      expect(message.class_name).to eq(test_class.name)
+      message.args.each do |h|
+        expect(h[:task]).to eq("test_method")
+        expect(h[:ids]).to eq([5])
+        expect(h[:userid]).to eq("admin")
+      end
+    end
+
+    it "defaults the userid to system in the queue message" do
+      EvmSpecHelper.create_guid_miq_server_zone
+      test_class.process_tasks(:task => "test_method", :ids => [5])
+
+      message = MiqQueue.first
+
+      expect(message.class_name).to eq(test_class.name)
+      message.args.each do |h|
+        expect(h[:task]).to eq("test_method")
+        expect(h[:ids]).to eq([5])
+        expect(h[:userid]).to eq("system")
+      end
+    end
+
+    it "raises if no ids are given" do
+      expect { test_class.process_tasks(:task => "test_method", :ids => []) }.to raise_error(RuntimeError)
+      expect { test_class.process_tasks(:task => "test_method") }.to raise_error(RuntimeError)
+    end
+
+    it "raises if the task is an unknown method" do
+      expect { test_class.process_tasks(:task => "bad_method", :ids => [1]) }.to raise_error(RuntimeError)
     end
   end
 end
