174e2bfee4530edd7d31552910ae75defc83b532
Erik Clarizio & Drew Bomhof <eclarizi@redhat.com>
2016-09-01 10:46:29 -0700

    Add genericObjectDefinitionController to handle all explorer interaction
    https://www.pivotaltracker.com/story/show/128426187

---
+++ app/assets/javascripts/controllers/generic_object/generic_object_definition_controller.js
@@ -0,0 +1,79 @@
+ManageIQ.angular.app.controller('genericObjectDefinitionFormController', ['$http', '$scope', 'miqService', 'genericObjectSubscriptionService', function($http, $scope, miqService, genericObjectSubscriptionService) {
+  var init = function() {
+    hideAndClearForm();
+
+    $scope.newRecord = true;
+    $scope.showSingleItem = false;
+
+    genericObjectSubscriptionService.subscribeToShowAddForm(showAddForm);
+    genericObjectSubscriptionService.subscribeToTreeClicks(showSelectedItem);
+    genericObjectSubscriptionService.subscribeToRootTreeclicks(showAllItems);
+  };
+
+  var showAddForm = function(_response) {
+    $scope.clearForm();
+    $scope.showAddForm = true;
+    $scope.$apply();
+    sendDataWithRx({eventType: 'deselectTreeNodes'});
+  };
+
+  var showAllItems = function(response) {
+    $scope.genericObjectList = response;
+    $scope.showSingleItem = false;
+    $scope.$apply();
+  };
+
+  var addedGenericObject = function(data) {
+    var successCallback = function(response) {
+      sendDataWithRx({eventType: 'treeUpdated', data: JSON.parse(response.data.tree_data)});
+    };
+
+    $http.get('get_tree_data').then(successCallback);
+  };
+
+  var hideAndClearForm = function() {
+    $scope.clearForm();
+    $scope.showAddForm = false;
+  };
+
+  var showSelectedItem = function(response) {
+    var modelData = response;
+    $scope.genericObjectDefinitionModel.name = modelData.name;
+    $scope.genericObjectDefinitionModel.description = modelData.description;
+    $scope.showAddForm = false;
+    $scope.showSingleItem = true;
+    miqService.sparkleOff();
+    $scope.$apply();
+  };
+
+  $scope.clearForm = function() {
+    $scope.genericObjectDefinitionModel = {
+      name: '',
+      description: ''
+    };
+  };
+
+  $scope.listObjectClicked = function(name) {
+    sendDataWithRx({eventType: 'singleItemSelected', response: name});
+  };
+
+  $scope.addClicked = function() {
+    var data = $scope.genericObjectDefinitionModel;
+
+    miqService.jqueryRequest('create', {
+      data: data,
+      dataType: 'json',
+      beforeSend: true,
+      complete: true,
+      done: addedGenericObject
+    });
+  };
+
+  $scope.cancelClicked = function() {
+    hideAndClearForm();
+    sendDataWithRx({eventType: 'cancelClicked'});
+    $scope.angularForm.$setPristine(true);
+  };
+
+  init();
+}]);
