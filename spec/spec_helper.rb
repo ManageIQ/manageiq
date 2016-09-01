458e540f87470d1e920272ca27ea2a7c7f3a747d
Brandon Dunne <bdunne@redhat.com>
2016-09-01 12:13:28 -0400

    Fail tests that try to include stuff in `main`
    Prevent code contamination

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,10 @@ require 'rspec/rails'
 require 'vcr'
 require 'cgi'
 
+# Fail tests that try to include stuff in `main`
+require_relative 'support/test_contamination'
+Spec::Support::TestContamination.setup
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
