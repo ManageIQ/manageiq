803cbf550bfbf5dbd8c11978226a2f94bc92f6ae
Keenan Brock <kbrock@redhat.com>
2016-09-01 23:58:05 -0400

    vandt tree roots - use size and sort in ruby
    no reason to sort in the db, use ruby
    have consistency with root nodes
    
    Passing a block to count_only_or_objects made logical
    sense, but the code was too dense

--- app/presenters/tree_builder_vandt.rb
+++ app/presenters/tree_builder_vandt.rb
@@ -19,15 +19,11 @@ class TreeBuilderVandt < TreeBuilder
   end
 
   def x_get_tree_roots(count_only, options)
-    objects = Rbac.filtered(EmsInfra.order("lower(name)"), :match_via_descendants => VmOrTemplate)
+    objects = count_only_or_objects_filtered(count_only, EmsInfra, "name", :match_via_descendants => VmOrTemplate)
+    objects.collect! { |o| TreeBuilderVmsAndTemplates.new(o, options.dup).tree } unless count_only
+    root_nodes = count_only_or_objects(count_only, x_get_tree_arch_orph_nodes("VMs and Templates"))
 
-    if count_only
-      objects.length + 2
-    else
-      objects = objects.to_a
-      objects.collect! { |o| TreeBuilderVmsAndTemplates.new(o, options.dup).tree } +
-        x_get_tree_arch_orph_nodes("VMs and Templates")
-    end
+    objects + root_nodes
   end
 
   # Handle custom tree nodes (object is a Hash)
