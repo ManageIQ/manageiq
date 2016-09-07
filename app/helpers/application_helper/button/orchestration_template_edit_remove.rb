c2ea53f8b0ab946110eff4f74f550128f09f8bde
Jozef Zigmund <jzigmund@redhat.com>
2016-09-07 18:44:38 +0200

    Remove old case when for OrchestrationTemplate*, create the disabled? and clean calculate_properties inside the button class

--- app/helpers/application_helper/button/orchestration_template_edit_remove.rb
+++ app/helpers/application_helper/button/orchestration_template_edit_remove.rb
@@ -1,15 +1,18 @@
 class ApplicationHelper::Button::OrchestrationTemplateEditRemove < ApplicationHelper::Button::Basic
+  needs_record
+
+  def disabled?
+    @record.in_use?
+  end
+
   def calculate_properties
     super
-    if @view_context.x_active_tree == :ot_tree && @record
-      if @record.in_use?
-        self[:enabled] = false
-        self[:title] = if self[:id] =~ /_edit$/
-                         N_('Orchestration Templates that are in use cannot be edited')
-                       else
-                         N_('Orchestration Templates that are in use cannot be removed')
-                       end
-      end
+    if disabled?
+      self[:title] = if self[:id] =~ /_edit$/
+                       N_('Orchestration Templates that are in use cannot be edited')
+                     else
+                       N_('Orchestration Templates that are in use cannot be removed')
+                     end
     end
   end
 end
