e3defc385631ab5d94f20eccef910aa711f42f18
Keenan Brock <kbrock@redhat.com>
2016-09-07 10:52:23 -0400

    env for static server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Vmdb::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.public_file_server.enabled = false
+  config.public_file_server.enabled = !!ENV['RAILS_SERVE_STATIC']
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
