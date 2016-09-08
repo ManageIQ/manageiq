48cb201cac71946e3c6ccca4f3d46f69652264a2
Hui Song <hsong@redhat.com>
2016-09-08 08:46:26 -0400

    [WIP] Add Cinder storage manager support: back-end changes

--- app/models/manageiq/providers/openstack/cloud_manager/refresh_parser.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/refresh_parser.rb
@@ -66,16 +66,19 @@ module ManageIQ::Providers
       # get_hosts
       get_images
       get_servers
-      # get_volumes
-      # get_backups
-      # get_snapshots
+      # TODO: change spec to remove this
+      #
+      get_volumes
+      get_backups
+      get_snapshots
+      #
       get_object_store
       get_cloud_services
 
       $fog_log.info("#{log_header}...Complete")
 
       link_vm_genealogy
-      # link_storage_associations
+      link_storage_associations
       filter_unused_disabled_flavors
 
       @data
@@ -109,11 +112,11 @@ module ManageIQ::Providers
       @availability_zones ||= availability_zones_compute + availability_zones_volume
     end
 
-    ### def volumes
-    ###   # TODO: support volumes through :nova as well?
-    ###   return [] unless @volume_service.name == :cinder
-    ###   @volumes ||= @volume_service.handled_list(:volumes)
-    ### end
+    def volumes
+      # TODO: support volumes through :nova as well?
+      return [] unless @volume_service.name == :cinder
+      @volumes ||= @volume_service.handled_list(:volumes)
+    end
 
     def get_flavors
       flavors = @connection.handled_list(:flavors)
@@ -157,23 +160,23 @@ module ManageIQ::Providers
       process_collection(kps, :key_pairs) { |kp| parse_key_pair(kp) }
     end
 
-    ### def get_volumes
-    ###   process_collection(volumes, :cloud_volumes) { |volume| parse_volume(volume) }
-    ### end
+    def get_volumes
+      process_collection(volumes, :cloud_volumes) { |volume| parse_volume(volume) }
+    end
 
-    ### def get_backups
-    ###   return unless @volume_service.name == :cinder
-    ###   # backups don't support pagination in juno or kilo
-    ###   process_collection(@volume_service.list_backups_detailed.body["backups"],
-    ###                      :cloud_volume_backups) { |backup| parse_backup(backup) }
-    ### end
+    def get_backups
+      return unless @volume_service.name == :cinder
+      # backups don't support pagination in juno or kilo
+      process_collection(@volume_service.list_backups_detailed.body["backups"],
+                         :cloud_volume_backups) { |backup| parse_backup(backup) }
+    end
 
-    ### def get_snapshots
-    ###   return unless @volume_service.name == :cinder
-    ###   process_collection(@volume_service.handled_list(:list_snapshots_detailed,
-    ###                                                   :__request_body_index => "snapshots"),
-    ###                      :cloud_volume_snapshots) { |snap| parse_snapshot(snap) }
-    ### end
+    def get_snapshots
+      return unless @volume_service.name == :cinder
+      process_collection(@volume_service.handled_list(:list_snapshots_detailed,
+                                                      :__request_body_index => "snapshots"),
+                         :cloud_volume_snapshots) { |snap| parse_snapshot(snap) }
+    end
 
     def get_servers
       openstack_infra_hosts = @ems.provider.try(:infra_ems).try(:hosts)
@@ -201,17 +204,17 @@ module ManageIQ::Providers
       end
     end
 
-    ### def link_storage_associations
-    ###   @data[:cloud_volumes].each do |cv|
-    ###     #
-    ###     # Associations between volumes and the snapshots on which
-    ###     # they are based, if any.
-    ###     #
-    ###     base_snapshot_uid = cv.delete(:snapshot_uid)
-    ###     base_snapshot = @data_index.fetch_path(:cloud_volume_snapshots, base_snapshot_uid)
-    ###     cv[:base_snapshot] = base_snapshot unless base_snapshot.nil?
-    ###   end if @data[:cloud_volumes]
-    ### end
+    def link_storage_associations
+      @data[:cloud_volumes].each do |cv|
+        #
+        # Associations between volumes and the snapshots on which
+        # they are based, if any.
+        #
+        base_snapshot_uid = cv.delete(:snapshot_uid)
+        base_snapshot = @data_index.fetch_path(:cloud_volume_snapshots, base_snapshot_uid)
+        cv[:base_snapshot] = base_snapshot unless base_snapshot.nil?
+      end if @data[:cloud_volumes]
+    end
 
     def parse_flavor(flavor)
       uid = flavor.id
@@ -337,109 +340,109 @@ module ManageIQ::Providers
       "ManageIQ::Providers::Openstack::CloudManager::Template"
     end
 
-    ### def parse_volume(volume)
-    ###   log_header = "MIQ(#{self.class.name}.#{__method__})"
+    def parse_volume(volume)
+      log_header = "MIQ(#{self.class.name}.#{__method__})"
 
-    ###   uid = volume.id
-    ###   new_result = {
-    ###     :ems_ref           => uid,
-    ###     :type              => "ManageIQ::Providers::Openstack::CloudManager::CloudVolume",
-    ###     :name              => volume_name(volume),
-    ###     :status            => volume.status,
-    ###     :bootable          => volume.attributes['bootable'],
-    ###     :creation_time     => volume.created_at,
-    ###     :description       => volume_description(volume),
-    ###     :volume_type       => volume.volume_type,
-    ###     :snapshot_uid      => volume.snapshot_id,
-    ###     :size              => volume.size.to_i.gigabytes,
-    ###     :tenant            => @data_index.fetch_path(:cloud_tenants, volume.tenant_id),
-    ###     :availability_zone => @data_index.fetch_path(:availability_zones, "volume-" + volume.availability_zone || "null_az"),
-    ###   }
+      uid = volume.id
+      new_result = {
+        :ems_ref           => uid,
+        :type              => "ManageIQ::Providers::Openstack::CloudManager::CloudVolume",
+        :name              => volume_name(volume),
+        :status            => volume.status,
+        :bootable          => volume.attributes['bootable'],
+        :creation_time     => volume.created_at,
+        :description       => volume_description(volume),
+        :volume_type       => volume.volume_type,
+        :snapshot_uid      => volume.snapshot_id,
+        :size              => volume.size.to_i.gigabytes,
+        :tenant            => @data_index.fetch_path(:cloud_tenants, volume.tenant_id),
+        :availability_zone => @data_index.fetch_path(:availability_zones, "volume-" + volume.availability_zone || "null_az"),
+      }
 
-    ###   volume.attachments.each do |a|
-    ###     if a['device'].blank?
-    ###       $fog_log.warn "#{log_header}: Volume: #{uid}, is missing a mountpoint, skipping the volume processing"
-    ###       $fog_log.warn "#{log_header}:   EMS: #{@ems.name}, Instance: #{a['server_id']}"
-    ###       next
-    ###     end
+      volume.attachments.each do |a|
+        if a['device'].blank?
+          $fog_log.warn "#{log_header}: Volume: #{uid}, is missing a mountpoint, skipping the volume processing"
+          $fog_log.warn "#{log_header}:   EMS: #{@ems.name}, Instance: #{a['server_id']}"
+          next
+        end
 
-    ###     dev = File.basename(a['device'])
-    ###     disks = @data_index.fetch_path(:vms, a['server_id'], :hardware, :disks)
+        dev = File.basename(a['device'])
+        disks = @data_index.fetch_path(:vms, a['server_id'], :hardware, :disks)
 
-    ###     unless disks
-    ###       $fog_log.warn "#{log_header}: Volume: #{uid}, attached to instance not visible in the scope of this EMS"
-    ###       $fog_log.warn "#{log_header}:   EMS: #{@ems.name}, Instance: #{a['server_id']}"
-    ###       next
-    ###     end
+        unless disks
+          $fog_log.warn "#{log_header}: Volume: #{uid}, attached to instance not visible in the scope of this EMS"
+          $fog_log.warn "#{log_header}:   EMS: #{@ems.name}, Instance: #{a['server_id']}"
+          next
+        end
 
-    ###     if (disk = disks.detect { |d| d[:location] == dev })
-    ###       disk[:size] = new_result[:size]
-    ###     else
-    ###       disk = add_instance_disk(disks, new_result[:size], dev, "OpenStack Volume")
-    ###     end
+        if (disk = disks.detect { |d| d[:location] == dev })
+          disk[:size] = new_result[:size]
+        else
+          disk = add_instance_disk(disks, new_result[:size], dev, "OpenStack Volume")
+        end
 
-    ###     if disk
-    ###       disk[:backing]      = new_result
-    ###       disk[:backing_type] = 'CloudVolume'
-    ###     end
-    ###   end
+        if disk
+          disk[:backing]      = new_result
+          disk[:backing_type] = 'CloudVolume'
+        end
+      end
 
-    ###   return uid, new_result
-    ### end
+      return uid, new_result
+    end
 
-    ### def volume_name(volume)
-    ###   # Cinder v1
-    ###   return volume.display_name if volume.respond_to?(:display_name)
-    ###   # Cinder v2
-    ###   return volume.name
-    ### end
+    def volume_name(volume)
+      # Cinder v1
+      return volume.display_name if volume.respond_to?(:display_name)
+      # Cinder v2
+      return volume.name
+    end
 
-    ### def volume_description(volume)
-    ###   # Cinder v1
-    ###   return volume.display_description if volume.respond_to?(:display_description)
-    ###   # Cinder v2
-    ###   return volume.description
-    ### end
+    def volume_description(volume)
+      # Cinder v1
+      return volume.display_description if volume.respond_to?(:display_description)
+      # Cinder v2
+      return volume.description
+    end
 
-    ### def parse_backup(backup)
-    ###   uid = backup['id']
-    ###   new_result = {
-    ###     :ems_ref               => uid,
-    ###     :type                  => "ManageIQ::Providers::Openstack::CloudManager::CloudVolumeBackup",
-    ###     # Supporting both Cinder v1 and Cinder v2
-    ###     :name                  => backup['display_name'] || backup['name'],
-    ###     :status                => backup['status'],
-    ###     :creation_time         => backup['created_at'],
-    ###     # Supporting both Cinder v1 and Cinder v2
-    ###     :description           => backup['display_description'] || backup['description'],
-    ###     :size                  => backup['size'].to_i.gigabytes,
-    ###     :object_count          => backup['object_count'].to_i,
-    ###     :is_incremental        => backup['is_incremental'],
-    ###     :has_dependent_backups => backup['has_dependent_backups'],
-    ###     :availability_zone     => @data_index.fetch_path(:availability_zones,
-    ###                                                      "volume-" + backup['availability_zone'] || "null_az"),
-    ###     :volume                => @data_index.fetch_path(:cloud_volumes, backup['volume_id'])
-    ###   }
-    ###   return uid, new_result
-    ### end
+    def parse_backup(backup)
+      uid = backup['id']
+      new_result = {
+        :ems_ref               => uid,
+        :type                  => "ManageIQ::Providers::Openstack::CloudManager::CloudVolumeBackup",
+        # Supporting both Cinder v1 and Cinder v2
+        :name                  => backup['display_name'] || backup['name'],
+        :status                => backup['status'],
+        :creation_time         => backup['created_at'],
+        # Supporting both Cinder v1 and Cinder v2
+        :description           => backup['display_description'] || backup['description'],
+        :size                  => backup['size'].to_i.gigabytes,
+        :object_count          => backup['object_count'].to_i,
+        :is_incremental        => backup['is_incremental'],
+        :has_dependent_backups => backup['has_dependent_backups'],
+        :availability_zone     => @data_index.fetch_path(:availability_zones,
+                                                         "volume-" + backup['availability_zone'] || "null_az"),
+        :volume                => @data_index.fetch_path(:cloud_volumes, backup['volume_id'])
+      }
+      return uid, new_result
+    end
 
-    ### def parse_snapshot(snap)
-    ###   uid = snap['id']
-    ###   new_result = {
-    ###     :ems_ref       => uid,
-    ###     :type          => "ManageIQ::Providers::Openstack::CloudManager::CloudVolumeSnapshot",
-    ###     # Supporting both Cinder v1 and Cinder v2
-    ###     :name          => snap['display_name'] || snap['name'],
-    ###     :status        => snap['status'],
-    ###     :creation_time => snap['created_at'],
-    ###     # Supporting both Cinder v1 and Cinder v2
-    ###     :description   => snap['display_description'] || snap['description'],
-    ###     :size          => snap['size'].to_i.gigabytes,
-    ###     :tenant        => @data_index.fetch_path(:cloud_tenants, snap['os-extended-snapshot-attributes:project_id']),
-    ###     :volume        => @data_index.fetch_path(:cloud_volumes, snap['volume_id'])
-    ###   }
-    ###   return uid, new_result
-    ### end
+    def parse_snapshot(snap)
+      uid = snap['id']
+      new_result = {
+        :ems_ref       => uid,
+        :type          => "ManageIQ::Providers::Openstack::CloudManager::CloudVolumeSnapshot",
+        # Supporting both Cinder v1 and Cinder v2
+        :name          => snap['display_name'] || snap['name'],
+        :status        => snap['status'],
+        :creation_time => snap['created_at'],
+        # Supporting both Cinder v1 and Cinder v2
+        :description   => snap['display_description'] || snap['description'],
+        :size          => snap['size'].to_i.gigabytes,
+        :tenant        => @data_index.fetch_path(:cloud_tenants, snap['os-extended-snapshot-attributes:project_id']),
+        :volume        => @data_index.fetch_path(:cloud_volumes, snap['volume_id'])
+      }
+      return uid, new_result
+    end
 
     def parse_server(server, parent_hosts = nil)
       uid = server.id
--- app/models/manageiq/providers/openstack/infra_manager.rb
+++ app/models/manageiq/providers/openstack/infra_manager.rb
@@ -20,8 +20,7 @@ class ManageIQ::Providers::Openstack::InfraManager < ::EmsInfra
 
   before_save :ensure_parent_provider
   before_destroy :destroy_parent_provider
-  before_validation :ensure_managers,
-                    :ensure_storage_managers
+  before_validation :ensure_managers
 
   def ensure_network_manager
     build_network_manager(:type => 'ManageIQ::Providers::Openstack::NetworkManager') unless network_manager
