f11568e6d45b097aa1909a25e258abd2d5e9f94f
d-m-u <drewuhlmann@gmail.com>
2016-09-09 11:53:02 -0600

    Fix merge conflicts

--- db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Provisioning/Naming.class/__methods__/vmname.rb
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Provisioning/Naming.class/__methods__/vmname.rb
@@ -8,33 +8,57 @@
 # 3. Add 3 digit suffix to vmname if more than one VM is being provisioned
 #
 
-# Returns the name prefix (preferences model over dialog) or nil
-def get_prefix(prov)
-  $evm.object['vm_prefix'] || prov.get_option(:vm_prefix).to_s.strip
+class VmName
+  def initialize(handle = $evm)
+    @handle = handle
+  end
+
+  def main
+    @handle.log("info", "Detected vmdb_object_type:<#{@handle.root['vmdb_object_type']}>")
+    @handle.object['vmname'] = derived_name.compact.join
+    @handle.log(:info, "vmname: \"#{@handle.object['vmname']}\"")
+  end
+
+  def derived_name
+    if supplied_name.present?
+      [supplied_name, suffix(true)]
+    else
+      [prefix, env_tag, suffix(false)]
+    end
+  end
+
+  def supplied_name
+    @supplied_name ||= begin
+      vm_name = provision_object.get_option(:vm_name).to_s.strip
+      vm_name unless vm_name == 'changeme'
+    end
+  end
+
+  def provision_object
+    @provision_object ||= begin
+      @handle.root['miq_provision_request'] ||
+      @handle.root['miq_provision']         ||
+      @handle.root['miq_provision_request_template']
+    end
+  end
+
+  # Returns the name prefix (preferences model over dialog) or nil
+  def prefix
+    @handle.object['vm_prefix'] || provision_object.get_option(:vm_prefix).to_s.strip
+  end
+
+  # Returns the first 3 characters of the "environment" tag (or nil)
+  def env_tag
+    env = provision_object.get_tags[:environment]
+    return env[0, 3] unless env.blank?
+  end
+
+  # Returns the name suffix (provision number) or nil if provisioning only one
+  def suffix(condensed)
+    "$n{3}" if provision_object.get_option(:number_of_vms) > 1 || !condensed
+  end
 end
 
-# Returns the first 3 characters of the "environment" tag (or nil)
-def get_env_tag(prov)
-  env = prov.get_tags[:environment]
-  return env[0, 3] unless env.blank?
+if __FILE__ == $PROGRAM_NAME
+  VmName.new.main
 end
-
-# Returns the name suffix (provision number) or nil if provisioning only one
-def get_suffix(prov, condensed)
-  "$n{3}" if prov.get_option(:number_of_vms) > 1 || !condensed
-end
-
-$evm.log("info", "Detected vmdb_object_type:<#{$evm.root['vmdb_object_type']}>")
-prov = $evm.root['miq_provision_request'] || $evm.root['miq_provision'] || $evm.root['miq_provision_request_template']
-vm_name = prov.get_option(:vm_name).to_s.strip
-vm_name = vm_name == 'changeme' ? nil : vm_name
-
-# If the name is already specified, use it and exit
-if !vm_name.blank?
-  derived_name = [vm_name, get_suffix(prov, true)]
-else
-  derived_name = [get_prefix(prov), get_env_tag(prov), get_suffix(prov, false)]
-end
-
-$evm.object['vmname'] = derived_name.compact.join("_")
-$evm.log(:info, "vmname: \"#{$evm.object['vmname']}\"")
