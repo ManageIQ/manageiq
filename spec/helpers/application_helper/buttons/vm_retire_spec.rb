a1d4badb58dc47934975b87231cbb7310197ad77
Roman Blanco <rblanco@redhat.com>
2016-09-09 19:28:24 +0200

    Corrected spec for vm_retire button

--- spec/helpers/application_helper/buttons/vm_retire_spec.rb
+++ spec/helpers/application_helper/buttons/vm_retire_spec.rb
@@ -20,32 +20,16 @@ describe ApplicationHelper::Button::VmRetire do
   end
 
   describe '#disabled?' do
-    context "when record has an error message" do
+    context "button should not be disabled" do
       before do
         @record = FactoryGirl.create(:vm_vmware)
         allow(@record).to receive(:retired).and_return(true)
       end
 
-      it "disables the button" do
+      it "does not disable the button" do
         view_context = setup_view_context_with_sandbox({})
         button = described_class.new(view_context, {}, {'record' => @record}, {})
-        expect(button.disabled?).to be_truthy
-      end
-    end
-  end
-
-  describe "#calculate_properties" do
-    context "when record is retired" do
-      before do
-        @record = FactoryGirl.create(:vm_vmware)
-        allow(@record).to receive(:retired).and_return(true)
-      end
-
-      it "sets the error message for disabled button" do
-        view_context = setup_view_context_with_sandbox({})
-        button = described_class.new(view_context, {}, {'record' => @record}, {})
-        button.calculate_properties
-        expect(button[:title]).to eq("VM is already retired")
+        expect(button.disabled?).to be_falsey
       end
     end
   end
