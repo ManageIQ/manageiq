91ca10852250b774dbd58c0665093af82e7ea109
Roman Blanco <rblanco@redhat.com>
2016-09-09 16:25:33 +0200

    Allow bring VM out of retirement from detail page

---
+++ app/helpers/application_helper/button/vm_retire_now.rb
@@ -0,0 +1,16 @@
+class ApplicationHelper::Button::VmRetireNow < ApplicationHelper::Button::Basic
+  needs_record
+
+  def calculate_properties
+    super
+    self[:title] = N_("VM is already retired") if disabled?
+  end
+
+  def visible?
+    @record.supports_retire?
+  end
+
+  def disabled?
+    @record.retired
+  end
+end
