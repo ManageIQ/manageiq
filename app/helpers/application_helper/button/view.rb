9ad5e646b9951fe80b6e715d24f3aab4ba59716e
Robin Knaur <rknaur@redhat.com>
2016-09-12 15:02:13 +0200

    Exclude GTL button from RBAC check

--- app/helpers/application_helper/button/view.rb
+++ app/helpers/application_helper/button/view.rb
@@ -1,4 +1,4 @@
-class ApplicationHelper::Button::View < ApplicationHelper::Button::Basic
+class ApplicationHelper::Button::View < ApplicationHelper::Button::ButtonWithoutRbackCheck
   def visible?
     # only hide gtl button if they are not in @gtl_buttons
     !@gtl_buttons || @gtl_buttons.include?(self[:id].to_s)
