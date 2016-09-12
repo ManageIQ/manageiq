1939a1a6c280f6e0303cab5aed1054530d6b97cb
Gregg Tanzillo <gtanzill@redhat.com>
2016-09-12 16:29:36 -0400

    Rails scripts for setting a server's zone and configuration settings from a command line
    tools/configure_server_zone.rb - Updates the zone of a specific server. The zone must exist.
    tools/configure_server_settings.rb - Generic script updating VMDB configuration settings of a specific server.

---
+++ tools/configure_server_settings.rb
@@ -0,0 +1,37 @@
+if ARGV.empty?
+  puts "USAGE: #{__FILE__} server_id settings_path_separated_by_a_/ value [settings_path_separated_by_a_/ value]"
+  exit 0
+end
+
+server_id = ARGV[0]
+new_settings = ARGV[1..-1]
+
+server = MiqServer.where(:id => server_id).take
+unless server
+  puts "Unable to find server with id [#{server_id}]"
+  exit 1
+end
+
+settings = server.get_config("vmdb")
+
+new_settings.each_slice(2) do |k, v|
+  path = settings.config
+  keys = k.split("/")
+  key = keys.pop.to_sym
+  keys.each { |p| path = path[p.to_sym] }
+
+  puts "Setting [#{k}], old value: [#{path[key]}], new value: [#{v}]"
+  path[key] = v
+end
+
+valid, errors = VMDB::Config::Validator.new(settings).validate
+unless valid
+  puts "ERROR: Configuration is invalid:"
+  errors.each { |k, v| puts "\t#{k}: #{v}" }
+  exit 1
+end
+
+server.set_config(settings)
+server.save!
+
+puts "Done"
