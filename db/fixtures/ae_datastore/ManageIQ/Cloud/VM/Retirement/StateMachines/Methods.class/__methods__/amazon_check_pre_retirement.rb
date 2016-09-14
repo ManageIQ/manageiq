39243fb9002e12a392f57c00607a451c65839a4d
william fitzgerald <wfitzger@redhat.com>
2016-09-14 17:34:58 -0400

    Automate - Retirement - Allow VM's with unknown power state or unknown ems to retire.
    Modified check_pre_retirement and amazon_check_pre_retirement methods to continue if power state is unknown
    
    or ems is nil.
    
    Added a test for ems nil and ems.try in log messages was removed.
    
    https://trello.com/c/Mlu7zC9a/74-retirement-treat-unknown-power-state-same-as-off-power-state-and-continue-to-next-state
    
    Added spec test for Amazon.
    
    Made all the same changes for check_pre_retirement in the Infrastructure Namespace.
    
    Added tests for Cloud Instances.  Added test for 'unknown' power state to both namespaces.

--- db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/amazon_check_pre_retirement.rb
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/Methods.class/__methods__/amazon_check_pre_retirement.rb
@@ -5,22 +5,24 @@
 
 # Get vm from root object
 vm = $evm.root['vm']
+ems = vm.ext_management_system if vm
 
-if vm
-  power_state = vm.attributes['power_state']
-  ems = vm.ext_management_system
-  $evm.log('info', "Instance:<#{vm.name}> on EMS:<#{ems.try(:name)} has Power State:<#{power_state}>")
+if vm.nil? || ems.nil?
+  $evm.log('info', "Skipping check pre retirement for Instance:<#{vm.try(:name)}> on EMS:<#{ems.try(:name)}>")
+  exit MIQ_OK
+end
 
-  # If VM is powered off or this instance is running on an instance store
-  if %w(off suspended terminated).include?(power_state) || vm.hardware.root_device_type == "instance-store"
-    # Bump State
-    $evm.root['ae_result'] = 'ok'
-  elsif power_state == "never"
-    # If never then this VM is a template so exit the retirement state machine
-    $evm.root['ae_result'] = 'error'
-  else
-    vm.refresh
-    $evm.root['ae_result']         = 'retry'
-    $evm.root['ae_retry_interval'] = '60.seconds'
-  end
+power_state = vm.power_state
+$evm.log('info', "Instance:<#{vm.name}> on EMS:<#{ems.name}> has Power State:<#{power_state}>")
+# If VM is powered off, suspended, terminated, unknown or this instance is running on an instance store exit
+if %w(off suspended terminated unknown).include?(power_state) || vm.hardware.root_device_type == "instance-store"
+  # Bump State
+  $evm.root['ae_result'] = 'ok'
+elsif power_state == "never"
+  # If never then this VM is a template so exit the retirement state machine
+  $evm.root['ae_result'] = 'error'
+else
+  vm.refresh
+  $evm.root['ae_result']         = 'retry'
+  $evm.root['ae_retry_interval'] = '60.seconds'
 end
