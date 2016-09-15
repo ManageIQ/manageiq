801295e3720a88280052460ef4bf21547373e843
Martin Hradil <mhradil@redhat.com>
2016-09-15 12:11:16 +0000

    fixup! Migrate retires_on and retirement_last_warn to datetime

--- db/migrate/20160912160750_change_retires_on_to_datetime.rb
+++ db/migrate/20160912160750_change_retires_on_to_datetime.rb
@@ -3,19 +3,13 @@ class ChangeRetiresOnToDatetime < ActiveRecord::Migration[5.0]
     change_column :vms, :retires_on, :datetime
     change_column :services, :retires_on, :datetime
     change_column :orchestration_stacks, :retires_on, :datetime
-
-    change_column :vms, :retirement_last_warn, :datetime
-    change_column :services, :retirement_last_warn, :datetime
-    change_column :orchestration_stacks, :retirement_last_warn, :datetime
+    change_column :load_balancers, :retires_on, :datetime
   end
 
   def down
     change_column :vms, :retires_on, :date
     change_column :services, :retires_on, :date
     change_column :orchestration_stacks, :retires_on, :date
-
-    change_column :vms, :retirement_last_warn, :date
-    change_column :services, :retirement_last_warn, :date
-    change_column :orchestration_stacks, :retirement_last_warn, :date
+    change_column :load_balancers, :retires_on, :date
   end
 end
