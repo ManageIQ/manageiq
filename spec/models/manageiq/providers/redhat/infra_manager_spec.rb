7fc8c773351e21f76f47b6905fed4c0c307dcb52
borod108 <bodnopoz@redhat.com>
2016-09-19 07:16:14 +0300

    improve tests for supported api features

--- spec/models/manageiq/providers/redhat/infra_manager_spec.rb
+++ spec/models/manageiq/providers/redhat/infra_manager_spec.rb
@@ -101,40 +101,44 @@ describe ManageIQ::Providers::Redhat::InfraManager do
     let(:supported_api_versions) { [3, 4]  }
     context "#process_api_features_support" do
       before(:each) do
-        allow(described_class).to receive(:api_features).and_return({ 3 => ['feature1'], 4 => ['feature2'] })
+        allow(described_class).to receive(:api_features).and_return({ 3 => ['feature1', 'feature3'], 4 => ['feature2', 'feature3'] })
         described_class.process_api_features_support
         allow(ems).to receive(:supported_api_versions).and_return(supported_api_versions)
       end
 
       context "no versions supported" do
         let(:supported_api_versions) { [] }
-        it 'supports the reight features' do
+        it 'supports the right features' do
           expect(ems.supports_feature1?).to be_falsey
           expect(ems.supports_feature2?).to be_falsey
+          expect(ems.supports_feature3?).to be_falsey
         end
       end
 
       context "version 3 supported" do
         let(:supported_api_versions) { [3] }
-        it 'supports the reight features' do
+        it 'supports the right features' do
           expect(ems.supports_feature1?).to be_truthy
           expect(ems.supports_feature2?).to be_falsey
+          expect(ems.supports_feature3?).to be_truthy
         end
       end
 
       context "version 4 supported" do
         let(:supported_api_versions) { [4] }
-        it 'supports the reight features' do
+        it 'supports the right features' do
           expect(ems.supports_feature1?).to be_falsey
           expect(ems.supports_feature2?).to be_truthy
+          expect(ems.supports_feature3?).to be_truthy
         end
       end
 
       context "all versions supported" do
         let(:supported_api_versions) { [3, 4] }
-        it 'supports the reight features' do
+        it 'supports the right features' do
           expect(ems.supports_feature1?).to be_truthy
           expect(ems.supports_feature2?).to be_truthy
+          expect(ems.supports_feature3?).to be_truthy
         end
       end
     end
