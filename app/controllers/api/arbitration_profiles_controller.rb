363ae728d051cbdc22babb8a569fdf480f209d5f
Jillian Tullo <jtullo@redhat.com>
2016-09-20 13:55:58 -0400

    removing validate_profile and validating object in create_resource
    name consistency

--- app/controllers/api/arbitration_profiles_controller.rb
+++ app/controllers/api/arbitration_profiles_controller.rb
@@ -3,8 +3,8 @@ module Api
     def create_resource(_type, _id, data)
       validate_profile_data(data)
       attributes = build_arbitration_attributes(data)
-      arbitration_profile = collection_class(:arbitration_profiles).create(attributes)
-      validate_profile(arbitration_profile)
+      arbitration_profile = ArbitrationProfile.create(attributes)
+      raise BadRequestError, arbitration_profile.errors.full_messages.join(', ') unless arbitration_profile.valid?
       arbitration_profile
     end
 
@@ -33,13 +33,6 @@ module Api
       if data.key?('provider') && data.key?('ext_management_system')
         raise BadRequestError, 'Only one of provider or ext_management_system may be specified'
       end
-    end
-  end
-
-  def validate_profile(profile)
-    if profile.invalid?
-      raise BadRequestError, "Failed to add new arbitration profile -
-            #{profile.errors.full_messages.join(', ')}"
     end
   end
 end
