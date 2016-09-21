91deac27173896f8ca561ae69c1e1fba544a8e7e
Artyom Lukianov <alukiano@redhat.com>
2016-09-21 23:43:01 +0300

    rubocop issues: custom_loader.rb
    == app/presenters/menu/custom_loader.rb ==
    R: 37:121: Line is too long. [129/120]
    R: 39:121: Line is too long. [125/120]

--- app/presenters/menu/custom_loader.rb
+++ app/presenters/menu/custom_loader.rb
@@ -34,9 +34,15 @@ module Menu
       rbac = properties['rbac'].each_with_object({}) { |(k, v), h| h[k.to_sym] = v }
       item_type = properties.key?('item_type') ? properties['item_type'].to_sym : :default
       %w(id name rbac parent).each do |property|
-        raise Menu::Manager::InvalidMenuDefinition, "incomplete definition -- missing #{property}" if properties[property].blank?
+        raise Menu::Manager::InvalidMenuDefinition,
+              "incomplete definition -- missing #{property}" if properties[property].blank?
       end
-      item = CustomItem.new(properties['id'], properties['name'], properties['feature'], rbac, properties['href'], item_type)
+      item = CustomItem.new(properties['id'],
+                            properties['name'],
+                            properties['feature'],
+                            rbac,
+                            properties['href'],
+                            item_type)
       item.parent = properties['parent'].to_sym
       item
     end
