505d910280c4336871748f0af91e5c58913379df
Brian McLaughlin <bmclaugh@redhat.com>
2016-09-21 10:59:42 -0400

    Update deprecated expect(response) reference
    https://bugzilla.redhat.com/show_bug.cgi?id=1364921

--- spec/views/miq_request/_prov_vm_grid.html.erb_spec.rb
+++ spec/views/miq_request/_prov_vm_grid.html.erb_spec.rb
@@ -18,8 +18,8 @@ describe 'miq_request/_prov_vm_grid.html.haml' do
     it 'validates links in vm grid' do
       login_as @vm_user
       render :partial => "miq_request/prov_vm_grid.html.haml", :locals => {:field_id => 'service__src_vm_id'}
-      expect(response).to have_selector("//tr[@onclick=\"miqAjax('/miq_request/prov_field_changed/?service__src_vm_id=__VM__NONE__&id=foo');\"]")
-      expect(response).to have_selector("//tr[@onclick=\"miqAjax('/miq_request/prov_field_changed/?service__src_vm_id=#{@vms.first.id}&id=foo');\"]")
+      expect(rendered).to have_selector("//tr[@onclick=\"miqAjax('/miq_request/prov_field_changed/?service__src_vm_id=__VM__NONE__&id=foo');\"]")
+      expect(rendered).to have_selector("//tr[@onclick=\"miqAjax('/miq_request/prov_field_changed/?service__src_vm_id=#{@vms.first.id}&id=foo');\"]")
     end
   end
 
