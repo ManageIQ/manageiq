8a3180410bca997d18f76fbfb44a93b52b90e0bf
Jillian Tullo <jtullo@redhat.com>
2016-09-22 09:38:35 -0400

    remove validation

--- app/controllers/api/blueprints_controller.rb
+++ app/controllers/api/blueprints_controller.rb
@@ -4,19 +4,6 @@ module Api
 
     before_action :set_additional_attributes, :only => [:show]
 
-    def create_resource(_type, _id, data)
-      raise BadRequestError, 'Failed to create Blueprint - ui_properties cannot be empty' unless data['ui_properties']
-      validate_ui_properties(data['ui_properties'])
-      Blueprint.create!(data)
-    end
-
-    def edit_resource(type, id, data)
-      validate_ui_properties(data['ui_properties']) if data['ui_properties']
-      blueprint = resource_search(id, type, Blueprint)
-      blueprint.update!(data)
-      blueprint
-    end
-
     def publish_resource(type, id, data)
       blueprint = resource_search(id, type, Blueprint)
       blueprint.publish(data['bundle_name'])
@@ -24,15 +11,6 @@ module Api
     end
 
     private
-
-    def validate_ui_properties(ui_properties)
-      unless ui_properties.key?('service_catalog') && ui_properties.key?('service_dialog') &&
-             ui_properties.key?('automate_entrypoints') && ui_properties.key?('chart_data_model')
-        raise BadRequestError,
-              'Failed to create Blueprint - ' \
-              'ui_properties must include service_catalog, service_dialog, automate_entrypoints, and chart_data_model'
-      end
-    end
 
     def set_additional_attributes
       @additional_attributes = %w(content)
