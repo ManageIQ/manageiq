850bc40eca306a4e49b303e1bb1db0dc37748694
Keenan Brock <keenan@thebrocks.net>
2016-09-21 23:21:22 -0400

    Merge pull request #11434 from mkanoor/workspace_in_drb_front
    Store the workspace in the DRb front object
    (cherry picked from commit b47179ba1f9dee12884d84f59423686475d4d0e8)

--- lib/miq_automation_engine/engine/miq_ae_service.rb
+++ lib/miq_automation_engine/engine/miq_ae_service.rb
@@ -10,6 +10,11 @@ module MiqAeMethodService
 
   class MiqAeServiceFront
     include DRbUndumped
+    attr_accessor :workspace
+    def initialize(workspace)
+      @workspace = workspace
+    end
+
     def find(id)
       MiqAeService.find(id)
     end
