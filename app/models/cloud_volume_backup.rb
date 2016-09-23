93b1cda22a1a5f55bdef7f0eaebbcc16c0ed0b1c
hsong-rh <hsong@redhat.com>
2016-09-23 18:00:52 -0400

    Enable cinder to handle cloud volume backup - revert changes

--- app/models/cloud_volume_backup.rb
+++ app/models/cloud_volume_backup.rb
@@ -4,7 +4,7 @@ class CloudVolumeBackup < ApplicationRecord
 
   acts_as_miq_taggable
 
-  belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::StorageManager::CinderManager"
+  belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::CloudManager"
   belongs_to :availability_zone
   belongs_to :cloud_volume
 end
