d09621cbd6490cca9b56bca64c97fb3b9a02d2d0
Madhu Kanoor <mkanoor@redhat.com>
2016-09-23 17:18:00 -0400

    Refactored specs based on eriks guidelines

--- spec/lib/miq_automation_engine/models/miq_ae_git_import_spec.rb
+++ spec/lib/miq_automation_engine/models/miq_ae_git_import_spec.rb
@@ -1,10 +1,11 @@
 describe MiqAeGitImport do
-  before do
-    EvmSpecHelper.local_guid_miq_server_zone
-    @user = FactoryGirl.create(:user_with_group)
-  end
+  context "#import" do
+    before do
+      EvmSpecHelper.local_guid_miq_server_zone
+      @user = FactoryGirl.create(:user_with_group)
+    end
 
-  context "git import" do
+    let(:miq_ae_git_import) { described_class.new(options) }
     let(:branch_name) { "b1" }
     let(:tag_name) { "t1" }
     let(:domain_name) { "BB8" }
@@ -26,88 +27,118 @@ describe MiqAeGitImport do
     let(:branch) { FactoryGirl.create(:git_branch, :name => branch_name) }
     let(:tag) { FactoryGirl.create(:git_tag, :name => tag_name) }
 
-    it "import a domain given a git repository and a branch" do
-      domain_name = domain.name
-      allow_any_instance_of(GitRepository).to receive(:refresh).with(no_args).and_return(nil)
-      allow_any_instance_of(GitRepository).to receive(:git_branches).with(no_args).and_return([branch])
-      allow_any_instance_of(MiqAeYamlImportGitfs).to receive(:import).with(any_args).and_return(domain)
-      allow_any_instance_of(MiqAeYamlImportGitfs).to receive(:load_repo).with(any_args).and_return(nil)
-      options = {'domain'            => domain_name,
-                 'git_repository_id' => repo.id,
-                 'tenant_id'         => @user.current_tenant.id,
-                 'ref'               => branch_name}
-      dom = MiqAeGitImport.new(options).import
-      expect(dom.attributes).to have_attributes(branch_hash)
+    context "when the ref type and ref are valid" do
+      let(:miq_ae_yaml_import_gitfs) { double("MiqAeYamlImportGitfs") }
+
+      before do
+        allow(GitRepository).to receive(:find).with(repo.id).and_return(repo)
+        allow(repo).to receive(:refresh).and_return(nil)
+
+        allow(MiqAeYamlImportGitfs).to receive(:new).with(domain_name, options).and_return(miq_ae_yaml_import_gitfs)
+      end
+
+      context "when there are branches returned" do
+        let(:options) do
+          {
+            'domain'            => domain_name,
+            'git_repository_id' => repo.id,
+            'tenant_id'         => @user.current_tenant.id,
+            'ref'               => branch_name
+          }
+        end
+
+        before do
+          allow(repo).to receive(:git_branches).and_return([branch])
+        end
+
+        it "imports correctly" do
+          allow(miq_ae_yaml_import_gitfs).to receive(:import).and_return(domain)
+          dom = miq_ae_git_import.import
+          expect(dom.attributes).to have_attributes(branch_hash)
+        end
+
+        it "import fails with domain not found" do
+          allow(miq_ae_yaml_import_gitfs).to receive(:import).and_return(nil)
+          expect { miq_ae_git_import.import }.to raise_error(MiqAeException::DomainNotFound)
+        end
+      end
+
+      context "when there are tags returned" do
+        let(:options) do
+          {
+            'domain'            => domain_name,
+            'git_repository_id' => repo.id,
+            'tenant_id'         => @user.current_tenant.id,
+            'ref'               => tag_name,
+            'ref_type'          => MiqAeDomain::TAG
+          }
+        end
+
+        before do
+          allow(repo).to receive(:git_tags).and_return([tag])
+        end
+
+        it "imports correctly" do
+          allow(miq_ae_yaml_import_gitfs).to receive(:import).and_return(domain)
+          dom = miq_ae_git_import.import
+          expect(dom.attributes).to have_attributes(tag_hash)
+        end
+      end
+
+      context "when a git_url and branch are given" do
+        let(:options) do
+          {
+            'git_url'  => url,
+            'ref'      => branch_name,
+            'userid'   => 'fred',
+            'password' => 'secret',
+            'preview'  => false,
+            'domain'   => domain_name
+          }
+        end
+
+        before do
+          allow(GitRepository).to receive(:find_or_create_by).with(:url => url).and_return(repo)
+          allow(repo).to receive(:git_branches).and_return([branch])
+        end
+
+        it "imports correctly" do
+          allow(miq_ae_yaml_import_gitfs).to receive(:import).and_return(domain)
+          dom = miq_ae_git_import.import
+          expect(dom.attributes).to have_attributes(branch_hash)
+        end
+      end
     end
 
-    it "import a domain given a git repository and a tag" do
-      domain_name = domain.name
-      allow_any_instance_of(GitRepository).to receive(:refresh).with(no_args).and_return(nil)
-      allow_any_instance_of(GitRepository).to receive(:git_tags).with(no_args).and_return([tag])
-      allow_any_instance_of(MiqAeYamlImportGitfs).to receive(:import).with(any_args).and_return(domain)
-      allow_any_instance_of(MiqAeYamlImportGitfs).to receive(:load_repo).with(any_args).and_return(nil)
-      options = {'domain'            => domain_name,
-                 'git_repository_id' => repo.id,
-                 'tenant_id'         => @user.current_tenant.id,
-                 'ref'               => tag_name,
-                 'ref_type'          => 'tag'}
+    context "when the ref type and ref are not valid" do
+      let(:options) { {'git_repository_id' => repo.id, 'ref_type' => ref_type, 'ref' => ref} }
 
-      dom = MiqAeGitImport.new(options).import
-      expect(dom.attributes).to have_attributes(tag_hash)
-    end
+      shared_examples_for "#import that has invalid ref or ref type" do
+        it "throws an argument error" do
+          expect { miq_ae_git_import.import }.to raise_error(ArgumentError)
+        end
+      end
 
-    it "import fails given a git repository and a tag" do
-      domain_name = domain.name
-      allow_any_instance_of(GitRepository).to receive(:refresh).with(no_args).and_return(nil)
-      allow_any_instance_of(GitRepository).to receive(:git_tags).with(no_args).and_return([tag])
-      allow_any_instance_of(MiqAeYamlImportGitfs).to receive(:import).with(any_args).and_return(nil)
-      allow_any_instance_of(MiqAeYamlImportGitfs).to receive(:load_repo).with(any_args).and_return(nil)
-      options = {'domain'            => domain_name,
-                 'git_repository_id' => repo.id,
-                 'tenant_id'         => @user.current_tenant.id,
-                 'ref'               => tag_name,
-                 'ref_type'          => 'tag'}
+      context "when the branch does not exist" do
+        let(:ref) { 'branch does not exist' }
+        let(:ref_type) { nil }
 
-      expect { MiqAeGitImport.new(options).import }.to raise_error(MiqAeException::DomainNotFound)
-    end
+        it_behaves_like "#import that has invalid ref or ref type"
+      end
 
-    it "import a domain given a git url and a branch" do
-      options = {'git_url'   => url,
-                 'tenant_id' => @user.current_tenant.id,
-                 'ref'       => branch_name,
-                 'userid'    => 'fred',
-                 'password'  => 'secret',
-                 'preview'   => false }
-      allow_any_instance_of(GitRepository).to receive(:refresh).with(no_args).and_return(nil)
-      allow_any_instance_of(GitRepository).to receive(:git_branches).with(no_args).and_return([branch])
-      allow_any_instance_of(MiqAeYamlImportGitfs).to receive(:import).with(any_args).and_return(domain)
-      allow_any_instance_of(MiqAeYamlImportGitfs).to receive(:load_repo).with(any_args).and_return(nil)
+      context "when the ref type is tag but the tag does not exist" do
+        let(:ref) { 'tag does not exist' }
+        let(:ref_type) { 'tag' }
 
-      dom = MiqAeGitImport.new(options).import
-      expect(dom.attributes).to have_attributes(branch_hash)
-    end
+        it_behaves_like "#import that has invalid ref or ref type"
+      end
 
-    it "non existent branch throws an exception" do
-      options = {'git_repository_id' => repo.id,
-                 'ref'               => 'Does not exist'}
+      context "when the ref type is invalid" do
+        let(:ref_type) { "Invalid" }
+        let(:ref) { "branch" }
 
-      expect { MiqAeGitImport.new(options).import }.to raise_error(ArgumentError)
-    end
-
-    it "non existent tag throws an exception" do
-      options = {'git_repository_id' => repo.id,
-                 'ref'               => 'Does not exist',
-                 'ref_type'          => 'tag'}
-
-      expect { MiqAeGitImport.new(options).import }.to raise_error(ArgumentError)
-    end
-
-    it "invalid ref_type throws an exception" do
-      options = {'git_repository_id' => repo.id,
-                 'ref_type'          => 'Invalid',
-                 'ref'               => 'branch'}
-
-      expect { MiqAeGitImport.new(options).import }.to raise_error(ArgumentError)
+        it_behaves_like "#import that has invalid ref or ref type"
+      end
     end
   end
 end
