ae77184665a1ddafc9d3bb711c3de4eca9348f15
Dan Clarizio <dclarizi@redhat.com>
2016-09-23 13:15:46 -0700

    Merge pull request #11212 from gildub/network-provisioning-subnet
    UI: Adds Subnet provisioning for Network provider
    (cherry picked from commit d12b07ace352a0d148341fcaf36c4ed2588aec83)

--- app/helpers/application_helper/toolbar/cloud_subnets_center.rb
+++ app/helpers/application_helper/toolbar/cloud_subnets_center.rb
@@ -1,4 +1,40 @@
 class ApplicationHelper::Toolbar::CloudSubnetsCenter < ApplicationHelper::Toolbar::Basic
+  button_group('cloud_subnet_vmdb', [
+    select(
+      :cloud_subnet_vmdb_choice,
+      'fa fa-cog fa-lg',
+      t = N_('Configuration'),
+      t,
+      :items => [
+        button(
+          :cloud_subnet_new,
+          'pficon pficon-add-circle-o fa-lg',
+          t = N_('Add a new Cloud Subnet'),
+          t,
+        ),
+        separator,
+        button(
+          :cloud_subnet_edit,
+          'pficon pficon-edit fa-lg',
+          t = N_('Edit selected Cloud Subnet'),
+          t,
+          :url_parms => 'main_div',
+          :enabled   => false,
+          :onwhen    => '1'
+        ),
+        button(
+          :cloud_subnet_delete,
+          'pficon pficon-delete fa-lg',
+          t = N_('Delete selected Cloud Subnets'),
+          t,
+          :url_parms => 'main_div',
+          :confirm   => N_('Warning: The selected Cloud Subnets and ALL of their components will be removed!'),
+          :enabled   => false,
+          :onwhen    => '1+'
+        ),
+      ]
+    )])
+
   button_group('cloud_subnet_policy', [
     select(
       :cloud_subnet_policy_choice,
@@ -15,7 +51,8 @@ class ApplicationHelper::Toolbar::CloudSubnetsCenter < ApplicationHelper::Toolba
           N_('Edit Tags'),
           :url_parms => "main_div",
           :enabled   => false,
-          :onwhen    => "1+"),
+          :onwhen    => "1+"
+        ),
       ]
     ),
   ])
