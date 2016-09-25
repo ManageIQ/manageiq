e23a29e6641f084f2bb6b3486ad7370930b386d7
Beni Cherniavsky-Paskin <cben@redhat.com>
2016-09-25 19:46:32 +0300

    Hash expectations: have_attributes -> eq/include: nil corner cases
    (Our `have_attributes(:key => nil)` matcher had bug where it passed
    if actual hash doesn't have :key.)

--- spec/models/manageiq/providers/openstack/infra_manager/event_parser_spec.rb
+++ spec/models/manageiq/providers/openstack/infra_manager/event_parser_spec.rb
@@ -4,15 +4,16 @@ describe ManageIQ::Providers::Openstack::InfraManager::EventParser do
       event = YAML.load_file(File.join(data_dir, 'compute_instance_create_end.yml'))
       data = described_class.event_to_hash(event, 123)
 
-      expected_attributes = common_attributes(event).merge(
+      expected_hash = common_fields(event).merge(
         :event_type   => "compute.instance.create.end",
         :chain_id     => "r-otxomvqw",
         :timestamp    => "2015-05-12 07:24:39.462895",
         :host_ems_ref => "cdab9a8d-d653-4dee-81f9-173f9a22bd2e",
-        :message      => "Success"
+        :message      => "Success",
+        :availability_zone_ems_ref => nil,
       )
 
-      expect(data).to have_attributes(expected_attributes)
+      expect(data).to eq(expected_hash)
 
       expect(data[:full_data]).to    be_instance_of Hash
       expect(data[:host_ems_ref]).to be_instance_of String
@@ -22,17 +23,18 @@ describe ManageIQ::Providers::Openstack::InfraManager::EventParser do
       event = YAML.load_file(File.join(data_dir, 'compute_instance_create_error.yml'))
       data = described_class.event_to_hash(event, 123)
 
-      expected_attributes = common_attributes(event).merge(
+      expected_hash = common_fields(event).merge(
         :event_type   => "compute.instance.create.error",
         :chain_id     => "r-36dfs67z",
         :timestamp    => "2015-05-12 07:22:19.122336",
         :host_ems_ref => "b94ebb7a-34f2-4146-94c3-5bbc46b4d5ff",
         :message      => "Failed to provision instance 3a0c66d5-d762-4b60-b604-850bc9a13cff: Failed to deploy. Error:" \
                          " Failed to execute command via SSH: LC_ALL=C /usr/bin/virsh --connect qemu:///system start"\
-                         " baremetal_2."
+                         " baremetal_2.",
+        :availability_zone_ems_ref => nil,
       )
 
-      expect(data).to have_attributes(expected_attributes)
+      expect(data).to eq(expected_hash)
 
       expect(data[:full_data]).to    be_instance_of Hash
       expect(data[:host_ems_ref]).to be_instance_of String
@@ -42,12 +44,12 @@ describe ManageIQ::Providers::Openstack::InfraManager::EventParser do
       event = YAML.load_file(File.join(data_dir, 'orchestration_stack_create_end.yml'))
       data = described_class.event_to_hash(event, 123)
 
-      expected_attributes = common_attributes(event).merge(
+      expected_hash = common_fields(event).merge(
         :event_type => "orchestration.stack.create.end",
         :timestamp  => "2015-05-12 07:24:45.026776"
       )
 
-      expect(data).to have_attributes(expected_attributes)
+      expect(data).to eq(expected_hash)
 
       expect(data[:full_data]).to be_instance_of Hash
     end
@@ -56,12 +58,12 @@ describe ManageIQ::Providers::Openstack::InfraManager::EventParser do
       event = YAML.load_file(File.join(data_dir, 'orchestration_stack_update_end.yml'))
       data = described_class.event_to_hash(event, 123)
 
-      expected_attributes = common_attributes(event).merge(
+      expected_hash = common_fields(event).merge(
         :event_type => "orchestration.stack.update.end",
         :timestamp  => "2015-05-12 07:33:57.772136"
       )
 
-      expect(data).to have_attributes(expected_attributes)
+      expect(data).to eq(expected_hash)
 
       expect(data[:full_data]).to be_instance_of Hash
     end
@@ -70,12 +72,12 @@ describe ManageIQ::Providers::Openstack::InfraManager::EventParser do
       event = YAML.load_file(File.join(data_dir, 'port_create_end.yml'))
       data = described_class.event_to_hash(event, 123)
 
-      expected_attributes = common_attributes(event).merge(
+      expected_hash = common_fields(event).merge(
         :event_type => "port.create.end",
         :timestamp  => "2015-05-12 07:22:37.008738"
       )
 
-      expect(data).to have_attributes(expected_attributes)
+      expect(data).to eq(expected_hash)
 
       expect(data[:full_data]).to be_instance_of Hash
     end
@@ -84,12 +86,12 @@ describe ManageIQ::Providers::Openstack::InfraManager::EventParser do
       event = YAML.load_file(File.join(data_dir, 'port_update_end.yml'))
       data = described_class.event_to_hash(event, 123)
 
-      expected_attributes = common_attributes(event).merge(
+      expected_hash = common_fields(event).merge(
         :event_type => "port.update.end",
         :timestamp  => "2015-05-12 07:22:43.948145"
       )
 
-      expect(data).to have_attributes(expected_attributes)
+      expect(data).to eq(expected_hash)
 
       expect(data[:full_data]).to be_instance_of Hash
     end
@@ -99,22 +101,13 @@ describe ManageIQ::Providers::Openstack::InfraManager::EventParser do
     File.expand_path(File.join(File.dirname(__FILE__), "event_parser_data"))
   end
 
-  def common_attributes(event)
+  def common_fields(event)
     {
-      :event_type   => "compute.instance.create.error",
-      :chain_id     => nil,
-      :is_task      => nil,
       :source       => "OPENSTACK",
       :message      => nil,
-      :timestamp    => nil,
       :full_data    => event,
       :ems_id       => 123,
       :username     => nil,
-      :vm_ems_ref   => nil,
-      :vm_name      => nil,
-      :vm_location  => nil,
-      :host_ems_ref => nil,
-      :host_name    => nil,
     }
   end
 end
