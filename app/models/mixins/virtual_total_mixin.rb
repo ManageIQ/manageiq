0a5029fb635c58bedc8c6686e9a549d73adf8d93
Keenan Brock <kbrock@redhat.com>
2016-09-26 09:04:37 -0400

    virtual_aggregate like totals, but for :sum

--- app/models/mixins/virtual_total_mixin.rb
+++ app/models/mixins/virtual_total_mixin.rb
@@ -5,24 +5,59 @@ module VirtualTotalMixin
     private
 
     # define an attribute to calculating the total of a child
+    def virtual_total(name, relation, options = {})
+      virtual_aggregate(name, relation, :size, nil, options)
+    end
+
+    # define an attribute to calculating the total of a child
+    #
+    #  example 1:
+    #
     #   virtual_total :total_vms, :vms
     #
     #   def total_vms
     #     vms.count
     #   end
-    #   virtual_attribute :total_vms, :integer, :uses => :vms
     #
-    #   It also defines the necessary arel so it will sort by the total in the database
+    #   virtual_attribute :total_vms, :integer, :uses => :vms, :arel => ...
     #
-    def virtual_total(name, relation, options = {})
-      define_method(name) do
-        (attribute_present?(name) ? self[name] : nil) || send(relation).try(:size) || 0
+    #  example 2:
+    #
+    #   virtual_aggregate :allocated_disk_storage, :disks, :sum, :size
+    #
+    #   def allocated_disk_storage
+    #     if disks.loaded?
+    #       disks.blank? ? nil : disks.map { |t| t.size.to_i }.sum
+    #     else
+    #       disks.sum(:size) || 0
+    #     end
+    #   end
+    #   virtual_attribute :allocated_disk_storage, :integer, :uses => :disks, :arel => ...
+    #
+    def virtual_aggregate(name, relation, method_name = :sum, column = nil, options = {})
+      if method_name == :size
+        define_method(name) do
+          (attribute_present?(name) ? self[name] : nil) || send(relation).try(:size) || 0
+        end
+      else
+        define_method(name) do
+          (attribute_present?(name) ? self[name] : nil) ||
+            rel = send(relation)
+            if rel.loaded?
+              rel.blank? ? nil : rel.map { |t| t.send(column).to_i }.send(method_name)
+            else
+              # aggregates are not smart enough to handle virtual attributes
+              arel_column = rel.klass.arel_attribute(column)
+              rel.try(method_name, arel_column) || 0
+            end
+        end
       end
 
       reflection = reflect_on_association(relation)
 
       if options.key?(:arel)
         arel = options.dup.delete(:arel)
+        # if there is no relation to get to the arel, have to throw it away
         arel = nil if !arel || !reflection
       elsif reflection && reflection.macro == :has_many && !reflection.options[:through]
         arel = lambda do |t|
@@ -30,8 +65,12 @@ module VirtualTotalMixin
           # need db access for the keys, so delaying all this lookup until call time
           local_key = reflection.active_record_primary_key
           foreign_key = reflection.foreign_key
-          # assuming has_many
-          Arel::Nodes::Grouping.new(foreign_table.project(Arel.star.count)
+          arel_column = if method_name == :size
+                          Arel.star.count
+                        else
+                          reflection.klass.arel_attribute(column).send(method_name)
+                        end
+          Arel::Nodes::Grouping.new(foreign_table.project(arel_column)
                                                  .where(t[local_key].eq(foreign_table[foreign_key])))
         end
       end
