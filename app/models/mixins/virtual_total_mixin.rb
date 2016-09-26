d079691316be7fe691691a0af1477bff4e8af522
Keenan Brock <kbrock@redhat.com>
2016-09-26 13:58:56 -0400

    virtual totals: add comments and extract 2 methods

--- app/models/mixins/virtual_total_mixin.rb
+++ app/models/mixins/virtual_total_mixin.rb
@@ -13,28 +13,62 @@ module VirtualTotalMixin
     #
     #  example 1:
     #
-    #   virtual_total :total_vms, :vms
+    #    class ExtManagementSystem
+    #      has_many :vms
+    #      virtual_total :total_vms, :vms
+    #    end
     #
-    #   def total_vms
-    #     vms.count
-    #   end
+    #    generates:
     #
-    #   virtual_attribute :total_vms, :integer, :uses => :vms, :arel => ...
+    #    def total_vms
+    #      vms.count
+    #    end
+    #
+    #    virtual_attribute :total_vms, :integer, :uses => :vms, :arel => ...
+    #
+    #   # arel == (SELECT COUNT(*) FROM vms where ems.id = vms.ems_id)
     #
     #  example 2:
     #
-    #   virtual_aggregate :allocated_disk_storage, :disks, :sum, :size
+    #    class Hardware
+    #      has_many :disks
+    #      virtual_aggregate :allocated_disk_storage, :disks, :sum, :size
+    #    end
     #
-    #   def allocated_disk_storage
-    #     if disks.loaded?
-    #       disks.blank? ? nil : disks.map { |t| t.size.to_i }.sum
-    #     else
-    #       disks.sum(:size) || 0
-    #     end
-    #   end
-    #   virtual_attribute :allocated_disk_storage, :integer, :uses => :disks, :arel => ...
+    #    generates:
     #
+    #    def allocated_disk_storage
+    #      if disks.loaded?
+    #        disks.blank? ? nil : disks.map { |t| t.size.to_i }.sum
+    #      else
+    #        disks.sum(:size) || 0
+    #      end
+    #    end
+    #
+    #    virtual_attribute :allocated_disk_storage, :integer, :uses => :disks, :arel => ...
+    #
+    #    # arel => (SELECT sum("disks"."size") where "hardware"."id" = "disks"."hardware_id")
+
     def virtual_aggregate(name, relation, method_name = :sum, column = nil, options = {})
+      define_virtual_aggregate_method(name, relation, method_name, column)
+      reflection = reflect_on_association(relation)
+
+      if options.key?(:arel)
+        arel = options.dup.delete(:arel)
+        # if there is no relation to get to the arel, have to throw it away
+        arel = nil if !arel || !reflection
+      else
+        arel = virtual_aggregate_arel(reflection, method_name, column)
+      end
+
+      if arel
+        virtual_attribute name, :integer, :uses => options[:uses] || relation, :arel => arel
+      else
+        virtual_attribute name, :integer, **options
+      end
+    end
+
+    def define_virtual_aggregate_method(name, relation, method_name, column)
       if method_name == :size
         define_method(name) do
           (attribute_present?(name) ? self[name] : nil) || send(relation).try(:size) || 0
@@ -42,43 +76,34 @@ module VirtualTotalMixin
       else
         define_method(name) do
           (attribute_present?(name) ? self[name] : nil) ||
-            rel = send(relation)
-            if rel.loaded?
-              rel.blank? ? nil : rel.map { |t| t.send(column).to_i }.send(method_name)
-            else
-              # aggregates are not smart enough to handle virtual attributes
-              arel_column = rel.klass.arel_attribute(column)
-              rel.try(method_name, arel_column) || 0
+            begin
+              rel = send(relation)
+              if rel.loaded?
+                rel.blank? ? nil : (rel.map { |t| t.send(column).to_i } || 0).send(method_name)
+              else
+                # aggregates are not smart enough to handle virtual attributes
+                arel_column = rel.klass.arel_attribute(column)
+                rel.try(method_name, arel_column) || 0
+              end
             end
         end
       end
+    end
 
-      reflection = reflect_on_association(relation)
-
-      if options.key?(:arel)
-        arel = options.dup.delete(:arel)
-        # if there is no relation to get to the arel, have to throw it away
-        arel = nil if !arel || !reflection
-      elsif reflection && reflection.macro == :has_many && !reflection.options[:through]
-        arel = lambda do |t|
-          foreign_table = reflection.klass.arel_table
-          # need db access for the keys, so delaying all this lookup until call time
-          local_key = reflection.active_record_primary_key
-          foreign_key = reflection.foreign_key
-          arel_column = if method_name == :size
-                          Arel.star.count
-                        else
-                          reflection.klass.arel_attribute(column).send(method_name)
-                        end
-          Arel::Nodes::Grouping.new(foreign_table.project(arel_column)
-                                                 .where(t[local_key].eq(foreign_table[foreign_key])))
-        end
-      end
-
-      if arel
-        virtual_attribute name, :integer, :uses => options[:uses] || relation, :arel => arel
-      else
-        virtual_attribute name, :integer, **options
+    def virtual_aggregate_arel(reflection, method_name, column)
+      return unless reflection && reflection.macro == :has_many && !reflection.options[:through]
+      lambda do |t|
+        foreign_table = reflection.klass.arel_table
+        # need db access for the keys, so delaying all this lookup until call time
+        local_key = reflection.active_record_primary_key
+        foreign_key = reflection.foreign_key
+        arel_column = if method_name == :size
+                        Arel.star.count
+                      else
+                        reflection.klass.arel_attribute(column).send(method_name)
+                      end
+        t.grouping(foreign_table.project(arel_column)
+                                .where(t[local_key].eq(foreign_table[foreign_key])))
       end
     end
   end
