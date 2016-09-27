2b3e4dbb4ef6c7ebe2fd0598d270506daf2b67bc
Greg Blomquist <blomquisg@gmail.com>
2016-09-27 17:33:49 -0400

    Merge pull request #10764 from durandom/automate_inflector
    get provider name by inflecting on the class
    (cherry picked from commit 34a7b0de57066f8c946dcef3a006a6fc722ba9cc)

---
+++ spec/models/manageiq/providers/inflector_spec.rb
@@ -0,0 +1,59 @@
+describe ManageIQ::Providers::Inflector do
+  context "#provider_name" do
+    it "returns the name for an instance of a manager" do
+      manager = ManageIQ::Providers::Amazon::CloudManager.new
+      expect(described_class.provider_name(manager)).to eq('Amazon')
+    end
+
+    it "returns the name for an instance of a manager when called on the instance" do
+      manager = ManageIQ::Providers::Amazon::CloudManager.new
+      expect(manager.provider_name).to eq('Amazon')
+    end
+
+    it "returns the name for a class of a manager" do
+      manager = ManageIQ::Providers::Amazon::CloudManager
+      expect(described_class.provider_name(manager)).to eq('Amazon')
+    end
+
+    it "returns the name for an instance of a Vm" do
+      vm = ManageIQ::Providers::Amazon::CloudManager::Vm.new
+      expect(described_class.provider_name(vm)).to eq('Amazon')
+    end
+
+    it "raises an error on not namespaced objects" do
+      vm = Vm.new
+      expect do
+        described_class.provider_name(vm)
+      end.to raise_error(ManageIQ::Providers::Inflector::ObjectNotNamespacedError)
+    end
+  end
+
+  context "#manager_type" do
+    it "returns the type for an instance of a manager" do
+      manager = ManageIQ::Providers::Amazon::CloudManager.new
+      expect(described_class.manager_type(manager)).to eq('Cloud')
+    end
+
+    it "returns the type for an instance of a manager when called on the instance" do
+      manager = ManageIQ::Providers::Amazon::CloudManager.new
+      expect(manager.manager_type).to eq('Cloud')
+    end
+
+    it "returns the type for a class of a manager" do
+      manager = ManageIQ::Providers::Amazon::CloudManager
+      expect(described_class.manager_type(manager)).to eq('Cloud')
+    end
+
+    it "returns the type for an instance of a Vm" do
+      vm = ManageIQ::Providers::Amazon::CloudManager::Vm.new
+      expect(described_class.manager_type(vm)).to eq('Cloud')
+    end
+
+    it "raises an error on not namespaced objects" do
+      vm = Vm.new
+      expect do
+        described_class.manager_type(vm)
+      end.to raise_error(ManageIQ::Providers::Inflector::ObjectNotNamespacedError)
+    end
+  end
+end
