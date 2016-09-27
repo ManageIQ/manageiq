c7676de286093d7541d7a84540b05ccc1fb8d4d7
Madhu Kanoor <mkanoor@redhat.com>
2016-09-27 16:41:57 -0400

    Merge pull request #10126 from gmcculloug/vm_name_refactor
    Refactor the VM naming method used in automate provisioning
    (cherry picked from commit 91ca2d3fb7fa317ec8f7f576fc93c8c206875b1e)

--- db/fixtures/ae_datastore/ManageIQ/Infrastructure/VM/Provisioning/Naming.class/__methods__/vmname.rb
+++ db/fixtures/ae_datastore/ManageIQ/Infrastructure/VM/Provisioning/Naming.class/__methods__/vmname.rb
@@ -1,58 +1,64 @@
+# / Infra / VM / Provisioning / Naming / default (vmname)
+
 #
-# Description: This is the default vmnaming method
+# Description: This is the default naming scheme
 # 1. If VM Name was not chosen during dialog processing then use vm_prefix
 #    from dialog else use model and [:environment] tag to generate name
 # 2. Else use VM name chosen in dialog
-# 3. Then add 3 digit suffix to vm_name
-# 4. Added support for dynamic service naming
+# 3. Add 3 digit suffix to vmname if more than one VM is being provisioned
 #
 
-$evm.log("info", "Detected vmdb_object_type:<#{$evm.root['vmdb_object_type']}>")
-
-prov = $evm.root['miq_provision_request'] || $evm.root['miq_provision'] || $evm.root['miq_provision_request_template']
-
-vm_name = prov.get_option(:vm_name).to_s.strip
-number_of_vms_being_provisioned = prov.get_option(:number_of_vms)
-diamethod = prov.get_option(:vm_prefix).to_s.strip
-
-# If no VM name was chosen during dialog
-if vm_name.blank? || vm_name == 'changeme'
-  vm_prefix = nil
-  vm_prefix ||= $evm.object['vm_prefix']
-  $evm.log("info", "vm_name from dialog:<#{vm_name.inspect}> vm_prefix from dialog:<#{diamethod.inspect}> vm_prefix from model:<#{vm_prefix.inspect}>")
-
-  # Get Provisioning Tags for VM Name
-  tags = prov.get_tags
-  $evm.log("info", "Provisioning Object Tags: #{tags.inspect}")
-
-  # Set a Prefix for VM Naming
-  if diamethod.blank?
-    vm_name = vm_prefix
-  else
-    vm_name = diamethod
+class VmName
+  def initialize(handle = $evm)
+    @handle = handle
   end
-  $evm.log("info", "VM Naming Prefix: <#{vm_name}>")
 
-  # Check :environment tag
-  env = tags[:environment]
-
-  # If environment tag is not nil
-  unless env.nil?
-    $evm.log("info", "Environment Tag: <#{env}> detected")
-    # Get the first 3 characters of the :environment tag
-    env_first = env[0, 3]
-
-    vm_name =  "#{vm_name}#{env_first}"
-    $evm.log("info", "Updating VM Name: <#{vm_name}>")
+  def main
+    @handle.log("info", "Detected vmdb_object_type:<#{@handle.root['vmdb_object_type']}>")
+    @handle.object['vmname'] = derived_name.compact.join
+    @handle.log(:info, "vmname: \"#{@handle.object['vmname']}\"")
   end
-  derived_name = "#{vm_name}$n{3}"
-else
-  if number_of_vms_being_provisioned == 1
-    derived_name = vm_name.to_s
-  else
-    derived_name = "#{vm_name}$n{3}"
+
+  def derived_name
+    if supplied_name.present?
+      [supplied_name, suffix(true)]
+    else
+      [prefix, env_tag, suffix(false)]
+    end
+  end
+
+  def supplied_name
+    @supplied_name ||= begin
+      vm_name = provision_object.get_option(:vm_name).to_s.strip
+      vm_name unless vm_name == 'changeme'
+    end
+  end
+
+  def provision_object
+    @provision_object ||= begin
+      @handle.root['miq_provision_request'] ||
+      @handle.root['miq_provision']         ||
+      @handle.root['miq_provision_request_template']
+    end
+  end
+
+  # Returns the name prefix (preferences model over dialog) or nil
+  def prefix
+    @handle.object['vm_prefix'] || provision_object.get_option(:vm_prefix).to_s.strip
+  end
+
+  # Returns the first 3 characters of the "environment" tag (or nil)
+  def env_tag
+    env = provision_object.get_tags[:environment]
+    return env[0, 3] unless env.blank?
+  end
+
+  # Returns the name suffix (provision number) or nil if provisioning only one
+  def suffix(condensed)
+    "$n{3}" if provision_object.get_option(:number_of_vms) > 1 || !condensed
   end
 end
 
-$evm.object['vmname'] = derived_name
-$evm.log("info", "VM Name: <#{derived_name}>")
+if __FILE__ == $PROGRAM_NAME
+  VmName.new.main
+end
