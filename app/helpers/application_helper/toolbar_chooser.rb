fccd09eb5bff896bd589e5e4979b49c65705463c
Dan Clarizio <dclarizi@redhat.com>
2016-09-28 15:50:40 -0700

    Merge pull request #11553 from rubenvp8510/Issue#11490
    Inconsistent Menus in Middleware Views
    (cherry picked from commit 6e8d8cdf899a7339c57266e849bb1cb9e5c5a8bc)

--- app/helpers/application_helper/toolbar_chooser.rb
+++ app/helpers/application_helper/toolbar_chooser.rb
@@ -439,7 +439,8 @@ class ApplicationHelper::ToolbarChooser
     # toolbar buttons on sub-screens
     to_display = %w(availability_zones cloud_networks cloud_object_store_containers cloud_subnets
                     cloud_tenants cloud_volumes ems_clusters flavors floating_ips hosts load_balancers
-                    network_ports network_routers orchestration_stacks resource_pools security_groups storages)
+                    network_ports network_routers orchestration_stacks resource_pools security_groups storages
+                    middleware_deployments middleware_datasources middleware_messagings middleware_servers)
     to_display_center = %w(stack_orchestration_template topology)
     if @lastaction == 'show' && (@view || @display != 'main') && !@layout.starts_with?("miq_request")
       if @display == "vms" || @display == "all_vms"
