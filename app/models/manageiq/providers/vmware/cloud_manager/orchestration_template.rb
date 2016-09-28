f2e27d41dccb894fb88d2930d73159791db928cc
Adam Grare <agrare@redhat.com>
2016-09-28 13:30:06 -0400

    Merge pull request #11340 from xlab-si/vmware_orchestration_service_support
    Enable support for vApp provisioning service
    (cherry picked from commit 8854669df1dcd5981871a6943e98fd025bfd035c)

--- app/models/manageiq/providers/vmware/cloud_manager/orchestration_template.rb
+++ app/models/manageiq/providers/vmware/cloud_manager/orchestration_template.rb
@@ -1,4 +1,34 @@
 class ManageIQ::Providers::Vmware::CloudManager::OrchestrationTemplate < OrchestrationTemplate
+  def parameter_groups
+    [OrchestrationTemplate::OrchestrationParameterGroup.new(
+      :label      => "vApp Parameters",
+      :parameters => vapp_parameters,
+    )]
+  end
+
+  def vapp_parameters
+    [
+      OrchestrationTemplate::OrchestrationParameter.new(
+        :name          => "deploy",
+        :label         => "Deploy vApp",
+        :data_type     => "string",
+        :default_value => "yes",
+        :constraints   => [
+          OrchestrationTemplate::OrchestrationParameterAllowed.new(:allowed_values => %w(no yes))
+        ]
+      ),
+      OrchestrationTemplate::OrchestrationParameter.new(
+        :name          => "powerOn",
+        :label         => "Power On vApp",
+        :data_type     => "string",
+        :default_value => "no",
+        :constraints   => [
+          OrchestrationTemplate::OrchestrationParameterAllowed.new(:allowed_values => %w(no yes))
+        ]
+      )
+    ]
+  end
+
   def self.eligible_manager_types
     [ManageIQ::Providers::Vmware::CloudManager]
   end
