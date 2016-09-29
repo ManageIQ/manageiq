748f9801c2e71425ce608b45f545709eb3f15c32
Ladislav Smola <lsmola@redhat.com>
2016-09-29 14:47:31 +0200

    Expose relation to all CloudSubnetNetworkPorts on Manager
    Expose relation to all CloudSubnetNetworkPorts on Manager

--- app/models/manageiq/providers/network_manager.rb
+++ app/models/manageiq/providers/network_manager.rb
@@ -10,6 +10,7 @@ module ManageIQ::Providers
     has_many :security_groups,             :foreign_key => :ems_id, :dependent => :destroy
     has_many :cloud_networks,              :foreign_key => :ems_id, :dependent => :destroy
     has_many :network_ports,               :foreign_key => :ems_id, :dependent => :destroy
+    has_many :cloud_subnet_network_ports,  :through => :network_ports
     has_many :network_routers,             :foreign_key => :ems_id, :dependent => :destroy
     has_many :network_groups,              :foreign_key => :ems_id, :dependent => :destroy
     has_many :load_balancers,              :foreign_key => :ems_id, :dependent => :destroy
