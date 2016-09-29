7d3464463ae0940023db1ffe2a704dcf4787cfe5
Ladislav Smola <lsmola@redhat.com>
2016-09-29 14:46:11 +0200

    Extend CloudSubnetNetworkPort with Dto capabilities
    Extend CloudSubnetNetworkPort with Dto capabilities

--- app/models/cloud_subnet_network_port.rb
+++ app/models/cloud_subnet_network_port.rb
@@ -1,4 +1,10 @@
 class CloudSubnetNetworkPort < ApplicationRecord
+  include DtoMixin
+
+  dto_dependencies :cloud_subnets, :network_ports
+  dto_manager_ref :address, :cloud_subnet, :network_port
+  dto_attributes :address, :cloud_subnet, :network_port
+
   self.table_name = "cloud_subnets_network_ports"
 
   belongs_to :cloud_subnet
