06e18d80dfc9543c34311493aa840e826d23ba35
Alberto Bellotti <abellotti@users.noreply.github.com>
2016-09-29 16:35:35 -0400

    Merge pull request #11487 from jntullo/upload_pictures
    Create Picture API
    (cherry picked from commit ffd25b04c760c876cbe0f701ae0eaa5ff822e6ba)

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -13,10 +13,27 @@ describe Picture do
   end
 
   context "#extension" do
+    it "accepts only png, jpg, or svg" do
+      expect(subject.extension).to be_nil
+      subject.extension = "foo"
+
+      expect(subject.valid?).to be_falsey
+      expect(subject.errors.messages).to eq(:extension =>['must be a png, jpg, or svg'])
+
+      subject.extension = "png"
+      expect(subject.valid?).to be_truthy
+
+      subject.extension = "jpg"
+      expect(subject.valid?).to be_truthy
+
+      subject.extension = "svg"
+      expect(subject.valid?).to be_truthy
+    end
+
     it "on new record" do
       expect(subject.extension).to be_nil
-      ext = "foo"
-      subject.extension         = ext.dup
+      ext = "png"
+      subject.extension = ext.dup
       expect(subject.extension).to eq(ext)
 
       subject.save
@@ -33,8 +50,8 @@ describe Picture do
 
       subject.reload
       expect(subject.extension).to be_nil
-      ext = "foo"
-      subject.extension         = ext.dup
+      ext = "jpg"
+      subject.extension = ext.dup
       expect(subject.extension).to eq(ext)
 
       subject.save
