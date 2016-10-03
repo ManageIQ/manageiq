71397f3a798731c3302cbb39504d26830b866192
Martin Povolny <mpovolny@redhat.com>
2016-10-03 15:54:49 +0200

    Merge pull request #11054 from romanblanco/refactor_generic_button_with_disable_test
    Correct disabling method and refactored test for it's class
    (cherry picked from commit 28924370cb82529cc0148cc3b483f38dcf32cc26)

--- app/helpers/application_helper/button/generic_feature_button_with_disable.rb
+++ app/helpers/application_helper/button/generic_feature_button_with_disable.rb
@@ -2,19 +2,21 @@ class ApplicationHelper::Button::GenericFeatureButtonWithDisable < ApplicationHe
   needs :@record
 
   def calculate_properties
-    self[:enabled] = !(self[:title] = @error_message if disabled?)
+    super
+    self[:title] = @error_message if disabled?
   end
 
   def disabled?
-    begin
-      begin
-        @error_message = @record.try(:unsupported_reason, @feature)
-      rescue NoMethodError # TODO: remove with deleting AvailabilityMixin module
-        @error_message = @record.try(:is_available_now_error_message, @feature) if @error_message.nil?
-      end
-    rescue
-      @error_message = 'Feature is not supported.'
-      true
-    end
+    @error_message = if @record.feature_known?(@feature)
+                       unless @record.supports?(@feature)
+                         @record.unsupported_reason(@feature)
+                       end
+                     else
+                       # TODO: remove with deleting AvailabilityMixin module
+                       unless @record.is_available?(@feature)
+                         @record.is_available_now_error_message(@feature)
+                       end
+                     end
+    @error_message.present?
   end
 end
