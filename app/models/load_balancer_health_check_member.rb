07467b8badbd4067bc39f5972c1f0f63dda92af3
Ladislav Smola <lsmola@redhat.com>
2016-10-04 18:32:25 +0200

    Add missing mapping tables as DTOs
    Add missing mapping tables as DTOs

--- app/models/load_balancer_health_check_member.rb
+++ app/models/load_balancer_health_check_member.rb
@@ -1,4 +1,7 @@
 class LoadBalancerHealthCheckMember < ApplicationRecord
+  include DtoMixin
+  dto_manager_ref :load_balancer_health_check, :load_balancer_pool_member
+
   belongs_to :load_balancer_health_check
   belongs_to :load_balancer_pool_member
 end
