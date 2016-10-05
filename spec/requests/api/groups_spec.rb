93e72231005b5b1b5a88288d147ec308088ef0eb
Tim Wade <hello@timjwade.com>
2016-10-05 11:51:39 -0700

    Add some tests for tags subcollection actions
    Adds some tests for tags subcollections of users, templates and groups.

--- spec/requests/api/groups_spec.rb
+++ spec/requests/api/groups_spec.rb
@@ -259,4 +259,61 @@ describe "Groups API" do
       expect(MiqGroup.exists?(g2_id)).to be_falsey
     end
   end
+
+  describe "tags subcollection" do
+    it "can list a group's tags" do
+      group = FactoryGirl.create(:miq_group)
+      FactoryGirl.create(:classification_department_with_tags)
+      Classification.classify(group, "department", "finance")
+      api_basic_authorize
+
+      run_get("#{groups_url(group.id)}/tags")
+
+      expect(response.parsed_body).to include("subcount" => 1)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "can assign a tag to a group" do
+      group = FactoryGirl.create(:miq_group)
+      FactoryGirl.create(:classification_department_with_tags)
+      api_basic_authorize(subcollection_action_identifier(:groups, :tags, :assign))
+
+      run_post("#{groups_url(group.id)}/tags", :action => "assign", :category => "department", :name => "finance")
+
+      expected = {
+        "results" => [
+          a_hash_including(
+            "success"      => true,
+            "message"      => a_string_matching(/assigning tag/i),
+            "tag_category" => "department",
+            "tag_name"     => "finance"
+          )
+        ]
+      }
+      expect(response.parsed_body).to include(expected)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "can unassign a tag from a group" do
+      group = FactoryGirl.create(:miq_group)
+      FactoryGirl.create(:classification_department_with_tags)
+      Classification.classify(group, "department", "finance")
+      api_basic_authorize(subcollection_action_identifier(:groups, :tags, :unassign))
+
+      run_post("#{groups_url(group.id)}/tags", :action => "unassign", :category => "department", :name => "finance")
+
+      expected = {
+        "results" => [
+          a_hash_including(
+            "success"      => true,
+            "message"      => a_string_matching(/unassigning tag/i),
+            "tag_category" => "department",
+            "tag_name"     => "finance"
+          )
+        ]
+      }
+      expect(response.parsed_body).to include(expected)
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
