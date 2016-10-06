a4a8c114368e43fecb08878c6fd1559522e223f1
Alberto Bellotti <abellott@redhat.com>
2016-10-06 16:12:31 -0400

    Adding support for /api/requests creation and edits
    - Support for creates via POST /api/requests
    - Support for edits via POST /api/requests/:id - action "edit"

--- app/controllers/api/requests_controller.rb
+++ app/controllers/api/requests_controller.rb
@@ -1,5 +1,35 @@
 module Api
   class RequestsController < BaseController
+    include Subcollections::RequestTasks
+    include Subcollections::Tasks
+
+    def create_resource(type, _id, data)
+      assert_id_not_specified(data, type)
+
+      request_type = data.delete("request_type")
+      validate_request_type(request_type)
+
+      if data["src_id"].blank? && data["src_ids"].blank?
+        raise BadRequestError, "Must specify a resource src_id or src_ids"
+      end
+
+      requester = Hash(data.delete("requester"))
+      user = fetch_requester_user(requester)
+      auto_approve = fetch_auto_approve(requester)
+
+      request_type.constantize.create_request(data.symbolize_keys, user, auto_approve)
+    end
+
+    def edit_resource(type, id = nil, data = {})
+      raise BadRequestError, "Must specify a id for editing a #{type} resource" unless id
+      request_klass = collection_class(:requests)
+      request = resource_search(id, type, request_klass)
+      user = fetch_requester_user(Hash(data.delete("requester")))
+
+      request_klass.update_request(request, data, user)
+      request
+    end
+
     def find_requests(id)
       klass = collection_class(:requests)
       return klass.find(id) if @auth_user_obj.admin?
@@ -9,6 +39,32 @@ module Api
     def requests_search_conditions
       return {} if @auth_user_obj.admin?
       {:requester => @auth_user_obj}
+    end
+
+    private
+
+    def validate_request_type(request_type)
+      raise BadRequestError, "Must specify a request_type" if request_type.blank?
+      unless MiqRequest::REQUEST_TYPES.stringify_keys.keys.include?(request_type)
+        raise BadRequestError, "Invalid Request Type #{request_type} specified"
+      end
+    end
+
+    def fetch_requester_user(requester_hash)
+      user_name = requester_hash["user_name"]
+      return @auth_user_obj if user_name.blank?
+      user = User.lookup_by_identity(user_name)
+      raise BadRequestError, "Unknown requester user_name #{user_name} specified" unless user
+      user
+    end
+
+    def fetch_auto_approve(requester_hash)
+      auto_approve = requester_hash["auto_approve"]
+      case auto_approve
+      when TrueClass, "true" then true
+      when FalseClass, "false" then false
+      else raise BadRequestError, "Invalid requester auto_approve value #{auto_approve} specified"
+      end
     end
   end
 end
