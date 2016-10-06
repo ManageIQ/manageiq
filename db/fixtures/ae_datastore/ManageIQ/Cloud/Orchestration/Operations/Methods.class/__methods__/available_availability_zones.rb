d1a9e176a5a61878181b8edba8ae6188b7fdaf5a
Bill Wei <bilwei@redhat.com>
2016-10-06 14:11:31 -0400

    Convert available_availability_zones to the testable class format

--- db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_availability_zones.rb
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_availability_zones.rb
@@ -1,35 +1,47 @@
 #
-# Description: provide the dynamic list content from available resource groups
+# Description: provide the dynamic list content from available availability zones
 #
-az_list = {}
-service = $evm.root.attributes["service_template"] || $evm.root.attributes["service"]
-if service.respond_to?(:orchestration_manager) && service.orchestration_manager
-  service.orchestration_manager.availability_zones.each { |t| az_list[t.ems_ref] = t.name }
+class AvailableAvailabilityZones
+  def initialize(handle = $evm)
+    @handle = handle
+  end
+
+  def main
+    az_list = {}
+    service = @handle.root.attributes["service_template"] || @handle.root.attributes["service"]
+    if service.respond_to?(:orchestration_manager) && service.orchestration_manager
+      service.orchestration_manager.availability_zones.each { |t| az_list[t.ems_ref] = t.name }
+    end
+
+    default_value = nil
+    case az_list.length
+    when 0
+      az_list = {nil => "<none>"}
+    when 1
+      default_value = az_list.keys.first
+    else
+      az_list[nil] = "<select>"
+    end
+
+    dialog_field = @handle.object
+
+    # sort_by: value / description / none
+    dialog_field["sort_by"] = "description"
+
+    # sort_order: ascending / descending
+    dialog_field["sort_order"] = "ascending"
+
+    # data_type: string / integer
+    dialog_field["data_type"] = "string"
+
+    # required: true / false
+    dialog_field["required"] = "true"
+
+    dialog_field["values"] = az_list
+    dialog_field["default_value"] = default_value
+  end
 end
 
-default_value = nil
-case az_list.length
-when 0
-  az_list = {nil => "<none>"}
-when 1
-  default_value = az_list.keys.first
-else
-  az_list[nil] = "<select>"
+if __FILE__ == $PROGRAM_NAME
+  AvailableAvailabilityZones.new.main
 end
-
-dialog_field = $evm.object
-
-# sort_by: value / description / none
-dialog_field["sort_by"] = "description"
-
-# sort_order: ascending / descending
-dialog_field["sort_order"] = "ascending"
-
-# data_type: string / integer
-dialog_field["data_type"] = "string"
-
-# required: true / false
-dialog_field["required"] = "true"
-
-dialog_field["values"] = az_list
-dialog_field["default_value"] = default_value
--- spec/automation/unit/method_validation/available_availability_zones_spec.rb
+++ spec/automation/unit/method_validation/available_availability_zones_spec.rb
@@ -1,113 +1,100 @@
-describe "Available_Availability_Zones Method Validation" do
-  let(:user) { FactoryGirl.create(:user_with_group) }
+require Rails.root.join('db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_availability_zones').to_s
+
+describe AvailableAvailabilityZones do
   let(:default_desc_none) { "<none>" }
   let(:default_desc_multiple) { "<select>" }
-  before do
-    @ins = "/Cloud/Orchestration/Operations/Methods/Available_Availability_Zones"
+  let(:root_object) { Spec::Support::MiqAeMockObject.new(root_hash) }
+  let(:ae_service) do
+    Spec::Support::MiqAeMockService.new(root_object).tap do |service|
+      current_object = Spec::Support::MiqAeMockObject.new
+      current_object.parent = root_object
+      service.object = current_object
+    end
   end
 
   context "workspace has no service template" do
+    let(:root_hash) { {} }
+
     it "provides only the no availability zones info" do
-      ws = MiqAeEngine.instantiate(@ins.to_s, user)
-      expect(ws.root["values"]).to eq(nil => default_desc_none)
-      expect(ws.root["default_value"]).to eq(nil)
+      described_class.new(ae_service).main
+      expect(ae_service.object["values"]).to eq(nil => default_desc_none)
+      expect(ae_service.object["default_value"]).to eq(nil)
     end
   end
 
   context "workspace has service template other than orchestration" do
     let(:service_template) { FactoryGirl.create(:service_template) }
+    let(:root_hash) do
+      { 'service_template' => MiqAeMethodService::MiqAeServiceServiceTemplate.find(service_template.id) }
+    end
 
     it "provides only the no availability zones info" do
-      ws = MiqAeEngine.instantiate("#{@ins}?ServiceTemplate::service_template=#{service_template.id}", user)
-      expect(ws.root["values"]).to eq(nil => default_desc_none)
-      expect(ws.root["default_value"]).to eq(nil)
+      described_class.new(ae_service).main
+      expect(ae_service.object["values"]).to eq(nil => default_desc_none)
+      expect(ae_service.object["default_value"]).to eq(nil)
     end
   end
 
-  let(:ems_with_single_az) do
-    @az1 = FactoryGirl.create(:availability_zone, :ems_ref => "ref1")
-    FactoryGirl.create(:ems_vmware_cloud, :availability_zones => [@az1])
-  end
+  shared_examples_for "orchestration manager" do
+    context "with a single availability zone" do
+      let(:az1) { FactoryGirl.create(:availability_zone, :ems_ref => "ref1") }
+      let(:ems) { FactoryGirl.create(:ems_vmware_cloud, :availability_zones => [az1]) }
 
-  let(:ems) do
-    @az2 = FactoryGirl.create(:availability_zone, :ems_ref => "ref1")
-    @az3 = FactoryGirl.create(:availability_zone, :ems_ref => "ref2")
-    FactoryGirl.create(:ems_vmware_cloud, :availability_zones => [@az2, @az3])
-  end
-
-  context "workspace has orchestration service template" do
-    let(:service_template_single_az) do
-      FactoryGirl.create(:service_template_orchestration, :orchestration_manager => ems_with_single_az)
+      it "finds the single availability zone and populates the list" do
+        described_class.new(ae_service).main
+        expect(ae_service.object["values"]).to eq(az1.ems_ref => az1.name)
+        expect(ae_service.object["default_value"]).to eq(az1.ems_ref)
+      end
     end
 
+    context "with multiple availability zones" do
+      let(:az2) { FactoryGirl.create(:availability_zone, :ems_ref => "ref2") }
+      let(:az3) { FactoryGirl.create(:availability_zone, :ems_ref => "ref3") }
+      let(:ems) { FactoryGirl.create(:ems_vmware_cloud, :availability_zones => [az2, az3]) }
+
+      it "finds all the availability zones and populates the list" do
+        described_class.new(ae_service).main
+        expect(ae_service.object["values"]).to include(
+          nil         => default_desc_multiple,
+          az2.ems_ref => az2.name,
+          az3.ems_ref => az3.name
+        )
+        expect(ae_service.object["default_value"]).to eq(nil)
+      end
+    end
+
+    context "does not exists" do
+      let(:ems) { nil }
+
+      it "provides only default value to the availability zones list" do
+        described_class.new(ae_service).main
+        expect(ae_service.object["values"]).to eq(nil => default_desc_none)
+        expect(ae_service.object["default_value"]).to eq(nil)
+      end
+    end
+  end
+
+  describe "workspace has orchestration service template" do
     let(:service_template) do
       FactoryGirl.create(:service_template_orchestration, :orchestration_manager => ems)
     end
 
-    let(:service_template_no_ems) do
-      FactoryGirl.create(:service_template_orchestration)
+    let(:root_hash) do
+      { 'service_template' => MiqAeMethodService::MiqAeServiceServiceTemplate.find(service_template.id) }
     end
 
-    it "finds a single availabilty zone and populates the list" do
-      ws = MiqAeEngine.instantiate("#{@ins}?ServiceTemplate::service_template=#{service_template_single_az.id}", user)
-      expect(ws.root["values"]).to include(
-        @az1.ems_ref => @az1.name,
-      )
-      expect(ws.root["default_value"]).to eq(@az1.ems_ref)
-    end
-
-    it "finds all the availability zones and populates the list" do
-      ws = MiqAeEngine.instantiate("#{@ins}?ServiceTemplate::service_template=#{service_template.id}", user)
-      expect(ws.root["values"]).to include(
-        nil          => default_desc_multiple,
-        @az2.ems_ref => @az2.name,
-        @az3.ems_ref => @az3.name
-      )
-      expect(ws.root["default_value"]).to eq(nil)
-    end
-
-    it "provides only default value to the availability zones list if orchestration manager does not exist" do
-      ws = MiqAeEngine.instantiate("#{@ins}?ServiceTemplate::service_template=#{service_template_no_ems.id}", user)
-      expect(ws.root["values"]).to eq(nil => default_desc_none)
-      expect(ws.root["default_value"]).to eq(nil)
-    end
+    it_behaves_like "orchestration manager"
   end
 
   context "workspace has orchestration service" do
-    let(:service_single_az) do
-      FactoryGirl.create(:service_orchestration, :orchestration_manager => ems_with_single_az)
-    end
-
-    let(:service) do
+    let(:service_orchestration) do
       FactoryGirl.create(:service_orchestration, :orchestration_manager => ems)
     end
 
-    let(:service_no_ems) do
-      FactoryGirl.create(:service_orchestration)
+    let(:root_hash) do
+      { 'service' => MiqAeMethodService::MiqAeServiceServiceOrchestration.find(service_orchestration.id) }
     end
 
-    it "finds a single availabilty zone and populates the list" do
-      ws = MiqAeEngine.instantiate("#{@ins}?Service::service=#{service_single_az.id}", user)
-      expect(ws.root["values"]).to include(
-        @az1.ems_ref => @az1.name,
-      )
-      expect(ws.root["default_value"]).to eq(@az1.ems_ref)
-    end
-
-    it "finds all the availability zones and populates the list" do
-      ws = MiqAeEngine.instantiate("#{@ins}?Service::service=#{service.id}", user)
-      expect(ws.root["values"]).to include(
-        nil          => default_desc_multiple,
-        @az2.ems_ref => @az2.name,
-        @az3.ems_ref => @az3.name
-      )
-      expect(ws.root["default_value"]).to eq(nil)
-    end
-
-    it "provides only default value to the availability zones list if orchestration manager does not exist" do
-      ws = MiqAeEngine.instantiate("#{@ins}?Service::service=#{service_no_ems.id}", user)
-      expect(ws.root["values"]).to eq(nil => default_desc_none)
-      expect(ws.root["default_value"]).to eq(nil)
-    end
+    it_behaves_like "orchestration manager"
   end
 end
