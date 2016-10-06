faa77cd0a4453711572232fb55e975fdb18e04c9
Ladislav Smola <lsmola@redhat.com>
2016-10-06 18:34:11 +0200

    Save inventory uses recursive alghoritm
    Save inventory uses recursive alghoritm

--- app/models/manager_refresh/save_inventory.rb
+++ app/models/manager_refresh/save_inventory.rb
@@ -1,53 +1,14 @@
 module ManagerRefresh
   class SaveInventory
-    extend EmsRefresh::SaveInventoryHelper
-
     class << self
-      def save_inventory(ems, hashes)
+      def save_inventory(ems, dto_collections)
         _log.info("#{log_header(ems)} Saving EMS Inventory...Start")
-
-        hashes.each do |_key, dto_collection|
-          save_collection(dto_collection, [])
-        end
-
+        ManagerRefresh::SaveCollection::Recursive.save_collections(dto_collections)
         _log.info("#{log_header(ems)} Saving EMS Inventory...Complete")
         ems
       end
 
-      def save_collection(dto_collection, traversed_collections)
-        unless dto_collection.is_a? ::DtoCollection
-          raise "A ManagerRefresh::SaveInventory needs a DtoCollection object, it got: #{dto_collection.inspect}"
-        end
-
-        return if dto_collection.saved?
-
-        traversed_collections << dto_collection
-
-        if dto_collection.saveable?
-          save_dto_inventory(dto_collection)
-        else
-          dto_collection.dependencies.each do |dependency|
-            if traversed_collections.include? dependency
-              raise "Edge from #{dto_collection} to #{dependency} creates a cycle"
-            end
-            save_collection(dependency, traversed_collections)
-          end
-
-          save_dto_inventory(dto_collection)
-        end
-      end
-
       private
-      def save_dto_inventory(dto_collection)
-        save_dto_inventory_multi(dto_collection.parent.send(dto_collection.association),
-                                 dto_collection,
-                                 :use_association,
-                                 dto_collection.manager_ref)
-        store_ids_for_new_dto_records(dto_collection.parent.send(dto_collection.association),
-                                      dto_collection,
-                                      dto_collection.manager_ref)
-        dto_collection.saved = true
-      end
 
       def log_header(ems)
         "EMS: [#{ems.name}], id: [#{ems.id}]"
