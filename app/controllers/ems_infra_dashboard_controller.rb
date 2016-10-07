8a84458566804fcd2c1900ce5d95ebb6bccb57be
Harpreet Kataria <hkataria@redhat.com>
2016-10-07 11:02:56 -0400

    setting @layout for the new controller

--- app/controllers/ems_infra_dashboard_controller.rb
+++ app/controllers/ems_infra_dashboard_controller.rb
@@ -2,7 +2,9 @@ class EmsInfraDashboardController < ApplicationController
   extend ActiveSupport::Concern
 
   before_action :check_privileges
+  before_action :get_session_data
   after_action :cleanup_action
+  after_action :set_session_data
 
   def show
     if params[:id].nil?
@@ -18,5 +20,13 @@ class EmsInfraDashboardController < ApplicationController
 
   def collect_data(ems_id)
     EmsInfraDashboardService.new(ems_id, self).all_data
+  end
+
+  def get_session_data
+    @layout = "ems_infra_dashboard"
+  end
+
+  def set_session_data
+    session[:layout] = @layout
   end
 end
