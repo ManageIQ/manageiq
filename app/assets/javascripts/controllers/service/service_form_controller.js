4742824c93392890f80c4aad9bade772b974fd4e
Martin Hradil <mhradil@redhat.com>
2016-10-07 14:44:00 +0000

    service_form_controller - fix indent from 4/2 to 2

--- app/assets/javascripts/controllers/service/service_form_controller.js
+++ app/assets/javascripts/controllers/service/service_form_controller.js
@@ -1,46 +1,46 @@
 ManageIQ.angular.app.controller('serviceFormController', ['$http', '$scope', 'serviceFormId', 'miqService', function($http, $scope, serviceFormId, miqService) {
-    var init = function() {
-      $scope.serviceModel = {
-        name: '',
-        description: ''
-      };
-      $scope.formId    = serviceFormId;
-      $scope.afterGet  = false;
-      $scope.newRecord = false;
+  var init = function() {
+    $scope.serviceModel = {
+      name: '',
+      description: '',
+    };
+    $scope.formId    = serviceFormId;
+    $scope.afterGet  = false;
+    $scope.newRecord = false;
+    $scope.modelCopy = angular.copy( $scope.serviceModel );
+    $scope.model     = "serviceModel";
+    ManageIQ.angular.scope = $scope;
+
+    miqService.sparkleOn();
+    $http.get('/service/service_form_fields/' + serviceFormId).success(function(data) {
+      $scope.serviceModel.name        = data.name;
+      $scope.serviceModel.description = data.description;
+
+      $scope.afterGet = true;
       $scope.modelCopy = angular.copy( $scope.serviceModel );
-      $scope.model     = "serviceModel";
-      ManageIQ.angular.scope = $scope;
+      miqService.sparkleOff();
+    });
+  };
 
-      miqService.sparkleOn();
-      $http.get('/service/service_form_fields/' + serviceFormId).success(function(data) {
-        $scope.serviceModel.name        = data.name;
-        $scope.serviceModel.description = data.description;
+  var serviceEditButtonClicked = function(buttonName, data) {
+    var url = '/service/service_edit/' + serviceFormId + '?button=' + buttonName;
+    miqService.miqAjaxButton(url, data);
+  };
 
-        $scope.afterGet = true;
-        $scope.modelCopy = angular.copy( $scope.serviceModel );
-        miqService.sparkleOff();
-      });
-    };
+  $scope.cancelClicked = function() {
+    serviceEditButtonClicked('cancel');
+  };
 
-    var serviceEditButtonClicked = function(buttonName, data) {
-      var url = '/service/service_edit/' + serviceFormId + '?button=' + buttonName;
-      miqService.miqAjaxButton(url, data);
-    };
+  $scope.resetClicked = function() {
+    $scope.serviceModel = angular.copy( $scope.modelCopy );
+    $scope.angularForm.$setUntouched(true);
+    $scope.angularForm.$setPristine(true);
+    miqService.miqFlash("warn", __("All changes have been reset"));
+  };
 
-    $scope.cancelClicked = function() {
-      serviceEditButtonClicked('cancel');
-    };
+  $scope.saveClicked = function() {
+    serviceEditButtonClicked('save', $scope.serviceModel);
+  };
 
-    $scope.resetClicked = function() {
-      $scope.serviceModel = angular.copy( $scope.modelCopy );
-      $scope.angularForm.$setUntouched(true);
-      $scope.angularForm.$setPristine(true);
-      miqService.miqFlash("warn", __("All changes have been reset"));
-    };
-
-    $scope.saveClicked = function() {
-      serviceEditButtonClicked('save', $scope.serviceModel);
-    };
-
-    init();
+  init();
 }]);
