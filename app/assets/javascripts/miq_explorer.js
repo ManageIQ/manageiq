42d769789de93a6155b712f599c1a8d36b83686e
Martin Povolny <mpovolny@redhat.com>
2016-10-08 08:21:12 +0200

    Merge pull request #11714 from skateman/bz-1381643
    Re-initialize accordions when replacing the right cell
    (cherry picked from commit 05675640e8b42649e78aa4b2e020b13f8a2cf155)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1381643
    https://bugzilla.redhat.com/show_bug.cgi?id=1381638

--- app/assets/javascripts/miq_explorer.js
+++ app/assets/javascripts/miq_explorer.js
@@ -250,6 +250,7 @@ ManageIQ.explorer.processReplaceRightCell = function(data) {
   }
 
   miqInitMainContent();
+  miqInitAccordions();
 
   if (data.hideModal) { $('#quicksearchbox').modal('hide'); }
   if (data.initAccords) { miqInitAccordions(); }
