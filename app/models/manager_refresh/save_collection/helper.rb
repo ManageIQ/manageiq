292a010b19d8626199e026c82d41fdc38a6e9333
Ladislav Smola <lsmola@redhat.com>
2016-10-10 20:22:25 +0200

    Helper for logging AR object
    Helper for logging AR object

--- app/models/manager_refresh/save_collection/helper.rb
+++ app/models/manager_refresh/save_collection/helper.rb
@@ -10,5 +10,23 @@ module ManagerRefresh::SaveCollection
                                     dto_collection.manager_ref)
       dto_collection.saved = true
     end
+
+    def log_format_deletes(deletes)
+      ret = deletes.collect do |d|
+        s = "id: [#{d.id}]"
+
+        [:name, :product_name, :device_name].each do |k|
+          next unless d.respond_to?(k)
+          v = d.send(k)
+          next if v.nil?
+          s << " #{k}: [#{v}]"
+          break
+        end
+
+        s
+      end
+
+      ret.join(", ")
+    end
   end
 end
