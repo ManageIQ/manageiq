02031a2c468317ba16ff7ad4e007e36df5fcb214
Mike Thompson <mithomps@redhat.com>
2016-10-12 15:08:07 -0700

    Refactor Fat controller to services. Isolating business logic to services and page control to controller.

--- app/assets/javascripts/controllers/middleware_datasource/middleware_datasource_controller.js
+++ app/assets/javascripts/controllers/middleware_datasource/middleware_datasource_controller.js
@@ -2,32 +2,16 @@ ManageIQ.angular.app.controller('mwAddDataSourceController', MwAddDataSourceCtrl
 
 DATASOURCE_EVENT = 'mwAddDatasourceEvent';
 
-MwAddDataSourceCtrl.$inject = ['$scope', '$rootScope', '$http', '$q', 'miqService'];
+MwAddDataSourceCtrl.$inject = ['$scope', '$rootScope', 'miqService', 'mwAddDataSourceService'];
 
-function MwAddDataSourceCtrl($scope, $rootScope, $http, $q, miqService) {
+function MwAddDataSourceCtrl($scope, $rootScope, miqService, mwAddDataSourceService) {
 
   $scope.dsModel = {};
   $scope.dsModel.step = 'CHOOSE_DS';
 
   $scope.chooseDsModel = {};
   $scope.chooseDsModel.selectedDatasource = undefined;
-  $scope.chooseDsModel.datasources = [
-    {id: 'H2', label: 'H2', name: 'H2DS', jndiName: 'java:/H2DS',
-      driverName: 'h2', driverModuleName: 'com.h2database.h2', driverClass: 'org.h2.Driver'},
-    {id : 'POSTGRES', label: 'Postgres', name: 'PostgresDS', jndiName: 'java:/PostgresDS',
-      driverName: 'postresql', driverModuleName: 'org.postgresql', driverClass: 'org.postgresql.Driver'},
-    {id: 'MSSQL', label: 'Microsoft SQL Server', name: 'MSSQLDS', jndiName: 'java:/MSSQLDS',
-      driverName: 'sqlserver', driverModuleName: 'com.microsoft',
-      driverClass: 'com.microsoft.sqlserver.jdbc.SQLServerDriver'},
-    {id: 'ORACLE', label: 'Oracle', name: 'OracleDS', jndiName: 'java:/OracleDS',
-      driverName: 'oracle', driverModuleName: 'com.oracle', driverClass: 'oracle.jdbc.driver.OracleDriver'},
-    {id: 'DB2', label: 'IBM DB2', name: 'DB2DS', jndiName: 'java:/DB2DS',
-      driverName: 'ibmdb2', driverModuleName: 'com.ibm', driverClass: 'COM.ibm.db2.jdbc.app.DB2Driver'},
-    {id: 'SYBASE', label: 'Sybase', name: 'SybaseDS', jndiName: 'java:/SybaseDB',
-      driverName: 'sybase', driverModuleName: 'com.sybase', driverClass: 'com.sybase.jdbc.SybDriver'},
-    {id: 'MYSQL', label: 'MySql', name: 'MySqlDS', jndiName: 'java:/MySqlDS',
-      driverName: 'mysql', driverModuleName: 'com.mysql', driverClass: 'com.mysql.jdbc.Driver'}
-    ];
+  $scope.chooseDsModel.datasources = undefined;
 
   $scope.step1DsModel = {};
   $scope.step1DsModel.datasourceName = '';
@@ -45,8 +29,13 @@ function MwAddDataSourceCtrl($scope, $rootScope, $http, $q, miqService) {
   $scope.step3DsModel.password = '';
   $scope.step3DsModel.securityDomain = '';
 
+  this.$onInit = function () {
+    $scope.chooseDsModel.datasources = mwAddDataSourceService.getDatasources();
+  };
+
   $scope.$on(DATASOURCE_EVENT, function(event, payload) {
-    submitJson(payload);
+    mwAddDataSourceService.sendAddDatasource(payload);
+    angular.element("#modal_ds_div").modal('hide');
     miqService.sparkleOff();
   });
 
@@ -73,7 +62,7 @@ function MwAddDataSourceCtrl($scope, $rootScope, $http, $q, miqService) {
   $scope.addDatasourceStep2Next = function() {
     var dsSelection = $scope.chooseDsModel.selectedDatasource;
     $scope.dsModel.step = 'STEP3';
-    $scope.step3DsModel.connectionUrl = determineConnectionUrl(dsSelection);
+    $scope.step3DsModel.connectionUrl = mwAddDataSourceService.determineConnectionUrl(dsSelection);
   };
 
   $scope.addDatasourceStep2Back = function() {
@@ -109,33 +98,6 @@ function MwAddDataSourceCtrl($scope, $rootScope, $http, $q, miqService) {
     $scope.step3DsModel.securityDomain = '';
   };
 
-  var determineConnectionUrl = function (dsSelection) {
-    var datasource = dsSelection.id;
-    var driverName = dsSelection.driverName;
-    var PREFIX = 'jdbc:';
-    var driverUrl = '';
-
-    if(datasource == 'POSTGRES'){
-     driverUrl = PREFIX + driverName + '://localhost:5432/postgresdb';
-    }else if(datasource == 'H2'){
-      driverUrl = PREFIX + driverName + ':mem:test;DB_CLOSE_DELAY=-1';
-    }else if(datasource == 'MSSQL'){
-      driverUrl = PREFIX + driverName + '://localhost:1433;DatabaseName=MyDatabase';
-    }else if(datasource == 'ORACLE'){
-      driverUrl = PREFIX + driverName + ':thin:@localhost:1521:orcalesid';
-    }else if(datasource == 'DB2'){
-      driverUrl = PREFIX + driverName + '://postgresdb';
-    }else if(datasource == 'Sybase'){
-      driverUrl = PREFIX + driverName + ':Tds:localhost:5000/mydatabase?JCONNECT_VERSION=6';
-    }else if(datasource == 'MYSQL'){
-      driverUrl = PREFIX + driverName + '://localhost:3306/mysqldb';
-    }else {
-      driverUrl = 'error - invalid datasource type: ' + datasource;
-      console.warn(driverUrl);
-    }
-    return driverUrl;
-  };
-
   var getPayload = function () {
     return {
       'id': angular.element('#server_id').val(),
@@ -150,30 +112,4 @@ function MwAddDataSourceCtrl($scope, $rootScope, $http, $q, miqService) {
       'securityDomain': $scope.step3DsModel.securityDomain
     };
   };
-
-  var submitJson = function submitJson(payload) {
-    var errorMsg = _('Error running add_datasource on this server.');
-    var deferred = $q.defer();
-
-    $http.post('/middleware_server/add_datasource', angular.toJson(payload))
-      .then(
-        function (response) { // success
-          var data = response.data;
-
-          if (data.status === 'ok') {
-            deferred.resolve(data.msg);
-          } else {
-            deferred.reject(data.msg);
-          }
-        })
-      .catch(function () {
-        deferred.reject(errorMsg);
-      })
-      .finally(function () {
-        angular.element("#modal_ds_div").modal('hide');
-        // we should already be resolved and promises can only fire once
-        deferred.resolve(data.msg);
-      });
-    return deferred.promise;
-  }
 }
