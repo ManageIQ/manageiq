194922478277df0bef9321ccb7856b2daea2788b
Dan Clarizio <dclarizi@redhat.com>
2016-10-12 01:55:44 -0700

    Merge pull request #11391 from dkorn/fix_default_port
    Allow to add a containers provider with a port other than 8443
    (cherry picked from commit ed274e96e00d62545ebd2630cf124c79937665f1)

--- spec/requests/api/providers_spec.rb
+++ spec/requests/api/providers_spec.rb
@@ -58,7 +58,7 @@ describe "Providers API" do
       "endpoint"       => {
         "role"     => "default",
         "hostname" => "sample_openshift_multi_end_point.provider.com",
-        "port"     => "8443"
+        "port"     => 8444
       },
       "authentication" => {
         "role"     => "bearer",
@@ -423,6 +423,10 @@ describe "Providers API" do
         connection["endpoint"]["hostname"]
       end
 
+      def port(connection)
+        connection["endpoint"]["port"]
+      end
+
       def token(connection)
         connection["authentication"]["auth_key"]
       end
@@ -444,6 +448,7 @@ describe "Providers API" do
 
       expect(provider.hostname).to eq(hostname(default_connection))
       expect(provider.authentication_token).to eq(token(default_connection))
+      expect(provider.port).to eq(port(default_connection))
       expect(provider.connection_configurations.hawkular.endpoint.hostname).to eq(hostname(hawkular_connection))
       expect(provider.connection_configurations.hawkular.authentication.auth_key).to eq(token(hawkular_connection))
     end
