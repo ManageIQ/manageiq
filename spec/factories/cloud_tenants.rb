6397890bda89eac84262071ac08be0d5d81a4a4e
lpichler <lpichler@redhat.com>
2016-10-13 10:46:29 +0200

    Populate also description

--- spec/factories/cloud_tenants.rb
+++ spec/factories/cloud_tenants.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :cloud_tenant do
-    sequence(:name)    { |n| "cloud_tenant_#{seq_padded_for_sorting(n)}" }
-    sequence(:ems_ref) { |n| "ems_ref_#{seq_padded_for_sorting(n)}" }
+    sequence(:name)         { |n| "cloud_tenant_#{seq_padded_for_sorting(n)}" }
+    sequence(:description)  { |n| "cloud_tenant_description_#{seq_padded_for_sorting(n)}" }
+    sequence(:ems_ref)      { |n| "ems_ref_#{seq_padded_for_sorting(n)}" }
   end
 
   factory :cloud_tenant_openstack, :class => "ManageIQ::Providers::Openstack::CloudManager::CloudTenant", :parent => :cloud_tenant do
