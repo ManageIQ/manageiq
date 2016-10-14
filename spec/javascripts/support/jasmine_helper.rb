445bad97c944fbbbfbb5e2d23a4d951ec18d3e23
Chris Arcand <chris@chrisarcand.com>
2016-10-14 14:36:29 -0500

    Don't install PhantomJS on CI from BitBucket
    The gemified version of Jasmine uses the gemified version of PhantomJS
    which auto-installs it if it can't find your installation in ~/.phantomjs
    Travis already has a version of PhantomJS installed in a different
    location, so the gem will auto-install even if it's pointless.  Also,
    gemified PhantomJS hardcodes install URLs from BitBucket which times out
    and causes failed builds.
    
    TLDR: Don't install auto-install PhantomJS on CI. In Travis we trust.

--- spec/javascripts/support/jasmine_helper.rb
+++ spec/javascripts/support/jasmine_helper.rb
@@ -2,13 +2,14 @@
 # You can remove it if you don't need it.
 # This file is loaded *after* jasmine.yml is interpreted.
 #
-# Example: using a different boot file.
-# Jasmine.configure do |config|
-#    config.boot_dir = '/absolute/path/to/boot_dir'
-#    config.boot_files = lambda { ['/absolute/path/to/boot_dir/file.js'] }
-# end
-#
-# Example: prevent PhantomJS auto install, uses PhantomJS already on your path.
-# Jasmine.configure do |config|
-#    config.prevent_phantom_js_auto_install = true
-# end
+Jasmine.configure do |config|
+  # The gemified version of Jasmine uses the gemified version of PhantomJS
+  # which auto-installs it if it can't find your installation in ~/.phantomjs
+  # Travis already has a version of PhantomJS installed in a different
+  # location, so the gem will auto-install even if it's pointless.  Also,
+  # gemified PhantomJS hardcodes install URLs from BitBucket which times out
+  # and causes failed builds.
+  #
+  # TLDR: Don't install auto-install PhantomJS on CI. In Travis we trust.
+  config.prevent_phantom_js_auto_install = true if ENV['CI']
+end
