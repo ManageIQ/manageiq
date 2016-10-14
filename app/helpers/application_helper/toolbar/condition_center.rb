115265ed318c1576a9ffe20976686574707c8d37
Jozef Zigmund <jzigmund@redhat.com>
2016-10-14 16:09:45 +0200

    Refactoring toolbar buttons for Condition

--- app/helpers/application_helper/toolbar/condition_center.rb
+++ app/helpers/application_helper/toolbar/condition_center.rb
@@ -18,16 +18,18 @@ class ApplicationHelper::Toolbar::ConditionCenter < ApplicationHelper::Toolbar::
           'fa fa-files-o fa-lg',
           t = N_('Copy this Condition to a new Condition'),
           t,
-          :url_parms => "?copy=true"),
+          :url_parms => "?copy=true",
+          :klass     => ApplicationHelper::Button::Condition),
         button(
           :condition_policy_copy,
           'fa fa-files-o fa-lg',
           t = proc do
             _('Copy this Condition to a new Condition assigned to Policy [%{condition_policy_description}]') %
-              {:condition_policy_description => @condition_policy.description}
+              {:condition_policy_description => @condition_policy.try(:description)}
           end,
           t,
-          :url_parms => "?copy=true"),
+          :url_parms => "?copy=true",
+          :klass     => ApplicationHelper::Button::ConditionPolicy),
         button(
           :condition_delete,
           'pficon pficon-delete fa-lg',
@@ -36,19 +38,23 @@ class ApplicationHelper::Toolbar::ConditionCenter < ApplicationHelper::Toolbar::
           end,
           t,
           :url_parms => "main_div",
-          :klass     => ApplicationHelper::Button::ReadOnly,
+          :klass     => ApplicationHelper::Button::Condition,
           :confirm   => proc { _("Are you sure you want to delete this %{condition_type} Condition?") % {:condition_type => ui_lookup(:model => @condition.towhat)} }),
         button(
           :condition_remove,
           'pficon pficon-delete fa-lg',
           t = proc do
-            _('Remove this Condition from Policy [%{condition_policy_description}]') % {:condition_policy_description => @condition_policy.description}
+            _('Remove this Condition from Policy [%{condition_policy_description}]') %
+            {:condition_policy_description => @condition_policy.try(:description)}
           end,
           t,
-          :url_parms => "?policy_id=\#{@condition_policy.id}",
-          :klass     => ApplicationHelper::Button::ReadOnly,
-          :confirm   => proc { _("Are you sure you want to remove this Condition from Policy [%{condition_policy_description}]?") % {:condition_policy_description => @condition_policy.description} }),
-      ]
-    ),
-  ])
+          :url_parms => "?policy_id=\#{@condition_policy.try(:id)}",
+          :klass     => ApplicationHelper::Button::ConditionPolicy,
+          :confirm   => proc do
+                          _("Are you sure you want to remove this Condition from Policy [%{condition_policy_description}]?") %
+                          {:condition_policy_description => @condition_policy.try(:description)}
+                        end
+        ),
+      ]),
+    ])
 end
