38b6a61946066bf53f56227bd529ac17aac0a07f
Harman Singh <harman28@gmail.com>
2016-10-15 11:58:34 +0530

    Fixed indentation

--- lib/extensions/string.rb
+++ lib/extensions/string.rb
@@ -1,11 +1,11 @@
 class String
-	def to_boolean
-		if [true, 1, "1", "t", "T", "true", "TRUE", "on", "ON"].include? self
-			return true
-		elsif [false, 0, "0", "f", "F", "false", "FALSE", "off", "OFF"].include? self
-			return false
-		else
-			return nil
-		end
-	end
+  def to_boolean
+    if [true, 1, "1", "t", "T", "true", "TRUE", "on", "ON"].include? self
+      return true
+    elsif [false, 0, "0", "f", "F", "false", "FALSE", "off", "OFF"].include? self
+      return false
+    else
+      return nil
+    end
+  end
 end
