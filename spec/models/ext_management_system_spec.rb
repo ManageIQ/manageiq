f4efdb438ee28b8bfd642d54ee02c5fa5ae80e27
Julian Cheal <jcheal@redhat.com>
2016-10-15 16:09:28 -0400

    Added Lenovo XClarity to required test files
    - ext_management_systems types & descriptions hash
    - permissions.tmpl.yml

--- spec/models/ext_management_system_spec.rb
+++ spec/models/ext_management_system_spec.rb
@@ -37,7 +37,8 @@ describe ExtManagementSystem do
       "openstack"                   => "OpenStack",
       "openstack_infra"             => "OpenStack Platform Director",
       "openstack_network"           => "OpenStack Network",
-      "physical_infra_manager"      => "PhysicalInfraManager", # TODO: (julian) remove once we have a physical_infra_manager implementation
+      "physical_infra_manager"      => "PhysicalInfraManager", # TODO: (Julian) Do we still need this? Test fails without it :(
+      "lenovo_ph_infra"             => "Lenovo XClarity",
       "nuage_network"               => "Nuage Network Manager",
       "rhevm"                       => "Red Hat Virtualization Manager",
       "scvmm"                       => "Microsoft System Center VMM",
