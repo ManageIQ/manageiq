db13325fa52ce62e5e997acf3deb1ab77afb7b48
Keenan Brock <kbrock@redhat.com>
2016-10-18 22:02:30 -0400

    aggregation mixin - remove overridden methods
    all vms and hosts relations are overridden. so don't define them

--- app/models/mixins/aggregation_mixin.rb
+++ app/models/mixins/aggregation_mixin.rb
@@ -40,38 +40,6 @@ module AggregationMixin
 
   # Default implementations which can be overridden with something more optimized
 
-  def all_vms_and_templates
-    descendants(:of_type => 'VmOrTemplate')
-  end
-
-  def all_vms
-    all_vms_and_templates.select { |v| v.kind_of?(Vm) }
-  end
-
-  def all_miq_templates
-    all_vms_and_templates.select { |v| v.kind_of?(MiqTemplate) }
-  end
-
-  def all_vm_or_template_ids
-    Relationship.resource_pairs_to_ids(descendant_ids(:of_type => 'VmOrTemplate'))
-  end
-
-  def all_vm_ids
-    all_vms.collect(&:id)
-  end
-
-  def all_miq_template_ids
-    all_miq_templates.collect(&:id)
-  end
-
-  def all_hosts
-    descendants(:of_type => 'Host')
-  end
-
-  def all_host_ids
-    Relationship.resource_pairs_to_ids(descendant_ids(:of_type => 'Host'))
-  end
-
   def all_storages
     hosts = all_hosts
     MiqPreloader.preload(hosts, :storages)
