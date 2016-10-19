ee6e26e08329facc3e97b4444dd555b0eca7161a
Greg McCullough <gmccullo@redhat.com>
2016-10-19 17:03:14 +0200

    Merge pull request #12006 from mkanoor/route_import_to_git_owner
    Route the import call to the correct server with the git owner role
    (cherry picked from commit 32a6bff8ae2dda47ae408fd92b68897704da8fb5)

--- app/services/git_based_domain_import_service.rb
+++ app/services/git_based_domain_import_service.rb
@@ -1,5 +1,5 @@
 class GitBasedDomainImportService
-  def import(git_repo_id, branch_or_tag, tenant_id)
+  def queue_import(git_repo_id, branch_or_tag, tenant_id)
     git_repo = GitRepository.find_by(:id => git_repo_id)
 
     ref_type = if git_repo.git_branches.any? { |git_branch| git_branch.name == branch_or_tag }
@@ -8,13 +8,34 @@ class GitBasedDomainImportService
                  "tag"
                end
 
-    options = {
+    import_options = {
       "git_repository_id" => git_repo.id,
       "ref"               => branch_or_tag,
       "ref_type"          => ref_type,
       "tenant_id"         => tenant_id
     }
-    domain = MiqAeDomain.import_git_repo(options)
+
+    task_options = {
+      :action => "Import git repository",
+      :userid => User.current_user.userid
+    }
+
+    queue_options = {
+      :class_name  => "MiqAeDomain",
+      :method_name => "import_git_repo",
+      :role        => "git_owner",
+      :args        => [import_options]
+    }
+
+    MiqTask.generic_action_with_callback(task_options, queue_options)
+  end
+
+  def import(git_repo_id, branch_or_tag, tenant_id)
+    task_id = queue_import(git_repo_id, branch_or_tag, tenant_id)
+    task = MiqTask.wait_for_taskid(task_id)
+
+    domain = task.task_results
+    raise MiqException::Error, "Domain object not available from task results" unless domain.kind_of?(MiqAeDomain)
     domain.update_attribute(:enabled, true)
   end
 
--- spec/services/git_based_domain_import_service_spec.rb
+++ spec/services/git_based_domain_import_service_spec.rb
@@ -1,66 +1,104 @@
 describe GitBasedDomainImportService do
-  describe "#import" do
+  shared_context "import setup" do
     let(:git_repo) { double("GitRepository", :git_branches => git_branches, :id => 123) }
-    let(:domain) { double("MiqAeDomain") }
+    let(:user) { double("User", :userid => userid, :id => 123) }
+    let(:domain) { FactoryGirl.build(:miq_ae_domain) }
+    let(:userid) { "fred" }
+    let(:task) { double("MiqTask", :id => 123) }
+    let(:ref_name) { 'the_branch_name' }
+    let(:ref_type) { 'branch' }
+    let(:task_options) { {:action => "Import git repository", :userid => userid} }
+    let(:queue_options) do
+      {
+        :class_name  => "MiqAeDomain",
+        :method_name => "import_git_repo",
+        :role        => "git_owner",
+        :args        => [import_options]
+      }
+    end
+    let(:import_options) do
+      {
+        "git_repository_id" => git_repo.id,
+        "ref"               => ref_name,
+        "ref_type"          => ref_type,
+        "tenant_id"         => 321
+      }
+    end
+  end
 
+  describe "#import" do
+    include_context "import setup"
     before do
-      allow(GitRepository).to receive(:find_by).with(:id => 123).and_return(git_repo)
+      allow(GitRepository).to receive(:find_by).with(:id => git_repo.id).and_return(git_repo)
       allow(domain).to receive(:update_attribute).with(:enabled, true)
+      allow(MiqTask).to receive(:wait_for_taskid).with(task.id).and_return(task)
+      allow(User).to receive(:current_user).and_return(user)
     end
 
     context "when git branches that match the given name exist" do
-      let(:git_branches) { [double("GitBranch", :name => "the_branch_name")] }
+      let(:git_branches) { [double("GitBranch", :name => ref_name)] }
 
-      before do
-        allow(MiqAeDomain).to receive(:import_git_repo).with(
-          "git_repository_id" => 123,
-          "ref"               => "the_branch_name",
-          "ref_type"          => "branch",
-          "tenant_id"         => 321
-        ).and_return(domain)
-      end
-
-      it "calls 'import_git_repo' with the correct options" do
-        expect(MiqAeDomain).to receive(:import_git_repo).with(
-          "git_repository_id" => 123,
-          "ref"               => "the_branch_name",
-          "ref_type"          => "branch",
-          "tenant_id"         => 321
-        )
-        subject.import(123, "the_branch_name", 321)
-      end
-
-      it "updates the enabled attribute on the domain to true" do
+      it "calls 'import' with the correct options" do
+        allow(task).to receive(:task_results).and_return(domain)
+        expect(MiqTask).to receive(:generic_action_with_callback).with(task_options, queue_options).and_return(task.id)
         expect(domain).to receive(:update_attribute).with(:enabled, true)
-        subject.import(123, "the_branch_name", 321)
+
+        subject.import(git_repo.id, ref_name, 321)
       end
     end
 
     context "when git branches that match the given name do not exist" do
       let(:git_branches) { [] }
+      let(:ref_name) { "the_tag_name" }
+      let(:ref_type) { "tag" }
 
-      before do
-        allow(MiqAeDomain).to receive(:import_git_repo).with(
-          "git_repository_id" => 123,
-          "ref"               => "the_branch_name",
-          "ref_type"          => "tag",
-          "tenant_id"         => 321
-        ).and_return(domain)
-      end
-
-      it "calls 'import_git_repo' with the correct options" do
-        expect(MiqAeDomain).to receive(:import_git_repo).with(
-          "git_repository_id" => 123,
-          "ref"               => "the_branch_name",
-          "ref_type"          => "tag",
-          "tenant_id"         => 321
-        )
-        subject.import(123, "the_branch_name", 321)
-      end
-
-      it "updates the enabled attribute on the domain to true" do
+      it "calls 'import' with the correct options" do
+        allow(task).to receive(:task_results).and_return(domain)
+        expect(MiqTask).to receive(:generic_action_with_callback).with(task_options, queue_options).and_return(task.id)
         expect(domain).to receive(:update_attribute).with(:enabled, true)
-        subject.import(123, "the_branch_name", 321)
+
+        subject.import(git_repo.id, ref_name, 321)
+      end
+    end
+
+    context "when import fails and the task result is nil" do
+      let(:git_branches) { [double("GitBranch", :name => ref_name)] }
+
+      it "raises exception" do
+        allow(task).to receive(:task_results).and_return(nil)
+        expect(MiqTask).to receive(:generic_action_with_callback).with(task_options, queue_options).and_return(task.id)
+
+        expect { subject.import(git_repo.id, ref_name, 321) }.to raise_exception(MiqException::Error)
+      end
+    end
+  end
+
+  describe "#queue_import" do
+    include_context "import setup"
+    before do
+      allow(GitRepository).to receive(:find_by).with(:id => git_repo.id).and_return(git_repo)
+      allow(User).to receive(:current_user).and_return(user)
+    end
+
+    context "when git branches that match the given name exist" do
+      let(:git_branches) { [double("GitBranch", :name => ref_name)] }
+
+      it "calls 'queue_import' with the correct options" do
+        expect(MiqTask).to receive(:generic_action_with_callback).with(task_options, queue_options).and_return(task.id)
+
+        expect(subject.queue_import(git_repo.id, ref_name, 321)).to eq(task.id)
+      end
+    end
+
+    context "when git branches that match the given name do not exist" do
+      let(:git_branches) { [] }
+      let(:ref_name) { "the_tag_name" }
+      let(:ref_type) { "tag" }
+
+      it "calls 'queue_import' with the correct options" do
+        expect(MiqTask).to receive(:generic_action_with_callback).with(task_options, queue_options).and_return(task.id)
+
+        expect(subject.queue_import(git_repo.id, ref_name, 321)).to eq(task.id)
       end
     end
   end
