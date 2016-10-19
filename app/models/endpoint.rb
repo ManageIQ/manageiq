b019e537139ae24db216acd40eb6cbaa7f2ab2fd
Brandon Dunne <bdunne@redhat.com>
2016-10-19 17:07:23 -0400

    Add uniqueness validation on Endpoint#url
    https://bugzilla.redhat.com/show_bug.cgi?id=1382627

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -4,6 +4,7 @@ class Endpoint < ApplicationRecord
   default_value_for :verify_ssl, OpenSSL::SSL::VERIFY_PEER
   validates :verify_ssl, :inclusion => {:in => [OpenSSL::SSL::VERIFY_NONE, OpenSSL::SSL::VERIFY_PEER]}
   validates :port, :numericality => {:only_integer => true, :allow_nil => true, :greater_than => 0}
+  validates :url, :uniqueness => true, :if => :url
 
   def verify_ssl=(val)
     val = resolve_verify_ssl_value(val)
