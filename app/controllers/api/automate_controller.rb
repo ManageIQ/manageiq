8f79f5650375b75487fde64a59cbd9b8f746e134
Alberto Bellotti <abellott@redhat.com>
2016-10-20 11:50:34 -0400

    Enhanced automate refresh_from_source to be async
    - Leverage new queue_refresh_and_import method from Automate
    - Allowed refreshing by domain name
    - Updated rspecs

--- app/controllers/api/automate_controller.rb
+++ app/controllers/api/automate_controller.rb
@@ -13,21 +13,50 @@ module Api
     end
 
     def refresh_from_source_resource(type, id = nil, data = nil)
+      raise BadRequestError, "Must specify an id for refreshing a #{type} resource from source" unless id
+
       api_action(type, id) do |klass|
-        unless GitBasedDomainImportService.available?
-          return action_result(false, 'Please enable the git owner role in order to import git repositories')
-        end
         domain = resource_search(id, type, klass)
-        unless domain.git_repository
-          return action_result(false, "Domain [id=#{id}] did not originate from git repository")
+        api_log_info("Refreshing #{automate_domain_ident(domain)}")
+
+        begin
+          unless GitBasedDomainImportService.available?
+            raise "Git owner role is not enabled to be able to import git repositories"
+          end
+          raise "#{automate_domain_ident(domain)} did not originate from git repository" unless domain.git_repository
+          ref = data["ref"] || domain.ref
+          ref_type = data["ref_type"] || domain.ref_type
+
+          description = "Refreshing #{automate_domain_ident(domain)} from git repository"
+          task_id = GitBasedDomainImportService.new.queue_refresh_and_import(domain.git_repository.url,
+                                                                             ref,
+                                                                             ref_type,
+                                                                             current_tenant.id)
+          action_result(true, description, :task_id => task_id)
+        rescue => err
+          action_result(false, err.to_s)
         end
-        ref = data[:ref] || domain.ref
-        GitBasedDomainImportService.new.import(domain.git_repository.id, ref, current_tenant.id)
-        action_result(true, 'Domain refreshed from git repository')
       end
     end
 
     private
+
+    def automate_domain_ident(domain)
+      "Automate Domain id:#{domain.id} name:'#{domain.name}'"
+    end
+
+    def resource_search(id, type, klass)
+      if cid?(id)
+        super
+      else
+        begin
+          domain = MiqAeBrowser.new(@auth_user_obj).search(id, :depth => 0).first
+        rescue => err
+          raise NotFoundError, err.to_s
+        end
+        super(domain.id, type, klass)
+      end
+    end
 
     def ae_search_options
       # For /api/automate (discovering domains, scope is 1 if unspecified)
