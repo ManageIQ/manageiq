1fc4abf86b4abced5e40dee290759f284f967fc6
Eric Winchell <ewinchel@redhat.com>
2016-10-20 09:26:14 -0400

    Convert Provider Table View images to font icons
    https://www.pivotaltracker.com/n/projects/1613907/stories/129081417

--- app/decorators/manageiq/providers/cloud_manager/orchestration_stack_decorator.rb
+++ app/decorators/manageiq/providers/cloud_manager/orchestration_stack_decorator.rb
@@ -1,6 +1,9 @@
 class ManageIQ::Providers::CloudManager::OrchestrationStackDecorator < Draper::Decorator
-
   def fonticon
     'product product-orchestration_stack'.freeze
+  end
+
+  def listicon_image
+    "100/orchestration_stack.png"
   end
 end
--- app/decorators/manageiq/providers/configuration_manager/inventory_group_decorator.rb
+++ app/decorators/manageiq/providers/configuration_manager/inventory_group_decorator.rb
@@ -4,4 +4,8 @@ class ManageIQ::Providers::ConfigurationManager::InventoryGroupDecorator < Drape
   def fonticon
     'pficon pficon-folder-close'.freeze
   end
+
+  def listicon_image
+    '100/inventory_group.png'
+  end
 end
