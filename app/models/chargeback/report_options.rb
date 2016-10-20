9257725644d51892f01f21cf77d02413f9c2c1b3
Šimon Lukašík <isimluk@fedoraproject.org>
2016-10-20 14:43:30 +0200

    Refactor: move and rename method: report_time_range
    The chargeback's timerange depends only on Chargeback report options.
    
    (cherry picked from commit f5df793288a8250880588869cf2eda63b3c314d5)

--- app/models/chargeback/report_options.rb
+++ app/models/chargeback/report_options.rb
@@ -22,11 +22,35 @@ class Chargeback
     def initialize(*)
       super
       self.interval ||= 'daily'
+      self.end_interval_offset ||= 0
     end
 
     def tz
       # TODO: Support time profiles via options[:ext_options][:time_profile]
       @tz ||= Metric::Helper.get_time_zone(ext_options)
+    end
+
+    def report_time_range
+      raise _("Option 'interval_size' is required") if interval_size.nil?
+
+      start_interval_offset = (end_interval_offset + interval_size - 1)
+
+      ts = Time.now.in_time_zone(tz)
+      case interval
+      when 'daily'
+        start_time = (ts - start_interval_offset.days).beginning_of_day.utc
+        end_time   = (ts - end_interval_offset.days).end_of_day.utc
+      when 'weekly'
+        start_time = (ts - start_interval_offset.weeks).beginning_of_week.utc
+        end_time   = (ts - end_interval_offset.weeks).end_of_week.utc
+      when 'monthly'
+        start_time = (ts - start_interval_offset.months).beginning_of_month.utc
+        end_time   = (ts - end_interval_offset.months).end_of_month.utc
+      else
+        raise _("interval '%{interval}' is not supported") % {:interval => interval}
+      end
+
+      start_time..end_time
     end
   end
 end
