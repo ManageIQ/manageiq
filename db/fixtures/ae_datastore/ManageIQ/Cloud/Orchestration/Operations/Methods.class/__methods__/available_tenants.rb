f1bce235aca1ab26960525fe325393ae9c9dd724
Patrik Kománek <pkomanek@redhat.com>
2016-10-24 19:19:21 +0200

    Refactoring available_tenants method for miq_ae_orchestrations.

--- db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_tenants.rb
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_tenants.rb
@@ -1,25 +1,45 @@
 #
 # Description: provide the dynamic list content from available tenants
 #
-tenant_list = {nil => "<default>"}
-service = $evm.root.attributes["service_template"] || $evm.root.attributes["service"]
-if service.respond_to?(:orchestration_manager) && service.orchestration_manager
-  service.orchestration_manager.cloud_tenants.each { |t| tenant_list[t.name] = t.name }
+class AvailableTenants
+  def initialize(handle = $evm)
+    @handle = handle
+  end
+
+  def main
+    fill_dialog_field(fetch_list_data)
+  end
+
+  def fetch_list_data
+    service = @handle.root.attributes["service_template"] || @handle.root.attributes["service"]
+    av_tenants = service.try(:orchestration_manager).try(:available_tenants)
+
+    tenant_list = { nil => "<default>" }
+    av_tenants.each { |tenant| tenant_list[tenant.ems_ref] = tenant.name } if av_tenants
+
+    tenant_list
+  end
+
+  def fill_dialog_field(list)
+    dialog_field = @handle.object
+
+    # sort_by: value / description / none
+    dialog_field["sort_by"] = "description"
+
+    # sort_order: ascending / descending
+    dialog_field["sort_order"] = "ascending"
+
+    # data_type: string / integer
+    dialog_field["data_type"] = "string"
+
+    # required: true / false
+    dialog_field["required"] = "false"
+
+    dialog_field["values"] = list
+    dialog_field["default_value"] = nil
+  end
 end
 
-dialog_field = $evm.object
-
-# sort_by: value / description / none
-dialog_field["sort_by"] = "description"
-
-# sort_order: ascending / descending
-dialog_field["sort_order"] = "ascending"
-
-# data_type: string / integer
-dialog_field["data_type"] = "string"
-
-# required: true / false
-dialog_field["required"] = "false"
-
-dialog_field["values"] = tenant_list
-dialog_field["default_value"] = nil
+if __FILE__ == $PROGRAM_NAME
+  AvailableTenants.new.main
+end
