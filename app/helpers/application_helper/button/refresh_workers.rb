0f79a50e0297a300d4e24678795aeed4f989fa0b
Attila Vecerek <avecerek@redhat.com>
2016-10-24 11:31:53 +0200

    Correct #visible? method so the button gets hidden when the record is miq_server or miq_region only if layout != ops and tailors the spec file accordingly

--- app/helpers/application_helper/button/refresh_workers.rb
+++ app/helpers/application_helper/button/refresh_workers.rb
@@ -1,8 +1,8 @@
 class ApplicationHelper::Button::RefreshWorkers < ApplicationHelper::Button::Basic
-  needs :@record, :@lastaction, @sb
+  needs :@record, :@lastaction, :@sb, :@layout
 
   def visible?
-    return true if diagnostics_worker_tab? && !miq?
+    return false if miq? && !(ops? && diagnostics_worker_tab?)
     return !%w(download_logs evm_logs audit_logs).include?(@lastaction) if @record.class == NilClass
     true
   end
@@ -15,5 +15,9 @@ class ApplicationHelper::Button::RefreshWorkers < ApplicationHelper::Button::Bas
 
   def miq?
     @record.class == MiqServer || @record.class == MiqRegion
+  end
+
+  def ops?
+    @layout == 'ops'
   end
 end
--- spec/helpers/application_helper/buttons/refresh_workers_spec.rb
+++ spec/helpers/application_helper/buttons/refresh_workers_spec.rb
@@ -1,20 +1,35 @@
 describe ApplicationHelper::Button::RefreshWorkers do
   before :all do
-    @record = FactoryGirl.create(
-      :assigned_server_role,
-      :miq_server => FactoryGirl.create(:miq_server))
+    @records = [FactoryGirl.create(:miq_server), FactoryGirl.create(:miq_region)]
   end
 
   describe '#visible?' do
-    it 'should be visible when active_tab == diagnostics_workers in :diagnostics_tree' do
-      view_context = setup_view_context_with_sandbox({})
-      button = described_class.new(view_context, {}, {'record' => @record}, {})
-      button.instance_variable_set(:@sb, :active_tab => 'diagnostics_workers')
-      expect(button.visible?).to be_truthy
+    context 'when layout == ops and active_tab == diagnostics_workers in :diagnostics_tree' do
+      it 'will not be skipped' do
+        view_context = setup_view_context_with_sandbox({})
+        button = described_class.new(view_context, {}, {'record' => nil, 'layout' => 'ops'}, {})
+        button.instance_variable_set(:@sb, :active_tab => 'diagnostics_workers')
+        allow(view_context).to receive(:x_active_tree).and_return(:diagnostics_tree)
+        expect(button.visible?).to be_truthy
+      end
+    end
+
+    context 'when layout != ops and' do
+      %w(miq_server miq_region).each_with_index do |record, i|
+        context "record is #{record}" do
+          it 'will be skipped' do
+            view_context = setup_view_context_with_sandbox({})
+            button = described_class.new(view_context, {}, {'record' => @records[i], 'layout' => 'not ops'}, {})
+            button.instance_variable_set(:@sb, :active_tab => 'does not matter')
+            allow(view_context).to receive(:x_active_tree).and_return(:diagnostics_tree)
+            expect(button.visible?).to be_falsey
+          end
+        end
+      end
     end
 
     %w(download_logs evm_logs audit_logs).each do |lastaction|
-      context "action #{lastaction} being taken as last" do
+      context "when action #{lastaction} being taken as last" do
         it 'will be skipped for this record' do
           view_context = setup_view_context_with_sandbox({})
           button = described_class.new(view_context, {}, {'record' => nil, 'lastaction' => lastaction}, {})
@@ -23,7 +38,7 @@ describe ApplicationHelper::Button::RefreshWorkers do
       end
     end
 
-    context 'other action being taken as last' do
+    context 'when other action being taken as last' do
       it 'will not be skipped for this record' do
         view_context = setup_view_context_with_sandbox({})
         button = described_class.new(view_context, {}, {'record' => nil, 'lastaction' => 'worker_logs'}, {})
