e926acccb74b4a42dc37e237a3aca4ecbc62afef
Ladislav Smola <lsmola@redhat.com>
2016-10-27 18:21:48 +0200

    Leave only batched saving for now, since it is faster
    Leave only batched saving for now, since it is faster than non
    batched.

--- app/models/manager_refresh/save_collection/helper.rb
+++ app/models/manager_refresh/save_collection/helper.rb
@@ -1,20 +1,14 @@
 module ManagerRefresh::SaveCollection
   module Helper
     def save_dto_inventory(ems, dto_collection)
-      dto_batch_saving = Settings.ems_refresh[ems.class.ems_type].try(:[], :dto_batch_saving)
-      if dto_batch_saving
-        save_dto_inventory_multi_batch(dto_collection.parent.send(dto_collection.association),
-                                       dto_collection,
-                                       :use_association,
-                                       dto_collection.manager_ref)
-      else
-        save_dto_inventory_multi(dto_collection.parent.send(dto_collection.association),
-                                 dto_collection,
-                                 :use_association,
-                                 dto_collection.manager_ref)
-      end
-      store_ids_for_new_dto_records(dto_collection.parent.send(dto_collection.association),
-                                    dto_collection)
+      _log.info("Synchronizing #{ems.name} collection #{dto_collection.size} of size #{dto_collection} to database")
+
+      save_dto_inventory_multi_batch(dto_collection.parent.send(dto_collection.association),
+                                     dto_collection,
+                                     :use_association,
+                                     dto_collection.manager_ref)
+
+      _log.info("Synchronized #{ems.name} collection #{dto_collection}")
       dto_collection.saved = true
     end
 
