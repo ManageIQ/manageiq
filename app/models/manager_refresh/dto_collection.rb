4944f0e653a6e8d9c04543d87717e61ed2c46b39
Ladislav Smola <lsmola@redhat.com>
2016-10-27 13:58:39 +0200

    Properly obtained Dependencies of DtoCollection
    First we need to get a list of fixed dependencies, that create
    dependency graph from mandatory attributes. Which are attributes
    present in unique manager_ref and attributes with presence
    validation. Without these attributes we are not able to save
    the record.
    
    Second we need to know which attributes are causing which of the
    dependencies in our DtoCollection.
    
    And third we need a way to blacklist attributes which also
    removes the dependencies.

--- app/models/manager_refresh/dto_collection.rb
+++ app/models/manager_refresh/dto_collection.rb
@@ -1,21 +1,23 @@
 module ManagerRefresh
   class DtoCollection
-    attr_accessor :saved, :data, :data_index, :dependencies,
+    attr_accessor :saved, :data, :data_index, :dependency_attributes,
                   :manager_ref, :attributes, :association, :parent
 
-    attr_reader :model_class
+    attr_reader :model_class, :attributes_blacklist
 
     def initialize(model_class, manager_ref: nil, attributes: nil, association: nil, parent: nil, strategy: nil)
-      @model_class  = model_class
-      @manager_ref  = manager_ref || [:ems_ref]
-      @attributes   = attributes || []
-      @association  = association || []
-      @parent       = parent || []
-      @dependencies = []
-      @data         = []
-      @data_index   = {}
-      @saved        = false
-      @strategy     = process_strategy(strategy)
+      @model_class           = model_class
+      @manager_ref           = manager_ref || [:ems_ref]
+      @attributes            = attributes || []
+      @association           = association || []
+      @parent                = parent || []
+      @dependency_attributes = {}
+      @data                  = []
+      @data_index            = {}
+      @saved                 = false
+      @strategy              = process_strategy(strategy)
+      @attributes_blacklist  = Set.new
+      @attributes_whitelist  = Set.new
     end
 
     def process_strategy(strategy_name)
@@ -83,6 +85,37 @@ module ManagerRefresh
       data_index
     end
 
+    def fixed_dependencies
+      presence_validators = model_class.validators.detect { |x| x.kind_of? ActiveRecord::Validations::PresenceValidator }
+      # Attributes that has to be always on the entity, so attributes making unique indec of the record + attributes
+      # that have presence validation
+      fixed_attributes = manager_ref
+      fixed_attributes += presence_validators.attributes unless presence_validators.blank?
+
+      fixed_dependencies = Set.new
+      dependency_attributes.each do |key, value|
+        fixed_dependencies += value if fixed_attributes.include?(key)
+      end
+      fixed_dependencies
+    end
+
+    def dependencies
+      dependency_attributes.values.map(&:to_a).flatten.uniq
+    end
+
+    def dependency_attributes_for(dto_collections)
+      attributes = Set.new
+      dto_collections.each do |dto_collection|
+        attributes += dependency_attributes.select { |_key, value| value.include?(dto_collection) }.keys
+      end
+      attributes
+    end
+
+    def blacklist_attributes!(attributes)
+      @attributes_blacklist += attributes
+      dependency_attributes.delete_if {|key, _value| attributes.include?(key) }
+    end
+
     def to_s
       "DtoCollection:<#{@model_class}>"
     end
@@ -94,14 +127,13 @@ module ManagerRefresh
     private
 
     def actualize_dependencies(dto)
-      dto.data.values.each do |value|
+      dto.data.each do |key, value|
         if value.kind_of? ::ManagerRefresh::DtoLazy
-          dependencies << value.dto_collection
+          (dependency_attributes[key] ||= Set.new) << value.dto_collection
         elsif value.kind_of?(Array) && value.any? { |x| x.kind_of? ::ManagerRefresh::DtoLazy }
-          dependencies << value.detect { |x| x.kind_of? ::ManagerRefresh::DtoLazy }.dto_collection
+          (dependency_attributes[key] ||= Set.new) <<  value.detect { |x| x.kind_of? ::ManagerRefresh::DtoLazy }.dto_collection
         end
       end
-      dependencies.uniq!
     end
   end
 end
