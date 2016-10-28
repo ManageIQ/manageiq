cb2d04c20477b1d872bd1c7c0b89505483a04e1e
Marcel Hild <hild@b4mad.net>
2016-10-28 11:57:16 +0200

    ignore templates folder in coverage_helper

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/kernel/reporting'
 # Require all ruby files for accuracte test coverage reports
 %w(app lib).each do |path|
   Dir.glob(Rails.root.join(path, "**", "*.rb")) do |file|
-    next if file.include?("/bin/") || file.include?("/spec/")
+    next if file.include?("/bin/") || file.include?("/spec/") || file.include?("/lib/generators/provider/templates/")
     begin
       silence_warnings { require file }
     rescue StandardError, LoadError, MissingSourceFile
