22c95489b0cfbc737af94ae75be3cf00edb08aef
Ladislav Smola <lsmola@redhat.com>
2016-10-31 12:38:52 +0100

    Proper passing of floating IP during provisioning
    Proper passing of floating IP during provisioning

--- gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/cloud_manager/provision/configuration.rb
+++ gems/manageiq-providers-amazon/app/models/manageiq/providers/amazon/cloud_manager/provision/configuration.rb
@@ -1,7 +1,12 @@
 module ManageIQ::Providers::Amazon::CloudManager::Provision::Configuration
   def associate_floating_ip(ip_address)
+    # TODO(lsmola) this should be moved to FloatingIp model
     destination.with_provider_object do |instance|
-      instance.client.associate_address(:instance_id => instance.instance_id, :public_ip => ip_address)
+      if ip_address.cloud_network_only?
+        instance.client.associate_address(:instance_id => instance.instance_id, :allocation_id => ip_address.ems_ref)
+      else
+        instance.client.associate_address(:instance_id => instance.instance_id, :public_ip => ip_address.address)
+      end
     end
   end
 
