4d244eb20bf6920ade5a0ccff8cc82a50d912fd0
Tim Wade <hello@timjwade.com>
2016-11-02 16:19:02 -0700

    Delete unused factory default_resource_pool_with_vms

--- spec/factories/resource_pool.rb
+++ spec/factories/resource_pool.rb
@@ -6,10 +6,4 @@ FactoryGirl.define do
   factory :default_resource_pool, :parent => :resource_pool do
     is_default true
   end
-
-  factory :default_resource_pool_with_vms, :parent => :resource_pool do
-    after(:create) do |rp|
-      rp.add_child(FactoryGirl.create(:vm_vmware))
-    end
-  end
 end
