3536e1305e803318e67be67f9a4cf94df1af3faf
Tim Wade <hello@timjwade.com>
2016-11-02 16:24:52 -0700

    Delete unused factory small_environment_host_with_default_resource_pool

--- spec/factories/small_environment_extended.rb
+++ spec/factories/small_environment_extended.rb
@@ -14,16 +14,4 @@ FactoryGirl.define do
       end
     end
   end
-
-  factory :small_environment_host_with_default_resource_pool, :parent => :small_environment_with_storages do
-    after(:create) do |x|
-      ems  = x.ext_management_systems.first
-      host = ems.hosts.first
-      default_res_pool = FactoryGirl.create(:resource_pool, :name => "Default for Host #{host.name}", :is_default => true)
-
-      ems.resource_pools << default_res_pool
-      default_res_pool.set_parent(host)
-      ems.vms.each { |vm| default_res_pool.add_vm(vm) }
-    end
-  end
 end
