0c4e1540639decc87d57e45ecff660e2b0b49668
Tim Wade <hello@timjwade.com>
2016-11-02 16:20:07 -0700

    Delete unused factory default_resource_pool

--- spec/factories/resource_pool.rb
+++ spec/factories/resource_pool.rb
@@ -2,8 +2,4 @@ FactoryGirl.define do
   factory :resource_pool do
     sequence(:name) { |n| "rp_#{seq_padded_for_sorting(n)}" }
   end
-
-  factory :default_resource_pool, :parent => :resource_pool do
-    is_default true
-  end
 end
