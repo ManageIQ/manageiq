86d8b471915b347565645b7c22dac3d1a7a9cc6e
Milan Zázrivec <mzazrivec@redhat.com>
2016-11-02 13:37:29 +0000

    Merge pull request #12351 from lpichler/add_also_daily_to_chargeback_rates
    Add daily to chargeback rate for 'per time' types
    (cherry picked from commit 10ebe804fab6a7cd1fc55c5b1f6fac1f576e4edf)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1410535

--- app/models/chargeback_rate_detail.rb
+++ app/models/chargeback_rate_detail.rb
@@ -10,7 +10,12 @@ class ChargebackRateDetail < ApplicationRecord
   validate :contiguous_tiers?
 
   FORM_ATTRIBUTES = %i(description per_time per_unit metric group source metric).freeze
-  PER_TIME_TYPES = {"hourly" => _("Hourly"), "weekly" => _("Weekly"), "monthly" => _("Monthly")}.freeze
+  PER_TIME_TYPES = {
+    "hourly"  => _("Hourly"),
+    "daily"   => _("Daily"),
+    "weekly"  => _("Weekly"),
+    "monthly" => _("Monthly")
+  }.freeze
 
   attr_accessor :hours_in_interval
 
