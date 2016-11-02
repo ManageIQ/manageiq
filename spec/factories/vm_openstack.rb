09e96d7dd6db43de40ff24b09607fa71ad1e1b7a
Tim Wade <hello@timjwade.com>
2016-11-02 16:29:13 -0700

    Delete unused factory vm_target_openstack

--- spec/factories/vm_openstack.rb
+++ spec/factories/vm_openstack.rb
@@ -8,8 +8,4 @@ FactoryGirl.define do
   factory :vm_perf_openstack, :parent => :vm_openstack do
     ems_ref         "openstack-perf-vm"
   end
-
-  factory :vm_target_openstack, :parent => :vm_openstack do
-    after(:create) { |x| x.raw_power_state = (toggle_on_name_seq(x) ? "ACTIVE" : "SHUTOFF") }
-  end
 end
