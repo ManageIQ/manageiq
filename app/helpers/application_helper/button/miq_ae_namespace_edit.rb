f4c8f27ad152c7dfb194aadcab052d5cc5098733
Attila Vecerek <avecerek@redhat.com>
2016-11-03 12:19:33 +0100

    Fix the found error by modifying the inheritance hierarchy

--- app/helpers/application_helper/button/miq_ae_namespace_edit.rb
+++ app/helpers/application_helper/button/miq_ae_namespace_edit.rb
@@ -1,5 +1,10 @@
-class ApplicationHelper::Button::MiqAeNamespaceEdit < ApplicationHelper::Button::MiqAeDomainLock
+class ApplicationHelper::Button::MiqAeNamespaceEdit < ApplicationHelper::Button::MiqAeDomain
   needs :@record
+
+  def disabled?
+    @error_message = N_('Domain is Locked.') if super
+    @error_message.present?
+  end
 
   def visible?
     super || editable_domain?(@record)
