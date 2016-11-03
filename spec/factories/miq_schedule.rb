623d68f441d3d4bffa901e18d0efe1dcb21b5f86
Drew Bomhof <dbomhof@redhat.com>
2016-11-03 16:38:49 -0400

    Added specs around the object_request rails side changes
    https://www.pivotaltracker.com/story/show/133269363

--- spec/factories/miq_schedule.rb
+++ spec/factories/miq_schedule.rb
@@ -16,4 +16,16 @@ FactoryGirl.define do
     run_at              run_at
     sched_action        sched_action
   end
+
+  factory :miq_automate_schedule, :class => :MiqSchedule do
+    run_at = {:start_time   => "2010-07-08 04:10:00 Z", :interval => {:unit => "daily", :value => "1"}}
+    sched_action = {:method => "automation_request"}
+    filter = {:uri_parts => {:instance => 'test', :message => 'create'}, :parameters => {'request' => 'test_request'}}
+    sequence(:name)     { |n| "automate_schedule_#{seq_padded_for_sorting(n)}" }
+    description         "test_automation"
+    towhat              "AutomationRequest"
+    run_at              run_at
+    sched_action        sched_action
+    filter              filter
+  end
 end
