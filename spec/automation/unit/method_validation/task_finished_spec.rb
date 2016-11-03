89ddb9ca803916a4d99d9562eaebc9a730735a92
Tina Fitzgerald <tfitzger@redhat.com>
2016-11-03 15:37:38 -0400

    Renamed vm_provisioned and service_provisioned notification_types to prevent them from being used for events. Add notification in task_finished common method.
    https://bugzilla.redhat.com/show_bug.cgi?id=1389312

--- spec/automation/unit/method_validation/task_finished_spec.rb
+++ spec/automation/unit/method_validation/task_finished_spec.rb
@@ -14,6 +14,7 @@ describe "task_finished_status" do
   let(:miq_server) { EvmSpecHelper.local_miq_server }
   let(:ems) { FactoryGirl.create(:ems_vmware_with_authentication) }
   let(:vm_template) { FactoryGirl.create(:template_vmware, :ext_management_system => ems) }
+  let(:vm) { FactoryGirl.create(:vm_vmware, :ext_management_system => ems) }
   let(:options) { {:src_vm_id => [vm_template.id, vm_template.name], :pass => 1} }
   let(:miq_provision_request) do
     FactoryGirl.create(:miq_provision_request,
@@ -27,19 +28,24 @@ describe "task_finished_status" do
     FactoryGirl.create(:miq_provision_vmware, :provision_type => 'template',
                        :state => 'pending', :status => 'Ok',
                        :miq_request => miq_provision_request,
-                       :options => options, :userid => user.userid)
+                       :options => options, :userid => user.userid, :vm => vm)
   end
 
   it "miq_provision method succeeds" do
+    type = :automate_vm_provisioned
+    FactoryGirl.create(:notification_type, :name => type)
+
+    msg = "[#{miq_server.name}] VM [] Provisioned Successfully"
     @args = "status=fred&ae_result=ok&MiqProvision::miq_provision=#{provision.id}&" \
             "MiqServer::miq_server=#{miq_server.id}&vmdb_object_type=miq_provision&" \
             "object=miq_provision&message=not used"
+    expect(Notification.count).to eq(0)
     add_call_method
     ws
     expect(provision.reload.status).to eq('Ok')
     expect(provision.state).to eq('finished')
-    msg = "[#{miq_server.name}] VM [] Provisioned Successfully"
     expect(miq_provision_request.reload.message).to eq(msg)
+    expect(Notification.find_by(:notification_type_id => NotificationType.find_by_name(type).id)).not_to be_nil
   end
 
   it "task_finished method input message" do
