48a1a0adf80e4074e8bda3e470a4f6ad9ffa1e43
william fitzgerald <wfitzger@redhat.com>
2016-11-03 10:47:37 -0400

    Automate - Notification for Service and VM Retirement error.
    Modified update_service_retirement method to send Notification when error.
    
    Modified update_retirement_status methods for Cloud/Vm, Infrastructure/Vm and Cloud/Orchestration.
    
    Previous Notification PR(s): 12306, 12347, 12370

--- db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/VMRetirement.class/__methods__/update_retirement_status.rb
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/VM/Retirement/StateMachines/VMRetirement.class/__methods__/update_retirement_status.rb
@@ -1,31 +1,45 @@
 #
-# Description: This method updates retirement status
+# Description: This method updates the vm retirement status.
+# Required inputs: status
 #
 
 # Get variables from Server object
 server = $evm.root['miq_server']
 
 # Get State Machine
-state  = $evm.current_object.class_name
-status = $evm.inputs['status']
+state = $evm.current_object.class_name
 
 # Get current step
 step = $evm.current_object.current_field_name
 
+# Get status from input field status
+status = $evm.inputs['status']
+
 # Get status_state ['on_entry', 'on_exit', 'on_error']
 status_state = $evm.root['ae_status_state']
 
-$evm.log("info", "Server:<#{server.name}> Ae_Result:<#{$evm.root['ae_result']}> State:<#{state}>")
-$evm.log("info", "Step:<#{step}> Status_State:<#{status_state}> Status:<#{status}>")
-
 vm = $evm.root['vm']
+
+$evm.log("info", "Server:<#{server.name}> Ae_Result:<#{$evm.root['ae_result']}> State:<#{state}> Step:<#{step}>")
+$evm.log("info", "Status_State:<#{status_state}> Status:<#{status}>")
+
+# Update Status Message
+updated_message  = "Server [#{server.name}] "
+updated_message += "VM [#{vm.name}] " if vm
+updated_message += "Step [#{step}] "
+updated_message += "Status [#{status}] "
+updated_message += "Current Retry Number [#{$evm.root['ae_state_retries']}]" if $evm.root['ae_result'] == 'retry'
 
 # Update Status for on_error for all states other than the first state which is startretirement
 # in the retirement state machine.
 if $evm.root['ae_result'] == 'error'
   if step.downcase == 'startretirement'
-    $evm.log("info", "Cannot continue because VM is already retired or is being retired.")
+    msg = "Cannot continue because VM is already retired or is being retired."
+    $evm.log("info", msg)
+    updated_message += msg
+    $evm.create_notification(:level => "warning", :message => "VM Retirement Warning: #{updated_message}")
   else
+    $evm.create_notification(:level => "error", :message => "VM Retirement Error: #{updated_message}")
     vm.retirement_state = 'error' if vm
   end
 end
