893e51bce9fcf068fe492c3eb1e2bfe2037cf1e8
Brian McLaughlin <bmclaugh@redhat.com>
2016-11-04 11:25:24 -0400

    Fix Travis failure on Darga
    https://bugzilla.redhat.com/show_bug.cgi?id=1386794

--- spec/views/miq_policy/_alert_details.html.haml_spec.rb
+++ spec/views/miq_policy/_alert_details.html.haml_spec.rb
@@ -1,6 +1,7 @@
 describe "miq_policy/_alert_details.html.haml" do
   before do
     @alert = FactoryGirl.create(:miq_alert)
+    @alert_profiles = Object.new
     exp = {:eval_method => 'nothing', :mode => 'internal', :options => {}}
     allow(@alert).to receive(:expression).and_return(exp)
     set_controller_for_view("miq_policy")
@@ -9,7 +10,9 @@ describe "miq_policy/_alert_details.html.haml" do
   it "Trap Number is displayed correctly" do
     opts = {:notifications => {:snmp => {:host => ['test.test.org'], :snmp_version => 'v1', :trap_id => '42'}}}
     allow(@alert).to receive(:options).and_return(opts)
-    render :partial => "miq_policy/alert_details"
+    allow(@alert_profiles).to receive(:empty?).and_return(true)
+    render :partial => 'miq_policy/alert_details',
+           :locals  => {:x_active_tree => :alert_tree}
     expect(rendered).to include('Trap Number')
   end
 end
