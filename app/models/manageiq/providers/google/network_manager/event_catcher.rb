a27c4fc1a7c0019324652be69ddffded120ab1a2
Keenan Brock <kbrock@redhat.com>
2016-11-04 21:48:02 -0400

    Remove unused google network manager workers

--- app/models/manageiq/providers/google/network_manager/event_catcher.rb
+++
@@ -1,9 +0,0 @@
-class ManageIQ::Providers::Google::NetworkManager::EventCatcher < ::MiqEventCatcher
-  def self.ems_class
-    ManageIQ::Providers::Google::NetworkManager
-  end
-
-  def self.settings_name
-    :event_catcher_google_network
-  end
-end
