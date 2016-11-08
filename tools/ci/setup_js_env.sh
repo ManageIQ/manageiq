881fafa6c37e6d009e62d9b51e18642c25739cd0
Nick LaMuro <nicklamuro@gmail.com>
2016-11-08 13:12:45 -0600

    Enables travis caches for the bower_components dir
    For this, bundler is still cached through the default mechanisms, but in
    addition to that, caching the vendor/assets/bower_components directory
    as well.
    
    To make sure the cache is not out of date, the bower.json file is
    cmp'd (compared, using the `cmp` command) to the one that was cached
    in vendor/assets/bower_components directory.  This is added to the cache
    in the `before_cache` step.  Unless this passes, the directory is
    removed and bower is run and updated normally.
    
    In this, bower is also skipped from running all together if the assets
    already exist.  So the only way that the directory should exist is if
    the bower.json file matches the one in the directory (it also has to
    exist for this to work), and there has been a successful run in the past
    that uploaded the cached assets, otherwise bower is run normally.

--- tools/ci/setup_js_env.sh
+++ tools/ci/setup_js_env.sh
@@ -1,8 +1,18 @@
 which bower || npm install -g bower
-bower install --allow-root -F --config.analytics=false
-STATUS=$?
-echo bower exit code: $STATUS
 
-# fail the whole test suite if bower install failed
-[ $STATUS = 0 ] || exit 1
-[ -d vendor/assets/bower_components ] || exit 1
+# Check if the bower cache is valid, otherwise delete it
+if ! cmp --silent bower.json vendor/assets/bower_components/bower.json; then
+  rm -rf vendor/assets/bower_components
+fi
+
+if [ -d vendor/assets/bower_components ]; then
+  echo "bower assets installed... moving on."
+else
+  bower install --allow-root -F --config.analytics=false
+  STATUS=$?
+  echo bower exit code: $STATUS
+
+  # fail the whole test suite if bower install failed
+  [ $STATUS = 0 ] || exit 1
+  [ -d vendor/assets/bower_components ] || exit 1
+fi
