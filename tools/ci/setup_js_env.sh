718510cdddbc902fe836db9280734afc9e9732cf
Martin Hradil <mhradil@redhat.com>
2016-11-08 17:31:03 +0000

    tools/ci/setup_js_env - fail the whole test suite if bower install fails
    this should at least make the vmdb suite fail early when bower has not succeeded in getting all the dependencies
    
    (cherry picked from commit 8d3daef311376da888040463bcf13d774d1268d7)

--- tools/ci/setup_js_env.sh
+++ tools/ci/setup_js_env.sh
@@ -1,2 +1,8 @@
 which bower || npm install -g bower
 bower install --allow-root -F --config.analytics=false
+STATUS=$?
+echo bower exit code: $STATUS
+
+# fail the whole test suite if bower install failed
+[ $STATUS = 0 ] || exit 1
+[ -d vendor/assets/bower_components ] || exit 1
