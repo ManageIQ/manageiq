c469b3958f3d0a1cc9463a704d6931cba7665d5c
Harpreet Kataria <hkataria@redhat.com>
2016-11-08 16:58:34 -0500

    Fixed code that was gathering events for each selected category
    Changed code that was looping thru records to gather timeline events for each selected category, previously it was skipping thru some of the categories with the way code was looping thru random events in Ruport dfata table. Also replaced double-quotes in the timeline event data with single-quote, new timeline widget does not like double quotes in the description.
    Added spec test to verify counts of events.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1391621

--- lib/report_formatter/timeline_message.rb
+++ lib/report_formatter/timeline_message.rb
@@ -105,6 +105,7 @@ module ReportFormatter
       if @row[@column].kind_of?(Time) || TIMELINE_TIME_COLUMNS.include?(@column)
         format_timezone(Time.parse(@row[@column].to_s).utc, @flags[:time_zone], "gtl")
       else
+        @row[@column].to_s.tr!('"', "'")
         @row[@column].to_s.gsub('/', "\\\\/")
         @row[@column].to_s.gsub('\\', "\\\\\\")
       end
