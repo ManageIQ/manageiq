92841e824035fedcbbbac42a93b4b7a68ebeb208
borod108 <bodnopoz@redhat.com>
2016-11-09 11:08:11 +0200

    Collect clusters using v4 api

--- app/models/manageiq/providers/redhat/infra_manager/refresh/strategies/api4.rb
+++ app/models/manageiq/providers/redhat/infra_manager/refresh/strategies/api4.rb
@@ -1,4 +1,59 @@
 module ManageIQ::Providers::Redhat::InfraManager::Refresh::Strategies
   class Api4 < ManageIQ::Providers::Redhat::InfraManager::Refresh::Refresher
+    attr_reader :ems
+
+    def inventory_from_rhv(ems)
+      @ems = ems
+      InventoryWrapper.new(old_inventory: super, ems: ems)
+    end
+
+    class InventoryWrapper
+      attr_reader :old_inventory
+      attr_reader :ems
+
+      def initialize(args)
+        @ems = args[:ems]
+        @old_inventory = args[:old_inventory]
+      end
+
+      def refresh
+        res = old_inventory.refresh
+        res[:cluster] = collect_clusters
+        res
+      end
+
+      def collect_clusters
+        clusters = @ems.with_provider_connection(:version => 4) do |connection|
+          connection.system_service.clusters_service.list
+        end
+        clusters.collect {|c| BracketNotationDecorator.new(c) }
+      end
+
+      def api
+        old_inventory.api
+      end
+
+      def service
+        old_inventory.service
+      end
+    end
+  end
+
+  class BracketNotationDecorator < SimpleDelegator
+    ALLOWED_METHODS = ["id", "name", "href"]
+
+    def initialize(obj)
+      @obj = obj
+      super
+    end
+
+    def [](key)
+      return super unless ALLOWED_METHODS.include?(key.to_s)
+      @obj.send(key)
+    end
+
+    def attributes
+      instance_values
+    end
   end
 end
---
+++ spec/models/manageiq/providers/redhat/infra_manager/refresher_spec.rb
@@ -0,0 +1,24 @@
+describe ManageIQ::Providers::Redhat::InfraManager::Refresh::Refresher do
+  let(:ems) { FactoryGirl.create(:ems_redhat) }
+  describe 'chooses the right refresher strategy' do
+    context "when v4 api" do
+      before(:each) do
+        allow(ems).to receive(:highest_supported_api_version).and_return(4)
+      end
+
+      it 'returns the api4 refresher' do
+        expect(ems.refresher).to eq(ManageIQ::Providers::Redhat::InfraManager::Refresh::Strategies::Api4)
+      end
+    end
+
+    context "when v3 api" do
+      before(:each) do
+        allow(ems).to receive(:highest_supported_api_version).and_return(3)
+      end
+
+      it 'returns the api3 refresher' do
+        expect(ems.refresher).to eq(ManageIQ::Providers::Redhat::InfraManager::Refresh::Strategies::Api3)
+      end
+    end
+  end
+end
