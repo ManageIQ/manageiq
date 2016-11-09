e68519a93b60433213f75b5c3cc53e20d7fa0302
Patrik Kománek <pkomanek@redhat.com>
2016-11-09 10:32:20 +0100

    Refactoring the available_flavors method for the automate Cloud Orchestration.

--- db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_flavors.rb
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Operations/Methods.class/__methods__/available_flavors.rb
@@ -1,26 +1,66 @@
 #
 # Description: provide the dynamic list content from available flavors
 #
-flavor_list = {}
-service = $evm.root.attributes["service_template"] || $evm.root.attributes["service"]
-if service.respond_to?(:orchestration_manager) && service.orchestration_manager
-  service.orchestration_manager.flavors.each { |f| flavor_list[f.name] = f.name }
+module ManageIQ
+  module Automate
+    module Cloud
+      module Orchestration
+        module Operations
+          class AvailableFlavors
+            def initialize(handle = $evm)
+              @handle = handle
+            end
+
+            def main
+              fill_dialog_field(fetch_selected_os)
+            end
+
+            def fetch_selected_os
+              flavor_list = {}
+              service = $evm.root.attributes["service_template"] || $evm.root.attributes["service"]
+              if service.respond_to?(:orchestration_manager) && service.orchestration_manager
+                service.orchestration_manager.flavors.each { |f| flavor_list[f.name] = f.name }
+              end
+              flavor_list[nil] = flavor_list.empty? ? "<None>" : "<Choose>"
+
+
+              service = @handle.root.attributes["service_template"] || @handle.root.attributes["service"]
+              flavors = service.try(:orchestration_manager).try(:flavors)
+
+              flavor_list = {}
+              flavor_list.each { |f| flavor_list[f.name] = f.name } if flavors
+
+              return nil => "<None>" if flavor_list.blank?
+
+              flavor_list[nil] = "<Choose>" if flavor_list.length > 1
+              flavor_list
+            end
+
+            def fill_dialog_field(list)
+              dialog_field = @handle.object
+
+              # sort_by: value / description / none
+              dialog_field["sort_by"] = "description"
+
+              # sort_order: ascending / descending
+              dialog_field["sort_order"] = "ascending"
+
+              # data_type: string / integer
+              dialog_field["data_type"] = "string"
+
+              # required: true / false
+              dialog_field["required"] = "true"
+
+              dialog_field["values"] = list
+              dialog_field["default_value"] = nil
+            end
+          end
+        end
+      end
+    end
+  end
 end
-flavor_list[nil] = flavor_list.empty? ? "<None>" : "<Choose>"
 
-dialog_field = $evm.object
-
-# sort_by: value / description / none
-dialog_field["sort_by"] = "description"
-
-# sort_order: ascending / descending
-dialog_field["sort_order"] = "ascending"
-
-# data_type: string / integer
-dialog_field["data_type"] = "string"
-
-# required: true / false
-dialog_field["required"] = "true"
-
-dialog_field["values"] = flavor_list
-dialog_field["default_value"] = nil
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::Cloud::Orchestration::Operations::AvailableFlavors.new.main
+end
