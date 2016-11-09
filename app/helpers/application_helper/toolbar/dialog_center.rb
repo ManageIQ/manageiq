05a64c7650450ac1b1a7ebe912dbbfff29f8f4d1
Attila Vecerek <avecerek@redhat.com>
2016-11-09 18:24:01 +0100

    Assign the buttons to their respective classes in the dialog toolbars

--- app/helpers/application_helper/toolbar/dialog_center.rb
+++ app/helpers/application_helper/toolbar/dialog_center.rb
@@ -11,20 +11,23 @@ class ApplicationHelper::Toolbar::DialogCenter < ApplicationHelper::Toolbar::Bas
           'pficon pficon-edit fa-lg',
           t = N_('Edit this Dialog'),
           t,
-          :url_parms => "main_div"),
+          :url_parms => "main_div",
+          :klass     => ApplicationHelper::Button::DialogAction),
         button(
           :dialog_copy,
           'fa fa-files-o fa-lg',
           t = N_('Copy this Dialog'),
           t,
-          :url_parms => "main_div"),
+          :url_parms => "main_div",
+          :klass     => ApplicationHelper::Button::DialogAction),
         button(
           :dialog_delete,
           'pficon pficon-delete fa-lg',
           N_('Remove this Dialog'),
           N_('Remove Dialog'),
           :url_parms => "main_div",
-          :confirm   => N_("Warning: This Dialog will be permanently removed!")),
+          :confirm   => N_("Warning: This Dialog will be permanently removed!"),
+          :klass     => ApplicationHelper::Button::DialogAction),
       ]
     ),
   ])
@@ -41,19 +44,22 @@ class ApplicationHelper::Toolbar::DialogCenter < ApplicationHelper::Toolbar::Bas
           'pficon pficon-add-circle-o fa-lg',
           t = N_('Add a new Tab to this Dialog'),
           t,
-          :url_parms => "?typ=tab&id=\#{@edit[:rec_id]}"),
+          :url_parms => "?typ=tab&id=\#{@edit[:rec_id]}",
+          :klass     => ApplicationHelper::Button::DialogAddTab),
         button(
           :dialog_add_box,
           'pficon pficon-add-circle-o fa-lg',
           t = N_('Add a new Box to this Tab'),
           t,
-          :url_parms => "?typ=box&id=\#{@edit[:rec_id]}"),
+          :url_parms => "?typ=box&id=\#{@edit[:rec_id]}",
+          :klass     => ApplicationHelper::Button::DialogAddBox),
         button(
           :dialog_add_element,
           'pficon pficon-add-circle-o fa-lg',
           t = N_('Add a new Element to this Box'),
           t,
-          :url_parms => "?typ=element&id=\#{@edit[:rec_id]}"),
+          :url_parms => "?typ=element&id=\#{@edit[:rec_id]}",
+          :klass     => ApplicationHelper::Button::DialogAddElement),
       ]
     ),
   ])
@@ -65,7 +71,8 @@ class ApplicationHelper::Toolbar::DialogCenter < ApplicationHelper::Toolbar::Bas
         _('Discard this new %{dialog_type}') % {:dialog_type => @sb[:node_typ].titleize}
       end,
       nil,
-      :url_parms => "?id=\#{@edit[:rec_id]}"),
+      :url_parms => "?id=\#{@edit[:rec_id]}",
+      :klass     => ApplicationHelper::Button::DialogResDiscard),
   ])
   button_group('dialog_edit_delete', [
     button(
@@ -75,6 +82,7 @@ class ApplicationHelper::Toolbar::DialogCenter < ApplicationHelper::Toolbar::Bas
         _('Delete selected %{dialog_resource}') % {:dialog_resource => @sb[:txt]}
       end,
       nil,
-      :url_parms => "?id=\#{@edit[:rec_id]}"),
+      :url_parms => "?id=\#{@edit[:rec_id]}",
+      :klass     => ApplicationHelper::Button::DialogResourceRemove),
   ])
 end
