d9837b735f2f536d506b0f014667999bc4edb4f0
d-m-u <duhlmann@redhat.com>
2016-11-10 06:03:09 -0700

    fix merge conflicts

--- app/models/dialog_field_drop_down_list.rb
+++ app/models/dialog_field_drop_down_list.rb
@@ -14,4 +14,30 @@ class DialogFieldDropDownList < DialogFieldSortedItem
   def initial_values
     [[nil, "<None>"]]
   end
+
+  def refresh_json_value(checked_value)
+    @raw_values = @default_value = nil
+
+    refreshed_values = values
+
+    if refreshed_values.collect { |value_pair| value_pair[0].to_s }.include?(checked_value)
+      @value = checked_value
+    else
+      @value = @default_value
+    end
+
+    {:refreshed_values => refreshed_values, :checked_value => @value, :read_only => read_only?, :visible => visible?}
+  end
+
+  def automate_output_value
+    return nil if @value.blank?
+    MiqAeEngine.create_automation_attribute_array_value(@value.split.map(&:to_i))
+  end
+
+  private
+
+  def load_values_on_init?
+    return true unless show_refresh_button
+    load_values_on_init
+  end
 end
