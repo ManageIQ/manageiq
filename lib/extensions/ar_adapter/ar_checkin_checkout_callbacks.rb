95c2d483f9fe20657cac23dd2bd4cc75872b3a75
Nick LaMuro <nicklamuro@gmail.com>
2016-11-11 15:14:00 -0600

    Backports from rails commit 2b5d139
    https://github.com/rails/rails/commit/2b5d139
    
    No idea if this was done properly, but did some poking around the
    application with this in place and it seemed to work.

--- lib/extensions/ar_adapter/ar_checkin_checkout_callbacks.rb
+++ lib/extensions/ar_adapter/ar_checkin_checkout_callbacks.rb
@@ -20,3 +20,73 @@ ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
     logger.debug { "#{self.class.name.demodulize}##{__method__}, #{connection_info_for_logging}" } if logger && ActiveRecord::Base.connected?
   end
 end
+
+
+
+# TODO:  Remove everything below this comment once the following is released in
+# Rails: https://github.com/rails/rails/commit/2b5d139
+
+# Yes, I realize this was opened up above... this is just to make it easier to
+# delete.
+#
+# Also, FYI, enable_query_cache! is not the same as what is defined below, but
+# something that already exists in this class.
+ActiveRecord::ConnectionAdapters::AbstractAdapter.class_eval do
+  set_callback(:checkout, :after, :configure_query_cache!)
+
+  def configure_query_cache!
+    enable_query_cache! if pool.query_cache_enabled
+  end
+end
+
+module MiqConnectionPoolConfiguration
+  def initialize(*)
+    super
+    @query_cache_enabled = Concurrent::Map.new { false }
+  end
+
+  def enable_query_cache!
+    @query_cache_enabled[connection_cache_key(Thread.current)] = true
+    connection.enable_query_cache! if active_connection?
+  end
+
+  def disable_query_cache!
+    @query_cache_enabled.delete connection_cache_key(Thread.current)
+    connection.disable_query_cache! if active_connection?
+  end
+
+  def query_cache_enabled
+    @query_cache_enabled[connection_cache_key(Thread.current)]
+  end
+end
+
+ActiveRecord::ConnectionAdapters::ConnectionPool.class_eval do
+  include MiqConnectionPoolConfiguration
+end
+
+# In the aforementioned commit, this is added to
+# `ActiveRecord::QueryCache::ClassMethods`, but that is only included in
+# `ActiveRecord::Base`.  Adding this directly to Base avoids load dependency
+# issues (hopefully).
+ActiveRecord::Base.class_eval do
+  def self.run
+    connection_id = ActiveRecord::Base.connection_id
+
+    caching_pool = ActiveRecord::Base.connection_pool
+    caching_was_enabled = caching_pool.query_cache_enabled
+
+    caching_pool.enable_query_cache!
+
+    [caching_pool, caching_was_enabled, connection_id]
+  end
+
+  def self.complete((caching_pool, caching_was_enabled, connection_id))
+    ActiveRecord::Base.connection_id = connection_id
+
+    caching_pool.disable_query_cache! unless caching_was_enabled
+
+    ActiveRecord::Base.connection_handler.connection_pool_list.each do |pool|
+      pool.release_connection if pool.active_connection? && !pool.connection.transaction_open?
+    end
+  end
+end
