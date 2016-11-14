e3d27535dc344111558f508dbf60e6af1e827589
Brian McLaughlin <bmclaugh@redhat.com>
2016-11-14 10:00:26 -0500

    Additional spec and miq-bot corrections
    https://bugzilla.redhat.com/show_bug.cgi?id=1387088

--- app/helpers/application_helper/button/tenant_add.rb
+++ app/helpers/application_helper/button/tenant_add.rb
@@ -3,7 +3,7 @@ class ApplicationHelper::Button::TenantAdd < ApplicationHelper::Button::GenericF
   delegate :role_allows?, :rbac_common_feature_for_buttons, :to => :@view_context
 
   def role_allows_feature?
-    return role_allows?(:feature => rbac_common_feature_for_buttons(self[:child_id]))
+    role_allows?(:feature => rbac_common_feature_for_buttons(self[:child_id]))
   end
 
   def visible?
