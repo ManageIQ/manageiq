b34a8756f5434ec627d8d68d5cab4e361e6742a6
Šimon Lukašík <isimluk@fedoraproject.org>
2016-11-15 13:33:47 +0100

    Move ext_auth? method to DashboardHelper
    It is only used by Dashboard controller and its views.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1860,10 +1860,4 @@ module ApplicationHelper
       _("Database")
     end
   end
-
-  def ext_auth?(auth_option = nil)
-    return false unless ::Settings.authentication.mode == "httpd"
-    auth_option ? ::Settings.authentication[auth_option] : true
-  end
-  public :ext_auth?
 end
