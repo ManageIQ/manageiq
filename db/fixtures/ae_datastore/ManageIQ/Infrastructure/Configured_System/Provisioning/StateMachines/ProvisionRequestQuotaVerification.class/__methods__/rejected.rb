94f08347682953e7bf55823bac950755c5f1bdc2
william fitzgerald <wfitzger@redhat.com>
2016-11-15 15:30:42 -0500

    Automate - Remove old Quota classes and Instances.
    Remove  3 old classes, instances and methods:
    
    Infrastructure/Configured_System/Provisioning/StateMachines/ProvisionRequestQuotaVerification
    
    Cloud/VM/Provisioning/StateMachines/ProvisionRequestQuotaVerification
    
    Infrastructure/VM/Provisioning/StateMachines/ProvisionRequestQuotaVerification
    
    https://www.pivotaltracker.com/story/show/134385127

--- db/fixtures/ae_datastore/ManageIQ/Infrastructure/Configured_System/Provisioning/StateMachines/ProvisionRequestQuotaVerification.class/__methods__/rejected.rb
+++
@@ -1,7 +0,0 @@
-#
-# Description: This method runs when the provision request quota validation has failed
-#
-
-# Deny the request
-$evm.log('info', "Request denied because of Quota")
-$evm.root["miq_request"].deny("admin", "Quota Exceeded")
