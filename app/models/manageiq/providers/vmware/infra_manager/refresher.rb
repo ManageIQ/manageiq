05011114148a30a2795e9097e335936066321f54
Adam Grare <agrare@redhat.com>
2016-11-22 16:19:54 -0500

    Check config for dto_refresh

--- app/models/manageiq/providers/vmware/infra_manager/refresher.rb
+++ app/models/manageiq/providers/vmware/infra_manager/refresher.rb
@@ -45,7 +45,10 @@ module ManageIQ::Providers
         log_header = format_ems_for_logging(ems)
         _log.debug "#{log_header} Parsing VC inventory..."
         hashes, = Benchmark.realtime_block(:parse_vc_data) do
-          InfraManager::RefreshParser.ems_inv_to_hashes(inventory)
+          if refresher_options.try(:[], :dto_refresh)
+          else
+            InfraManager::RefreshParser.ems_inv_to_hashes(inventory)
+          end
         end
         _log.debug "#{log_header} Parsing VC inventory...Complete"
 
