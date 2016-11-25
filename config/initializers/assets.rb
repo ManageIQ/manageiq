c27dca7548ec6a208b54e6987511fb33e9ec3695
Martin Hradil <mhradil@redhat.com>
2016-11-25 16:43:23 +0000

    use node_modules for assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w(
   jquery-1.8/jquery.js jquery_overrides.js jquery vmrc.css
