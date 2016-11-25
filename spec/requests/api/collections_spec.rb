865b8633defbdfa4b80f214b78a4d53d5f26dcaa
Milan Zázrivec <mzazrivec@redhat.com>
2016-11-25 13:05:51 +0100

    Merge pull request #12844 from lpichler/remove_list_and_show_from_miq_features
    Remove show and list permission from MiqProductFeatures for Tenants
    (cherry picked from commit f90be9b1967efebbc737c1121e6f61ac8552feb0)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1399216

--- spec/requests/api/collections_spec.rb
+++ spec/requests/api/collections_spec.rb
@@ -244,7 +244,7 @@ describe "Rest API Collections" do
     end
 
     it "query Tenants" do
-      api_basic_authorize "rbac_tenant_show_list"
+      api_basic_authorize "rbac_tenant_view"
       Tenant.seed
       test_collection_query(:tenants, tenants_url, Tenant)
     end
@@ -484,7 +484,7 @@ describe "Rest API Collections" do
     end
 
     it "bulk query Tenants" do
-      api_basic_authorize "rbac_tenant_show_list"
+      api_basic_authorize "rbac_tenant_view"
       Tenant.seed
       test_collection_bulk_query(:tenants, tenants_url, Tenant)
     end
