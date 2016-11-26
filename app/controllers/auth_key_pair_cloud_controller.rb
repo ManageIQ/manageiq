7d26b742247efe064e410e1da6179eb1dffcefc8
Martin Povolny <mpovolny@redhat.com>
2016-11-26 20:10:52 +0100

    Refactor AuthKeyPairCloudController using Generic* mixins.

--- app/controllers/auth_key_pair_cloud_controller.rb
+++ app/controllers/auth_key_pair_cloud_controller.rb
@@ -4,8 +4,16 @@ class AuthKeyPairCloudController < ApplicationController
   after_action :cleanup_action
   after_action :set_session_data
 
-  def index
-    redirect_to :action => 'show_list'
+  include Mixins::GenericShowMixin
+  include Mixins::GenericListMixin
+  include Mixins::GenericSessionMixin
+
+  def self.display_methods
+    %w(instances)
+  end
+
+  def show_list
+    process_show_list(:dbname => :authkeypaircloud)
   end
 
   def breadcrumb_name(_model)
@@ -141,50 +149,6 @@ class AuthKeyPairCloudController < ApplicationController
     end
   end
 
-  def show
-    @display = params[:display] || "main" unless control_selected?
-
-    @lastaction = "show"
-    @showtype = "main"
-    @auth_key_pair_cloud = @record = identify_record(params[:id])
-    return if record_no_longer_exists?(@auth_key_pair_cloud)
-
-    @gtl_url = "/show"
-    drop_breadcrumb(
-      {:name => _("Key Pairs"), :url => "/auth_key_pair_cloud/show_list?page=#{@current_page}&refresh=y"},
-      true
-    )
-
-    case @display
-    when "download_pdf", "main", "summary_only"
-      get_tagdata(@auth_key_pair_cloud)
-      drop_breadcrumb(
-        :name => _("%{name} (Summary)") % {:name => @auth_key_pair_cloud.name},
-        :url  => "/auth_key_pair_cloud/show/#{@auth_key_pair_cloud.id}"
-      )
-      @showtype = "main"
-      set_summary_pdf_data if %w(download_pdf summary_only).include?(@display)
-    when "instances"
-      title = ui_lookup(:tables => 'vm_cloud')
-      kls   = ManageIQ::Providers::CloudManager::Vm
-      drop_breadcrumb(
-        :name => _("%{name} (All %{title})") % {:name => @auth_key_pair_cloud.name, :title => title},
-        :url  => "/auth_key_pair_cloud/show/#{@auth_key_pair_cloud.id}?display=instances"
-      )
-      @view, @pages = get_view(kls, :parent => @auth_key_pair_cloud) # Get the records (into a view) and the paginator
-      @showtype = @display
-    end
-
-    # Came in from outside show_list partial
-    if params[:ppsetting] || params[:searchtag] || params[:entry] || params[:sort_choice]
-      replace_gtl_main_div
-    end
-  end
-
-  def show_list
-    process_show_list(:dbname => :authkeypaircloud)
-  end
-
   # delete selected auth key pairs
   def delete_auth_key_pairs
     assert_privileges("auth_key_pair_cloud_delete")
@@ -248,24 +212,6 @@ class AuthKeyPairCloudController < ApplicationController
     end
     add_flash(n_("Delete initiated for %{number} Key Pair",
                  "Delete initiated for %{number} Key Pairs", key_pairs.length) % {:number => key_pairs.length})
-  end
-
-  private
-
-  def get_session_data
-    @title      = _("Key Pair")
-    @layout     = "auth_key_pair_cloud"
-    @lastaction = session[:auth_key_pair_cloud_lastaction]
-    @display    = session[:auth_key_pair_cloud_display]
-    @filters    = session[:auth_key_pair_cloud_filters]
-    @catinfo    = session[:auth_key_pair_cloud_catinfo]
-  end
-
-  def set_session_data
-    session[:auth_key_pair_cloud_lastaction] = @lastaction
-    session[:auth_key_pair_cloud_display]    = @display unless @display.nil?
-    session[:auth_key_pair_cloud_filters]    = @filters
-    session[:auth_key_pair_cloud_catinfo]    = @catinfo
   end
 
   menu_section :clo
