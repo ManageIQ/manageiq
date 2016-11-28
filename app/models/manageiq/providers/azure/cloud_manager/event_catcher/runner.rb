b4fcc38ed3637aadfc854c94f453255f72ef3a70
Greg Blomquist <blomquisg@gmail.com>
2016-11-28 13:04:27 -0500

    Merge pull request #12 from bronaghs/azure_filter_event_classicvms
    Filter out Azure events from Classic Providers
    (cherry picked from commit 7852cb9aadb1d0acd2901d01fddbcae3762dad14)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1413655

--- app/models/manageiq/providers/azure/cloud_manager/event_catcher/runner.rb
+++ app/models/manageiq/providers/azure/cloud_manager/event_catcher/runner.rb
@@ -40,7 +40,9 @@ class ManageIQ::Providers::Azure::CloudManager::EventCatcher::Runner <
   end
 
   def filtered?(event)
-    event_type = parse_event_type(event)
-    filtered_events.include?(event_type)
+    event_type    = parse_event_type(event)
+    provider_type = event["resourceProviderName"]["value"]
+
+    @ems.blacklisted_provider_types.match(provider_type) || filtered_events.include?(event_type)
   end
 end
