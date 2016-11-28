7e7ccd4d08ef6e919deb8c2284c739a3f3df7e64
yaacov <kobi.zamir@gmail.com>
2016-11-28 15:44:14 +0200

    move from datepicker to date-timepicker widget

--- app/assets/javascripts/controllers/container_live_dashboard/container_live_dashboard_controller.js
+++ app/assets/javascripts/controllers/container_live_dashboard/container_live_dashboard_controller.js
@@ -25,14 +25,12 @@ miqHttpInject(angular.module('containerLiveDashboard', ['ui.bootstrap', 'pattern
 
     $scope.timeFilter = {
       time_range: 24,
-      range_count: 2,
-      date: new Date()
+      range_count: 1,
+      date: moment()
     };
 
     $scope.dateOptions = {
-      autoclose: true,
-      todayHighlight: true,
-      orientation: 'bottom'
+      format: __('MM/DD/YYYY HH:mm')
     };
 
     $scope.countDecrement = function() {
@@ -54,6 +52,7 @@ miqHttpInject(angular.module('containerLiveDashboard', ['ui.bootstrap', 'pattern
       axis         : {
         x: {
           tick: {
+            count: 25,
             format: function (value) { return moment(value).utc().format(__('MM/DD/YYYY HH:mm')); }
           }},
         y: {
@@ -219,10 +218,10 @@ miqHttpInject(angular.module('containerLiveDashboard', ['ui.bootstrap', 'pattern
 
     $scope.refresh_graph = function(metric_id, n) {
       // TODO: replace with a datetimepicker, until then add 24 hours to the date
-      var ends = $scope.timeFilter.date.getTime() + 24 * 60 * 60 * 1000;
+      var ends = $scope.timeFilter.date.valueOf() + 24 * 60 * 60;
       var diff = $scope.timeFilter.time_range * $scope.timeFilter.range_count * 60 * 60 * 1000; // time_range is in hours
       var starts = ends - diff;
-      var bucket_duration = diff / 1000 / 100; // bucket duration is in seconds
+      var bucket_duration = parseInt(diff / 1000 / 200); // bucket duration is in seconds
       var params = '&query=get_data&metric_id=' + metric_id + '&ends=' + ends + 
                    '&starts=' + starts+ '&bucket_duration=' + bucket_duration + 's';
 
