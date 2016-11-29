d6ac0b5c7fd7d77f662fd0b9c927541df05db7b8
Jason Frey <jfrey@redhat.com>
2016-11-28 22:31:04 -0500

    Remove constants so they can be cleaned up.

--- config/routes.rb
+++ config/routes.rb
@@ -5,46 +5,48 @@ Vmdb::Application.routes.draw do
   # Let's serve pictures directly from the DB
   get '/pictures/:basename' => 'picture#show', :basename => /[\da-zA-Z]+\.[\da-zA-Z]+/
 
-  # Enablement for the REST API
+  #
+  # REST API
+  #
 
-  # Semantic Versioning Regex for API, i.e. vMajor.minor.patch[-pre]
-  API_VERSION_REGEX = /v[\d]+(\.[\da-zA-Z]+)*(\-[\da-zA-Z]+)?/ unless defined?(API_VERSION_REGEX)
+  api_actions = {
+    :get     => "show",
+    :post    => "update",
+    :put     => "update",
+    :patch   => "update",
+    :delete  => "destroy",
+    :options => "options"
+  }
 
-  namespace :api, :path => "api(/:version)", :version => API_VERSION_REGEX, :defaults => {:format => "json"} do
+  api_namespace_options = {
+    :path        => "api(/:version)",
+    :defaults    => {:format  => "json"},
+    :constraints => {:version => Api::ApiConfig.version.route_regex}
+  }
+
+  namespace :api, api_namespace_options do
     root :to => "api#index"
     match "/", :to => "api#options", :via => :options
 
-    unless defined?(API_ACTIONS)
-      API_ACTIONS = {
-        :get    => "show",
-        :post   => "update",
-        :put    => "update",
-        :patch  => "update",
-        :delete => "destroy",
-        :options => "options"
-      }.freeze
-    end
-
     Api::ApiConfig.collections.each do |collection_name, collection|
-      # OPTIONS action for each collection
       match collection_name.to_s, :controller => collection_name, :action => :options, :via => :options
 
       scope collection_name, :controller => collection_name do
         collection.verbs.each do |verb|
-          root :action => API_ACTIONS[verb], :via => verb if collection.options.include?(:primary)
+          root :action => api_actions[verb], :via => verb if collection.options.include?(:primary)
 
           next unless collection.options.include?(:collection)
 
           if collection.options.include?(:arbitrary_resource_path)
-            match "(/*c_suffix)", :action => API_ACTIONS[verb], :via => verb
+            match "(/*c_suffix)", :action => api_actions[verb], :via => verb
           else
-            match "(/:c_id)", :action => API_ACTIONS[verb], :via => verb
+            match "(/:c_id)", :action => api_actions[verb], :via => verb
           end
         end
 
         Array(collection.subcollections).each do |subcollection_name|
           Api::ApiConfig.collections[subcollection_name].verbs.each do |verb|
-            match("/:c_id/#{subcollection_name}(/:s_id)", :action => API_ACTIONS[verb], :via => verb)
+            match("/:c_id/#{subcollection_name}(/:s_id)", :action => api_actions[verb], :via => verb)
           end
         end
       end
