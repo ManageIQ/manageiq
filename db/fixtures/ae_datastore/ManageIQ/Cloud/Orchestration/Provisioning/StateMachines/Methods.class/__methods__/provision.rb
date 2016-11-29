b30826ddd01aa3ef162056ea750bc4e5aaa1c987
Patrik Kománek <pkomanek@redhat.com>
2016-11-29 15:15:42 +0100

    Refactoring the provision method.

--- db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Provisioning/StateMachines/Methods.class/__methods__/provision.rb
+++ db/fixtures/ae_datastore/ManageIQ/Cloud/Orchestration/Provisioning/StateMachines/Methods.class/__methods__/provision.rb
@@ -1,18 +1,41 @@
 #
 # Description: This method launches the orchestration provisioning job
 #
+module ManageIQ
+  module Automate
+    module Cloud
+      module Orchestration
+        module Provisioning
+          module StateMachines
+            class Provision
+              def initialize(handle = $evm)
+                @handle = handle
+              end
 
-$evm.log("info", "Starting Orchestration Provisioning")
+              def main
+                @handle.log("info", "Starting Orchestration Provisioning")
 
-task = $evm.root["service_template_provision_task"]
-service = task.destination
+                task = @handle.root["service_template_provision_task"]
+                service = task.destination
 
-begin
-  stack = service.deploy_orchestration_stack
-  $evm.log("info", "Stack #{service.stack_name} with reference id (#{stack.ems_ref}) is being created")
-rescue => err
-  $evm.root['ae_result'] = 'error'
-  $evm.root['ae_reason'] = err.message
-  task.miq_request.user_message = err.message.truncate(255)
-  $evm.log("error", "Stack #{service.stack_name} creation failed. Reason: #{err.message}")
+                begin
+                  stack = service.deploy_orchestration_stack
+                  @handle.log("info", "Stack #{service.stack_name} with reference id (#{stack.ems_ref}) is being created")
+                rescue => err
+                  @handle.root['ae_result'] = 'error'
+                  @handle.root['ae_reason'] = err.message
+                  task.miq_request.user_message = err.message.truncate(255)
+                  @handle.log("error", "Stack #{service.stack_name} creation failed. Reason: #{err.message}")
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  ManageIQ::Automate::Cloud::Orchestration::Provisioning::StateMachines::Provision.new.main
 end
