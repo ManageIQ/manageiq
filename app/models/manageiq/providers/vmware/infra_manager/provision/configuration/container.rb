bf6afbd3ae4f99c561f90f5b617283a5c1b28f2a
Greg Blomquist <blomquisg@gmail.com>
2016-11-30 10:16:16 -0500

    Merge pull request #12450 from agrare/vmware/infra/bz_1384122_vm_memory_reservation
    Disable MemoryReserveLockedToMax
    (cherry picked from commit df363f1bde88d5c9850e3a7a67b343924b5df96b)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1496597

--- app/models/manageiq/providers/vmware/infra_manager/provision/configuration/container.rb
+++ app/models/manageiq/providers/vmware/infra_manager/provision/configuration/container.rb
@@ -114,6 +114,13 @@ module ManageIQ::Providers::Vmware::InfraManager::Provision::Configuration::Cont
         set_spec_option(rai, :limit,       :memory_limit,   nil, :to_i)
         set_spec_option(rai, :reservation, :memory_reserve, nil, :to_i)
       end
+
+      # Only explicitly disable #MemoryReservationLockedToMax if the memory reserve
+      # is less than the total vm memory
+      if get_option(:memory_reserve).to_i < get_option(:vm_memory).to_i
+        # memoryReservationLockedToMax was added in vSphere 5.0
+        vmcs.memoryReservationLockedToMax = false if source.ext_management_system.api_version >= '5.0'
+      end
     end
 
     _log.info("Calling VM reconfiguration")
