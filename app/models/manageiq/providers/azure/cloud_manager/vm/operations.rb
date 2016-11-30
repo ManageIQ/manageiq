6c2d0c9486f82cc9e6a60a87397b5ebeab1c420a
Greg Blomquist <blomquisg@gmail.com>
2016-11-30 14:21:23 -0500

    Merge pull request #14 from bronaghs/delete_vm_and_associated_resources
    Delete VM associated resources (NIC, IP, VHD, .status)
    (cherry picked from commit ce3dbbf6bc896a35d94e767a6b746bd55cf92b65)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1403019

--- app/models/manageiq/providers/azure/cloud_manager/vm/operations.rb
+++ app/models/manageiq/providers/azure/cloud_manager/vm/operations.rb
@@ -6,7 +6,7 @@ module ManageIQ::Providers::Azure::CloudManager::Vm::Operations
     unless ext_management_system
       raise _("VM has no %{table}, unable to destroy VM") % {:table => ui_lookup(:table => "ext_management_systems")}
     end
-    provider_service.delete(name, resource_group)
+    provider_service.delete_associated_resources(name, resource_group)
     update_attributes!(:raw_power_state => "Deleting")
   end
 end
