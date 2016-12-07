7fae87bd160c9d2af0e3321ac897d2dbfd7824c2
Attila Vecerek <avecerek@redhat.com>
2016-12-07 11:37:13 +0100

    Remove buttons from #hide_button? that do not render when the layout condition evals as true

--- app/helpers/application_helper/toolbar_builder.rb
+++ app/helpers/application_helper/toolbar_builder.rb
@@ -503,9 +503,8 @@ class ApplicationHelper::ToolbarBuilder
                    %w(instance_check_compliance instance_compare).include?(id)
 
     # don't hide view buttons in toolbar
-    return false if %w(view_grid view_tile view_list view_dashboard view_summary view_topology
-      refresh_log fetch_log common_drift download_text download_csv download_pdf download_view
-      vm_download_pdf).include?(id) && !%w(miq_policy_rsop ops).include?(@layout)
+    return false if %w(refresh_log fetch_log common_drift download_view).include?(id) &&
+                    !%w(miq_policy_rsop ops).include?(@layout)
 
     # dont hide back to summary button button when not in explorer
     return false if id == "show_summary" && !@explorer
