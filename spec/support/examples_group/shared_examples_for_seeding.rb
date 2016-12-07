db5b9990aafddb9d72b949b7ff2698db0a244f68
Šimon Lukašík <isimluk@fedoraproject.org>
2016-12-07 14:13:41 +0100

    Remove default consumption admin user
    We offer consumption-role. Anyone who has a use-case to oversee the
    consumption should create their own user based on the consumption role.

--- spec/support/examples_group/shared_examples_for_seeding.rb
+++ spec/support/examples_group/shared_examples_for_seeding.rb
@@ -6,7 +6,7 @@ shared_examples_for ".seed called multiple times" do |count = 1|
 end
 
 shared_examples_for "seeding users with" do |klasses|
-  let(:users) { {"admin" => "super_administrator", "consumption_admin" => "consumption_administrator"} }
+  let(:users) { {'admin' => 'super_administrator' } }
 
   it "seeds users #{klasses.present? ? 'with' : ''} #{klasses.collect(&:to_s).join(', ')}" do
     klasses.push(User)
