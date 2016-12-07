4254abe7f7fb8a98277a373eceb0d3cc41419c82
Gregg Tanzillo <gtanzill@redhat.com>
2016-12-07 13:36:42 -0500

    Select chargeback rates deterministically
    There is a case that more rates can be selected
    (for example when storage have more tags)
    and in this case, values are accumulated together in report.
    
    so this first way how to deterministically select just one rate
    of storage and compute.
    
    So when more rates are selected then
    rate with matching tag of VM is used.

--- app/models/chargeback/rates_cache.rb
+++ app/models/chargeback/rates_cache.rb
@@ -4,15 +4,38 @@ class Chargeback
       # we need to select ChargebackRates for groups of MetricRollups records
       # and rates are selected by first MetricRollup record
       @rates ||= {}
-      @rates[consumption.hash_features_affecting_rate] ||=
-        ChargebackRate.get_assigned_for_target(consumption.resource,
-                                               :tag_list => consumption.tag_list_with_prefix,
-                                               :parents  => consumption.parents_determining_rate)
-      if consumption.resource_type == Container.name && @rates[consumption.hash_features_affecting_rate].empty?
-        @rates[consumption.hash_features_affecting_rate] = [ChargebackRate.find_by(
-          :description => 'Default Container Image Rate', :rate_type => 'Compute')]
+      @rates[consumption.hash_features_affecting_rate] ||= rates(consumption)
+    end
+
+    private
+
+    def rates(consumption)
+      rates = ChargebackRate.get_assigned_for_target(consumption.resource,
+                                                     :tag_list => consumption.tag_list_with_prefix,
+                                                     :parents  => consumption.parents_determining_rate)
+
+      if consumption.resource_type == Container.name && rates.empty?
+        rates = [ChargebackRate.find_by(:description => 'Default Container Image Rate', :rate_type => 'Compute')]
       end
-      @rates[consumption.hash_features_affecting_rate]
+
+      metric_rollup_record_tags = consumption.tag_names
+      unique_rates_by_tagged_resources(rates, metric_rollup_record_tags)
+    end
+
+    def unique_rates_by_tagged_resources(rates, metric_rollup_record_tags)
+      grouped_rates = rates.group_by(&:rate_type)
+
+      compute_rates = select_rate_by_tags(grouped_rates["Compute"] || [], metric_rollup_record_tags)
+      storage_rates = select_rate_by_tags(grouped_rates["Storage"] || [], metric_rollup_record_tags)
+
+      [compute_rates, storage_rates].flatten
+    end
+
+    def select_rate_by_tags(rates, metric_rollup_record_tags)
+      return rates if rates.empty? || rates.count == 1
+      return rates unless rates.all?(&:assigned_tags?) # Are rates assigned to tagged resources ?
+
+      rates.sort_by(&:description).detect { |rate| (rate.assigned_tags & metric_rollup_record_tags).present? }
     end
   end
 end
