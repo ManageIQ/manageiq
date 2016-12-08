30382adb2cae91f18aef71cc0cad6d177b55a12d
Attila Vecerek <avecerek@redhat.com>
2016-12-08 12:27:10 +0100

    Refactor #calculate_properties and simplifies the logic applied in #disabled?

--- app/helpers/application_helper/button/server_promote.rb
+++ app/helpers/application_helper/button/server_promote.rb
@@ -1,14 +1,9 @@
 class ApplicationHelper::Button::ServerPromote < ApplicationHelper::Button::ServerLevelOptions
   needs :@record, :@sb
 
-  def calculate_properties
-    super
-    self[:title] = @error_message if disabled?
-  end
-
   def disabled?
     @error_message = if @record.master_supported?
-                       if (@record.priority != 1 && @record.priority != 2) || @record.priority == 2
+                       if @record.priority != 1
                          if @view_context.x_node != "root" && @record.server_role.regional_role?
                            _("This role can only be managed at the Region level")
                          end
