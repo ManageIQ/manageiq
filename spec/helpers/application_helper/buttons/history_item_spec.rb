b9237bb63abfa1f8703c99232097dda2e5381c4a
Attila Vecerek <avecerek@redhat.com>
2016-12-08 16:22:16 +0100

    Refactor HistoryItem spec file and add examples for testing #disabled?

--- spec/helpers/application_helper/buttons/history_item_spec.rb
+++ spec/helpers/application_helper/buttons/history_item_spec.rb
@@ -1,32 +1,49 @@
 describe ApplicationHelper::Button::HistoryItem do
-  describe '#visible?' do
-    subject do
-      sandbox = @sandbox || {
-        :history     => {:testing => %w(some thing to test with)},
-        :active_tree => :testing
-      }
+  let(:sandbox) do
+    {:history     => {:testing => testing_history},
+     :active_tree => :testing}
+  end
+  let(:view_context) { setup_view_context_with_sandbox(sandbox) }
+  let(:button) { described_class.new(view_context, {}, {}, {:id => id}) }
 
-      view_context = setup_view_context_with_sandbox(sandbox)
-      button = described_class.new(view_context, {}, {}, :id => @id)
-      button.visible?
-    end
+  describe '#visible?' do
+    let(:testing_history) { %w(some thing to test with) }
+    subject { button.visible? }
 
     %w(1 2 3 4).each do |n|
-      it "when with existing history_#{n}" do
-        @id = "history_#{n}".to_sym
-        expect(subject).to be_truthy
+      context "when with existing history_#{n}" do
+        let(:id) { "history_#{n}".to_sym }
+        it { expect(subject).to be_truthy }
       end
     end
-
-    it "when not history_1 and the tree history not exist" do
-      @id = :history_10
-      expect(subject).to be_falsey
+    context 'when not history_1 and the tree history not exist' do
+      let(:id) { :history_10 }
+      it { expect(subject).to be_falsey }
     end
+  end
 
-    it "when with history_1" do
-      @id = :history_1
-      expect(subject).to be_truthy
+  describe '#disabled?' do
+    subject { button.disabled? }
+
+    context 'when history_item_id == 1' do
+      let(:id) { :history_1 }
+      (0...2).each do |n|
+        context "when x_tree_history.length == #{n}" do
+          let(:testing_history) { [*0...n] }
+          it { expect(subject).to be_truthy }
+        end
+      end
+      (2..4).each do |n|
+        context "when x_tree_history.length == #{n}" do
+          let(:testing_history) { [*0...n] }
+          it { expect(subject).to be_falsey }
+        end
+      end
+    end
+    context 'when history_item_id != 1' do
+      let(:id) { :history_2 }
+      let(:testing_history) { [nil] }
+      it { expect(subject).to be_falsey }
     end
   end
 end
-
