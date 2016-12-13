ffd5781ebdb4355d0470270f33cd00343cf6ba9c
Martin Hradil <mhradil@redhat.com>
2016-12-13 12:21:53 +0000

    Textual summaries - transform in (?<!textual)_summary_helper.rb
    missed these files, same transforms as the rest

--- app/helpers/compliance_summary_helper.rb
+++ app/helpers/compliance_summary_helper.rb
@@ -35,7 +35,7 @@ module ComplianceSummaryHelper
     if @record.number_of(:compliances) == 0
       h[:value] = _("Not Available")
     else
-      h[:image] = "compliance"
+      h[:image] = "100/compliance.png"
       h[:value] = _("Available")
       h[:title] = _("Show Compliance History of this %{model} (Last 10 Checks)") %
                   {:model => ui_lookup(:model => controller.class.model.name)}
