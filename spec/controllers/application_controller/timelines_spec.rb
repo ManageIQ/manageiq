c4a80f8a4f8691840c264a26be6ec48b778897ce
Harpreet Kataria <hkataria@redhat.com>
2016-12-13 11:57:26 -0500

    Show detailed events checkbox should only toggle detailed events.
    "Show detailed events" checkbox of Timelines view should not remove critical events from timeline. Critical events should always show on timeline, checkbox should only toggle display of details evenrts on timeline.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1402487

--- spec/controllers/application_controller/timelines_spec.rb
+++ spec/controllers/application_controller/timelines_spec.rb
@@ -1,17 +1,20 @@
 describe ApplicationController, "#Timelines" do
   describe EmsInfraController do
     context "#tl_chooser" do
-      it "resets timeline options correctly when apply button is pressed" do
-        ems = FactoryGirl.create(:ems_openstack_infra)
+      before do
+        @ems = FactoryGirl.create(:ems_openstack_infra)
         controller.instance_variable_set(:@tl_options,
                                          ApplicationController::Timelines::Options.new)
+      end
+
+      it "resets timeline options correctly when apply button is pressed" do
         options            = assigns(:tl_options)
         dt                 = Time.zone.now
         options.date       = ApplicationController::Timelines::DateOptions.new
         options.date.end   = dt
         options.date.start = dt
 
-        controller.instance_variable_set(:@_params, :id => ems.id, :tl_show => "policy_timeline")
+        controller.instance_variable_set(:@_params, :id => @ems.id, :tl_show => "policy_timeline")
         expect(controller).to receive(:render)
 
         expect(options.date[:start]).to eq(dt)
@@ -25,18 +28,40 @@ describe ApplicationController, "#Timelines" do
       end
 
       it "sets categories for policy timelines correctly" do
-        ems = FactoryGirl.create(:ems_openstack_infra)
-        controller.instance_variable_set(:@tl_options,
-                                         ApplicationController::Timelines::Options.new)
-
         controller.instance_variable_set(:@_params,
-                                         :id            => ems.id,
+                                         :id            => @ems.id,
                                          :tl_show       => "policy_timeline",
                                          :tl_categories => ["VM Operation"])
         expect(controller).to receive(:render)
         controller.send(:tl_chooser)
         options = assigns(:tl_options)
         expect(options.policy[:categories]).to include('VM Operation')
+      end
+
+      it "unchecking Detailed events checkbox of Timelines options should remove them from list of events" do
+        controller.instance_variable_set(:@_params,
+                                         :id            => @ems.id,
+                                         :tl_show       => "timeline",
+                                         :tl_fl_typ     => "critical",
+                                         :tl_categories => ["Power Activity"])
+        expect(controller).to receive(:render)
+        controller.send(:tl_chooser)
+        options = assigns(:tl_options)
+        expect(options.management[:categories][:power][:event_groups]).to include('AUTO_FAILED_SUSPEND_VM')
+        expect(options.management[:categories][:power][:event_groups]).to_not include('PowerOffVM_Task')
+      end
+
+      it "checking Detailed events checkbox of Timelines options should append them to list of events" do
+        controller.instance_variable_set(:@_params,
+                                         :id            => @ems.id,
+                                         :tl_show       => "timeline",
+                                         :tl_fl_typ     => "detail",
+                                         :tl_categories => ["Power Activity"])
+        expect(controller).to receive(:render)
+        controller.send(:tl_chooser)
+        options = assigns(:tl_options)
+        expect(options.management[:categories][:power][:event_groups]).to include('PowerOffVM_Task')
+        expect(options.management[:categories][:power][:event_groups]).to include('AUTO_FAILED_SUSPEND_VM')
       end
     end
   end
