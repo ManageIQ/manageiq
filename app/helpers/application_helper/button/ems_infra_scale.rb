e99b4fedc19237bf9d2cc9bf112ac272854c042f
Attila Večerek <veat24@gmail.com>
2016-12-13 13:33:33 +0100

    Review response: remove redundant #role_allows_feature?

--- app/helpers/application_helper/button/ems_infra_scale.rb
+++ app/helpers/application_helper/button/ems_infra_scale.rb
@@ -1,8 +1,4 @@
 class ApplicationHelper::Button::EmsInfraScale < ApplicationHelper::Button::Basic
-  def role_allows_feature?
-    super && role_allows?(:feature => "ems_infra_scale")
-  end
-
   def visible?
     @record.class == ManageIQ::Providers::Openstack::InfraManager && @record.orchestration_stacks.count != 0
   end
