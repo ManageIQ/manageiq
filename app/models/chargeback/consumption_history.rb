c92740ea631592a3626b88944e87e56614ef4ecc
Šimon Lukašík <isimluk@fedoraproject.org>
2016-12-16 16:36:46 +0100

    Refactor: Extract method: for_report
    (cherry picked from commit 5532ebaa287c1d296b5bdc6f8f6f9152d8c1b644)

--- app/models/chargeback/consumption_history.rb
+++ app/models/chargeback/consumption_history.rb
@@ -4,6 +4,29 @@ class Chargeback
       'v_derived_cpu_total_cores_used' => 'cpu_usage_rate_average'
     }.freeze
 
+    def self.for_report(cb_class, options)
+      base_rollup = base_rollup_scope
+      timerange = options.report_time_range
+      interval_duration = options.duration_of_report_step
+
+      timerange.step_value(interval_duration).each_cons(2) do |query_start_time, query_end_time|
+        records = base_rollup.where(:timestamp => query_start_time...query_end_time, :capture_interval_name => 'hourly')
+        records = cb_class.where_clause(records, options)
+        records = Metric::Helper.remove_duplicate_timestamps(records)
+        next if records.empty?
+        _log.info("Found #{records.length} records for time range #{[query_start_time, query_end_time].inspect}")
+
+        # we are building hash with grouped calculated values
+        # values are grouped by resource_id and timestamp (query_start_time...query_end_time)
+        records.group_by(&:resource_id).each do |_, metric_rollup_records|
+          metric_rollup_records = metric_rollup_records.select { |x| x.resource.present? }
+          consumption = Consumption.new(metric_rollup_records, query_start_time, query_end_time)
+          next if metric_rollup_records.empty?
+          yield(consumption, metric_rollup_records.first)
+        end
+      end
+    end
+
     def self.base_rollup_scope
       base_rollup = MetricRollup.includes(
         :resource           => [:hardware, :tenant, :tags, :vim_performance_states, :custom_attributes,
@@ -21,5 +44,6 @@ class Chargeback
       rate_cols.map! { |x| VIRTUAL_COL_USES[x] || x }.flatten!
       base_rollup.select(*rate_cols)
     end
+    private_class_method :base_rollup_scope
   end
 end
