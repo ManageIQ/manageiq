b234e2a1920d0dd228418440027d5b978c0e0a0a
Šimon Lukašík <isimluk@fedoraproject.org>
2016-12-16 16:23:17 +0100

    Refactor: Extract method: base_rollup_scope

--- app/models/chargeback.rb
+++ app/models/chargeback.rb
@@ -10,30 +10,12 @@ class Chargeback < ActsAsArModel
     :fixed_compute_metric => :integer,
   )
 
-  VIRTUAL_COL_USES = {
-    "v_derived_cpu_total_cores_used" => "cpu_usage_rate_average"
-  }
-
   def self.build_results_for_report_chargeback(options)
     _log.info("Calculating chargeback costs...")
     @options = options = ReportOptions.new_from_h(options)
 
     rates = RatesCache.new
-
-    base_rollup = MetricRollup.includes(
-      :resource           => [:hardware, :tenant, :tags, :vim_performance_states, :custom_attributes, {:container_image => :custom_attributes}],
-      :parent_host        => :tags,
-      :parent_ems_cluster => :tags,
-      :parent_storage     => :tags,
-      :parent_ems         => :tags)
-                              .select(*Metric::BASE_COLS).order("resource_id, timestamp")
-    perf_cols = MetricRollup.attribute_names
-    rate_cols = ChargebackRate.where(:default => true).flat_map do |rate|
-      rate.chargeback_rate_details.map(&:metric).select { |metric| perf_cols.include?(metric.to_s) }
-    end
-
-    rate_cols.map! { |x| VIRTUAL_COL_USES.include?(x) ? VIRTUAL_COL_USES[x] : x }.flatten!
-    base_rollup = base_rollup.select(*rate_cols)
+    base_rollup = ConsumptionHistory.base_rollup_scope
 
     timerange = options.report_time_range
     data = {}
