456486a93578a675147374ade658b915f21b870c
Šimon Lukašík <isimluk@fedoraproject.org>
2016-12-16 20:32:23 +0100

    Extract child class: ConsumptionWithRollups
    (cherry picked from commit ab53b67d12fd3368dbb0cfcdce8479a6f886065a)
    
    Conflicts:
    	spec/models/chargeback_vm_spec.rb

---
+++ app/models/chargeback/consumption_with_rollups.rb
@@ -0,0 +1,44 @@
+class Chargeback
+  class ConsumptionWithRollups < Consumption
+    delegate :timestamp, :resource, :resource_id, :resource_name, :resource_type, :parent_ems,
+             :hash_features_affecting_rate, :tag_list_with_prefix, :parents_determining_rate,
+             :to => :first_metric_rollup_record
+
+    def initialize(metric_rollup_records, start_time, end_time)
+      super(start_time, end_time)
+      @rollups = metric_rollup_records
+    end
+
+    def tag_names
+      first_metric_rollup_record.tag_names.split('|')
+    end
+
+    def max(metric)
+      values(metric).max
+    end
+
+    def avg(metric)
+      metric_sum = values(metric).sum
+      metric_sum / hours_in_interval
+    end
+
+    def none?(metric)
+      values(metric).empty?
+    end
+
+    def chargeback_fields_present
+      @chargeback_fields_present ||= @rollups.count(&:chargeback_fields_present?)
+    end
+
+    private
+
+    def values(metric)
+      @values ||= {}
+      @values[metric] ||= @rollups.collect(&metric.to_sym).compact
+    end
+
+    def first_metric_rollup_record
+      @fmrr ||= @rollups.first
+    end
+  end
+end
