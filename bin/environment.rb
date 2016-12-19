d40f17eaf18f807ace99739e9ea78ba86061f6ca
Brandon Dunne <bdunne@redhat.com>
2016-12-19 14:19:26 -0500

    Move bower update logic to Environment#while_updating_bower

--- bin/environment.rb
+++ bin/environment.rb
@@ -18,4 +18,23 @@ module Environment
       FileUtils.cp(APP_ROOT.join(source), file)
     end
   end
+
+  def self.while_updating_bower
+    # Run bower in a thread and continue to do the non-js stuff
+    puts "Updating bower assets in parallel..."
+    bower_thread = Thread.new { update_bower }
+
+    yield
+
+    bower_thread.join
+    puts "Updating bower assets complete."
+  end
+
+  def self.update_bower
+    system!("bower update --allow-root -F --silent --config.analytics=false")
+  end
+
+  def self.system!(*args)
+    system(*args) || abort("\n== Command #{args} failed ==")
+  end
 end
