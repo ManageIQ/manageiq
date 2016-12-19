0476bc3a6ff0953b736e413ea3856664eebcd1c5
Mooli Tayer <mtayer@redhat.com>
2016-12-19 16:20:13 +0200

    Add alert statuses api
    GET /api/alerts
    GET /api/alerts/:id
    
    Adds an api to view MiqAlertStatuses The entity is renamed to `alerts`
    since that is thought to be the most useful name for users. When adding
    MiqAlert in the future the intention is to use the name
    `alert_definitions` - that is consistent with
    miq_event/miq_event_definition.

---
+++ spec/requests/api/alerts_spec.rb
@@ -0,0 +1,47 @@
+describe "Alerts API" do
+  let(:alert_definition) { FactoryGirl.create(:miq_alert, :severity => "info") }
+
+  it "forbids access to alerts list without an appropriate role" do
+    api_basic_authorize
+    run_get(alerts_url)
+    expect(response).to have_http_status(:forbidden)
+  end
+
+  it "reads 2 alerts as a collection" do
+    api_basic_authorize collection_action_identifier(:alerts, :read, :get)
+    alert_statuses = FactoryGirl.create_list(:miq_alert_status, 2)
+    run_get(alerts_url)
+    expect(response).to have_http_status(:ok)
+    expect(response.parsed_body).to eq(
+      "name"      => "alerts",
+      "count"     => 2,
+      "subcount"  => 2,
+      "resources" => [
+        {
+          "href" => "http://www.example.com/api/alerts/#{alert_statuses[0].id}"
+        },
+        {
+          "href" => "http://www.example.com/api/alerts/#{alert_statuses[1].id}"
+        }
+      ]
+    )
+  end
+
+  it "forbids access to an alert resource without an appropriate role" do
+    api_basic_authorize
+    alert_status = FactoryGirl.create(:miq_alert_status)
+    run_get(alerts_url(alert_status.id))
+    expect(response).to have_http_status(:forbidden)
+  end
+
+  it "reads an alert as a resource" do
+    api_basic_authorize action_identifier(:alerts, :read, :resource_actions, :get)
+    alert_status = FactoryGirl.create(:miq_alert_status)
+    run_get(alerts_url(alert_status.id))
+    expect(response).to have_http_status(:ok)
+    expect(response.parsed_body).to have_attributes(
+      "href" => "http://www.example.com/api/alerts/#{alert_status.id}",
+      "id"   => alert_status.id
+    )
+  end
+end
