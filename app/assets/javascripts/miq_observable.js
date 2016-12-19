cc7a17b51ce221e59263e1f2ad9e8da415ac4144
Martin Hradil <mhradil@redhat.com>
2016-12-19 21:09:12 +0000

    miq_observable - add listenToRx

--- app/assets/javascripts/miq_observable.js
+++ app/assets/javascripts/miq_observable.js
@@ -3,3 +3,7 @@ ManageIQ.angular.rxSubject = new Rx.Subject();
 function sendDataWithRx(data) {
   ManageIQ.angular.rxSubject.onNext(data);
 }
+
+function listenToRx(callback) {
+  ManageIQ.angular.rxSubject.subscribe(callback);
+}
