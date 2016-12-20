622ced2797bf7bc77b49941eb0f9a6ec36d7d548
Sam Lucidi <slucidi@redhat.com>
2016-12-20 14:56:11 -0500

    Fix i18n for attaching/detaching cloud volumes

--- app/helpers/application_helper/button/instance_detach.rb
+++ app/helpers/application_helper/button/instance_detach.rb
@@ -1,11 +1,7 @@
 class ApplicationHelper::Button::InstanceDetach < ApplicationHelper::Button::Basic
   def disabled?
     if @record.number_of(:cloud_volumes).zero?
-      @error_message = _("This %{model} has no attached %{volumes}.") % {
-        :model   => ui_lookup(:table => 'vm_cloud'),
-        :name    => @record.name,
-        :volumes => ui_lookup(:tables => 'cloud_volumes')
-      }
+      @error_message = _("This Instance has no attached Cloud Volumes.")
     end
     @error_message.present?
   end
