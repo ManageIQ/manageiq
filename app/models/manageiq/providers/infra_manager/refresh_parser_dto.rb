bde69ba88e998a66a3c8c2beee97ac91b5fe61d3
Adam Grare <agrare@redhat.com>
2016-12-21 10:15:49 -0500

    Allow adding dto_collections without needing @data

--- app/models/manageiq/providers/infra_manager/refresh_parser_dto.rb
+++ app/models/manageiq/providers/infra_manager/refresh_parser_dto.rb
@@ -1,2 +1,11 @@
 class ManageIQ::Providers::InfraManager::RefreshParserDto < ::ManagerRefresh::RefreshParserDto
+    def add_dto_collection(model_class, association, manager_ref = nil)
+      delete_method = model_class.new.respond_to?(:disconnect_inv) ? :disconnect_inv : nil
+
+      ::ManagerRefresh::DtoCollection.new(model_class,
+                                          :parent        => @ems,
+                                          :association   => association,
+                                          :manager_ref   => manager_ref,
+                                          :delete_method => delete_method)
+    end
 end
