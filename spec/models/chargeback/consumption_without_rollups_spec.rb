44aa15beb975b32ec53e118a1d262ce8fb6f1c48
Šimon Lukašík <isimluk@fedoraproject.org>
2017-01-02 14:42:41 +0100

    Unit test for ConsumptionWithoutRollups
    (cherry picked from commit f3a2d6eba57ed52adbae5b8e7bb08711b95df32c)

---
+++ spec/models/chargeback/consumption_without_rollups_spec.rb
@@ -0,0 +1,21 @@
+describe Chargeback::ConsumptionWithoutRollups do
+  let(:cores) { 7 }
+  let(:mem_mb) { 1777 }
+  let(:disk_size) { 12_345 }
+  let(:hardware) do
+    FactoryGirl.build(:hardware,
+                      :cpu_total_cores => cores,
+                      :memory_mb       => mem_mb,
+                      :disks           => [FactoryGirl.build(:disk, :size => disk_size)])
+  end
+  let(:vm) { FactoryGirl.build(:vm_microsoft, :hardware => hardware) }
+  let(:consumption) { described_class.new(vm, nil, nil) }
+
+  describe '#avg' do
+    it 'returns current values' do
+      expect(consumption.avg('derived_vm_numvcpus')).to eq(cores)
+      expect(consumption.avg('derived_memory_available')).to eq(mem_mb)
+      expect(consumption.avg('derived_vm_allocated_disk_storage')).to eq(disk_size)
+    end
+  end
+end
