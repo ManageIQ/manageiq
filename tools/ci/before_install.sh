a58218b093023c5ab29158044178fd16845f46aa
Dávid Halász <dhalasz@redhat.com>
2017-01-03 14:45:44 +0100

    Remove setup_js_env.sh and its calls as it has been moved to the UI repo

--- tools/ci/before_install.sh
+++ tools/ci/before_install.sh
@@ -3,9 +3,4 @@ set -v
 source $TRAVIS_BUILD_DIR/tools/ci/setup_vmdb_configs.sh
 source $TRAVIS_BUILD_DIR/tools/ci/setup_ruby_env.sh
 
-# suites that need bower assets to work: javascript, vmdb
-if [[ "$TEST_SUITE" = "javascript" ]] || [[ "$TEST_SUITE" = "vmdb" ]]; then
-  source $TRAVIS_BUILD_DIR/tools/ci/setup_js_env.sh
-fi
-
 set +v
