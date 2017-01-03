2ab5b9a3f1e6de96857e030fbded2bfc3a05da3e
Brandon Dunne <bdunne@redhat.com>
2017-01-03 17:03:24 -0500

    Fix requires for MiqAeMethodService::MiqAeService

--- lib/miq_automation_engine/engine/miq_ae_method_service.rb
+++ lib/miq_automation_engine/engine/miq_ae_method_service.rb
@@ -1,4 +1,5 @@
 require_relative 'miq_ae_method_service/deprecation'
+require_relative 'miq_ae_method_service/miq_ae_service'
 require_relative 'miq_ae_method_service/miq_ae_service_front'
 require_relative 'miq_ae_method_service/miq_ae_service_object'
 module MiqAeMethodService
