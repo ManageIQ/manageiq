61d2bd43df0ae17110708bc587704b59e8d3b3d3
Brandon Dunne <bdunne@redhat.com>
2017-01-03 17:23:11 -0500

    Move MiqAeMethodService::MiqAeServiceModelBase to the correct path

--- lib/miq_automation_engine/engine/miq_ae_method_service.rb
+++ lib/miq_automation_engine/engine/miq_ae_method_service.rb
@@ -2,6 +2,7 @@ require_relative 'miq_ae_method_service/deprecation'
 require_relative 'miq_ae_method_service/miq_ae_service'
 require_relative 'miq_ae_method_service/miq_ae_service_converter'
 require_relative 'miq_ae_method_service/miq_ae_service_front'
+require_relative 'miq_ae_method_service/miq_ae_service_model_base'
 require_relative 'miq_ae_method_service/miq_ae_service_object'
 module MiqAeMethodService
 end
