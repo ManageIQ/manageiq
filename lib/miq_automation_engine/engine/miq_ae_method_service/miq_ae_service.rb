09cfaba09a8f77ca8dda49ceaf20ee5fbe319dd8
Brandon Dunne <bdunne@redhat.com>
2017-01-03 17:40:51 -0500

    Move MiqAeMethodService::MiqAeServiceRbac to the correct path

--- lib/miq_automation_engine/engine/miq_ae_method_service/miq_ae_service.rb
+++ lib/miq_automation_engine/engine/miq_ae_method_service/miq_ae_service.rb
@@ -1,6 +1,6 @@
 require_relative './miq_ae_service_model_legacy'
 require_relative './miq_ae_service_vmdb'
-require_relative '../miq_ae_service/miq_ae_service_rbac'
+require_relative './miq_ae_service_rbac'
 module MiqAeMethodService
   class MiqAeService
     include Vmdb::Logging
