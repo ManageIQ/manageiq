ab3412b62f7cdf5a1272683da409a42e6ede0fdd
d-m-u <drewuhlmann@gmail.com>
2017-01-04 10:27:43 -0700

    Removes files split off in classic ui

--- app/models/dialog_field_drop_down_list.rb
+++ app/models/dialog_field_drop_down_list.rb
@@ -3,6 +3,14 @@ class DialogFieldDropDownList < DialogFieldSortedItem
     !!show_refresh_button
   end
 
+  def multi_value?
+    return true if options[:force_multi_value].present? && options[:force_multi_value] != "null"
+  end
+
+  def force_multi_value=(setting)
+    options[:force_multi_value] = setting
+  end
+
   def initial_values
     [[nil, "<None>"]]
   end
