914a0d563801259f5864590d255e8852653e4b5c
Martin Hradil <mhradil@redhat.com>
2017-01-04 13:51:38 +0000

    ops_rbac - initialize @sb[:active_rbac_group_tab] when in rbac group detail
    this is used to keep track of the current tab in rbac_group detail
    
    not using `@sb[:active_tab]` because technically, the whole rbac detail is a tab (of one), aand there's code that assumes a `#{@sb[:active_tab]}_tab` partial exists
    
    (cherry picked from commit 62aaf8fd59e0e8c26e582d5a4ff79796be0a8d18)

--- app/controllers/ops_controller.rb
+++ app/controllers/ops_controller.rb
@@ -244,7 +244,16 @@ class OpsController < ApplicationController
     end
 
     if x_active_tree == :rbac_tree
-      x_node_set("root", :rbac_tree) unless x_node(:rbac_tree)
+      node = x_node(:rbac_tree)
+      if node
+        kind = node.split('-').first
+
+        # default to the first tab in group detail
+        @sb[:active_rbac_group_tab] = "rbac_customer_tags" if kind == 'g'
+      else
+        x_node_set("root", :rbac_tree)
+      end
+
       @sb[:active_tab] ||= "rbac_details"
     end
 
@@ -325,6 +334,9 @@ class OpsController < ApplicationController
       end
     when :rbac_tree
       @sb[:active_tab] = "rbac_details"
+
+      # default to the first tab in group detail
+      @sb[:active_rbac_group_tab] ||= "rbac_customer_tags" if node[0] == 'g'
     when :diagnostics_tree
       case node[0]
       when "root"
@@ -335,7 +347,6 @@ class OpsController < ApplicationController
         @sb[:diag_selected_id] = nil
       when "svr"
         @sb[:active_tab] = "diagnostics_summary"
-        svr = MiqServer.find(from_cid(node[1]))
       end
     when :analytics_tree
       @sb[:active_tab] = "analytics_details"
