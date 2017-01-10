014a286fe7bf920645ffb9738bf645f7ccd047a9
Sam Lucidi <slucidi@redhat.com>
2017-01-10 16:42:13 -0500

    Use old calculate_properties method for attach and detach buttons

--- app/helpers/application_helper/button/instance_attach.rb
+++ app/helpers/application_helper/button/instance_attach.rb
@@ -1,4 +1,9 @@
 class ApplicationHelper::Button::InstanceAttach < ApplicationHelper::Button::Basic
+  def calculate_properties
+    super
+    self[:title] = @error_message if disabled?
+  end
+
   def disabled?
     if @record.cloud_tenant.cloud_volumes.where(:status => 'available').count.zero?
       @error_message = _("There are no Cloud Volumes available to attach to this Instance.")
--- app/helpers/application_helper/button/instance_detach.rb
+++ app/helpers/application_helper/button/instance_detach.rb
@@ -1,12 +1,13 @@
 class ApplicationHelper::Button::InstanceDetach < ApplicationHelper::Button::Basic
   def calculate_properties
     super
-    if @record.number_of(:cloud_volumes).zero?
-      self[:title] = _("This Instance has no attached Cloud Volumes.")
-    end
+    self[:title] = @error_message if disabled?
   end
 
   def disabled?
-    @record.number_of(:cloud_volumes).zero?
+    if @record.number_of(:cloud_volumes).zero?
+      @error_message = _("This Instance has no attached Cloud Volumes.")
+    end
+    @error_message.present?
   end
 end
