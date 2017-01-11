e6bff3afe4ed1e40805a13ba647b425883654033
Madhu Kanoor <mkanoor@redhat.com>
2017-01-11 10:01:46 -0500

    Fixed Rubocop warnings

--- spec/lib/miq_automation_engine/miq_ae_object_spec.rb
+++ spec/lib/miq_automation_engine/miq_ae_object_spec.rb
@@ -75,15 +75,14 @@ describe MiqAeEngine::MiqAeObject do
   end
 
   it "#process_args_as_attributes with mixed types and case insensitive" do
-    result = @miq_obj.process_args_as_attributes({"Array::VMs" => "VmOrTemplate::#{@vm.id}",
-                                                  "Name" => "fred"})
+    result = @miq_obj.process_args_as_attributes("Array::VMs" => "VmOrTemplate::#{@vm.id}",
+                                                 "Name"       => "fred")
     expect(result["vms"]).to be_kind_of(Array)
     expect(result["vms"].length).to eq(1)
     expect(result["name"]).to eq("fred")
     expect(result["Name"]).to be_nil
     expect(result["VMs"]).to be_nil
   end
-
 
   it "#process_args_as_attributes with an array" do
     vm2 = FactoryGirl.create(:vm_vmware)
