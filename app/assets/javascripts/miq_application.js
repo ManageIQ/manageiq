d7c7dd1c227d8b5709590a0080ae00236018e861
Milan Zázrivec <mzazrivec@redhat.com>
2017-01-16 13:20:42 +0100

    Merge pull request #146 from h-kataria/no_cofirmation_needed_on_timeline_screen
    Confirmation popup is not needed on timelines screen.
    (cherry picked from commit 3fdb896cc812c11efec4cd318909391136388798)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1414015

--- app/assets/javascripts/miq_application.js
+++ app/assets/javascripts/miq_application.js
@@ -293,7 +293,9 @@ function miqDimDiv(divname, status) {
 // Check for changes and prompt
 function miqCheckForChanges() {
   if (ManageIQ.angular.scope) {
-    if (angular.isDefined(ManageIQ.angular.scope.angularForm) && ManageIQ.angular.scope.angularForm.$dirty) {
+    if (angular.isDefined(ManageIQ.angular.scope.angularForm) &&
+      ManageIQ.angular.scope.angularForm.$dirty &&
+      !miqDomElementExists('ignore_form_changes')) {
       var answer = confirm(__("Abandon changes?"));
       if (answer) {
         ManageIQ.angular.scope.angularForm.$setPristine(true);
