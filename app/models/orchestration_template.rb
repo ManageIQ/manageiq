1760d81011ac128dd040209766b4a23595e68d39
Daniel Berger <dberger@redhat.com>
2017-01-16 14:04:59 -0700

    Make OrchestrationTemplate self referential.

--- app/models/orchestration_template.rb
+++ app/models/orchestration_template.rb
@@ -8,6 +8,9 @@ class OrchestrationTemplate < ApplicationRecord
 
   has_many :stacks, :class_name => "OrchestrationStack"
 
+  belongs_to :parent, :class_name => "OrchestrationTemplate"
+  has_many :children, :class_name => "OrchestrationTemplate", :foreign_key => 'parent_id'
+
   default_value_for :draft, false
   default_value_for :orderable, true
 
