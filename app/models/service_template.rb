a729022a10d7662859da6f4d1c0c055bb00c0d45
Harpreet Kataria <hkataria@redhat.com>
2017-01-16 12:49:25 -0500

    Moved list of CATALOG_ITEM_TYPES to model.
    https://bugzilla.redhat.com/show_bug.cgi?id=1348239

--- app/models/service_template.rb
+++ app/models/service_template.rb
@@ -10,6 +10,20 @@ class ServiceTemplate < ApplicationRecord
     "storage"         => _("Storage")
   }.freeze
 
+
+  CATALOG_ITEM_TYPES = {
+    "amazon"                => _("Amazon"),
+    "azure"                 => _("Azure"),
+    "generic"               => _("Generic"),
+    "generic_orchestration" => _("Orchestration"),
+    "generic_ansible_tower" => _("AnsibleTower"),
+    "google"                => _("Google"),
+    "microsoft"             => _("SCVMM"),
+    "openstack"             => _("OpenStack"),
+    "redhat"                => _("RHEV"),
+    "vmware"                => _("VMware")
+  }
+
   include ServiceMixin
   include OwnershipMixin
   include NewWithTypeStiMixin
