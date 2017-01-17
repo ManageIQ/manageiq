d163bfd25ff617cba3739ac90194b29d81074cc0
Nick Carboni <ncarboni@redhat.com>
2017-01-17 16:56:53 -0500

    Add additional methods to the EmbeddedAnsible class
    These methods run the setup playbook with particular arguments
    to accomplish configuration and startup at different times.
    
    Also added stop and running? methods to control and monitor the
    services running on the server

--- lib/embedded_ansible.rb
+++ lib/embedded_ansible.rb
@@ -1,6 +1,10 @@
+require "awesome_spawn"
+require "linux_admin"
+
 class EmbeddedAnsible
   APPLIANCE_ANSIBLE_DIRECTORY = "/opt/ansible-installer".freeze
   ANSIBLE_ROLE                = "embedded_ansible".freeze
+  SETUP_SCRIPT                = "#{APPLIANCE_ANSIBLE_DIRECTORY}/setup.sh".freeze
 
   def self.available?
     path = ENV["APPLIANCE_ANSIBLE_DIRECTORY"] || APPLIANCE_ANSIBLE_DIRECTORY
@@ -12,7 +16,30 @@ class EmbeddedAnsible
   end
 
   def self.running?
-    # TODO
-    false
+    services.all? { |service| LinuxAdmin::Service.new(service).running? }
+  end
+
+  def self.configure
+    setup_params = {
+      :e => "minimum_var_space=0",
+      :k => "packages,migrations,supervisor"
+    }
+    AwesomeSpawn.run!(SETUP_SCRIPT, :params => setup_params)
+  end
+
+  def self.start
+    setup_params = {
+      :e => "minimum_var_space=0",
+      :k => "packages,migrations"
+    }
+    AwesomeSpawn.run!(SETUP_SCRIPT, :params => setup_params)
+  end
+
+  def self.stop
+    services.each { |service| LinuxAdmin::Service.new(service).stop.disable }
+  end
+
+  def self.services
+    AwesomeSpawn.run!("source /etc/sysconfig/ansible-tower; echo $TOWER_SERVICES").output.split
   end
 end
--- spec/lib/embedded_ansible_spec.rb
+++ spec/lib/embedded_ansible_spec.rb
@@ -1,3 +1,6 @@
+require "linux_admin"
+require "awesome_spawn"
+
 describe EmbeddedAnsible do
   before do
     ENV["APPLIANCE_ANSIBLE_DIRECTORY"] = nil
@@ -22,6 +25,52 @@ describe EmbeddedAnsible do
       allow(Dir).to receive(:exist?).with("/opt/ansible-installer").and_return(false)
 
       expect(described_class.available?).to be_falsey
+    end
+  end
+
+  context "with services" do
+    let(:nginx_service)       { double("nginx service") }
+    let(:supervisord_service) { double("supervisord service") }
+    let(:rabbitmq_service)    { double("rabbitmq service") }
+
+    before do
+      expect(AwesomeSpawn).to receive(:run!)
+        .with("source /etc/sysconfig/ansible-tower; echo $TOWER_SERVICES")
+        .and_return(double(:output => "nginx supervisord rabbitmq"))
+      allow(LinuxAdmin::Service).to receive(:new).with("nginx").and_return(nginx_service)
+      allow(LinuxAdmin::Service).to receive(:new).with("supervisord").and_return(supervisord_service)
+      allow(LinuxAdmin::Service).to receive(:new).with("rabbitmq").and_return(rabbitmq_service)
+    end
+
+    describe ".running?" do
+      it "returns true when all services are running" do
+        expect(nginx_service).to receive(:running?).and_return(true)
+        expect(supervisord_service).to receive(:running?).and_return(true)
+        expect(rabbitmq_service).to receive(:running?).and_return(true)
+
+        expect(described_class.running?).to be true
+      end
+
+      it "returns false when a service is not running" do
+        expect(nginx_service).to receive(:running?).and_return(true)
+        expect(supervisord_service).to receive(:running?).and_return(false)
+
+        expect(described_class.running?).to be false
+      end
+    end
+
+    describe ".stop" do
+      it "stops all the services" do
+        expect(nginx_service).to receive(:stop).and_return(nginx_service)
+        expect(supervisord_service).to receive(:stop).and_return(supervisord_service)
+        expect(rabbitmq_service).to receive(:stop).and_return(rabbitmq_service)
+
+        expect(nginx_service).to receive(:disable).and_return(nginx_service)
+        expect(supervisord_service).to receive(:disable).and_return(supervisord_service)
+        expect(rabbitmq_service).to receive(:disable).and_return(rabbitmq_service)
+
+        described_class.stop
+      end
     end
   end
 end
