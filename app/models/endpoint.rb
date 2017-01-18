b3738422123e537e63d586415ed2e5c0edd4cbb0
Beni Cherniavsky-Paskin <cben@redhat.com>
2017-01-18 12:00:39 +0200

    Parsing & validation of Endpoint#certificate_authority

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -1,3 +1,5 @@
+require 'openssl'
+
 class Endpoint < ApplicationRecord
   belongs_to :resource, :polymorphic => true
 
@@ -5,6 +7,7 @@ class Endpoint < ApplicationRecord
   validates :verify_ssl, :inclusion => {:in => [OpenSSL::SSL::VERIFY_NONE, OpenSSL::SSL::VERIFY_PEER]}
   validates :port, :numericality => {:only_integer => true, :allow_nil => true, :greater_than => 0}
   validates :url, :uniqueness => true, :if => :url
+  validate :validate_certificate_authority
 
   def verify_ssl=(val)
     val = resolve_verify_ssl_value(val)
@@ -15,6 +18,29 @@ class Endpoint < ApplicationRecord
     verify_ssl != OpenSSL::SSL::VERIFY_NONE
   end
 
+  # From endpoint, falling back to Settings, then to system CA bundle
+  def ssl_cert_store
+    certs = parse_certificate_authority
+    if certs.present?
+      store = OpenSSL::X509::Store.new
+      certs.each do |cert|
+        store.add_cert(cert)
+      end
+      return store
+    end
+
+    file = Settings.ssl.ssl_ca_file
+    path = Settings.ssl.ssl_ca_path
+    if file.present? || path.present?
+      store = OpenSSL::X509::Store.new
+      store.add_file(file) if file.present?
+      store.add_path(path) if path.present?
+      return store
+    end
+
+    nil # use system defaults
+  end
+
   private
 
   def resolve_verify_ssl_value(val)
@@ -23,5 +49,19 @@ class Endpoint < ApplicationRecord
     when false then OpenSSL::SSL::VERIFY_NONE
     else            val
     end
+  end
+
+  # Returns a list, to support concatenated PEM certs.
+  def parse_certificate_authority
+    return [] if certificate_authority.blank?
+    certificate_authority.split(/(?=-----BEGIN)/).reject(&:blank?).collect do |pem_fragment|
+      OpenSSL::X509::Certificate.new(pem_fragment)
+    end
+  end
+
+  def validate_certificate_authority
+    parse_certificate_authority
+  rescue OpenSSL::OpenSSLError => err
+    errors.add(:certificate_authority, err.to_s)
   end
 end
