472a31881e19f41aa4ce5382887cd8bc4a5ff667
Tim Wade <hello@timjwade.com>
2017-01-19 07:07:59 -0800

    No need to set temp locale

--- lib/services/locale_resolver.rb
+++ lib/services/locale_resolver.rb
@@ -3,14 +3,13 @@ class LocaleResolver
     user_locale = (User.current_user.try(:settings) || {}).fetch_path(:display, :locale)
     if user_locale == 'default' || user_locale.nil?
       server_locale = ::Settings.server.locale
-      locale = if server_locale == "default" || server_locale.nil?
-                 headers['Accept-Language']
-               else
-                 server_locale
-               end
+      if server_locale == "default" || server_locale.nil?
+        headers['Accept-Language']
+      else
+        server_locale
+      end
     else
-      locale = user_locale
+      user_locale
     end
-    locale
   end
 end
