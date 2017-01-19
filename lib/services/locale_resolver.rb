e7295488c8c566375d734b4534ec50eb1c7e8d5a
Tim Wade <hello@timjwade.com>
2017-01-19 07:06:53 -0800

    Use fetch_path to get the user locale

--- lib/services/locale_resolver.rb
+++ lib/services/locale_resolver.rb
@@ -1,9 +1,6 @@
 class LocaleResolver
   def self.resolve(headers = {})
-    user_settings = User.current_user.try(:settings)
-    user_locale = user_settings[:display][:locale] if user_settings &&
-                                                      user_settings.key?(:display) &&
-                                                      user_settings[:display].key?(:locale)
+    user_locale = (User.current_user.try(:settings) || {}).fetch_path(:display, :locale)
     if user_locale == 'default' || user_locale.nil?
       server_locale = ::Settings.server.locale
       locale = if server_locale == "default" || server_locale.nil?
