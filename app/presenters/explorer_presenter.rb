c6bfe0065d8810ff8f374b38c45d0a8470784721
Martin Povolny <mpovolny@redhat.com>
2017-01-19 08:31:58 +0100

    Merge pull request #187 from mzazrivec/escape_right_cell_text
    Escape special HTML characters in right cell header
    (cherry picked from commit ea667b83edb498a66eda1eb735cd00ab3bbac651)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1414889

--- app/presenters/explorer_presenter.rb
+++ app/presenters/explorer_presenter.rb
@@ -247,7 +247,7 @@ class ExplorerPresenter
     data[:ajaxUrl] = ajax_action_url(@options[:ajax_action]) if @options[:ajax_action]
     data[:clearGtlListGrid] = !!@options[:clear_gtl_list_grid]
     data[:setVisibility] = @options[:set_visible_elements]
-    data[:rightCellText] = @options[:right_cell_text] if @options[:right_cell_text]
+    data[:rightCellText] = ERB::Util.html_escape(@options[:right_cell_text]) if @options[:right_cell_text]
 
     data[:reloadToolbars] = @options[:reload_toolbars].collect do |_div_name, toolbar|
       toolbar
