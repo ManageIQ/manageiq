e54076bf76cde1a19f7b9539801dae6eaa704abb
Eric Hayes <eric@erichayes.net>
2017-01-18 16:18:40 -0800

    Build tree by counting all descendants
    Currently selected boxes deeper in the tree do not “bubble up” the select state to their ancestors. This changes that by counting all descendants including hidden features, also taking into account 3 possible select states.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1348623
    https://bugzilla.redhat.com/show_bug.cgi?id=1411831

--- spec/controllers/ops_controller/rbac_tree_spec.rb
+++ spec/controllers/ops_controller/rbac_tree_spec.rb
@@ -1,14 +1,46 @@
 describe OpsController::RbacTree do
-  let(:role) do
-    # Pick a small subset of the product features tree to allow the spec to
-    #   exercise building more than a single node
-    FactoryGirl.create(:miq_user_role, :features =>
-      %w(all_vm_rules instance instance_view instance_show_list instance_control instance_scan)
-    )
+  before(:all) do
+    MiqProductFeature.seed_features
   end
 
-  it ".build" do
-    features_tree = described_class.build(role, role.feature_identifiers.sort, false).to_json
-    expect(features_tree).to include("Access Rules for all Virtual Machines")
+  let(:role) do
+    roles = YAML.load_file(MiqUserRole::FIXTURE_YAML)
+    role = roles.detect { |r| r[:name] == "EvmRole-approver" }
+
+    FactoryGirl.create(:miq_user_role, :role => "approver", :features => role[:miq_product_feature_identifiers])
+  end
+
+  let(:features) { role.miq_product_features.order(:identifier).pluck(:identifier) }
+
+  subject { described_class.new(role, features, false).build }
+
+  it 'builds a hash tree' do
+    expect(subject[:children].first[:title]).to eq "Cloud Intel"
+  end
+
+  it 'bubbles select states from child to parent' do
+    expect(subject[:select]).to eq "undefined"
+
+    control = subject[:children].detect { |c| c[:title] == "Control" }
+    explore = control[:children].detect { |c| c[:title] == "Explorer" }
+    imp_exp = control[:children].detect { |c| c[:title] == "Import/Export" }
+
+    expect(control[:select]).to eq "undefined"
+    expect(explore[:select]).to eq "undefined"
+    expect(imp_exp[:select]).to be false
+  end
+
+  context "when read-only" do
+    it 'sets the checkable key to false' do
+      expect(subject[:children].first[:checkable]).to be false
+    end
+  end
+
+  context "when editable" do
+    subject { described_class.new(role, features, true).build }
+
+    it 'sets the checkable key to true' do
+      expect(subject[:children].first[:checkable]).to be true
+    end
   end
 end
