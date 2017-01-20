6e5ae254ff66a1d744ef0cd0b6b09cf96b4f976c
Jason Frey <jfrey@redhat.com>
2017-01-20 11:04:15 -0500

    Fix incorrect adapter commit

--- lib/active_metrics.rb
+++ lib/active_metrics.rb
@@ -1,4 +1,4 @@
+require 'active_metrics/connection_adapters/elasticsearch_adapter'
 require 'active_metrics/connection_adapters/hawkular_metrics_adapter'
 require 'active_metrics/connection_adapters/influxdb_adapter'
-require 'active_metrics/connection_adapters/miq_postgres_adapter'
 require 'active_metrics/base'
