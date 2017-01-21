91d556535619446e16cde15b93a28b40c9ce6b19
Dávid Halász <dhalasz@redhat.com>
2017-01-21 18:29:45 +0100

    Reimplemented WebsocketServer using SurroGate and WebsocketDecorator

--- spec/lib/websocket_server_spec.rb
+++ spec/lib/websocket_server_spec.rb
@@ -1,28 +1,114 @@
 describe WebsocketServer do
-  before(:each) do
-    allow(logger).to receive(:info)
+  before do
+    %i(fatal error warn info debug level).each { |method| allow(logger).to receive(method) }
     @server = described_class.new(:logger => logger)
     Thread.list.reject { |t| t == Thread.current }.each(&:kill)
   end
 
-  let(:sockets) { @server.instance_variable_get(:@sockets) }
-  let(:pairing) { @server.instance_variable_get(:@pairing) }
   let(:logger) { double }
 
-  let(:pipes) { IO.pipe }
-  let(:left) { pipes.first }
-  let(:right) { pipes.last }
-  let(:proxy) { double }
-
   describe '#call' do
+    let(:env) { Hash.new }
     subject { @server.call(env) }
 
-    context 'non-websocket' do
-      let(:env) { Hash.new }
+    context 'invalid request' do
+      before { allow(@server).to receive(:parse_request).and_return(:error) }
 
       it 'returns with a not found error' do
         is_expected.to eq(@server.send(:not_found))
       end
+    end
+
+    context 'ActionCable request' do
+      before { allow(@server).to receive(:parse_request).and_return(:cable) }
+
+      it 'passes the request to ActionCable' do
+        expect(ActionCable.server).to receive(:call).with(env)
+        is_expected.to be_nil
+      end
+    end
+
+    context 'request for a remote console' do
+      before { allow(@server).to receive(:parse_request).and_return('123456') }
+
+      it 'initializes the proxy' do
+        expect(@server).to receive(:init_proxy)
+        is_expected.to be_nil
+      end
+    end
+  end
+
+  describe '#init_proxy' do
+    let(:proxy) { @server.instance_variable_get(:@proxy) }
+    let(:ws) { dup }
+    let(:sock) { dup }
+
+    let(:env) { { 'rack.hijack' => -> { ws } } }
+    let(:url_secret) { '123456' }
+    let(:ssl) { false }
+    let(:driver) do
+      drv = WebSocket::Handshake::Server.new
+      drv.from_rack(env)
+      drv
+    end
+
+    before do
+      FactoryGirl.create(:system_console, :url_secret => '123456', :ssl => ssl)
+      allow(ws).to receive(:close)
+      allow(sock).to receive(:close)
+      allow(driver).to receive(:to_s).and_return("abcd")
+      allow(ws).to receive(:write_nonblock).with("abcd")
+      allow(WebsocketDecorator).to receive(:decorate).with(ws, driver.version).and_return(ws)
+      allow(TCPSocket).to receive(:open).and_return(sock)
+    end
+
+    subject { @server.send(:init_proxy, env, url_secret, driver) }
+
+    context 'console does not exist' do
+      let(:url_secret) { '1234567' }
+
+      it 'returns with not_found' do
+        is_expected.to eq(@server.send(:not_found))
+      end
+    end
+
+    context 'rack.hijack is unsuccessful' do
+      let(:env) { { 'rack.hijack' => -> { raise } } }
+
+      it 'returns with not_found' do
+        is_expected.to eq(@server.send(:not_found))
+      end
+    end
+
+    context 'cannot write to the websocket' do
+      before { allow(ws).to receive(:write_nonblock).and_raise }
+
+      it 'returns with not found' do
+        is_expected.to eq(@server.send(:not_found))
+      end
+    end
+
+    context 'cannot open remote endpoint' do
+      before { allow(TCPSocket).to receive(:open).and_raise }
+
+      it 'returns with not found' do
+        is_expected.to eq(@server.send(:not_found))
+      end
+    end
+
+    context 'SSL is enabled' do
+      let(:ssl) { true }
+
+      it 'sets up SSL' do
+        expect(@server).to receive(:init_ssl).and_return(sock)
+        expect(proxy).to receive(:push).with(sock, ws)
+        is_expected.to eq([-1, {}, []])
+      end
+    end
+
+    it 'sets up proxying' do
+      expect(proxy).to receive(:push).with(sock, ws)
+      is_expected.to eq([-1, {}, []])
     end
   end
 
@@ -38,25 +124,91 @@ describe WebsocketServer do
     end
   end
 
-  describe '#cleanup' do
-    subject { @server.send(:cleanup, error) }
+  describe '#parse_request' do
+    subject { @server.send(:parse_request, env, driver) }
 
-    before(:each) do
-      pairing.merge!(
-        left  => WebsocketServer::Pairing.new(true, proxy),
-        right => WebsocketServer::Pairing.new(false, proxy)
-      )
-      sockets.push(left, right)
-      allow(proxy).to receive(:vm_id).and_return('unknown')
+    before do
+      allow(@server).to receive(:same_origin_as_host?).and_return(same)
     end
 
-    let(:error) { right }
+    context 'invalid websocket request' do
+      let(:env) { Hash.new }
+      let(:same) { true }
+      let(:driver) { OpenStruct.new(:valid? => false, :finished? => true) }
 
-    it 'removes the failed socket' do
-      expect(proxy).to receive(:cleanup)
-      subject
-      expect(sockets).to_not include(right)
-      expect(pairing.keys).to_not include(right)
+      it 'returns with :error' do
+        is_expected.to eq(:error)
+      end
+    end
+
+    context 'unfinished websocket handshake' do
+      let(:env) { Hash.new }
+      let(:same) { true }
+      let(:driver) { OpenStruct.new(:valid? => true, :finished? => false) }
+
+      it 'returns with :error' do
+        is_expected.to eq(:error)
+      end
+    end
+
+    context 'not the same origin as host' do
+      let(:env) { Hash.new }
+      let(:same) { false }
+      let(:driver) { OpenStruct.new(:valid? => true, :finished? => true) }
+
+      it 'returns with :error' do
+        is_expected.to eq(:error)
+      end
+    end
+
+    context 'ActionCable request' do
+      let(:env) { {'REQUEST_URI' => '/ws/notifications'} }
+      let(:same) { true }
+      let(:driver) { OpenStruct.new(:valid? => true, :finished? => true) }
+
+      it 'returns with :cable' do
+        is_expected.to eq(:cable)
+      end
+    end
+
+    context 'invalid url' do
+      let(:env) { {'REQUEST_URI' => '/ws/blablabla'} }
+      let(:same) { true }
+      let(:driver) { OpenStruct.new(:valid? => true, :finished? => true) }
+
+      it 'returns with :error' do
+        is_expected.to eq(:error)
+      end
+    end
+
+    context 'console request' do
+      let(:env) { {'REQUEST_URI' => '/ws/console/123456'} }
+      let(:same) { true }
+      let(:driver) { OpenStruct.new(:valid? => true, :finished? => true) }
+
+      it 'returns with the console id' do
+        is_expected.to eq('123456')
+      end
+    end
+  end
+
+  describe '#same_origin_as_host?' do
+    subject { @server.send(:same_origin_as_host?, env) }
+
+    context 'same origin as host' do
+      let(:env) { {'HTTP_ORIGIN' => 'http://manageiq.org', 'HTTP_HOST' => 'manageiq.org'} }
+
+      it 'returns with true' do
+        is_expected.to be_truthy
+      end
+    end
+
+    context 'origin different from the host' do
+      let(:env) { {'HTTP_ORIGIN' => 'http://manageiq.org', 'HTTP_HOST' => 'github.com'} }
+
+      it 'returns with false' do
+        is_expected.to be_falsey
+      end
     end
   end
 end
