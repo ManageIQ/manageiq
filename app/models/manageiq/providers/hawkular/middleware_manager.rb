10d36ceba831e5f2a0a6350a29922694a6e53f35
Greg Blomquist <blomquisg@gmail.com>
2017-01-23 15:49:14 -0500

    Merge pull request #13368 from mtho11/jdbc_drivers_for_feed
    Retrieve middleware JDBC Drivers for a feed.
    (cherry picked from commit c0cef1da6a4fe5f84ec7e780d333810e5105b13e)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1426639

--- app/models/manageiq/providers/hawkular/middleware_manager.rb
+++ app/models/manageiq/providers/hawkular/middleware_manager.rb
@@ -139,6 +139,14 @@ module ManageIQ::Providers
       end
     end
 
+    def jdbc_drivers(feed)
+      with_provider_connection do |connection|
+        path = ::Hawkular::Inventory::CanonicalPath.new(:feed_id          => hawk_escape_id(feed),
+                                                        :resource_type_id => hawk_escape_id('JDBC Driver'))
+        connection.inventory.list_resources_for_type(path.to_s, :fetch_properties => true)
+      end
+    end
+
     def child_resources(resource_path, recursive = false)
       with_provider_connection do |connection|
         connection.inventory.list_child_resources(resource_path, recursive)
