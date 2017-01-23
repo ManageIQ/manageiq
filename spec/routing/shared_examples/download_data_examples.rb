61e7b2924286eccf827a0d6e2b25c81fcd142e20
Martin Hradil <mhradil@redhat.com>
2017-01-23 19:23:56 +0000

    Move ui-specific tests out to manageiq-ui-classic
    requests/* (except for requests/api/*) are definitely ui specific
    so are routing/ specs
    
    as for services, some are definitely ui-specific, others maybe not so much.. but in the end, all the services live in ui-classic now, so so should the specs

--- spec/routing/shared_examples/download_data_examples.rb
+++
@@ -1,7 +0,0 @@
-shared_examples_for "A controller that has download_data routes" do
-  describe "#download_data" do
-    it "routes with GET" do
-      expect(get("/#{controller_name}/download_data")).to route_to("#{controller_name}#download_data")
-    end
-  end
-end
