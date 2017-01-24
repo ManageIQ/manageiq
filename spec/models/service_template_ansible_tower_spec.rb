22fa778e3b60cfc73c3df25b630acd6eee33c257
Jillian Tullo <jtullo@redhat.com>
2017-01-24 16:21:28 -0500

    add validation on config options
    allow for configuration to be passed in

--- spec/models/service_template_ansible_tower_spec.rb
+++ spec/models/service_template_ansible_tower_spec.rb
@@ -14,24 +14,40 @@ describe ServiceTemplateAnsibleTower do
         :config_info  => {
           :configuration_script_id => configuration_script.id,
           :provision               => {
-            :fqname            => ra1.fqname,
-            :service_dialog_id => service_dialog.id
+            :fqname    => ra1.fqname,
+            :dialog_id => service_dialog.id
           },
           :retirement              => {
-            :fqname            => ra2.fqname,
-            :service_dialog_id => service_dialog.id
+            :fqname    => ra2.fqname,
+            :dialog_id => service_dialog.id
           }
         }
       }
     end
 
-    it 'creates and returns a catalog item' do
+    it 'creates and returns an ansible tower catalog item' do
       service_template = ServiceTemplateAnsibleTower.create_catalog_item(catalog_item_options)
+      service_template.reload
 
       expect(service_template.name).to eq('Ansible Tower')
       expect(service_template.service_resources.count).to eq(1)
       expect(service_template.dialogs.first).to eq(service_dialog)
       expect(service_template.resource_actions.pluck(:action)).to include('Provision', 'Retirement')
+      expect(service_template.job_template).to eq(configuration_script)
+    end
+
+    it 'validates the presence of a configuration_script_id or configuration' do
+      catalog_item_options[:config_info].delete(:configuration_script_id)
+
+      expect do
+        ServiceTemplateAnsibleTower.create_catalog_item(catalog_item_options)
+      end.to raise_error(StandardError, 'Must provide configuration_script_id or configuration')
+    end
+
+    it 'accepts a configuration' do
+      catalog_item_options[:config_info] = { :configuration => configuration_script }
+      service_template = ServiceTemplateAnsibleTower.create_catalog_item(catalog_item_options)
+
       expect(service_template.job_template).to eq(configuration_script)
     end
   end
