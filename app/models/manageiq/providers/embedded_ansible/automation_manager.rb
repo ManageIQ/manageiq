af2c269fc7e8a477b6032d730c3bbf2afa23cfb6
James Wong <jwong@redhat.com>
2017-01-25 11:58:37 -0500

    remove automation_manager_mixin.rb for now

--- app/models/manageiq/providers/embedded_ansible/automation_manager.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager.rb
@@ -1,6 +1,4 @@
 class ManageIQ::Providers::EmbeddedAnsible::AutomationManager < ManageIQ::Providers::EmbeddedAutomationManager
-  include ManageIQ::Providers::AnsibleTower::AutomationManagerMixin
-
   def self.ems_type
     @ems_type ||= "embedded_ansible_automation".freeze
   end
