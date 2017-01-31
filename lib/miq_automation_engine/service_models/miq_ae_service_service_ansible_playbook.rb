063689f2de9062f61dcad9099d43fb6b61f4af8a
Tina Fitzgerald <tfitzger@redhat.com>
2017-01-31 15:59:52 -0500

    Initial commit of Ansible playbook methods and expose methods in service model.
    Related Automate Model State Machines changes:
    https://github.com/ManageIQ/manageiq-content/pull/24

--- lib/miq_automation_engine/service_models/miq_ae_service_service_ansible_playbook.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_service_ansible_playbook.rb
@@ -1,4 +1,10 @@
 module MiqAeMethodService
   class MiqAeServiceServiceAnsiblePlaybook < MiqAeServiceService
+    expose :preprocess
+    expose :execute
+    expose :wait_for_completion
+    expose :refresh_provider
+    expose :check_refreshed
+    expose :postprocess
   end
 end
