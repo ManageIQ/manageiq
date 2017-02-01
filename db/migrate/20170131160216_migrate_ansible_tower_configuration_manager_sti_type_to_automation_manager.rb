4057951e0ad68af6b20f48d546419e7207dd6010
James Wong <jwong@redhat.com>
2017-02-01 16:48:59 -0500

    Migrate STI type of Ansible Tower's ConfigurationScript/ConfiguredSystem to be of AutomationManager

--- db/migrate/20170131160216_migrate_ansible_tower_configuration_manager_sti_type_to_automation_manager.rb
+++ db/migrate/20170131160216_migrate_ansible_tower_configuration_manager_sti_type_to_automation_manager.rb
@@ -3,10 +3,30 @@ class MigrateAnsibleTowerConfigurationManagerStiTypeToAutomationManager < Active
     self.inheritance_column = :_type_disabled
   end
 
+  class ConfigurationScript < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
+  class ConfiguredSystem < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+  end
+
   def up
     say_with_time("Migrating STI_type_of ansible_tower_configuration_managers to automation_managers") do
       ExtManagementSystem.where(:type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager').update_all(
         :type => 'ManageIQ::Providers::AnsibleTower::AutomationManager'
+      )
+    end
+
+    say_with_time("Migrating STI_type_of ansible_tower_configuration_scripts to_be_of automation_manager") do
+      ConfigurationScript.where(:type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager::ConfigurationScript').update_all(
+        :type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript'
+      )
+    end
+
+    say_with_time("Migrating STI_type_of ansible_tower_configured_systems to_be_of automation_manager") do
+      ConfiguredSystem.where(:type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager::ConfiguredSystem').update_all(
+        :type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::ConfiguredSystem'
       )
     end
   end
@@ -15,6 +35,18 @@ class MigrateAnsibleTowerConfigurationManagerStiTypeToAutomationManager < Active
     say_with_time("Migrating STI_type_of ansible_tower_automation_managers to configuration_managers") do
       ExtManagementSystem.where(:type => 'ManageIQ::Providers::AnsibleTower::AutomationManager').update_all(
         :type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager'
+      )
+    end
+
+    say_with_time("Migrating STI_type_of ansible_tower_configuration_scripts to_be_of configuration_managers") do
+      ConfigurationScript.where(:type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript').update_all(
+        :type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager::ConfigurationScript'
+      )
+    end
+
+    say_with_time("Migrating STI_type_of ansible_tower_configured_systems to_be_of configuration_managers") do
+      ConfiguredSystem.where(:type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::ConfiguredSystem').update_all(
+        :type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager::ConfiguredSystem'
       )
     end
   end
--- spec/migrations/20170131160216_migrate_ansible_tower_configuration_manager_sti_type_to_automation_manager_spec.rb
+++ spec/migrations/20170131160216_migrate_ansible_tower_configuration_manager_sti_type_to_automation_manager_spec.rb
@@ -2,32 +2,96 @@ require_migration
 
 describe MigrateAnsibleTowerConfigurationManagerStiTypeToAutomationManager do
   let(:ems_stub) { migration_stub(:ExtManagementSystem) }
+  let(:configuration_script_stub) { migration_stub(:ConfigurationScript) }
+  let(:configured_system_stub) { migration_stub(:ConfiguredSystem) }
 
   migration_context :up do
-    it 'migrates Ansible Tower Configuration Manager to Automation Manager type' do
-      configuration_manager = ems_stub.create!(:type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager')
+    context 'migrate_configuration_managers' do
+      it 'migrates Ansible Tower ConfigurationManager to AutomationManager type' do
+        configuration_manager = ems_stub.create!(:type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager')
 
-      migrate
+        migrate
 
-      expect(configuration_manager.reload.type).to eq('ManageIQ::Providers::AnsibleTower::AutomationManager')
+        expect(configuration_manager.reload.type).to eq('ManageIQ::Providers::AnsibleTower::AutomationManager')
+      end
+
+      it 'will not migrate other than Ansible Tower ConfigurationManager' do
+        some_mananger = ems_stub.create!(:type => 'ManageIQ::Providers::SomeManager')
+
+        migrate
+
+        expect(some_mananger.reload.type).to eq('ManageIQ::Providers::SomeManager')
+      end
     end
 
-    it 'will not migrate other than Ansible Tower Configuration Manager' do
-      some_mananger = ems_stub.create!(:type => 'ManageIQ::Providers::SomeManager')
+    context 'migrate_configuration_scripts' do
+      it 'migrates Ansible Tower ConfigurationScript to be of AutomationManager type' do
+        cs = configuration_script_stub.create!(
+          :type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager::ConfigurationScript'
+        )
 
-      migrate
+        migrate
 
-      expect(some_mananger.reload.type).to eq('ManageIQ::Providers::SomeManager')
+        expect(cs.reload.type).to eq('ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript')
+      end
+
+      it 'will not migrate ConfigurationScript of other than Ansible Tower ConfigurationManager' do
+        cs = configuration_script_stub.create!(:type => 'ManageIQ::Providers::SomeManager::ConfigurationScript')
+
+        migrate
+
+        expect(cs.reload.type).to eq('ManageIQ::Providers::SomeManager::ConfigurationScript')
+      end
+    end
+
+    context 'migrate_configured_systems' do
+      it 'migrates Ansible Tower ConfiguredSystem to be of AutomationManager type' do
+        cs = configured_system_stub.create!(
+          :type => 'ManageIQ::Providers::AnsibleTower::ConfigurationManager::ConfiguredSystem'
+        )
+
+        migrate
+
+        expect(cs.reload.type).to eq('ManageIQ::Providers::AnsibleTower::AutomationManager::ConfiguredSystem')
+      end
+
+      it 'will not migrate ConfiguredSystem of other than Ansible Tower ConfigurationManager' do
+        cs = configured_system_stub.create!(:type => 'ManageIQ::Providers::SomeManager::ConfiguredSystem')
+
+        migrate
+
+        expect(cs.reload.type).to eq('ManageIQ::Providers::SomeManager::ConfiguredSystem')
+      end
     end
   end
 
   migration_context :down do
-    it 'migrates Ansible Tower Automation Manager to Configuration Manager type' do
+    it 'migrates Ansible Tower AutomationManager to ConfigurationManager type' do
       automation_manager = ems_stub.create!(:type => 'ManageIQ::Providers::AnsibleTower::AutomationManager')
 
       migrate
 
       expect(automation_manager.reload.type).to eq('ManageIQ::Providers::AnsibleTower::ConfigurationManager')
+    end
+
+    it 'migrates Ansible Tower ConfigurationScript to be of ConfigurationManager type' do
+      cs = configuration_script_stub.create!(
+        :type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript'
+      )
+
+      migrate
+
+      expect(cs.reload.type).to eq('ManageIQ::Providers::AnsibleTower::ConfigurationManager::ConfigurationScript')
+    end
+
+    it 'migrates Ansible Tower AutomationManager to ConfigurationManager type' do
+      cs = configured_system_stub.create!(
+        :type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::ConfiguredSystem'
+      )
+
+      migrate
+
+      expect(cs.reload.type).to eq('ManageIQ::Providers::AnsibleTower::ConfigurationManager::ConfiguredSystem')
     end
   end
 end
