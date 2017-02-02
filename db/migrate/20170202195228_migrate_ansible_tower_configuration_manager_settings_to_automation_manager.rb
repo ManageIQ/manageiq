c3ab45838a2569261751359725a43104dc656bdd
James Wong <jwong@redhat.com>
2017-02-02 16:18:43 -0500

    Migrate Ansible Tower Configuration Manager settings to be of Automation Manager
    https://www.pivotaltracker.com/story/show/138675707

---
+++ db/migrate/20170202195228_migrate_ansible_tower_configuration_manager_settings_to_automation_manager.rb
@@ -0,0 +1,45 @@
+class MigrateAnsibleTowerConfigurationManagerSettingsToAutomationManager < ActiveRecord::Migration[5.0]
+  class SettingsChange < ActiveRecord::Base
+    serialize :value
+  end
+
+  def up
+    say_with_time('Migrate /ems_refresh/ansible_tower_configuration% to be of Automation Manager') do
+      SettingsChange.where('key LIKE ?', '/ems_refresh/ansible_tower_configuration%').each do |s|
+        s.key = s.key.sub(%r{^/ems_refresh/ansible_tower_configuration}, '/ems_refresh/ansible_tower_automation')
+        s.save!
+      end
+    end
+
+    say_with_time('Migrate /workers/worker_base/queue_worker_base/ems_refresh_worker/ems_refresh_worker_ansible_tower_configuration%' \
+      ' to be of Automation Manager') do
+      SettingsChange.where('key LIKE ?', '/workers/worker_base/queue_worker_base/ems_refresh_worker/ems_refresh_worker_ansible_tower_configuration%').each do |s|
+        s.key = s.key.sub(
+          %r{^/workers/worker_base/queue_worker_base/ems_refresh_worker/ems_refresh_worker_ansible_tower_configuration},
+          '/workers/worker_base/queue_worker_base/ems_refresh_worker/ems_refresh_worker_ansible_tower_automation'
+        )
+        s.save!
+      end
+    end
+  end
+
+  def down
+    say_with_time('Migrate /ems_refresh/ansible_tower_automation% back to be of Configuration Manager') do
+      SettingsChange.where("key LIKE ?", "/ems_refresh/ansible_tower_automation%").each do |s|
+        s.key = s.key.sub(%r{^/ems_refresh/ansible_tower_automation}, '/ems_refresh/ansible_tower_configuration')
+        s.save!
+      end
+    end
+
+    say_with_time('Migrate /workers/worker_base/queue_worker_base/ems_refresh_worker/ems_refresh_worker_ansible_tower_configuration%' \
+      ' back to be of Configuration Manager') do
+      SettingsChange.where('key LIKE ?', '/workers/worker_base/queue_worker_base/ems_refresh_worker/ems_refresh_worker_ansible_tower_automation%').each do |s|
+        s.key = s.key.sub(
+          %r{^/workers/worker_base/queue_worker_base/ems_refresh_worker/ems_refresh_worker_ansible_tower_automation},
+          '/workers/worker_base/queue_worker_base/ems_refresh_worker/ems_refresh_worker_ansible_tower_configuration'
+        )
+        s.save!
+      end
+    end
+  end
+end
