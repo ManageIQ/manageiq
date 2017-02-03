d4d83ccaa328599dcdaeea6b8436311de91ac840
James Wong <jwong@redhat.com>
2017-02-03 12:48:16 -0500

    Fix a typo

--- db/migrate/20170116210702_add_manager_ref_to_authentications.rb
+++ db/migrate/20170116210702_add_manager_ref_to_authentications.rb
@@ -1,4 +1,4 @@
-class AddMangerRefToAuthentications < ActiveRecord::Migration[5.0]
+class AddManagerRefToAuthentications < ActiveRecord::Migration[5.0]
   def change
     add_column :authentications, :manager_ref, :string
   end
