84a1ea76f30d299752ce0066a85e270d38c2e3ab
James Wong <jwong@redhat.com>
2017-02-03 13:21:18 -0500

    rename index name to index_configuration_script_base_id

--- db/migrate/20170118232430_create_join_table_authentication_configuration_script_base.rb
+++ db/migrate/20170118232430_create_join_table_authentication_configuration_script_base.rb
@@ -3,7 +3,7 @@ class CreateJoinTableAuthenticationConfigurationScriptBase < ActiveRecord::Migra
     create_table :authentication_configuration_script_bases do |t|
       t.bigint  :authentication_id
       t.bigint  :configuration_script_base_id
-      t.index   :configuration_script_base_id, :name => 'configuration_script_base_id'
+      t.index   :configuration_script_base_id, :name => 'index_configuration_script_base_id'
     end
   end
 end
