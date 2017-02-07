891bb331f900aec64d32c20feddf1a09cc096f3d
Milan Zazrivec <mzazrivec@redhat.com>
2017-02-07 19:31:03 +0100

    Core changes for Ansible Tower Playbooks & Repositories UI

--- app/models/configuration_script_source.rb
+++ app/models/configuration_script_source.rb
@@ -1,4 +1,6 @@
 class ConfigurationScriptSource < ApplicationRecord
   has_many    :configuration_script_payloads
   belongs_to  :manager, :class_name => "ExtManagementSystem"
+
+  virtual_total :total_payloads, :configuration_script_payloads
 end
