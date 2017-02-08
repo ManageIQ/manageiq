dd66d23c6948d09b72498377d368d0d2fe6a8eac
Ladislav Smola <lsmola@redhat.com>
2017-02-08 14:04:44 +0100

    Shared configurations of InventoryCollections for StorageManager
    Shared configurations of InventoryCollections for StorageManager

---
+++ app/models/manager_refresh/inventory_collection_default/storage_manager.rb
@@ -0,0 +1,39 @@
+class ManagerRefresh::InventoryCollectionDefault::NetworkManager < ManagerRefresh::InventoryCollectionDefault
+  class << self
+    def cloud_volumes(extra_attributes = {})
+      attributes = {
+        :model_class => ::CloudVolume,
+        :association => :cloud_volumes,
+      }
+
+      attributes.merge!(extra_attributes)
+    end
+
+    def cloud_volume_snapshots(extra_attributes = {})
+      attributes = {
+        :model_class => ::CloudVolumeSnapshot,
+        :association => :cloud_volume_snapshots,
+      }
+
+      attributes.merge!(extra_attributes)
+    end
+
+    def cloud_object_store_containers(extra_attributes = {})
+      attributes = {
+        :model_class => ::CloudObjectStoreContainer,
+        :association => :cloud_object_store_containers,
+      }
+
+      attributes.merge!(extra_attributes)
+    end
+
+    def cloud_object_store_objects(extra_attributes = {})
+      attributes = {
+        :model_class => ::CloudObjectStoreObject,
+        :association => :cloud_object_store_objects,
+      }
+
+      attributes.merge!(extra_attributes)
+    end
+  end
+end
