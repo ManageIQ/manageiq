07c42bc174b0bee88c52ebe5a0aa4b2d894c45df
Martin Povolny <mpovolny@redhat.com>
2017-02-09 01:00:18 +0100

    Add factory for SecurityGroup with FirewallRules.

--- spec/factories/security_group.rb
+++ spec/factories/security_group.rb
@@ -3,6 +3,10 @@ FactoryGirl.define do
     sequence(:name) { |n| "security_group_#{seq_padded_for_sorting(n)}" }
   end
 
+  factory :security_group_with_firewall_rules, :parent => :security_group do
+    firewall_rules { build_list :firewall_rule, 3 }
+  end
+
   factory :security_group_amazon, :parent => :security_group,
                                   :class  => "ManageIQ::Providers::Amazon::NetworkManager::SecurityGroup"
   factory :security_group_openstack, :parent => :security_group,
