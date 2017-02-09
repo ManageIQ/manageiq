b11c1b15402a381638fd1c00d4d57b8e56826695
Jillian Tullo <jtullo@redhat.com>
2017-02-09 10:31:44 -0500

    return config_info of service_templates

--- app/controllers/api/service_templates_controller.rb
+++ app/controllers/api/service_templates_controller.rb
@@ -4,5 +4,13 @@ module Api
     include Subcollections::Tags
     include Subcollections::ResourceActions
     include Subcollections::ServiceRequests
+
+    before_action :set_additional_attributes, :only => [:index, :show]
+
+    private
+
+    def set_additional_attributes
+      @additional_attributes = %w(config_info)
+    end
   end
 end
