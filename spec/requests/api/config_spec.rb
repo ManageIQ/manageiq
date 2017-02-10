040017128ffdccf41b9c72b0298f4f5e97df16d1
Tim Wade <hello@timjwade.com>
2017-02-10 07:19:53 -0800

    Keep all keys together

--- spec/requests/api/config_spec.rb
+++ spec/requests/api/config_spec.rb
@@ -16,8 +16,8 @@ describe 'API configuration (config/api.yml)' do
         collection_settings.each_with_object(Set.new) do |(_, cfg), set|
           set.add(cfg[:identifier]) if cfg[:identifier]
           keys = [:collection_actions, :resource_actions]
-          subcollections = Array(cfg[:subcollections]).collect { |s| "#{s}_subcollection_actions" }
-          (subcollections + keys).each do |action_type|
+          Array(cfg[:subcollections]).each { |s| keys << "#{s}_subcollection_actions" }
+          keys.each do |action_type|
             next unless cfg[action_type]
             cfg[action_type].each do |_, method_cfg|
               method_cfg.each do |action_cfg|
