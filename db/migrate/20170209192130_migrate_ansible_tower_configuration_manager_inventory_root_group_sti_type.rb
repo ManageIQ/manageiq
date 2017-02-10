c9a187191e91dc5fcb6734e2cd8d2e7e29c96e5b
James Wong <jwong@redhat.com>
2017-02-09 19:14:59 -0500

    Migrating STI type of ansible_tower inventory_root_groups to automation_manager inventorys

--- db/migrate/20170209192130_migrate_ansible_tower_configuration_manager_inventory_root_group_sti_type.rb
+++ db/migrate/20170209192130_migrate_ansible_tower_configuration_manager_inventory_root_group_sti_type.rb
@@ -4,16 +4,16 @@ class MigrateAnsibleTowerConfigurationManagerInventoryRootGroupStiType < ActiveR
   end
 
   def up
-    say_with_time('Migrating STI type of ansible_tower inventory_root_groups to be of automation_manager') do
+    say_with_time('Migrating STI type of ansible_tower inventory_root_groups to automation_manager inventorys') do
       EmsFolder.where(:type => 'ManageIQ::Providers::ConfigurationManager::InventoryRootGroup').update_all(
-        :type => 'ManageIQ::Providers::AutomationManager::InventoryRootGroup'
+        :type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::Inventory'
       )
     end
   end
 
   def down
-    say_with_time('Migrating STI type of ansible_tower inventory_root_groups to be of configuration_manager') do
-      EmsFolder.where(:type => 'ManageIQ::Providers::AutomationManager::InventoryRootGroup').update_all(
+    say_with_time('Migrating STI type of ansible_tower inventorys to configuration_manager inventory_root_groups') do
+      EmsFolder.where(:type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::Inventory').update_all(
         :type => 'ManageIQ::Providers::ConfigurationManager::InventoryRootGroup'
       )
     end
--- spec/migrations/20170209192130_migrate_ansible_tower_configuration_manager_inventory_root_group_sti_type_spec.rb
+++ spec/migrations/20170209192130_migrate_ansible_tower_configuration_manager_inventory_root_group_sti_type_spec.rb
@@ -4,14 +4,14 @@ describe MigrateAnsibleTowerConfigurationManagerInventoryRootGroupStiType do
   let(:inventory_root_group_stub) { migration_stub(:EmsFolder) }
 
   migration_context :up do
-    it 'migrates Ansible Tower InventoryRootGroup to be of AutomationManager type' do
+    it 'migrates Ansible Tower InventoryRootGroup to ManageIQ::Providers::AnsibleTower::AutomationManager::Inventory' do
       irg = inventory_root_group_stub.create!(
         :type => 'ManageIQ::Providers::ConfigurationManager::InventoryRootGroup'
       )
 
       migrate
 
-      expect(irg.reload.type).to eq('ManageIQ::Providers::AutomationManager::InventoryRootGroup')
+      expect(irg.reload.type).to eq('ManageIQ::Providers::AnsibleTower::AutomationManager::Inventory')
     end
 
     it 'will not migrate InventoryRootGroup other than those of Ansible Tower ConfigurationManager' do
@@ -24,8 +24,8 @@ describe MigrateAnsibleTowerConfigurationManagerInventoryRootGroupStiType do
   end
 
   migration_context :down do
-    it 'migrates Ansible Tower InventoryRootGroup to be of ConfigurationManager type' do
-      irg = inventory_root_group_stub.create!(:type => 'ManageIQ::Providers::AutomationManager::InventoryRootGroup')
+    it 'migrates Ansible Tower InventoryRootGroup to ManageIQ::Providers::AnsibleTower::AutomationManager::Inventory' do
+      irg = inventory_root_group_stub.create!(:type => 'ManageIQ::Providers::AnsibleTower::AutomationManager::Inventory')
 
       migrate
 
