a1be724d8c8f16c7358402b31a29493950675ddc
Ladislav Smola <lsmola@redhat.com>
2017-02-13 13:01:41 +0100

    Omit the empty base class
    Omit the empty base class, which should also simplify autoload,
    not needing the require_nested for each subclass.

--- app/models/manager_refresh/inventory_collection_default.rb
+++
@@ -1,2 +0,0 @@
-class ManagerRefresh::InventoryCollectionDefault
-end
--- app/models/manager_refresh/inventory_collection_default/network_manager.rb
+++ app/models/manager_refresh/inventory_collection_default/network_manager.rb
@@ -1,143 +1,147 @@
-class ManagerRefresh::InventoryCollectionDefault::NetworkManager < ManagerRefresh::InventoryCollectionDefault
-  class << self
-    def cloud_subnet_network_ports(extra_attributes = {})
-      attributes = {
-        :model_class => ::CloudSubnetNetworkPort,
-        :manager_ref => [:address, :cloud_subnet, :network_port],
-        :association => :cloud_subnet_network_ports,
-      }
+module ManagerRefresh
+  module InventoryCollectionDefault
+    class NetworkManager
+      class << self
+        def cloud_subnet_network_ports(extra_attributes = {})
+          attributes = {
+            :model_class => ::CloudSubnetNetworkPort,
+            :manager_ref => [:address, :cloud_subnet, :network_port],
+            :association => :cloud_subnet_network_ports,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def network_ports(extra_attributes = {})
-      attributes = {
-        :model_class => ::NetworkPort,
-        :association => :network_ports,
-      }
+        def network_ports(extra_attributes = {})
+          attributes = {
+            :model_class => ::NetworkPort,
+            :association => :network_ports,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def floating_ips(extra_attributes = {})
-      attributes = {
-        :model_class => ::FloatingIp,
-        :association => :floating_ips,
-      }
+        def floating_ips(extra_attributes = {})
+          attributes = {
+            :model_class => ::FloatingIp,
+            :association => :floating_ips,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def cloud_subnets(extra_attributes = {})
-      attributes = {
-        :model_class => ::CloudSubnet,
-        :association => :cloud_subnets,
-      }
+        def cloud_subnets(extra_attributes = {})
+          attributes = {
+            :model_class => ::CloudSubnet,
+            :association => :cloud_subnets,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def cloud_networks(extra_attributes = {})
-      attributes = {
-        :model_class => ::CloudNetwork,
-        :association => :cloud_networks,
-      }
+        def cloud_networks(extra_attributes = {})
+          attributes = {
+            :model_class => ::CloudNetwork,
+            :association => :cloud_networks,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def security_groups(extra_attributes = {})
-      attributes = {
-        :model_class => ::SecurityGroup,
-        :association => :security_groups,
-      }
+        def security_groups(extra_attributes = {})
+          attributes = {
+            :model_class => ::SecurityGroup,
+            :association => :security_groups,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def firewall_rules(extra_attributes = {})
-      attributes = {
-        :model_class => ::FirewallRule,
-        :manager_ref => [:resource, :source_security_group, :direction, :host_protocol, :port, :end_port, :source_ip_range],
-        :association => :firewall_rules,
-      }
+        def firewall_rules(extra_attributes = {})
+          attributes = {
+            :model_class => ::FirewallRule,
+            :manager_ref => [:resource, :source_security_group, :direction, :host_protocol, :port, :end_port, :source_ip_range],
+            :association => :firewall_rules,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def load_balancers(extra_attributes = {})
-      attributes = {
-        :model_class => ::LoadBalancer,
-        :association => :load_balancers,
-      }
+        def load_balancers(extra_attributes = {})
+          attributes = {
+            :model_class => ::LoadBalancer,
+            :association => :load_balancers,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def load_balancer_pools(extra_attributes = {})
-      attributes = {
-        :model_class => ::LoadBalancerPool,
-        :association => :load_balancer_pools,
-      }
+        def load_balancer_pools(extra_attributes = {})
+          attributes = {
+            :model_class => ::LoadBalancerPool,
+            :association => :load_balancer_pools,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def load_balancer_pool_members(extra_attributes = {})
-      attributes = {
-        :model_class => ::LoadBalancerPoolMember,
-        :association => :load_balancer_pool_members,
-      }
+        def load_balancer_pool_members(extra_attributes = {})
+          attributes = {
+            :model_class => ::LoadBalancerPoolMember,
+            :association => :load_balancer_pool_members,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def load_balancer_pool_member_pools(extra_attributes = {})
-      attributes = {
-        :model_class => ::LoadBalancerPoolMemberPool,
-        :manager_ref => [:load_balancer_pool, :load_balancer_pool_member],
-        :association => :load_balancer_pool_member_pools,
-      }
+        def load_balancer_pool_member_pools(extra_attributes = {})
+          attributes = {
+            :model_class => ::LoadBalancerPoolMemberPool,
+            :manager_ref => [:load_balancer_pool, :load_balancer_pool_member],
+            :association => :load_balancer_pool_member_pools,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def load_balancer_listeners(extra_attributes = {})
-      attributes = {
-        :model_class => ::LoadBalancerListener,
-        :association => :load_balancer_listeners,
-      }
+        def load_balancer_listeners(extra_attributes = {})
+          attributes = {
+            :model_class => ::LoadBalancerListener,
+            :association => :load_balancer_listeners,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def load_balancer_listener_pools(extra_attributes = {})
-      attributes = {
-        :model_class => ::LoadBalancerListenerPool,
-        :manager_ref => [:load_balancer_listener, :load_balancer_pool],
-        :association => :load_balancer_listener_pools,
-      }
+        def load_balancer_listener_pools(extra_attributes = {})
+          attributes = {
+            :model_class => ::LoadBalancerListenerPool,
+            :manager_ref => [:load_balancer_listener, :load_balancer_pool],
+            :association => :load_balancer_listener_pools,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def load_balancer_health_checks(extra_attributes = {})
-      attributes = {
-        :model_class => ::LoadBalancerHealthCheck,
-        :association => :load_balancer_health_checks,
-      }
+        def load_balancer_health_checks(extra_attributes = {})
+          attributes = {
+            :model_class => ::LoadBalancerHealthCheck,
+            :association => :load_balancer_health_checks,
+          }
 
-      attributes.merge!(extra_attributes)
-    end
+          attributes.merge!(extra_attributes)
+        end
 
-    def load_balancer_health_check_members(extra_attributes = {})
-      attributes = {
-        :model_class => ::LoadBalancerHealthCheckMember,
-        :manager_ref => [:load_balancer_health_check, :load_balancer_pool_member],
-        :association => :load_balancer_health_check_members,
-      }
+        def load_balancer_health_check_members(extra_attributes = {})
+          attributes = {
+            :model_class => ::LoadBalancerHealthCheckMember,
+            :manager_ref => [:load_balancer_health_check, :load_balancer_pool_member],
+            :association => :load_balancer_health_check_members,
+          }
 
-      attributes.merge!(extra_attributes)
+          attributes.merge!(extra_attributes)
+        end
+      end
     end
   end
 end
