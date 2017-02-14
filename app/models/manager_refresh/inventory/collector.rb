0f57da2541b6102c42d685c0f6c8e44010f0dcff
Ladislav Smola <lsmola@redhat.com>
2017-02-14 14:11:44 +0100

    Modify Collector base class to align with Amazon usecase
    Modify Collector base class to align with Amazon usecase

--- app/models/manager_refresh/inventory/collector.rb
+++ app/models/manager_refresh/inventory/collector.rb
@@ -1,9 +1,14 @@
 class ManagerRefresh::Inventory::Collector
-  attr_accessor :manager
-  attr_accessor :target
+  attr_accessor :manager, :target
 
+  # @param manager [ManageIQ::Providers::BaseManager] A manager object
+  # @param target [ActiveRecord|Hash] A refresh Target object
   def initialize(manager, target)
     @manager = manager
     @target  = target
+  end
+
+  def options
+    @options ||= Settings.ems_refresh[manager.class.ems_type]
   end
 end
