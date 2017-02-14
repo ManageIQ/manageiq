9435aca4db0b3e1f56339a9cd176d1d15f4f1381
Ladislav Smola <lsmola@redhat.com>
2017-02-14 14:12:25 +0100

    Modify Inventory base class to align with Amazon usecase
    Modify Inventory base class to align with Amazon usecase

--- app/models/manager_refresh/inventory.rb
+++ app/models/manager_refresh/inventory.rb
@@ -4,20 +4,25 @@ module ManagerRefresh
     require_nested :Parser
     require_nested :Target
 
-    attr_accessor :collector
-    attr_accessor :target
-    attr_accessor :parser
+    attr_accessor :collector, :parsers, :target
 
-    def initialize(target, collector, parser)
+    # @param target [ManagerRefresh::Inventory::Target] A target object
+    # @param collector [ManagerRefresh::Inventory::Collector] A collector object
+    # @param parsers [ManagerRefresh::Inventory::Parser|Array] A Parser object or an array of
+    #   ManagerRefresh::Inventory::Parser objects
+    def initialize(target, collector, parsers)
       @collector = collector
       @target    = target
-      @parser    = parser
+      @parsers   = parsers.kind_of?(Array) ? parsers : [parsers]
     end
 
     def inventory_collections
-      parser.collector = collector
-      parser.target = target
-      parser.parse
+      parsers.each do |parser|
+        parser.collector = collector
+        parser.target    = target
+        parser.parse
+      end
+
       target.inventory_collections
     end
   end
