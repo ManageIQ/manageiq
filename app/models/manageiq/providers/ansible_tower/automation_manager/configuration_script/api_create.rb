72fcd14e46cc8d5819be57362fb19014dc252b06
Drew Bomhof <dbomhof@redhat.com>
2017-02-14 10:21:03 -0500

    Adjust queued methods
    Accept an auth_user or default to 'system' as a backup
    Always call my_zone
    
    https://www.pivotaltracker.com/story/show/137306185

--- app/models/manageiq/providers/ansible_tower/automation_manager/configuration_script/api_create.rb
+++ app/models/manageiq/providers/ansible_tower/automation_manager/configuration_script/api_create.rb
@@ -8,15 +8,15 @@ class ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript
 
       # Get the record in our database
       # TODO: This needs to be targeted refresh so it doesn't take too long
-      EmsRefresh.queue_refresh(manager, nil, true) if !manager.missing_credentials? && manager.authentication_status_ok?
+      EmsRefresh.queue_refresh(manager, nil, true) if manager.authentication_status_ok?
 
       find_by(:manager_id => manager.id, :manager_ref => job_template.id)
     end
 
-    def create_in_provider_queue(manager_id, params)
+    def create_in_provider_queue(manager_id, params, auth_user = nil)
       task_opts = {
         :action => "Creating Ansible Tower Job Template",
-        :userid => "system"
+        :userid => auth_user || "system"
       }
 
       manager = ExtManagementSystem.find(manager_id)
@@ -27,7 +27,7 @@ class ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript
         :method_name => "create_in_provider",
         :priority    => MiqQueue::HIGH_PRIORITY,
         :role        => "ems_operations",
-        :zone        => manager.zone_id
+        :zone        => manager.my_zone
       }
 
       MiqTask.generic_action_with_callback(task_opts, queue_opts)
