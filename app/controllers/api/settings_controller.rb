7df0015ceddcb82c8d0c0010daca4dc75adf2297
Alberto Bellotti <abellott@redhat.com>
2017-02-15 17:02:46 -0500

    - Adding support for arbitrary path categories, can include top level   categories like it does today like product, authentication, server,   but also sub pathing, server/role, session/timeout even lower level   entries like server/worker_monitor/start_algorithm/value
    - whitelists a subset of the settings as driven by the new categories
    
    - Can fetch top level categories or any setting entry path:
      GET /api/settings/product, or GET /api/settings/server/role (if exposed)
    
    - Added rspecs

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -1,22 +1,39 @@
 module Api
   class SettingsController < BaseController
     def index
-      render_resource :settings, slice_settings(exposed_settings)
+      render_resource :settings, whitelist_settings(settings_hash)
     end
 
     def show
-      raise NotFoundError, "Settings category #{@req.c_id} not found" unless exposed_settings.include?(@req.c_id)
-      render_resource :settings, slice_settings(@req.c_id)
+      settings_value = entry_value(whitelist_settings(settings_hash), @req.c_suffix)
+
+      raise NotFoundError, "Settings entry #{@req.c_suffix} not found" if settings_value.nil?
+      render_resource :settings, settings_entry_to_hash(@req.c_suffix, settings_value)
     end
 
     private
 
-    def exposed_settings
-      ApiConfig.collections[:settings][:categories]
+    def whitelist_settings(settings)
+      result_hash = {}
+      ApiConfig.collections[:settings][:categories].each do |category_path|
+        result_hash.deep_merge!(settings_entry_to_hash(category_path, entry_value(settings, category_path)))
+      end
+      result_hash
     end
 
-    def slice_settings(keys)
-      ::Settings.to_hash.slice(*Array(keys).collect(&:to_sym))
+    def settings_hash
+      @settings_hash ||= Settings.to_hash.deep_stringify_keys
+    end
+
+    def entry_value(settings, path)
+      settings.fetch_path(path.split('/'))
+    end
+
+    def settings_entry_to_hash(path, value)
+      path_elements = path.split("/")
+      result_hash = {path_elements.last => value}
+      path_elements[0..-2].reverse.each { |entry| result_hash = { entry => result_hash } } if path_elements.length > 1
+      result_hash
     end
   end
 end
--- spec/requests/api/settings_spec.rb
+++ spec/requests/api/settings_spec.rb
@@ -4,6 +4,22 @@
 describe "Settings API" do
   let(:api_settings) { Api::ApiConfig.collections[:settings][:categories] }
 
+  def normalize_settings(settings)
+    normalize_hash(settings.to_hash.deep_stringify_keys)
+  end
+
+  def normalize_hash(settings)
+    settings.keys.each_with_object({}) do |key, hash|
+      value = settings[key]
+      new_value = case value
+                  when Hash   then normalize_hash(value)
+                  when Symbol then value.to_s
+                  else value
+                  end
+      hash[key] = new_value unless new_value.nil?
+    end
+  end
+
   context "Settings Queries" do
     it "tests queries of all exposed settings" do
       api_basic_authorize action_identifier(:settings, :read, :collection_actions, :get)
@@ -37,6 +53,62 @@ describe "Settings API" do
       run_get settings_url("invalid_setting")
 
       expect(response).to have_http_status(:not_found)
+    end
+
+    it "supports multiple categories" do
+      stub_api_collection_config("settings", "categories", %w(product authentication server))
+      api_basic_authorize action_identifier(:settings, :read, :resource_actions, :get)
+
+      run_get settings_url
+
+      expect(response.parsed_body).to match(
+        "product"        => normalize_settings(Settings.product),
+        "authentication" => normalize_settings(Settings.authentication),
+        "server"         => normalize_settings(Settings.server)
+      )
+    end
+
+    it "supports partial categories" do
+      stub_api_collection_config("settings", "categories", %w(product server/role))
+      api_basic_authorize action_identifier(:settings, :read, :resource_actions, :get)
+
+      run_get settings_url
+
+      expect(response.parsed_body).to match(
+        "product" => normalize_settings(Settings.product),
+        "server"  => { "role" => Settings.server.role }
+      )
+    end
+
+    it "supports second level partial categories" do
+      stub_api_collection_config("settings", "categories", %w(product server/role server/worker_monitor/sync_interval))
+      api_basic_authorize action_identifier(:settings, :read, :resource_actions, :get)
+
+      run_get settings_url
+
+      expect(response.parsed_body).to match(
+        "product" => normalize_settings(Settings.product),
+        "server"  => {
+          "role"           => Settings.server.role,
+          "worker_monitor" => { "sync_interval" => Settings.server.worker_monitor.sync_interval }
+        }
+      )
+    end
+
+    it "supports multiple and partial categories" do
+      stub_api_collection_config("settings", "categories", %w(product server/role server/worker_monitor authentication))
+      api_basic_authorize action_identifier(:settings, :read, :resource_actions, :get)
+
+      run_get settings_url
+
+      expect(response.parsed_body).to match(
+        "product"        => normalize_settings(Settings.product),
+        "server"         => {
+          "role"           => Settings.server.role,
+          "worker_monitor" => normalize_settings(Settings.server.worker_monitor),
+        },
+        "authentication" => normalize_settings(Settings.authentication)
+      )
     end
   end
 end
