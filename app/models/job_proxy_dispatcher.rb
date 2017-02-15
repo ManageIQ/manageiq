51140edf94ef7428a486ce0e6e79b8433f0d5c6e
Yuri Rudman <yrudman@redhat.com>
2017-02-15 09:52:38 -0500

    removed dupplicated assignment

--- app/models/job_proxy_dispatcher.rb
+++ app/models/job_proxy_dispatcher.rb
@@ -175,8 +175,6 @@ class JobProxyDispatcher
   def assign_proxy_to_job(proxy, job)
     job.agent_id        = proxy.id
     job.agent_class     = proxy.class.to_s
-    job.agent_id        = proxy.id
-    job.agent_class     = proxy.class.to_s
     job.agent_name      = proxy.name
     job.started_on      = Time.now.utc
     job.dispatch_status = "active"
