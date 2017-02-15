4214aa2414c16eaef5b59a0a9e3bc64369aa1c9c
Ladislav Smola <lsmola@redhat.com>
2017-02-15 10:56:45 +0100

    Renames in Persister according to review
    Renames in Persister according to review

--- app/models/manager_refresh/inventory/persister.rb
+++ app/models/manager_refresh/inventory/persister.rb
@@ -3,9 +3,9 @@ class ManagerRefresh::Inventory::Persister
 
   # @param manager [ManageIQ::Providers::BaseManager] A manager object
   # @param target [Object] A refresh Target object
-  def initialize(manager, refresh_target)
+  def initialize(manager, target)
     @manager = manager
-    @target  = refresh_target
+    @target  = target
 
     @collections = {}
 
@@ -39,7 +39,7 @@ class ManagerRefresh::Inventory::Persister
 
   def self.define_collections_reader(collection_key)
     define_method(collection_key) do
-      collections.try(:[], collection_key)
+      collections[collection_key]
     end
   end
 
@@ -51,35 +51,34 @@ class ManagerRefresh::Inventory::Persister
 
   # Adds 1 ManagerRefresh::InventoryCollection under a target.collections using :association key as index
   #
-  # @param inventory_collection_data [Hash] Hash used for ManagerRefresh::InventoryCollection initialize
-  def add_inventory_collection(inventory_collection_data)
-    data = inventory_collection_data
-    data[:parent] ||= manager
+  # @param options [Hash] Hash used for ManagerRefresh::InventoryCollection initialize
+  def add_inventory_collection(options)
+    options[:parent] ||= manager
 
-    if !data.key?(:delete_method) && data[:model_class]
+    if !options.key?(:delete_method) && options[:model_class]
       # Automatically infer what the delete method should be, unless the delete methods was given
-      data[:delete_method] = data[:model_class].new.respond_to?(:disconnect_inv) ? :disconnect_inv : nil
+      options[:delete_method] = options[:model_class].new.respond_to?(:disconnect_inv) ? :disconnect_inv : nil
     end
 
-    collections[data[:association]] = ::ManagerRefresh::InventoryCollection.new(data)
+    collections[options[:association]] = ::ManagerRefresh::InventoryCollection.new(options)
   end
 
   # Adds multiple inventory collections with the same data
   #
   # @param default [ManagerRefresh::InventoryCollectionDefault] Default
   # @param inventory_collections [Array] Array of method names for passed default parameter
-  # @param inventory_collections_data [Hash] Hash used for ManagerRefresh::InventoryCollection initialize
-  def add_inventory_collections(default, inventory_collections, inventory_collections_data = {})
+  # @param options [Hash] Hash used for ManagerRefresh::InventoryCollection initialize
+  def add_inventory_collections(default, inventory_collections, options = {})
     inventory_collections.each do |inventory_collection|
-      add_inventory_collection(default.send(inventory_collection, inventory_collections_data))
+      add_inventory_collection(default.send(inventory_collection, options))
     end
   end
 
   # Adds remaining inventory collections with the same data
   #
   # @param defaults [Array] Array of ManagerRefresh::InventoryCollectionDefault
-  # @param inventory_collections_data [Hash] Hash used for ManagerRefresh::InventoryCollection initialize
-  def add_remaining_inventory_collections(defaults, inventory_collections_data = {})
+  # @param options [Hash] Hash used for ManagerRefresh::InventoryCollection initialize
+  def add_remaining_inventory_collections(defaults, options = {})
     defaults.each do |default|
       # Get names of all inventory collections defined in passed classes with Defaults
       all_inventory_collections     = default.methods - ::ManagerRefresh::InventoryCollectionDefault.methods
@@ -89,7 +88,7 @@ class ManagerRefresh::Inventory::Persister
       # Add all missing inventory_collections with defined init_data
       add_inventory_collections(default,
                                 all_inventory_collections - defined_inventory_collections,
-                                inventory_collections_data)
+                                options)
     end
   end
 end
