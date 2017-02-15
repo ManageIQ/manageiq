0d8628492ea97cbdd5f15d623b394d6e15e6d2c9
Tim Wade <hello@timjwade.com>
2017-02-15 14:39:47 -0800

    Extract method slice_settings

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -1,18 +1,22 @@
 module Api
   class SettingsController < BaseController
     def index
-      render_resource :settings, ::Settings.to_hash.slice(*Array(exposed_settings).collect(&:to_sym))
+      render_resource :settings, slice_settings(exposed_settings)
     end
 
     def show
       raise NotFoundError, "Settings category #{@req.c_id} not found" unless exposed_settings.include?(@req.c_id)
-      render_resource :settings, ::Settings.to_hash.slice(*Array(@req.c_id).collect(&:to_sym))
+      render_resource :settings, slice_settings(@req.c_id)
     end
 
     private
 
     def exposed_settings
       ApiConfig.collections[:settings][:categories]
+    end
+
+    def slice_settings(keys)
+      ::Settings.to_hash.slice(*Array(keys).collect(&:to_sym))
     end
   end
 end
