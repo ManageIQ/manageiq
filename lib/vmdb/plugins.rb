edc86e2b26292d612e0f4ff22efeaccd1b27af4c
Marcel Hild <hild@b4mad.net>
2017-02-20 12:33:11 +0100

    Add registered_provider_plugins to Vmdb::Plugins
    this allows for querying which providers are registered
    via plugins
    
    it also provides a helper method to query for provider names
    of all registered providers

--- lib/vmdb/plugins.rb
+++ lib/vmdb/plugins.rb
@@ -4,9 +4,11 @@ module Vmdb
 
     attr_reader :vmdb_plugins
     attr_reader :registered_automate_domains
+    attr_reader :registered_provider_plugins
 
     def initialize
       @registered_automate_domains = []
+      @registered_provider_plugins = []
       @vmdb_plugins = []
     end
 
@@ -14,19 +16,35 @@ module Vmdb
       @vmdb_plugins << engine
 
       register_automate_domains(engine)
+      register_provider_plugin(engine)
 
       # make sure STI models are recognized
       DescendantLoader.instance.descendants_paths << engine.root.join('app')
+    end
+
+    def provider_plugin_names
+      @provider_plugins_names ||= registered_provider_plugins.collect do |plugin|
+        ManageIQ::Providers::Inflector.provider_name(plugin).downcase.to_sym
+      end
+    end
+
+    def system_automate_domains
+      registered_automate_domains.select(&:system?)
+    end
+
+    private
+
+    def register_provider_plugin(engine)
+      if engine.class.name.start_with?("ManageIQ::Providers::")
+        @registered_provider_plugins << engine
+        @provider_plugins_names = nil
+      end
     end
 
     def register_automate_domains(engine)
       Dir.glob(engine.root.join("content", "automate", "*")).each do |domain_directory|
         @registered_automate_domains << AutomateDomain.new(domain_directory)
       end
-    end
-
-    def system_automate_domains
-      registered_automate_domains.select(&:system?)
     end
   end
 end
