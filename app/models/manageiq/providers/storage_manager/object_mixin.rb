9215caa7f9ab0d7a377233a750fa0c0e75edecc0
Ilya Shavrin <shavrin.ip@gmail.com>
2017-02-21 21:52:35 +0100

    Perfomance fix for Object Storage Manager deletion

--- app/models/manageiq/providers/storage_manager/object_mixin.rb
+++ app/models/manageiq/providers/storage_manager/object_mixin.rb
@@ -3,8 +3,16 @@ module ManageIQ::Providers::StorageManager::ObjectMixin
 
   included do
     has_many :cloud_object_store_containers, :foreign_key => :ems_id, :dependent => :destroy
-    has_many :cloud_object_store_objects,    :foreign_key => :ems_id, :dependent => :destroy
+    has_many :cloud_object_store_objects,    :foreign_key => :ems_id
 
     supports :object_storage
+
+    after_destroy :cleanup_objects
+
+    private
+
+    def cleanup_objects
+      cloud_object_store_objects.includes(:taggings).find_each(&:destroy)
+    end
   end
 end
