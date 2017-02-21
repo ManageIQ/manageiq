8d970be11948bf371ede3569222070b88895e439
Ladislav Smola <lsmola@redhat.com>
2017-02-21 11:10:23 +0100

    Before the saving, we need to scan the inventory collections
    Before the saving, we need to scan the inventory collections

--- app/models/manager_refresh/save_inventory.rb
+++ app/models/manager_refresh/save_inventory.rb
@@ -2,6 +2,10 @@ module ManagerRefresh
   class SaveInventory
     class << self
       def save_inventory(ems, inventory_collections)
+        _log.info("#{log_header(ems)} Scanning Inventory Collections...Start")
+        ManagerRefresh::InventoryCollection::Scanner.scan!(inventory_collections)
+        _log.info("#{log_header(ems)} Scanning Inventory Collections...Complete")
+
         _log.info("#{log_header(ems)} Saving EMS Inventory...Start")
 
         inventory_object_saving_strategy = Settings.ems_refresh[ems.class.ems_type].try(:[], :inventory_object_saving_strategy)
