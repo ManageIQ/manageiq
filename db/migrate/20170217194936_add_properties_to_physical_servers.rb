7c13c855b69909637d84c101cbab1a05d32d931d
Julian Cheal <jcheal@redhat.com>
2017-02-22 11:16:33 -0500

    Removed UUID as we're going to use EMS_Ref

--- db/migrate/20170217194936_add_properties_to_physical_servers.rb
+++ db/migrate/20170217194936_add_properties_to_physical_servers.rb
@@ -6,7 +6,6 @@ class AddPropertiesToPhysicalServers < ActiveRecord::Migration[5.0]
     add_column :physical_servers, :machine_type, :string
     add_column :physical_servers, :model, :string
     add_column :physical_servers, :serial_number, :string
-    add_column :physical_servers, :uuid, :string, :index => true
     add_column :physical_servers, :field_replaceable_unit, :string
     add_column :physical_servers, :host_id, :bigint
   end
