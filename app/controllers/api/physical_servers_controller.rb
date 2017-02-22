0194536b8e4f56f299a0698fe515ebc3469d7157
Rodney H. Brown <rbrown4@lenovo.com>
2017-02-22 16:57:12 -0500

    Add firmware model and physical_server references

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -1,16 +1,17 @@
 module Api
   class PhysicalServersController < BaseController
-
     def show
       if params[:c_id]
         physical_server = PhysicalServer.find(params[:c_id])
         response_payload = physical_server.as_json
+        firmwares = physical_server.firmwares.map(&:as_json)
+        response_payload['firmwares'] = firmwares      
         response_payload['host'] = case physical_server.host
-                                      when nil then nil
-                                      else physical_server.host.id
-				   end
+                                   when nil then nil
+                                   else physical_server.host.id
+        end
 
-        render json: response_payload
+        render :json => response_payload
       else
         super
       end
