5a832dcbecf4a74398b50439a5dbdd418d006481
Madhu Kanoor <mkanoor@redhat.com>
2017-02-23 16:15:39 -0500

    Add column comments

--- db/migrate/20170222214902_add_initiator_to_service.rb
+++ db/migrate/20170222214902_add_initiator_to_service.rb
@@ -2,6 +2,7 @@ class AddInitiatorToService < ActiveRecord::Migration[5.0]
   class Service < ActiveRecord::Base; end
   def up
     add_column :services, :initiator, :string
+    change_column_comment :services, :initiator, "Record who initiated the service creation user|control|alert. It helps the UI display the services based on categories"
     say_with_time('Adding initiator to Service') do
       Service.update_all(:initiator => 'user')
     end
