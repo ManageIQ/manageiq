f5a1f462f94f76ce5594c5723bc67ce9208ebe7e
lpichler <lpichler@redhat.com>
2017-02-23 13:57:08 +0100

    Pass user or group to the rss generator
    to be able use it for RBAC

--- app/models/rss_feed.rb
+++ app/models/rss_feed.rb
@@ -17,7 +17,7 @@ class RssFeed < ApplicationRecord
     "#{host_url}#{link}"
   end
 
-  def generate(host = nil, local = false, proto = nil)
+  def generate(host = nil, local = false, proto = nil, user_or_group = nil)
     proto ||= ::Settings.webservices.consume_protocol
     host_url = host.nil? ? "#{proto}://localhost:3000" : "#{proto}://" + host
 
