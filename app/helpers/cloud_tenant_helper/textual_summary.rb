6bb8455d92992f78bc1d7cf222dc9b32e01ca111
Milan Zázrivec <mzazrivec@redhat.com>
2017-02-23 11:31:07 +0100

    Merge pull request #457 from tzumainn/cloud-tenant-security-group-fix
    Removed duplicate security group listing in cloud tenant table
    (cherry picked from commit b20f71904d9771d74f1658404c311566ac19c621)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1426638

--- app/helpers/cloud_tenant_helper/textual_summary.rb
+++ app/helpers/cloud_tenant_helper/textual_summary.rb
@@ -5,7 +5,7 @@ module CloudTenantHelper::TextualSummary
   # Groups
   #
   def textual_group_relationships
-    %i(ems_cloud security_groups instances images cloud_object_store_containers cloud_volumes cloud_volume_snapshots
+    %i(ems_cloud instances images cloud_object_store_containers cloud_volumes cloud_volume_snapshots
        cloud_networks cloud_subnets network_routers security_groups floating_ips network_ports)
   end
 
