da14c5eda0ac14b5c0699f50a7e96b47c5cfe521
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2017-02-24 14:02:52 +0100

    Targeted refresh using api4

--- app/models/manageiq/providers/redhat/infra_manager/refresh/strategies/api3.rb
+++ app/models/manageiq/providers/redhat/infra_manager/refresh/strategies/api3.rb
@@ -1,4 +1,37 @@
 module ManageIQ::Providers::Redhat::InfraManager::Refresh::Strategies
   class Api3 < ManageIQ::Providers::Redhat::InfraManager::Refresh::Refresher
+    def host_targeted_refresh(inventory, target)
+      methods = {
+        :primary => {
+          :host => target.ems_ref,
+        },
+        :secondary => {
+          :host => [:statistics, :host_nics],
+        }
+      }
+      inventory.targeted_refresh(methods)
+    end
+
+    def vm_targeted_refresh(inventory, target)
+      require 'uri'
+
+      vm = target.ems_ref
+      vm_id = URI(vm).path.split('/').last
+
+      methods = {
+        :primary => {
+          :cluster    => {:clusters => "cluster"},
+          :datacenter => {:datacenters => "data_center"},
+          :vm         => vm,
+          :template   => "/api/templates?search=vm.id=#{vm_id}",
+          :storage    => target.storages.empty? ? {:storagedomains => "storage_domain"} : target.storages.map(&:ems_ref)
+        },
+        :secondary => {
+          :vm         => [:disks, :snapshots, :nics],
+          :template   => [:disks]
+        }
+      }
+      inventory.targeted_refresh(methods)
+    end
   end
 end
