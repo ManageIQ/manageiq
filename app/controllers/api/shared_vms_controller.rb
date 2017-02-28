3e1a71f2efebc5d9867b7a6f3ef79c11f9ba2f6a
Tim Wade <hello@timjwade.com>
2017-02-28 10:09:26 -0800

    Roughly outline where we're deficient

--- app/controllers/api/shared_vms_controller.rb
+++ app/controllers/api/shared_vms_controller.rb
@@ -4,6 +4,22 @@ module Api
       resources = Rbac.resources_shared_with(User.current_user)
       # TODO: if this is a relation, we need to filter in SQL
       shared_vms = resources.select { |resource| resource.kind_of?(Vm) }
+
+      # # Roughly what we do elsewhere:
+      # miq_expression = filter_param(Vm)
+
+      # # can't sort - it's an array
+      # sort_options = sort_params(klass) if shared_vms.respond_to?(:reorder)
+
+      # shared_vms = shared_vms.reorder(sort_options) if sort_options.present?
+
+      # options = {}
+      # options[:order] = sort_options if sort_options.present?
+      # options[:offset], options[:limit] = expand_paginate_params if paginate_params?
+      # options[:filter] = miq_expression if miq_expression
+
+      # shared_vms = Rbac.filtered(shared_vms, options)
+
       render_collection(:shared_vms, shared_vms)
     end
 
