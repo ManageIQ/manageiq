1762ad0df38e1f8fd56daa6509572a3f854c5149
Drew Bomhof <dbomhof@redhat.com>
2017-03-01 12:13:41 -0500

    Consolidate update_in_provider_queue
    create_in_provider_queue now handles both the create and update queuing
    
    https://www.pivotaltracker.com/story/show/139020599

--- app/models/manageiq/providers/ansible_tower/shared/automation_manager/configuration_script.rb
+++ app/models/manageiq/providers/ansible_tower/shared/automation_manager/configuration_script.rb
@@ -17,24 +17,6 @@ module ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::Configurati
       find_by!(:manager_id => manager.id, :manager_ref => job_template.id)
     end
 
-    def create_in_provider_queue(manager_id, params, auth_user = nil)
-      manager = ExtManagementSystem.find(manager_id)
-      task_opts = {
-        :action => "Creating Ansible Tower Job Template",
-        :userid => auth_user || "system"
-      }
-      queue_opts = {
-        :args        => [manager_id, params],
-        :class_name  => self.name,
-        :method_name => "create_in_provider",
-        :priority    => MiqQueue::HIGH_PRIORITY,
-        :role        => "ems_operations",
-        :zone        => manager.my_zone
-      }
-
-      MiqTask.generic_action_with_callback(task_opts, queue_opts)
-    end
-
     def update_in_provider(manager_id, params)
       manager = ExtManagementSystem.find(manager_id)
       manager.with_provider_connection do |connection|
@@ -46,18 +28,18 @@ module ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::Configurati
       EmsRefresh.queue_refresh(manager, nil) if manager.authentication_status_ok?
     end
 
-    def update_in_provider_queue(manager_id, params, auth_user = nil)
-      task_opts = {
-        :action => "Updating Ansible Tower Job Template",
-        :userid => auth_user || "system"
-      }
-
+    def create_in_provider_queue(manager_id, params, auth_user = nil)
+      action = params[:manager_ref] ? "update" : "create"
       manager = ExtManagementSystem.find(manager_id)
 
+      task_opts = {
+        :action => "#{(action == "create" ? "Creating" : "Updating")} Ansible Tower Job Template",
+        :userid => auth_user || "system"
+      }
       queue_opts = {
         :args        => [manager_id, params],
-        :class_name  => "ManageIQ::Providers::AnsibleTower::AutomationManager::ConfigurationScript",
-        :method_name => "update_in_provider",
+        :class_name  => self.name,
+        :method_name => "#{action}_in_provider",
         :priority    => MiqQueue::HIGH_PRIORITY,
         :role        => "ems_operations",
         :zone        => manager.my_zone
