585a43e1cb100876c0a8771b8fb891f83cc03cc8
Daniel Berger <dberger@redhat.com>
2017-03-02 10:40:36 -0700

    Add label and classification_id.

--- db/migrate/20170301192129_create_provider_tags_table.rb
+++ db/migrate/20170301192129_create_provider_tags_table.rb
@@ -5,6 +5,8 @@ class CreateProviderTagsTable < ActiveRecord::Migration[5.0]
       t.string :value, :comment => "The value within a key/value pair."
       t.string :resource_id, :null => false, :comment => "Reference to the field used as primary key of the resource."
       t.string :type, :null => false, :comment => "The model name of the resource type."
+      t.string :label, :comment => "Optional symbolic label for the key/value pair."
+      t.integer :classification_id, :comment => "Optional reference to a Classification."
       t.timestamps
     end
   end
