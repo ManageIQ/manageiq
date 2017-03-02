7a0625477b9188dcce17e3b88c3f1bccb75ffe63
Lucy Fu <lufu@redhat.com>
2017-03-02 16:05:18 -0500

    Add Ansible event target type into event_type.
    Add Ansible event target type into event_type to distinguish the event from different event targets.
    No need to repeat event.changes in message to reduce the event object's size.

--- app/models/manageiq/providers/ansible_tower/shared/automation_manager/event_parser.rb
+++ app/models/manageiq/providers/ansible_tower/shared/automation_manager/event_parser.rb
@@ -1,9 +1,8 @@
 module ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::EventParser
   def event_to_hash(event, ems_id)
     {
-      :event_type => "#{self.event_type}_#{event.operation}",
+      :event_type => "#{event.object1}_#{event.operation}",
       :source     => "#{self.source}",
-      :message    => event.changes.to_s,
       :timestamp  => event.timestamp,
       :full_data  => event.to_h,
       :ems_id     => ems_id
