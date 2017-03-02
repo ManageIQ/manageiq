21aecda00dc3b126ee4be4a0940012646d762286
Lucy Fu <lufu@redhat.com>
2017-03-02 15:59:25 -0500

    Get rid of the redundant Ansible event_type which is the same as event source.

--- app/models/manageiq/providers/ansible_tower/automation_manager/event_parser.rb
+++ app/models/manageiq/providers/ansible_tower/automation_manager/event_parser.rb
@@ -1,10 +1,6 @@
 module ManageIQ::Providers::AnsibleTower::AutomationManager::EventParser
   extend ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::EventParser
 
-  def self.event_type
-    "ansible_tower"
-  end
-
   def self.source
     "ANSIBLE_TOWER"
   end
