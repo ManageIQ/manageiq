97c5834c581f071789448ad28ddaf6605d478818
Daniel Berger <dberger@redhat.com>
2017-03-02 08:52:18 -0700

    Add comments to the ProviderTag model.

--- app/models/provider_tag.rb
+++ app/models/provider_tag.rb
@@ -1,3 +1,14 @@
+# The ProviderTag model serves as the class for tags (key/value pairs) that
+# exist on remote (e.g. Azure, Amazon) resources. We then store and manage
+# them locally.
+#
+# When establishing a relationship from a model, it should look like this:
+#
+#   has_many :provider_tags, :foreign_key => :resource_id, :primary_key => :some_unique_key
+#
+# Where 'some_unique_key' is ems_ref, guid, or whatever stringy key uniquely
+# identifies that resource.
+#
 class ProviderTag < ApplicationRecord
   validates :key, :presence => true
   validates :resource_id, :presence => true
