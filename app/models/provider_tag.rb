f5ae8e6d6d06f2a55930de82af2497b3e92d1c47
Daniel Berger <dberger@redhat.com>
2017-03-02 08:12:19 -0700

    Use modern validation syntax.

--- app/models/provider_tag.rb
+++ app/models/provider_tag.rb
@@ -1,6 +1,6 @@
 class ProviderTag < ApplicationRecord
-  validates_presence_of :key
-  validates_presence_of :resource_id
-  validates_presence_of :type
-  validates_uniqueness_of :key, :scope => [:value, :resource_id]
+  validates :key, :presence => true
+  validates :resource_id, :presence => true
+  validates :type, :presence => true
+  validates :key, :uniqueness => { :scope => [:value, :resource_id] }
 end
