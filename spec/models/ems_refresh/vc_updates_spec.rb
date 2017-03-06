fb09c6da0c732570bbf1fb70780c79db2935f290
Adam Grare <agrare@redhat.com>
2017-03-06 10:48:45 -0500

    Extract VIM_SELECTOR_SPEC from EmsRefresh::VcUpdates

--- spec/models/ems_refresh/vc_updates_spec.rb
+++ spec/models/ems_refresh/vc_updates_spec.rb
@@ -40,43 +40,4 @@ describe EmsRefresh::VcUpdates do
       expect(@vm.send(meth)).to eq(expected)
     end
   end
-
-  it ".selected_property?" do
-    EmsRefresh::VcUpdates.with_constants(
-      :VIM_SELECTOR_SPEC => {
-        :ems_refresh_vm => [
-          "config.extraConfig[*].key",
-          "config.hardware.device[*].backing.compatibilityMode",
-          "summary.guest.hostName",
-          "summary.runtime.powerState"
-        ]
-      }
-    ) do
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "summary.runtime.powerState")).to be_truthy
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "summary.runtime")).to be_truthy
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "summary")).to be_truthy
-
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "summary.runtime.power")).to be_falsey
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "summary.run")).to be_falsey
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "sum")).to be_falsey
-
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.hardware.device[2000].backing.compatibilityMode"))
-        .to be_truthy
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.hardware.device[2000].backing")).to be_truthy
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.hardware.device[2000]")).to be_truthy
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.hardware.device")).to be_truthy
-
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.hardware.device[2000].back")).to be_falsey
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.hardware.dev")).to be_falsey
-
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.extraConfig[\"vmsafe.enable\"].key")).to be_truthy
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.extraConfig[\"vmsafe.enable\"]")).to be_truthy
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "config.extraConfig")).to be_truthy
-
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "summary.guest")).to be_truthy
-      expect(EmsRefresh::VcUpdates.selected_property?(:vm, "summary.guest.disk")).to be_falsey
-
-      expect(EmsRefresh::VcUpdates.selected_property?(:other, "does.not.matter")).to be_falsey
-    end
-  end
 end
