1770329bd97b0638179d3168811c95cafaf51021
Mooli Tayer <mtayer@redhat.com>
2017-03-06 13:43:50 +0200

    Add skeletal hawkular event collection

--- app/models/manageiq/providers/hawkular/datawarehouse_manager.rb
+++ app/models/manageiq/providers/hawkular/datawarehouse_manager.rb
@@ -2,6 +2,7 @@ module ManageIQ::Providers
   class Hawkular::DatawarehouseManager < ManageIQ::Providers::DatawarehouseManager
     require 'hawkular/hawkular_client'
 
+    require_nested :EventCatcher
     require_nested :RefreshParser
     require_nested :RefreshWorker
     require_nested :Refresher
@@ -102,6 +103,10 @@ module ManageIQ::Providers
 
     def self.description
       @description ||= "Hawkular Datawarehouse".freeze
+    end
+
+    def self.event_monitor_class
+      ManageIQ::Providers::Hawkular::DatawarehouseManager::EventCatcher
     end
   end
 end
