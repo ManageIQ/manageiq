3d3f4b8a0e71cc374e4757ac8795300f09162551
Daniel Berger <dberger@redhat.com>
2017-03-09 09:26:49 -0700

    Add manager_type column, remove labeled_resource_type column.

--- db/migrate/20170307215002_rename_container_label_tag_mapping.rb
+++ db/migrate/20170307215002_rename_container_label_tag_mapping.rb
@@ -4,25 +4,34 @@ class RenameContainerLabelTagMapping < ActiveRecord::Migration[5.0]
 
   def change
     create_table :mappable_entities do |t|
-      t.string :name,     :comment => "The name of the mappable resource type."
-      t.string :provider, :comment => "The provider name associated with the resource type."
+      t.string :name,          :comment => "The name of the mappable resource type."
+      t.string :provider,      :comment => "The provider name associated with the resource type."
+      t.string :manager_type,  :comment => "The full class name of the resource type."
     end
 
     # Add the current default list of mappable entities currently defined by
     # the ContainerLabelTagMapping::MAPPABLE_ENTITIES array.
-    [
-      'ContainerProject',
-      'ContainerRoute',
-      'ContainerNode',
-      'ContainerReplicator',
-      'ContainerService',
-      'ContainerGroup',
-      'ContainerBuild',
+    %w[
+      ContainerProject
+      ContainerRoute
+      ContainerNode
+      ContainerReplicator
+      ContainerService
+      ContainerGroup
+      ContainerBuild
     ].each do |entity|
-      MappableEntity.create!(:name => entity, :provider => 'kubernetes')
+      MappableEntity.create!(
+        :name         => entity,
+        :provider     => 'kubernetes',
+        :manager_type => 'ManageIQ::Providers::Kubernetes::ContainerManager::' + entity
+      )
     end
 
-    MappableEntity.create!(:name => 'VmOrTemplate', :provider => 'amazon')
+    MappableEntity.create!(
+      :name         => 'VmOrTemplate',
+      :provider     => 'amazon',
+      :manager_type => 'ManageIQ::Providers::Amazon::CloudManager::VmOrTemplate'
+    )
 
     rename_table :container_label_tag_mappings, :provider_label_tag_mappings
     add_column :provider_label_tag_mappings, :mappable_entity_id, :integer
@@ -32,5 +41,7 @@ class RenameContainerLabelTagMapping < ActiveRecord::Migration[5.0]
       mapping.mappable_entity_id = MappableEntity.find_by_name(name).id if name
       mapping.save!
     end
+
+    remove_column :provider_label_tag_mappings, :labeled_resource_type, :string
   end
 end
