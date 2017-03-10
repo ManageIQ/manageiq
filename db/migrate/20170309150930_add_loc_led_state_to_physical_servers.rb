00c4681a2896897e6bc7711ef16ff9c2d1a28fcc
skovic <skovic@lenovo.com>
2017-03-10 16:50:44 -0500

    Convert locLedState column name to snake case

--- db/migrate/20170309150930_add_loc_led_state_to_physical_servers.rb
+++ db/migrate/20170309150930_add_loc_led_state_to_physical_servers.rb
@@ -1,5 +1,5 @@
 class AddLocLedStateToPhysicalServers < ActiveRecord::Migration[5.0]
   def change
-    add_column :physical_servers, :locLedState, :string
+    add_column :physical_servers, :loc_led_state, :string
   end
 end
