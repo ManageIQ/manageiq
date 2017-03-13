10682a3be4fcbbabcd0f665969c237c49f449d6e
supernoodz <ncatling@redhat.com>
2017-03-13 15:24:19 +0000

    Extend container service model

--- lib/miq_automation_engine/service_models/miq_ae_service_container_template.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_container_template.rb
@@ -2,7 +2,5 @@ module MiqAeMethodService
   class MiqAeServiceContainerTemplate < MiqAeServiceModelBase
     expose :container_template_parameters, :association => true
     expose :ext_management_system,         :association => true
-    expose :is_tagged_with?
-    expose :tags
   end
 end
