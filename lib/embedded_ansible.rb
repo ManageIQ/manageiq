b12c8fd5a73f58e6bf0e0f8b4fbdb475ccb94eab
Nick Carboni <ncarboni@redhat.com>
2017-03-13 18:02:33 -0400

    Use the new setup script argument types
    A change was made to pass options that come to the setup script
    after a "--" straight through, unaltered to the ansible-playbook
    command.
    
    This is how we will get access to the --tags and --skip-tags options
    that we need.

--- lib/embedded_ansible.rb
+++ lib/embedded_ansible.rb
@@ -43,13 +43,13 @@ class EmbeddedAnsible
 
   def self.configure
     configure_secret_key
-    run_setup_script(playbook_extra_variables.merge(:k => CONFIGURE_EXCLUDE_TAGS))
+    run_setup_script(CONFIGURE_EXCLUDE_TAGS)
     stop
   end
 
   def self.start
     configure_secret_key
-    run_setup_script(playbook_extra_variables.merge(:k => START_EXCLUDE_TAGS))
+    run_setup_script(START_EXCLUDE_TAGS)
   end
 
   def self.stop
@@ -64,19 +64,21 @@ class EmbeddedAnsible
     AwesomeSpawn.run!("source /etc/sysconfig/ansible-tower; echo $TOWER_SERVICES").output.split
   end
 
-  def self.playbook_extra_variables
-    json_value = {
+  def self.run_setup_script(exclude_tags)
+    json_extra_vars = {
       :minimum_var_space => 0,
       :http_port         => HTTP_PORT,
       :https_port        => HTTPS_PORT
     }.to_json
-    {:e => json_value}
-  end
-  private_class_method :playbook_extra_variables
 
-  def self.run_setup_script(params)
     with_inventory_file do |inventory_file_path|
-      AwesomeSpawn.run!(SETUP_SCRIPT, :params => params.merge(:i => inventory_file_path))
+      params = {
+        "--"         => nil,
+        :extra_vars= => json_extra_vars,
+        :inventory=  => inventory_file_path,
+        :skip_tags=  => exclude_tags
+      }
+      AwesomeSpawn.run!(SETUP_SCRIPT, :params => params)
     end
   end
   private_class_method :run_setup_script
