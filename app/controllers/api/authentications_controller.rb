aed46b5e61834b2fc25d013d40f1f2997fe81436
Jillian Tullo <jtullo@redhat.com>
2017-03-14 08:21:06 -0400

    edit authentications
    update specs

--- app/controllers/api/authentications_controller.rb
+++ app/controllers/api/authentications_controller.rb
@@ -1,5 +1,13 @@
 module Api
   class AuthenticationsController < BaseController
+    def edit_resource(type, id, data)
+      auth = resource_search(id, type, collection_class(:authentications))
+      task_id = auth.update_in_provider_queue(data)
+      action_result(true, "Updating Authentication with id #{id}", :task_id => task_id)
+    rescue => err
+      raise "Could not update Authentication - #{err}"
+    end
+
     def delete_resource(type, id, _data = {})
       auth = resource_search(id, type, collection_class(:authentications))
       raise "Delete not supported for #{authentication_ident(auth)}" unless auth.respond_to?(:delete_in_provider_queue)
--- spec/requests/api/authentications_spec.rb
+++ spec/requests/api/authentications_spec.rb
@@ -31,7 +31,6 @@ RSpec.describe 'Authentications API' do
 
   describe 'GET /api/authentications/:id' do
     it 'will show an authentication configuration script base' do
-      auth = FactoryGirl.create(:authentication)
       api_basic_authorize action_identifier(:authentications, :read, :resource_actions, :get)
 
       run_get(authentications_url(auth.id))
@@ -44,7 +43,6 @@ RSpec.describe 'Authentications API' do
     end
 
     it 'forbids access to an authentication configuration script base' do
-      auth = FactoryGirl.create(:authentication)
       api_basic_authorize
 
       run_get(authentications_url(auth.id))
@@ -54,6 +52,14 @@ RSpec.describe 'Authentications API' do
   end
 
   describe 'POST /api/authentications' do
+    let(:params) do
+      {
+        :id          => auth.id,
+        :description => 'Description',
+        :name        => 'Updated Credential'
+      }
+    end
+
     it 'will delete an authentication' do
       api_basic_authorize collection_action_identifier(:authentications, :delete, :post)
 
@@ -120,9 +126,66 @@ RSpec.describe 'Authentications API' do
       run_post(authentications_url, :action => 'delete', :resources => [{ 'id' => auth.id }])
       expect(response).to have_http_status(:forbidden)
     end
+
+    it 'can update an authentication with an appropriate role' do
+      api_basic_authorize collection_action_identifier(:authentications, :edit)
+
+      run_post(authentications_url, :action => 'edit', :resources => [params])
+
+      expected = {
+        'results' => [
+          a_hash_including(
+            'success' => true,
+            'message' => "Updating Authentication with id #{auth.id}",
+            'task_id' => a_kind_of(Numeric)
+          )
+        ]
+      }
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to include(expected)
+    end
+
+    it 'can update an authentication with an appropriate role' do
+      params2 = params.dup.merge(:id => auth_2.id)
+      api_basic_authorize collection_action_identifier(:authentications, :edit)
+
+      run_post(authentications_url, :action => 'edit', :resources => [params, params2])
+
+      expected = {
+        'results' => [
+          a_hash_including(
+            'success' => true,
+            'message' => "Updating Authentication with id #{auth.id}",
+            'task_id' => a_kind_of(Numeric)
+          ),
+          a_hash_including(
+            'success' => true,
+            'message' => "Updating Authentication with id #{auth_2.id}",
+            'task_id' => a_kind_of(Numeric)
+          )
+        ]
+      }
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to include(expected)
+    end
+
+    it 'will forbid update to an authentication without appropriate role' do
+      api_basic_authorize
+
+      run_post(authentications_url, :action => 'edit', :resources => [params])
+
+      expect(response).to have_http_status(:forbidden)
+    end
   end
 
   describe 'POST /api/authentications/:id' do
+    let(:params) do
+      {
+        :description => 'Description',
+        :name        => 'Updated Credential'
+      }
+    end
+
     it 'will delete an authentication' do
       api_basic_authorize action_identifier(:authentications, :delete, :resource_actions, :post)
 
@@ -141,6 +204,28 @@ RSpec.describe 'Authentications API' do
       api_basic_authorize
 
       run_post(authentications_url(auth.id), :action => 'delete')
+
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'can update an authentication with an appropriate role' do
+      api_basic_authorize collection_action_identifier(:authentications, :edit)
+
+      run_post(authentications_url(auth.id), :action => 'edit', :resource => params)
+
+      expected = {
+        'success' => true,
+        'message' => "Updating Authentication with id #{auth.id}",
+        'task_id' => a_kind_of(Numeric)
+      }
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to include(expected)
+    end
+
+    it 'will forbid update to an authentication without appropriate role' do
+      api_basic_authorize
+
+      run_post(authentications_url(auth.id), :action => 'edit', :resource => params)
 
       expect(response).to have_http_status(:forbidden)
     end
