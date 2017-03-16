592121989109bede0e7e30b110ef3d8193efdaf7
Nick Carboni <ncarboni@redhat.com>
2017-03-16 16:38:41 -0400

    Add a concern for storing and accessing default embedded ansible object ids
    This will allow us to determine the ansible side object to relate
    new objects to.
    
    This also frees us from having to store an extra flag in our inventory
    to identify which objects are the "system created" ones and which
    are the other objects that may be artifacts of running jobs.
    
    https://www.pivotaltracker.com/story/show/140098929

--- app/models/manageiq/providers/embedded_ansible/provider.rb
+++ app/models/manageiq/providers/embedded_ansible/provider.rb
@@ -1,6 +1,8 @@
 class ManageIQ::Providers::EmbeddedAnsible::Provider < ::Provider
   include ManageIQ::Providers::AnsibleTower::Shared::Provider
 
+  include_concern 'DefaultAnsibleObjects'
+
   has_one :automation_manager,
           :foreign_key => "provider_id",
           :class_name  => "ManageIQ::Providers::EmbeddedAnsible::AutomationManager",
--- spec/models/manageiq/providers/embedded_ansible/provider_spec.rb
+++ spec/models/manageiq/providers/embedded_ansible/provider_spec.rb
@@ -2,4 +2,44 @@ require 'support/ansible_shared/provider'
 
 describe ManageIQ::Providers::EmbeddedAnsible::Provider do
   it_behaves_like 'ansible provider'
+
+  subject { FactoryGirl.build(:provider_embedded_ansible) }
+
+  context "DefaultAnsibleObjects concern" do
+    before do
+      subject.save
+    end
+
+    context "with no attributes" do
+      %w(organization credential inventory host).each do |obj_name|
+        it "#default_#{obj_name} returns nil" do
+          expect(subject.public_send("default_#{obj_name}")).to be_nil
+        end
+
+        it "#default_#{obj_name}= creates a new custom attribute" do
+          subject.public_send("default_#{obj_name}=", obj_name.length)
+          expect(subject.default_ansible_objects.find_by(:name => obj_name).value.to_i).to eq(obj_name.length)
+        end
+      end
+    end
+
+    context "with attributes saved" do
+      before do
+        %w(organization credential inventory host).each do |obj_name|
+          subject.default_ansible_objects.create(:name => obj_name, :value => obj_name.length)
+        end
+      end
+
+      %w(organization credential inventory host).each do |obj_name|
+        it "#default_#{obj_name} returns the saved value" do
+          expect(subject.public_send("default_#{obj_name}")).to eq(obj_name.length)
+        end
+
+        it "#default_#{obj_name}= doesn't create a second object if we pass the same value" do
+          subject.public_send("default_#{obj_name}=", obj_name.length)
+          expect(subject.default_ansible_objects.where(:name => obj_name).count).to eq(1)
+        end
+      end
+    end
+  end
 end
