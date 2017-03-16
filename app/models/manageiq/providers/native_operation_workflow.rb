80cdc8c75e3b8d004c343398af1d1dc8c1c02d9f
Adam Grare <agrare@redhat.com>
2017-03-16 15:24:07 -0400

    Wait for refresh and send notification

--- app/models/manageiq/providers/native_operation_workflow.rb
+++ app/models/manageiq/providers/native_operation_workflow.rb
@@ -29,14 +29,52 @@ class ManageIQ::Providers::NativeOperationWorkflow < Job
   end
 
   def refresh
+    target = target_entity
+
+    task_ids = EmsRefresh.queue_refresh_task(target)
+
+    context[:refresh_task_ids] = task_ids
+    update_attributes!(:context => context)
+
     signal(:poll_refresh)
   end
 
   def poll_refresh
-    signal(:notify)
+    refresh_finished = true
+
+    context[:refresh_task_ids].each do |task_id|
+      task = MiqTask.find(task_id)
+      if task.state != MiqTask::STATE_FINISHED
+        refresh_finished = false
+        break
+      end
+    end
+
+    if refresh_finished
+      signal(:notify)
+    else
+      # TODO use queue_signal and deliver_on
+      sleep(10)
+
+      signal(:poll_refresh)
+    end
   end
 
   def notify
+    notification_options = {
+      :target_name => target_entity.name,
+      :method      => options[:method]
+    }
+
+    if status == "ok"
+      type = :provider_operation_success
+    else
+      type = :provider_operation_failure
+      notification_options[:error] = message
+    end
+
+    Notification.create(:type => type, :options => notification_options)
+
     signal(:finish)
   end
 
@@ -45,5 +83,4 @@ class ManageIQ::Providers::NativeOperationWorkflow < Job
   alias finish       process_finished
   alias abort_job    process_abort
   alias cancel       process_cancel
-  alias error        process_error
 end
