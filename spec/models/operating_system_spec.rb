0312410af23de68e583604e4dbd3439cbeab9760
Keenan Brock <kbrock@redhat.com>
2017-03-16 17:02:18 -0400

    OS name detection - add tests

--- spec/models/operating_system_spec.rb
+++ spec/models/operating_system_spec.rb
@@ -18,4 +18,44 @@ describe OperatingSystem do
       end
     end
   end
+
+  # passing in a vm or host
+  context "#image_name" do
+    it "uses os.distribution over os.product_type" do
+      expect(
+        described_class.image_name(host_with_os(:distribution => "rhel-7", :product_type => "centos-7"))
+      ).to eq("linux_redhat")
+    end
+
+    it "falls back to os.product_type if invalid os.distribution" do
+      expect(
+        described_class.image_name(host_with_os(:distribution => "undiscovered-7", :product_type => "rhel-7"))
+      ).to eq("linux_redhat")
+    end
+
+    it "falls back to os.product_name" do
+      expect(
+        described_class.image_name(host_with_os(:distribution => "undiscovered-7", :product_name => "rhel-7"))
+      ).to eq("linux_redhat")
+    end
+
+    it "falls back to Host#user_assigned_os" do
+      expect(
+        described_class.image_name(host_with_os({:distribution => "undiscovered-7"}, {:user_assigned_os => "rhel-7"}))
+      ).to eq("linux_redhat")
+    end
+
+    it "falls back to hardware.guest_os" do
+      expect(
+        described_class.image_name(host_with_os({:distribution => "undiscovered-7"}, {}, {:guest_os => "rhel-7"}))
+      ).to eq("linux_redhat")
+    end
+  end
+
+  def host_with_os(os_attributes = nil, host_attributes = nil, hardware_attributes = nil)
+    host = Host.new(host_attributes || {})
+    host.operating_system = OperatingSystem.new(os_attributes) if os_attributes
+    host.hardware = Hardware.new(hardware_attributes) if hardware_attributes
+    host
+  end
 end
