43da27bc57458dbd8ac6ff30af1b5370f943972b
lpichler <lpichler@redhat.com>
2017-03-17 15:27:49 +0100

    Use way for displaying title as is in euwe
    (in master it is different )

--- app/helpers/application_helper/button/set_ownership.rb
+++ app/helpers/application_helper/button/set_ownership.rb
@@ -2,8 +2,11 @@ class ApplicationHelper::Button::SetOwnership < ApplicationHelper::Button::Basic
   needs :@record
 
   def disabled?
-    @error_message = _('Ownership is controlled by tenant mapping') if
-      @record.ext_management_system.tenant_mapping_enabled?
-    @error_message.present?
+    @record.ext_management_system.tenant_mapping_enabled?
+  end
+
+  def calculate_properties
+    super
+    self[:title] = _('Ownership is controlled by tenant mapping') if disabled?
   end
 end
