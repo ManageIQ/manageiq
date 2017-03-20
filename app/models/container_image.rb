71caa795e82236677370d09028cb530f5b107451
Erez Freiberger <efreiber@redhat.com>
2017-03-20 19:57:49 +0200

    fixing full_name not returning docker_id when it should

--- app/models/container_image.rb
+++ app/models/container_image.rb
@@ -36,7 +36,7 @@ class ContainerImage < ApplicationRecord
   after_create :raise_creation_event
 
   def full_name
-    docker_id if image_ref && image_ref.start_with?(DOCKER_PULLABLE_PREFIX)
+    return docker_id if image_ref && image_ref.start_with?(DOCKER_PULLABLE_PREFIX)
     result = ""
     result << "#{container_image_registry.full_name}/" unless container_image_registry.nil?
     result << name
