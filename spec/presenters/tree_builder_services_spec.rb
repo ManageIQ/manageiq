f67bd9deee913710c8d9bd2c78d9475f4a3b01d9
lgalis <lgalis@redhat.com>
2017-03-20 08:48:38 -0400

    Updates specs for services tree

--- spec/presenters/tree_builder_services_spec.rb
+++ spec/presenters/tree_builder_services_spec.rb
@@ -7,18 +7,12 @@ describe TreeBuilderServices do
     expect(root_nodes.size).to eq(2)
     active_nodes = kid_nodes(root_nodes[0])
     retired_nodes = kid_nodes(root_nodes[1])
-    expect(active_nodes).to eq(
-      @service => {
-        @service_c1 => {
-          @service_c11 => {},
-          @service_c12 => {
-            @service_c121 => {}
-          }
-        },
-        @service_c2 => {}
-      }
-    )
-    expect(retired_nodes).to eq(@service_c3 => {})
+    expect(active_nodes).to match(
+                                @service => {},
+                                @service_c1 => {},
+                                @service_c2 => {}
+                            )
+    expect(retired_nodes).to match(@service_c3 => {})
   end
 
   private
@@ -33,12 +27,11 @@ describe TreeBuilderServices do
 
   def create_deep_tree
     @service      = FactoryGirl.create(:service, :display => true, :retired => false)
-    @service_c1   = FactoryGirl.create(:service, :service => @service, :display => true, :retired => false)
+    @service_c1   = FactoryGirl.create(:service, :display => true, :retired => false)
     @service_c11  = FactoryGirl.create(:service, :service => @service_c1, :display => true, :retired => false)
     @service_c12  = FactoryGirl.create(:service, :service => @service_c1, :display => true, :retired => false)
     @service_c121 = FactoryGirl.create(:service, :service => @service_c12, :display => true, :retired => false)
-    @service_c2   = FactoryGirl.create(:service, :service => @service, :display => true, :retired => false)
-    # hidden
-    @service_c3   = FactoryGirl.create(:service, :service => @service, :retired => true)
+    @service_c2   = FactoryGirl.create(:service, :display => true, :retired => false)
+    @service_c3   = FactoryGirl.create(:service, :retired => true)
   end
 end
