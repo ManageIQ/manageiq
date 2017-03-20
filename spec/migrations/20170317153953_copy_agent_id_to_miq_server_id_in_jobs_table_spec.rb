b959f9fbb5d0357fb5d8b4d898d8afc94cf013e7
Yuri Rudman <yrudman@redhat.com>
2017-03-20 12:46:14 -0400

    DRY in spec

--- spec/migrations/20170317153953_copy_agent_id_to_miq_server_id_in_jobs_table_spec.rb
+++ spec/migrations/20170317153953_copy_agent_id_to_miq_server_id_in_jobs_table_spec.rb
@@ -1,21 +1,22 @@
 require_migration
 
 describe CopyAgentIdToMiqServerIdInJobsTable do
-  let(:jobs_stub) { migration_stub(:Job) }
+  let(:job_stub) { migration_stub(:Job) }
+  let(:job_name) { "Hello Test Job" }
 
   migration_context :up do
     it "copies data from 'agent_id' to 'miq_server_id' column on jobs table" do
-      jobs_stub.create!(:name => "Hello Test Job", :agent_id => 111)
+      job_stub.create!(:name => job_name, :agent_id => 111)
       migrate
-      expect(Job.find_by(:name => "Hello Test Job").miq_server_id).to eq 111
+      expect(Job.find_by(:name => job_name).miq_server_id).to eq 111
     end
   end
 
   migration_context :down do
     it "nullifies 'miq_server_id' column on jobs table" do
-      jobs_stub.create!(:name => "Hello Test Job", :miq_server_id => 111)
+      job_stub.create!(:name => job_name, :miq_server_id => 111)
       migrate
-      expect(Job.find_by(:name => "Hello Test Job").miq_server_id).to be nil
+      expect(Job.find_by(:name => job_name).miq_server_id).to be nil
     end
   end
 end
