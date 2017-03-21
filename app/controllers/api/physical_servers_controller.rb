1fc35c4110ab148a95280f6dd123d55fe816b2b4
skovic <skovic@lenovo.com>
2017-03-21 17:16:30 -0400

    Fix whitespace in PhysicalServersController

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -5,7 +5,7 @@ module Api
         physical_server = PhysicalServer.find(params[:c_id])
         response_payload = physical_server.as_json
         firmwares = physical_server.firmwares.map(&:as_json)
-        response_payload['firmwares'] = firmwares 
+        response_payload['firmwares'] = firmwares
         response_payload['host'] = case physical_server.host
                                    when nil then nil
                                    else physical_server.host.id
