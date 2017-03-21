f0207c2bad8b7c4acd0bbb4fd88eb95d2aace959
Rodney Brown <rbrown4@lenovo.com>
2017-03-21 13:06:36 -0400

    Modifies the REST response change host to host_id

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -4,10 +4,10 @@ module Api
       if params[:c_id]
         physical_server = PhysicalServer.find(params[:c_id])
         response_payload = physical_server.as_json
-        response_payload['host'] = case physical_server.host
-                                   when nil then nil
-                                   else physical_server.host.id
-                                   end
+        response_payload['host_id'] = case physical_server.host
+                                      when nil then nil
+                                      else physical_server.host.id
+                                      end
 
         render :json=> response_payload
       else
