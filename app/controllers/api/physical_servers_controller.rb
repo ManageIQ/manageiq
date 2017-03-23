3c72c7e9e16c6bef33cb4de29c4ca34f3aba49e3
Rodney Brown <rbrown4@lenovo.com>
2017-03-23 16:00:16 -0400

    Removes custom show method from physical_servers_controller

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -1,19 +1,4 @@
 module Api
   class PhysicalServersController < BaseController
-    def show
-      if params[:c_id]
-        physical_server = PhysicalServer.find(params[:c_id])
-        response_payload = physical_server.as_json
-        response_payload['host_id'] = physical_server.host.try(:id)
-
-        render :json=> response_payload
-      else
-        super
-      end
-    end
-
-    def server_ident(server)
-      "Server instance: #{server.id} name:'#{server.name}'"
-    end
   end
 end
