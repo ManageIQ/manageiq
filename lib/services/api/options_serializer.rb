52b291baf46dd52e279a36777bbb82d937498062
Tim Wade <hello@timjwade.com>
2017-03-24 10:14:49 -0700

    Extract OptionsSerializer

---
+++ lib/services/api/options_serializer.rb
@@ -0,0 +1,28 @@
+module Api
+  class OptionsSerializer
+    def self.serialize(klass, resource, data = {})
+      options = if klass
+                  {
+                    :attributes         => options_attribute_list(klass.attribute_names -
+                                                                  klass.virtual_attribute_names),
+                    :virtual_attributes => options_attribute_list(klass.virtual_attribute_names),
+                    :relationships      => (klass.reflections.keys |
+                                            klass.virtual_reflections.keys.collect(&:to_s)).sort
+                  }
+                else
+                  {:attributes => [], :virtual_attributes => [], :relationships => []}
+                end
+      options[:subcollections] = Array(CollectionConfig.new[resource].subcollections).sort
+      options[:data] = data
+      options
+    end
+
+    def self.options_attribute_list(attrlist)
+      attrlist.sort.select { |attr| !encrypted_attribute?(attr) }
+    end
+
+    def self.encrypted_attribute?(attr)
+      Environment.normalized_attributes[:encrypted].key?(attr.to_s) || attr.to_s.include?('password')
+    end
+  end
+end
