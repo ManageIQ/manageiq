9335208f05e4baa2421cb58c56b2a257951ba886
Tim Wade <hello@timjwade.com>
2017-03-24 11:23:37 -0700

    Eliminate ServiceTemplateWorkflow service

--- app/controllers/api/service_orders_controller.rb
+++ app/controllers/api/service_orders_controller.rb
@@ -81,7 +81,7 @@ module Api
         raise BadRequestError, "Must specify a service_template_href for adding a service_request"
       end
       service_template = resource_search(service_template_id, :service_templates, ServiceTemplate)
-      ServiceTemplateWorkflow.create(service_template, service_request)
+      service_template.provision_workflow(User.current_user, service_request)
     end
 
     def check_validation(validation)
--- lib/services/api/service_template_workflow.rb
+++
@@ -1,7 +0,0 @@
-module Api
-  class ServiceTemplateWorkflow
-    def self.create(service_template, service_request)
-      service_template.provision_workflow(User.current_user, service_request)
-    end
-  end
-end
