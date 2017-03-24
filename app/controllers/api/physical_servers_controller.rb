c919b41987294892eaf924ab201de8a60a3ed3e1
Walter Alves <walter.arruda.alves@gmail.com>
2017-03-24 13:10:46 -0300

    Fix rubocop style warnings

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -1,20 +1,5 @@
 module Api
   class PhysicalServersController < BaseController
-    def show
-      if params[:c_id]
-        physical_server = PhysicalServer.find(params[:c_id])
-        response_payload = physical_server.as_json
-        response_payload['host'] = case physical_server.host
-                                   when nil then nil
-                                   else physical_server.host.id
-                                   end
-
-        render :json => response_payload
-      else
-        super
-      end
-    end
-
     def blink_loc_led_resource(type, id, _data)
       change_resource_state(:blink_loc_led, type, id)
     end
