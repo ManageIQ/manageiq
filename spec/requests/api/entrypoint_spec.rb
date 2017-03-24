ManageIQ/manageiq

commit ff3f2ab298f1947128f1993c5e1106ab9469f88a

Move handle_options_request into ApiController
Moves this action into the ApiController (since it is only matched
against `OPTIONS /api/`) and renames to simply `options`, making it
polymorphic with other API controllers' options actions.

app/controllers/api/base_controller.rb
@@ -48,10 +48,6 @@ module Api
     respond_to :json
     rescue_from_api_errors
 
-    def handle_options_request
-      head(:ok)
-    end
-
     private
 
     def validate_response_format

app/controllers/api/api_controller.rb
@@ -1,5 +1,9 @@
 module Api
   class ApiController < Api::BaseController
+    def options
+      head(:ok)
+    end
+
     def index
       res = {
         :name         => Settings.base.name,

config/routes.rb
@@ -2849,9 +2849,7 @@ Vmdb::Application.routes.draw do
 
   namespace :api, :path => "api(/:version)", :version => API_VERSION_REGEX, :defaults => {:format => "json"} do
     root :to => "api#index"
-
-    # OPTIONS requests for REST API pre-flight checks
-    match '/' => 'base#handle_options_request', :via => :options
+    match "/", :to => "api#options", :via => :options
 
     unless defined?(API_ACTIONS)
       API_ACTIONS = {

commit c6d167848cfc369c98aa8d76194f24f67987a262

Don't log or require user/token on options
This takes care of a regression that was introduced in ff3f2ab298. The
`handle_options_request` was moved and renamed to be consistent with
the other `options` actions, but these `before_action` hooks were not
updated.

app/controllers/api/base_controller.rb
@@ -27,12 +27,12 @@ module Api
     include ActionController::HttpAuthentication::Basic::ControllerMethods
     extend ErrorHandler::ClassMethods
 
-    before_action :require_api_user_or_token, :except => [:handle_options_request]
+    before_action :require_api_user_or_token, :except => [:options]
     before_action :set_gettext_locale
     before_action :set_access_control_headers
     before_action :parse_api_request, :log_api_request, :validate_api_request
     before_action :validate_api_action, :except => [:options]
-    before_action :log_request_initiated, :only => [:handle_options_request]
+    before_action :log_request_initiated, :only => [:options]
     before_action :validate_response_format, :except => [:destroy]
     after_action :log_api_response
 

spec/requests/api/entrypoint_spec.rb
@@ -69,4 +69,12 @@ RSpec.describe "API entrypoint" do
       )
     )
   end
+
+  describe "OPTIONS /api" do
+    it "does not require a user or token" do
+      run_options(entrypoint_url)
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end