e36dba162cba2eac8494106ed09a3b4da67b3df4
Sergio Ocón <sergio.ocon@redhat.com>
2017-03-25 20:34:47 +0100

    fixing bucket migration, charge :destroy and adding specs (#17)
    * fixing bucket migration, charge :destroy and adding specs
    
    * minor fixes

--- db/migrate/20170316124240_create_showback_charges.rb
+++ db/migrate/20170316124240_create_showback_charges.rb
@@ -1,8 +1,8 @@
 class CreateShowbackCharges < ActiveRecord::Migration[5.0]
   def change
     create_table :showback_charges, id: :bigserial, force: :cascade do |t|
-      t.money :fixed_cost, allow_nil: true
-      t.money :variable_cost, allow_nil: true
+      t.decimal :fixed_cost, allow_nil: true
+      t.decimal :variable_cost, allow_nil: true
       t.belongs_to :showback_bucket, type: :bigint, index: true
       t.belongs_to :showback_event, type: :bigint, index: true
       t.timestamps
