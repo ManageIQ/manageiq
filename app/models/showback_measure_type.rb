9212d1d5d078976c1008893d538235edfc515d3f
Alberto Jesus Gutierrez Juanes <alberto@iMac-de-Alberto.local>
2017-03-26 21:54:48 +0200

    Migrate

--- app/models/showback_measure_type.rb
+++ app/models/showback_measure_type.rb
@@ -1,13 +1,14 @@
-class ShowbackConfiguration < ApplicationRecord
-  validates :name, :description, :name, :types, :presence => true
+class ShowbackMeasureType < ApplicationRecord
+  validates :name, :description, :measure, :types, :presence => true
 
-  serialize :types, Array # Implement types column as an array
-  default_value_for(:types) { [] }
-
-  VALID_MEASURE_TYPES = %w(Occurence Throughput Frequency Capacity Integer).freeze
+  VALID_MEASURE_TYPES = %w(CPU).freeze
 
   validates :measure,
             :inclusion => { :in => VALID_MEASURE_TYPES }
+
+  VALID_TYPES = %w(average number).freeze
+  validate :validate_types_measures
+
 
   def self.seed
     seed_data.each do |con_conf_attributtes|
@@ -22,13 +23,21 @@ class ShowbackConfiguration < ApplicationRecord
     end
   end
 
+  private∫
+
   def self.seed_file_name
     @seed_file_name ||= Rails.root.join("db", "fixtures", "#{table_name}.yml")
   end
-  private_class_method :seed_file_name
 
   def self.seed_data
     File.exist?(seed_file_name) ? YAML.load_file(seed_file_name) : []
   end
-  private_class_method :seed_data
+
+  def validate_types_measures
+    if (invalid_interests = (types - VALID_TYPES))
+      invalid_interests.each do |type|
+        errors.add(:types, type + " is not a valid measure type")
+      end
+    end
+  end
 end
