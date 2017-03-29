0f84170f56582adee97ce1f1a9b20dbac1c8e07a
Tim Wade <hello@timjwade.com>
2017-03-29 12:42:43 -0700

    Inline #api_error locals

--- app/controllers/api/base_controller/error_handler.rb
+++ app/controllers/api/base_controller/error_handler.rb
@@ -30,16 +30,11 @@ module Api
       private
 
       def api_error(type, error)
-        message = error.message
-        klass = error.class.name
-        backtrace = error.backtrace.join("\n")
-        status = Rack::Utils.status_code(type)
-
-        api_log_error("#{klass}: #{message}")
+        api_log_error("#{error.class.name}: #{error.message}")
         # We don't want to return the stack trace, but only log it in case of an internal error
-        api_log_error("\n\n#{backtrace}") if type == :internal_server_error && !backtrace.empty?
+        api_log_error("\n\n#{error.backtrace.join("\n")}") if type == :internal_server_error && !error.backtrace.empty?
 
-        render :json => ErrorSerializer.new(type, error).serialize, :status => status
+        render :json => ErrorSerializer.new(type, error).serialize, :status => Rack::Utils.status_code(type)
         log_api_response
       end
     end
