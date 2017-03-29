51b4c123f87ec1b33224a815b58e56b4e73c5ed0
Tim Wade <hello@timjwade.com>
2017-03-29 12:47:27 -0700

    Move #api_error into BaseController

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -53,5 +53,14 @@ module Api
     def collection_config
       @collection_config ||= CollectionConfig.new
     end
+
+    def api_error(type, error)
+      api_log_error("#{error.class.name}: #{error.message}")
+      # We don't want to return the stack trace, but only log it in case of an internal error
+      api_log_error("\n\n#{error.backtrace.join("\n")}") if type == :internal_server_error && !error.backtrace.empty?
+
+      render :json => ErrorSerializer.new(type, error).serialize, :status => Rack::Utils.status_code(type)
+      log_api_response
+    end
   end
 end
--- app/controllers/api/base_controller/error_handler.rb
+++ app/controllers/api/base_controller/error_handler.rb
@@ -1,16 +1,6 @@
 module Api
   class BaseController
     module ErrorHandler
-      private
-
-      def api_error(type, error)
-        api_log_error("#{error.class.name}: #{error.message}")
-        # We don't want to return the stack trace, but only log it in case of an internal error
-        api_log_error("\n\n#{error.backtrace.join("\n")}") if type == :internal_server_error && !error.backtrace.empty?
-
-        render :json => ErrorSerializer.new(type, error).serialize, :status => Rack::Utils.status_code(type)
-        log_api_response
-      end
     end
   end
 end
