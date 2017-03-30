2bede28352b02e99235b004d9422a3ea97caef97
Šimon Lukašík <isimluk@fedoraproject.org>
2017-03-30 14:31:28 +0200

    Make sure the specs fill in the MiqPolicy#mode

--- spec/factories/miq_policy.rb
+++ spec/factories/miq_policy.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :miq_policy do
     sequence(:name)        { |num| "policy_#{seq_padded_for_sorting(num)}" }
     sequence(:description) { |num| "Test Policy_#{seq_padded_for_sorting(num)}" }
+    mode                   'control'
     towhat                 "Vm"
   end
 
