f83e12769968ffd80d629ebe9bbb538737cdb7ef
Brant Evans <bevans@redhat.com>
2017-04-06 15:38:56 -0700

    Add tests for safe_filename

---
+++ spec/lib/task_helpers/exports_spec.rb
@@ -0,0 +1,44 @@
+describe '#safe_filename' do
+  context 'with filename' do
+    it 'should return a filename without spaces' do
+      filename = TaskHelpers::Exports.safe_filename('filename without spaces')
+      expect(filename).to eq('filename_without_spaces')
+    end
+
+    it 'should return a filename with spaces' do
+      filename = TaskHelpers::Exports.safe_filename('filename without spaces', true)
+      expect(filename).to eq('filename without spaces')
+    end
+
+    it 'should return a filename without / or spaces' do
+      filename = TaskHelpers::Exports.safe_filename('filename with / removed')
+      expect(filename).to eq('filename_with_slash_removed')
+    end
+
+    it 'should return a filename without / and with spaces' do
+      filename = TaskHelpers::Exports.safe_filename('filename with / removed', true)
+      expect(filename).to eq('filename with slash removed')
+    end
+
+    it 'should return a filename without | or spaces' do
+      filename = TaskHelpers::Exports.safe_filename('filename with | removed')
+      expect(filename).to eq('filename_with_pipe_removed')
+    end
+
+    it 'should return a filename without | and with spaces' do
+      filename = TaskHelpers::Exports.safe_filename('filename with | removed', true)
+      expect(filename).to eq('filename with pipe removed')
+    end
+
+    it 'should return a filename without /,  | or spaces' do
+      filename = TaskHelpers::Exports.safe_filename('filename with / and | removed')
+      expect(filename).to eq('filename_with_slash_and_pipe_removed')
+    end
+
+    it 'should not create duplicate filenames' do
+      filename1 = TaskHelpers::Exports.safe_filename('filename with / removed')
+      filename2 = TaskHelpers::Exports.safe_filename('filename with | removed')
+      expect(filename1).not_to eq(filename2)
+    end
+  end
+end
