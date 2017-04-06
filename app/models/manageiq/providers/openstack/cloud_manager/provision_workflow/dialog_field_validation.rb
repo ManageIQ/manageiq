c4918e47e63caef14e84c82e0aa5efca06cf4f6c
Marcel Hild <hild@b4mad.net>
2017-04-06 16:29:31 +0200

    extract openstack provider

--- app/models/manageiq/providers/openstack/cloud_manager/provision_workflow/dialog_field_validation.rb
+++
@@ -1,7 +0,0 @@
-# These methods are available for dialog field validation, do not erase.
-module ManageIQ::Providers::Openstack::CloudManager::ProvisionWorkflow::DialogFieldValidation
-  def validate_cloud_network(field, values, dlg, fld, value)
-    return nil if allowed_cloud_networks.length <= 1
-    validate_placement(field, values, dlg, fld, value)
-  end
-end
--- spec/models/manageiq/providers/openstack/cloud_manager/refresher_spec.rb
+++
@@ -1,5 +0,0 @@
-describe ManageIQ::Providers::Openstack::CloudManager::Refresher do
-  it ".ems_type" do
-    expect(described_class.ems_type).to eq(:openstack)
-  end
-end
