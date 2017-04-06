22e0867b68627d4412e0c159219b538dea8ffce1
Jillian Tullo <jtullo@redhat.com>
2017-04-06 17:03:08 -0400

    symbolize keys that are passed to provider methods

--- lib/services/api/authentication_service.rb
+++ lib/services/api/authentication_service.rb
@@ -4,7 +4,7 @@ module Api
       klass = ::Authentication.class_from_request_data(attrs)
       # TODO: Temporary validation - remove
       raise 'type not currently supported' unless klass.respond_to?(:create_in_provider_queue)
-      klass.create_in_provider_queue(manager_resource.id, attrs)
+      klass.create_in_provider_queue(manager_resource.id, attrs.deep_symbolize_keys)
     end
   end
 end
