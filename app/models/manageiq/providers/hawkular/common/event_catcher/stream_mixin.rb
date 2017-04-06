1a072cc395a67cb1a3b7f149e4fbb8fbbe257e43
Mooli Tayer <mtayer@redhat.com>
2017-04-06 16:52:29 +0300

    Tag seen alerts in hawkular

--- app/models/manageiq/providers/hawkular/common/event_catcher/stream_mixin.rb
+++ app/models/manageiq/providers/hawkular/common/event_catcher/stream_mixin.rb
@@ -21,7 +21,7 @@ module ManageIQ::Providers::Hawkular::Common::EventCatcher::StreamMixin
       log_handle.debug "Fetching tenant [#{tenant}] events using criteria [#{hawkular_alert_criteria}]"
       new_alerts.concat(@ems.alerts_client(:tenant => tenant).list_alerts(hawkular_alert_criteria))
     end
-    post_fetch(new_alerts)
+    @ems.alerts_client(:tenant => tenant).tag(new_alerts.map(:id).uniq, seen_alerts_tag) if method(:seen_alerts_tags)
     new_alerts
   rescue => err
     log_handle.error "Error capturing alerts #{err}"
