c02e4ae666eef0ffdd8f8ea69240a1a9c6e3ba66
Adam Grare <agrare@redhat.com>
2017-04-12 08:37:09 -0400

    Merge pull request #14742 from Ladas/ensure_google_managers_change_zone_and_provider_region_with_cloud_manager
    Ensure google managers change zone and provider region with cloud manager
    (cherry picked from commit 6d06ddeee20369e0aa49920ad5c45fe948eab236)

--- app/models/manageiq/providers/google/cloud_manager.rb
+++ app/models/manageiq/providers/google/cloud_manager.rb
@@ -20,6 +20,7 @@ class ManageIQ::Providers::Google::CloudManager < ManageIQ::Providers::CloudMana
   supports :regions
 
   before_create :ensure_managers
+  before_update :ensure_managers_zone_and_provider_region
 
   def ensure_network_manager
     build_network_manager(:type => 'ManageIQ::Providers::Google::NetworkManager') unless network_manager
