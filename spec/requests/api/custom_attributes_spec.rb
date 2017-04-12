a9048afc668b186445d56818cf15d871b2546a5e
Tim Wade <hello@timjwade.com>
2017-04-12 11:30:20 -0700

    Correctly configure custom attributes for DELETEs
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1422596

--- spec/requests/api/custom_attributes_spec.rb
+++ spec/requests/api/custom_attributes_spec.rb
@@ -1,4 +1,23 @@
 RSpec.describe "Custom Attributes API" do
+  describe "GET /api/<collection>/:cid/custom_attributes/:sid" do
+    it "renders the actions available on custom attribute members" do
+      vm = FactoryGirl.create(:vm_vmware)
+      custom_attribute = FactoryGirl.create(:custom_attribute, :resource => vm)
+      api_basic_authorize
+
+      run_get("#{vms_url(vm.id)}/custom_attributes/#{custom_attribute.id}")
+
+      expected = {
+        "actions" => a_collection_including(
+          a_hash_including("name" => "edit", "method" => "post"),
+          a_hash_including("name" => "delete", "method" => "post"),
+          a_hash_including("name" => "delete", "method" => "delete")
+        )
+      }
+      expect(response.parsed_body).to include(expected)
+    end
+  end
+
   it "can delete a custom attribute through its nested URI" do
     vm = FactoryGirl.create(:vm_vmware)
     custom_attribute = FactoryGirl.create(:custom_attribute, :resource => vm)
