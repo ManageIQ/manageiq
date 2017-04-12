b235985c4b21db324ff776380d7ef9c4eb633f47
Tzu-Mainn Chen <tzumainn@redhat.com>
2017-04-12 20:27:24 +0200

    remove responds_to in cloud tenant subcollection

--- app/controllers/api/subcollections/cloud_tenants.rb
+++ app/controllers/api/subcollections/cloud_tenants.rb
@@ -2,7 +2,7 @@ module Api
   module Subcollections
     module CloudTenants
       def cloud_tenants_query_resource(object)
-        object.respond_to?(:cloud_tenants) ? object.cloud_tenants : []
+        object.cloud_tenants
       end
     end
   end
