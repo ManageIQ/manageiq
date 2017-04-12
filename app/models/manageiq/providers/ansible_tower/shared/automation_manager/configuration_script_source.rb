eab192e2bd4588dfc3ae4396d04935bcdec5c346
Marcel Hild <hild@b4mad.net>
2017-04-12 11:22:18 +0200

    wrap update in a timeout block

--- app/models/manageiq/providers/ansible_tower/shared/automation_manager/configuration_script_source.rb
+++ app/models/manageiq/providers/ansible_tower/shared/automation_manager/configuration_script_source.rb
@@ -22,10 +22,17 @@ module ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::Configurati
 
       project_update = project.update
 
-      while project_update.finished.blank?
-        sleep REFRESH_ON_TOWER_SLEEP
-        project_update = project_update.api.project_updates.find(project_update.id)
+      # this is really just a quick hack. We should do this properly once
+      # https://github.com/ManageIQ/manageiq/pull/14405 is merged
+      log_header = "updating project #{project.id} (#{self.class.name} #{id})"
+      _log.info "#{log_header}..."
+      Timeout.timeout(5.minutes) do
+        while project_update.finished.blank?
+          sleep REFRESH_ON_TOWER_SLEEP
+          project_update = project_update.api.project_updates.find(project_update.id)
+        end
       end
+      _log.info "#{log_header}...Complete"
     end
   end
 end
