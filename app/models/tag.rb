f4b87cddf2fedfb4e81fc69b01956d7417015644
lpichler <lpichler@redhat.com>
2017-04-18 09:00:47 +0200

    Load virtual custom attribute before it is used
    In add_custom_attribute is also checking whether attribute
    has been already loaded

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -17,6 +17,10 @@ class Tag < ApplicationRecord
 
       if virtual_custom_attribute
         predicate.map! { |x| URI::RFC2396_Parser.new.unescape(x) }
+        # it is always array with one string element - name of virtual custom attribute because they are supported only
+        # in direct relations
+        custom_attribute = predicate.first
+        object.class.add_custom_attribute(custom_attribute) if object.class < CustomAttributeMixin
       end
 
       begin
