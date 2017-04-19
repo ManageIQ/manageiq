0b7f900789c9dc2f9a4707f6cd6f1090a80accbf
Rodney Brown <rbrown4@lenovo.com>
2017-04-19 09:05:14 -0400

    Changes uuid references to ems_ref

--- app/models/physical_server/operations/led.rb
+++ app/models/physical_server/operations/led.rb
@@ -19,8 +19,8 @@ module PhysicalServer::Operations::Led
 with a provider.") % {:name => name, :id => id}
     end
 
-    options = {:uuid => uuid}
-    _log.info("Begin #{verb} server: #{name} with UUID: #{uuid}")
+    options = {:uuid => ems_ref}
+    _log.info("Begin #{verb} server: #{name} with UUID: #{ems_ref}")
     ext_management_system.send(verb, self, options)
     _log.info("Complete #{verb} #{self}")
   end
--- app/models/physical_server/operations/power.rb
+++ app/models/physical_server/operations/power.rb
@@ -18,8 +18,8 @@ module PhysicalServer::Operations::Power
       raise _(" A Server #{self} <%{name}> with Id: <%{id}>
       is not associated with a provider.") % {:name => name, :id => id}
     end
-    options = {:uuid => uuid}
-    _log.info("Begin #{verb} server: #{name}  with UUID: #{uuid}")
+    options = {:uuid => ems_ref}
+    _log.info("Begin #{verb} server: #{name}  with UUID: #{ems_ref}")
     ext_management_system.send(verb, self, options)
     _log.info("Complete #{verb} #{self}")
   end
