30e0f0fcc214c2a55db2b94df3ce49e854745005
Marcel Hild <hild@b4mad.net>
2017-04-20 16:52:28 +0200

    extract openshift provider

--- app/models/manageiq/providers/openshift/container_manager/event_catcher.rb
+++
@@ -1,6 +0,0 @@
-class ManageIQ::Providers::Openshift::ContainerManager::EventCatcher < ManageIQ::Providers::BaseManager::EventCatcher
-  require_nested :Runner
-  def self.ems_class
-    ManageIQ::Providers::Openshift::ContainerManager
-  end
-end
