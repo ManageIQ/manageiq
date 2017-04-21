f422f711c6aed5293cc055e1ceab766158a4bbef
skovic <skovic@lenovo.com>
2017-04-21 10:39:18 -0400

    Address additional review comments

--- spec/requests/api/firmwares_spec.rb
+++ spec/requests/api/firmwares_spec.rb
@@ -2,23 +2,22 @@ RSpec.describe "firmwares API" do
   describe "display firmware details" do
     context "with a valid role" do
       it "shows its properties" do
-        fw = FactoryGirl.create(:firmware, :id => 1, :name => "UEFI",
-                                :version => "D7E152CUS-2.11", :resource_id => 1)
+        fw = FactoryGirl.create(:firmware,
+                                :name    => "UEFI",
+                                :version => "D7E152CUS-2.11")
 
         api_basic_authorize action_identifier(:firmwares, :read, :resource_actions, :get)
 
         run_get(firmwares_url(fw.id))
 
-        expect_single_resource_query("id"          => 1,
-                                     "name"        => "UEFI",
-                                     "version"     => "D7E152CUS-2.11",
-                                     "resource_id" => 1)
+        expect_single_resource_query("name"    => "UEFI",
+                                     "version" => "D7E152CUS-2.11")
       end
     end
 
     context "with an invalid role" do
       it "fails to show its properties" do
-        fw = FactoryGirl.create(:firmware, :id => 1)
+        fw = FactoryGirl.create(:firmware)
 
         api_basic_authorize
 
