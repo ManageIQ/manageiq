934e6643bc23319a87a686588aeff4bda80fe0de
Marcel Hild <hild@b4mad.net>
2017-04-24 20:45:43 +0200

    temp hack to make organization assignable
    [1] pry(main)> AnsibleTowerClient::Credential.send(:modelized_list)
    => #<Set: {}>
    [2] pry(main)> a = AnsibleTowerClient::Credential.new(nil, {org_id: 1})
    => <AnsibleTowerClient::Credential org_id=1>
    [3] pry(main)> a.org=1
    NoMethodError: undefined method `org=' for <AnsibleTowerClient::Credential org_id=1>:AnsibleTowerClient::Credential
    from (pry):3:in `<main>'
    [5] pry(main)> AnsibleTowerClient::Credential.send(:modelized_list)
    => #<Set: {"org_id"}>
    [6] pry(main)> b = AnsibleTowerClient::Credential.new(nil, {org: 1})
    => <AnsibleTowerClient::Credential org=1>
    [7] pry(main)> AnsibleTowerClient::Credential.send(:modelized_list)
    => #<Set: {"org_id", "org"}>
    [8] pry(main)> a.org=1
    => 1

--- app/models/manageiq/providers/embedded_ansible/automation_manager/credential.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/credential.rb
@@ -10,6 +10,8 @@ class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Credential < Mana
   include ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::TowerApi
 
   def self.provider_params(params)
+    # FIXME: workaround until https://github.com/ansible/ansible_tower_client_ruby/issues/68 is closed
+    AnsibleTowerClient::Credential.new(nil, :organization => 1)
     super.merge(:organization => ManageIQ::Providers::EmbeddedAnsible::AutomationManager.first.provider.default_organization)
   end
 end
