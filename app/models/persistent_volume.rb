cb0d234b24219bbda90aec82c9588ef146be14ff
nimrodshn <nshneor@redhat.com>
2017-04-24 10:58:58 +0300

    added factory for pvc
    small refactoring
    
    refactored to fix container volumes relation

--- app/models/persistent_volume.rb
+++ app/models/persistent_volume.rb
@@ -3,7 +3,7 @@ class PersistentVolume < ContainerVolume
   include NewWithTypeStiMixin
   serialize :capacity, Hash
   delegate :name, :to => :parent, :prefix => true
-  has_many :container_volumes, :through => :persistent_volume_claim
+  has_many :container_volumes, -> { where(:type => 'ContainerVolume') }, :through => :persistent_volume_claim
   has_many :parents, -> { distinct }, :through => :container_volumes, :source_type => 'ContainerGroup'
   alias_attribute :container_groups, :parents
 end
