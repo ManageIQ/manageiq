399f185e7840a8ab60f0dae6251b53d2b7477300
Nick LaMuro <nicklamuro@gmail.com>
2017-05-01 15:24:21 -0500

    Ignore RuboCop Security/YAMLLoad
    miq-yaml, from manageiq-gems-pending, doesn't currently seem to support
    `safe_load` properly, so using that, for now, is not an option.
    
    It should, however, probably be fixed (or miq-yaml should be removed) to
    support this, since this is most likely a good thing to have in place
    here.

--- lib/patches/database_configuration_patch.rb
+++ lib/patches/database_configuration_patch.rb
@@ -12,7 +12,13 @@ module DatabaseConfigurationPatch
       data = ERB.new(data).result if !ManageIQ.env.production? || ENV['ERB_IN_CONFIG']
 
       begin
+        # TODO: Allow this to work with miq-yaml:
+        #
+        # manageiq-gems-pending/gems/pending/util/extensions/miq-yaml
+        #
+        # rubocop:disable Security/YAMLLoad
         data = YAML.load(data) || {}
+        # rubocop:enable Security/YAMLLoad
       rescue Psych::SyntaxError => e
         raise "YAML syntax error occurred while parsing #{paths["config/database"].first}. " \
               "Please note that YAML must be consistently indented using spaces. Tabs are not allowed. " \
