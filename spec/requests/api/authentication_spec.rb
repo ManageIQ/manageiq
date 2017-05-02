841e595547c751c54f93c297203f59e03a2ff78e
Tim Wade <hello@timjwade.com>
2017-05-02 14:21:11 -0700

    Test sql and memory stores for token based authentication

--- spec/requests/api/authentication_spec.rb
+++ spec/requests/api/authentication_spec.rb
@@ -122,115 +122,121 @@ describe "Authentication API" do
   end
 
   context "Token Based Authentication" do
-    it "gets a token based identifier" do
-      api_basic_authorize
+    %w(sql memory).each do |session_store|
+      context "when using a #{session_store} session store" do
+        before { stub_settings_merge(:server => {:session_store => session_store}) }
 
-      run_get auth_url
+        it "gets a token based identifier" do
+          api_basic_authorize
 
-      expect(response).to have_http_status(:ok)
-      expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
-    end
+          run_get auth_url
 
-    it "authentication using a bad token" do
-      run_get entrypoint_url, :headers => {Api::HttpHeaders::AUTH_TOKEN => "badtoken"}
+          expect(response).to have_http_status(:ok)
+          expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
+        end
 
-      expect(response).to have_http_status(:unauthorized)
-    end
+        it "authentication using a bad token" do
+          run_get entrypoint_url, :headers => {Api::HttpHeaders::AUTH_TOKEN => "badtoken"}
 
-    it "authentication using a valid token" do
-      api_basic_authorize
+          expect(response).to have_http_status(:unauthorized)
+        end
 
-      run_get auth_url
+        it "authentication using a valid token" do
+          api_basic_authorize
 
-      expect(response).to have_http_status(:ok)
-      expect_result_to_have_keys(%w(auth_token))
+          run_get auth_url
 
-      auth_token = response.parsed_body["auth_token"]
+          expect(response).to have_http_status(:ok)
+          expect_result_to_have_keys(%w(auth_token))
 
-      run_get entrypoint_url, :headers => {Api::HttpHeaders::AUTH_TOKEN => auth_token}
+          auth_token = response.parsed_body["auth_token"]
 
-      expect(response).to have_http_status(:ok)
-      expect_result_to_have_keys(ENTRYPOINT_KEYS)
-    end
+          run_get entrypoint_url, :headers => {Api::HttpHeaders::AUTH_TOKEN => auth_token}
 
-    it "authentication using a valid token updates the token's expiration time" do
-      api_basic_authorize
+          expect(response).to have_http_status(:ok)
+          expect_result_to_have_keys(ENTRYPOINT_KEYS)
+        end
 
-      run_get auth_url
+        it "authentication using a valid token updates the token's expiration time" do
+          api_basic_authorize
 
-      expect(response).to have_http_status(:ok)
-      expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
+          run_get auth_url
 
-      auth_token = response.parsed_body["auth_token"]
-      token_expires_on = response.parsed_body["expires_on"]
+          expect(response).to have_http_status(:ok)
+          expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
 
-      tm = TokenManager.new("api")
-      token_info = tm.token_get_info(auth_token)
-      expect(token_info[:expires_on].utc.iso8601).to eq(token_expires_on)
+          auth_token = response.parsed_body["auth_token"]
+          token_expires_on = response.parsed_body["expires_on"]
 
-      expect_any_instance_of(TokenManager).to receive(:reset_token).with(auth_token)
-      run_get entrypoint_url, :headers => {Api::HttpHeaders::AUTH_TOKEN => auth_token}
+          tm = TokenManager.new("api")
+          token_info = tm.token_get_info(auth_token)
+          expect(token_info[:expires_on].utc.iso8601).to eq(token_expires_on)
 
-      expect(response).to have_http_status(:ok)
-      expect_result_to_have_keys(ENTRYPOINT_KEYS)
-    end
+          expect_any_instance_of(TokenManager).to receive(:reset_token).with(auth_token)
+          run_get entrypoint_url, :headers => {Api::HttpHeaders::AUTH_TOKEN => auth_token}
 
-    it "gets a token based identifier with the default API based token_ttl" do
-      api_basic_authorize
-      run_get auth_url
+          expect(response).to have_http_status(:ok)
+          expect_result_to_have_keys(ENTRYPOINT_KEYS)
+        end
 
-      expect(response).to have_http_status(:ok)
-      expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
-      expect(response.parsed_body["token_ttl"]).to eq(::Settings.api.token_ttl.to_i_with_method)
-    end
+        it "gets a token based identifier with the default API based token_ttl" do
+          api_basic_authorize
+          run_get auth_url
 
-    it "gets a token based identifier with an invalid requester_type" do
-      api_basic_authorize
+          expect(response).to have_http_status(:ok)
+          expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
+          expect(response.parsed_body["token_ttl"]).to eq(::Settings.api.token_ttl.to_i_with_method)
+        end
 
-      run_get auth_url, :requester_type => "bogus_type"
+        it "gets a token based identifier with an invalid requester_type" do
+          api_basic_authorize
 
-      expect_bad_request(/invalid requester_type/i)
-    end
+          run_get auth_url, :requester_type => "bogus_type"
 
-    it "gets a token based identifier with a UI based token_ttl" do
-      api_basic_authorize
+          expect_bad_request(/invalid requester_type/i)
+        end
 
-      run_get auth_url, :requester_type => "ui"
+        it "gets a token based identifier with a UI based token_ttl" do
+          api_basic_authorize
 
-      expect(response).to have_http_status(:ok)
-      expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
-      expect(response.parsed_body["token_ttl"]).to eq(::Settings.session.timeout.to_i_with_method)
-    end
+          run_get auth_url, :requester_type => "ui"
 
-    it "forgets the current token when asked to" do
-      api_basic_authorize
+          expect(response).to have_http_status(:ok)
+          expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
+          expect(response.parsed_body["token_ttl"]).to eq(::Settings.session.timeout.to_i_with_method)
+        end
 
-      run_get auth_url
+        it "forgets the current token when asked to" do
+          api_basic_authorize
 
-      auth_token = response.parsed_body["auth_token"]
+          run_get auth_url
 
-      expect_any_instance_of(TokenManager).to receive(:invalidate_token).with(auth_token)
-      run_delete auth_url, Api::HttpHeaders::AUTH_TOKEN => auth_token
-    end
+          auth_token = response.parsed_body["auth_token"]
 
-    context 'Tokens for Web Sockets' do
-      it 'gets a UI based token_ttl when requesting token for web sockets' do
-        api_basic_authorize
+          expect_any_instance_of(TokenManager).to receive(:invalidate_token).with(auth_token)
+          run_delete auth_url, Api::HttpHeaders::AUTH_TOKEN => auth_token
+        end
 
-        run_get auth_url, :requester_type => 'ws'
-        expect(response).to have_http_status(:ok)
-        expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
-        expect(response.parsed_body["token_ttl"]).to eq(::Settings.session.timeout.to_i_with_method)
-      end
+        context 'Tokens for Web Sockets' do
+          it 'gets a UI based token_ttl when requesting token for web sockets' do
+            api_basic_authorize
 
-      it 'cannot authorize user to api based on token that is dedicated for web sockets' do
-        api_basic_authorize
-        run_get auth_url, :requester_type => 'ws'
-        ws_token = response.parsed_body["auth_token"]
+            run_get auth_url, :requester_type => 'ws'
+            expect(response).to have_http_status(:ok)
+            expect_result_to_have_keys(%w(auth_token token_ttl expires_on))
+            expect(response.parsed_body["token_ttl"]).to eq(::Settings.session.timeout.to_i_with_method)
+          end
 
-        run_get entrypoint_url, :headers => {Api::HttpHeaders::AUTH_TOKEN => ws_token}
+          it 'cannot authorize user to api based on token that is dedicated for web sockets' do
+            api_basic_authorize
+            run_get auth_url, :requester_type => 'ws'
+            ws_token = response.parsed_body["auth_token"]
 
-        expect(response).to have_http_status(:unauthorized)
+            run_get entrypoint_url, :headers => {Api::HttpHeaders::AUTH_TOKEN => ws_token}
+
+            expect(response).to have_http_status(:unauthorized)
+          end
+        end
       end
     end
   end
