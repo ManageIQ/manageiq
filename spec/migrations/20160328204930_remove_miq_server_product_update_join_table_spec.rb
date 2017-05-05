d8779f640eda496fd12c8389adde24de31632671
Jason Frey <jfrey@redhat.com>
2017-05-05 12:39:53 -0400

    Various migration cleanups
    These are various places where we are accessing models incorrectly,
    which makes having the specs run against a dummy app impossible.

--- spec/migrations/20160328204930_remove_miq_server_product_update_join_table_spec.rb
+++ spec/migrations/20160328204930_remove_miq_server_product_update_join_table_spec.rb
@@ -2,18 +2,26 @@ require_migration
 
 describe RemoveMiqServerProductUpdateJoinTable do
   let(:settings_change_stub) { migration_stub(:SettingsChange) }
-  let(:my_server)            { FactoryGirl.create(:miq_server) }
-  let(:other_server)         { FactoryGirl.create(:miq_server) }
+
+  def next_miq_server_id
+    @miq_server_id ||= ArRegion.anonymous_class_with_ar_region.rails_sequence_start
+    @miq_server_id += 1
+  end
 
   migration_context :up do
     it "removes miq_servers_product_updates from replication excludes" do
-      my_server.settings_changes
-               .create!(:key   => described_class::EXCLUDES_KEY,
-                        :value => %w(miq_servers_product_updates schema_migrations))
-
-      other_server.settings_changes
-                  .create!(:key   => described_class::EXCLUDES_KEY,
-                           :value => %w(miq_servers_product_updates ar_internal_metadata))
+      settings_change_stub.create!(
+        :resource_type => "MiqServer",
+        :resource_id   => next_miq_server_id,
+        :key           => described_class::EXCLUDES_KEY,
+        :value         => %w(miq_servers_product_updates schema_migrations)
+      )
+      settings_change_stub.create!(
+        :resource_type => "MiqServer",
+        :resource_id   => next_miq_server_id,
+        :key           => described_class::EXCLUDES_KEY,
+        :value         => %w(miq_servers_product_updates ar_internal_metadata)
+      )
 
       migrate
 
@@ -26,13 +34,18 @@ describe RemoveMiqServerProductUpdateJoinTable do
 
   migration_context :down do
     it "adds miq_servers_product_updates to replication excludes" do
-      my_server.settings_changes
-               .create!(:key   => described_class::EXCLUDES_KEY,
-                        :value => ["schema_migrations"])
-
-      other_server.settings_changes
-                  .create!(:key   => described_class::EXCLUDES_KEY,
-                           :value => ["ar_internal_metadata"])
+      settings_change_stub.create!(
+        :resource_type => "MiqServer",
+        :resource_id   => next_miq_server_id,
+        :key           => described_class::EXCLUDES_KEY,
+        :value         => ["schema_migrations"]
+      )
+      settings_change_stub.create!(
+        :resource_type => "MiqServer",
+        :resource_id   => next_miq_server_id,
+        :key           => described_class::EXCLUDES_KEY,
+        :value         => ["ar_internal_metadata"]
+      )
 
       migrate
 
