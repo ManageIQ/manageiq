36e042b5301eda3974b355efcb8a9b3e446ab5eb
Šimon Lukašík <isimluk@fedoraproject.org>
2017-05-09 13:46:14 +0200

    Create MiqReportResult accessible by current_user.

--- spec/requests/api/collections_spec.rb
+++ spec/requests/api/collections_spec.rb
@@ -438,7 +438,7 @@ describe "Rest API Collections" do
     end
 
     it "bulk query Report Results" do
-      FactoryGirl.create(:miq_report_result)
+      FactoryGirl.create(:miq_report_result, :miq_group => @user.current_group)
       test_collection_bulk_query(:results, results_url, MiqReportResult)
     end
 
