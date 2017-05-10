477700b514328d657dc653b8de5fe747585a1e13
Tina Fitzgerald <tfitzger@redhat.com>
2017-05-10 17:37:19 -0400

    Exclude retired objects in retirement_check methods.
    https://www.pivotaltracker.com/story/show/145237979

--- app/models/load_balancer/retirement_management.rb
+++ app/models/load_balancer/retirement_management.rb
@@ -8,7 +8,6 @@ class LoadBalancer
         ems_ids        = MiqServer.my_server.zone.ext_management_systems.pluck(:id)
         table          = LoadBalancer.arel_table
         load_balancers = LoadBalancer.where(table[:retires_on].not_eq(nil)
-                                              .or(table[:retired].eq(true))
                                               .and(table[:ems_id].in(ems_ids)))
         load_balancers.each(&:retirement_check)
       end
--- app/models/orchestration_stack/retirement_management.rb
+++ app/models/orchestration_stack/retirement_management.rb
@@ -8,7 +8,6 @@ class OrchestrationStack
         ems_ids = MiqServer.my_server.zone.ext_management_systems.pluck(:id)
         table   = OrchestrationStack.arel_table
         stacks  = OrchestrationStack.where(table[:retires_on].not_eq(nil)
-          .or(table[:retired].eq(true))
           .and(table[:ems_id].in(ems_ids)))
         stacks.each(&:retirement_check)
       end
