2a371088982be8a1e30fd07d31475e61b2e0aca8
Jason Frey <jfrey@redhat.com>
2017-05-11 12:59:26 -0400

    Cleanup replication spec messages
    This prevents the "double rendering" of the long error messages, which
    is visually confusing to the developer receiving the error.  The way
    RSpec prints errors, it writes the line of code in error to the screen,
    followed by the error message.  In these cases, the line of code also
    contains the custom error message, so it is rendered twice; first,
    uninterpolated as code, and again interpolated as the error.  Moving the
    text to a method prevents this.

--- spec/replication/util/schema_structure_spec.rb
+++ spec/replication/util/schema_structure_spec.rb
@@ -1,11 +1,15 @@
 describe "database schema" do
-  it "is structured as expected" do
-    ret = EvmDatabase.check_schema
-
-    expect(ret).to be_nil, <<-EOS.gsub!(/^ +/, "")
+  def invalid_schema_message(message)
+    <<-EOS.gsub!(/^ +/, "")
       #{Rails.configuration.database_configuration[Rails.env]["database"]} is not structured as expected.
-      #{ret}
+      #{message}
       Refer to http://talk.manageiq.org/t/new-schema-specs-for-new-replication/1404 for detail
     EOS
+  end
+
+  it "is structured as expected" do
+    message = EvmDatabase.check_schema
+
+    expect(message).to be_nil, invalid_schema_message(message)
   end
 end
