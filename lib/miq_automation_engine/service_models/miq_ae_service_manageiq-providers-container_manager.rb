6231f9dbe072730ba05d7c9fed7277cdf7e61afe
Greg McCullough <gmccullo@redhat.com>
2017-05-15 08:48:39 -0400

    Merge pull request #17 from zeari/missing_service_model
    Add a missing container manager service model
    (cherry picked from commit d4fd27ef34b1ef618a9942401f40a73464669b34)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1460027

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-container_manager.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-container_manager.rb
@@ -1,4 +1,6 @@
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_ContainerManager < MiqAeServiceManageIQ_Providers_BaseManager
+    expose :container_image_registries, :association => true
+    expose :container_projects,         :association => true
   end
 end
