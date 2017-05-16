c8541938ffad3e5047950236d66eb15f0322d252
Aparna Karve <akarve@redhat.com>
2017-05-16 14:35:09 -0700

    Spec for single Cloud Volume delete

--- spec/requests/api/cloud_volumes_spec.rb
+++ spec/requests/api/cloud_volumes_spec.rb
@@ -49,6 +49,26 @@ describe "Cloud Volumes API" do
     expect(response).to have_http_status(:forbidden)
   end
 
+  it "can delete a single cloud volume" do
+    zone = FactoryGirl.create(:zone, :name => "api_zone")
+    aws = FactoryGirl.create(:ems_amazon, :zone => zone)
+
+    cloud_volume1 = FactoryGirl.create(:cloud_volume, :ext_management_system => aws, :name => "CloudVolume1")
+
+    api_basic_authorize action_identifier(:cloud_volumes, :delete, :resource_actions, :post)
+
+    run_post(cloud_volumes_url(cloud_volume1.id), :action => "delete")
+
+    expected = {
+      'message' => 'Deleting Cloud Volume CloudVolume1',
+      'success' => true,
+      'task_id' => a_kind_of(Numeric)
+    }
+
+    expect(response.parsed_body).to include(expected)
+    expect(response).to have_http_status(:ok)
+  end
+
   it 'can delete cloud volumes through POST' do
     zone = FactoryGirl.create(:zone, :name => "api_zone")
     aws = FactoryGirl.create(:ems_amazon, :zone => zone)
