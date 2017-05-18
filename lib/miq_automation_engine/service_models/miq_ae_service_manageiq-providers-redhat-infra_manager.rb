e74f58e7690935110b378f9d9cd6afd7aab91bb3
Greg McCullough <gmccullo@redhat.com>
2017-05-18 10:24:00 -0400

    Merge pull request #4 from matobet/v2v-rhv-expose
    Expose RHV import methods to automate
    (cherry picked from commit 652ebbc6526f073a2e760977ec52a2b1f366c513)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1459996

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
@@ -1,4 +1,6 @@
 module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Redhat_InfraManager < MiqAeServiceEmsInfra
+    expose :validate_import_vm
+    expose :submit_import_vm
   end
 end
