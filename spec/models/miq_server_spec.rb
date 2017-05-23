994417638d916756d31c96e1bc3a31b9ca811a5f
Nick Carboni <ncarboni@redhat.com>
2017-05-23 14:56:50 -0400

    Move signal handling specs from evm_server_spec.rb to miq_server_spec.rb

--- spec/models/miq_server_spec.rb
+++ spec/models/miq_server_spec.rb
@@ -46,6 +46,23 @@ describe MiqServer do
       @guid, @miq_server, @zone = EvmSpecHelper.create_guid_miq_server_zone
     end
 
+    describe "#monitor_loop" do
+      it "calls shutdown_and_exit if SIGTERM is raised" do
+        expect(@miq_server).to receive(:monitor).and_raise(SignalException, "SIGTERM")
+        expect(@miq_server).to receive(:shutdown_and_exit)
+
+        @miq_server.monitor_loop
+      end
+
+      it "kills the server and exits if SIGINT is raised" do
+        expect(@miq_server).to receive(:monitor).and_raise(Interrupt)
+        expect(MiqServer).to receive(:kill)
+        expect(@miq_server).to receive(:exit).with(1)
+
+        @miq_server.monitor_loop
+      end
+    end
+
     it "should have proper guid" do
       expect(@miq_server.guid).to eq(@guid)
     end
