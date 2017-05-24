801b94e19070ed5b4f81a75cec09272e3c8603c4
Šimon Lukašík <isimluk@fedoraproject.org>
2017-05-24 12:46:04 +0200

    Switch the retired logic as proposed by tinaafitz
    See: https://www.pivotaltracker.com/story/show/145237979

--- app/models/retirement_manager.rb
+++ app/models/retirement_manager.rb
@@ -3,7 +3,7 @@ class RetirementManager
     ems_ids = MiqServer.my_server.zone.ext_management_system_ids
     [LoadBalancer, OrchestrationStack, Vm, Service].each do |model|
       table = model.arel_table
-      arel = table[:retires_on].not_eq(nil).or(table[:retired].eq(true))
+      arel = table[:retires_on].not_eq(nil).or(table[:retired].not_eq(true))
       arel = arel.and(table[:ems_id].in(ems_ids)) if model.column_names.include?('ems_id') # Service not assigned to ems_ids
       model.where(arel).each(&:retirement_check)
     end
