d9e1ec6fbcf5474faf677968c7c99dffbbb4540d
Chris Arcand <chris@chrisarcand.com>
2017-05-24 13:07:59 -0500

    Move realtime capture into it's own nicely wrapped context

--- spec/models/metric/ci_mixin/capture_spec.rb
+++ spec/models/metric/ci_mixin/capture_spec.rb
@@ -27,6 +27,107 @@ describe Metric::CiMixin::Capture do
     $log = @orig_log
   end
 
+  describe "#perf_capture_realtime" do
+    def capture_data(second_collection_period_start, collection_overlap_period)
+      # 1.collection period, save all metrics
+      allow(metering).to receive(:get_statistics) do |name, _options|
+        first_collection_period = filter_statistics(mock_stats_data.get_statistics(name,
+                                                                                   "multiple_collection_periods"),
+                                                                                   '<=',
+                                                                                   second_collection_period_start)
+
+        OpenstackApiResult.new(first_collection_period)
+      end
+
+      allow(vm).to receive(:state_changed_on).and_return(second_collection_period_start)
+      vm.perf_capture_realtime(Time.parse('2013-08-28T11:01:40Z').utc, Time.parse(second_collection_period_start).utc)
+
+      # 2.collection period, save all metrics
+      allow(metering).to receive(:get_statistics) do |name, _options|
+        second_collection_period = filter_statistics(mock_stats_data.get_statistics(name,
+                                                                                    "multiple_collection_periods"),
+                                                                                    '>',
+                                                                                    second_collection_period_start,
+                                                                                    collection_overlap_period)
+
+        OpenstackApiResult.new(second_collection_period)
+      end
+
+      allow(vm).to receive(:state_changed_on).and_return(second_collection_period_start)
+      vm.perf_capture_realtime(Time.parse(second_collection_period_start).utc, Time.parse('2013-08-28T14:02:00Z').utc)
+
+      @metrics_by_ts = {}
+      vm.metrics.each do |x|
+        @metrics_by_ts[x.timestamp.iso8601] = x
+      end
+
+      # grab read bytes and write bytes data, these values are pulled directly from
+      # spec/tools/openstack_data/openstack_perf_data/multiple_collection_periods.yml
+      @read_bytes = mock_stats_data.get_statistics("network.incoming.bytes",
+                                                   "multiple_collection_periods")
+
+      @write_bytes = mock_stats_data.get_statistics("network.outgoing.bytes",
+                                                    "multiple_collection_periods")
+    end
+
+    def filter_statistics(stats, op, date, subtract_by = nil)
+      filter_date  = parse_datetime(date)
+      filter_date -= subtract_by if subtract_by
+      stats.select { |x| x['period_end'].send(op, filter_date) }
+    end
+
+    def api_time_as_utc(api_result)
+      period_end = api_result["period_end"]
+      parse_datetime(period_end)
+    end
+
+    def parse_datetime(datetime)
+      datetime << "Z" if datetime.size == 19
+      Time.parse(datetime).utc
+    end
+    context "2 collection periods total, end of 1. period has complete stats" do
+      it "checks that saved metrics are correct" do
+        capture_data('2013-08-28T12:06:00Z', 20.minutes)
+
+        stats_period_start = [api_time_as_utc(@read_bytes.first), api_time_as_utc(@write_bytes.first)].min
+        stats_period_end = [api_time_as_utc(@read_bytes.last), api_time_as_utc(@write_bytes.last)].min
+
+        # check start date and end date
+        expect(stats_period_start).to eq expected_stats_period_start
+        expect(stats_period_end).to eq expected_stats_period_end
+
+        # check that 20s block is not interrupted between start and end time for net_usage_rate_average
+        (expected_stats_period_start + 20.seconds..expected_stats_period_end).step_value(20.seconds).each do |timestamp|
+          expect(@metrics_by_ts[timestamp.iso8601].try(:net_usage_rate_average)).not_to eq nil
+        end
+
+        # check that 20s block is not interrupted between start and end time for disk_usage_rate_average
+        (expected_stats_period_start + 20.seconds..expected_stats_period_end).step_value(20.seconds).each do |timestamp|
+          expect(@metrics_by_ts[timestamp.iso8601].try(:disk_usage_rate_average)).not_to eq nil
+        end
+
+        # check that 20s block is not interrupted between start and end time for cpu_usage_rate_average
+        (expected_stats_period_start + 20.seconds..expected_stats_period_end).step_value(20.seconds).each do |timestamp|
+          expect(@metrics_by_ts[timestamp.iso8601].try(:cpu_usage_rate_average)).not_to eq nil
+        end
+      end
+    end
+
+    context "2 collection periods total, there is data hole between periods" do
+      it "verifies that hole in the data is logged, corrupted data is logged and no other warnings are logged" do
+        # Hole in the data is logged
+        expect($log).to receive(:warn).with(/expected to get data as of/).exactly(:once)
+        # Corrupted data is logged NOTE: This is emitted from the provider
+        expect($log).to receive(:warn).with(/Distance of the multiple streams of data is invalid/).exactly(:once)
+        # No to other warnings should be logged
+        expect($log).not_to receive(:warn)
+
+        # sending no collection period overlap will cause hole in the data
+        capture_data('2013-08-28T11:56:00Z', nil)
+      end
+    end
+  end
+
   context "#perf_capture_queue('realtime')" do
     def verify_realtime_queue_item(queue_item, expected_start_time = nil)
       expect(queue_item.method_name).to eq "perf_capture_realtime"
@@ -195,110 +296,6 @@ describe Metric::CiMixin::Capture do
         expect(@metrics_by_ts[timestamp.iso8601].try(:cpu_usage_rate_average)).not_to eq nil
       end
     end
-  end
-
-  context "2 collection periods total, end of 1. period has complete stats" do
-    ###################################################################################################################
-    # DESCRIPTION FOR: net_usage_rate_average
-    # MAIN SCENARIOS :
-
-    it "checks that saved metrics are correct" do
-      capture_data('2013-08-28T12:06:00Z', 20.minutes)
-
-      stats_period_start = [api_time_as_utc(@read_bytes.first), api_time_as_utc(@write_bytes.first)].min
-      stats_period_end = [api_time_as_utc(@read_bytes.last), api_time_as_utc(@write_bytes.last)].min
-
-      # check start date and end date
-      expect(stats_period_start).to eq expected_stats_period_start
-      expect(stats_period_end).to eq expected_stats_period_end
-
-      # check that 20s block is not interrupted between start and end time for net_usage_rate_average
-      (expected_stats_period_start + 20.seconds..expected_stats_period_end).step_value(20.seconds).each do |timestamp|
-        expect(@metrics_by_ts[timestamp.iso8601].try(:net_usage_rate_average)).not_to eq nil
-      end
-
-      # check that 20s block is not interrupted between start and end time for disk_usage_rate_average
-      (expected_stats_period_start + 20.seconds..expected_stats_period_end).step_value(20.seconds).each do |timestamp|
-        expect(@metrics_by_ts[timestamp.iso8601].try(:disk_usage_rate_average)).not_to eq nil
-      end
-
-      # check that 20s block is not interrupted between start and end time for cpu_usage_rate_average
-      (expected_stats_period_start + 20.seconds..expected_stats_period_end).step_value(20.seconds).each do |timestamp|
-        expect(@metrics_by_ts[timestamp.iso8601].try(:cpu_usage_rate_average)).not_to eq nil
-      end
-    end
-  end
-
-  context "2 collection periods total, there is data hole between periods" do
-    it "verifies that hole in the data is logged, corrupted data is logged and no other warnings are logged" do
-      # Hole in the data is logged
-      expect($log).to receive(:warn).with(/expected to get data as of/).exactly(:once)
-      # Corrupted data is logged
-      expect($log).to receive(:warn).with(/Distance of the multiple streams of data is invalid/).exactly(:once)
-      # No to other warnings should be logged
-      expect($log).not_to receive(:warn)
-
-      # sending no collection period overlap will cause hole in the data
-      capture_data('2013-08-28T11:56:00Z', nil)
-    end
-  end
-
-  def capture_data(second_collection_period_start, collection_overlap_period)
-    # 1.collection period, save all metrics
-    allow(metering).to receive(:get_statistics) do |name, _options|
-      first_collection_period = filter_statistics(mock_stats_data.get_statistics(name,
-                                                                                  "multiple_collection_periods"),
-                                                  '<=',
-                                                  second_collection_period_start)
-
-      OpenstackApiResult.new(first_collection_period)
-    end
-
-    allow(vm).to receive(:state_changed_on).and_return(second_collection_period_start)
-    vm.perf_capture_realtime(Time.parse('2013-08-28T11:01:40Z').utc, Time.parse(second_collection_period_start).utc)
-
-    # 2.collection period, save all metrics
-    allow(metering).to receive(:get_statistics) do |name, _options|
-      second_collection_period = filter_statistics(mock_stats_data.get_statistics(name,
-                                                                                   "multiple_collection_periods"),
-                                                   '>',
-                                                   second_collection_period_start,
-                                                   collection_overlap_period)
-
-      OpenstackApiResult.new(second_collection_period)
-    end
-
-    allow(vm).to receive(:state_changed_on).and_return(second_collection_period_start)
-    vm.perf_capture_realtime(Time.parse(second_collection_period_start).utc, Time.parse('2013-08-28T14:02:00Z').utc)
-
-    @metrics_by_ts = {}
-    vm.metrics.each do |x|
-      @metrics_by_ts[x.timestamp.iso8601] = x
-    end
-
-    # grab read bytes and write bytes data, these values are pulled directly from
-    # spec/tools/openstack_data/openstack_perf_data/multiple_collection_periods.yml
-    @read_bytes = mock_stats_data.get_statistics("network.incoming.bytes",
-                                                  "multiple_collection_periods")
-
-    @write_bytes = mock_stats_data.get_statistics("network.outgoing.bytes",
-                                                   "multiple_collection_periods")
-  end
-
-  def filter_statistics(stats, op, date, subtract_by = nil)
-    filter_date  = parse_datetime(date)
-    filter_date -= subtract_by if subtract_by
-    stats.select { |x| x['period_end'].send(op, filter_date) }
-  end
-
-  def api_time_as_utc(api_result)
-    period_end = api_result["period_end"]
-    parse_datetime(period_end)
-  end
-
-  def parse_datetime(datetime)
-    datetime << "Z" if datetime.size == 19
-    Time.parse(datetime).utc
   end
 
   context "handles archived container entities" do
