d56d040cd123bfba6fada8cbc089645abb7a956a
Brant Evans <bevans@redhat.com>
2017-05-24 19:03:33 -0700

    Use FactoryGirl to create alertsets to test exports

--- spec/lib/task_helpers/exports/alert_sets_spec.rb
+++ spec/lib/task_helpers/exports/alert_sets_spec.rb
@@ -1,8 +1,36 @@
-describe 'TaskHelpers::Exports::AlertSets' do
-  let(:data_dir) { File.join(File.expand_path(__dir__), '..', 'imports', 'data', 'alert_sets') }
-  let(:alert) { 'Alert_Profile_VM_Import_Test.yaml' }
+describe TaskHelpers::Exports::AlertSets do
+  let(:guid) { "eca41687-5ca9-40f2-93f7-3fe1ef08e16e" }
+
+  let(:alert_set_export_attrs) do
+    [
+      {
+        "MiqAlertSet" => {
+          "name"        => "alert_profile_0000000000001",
+          "description" => "Alert Set Export Test",
+          "set_type"    => "MiqAlertSet",
+          "guid"        => "eca41687-5ca9-40f2-93f7-3fe1ef08e16e",
+          "read_only"   => nil,
+          "set_data"    => nil,
+          "mode"        => "VmOrTemplate",
+          "owner_type"  => nil,
+          "owner_id"    => nil,
+          "userid"      => nil,
+          "group_id"    => nil,
+          "MiqAlert"    => []
+        }
+      }
+    ]
+  end
+
+  let(:alert_set_create_attrs) do
+    {
+      :description => "Alert Set Export Test",
+      :guid        => guid,
+    }
+  end
 
   before(:each) do
+    FactoryGirl.create(:miq_alert_set_vm, alert_set_create_attrs)
     @export_dir = Dir.mktmpdir('miq_exp_dir')
   end
 
@@ -11,11 +39,8 @@ describe 'TaskHelpers::Exports::AlertSets' do
   end
 
   it 'should export alert sets to a given directory' do
-    options = { :source => "#{data_dir}/#{alert}" }
-    TaskHelpers::Imports::AlertSets.new.import(options)
-
-    options = { :directory => @export_dir }
-    TaskHelpers::Exports::AlertSets.new.export(options)
-    expect((Dir.entries(@export_dir) - %w(. ..))).not_to be_empty
+    TaskHelpers::Exports::AlertSets.new.export(:directory => @export_dir)
+    file_contents = File.read("#{@export_dir}/Alert_Set_Export_Test.yaml")
+    expect(YAML.safe_load(file_contents)).to eq(alert_set_export_attrs)
   end
 end
