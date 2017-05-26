42d5818768c18ec57a48aa3176f420cb806edb00
Šimon Lukašík <isimluk@fedoraproject.org>
2017-05-26 09:59:14 +0200

    Do not select 13 times from ChargeableField each start

--- app/models/chargeable_field.rb
+++ app/models/chargeable_field.rb
@@ -62,12 +62,13 @@ class ChargeableField < ApplicationRecord
 
   def self.seed
     measures = ChargebackRateDetailMeasure.all.index_by(&:name)
+    existing = ChargeableField.all.index_by(&:metric)
     seed_data.each do |f|
-      rec = ChargeableField.find_by(:metric => f[:metric])
       measure = f.delete(:measure)
       if measure
         f[:chargeback_rate_detail_measure_id] = measures[measure].id
       end
+      rec = existing[f[:metric]]
       if rec.nil?
         create(f)
       else
