0ec4e762ea05fd491376ae4ea8cad5690a81a44f
Greg McCullough <gmccullo@redhat.com>
2017-05-30 18:51:26 -0400

    Merge pull request #15228 from lfu/vm_scan_with_automate_1454936
    Queue the VM scan command after vm_scan_start event is handled by automate.
    (cherry picked from commit 7a7350e514d0a06c4ef555486486bbf75aceb74d)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1460339

--- spec/models/vm_scan_spec.rb
+++ spec/models/vm_scan_spec.rb
@@ -127,7 +127,7 @@ describe VmScan do
 
           it "should call callback when message is delivered" do
             allow_any_instance_of(VmScan).to receive_messages(:signal => true)
-            expect_any_instance_of(VmScan).to receive(:check_policy_complete)
+            expect_any_instance_of(VmScan).to receive(:check_policy_complete).with(@server.my_zone, "ok", any_args)
             q = MiqQueue.where(:class_name => "MiqAeEngine", :method_name => "deliver").first
             q.delivered(*q.deliver)
           end
@@ -252,6 +252,111 @@ describe VmScan do
         expect(@job).to receive(:delete_snapshot)
         @job.call_snapshot_delete
         expect(@job.message).to eq "Snapshot deleted: reference: [#{snapshot_description}]"
+      end
+    end
+  end
+
+  # test cases for BZ #1454936
+  context "A VM Scan job in multiple zones" do
+    before do
+      # local zone
+      @server1 = EvmSpecHelper.local_miq_server(:capabilities => {:vixDisk => true})
+      @user      = FactoryGirl.create(:user_with_group, :userid => "tester")
+      @ems       = FactoryGirl.create(:ems_vmware_with_authentication, :name   => "Test EMS", :zone => @server1.zone,
+                                      :tenant                                  => FactoryGirl.create(:tenant))
+      @storage   = FactoryGirl.create(:storage, :name => "test_storage", :store_type => "VMFS")
+      @host      = FactoryGirl.create(:host, :name => "test_host", :hostname => "test_host",
+                                      :state       => 'on', :ext_management_system => @ems)
+      @vm        = FactoryGirl.create(:vm_vmware, :name => "test_vm", :location => "abc/abc.vmx",
+                                      :raw_power_state       => 'poweredOn',
+                                      :host                  => @host,
+                                      :ext_management_system => @ems,
+                                      :miq_group             => @user.current_group,
+                                      :evm_owner             => @user,
+                                      :storage               => @storage)
+
+      # remote zone
+      @server2 = EvmSpecHelper.remote_miq_server(:capabilities => {:vixDisk => true})
+      @user2     = FactoryGirl.create(:user_with_group, :userid => "tester2")
+      @storage2  = FactoryGirl.create(:storage, :name => "test_storage2", :store_type => "VMFS")
+      @host2     = FactoryGirl.create(:host, :name => "test_host2", :hostname => "test_host2",
+                                      :state       => 'on', :ext_management_system => @ems)
+      @vm2       = FactoryGirl.create(:vm_vmware, :name => "test_vm2", :location => "abc2/abc2.vmx",
+                                      :raw_power_state       => 'poweredOn',
+                                      :host                  => @host2,
+                                      :ext_management_system => @ems,
+                                      :miq_group             => @user2.current_group,
+                                      :evm_owner             => @user2,
+                                      :storage               => @storage2)
+
+      allow(MiqEventDefinition).to receive_messages(:find_by => true)
+      allow(@server1).to receive(:has_active_role?).with('automate').and_return(true) # set automate role in local zone
+    end
+
+    describe "#check_policy_complete" do
+      context "in local zone" do
+        before do
+          @vm.scan
+          job_item = MiqQueue.find_by(:class_name => "MiqAeEngine", :method_name => "deliver")
+          job_item.delivered(*job_item.deliver)
+
+          @job = Job.first
+        end
+
+        it "signals :abort if passed status is not 'ok' to local zone" do
+          message = "Hello, World!"
+          expect(@job).to receive(:signal).with(:abort, message, "error")
+          @job.check_policy_complete(@server1.my_zone, 'some status', message, nil)
+        end
+
+        it "does not send signal :abort if passed status is 'ok' " do
+          expect(@job).not_to receive(:signal).with(:abort, nil, "error")
+          @job.check_policy_complete(@server1.my_zone, 'ok', nil, nil)
+        end
+
+        it "sends signal :start_snapshot if status is 'ok' to local zone" do
+          expect(MiqQueue).to receive(:put).with(
+            :class_name  => @job.class.to_s,
+            :instance_id => @job.id,
+            :method_name => "signal",
+            :args        => [:start_snapshot],
+            :zone        => @server1.my_zone,
+            :role        => "smartstate"
+          )
+          @job.check_policy_complete(@server1.my_zone, 'ok', nil, nil)
+        end
+      end
+
+      context "in remote zone" do
+        before do
+          @vm2.scan
+          job_item = MiqQueue.find_by(:class_name => "MiqAeEngine", :method_name => "deliver")
+          job_item.delivered(*job_item.deliver)
+
+          @job = Job.first
+        end
+        it "signals :abort if status is not 'ok' to remote zone" do
+          message = "Hello, World!"
+          expect(@job).to receive(:signal).with(:abort, message, "error")
+          @job.check_policy_complete(@server2.my_zone, 'some status', message, nil)
+        end
+
+        it "does not send signal :abort if passed status is 'ok' " do
+          expect(@job).not_to receive(:signal).with(:abort, nil, "error")
+          @job.check_policy_complete(@server2.my_zone, 'ok', nil, nil)
+        end
+
+        it "signals :start_snapshot if status is 'ok' to remote zone" do
+          expect(MiqQueue).to receive(:put).with(
+            :class_name  => @job.class.to_s,
+            :instance_id => @job.id,
+            :method_name => "signal",
+            :args        => [:start_snapshot],
+            :zone        => @server2.my_zone,
+            :role        => "smartstate"
+          )
+          @job.check_policy_complete(@server2.my_zone, 'ok', nil, nil)
+        end
       end
     end
   end
