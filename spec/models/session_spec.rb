2cba73e8c78d0548864425c9f428cb80ad908367
Tim Wade <hello@timjwade.com>
2017-05-31 14:53:43 -0700

    Don't create 2 sessions where 1 is needed

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -26,8 +26,8 @@ describe Session do
 
   describe ".purge" do
     it "purges an old session" do
-      FactoryGirl.create_list(:session, 2, :updated_at => 1.year.ago, :raw_data => {:userid => "admin"})
-      expect(described_class.count).to eq(2)
+      FactoryGirl.create(:session, :updated_at => 1.year.ago, :raw_data => {:userid => "admin"})
+      expect(described_class.count).to eq(1)
 
       described_class.purge(0, 1)
 
@@ -35,8 +35,8 @@ describe Session do
     end
 
     it "logs out users before destroying stale sessions" do
-      FactoryGirl.create_list(:session, 2, :updated_at => 1.year.ago, :raw_data => {:userid => "admin"})
-      expect(described_class.count).to eq(2)
+      FactoryGirl.create(:session, :updated_at => 1.year.ago, :raw_data => {:userid => "admin"})
+      expect(described_class.count).to eq(1)
       expect(User).to receive(:where).and_return([User.new]).exactly(1).times
 
       described_class.purge(0)
