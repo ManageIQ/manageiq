afb10c142b4aafd8136ef8e153e5704d2a15c888
Šimon Lukašík <isimluk@fedoraproject.org>
2017-05-31 14:18:43 +0200

    Do not calculate useless group metrics
    https://bugzilla.redhat.com/show_bug.cgi?id=1455690
    https://bugzilla.redhat.com/show_bug.cgi?id=1454456
    
     - cpu is useless as it counts usagemhz and vm_numvcpus together
     - cpu_cores is useless as it only reports cpu_cores
     - memory is useless as counts used and available together
     - net_io is useless as it only reports net_ui_rate_avg
     - disk_io is useless as it only reports disk_usage_rate_avg
     - storage is useless as it counts used and allocated together
     - fixed is useless as it counts all the fixed metrics together
       (that equals to 4 for each day, not useful)

--- app/models/chargeable_field.rb
+++ app/models/chargeable_field.rb
@@ -36,8 +36,7 @@ class ChargeableField < ApplicationRecord
   end
 
   def metric_keys
-    ["#{rate_name}_metric", # metric value (e.g. Storage [Used|Allocated|Fixed])
-     "#{group}_metric"]     # total of metric's group (e.g. Storage Total)
+    ["#{rate_name}_metric"] # metric value (e.g. Storage [Used|Allocated|Fixed])
   end
 
   def cost_keys
