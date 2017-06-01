1fe04b3d36c79375ce2d7179a29c696ad3f0da82
Ladislav Smola <lsmola@redhat.com>
2017-06-01 10:34:39 +0200

    Add partial index to :deleted col to speedup queries
    Add partial index to :deleted col to speedup queries. It has
    to be a partial index, otherwise the cost is too high and
    PG will not pick it.

--- db/migrate/20170530102506_add_deleted_to_containers_tables.rb
+++ db/migrate/20170530102506_add_deleted_to_containers_tables.rb
@@ -5,5 +5,21 @@ class AddDeletedToContainersTables < ActiveRecord::Migration[5.0]
     add_column :container_images, :deleted, :boolean, :default => false, :null => false
     add_column :container_projects, :deleted, :boolean, :default => false, :null => false
     add_column :containers, :deleted, :boolean, :default => false, :null => false
+
+    add_index :container_definitions, :deleted,
+              :name  => "index_container_definitions_on_deleted_false",
+              :where => "NOT deleted"
+    add_index :container_groups, :deleted,
+              :name  => "container_groups_on_deleted_false",
+              :where => "NOT deleted"
+    add_index :container_images, :deleted,
+              :name  => "index_container_images_on_deleted_false",
+              :where => "NOT deleted"
+    add_index :container_projects, :deleted,
+              :name  => "index_container_projects_on_deleted_false",
+              :where => "NOT deleted"
+    add_index :containers, :deleted,
+              :name  => "index_containers_on_deleted_false",
+              :where => "NOT deleted"
   end
 end
