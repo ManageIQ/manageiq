4362d7e4d972a9f6b81378a03be3ee48b894b61a
Tim Wade <hello@timjwade.com>
2017-06-01 11:16:09 -0700

    wip

--- lib/services/share_sweeper.rb
+++ lib/services/share_sweeper.rb
@@ -1,15 +1,25 @@
 class ShareSweeper
   def self.sweep(model)
+    new(model).sweep
+  end
+
+  attr_reader :model
+
+  def initialize(model)
+    @model = model
+  end
+
+  def sweep
     public_send("sweep_after_change_to_#{model.class.name}", model)
   end
 
-  def self.sweep_after_change_to_MiqUserRole(role)
+  def sweep_after_change_to_MiqUserRole(role)
     Share.joins(:user => :miq_groups).where(:miq_groups => {:id => role.miq_groups}).each do |share|
       share.destroy unless ResourceSharer.valid_share?(share)
     end
   end
 
-  def self.sweep_after_change_to_Entitlement(entitlement)
+  def sweep_after_change_to_Entitlement(entitlement)
     shares = Share
       .joins(:user => {:miq_groups => :entitlement})
       .where(:user => {:miq_groups => {"entitlements" => {:id => entitlement}}})
