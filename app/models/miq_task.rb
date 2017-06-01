403b51d97b0c730db157c065c704e0994c9704fa
Gregg Tanzillo <gtanzill@redhat.com>
2017-06-01 17:57:11 -0400

    Merge pull request #15134 from yrudman/fix-deleting-report-when-task-deleted
    Do not delete report if task associated with this report deleted
    (cherry picked from commit c17d0430bd143dbf3e40c131c2dc831bd4958bae)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1460394

--- app/models/miq_task.rb
+++ app/models/miq_task.rb
@@ -19,7 +19,7 @@ class MiqTask < ApplicationRecord
 
   has_one :log_file, :dependent => :destroy
   has_one :binary_blob, :as => :resource, :dependent => :destroy
-  has_one :miq_report_result, :dependent => :destroy
+  has_one :miq_report_result
   has_one :job, :dependent => :destroy
 
   before_validation :initialize_attributes, :on => :create
