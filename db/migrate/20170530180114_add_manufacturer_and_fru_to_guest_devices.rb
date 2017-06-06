995efbb45f3a398a09b35fc124d181e78e404e02
skovic <skovic@lenovo.com>
2017-06-06 09:25:52 -0400

    Remove trailing whitespace in migration

--- db/migrate/20170530180114_add_manufacturer_and_fru_to_guest_devices.rb
+++ db/migrate/20170530180114_add_manufacturer_and_fru_to_guest_devices.rb
@@ -3,6 +3,6 @@ class AddManufacturerAndFruToGuestDevices < ActiveRecord::Migration[5.0]
     add_column :guest_devices, :manufacturer, :string
     add_column :guest_devices, :fru, :string
     add_column :guest_devices, :guest_device_id, :bigint
-    add_index :guest_devices, :guest_device_id, :name => "index_guest_devices_on_guest_device_id"  
+    add_index :guest_devices, :guest_device_id, :name => "index_guest_devices_on_guest_device_id"
   end
 end
