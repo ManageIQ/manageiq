50b4153b61663af72ef1d14412fed371ca2a9545
Ladislav Smola <lsmola@redhat.com>
2017-06-06 09:45:21 +0200

    Move dependent destroy to _all relations
    Move dependent destroy to _all relations, since we want
    to cascade deleted both active and archived records.

--- app/models/manageiq/providers/container_manager.rb
+++ app/models/manageiq/providers/container_manager.rb
@@ -4,16 +4,16 @@ module ManageIQ::Providers
     include SupportsFeatureMixin
 
     has_many :container_nodes, :foreign_key => :ems_id, :dependent => :destroy
-    has_many :container_groups, -> { active }, :foreign_key => :ems_id, :dependent => :destroy
+    has_many :container_groups, -> { active }, :foreign_key => :ems_id
     has_many :container_services, :foreign_key => :ems_id, :dependent => :destroy
     has_many :container_replicators, :foreign_key => :ems_id, :dependent => :destroy
     has_many :containers, -> { active }, :foreign_key => :ems_id
     has_many :container_definitions, -> { active }, :foreign_key => :ems_id
-    has_many :container_projects, -> { active }, :foreign_key => :ems_id, :dependent => :destroy
+    has_many :container_projects, -> { active }, :foreign_key => :ems_id
     has_many :container_quotas, :foreign_key => :ems_id, :dependent => :destroy
     has_many :container_limits, :foreign_key => :ems_id, :dependent => :destroy
     has_many :container_image_registries, :foreign_key => :ems_id, :dependent => :destroy
-    has_many :container_images, -> { active }, :foreign_key => :ems_id, :dependent => :destroy
+    has_many :container_images, -> { active }, :foreign_key => :ems_id
     has_many :persistent_volumes, :as => :parent, :dependent => :destroy
     has_many :persistent_volume_claims, :foreign_key => :ems_id, :dependent => :destroy
     has_many :container_component_statuses, :foreign_key => :ems_id, :dependent => :destroy
@@ -37,10 +37,13 @@ module ManageIQ::Providers
     has_many :container_limit_items, :through => :container_limits
     has_many :container_template_parameters, :through => :container_templates
 
-    # Archived entities to destroy when the container manager is deleted
-    has_many :old_container_groups, :foreign_key => :old_ems_id, :dependent => :destroy, :class_name => "ContainerGroup"
-    has_many :old_container_projects, :foreign_key => :old_ems_id, :dependent => :destroy, :class_name => "ContainerProject"
-    has_many :old_container_images, :foreign_key => :old_ems_id, :dependent => :destroy, :class_name => "ContainerImage"
+    # Archived and active entities to destroy when the container manager is deleted
+    has_many :all_containers, :foreign_key => :ems_id, :dependent => :destroy, :class_name => "Container"
+    has_many :all_container_groups, :foreign_key => :ems_id, :dependent => :destroy, :class_name => "ContainerGroup"
+    has_many :all_container_projects, :foreign_key => :ems_id, :dependent => :destroy, :class_name => "ContainerProject"
+    has_many :all_container_images, :foreign_key => :ems_id, :dependent => :destroy, :class_name => "ContainerImage"
+    has_many :all_container_definitions, :foreign_key => :ems_id, :dependent => :destroy, :class_name => "ContainerDefinition"
+
 
     virtual_column :port_show, :type => :string
 
