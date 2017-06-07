e3dd7979396df33e818fcedb4cc867fd81282780
Lucy Fu <lufu@redhat.com>
2017-06-07 11:51:53 -0400

    Add project option to container template service dialog.
    https://www.pivotaltracker.com/story/show/145899183

--- app/models/dialog/container_template_service_dialog.rb
+++ app/models/dialog/container_template_service_dialog.rb
@@ -8,12 +8,53 @@ class Dialog
     def create_dialog(label, parameters)
       Dialog.new(:label => label, :buttons => "submit,cancel").tap do |dialog|
         tab = dialog.dialog_tabs.build(:display => "edit", :label => "Basic Information", :position => 0)
-        add_parameters_group(tab, 0, parameters)
+        add_options_group(tab, 0)
+        add_parameters_group(tab, 1, parameters)
         dialog.save!
       end
     end
 
     private
+
+    def add_options_group(tab, position)
+      tab.dialog_groups.build(
+        :display  => "edit",
+        :label    => "Options",
+        :position => position
+      ).tap do |dialog_group|
+        add_project_dropdown(dialog_group, 0)
+        add_project_textbox(dialog_group, 1)
+      end
+    end
+
+    def add_project_dropdown(group, position)
+      group.dialog_fields.build(
+        :type         => "DialogFieldDropDownList",
+        :name         => "existing_project_name",
+        :data_type    => "string",
+        :dynamic      => true,
+        :display      => "edit",
+        :label        => "Add To Existing Project",
+        :description  => "The desired existing project for the provisioning",
+        :position     => position,
+        :dialog_group => group
+      ).tap do |dialog_field|
+        dialog_field.resource_action.fqname = "Container/Openshift/Operations/Methods/Available_Projects"
+      end
+    end
+
+    def add_project_textbox(group, position)
+      group.dialog_fields.build(
+        :type         => "DialogFieldTextBox",
+        :name         => "new_project_name",
+        :data_type    => "string",
+        :display      => "edit",
+        :label        => "(or) Add To New Project",
+        :description  => "The desired new project for the provisioning",
+        :position     => position,
+        :dialog_group => group
+      )
+    end
 
     def add_parameters_group(tab, position, parameters)
       tab.dialog_groups.build(
