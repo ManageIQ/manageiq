9a470a5e11c0d9256a64459f5d1e176e5c31ca2c
Nick LaMuro <nicklamuro@gmail.com>
2017-06-08 19:52:22 -0500

    Skip loading Rails in rake for evm:status tasks
    Adds helpers to the evm_rake_helper to dynamically load portions of the
    application depending of what rake tasks are being required.
    Drastically speeds up the time it takes for task to be executed and the
    time taken to do so.
    
    Before
    ------
    
        $ bin/rake evm:status_full
        Checking EVM status...
         Zone    | Server | Status  | ID  ...
        ---------+--------+---------+---- ...
         default | EVM    | stopped |  1  ...
    
        TOTAL_MEMORY_USED: 164.75390625MiB
    
        Timings
        -------
        real    0m5.196s
        user    0m4.000s
        sys     0m1.160s
        cuser   0m0.000s
        csys    0m0.000s
    
    After
    ------
    
        $ bin/rake evm:status_full
        Checking EVM status...
         Zone    | Server | Status  | ID  ...
        ---------+--------+---------+---- ...
         default | EVM    | stopped |  1  ...
    
        TOTAL_MEMORY_USED: 58.84375MiB
    
        Timings
        -------
        real    0m1.131s
        user    0m0.740s
        sys     0m0.380s
        cuser   0m0.000s
        csys    0m0.000s

--- lib/tasks/evm_rake_helper.rb
+++ lib/tasks/evm_rake_helper.rb
@@ -1,4 +1,10 @@
 module EvmRakeHelper
+
+  EVM_APPLIANCE_TASKS = %w[
+    evm:status
+    evm:status_full
+  ]
+
   # Loading environment will try to read database.yml unless DATABASE_URL is set.
   # For some rake tasks, the database.yml may not yet be setup and is not required anyway.
   # Note: Rails will not actually use the configuration and connect until you issue a query.
@@ -36,5 +42,48 @@ module EvmRakeHelper
   def self.extract_command_options
     i = ARGV.index("--")
     i ? ARGV.slice((i + 1)..-1) : ARGV.dup
+  end
+
+  # Loads the necessary rake task files given the ARGUMENTS passed to rake
+  #
+  # Tasks like like evm:status should be snappy, and don't really require a lot
+  # of depdendencies to function.  But by loading config/application.rb and
+  # using the Vmdb::Application.load_tasks interface, we boot up and configure
+  # the entire application to load every task that we need, when that is
+  # usually overkill.
+  #
+  # By doing a small of ARGV analysis, we can quickly determine what files are
+  # actually needed for the tasks being requested, and skip loading a large
+  # portion of the code base.
+  def self.load_rake_environment
+    if only_calling_evm_tasks
+      load_evm_rake_tasks
+    else
+      load_all_rake_tasks
+    end
+  end
+
+  # Loads everything normally via the Rails conventions
+  def self.load_all_rake_tasks
+    require File.expand_path("../../../config/application", __FILE__)
+
+    Vmdb::Application.load_tasks
+
+    # Clear noisy and unusable tasks added by rspec-rails
+    if defined?(RSpec)
+      Rake::Task.tasks.select { |t| t.name =~ /^spec(:)?/ }.each(&:clear)
+    end
+  end
+
+  # Loads only the evm.rake, and sets up the path for that
+  def self.load_evm_rake_tasks
+    $:.push(File.expand_path("../../", __FILE__))
+
+    load File.expand_path("../evm.rake", __FILE__)
+  end
+
+  # Determines if we are only using evm application tasks
+  def self.only_calling_evm_tasks
+    ARGV.all? {|arg| EVM_APPLIANCE_TASKS.include?(arg)}
   end
 end
