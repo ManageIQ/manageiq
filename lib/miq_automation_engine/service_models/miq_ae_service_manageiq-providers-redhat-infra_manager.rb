7de2fed1fcbeadcf4c31852f89ff6e9ce88882a7
Greg McCullough <gmccullo@redhat.com>
2017-06-13 14:56:31 -0400

    Merge pull request #35 from matobet/v2v-network-expose
    v2v: Expose network configuration method
    (cherry picked from commit e789d0c299b67192074602c75dec758a1f457f67)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1459996

--- lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_manageiq-providers-redhat-infra_manager.rb
@@ -2,5 +2,6 @@ module MiqAeMethodService
   class MiqAeServiceManageIQ_Providers_Redhat_InfraManager < MiqAeServiceEmsInfra
     expose :validate_import_vm
     expose :submit_import_vm
+    expose :submit_configure_imported_vm_networks
   end
 end
