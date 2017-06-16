de396537b4993b9d99efc9c39ed21dc547af8a50
skovic <skovic@lenovo.com>
2017-06-16 13:29:13 -0400

    Rename guest_device_id to parent_device_id

--- db/migrate/20170530180114_add_manufacturer_and_fru_to_guest_devices.rb
+++ db/migrate/20170530180114_add_manufacturer_and_fru_to_guest_devices.rb
@@ -2,7 +2,7 @@ class AddManufacturerAndFruToGuestDevices < ActiveRecord::Migration[5.0]
   def change
     add_column :guest_devices, :manufacturer, :string
     add_column :guest_devices, :field_replaceable_unit, :string
-    add_column :guest_devices, :guest_device_id, :bigint
-    add_index :guest_devices, :guest_device_id, :name => "index_guest_devices_on_guest_device_id"
+    add_column :guest_devices, :parent_device_id, :bigint
+    add_index :guest_devices, :parent_device_id, :name => "index_guest_devices_on_parent_device_id"
   end
 end
