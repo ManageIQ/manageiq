64e655f15e6299c7939baebd48533aa6f65e2e73
Ladislav Smola <lsmola@redhat.com>
2017-06-22 11:39:22 +0200

    Reword the migration comments
    Reword the migration comments

--- db/migrate/20170530102536_use_deleted_in_containers_tables.rb
+++ db/migrate/20170530102536_use_deleted_in_containers_tables.rb
@@ -22,45 +22,45 @@ class UseDeletedInContainersTables < ActiveRecord::Migration[5.0]
   end
 
   def up
-    say_with_time("Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for ContainerDefinition") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to :old_ems_id for ContainerDefinition") do
       disconnect_to_soft_delete(ContainerDefinition)
     end
 
-    say_with_time("Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for ContainerGroup") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to :old_ems_id for ContainerGroup") do
       disconnect_to_soft_delete(ContainerGroup)
     end
 
-    say_with_time("Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for ContainerImages") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to :old_ems_id for ContainerImages") do
       disconnect_to_soft_delete(ContainerImage)
     end
 
-    say_with_time("Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for ContainerProject") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to :old_ems_id for ContainerProject") do
       disconnect_to_soft_delete(ContainerProject)
     end
 
-    say_with_time("Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for Container") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to :old_ems_id for Container") do
       disconnect_to_soft_delete(Container)
     end
   end
 
   def down
-    say_with_time("Change :deleted => true to :ems_id => nil for ContainerDefinition") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to nil for ContainerDefinition") do
       soft_delete_to_disconnect(ContainerDefinition)
     end
 
-    say_with_time("Change :deleted => true to :ems_id => nil for ContainerGroup") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to nil for ContainerGroup") do
       soft_delete_to_disconnect(ContainerGroup)
     end
 
-    say_with_time("Change :deleted => true to :ems_id => nil for ContainerImages") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to nil for ContainerImages") do
       soft_delete_to_disconnect(ContainerImage)
     end
 
-    say_with_time("Change :deleted => true to :ems_id => nil for ContainerProject") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to nil for ContainerProject") do
       soft_delete_to_disconnect(ContainerProject)
     end
 
-    say_with_time("Change :deleted => true to :ems_id => nil for Container") do
+    say_with_time("Change ':deleted_on not nil' :ems_id to nil for Container") do
       soft_delete_to_disconnect(Container)
     end
   end
