143d67d4ba0186a3adaaa38b504907168419791c
Ladislav Smola <lsmola@redhat.com>
2017-06-22 12:03:02 +0200

    Update specs to work with deleted_on
    Update specs to work with deleted_on

--- spec/migrations/20170530102536_use_deleted_in_containers_tables_spec.rb
+++ spec/migrations/20170530102536_use_deleted_in_containers_tables_spec.rb
@@ -10,29 +10,29 @@ describe UseDeletedInContainersTables do
   def create_before_migration_stub_data_for(model)
     model.create!(:ems_id => 10, :old_ems_id => nil)
     model.create!(:ems_id => 10, :old_ems_id => 10)
-    model.create!(:ems_id => nil, :old_ems_id => 10)
-    model.create!(:ems_id => nil, :old_ems_id => 20)
-    model.create!(:ems_id => nil, :old_ems_id => nil)
+    model.create!(:ems_id => nil, :old_ems_id => 10, :deleted_on => Time.now.utc)
+    model.create!(:ems_id => nil, :old_ems_id => 20, :deleted_on => Time.now.utc)
+    model.create!(:ems_id => nil, :old_ems_id => nil, :deleted_on => Time.now.utc)
   end
 
   def create_after_migration_stub_data_for(model)
     model.create!(:ems_id => 10, :old_ems_id => nil)
     model.create!(:ems_id => 10, :old_ems_id => 10)
-    model.create!(:ems_id => 10, :old_ems_id => 10, :deleted => true)
-    model.create!(:ems_id => 20, :old_ems_id => 20, :deleted => true)
-    model.create!(:ems_id => nil, :old_ems_id => nil, :deleted => true)
+    model.create!(:ems_id => 10, :old_ems_id => 10, :deleted_on => Time.now.utc)
+    model.create!(:ems_id => 20, :old_ems_id => 20, :deleted_on => Time.now.utc)
+    model.create!(:ems_id => nil, :old_ems_id => nil, :deleted_on => Time.now.utc)
   end
 
   def assert_before_migration_data_of(model)
-    expect(model.where(:deleted => true).count).to eq 0
-    expect(model.where(:deleted => false).count).to eq 5
+    expect(model.where.not(:deleted_on => nil).count).to eq 3
+    expect(model.where(:deleted_on => nil).count).to eq 2
     expect(model.where(:ems_id => nil).count).to eq 3
     expect(model.where.not(:ems_id => nil).count).to eq 2
   end
 
   def assert_after_migration_data_of(model)
-    expect(model.where(:deleted => true).count).to eq 3
-    expect(model.where(:deleted => false).count).to eq 2
+    expect(model.where.not(:deleted_on => nil).count).to eq 3
+    expect(model.where(:deleted_on => nil).count).to eq 2
     expect(model.where(:ems_id => nil).count).to eq 1
     expect(model.where.not(:ems_id => nil).count).to eq 4
   end
@@ -54,45 +54,45 @@ describe UseDeletedInContainersTables do
   end
 
   migration_context :up do
-    it "Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for ContainerDefinition" do
+    it "Change ':deleted_on not nil' :ems_id to :old_ems_id for ContainerDefinition" do
       assert_up_migration_for(container_definitions_stub)
     end
 
-    it "Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for ContainerGroup" do
+    it "Change ':deleted_on not nil' :ems_id to :old_ems_id for ContainerGroup" do
       assert_up_migration_for(container_groups_stub)
     end
 
-    it "Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for ContainerImages" do
+    it "Change ':deleted_on not nil' :ems_id to :old_ems_id for ContainerImages" do
       assert_up_migration_for(container_images_stub)
     end
 
-    it "Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for ContainerProjects" do
+    it "Change ':deleted_on not nil' :ems_id to :old_ems_id for ContainerProjects" do
       assert_up_migration_for(container_projects_stub)
     end
 
-    it "Change :ems_id => nil to :deleted => true and set :ems_id using :old_ems_id for Containers" do
+    it "Change ':deleted_on not nil' :ems_id to :old_ems_id for Containers" do
       assert_up_migration_for(containers_stub)
     end
   end
 
   migration_context :down do
-    it "Change :deleted => true to :ems_id => nil for ContainerDefinition" do
+    it "Change ':deleted_on not nil' :ems_id to nil for ContainerDefinition" do
       assert_down_migration_for(container_definitions_stub)
     end
 
-    it "Change :deleted => true to :ems_id => nil for ContainerGroup" do
+    it "Change ':deleted_on not nil' :ems_id to nil for ContainerGroup" do
       assert_down_migration_for(container_groups_stub)
     end
 
-    it "Change :deleted => true to :ems_id => nil for ContainerImages" do
+    it "Change ':deleted_on not nil' :ems_id to nil for ContainerImages" do
       assert_down_migration_for(container_images_stub)
     end
 
-    it "Change :deleted => true to :ems_id => nil for ContainerProjects" do
+    it "Change ':deleted_on not nil' :ems_id to nil for ContainerProjects" do
       assert_down_migration_for(container_projects_stub)
     end
 
-    it "Change :deleted => true to :ems_id => nil for Containers" do
+    it "Change ':deleted_on not nil' :ems_id to nil for Containers" do
       assert_down_migration_for(containers_stub)
     end
   end
