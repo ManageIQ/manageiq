6eeec01542e27442e0d6515c39417abc0470b42b
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2017-06-23 16:51:30 +0200

    graph full refresh
    This PR contains default and custom save blocks to enable full refresh.

--- app/models/host.rb
+++ app/models/host.rb
@@ -49,6 +49,7 @@ class Host < ApplicationRecord
   has_many                  :host_switches, :dependent => :destroy
   has_many                  :switches, :through => :host_switches
   has_many                  :lans,     :through => :switches
+  has_many                  :networks, :through => :hardware
   has_many                  :patches, :dependent => :destroy
   has_many                  :system_services, :dependent => :destroy
   has_many                  :host_services, :class_name => "SystemService", :foreign_key => "host_id", :inverse_of => :host
--- app/models/operating_system.rb
+++ app/models/operating_system.rb
@@ -1,6 +1,7 @@
 require 'miq-encode'
 
 class OperatingSystem < ApplicationRecord
+  belongs_to :host
   belongs_to :vm_or_template
   belongs_to :vm,           :foreign_key => :vm_or_template_id
   belongs_to :miq_template, :foreign_key => :vm_or_template_id
