ac81b8f127e6748e4833f3046a50b444718c24b9
Tim Wade <hello@timjwade.com>
2017-07-05 15:11:44 -0700

    Extract ApiLogger

--- app/controllers/api/base_controller/logger.rb
+++ app/controllers/api/base_controller/logger.rb
@@ -1,40 +1,16 @@
 module Api
   class BaseController
     module Logger
-      def api_get_method_name(call_stack, method)
-        match = /`(?<mname>[^']*)'/.match(call_stack)
-        (match ? match[:mname] : method).sub(/block .*in /, "")
+      def api_log_debug(msg = nil, &block)
+        $api_log.debug(msg, &block)
       end
 
-      def api_log_debug(msg = nil)
-        return unless $api_log.debug?
-        prefix = log_prefix(caller.first, __method__)
-        (block_given? ? yield : msg).split("\n").each do |l|
-          $api_log.debug("#{prefix} #{l}")
-        end
+      def api_log_error(msg = nil, &block)
+        $api_log.error(msg, &block)
       end
 
-      def api_log_error(msg = nil)
-        return unless $api_log.error?
-        prefix = log_prefix(caller.first, __method__)
-        $api_log.error("#{prefix} #{ApiConfig.base.name} Error")
-        (block_given? ? yield : msg).split("\n").each do |l|
-          $api_log.error("#{prefix} #{l}")
-        end
-      end
-
-      def api_log_info(msg = nil)
-        return unless $api_log.info
-        prefix = log_prefix(caller.first, __method__)
-        (block_given? ? yield : msg).split("\n").each do |l|
-          $api_log.info("#{prefix} #{l}")
-        end
-      end
-
-      private
-
-      def log_prefix(backtrace, meth)
-        "MIQ(#{self.class.name}.#{api_get_method_name(backtrace, meth)})"
+      def api_log_info(msg = nil, &block)
+        $api_log.info(msg, &block)
       end
     end
   end
--- spec/requests/api/logging_spec.rb
+++ spec/requests/api/logging_spec.rb
@@ -11,12 +11,12 @@ describe "Logging" do
 
     after { $api_log.loggers.delete(@logger) }
 
-    it "formats the log line as '[LOG LEVEL] -- : MIQ([Class].[method])'" do
+    it "formats the log line as '[LOG LEVEL] -- : MIQ([method])'" do
       api_basic_authorize
 
       run_get(entrypoint_url)
 
-      expect(@log.string).to include("INFO -- : MIQ(Api::ApiController.log_request_initiated)")
+      expect(@log.string).to include("INFO -- : MIQ(log_request_initiated)")
     end
 
     it "logs hashed details about the request" do
