ManageIQ/manageiq-ui-classic

Add RBAC check for resize_vms

app/controllers/application_controller/ci_processing.rb
@@ -400,7 +400,7 @@ module ApplicationController::CiProcessing
     load_edit("vm_resize__#{params[:id]}")
     flavor_id = @edit[:new][:flavor]
     flavor = find_by_id_filtered(Flavor, flavor_id)
-    @record = VmOrTemplate.find_by_id(params[:id])
+    @record = find_by_id_filtered(VmOrTemplate, params[:id])
 
     case params[:button]
     when "cancel"

ManageIQ/manageiq

app/controllers/application_controller/ci_processing.rb
@@ -414,7 +414,7 @@ module ApplicationController::CiProcessing
     load_edit("vm_resize__#{params[:id]}")
     flavor_id = @edit[:new][:flavor]
     flavor = find_record_with_rbac(Flavor, flavor_id)
-    @record = VmOrTemplate.find_by_id(params[:id])
+    @record = find_record_with_rbac(VmOrTemplate, params[:id])
 
     case params[:button]
     when "cancel"