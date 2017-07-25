7fffe1bd7db0cd09dc4cc4dfc99c71f96f7ef3e7
Cainã Costa <cainan.costa@gmail.com>
2017-07-24 21:39:00 -0300

    Add read-only API for middleware domains

---
+++ spec/requests/api/middleware_domains_spec.rb
@@ -0,0 +1,66 @@
+describe 'Middleware Domains API' do
+  let(:domain) { FactoryGirl.create :middleware_domain }
+
+  # For some reason middleware_domains_url is not returning the full
+  # url, just the path portion. This is a hack, but will do the trick.
+  def domain_url
+    "http://www.example.com#{middleware_domains_url(domain.id)}"
+  end
+
+  describe '/' do
+    it 'forbids access without an appropriate role' do
+      api_basic_authorize
+
+      run_get middleware_domains_url
+
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'returns an empty listing of domains' do
+      api_basic_authorize collection_action_identifier(:middleware_domains, :read, :get)
+
+      run_get middleware_domains_url
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to eq(
+        'name'      => 'middleware_domains',
+        'count'     => 0,
+        'resources' => [],
+        'subcount'  => 0
+      )
+    end
+
+    it 'returns a listing of domains' do
+      domain
+
+      api_basic_authorize collection_action_identifier(:middleware_domains, :read, :get)
+
+      run_get middleware_domains_url
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to eq(
+        'name'      => 'middleware_domains',
+        'count'     => 1,
+        'resources' => [{
+          'href' => domain_url
+        }],
+        'subcount'  => 1
+      )
+    end
+  end
+
+  describe '/:id' do
+    it 'returns the attributes of one domain' do
+      api_basic_authorize
+
+      run_get middleware_domains_url(domain.id)
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to include(
+        'href'       => domain_url,
+        'id'         => domain.id.to_s,
+        'name'       => domain.name
+      )
+    end
+  end
+end
