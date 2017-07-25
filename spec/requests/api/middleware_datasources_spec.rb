d3da9cabf9359fb5066f36c055074208e4823673
Cainã Costa <cainan.costa@gmail.com>
2017-07-24 21:51:01 -0300

    Add read-only API for middleware datasources

---
+++ spec/requests/api/middleware_datasources_spec.rb
@@ -0,0 +1,65 @@
+describe 'Middleware Datasources API' do
+  let(:datasource) { FactoryGirl.create(:middleware_datasource) }
+
+  # For some reason middleware_datasources_url is not returning the full
+  # url, just the path portion. This is a hack, but will do the trick.
+  def datasource_url
+    "http://www.example.com#{middleware_datasources_url(datasource.id)}"
+  end
+
+  describe '/' do
+    it 'forbids access without an appropriate role' do
+      api_basic_authorize
+
+      run_get middleware_datasources_url
+
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'returns an empty listing of datasources' do
+      api_basic_authorize collection_action_identifier(:middleware_datasources, :read, :get)
+
+      run_get middleware_datasources_url
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to eq(
+        'name'      => 'middleware_datasources',
+        'count'     => 0,
+        'resources' => [],
+        'subcount'  => 0
+      )
+    end
+
+    it 'returns a a listing of datasources' do
+      datasource
+
+      api_basic_authorize collection_action_identifier(:middleware_datasources, :read, :get)
+
+      run_get middleware_datasources_url
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to eq(
+        'name'      => 'middleware_datasources',
+        'count'     => 1,
+        'resources' => [{
+          'href' => datasource_url
+        }],
+        'subcount'  => 1
+      )
+    end
+  end
+
+  describe '/:id' do
+    it 'returns the attributes of one datasource' do
+      api_basic_authorize
+
+      run_get middleware_datasources_url(datasource.id)
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).to include(
+        'href'       => datasource_url,
+        'id'         => datasource.id.to_s,
+      )
+    end
+  end
+end
