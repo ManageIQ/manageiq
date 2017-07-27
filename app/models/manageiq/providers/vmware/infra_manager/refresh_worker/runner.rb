7bfdee3b1fe7cb5d61f00426fb540d1bc155f65d
Greg Blomquist <blomquisg@gmail.com>
2017-07-27 12:44:22 -0400

    Merge pull request #75 from agrare/bz_1469198_dont_run_queue_full_refresh_every_refresh_worker_start
    Don't queue a refresh on RefreshWorker start
    (cherry picked from commit 26ae511caff67b5fa6aad5bf896c30bc8c7e2c38)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1476284

--- app/models/manageiq/providers/vmware/infra_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/vmware/infra_manager/refresh_worker/runner.rb
@@ -3,10 +3,6 @@ class ManageIQ::Providers::Vmware::InfraManager::RefreshWorker::Runner < ManageI
 
   def do_before_work_loop
     # Override Standard EmsRefreshWorker's method of queueing up a Refresh
-    # if the VimBrokerWorker isn't available yet.
     # This will be done by the VimBrokerWorker, when he is ready.
-    #
-    # If the VimBrokerWorker is running already then queue up an initial refresh
-    super if MiqVimBrokerWorker.available?
   end
 end
