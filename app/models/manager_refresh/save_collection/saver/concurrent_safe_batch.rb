1eff68a427a8819af5ad5a2b26724b811d055952
Ladislav Smola <lsmola@redhat.com>
2017-08-02 16:39:33 +0200

    Log message on 1 line based on the review request
    Log message on 1 line based on the review request

--- app/models/manager_refresh/save_collection/saver/concurrent_safe_batch.rb
+++ app/models/manager_refresh/save_collection/saver/concurrent_safe_batch.rb
@@ -97,8 +97,7 @@ module ManagerRefresh::SaveCollection
                   "updated=#{inventory_collection.updated_records.count}, "\
                   "deleted=#{inventory_collection.deleted_records.count} *************")
       rescue => e
-        _log.error("Error when saving #{inventory_collection} with "\
-                   "#{inventory_collection_details(inventory_collection)}. Message: #{e.message}")
+        _log.error("Error when saving #{inventory_collection} with #{inventory_collection_details(inventory_collection)}. Message: #{e.message}")
         raise e
       end
 
