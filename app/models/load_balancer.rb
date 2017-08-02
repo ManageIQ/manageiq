b6436068c6a96745816cb42db7b12b9d9f2dafd6
Andrei Veselov <aveselov@redhat.com>
2017-08-02 13:35:26 +0200

    Include operations concern to LoadBalancer

--- app/models/load_balancer.rb
+++ app/models/load_balancer.rb
@@ -6,6 +6,8 @@ class LoadBalancer < ApplicationRecord
   include TenantIdentityMixin
   include CloudTenancyMixin
 
+  include_concern 'Operations'
+
   acts_as_miq_taggable
 
   belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::NetworkManager"
