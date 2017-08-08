f8ebe35bc88a2a54e731431271f894dfa0619d0c
Joe Rafaniello <jrafanie@redhat.com>
2017-08-08 14:17:44 -0400

    Move routes to ui-classic, where these controllers live
    Depends on https://github.com/ManageIQ/manageiq-ui-classic/pull/1763

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Vmdb::Application.routes.draw do
-  root :to => 'dashboard#login'
-
-  # Let's serve pictures directly from the DB
-  get '/pictures/:basename' => 'picture#show', :basename => /[\da-zA-Z]+\.[\da-zA-Z]+/
-
-  # ping response for load balancing
-  get '/ping' => 'ping#index'
-
   if Rails.env.development? && defined?(Rails::Server)
     logger = Logger.new(STDOUT)
     logger.level = Logger.const_get(::Settings.log.level_websocket.upcase)
