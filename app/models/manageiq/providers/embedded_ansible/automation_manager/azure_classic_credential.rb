1c573c44403795218f6f952f6557eec27e5500e2
James Wong <jwong@redhat.com>
2017-08-09 22:28:21 -0400

    add newly supported Tower credentials to embedded_ansible

---
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/azure_classic_credential.rb
@@ -0,0 +1,3 @@
+class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::AzureClassicCredential < ManageIQ::Providers::EmbeddedAnsible::AutomationManager::CloudCredential
+  include ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::AzureClassicCredential
+end
