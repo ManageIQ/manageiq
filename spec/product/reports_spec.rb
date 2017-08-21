162d259027257ada738ac57e8a0d2a53d41033c5
Adrian Toth <adtoth@redhat.com>
2017-08-21 10:59:08 +0200

    Remove CHARTS_REPORTS_FOLDER from UiConstants
    (transferred from ManageIQ/manageiq-ui-classic@6e29d85d09b4b124719b5eefc8f083021fc83aa9)

--- spec/product/reports_spec.rb
+++ spec/product/reports_spec.rb
@@ -1,7 +1,7 @@
 describe 'YAML reports' do
   let(:report_dirs) { [Rails.root.join("product", "reports"), "#{TIMELINES_FOLDER}/miq_reports"] }
   let(:report_yamls) { report_dirs.collect { |dir| Dir.glob(File.join(dir, "**", "*.yaml")) }.flatten }
-  let(:chart_dirs) { [CHARTS_REPORTS_FOLDER] }
+  let(:chart_dirs) { [ApplicationController::Performance::CHARTS_REPORTS_FOLDER] }
   let(:chart_yamls) { chart_dirs.collect { |dir| Dir.glob(File.join(dir, "**", "*.yaml")) }.flatten }
   let!(:user) { FactoryGirl.create(:user_with_group) }
 
@@ -15,7 +15,7 @@ describe 'YAML reports' do
   end
 
   it 'can be build even though without data' do
-    # TODO: CHARTS_REPORTS_FOLDER
+    # TODO: ApplicationController::Performance::CHARTS_REPORTS_FOLDER
     report_yamls.each do |yaml|
       report_data = YAML.load(File.open(yaml))
       report_data.delete('menu_name')
