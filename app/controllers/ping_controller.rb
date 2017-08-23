fd758dc0226acf4a4d1a0df2077b70dfc2600ae9
Brandon Dunne <bdunne@redhat.com>
2017-08-23 10:42:19 -0400

    Add protect_from_forgery to address Brakeman warning

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -1,4 +1,6 @@
 class PingController < ActionController::Base
+  protect_from_forgery :secret => SecureRandom.hex(64), :with => :exception
+
   def index
     render :plain => 'pong', :status => 200
   end
