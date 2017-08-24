ce96b834fbdf69978d1a764823d9417311ee7637
Milan Zázrivec <mzazrivec@redhat.com>
2017-08-24 16:31:08 +0200

    Merge pull request #1964 from ZitaNemeckova/tag_from_tenant
    CloudTenant uses GenericButtonMixin for tagging
    (cherry picked from commit 1b4e834fa451da132f53c3c9e8b3c0f294a6764e)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1487307

--- app/controllers/cloud_tenant_controller.rb
+++ app/controllers/cloud_tenant_controller.rb
@@ -61,7 +61,7 @@ class CloudTenantController < ApplicationController
       return
     else
       editable_objects = CloudTenantController.display_methods.map(&:singularize) - %w(instance image) # handled in super
-      if params[:pressed].starts_with?(*editable_objects)
+      if params[:pressed].starts_with?(*editable_objects) && !params[:pressed].ends_with?('_tag')
         target_controller = editable_objects.find { |n| params[:pressed].starts_with?(n) }
         action = params[:pressed].sub("#{target_controller}_", '')
         action = "#{action}_#{target_controller.sub('cloud_','').pluralize}" if action == 'delete'
