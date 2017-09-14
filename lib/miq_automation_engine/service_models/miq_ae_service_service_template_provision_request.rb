1bc5eb462244e74babd3eadea088be3650316438
Madhu Kanoor <mkanoor@redhat.com>
2017-09-14 16:01:47 -0400

    Merge pull request #69 from tinaafitz/add_quota_mixin
    Add missing service model change for calculating active quota counts for Service requests.
    (cherry picked from commit ca32a7918cd0e9a5664e5eb4b44d9b11458037e6)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1493207

--- lib/miq_automation_engine/service_models/miq_ae_service_service_template_provision_request.rb
+++ lib/miq_automation_engine/service_models/miq_ae_service_service_template_provision_request.rb
@@ -1,5 +1,8 @@
 module MiqAeMethodService
   class MiqAeServiceServiceTemplateProvisionRequest < MiqAeServiceMiqRequest
+    require_relative "mixins/miq_ae_service_miq_provision_mixin"
+    include MiqAeServiceMiqProvisionMixin
+
     def ci_type
       'service'
     end
