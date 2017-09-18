f04b5bddb2c04a49f3646af33e77934a7c75c5f2
Zahi Akiva <zakiva@redhat.com>
2017-09-18 17:00:19 +0300

    Add openshift template factory

--- spec/factories/container_template.rb
+++ spec/factories/container_template.rb
@@ -1,4 +1,10 @@
 FactoryGirl.define do
   factory :container_template do
   end
+
+  factory :openshift_template,
+          :aliases => ['manageiq/providers/openshift/container_manager/container_template'],
+          :class   => 'ManageIQ::Providers::Openshift::ContainerManager::ContainerTemplate',
+          :parent  => :container_template do
+  end
 end
