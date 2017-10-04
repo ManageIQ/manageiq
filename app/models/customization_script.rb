dda559a43d661e342f937675b8e8811a2e8922ef
skovic <skovic@lenovo.com>
2017-10-04 15:50:57 -0400

    Undo customization_script model changes

--- app/models/customization_script.rb
+++ app/models/customization_script.rb
@@ -2,5 +2,5 @@ class CustomizationScript < ApplicationRecord
   include NewWithTypeStiMixin
 
   acts_as_miq_taggable
-  belongs_to :ext_management_system, :foreign_key => "manager_id"
+  belongs_to :provisioning_manager
 end
