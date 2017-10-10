6b9b079b0e48f2fc16332091e05f424eb1ea8375
Nick Carboni <ncarboni@redhat.com>
2017-10-10 11:37:52 -0400

    Define availability for ApplianceEmbeddedAnsible
    This class is available for use if the ansible tower rpms
    are installed on an appliance.

--- lib/embedded_ansible/appliance_embedded_ansible.rb
+++ lib/embedded_ansible/appliance_embedded_ansible.rb
@@ -1,5 +1,10 @@
+require 'linux_admin'
+
 class ApplianceEmbeddedAnsible < EmbeddedAnsible
   def self.available?
+    return false unless MiqEnvironment::Command.is_appliance?
+    required_rpms = Set["ansible-tower-server", "ansible-tower-setup"]
+    required_rpms.subset?(LinuxAdmin::Rpm.list_installed.keys.to_set)
   end
 
   def start
