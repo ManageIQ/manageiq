d4cfd7ff47ef88e0b79be0d29078e77df6bb5e4b
Bo Yao <icerove@gmail.com>
2017-10-11 16:43:08 -0400

    remove old tar only if all steps success

--- tools/pg_inspector/inspect_pg.sh
+++ tools/pg_inspector/inspect_pg.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
+
+# ensure pathname correct when runs in a different place
 basedir=$(dirname $0)
+# default postgresql password: smartvm
 password="${PGPASSWORD:-smartvm}"
 
 # Run step 1, 3 and 4 in sequence, assume step 2 has done before.
 ${basedir}/../pg_inspector.rb connections
 ${basedir}/../pg_inspector.rb human
 ${basedir}/../pg_inspector.rb locks
+
+# remove first only if all steps success
+if [ $? -ne '0' ]; then
+  echo "Fails to generate lock output."
+  exit 1
+fi
+rm pg_inspector_output.tar.gz
 
 # collect the output
 tar czf pg_inspector_output.tar.gz ${basedir}/output
