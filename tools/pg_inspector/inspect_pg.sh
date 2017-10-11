8544c89a5bfcc7effc5cbe4524d07a76a63e273e
Bo Yao <icerove@gmail.com>
2017-10-11 17:14:17 -0400

    now runs correctly!

--- tools/pg_inspector/inspect_pg.sh
+++ tools/pg_inspector/inspect_pg.sh
@@ -3,7 +3,7 @@
 # ensure pathname correct when runs in a different place
 basedir=$(dirname $0)
 # default postgresql password: smartvm
-password="${PGPASSWORD:-smartvm}"
+export PGPASSWORD="${PGPASSWORD:-smartvm}"
 
 # Run step 1, 3 and 4 in sequence, assume step 2 has done before.
 ${basedir}/../pg_inspector.rb connections
@@ -15,7 +15,7 @@ if [ $? -ne '0' ]; then
   echo "Fails to generate lock output."
   exit 1
 fi
-rm pg_inspector_output.tar.gz
+rm -f pg_inspector_output.tar.gz
 
 # collect the output
 tar czf pg_inspector_output.tar.gz ${basedir}/output
