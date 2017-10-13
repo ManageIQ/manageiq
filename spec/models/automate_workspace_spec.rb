6b65848ccc61ab8b4c8dc2acfdcb573f51dc3edd
Madhu Kanoor <mkanoor@redhat.com>
2017-10-13 14:34:32 -0400

    Encrypt/Decrypt passwords in Automate Workspace

--- spec/models/automate_workspace_spec.rb
+++ spec/models/automate_workspace_spec.rb
@@ -1,7 +1,18 @@
 describe AutomateWorkspace do
   describe "#merge_output!" do
     let(:user) { FactoryGirl.create(:user_with_group, :userid => "admin") }
-    let(:aw) { FactoryGirl.create(:automate_workspace, :user => user, :tenant => user.current_tenant) }
+    let(:aw) do
+      FactoryGirl.create(:automate_workspace, :user   => user,
+                                              :tenant => user.current_tenant,
+                                              :input  => input)
+    end
+    let(:password) { "secret" }
+    let(:encrypted) { MiqAePassword.encrypt(password) }
+    let(:input) do
+      { 'objects'           => {'root' => { 'var1' => '1', 'var2' => "password::#{encrypted}"}},
+        'method_parameters' => {'arg1' => "password::#{encrypted}"} }
+    end
+
     it "raises error on invalid hash" do
       expect { aw.merge_output!({}) }.to raise_exception(ArgumentError)
     end
@@ -21,6 +32,32 @@ describe AutomateWorkspace do
 
     it "#href_slug" do
       expect(aw.href_slug).to eq("automate_workspaces/#{aw.guid}")
+    end
+
+    it "#decrypt from object" do
+      expect(aw.decrypt('root', 'var2')).to eq(password)
+    end
+
+    it "#decrypt from method parameters" do
+      expect(aw.decrypt('method_parameters', 'arg1')).to eq(password)
+    end
+
+    it "#decrypt raises error when object doesn't exist" do
+      expect { aw.decrypt('frooti', 'var2') }.to raise_exception(ArgumentError)
+    end
+
+    it "#decrypt raises error when attribute doesn't exist" do
+      expect { aw.decrypt('root', 'nada') }.to raise_exception(ArgumentError)
+    end
+
+    it "#decrypt raises error when type is invalid" do
+      expect { aw.decrypt('root', 'var1') }.to raise_exception(ArgumentError)
+    end
+
+    it "#encrypt" do
+      aw.encrypt('root', 'mypassword', password)
+      aw.reload
+      expect(aw.output.fetch_path('objects', 'root', 'mypassword')).to eq("password::#{encrypted}")
     end
   end
 end
