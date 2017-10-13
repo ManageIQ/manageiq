b2d7385b887cbe697f34ec91a60794a5fd9d4586
skovic <skovic@lenovo.com>
2017-10-13 17:34:16 -0400

    Update for button identifier change in UI

--- app/models/physical_server_provision_request.rb
+++ app/models/physical_server_provision_request.rb
@@ -9,4 +9,12 @@ class PhysicalServerProvisionRequest < MiqProvisionConfiguredSystemRequest
   def self.request_task_class_from(_attribs)
     ManageIQ::Providers::Lenovo::PhysicalInfraManager
   end
+
+  def event_name(mode)
+    "physical_server_provision_request_#{mode}"
+  end
+
+  def originating_controller
+    "physical_server"
+  end
 end
