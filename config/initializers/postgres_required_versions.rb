034fcdfc200476d56f1b6b02eaa9322511b39ccc
Martin Hradil <mhradil@redhat.com>
2017-10-13 13:27:06 +0000

    Postgres version check - only raise in production
    The feature is meant to protect users from using a wrong version of external DB.
    
    But it also breaks devel setups for anybody using PG9.6+.
    
    Thus, changing to only raise in production.
    Development mode will warn to stderr (not sure about loggers in initializers), but not raise an exception.

--- config/initializers/postgres_required_versions.rb
+++ config/initializers/postgres_required_versions.rb
@@ -2,7 +2,10 @@ ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend Module.new {
   def initialize(*args)
     super
     if postgresql_version < 90400 || postgresql_version >= 90600
-      raise "The version of PostgreSQL being connected to is incompatible with #{I18n.t("product.name")}"
+      msg = "The version of PostgreSQL being connected to is incompatible with #{I18n.t("product.name")}"
+
+      raise msg if Rails.env.production?
+      $stderr.puts msg
     end
   end
 }
