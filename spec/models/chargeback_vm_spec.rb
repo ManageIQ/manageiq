2bea32d8398cb7156b4de2c77a5faf0dd3d3cfc0
lpichler <lpichler@redhat.com>
2017-10-17 10:58:16 +0200

    Add seeding for vm specs

--- spec/models/chargeback_vm_spec.rb
+++ spec/models/chargeback_vm_spec.rb
@@ -58,6 +58,9 @@ describe ChargebackVm do
     MiqRegion.seed
     ChargebackRateDetailMeasure.seed
     ChargeableField.seed
+    ManageIQ::Consumption::ShowbackUsageType.seed
+    ManageIQ::Consumption::ShowbackPricePlan.seed
+    MiqEnterprise.seed
 
     EvmSpecHelper.create_guid_miq_server_zone
     cat = FactoryGirl.create(:classification, :description => "Environment", :name => "environment", :single_value => true, :show => true)
