b5204240a0e5ef8e1be13ba3b721a4bc6e5437a4
Nick Carboni <ncarboni@redhat.com>
2017-10-18 15:11:53 -0400

    Base the monolithic docker image on the pods frontend image
    This will allow us to maintain the package list in fewer places
    while also removing the need for running privileged containers
    because we can remove systemd.
    
    This change uses the existing entrypoint scrips from the pods
    image and just drops in a replacement script for appliance-initialize.sh
    
    We also need a replacement for check-dependent-services.sh as
    we are running all the services internal to the container in this case.

--- docker-assets/appliance-initialize.sh
+++ docker-assets/appliance-initialize.sh
@@ -1,5 +1,35 @@
-#!/bin/sh
-[[ -d /var/opt/rh/rh-postgresql95/lib/pgsql/data/base ]] && exit 0
-[[ -s /etc/default/evm ]] && source /etc/default/evm
-echo "Initializing Appliance, please wait ..."
-appliance_console_cli --region 0 --internal --password smartvm --key
+#!/bin/bash
+
+# Source EVM environment 
+[ -f /etc/default/evm ] &&  . /etc/default/evm
+
+# Check postgres server DB init status, if necessary, initdb, start/enable service and inject MIQ role
+
+echo "== Checking MIQ database status =="
+
+[[ -d /var/opt/rh/rh-postgresql95/lib/pgsql/data/base ]]
+	if [ $? -eq 0 ]; then
+		echo "** DB already initialized"
+		exit 0
+	else
+		echo "** DB has not been initialized"
+		echo "** Launching initdb"
+		su postgres -c "initdb -D ${APPLIANCE_PG_DATA}"
+		test $? -ne 0 && echo "!! Failed to initdb" && exit 1
+		echo "** Starting postgresql"
+		su postgres -c "pg_ctl -D ${APPLIANCE_PG_DATA} start"
+		test $? -ne 0 && echo "!! Failed to start postgresql service" && exit 1
+		sleep 5
+		echo "** Creating MIQ role"
+		su postgres -c "psql -c \"CREATE ROLE root SUPERUSER LOGIN PASSWORD 'smartvm'\""
+		test $? -ne 0 && echo "!! Failed to inject MIQ root Role" && exit 1
+		# Check if memcached is running, if not start it
+		pidof memcached
+		test $? -ne 0 && /usr/bin/memcached -u memcached -p 11211 -m 64 -c 1024 -l 127.0.0.1 -d
+		echo "** Starting DB setup"
+		SKIP_TEST_RESET=true ${APP_ROOT}/bin/setup
+		test $? -ne 0 && echo "!! ${APP_ROOT}/docker-assets/docker_setup failed to run" && exit 1
+		echo "** MIQ database has been initialized"
+    generate_miq_server_cert.sh
+		exit 0
+	fi
