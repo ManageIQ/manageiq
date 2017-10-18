620d8ccc30f6cc9a65d780775cf928bb967aa708
hsong-rh <hsong@redhat.com>
2017-10-18 08:59:38 -0400

    Move changes to Amazon plugin

--- app/models/amazon_agent_coordinator_worker.rb
+++
@@ -1,8 +0,0 @@
-class AmazonAgentCoordinatorWorker < MiqWorker
-  require_nested :Runner
-
-  self.required_roles = ['smartproxy']
-
-  # Don't allow multiple workers to run at once
-  self.include_stopping_workers_on_synchronize = true
-end
--- lib/workers/miq_worker_types.rb
+++ lib/workers/miq_worker_types.rb
@@ -2,6 +2,7 @@
 # global namespace
 
 MIQ_WORKER_TYPES = {
+  "ManageIQ::Providers::Amazon::AgentCoordinatorWorker"                       => %i(manageiq_default),
   "ManageIQ::Providers::Amazon::CloudManager::EventCatcher"                   => %i(manageiq_default),
   "ManageIQ::Providers::Amazon::CloudManager::MetricsCollectorWorker"         => %i(manageiq_default),
   "ManageIQ::Providers::Amazon::CloudManager::RefreshWorker"                  => %i(manageiq_default),
@@ -57,7 +58,6 @@ MIQ_WORKER_TYPES = {
   "ManageIQ::Providers::Vmware::InfraManager::RefreshWorker"                  => %i(manageiq_default),
   "ManageIQ::Providers::Vmware::NetworkManager::RefreshWorker"                => %i(manageiq_default),
   "EmbeddedAnsibleWorker"                                                     => %i(manageiq_default),
-  "AmazonAgentManagerWorker"                                                  => %i(manageiq_default),
   "MiqCockpitWsWorker"                                                        => %i(manageiq_default),
   "MiqEmsMetricsProcessorWorker"                                              => %i(manageiq_default),
   "MiqEmsRefreshCoreWorker"                                                   => %i(manageiq_default),
@@ -87,7 +87,6 @@ MIQ_WORKER_TYPES_IN_KILL_ORDER = %w(
   ManageIQ::Providers::Redhat::NetworkManager::MetricsCollectorWorker
   ManageIQ::Providers::Openstack::InfraManager::MetricsCollectorWorker
   EmbeddedAnsibleWorker
-  AmazonAgentManagerWorker
   MiqReportingWorker
   MiqSmartProxyWorker
   MiqGenericWorker
@@ -98,6 +97,7 @@ MIQ_WORKER_TYPES_IN_KILL_ORDER = %w(
   ManageIQ::Providers::Amazon::NetworkManager::RefreshWorker
   ManageIQ::Providers::Amazon::StorageManager::Ebs::RefreshWorker
   ManageIQ::Providers::Amazon::StorageManager::S3::RefreshWorker
+  ManageIQ::Providers::Amazon::AgentCoordinatorWorker
   ManageIQ::Providers::Google::CloudManager::RefreshWorker
   ManageIQ::Providers::Google::NetworkManager::RefreshWorker
   ManageIQ::Providers::AnsibleTower::AutomationManager::RefreshWorker
