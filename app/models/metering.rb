42882db196c823c45e48988e79579b51ab42beeb
lpichler <lpichler@redhat.com>
2017-10-30 10:07:45 +0100

    FixBug:Use count of metric rollups for metering hours
    it is same as fixed compute metric
    Chargeable field is not used for getting metric value for
    metering hours metric and for fixed

--- app/models/metering.rb
+++ app/models/metering.rb
@@ -1,13 +1,15 @@
 module Metering
   def calculate_costs(consumption, _)
     self.fixed_compute_metric = consumption.chargeback_fields_present if consumption.chargeback_fields_present
+    self.metering_used_metric = fixed_compute_metric
 
     relevant_fields.each do |field|
       next unless self.class.report_col_options.include?(field)
       group, source, * = field.split('_')
       chargable_field = ChargeableField.find_by(:group => group, :source => source)
+      next if field == "fixed_compute_metric" || chargable_field && chargable_field.metering?
       value = chargable_field.measure_metering(consumption, @options) if chargable_field
-      self[field] = (value || 0) unless field == 'fixed_compute_metric'
+      self[field] = (value || 0)
     end
   end
 end
