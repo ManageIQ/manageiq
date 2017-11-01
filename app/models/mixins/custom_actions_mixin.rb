13971bcfe5c2b9f554f39abaffe36327bc456ba4
d-m-u <drewuhlmann@gmail.com>
2017-11-01 14:12:04 -0400

    Fixes custom button method for things with subclasses

--- app/models/mixins/custom_actions_mixin.rb
+++ app/models/mixins/custom_actions_mixin.rb
@@ -48,6 +48,6 @@ module CustomActionsMixin
   end
 
   def generic_custom_buttons
-    CustomButton.buttons_for(self.class.name)
+    CustomButton.buttons_for(self.class.base_model.name)
   end
 end
