ff23a1c807e7e2e820e497415ffa92dc03b3acba
Oleg Barenboim <chessbyte@gmail.com>
2017-11-14 17:09:48 +0200

    Merge pull request #16468 from mzazrivec/fix_string_interpolations
    Fix string interpolations
    (cherry picked from commit 44c7c81839998d991f3bf25946d5c118a6fd2cb8)

--- app/models/physical_server/operations/config_pattern.rb
+++ app/models/physical_server/operations/config_pattern.rb
@@ -1,7 +1,8 @@
 module PhysicalServer::Operations::ConfigPattern
   def apply_config_pattern(pattern_id)
     unless ext_management_system
-      raise _("A Server #{self} <%{name}> with Id: <%{id}> is not associated with a provider.") % {:name => name, :id => id}
+      raise _("A Server %{server} <%{name}> with Id: <%{id}> is not associated with a provider.") %
+            {:server => self, :name => name, :id => id}
     end
 
     _log.info("Apply config pattern with ID: #{pattern_id} to server with UUID: #{ems_ref}")
