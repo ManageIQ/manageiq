1fed5270523007c6fbe0232b0ba2c8864b92add3
Jason Frey <fryguy9@gmail.com>
2017-11-16 14:12:35 -0500

    Merge pull request #16442 from agrare/reduce_graph_refresh_logging
    Move graph refresh internals logging to debug
    (cherry picked from commit 1a73cd2183d89ad132a0f103d61f3a6201dd7b7d)

--- app/models/manager_refresh/save_collection/base.rb
+++ app/models/manager_refresh/save_collection/base.rb
@@ -2,16 +2,16 @@ module ManagerRefresh::SaveCollection
   class Base
     class << self
       def save_inventory_object_inventory(ems, inventory_collection)
-        _log.info("SYNCHRONIZING collection #{inventory_collection} of size #{inventory_collection.size} to"\
-                  " the database, for the manager: '#{ems.name}'")
+        _log.debug("Saving collection #{inventory_collection} of size #{inventory_collection.size} to"\
+                   " the database, for the manager: '#{ems.name}'...")
 
         if inventory_collection.custom_save_block.present?
-          _log.info("SYNCHRONIZING collection #{inventory_collection} using a custom save block")
+          _log.debug("Saving collection #{inventory_collection} using a custom save block")
           inventory_collection.custom_save_block.call(ems, inventory_collection)
         else
           save_inventory(inventory_collection)
         end
-        _log.info("SYNCHRONIZED collection #{inventory_collection}, for the manager: '#{ems.name}'")
+        _log.debug("Saving collection #{inventory_collection}, for the manager: '#{ems.name}'...Complete")
         inventory_collection.saved = true
       end
 
