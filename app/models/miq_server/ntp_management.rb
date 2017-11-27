19581f06bed347d8e30b4408621eca0e435d176b
Jason Frey <fryguy9@gmail.com>
2017-11-27 11:32:57 -0500

    Merge pull request #16393 from bdunne/ntp_settings
    Finish simplifying NTP configuration using Settings
    (cherry picked from commit b631e1af2836401f2622a07cc0c07d7b33759c5b)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1517959

--- app/models/miq_server/ntp_management.rb
+++ app/models/miq_server/ntp_management.rb
@@ -3,31 +3,13 @@ require 'linux_admin'
 module MiqServer::NtpManagement
   extend ActiveSupport::Concern
 
-  def server_ntp_settings
-    # Get the ntp servers from the vmdb.yml first, zone second, else use some defaults
-    ntp = ntp_config
-    if server_ntp_settings_blank?(ntp)
-      zone.ntp_settings
-    else
-      ntp[:source] = :server
-      ntp
-    end
-  end
-
-  def ntp_config
-    get_config("vmdb").config[:ntp]
-  end
-
-  def server_ntp_settings_blank?(ntp)
-    # verify the ntp settings are like this and not blank:  {:ntp => {:server => ['blah'], :timeout => 5}}
-    ntp.values.flatten.compact.blank? rescue true
-  end
-
   # Called when zone ntp settings changed... run by the appropriate server
   # Also, called in start of miq_server and on a configuration change for the server
-  def ntp_reload(ntp_settings = server_ntp_settings)
+  def ntp_reload
     # matches ntp_reload_queue's guard clause
     return if !MiqEnvironment::Command.is_appliance? || MiqEnvironment::Command.is_container?
+
+    ntp_settings = get_config("vmdb").config[:ntp]
 
     if @ntp_settings && @ntp_settings == ntp_settings
       _log.info("Skipping reload of ntp settings since they are unchanged")
@@ -39,15 +21,6 @@ module MiqServer::NtpManagement
       return
     end
 
-    if ntp_settings.delete(:source) != :server && !server_ntp_settings_blank?(ntp_config)
-      _log.info("Skipping reload of ntp settings from zone since this server is configured with it's own ntp settings")
-      return
-    end
-
-    if ntp_settings[:server].nil?
-      _log.warn("No ntp server settings to synchronize")
-      return
-    end
     _log.info("Synchronizing ntp settings: #{ntp_settings.inspect}")
     apply_ntp_server_settings(ntp_settings)
     @ntp_settings = ntp_settings
