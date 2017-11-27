4edee6e8d2195a1cbc18198adc6be5f6e5593a43
Jillian Tullo <jtullo@redhat.com>
2017-11-27 10:17:51 -0500

    Create CockpitSupportMixin
    Because cockpit is supported by both VMs and containers, removing those methods from the operations mixin to a shared mixin seemed to make the most sense.

--- spec/models/vm/operations_spec.rb
+++ spec/models/vm/operations_spec.rb
@@ -163,35 +163,6 @@ describe 'VM::Operations' do
     end
   end
 
-  context '#supports_cockpit_console?' do
-    before do
-      @my_region = double
-      allow(MiqRegion).to receive(:my_region).and_return(@my_region)
-    end
-
-    it 'is not supported if the cockpit role is not enabled' do
-      allow(@my_region).to receive(:role_active?).with('cockpit_ws').and_return(false)
-
-      expect(@vm.supports_cockpit_console?).to be_falsey
-      expect(@vm.unsupported_reason(:cockpit_console)).to include('role is not enabled')
-    end
-
-    it 'is not supported for windows platforms' do
-      allow(@my_region).to receive(:role_active?).with('cockpit_ws').and_return(true)
-      allow(@vm).to receive(:platform).and_return('windows')
-
-      expect(@vm.supports_cockpit_console?).to be_falsey
-      expect(@vm.unsupported_reason(:cockpit_console)).to include('Windows platform is not supported')
-    end
-
-    it 'supports it if all conditions are met' do
-      allow(@my_region).to receive(:role_active?).with('cockpit_ws').and_return(true)
-      allow(@vm).to receive(:platform).and_return('vmware')
-
-      expect(@vm.supports_cockpit_console?).to be_truthy
-    end
-  end
-
   context '#supports_vmrc_console?' do
     it 'returns false if type is not supported' do
       allow(@vm).to receive(:console_supports_type?).with('VMRC').and_return(false)
@@ -219,30 +190,6 @@ describe 'VM::Operations' do
       allow(@vm).to receive(:console_supports_type?).with('SPICE').and_return(true)
 
       expect(@vm.supports_spice_console?).to be_truthy
-    end
-  end
-
-  context '#supports_launch_cockpit?' do
-    it 'does not support it if there are no ipaddresses present' do
-      allow(@vm).to receive(:ipaddresses).and_return([])
-
-      expect(@vm.supports_launch_cockpit?).to be_falsey
-      expect(@vm.unsupported_reason(:launch_cockpit)).to include('requires an IP address')
-    end
-
-    it 'does not support it if the vm is not powered on' do
-      allow(@vm).to receive(:ipaddresses).and_return(['000.0.0.0'])
-      allow(@vm).to receive(:power_state).and_return('off')
-
-      expect(@vm.supports_launch_cockpit?).to be_falsey
-      expect(@vm.unsupported_reason(:launch_cockpit)).to include('not available because the VM is not powered on')
-    end
-
-    it 'supports it if all conditions are met' do
-      allow(@vm).to receive(:ipaddresses).and_return(['000.0.0.0'])
-      allow(@vm).to receive(:power_state).and_return('on')
-
-      expect(@vm.supports_launch_cockpit?).to be_truthy
     end
   end
 
