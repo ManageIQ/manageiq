38d0b8159bfa26db608ccf715cc4207e6ca36bbe
Nick LaMuro <nicklamuro@gmail.com>
2017-11-27 23:23:35 -0600

    Additions to cloud_tenant_spec.rb
    Adds a extra VM that doesn't show up in the `cloud_tenant.vms` call, to
    confirm the `virtual_total` works as expected.

--- spec/models/cloud_tenant_spec.rb
+++ spec/models/cloud_tenant_spec.rb
@@ -12,18 +12,21 @@ describe CloudTenant do
 
   describe '#total_vms' do
     let(:ems)          { FactoryGirl.create(:ems_openstack) }
-    let(:vm)           { FactoryGirl.create(:vm_openstack, :ext_management_system => ems) }
+    let(:vm1)          { FactoryGirl.create(:vm_openstack, :ext_management_system => ems) }
+    let(:vm2)          { FactoryGirl.create(:vm_openstack, :ext_management_system => nil) }
+    let(:vms)          { [vm1, vm2] }
     let(:template)     { FactoryGirl.create(:miq_template) }
-    let(:cloud_tenant) { FactoryGirl.create(:cloud_tenant, :ext_management_system => ems, :vms => [vm], :miq_templates => [template]) }
+    let(:cloud_tenant) { FactoryGirl.create(:cloud_tenant, :ext_management_system => ems, :vms => vms, :miq_templates => [template]) }
 
     it 'counts only vms' do
-      expect(cloud_tenant.vms).to match_array(vm)
+      cloud_tenant.reload
+      expect(cloud_tenant.vms.map(&:id)).to match_array([vm1.id])
       expect(cloud_tenant.total_vms).to eq(1)
 
       total_vms_from_select = CloudTenant.where(:id => cloud_tenant).select(:total_vms).first[:total_vms]
       expect(total_vms_from_select).to eq(1)
       expect(total_vms_from_select).to eq(cloud_tenant.total_vms)
-      expect(cloud_tenant.vms_and_templates.count).to eq(2)
+      expect(cloud_tenant.vms_and_templates.count).to eq(3)
     end
   end
 end
