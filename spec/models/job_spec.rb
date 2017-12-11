44d1dce1d2030fdf9caddb17094d1f5cde375f08
Gregg Tanzillo <gtanzill@redhat.com>
2017-12-11 11:00:04 -0500

    Merge pull request #16627 from yrudman/fix-error-in-timeout-checking-for-job-without-target
    Fix error in timeout checking for job without target
    (cherry picked from commit 57293594e36ee1c4597e91c34d956a654c8eca4d)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1524592

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -271,6 +271,11 @@ describe Job do
           expect(@job.timeout_adjustment).to eq(1)
           expect(@image_scan_job.timeout_adjustment).to eq(1)
         end
+
+        it "returns the correct adjusment 1 if target class was not defined" do
+          job_without_target = Job.create_job("VmScan", :target_class => nil)
+          expect(job_without_target.timeout_adjustment).to eq(1)
+        end
       end
     end
 
