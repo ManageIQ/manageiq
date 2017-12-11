474010f69a1e1bb5a79fc343881828fc381625d6
Ari Zellner <azellner@redhat.com>
2017-12-11 12:42:09 +0200

    ensure monitoring manager deletion and creation on provider update

--- app/models/mixins/has_monitoring_manager_mixin.rb
+++ app/models/mixins/has_monitoring_manager_mixin.rb
@@ -5,17 +5,14 @@ module HasMonitoringManagerMixin
 
   def ensure_monitoring_manager
     # monitoring_manager should be defined by child classes.
-    if try(:monitoring_manager_needed?)
+    if monitoring_manager_needed? && monitoring_manager.nil?
       build_monitoring_manager(:parent_manager => self)
       monitoring_manager.name = "#{name} Monitoring Manager"
-    end
-    ensure_monitoring_manager_properties
-  end
-
-  def ensure_monitoring_manager_properties
-    if monitoring_manager
       monitoring_manager.zone_id = zone_id
       monitoring_manager.provider_region = provider_region
+    elsif !monitoring_manager_needed? && monitoring_manager.present?
+      # TODO: if someone deletes the alerts endpoint and then quickly readds it they can end up without a manager.
+      monitoring_manager.delete_queue
     end
   end
 end
