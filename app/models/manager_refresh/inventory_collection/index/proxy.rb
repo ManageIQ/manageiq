e471845665457ebac7a251358dccb6e6093f6bc1
Ladislav Smola <lsmola@redhat.com>
2017-12-12 12:04:27 +0100

    All finders are using references now
    All finders are using references now

--- app/models/manager_refresh/inventory_collection/index/proxy.rb
+++ app/models/manager_refresh/inventory_collection/index/proxy.rb
@@ -7,7 +7,7 @@ module ManagerRefresh
         def initialize(inventory_collection, secondary_refs = {})
           @inventory_collection = inventory_collection
 
-          @primary_ref    = {:manager_ref => @inventory_collection.manager_ref}
+          @primary_ref    = {primary_index_ref => @inventory_collection.manager_ref}
           @secondary_refs = secondary_refs
           @all_refs       = @primary_ref.merge(@secondary_refs)
 
@@ -17,11 +17,13 @@ module ManagerRefresh
           @all_refs.each do |index_name, attribute_names|
             @data_indexes[index_name] = ManagerRefresh::InventoryCollection::Index::Type::Data.new(
               inventory_collection,
+              index_name,
               attribute_names
             )
 
             @local_db_indexes[index_name] = ManagerRefresh::InventoryCollection::Index::Type::LocalDb.new(
               inventory_collection,
+              index_name,
               attribute_names,
               @data_indexes[index_name]
             )
@@ -48,44 +50,40 @@ module ManagerRefresh
           end
         end
 
-        def primary_index_ref
-          :manager_ref
-        end
-
         def primary_index
           data_index(primary_index_ref)
         end
 
-        def find(manager_uuid, ref: :manager_ref)
+        def find(reference, ref: primary_index_ref)
           # TODO(lsmola) lazy_find will support only hash, then we can remove the _by variant
-          return if manager_uuid.nil?
+          # TODO(lsmola) this method shoul return lazy too, the rest of the finders should be deprecated
+          return if reference.nil?
+          return unless assert_index(reference, ref)
 
-          manager_uuid = stringify_index_value(manager_uuid, ref)
-
-          return unless assert_index(manager_uuid, ref)
+          reference = inventory_collection.build_reference(reference, ref)
 
           case strategy
           when :local_db_find_references, :local_db_cache_all
-            local_db_index(ref).find(manager_uuid)
+            local_db_index_find(reference)
           when :local_db_find_missing_references
-            data_index(ref).find(manager_uuid) || local_db_index(ref).find(manager_uuid)
+            data_index_find(reference) || local_db_index_find(reference)
           else
-            data_index(ref).find(manager_uuid)
+            data_index_find(reference)
           end
         end
 
-        def find_by(manager_uuid_hash, ref: :manager_ref)
+        def find_by(manager_uuid_hash, ref: primary_index_ref)
           # TODO(lsmola) deprecate this, it's enough to have find method
           find(manager_uuid_hash, :ref => ref)
         end
 
-        def lazy_find_by(manager_uuid_hash, ref: :manager_ref, key: nil, default: nil)
+        def lazy_find_by(manager_uuid_hash, ref: primary_index_ref, key: nil, default: nil)
           # TODO(lsmola) deprecate this, it's enough to have lazy_find method
 
           lazy_find(manager_uuid_hash, :ref => ref, :key => key, :default => default)
         end
 
-        def lazy_find(manager_uuid, ref: :manager_ref, key: nil, default: nil)
+        def lazy_find(manager_uuid, ref: primary_index_ref, key: nil, default: nil)
           # TODO(lsmola) also, it should be enough to have only 1 find method, everything can be lazy, until we try to
           # access the data
           # TODO(lsmola) lazy_find will support only hash, then we can remove the _by variant
@@ -93,29 +91,30 @@ module ManagerRefresh
           return unless assert_index(manager_uuid, ref)
 
           ::ManagerRefresh::InventoryObjectLazy.new(inventory_collection,
-                                                    stringify_index_value(manager_uuid, ref),
                                                     manager_uuid,
                                                     :ref => ref, :key => key, :default => default)
         end
 
+        def named_ref(ref)
+          all_refs[ref]
+        end
+
         private
 
-        delegate :strategy, :hash_index_with_keys, :to => :inventory_collection
+        delegate :build_stringified_reference, :strategy, :to => :inventory_collection
 
         attr_reader :all_refs, :data_indexes, :inventory_collection, :primary_ref, :local_db_indexes, :secondary_refs
 
-        def stringify_index_value(index_value, ref)
-          # TODO(lsmola) !!!!!!!!!! Important, move this inside of the index. We should be passing around a full hash
-          # index. Then all references should be turned into {stringified_index => full_index} hash. So that way, we can
-          # keep fast indexing using string, but we can use references to write queries autmatically (targeted,
-          # db_based, etc.)
-          # We can also save {stringified_index => full_index}, so we don't have to compute it twice.
-          if index_value.kind_of?(Hash)
-            hash_index_with_keys(named_ref(ref), index_value)
-          else
-            # TODO(lsmola) raise deprecation warning, we want to use only hash indexes
-            index_value
-          end
+        def data_index_find(reference)
+          data_index(reference.ref).find(reference.stringified_reference)
+        end
+
+        def local_db_index_find(reference)
+          local_db_index(reference.ref).find(reference)
+        end
+
+        def primary_index_ref
+          :manager_ref
         end
 
         def data_index(name)
@@ -124,10 +123,6 @@ module ManagerRefresh
 
         def local_db_index(name)
           local_db_indexes[name] || raise("Index #{name} not defined for #{inventory_collection}")
-        end
-
-        def named_ref(ref)
-          all_refs[ref]
         end
 
         def missing_keys(data_keys, ref)
