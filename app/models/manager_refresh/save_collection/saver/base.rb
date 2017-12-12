103473f618117a6cc55e84e0f795678a72bc3c5d
Ladislav Smola <lsmola@redhat.com>
2017-12-12 12:14:00 +0100

    Use reference builder in savers
    Use reference builder in savers

--- app/models/manager_refresh/save_collection/saver/base.rb
+++ app/models/manager_refresh/save_collection/saver/base.rb
@@ -67,6 +67,9 @@ module ManagerRefresh::SaveCollection
         save!(association)
       end
 
+      protected
+      delegate :build_stringified_reference, :build_stringified_reference_for_record, :to => :inventory_collection
+
       private
 
       attr_reader :unique_index_keys, :unique_index_keys_to_s, :select_keys, :unique_db_primary_keys, :unique_db_indexes,
@@ -78,7 +81,7 @@ module ManagerRefresh::SaveCollection
         inventory_objects_index = {}
         inventory_collection.each do |inventory_object|
           attributes = inventory_object.attributes(inventory_collection)
-          index      = inventory_collection.hash_index_with_keys(unique_index_keys, attributes)
+          index      = build_stringified_reference(attributes, unique_index_keys)
 
           attributes_index[index]        = attributes
           inventory_objects_index[index] = inventory_object
@@ -88,7 +91,7 @@ module ManagerRefresh::SaveCollection
         # Records that are in the DB, we will be updating or deleting them.
         ActiveRecord::Base.transaction do
           association.find_each do |record|
-            index = inventory_collection.object_index_with_keys(unique_index_keys, record)
+            index = build_stringified_reference_for_record(record, unique_index_keys)
 
             next unless assert_distinct_relation(record.id)
             next unless assert_unique_record(record, index)
