565916c300446693a9c10c74a0106326aa26fe2b
Ari Zellner <azellner@redhat.com>
2017-12-28 13:40:37 +0200

    shorten to ensure_monitoring_manager

--- app/models/mixins/has_monitoring_manager_mixin.rb
+++ app/models/mixins/has_monitoring_manager_mixin.rb
@@ -1,20 +1,9 @@
 module HasMonitoringManagerMixin
   extend ActiveSupport::Concern
 
-  def ensure_monitoring_manager_with_params
-    if monitoring_manager.nil?
-      build_monitoring_manager(:parent_manager  => self,
-                               :name            => "#{name} Monitoring Manager",
-                               :zone_id         => zone_id,
-                               :provider_region => provider_region)
-    end
-
-    monitoring_manager
-  end
-
   def endpoint_created(role)
     if role == "prometheus_alerts" && monitoring_manager.nil?
-      monitoring_manager = ensure_monitoring_manager_with_params
+      monitoring_manager = ensure_monitoring_manager
       monitoring_manager.save
     end
   end
@@ -24,5 +13,18 @@ module HasMonitoringManagerMixin
       # TODO: if someone deletes the alerts endpoint and then quickly readds it they can end up without a manager.
       monitoring_manager.destroy_queue
     end
+  end
+
+  private
+
+  def ensure_monitoring_manager
+    if monitoring_manager.nil?
+      build_monitoring_manager(:parent_manager  => self,
+                               :name            => "#{name} Monitoring Manager",
+                               :zone_id         => zone_id,
+                               :provider_region => provider_region)
+    end
+
+    monitoring_manager
   end
 end
