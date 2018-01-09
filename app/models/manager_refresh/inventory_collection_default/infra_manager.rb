bf37ca57fca0a572c50d0526e63109e9e3b16eb4
Adam Grare <agrare@redhat.com>
2018-01-09 09:25:45 -0500

    Merge pull request #16767 from pkliczewski/reconnect
    Reconnect host on provider add
    (cherry picked from commit b3c7633ba07f369c6cc514d3b31b9fcbedca222c)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1532332

--- app/models/manager_refresh/inventory_collection_default/infra_manager.rb
+++ app/models/manager_refresh/inventory_collection_default/infra_manager.rb
@@ -140,7 +140,30 @@ class ManagerRefresh::InventoryCollectionDefault::InfraManager < ManagerRefresh:
         :builder_params => {
           :ems_id => ->(persister) { persister.manager.id },
         },
-        :custom_reconnect_block      => reconnect_block
+        :custom_reconnect_block      => lambda do |inventory_collection, inventory_objects_index, attributes_index|
+          relation = inventory_collection.model_class.where(:ems_id => nil)
+
+          return if relation.count <= 0
+
+          inventory_objects_index.each_slice(100) do |batch|
+            relation.where(inventory_collection.manager_ref.first => batch.map(&:first)).each do |record|
+              index = inventory_collection.object_index_with_keys(inventory_collection.manager_ref_to_cols, record)
+
+              # We need to delete the record from the inventory_objects_index and attributes_index, otherwise it
+              # would be sent for create.
+              inventory_object = inventory_objects_index.delete(index)
+              hash             = attributes_index.delete(index)
+
+              record.assign_attributes(hash.except(:id, :type))
+              if !inventory_collection.check_changed? || record.changed?
+                record.save!
+                inventory_collection.store_updated_records(record)
+              end
+
+              inventory_object.id = record.id
+            end
+          end
+        end
       }
 
       attributes.merge!(extra_attributes)
@@ -148,37 +171,33 @@ class ManagerRefresh::InventoryCollectionDefault::InfraManager < ManagerRefresh:
 
     def vms(extra_attributes = {})
       attributes = {
-        :custom_reconnect_block => reconnect_block
+        :custom_reconnect_block => lambda do |inventory_collection, inventory_objects_index, attributes_index|
+          relation = inventory_collection.model_class.where(:ems_id => nil)
+
+          return if relation.count <= 0
+
+          inventory_objects_index.each_slice(100) do |batch|
+            relation.where(inventory_collection.manager_ref.first => batch.map(&:first)).each do |record|
+              index = inventory_collection.object_index_with_keys(inventory_collection.manager_ref_to_cols, record)
+
+              # We need to delete the record from the inventory_objects_index and attributes_index, otherwise it
+              # would be sent for create.
+              inventory_object = inventory_objects_index.delete(index)
+              hash             = attributes_index.delete(index)
+
+              record.assign_attributes(hash.except(:id, :type))
+              if !inventory_collection.check_changed? || record.changed?
+                record.save!
+                inventory_collection.store_updated_records(record)
+              end
+
+              inventory_object.id = record.id
+            end
+          end
+        end
       }
 
       super(attributes.merge!(extra_attributes))
-    end
-
-    def reconnect_block
-      lambda do |inventory_collection, inventory_objects_index, attributes_index|
-        relation = inventory_collection.model_class.where(:ems_id => nil)
-
-        return if relation.count <= 0
-
-        inventory_objects_index.each_slice(100) do |batch|
-          relation.where(inventory_collection.manager_ref.first => batch.map(&:first)).each do |record|
-            index = inventory_collection.object_index_with_keys(inventory_collection.manager_ref_to_cols, record)
-
-            # We need to delete the record from the inventory_objects_index and attributes_index, otherwise it
-            # would be sent for create.
-            inventory_object = inventory_objects_index.delete(index)
-            hash             = attributes_index.delete(index)
-
-            record.assign_attributes(hash.except(:id, :type))
-            if !inventory_collection.check_changed? || record.changed?
-              record.save!
-              inventory_collection.store_updated_records(record)
-            end
-
-            inventory_object.id = record.id
-          end
-        end
-      end
     end
 
     def host_storages(extra_attributes = {})
