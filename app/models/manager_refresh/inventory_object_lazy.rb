9ef761967b8ac86f2a30932e4507023451fa8b27
Ladislav Smola <lsmola@redhat.com>
2018-01-12 12:37:13 +0100

    Add skeletal precreate to lazy_find
    Add skeletal precreate to lazy_find, doing this for each lazy_find
    removes the need for recursive scan, that would be needed in scanner.
    
    Also enhancing condition where skeletal-precreate can apply. Limiting
    it only to targeted parallel safe refresh. Only for collections that
    haven't been saved. And only for lazy_find pointing to primary key.
    
    This can be applied for full refresh, but the skeletal refresh can
    break occurences where we use only build. Since calling build second,
    will not fill missing attributes.

--- app/models/manager_refresh/inventory_object_lazy.rb
+++ app/models/manager_refresh/inventory_object_lazy.rb
@@ -11,6 +11,10 @@ module ManagerRefresh
       @reference            = inventory_collection.build_reference(index_data, ref)
       @key                  = key
       @default              = default
+
+      # We do not support skeletal pre-create for :key, since :key will not be available, we want to use local_db_find
+      # instead.
+      skeletal_precreate! unless @key
     end
 
     # TODO(lsmola) do we need this method?
@@ -63,6 +67,43 @@ module ManagerRefresh
     end
 
     private
+
+    delegate :saved?, :saver_strategy, :targeted?, :to => :inventory_collection
+    delegate :full_reference, :keys, :primary?, :to => :reference
+
+
+    # Instead of loading the reference from the DB, we'll add the skeletal InventoryObject (having manager_ref and
+    # info from the builder_params) to the correct InventoryCollection. Which will either be found in the DB or
+    # created as a skeletal object. The later refresh of the object will then fill the rest of the data, while not
+    # touching the reference.
+    # @return [ManagerRefresh::InventoryObject| Returns pre-created InvetoryObject or nil
+    def skeletal_precreate!
+      # We can do skeletal pre-create only for strategies using unique indexes. Since this can build records out of
+      # the given :arel scope, we will always attempt to create the recod, so we need unique index to avoid duplication
+      # of records.
+      return unless %i(concurrent_safe concurrent_safe_batch).include?(saver_strategy)
+      # Allow skeletal pre-create only for targeted refresh, full refresh should have all edges connected
+      # TODO(lsmola) actually this is not true, e.g. cloud have several full refreshes, where e.g. storage manager
+      # records can have missing edges to cloud manager records. For this to drop, we need build method to call
+      # assign_attributes, or use everywhere find_or_build(hash).assign_attributes(hash)
+      return unless targeted?
+      # Pre-create only for strategies that will be persisting data, i.e. are not saved already
+      return if saved?
+      # We can only do skeletal pre-create for primary index reference, since that is needed to create DB unique index
+      return unless primary?
+      # Full reference must be present
+      return if full_reference.blank?
+
+      # To avoid pre-creating invalid records all fields of a primary key must have value
+      # TODO(lsmola) for composite keys, it's still valid to have one of the keys nil, figure out how to allow this
+      return if keys.any? { |x| full_reference[x].blank? }
+
+      inventory_collection.build(full_reference)
+
+      # TODO(lsmola) what do I need this for? Skeletal record can break :key, since the record won't be fetched by DB
+      # strategy and :key is missing in skeletal record. Write spec!
+      # value_inventory_collection.skeletal_manager_uuids << value.stringified_reference
+    end
 
     def load_object_with_key
       # TODO(lsmola) Log error if we are accessing path that is present in blacklist or not present in whitelist
