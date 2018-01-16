fd6bce26bff4713a472980b367a46a26fcbd37b2
Gregg Tanzillo <gtanzill@redhat.com>
2018-01-16 08:07:26 -0500

    Added container project Metric rollups are separated by timestamp, CustomAttribute (label) and project name (resource_name)
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1531561

--- tools/capture_radar.rb
+++ tools/capture_radar.rb
@@ -12,35 +12,37 @@ class CustomAttribute
 end
 
 labels = CustomAttribute.where(:name => "com.redhat.component")
-# TODO Need to add project to this here
 # Question - If a container is short lived, will we still be able to find it here?
 labels_containers = labels.each_with_object(Hash.new { |h,k| h[k] = [] }) { |l, h| h[l] += l.resource.containers.to_a }
 
 labels_metrics = Hash.new { |h,k| h[k] = [] }
 labels_containers.each do |l, containers|
   containers.each do |c| 
-    labels_metrics[l] += c.metrics.to_a # This should be scoped to time range for the hour
+    labels_metrics[[l, c.container_project]] += c.metrics.to_a # This should be scoped to time range for the hour
   end
 end
 
-# labels_metrics.each {|l,m| puts "#{l} => #{m.map(&:resource_id).inspect}"}
-
 labels_maxes = Hash.new { |h,k| h[k] = {} }
-labels_metrics.each do |label, metrics|
+labels_metrics.each do |label_project, metrics|
   metrics.each do |m|
     hour_ts = m.timestamp.beginning_of_hour
     cpu_cores = m.derived_cpu_total_cores_used
 
-    labels_maxes[label][hour_ts] ||= { :cpu_usage_rate_average => 0 }
-    labels_maxes[label][hour_ts][:cpu_usage_rate_average] = cpu_cores if cpu_cores > labels_maxes[label][hour_ts][:cpu_usage_rate_average]
+    labels_maxes[label_project][hour_ts] ||= { :cpu_usage_rate_average => 0 }
+    labels_maxes[label_project][hour_ts][:cpu_usage_rate_average] = cpu_cores if cpu_cores > labels_maxes[label_project][hour_ts][:cpu_usage_rate_average]
   end
 end
 
-labels_maxes.each do |label, hours|
+labels_maxes.each do |label_project, hours|
+  label, project = label_project
   hours.each do |hour_ts, values|
-    mr = label.metric_rollups.find_or_initialize_by(:timestamp => hour_ts, :capture_interval_name => "hourly")
+    mr = label.metric_rollups.find_or_initialize_by(
+      :timestamp => hour_ts,
+      :resource_name => project.name,
+      :capture_interval_name => "hourly"
+    )
+
     mr.update_attributes(
-      :resource_name          => label.value,
       :cpu_usage_rate_average => values[:cpu_usage_rate_average]
     )
   end
