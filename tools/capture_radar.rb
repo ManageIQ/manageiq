2ab8e58f92f107b606be4ff68bab5f56d32465c8
lpichler <lpichler@redhat.com>
2018-01-17 11:44:06 +0100

    Control scope of container's metric according to input time range

--- tools/capture_radar.rb
+++ tools/capture_radar.rb
@@ -11,14 +11,17 @@ class CustomAttribute
   has_many   :metric_rollups, :as => :resource
 end
 
+TIME_RANGE = [(Time.now - 3.month).beginning_of_hour..Time.now.beginning_of_hour]
+
 labels = CustomAttribute.where(:name => "com.redhat.component")
+
 # Question - If a container is short lived, will we still be able to find it here?
 labels_containers = labels.each_with_object(Hash.new { |h,k| h[k] = [] }) { |l, h| h[l] += l.resource.containers.to_a }
 
 labels_metrics = Hash.new { |h,k| h[k] = [] }
 labels_containers.each do |l, containers|
-  containers.each do |c| 
-    labels_metrics[[l, c.container_project]] += c.metrics.to_a # This should be scoped to time range for the hour
+  containers.each do |c|
+    labels_metrics[[l, c.container_project]] += c.metrics.where(:timestamp => TIME_RANGE).to_a # This should be scoped to time range for the hour
   end
 end
 
