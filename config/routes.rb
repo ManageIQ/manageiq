48f21b4fdf6cabe5e59fbb371874da058dbc6187
Tim Wade <hello@timjwade.com>
2018-01-17 15:34:45 -0800

    Require graphiql-rails gem in development
    This needs to be required here because of the peculiarities of how
    engines hook into the rails application. Essentially, requiring an
    engine within an engine will cause this to fail - although the library
    is loaded correctly, the app code never gets loaded causing a 404
    error when performing a GET to /graphql/explorer.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Vmdb::Application.routes.draw do
     logger.level = Logger.const_get(::Settings.log.level_websocket.upcase)
     mount WebsocketServer.new(:logger => logger) => '/ws'
   end
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, :at => "/graphql/explorer", :graphql_path => "/graphql"
+  end
 end
