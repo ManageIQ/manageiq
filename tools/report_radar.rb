bc37c9114333bbfc98c29c94a420590216a9af63
lpichler <lpichler@redhat.com>
2018-01-18 14:00:30 +0100

    Add control for absolute time range to console parameters

--- tools/report_radar.rb
+++ tools/report_radar.rb
@@ -3,12 +3,43 @@ require File.expand_path("../config/environment", __dir__)
 require 'trollop'
 
 opts = Trollop.options(ARGV) do
-  banner "USAGE:  #{__FILE__} -h <number of days back to query metrics>\n"
+  banner "USAGE:  #{__FILE__} -d <number of days back to query metrics, default is 1 day>\n" \
+         "        or                                                                     \n" \
+         "        #{__FILE__} -s <start date to query metrics, required>                 \n" \
+         "        #{__FILE__} -e <end date to query metrics, default is current date>    \n" \
 
   opt :days, "Days", :short => "d", :type => :int, :default => 1
+  opt :start, "Start Date", :short => "s", :type => :int
+  opt :end, "End Date", :short => "e", :type => :int
 end
 
-time_range = [opts[:days].days.ago.utc.beginning_of_hour..Time.now.utc.end_of_hour]
+def absolute_time_range(start_date, end_date)
+  Trollop.die "Start date is missing" if end_date && !start_date
+
+  begin
+    end_date = if end_date
+                 Date.strptime(end_date.to_s, "%Y%m%d").end_of_day.utc
+               else
+                 Time.now.end_of_day.utc
+               end
+
+    start_date = Date.strptime(start_date.to_s, "%Y%m%d").beginning_of_day.utc
+  rescue ArgumentError
+    Trollop.die "Cannot parse any of input date"
+  end
+
+  if start_date > end_date
+    Trollop.die "Start date cannot be greater then end date"
+  end
+
+  [start_date..end_date]
+end
+
+time_range = if opts[:start] || opts[:end]
+               absolute_time_range(opts[:start], opts[:end])
+             else
+               [opts[:days].days.ago.utc.beginning_of_hour..Time.now.utc.end_of_hour]
+             end
 
 OUTPUT_CSV_FILE_PATH = 'cores_usage_per_label.csv'.freeze
 
