4b0c50b4b3fb49b4cbf467b7f4c5f4b77dd93e85
lpichler <lpichler@redhat.com>
2018-01-18 15:59:39 +0100

    WIP: use sql

--- tools/capture_radar.rb
+++ tools/capture_radar.rb
@@ -20,6 +20,9 @@ class Metric
   end
 end
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+
 CustomAttribute
 class CustomAttribute
   has_many   :metric_rollups, :as => :resource
@@ -63,9 +66,9 @@ labels_maxes.each do |label_project, hours|
   label, project = label_project
   hours.each do |hour_ts, values|
     mr = label.metric_rollups.find_or_create_by(
-      :timestamp => hour_ts,
-      :resource_name => project.name,
-      :capture_interval_name => "hourly"
+        :timestamp => hour_ts,
+        :resource_name => project.name,
+        :capture_interval_name => "hourly"
     )
 
     mr.update_attributes(
@@ -73,3 +76,50 @@ labels_maxes.each do |label_project, hours|
     ) if mr.cpu_usage_rate_average < values[:cpu_usage_rate_average]
   end
 end
+
+# DIFFERENT STRATEGY: USING SQL
+
+def quote(value)
+  ActiveRecord::Base.connection.quote(value)
+end
+
+def get_hourly_maxes_per_group(label_name)
+  sums_query = Metric.where(:resource_type => "Container")
+                   .where(:timestamp => TIME_RANGE)
+                   .joins("INNER JOIN containers ON metrics.resource_type = 'Container' AND metrics.resource_id = containers.id")
+                   .joins("INNER JOIN container_groups ON container_groups.id = containers.container_group_id")
+                   .joins("INNER JOIN container_images ON container_images.id = containers.container_image_id")
+                   .joins("INNER JOIN custom_attributes ON custom_attributes.name = #{quote(label_name)} AND "\
+                    "custom_attributes.resource_type = 'ContainerImage' AND custom_attributes.resource_id = container_images.id")
+                   .select("custom_attributes.id as label_id,"\
+                    "container_groups.container_project_id as container_project_id, metrics.timestamp as timestamp, "\
+                    "sum((metrics.cpu_usage_rate_average * metrics.derived_vm_numvcpus) / 100.0) AS sum_used_cores, "\
+                    "count(*) AS containers_in_group")
+                   .group("custom_attributes.id, container_groups.container_project_id, metrics.timestamp")
+                   .order("container_groups.container_project_id")
+
+  maxes_query = <<-SQL
+    WITH sums AS (
+      #{sums_query.to_sql}
+    )
+    SELECT sums.label_id, container_projects.name as container_project_name, date_trunc('hour', sums.timestamp) as hourly_timestamp, max(sums.sum_used_cores) as max_sum_used_cores
+      FROM sums
+        INNER JOIN container_projects ON container_projects.id = sums.container_project_id
+        GROUP BY sums.label_id, container_projects.name, date_trunc('hour', sums.timestamp)
+  SQL
+
+  ActiveRecord::Base.connection.execute(maxes_query).to_a
+end
+
+results = get_hourly_maxes_per_group('com.redhat.component').sort_by{ |x| [x['hourly_timestamp'], x['label_id'], x['container_project_name']] }
+
+# convert to same structure with same data structure
+vintage_maxes = labels_maxes.each_with_object([]) do |(label_project, hours), obj|
+  label, project = label_project
+  hours.each do |hour_ts, values|
+    obj << {"label_id"               => label.id,
+            "container_project_name" => project.name,
+            "hourly_timestamp"       => hour_ts.to_s,
+            "max_sum_used_cores"     => values[:cpu_usage_rate_average]}
+  end
+end.sort_by{ |x| [x['hourly_timestamp'], x['label_id'], x['container_project_name']] }
