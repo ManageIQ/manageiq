1306f2719f5fface3f9d8885b26f6b29155afbb3
Chris Arcand <chris@chrisarcand.com>
2018-01-18 20:10:13 -0600

    Attempt to fix regions spec

--- spec/requests/api/regions_spec.rb
+++ spec/requests/api/regions_spec.rb
@@ -48,12 +48,18 @@ describe "Regions API" do
     let(:zone) { FactoryGirl.create(:zone, :id => id) }
     let!(:server) { EvmSpecHelper.remote_miq_server(:id => id, :zone => zone) }
     let(:original_timeout) { region.settings_for_resource[:api][:authentication_timeout] }
-    let(:super_admin) { FactoryGirl.create(:user, :role => 'super_administrator', :userid => 'alice', :password => 'alicepassword') }
+    let(:user) { FactoryGirl.create(:user, :role => role_name, :userid => 'alice', :password => 'alicepassword') }
+    let(:role_name) { 'user' }
+
+    def define_user
+      @user = user
+      @role = user.miq_groups.first.miq_user_role
+    end
 
     it "shows the settings to an authenticated user with the proper role" do
       api_basic_authorize(:ops_settings)
 
-      get(api_region_settings_url(nil, region))
+      run_get(api_region_settings_url(nil, region))
 
       expect(response).to have_http_status(:ok)
     end
@@ -61,33 +67,37 @@ describe "Regions API" do
     it "does not allow an authenticated user who doesn't have the proper role to view the settings" do
       api_basic_authorize
 
-      get(api_region_settings_url(nil, region))
+      run_get(api_region_settings_url(nil, region))
 
       expect(response).to have_http_status(:forbidden)
     end
 
     it "does not allow an unauthenticated user to view the settings" do
-      get(api_region_settings_url(nil, region))
+      run_get(api_region_settings_url(nil, region))
 
       expect(response).to have_http_status(:unauthorized)
     end
 
-    it "permits updates to settings for an authenticated super-admin user" do
-      api_basic_authorize(:user => super_admin.userid, :password => super_admin.password)
+    context "as an authenticated super-admin user" do
+      let(:role_name) { 'super_administrator' }
 
-      expect {
-        patch(api_region_settings_url(nil, region), :params => {:api => {:authentication_timeout => "1337.minutes"}})
-      }.to change { region.settings_for_resource[:api][:authentication_timeout] }.from(original_timeout).to("1337.minutes")
+      it "permits updates to settings" do
+        api_basic_authorize
 
-      expect(response.parsed_body).to include("api" => a_hash_including("authentication_timeout" => "1337.minutes"))
-      expect(response).to have_http_status(:ok)
+        expect {
+          run_patch(api_region_settings_url(nil, region), :params => {:api => {:authentication_timeout => "1337.minutes"}})
+        }.to change { region.settings_for_resource[:api][:authentication_timeout] }.from(original_timeout).to("1337.minutes")
+
+        expect(response.parsed_body).to include("api" => a_hash_including("authentication_timeout" => "1337.minutes"))
+        expect(response).to have_http_status(:ok)
+      end
     end
 
     it "does not allow an authenticated non-super-admin user to update settings" do
       api_basic_authorize
 
       expect {
-        patch(api_region_settings_url(nil, region), :params => {:api => {:authentication_timeout => "10.minutes"}})
+        run_patch(api_region_settings_url(nil, region), :params => {:api => {:authentication_timeout => "10.minutes"}})
       }.not_to change { region.settings_for_resource[:api][:authentication_timeout] }
 
       expect(response).to have_http_status(:forbidden)
@@ -95,7 +105,7 @@ describe "Regions API" do
 
     it "does not allow an unauthenticated user to update the settings" do
       expect {
-        patch(api_region_settings_url(nil, region), :params => {:api => {:authentication_timeout => "10.minutes"}})
+        run_patch(api_region_settings_url(nil, region), :params => {:api => {:authentication_timeout => "10.minutes"}})
       }.not_to change { region.settings_for_resource[:api][:authentication_timeout] }
 
       expect(response).to have_http_status(:unauthorized)
@@ -106,18 +116,22 @@ describe "Regions API" do
         region.add_settings_for_resource("api" => {"authentication_timeout" => "7331.minutes"})
       end
 
-      it "allows an authenticated super-admin user to delete settings" do
-        api_basic_authorize(:user => super_admin.userid, :password => super_admin.password)
-        expect(region.settings_for_resource["api"]["authentication_timeout"]).to eq("7331.minutes")
+      context "as an authenticated super-admin" do
+        let(:role_name) { 'super_administrator' }
 
-        expect {
-          delete(
-            api_region_settings_url(nil, region),
-            :params => %i[api authentication_timeout].to_json # => hack because Rails will interpret these as query params in a DELETE
-          )
-        }.to change { region.settings_for_resource["api"]["authentication_timeout"] }.from("7331.minutes").to("30.seconds")
+        it "allows user to delete settings" do
+          api_basic_authorize
+          expect(region.settings_for_resource["api"]["authentication_timeout"]).to eq("7331.minutes")
 
-        expect(response).to have_http_status(:no_content)
+          expect {
+            delete(
+              api_region_settings_url(nil, region),
+              :params => %i[api authentication_timeout].to_json # => hack because Rails will interpret these as query params in a DELETE
+            )
+          }.to change { region.settings_for_resource["api"]["authentication_timeout"] }.from("7331.minutes").to("30.seconds")
+
+          expect(response).to have_http_status(:no_content)
+        end
       end
 
       it "does not allow an authenticated non-super-admin user to delete settings" do
