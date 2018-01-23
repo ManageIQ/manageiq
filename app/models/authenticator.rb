58d6bd28bd5b857cb1b9551fdeff46e414d58eac
Brandon Dunne <brandondunne@hotmail.com>
2018-01-23 14:46:41 -0500

    Merge pull request #16869 from Fryguy/refactor_authenticator_to_remove_require_nested
    Refactor Authenticator to remove const manipulations
    (cherry picked from commit 61e7ea95d8f1b508720a64c3d045aa98790fb836)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1537788

--- app/models/authenticator.rb
+++ app/models/authenticator.rb
@@ -1,35 +1,38 @@
 module Authenticator
-  require_nested :Base
-  require_nested :Database
-  require_nested :Httpd
-  require_nested :Ldap
-
   def self.for(config, username = nil)
     if username == 'admin'
       Database.new(config)
     else
-      authenticator_class(config[:mode]).new(config)
+      authenticator_class(config[:mode])&.new(config)
     end
   end
 
-  def self.authenticator_class(name)
+  def self.validate_config(config)
+    return [] if config[:mode] == "none"
+
+    authenticator = self.for(config)
+    if authenticator
+      authenticator.validate_config
+    else
+      [:mode, "authentication type, #{config[:mode].inspect}, invalid. Should be one of: #{valid_modes.join(", ")}"]
+    end
+  end
+
+  private_class_method def self.valid_modes
+    Base.subclasses.flat_map(&:authenticates_for).uniq << "none"
+  end
+
+  private_class_method def self.authenticator_class(name)
     authenticator_classes_cache[name.to_s]
   end
 
-  def self.authenticator_classes_cache
-    @authenticator_classes_cache ||= Concurrent::Hash.new do |hash, name|
-      hash[name] = authenticator_class_for(name) || force_load_authenticator_for(name)
+  private_class_method def self.authenticator_classes_cache
+    @authenticator_classes_cache ||= Hash.new do |hash, name|
+      hash[name] = authenticator_class_for(name)
     end
   end
 
-  def self.force_load_authenticator_for(name)
-    # try to constantize in case the subclass is not loaded yet, but it still exists
-    # this is happens with authenticators from provider gems
-    subclass = "Authenticator::#{name.camelize}".safe_constantize
-    subclass if subclass && subclass.authenticates_for.include?(name)
-  end
-
-  def self.authenticator_class_for(name)
+  private_class_method def self.authenticator_class_for(name)
     Base.subclasses.find do |subclass|
       subclass.authenticates_for.include?(name)
     end
