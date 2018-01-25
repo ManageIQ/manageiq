7ac758e4fd70b68832d5848f08575f2dfbaea0fc
Brandon Dunne <brandondunne@hotmail.com>
2018-01-25 15:54:05 -0500

    Return early to remove "unless" conditional

--- app/models/server_role.rb
+++ app/models/server_role.rb
@@ -34,13 +34,9 @@ class ServerRole < ApplicationRecord
 
   def self.to_role(server_role)
     # server_role can either be a Role Name (string or symbol) or an instance of a ServerRole
-    unless server_role.kind_of?(ServerRole)
-      role_name   = server_role.to_s.strip.downcase
-      server_role = ServerRole.find_by(:name => role_name)
-      raise _("Role <%{name}> not defined in server_roles table") % {:name => role_name} if server_role.nil?
-    end
-
-    server_role
+    return server_role if server_role.kind_of?(ServerRole)
+    role_name = server_role.to_s.strip.downcase
+    ServerRole.find_by(:name => role_name) || raise(_("Role <%{name}> not defined in server_roles table") % {:name => role_name})
   end
 
   def self.all_names
