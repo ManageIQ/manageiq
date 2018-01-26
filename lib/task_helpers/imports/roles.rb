56013d80443ddfc4d2314ac7d0d1e31d5b1bc263
Brant Evans <bevans@redhat.com>
2018-01-26 15:46:15 -0700

    Only call MiqProductFeatures once

--- lib/task_helpers/imports/roles.rb
+++ lib/task_helpers/imports/roles.rb
@@ -4,11 +4,13 @@ module TaskHelpers
       def import(options = {})
         return unless options[:source]
 
+        available_features = MiqProductFeature.all
+
         glob = File.file?(options[:source]) ? options[:source] : "#{options[:source]}/*.yaml"
         Dir.glob(glob) do |fname|
           begin
             roles = YAML.load_file(fname)
-            import_roles(roles)
+            import_roles(roles, available_features)
           rescue ActiveRecord::RecordInvalid => e
             warn("Error importing #{fname} : #{e.message}")
           end
@@ -17,9 +19,9 @@ module TaskHelpers
 
       private
 
-      def import_roles(roles)
+      def import_roles(roles, available_features)
         roles.each do |r|
-          r['miq_product_feature_ids'] = MiqProductFeature.all.collect do |f|
+          r['miq_product_feature_ids'] = available_features.collect do |f|
             f.id if r['feature_identifiers']&.include?(f.identifier)
           end.compact
           role = MiqUserRole.find_or_create_by(:name => r['name'])
