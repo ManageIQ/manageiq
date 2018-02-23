074b66c8b1e92c71267a230512245a6aeb1badbf
Ron Valente <rawn027@gmail.com>
2018-02-23 06:24:10 -0500

    Don't break configuration if advanced config requires an int.

--- tools/configure_server_settings.rb
+++ tools/configure_server_settings.rb
@@ -4,19 +4,26 @@ require 'trollop'
 
 opts = Trollop.options(ARGV) do
   banner "USAGE:   #{__FILE__} -s <server id> -p <settings path separated by a /> -v <new value>\n" \
-         "Example: #{__FILE__} -d -s 1 -p reporting/history/keep_reports -v 42"
+         "Example: #{__FILE__} -d -s 1 -p reporting/history/keep_reports -v 42\n" \
+         "Example: #{__FILE__} -s 1 -p workers/worker_base/queue_worker_base/ems_metrics_collector_worker/defaults/count -v 4 -i"
 
   opt :dry_run,  "Dry Run",                            :short => "d"
-  opt :serverid, "Server Id",                          :short => "s", :default => 0
-  opt :path,     "Path within advanced settings hash", :short => "p", :default => ""
-  opt :value,    "New value for setting",              :short => "v", :default => ""
+  opt :serverid, "Server Id",                          :short => "s", :type => :integer, :required => true
+  opt :path,     "Path within advanced settings hash", :short => "p", :type => :string, :required => true
+  opt :value,    "New Value for setting",              :short => "v", :type => :string, :required => true
+  opt :integer,  "Value Provided is an Integer",       :short => "i", :type => :boolean, :default => false
+
+  depends :path, :serverid, :value
 end
 
 puts opts.inspect
 
-Trollop.die :serverid, "is required" unless opts[:serverid_given]
-Trollop.die :path,     "is required" unless opts[:path_given]
-Trollop.die :value,    "is required" unless opts[:value_given]
+# Grab the value that we have set
+if opts[:integer]
+  newval = opts[:value].to_i
+else
+  newval = opts[:value]
+end
 
 server = MiqServer.where(:id => opts[:serverid]).take
 unless server
@@ -32,7 +39,7 @@ key = keys.pop.to_sym
 keys.each { |p| path = path[p.to_sym] }
 
 puts "Setting [#{opts[:path]}], old value: [#{path[key]}], new value: [#{opts[:value]}]"
-path[key] = opts[:value]
+path[key] = newval
 
 valid, errors = VMDB::Config::Validator.new(settings).validate
 unless valid
