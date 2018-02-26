4097b5c8d6cf5ef70af538dcdd125777c75f41d5
Keenan Brock <keenan@thebrocks.net>
2018-02-26 12:38:52 -0500

    Allow individual tables to be specified for metrics
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1553473

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -2,4 +2,12 @@ class Metric < ApplicationRecord
   BASE_COLS = ["id", "timestamp", "capture_interval_name", "resource_type", "resource_id", "resource_name", "tag_names", "parent_host_id", "parent_ems_cluster_id", "parent_ems_id", "parent_storage_id"]
 
   include Metric::Common
+
+  # @param time [ActiveSupport::TimeWithZone, Time, Integer, nil] the hour to run (default: 1 hour from now)
+  # @return the table for the given hour
+  # Unfortunatly, Integer responds_to :hour, so :strftime was used instead.
+  def self.reindex_table_name(time = Time.now.utc.hour + 1)
+    hour = (time.respond_to?(:strftime) ? time.hour : time) % 24
+    "metrics_%02d" % hour
+  end
 end
