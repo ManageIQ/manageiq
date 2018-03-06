44061279aba9af2d1804dc0558e24f1a57b0ffcb
Drew Bomhof <dbomhof@redhat.com>
2018-03-06 11:30:44 -0500

    Added Embedded Ansible Content plugin

---
+++ lib/vmdb/plugins/ansible_content.rb
@@ -0,0 +1,15 @@
+module Vmdb
+  class Plugins
+    class AnsibleContent
+      attr_reader :datastores_path
+      attr_reader :name
+      attr_reader :path
+
+      def initialize(path)
+        raise "#{path} does not exist" unless File.directory?(path)
+        @roles_path = Pathname.new(path)
+        @path = @roles_path.split.first
+      end
+    end
+  end
+end
