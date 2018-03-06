4b48ef45075e71cca4ce2eecc72405075ac75bce
Tim Wade <hello@timjwade.com>
2018-03-06 10:41:12 -0800

    Remove graphiql-rails gem
    In favor of https://github.com/ManageIQ/manageiq-ui-classic/pull/3357
    and https://github.com/ManageIQ/manageiq-graphql/pull/44

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,4 @@ Vmdb::Application.routes.draw do
     logger.level = Logger.const_get(::Settings.log.level_websocket.upcase)
     mount WebsocketServer.new(:logger => logger) => '/ws'
   end
-
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, :at => "/graphql/explorer", :graphql_path => "/graphql"
-  end
 end
