6290bc9e12a5c59df24a72b348b0e9201bd7cefe
CZJanRichter <cz.jan.richter@gmail.com>
2018-03-08 11:23:44 +0100

    miq_task before each to let transform

--- spec/models/miq_task_spec.rb
+++ spec/models/miq_task_spec.rb
@@ -1,181 +1,179 @@
 describe MiqTask do
   context "when I add an MiqTask" do
-    before(:each) do
-      @miq_task = FactoryGirl.create(:miq_task_plain)
-    end
+    let (:miq_task) { FactoryGirl.create(:miq_task_plain) }
 
     it "should initialize properly" do
-      expect(@miq_task.state).to eq(MiqTask::STATE_INITIALIZED)
-      expect(@miq_task.status).to eq(MiqTask::STATUS_OK)
-      expect(@miq_task.message).to eq(MiqTask::DEFAULT_MESSAGE)
-      expect(@miq_task.userid).to eq(MiqTask::DEFAULT_USERID)
+      expect(miq_task.state).to eq(MiqTask::STATE_INITIALIZED)
+      expect(miq_task.status).to eq(MiqTask::STATUS_OK)
+      expect(miq_task.message).to eq(MiqTask::DEFAULT_MESSAGE)
+      expect(miq_task.userid).to eq(MiqTask::DEFAULT_USERID)
     end
 
     it "should respond to update_status class method properly" do
       state   = MiqTask::STATE_QUEUED
       status  = MiqTask::STATUS_OK
       message = 'This is only a class test'
-      MiqTask.update_status(@miq_task.id, state, status, message)
-      @miq_task.reload
-      expect(@miq_task.state).to eq(state)
-      expect(@miq_task.status).to eq(status)
-      expect(@miq_task.message).to eq(message)
+      MiqTask.update_status(miq_task.id, state, status, message)
+      miq_task.reload
+      expect(miq_task.state).to eq(state)
+      expect(miq_task.status).to eq(status)
+      expect(miq_task.message).to eq(message)
     end
 
     it "should respond to update_status instance method properly" do
       state   = MiqTask::STATE_QUEUED
       status  = MiqTask::STATUS_OK
       message = 'This is only a test'
-      @miq_task.update_status(state, status, message)
-      expect(@miq_task.state).to eq(state)
-      expect(@miq_task.status).to eq(status)
-      expect(@miq_task.message).to eq(message)
+      miq_task.update_status(state, status, message)
+      expect(miq_task.state).to eq(state)
+      expect(miq_task.status).to eq(status)
+      expect(miq_task.message).to eq(message)
     end
 
     it "should update context upon request" do
       context = {:a => 1, :b => 2}
-      @miq_task.update_context(context)
-      expect(@miq_task.context_data).to eq(context)
+      miq_task.update_context(context)
+      expect(miq_task.context_data).to eq(context)
     end
 
     it "should respond to info instance method properly" do
       message      = "Hello World"
       pct_complete = 19
-      @miq_task.info(message, pct_complete)
-      expect(@miq_task.message).to eq(message)
-      expect(@miq_task.pct_complete).to eq(pct_complete)
+      miq_task.info(message, pct_complete)
+      expect(miq_task.message).to eq(message)
+      expect(miq_task.pct_complete).to eq(pct_complete)
     end
 
     it "should respond to info class method properly" do
       message      = "Goodbye World"
       pct_complete = 29
-      MiqTask.info(@miq_task.id, message, pct_complete)
-      @miq_task.reload
-      expect(@miq_task.message).to eq(message)
-      expect(@miq_task.pct_complete).to eq(pct_complete)
+      MiqTask.info(miq_task.id, message, pct_complete)
+      miq_task.reload
+      expect(miq_task.message).to eq(message)
+      expect(miq_task.pct_complete).to eq(pct_complete)
     end
 
     it "should respond to warn instance method properly" do
       message      = "There may be a fire on your floor"
-      @miq_task.warn(message)
-      expect(@miq_task.message).to eq(message)
-      expect(@miq_task.status).to eq(MiqTask::STATUS_WARNING)
+      miq_task.warn(message)
+      expect(miq_task.message).to eq(message)
+      expect(miq_task.status).to eq(MiqTask::STATUS_WARNING)
     end
 
     it "should respond to warn class method properly" do
       message      = "There may be a fire on your floor (class)"
-      MiqTask.warn(@miq_task.id, message)
-      @miq_task.reload
-      expect(@miq_task.message).to eq(message)
-      expect(@miq_task.status).to eq(MiqTask::STATUS_WARNING)
+      MiqTask.warn(miq_task.id, message)
+      miq_task.reload
+      expect(miq_task.message).to eq(message)
+      expect(miq_task.status).to eq(MiqTask::STATUS_WARNING)
     end
 
     it "should respond to error instance method properly" do
       message      = "Red Alert"
-      @miq_task.error(message)
-      expect(@miq_task.message).to eq(message)
-      expect(@miq_task.status).to eq(MiqTask::STATUS_ERROR)
+      miq_task.error(message)
+      expect(miq_task.message).to eq(message)
+      expect(miq_task.status).to eq(MiqTask::STATUS_ERROR)
     end
 
     it "should respond to error class method properly" do
       message      = "Red Alert (class)"
-      MiqTask.error(@miq_task.id, message)
-      @miq_task.reload
-      expect(@miq_task.message).to eq(message)
-      expect(@miq_task.status).to eq(MiqTask::STATUS_ERROR)
+      MiqTask.error(miq_task.id, message)
+      miq_task.reload
+      expect(miq_task.message).to eq(message)
+      expect(miq_task.status).to eq(MiqTask::STATUS_ERROR)
     end
 
     it "should respond to state_initialized instance method properly" do
-      @miq_task.state_initialized
-      expect(@miq_task.state).to eq(MiqTask::STATE_INITIALIZED)
+      miq_task.state_initialized
+      expect(miq_task.state).to eq(MiqTask::STATE_INITIALIZED)
     end
 
     it "should respond to state_initialized class method properly" do
-      MiqTask.state_initialized(@miq_task.id)
-      @miq_task.reload
-      expect(@miq_task.state).to eq(MiqTask::STATE_INITIALIZED)
+      MiqTask.state_initialized(miq_task.id)
+      miq_task.reload
+      expect(miq_task.state).to eq(MiqTask::STATE_INITIALIZED)
     end
 
     it "should respond to state_queued instance method properly" do
-      @miq_task.state_queued
-      expect(@miq_task.state).to eq(MiqTask::STATE_QUEUED)
+      miq_task.state_queued
+      expect(miq_task.state).to eq(MiqTask::STATE_QUEUED)
     end
 
     it "should respond to state_queued class method properly" do
-      MiqTask.state_queued(@miq_task.id)
-      @miq_task.reload
-      expect(@miq_task.state).to eq(MiqTask::STATE_QUEUED)
+      MiqTask.state_queued(miq_task.id)
+      miq_task.reload
+      expect(miq_task.state).to eq(MiqTask::STATE_QUEUED)
     end
 
     it "should respond to state_active instance method properly" do
-      @miq_task.state_active
-      expect(@miq_task.state).to eq(MiqTask::STATE_ACTIVE)
+      miq_task.state_active
+      expect(miq_task.state).to eq(MiqTask::STATE_ACTIVE)
     end
 
     it "should respond to state_active class method properly" do
-      MiqTask.state_active(@miq_task.id)
-      @miq_task.reload
-      expect(@miq_task.state).to eq(MiqTask::STATE_ACTIVE)
+      MiqTask.state_active(miq_task.id)
+      miq_task.reload
+      expect(miq_task.state).to eq(MiqTask::STATE_ACTIVE)
     end
 
     it "should respond to state_finished instance method properly" do
-      @miq_task.state_finished
-      expect(@miq_task.state).to eq(MiqTask::STATE_FINISHED)
+      miq_task.state_finished
+      expect(miq_task.state).to eq(MiqTask::STATE_FINISHED)
     end
 
     it "should respond to state_finished class method properly" do
-      MiqTask.state_finished(@miq_task.id)
-      @miq_task.reload
-      expect(@miq_task.state).to eq(MiqTask::STATE_FINISHED)
+      MiqTask.state_finished(miq_task.id)
+      miq_task.reload
+      expect(miq_task.state).to eq(MiqTask::STATE_FINISHED)
     end
 
     it "should get/set task_results properly" do
       results = {:a => 1, :b => 2}
-      @miq_task.task_results = results
-      @miq_task.save
-      expect(@miq_task.task_results).to eq(results)
+      miq_task.task_results = results
+      miq_task.save
+      expect(miq_task.task_results).to eq(results)
     end
 
     it "should queue callback properly" do
       state   = MiqTask::STATE_QUEUED
       message = 'Message for testing: queue_callback'
       result  = {:a => 1, :b => 2}
-      @miq_task.queue_callback(state, 'ok', message, result)
-      expect(@miq_task.state).to eq(state)
-      expect(@miq_task.status).to eq(MiqTask::STATUS_OK)
-      expect(@miq_task.message).to eq(MiqTask::MESSAGE_TASK_COMPLETED_SUCCESSFULLY)
-      expect(@miq_task.task_results).to eq(result)
+      miq_task.queue_callback(state, 'ok', message, result)
+      expect(miq_task.state).to eq(state)
+      expect(miq_task.status).to eq(MiqTask::STATUS_OK)
+      expect(miq_task.message).to eq(MiqTask::MESSAGE_TASK_COMPLETED_SUCCESSFULLY)
+      expect(miq_task.task_results).to eq(result)
 
       status  = MiqTask::STATUS_ERROR
-      @miq_task.queue_callback(state, status, "", result)
-      expect(@miq_task.state).to eq(state)
-      expect(@miq_task.status).to eq(status)
-      expect(@miq_task.message).to eq(MiqTask::MESSAGE_TASK_COMPLETED_UNSUCCESSFULLY)
-      expect(@miq_task.task_results).to eq(result)
+      miq_task.queue_callback(state, status, "", result)
+      expect(miq_task.state).to eq(state)
+      expect(miq_task.status).to eq(status)
+      expect(miq_task.message).to eq(MiqTask::MESSAGE_TASK_COMPLETED_UNSUCCESSFULLY)
+      expect(miq_task.task_results).to eq(result)
 
       result  = {:c => 1, :d => 2}
-      @miq_task.queue_callback(state, status, message, result)
-      expect(@miq_task.state).to eq(state)
-      expect(@miq_task.status).to eq(status)
-      expect(@miq_task.message).to eq(message)
-      expect(@miq_task.task_results).to eq(result)
+      miq_task.queue_callback(state, status, message, result)
+      expect(miq_task.state).to eq(state)
+      expect(miq_task.status).to eq(status)
+      expect(miq_task.message).to eq(message)
+      expect(miq_task.task_results).to eq(result)
     end
 
     it "should queue callback on exceptions properly" do
       state   = MiqTask::STATE_QUEUED
       message = 'Message for testing: queue_callback_on_exceptions'
       result  = {:a => 1, :b => 2}
-      @miq_task.queue_callback_on_exceptions(state, 'ok', message, result)
-      expect(@miq_task.state).to eq(MiqTask::STATE_INITIALIZED)
-      expect(@miq_task.status).to eq(MiqTask::STATUS_OK)
-      expect(@miq_task.message).to eq(MiqTask::DEFAULT_MESSAGE)
-      expect(@miq_task.task_results).to be_nil
+      miq_task.queue_callback_on_exceptions(state, 'ok', message, result)
+      expect(miq_task.state).to eq(MiqTask::STATE_INITIALIZED)
+      expect(miq_task.status).to eq(MiqTask::STATUS_OK)
+      expect(miq_task.message).to eq(MiqTask::DEFAULT_MESSAGE)
+      expect(miq_task.task_results).to be_nil
 
-      @miq_task.queue_callback_on_exceptions(state, "MAYDAY", message, result)
-      expect(@miq_task.state).to eq(state)
-      expect(@miq_task.status).to eq(MiqTask::STATUS_ERROR)
-      expect(@miq_task.message).to eq(message)
-      expect(@miq_task.task_results).to eq(result)
+      miq_task.queue_callback_on_exceptions(state, "MAYDAY", message, result)
+      expect(miq_task.state).to eq(state)
+      expect(miq_task.status).to eq(MiqTask::STATUS_ERROR)
+      expect(miq_task.message).to eq(message)
+      expect(miq_task.task_results).to eq(result)
     end
 
     it "should properly process MiqTask#generic_action_with_callback" do
