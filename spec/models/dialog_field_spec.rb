b3b3f92c11e186174ab2575c48791b2dcb53e42d
swi2 <swi2@evo07.mendelu.cz>
2018-03-08 12:10:55 +0100

    repaired at let

--- spec/models/dialog_field_spec.rb
+++ spec/models/dialog_field_spec.rb
@@ -1,12 +1,11 @@
 describe DialogField do
   context "legacy tests" do
-    before(:each) do
-      @df = FactoryGirl.create(:dialog_field)
-    end
 
+    let(:df) { FactoryGirl.create(:dialog_field)}
+		    
     it "sets default value for required attribute" do
-      expect(@df.required).to eq(false)
-      expect(@df.visible).to eq(true)
+      expect(df.required).to eq(false)
+      expect(df.visible).to eq(true)
     end
 
     it "fields named 'action' or 'controller' are invalid" do
@@ -20,10 +19,10 @@ describe DialogField do
 
     it "supports more than 255 characters within default_value" do
       str = "0" * 10000
-      @df.default_value = str
-      expect { @df.save }.to_not raise_error
-      @df.reload
-      expect(@df.default_value).to eq(str)
+      df.default_value = str
+      expect { df.save }.to_not raise_error
+      df.reload
+      expect(df.default_value).to eq(str)
     end
 
     describe "#validate" do
@@ -110,34 +109,34 @@ describe DialogField do
     describe "#initialize_with_values" do
       it "uses #automate_key_name for extracting initial dialog values" do
         dialog_value = "dummy dialog value"
-        @df.initialize_with_values(@df.automate_key_name => dialog_value)
-        expect(@df.value).to eq(dialog_value)
+        df.initialize_with_values(df.automate_key_name => dialog_value)
+        expect(df.value).to eq(dialog_value)
       end
 
       it "initializes to nil with no initial value and no default value" do
         initial_dialog_values = {}
-        @df.initialize_with_values(initial_dialog_values)
-        expect(@df.value).to be_nil
+        df.initialize_with_values(initial_dialog_values)
+        expect(df.value).to be_nil
       end
 
       it "initializes to the default value with no initial value and a default value" do
         initial_dialog_values = {}
-        @df.default_value = "default_test"
-        @df.initialize_with_values(initial_dialog_values)
-        expect(@df.value).to eq("default_test")
+        df.default_value = "default_test"
+        df.initialize_with_values(initial_dialog_values)
+        expect(df.value).to eq("default_test")
       end
 
       it "initializes to the dialog value with a dialog value and no default value" do
-        initial_dialog_values = {@df.automate_key_name => "test"}
-        @df.initialize_with_values(initial_dialog_values)
-        expect(@df.value).to eq("test")
+        initial_dialog_values = {df.automate_key_name => "test"}
+        df.initialize_with_values(initial_dialog_values)
+        expect(df.value).to eq("test")
       end
 
       it "initializes to the dialog value with a dialog value and a default value" do
-        initial_dialog_values = {@df.automate_key_name => "test"}
-        @df.default_value = "default_test"
-        @df.initialize_with_values(initial_dialog_values)
-        expect(@df.value).to eq("test")
+        initial_dialog_values = {df.automate_key_name => "test"}
+        df.default_value = "default_test"
+        df.initialize_with_values(initial_dialog_values)
+        expect(df.value).to eq("test")
       end
     end
   end
