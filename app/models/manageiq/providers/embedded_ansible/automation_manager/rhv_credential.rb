76639ad75236c26d7506d439c699e1100bd5087b
Adam Grare <agrare@redhat.com>
2018-03-13 09:01:05 -0400

    Merge pull request #17044 from jameswnl/rhev
    Tower Rhv credential type
    (cherry picked from commit a7beb13490c24030b005a7e757bc53a4a21d19df)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1559479

---
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/rhv_credential.rb
@@ -0,0 +1,3 @@
+class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::RhvCredential < ManageIQ::Providers::EmbeddedAnsible::AutomationManager::CloudCredential
+  include ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::RhvCredential
+end
