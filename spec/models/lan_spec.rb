f3e2e9e1d1b3032e926b1f549dffa60fc5a4e3c0
swi2 <swi2@evo14.mendelu.cz>
2018-03-15 11:31:42 +0100

    fix error with let

--- spec/models/lan_spec.rb
+++ spec/models/lan_spec.rb
@@ -1,7 +1,7 @@
 describe Lan do
   let(:lan) { FactoryGirl.create(:lan) }
-  let(:vm) { FactoryGirl.create(:vm_vmware,       :hardware => FactoryGirl.create(:hardware, :guest_devices => [FactoryGirl.create(:guest_device_nic, :lan => lan)])) }
-  let(:template) { FactoryGirl.create(:template_vmware, :hardware => FactoryGirl.create(:hardware, :guest_devices => [FactoryGirl.create(:guest_device_nic, :lan => lan)])) }
+  let!(:vm) { FactoryGirl.create(:vm_vmware, :hardware => FactoryGirl.create(:hardware, :guest_devices => [FactoryGirl.create(:guest_device_nic, :lan => lan)])) }
+  let!(:template) { FactoryGirl.create(:template_vmware, :hardware => FactoryGirl.create(:hardware, :guest_devices => [FactoryGirl.create(:guest_device_nic, :lan => lan)])) }
 
   it "#vms_and_templates" do
     expect(lan.vms_and_templates).to match_array [vm, template]
