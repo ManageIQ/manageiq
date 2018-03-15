0fcdd24b8bf35fb1901e660228ade91cbd5c0ba1
swi2 <swi2@evo12.mendelu.cz>
2018-03-15 11:41:25 +0100

    Added three more let in condition_spec.rb

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -37,21 +37,21 @@ describe Condition do
     end
 
     context "expression with <find>" do
-      let(:cluster) {  FactoryGirl.create(:ems_cluster) }
+      let(:cluster) { FactoryGirl.create(:ems_cluster) }
+      let(:host1) { FactoryGirl.create(:host, :ems_cluster => cluster, :name => "XXX") }
+      let(:host2) { FactoryGirl.create(:host, :ems_cluster => cluster) }
       before do
 
-        @host1 = FactoryGirl.create(:host, :ems_cluster => cluster, :name => "XXX")
-        @host2 = FactoryGirl.create(:host, :ems_cluster => cluster)
         @rp1 = FactoryGirl.create(:resource_pool)
         @rp2 = FactoryGirl.create(:resource_pool)
 
         cluster.with_relationship_type("ems_metadata") { cluster.add_child @rp1 }
         @rp1.with_relationship_type("ems_metadata") { @rp1.add_child @rp2 }
 
-        @vm1 = FactoryGirl.create(:vm_vmware, :host => @host1, :ems_cluster => cluster)
+        @vm1 = FactoryGirl.create(:vm_vmware, :host => host1, :ems_cluster => cluster)
         @vm1.with_relationship_type("ems_metadata") { @vm1.parent = @rp1 }
 
-        @vm2 = FactoryGirl.create(:vm_vmware, :host => @host2, :ems_cluster => cluster)
+        @vm2 = FactoryGirl.create(:vm_vmware, :host => host2, :ems_cluster => cluster)
         @vm2.with_relationship_type("ems_metadata") { @vm2.parent = @rp2 }
       end
 
@@ -97,9 +97,9 @@ describe Condition do
     end
 
     context "expression with <registry>" do
-      let(:vm) {  FactoryGirl.create(:vm_vmware, :registry_items => [@reg_num, @reg_string]) }
+      let(:vm) { FactoryGirl.create(:vm_vmware, :registry_items => [reg_num, @reg_string]) }
+      let(:reg_num) { FactoryGirl.create(:registry_item, :name => "HKLM\\SOFTWARE\\WindowsFirewall : EnableFirewall", :data => 0) }
       before do
-        @reg_num    = FactoryGirl.create(:registry_item, :name => "HKLM\\SOFTWARE\\WindowsFirewall : EnableFirewall", :data => 0)
         @reg_string = FactoryGirl.create(:registry_item, :name => "HKLM\\SOFTWARE\\Microsoft\\Ole : EnableDCOM", :data => 'y')
       end
 
@@ -109,7 +109,7 @@ describe Condition do
       end
 
       it "numerical type registry key data is single quoted" do
-        expr = "<registry>#{@reg_num.name}</registry>"
+        expr = "<registry>#{reg_num.name}</registry>"
         expect(Condition.subst(expr, vm)).to eq('"0"')
       end
     end
