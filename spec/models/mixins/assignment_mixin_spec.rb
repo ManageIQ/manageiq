18024eefbcec1ab6bb558761414121cd9a27874d
Jillian Tullo <jtullo@redhat.com>
2018-03-21 08:22:52 -0400

    Expose get_assigned_tos as virtual attribute to allow access via the API

--- spec/models/mixins/assignment_mixin_spec.rb
+++ spec/models/mixins/assignment_mixin_spec.rb
@@ -78,6 +78,22 @@ describe AssignmentMixin do
     end
   end
 
+  describe "#get_assigned_tos" do
+    it "returns objects and tags" do
+      cc_classification = FactoryGirl.create(:classification_cost_center)
+      classification_tag = FactoryGirl.create(:classification_tag, :parent => cc_classification)
+      vm = FactoryGirl.create(:vm)
+      alert_set = FactoryGirl.create(:miq_alert_set)
+
+      alert_set.assign_to_objects([vm])
+      alert_set.assign_to_tags([classification_tag], "vms")
+
+      assigned_tos = alert_set.get_assigned_tos
+      expect(assigned_tos[:objects]).to include(vm)
+      expect(assigned_tos[:tags]).to include([classification_tag, "vms"])
+    end
+  end
+
   describe ".all_assignments" do
     it "returns only tags representing assignments" do
       t1 = Tag.create(:name => "/chargeback_rate/assigned_to/vm/tag/managed/environment/any1")
