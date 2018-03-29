627390f7423321d216f58ab288020c71b7890fa5
Samuel Lucidi <mansam@csh.rit.edu>
2018-03-29 10:00:11 -0400

    Merge pull request #262 from aufi/default_empty_event_payload
    Default Event payload to empty Hash
    (cherry picked from commit e62a44f3655c1285bcabd611c7f23559bd52c64b)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1565169

--- app/models/manageiq/providers/openstack/cloud_manager/event_parser.rb
+++ app/models/manageiq/providers/openstack/cloud_manager/event_parser.rb
@@ -2,7 +2,7 @@ module ManageIQ::Providers::Openstack::CloudManager::EventParser
   def self.event_to_hash(event, ems_id)
     content = message_content(event, ems_id)
     event_type = content["event_type"]
-    payload = content["payload"]
+    payload = content["payload"] || {}
 
     log_header = "ems_id: [#{ems_id}] " unless ems_id.nil?
     _log.debug("#{log_header}event: [#{event_type}]") if $log && $log.debug?
