fc5a68cf74b019ad833c2b301b323a23db77a32a
Keenan Brock <keenan@thebrocks.net>
2018-04-02 15:04:15 -0400

    convert Vm#miq_provision_template to has_one
    Provision Activity report uses this field in a where clause
    This change allows the report to run without downloading all data
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1560113

--- app/models/vm_or_template.rb
+++ app/models/vm_or_template.rb
@@ -172,7 +172,7 @@ class VmOrTemplate < ApplicationRecord
   virtual_has_many   :lans,                                                  :uses => {:hardware => {:nics => :lan}}
   virtual_has_many   :child_resources,        :class_name => "VmOrTemplate"
 
-  virtual_belongs_to :miq_provision_template, :class_name => "Vm",           :uses => {:miq_provision => :vm_template}
+  has_one            :miq_provision_template, :through => "miq_provision", :source => "source", :source_type => "VmOrTemplate"
   virtual_belongs_to :parent_resource_pool,   :class_name => "ResourcePool", :uses => :all_relationships
 
   virtual_has_one   :direct_service,       :class_name => 'Service'
@@ -1430,10 +1430,6 @@ class VmOrTemplate < ApplicationRecord
   def v_datastore_path
     datastorepath = location || ""
     storage ? "#{storage.name}/#{datastorepath}" : datastorepath
-  end
-
-  def miq_provision_template
-    miq_provision.try(:vm_template)
   end
 
   def event_threshold?(options = {:time_threshold => 30.minutes, :event_types => ["MigrateVM_Task_Complete"], :freq_threshold => 2})
