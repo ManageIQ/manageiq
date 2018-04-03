a1996116383f45f04c4e70d48b6683f8b186e445
Yuri Rudman <yrudman@redhat.com>
2018-04-03 18:28:40 -0400

    use more complicated expression to test removal of temporary token

--- spec/lib/miq_expression/subst_mixin_spec.rb
+++ spec/lib/miq_expression/subst_mixin_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe MiqExpression::SubstMixin do
 
   describe "#exp_replace_qs_tokens" do
     it "removes :token key from passed expression" do
-      exp = {"=" => {"field" => "Vm-active", "value" => "true"}, :token => 1}
+      exp = {"and" => [{"=" => {"field" => "Vm-active", "value" => "true"}, :token => 1}, {"=" => {"field" => "Vm-archived", "value" => "true"}, :token => 2}]}
       test_obj.exp_replace_qs_tokens(exp, {})
-      expect(exp).to eq("=" => {"field" => "Vm-active", "value" => "true"})
+      expect(exp).to eq("and" => [{"=" => {"field" => "Vm-active", "value" => "true"}}, {"=" => {"field" => "Vm-archived", "value" => "true"}}])
     end
   end
 end
