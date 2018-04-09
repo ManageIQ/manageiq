033914a3e7230cf0799af2b32eb6437568d36ef4
Jakub Kubišta <Jakub Kubita@192.168.94.148>
2018-04-09 16:15:20 +0200

    remove before, add forcing the invocation

--- spec/models/guest_device_spec.rb
+++ spec/models/guest_device_spec.rb
@@ -1,14 +1,12 @@
 describe GuestDevice do
-  before do
-    let(:vm_gd) { FactoryGirl.create(:guest_device_nic) }
-    let(:vm) { FactoryGirl.create(:vm_vmware, :hardware => FactoryGirl.create(:hardware, :guest_devices => [vm_gd])) }
+  let!(:vm_gd) { FactoryGirl.create(:guest_device_nic) }
+  let!(:vm) { FactoryGirl.create(:vm_vmware, :hardware => FactoryGirl.create(:hardware, :guest_devices => [vm_gd])) }
 
-    let(:template_gd) { FactoryGirl.create(:guest_device_nic) }
-    let(:template) { FactoryGirl.create(:template_vmware, :hardware => FactoryGirl.create(:hardware, :guest_devices => [template_gd])) }
+  let!(:template_gd) { FactoryGirl.create(:guest_device_nic) }
+  let!(:template) { FactoryGirl.create(:template_vmware, :hardware => FactoryGirl.create(:hardware, :guest_devices => [template_gd])) }
 
-    let(:host_gd) { FactoryGirl.create(:guest_device_nic) }
-    let(:host) { FactoryGirl.create(:host, :hardware => FactoryGirl.create(:hardware, :guest_devices => [host_gd])) }
-  end
+  let!(:host_gd) { FactoryGirl.create(:guest_device_nic) }
+  let!(:host) { FactoryGirl.create(:host, :hardware => FactoryGirl.create(:hardware, :guest_devices => [host_gd])) }
 
   it "#vm_or_template" do
     expect(vm_gd.vm_or_template).to eq(vm)
