607d2c23f5b6b475a53227c54f6d50baa7e03f3f
Unknown <esdras015@gmail.com>
2018-04-20 10:41:15 -0300

    Adjusting ManageIQ core to enable PhysicalChassis API endpoint

--- app/models/physical_chassis.rb
+++ app/models/physical_chassis.rb
@@ -8,4 +8,23 @@ class PhysicalChassis < ApplicationRecord
   has_one :hardware, :through => :computer_system
   has_one :asset_detail, :as => :resource, :dependent => :destroy, :inverse_of => false
   has_many :guest_devices, :through => :hardware
+
+  def my_zone
+    ems = ext_management_system
+    ems ? ems.my_zone : MiqServer.my_zone
+  end
+
+  def refresh_ems
+    unless ext_management_system
+      raise _("No Provider defined")
+    end
+    unless ext_management_system.has_credentials?
+      raise _("No Provider credentials defined")
+    end
+    unless ext_management_system.authentication_status_ok?
+      raise _("Provider failed last authentication check")
+    end
+
+    EmsRefresh.queue_refresh(ext_management_system)
+  end
 end
