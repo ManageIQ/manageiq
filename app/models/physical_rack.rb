257b0f9ae7bbdf2cf9e212c79cbce8f736e419e0
felipedf <felipe.diogenes13@gmail.com>
2018-05-11 14:30:18 -0300

    Add a method to process the refesh action for a PhysicalRack

--- app/models/physical_rack.rb
+++ app/models/physical_rack.rb
@@ -6,4 +6,23 @@ class PhysicalRack < ApplicationRecord
 
   has_many :physical_chassis, :dependent => :nullify, :inverse_of => :physical_rack
   has_many :physical_servers, :dependent => :nullify, :inverse_of => :physical_rack
+
+  def my_zone
+    ems = ext_management_system
+    ems ? ems.my_zone : MiqServer.my_zone
+  end
+
+  def refresh_ems
+    unless ext_management_system
+      raise _("No Provider defined")
+    end
+    unless ext_management_system.has_credentials?
+      raise _("No Provider credentials defined")
+    end
+    unless ext_management_system.authentication_status_ok?
+      raise _("Provider failed last authentication check")
+    end
+
+    EmsRefresh.queue_refresh(ext_management_system)
+  end
 end
