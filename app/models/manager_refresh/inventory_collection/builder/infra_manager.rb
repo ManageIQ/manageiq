a124e9759364a2a6898bb90c434b6af78b8a189f
Martin Slemr <mslemr@redhat.com>
2018-05-24 11:33:18 +0200

    Fixed infra emc_clusters definition
    Datacenter_id inventory attribute have to be added manually

--- app/models/manager_refresh/inventory_collection/builder/infra_manager.rb
+++ app/models/manager_refresh/inventory_collection/builder/infra_manager.rb
@@ -80,6 +80,8 @@ module ManagerRefresh
           add_properties(
             :attributes_blacklist => %i(ems_children datacenter_id),
           )
+
+          add_inventory_attributes(%i(datacenter_id))
           shared_builder_params
         end
 
@@ -125,31 +127,33 @@ module ManagerRefresh
         def vms
           super
 
-          add_custom_reconnect_block(
-            lambda do |inventory_collection, inventory_objects_index, attributes_index|
-              relation = inventory_collection.model_class.where(:ems_id => nil)
+          custom_reconnect_block = lambda do |inventory_collection, inventory_objects_index, attributes_index|
+            relation = inventory_collection.model_class.where(:ems_id => nil)
 
-              return if relation.count <= 0
+            return if relation.count <= 0
 
-              inventory_objects_index.each_slice(100) do |batch|
-                relation.where(inventory_collection.manager_ref.first => batch.map(&:first)).each do |record|
-                  index = inventory_collection.object_index_with_keys(inventory_collection.manager_ref_to_cols, record)
+            inventory_objects_index.each_slice(100) do |batch|
+              relation.where(inventory_collection.manager_ref.first => batch.map(&:first)).each do |record|
+                index = inventory_collection.object_index_with_keys(inventory_collection.manager_ref_to_cols, record)
 
-                  # We need to delete the record from the inventory_objects_index and attributes_index, otherwise it
-                  # would be sent for create.
-                  inventory_object = inventory_objects_index.delete(index)
-                  hash             = attributes_index.delete(index)
+                # We need to delete the record from the inventory_objects_index and attributes_index, otherwise it
+                # would be sent for create.
+                inventory_object = inventory_objects_index.delete(index)
+                hash = attributes_index.delete(index)
 
-                  record.assign_attributes(hash.except(:id, :type))
-                  if !inventory_collection.check_changed? || record.changed?
-                    record.save!
-                    inventory_collection.store_updated_records(record)
-                  end
-
-                  inventory_object.id = record.id
+                record.assign_attributes(hash.except(:id, :type))
+                if !inventory_collection.check_changed? || record.changed?
+                  record.save!
+                  inventory_collection.store_updated_records(record)
                 end
+
+                inventory_object.id = record.id
               end
             end
+          end
+
+          add_properties(
+            :custom_reconnect_block => custom_reconnect_block
           )
         end
 
