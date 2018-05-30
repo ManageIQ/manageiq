af1e882c95d14d848e8fa4869470b4274c7ec01d
douglasgabriel <douglasxgabriel@gmail.com>
2018-05-30 08:57:05 -0300

    Adding connection b/w physical servers and switches

--- app/models/physical_network_port.rb
+++ app/models/physical_network_port.rb
@@ -1,4 +1,7 @@
 class PhysicalNetworkPort < ApplicationRecord
   belongs_to :guest_device
-  belongs_to :physical_switch
+  belongs_to :physical_switch, :foreign_key => :switch_id, :inverse_of => :physical_network_ports
+
+  has_one :connected_port, :foreign_key => "connected_port_uid", :primary_key => "uid_ems", :class_name => "PhysicalNetworkPort", :dependent => :nullify, :inverse_of => :connected_port
+  has_one :connected_physical_switch, :through => :connected_port, :source => :physical_switch
 end
