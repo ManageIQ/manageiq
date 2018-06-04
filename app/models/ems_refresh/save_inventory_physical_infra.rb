b431a98a73c313ee3b5898f0d3bab00e2d87d8cf
Douglas Gabriel <douglasxgabriel@gmail.com>
2018-06-04 14:20:13 -0300

    Adding PhysicalComponent model and it relationship with PhysicalSwitch

--- app/models/ems_refresh/save_inventory_physical_infra.rb
+++ app/models/ems_refresh/save_inventory_physical_infra.rb
@@ -51,7 +51,7 @@ module EmsRefresh::SaveInventoryPhysicalInfra
 
     deletes = target == ems ? :use_association : []
 
-    save_inventory_multi(ems.physical_switches, hashes, deletes, [:uid_ems], %i(asset_detail hardware physical_network_ports))
+    save_inventory_multi(ems.physical_switches, hashes, deletes, [:uid_ems], %i(asset_detail hardware physical_network_ports physical_component))
   end
 
   def save_physical_servers_inventory(ems, hashes, target = nil)
@@ -97,6 +97,11 @@ module EmsRefresh::SaveInventoryPhysicalInfra
   def save_asset_detail_inventory(parent, hash)
     return if hash.nil?
     save_inventory_single(:asset_detail, parent, hash)
+  end
+
+  def save_physical_component_inventory(parent, hash)
+    return if hash.nil?
+    save_inventory_single(:physical_component, parent, hash, [:asset_detail])
   end
 
   def save_physical_network_ports_inventory(guest_device, hashes, target = nil)
---
+++ app/models/physical_component.rb
@@ -0,0 +1,15 @@
+#
+# A hub to centralize the Physical Components relationships.
+#   and a common way to access the Physical Components.
+#
+# Physical Components:
+#   - Physical Server;
+#   - Physical Switch;
+#   - Physical Chassis;
+#   - Physical Rack.
+#
+class PhysicalComponent < ApplicationRecord
+  belongs_to :component, :polymorphic => true
+
+  has_one :asset_detail, :as => :resource, :dependent => :destroy, :inverse_of => :resource
+end
