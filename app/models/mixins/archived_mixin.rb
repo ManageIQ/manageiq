17a3e84538a452304556990478627e1d080c7e7b
Adam Grare <agrare@redhat.com>
2018-06-06 15:45:12 -0400

    Merge pull request #17539 from bzwei/old_ems_id
    move #my_zone from ArchivedMixin to OldEmsMixin
    (cherry picked from commit 8bd16646c6efd64a808731f50d99eef12bc53962)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1595418

--- app/models/mixins/archived_mixin.rb
+++ app/models/mixins/archived_mixin.rb
@@ -4,8 +4,6 @@ module ArchivedMixin
   included do
     scope :archived, -> { where.not(:deleted_on => nil) }
     scope :active, -> { where(:deleted_on => nil) }
-
-    belongs_to :old_ext_management_system, :foreign_key => :old_ems_id, :class_name => 'ExtManagementSystem'
   end
 
   def archived?
@@ -24,16 +22,5 @@ module ArchivedMixin
 
   def unarchive!
     update_attributes!(:deleted_on => nil)
-  end
-
-  # Needed for metrics
-  def my_zone
-    if ext_management_system.present?
-      ext_management_system.my_zone
-    elsif old_ext_management_system.present?
-      # Archived container entities need to retain their zone for metric collection
-      # This makes the association more complex and might need a performance fix
-      old_ext_management_system.my_zone
-    end
   end
 end
---
+++ app/models/mixins/old_ems_mixin.rb
@@ -0,0 +1,18 @@
+module OldEmsMixin
+  extend ActiveSupport::Concern
+
+  included do
+    belongs_to :old_ext_management_system, :foreign_key => :old_ems_id, :class_name => 'ExtManagementSystem'
+  end
+
+  # Needed for metrics
+  def my_zone
+    if ext_management_system.present?
+      ext_management_system.my_zone
+    elsif old_ext_management_system.present?
+      # Archived container entities need to retain their zone for metric collection
+      # This makes the association more complex and might need a performance fix
+      old_ext_management_system.my_zone
+    end
+  end
+end
