0c2bf68625ccf36d421dc139bf7b81d42a6ad39f
Joe Rafaniello <jrafanie@redhat.com>
2018-06-07 16:37:19 -0400

    Allow duplicate nil pxe_image_types during seed
    https://bugzilla.redhat.com/show_bug.cgi?id=1588417
    
    We want to disallow the same name template in different regions with the
    same pxe_image_type.  Normally, region id sequences prevent this from
    happening but we were treating nil pxe_image_types as the same.
    Therefore you could have the "same" pxe_image_type in different regions
    for the same name template.

--- app/models/customization_template.rb
+++ app/models/customization_template.rb
@@ -5,10 +5,14 @@ class CustomizationTemplate < ApplicationRecord
   has_many   :pxe_images, :through => :pxe_image_type
 
   validates :pxe_image_type, :presence => true, :unless => :system?
-  validates :name,           :unique_within_region => true
+  validates :name, :uniqueness => {:scope => :pxe_image_type }, :unless => :seeding?
 
   scope :with_pxe_image_type_id, ->(pxe_image_type_id) { where(:pxe_image_type_id => pxe_image_type_id) }
   scope :with_system,            ->(bool = true)       { where(:system => bool) }
+
+  def seeding?
+    system? && pxe_image_type.nil?
+  end
 
   def self.seed_file_name
     @seed_file_name ||= Rails.root.join("db", "fixtures", "#{table_name}.yml")
