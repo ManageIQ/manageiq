5b709f4f9211ae6cb1c0181141a17b29ec3b359c
Gregg Tanzillo <gtanzill@redhat.com>
2018-06-15 13:44:03 -0400

    Merge pull request #17581 from bdunne/miq_schedule_resources
    Add resource relation to MiqSchedule
    (cherry picked from commit 381f94aa73db00c66f7fa51f253e303cbb2168f6)

--- app/models/miq_schedule.rb
+++ app/models/miq_schedule.rb
@@ -1,4 +1,7 @@
 class MiqSchedule < ApplicationRecord
+  include ReservedMixin
+  reserve_attribute :resource_id, :big_integer
+
   validates :name, :uniqueness => {:scope => [:userid, :towhat]}
   validates :name, :description, :towhat, :run_at, :presence => true
   validate  :validate_run_at, :validate_file_depot
@@ -39,6 +42,12 @@ class MiqSchedule < ApplicationRecord
   default_value_for :enabled, true
   default_value_for(:zone_id) { MiqServer.my_server.zone_id }
 
+  def resource
+    # HACK: this should be a real relation, but for now it's using a reserve_attribute for backport reasons
+    return unless resource_id
+    towhat.safe_constantize.find_by(:id => resource_id)
+  end
+
   def set_start_time_and_prod_default
     run_at # Internally this will correct :start_time to UTC
     self.prod_default = "system" if SYSTEM_SCHEDULE_CLASSES.include?(towhat.to_s)
@@ -72,21 +81,21 @@ class MiqSchedule < ApplicationRecord
     _log.info("Queueing start of schedule id: [#{id}] [#{sched.name}] [#{sched.towhat}] [#{method}]")
 
     action = "action_" + method
-    unless sched.respond_to?(action)
+
+    if sched.respond_to?(action)
+      msg = MiqQueue.submit_job(
+        :class_name  => name,
+        :instance_id => sched.id,
+        :method_name => "invoke_actions",
+        :args        => [action, at],
+        :msg_timeout => 1200
+      )
+
+      _log.info("Queueing start of schedule id: [#{id}] [#{sched.name}] [#{sched.towhat}] [#{method}]...complete")
+      msg
+    else
       _log.warn("[#{sched.name}] no such action: [#{method}], aborting schedule")
-      return
     end
-
-    msg = MiqQueue.submit_job(
-      :class_name  => name,
-      :instance_id => sched.id,
-      :method_name => "invoke_actions",
-      :args        => [action, at],
-      :msg_timeout => 1200
-    )
-
-    _log.info("Queueing start of schedule id: [#{id}] [#{sched.name}] [#{sched.towhat}] [#{method}]...complete")
-    msg
   end
 
   def invoke_actions(action, at)
