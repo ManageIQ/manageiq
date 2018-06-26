3aa5ee10946be9ad4c8bbe9a4663b0b9437ac91e
Jerry Keselman <jkeselma@redhat.com>
2018-06-26 10:18:02 -0400

    Support S3 for DB Backups
    Initial commit to support DB backups to S3.
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1513520
    
    
    (transferred from ManageIQ/manageiq-gems-pending@65ee18925b687af0289f9d3f588b4b05184577ce)

--- lib/gems/pending/util/mount/miq_generic_mount_session.rb
+++ lib/gems/pending/util/mount/miq_generic_mount_session.rb
@@ -9,6 +9,7 @@ require 'util/miq-uuid'
 
 class MiqGenericMountSession
   require 'util/mount/miq_nfs_session'
+  require 'util/mount/miq_s3_session'
   require 'util/mount/miq_smb_session'
   require 'util/mount/miq_glusterfs_session'
 
@@ -53,7 +54,7 @@ class MiqGenericMountSession
 
   def self.new_session(opts)
     klass = uri_scheme_to_class(opts[:uri])
-    session = klass.new(:uri => opts[:uri], :username => opts[:username], :password => opts[:password])
+    session = klass.new(:uri => opts[:uri], :username => opts[:username], :password => opts[:password], :region => opts[:region])
     session.connect
     session
   end
@@ -62,6 +63,8 @@ class MiqGenericMountSession
     require 'uri'
     scheme, userinfo, host, port, registry, share, opaque, query, fragment = URI.split(URI.encode(uri))
     case scheme
+    when 's3'
+      MiqS3Session
     when 'smb'
       MiqSmbSession
     when 'nfs'
@@ -462,6 +465,10 @@ class MiqGenericMountSession
     else
       raise "platform not supported"
     end
+  end
+
+  def copy_dump_to_store
+    # nothing to do
   end
 
   private
