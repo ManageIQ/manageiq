6faa459879ab9a46ee1b2b1e171ddd5e885fb312
Jason Frey <jfrey@redhat.com>
2018-06-28 14:32:21 -0400

    Add specs for Vmdb::Plugins methods.

--- spec/lib/vmdb/plugins_spec.rb
+++ spec/lib/vmdb/plugins_spec.rb
@@ -1,4 +1,46 @@
 describe Vmdb::Plugins do
+  it ".all" do
+    all = described_class.all
+
+    expect(all).to include(
+      ManageIQ::Providers::Vmware::Engine,
+      ManageIQ::UI::Classic::Engine
+    )
+    expect(all).to_not include(
+      ActionCable::Engine
+    )
+  end
+
+  it ".ansible_content" do
+    ansible_content = described_class.ansible_content
+
+    content = ansible_content.detect { |ac| ac.path.to_s.include?("manageiq-content") }
+    expect(content.path).to eq ManageIQ::Content::Engine.root.join("content/ansible")
+
+    content = ansible_content.detect { |ac| ac.path.to_s.include?("manageiq-ui-classic") }
+    expect(content).to_not be
+  end
+
+  it ".automate_domains" do
+    automate_domains = described_class.automate_domains
+
+    domain = automate_domains.detect { |ac| ac.name == "ManageIQ" }
+    expect(domain.path).to eq ManageIQ::Content::Engine.root.join("content/automate/ManageIQ")
+
+    domain = automate_domains.detect { |ac| ac.path.to_s.include?("manageiq-ui-classic") }
+    expect(domain).to_not be
+  end
+
+  it ".system_automate_domains" do
+    automate_domains = described_class.system_automate_domains
+
+    domain = automate_domains.detect { |ac| ac.name == "ManageIQ" }
+    expect(domain.system?).to be_truthy
+
+    domain = automate_domains.detect { |ac| ac.path.to_s.include?("manageiq-ui-classic") }
+    expect(domain).to_not be
+  end
+
   describe ".asset_paths" do
     it "with normal engines" do
       asset_paths = described_class.asset_paths
@@ -15,5 +57,18 @@ describe Vmdb::Plugins do
       expect(asset_path.path).to eq ManageIQ::V2V::Engine.root
       expect(asset_path.namespace).to eq "manageiq-v2v"
     end
+  end
+
+  it ".provider_plugins" do
+    provider_plugins = described_class.provider_plugins
+
+    expect(provider_plugins).to include(
+      ManageIQ::Providers::Vmware::Engine,
+      ManageIQ::Providers::Amazon::Engine
+    )
+    expect(provider_plugins).to_not include(
+      ManageIQ::Api::Engine,
+      ManageIQ::UI::Classic::Engine
+    )
   end
 end
