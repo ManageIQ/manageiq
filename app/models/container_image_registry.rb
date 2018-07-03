4fe3f2d8abe19e6c9c618c1a92e613cf6330bf93
Martin Slemr <mslemr@redhat.com>
2018-07-03 17:27:23 +0200

    ContainerImage assoc. destroying
    ContainerImageRegistry only nullify dependency.
    Fixes OpenShift specs

--- app/models/container_image_registry.rb
+++ app/models/container_image_registry.rb
@@ -1,6 +1,6 @@
 class ContainerImageRegistry < ApplicationRecord
   belongs_to :ext_management_system, :foreign_key => "ems_id"
-  has_many :container_images, :dependent => :destroy # What about deleted registry but containers are still running
+  has_many :container_images, :dependent => :nullify
   has_many :containers, :through => :container_images
   has_many :container_services
   has_many :container_groups, :through => :container_services
