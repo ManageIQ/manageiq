cba8356f90ae92c81c9a0ed44261eedcc9d23dc7
Jerry Keselman <jkeselma@redhat.com>
2018-07-09 15:25:57 -0400

    Rework Step to Upload DB Dump to S3
    Based on PR review comments, use the existing MountSession "add" operation
    to upload the database file to S3.
    
    Correctly handle the situation where the bucket name requested exists but
    access is denied, indicating (probably) that is owned by another user.
    (The S3 namespace is Global - each user does not get their own a namespace).
    
    
    (transferred from ManageIQ/manageiq-gems-pending@67fd6ee20ba91ffc6a35e12076c7bd65ed444542)

--- lib/gems/pending/util/mount/miq_generic_mount_session.rb
+++ lib/gems/pending/util/mount/miq_generic_mount_session.rb
@@ -246,7 +246,7 @@ class MiqGenericMountSession
     res
   end
 
-  def add(source, dest_uri)
+  def add(source, dest_uri, _object_file = nil)
     log_header = "MIQ(#{self.class.name}-add)"
 
     logger.info("#{log_header} Source: [#{source}], Destination: [#{dest_uri}]...")
