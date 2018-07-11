7bc49ac68f52e3f5b3591b9f435c426e18e7444b
Ladislav Smola <lsmola@redhat.com>
2018-07-11 14:04:22 +0200

    Use mktmpdir to clean up ansible-runner files after the run
    Use mktmpdir to clean up ansible-runner files after the run

--- lib/ansible/runner.rb
+++ lib/ansible/runner.rb
@@ -24,25 +24,21 @@ module Ansible
       private
 
       def run_via_cli(env_vars, extra_vars, playbook_path)
-        prepare_tmp_structure
+        Dir.mktmpdir("ansible-runner") do |base_dir|
+          mkdir(base_dir + '/project') # without this, there is a silent fail of the ansible-runner command see https://github.com/ansible/ansible-runner/issues/88
 
-        result = AwesomeSpawn.run!(ansible_command, :env => env_vars, :params => [{:cmdline => "--extra-vars '#{JSON.dump(extra_vars)}'", :playbook => playbook_path}])
-        JSON.parse(result.output)
-      end
-
-      def prepare_tmp_structure
-        base_dir = "/tmp/ansible-runner/"
-        mkdir(base_dir)
-        mkdir(base_dir + 'project') # without this, there is a silent fail of the ansible-runner command see https://github.com/ansible/ansible-runner/issues/88
+          result = AwesomeSpawn.run!(ansible_command(base_dir), :env => env_vars, :params => [{:cmdline => "--extra-vars '#{JSON.dump(extra_vars)}'", :playbook => playbook_path}])
+          JSON.parse(result.output)
+        end
       end
 
       def mkdir(base_dir)
         Dir.mkdir(base_dir) unless Dir.exist?(base_dir)
       end
 
-      def ansible_command
+      def ansible_command(base_dir)
         # TODO add possibility to use custom path, e.g. from virtualenv
-        "ansible-runner run /tmp/ansible-runner --json"
+        "ansible-runner run #{base_dir} --json"
       end
     end
   end
