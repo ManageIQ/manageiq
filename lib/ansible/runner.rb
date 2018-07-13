653b072dd365652197337dce94f821c20d26bac8
Adam Grare <agrare@redhat.com>
2018-07-13 11:42:39 -0400

    Add a method to queue an Ansible::Runner.run
    This adds a way to queue an Ansible::Runner.run and wraps it in a task
    so that it can be invoked asynchronously from automate.

--- lib/ansible/runner.rb
+++ lib/ansible/runner.rb
@@ -5,6 +5,22 @@ module Ansible
         run_via_cli(env_vars, extra_vars, playbook_path)
       end
 
+      def run_queue(env_vars, extra_vars, playbook_path, user_id, queue_opts)
+        queue_opts = {
+          :args        => [env_vars, extra_vars, playbook_path],
+          :queue_name  => "generic",
+          :class_name  => name,
+          :method_name => "run",
+        }.merge(queue_opts)
+
+        task_opts = {
+          :action => "Run Ansible Playbook",
+          :userid => user_id,
+        }
+
+        MiqTask.generic_action_with_callback(task_opts, queue_opts)
+      end
+
       private
 
       def run_via_cli(env_vars, extra_vars, playbook_path)
