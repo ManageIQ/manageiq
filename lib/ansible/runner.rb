ce9af21ed0f5eb1eecc753a81b70cc2715cfa6bf
Ladislav Smola <lsmola@redhat.com>
2018-07-17 13:18:15 +0200

    Simplify the mkdir

--- lib/ansible/runner.rb
+++ lib/ansible/runner.rb
@@ -25,7 +25,7 @@ module Ansible
 
       def run_via_cli(env_vars, extra_vars, playbook_path)
         Dir.mktmpdir("ansible-runner") do |base_dir|
-          mkdir(base_dir + '/project') # without this, there is a silent fail of the ansible-runner command see https://github.com/ansible/ansible-runner/issues/88
+          Dir.mkdir(File.join(base_dir, 'project')) # without this, there is a silent fail of the ansible-runner command see https://github.com/ansible/ansible-runner/issues/88
 
           result = AwesomeSpawn.run!(ansible_command(base_dir), :env => env_vars, :params => [{:cmdline => "--extra-vars '#{JSON.dump(extra_vars)}'", :playbook => playbook_path}])
 
@@ -35,10 +35,6 @@ module Ansible
 
       def return_code(base_dir)
         File.read(File.join(base_dir, "artifacts/result/rc"))
-      end
-
-      def mkdir(base_dir)
-        Dir.mkdir(base_dir) unless Dir.exist?(base_dir)
       end
 
       def ansible_command(base_dir)
