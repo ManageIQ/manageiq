9f0bcea7e145e360754de3324d94cdade0b2c713
James Wong <jwong@redhat.com>
2018-07-23 16:36:24 -0400

    remove the redundnat belongs_to from configuration_script

--- app/models/configuration_script.rb
+++ app/models/configuration_script.rb
@@ -2,5 +2,4 @@ class ConfigurationScript < ConfigurationScriptBase
   def self.base_model
     ConfigurationScript
   end
-  belongs_to :manager, :class_name => "ExtManagementSystem", :inverse_of => :configuration_scripts
 end
