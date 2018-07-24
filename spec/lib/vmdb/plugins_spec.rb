dbd79bf6fd1d282acdf213af86b6e0c9cb26f295
Jason Frey <jfrey@redhat.com>
2018-07-24 13:12:35 -0400

    Add Vmdb::Plugins#versions
    https://github.com/ManageIQ/manageiq-v2v/issues/422
    https://bugzilla.redhat.com/show_bug.cgi?id=1594348

--- spec/lib/vmdb/plugins_spec.rb
+++ spec/lib/vmdb/plugins_spec.rb
@@ -71,4 +71,134 @@ describe Vmdb::Plugins do
       ManageIQ::UI::Classic::Engine
     )
   end
+
+  it ".versions" do
+    versions = described_class.versions
+
+    expect(versions).to be_kind_of(Hash)
+    expect(versions.keys).to match_array described_class.all
+  end
+
+  describe ".version (private)" do
+    subject { described_class.send(:instance).send(:version, engine) }
+
+    let(:engine) { Class.new(Rails::Engine) }
+
+    def clear_versions_caches
+      described_class.send(:instance).instance_variable_set(:@bundler_specs_by_path, nil)
+    end
+
+    before { clear_versions_caches }
+    after  { clear_versions_caches }
+
+    def with_temp_dir(_options)
+      Dir.mktmpdir("plugins_spec") do |dir|
+        allow(engine).to receive(:root).and_return(Pathname.new(dir))
+        yield dir
+      end
+    end
+
+    def with_temp_git_dir(options)
+      with_temp_dir(options) do |dir|
+        sha = nil
+
+        Dir.chdir(dir) do
+          `
+          git init &&
+          touch foo  && git add -A && git commit -m "Added foo" &&
+          touch foo2 && git add -A && git commit -m "Added foo2"
+          `
+
+          if options[:branch] == "master"
+            sha = `git rev-parse HEAD`.strip
+          else
+            sha = `git rev-parse HEAD~`.strip
+            `git checkout #{"-b #{options[:branch]}" if options[:branch]} #{sha} 2>/dev/null`
+            `git tag #{options[:tag]}` if options[:tag]
+          end
+        end
+
+        yield dir, sha
+      end
+    end
+
+    def with_spec(type, options = {})
+      raise "Unexpected type '#{type}'" unless %i(git path_with_git path).include?(type)
+
+      source =
+        if type == :git
+          instance_double(Bundler::Source::Git, :branch => options[:branch], :options => {"tag" => options[:tag]})
+        else
+          instance_double(Bundler::Source::Path)
+        end
+
+      allow(Bundler::Source::Git).to receive(:===).with(source).and_return(type == :git)
+      allow(Bundler::Source::Path).to receive(:===).with(source).and_return(type != :git)
+
+      method = (type == :path ? :with_temp_dir : :with_temp_git_dir)
+
+      send(method, options) do |dir, sha|
+        expect(source).to receive(:revision).and_return(sha) if type == :git
+
+        spec = instance_double(Gem::Specification, :full_gem_path => dir, :source => source)
+        expect(Bundler.environment).to receive(:specs).and_return([spec])
+
+        yield(sha && sha[0, 8])
+      end
+    end
+
+    it "git based, on master" do
+      with_spec(:git, :branch => "master") do |sha|
+        expect(subject).to eq("master@#{sha}")
+      end
+    end
+
+    it "git based, on a branch" do
+      with_spec(:git, :branch => "my_branch") do |sha|
+        expect(subject).to eq("my_branch@#{sha}")
+      end
+    end
+
+    it "git based, on a tag" do
+      with_spec(:git, :tag => "my_tag") do |sha|
+        expect(subject).to eq("my_tag@#{sha}")
+      end
+    end
+
+    it "git based, on a sha" do
+      with_spec(:git) do |sha|
+        expect(subject).to eq(sha)
+      end
+    end
+
+    it "path based, with git, on master" do
+      with_spec(:path_with_git, :branch => "master") do |sha|
+        expect(subject).to eq("master@#{sha}")
+      end
+    end
+
+    it "path based, with git, on a branch" do
+      with_spec(:path_with_git, :branch => "my_branch") do |sha|
+        expect(subject).to eq("my_branch@#{sha}")
+      end
+    end
+
+    it "path based, with git, on a tag" do
+      with_spec(:path_with_git, :tag => "my_tag") do |sha|
+        expect(subject).to eq("my_tag@#{sha}")
+      end
+    end
+
+    it "path based, with git, on a sha" do
+      with_spec(:path_with_git) do |sha|
+        expect(subject).to eq(sha)
+      end
+    end
+
+    it "path based, without git" do
+      with_spec(:path) do
+        expect(subject).to be_nil
+      end
+    end
+  end
 end
