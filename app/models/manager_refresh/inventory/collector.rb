9fc96cfee96896382be32c415ce8ed4fe844b4ca
Martin Slemr <mslemr@redhat.com>
2018-08-22 13:13:23 +0200

    Added _log() to Inventory collector/parser

--- app/models/manager_refresh/inventory/collector.rb
+++ app/models/manager_refresh/inventory/collector.rb
@@ -1,6 +1,8 @@
 class ManagerRefresh::Inventory::Collector
   attr_reader :manager, :target
 
+  include Vmdb::Logging
+
   # @param manager [ManageIQ::Providers::BaseManager] A manager object
   # @param refresh_target [Object] A refresh Target object
   def initialize(manager, refresh_target)
--- app/models/manager_refresh/inventory/parser.rb
+++ app/models/manager_refresh/inventory/parser.rb
@@ -2,6 +2,8 @@ class ManagerRefresh::Inventory::Parser
   attr_accessor :collector
   attr_accessor :persister
 
+  include Vmdb::Logging
+
   def parse
     raise NotImplementedError, _("must be implemented in a subclass")
   end
