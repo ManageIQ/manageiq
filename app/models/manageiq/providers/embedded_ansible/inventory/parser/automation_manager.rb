93dff1ee954f345bd10f2a5594801f746e3a29d2
Adam Grare <agrare@redhat.com>
2018-08-24 15:39:24 -0400

    Update EmbeddedAnsible and Provider template

--- app/models/manageiq/providers/embedded_ansible/inventory/parser/automation_manager.rb
+++ app/models/manageiq/providers/embedded_ansible/inventory/parser/automation_manager.rb
@@ -1,3 +1,3 @@
-class ManageIQ::Providers::EmbeddedAnsible::Inventory::Parser::AutomationManager < ManagerRefresh::Inventory::Parser
+class ManageIQ::Providers::EmbeddedAnsible::Inventory::Parser::AutomationManager < ManageIQ::Providers::Inventory::Parser
   include ManageIQ::Providers::AnsibleTower::Shared::Inventory::Parser::AutomationManager
 end
