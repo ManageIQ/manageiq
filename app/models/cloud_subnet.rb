fd18082ae9be7ef0990a4c4e53ea5ae273cb3d1b
Miha Pleško <miha.plesko@xlab.si>
2018-08-24 15:35:27 +0200

    Inform Rails that SecurityGroup now belongs to Router/Subnet
    With this commit we inform Rails about two new foreign keys
    on :security_groups table:
    
    ```ruby
    security_group.cloud_subnet
    security_group.network_router
    
    cloud_subnet.security_groups
    network_router.security_groups
    ```
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- app/models/cloud_subnet.rb
+++ app/models/cloud_subnet.rb
@@ -18,6 +18,7 @@ class CloudSubnet < ApplicationRecord
   has_many :network_ports, :through => :cloud_subnet_network_ports, :dependent => :destroy
   has_many :vms, -> { distinct }, :through => :network_ports, :source => :device, :source_type => 'VmOrTemplate'
   has_many :cloud_subnets, :foreign_key => :parent_cloud_subnet_id
+  has_many :security_groups, :dependent => :nullify
 
   has_one :public_network, :through => :network_router, :source => :cloud_network
 
