5fc0e48c69774e37122eaa34ff6a2b3ac58a6885
Martin Slemr <mslemr@redhat.com>
2018-08-27 21:07:52 +0200

    Inventory Builder for TargetCollection

--- app/models/manageiq/providers/inventory/builder.rb
+++ app/models/manageiq/providers/inventory/builder.rb
@@ -14,15 +14,34 @@ class ManageIQ::Providers::Inventory::Builder
     # parser:    ManageIQ::Providers::Amazon::Inventory:Parser::CloudManager
     # persister: ManageIQ::Providers::Amazon::Inventory:Persister::CloudManager
     def build_inventory(ems, target)
-      manager_type = ManageIQ::Providers::Inflector.manager_type(target)
+      case target
+      when ManagerRefresh::TargetCollection
+        build_target_collection_inventory(ems, target)
+      else
+        manager_type = ManageIQ::Providers::Inflector.manager_type(target)
 
-      manager_type = default_manager_type unless allowed_manager_types.include?(manager_type)
+        manager_type = default_manager_type unless allowed_manager_types.include?(manager_type)
 
-      collector_type = "#{collector_class}::#{manager_type}Manager".safe_constantize
-      persister_type = "#{persister_class}::#{manager_type}Manager".safe_constantize
-      parser_type    = "#{parser_class}::#{manager_type}Manager".safe_constantize
+        collector_type = "#{collector_class}::#{manager_type}Manager".safe_constantize
+        persister_type = "#{persister_class}::#{manager_type}Manager".safe_constantize
+        parser_type    = "#{parser_class}::#{manager_type}Manager".safe_constantize
 
-      inventory(ems, target, collector_type, persister_type, [parser_type])
+        inventory(ems, target, collector_type, persister_type, [parser_type])
+      end
+    end
+
+    def build_target_collection_inventory(ems, target)
+      parsers = allowed_manager_types.collect do |manager_type|
+        "#{parser_class}::#{manager_type}Manager".safe_constantize
+      end
+
+      inventory(
+        ems,
+        target,
+        "#{collector_class}::TargetCollection".safe_constantize,
+        "#{persister_class}::TargetCollection".safe_constantize,
+        parsers
+      )
     end
 
     protected
@@ -50,32 +69,32 @@ class ManageIQ::Providers::Inventory::Builder
     # one of allowed_manager_types in name
     # Example: 'Cloud'
     def default_manager_type
-      raise NotImplementedError
+      allowed_manager_types.first
     end
 
     # Automatically chooses inventory class based on builder class
     # Example:
     # - ManageIQ::Providers::Amazon::Builder => ManageIQ::Providers::Amazon::Inventory
     def inventory_class
-      "#{ManageIQ::Providers::Inflector.provider_module(self)}::Inventory".safe_constantize
+      "#{ManageIQ::Providers::Inflector.provider_module(self)}::Inventory".constantize
     rescue
       ManageIQ::Providers::Inventory
     end
 
     def collector_class
-      "#{inventory_class}::Collector".safe_constantize
+      "#{inventory_class}::Collector".constantize
     rescue
       ManageIQ::Providers::Inventory::Collector
     end
 
     def parser_class
-      "#{inventory_class}::Parser".safe_constantize
+      "#{inventory_class}::Parser".constantize
     rescue
       ManageIQ::Providers::Inventory::Collector
     end
 
     def persister_class
-      "#{inventory_class}::Persister".safe_constantize
+      "#{inventory_class}::Persister".constantize
     rescue
       ManageIQ::Providers::Inventory::Collector
     end
