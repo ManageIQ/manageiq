c13232722edabf148e84b53cca998d12b4929641
Aparna Karve <akarve@redhat.com>
2018-09-17 12:48:32 -0700

    Extract `validate_config_info` in a module
    `validate_config_info` needs to be a Class method for Create and Instance method for Update

--- app/models/service_template_transformation_plan.rb
+++ app/models/service_template_transformation_plan.rb
@@ -1,4 +1,5 @@
 class ServiceTemplateTransformationPlan < ServiceTemplate
+  include_concern 'ValidateConfigInfo'
   def request_class
     ServiceTemplateTransformationPlanRequest
   end
@@ -70,41 +71,4 @@ class ServiceTemplateTransformationPlan < ServiceTemplate
   def enforce_single_service_parent(_resource)
   end
 
-  def self.validate_config_info(options)
-    config_info = options[:config_info]
-
-    mapping = if config_info[:transformation_mapping_id]
-                TransformationMapping.find(config_info[:transformation_mapping_id])
-              else
-                config_info[:transformation_mapping]
-              end
-
-    raise _('Must provide an existing transformation mapping') if mapping.blank?
-
-    pre_service_id  = config_info[:pre_service].try(:id) || config_info[:pre_service_id]
-    post_service_id = config_info[:post_service].try(:id) || config_info[:post_service_id]
-
-    vms = []
-    if config_info[:actions]
-      vm_objects = VmOrTemplate.where(:id => config_info[:actions].collect { |vm_hash| vm_hash[:vm_id] }.compact).index_by(&:id).stringify_keys
-      config_info[:actions].each do |vm_hash|
-        vm_obj = vm_objects[vm_hash[:vm_id]] || vm_hash[:vm]
-        next if vm_obj.nil?
-
-        vm_options = {}
-        vm_options[:pre_ansible_playbook_service_template_id] = pre_service_id if vm_hash[:pre_service]
-        vm_options[:post_ansible_playbook_service_template_id] = post_service_id if vm_hash[:post_service]
-        vms << {:vm => vm_obj, :options => vm_options}
-      end
-    end
-
-    raise _('Must select a list of valid vms') if vms.blank?
-
-    {
-      :transformation_mapping => mapping,
-      :vms                    => vms,
-      :provision              => config_info[:provision] || {}
-    }
-  end
-  private_class_method :validate_config_info
 end
