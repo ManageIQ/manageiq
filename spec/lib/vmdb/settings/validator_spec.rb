9c2aa38e0b1c59e766defd76ce78e0978a642269
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-09-19 10:17:44 +0200

    Rubocop fixes for moved files
    This commit cleans up the files that were moved in previous commit in
    order to keep miq-bot at bay.

--- spec/lib/vmdb/settings/validator_spec.rb
+++ spec/lib/vmdb/settings/validator_spec.rb
@@ -29,9 +29,9 @@ describe Vmdb::Settings::Validator do
     {:webservices => {:timeout => 123}},   true,
     {:webservices => {:timeout => "xxx"}}, false,
 
-    {:authentication => {:mode => "ldaps"}}, false,
+    {:authentication => {:mode => "ldaps"}},                     false,
     {:authentication => {:mode => "ldaps", :ldaphost => "foo"}}, true,
-    {:authentication => {:mode => "xxx"}},   false,
+    {:authentication => {:mode => "xxx"}},                       false,
 
     {:authentication => {:mode => "ldap", :ldaphost => "foo"}}, true,
     {:authentication => {:mode => "ldap", :ldaphost => nil}},   false,
