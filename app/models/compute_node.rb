a4d8690afb7b24168da5b63c5a4f35c17d3ba125
Brandon Dunne <brandondunne@hotmail.com>
2018-09-20 11:41:02 -0400

    I guess this isn't being used since there's no table for it

--- app/models/compute_node.rb
+++
@@ -1,7 +0,0 @@
-class ComputeNode < ApplicationRecord
-  include NewWithTypeStiMixin
-
-  acts_as_miq_taggable
-
-  belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::PhysicalInfraManager"
-end
