6a809cbd3dfbaccc89ff53f384bea797e3166508
Gregg Tanzillo <gtanzill@redhat.com>
2018-09-26 17:51:58 -0400

    Merge pull request #17842 from jrafanie/detect_long_running_requests
    Detect and log long running http(s) requests
    
    (cherry picked from commit cc6e263931c3b6bffc60af64168f9ba26207f2a9)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1614918

--- app/models/mixins/miq_web_server_runner_mixin.rb
+++ app/models/mixins/miq_web_server_runner_mixin.rb
@@ -33,4 +33,22 @@ module MiqWebServerRunnerMixin
       server.start
     end
   end
+
+  def do_heartbeat_work
+    log_long_running_requests
+  end
+
+  CHECK_LONG_RUNNING_REQUESTS_INTERVAL = 30.seconds
+  def log_long_running_requests
+    @last_checked_hung_requests ||= Time.now.utc
+    return if @last_checked_hung_requests > CHECK_LONG_RUNNING_REQUESTS_INTERVAL.ago
+
+    RequestStartedOnMiddleware.long_running_requests.each do |request, duration, thread|
+      message = "Long running http(s) request: '#{request}' handled by ##{Process.pid}:#{thread.object_id.to_s(16)}, running for #{duration.round(2)} seconds"
+      _log.warn(message)
+      Rails.logger.warn(message)
+    end
+
+    @last_checked_hung_requests = Time.now.utc
+  end
 end
