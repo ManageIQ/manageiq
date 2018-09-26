3d81d8e6a3e8c9159a3a38e1974d3c1cdc60cf1d
Brandon Dunne <brandondunne@hotmail.com>
2018-09-26 11:25:42 -0400

    update comment

--- app/models/miq_worker.rb
+++ app/models/miq_worker.rb
@@ -372,7 +372,7 @@ class MiqWorker < ApplicationRecord
   end
 
   def self.containerized_worker?
-    # un-rearch containers for Ivanchuk
+    # un-rearch containers until further notice
     return false
     MiqEnvironment::Command.is_podified? && supports_container?
   end
