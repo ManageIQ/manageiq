067f2e11e9fe726a07294069ed0441a8b0b2dda6
Fabien Dupont <fdupont@redhat.com>
2018-10-05 10:13:49 -0400

    Backport methods from Automate to conversion_host - part2

--- app/models/conversion_host.rb
+++ app/models/conversion_host.rb
@@ -29,7 +29,41 @@ class ConversionHost < ApplicationRecord
     return 'ssh' if ssh_transport_supported
   end
 
+  def ipaddresses
+    resource.ipaddresses.unshift(address).unshift(resource.try(:ipaddress)).reject(&:blank?)
+  end 
+
+  def ipaddress(family = nil)
+    ips = ipaddresses
+    return ips.first unless %w(ipv4 ipv6).include?(family)
+    ips.select { |ip| IPAddr.new(ip).send("#{family}?") }.first
+  end 
+
+  def ssh_credentials
+    send("ssh_credentials_#{resource.type.gsub('::', '_').downcase}")
+  end 
+
+  def run_conversion(conversion_options)
+    connect_ssh.su_exec('/usr/bin/virt-v2v-wrapper.py', nil, conversion_options.to_json)
+  end
+
+  def conversion_log(path)
+    unless check_resource_credentials
+      msg = "Credential was not found for host #{host.resource.name}. Download of transformation log aborted."
+      _log.error(msg)
+      raise MiqException::Error, msg 
+    end 
+
+    begin
+      Net::SCP.download!(ipaddress, resourcece.authentication_userid, path, nil, :ssh => {:password => resource.authentication_password})
+    rescue Net::SCP::Error => scp_err
+      _log.error("Download of transformation log for #{description} with ID [#{id}] failed with error: #{scp_err.message}")
+      raise scp_err
+    end 
+  end 
+
   private
+
 
   def check_resource_credentials_rhevm
     !(resource.authentication_userid.nil? || resource.authentication_password.nil?)
@@ -41,4 +75,39 @@ class ConversionHost < ApplicationRecord
                                   .where.not(:userid => nil, :auth_key => nil)
     !ssh_authentications.empty?
   end
+
+  def ssh_credentials_options_manageiq_providers_redhat_inframanager_host
+    raise "Userid for #{resource.name} is empty" if resource.authentication_userid.blank?
+    raise "Password for #{resource.name} is empty" if resource.authentication_password.blank?
+    return resource.authentication_userid, resource.authentication_password, nil, nil, {}
+  end 
+
+  def ssh_credentials_manageiq_providers_openstack_cloudmanager_vm
+    ems = resource.ext_management_system
+    authentication = ems.authentication.where(:authtype => 'ssh_keypair').first
+    raise "SSH authentication for #{ems.name} does not exist" if authentication.nil?
+    raise "Authentication user for #{ems.name} is empty" if authentication.userid.blank?
+    raise "Authentication private key for #{ems.name} is empty" if authentication.auth_key.blank?
+    return authentication.userid, nil, 'root', nil, { :key_data => authentication.auth_key, :keys_only => true, :passwordless_sudo => true }
+  end
+
+  def connect_ssh(options = {}) 
+    require 'MiqSshUtil'
+
+    rl_user, rl_password, su_user, su_password, additional_options = ssh_credentials
+    options.merge!(ssh_session_options)
+
+    prompt_delay = ::Settings.ssh.try(:authentication_prompt_delay)
+    options[:authentication_prompt_delay] = prompt_delay unless prompt_delay.nil?
+
+    users = su_user.nil? ? rl_user : "#{rl_user}/#{su_user}"
+    _log.info("Initiating SSH connection to Host:[#{name}] using [#{hostname}] for user:[#{users}].  Options:[#{logged_options.inspect}]")
+    begin
+      MiqSshUtil.shell_with_su(ipaddress, rl_user, rl_password, su_user, su_password, options) do |ssu, _shell|
+        yield(ssu)
+      end  
+    rescue Exception => err 
+      raise err 
+    end  
+  end 
 end
