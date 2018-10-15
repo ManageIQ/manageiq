90da9fa8124dcfef7ffe6473b7571f82f4ff34e8
Fabien Dupont <fdupont@redhat.com>
2018-10-15 15:13:02 -0400

    Add allow_nil to delegate

--- app/models/conversion_host.rb
+++ app/models/conversion_host.rb
@@ -6,7 +6,7 @@ class ConversionHost < ApplicationRecord
   belongs_to :resource, :polymorphic => true
   has_many :service_template_transformation_plan_tasks, :dependent => :nullify
   has_many :active_tasks, -> { where(:state => 'active') }, :class_name => ServiceTemplateTransformationPlanTask, :inverse_of => :conversion_host
-  delegate :ext_management_system, :to => :resource
+  delegate :ext_management_system, :to => :resource, :allow_nil => true
 
   # To be eligible, a conversion host must have the following properties
   #  - A transport mechanism is configured for source (set by 3rd party)
