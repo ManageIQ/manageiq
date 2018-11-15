5621fd78608fe7be6cce8fb1194b19e5efa08b61
d-m-u <drewuhlmann@gmail.com>
2018-11-15 08:45:39 -0500

    Add owner to orch stack model provisioning

--- spec/models/manageiq/providers/cloud_manager/orchestration_stack_spec.rb
+++ spec/models/manageiq/providers/cloud_manager/orchestration_stack_spec.rb
@@ -15,6 +15,32 @@ describe ManageIQ::Providers::CloudManager::OrchestrationStack do
     end
   end
 
+  describe 'set_tenant_from_group' do
+    before { Tenant.seed }
+    let(:tenant1) { FactoryGirl.create(:tenant) }
+    let(:tenant2) { FactoryGirl.create(:tenant) }
+    let(:group1) { FactoryGirl.create(:miq_group, :tenant => tenant1) }
+    let(:group2) { FactoryGirl.create(:miq_group, :tenant => tenant2) }
+
+    it "assigns the tenant from the group" do
+      expect(FactoryGirl.create(:orchestration_stack, :miq_group => group1).tenant).to eq(tenant1)
+    end
+
+    it "assigns the tenant from the group_id" do
+      expect(FactoryGirl.create(:orchestration_stack, :miq_group_id => group1.id).tenant).to eq(tenant1)
+    end
+
+    it "assigns the tenant from the group over the tenant" do
+      expect(FactoryGirl.create(:orchestration_stack, :miq_group => group1, :tenant_id => tenant2).tenant).to eq(tenant1)
+    end
+
+    it "changes the tenant after changing the group" do
+      stack = FactoryGirl.create(:orchestration_stack, :miq_group => group1)
+      stack.update_attributes(:miq_group_id => group2.id)
+      expect(stack.tenant).to eq(tenant2)
+    end
+  end
+
   describe 'direct_<resource> methods' do
     it 'defines a set of methods for vms' do
       expect(root_stack.direct_vms.size).to eq(1)
