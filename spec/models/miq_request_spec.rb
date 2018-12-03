740e6b551760f071217fe25b5ad1296a2a3d9d37
Gregg Tanzillo <gtanzill@redhat.com>
2018-12-03 14:50:54 -0500

    Merge pull request #18257 from kbrock/fix-miq_request-ownership
    Fix miq request ownership
    
    (cherry picked from commit aa6fba547ce4fc19e58869e282f836eeb08c44ec)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1647169

--- spec/models/miq_request_spec.rb
+++ spec/models/miq_request_spec.rb
@@ -563,4 +563,32 @@ describe MiqRequest do
       expect(orch_stack_request.class::SOURCE_CLASS_NAME).to eq('OrchestrationStack')
     end
   end
+
+  describe ".user_owned" do
+    let(:regional_id) { ApplicationRecord.id_in_region(1, ApplicationRecord.my_region_number + 1) }
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:regional_user) { FactoryGirl.create(:user, :userid => user.userid.upcase, :id => regional_id) }
+    let!(:request) { FactoryGirl.create(:vm_migrate_request, :requester => user) }
+    let!(:regional_request) { FactoryGirl.create(:vm_migrate_request, :requester => regional_user) }
+    it "finds request for cross region users" do
+      FactoryGirl.create(:vm_migrate_request, :requester => FactoryGirl.create(:user))
+      FactoryGirl.create(:vm_migrate_request, :requester => FactoryGirl.create(:user), :id => regional_id + 1)
+      expect(MiqRequest.user_owned(regional_user)).to match_array([request, regional_request])
+    end
+  end
+
+  describe ".group_owned" do
+    let(:regional_id) { ApplicationRecord.id_in_region(1, ApplicationRecord.my_region_number + 1) }
+    let(:group) { FactoryGirl.create(:miq_group) }
+    let(:regional_group) { FactoryGirl.create(:miq_group, :id => regional_id) }
+    let(:user) { FactoryGirl.create(:user, :miq_groups => [group]) }
+    let!(:regional_user) { FactoryGirl.create(:user, :userid => user.userid.upcase, :id => regional_id, :miq_groups => [regional_group]) }
+    let!(:request) { FactoryGirl.create(:vm_migrate_request, :requester => user) }
+    let!(:regional_request) { FactoryGirl.create(:vm_migrate_request, :requester => regional_user) }
+    it "finds request for cross region groups" do
+      FactoryGirl.create(:vm_migrate_request, :requester => FactoryGirl.create(:user))
+      FactoryGirl.create(:vm_migrate_request, :requester => FactoryGirl.create(:user), :id => regional_id + 1)
+      expect(MiqRequest.user_owned(regional_user)).to match_array([request, regional_request])
+    end
+  end
 end
