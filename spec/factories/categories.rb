7c5f3d1f87712b005dd6766f485f2fd6c123d1bf
Keenan Brock <keenan@thebrocks.net>
2018-12-18 07:35:23 -0500

    Classification: Use nil for no parent

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory(:category) do
     sequence(:name)        { |n| "category_#{seq_padded_for_sorting(n)}" }
     sequence(:description) { |n| "category #{seq_padded_for_sorting(n)}" }
-    parent_id { 0 }
   end
 end
