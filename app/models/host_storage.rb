afddd9479639d504bd157260e7377359b9c75bd7
Adam Grare <agrare@redhat.com>
2019-01-24 10:23:12 -0500

    Add method for restricting to accessible storages
    Use the host_storage.accessible property to filter storages that cannot
    be accessed by the provider due to be disconnected or otherwise
    inaccessible.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1668020

--- app/models/host_storage.rb
+++ app/models/host_storage.rb
@@ -1,4 +1,16 @@
 class HostStorage < ApplicationRecord
   belongs_to :host
   belongs_to :storage
+
+  scope :writable, -> { where(:read_only => [nil, false]) }
+  scope :read_only, -> { where(:read_only => true) }
+
+  scope :accessible, -> { where(:accessible => [true, nil]) }
+  scope :inaccessible, -> { where(:accessible => false) }
+
+  class << self
+    def writable_accessible
+      writable.accessible
+    end
+  end
 end
