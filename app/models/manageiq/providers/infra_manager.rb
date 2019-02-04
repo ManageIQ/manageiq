7a1d802bb43cee2561ca60335b9bca0e22e4f801
Adam Grare <agrare@redhat.com>
2019-02-04 10:29:12 -0500

    Add distributed_virtual_switches model

--- app/models/manageiq/providers/infra_manager.rb
+++ app/models/manageiq/providers/infra_manager.rb
@@ -7,6 +7,9 @@ module ManageIQ::Providers
 
     include AvailabilityMixin
 
+    has_many :distributed_virtual_switches, :dependent => :destroy, :foreign_key => :ems_id, :inverse_of => :ext_management_system
+    has_many :host_virtual_switches, -> { distinct }, :through => :hosts
+
     has_many :host_hardwares,             :through => :hosts, :source => :hardware
     has_many :host_operating_systems,     :through => :hosts, :source => :operating_system
     has_many :host_storages,              :through => :hosts
