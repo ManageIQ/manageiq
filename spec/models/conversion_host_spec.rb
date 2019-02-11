cc20f663c1cbd1d023a951cecbe7aca8e7d939ab
Daniel Berger <dberger@redhat.com>
2019-02-11 10:07:47 -0500

    Use provider-specific factories instead of ignoring validations.

--- spec/models/conversion_host_spec.rb
+++ spec/models/conversion_host_spec.rb
@@ -4,10 +4,11 @@ describe ConversionHost do
   let(:apst) { FactoryBot.create(:service_template_ansible_playbook) }
 
   context "provider independent methods" do
-    let(:host) { FactoryBot.create(:host) }
-    let(:vm) { FactoryBot.create(:vm_or_template) }
-    let(:conversion_host_1) { FactoryBot.create(:conversion_host, :skip_validate, :resource => host) }
-    let(:conversion_host_2) { FactoryBot.create(:conversion_host, :skip_validate, :resource => vm) }
+    let(:ems) { FactoryBot.create(:ems_redhat, :zone => FactoryBot.create(:zone), :api_version => '4.2.4') }
+    let(:host) { FactoryBot.create(:host_redhat, :ext_management_system => ems) }
+    let(:vm) { FactoryBot.create(:vm_openstack) }
+    let(:conversion_host_1) { FactoryBot.create(:conversion_host, :resource => host) }
+    let(:conversion_host_2) { FactoryBot.create(:conversion_host, :resource => vm) }
     let(:task_1) { FactoryBot.create(:service_template_transformation_plan_task, :state => 'active', :conversion_host => conversion_host_1) }
     let(:task_2) { FactoryBot.create(:service_template_transformation_plan_task, :conversion_host => conversion_host_1) }
     let(:task_3) { FactoryBot.create(:service_template_transformation_plan_task, :state => 'active', :conversion_host => conversion_host_2) }
