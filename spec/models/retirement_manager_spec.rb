2aa3228428546cfd16f4d67319fb5aac01b7d25e
Greg McCullough <gmccullo@redhat.com>
2019-02-11 12:04:57 -0500

    Merge pull request #18443 from d-m-u/remove_lbs_from_retirement_check
    Don't retire load balancers
    
    (cherry picked from commit d8a6ba00be00d215a44af0f71e27f7ad364967ba)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1678476

--- spec/models/retirement_manager_spec.rb
+++ spec/models/retirement_manager_spec.rb
@@ -4,8 +4,6 @@ describe RetirementManager do
       _, _, zone = EvmSpecHelper.local_guid_miq_server_zone
       ems = FactoryGirl.create(:ems_network, :zone => zone)
 
-      load_balancer = FactoryGirl.create(:load_balancer, :retires_on => Time.zone.today + 1.day, :ext_management_system => ems)
-      FactoryGirl.create(:load_balancer, :retired => true)
       orchestration_stack = FactoryGirl.create(:orchestration_stack, :retires_on => Time.zone.today + 1.day, :ext_management_system => ems)
       FactoryGirl.create(:orchestration_stack, :retired => true)
       vm = FactoryGirl.create(:vm, :retires_on => Time.zone.today + 1.day, :ems_id => ems.id)
@@ -13,7 +11,7 @@ describe RetirementManager do
       service = FactoryGirl.create(:service, :retires_on => Time.zone.today + 1.day)
       FactoryGirl.create(:service, :retired => true)
 
-      expect(RetirementManager.check).to match_array([load_balancer, orchestration_stack, vm, service])
+      expect(RetirementManager.check).to match_array([orchestration_stack, vm, service])
     end
   end
 end
