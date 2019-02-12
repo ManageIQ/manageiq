0e4e02e00cbeb240d6c4d507fb933a9e149b34cf
Martin Slemr <mslemr@redhat.com>
2019-02-12 08:59:07 -0500

    Targeted refresh spec for EmbeddedAnsible

--- spec/models/manageiq/providers/embedded_ansible/automation_manager/refresher_targeted_spec.rb
+++ spec/models/manageiq/providers/embedded_ansible/automation_manager/refresher_targeted_spec.rb
@@ -3,10 +3,9 @@ describe ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Refresher do
     EvmSpecHelper.assign_embedded_ansible_role
   end
 
-  # TODO: uncomment later
-  # it_behaves_like 'refresh targeted',
-  #                 :provider_embedded_ansible,
-  #                 described_class.parent,
-  #                 :embedded_ansible,
-  #                 ManageIQ::Providers::AnsibleTower::AutomationManager::Refresher.name.underscore + '_targeted'
+  it_behaves_like 'refresh targeted',
+                  :provider_embedded_ansible,
+                  described_class.parent,
+                  :embedded_ansible,
+                  ManageIQ::Providers::AnsibleTower::AutomationManager::Refresher.name.underscore + '_targeted'
 end
