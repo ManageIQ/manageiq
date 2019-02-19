ac1ce62fa8652276361df5e04748b11cae227cee
Adam Grare <agrare@redhat.com>
2019-02-19 11:56:22 -0500

    Remove old ManagerRefresher class

--- app/models/manageiq/providers/embedded_ansible/automation_manager/refresher.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/refresher.rb
@@ -1,4 +1,4 @@
-class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Refresher < ManageIQ::Providers::BaseManager::ManagerRefresher
+class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Refresher < ManageIQ::Providers::BaseManager::Refresher
   include ManageIQ::Providers::AnsibleTower::Shared::AutomationManager::Refresher
 
   def self.display_name(number = 1)
