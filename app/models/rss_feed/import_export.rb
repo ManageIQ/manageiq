b9851bb2c2c8a4e68caeb705e7e15c5c6b4ac2ea
Julian Cheal <jcheal@redhat.com>
2019-02-27 13:19:25 +0000

    Removing RSS Specs and lib files

--- app/models/rss_feed/import_export.rb
+++
@@ -1,9 +0,0 @@
-module RssFeed::ImportExport
-  extend ActiveSupport::Concern
-
-  def export_to_array
-    h = attributes
-    ["id", "created_on", "updated_on", "yml_file_mtime"].each { |k| h.delete(k) }
-    [self.class.to_s => h]
-  end
-end
--- lib/resource_feeder/lib/resource_feeder.rb
+++
@@ -1,3 +0,0 @@
-$:.push(File.dirname(__FILE__))
-require 'resource_feeder/rss'
-require 'resource_feeder/atom'
