ebe26451edcdbc14d51c8b8a6c6225ffeaee199d
Brandon Dunne <brandondunne@hotmail.com>
2019-02-28 17:20:45 -0500

    Switch to default_value_for rather than custom logic

--- lib/uuid_mixin.rb
+++ lib/uuid_mixin.rb
@@ -1,17 +1,12 @@
 module UuidMixin
   extend ActiveSupport::Concern
   included do
-    before_validation :set_guid, :on => :create if respond_to?(:before_validation)
+    default_value_for(:guid) { SecureRandom.uuid }
   end
 
   private
 
-  def set_guid
-    self.guid ||= SecureRandom.uuid if self.respond_to?(:guid) && self.respond_to?(:guid=)
-  end
-
   def default_name_to_guid
-    set_guid
     self.name ||= self.guid if self.respond_to?(:guid) && self.respond_to?(:name) && self.respond_to?(:name=)
   end
 end
