b85a83f8b0dc06fc53efd9eab874405a53cbc381
Adam Grare <agrare@redhat.com>
2019-03-05 16:22:22 -0500

    Merge pull request #18521 from jameswnl/fix-spec
    Fixing a spec failure
    
    (cherry picked from commit 1817c2088ad52016a53c119291a13692cc4be2de)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1686877

--- spec/factories/job.rb
+++ spec/factories/job.rb
@@ -3,5 +3,5 @@ FactoryBot.define do
     sequence(:name) { |n| "job_#{seq_padded_for_sorting(n)}" }
   end
 
-  factory :infra_conversion_job, :parent => :job
+  factory :infra_conversion_job, :class => 'InfraConversionJob', :parent => :job
 end
