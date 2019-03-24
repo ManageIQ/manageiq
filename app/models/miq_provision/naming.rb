5abe384bd228865dcbea28f8c1c255fa91d4e2b0
Greg McCullough <gmccullo@redhat.com>
2019-03-24 09:38:27 -0400

    Split get_vm_full_name method for easier testing

--- app/models/miq_provision/naming.rb
+++ app/models/miq_provision/naming.rb
@@ -18,21 +18,21 @@ module MiqProvision::Naming
     end
 
     def get_vm_full_name(unresolved_vm_name, prov_obj, determine_index)
-      # Split name to find the index substitution string
-      if unresolved_vm_name =~ NAME_SEQUENCE_REGEX
-        name = {:prefix => $`, :suffix => $', :index => $&, :index_length => Regexp.last_match(1).to_i}
-      else
-        # If we did not find the index substitution string just return what was passed in
-        return unresolved_vm_name
-      end
+      return unresolved_vm_name unless unresolved_vm_name =~ NAME_SEQUENCE_REGEX
+
+      name = {:prefix => $`, :suffix => $', :index => $&, :index_length => Regexp.last_match(1).to_i}
 
       # if we are just building a sample of what the vm_name will look like use '#' inplace of actual number.
       return "#{name[:prefix]}#{'#' * name[:index_length]}#{name[:suffix]}" if determine_index == false
 
+      resolve_vm_name(name, unresolved_vm_name, prov_obj)
+    end
+
+    def resolve_vm_name(name, unresolved_vm_name, prov_obj)
       index_length = name[:index_length]
       loop do
         next_number = MiqRegion.my_region.next_naming_sequence(unresolved_vm_name, SOURCE_IDENTIFIER)
-        idx_str = "%0#{index_length}d" % next_number
+        idx_str = next_number.to_s.rjust(index_length, '0')
         if idx_str.length > index_length
           index_length += 1
           unresolved_vm_name = "#{name[:prefix]}$n{#{index_length}}#{name[:suffix]}"
@@ -40,16 +40,14 @@ module MiqProvision::Naming
         end
 
         fullname = "#{name[:prefix]}#{idx_str}#{name[:suffix]}"
-        vm       = check_vm_name_uniqueness(fullname, prov_obj)
-        return fullname if vm.nil?
+        return fullname if vm_name_unique?(fullname, prov_obj)
       end
     end
 
-    def check_vm_name_uniqueness(fullname, prov_obj)
-      return nil if prov_obj.vm_template.nil?
-      ems = prov_obj.vm_template.ext_management_system
-      return nil if ems.nil?
-      VmOrTemplate.find_by("ems_id = ? and lower(name) = ?", ems.id, fullname.downcase)
+    def vm_name_unique?(fullname, prov_obj)
+      ems = prov_obj&.vm_template&.ext_management_system
+      return true if ems.nil?
+      !VmOrTemplate.find_by("ems_id = ? and lower(name) = ?", ems.id, fullname.downcase)
     end
   end
 
