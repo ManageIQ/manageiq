b06b3427bb4d9214c3f9e47c9ecb1f224a417543
Adam Grare <agrare@redhat.com>
2019-04-02 13:02:40 -0400

    Add a dry-run option

--- tools/reconnect_vms.rb
+++ tools/reconnect_vms.rb
@@ -6,6 +6,7 @@ opts = Optimist.options do
   opt :ems_id, "The ID of the ExtManagementSystem to reconnect VMs for", :type => :integer
   opt :ems_name, "The name of the ExtManagementSystem to reconnect VMs for", :type => :string
   opt :by, "Property to treat as unique, defaults to :uid_ems", :type => :string, :default => "uid_ems"
+  opt :dry_run, "Just print out what would be done without modifying anything", :type => :boolean, :default => true
 end
 
 if opts[:ems_id].nil? && opts[:ems_name].nil?
@@ -32,6 +33,14 @@ vms_index = VmOrTemplate.where(opts[:by] => ems.vms_and_templates.pluck(opts[:by
 duplicate_vms = vms_index.select { |_by, vms| vms.count == 2 && vms.select(&:active).count == 1 }
 puts "Found #{duplicate_vms.count} duplicate VMs..."
 
+if opts[:dry_run]
+  puts "**** This is a dry-run, nothing will be updated! ****"
+else
+  puts "**** THIS WILL MODIFY YOUR VMS ****"
+  puts "     Press Enter to Continue: "
+  STDIN.getc
+end
+
 activated_vms = duplicate_vms.map do |_by, vms|
   active_vms, inactive_vms = vms.partition(&:active)
 
@@ -43,11 +52,11 @@ activated_vms = duplicate_vms.map do |_by, vms|
   next if active_vm.created_on < inactive_vm.created_on # Always pick the older VM
 
   # Disconnect the new VM and activate the old VM
-  puts "Disconnecting Vm [#{active_vm.name}] id [#{active_vm.name}] uid_ems [#{active_vm.uid_ems}] ems_ref [#{active_vm.ems_ref}]"
-  active_vm.disconnect_inv
+  puts "Disconnecting Vm [#{active_vm.name}] id [#{active_vm.id}] uid_ems [#{active_vm.uid_ems}] ems_ref [#{active_vm.ems_ref}]"
+  active_vm.disconnect_inv unless opts[:dry_run]
 
-  puts "Activating Vm [#{inactive_vm.name}] id [#{inactive_vm.id}] uid_ems [#{inactive_vm.uid_ems}] ems_ref [#{inactive_vm.ems_ref}]"
-  inactive_vm.update_attributes!(:ems_id => ems_id, :uid_ems => active_vm.uid_ems, :ems_ref => active_vm.ems_ref)
+  puts "Activating Vm    [#{inactive_vm.name}] id [#{inactive_vm.id}] uid_ems [#{inactive_vm.uid_ems}] ems_ref [#{inactive_vm.ems_ref}]"
+  inactive_vm.update_attributes!(:ems_id => ems_id, :uid_ems => active_vm.uid_ems, :ems_ref => active_vm.ems_ref) unless opts[:dry_run]
 
   inactive_vm
 end.compact
