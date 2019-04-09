8fd4b217df1cdf45821abca7edae5781313493fe
Jason Frey <jfrey@redhat.com>
2019-04-09 13:40:37 -0400

    Change simulate_queue_worker for quiet polling

--- lib/vmdb/console_methods.rb
+++ lib/vmdb/console_methods.rb
@@ -12,16 +12,27 @@ module Vmdb
       ActiveRecord::Base.logger.level == 0 ? disable_console_sql_logging : enable_console_sql_logging
     end
 
+    def with_console_sql_logging_level(level)
+      old_level = ActiveRecord::Base.logger.level
+      ActiveRecord::Base.logger.level = level
+      yield
+    ensure
+      ActiveRecord::Base.logger.level = old_level
+    end
+
     def backtrace(include_external = false)
       caller.select { |path| include_external || path.start_with?(Rails.root.to_s) }
     end
 
     # Development helper method for Rails console for simulating queue workers.
-    def simulate_queue_worker(break_on_complete = false)
+    def simulate_queue_worker(break_on_complete: false, quiet_polling: true)
       raise NotImplementedError, "not implemented in production mode" if Rails.env.production?
       loop do
-        q = MiqQueue.where(MiqQueue.arel_table[:queue_name].not_eq("miq_server")).order(:id).first
+        q = with_console_sql_logging_level(quiet_polling ? 1 : ActiveRecord::Base.logger.level) do
+          MiqQueue.where(MiqQueue.arel_table[:queue_name].not_eq("miq_server")).order(:id).first
+        end
         if q
+          puts "\e[33;1m\n** Delivering #{MiqQueue.format_full_log_msg(q)}\n\e[0;m"
           status, message, result = q.deliver
           q.delivered(status, message, result) unless status == MiqQueue::STATUS_RETRY
         else
