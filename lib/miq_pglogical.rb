c826707df42489c24f18b46dc621149fc0720168
Nick Carboni <ncarboni@redhat.com>
2019-04-23 16:43:28 -0400

    Use the new logical replication gem in MiqPglogical
    Most notably, the concept of a node goes away and we can set the
    tables in a publication directly rather than adding and removing
    them one by one.
    
    This also eliminates the need for a lock around the publication
    because the operation is atomic.

--- lib/miq_pglogical.rb
+++ lib/miq_pglogical.rb
@@ -1,90 +1,58 @@
 require 'pg'
-require 'pg/pglogical'
-require 'pg/pglogical/active_record_extension'
+require 'pg/logical_replication'
 
 class MiqPglogical
   include Vmdb::Logging
 
   PUBLICATION_NAME = 'miq'.freeze
-  NODE_PREFIX = "region_".freeze
   ALWAYS_EXCLUDED_TABLES = %w(ar_internal_metadata schema_migrations repl_events repl_monitor repl_nodes).freeze
 
   attr_reader :configured_excludes
 
   def initialize
-    @connection = ApplicationRecord.connection
+    @connection = ApplicationRecord.connection.raw_connection
     self.configured_excludes = provider? ? active_excludes : self.class.default_excludes
   end
 
-  # Sets the tables that should be used to create the replication set using refresh_excludes
+  # Sets the tables that should be used to create the publication using refresh_excludes
   def configured_excludes=(new_excludes)
     @configured_excludes = new_excludes | ALWAYS_EXCLUDED_TABLES
   end
 
-  # Returns the excluded tables that are currently being used by pglogical
+  # Returns the excluded tables that are currently being used
   # @return Array<String> the table list
   def active_excludes
     return [] unless provider?
-    @connection.tables - included_tables
+    ApplicationRecord.connection.tables - included_tables
   end
 
-  # Returns whether or not this server is configured as a provider node
-  # @return Boolean
   def provider?
-    pglogical.enabled? && pglogical.replication_sets.include?(PUBLICATION_NAME)
+    pglogical.publications.map { |p| p["name"] }.include?(PUBLICATION_NAME)
   end
 
-  # Returns whether or not this server is configured as a subscriber node
-  # @return Boolean
   def subscriber?
-    pglogical.enabled? && !pglogical.subscriptions.empty?
+    !pglogical.subscriptions.empty?
   end
 
-  # Returns whether or not this server is a pglogical node
-  def node?
-    pglogical.enabled? && pglogical.nodes.field_values("name").include?(self.class.local_node_name)
-  end
-
-  # Creates a pglogical node using the rails connection
-  def create_node
-    pglogical.node_create(self.class.local_node_name, connection_dsn)
-  end
-
-  # Drops the pglogical node associated with this connection
-  def drop_node
-    pglogical.node_drop(self.class.local_node_name, true)
-  end
-
-  # Configures the database as a pglogical replication source
-  #   This includes enabling the extension, creating the
-  #   node and creating the replication set
   def configure_provider
     return if provider?
-    @connection.transaction(:requires_new => true) do
-      pglogical.enable
-      create_node unless node?
-      create_replication_set
-    end
+    create_replication_set
   end
 
-  # Removes the replication configuration and pglogical node from the
-  # database
   def destroy_provider
     return unless provider?
-    pglogical.replication_set_drop(PUBLICATION_NAME)
-    drop_node
-    pglogical.disable
+    pglogical.drop_publication(PUBLICATION_NAME)
   end
 
-  # Lists the tables currently being replicated by pglogical
+  # Lists the tables currently being replicated
   # @return Array<String> the table list
   def included_tables
-    pglogical.tables_in_replication_set(PUBLICATION_NAME)
+    pglogical.tables_in_publication(PUBLICATION_NAME)
   end
 
-  # Creates the 'miq' replication set and refreshes the excluded tables
+  # Creates the 'miq' publication and refreshes the excluded tables
   def create_replication_set
-    pglogical.replication_set_create(PUBLICATION_NAME)
+    pglogical.create_publication(PUBLICATION_NAME)
     refresh_excludes
   end
 
@@ -102,21 +70,10 @@ class MiqPglogical
     pgl.refresh_excludes
   end
 
-  # Aligns the contents of the 'miq' replication set with the currently configured vmdb excludes
+  # Aligns the contents of the 'miq' publication with the currently configured excludes
   def refresh_excludes
-    pglogical.with_replication_set_lock(PUBLICATION_NAME) do
-      # remove newly excluded tables from replication set
-      newly_excluded_tables.each do |table|
-        _log.info("Removing #{table} from #{PUBLICATION_NAME} replication set")
-        pglogical.replication_set_remove_table(PUBLICATION_NAME, table)
-      end
-
-      # add tables to the set which are no longer excluded (or new)
-      newly_included_tables.each do |table|
-        _log.info("Adding #{table} to #{PUBLICATION_NAME} replication set")
-        pglogical.replication_set_add_table(PUBLICATION_NAME, table, true)
-      end
-    end
+    tables = ApplicationRecord.connection.tables - configured_excludes
+    pglogical.set_publication_tables(PUBLICATION_NAME, tables)
   end
 
   def replication_lag
@@ -125,18 +82,6 @@ class MiqPglogical
 
   def replication_wal_retained
     pglogical.wal_retained_bytes
-  end
-
-  def self.local_node_name
-    region_to_node_name(MiqRegion.my_region_number)
-  end
-
-  def self.region_to_node_name(region_id)
-    "#{NODE_PREFIX}#{region_id}"
-  end
-
-  def self.node_name_to_region(name)
-    name.sub(NODE_PREFIX, "").to_i
   end
 
   def self.default_excludes
@@ -160,21 +105,6 @@ class MiqPglogical
 
   def pglogical(refresh = false)
     @pglogical = nil if refresh
-    @pglogical ||= @connection.pglogical
-  end
-
-  def connection_dsn
-    config = @connection.raw_connection.conninfo_hash.delete_blanks
-    PG::Connection.parse_connect_args(config)
-  end
-
-  # tables that are currently included, but we want them excluded
-  def newly_excluded_tables
-    included_tables & configured_excludes
-  end
-
-  # tables that are currently excluded, but we want them included
-  def newly_included_tables
-    (@connection.tables - configured_excludes) - included_tables
+    @pglogical ||= PG::LogicalReplication::Client.new(@connection)
   end
 end
