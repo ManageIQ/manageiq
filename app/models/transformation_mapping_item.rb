379002a35b533dba239ca5274de749c3b4569446
thearifismail <arifismail@yahoo.com>
2019-05-02 09:05:51 -0400

    validations by Dan for source and target checks

--- app/models/transformation_mapping_item.rb
+++ app/models/transformation_mapping_item.rb
@@ -4,4 +4,23 @@ class TransformationMappingItem < ApplicationRecord
   belongs_to :destination, :polymorphic => true
 
   validates :source_id, :uniqueness => {:scope => [:transformation_mapping_id, :source_type]}
+
+  validates :destination_type, :inclusion => { :in => %w[EmsCluster CloudTenant Storage Lan CloudVolumeType CloudNetwork] }
+
+  validate :target_cluster, :if => -> { destination_type.casecmp?('EmsCluster') }
+  validate :source_cluster, :if => -> { source_type.casecmp?(EmsCluster) }
+
+  private
+
+  def target_cluster
+    unless destination.ext_management_system.emstype.casecmp?('rhevm')
+      errors.add(:destination_type, "EMS type of target cluster must be rhevm")
+    end
+  end
+
+  def source_cluster
+    unless source.ext_management_system.emstype.casecmp?('vmwarews')
+      errors.add(:source_type, "EMS type of source cluster must be vmwarews")
+    end
+  end
 end
