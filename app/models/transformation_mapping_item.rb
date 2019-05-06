2527822ee2cee32a4ea82d44c51e60b371b93e76
thearifismail <arifismail@yahoo.com>
2019-05-06 17:20:18 -0400

    Dan's initial check

--- app/models/transformation_mapping_item.rb
+++ app/models/transformation_mapping_item.rb
@@ -13,10 +13,15 @@ class TransformationMappingItem < ApplicationRecord
   validate :destination_cluster, :if => -> { destination_type.casecmp?('EmsCluster') }
   validate :source_cluster, :if => -> { source_type.casecmp?('EmsCluster') }
 
-  validate :destination_datastore, :if => -> { destination_type.casecmp?('Storage') }
-  validate :source_datastore,      :if => -> { source_type.casecmp?('Storage') }
+  validate :destination_datastore, :if => -> { destination_type.casecmp?('Storage') ||
+                                               destination_type.casecmp?('CloudVolumeType')
+                                             }
 
-  validate :destination_network, :if => -> { destination_type.casecmp?('Lan') }
+  validate :source_datastore, :if => -> { source_type.casecmp?('Storage') }
+
+  validate :destination_network, :if => -> { source_type.casecmp?('Lan') ||
+                                             source_type.casecmp?('CloudNetwork')
+                                           }
   validate :source_network,      :if => -> { destination_type.casecmp?('Lan') }
 
   VALID_SOURCE_CLUSTER_PROVIDERS    = %w[vmwarews]
@@ -25,25 +30,10 @@ class TransformationMappingItem < ApplicationRecord
   VALID_SOURCE_DATASTORE_TYPES      = %w[Storage]
   VALID_DESTINATION_DATASTORE_TYPES = %w[Storage CloudVolumeType]
 
-  VALID_SOURCE_NETWORK_TYPES        = %w[LAN]
-  VALID_DESTINATION_NETWORK_TYPES   = %w[LAN CloudNetwork]
+  VALID_SOURCE_NETWORK_TYPES        = %w[Lan]
+  VALID_DESTINATION_NETWORK_TYPES   = %w[Lan CloudNetwork]
 
   private
-
-  # ----------------------------------------------------------------------------
-  # First check if types are valid
-  # ----------------------------------------------------------------------------
-
-  # Validator used if the target is an EMS cluster. This is more specific than
-  # the source_cluster validation because here it only validates against
-  # EmsCluster types.
-  #
-  def destination_cluster
-    unless VALID_DESTINATION_CLUSTER_TYPES.include?(destination.ext_management_system.emstype)
-      destination_types = VALID_DESTINATION_CLUSTER_TYPES.join(', ')
-      errors.add(:destination_type, "EMS type of target cluster must be in: #{destination_types}")
-    end
-  end
 
   # Validator used if the source type is an EMS cluster. In this case, both
   # the source and target types must be validated.
@@ -61,45 +51,18 @@ class TransformationMappingItem < ApplicationRecord
       destination_types = VALID_DESTINATION_CLUSTER_TYPES.join(', ')
       errors.add(:destination_type, "Class of target cluster must be in: #{destination_types}")
     end
-  end
+  end # of source_cluster
 
-  # Check the storage types are valid.
+  # Validator used if the target is an EMS cluster. This is more specific than
+  # the source_cluster validation because here it only validates against
+  # EmsCluster types.
   #
-  def destination_datastore
-    tmilogger = Rails.logger
-    # transformation_mapping_items.where(:destination_type => 'Storage').map(&:source)
-    #
-    # check the storages are valid types.
-    # Question: have to deal with multiple storages.  I guess only one storage is passed in.  Multiple storages have
-    # to be dealt by the validation call.
-    #
-    # Storage types can be NFS, VMFS, GlusterFS, do we need to verify those?  This doc doesnt talk about specific
-    # filesystem
-    #
-    # from IRB: tm.transformation_mapping_items.first.source.storages.second.store_type
-    # => "NFS"
-    # unless VALID_DESTINATION_DATASTORE_TYPES.include?(destination.ext_management_system.where(:destination_type => 'Storage').source.map(&:store_type))
-
-    # from irb
-    # myds.destination.hosts.collect{ |host| host.ems_cluster }.collect{ |cluster| cluster.storages }.flatten.include?(myds.destination)
-
-    storageClassNames = destination.hosts.     # Get hosts using this source storage
-        collect { |host| host.ems_cluster }.   # How many clusters does each host has
-        collect { |cluster| cluster.storages}. # How many storages each host is mapped to that belong to the cluster
-        flatten.collect { |s| s.class.name }   # Get storage types represented by its class name
-
-    tmilogger.info( "#{__method__.to_s} :  #{storageClassNames.to_s}" )
-
-    # check if desitination storage belongs to cluster storage.
-    if storageClassNames.include?(VALID_DESTINATION_DATASTORE_TYPES.first)
-      result = true
-    elsif storageClassNames.include?(VALID_DESTINATION_DATASTORE_TYPES.second)
-      result = true
-    else
-      store_types = VALID_DESTINATION_DATASTORE_TYPES.join(', ')
-      errors.add(:store_type, "The type of destination type must be in: #{store_types}")
+  def destination_cluster
+    unless VALID_DESTINATION_CLUSTER_TYPES.include?(destination.ext_management_system.emstype)
+      destination_types = VALID_DESTINATION_CLUSTER_TYPES.join(', ')
+      errors.add(:destination_type, "EMS type of target cluster must be in: #{destination_types}")
     end
-  end
+  end # of destination_cluster
 
   # How to check the datastore(s) belong to the source.
   #
@@ -120,24 +83,72 @@ class TransformationMappingItem < ApplicationRecord
       storage_types = VALID_SOURCE_DATASTORE_TYPES.join(', ')
       errors.add(:storage_type, "The type of destination type must be in: #{storage_types}")
     end
-  end
+  end # of source_datastore
+
+  # Check the storage types are valid.
+  #
+  def destination_datastore
+    tmilogger = Rails.logger
+    # from irb
+    # myds.destination.hosts.collect{ |host| host.ems_cluster }.collect{ |cluster| cluster.storages }.flatten.include?(myds.destination)
+
+    storageClassNames = destination.hosts. # Get hosts using this source storage
+    collect { |host| host.ems_cluster }.   # How many clusters does each host has
+    collect { |cluster| cluster.storages}. # How many storages each host is mapped to that belong to the cluster
+    flatten.collect { |s| s.class.name }   # Get storage types represented by its class name
+
+    logger.info("ARIF ARIF ARIF TEST TEST TEST")
+    tmilogger.info( "#{__method__.to_s} :  #{storageClassNames.to_s}" )
+
+    # check if desitination storage belongs to cluster storage.
+    if storageClassNames.include?(VALID_DESTINATION_DATASTORE_TYPES.first)
+      result = true
+    elsif storageClassNames.include?(VALID_DESTINATION_DATASTORE_TYPES.second)
+      result = true
+    else
+      tistore_types = VALID_DESTINATION_DATASTORE_TYPES.join(', ')
+      errors.add(:store_type, "The type of destination type must be in: #{store_types}")
+    end
+  end # of destination_datastore
 
   # Verify that Network type is LAN and belongs the source cluster .
   # irb(main):043:0> tm.transformation_mapping_items.first.source.lans.count
   # => 16
   #
   def source_network
-    transformation_mapping_items.where(:source_type => 'Lan').map(&:source)
-  end
+    source_lan = source
+
+    tmi_cluster = TransformationMappingItem.where(:source_type => "EmsCluster").first #TODO: handle collection?
+    source_cluster = tmi_cluster.source
+    tempCluster = source_lan.switch.host.ems_cluster
+
+    if tempCluster == source_cluster
+      result = true
+    else
+      network_types = VALID_SOURCE_NETWORK_TYPES.join(', ')
+      errors.add(:network_types, "The network type must be in: #{network_types}")
+    end
+  end # of source_network
 
   # Verify that Network type is LAN or CloudNetwork and belongs the destination cluster.
   #
   def destination_network
-    transformation_mapping_items.where(:destination_type => 'Lan').map(&:source)
-  end
+    # transformation_mapping_items.where(:destination_type => 'Lan').map(&:source)
+    #
+    # source is a TransformationMappingItem
+    # tmi_lan = TransformationMappingItem.where(:source_type => "Lan").first
+    destination_lan = destination
 
-  # ----------------------------------------------------------------------------
-  # Verify that source and destination cluster type is not the same
-  # ----------------------------------------------------------------------------
+    tmi_cluster = TransformationMappingItem.where(:source_type => "EmsCluster").first  #TODO: handle collection?
+    destination_cluster = tmi_cluster.destination
+    tempCluster = destination_lan.switch.host.ems_cluster
 
-end
+    if tempCluster == destination_cluster
+      result = true
+    else
+      network_types = VALID_SOURCE_NETWORK_TYPES.join(', ')
+      errors.add(:network_types, "The network type must be in: #{network_types}")
+    end
+  end # of destination_network
+
+end # of cla
