836f43ebf98c17120465dfe518c47ebf88b6ce7d
Adam Grare <agrare@redhat.com>
2019-05-16 10:58:49 -0400

    Merge pull request #18762 from fdupont-redhat/v2v_conversion_host_handle_cacert_from_ui
    [V2V] Conversion Host - Handle CA bundle from UI
    
    (cherry picked from commit d3248d61d29d109f481818ed78dec5e87b6cba6f)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1711036

--- app/models/conversion_host/configurations.rb
+++ app/models/conversion_host/configurations.rb
@@ -67,6 +67,8 @@ module ConversionHost::Configurations
 
       ssh_key = params.delete(:conversion_host_ssh_private_key)
 
+      openstack_tls_ca_certs = params.delete(:openstack_tls_ca_certs)
+
       new(params).tap do |conversion_host|
         if ssh_key
           conversion_host.authentications << AuthPrivateKey.create!(
@@ -77,7 +79,7 @@ module ConversionHost::Configurations
           )
         end
 
-        conversion_host.enable_conversion_host_role(vmware_vddk_package_url, vmware_ssh_private_key, miq_task_id)
+        conversion_host.enable_conversion_host_role(vmware_vddk_package_url, vmware_ssh_private_key, openstack_tls_ca_certs, miq_task_id)
         conversion_host.save!
 
         if miq_task_id
