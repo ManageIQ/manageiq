37335d4935027f702b9a2de7f8ed17c1fa0cd915
Keenan Brock <keenan@thebrocks.net>
2019-05-30 14:52:11 -0400

    ContainerGroup preloaded container_conditions
    before:
    ready_condition_status preloads container_conditions
    but does not use them
    
    after:
    the loaded container_conditions are used

--- spec/models/container_group_spec.rb
+++ spec/models/container_group_spec.rb
@@ -41,6 +41,27 @@ describe ContainerGroup do
     assert_pod_to_pv_relationships(group)
   end
 
+  context "#ready_condition_status" do
+    let(:condition_ready) { FactoryBot.create(:container_condition, :container_entity => container_group, :name => "Ready") }
+    let(:condition_other) { FactoryBot.create(:container_condition, :container_entity => container_group, :name => "Other") }
+    let(:container_group) { FactoryBot.create(:container_group) }
+
+    it "preloads the conditions" do
+      condition_other
+      cr = condition_ready
+      cg = ContainerGroup.includes(:ready_condition_status).references(:ready_condition_status => {}).find_by(:id => container_group.id)
+
+      expect { expect(cg.ready_condition).to eq(cr) }.to match_query_limit_of(0)
+    end
+
+    it "handles non-preloaded conditions" do
+      condition_other
+      cr = condition_ready
+      cg = ContainerGroup.find_by(:id => container_group.id)
+      expect { expect(cg.ready_condition).to eq(cr) }.to match_query_limit_of(1)
+    end
+  end
+
   def assert_pod_to_pv_relationships(group)
     expect(group.persistent_volume_claim.first.name).to eq("test_claim")
     expect(group.persistent_volume_claim.count).to eq(1)
