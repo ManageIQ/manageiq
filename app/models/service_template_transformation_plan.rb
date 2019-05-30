d5a59fb095402cb9523c24e4a670d27224284fa7
Keenan Brock <keenan@thebrocks.net>
2019-05-30 14:55:24 -0400

    Fix ServiceTemplateTransformationPlan
    virtual_relation :transformation_mapping uses itself.
    
    This is an erroneous self reference. it is removed

--- app/models/service_template_transformation_plan.rb
+++ app/models/service_template_transformation_plan.rb
@@ -1,6 +1,6 @@
 class ServiceTemplateTransformationPlan < ServiceTemplate
   include_concern 'ValidateConfigInfo'
-  virtual_has_one :transformation_mapping, :uses => :transformation_mapping
+  virtual_has_one :transformation_mapping
   def request_class
     ServiceTemplateTransformationPlanRequest
   end
