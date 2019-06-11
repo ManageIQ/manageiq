7dfbba543d68a79a7e3a85d897a73bef2e5cb492
Nick LaMuro <nicklamuro@gmail.com>
2019-06-11 12:46:56 -0500

    Re-Add API_OPTIONS to EmbeddedAnsible credentials
    Removed as part of the ansible-runner effort, but added back manually
    now (instead of inheriting from `manageiq-providers-ansible_tower`).
    
    This only allows the UI to display and insert credentials, but it does
    not implement them when using runner.

--- app/models/manageiq/providers/embedded_ansible/automation_manager/rhv_credential.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/rhv_credential.rb
@@ -1,4 +1,34 @@
 class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::RhvCredential < ManageIQ::Providers::EmbeddedAnsible::AutomationManager::CloudCredential
+  COMMON_ATTRIBUTES = {
+    :userid   => {
+      :label     => N_('Username'),
+      :help_text => N_('Username for this credential')
+    },
+    :password => {
+      :type      => :password,
+      :label     => N_('Password'),
+      :help_text => N_('Password for this credential')
+    }
+  }.freeze
+
+  EXTRA_ATTRIBUTES = {
+    :host => {
+      :type       => :string,
+      :label      => N_('Host'),
+      :help_text  => N_('The host to authenticate with'),
+      :max_length => 1024,
+      :required   => true
+    }
+  }.freeze
+
+  API_ATTRIBUTES = COMMON_ATTRIBUTES.merge(EXTRA_ATTRIBUTES).freeze
+
+  API_OPTIONS = {
+    :label      => N_('Red Hat Virtualization'),
+    :type       => 'cloud',
+    :attributes => API_ATTRIBUTES
+  }.freeze
+
   def self.display_name(number = 1)
     n_('Credential (RHV)', 'Credentials (RHV)', number)
   end
