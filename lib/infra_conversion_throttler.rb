46e705f89242de6e8a61a03076886ea7f7fb87ec
Daniel Berger <djberg96@gmail.com>
2019-06-12 10:21:09 -0400

    Add some conversion host debugging.

--- lib/infra_conversion_throttler.rb
+++ lib/infra_conversion_throttler.rb
@@ -4,13 +4,27 @@ class InfraConversionThrottler
     pending_conversion_jobs.each do |ems, jobs|
       _log.warn("EMS: #{ems.name}")
       _log.warn("Total number of jobs: #{jobs.size}")
-      running = ems.conversion_hosts.inject(0) { |sum, ch| sum + ch.active_tasks.size }
+
+      conversion_hosts = ems.conversion_hosts
+
+      conversion_hosts.each do |ch|
+        max = ch.max_concurrent_tasks || Settings.transformation.limits.max_concurrent_tasks_per_host
+        _log.warn("Conversion host: #{ch.name}, max concurrent tasks: #{max}")
+      end
+
+      #running = ems.conversion_hosts.inject(0) { |sum, ch| sum + ch.active_tasks.size }
+      running = conversion_hosts.inject(0) { |sum, ch| sum + ch.active_tasks.size }
+
       _log.warn("Total running: #{running}")
+
       slots = (ems.miq_custom_get('Max Transformation Runners') || Settings.transformation.limits.max_concurrent_tasks_per_ems).to_i - running
+
       _log.warn("Total slots: #{slots}")
+
       jobs.each do |job|
         sleep 0.5
-        eligible_hosts = ems.conversion_hosts.select(&:eligible?).sort_by { |ch| ch.active_tasks.size }
+        #eligible_hosts = ems.conversion_hosts.select(&:eligible?).sort_by { |ch| ch.active_tasks.size }
+        eligible_hosts = conversion_hosts.select(&:eligible?).sort_by { |ch| ch.active_tasks.size }
         _log.warn("Total eligible hosts: #{eligible_hosts.size}")
         break if slots <= 0 || eligible_hosts.empty?
         job.migration_task.update_attributes!(:conversion_host => eligible_hosts.first)
