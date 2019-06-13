2e321dbc22c7ab8a4abfddb0c650427bd92b1e33
Nick LaMuro <nicklamuro@gmail.com>
2019-06-13 13:11:52 -0500

    [EmbeddedAnsible::Credential] update base spec
    - Adds back a test for native_ref (pulled from tower provider)
    - Uses `EmbeddedAnsible::AutomationManager::Credential` as base class

--- spec/models/manageiq/providers/embedded_ansible/automation_manager/credential_spec.rb
+++ spec/models/manageiq/providers/embedded_ansible/automation_manager/credential_spec.rb
@@ -1,10 +1,25 @@
-describe ManageIQ::Providers::EmbeddedAnsible::AutomationManager::ScmCredential do
+describe ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Credential do
   let(:manager) do
     FactoryBot.create(:provider_embedded_ansible, :default_organization => 1).managers.first
   end
 
   before do
     EvmSpecHelper.assign_embedded_ansible_role
+  end
+
+  context "#native_ref" do
+    let(:simple_credential) { described_class.new(:manager_ref => '1', :resource => manager) }
+
+    it "returns integer" do
+      expect(simple_credential.manager_ref).to eq('1')
+      expect(simple_credential.native_ref).to eq(1)
+    end
+
+    it "blows up for nil manager_ref" do
+      simple_credential.manager_ref = nil
+      expect(simple_credential.manager_ref).to be_nil
+      expect { simple_credential.native_ref }.to raise_error(TypeError)
+    end
   end
 
   # it_behaves_like 'ansible credential'
