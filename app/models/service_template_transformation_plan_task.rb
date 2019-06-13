1a17f798d50542d1331a0079203b36bc77ddf83c
Fabien Dupont <fdupont@redhat.com>
2019-06-13 14:50:39 -0400

    Add a helper method to check if virtv2v is running

--- app/models/service_template_transformation_plan_task.rb
+++ app/models/service_template_transformation_plan_task.rb
@@ -238,7 +238,7 @@ class ServiceTemplateTransformationPlanTask < ServiceTemplateProvisionTask
   def kill_virtv2v(signal = 'TERM')
     get_conversion_state
 
-    if options[:virtv2v_started_on].blank? || options[:virtv2v_finished_on].present? || options[:virtv2v_wrapper].blank?
+    unless virtv2v_running?
       _log.info("virt-v2v is not running, so there is nothing to do.")
       return false
     end
@@ -256,6 +256,11 @@ class ServiceTemplateTransformationPlanTask < ServiceTemplateProvisionTask
 
   def vm_resource
     miq_request.vm_resources.find_by(:resource => source)
+  end
+
+  def virtv2v_running?
+    return false if options[:virtv2v_started_on].blank? || options[:virtv2v_finished_on].present? || options[:virtv2v_wrapper].blank?
+    true
   end
 
   def create_error_status_task(userid, msg)
