efa01bf52231353586b0d0da6c49c5299b1118bb
d-m-u <drewuhlmann@gmail.com>
2019-06-18 13:46:03 -0400

    Correctly recurse over nested field associations

--- app/models/dialog_field_association_validator.rb
+++ app/models/dialog_field_association_validator.rb
@@ -1,28 +1,11 @@
 class DialogFieldAssociationValidator
-  def circular_references(associations)
-    return false if associations.empty?
-    association_path_list = initial_paths(associations)
-    association_path_list.each do |path|
-      fieldname_being_triggered = path.last
-      next if associations[fieldname_being_triggered].blank?
-      circular_references = walk_value_path(fieldname_being_triggered, associations, path)
-      return circular_references if circular_references.present?
+  class DialogFieldAssociationCircularReferenceError < RuntimeError; end
+  def check_for_circular_references(hash, k, collection = [])
+    raise DialogFieldAssociationCircularReferenceError, "#{k} already exists in #{collection}" if collection.include?(k)
+    collection << k
+    hash[k]&.each do |val|
+      check_for_circular_references(hash, val, collection.dup)
     end
-    false
-  end
-
-  private
-
-  def initial_paths(associations)
-    associations.flat_map { |key, values| values.map { |value| [key, value] } }
-  end
-
-  def walk_value_path(fieldname_being_triggered, associations, path)
-    while associations[fieldname_being_triggered].present?
-      return [fieldname_being_triggered, associations[fieldname_being_triggered].first] if path.include?(associations[fieldname_being_triggered].first)
-      path << associations[fieldname_being_triggered]
-      path.flatten!
-      fieldname_being_triggered = path.last
-    end
+    nil
   end
 end
