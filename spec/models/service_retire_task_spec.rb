c6822d3f9cf2731ad69114ee6360abe27ade7f87
d-m-u <drewuhlmann@gmail.com>
2019-06-19 11:44:25 -0400

    Don't create tasks for things that are already retired

--- spec/models/service_retire_task_spec.rb
+++ spec/models/service_retire_task_spec.rb
@@ -144,6 +144,15 @@ describe ServiceRetireTask do
         expect(VmRetireTask.count).to eq(1)
         expect(ServiceRetireTask.count).to eq(1)
       end
+
+      it "does not create vm retire subtask for retired vm" do
+        service.add_resource!(FactoryBot.create(:vm_openstack, :retired => true))
+        service_retire_task.after_request_task_create
+
+        expect(service_retire_task.description).to eq("Service Retire for: #{service.name} - ")
+        expect(VmRetireTask.count).to eq(0)
+        expect(ServiceRetireTask.count).to eq(1)
+      end
     end
 
     context "bundled service retires all children" do
