08c2cf21c204bdf64b2b11c4c3420d7d1a8086f6
Keenan Brock <keenan@thebrocks.net>
2019-07-01 13:06:12 -0400

    order: support other nodes for to_sql

--- lib/extensions/ar_order.rb
+++ lib/extensions/ar_order.rb
@@ -1,3 +1,5 @@
+# This monkey patches rails to support proper joins
+# current PR: https://github.com/rails/rails/pull/36531
 module ActiveRecord
   module ConnectionAdapters
     module PostgreSQL
@@ -5,7 +7,16 @@ module ActiveRecord
         def columns_for_distinct(columns, orders) #:nodoc:
           order_columns = orders.reject(&:blank?).map { |s|
               # Convert Arel node to string
-              s = s.to_sql unless s.is_a?(String)
+              unless s.is_a?(String)
+                if s.respond_to?(:to_sql)
+                  s = s.to_sql
+                else # for Arel::Nodes::Attribute
+                  engine = Arel::Table.engine
+                  collector = Arel::Collectors::SQLString.new
+                  collector = engine.connection.visitor.accept s, collector
+                  s = collector.value
+                end
+              end
               # Remove any ASC/DESC modifiers
               s.gsub(/\s+(?:ASC|DESC)\b/i, "")
                .gsub(/\s+NULLS\s+(?:FIRST|LAST)\b/i, "")
