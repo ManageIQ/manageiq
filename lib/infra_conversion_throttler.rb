99bbf47929122fdebc7fc77d53e52206fac11a54
Adam Grare <agrare@redhat.com>
2019-07-02 12:46:57 -0400

    Merge pull request #18929 from fdupont-redhat/v2v_enhance_throttler_logging
    [V2V] Enhance throttler logging
    
    (cherry picked from commit fd7042726763fa37450efd587a83dbad575d68ec)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1726394

--- lib/infra_conversion_throttler.rb
+++ lib/infra_conversion_throttler.rb
@@ -1,24 +1,45 @@
 class InfraConversionThrottler
-  def self.start_conversions
-    pending_conversion_jobs.each do |ems, jobs|
-      running = ems.conversion_hosts.inject(0) { |sum, ch| sum + ch.active_tasks.count }
-      $log&.debug("There are currently #{running} conversion hosts running.")
-      slots = (ems.miq_custom_get('Max Transformation Runners') || Settings.transformation.limits.max_concurrent_tasks_per_ems).to_i - running
-      $log&.debug("The maximum number of concurrent tasks for the EMS is: #{slots}.")
-      jobs.each do |job|
-        eligible_hosts = ems.conversion_hosts.select(&:eligible?).sort_by { |ch| ch.active_tasks.count }
+  include Vmdb::Logging
 
-        if eligible_hosts.size > 0
-          $log&.debug("The following conversion hosts are currently eligible: " + eligible_hosts.map(&:name).join(', '))
+  def self.start_conversions
+    _log.debug("InfraConversionThrottler.start_conversions")
+    pending_conversion_jobs.each do |ems, jobs|
+      _log.debug("- EMS: #{ems.name}")
+      _log.debug("-- Number of pending jobs: #{jobs.size}")
+      running = ems.conversion_hosts.inject(0) { |sum, ch| sum + ch.active_tasks.count }
+      _log.debug("-- Currently running jobs in EMS: #{running}")
+      slots = (ems.miq_custom_get('Max Transformation Runners') || Settings.transformation.limits.max_concurrent_tasks_per_ems).to_i - running
+
+      if slots <= 0
+        _log.debug("-- No available slot in EMS. Stopping.")
+        next
+      end
+      _log.debug("-- Available slots in EMS: #{slots}")
+
+      jobs.each do |job|
+        vm_name = job.migration_task.source.name
+        _log.debug("- Looking for a conversion host for task for #{vm_name}")
+
+        eligible_hosts = ems.conversion_hosts.select(&:eligible?).sort_by { |ch| ch.active_tasks.count }
+        if eligible_hosts.empty?
+          _log.debug("-- No eligible conversion host for task for '#{vm_name}'")
+          break
         end
 
-        break if slots <= 0 || eligible_hosts.empty?
+        _log.debug("-- Eligible conversion hosts:")
+        eligible_hosts.each do |eh|
+          max_tasks = eh.max_concurrent_tasks || Settings.transformation.limits.max_concurrent_tasks_per_host
+          _log.debug("--- #{eh.name} [#{eh.active_tasks.count}/#{max_tasks}]")
+        end
+
+        eligible_host = eligible_hosts.first
+        _log.debug("-- Associating  #{eligible_host.name} to the task for '#{vm_name}'.")
         job.migration_task.update_attributes!(:conversion_host => eligible_hosts.first)
+
+        _log.debug("-- Queuing :start signal for the job for '#{vm_name}': current state is '#{job.state}'.")
         job.queue_signal(:start)
         _log.info("Pening InfraConversionJob: id=#{job.id} signaled to start")
         slots -= 1
-
-        $log&.debug("The current number of available tasks is: #{slots}.")
       end
     end
   end
