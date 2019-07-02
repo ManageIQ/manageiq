57fa0ddecf9e44300bc81f0d7fc1a2b39a4ea6ea
Nick Carboni <ncarboni@redhat.com>
2019-07-02 09:53:46 -0400

    Remove unnecessary rspec magic that makes specs hard to understand

--- spec/models/notification_type_spec.rb
+++ spec/models/notification_type_spec.rb
@@ -1,12 +1,15 @@
 describe NotificationType, :type => :model do
   describe '.seed' do
-    subject { described_class.count }
-    context 'before the seed is run' do
-      it { is_expected.to be_zero }
+    it 'has not seeded records before seed is run' do
+      expect(described_class.count).to be_zero
     end
+
     context 'after the seed is run' do
       before { described_class.seed }
-      it { is_expected.to be > 0 }
+      it 'has added rows' do
+        expect(described_class.count).to be > 0
+      end
+
       it 'can be run again without any effects' do
         expect { described_class.seed }.not_to change(described_class, :count)
       end
@@ -18,29 +21,30 @@ describe NotificationType, :type => :model do
     let(:tenant) { FactoryBot.create(:tenant) }
     let!(:user2) { FactoryBot.create(:user_with_group, :tenant => tenant) }
     let(:vm) { FactoryBot.create(:vm, :tenant => tenant) }
-    subject { notification.subscriber_ids(vm, user1) }
+
     context 'global notification type' do
       let(:notification) { FactoryBot.create(:notification_type, :audience => 'global') }
       it 'returns all the users' do
-        is_expected.to match_array(User.pluck(:id))
+        expect(notification.subscriber_ids(vm, user1)).to match_array(User.pluck(:id))
       end
     end
 
     context 'user specific notification type' do
       let(:notification) { FactoryBot.create(:notification_type, :audience => 'user') }
       it 'returns just the user, who initiated the task' do
-        is_expected.to match_array([user1.id])
+        expect(notification.subscriber_ids(vm, user1)).to match_array([user1.id])
       end
     end
 
     context 'tenant specific notification type' do
       let(:notification) { FactoryBot.create(:notification_type, :audience => 'tenant') }
       it 'returns the users in the tenant same tenant as concerned vm' do
-        is_expected.to match_array([user2.id])
+        expect(notification.subscriber_ids(vm, user1)).to match_array([user2.id])
       end
+
       it "returns single id if user belongs to different group" do
         user2.miq_groups << FactoryBot.create(:miq_group, :tenant => tenant)
-        is_expected.to match_array([user2.id])
+        expect(notification.subscriber_ids(vm, user1)).to match_array([user2.id])
       end
     end
   end
