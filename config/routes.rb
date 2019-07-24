78fc5f8fc3c0c2dcc077fe7b3839105bb0b82e9f
Dávid Halász <dhalasz@redhat.com>
2019-07-24 14:46:59 +0200

    Do not run the remote console proxy with the UI by default in dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Vmdb::Application.routes.draw do
-  if Rails.env.development? && defined?(Rails::Server)
+  if Rails.env.development? && ENV['MOUNT_REMOTE_CONSOLE_PROXY']
     logger = Logger.new(STDOUT)
     logger.level = Logger.const_get(::Settings.log.level_remote_console.upcase)
     mount RemoteConsole::RackServer.new(:logger => logger) => '/ws/console'
