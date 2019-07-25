85a9399af44b40826ffeca44190044f6763dbc3d
Jason Frey <jfrey@redhat.com>
2019-07-25 14:13:45 -0400

    Merge pull request #19056 from carbonin/dont_create_a_default_project
    Don't create the embedded ansible default project
    
    (cherry picked from commit 564a6beb0e2a71f3ccc83f0c5306677185af948f)

--- app/models/manageiq/providers/embedded_ansible/seeding.rb
+++ app/models/manageiq/providers/embedded_ansible/seeding.rb
@@ -19,42 +19,7 @@ module ManageIQ::Providers::EmbeddedAnsible::Seeding
         :resource => manager
       )
 
-      create_local_playbook_repo
-      local_repo = ManageIQ::Providers::EmbeddedAnsible::AutomationManager::ConfigurationScriptSource.find_or_create_by!(
-        :name       => "#{Vmdb::Appliance.PRODUCT_NAME} Default Project",
-        :manager_id => manager.id
-      )
-      local_repo.raw_update_in_provider(
-        :scm_type             => "git",
-        :scm_url              => "file://#{local_playbook_repo_dir}",
-        :scm_update_on_launch => false
-      )
-    end
-
-    private
-
-    def create_local_playbook_repo
       Ansible::Content.consolidate_plugin_playbooks
-
-      Dir.chdir(local_playbook_repo_dir) do
-        require 'rugged'
-        repo = Rugged::Repository.init_at(".")
-        index = repo.index
-        index.add_all("*")
-        index.write
-
-        options              = {}
-        options[:tree]       = index.write_tree(repo)
-        options[:author]     = options[:committer] = { :email => "system@localhost", :name => "System", :time => Time.now.utc }
-        options[:message]    = "Initial Commit"
-        options[:parents]    = []
-        options[:update_ref] = 'HEAD'
-        Rugged::Commit.create(repo, options)
-      end
-    end
-
-    def local_playbook_repo_dir
-      Ansible::Content::PLUGIN_CONTENT_DIR
     end
   end
 end
