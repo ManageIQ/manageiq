6724fa44c35bc14a8768f5dd676e118cd5ac772f
Peter McGowan <pmcgowan@redhat.com>
2019-07-31 17:55:49 +0100

    Re-worded "Ansible Automation Inside" to "Embedded Ansible" for standardisation

--- app/models/manageiq/providers/embedded_ansible/automation_manager/configuration_script.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/configuration_script.rb
@@ -1,5 +1,5 @@
 class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::ConfigurationScript < ManageIQ::Providers::EmbeddedAutomationManager::ConfigurationScript
-  FRIENDLY_NAME = "Ansible Automation Inside Job Template".freeze
+  FRIENDLY_NAME = "Embedded Ansible Job Template".freeze
 
   include ManageIQ::Providers::EmbeddedAnsible::CrudCommon
 
