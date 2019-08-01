575a2b401210054ff3a63dcea9c437da8b16ca46
Adam Grare <agrare@redhat.com>
2019-08-01 16:10:47 -0400

    Merge pull request #19088 from AlexanderZagaynov/features/aws_sts_assume_role
    support for 'assume role' aws feature
    
    (cherry picked from commit 682f4dfeea85968b420933d498db7212a6b801ab)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1631845

--- app/models/mixins/supports_feature_mixin.rb
+++ app/models/mixins/supports_feature_mixin.rb
@@ -144,7 +144,8 @@ module SupportsFeatureMixin
     :volume_multiattachment              => 'Volume Multiattachment',
     :volume_resizing                     => 'Volume Resizing',
     :change_password                     => 'Change Password',
-    :volume_availability_zones           => 'Volume Availability Zones'
+    :volume_availability_zones           => 'Volume Availability Zones',
+    :assume_role                         => 'Assume Role',
   }.freeze
 
   # Whenever this mixin is included we define all features as unsupported by default.
