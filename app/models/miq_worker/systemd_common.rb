ec10aca0f1b9a98d12d97f99c1e7e84ccf7fe7d3
Adam Grare <agrare@redhat.com>
2019-08-02 08:57:55 -0400

    MiqServer creates worker records so need the guid
    For now MiqServer is creating the worker records and assigning a guid,
    so we need to pass the guid to the systemd service in all cases for now.

--- app/models/miq_worker/systemd_common.rb
+++ app/models/miq_worker/systemd_common.rb
@@ -24,7 +24,7 @@ class MiqWorker
       end
 
       def service_name
-        scalable? ? service_base_name : "#{service_base_name}@"
+        "#{service_base_name}@"
       end
 
       def service_file_name
@@ -131,7 +131,7 @@ class MiqWorker
     end
 
     def unit_instance
-      scalable? ? "" : "@#{guid}"
+      "@#{guid}"
     end
 
     def write_unit_settings_file
