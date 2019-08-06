684308aa507a68b3d59a0a4d97498471b5d776a8
Daniel Berger <dberger@redhat.com>
2019-08-06 10:07:50 -0400

    Add upcoming validations.

--- app/models/transformation_mapping_item.rb
+++ app/models/transformation_mapping_item.rb
@@ -8,6 +8,32 @@ class TransformationMappingItem < ApplicationRecord
   validate :source_cluster,      :if => -> { source.kind_of?(EmsCluster) }
   validate :destination_cluster, :if => -> { destination.kind_of?(EmsCluster) || destination.kind_of?(CloudTenant) }
 
+  #validate :source_datastore,      :if => -> { source.kind_of?(Storage) }
+  #validate :destination_datastore, :if => -> { destination.kind_of?(Storage) || destination.kind_of?(CloudVolume) }
+
+  def destination_datastore
+    if destination.kind_of?(Storage) # Redhat
+      mapping_items = transformation_mapping.transformation_mapping_items.where(:destination_type=> "EmsCluster")
+      dst_cluster_storages = mapping_items.collect(&:destination).collect(&:storages).flatten
+    elsif destination.kind_of?(CloudVolume) # Openstack
+      mapping_items = transformation_mapping.transformation_mapping_items.where(:destination_type => "CloudTenant")
+      dst_cluster_storages = mapping_items.collect(&:destination).collect(&:cloud_volumes).flatten
+    end
+
+    unless dst_cluster_storages.include?(destination)
+      errors.add(:destination, "cluster storages must include destination storage: #{destination}")
+    end
+  end
+
+  def source_datastore
+    mappings = transformation_mapping.transformation_mapping_items.where(:source_type => "EmsCluster")
+    storages = mappings.collect(&:source).collect(&:storages).flatten
+
+    unless storages.include?(source)
+      errors.add(:source, "cluster storages must include source storage: #{source}")
+    end
+  end
+
   VALID_SOURCE_CLUSTER_PROVIDERS = %w[vmwarews].freeze
   VALID_DESTINATION_CLUSTER_PROVIDERS = %w[rhevm openstack].freeze
 
