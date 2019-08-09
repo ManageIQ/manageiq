03a4c95a9558cc0d285501f845778f7c146e23bb
Nick Carboni <ncarboni@redhat.com>
2019-08-09 14:07:45 -0400

    Merge pull request #19129 from Fryguy/embedded_ansible_ssh_user_host
    Add support to Embedded Ansible for ssh user@host:path urls
    
    (cherry picked from commit 11ad9b8a4ae6690b03b6a62a5b51301bb3390d4d)

--- app/models/git_repository.rb
+++ app/models/git_repository.rb
@@ -6,7 +6,7 @@ class GitRepository < ApplicationRecord
 
   GIT_REPO_DIRECTORY = Rails.root.join('data/git_repos')
 
-  validates :url, :format => URI.regexp(%w[http https file ssh]), :allow_nil => false
+  validates :url, :format => Regexp.union(URI.regexp(%w[http https file ssh]), /\A[-\w:.]+@.*:/), :allow_nil => false
 
   default_value_for :verify_ssl, OpenSSL::SSL::VERIFY_PEER
   validates :verify_ssl, :inclusion => {:in => [OpenSSL::SSL::VERIFY_NONE, OpenSSL::SSL::VERIFY_PEER]}
