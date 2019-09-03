67cfb9b993f2b908d269575ff2e348abb3b94f3a
Daniel Berger <dberger@redhat.com>
2019-09-03 12:54:13 -0400

    Use proc instead of Proc.new.

--- app/models/auth_token.rb
+++ app/models/auth_token.rb
@@ -1,6 +1,6 @@
 class AuthToken < Authentication
   after_update :after_authentication_changed,
-    :if => Proc.new{ |auth| auth.saved_change_to_auth_key? }
+    :if => proc { |auth| auth.saved_change_to_auth_key? }
 
   def self.display_name(number = 1)
     n_('Authentication Token', 'Authentication Tokens', number)
