1a0a06d645735313b93d44092d58238c491f7922
Daniel Berger <dberger@redhat.com>
2019-09-03 12:34:30 -0400

    Remove redundant accessors.

--- app/models/auth_userid_password.rb
+++ app/models/auth_userid_password.rb
@@ -2,16 +2,6 @@ class AuthUseridPassword < Authentication
   after_update :after_authentication_changed,
     :if => Proc.new{ |auth| auth.saved_change_to_userid || auth.saved_changed_to_password? }
 
-  def password=(val)
-    auth_changed = true if password && (val != password)
-    super(val)
-  end
-
-  def userid=(val)
-    auth_changed = true if userid && (val != userid)
-    super(val)
-  end
-
   def self.display_name(number = 1)
     n_('Password', 'Passwords', number)
   end
