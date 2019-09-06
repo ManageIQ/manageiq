8e14b09cfa7ff6880090abe320d0a4f6f8aafcb3
Milan Zazrivec <mzazrivec@redhat.com>
2019-09-06 11:22:52 +0200

    Test that all human locale names are valid

--- spec/i18n/locale_name_spec.rb
+++ spec/i18n/locale_name_spec.rb
@@ -8,4 +8,11 @@ describe :locale_name do
 
     FastGettext.locale = 'en' # set the locale for runnin specs back to English
   end
+
+  it "all entries in human_locale_names.yml are valid" do
+    YAML.load_file(Rails.root.join('config/human_locale_names.yaml'))['human_locale_names'].each do |locale_name, human_locale_name|
+      expect(human_locale_name).not_to be_empty
+      expect(human_locale_name).not_to eq(locale_name)
+    end
+  end
 end
