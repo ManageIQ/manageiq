be0c5b74e3d027c722a81124c34c4488b117168c
Libor Pichler <lpichler@redhat.com>
2019-09-13 15:38:34 +0200

    Load whole enviroment in FixAuth tool

--- tools/fix_auth.rb
+++ tools/fix_auth.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+require File.expand_path('../config/environment', __dir__)
 
 # usage: ruby fix_auth -h
 #
@@ -12,8 +11,6 @@ if __FILE__ == $PROGRAM_NAME
   $LOAD_PATH.push(File.expand_path(File.join(__dir__, %w(.. lib))))
 end
 
-require 'active_support/all'
-require 'active_support/concern'
 # this gets around a bug if a user mistakingly
 # serializes a drb object into a configuration hash
 require 'drb'
