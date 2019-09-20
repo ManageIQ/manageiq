2efc00bed6ead25ee8404bcd15c0db60d2b5e04b
Keenan Brock <keenan@thebrocks.net>
2019-09-20 16:02:10 -0400

    Merge category and classification factory
    Categories and classifications use the same table.
    The factories use different sequence numbers but the same template
    for text.
    This means the name or description field can be non-unique.
    
    Solution is to pointing category and classification to the same
    number sequence generators, so they no longer will produce
    duplicate names.

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,3 @@
 FactoryBot.define do
-  factory(:category) do
-    sequence(:name)        { |n| "category_#{seq_padded_for_sorting(n)}" }
-    sequence(:description) { |n| "category #{seq_padded_for_sorting(n)}" }
-  end
+  factory(:category, :parent => :classification, :class => 'Category')
 end
