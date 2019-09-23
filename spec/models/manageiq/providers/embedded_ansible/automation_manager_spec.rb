2013330562ecc835ca6d888d06a4f098ebeb7a0f
James Wong <2421248+jameswnl@users.noreply.github.com>
2019-09-23 11:16:59 -0400

    clean up Ansible factory

--- spec/models/manageiq/providers/embedded_ansible/automation_manager_spec.rb
+++ spec/models/manageiq/providers/embedded_ansible/automation_manager_spec.rb
@@ -1,6 +1,6 @@
 describe ManageIQ::Providers::EmbeddedAnsible::AutomationManager do
   context 'catalog types' do
-    let(:ems) { FactoryBot.create(:embedded_automation_manager) }
+    let(:ems) { FactoryBot.create(:embedded_automation_manager_ansible) }
 
     it "#supported_catalog_types" do
       expect(ems.supported_catalog_types).to eq(%w(generic_ansible_playbook))
