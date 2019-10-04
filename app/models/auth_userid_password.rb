0f22797c1056c6b768ab2e0cedf26b71873da543
Daniel Berger <djberg96@gmail.com>
2019-10-04 14:24:08 -0400

    Typo fix.

--- app/models/auth_userid_password.rb
+++ app/models/auth_userid_password.rb
@@ -1,6 +1,6 @@
 class AuthUseridPassword < Authentication
   after_update :after_authentication_changed,
-    :if => proc { |auth| auth.saved_change_to_userid? || auth.saved_changed_to_password? }
+    :if => proc { |auth| auth.saved_change_to_userid? || auth.saved_change_to_password? }
 
   def self.display_name(number = 1)
     n_('Password', 'Passwords', number)
