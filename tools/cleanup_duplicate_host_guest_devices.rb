0af0f6a571d6180713e61429785206a5f318cf0f
Keenan Brock <keenan@thebrocks.net>
2019-10-15 11:24:28 -0400

    Merge pull request #19235 from agrare/turbo_button_for_guest_device_cleanup
    Make destroying guest_devices faster
    
    (cherry picked from commit 236cbdf91adad47c9e6ddd07f62848f482996098)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1767819

--- tools/cleanup_duplicate_host_guest_devices.rb
+++ tools/cleanup_duplicate_host_guest_devices.rb
@@ -6,6 +6,7 @@ opts = Optimist.options do
   opt :ems_id, "The ID of the ExtManagementSystem to reconnect VMs for", :type => :integer
   opt :ems_name, "The name of the ExtManagementSystem to reconnect VMs for", :type => :string
   opt :dry_run, "Just print out what would be done without modifying anything", :type => :boolean, :default => true
+  opt :page_size, "How many records to delete at a time", :type => :integer, :default => 100
 end
 
 if opts[:ems_id].nil? && opts[:ems_name].nil?
@@ -45,6 +46,28 @@ end
 
 return if opts[:dry_run]
 
-GuestDevice.destroy(guest_devices_to_delete) unless opts[:dry_run]
+associations = %i[has_one has_many].flat_map { |assoc| GuestDevice.reflect_on_all_associations(assoc) }
+dependents   = associations.select { |assoc| assoc.options[:dependent].present? }
+
+total_slices = (guest_devices_to_delete.count / opts[:page_size].to_f).ceil
+guest_devices_to_delete.each_slice(opts[:page_size]).with_index do |slice, index|
+  puts "Destroying slice #{index + 1} of #{total_slices}..."
+  GuestDevice.delete(slice)
+
+  dependents.each do |assoc|
+    delete_meth = assoc.options[:dependent]
+    foreign_key = assoc.join_keys.key
+
+    if %i[delete destroy].include?(delete_meth)
+      assoc.klass.where(foreign_key => slice).send("#{delete_meth}_all")
+    elsif delete_meth == :nullify
+      assoc.klass.where(foreign_key => slice).update_all(foreign_key => nil)
+    else
+      assoc.klass.where(foreign_key => slice).each(&delete_meth)
+    end
+  end
+
+  puts "Destroying slice #{index + 1} of #{total_slices}...Complete"
+end
 
 puts "Destroyed #{guest_devices_to_delete.count} duplicate Guest Devices"
