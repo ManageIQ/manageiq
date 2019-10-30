9e2d5fbf7398d6b2cbf9abfeeaddcb6f1513922a
Keenan Brock <keenan@thebrocks.net>
2019-10-30 17:18:44 -0400

    Extract metric/target_spec.rb

--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -4,7 +4,7 @@ describe Metric do
   before do
     MiqRegion.seed
 
-    _guid, _server, @zone = EvmSpecHelper.create_guid_miq_server_zone
+    @zone = EvmSpecHelper.local_miq_server.zone
   end
 
   describe "metrics view" do
@@ -48,18 +48,6 @@ describe Metric do
         end
 
         MiqQueue.delete_all
-      end
-
-      context "executing capture_ems_targets" do
-        it "should find enabled targets" do
-          targets = Metric::Targets.capture_ems_targets(@ems_vmware.reload)
-          assert_infra_targets_enabled targets, %w(ManageIQ::Providers::Vmware::InfraManager::Vm ManageIQ::Providers::Vmware::InfraManager::Host ManageIQ::Providers::Vmware::InfraManager::Host ManageIQ::Providers::Vmware::InfraManager::Vm ManageIQ::Providers::Vmware::InfraManager::Host Storage)
-        end
-
-        it "should find enabled targets excluding storages" do
-          targets = Metric::Targets.capture_ems_targets(@ems_vmware.reload, :exclude_storages => true)
-          assert_infra_targets_enabled targets, %w(ManageIQ::Providers::Vmware::InfraManager::Vm ManageIQ::Providers::Vmware::InfraManager::Host ManageIQ::Providers::Vmware::InfraManager::Host ManageIQ::Providers::Vmware::InfraManager::Vm ManageIQ::Providers::Vmware::InfraManager::Host)
-        end
       end
 
       context "executing perf_capture_timer" do
@@ -1032,13 +1020,6 @@ describe Metric do
         MiqQueue.delete_all
       end
 
-      context "executing capture_ems_targets" do
-        it "should find enabled targets" do
-          targets = Metric::Targets.capture_ems_targets(@ems_openstack)
-          assert_cloud_targets_enabled targets, %w(ManageIQ::Providers::Openstack::CloudManager::Vm ManageIQ::Providers::Openstack::CloudManager::Vm ManageIQ::Providers::Openstack::CloudManager::Vm ManageIQ::Providers::Openstack::CloudManager::Vm ManageIQ::Providers::Openstack::CloudManager::Vm)
-        end
-      end
-
       context "executing perf_capture_timer" do
         before do
           stub_settings(:performance => {:history => {:initial_capture_days => 7}})
@@ -1267,41 +1248,6 @@ describe Metric do
       values[0][2] = time_profile.id
     end
     assert_queued_rollup(q_item, obj.id, obj.class.name, *values)
-  end
-
-  def assert_infra_targets_enabled(targets, expected_types)
-    # infra only
-    selected_types = []
-    targets.each do |t|
-      selected_types << t.class.name
-
-      expected_enabled = case t
-                         when Vm then      t.host.perf_capture_enabled?
-                         when Host then    t.perf_capture_enabled? || t.ems_cluster.perf_capture_enabled?
-                         when Storage then t.perf_capture_enabled?
-                         end
-      expect(expected_enabled).to be_truthy
-    end
-
-    expect(selected_types).to match_array(expected_types)
-  end
-
-  def assert_cloud_targets_enabled(targets, expected_types)
-    selected_types = []
-    targets.each do |t|
-      selected_types << t.class.name
-
-      expected_enabled = case t
-                         # Vm's perf_capture_enabled? is its availability_zone's perf_capture setting,
-                         #   or true if it has no availability_zone
-                         when Vm then                t.availability_zone ? t.availability_zone.perf_capture_enabled? : true
-                         when AvailabilityZone then  t.perf_capture_enabled?
-                         when Storage then           t.perf_capture_enabled?
-                         end
-      expect(expected_enabled).to be_truthy
-    end
-
-    expect(selected_types).to match_array(expected_types)
   end
 
   def assert_perf_capture_now(target, mode)
--- spec/support/metric_helper.rb
+++ spec/support/metric_helper.rb
@@ -24,3 +24,46 @@ module Spec
     end
   end
 end
+
+
+# expecting to have setup:
+#
+# before do
+#   MiqRegion.seed
+#   @zone = EvmSpecHelper.local_miq_server.zone
+# end
+RSpec.shared_context 'with enabled/disabled vmware targets', :with_enabled_disabled_vmware do
+  before do
+    @ems_vmware = FactoryBot.create(:ems_vmware, :zone => @zone)
+    @storages = FactoryBot.create_list(:storage_target_vmware, 2)
+    @vmware_clusters = FactoryBot.create_list(:cluster_target, 2)
+    @ems_vmware.ems_clusters = @vmware_clusters
+
+    6.times do |n|
+      host = FactoryBot.create(:host_target_vmware, :ext_management_system => @ems_vmware)
+      @ems_vmware.hosts << host
+
+      @vmware_clusters[n / 2].hosts << host if n < 4
+      host.storages << @storages[n / 3]
+    end
+
+    MiqQueue.delete_all
+    @ems_vmware.reload
+  end
+
+  let(:all_targets) { Metric::Targets.capture_ems_targets(@ems_vmware) }
+end
+
+RSpec.shared_context "with openstack", :with_openstack_and_availability_zones do
+  before do
+    @ems_openstack = FactoryBot.create(:ems_openstack, :zone => @zone)
+    @availability_zone = FactoryBot.create(:availability_zone_target)
+    @ems_openstack.availability_zones << @availability_zone
+    @vms_in_az = FactoryBot.create_list(:vm_openstack, 2, :ems_id => @ems_openstack.id)
+    @availability_zone.vms = @vms_in_az
+    @availability_zone.vms.push(FactoryBot.create(:vm_openstack, :ems_id => nil))
+    @vms_not_in_az = FactoryBot.create_list(:vm_openstack, 3, :ems_id => @ems_openstack.id)
+
+    MiqQueue.delete_all
+  end
+end
