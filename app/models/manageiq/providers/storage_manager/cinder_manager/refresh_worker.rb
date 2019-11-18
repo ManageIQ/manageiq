6f4466258f20f12af6dfafbf4d60e12711b19602
Nick Carboni <ncarboni@redhat.com>
2019-11-18 14:59:49 -0500

    Remove unused refresh workers
    These were meant to be used for standalone storage managers
    (without a parent manager), but that situation has never happened.

--- app/models/manageiq/providers/storage_manager/cinder_manager/refresh_worker.rb
+++
@@ -1,7 +0,0 @@
-class ManageIQ::Providers::StorageManager::CinderManager::RefreshWorker < ::MiqEmsRefreshWorker
-  require_nested :Runner
-
-  def self.settings_name
-    :ems_refresh_worker_cinder
-  end
-end
--- spec/models/manageiq/providers/storage_manager/swift_manager/refresh_worker_spec.rb
+++
@@ -1,7 +0,0 @@
-describe ManageIQ::Providers::StorageManager::SwiftManager::RefreshWorker do
-  describe ".ems_class" do
-    it "is the parent manager" do
-      expect(described_class.ems_class).to eq(ManageIQ::Providers::StorageManager::SwiftManager)
-    end
-  end
-end
