13e4517a16d283701ac02edd700fb291b7fa7d35
Jason Frey <jfrey@redhat.com>
2019-11-19 21:18:53 -0500

    Add comment to clarify intent

--- lib/extensions/require_nested.rb
+++ lib/extensions/require_nested.rb
@@ -11,6 +11,27 @@ module RequireNested
       require_dependency filename
     end
 
+    # If the nested constant has a top-level constant with the same name, then both
+    # must be defined at the same time, otherwise the Rails autoloader can get
+    # confused.
+    #
+    # For example, suppose we have the following
+    #
+    #     # app/models/thing_grouper.rb
+    #     class ThingGrouper < ApplicationRecord
+    #       require_nested :Thing
+    #     end
+    #
+    #     # app/models/thing_grouper/thing.rb
+    #     class ThingGrouper::Thing; end
+    #
+    #     # app/models/thing.rb
+    #     class Thing < ApplicationRecord; end
+    #
+    # When the require_nested call is made, we will define `ThingGrouper`'s nested
+    # `Thing`, but at the same time we must also define `::Thing` in order to allow
+    # the Rails autloader to work correctly.  We do this by using a special-case call
+    # to `require_nested` on `Object`, if a top-level `thing.rb` file exists.
     if ActiveSupport::Dependencies.search_for_file(name.to_s.underscore) && self != Object
       Object.require_nested name
     end
