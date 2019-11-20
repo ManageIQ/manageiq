2a116ec6f5534ba2e883681bc0c3cbe09c840211
Jason Frey <jfrey@redhat.com>
2019-11-19 21:20:43 -0500

    Raise exception if nested file will never be found
    It is possible to give autoload an invalid path, and that issue will
    never be found until the associated constant is vivified.  This causes
    issues where people forget to remove the require_nested calls when they
    remove the files.  Now, it's not possible to forget, because an
    exception will be raised as soon as the parent file is loaded.

--- lib/extensions/require_nested.rb
+++ lib/extensions/require_nested.rb
@@ -6,6 +6,7 @@ module RequireNested
     filename = "#{self}::#{name}".underscore
     filename = name.to_s.underscore if self == Object
     if Rails.application.config.cache_classes
+      raise LoadError, "No such file to load -- #{filename}" unless ActiveSupport::Dependencies.search_for_file(filename)
       autoload name, filename
     else
       require_dependency filename
