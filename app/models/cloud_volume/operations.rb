340f9ec3dabeb95a5be526bb360d72e80ce78dc9
Daniel Berger <djberg96@gmail.com>
2019-12-06 14:01:19 -0500

    Add queue_name to ems operations within CloudVolume::Operations.

--- app/models/cloud_volume/operations.rb
+++ app/models/cloud_volume/operations.rb
@@ -5,19 +5,27 @@ module CloudVolume::Operations
   end
 
   module InstanceMethods
+
+    # Attach a cloud volume as a queued task and return the task id. The queue
+    # name and the queue zone are derived from the server EMS, and both a userid
+    # and server EMS ref are mandatory. The device is optional.
+    #
     def attach_volume_queue(userid, server_ems_ref, device = nil)
       task_opts = {
         :action => "attaching Cloud Volume for user #{userid}",
         :userid => userid
       }
+
       queue_opts = {
         :class_name  => self.class.name,
         :method_name => 'attach_volume',
         :instance_id => id,
         :role        => 'ems_operations',
+        :queue_name  => ext_management_system.queue_name_for_ems_operations,
         :zone        => ext_management_system.my_zone,
         :args        => [server_ems_ref, device]
       }
+
       MiqTask.generic_action_with_callback(task_opts, queue_opts)
     end
 
@@ -29,19 +37,26 @@ module CloudVolume::Operations
       validate_unsupported(_("Attach Volume Operation"))
     end
 
+    # Detach a cloud volume as a queued task and return the task id. The queue
+    # name and the queue zone are derived from the server EMS, and both a userid
+    # and server EMS ref are mandatory.
+    #
     def detach_volume_queue(userid, server_ems_ref)
       task_opts = {
         :action => "detaching Cloud Volume for user #{userid}",
         :userid => userid
       }
+
       queue_opts = {
         :class_name  => self.class.name,
         :method_name => 'detach_volume',
         :instance_id => id,
         :role        => 'ems_operations',
+        :queue_name  => ext_management_system.queue_name_for_ems_operations,
         :zone        => ext_management_system.my_zone,
         :args        => [server_ems_ref]
       }
+
       MiqTask.generic_action_with_callback(task_opts, queue_opts)
     end
 
