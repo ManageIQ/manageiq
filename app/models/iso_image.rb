ccec54f5d53d047ff0b9e43cb9958ee7da310a8f
Zita Nemeckova <znemecko@redhat.com>
2019-12-09 12:47:42 +0100

    Add tags to IsoImage

--- app/models/iso_image.rb
+++ app/models/iso_image.rb
@@ -4,6 +4,8 @@ class IsoImage < ApplicationRecord
 
   has_many :customization_templates, :through => :pxe_image_type
 
+  acts_as_miq_taggable
+
   def self.display_name(number = 1)
     n_('ISO Image', 'ISO Images', number)
   end
