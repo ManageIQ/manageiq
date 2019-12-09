f0e480272fc42105a5df6b6772e65366e43bba2d
Daniel Berger <djberg96@gmail.com>
2019-12-09 12:53:10 -0500

    Add initial set of specs.

---
+++ spec/models/vm_or_template/operations/snapshot_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe VmOrTemplate::Operations::Snapshot do
+  before { EvmSpecHelper.local_miq_server }
+
+  let(:ems)      { FactoryBot.create(:ems_vmware) }
+  let(:vm)       { FactoryBot.create(:vm_vmware, :ext_management_system => ems) }
+  let(:snapshot) { FactoryBot.create(:snapshot, :vm_or_template => vm) }
+
+  context "queued methods" do
+    it 'queues an update task with remove_snapshot_queue' do
+      queue = vm.remove_snapshot_queue(snapshot.id)
+
+      expect(queue).to have_attributes(
+        :class_name  => vm.class.name,
+        :method_name => 'remove_snapshot',
+        :role        => 'ems_operations',
+        :queue_name  => 'generic',
+        :zone        => vm.my_zone,
+        :args        => [snapshot.id],
+        :task_id     => nil
+      )
+    end
+  end
+end
