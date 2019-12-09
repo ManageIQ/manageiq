f9c2543cbaed0d7ff96e6d641b167ddd0a96735a
Zita Nemeckova <znemecko@redhat.com>
2019-12-09 12:48:09 +0100

    Add tags to WindowsImage

--- app/models/windows_image.rb
+++ app/models/windows_image.rb
@@ -4,6 +4,8 @@ class WindowsImage < ApplicationRecord
 
   has_many :customization_templates, :through => :pxe_image_type
 
+  acts_as_miq_taggable
+
   def self.display_name(number = 1)
     n_('Windows Image', 'Windows Images', number)
   end
