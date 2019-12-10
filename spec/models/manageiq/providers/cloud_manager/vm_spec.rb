522aebeb3ecb93f1f0ea03fea29d483dd8d8d9c2
Daniel Berger <djberg96@gmail.com>
2019-12-10 12:57:07 -0500

    Add queue name to associate_floating_ip_queue method, and add specs.

--- spec/models/manageiq/providers/cloud_manager/vm_spec.rb
+++ spec/models/manageiq/providers/cloud_manager/vm_spec.rb
@@ -1,5 +1,9 @@
 describe VmCloud do
-  subject { FactoryBot.create(:vm_cloud) }
+  let(:ems) { FactoryBot.create(:ems_cloud) }
+  let(:user) { FactoryBot.create(:user, :userid => 'test') }
+  let(:queue_name) { 'vm_cloud_queue' }
+
+  subject { FactoryBot.create(:vm_cloud, :ext_management_system => ems) }
 
   context "relationships" do
     let(:resource_group) { FactoryBot.create(:resource_group) }
@@ -42,6 +46,36 @@ describe VmCloud do
         expect(subject.service).to eq(service_root)
         expect(subject.direct_service).to eq(service)
       end
+    end
+  end
+
+  context "queued methods" do
+    before do
+      allow(ems).to receive(:queue_name_for_ems_operations).and_return(queue_name)
+    end
+
+    it 'queues an associate floating IP task with associate_floating_ip_queue' do
+      ip_address = '1.2.3.4'
+      task_id = subject.associate_floating_ip_queue(user.userid, ip_address)
+
+      expect(MiqTask.find(task_id)).to have_attributes(
+        :name   => "associating floating IP with Instance for user #{user.userid}",
+        :state  => "Queued",
+        :status => "Ok"
+      )
+
+      expect(MiqQueue.where(:class_name => described_class.name).first).to have_attributes(
+        :class_name  => described_class.name,
+        :method_name => 'associate_floating_ip',
+        :role        => 'ems_operations',
+        :queue_name  => queue_name,
+        :zone        => ems.my_zone,
+        :args        => [ip_address]
+      )
+    end
+
+    it 'requires an ip address for the associate floating ip queue task' do
+      expect { subject.associate_floating_ip_queue }.to raise_error(ArgumentError)
     end
   end
 end
