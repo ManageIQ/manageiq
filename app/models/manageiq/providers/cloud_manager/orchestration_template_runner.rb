ccc0b9c679aec5d5355776f450ecbaa15d4f2c8f
Daniel Berger <djberg96@gmail.com>
2019-12-12 11:13:47 -0500

    Add explicit deliver_on and priority to queue_signal.

--- app/models/manageiq/providers/cloud_manager/orchestration_template_runner.rb
+++ app/models/manageiq/providers/cloud_manager/orchestration_template_runner.rb
@@ -96,8 +96,8 @@ class ManageIQ::Providers::CloudManager::OrchestrationTemplateRunner < ::Job
     end
   end
 
-  def queue_signal
-    super(role: 'ems_operations')
+  def queue_signal(*args, deliver_on: nil, priority: MiqQueue::NORMAL_PRIORITY)
+    super(*args, role: 'ems_operations', deliver_on: deliver_on, priority: priority)
   end
 
   def orchestration_stack
