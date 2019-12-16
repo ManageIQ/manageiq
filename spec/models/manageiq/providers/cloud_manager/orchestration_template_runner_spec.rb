b126cd85738851e001b489b67f89c042d6a997f6
Daniel Berger <djberg96@gmail.com>
2019-12-16 09:44:46 -0500

    Fix expected instance id, fix zone handling.

--- spec/models/manageiq/providers/cloud_manager/orchestration_template_runner_spec.rb
+++ spec/models/manageiq/providers/cloud_manager/orchestration_template_runner_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe ManageIQ::Providers::CloudManager::OrchestrationTemplateRunner do
 
   context "#queue_signal" do
     it "queues a signal with the queue_signal method" do
-      runner = described_class.create_job(:options => {:orchestration_template_id => template.id, :zone => zone})
+      runner = described_class.create_job(:options => {:orchestration_template_id => template.id}, :zone => zone.name)
       queue = runner.queue_signal
 
       expect(queue).to have_attributes(
         :class_name  => described_class.name,
         :method_name => 'signal',
-        :instance_id => template.id,
+        :instance_id => runner.id,
         :role        => 'ems_operations',
         :queue_name  => 'generic',
-        :zone        => zone.id,
+        :zone        => zone.name,
         :priority    => MiqQueue::NORMAL_PRIORITY,
         :args        => []
       )
