48dedee322148ccaef4d5ca38de19903a22db89b
Jason Frey <jfrey@redhat.com>
2019-12-16 11:47:11 -0500

    Merge pull request #19645 from lpichler/add_trait_in_other_region_to_time_profile
    Add trait in_other_region to TimeProfile factory
    
    (cherry picked from commit f0431da6f69327d36e9f07fc060104298738e4ef)

--- spec/factories/time_profile.rb
+++ spec/factories/time_profile.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :time_profile do
     sequence(:description) { |n| "Time Profile #{seq_padded_for_sorting(n)}" }
+
+    trait :in_other_region do
+      other_region
+    end
   end
 
   factory :time_profile_with_rollup, :parent => :time_profile do
