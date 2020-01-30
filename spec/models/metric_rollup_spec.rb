6aafbd6c0c889455b47fdc555880ed0e8b7c3e85
Keenan Brock <keenan@thebrocks.net>
2020-01-30 10:16:05 -0500

    removing invalid polymorphic test
    it is not valid to references() a polymorphic column
    
    Our understanding was not quite right before.
    we did want to preload/include the column, just not use references

--- spec/models/metric_rollup_spec.rb
+++ spec/models/metric_rollup_spec.rb
@@ -20,25 +20,6 @@ RSpec.describe MetricRollup do
     end
   end
 
-  context "test" do
-    it "should not raise an error when a polymorphic reflection is included and references are specified in a query" do
-      skip "until ActiveRecord is fixed"
-      # TODO: A fix in ActiveRecord will make this test pass
-      expect do
-        MetricRollup.where(:id => 1)
-                    .includes(:resource => {}, :time_profile => {})
-                    .references(:time_profile => {}).last
-      end.not_to raise_error
-
-      # TODO: Also, there is a bug that exists in only the manageiq repo and not rails
-      # TODO: that causes the error "ActiveRecord::ConfigurationError: nil"
-      # TODO: instead of the expected "ActiveRecord::EagerLoadPolymorphicError" error.
-      expect do
-        Tagging.includes(:taggable => {}).where('bogus_table.column = 1').references(:bogus_table).to_a
-      end.to raise_error ActiveRecord::EagerLoadPolymorphicError
-    end
-  end
-
   context ".rollups_in_range" do
     before do
       @current = FactoryBot.create_list(:metric_rollup_vm_hr, 2)
