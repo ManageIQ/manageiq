89cdc02b9c1820cf76746c87ba308f00aca363c2
Keenan Brock <keenan@thebrocks.net>
2020-02-03 14:55:04 -0500

    remaining pieces of skip_references

--- app/models/miq_report.rb
+++ app/models/miq_report.rb
@@ -47,7 +47,7 @@ class MiqReport < ApplicationRecord
   attr_accessor :ext_options
   attr_accessor_that_yamls :table, :sub_table, :filter_summary, :extras, :ids, :scoped_association, :html_title, :file_name,
                            :extras, :record_id, :tl_times, :user_categories, :trend_data, :performance, :include_for_find,
-                           :report_run_time, :chart, :skip_references
+                           :report_run_time, :chart
 
   attr_accessor_that_yamls :reserved # For legacy imports
 
