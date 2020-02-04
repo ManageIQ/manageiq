f4e01117a050bc740643263e046c075b486243a7
Daniel Berger <djberg96@gmail.com>
2020-02-04 05:07:27 -0500

    Refactor MiqRequestTask::StateMachine specs.

--- spec/models/miq_request_task/state_machine_spec.rb
+++ spec/models/miq_request_task/state_machine_spec.rb
@@ -1,15 +1,19 @@
 RSpec.describe MiqRequestTask do
+  let(:task) { FactoryBot.create(:miq_request_task) }
+
   context "::StateMachine" do
     context "#signal" do
       it "will deal with exceptions" do
-        task = FactoryBot.create(:miq_request_task)
         allow(task).to receive_messages(:miq_request => double("MiqRequest").as_null_object)
         exception = String.xxx rescue $!
-        allow(task).to receive(:some_state).and_raise(exception)
+        allow(task).to receive(:send).with(:some_state).and_raise(exception)
 
         expect($log).to receive(:error).with(/#{exception.class.name}/)
         expect($log).to receive(:error).with(exception.backtrace.join("\n"))
-        expect(task).to receive(:finish)
+
+        expect(task).to receive(:signal).with(:some_state)
+        expect(task).to receive(:signal).with(:provision_error)
+        expect(task).to receive(:signal).with(:finish)
 
         task.signal(:some_state)
 
