83136b470c4effb425d1709a960e7a4f9a2363b1
Joe Rafaniello <jrafanie@redhat.com>
2020-02-14 14:59:16 -0500

    Eager load Rails::Server and lazy load them otherwise
    Parsing routes.rb for the application and any engine is an expensive operation
    if you have numerous routes.  These routes are typically never used in rails
    console, runner, and other backend rails processes that just load
    config/environment.
    
    Eliminating routes processing in manageiq:
    * improves rails boot by nearly 10%
    * decreases the retained objects at boot by nearly 60%
    * decreases memory slightly
    
    This helps to isolate the bloat that the dynamic :action segment removal caused
    with exploding the number of routes, see also:
    https://github.com/rails/rails/issues/27231#issue-192637500

---
+++ config/initializers/lazy_load_routes.rb
@@ -0,0 +1,42 @@
+# To lazy load routes:
+# 1) Modify the RoutesReloader to skip normal reload! unless opted-in or Rails server
+# 2) Insert a Rack::Middleware to catch the first request with empty routes, opt-in and reload!
+# 3) Modify the RoutesInspector used by rake routes to detect empty routes, opt-in and reload!
+module RoutesReloaderLazyLoadRoutes
+  def reload!
+    if $force_routes_load || defined?(Rails::Server)
+      puts "loading routes..."
+      super
+    end
+  end
+end
+Rails::Application::RoutesReloader.prepend(RoutesReloaderLazyLoadRoutes)
+
+class LazyLoadRoutesMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if Rails.application.routes.empty?
+      $force_routes_load = true
+      Rails.application.reloader.reload!
+    end
+    @app.call(env)
+  end
+end
+Rails.application.config.middleware.use LazyLoadRoutesMiddleware
+
+module RoutesInspectorLazyLoadRoutes
+  def format(*args)
+    if @routes.empty?
+      $force_routes_load = true
+      Rails.application.reloader.reload!
+    end
+    super
+  end
+end
+
+# This is loaded anyway by actionpack/lib/action_dispatch/middleware/debug_exceptions.rb
+require "action_dispatch/routing/inspector"
+ActionDispatch::Routing::RoutesInspector.prepend(RoutesInspectorLazyLoadRoutes)
