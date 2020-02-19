fa6a0995ea5deb1fbfd622d0de560029e1ae88b1
d-m-u <drewuhlmann@gmail.com>
2020-02-19 11:03:32 -0500

    Add zone delegation

--- app/models/ems_folder.rb
+++ app/models/ems_folder.rb
@@ -19,6 +19,7 @@ class EmsFolder < ApplicationRecord
   virtual_has_many :hosts,             :uses => :all_relationships
 
   delegate :queue_name_for_ems_operations, :to => :ext_management_system, :allow_nil => true
+  delegate :my_zone, :to => :ext_management_system
 
   #
   # Relationship methods
