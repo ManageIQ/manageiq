a69b2c7151f5295449545e50b67217881954de55
Nick Carboni <ncarboni@redhat.com>
2020-03-03 14:31:42 -0500

    Start memcached even if the database is initialized
    Fixes #19879

--- docker-assets/appliance-initialize.sh
+++ docker-assets/appliance-initialize.sh
@@ -4,6 +4,10 @@
 [ -f /etc/default/evm ] &&  . /etc/default/evm
 [[ -s ${CONTAINER_SCRIPTS_ROOT}/container-deploy-common.sh ]] && source "${CONTAINER_SCRIPTS_ROOT}/container-deploy-common.sh"
 
+# Check if memcached is running, if not start it
+pidof memcached
+test $? -ne 0 && /usr/bin/memcached -u memcached -p 11211 -m 64 -c 1024 -l 127.0.0.1 -d
+
 function create_v2_key() {
   V2_KEY=$(ruby -ropenssl -rbase64 -e 'puts Base64.strict_encode64(Digest::SHA256.digest(OpenSSL::Random.random_bytes(32))[0, 32])')
   write_v2_key
@@ -34,10 +38,6 @@ else
   echo "** Creating MIQ role"
   su postgres -c "psql -c \"CREATE ROLE root SUPERUSER LOGIN PASSWORD 'smartvm'\""
   test $? -ne 0 && echo "!! Failed to inject MIQ root Role" && exit 1
-
-  # Check if memcached is running, if not start it
-  pidof memcached
-  test $? -ne 0 && /usr/bin/memcached -u memcached -p 11211 -m 64 -c 1024 -l 127.0.0.1 -d
 
   echo "** Starting DB setup"
   pushd ${APP_ROOT}
