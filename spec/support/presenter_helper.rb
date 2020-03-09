b40896d955884d173c2b9f7f913a06d16042eeee
Daniel Berger <djberg96@gmail.com>
2020-03-09 07:29:25 -0400

    Remove invalid partials from presenter helper.

--- spec/support/presenter_helper.rb
+++ spec/support/presenter_helper.rb
@@ -5,8 +5,6 @@ module Spec
 
       def login_as(user)
         User.current_user = user
-        allow_any_instance_of(ActionController::TestSession).to receive(:userid).and_return(user.userid)
-        allow_any_instance_of(ActionController::TestSession).to receive(:group).and_return(user.current_group.try(:id))
       end
     end
   end
