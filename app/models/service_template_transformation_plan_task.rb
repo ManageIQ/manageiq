eabc200b7c14771787feb97b346eaba137f30828
Satoe Imaishi <simaishi@redhat.com>
2020-03-09 13:56:55 -0400

    Revert "Merge pull request #19870 from djberg96/default_kill_virtv2v_signal"
    This reverts commit 5e2e169863c0203b210c8f0f0555d19b0f7604eb.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1806750

--- app/models/service_template_transformation_plan_task.rb
+++ app/models/service_template_transformation_plan_task.rb
@@ -270,7 +270,7 @@ class ServiceTemplateTransformationPlanTask < ServiceTemplateProvisionTask
     end
   end
 
-  def kill_virtv2v(signal = 'TERM')
+  def kill_virtv2v
     get_conversion_state
 
     unless virtv2v_running?
@@ -279,7 +279,7 @@ class ServiceTemplateTransformationPlanTask < ServiceTemplateProvisionTask
     end
 
     _log.info("Killing conversion pod for task '#{id}'.")
-    conversion_host.kill_virtv2v(id, signal)
+    conversion_host.kill_virtv2v(id)
   end
 
   def virtv2v_running?
