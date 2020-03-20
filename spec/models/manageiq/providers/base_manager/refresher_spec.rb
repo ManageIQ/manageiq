28e63ca05aa553dbbc4ee284fb5f07ff8d975259
d-m-u <drewuhlmann@gmail.com>
2020-03-20 18:08:37 -0400

    Remove inventory_obj_refresher
    https://github.com/ManageIQ/manageiq/pull/19972#issuecomment-601926020
    hehe

--- spec/models/manageiq/providers/base_manager/refresher_spec.rb
+++ spec/models/manageiq/providers/base_manager/refresher_spec.rb
@@ -11,20 +11,66 @@ RSpec.describe ManageIQ::Providers::BaseManager::Refresher do
     end
   end
 
-  context "#refresh" do
-    context "#preprocess_targets" do
-      let(:ems) { FactoryBot.create(:ext_management_system) }
-      let(:vm)  { FactoryBot.create(:vm, :ext_management_system => ems) }
-      let(:lots_of_vms) do
-        num_targets = Settings.ems_refresh.full_refresh_threshold + 1
-        Array.new(num_targets) { FactoryBot.create(:vm, :ext_management_system => ems) }
+  context "#preprocess_targets" do
+    let(:ems) { FactoryBot.create(:ext_management_system) }
+    let(:vm)  { FactoryBot.create(:vm, :ext_management_system => ems) }
+    let(:stack) { FactoryBot.create(:orchestration_stack, :ext_management_system => ems) }
+    let(:lots_of_vms) do
+      num_targets = Settings.ems_refresh.full_refresh_threshold + 1
+      Array.new(num_targets) { FactoryBot.create(:vm, :ext_management_system => ems) }
+    end
+
+    context "allow_targeted_refresh true" do
+      before { allow(ems).to receive(:allow_targeted_refresh?).and_return(true) }
+      it "does full refresh on any event" do
+        refresher = described_class.new([vm])
+        refresher.preprocess_targets
+
+        targets_by_ems = refresher.targets_by_ems_id[vm.ext_management_system.id].first
+        expect(targets_by_ems).to be_a(InventoryRefresh::TargetCollection)
+        expect(targets_by_ems.targets.first).to eq(vm)
       end
 
+      it "does a refresh with a stack" do
+        refresher = described_class.new([stack])
+        refresher.preprocess_targets
+
+        targets_by_ems = refresher.targets_by_ems_id[stack.ext_management_system.id].first
+        expect(targets_by_ems).to be_a(InventoryRefresh::TargetCollection)
+        expect(targets_by_ems.targets.first).to eq(stack)
+      end
+
+      it "does a full refresh with an EMS and a VM" do
+        refresher = described_class.new([vm, ems])
+        refresher.preprocess_targets
+
+        expect(refresher.targets_by_ems_id[vm.ext_management_system.id]).to eq([ems])
+      end
+
+      it "does a full refresh with a lot of targets" do
+        refresher = described_class.new(lots_of_vms)
+        refresher.preprocess_targets
+
+        targets_by_ems = refresher.targets_by_ems_id[vm.ext_management_system.id].first
+        expect(targets_by_ems).to be_a(InventoryRefresh::TargetCollection)
+        expect(targets_by_ems.targets.count).to eq(101)
+      end
+    end
+
+    context "allow_targeted_refresh false" do
+      before { allow(ems).to receive(:allow_targeted_refresh?).and_return(false) }
       it "keeps a single vm target" do
         refresher = described_class.new([vm])
         refresher.preprocess_targets
 
-        expect(refresher.targets_by_ems_id[vm.ext_management_system.id]).to eq([vm])
+        expect(refresher.targets_by_ems_id[vm.ext_management_system.id]).to eq([ems])
+      end
+
+      it "does a refresh with a stack" do
+        refresher = described_class.new([stack])
+        refresher.preprocess_targets
+
+        expect(refresher.targets_by_ems_id[stack.ext_management_system.id].first).to eq(ems)
       end
 
       it "does a full refresh with an EMS and a VM" do
