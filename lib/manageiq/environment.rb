381bf65c41fe3ce8594e3d2b9fb789439594da16
Brandon Dunne <brandondunne@hotmail.com>
2020-04-03 16:25:46 -0400

    Move messaging config to files
    - Ignore config/messaging.yml
    - Move example configs for artemis and kafka to files
    - Update messaging_client_options to read from files instead of Settings
    - Update config file list in ManageIQ::Environment for bin/setup and bin/update
    - Add support for encrypted passwords
    - Add tests

--- lib/manageiq/environment.rb
+++ lib/manageiq/environment.rb
@@ -28,9 +28,10 @@ module ManageIQ
 
     def self.ensure_config_files
       config_files = {
-        "certs/v2_key.dev"        => "certs/v2_key",
-        "config/cable.yml.sample" => "config/cable.yml",
-        "config/database.pg.yml"  => "config/database.yml",
+        "certs/v2_key.dev"           => "certs/v2_key",
+        "config/cable.yml.sample"    => "config/cable.yml",
+        "config/database.pg.yml"     => "config/database.yml",
+        "config/messaging.kafka.yml" => "config/messaging.yml",
       }
 
       config_files.each do |source, dest|
