e9e48775a0b9fd113145165661f55af5ce610cbe
Siddhesh-Ghadi <61187612+Siddhesh-Ghadi@users.noreply.github.com>
2020-04-28 13:28:56 +0530

    Add cxxflags for unf_ext gem installation on power
    Installation of unf_ext gem < v0.0.7.4 fails on power
    
    More info: https://github.com/ManageIQ/manageiq-pods/issues/460#issuecomment-617071986

--- tools/ci/setup_ruby_env.sh
+++ tools/ci/setup_ruby_env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./tools/ci/setup_ruby_environment.rb
 bundle config --local build.sassc --disable-march-tune-native
+bundle config --local build.unf_ext --with-cxxflags=-fsigned-char
 export BUNDLE_WITHOUT=development
 export BUNDLE_GEMFILE=${PWD}/Gemfile
