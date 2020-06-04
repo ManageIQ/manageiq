25f39dbd0f7b0010e0ded6490dc760a109afd055
Fabien Dupont <fdupont@redhat.com>
2020-06-04 17:58:22 -0400

    Remove references to v2v
    Part of https://github.com/ManageIQ/manageiq/issues/21379

--- app/models/infra_conversion_job/dispatcher.rb
+++ app/models/infra_conversion_job/dispatcher.rb
@@ -5,12 +5,6 @@ class InfraConversionJob
     end
 
     def dispatch
-      _, total_time = Benchmark.realtime_block(:total_time) do
-        Benchmark.realtime_block(:v2v_dispatching) { dispatch_v2v_migrations }
-        Benchmark.realtime_block(:v2v_limits) { apply_v2v_limits }
-      end
-
-      _log.info("Complete - Timings: #{total_time.inspect}")
     end
   end
 end
