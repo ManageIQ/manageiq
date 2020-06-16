bbdc24ccf154b7af9cb56513d880d0268b3712d1
Martin Hradil <mhradil@redhat.com>
2020-06-16 16:37:18 +0000

    Also remove TemplateXen and the factory

--- spec/factories/vm_or_template.rb
+++ spec/factories/vm_or_template.rb
@@ -69,8 +69,6 @@ FactoryBot.define do
     vendor { "vmware" }
   end
 
-  factory(:template_xen, :class => "TemplateXen", :parent => :template_infra)
-
   factory :vm_amazon, :class => "ManageIQ::Providers::Amazon::CloudManager::Vm", :parent => :vm_cloud do
     location { |x| "#{x.name}.us-west-1.compute.amazonaws.com" }
     vendor   { "amazon" }
