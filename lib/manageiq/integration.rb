a4688f536bdf2060b4559e5d6918ea3bb31f4f9e
Nick LaMuro <nicklamuro@gmail.com>
2020-06-29 13:46:25 -0500

    Add integration.rake tasks
    Helper tasks that is used to run the necessary MiqWorker components for
    running integration tests.
    
    CYPRESS_DEV
    -----------
    
    Note on different modes:
    
    - no `CYPRESS_DEV`:  "prod mode" where assets are static (CI, "faster")
    - w/ `CYPRESS_DEV`:  dev mode where javascript assets might be edited
    
    When using $CYPRESS_DEV, it is assumed that the user is modifying specs
    or what they are testing, so being able to change those without having
    to re-compile is ideal, but on CI, compiling should be a speed up for
    the specs overall, and more "production like".

---
+++ lib/manageiq/integration.rb
@@ -0,0 +1,9 @@
+require_relative 'integration/environment'
+require_relative 'integration/foreman_manager'
+require_relative 'integration/cypress_rake_task'
+
+module ManageIQ
+  module Integration
+
+  end
+end
