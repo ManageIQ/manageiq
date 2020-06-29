5c95c77a1a61aa887ed0739d2f2ec851f9612c9a
Nick LaMuro <nicklamuro@gmail.com>
2020-06-29 13:46:25 -0500

    Add integration.rake tasks
    Helper tasks that is used to run the necessary MiqWorker components for
    running integration tests.
    
    CYPRESS_DEV
    -----------
    
    Note on different modes:
    
    - no `CYPRESS_DEV`:  "prod mode" where assets are static (CI, "faster")
    - w/ `CYPRESS_DEV`:  dev mode where javascript assets might be edited
    
    When using $CYPRESS_DEV, it is assumed that the user is modifying specs
    or what they are testing, so being able to change those without having
    to re-compile is ideal, but on CI, compiling should be a speed up for
    the specs overall, and more "production like".

---
+++ lib/manageiq/integration/foreman_manager.rb
@@ -0,0 +1,208 @@
+require "fileutils"
+
+# A wrapper around the foreman utility for running ManageIQ in the integration
+# environment, with either the server acting more "dev like" or "prod like"
+# depending on the scenario (local development versus CI).
+#
+module ManageIQ
+  module Integration
+    class ForemanManager
+      def self.pid_filename
+        @pid_filename ||= Environment.tmp_dir.join('foreman.pid')
+      end
+
+      def self.log_file
+        Rails.root.join("log", "integration.foreman.log")
+      end
+
+      def self.procfile
+        @procfile ||= Environment.tmp_dir.join('Procfile')
+      end
+
+      def self.procfile_template_data
+        require 'erb'
+        template_path = File.expand_path('Procfile.example.erb', __dir__)
+
+        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new(2.6)
+          ERB.new(File.read(template_path), trim_mode: "-").result(binding)
+        else
+          ERB.new(File.read(template_path), nil, "-").result(binding)
+        end
+      end
+
+      def self.setup
+        FileUtils.mkdir_p(Environment.tmp_dir)
+        FileUtils.rm_f(procfile)
+
+        File.write(procfile, procfile_template_data)
+      end
+
+      def self.run
+        if process_alive?
+          warn "foreman process already running with pid: #{server_pid} from #{pid_filename}"
+        elsif ui_ping
+          ui_server_host = "#{Environment.ui_host}:#{Environment.ui_port}"
+          warn <<~WARN
+            Existing process for #{ui_server_host} exist already!  Cowardly refusing to kill it...
+
+            Unless you "absolutely know what you are doing", make sure this
+            environment is running in `integration` mode, or restart this test
+            run after you have closed that server down so this task can execute
+            an isolated process for you.  Otherwise test will likely fail due
+            to missing resources that only exist in the integration ENV.
+
+          WARN
+        else
+          foreman_cmd = %W[
+            foreman start --port=3000
+                          --root=#{Rails.root}
+                          --procfile=#{procfile}
+          ].join(" ")
+
+          pid = Process.spawn(foreman_cmd, [:out, :err] => [log_file, "a"])
+          Process.detach(pid)
+          File.write(pid_filename, pid)
+        end
+      end
+
+      def self.stop
+        PidFile.new(pid_filename).terminate
+      end
+
+      # Simple status check:
+      #
+      # - Checks for existing pid file
+      # - If the process is running
+      # - If the UI is responding to a request
+      #
+      def self.status
+        if server_pid
+          puts "Server PID##{server_pid}: ".ljust(18) + (process_alive? ? "running" : "stopped")
+        else
+          puts "Server PID#NA:    PID file (#{pid_filename}) doesn't exist..."
+        end
+        puts "UI running?:      #{ui_ping}"
+      end
+
+      def self.process_alive?
+        PidFile.new(pid_filename).running?
+      end
+
+      def self.server_pid
+        PidFile.new(pid_filename).pid
+      end
+
+      def self.ui_ping
+        require 'net/http'
+
+        net_http = Net::HTTP.new(Environment.ui_host, Environment.ui_port)
+        net_http.request_head(Environment.ui_ping_route).code == "200"
+      rescue Errno::ECONNREFUSED
+        false
+      end
+
+      def self.ui_running?
+        ui_ready = false
+        wait_num = ENV["CI"] ? 60 : 15
+
+        wait_num.times do
+          break if (ui_ready = ui_ping)
+
+          sleep 2
+        end
+
+        unless ui_ready
+          raise "ERR: UI server was not able to start or start properly" \
+                " (after waiting ~#{wait_num * 2 / 60.0} minutes)"
+        end
+      rescue => err
+        if ENV["CI"]
+          stop
+          sleep 5 # allow server to stop and flush logs
+
+          debug_output_for_server_logs
+          debug_server_boot
+        end
+
+        raise err
+      end
+
+      def self.debug_output_for_server_logs
+        integration_log_file = Rails.root.join("log", "integration.log")
+        evm_log_file         = Rails.root.join("log", "evm.log")
+
+        puts <<~DEBUGGING_OUTPUT
+
+          ***************************************************
+          *     Printing Debugging log info for server:     *
+          ***************************************************
+
+          Procfile contents
+          -----------------
+
+          #{File.read(procfile)}
+
+
+          last 100 lines of '#{log_file}':
+          #{'-' * (21 + log_file.to_s.length)}
+
+          #{tail_file(log_file)}
+
+
+          last 100 lines of 'log/integration.log':
+          ----------------------------------------
+
+          #{tail_file(integration_log_file)}
+
+
+          last 100 lines of 'log/evm.log':
+          --------------------------------
+
+          #{tail_file(evm_log_file)}
+
+
+        DEBUGGING_OUTPUT
+      end
+
+      def self.debug_server_boot
+        log_file  = Rails.root.join("log", "integration.debug.log")
+        test_boot = "#{Gem.ruby} #{Environment.run_single_worker_bin} MiqUiWorker"
+        pid       = Process.spawn(test_boot, [:out, :err] => [log_file, 'w'])
+
+        puts "running inline ruby server to test for errors..."
+        puts
+        puts "  cmd: #{test_boot}"
+        puts
+
+        Timeout.timeout(30) { Process.waitpid(pid) }
+      rescue Timeout::Error => e
+        Process.kill("TERM", pid)
+      ensure
+        sleep 5 # give a few seconds for the logs output to flush to log_file
+
+        puts File.read(log_file)
+        puts
+        puts
+      end
+
+      def self.tail_file(filename, num_lines = 100)
+        require 'elif'
+
+        line_buffer = []
+
+        Elif.open(filename) do |log_io|
+          num_lines.times do
+            log_line = log_io.readline
+            break if log_line.nil?
+            line_buffer.unshift log_io.readline
+          rescue EOFError
+            # if we reach then end of a file, just end the loop
+          end
+        end
+
+        # new lines are already included, so just convert to a string
+        line_buffer.join
+      end
+    end
+  end
+end
---
+++ lib/tasks/integration.rake
@@ -0,0 +1,69 @@
+namespace :integration do
+  desc "Seed the database and configure assets for integration tests"
+  task :seed => ['seed:all']
+
+  namespace :seed do
+    task :all    => [:db, :assets]
+    task :db     => [:env, "test:vmdb:setup", "evm:foreman:seed"]
+    task :assets => [:env, "test:initialize", :compile_assets]
+
+    task :compile_assets do |rake_task|
+      app_prefix = rake_task.name.chomp('integration:seed:compile_assets')
+      if ENV["CYPRESS_DEV"]
+        Rake::Task["#{app_prefix}update:ui"].invoke
+      else
+        Rake::Task["#{app_prefix}evm:compile_assets"].invoke
+      end
+    end
+  end
+
+  desc "Setup the database for integration tests"
+  task :setup => [:db_setup, "evm:foreman:setup"] do
+    ManageIQ::Integration::ForemanManager.setup
+  end
+
+  desc "Run a foreman server in the background for integration tests"
+  task :run_server => [:setup] do
+    ManageIQ::Integration::ForemanManager.run
+  end
+
+  desc "Start and wait for an integration server to boot"
+  task :start_server => [:run_server, :ui_ready] do
+    MiqServer.my_server.update(:status => "started")
+  end
+
+  desc "Stop server for integration tests"
+  task :stop_server => [:load_manager] do
+    ManageIQ::Integration::ForemanManager.stop
+  end
+
+  desc "Status of the currently running process"
+  task :server_status => [:load_manager] do
+    ManageIQ::Integration::ForemanManager.status
+  end
+
+  # Check if a UI worker is running
+  task :ui_ready => :run_server do
+    ManageIQ::Integration::ForemanManager.ui_running?
+  end
+
+  # For tasks that don't need the full Rails ENV, but need the
+  # ManageIQ::Integration file loaded (:stop_server).
+  task :load_manager do
+    require File.expand_path(File.join("..", "manageiq", "integration"), __dir__)
+  end
+
+  task :env do
+    ENV["RAILS_ENV"] = "integration"
+    ENV["RAILS_SERVE_STATIC_FILES"] = "true"
+    Rails.env = ENV["RAILS_ENV"] if defined?(Rails)
+  end
+
+  task :db_setup => [:env, :environment] do
+    # Reset Rails.env and database config to make sure we are using the
+    # integration environment
+    Rails.env = ENV["RAILS_ENV"]
+    ActiveRecord::Base.remove_connection
+    ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[Rails.env])
+  end
+end
