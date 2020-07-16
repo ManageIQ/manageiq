ca162ceb5d41c3c9bd5d23b18f0ce1d402b3d335
Joe Rafaniello <jrafanie@redhat.com>
2020-07-16 10:19:23 -0400

    Ensure remaining models are translated but not through rails' titleize
    Fixes https://github.ibm.com/IBMPrivateCloud/CP4MCM/issues/10405

--- spec/models/miq_compare_spec.rb
+++ spec/models/miq_compare_spec.rb
@@ -29,4 +29,28 @@ RSpec.describe MiqCompare do
       expect { loaded = Marshal.load(dumped)  }.not_to raise_error
     end
   end
+
+  context "headers are translated" do
+    it "for EmsCluster" do
+      skip "Assumptions based on English message catalog" unless FastGettext.locale == "en"
+
+      obj1 = FactoryBot.create(:ems_cluster)
+      obj2 = FactoryBot.create(:ems_cluster)
+      MiqReport.seed_report("clusters")
+      report = MiqReport.find_by(:title => "Cluster Compare Template")
+      compare = MiqCompare.new({:ids => [obj1.id, obj2.id]}, report)
+      expect(compare.master_list[0][:header]).to eq("Cluster")
+    end
+
+    it "for Host" do
+      skip "Assumptions based on English message catalog" unless FastGettext.locale == "en"
+
+      obj1 = FactoryBot.create(:host_vmware)
+      obj2 = FactoryBot.create(:host_vmware)
+      MiqReport.seed_report("hosts")
+      report = MiqReport.find_by(:name => "Hosts: Compare Template")
+      compare = MiqCompare.new({:ids => [obj1.id, obj2.id]}, report)
+      expect(compare.master_list[0][:header]).to eq("Host Properties")
+    end
+  end
 end
