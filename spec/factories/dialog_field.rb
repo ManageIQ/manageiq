762d0cba59cd0e8777ddd66959c25629cf09ccb0
d-m-u <drewuhlmann@gmail.com>
2020-08-05 17:50:42 -0400

    remove unused dialog field type
    we have never used this and i doubt we're about to start anytime soon

--- spec/factories/dialog_field.rb
+++ spec/factories/dialog_field.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     name { "Dialog Field" }
   end
 
-  factory :dialog_field_button,      :parent => :dialog_field, :class => "DialogFieldButton"
   factory :dialog_field_sorted_item, :parent => :dialog_field, :class => "DialogFieldSortedItem"
   factory :dialog_field_tag_control, :parent => :dialog_field, :class => "DialogFieldTagControl"
   factory :dialog_field_text_box,    :parent => :dialog_field, :class => "DialogFieldTextBox"
