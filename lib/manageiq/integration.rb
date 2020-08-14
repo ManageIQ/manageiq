5b4b2b672c10fd98c3e8db1c441a92767948b622
Nick LaMuro <nicklamuro@gmail.com>
2020-08-13 19:53:07 -0500

    [POC][WIP][DONOTMERGE] Integration Factories
    Implements endpoints for Factories and cleaning the database via a
    internal Rails::Engine specifically used for the integration ENV.
    
    Will probably be split up and cherry-picked into a different branch, but
    keeping in this one for now for testing.

--- lib/manageiq/integration.rb
+++ lib/manageiq/integration.rb
@@ -2,6 +2,7 @@ require_relative 'integration/environment'
 require_relative 'integration/foreman_manager'
 require_relative 'integration/cypress_rake_task'
 require_relative 'integration/database_cleaner'
+require_relative 'integration/engine'
 
 module ManageIQ
   module Integration
