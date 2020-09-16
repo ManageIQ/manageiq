1f176f9cc608338820548c314113cbe8d3a2cfbb
d-m-u <drewuhlmann@gmail.com>
2020-09-16 18:15:58 -0400

    add spec showing current valid query count

--- spec/models/transformation_mapping_item_spec.rb
+++ spec/models/transformation_mapping_item_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe TransformationMappingItem, :v2v do
   let(:ems_openstack) { FactoryBot.create(:ems_openstack) }
   let(:openstack_cluster) { FactoryBot.create(:ems_cluster_openstack, :ext_management_system => ems_openstack) }
 
+  it "doesn't access database when unchanged model is saved" do
+    m = FactoryBot.create(:transformation_mapping_item)
+    expect { m.valid? }.to make_database_queries(:count => 1)
+  end
+
   # ---------------------------------------------------------------------------
   # Cluster Validation
   # ---------------------------------------------------------------------------
