6984beb27ed0e77ddedc361756ffe43e7f63bec3
Keenan Brock <keenan@thebrocks.net>
2020-09-18 17:04:34 -0400

    extract options from TokenStore.aquire

--- lib/token_store.rb
+++ lib/token_store.rb
@@ -1,17 +1,20 @@
 class TokenStore
   @token_caches = {} # Hash of Memory/Dalli Store Caches, Keyed by namespace
 
+  # only used by TokenManager.token_store
+  # @return a token store for users
   def self.acquire(namespace, token_ttl)
     @token_caches[namespace] ||= begin
+      options = cache_store_options(namespace, token_ttl)
       case ::Settings.server.session_store
       when "sql"
-        SqlStore.new(cache_store_options(namespace, token_ttl))
+        SqlStore.new(options)
       when "memory"
         require 'active_support/cache/memory_store'
-        ActiveSupport::Cache::MemoryStore.new(cache_store_options(namespace, token_ttl))
+        ActiveSupport::Cache::MemoryStore.new(options)
       when "cache"
         require 'active_support/cache/dalli_store'
-        ActiveSupport::Cache::DalliStore.new(MiqMemcached.server_address, cache_store_options(namespace, token_ttl))
+        ActiveSupport::Cache::DalliStore.new(MiqMemcached.server_address, options)
       else
         raise "unsupported session store type: #{::Settings.server.session_store}"
       end
