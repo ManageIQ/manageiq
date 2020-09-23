a116c41f29c952863f76e314a635b307bf92ac21
d-m-u <drewuhlmann@gmail.com>
2020-09-23 00:05:44 -0400

    use uniqueness_when_changed validator

--- spec/models/transformation_mapping_spec.rb
+++ spec/models/transformation_mapping_spec.rb
@@ -54,7 +54,7 @@ RSpec.describe TransformationMapping, :v2v do
 
   it "doesn't access database when unchanged model is saved" do
     m = FactoryBot.create(:transformation_mapping)
-    expect { m.valid? }.to make_database_queries(:count => 1)
+    expect { m.valid? }.not_to make_database_queries
   end
 
   context '#destination' do
