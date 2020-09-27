465c9e23d4e56702c1251e24d7d2937723bdcacb
Gregory Beyer <gregoryb@il.ibm.com>
2020-09-27 12:13:41 +0300

    SDE-1405: Made PhysicalStorage references to EMS more general
    Removed :class_name.
    Removed :inverse_of because it can be on a mixin.

--- app/models/physical_storage.rb
+++ app/models/physical_storage.rb
@@ -1,9 +1,8 @@
 class PhysicalStorage < ApplicationRecord
   include SupportsFeatureMixin
 
-  belongs_to :ext_management_system, :foreign_key => :ems_id, :inverse_of => :physical_storages,
-   :class_name => "ManageIQ::Providers::PhysicalInfraManager"
-  belongs_to :physical_rack, :foreign_key => :physical_rack_id, :inverse_of => :physical_storages
+  belongs_to :ext_management_system, :foreign_key => :ems_id
+  belongs_to :physical_rack, :foreign_key => :physical_rack_id
   belongs_to :physical_chassis, :inverse_of => :physical_storages
 
   has_many :storage_resources, :dependent => :destroy
