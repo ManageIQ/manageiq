64f876502ca52b1f189d690a60a584227e53f949
Nick LaMuro <nicklamuro@gmail.com>
2020-09-29 16:07:53 -0500

    [rails6][session_extension.rb] Fix superclass mismatch
    This was changed in new versions of `actionpack`, and without this
    change, the following error is given:
    
        TypeError:
          superclass mismatch for class TestSession
        # actionpack-6.0.3.3/lib/action_controller/test_case.rb:179:in `<module:ActionController>'
        # ...
        # ./spec/spec_helper.rb:11:in `<top (required)>'
        No examples found.

--- lib/extensions/session_extension.rb
+++ lib/extensions/session_extension.rb
@@ -2,6 +2,6 @@ class ActionDispatch::Request::Session
   include MoreCoreExtensions::Shared::Nested
 end
 
-class ActionController::TestSession < Rack::Session::Abstract::SessionHash
+class ActionController::TestSession < Rack::Session::Abstract::PersistedSecure::SecureSessionHash
   include MoreCoreExtensions::Shared::Nested
 end
