1f6a3bacf60fa9b7d756678aad3355d532305f23
Keenan Brock <keenan@thebrocks.net>
2020-11-02 15:33:43 -0500

    Merge pull request #20771 from NickLaMuro/embedded_ansible_scm_cred_ensure_newline
    [EmbeddedAnsible] Ensure newline for :ssh_key_data
    
    (cherry picked from commit 1895a653cbb81f0c472fd816f899dbc6f95265e5)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1893014

--- app/models/manageiq/providers/embedded_ansible/automation_manager/scm_credential.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/scm_credential.rb
@@ -42,6 +42,8 @@ class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::ScmCredential < M
   alias ssh_key_data   auth_key
   alias ssh_key_unlock auth_key_password
 
+  before_validation :ensure_newline_for_ssh_key
+
   def self.display_name(number = 1)
     n_('Credential (SCM)', 'Credentials (SCM)', number)
   end
@@ -53,5 +55,11 @@ class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::ScmCredential < M
     attrs[:auth_key_password] = attrs.delete(:ssh_key_unlock)  if attrs.key?(:ssh_key_unlock)
 
     attrs
+  end
+
+  private
+
+  def ensure_newline_for_ssh_key
+    self.auth_key = "#{auth_key}\n" if auth_key.present? && auth_key[-1] != "\n"
   end
 end
