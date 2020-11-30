3bcc1888eee19ccea5e257673b4b856553d6cdbe
Nick LaMuro <nicklamuro@gmail.com>
2020-11-30 14:03:52 -0600

    [EmbeddedAnsible][MachineCredential] Ensure newline

--- spec/models/manageiq/providers/embedded_ansible/automation_manager/credential_spec.rb
+++ spec/models/manageiq/providers/embedded_ansible/automation_manager/credential_spec.rb
@@ -149,79 +149,104 @@ RSpec.describe ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Credenti
   end
 
   context "MachineCredential" do
-    it_behaves_like 'an embedded_ansible credential' do
-      let(:credential_class) { embedded_ansible::MachineCredential }
+    let(:credential_class) { embedded_ansible::MachineCredential }
+    let(:expected_ssh_key) { "secret2\n" }
 
-      let(:params) do
-        {
-          :name            => "Machine Credential",
-          :userid          => "userid",
-          :password        => "secret1",
-          :ssh_key_data    => "secret2",
-          :become_method   => "sudo",
-          :become_password => "secret3",
-          :become_username => "admin",
-          :ssh_key_unlock  => "secret4"
+    let(:params) do
+      {
+        :name            => "Machine Credential",
+        :userid          => "userid",
+        :password        => "secret1",
+        :ssh_key_data    => passed_in_ssh_key,
+        :become_method   => "sudo",
+        :become_password => "secret3",
+        :become_username => "admin",
+        :ssh_key_unlock  => "secret4"
+      }
+    end
+    let(:queue_create_params) do
+      {
+        :name            => "Machine Credential",
+        :userid          => "userid",
+        :password        => ManageIQ::Password.encrypt("secret1"),
+        :ssh_key_data    => ManageIQ::Password.encrypt(passed_in_ssh_key),
+        :become_method   => "sudo",
+        :become_password => ManageIQ::Password.encrypt("secret3"),
+        :become_username => "admin",
+        :ssh_key_unlock  => ManageIQ::Password.encrypt("secret4")
+      }
+    end
+    let(:params_to_attributes) do
+      {
+        :name              => "Machine Credential",
+        :userid            => "userid",
+        :password          => "secret1",
+        :auth_key          => passed_in_ssh_key,
+        :become_password   => "secret3",
+        :become_username   => "admin",
+        :auth_key_password => "secret4",
+        :options           => {
+          :become_method => "sudo"
         }
-      end
-      let(:queue_create_params) do
-        {
-          :name            => "Machine Credential",
-          :userid          => "userid",
-          :password        => ManageIQ::Password.encrypt("secret1"),
-          :ssh_key_data    => ManageIQ::Password.encrypt("secret2"),
-          :become_method   => "sudo",
-          :become_password => ManageIQ::Password.encrypt("secret3"),
-          :become_username => "admin",
-          :ssh_key_unlock  => ManageIQ::Password.encrypt("secret4")
+      }
+    end
+    let(:expected_values) do
+      {
+        :name                        => "Machine Credential",
+        :userid                      => "userid",
+        :password                    => "secret1",
+        :ssh_key_data                => expected_ssh_key,
+        :become_password             => "secret3",
+        :become_username             => "admin",
+        :become_method               => "sudo",
+        :auth_key_password           => "secret4",
+        :password_encrypted          => ManageIQ::Password.try_encrypt("secret1"),
+        :auth_key_encrypted          => expected_ssh_key.present? ? ManageIQ::Password.try_encrypt(expected_ssh_key) : expected_ssh_key,
+        :become_password_encrypted   => ManageIQ::Password.try_encrypt("secret3"),
+        :auth_key_password_encrypted => ManageIQ::Password.try_encrypt("secret4"),
+        :options                     => {
+          :become_method => "sudo"
         }
-      end
-      let(:params_to_attributes) do
-        {
-          :name              => "Machine Credential",
-          :userid            => "userid",
-          :password          => "secret1",
-          :auth_key          => "secret2",
-          :become_password   => "secret3",
-          :become_username   => "admin",
-          :auth_key_password => "secret4",
-          :options           => {
-            :become_method => "sudo"
-          }
-        }
-      end
-      let(:expected_values) do
-        {
-          :name                        => "Machine Credential",
-          :userid                      => "userid",
-          :password                    => "secret1",
-          :ssh_key_data                => "secret2",
-          :become_password             => "secret3",
-          :become_username             => "admin",
-          :become_method               => "sudo",
-          :auth_key_password           => "secret4",
-          :password_encrypted          => ManageIQ::Password.try_encrypt("secret1"),
-          :auth_key_encrypted          => ManageIQ::Password.try_encrypt("secret2"),
-          :become_password_encrypted   => ManageIQ::Password.try_encrypt("secret3"),
-          :auth_key_password_encrypted => ManageIQ::Password.try_encrypt("secret4"),
-          :options                     => {
-            :become_method => "sudo"
-          }
-        }
-      end
-      let(:params_to_attrs) { [:auth_key, :auth_key_password, :become_method] }
-      let(:update_params) do
-        {
-          :name     => "Updated Credential",
-          :password => "supersecret"
-        }
-      end
-      let(:update_queue_params) do
-        {
-          :name     => "Updated Credential",
-          :password => ManageIQ::Password.encrypt("supersecret")
-        }
-      end
+      }
+    end
+    let(:params_to_attrs) { [:auth_key, :auth_key_password, :become_method] }
+    let(:update_params) do
+      {
+        :name     => "Updated Credential",
+        :password => "supersecret"
+      }
+    end
+    let(:update_queue_params) do
+      {
+        :name     => "Updated Credential",
+        :password => ManageIQ::Password.encrypt("supersecret")
+      }
+    end
+
+    context "with an SSH key that ends with a newline" do
+      let(:passed_in_ssh_key) { "secret2\n" }
+
+      it_behaves_like 'an embedded_ansible credential'
+    end
+
+    context "with an SSH key that does not end with a newline" do
+      let(:passed_in_ssh_key) { "secret2" }
+
+      it_behaves_like 'an embedded_ansible credential'
+    end
+
+    context "with an nil SSH key" do
+      let(:passed_in_ssh_key) { nil }
+      let(:expected_ssh_key)  { nil }
+
+      it_behaves_like 'an embedded_ansible credential'
+    end
+
+    context "with a empty string SSH key" do
+      let(:passed_in_ssh_key) { "" }
+      let(:expected_ssh_key)  { "" }
+
+      it_behaves_like 'an embedded_ansible credential'
     end
   end
 
