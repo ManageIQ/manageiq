8c51363efbe22a0bd4e08c9eea9f0d5484241aa5
Liran Livne <liran.livne@ibm.com>
2020-12-06 10:14:06 +0200

    Revert "after code-review"
    This reverts commit e85293af2a013a519ce84d8067912118fa4771cc.

--- app/models/fiber_channel_address.rb
+++ app/models/fiber_channel_address.rb
@@ -1,5 +1,13 @@
-class FiberChannelAddress < Address
-  def get_address_string()
-    "fc"
+class FiberChannelAddress < ApplicationRecord
+  include ProviderObjectMixin
+
+  has_many :addresses, :as => :port
+
+  acts_as_miq_taggable
+
+  def self.class_by_ems(ext_management_system)
+    # TODO(lsmola) taken from Orchestration stacks, correct approach should be to have a factory on ExtManagementSystem
+    # side, that would return correct class for each provider
+    ext_management_system && ext_management_system.class::FiberChannelAddress
   end
 end
