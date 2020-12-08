151b05725123706e01bb94db0837524998add4e3
Keenan Brock <keenan@thebrocks.net>
2020-12-08 14:48:48 -0500

    Add tests to ensure child/STI classes are hidden correctly

--- lib/extensions/ar_visible_attribute.rb
+++ lib/extensions/ar_visible_attribute.rb
@@ -9,6 +9,8 @@ module ArVisibleAttribute
     # @param [String|Symbol] attribute name of attribute to be hidden from the api and reporting
     # this attribute is accessible to all ruby methods. But it is not advertised.
     # we do this when deprecating an attribute or when introducing an internal attribute
+    #
+    # NOTE: only use in class definitions, or child classes will be broken
     def hide_attribute(attribute)
       self.hidden_attribute_names += [attribute.to_s]
     end
@@ -16,7 +18,7 @@ module ArVisibleAttribute
     # @return Array[String] attribute names that can be advertised in the api and reporting
     # Other attributes are accessible, they are just no longer in our public api (or never were)
     def visible_attribute_names
-      self.attribute_names - self.hidden_attribute_names
+      attribute_names - hidden_attribute_names
     end
   end
 end
--- spec/lib/extensions/ar_visible_attribute_spec.rb
+++ spec/lib/extensions/ar_visible_attribute_spec.rb
@@ -2,10 +2,22 @@ RSpec.describe ArVisibleAttribute do
   # NOTE: ApplicationRecord already included ArVisibleAttribute
   let(:klass) { Class.new(ApplicationRecord) { self.table_name = "vms" } }
   let(:other_klass) { Class.new(ApplicationRecord) { self.table_name = "vms" } }
+  let(:child_klass) { Class.new(klass) }
 
   context ".visible_attribute_names" do
     it "shows regular attributes" do
       expect(klass.visible_attribute_names).to include("type")
+    end
+
+    it "shows virtual attributes" do
+      klass.virtual_attribute :superb, :string
+      expect(klass.visible_attribute_names).to include("superb")
+    end
+
+    it "hides hidden virtual attributes" do
+      klass.virtual_attribute :superb, :string
+      klass.hide_attribute :superb
+      expect(klass.visible_attribute_names).not_to include("superb")
     end
 
     it "hides hidden attributes" do
@@ -16,6 +28,36 @@ RSpec.describe ArVisibleAttribute do
     it "only hides for specified class" do
       klass.hide_attribute :type
       expect(other_klass.visible_attribute_names).to include("type")
+    end
+
+    context "child class" do
+      it "shows regular attributes" do
+        expect(child_klass.visible_attribute_names).to include("type")
+      end
+
+      it "hides attributes that are hidden in parent class" do
+        klass.hide_attribute :type
+        expect(child_klass.visible_attribute_names).not_to include("type")
+      end
+
+      it "hides virtual attributes that are hidden in the parent class" do
+        klass.virtual_attribute :superb, :string
+        klass.hide_attribute :superb
+        expect(child_klass.visible_attribute_names).not_to include("superb")
+      end
+
+      it "hides attributes that are hidden in class and parent class" do
+        klass.hide_attribute :type
+        child_klass.hide_attribute :name
+        expect(child_klass.visible_attribute_names).not_to include("type")
+        expect(child_klass.visible_attribute_names).not_to include("name")
+      end
+
+      it "hides attribute only for class and below" do
+        child_klass.hide_attribute :name
+        expect(klass.visible_attribute_names).to include("name")
+        expect(child_klass.visible_attribute_names).not_to include("name")
+      end
     end
   end
 
@@ -32,6 +74,24 @@ RSpec.describe ArVisibleAttribute do
     it "only hides for specified class" do
       klass.hide_attribute :type
       expect(other_klass.hidden_attribute_names).not_to include("type")
+    end
+
+    context "child class" do
+      it "starts with no hidden attributes" do
+        expect(child_klass.hidden_attribute_names).to be_empty
+      end
+
+      it "hides attributes that are hidden in parent class" do
+        klass.hide_attribute :type
+        expect(child_klass.hidden_attribute_names).to include("type")
+      end
+
+      it "hides attributes that are hidden in parent class" do
+        klass.hide_attribute :type
+        child_klass.hide_attribute :name
+        expect(child_klass.hidden_attribute_names).to include("type")
+        expect(child_klass.hidden_attribute_names).to include("name")
+      end
     end
   end
 end
