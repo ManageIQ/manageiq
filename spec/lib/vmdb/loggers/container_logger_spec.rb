6dbba1e79b28eeacfb48c4588bd92848ed8e2c45
Jason Frey <fryguy9@gmail.com>
2020-12-09 14:57:09 -0500

    Fix escaping of < and > in ContainerLogger

--- spec/lib/vmdb/loggers/container_logger_spec.rb
+++ spec/lib/vmdb/loggers/container_logger_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Vmdb::Loggers::ContainerLogger::Formatter do
-  it "stuff" do
+  it "logs a message" do
     time = Time.now
     result = described_class.new.call("INFO", time, "some_program", "testing 1, 2, 3")
     expected = {
@@ -12,5 +12,11 @@ RSpec.describe Vmdb::Loggers::ContainerLogger::Formatter do
       "tid"        => Thread.current.object_id.to_s(16),
     }.delete_nils
     expect(JSON.parse(result)).to eq(expected)
+  end
+
+  it "does not escape characters as in ActiveSupport::JSON extensions" do
+    time = Time.now
+    result = described_class.new.call("INFO", time, "some_program", "xxx < yyy > zzz")
+    expect(result).to include('"message":"xxx < yyy > zzz"')
   end
 end
