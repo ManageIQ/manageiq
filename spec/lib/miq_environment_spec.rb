88593d336164ea421cafe2a7ddcf3ce379f39a83
Adam Grare <agrare@redhat.com>
2021-01-28 10:56:28 -0500

    Fix MiqEnvironment.local_ip_address to not prefer loopback
    `#local_ip_address` currently prefers loopback addresses since it
    filters out "private" addresses and not "loopback" addresses.
    
    IMO we should prefer public, then private, then loopback.

--- spec/lib/miq_environment_spec.rb
+++ spec/lib/miq_environment_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe MiqEnvironment do
       end
 
       example "local_ip_address" do
-        expect(described_class.local_ip_address).to eq(`hostname -i`.chomp.split.first)
+        expect(described_class.local_ip_address).to be_in(Socket.ip_address_list.map(&:ip_address))
       end
     end
 
