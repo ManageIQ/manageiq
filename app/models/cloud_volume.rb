0a0712e90e5ef28669b552b7700d992344d53b89
Gregory Beyer <gregoryb@il.ibm.com>
2021-02-07 11:20:09 +0200

    Added creation of new VolumeMapping
    Fixed a bug where HostInitiator was missing NewWithTypeStiMixin

--- app/models/cloud_volume.rb
+++ app/models/cloud_volume.rb
@@ -15,6 +15,7 @@ class CloudVolume < ApplicationRecord
   belongs_to :base_snapshot, :class_name => 'CloudVolumeSnapshot', :foreign_key => :cloud_volume_snapshot_id
   belongs_to :storage_resource, :inverse_of => :cloud_volumes
   belongs_to :storage_service, :inverse_of => :cloud_volumes
+  has_one    :physical_storage, :through => :storage_resource
   has_many   :cloud_volume_backups
   has_many   :cloud_volume_snapshots
   has_many   :attachments, :class_name => 'Disk', :as => :backing
