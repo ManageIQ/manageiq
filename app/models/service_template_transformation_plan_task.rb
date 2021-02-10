1bf76ed68746eec46b52a018c83ad85fcc495971
Joe Rafaniello <jrafanie@redhat.com>
2021-02-10 15:34:01 -0500

    Use URI::Parser escape/unescape to avoid warnings
    Fixes various warnings of this variety:
    app/models/file_depot_ftp.rb:128: warning: URI.escape is obsolete
    app/models/mixins/file_depot_mixin.rb:42: warning: URI.escape is obsolete
    
    Ruby 2.7 prints this warning and they suggest the following from https://ruby-doc.org/stdlib-2.7.2/libdoc/uri/rdoc/URI/Escape.html#method-i-escape-label-Description:
    
    ```
    This method is obsolete and should not be used. Instead, use CGI.escape, URI.encode_www_form or URI.encode_www_form_component depending on your specific use case.
    ```

--- app/models/service_template_transformation_plan_task.rb
+++ app/models/service_template_transformation_plan_task.rb
@@ -382,7 +382,7 @@ class ServiceTemplateTransformationPlanTask < ServiceTemplateProvisionTask
         :scheme   => 'ssh',
         :userinfo => 'root',
         :host     => source.host.miq_custom_get('TransformationIPAddress') || source.host.ipaddress,
-        :path     => "/vmfs/volumes/#{Addressable::URI.escape(storage.name)}/#{Addressable::URI.escape(source.location)}"
+        :path     => "/vmfs/volumes/#{URI::Parser.new.escape(storage.name)}/#{URI::Parser.new.escape(source.location)}"
       ).to_s,
       :vm_uuid              => source.uid_ems,
       :conversion_host_uuid => conversion_host.resource.ems_ref,
