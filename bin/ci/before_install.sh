d9249451d0e02d7d5826ec3b85ae1a180428b8a5
Jason Frey <fryguy9@gmail.com>
2021-02-18 09:55:16 -0500

    Only set git user/email on CI

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -v
 
-git config --global user.name "ManageIQ"
-git config --global user.email "contact@manageiq.org"
+if [ -n "$CI" ]; then
+  git config --global user.name "ManageIQ"
+  git config --global user.email "contact@manageiq.org"
+fi
 source $TRAVIS_BUILD_DIR/bin/ci/setup_vmdb_configs.sh
 source $TRAVIS_BUILD_DIR/bin/ci/setup_ruby_env.sh
 
