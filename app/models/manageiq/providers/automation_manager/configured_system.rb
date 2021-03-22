2dd443a4e5a778017c292bbe02e88303b88fff07
Harpreet Kataria <hkataria@redhat.com>
2021-03-22 09:56:59 -0400

    added `:uses => :inventory_root_group` clause here for performance improvement

--- app/models/manageiq/providers/automation_manager/configured_system.rb
+++ app/models/manageiq/providers/automation_manager/configured_system.rb
@@ -1,5 +1,5 @@
 class ManageIQ::Providers::AutomationManager::ConfiguredSystem < ::ConfiguredSystem
-  virtual_column  :inventory_root_group_name, :type => :string
+  virtual_column  :inventory_root_group_name, :type => :string, :uses => :inventory_root_group
 
   def inventory_root_group_name
     inventory_root_group.name
