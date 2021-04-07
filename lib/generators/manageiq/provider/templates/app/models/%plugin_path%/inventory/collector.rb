49d3279fb541a23c76e8bfb5a263f27c79152f9c
Adam Grare <adam@grare.com>
2021-04-07 15:40:06 -0400

    Updates for combined manager refresh

--- lib/generators/manageiq/provider/templates/app/models/%plugin_path%/inventory/collector.rb
+++ lib/generators/manageiq/provider/templates/app/models/%plugin_path%/inventory/collector.rb
@@ -1,3 +1,14 @@
 class <%= class_name %>::Inventory::Collector < ManageIQ::Providers::Inventory::Collector
   require_nested :<%= manager_type %>
+
+  def connection
+    @connection ||= manager.connect
+  end
+
+  def vms
+    [
+      OpenStruct.new(:id => '1', :name => 'funky', :location => 'dc-1', :vendor => 'unknown'),
+      OpenStruct.new(:id => '2', :name => 'bunch', :location => 'dc-1', :vendor => 'unknown')
+    ]
+  end
 end
--- lib/generators/manageiq/provider/templates/app/models/%plugin_path%/inventory/parser.rb
+++ lib/generators/manageiq/provider/templates/app/models/%plugin_path%/inventory/parser.rb
@@ -1,3 +1,16 @@
 class <%= class_name %>::Inventory::Parser < ManageIQ::Providers::Inventory::Parser
   require_nested :<%= manager_type %>
+
+  def parse
+    vms
+  end
+
+  def vms
+    collector.vms.each do |inventory|
+      inventory_object = persister.vms.find_or_build(inventory.id.to_s)
+      inventory_object.name = inventory.name
+      inventory_object.location = inventory.location
+      inventory_object.vendor = inventory.vendor
+    end
+  end
 end
