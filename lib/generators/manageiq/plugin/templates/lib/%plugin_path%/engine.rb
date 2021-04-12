3e9044ee22ea9fd30ee2701c13ed8d970a0150ba
Jason Frey <fryguy9@gmail.com>
2021-04-12 16:18:11 -0400

    Merge pull request #21163 from agrare/add_pluggable_secrets_to_plugin_generator
    Add pluggable secrets to provider generator
    
    (cherry picked from commit 461abb8e2e7774692b0e3ce8553ced2b3469636e)

--- lib/generators/manageiq/plugin/templates/lib/%plugin_path%/engine.rb
+++ lib/generators/manageiq/plugin/templates/lib/%plugin_path%/engine.rb
@@ -4,6 +4,11 @@
 
     config.autoload_paths << root.join('lib').to_s
 
+    initializer :append_secrets do |app|
+      app.config.paths["config/secrets"] << root.join("config", "secrets.defaults.yml").to_s
+      app.config.paths["config/secrets"] << root.join("config", "secrets.yml").to_s
+    end
+
     def self.vmdb_plugin?
       true
     end
