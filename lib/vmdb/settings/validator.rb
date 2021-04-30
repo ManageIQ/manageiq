684d1f9e434a49ebf6b547785f42d23a3a292c6f
Jason Frey <fryguy9@gmail.com>
2021-04-30 17:47:19 -0400

    Merge pull request #20889 from jrafanie/validate_request_limit_settings
    Add validation of worker memory/cpu request/limit
    
    (cherry picked from commit 97bc5379adf0b921d63bb06a2ffa76f77b3b0c4d)

--- lib/vmdb/settings/validator.rb
+++ lib/vmdb/settings/validator.rb
@@ -167,6 +167,58 @@ module Vmdb
 
         return valid, errors
       end
+
+      def workers(data)
+        valid, errors = true, []
+        # worker_settings expects a hash-like structure with nested keys: :config and :workers
+        data = {:config => {:workers => data}}
+        MiqWorker.descendants.each do |worker_class|
+          result, new_errors = validate_worker_request_limit(worker_class, data)
+          if result == false
+            valid = false
+            errors += new_errors
+          end
+        end
+
+        return valid, errors
+      end
+
+      def validate_worker_request_limit(worker_class, data)
+        valid = true
+        errors = []
+        worker_settings = worker_class.fetch_worker_settings_from_options_hash(data[:config])
+
+        # Only validate request/limits if you specify any of them.  Specifying at least one of these four, requires
+        # all of them to be enumerated. This gets around tests that want to stub some of the worker_settings.
+        worker_settings_keys_with_dependencies = %i[cpu_request_percent cpu_threshold_percent memory_request memory_threshold]
+        return valid, errors if (worker_settings.keys & worker_settings_keys_with_dependencies).empty?
+
+        worker_settings_keys_with_dependencies.each do |key|
+          if !worker_settings.key?(key)
+            errors << [key, "#{worker_class.settings_name} #{key} is missing!"]
+            return false, errors
+          elsif !worker_settings[key].kind_of?(Numeric)
+            errors << [key, "#{worker_class.settings_name} #{key} has non-numeric value: #{worker_settings[key].inspect}"]
+            return false, errors
+          end
+        end
+
+        cpu_request     = worker_settings[:cpu_request_percent]
+        cpu_limit       = worker_settings[:cpu_threshold_percent]
+        memory_request  = worker_settings[:memory_request]
+        memory_limit    = worker_settings[:memory_threshold]
+
+        if cpu_request > cpu_limit
+          valid = false
+          errors << [:cpu_request_percent, "#{worker_class.settings_name}: cpu_request_percent: #{cpu_request} cannot exceed cpu_threshold_percent: #{cpu_limit}"]
+        end
+
+        if memory_request > memory_limit
+          valid = false
+          errors << [:memory_request, "#{worker_class.settings_name}: memory_request: #{memory_request} cannot exceed memory_threshold: #{memory_limit}"]
+        end
+        return valid, errors
+      end
     end
   end
 end
