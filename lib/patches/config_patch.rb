9d3ed0900c9faeba12c1bb9fcb5c6b277ecea817
Jason Frey <fryguy9@gmail.com>
2021-05-21 12:21:48 -0400

    Merge pull request #21233 from kbrock/dont_dump_settings
    no longer dump settings
    
    (cherry picked from commit 2c2d9c47fe8d449e15a620de405e7e7ec0044340)

--- lib/patches/config_patch.rb
+++ lib/patches/config_patch.rb
@@ -1,10 +1,6 @@
 module ConfigDecryptPasswords
   def reload!
-    Vmdb::Settings.decrypt_passwords!(super).tap do
-      # The following should only be done when loading/reloading the current
-      #   process' Settings, as opposed to a remote server's settings.
-      Vmdb::Settings.dump_to_log_directory(::Settings) if equal?(::Settings)
-    end
+    Vmdb::Settings.decrypt_passwords!(super)
   end
 
   alias load! reload!
