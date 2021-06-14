1ec0a5e3721c0c1ef03db5d531e5c7d7ba494377
Jason Frey <fryguy9@gmail.com>
2021-06-14 14:35:15 -0400

    Refactor MiqReport::Formats to remove the constants
    While there is nothing wrong with the original code, brakeman flags the
    code as bad, which forces us to ignore it. This is the only ignore left
    in the ignore file, so this commit refactors the code and the ignore is
    no longer needed.
    
    As a side-effect, this delay-loads the yml file, so we get some
    object/memory savings in code paths where MiqReports are not needed.

--- app/models/miq_report/formats.rb
+++ app/models/miq_report/formats.rb
@@ -1,11 +1,19 @@
 class MiqReport::Formats
-  format_hash = YAML.load_file(ApplicationRecord::FIXTURE_DIR.join('miq_report_formats.yml')).freeze
-  FORMATS                = format_hash[:formats].freeze
-  DEFAULTS_AND_OVERRIDES = format_hash[:defaults_and_overrides].freeze
+  private_class_method def self.format_hash
+    @format_hash ||= YAML.load_file(ApplicationRecord::FIXTURE_DIR.join('miq_report_formats.yml')).freeze
+  end
+
+  private_class_method def self.formats
+    format_hash[:formats]
+  end
+
+  private_class_method def self.defaults_and_overrides
+    format_hash[:defaults_and_overrides]
+  end
 
   def self.available_formats_for(column, suffix, datatype)
     is_break_sfx = (suffix && MiqReport.is_break_suffix?(suffix))
-    FORMATS.each_with_object({}) do |(format_name, properties), result|
+    formats.each_with_object({}) do |(format_name, properties), result|
       # Ignore formats that don't include suffix if the column name has a break suffix
       next if is_break_sfx && (properties[:suffixes].nil? || !properties[:suffixes].include?(suffix.to_sym))
       next unless (properties[:columns] && properties[:columns].include?(column)) ||
@@ -13,7 +21,7 @@ class MiqReport::Formats
                   (properties[:data_types] && properties[:data_types].include?(datatype)) ||
                   (properties[:suffixes] && properties[:suffixes].include?(suffix.to_sym)) ||
                   format_name == sub_type(column) ||
-                  format_name == DEFAULTS_AND_OVERRIDES[:formats_by_sub_type][sub_type(column)]
+                  format_name == defaults_and_overrides[:formats_by_sub_type][sub_type(column)]
 
       result[format_name] = properties[:description]
     end
@@ -24,30 +32,30 @@ class MiqReport::Formats
     suffix = column.to_s.split('__').last.try(:to_sym)
     # HACK: formats for columns are unqualified, so we need a
     # temporary way to avoid collisions
-    DEFAULTS_AND_OVERRIDES[:formats_by_path].fetch(path.to_sym) do
-      DEFAULTS_AND_OVERRIDES[:formats_by_suffix][suffix] ||
-        DEFAULTS_AND_OVERRIDES[:formats_by_column][column] ||
-        DEFAULTS_AND_OVERRIDES[:formats_by_sub_type][sub_type(column)] ||
-        DEFAULTS_AND_OVERRIDES[:formats_by_data_type][datatype]
+    defaults_and_overrides[:formats_by_path].fetch(path.to_sym) do
+      defaults_and_overrides[:formats_by_suffix][suffix] ||
+        defaults_and_overrides[:formats_by_column][column] ||
+        defaults_and_overrides[:formats_by_sub_type][sub_type(column)] ||
+        defaults_and_overrides[:formats_by_data_type][datatype]
     end
   end
 
   def self.default_format_details_for(path, column, datatype)
-    format = FORMATS[default_format_for_path(path, datatype)]
+    format = formats[default_format_for_path(path, datatype)]
     if format
       format = format.deep_clone # Make sure we don't taint the original
-      if DEFAULTS_AND_OVERRIDES[:precision_by_column].key?(column.to_sym)
-        format[:precision] = DEFAULTS_AND_OVERRIDES[:precision_by_column][column.to_sym]
+      if defaults_and_overrides[:precision_by_column].key?(column.to_sym)
+        format[:precision] = defaults_and_overrides[:precision_by_column][column.to_sym]
       end
     end
     format
   end
 
   def self.details(format_name)
-    FORMATS[format_name.try(:to_sym)]
+    formats[format_name.try(:to_sym)]
   end
 
   def self.sub_type(column)
-    DEFAULTS_AND_OVERRIDES[:sub_types_by_column][column]
+    defaults_and_overrides[:sub_types_by_column][column]
   end
 end
