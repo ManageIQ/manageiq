d774c2a1b601d2cffdbc2e549aa3106475e0187a
Brandon Dunne <brandondunne@hotmail.com>
2021-06-30 16:12:50 -0400

    In order to connect with SSL we need to use hostnames, not IP address

--- lib/miq_memcached.rb
+++ lib/miq_memcached.rb
@@ -3,7 +3,7 @@ require 'linux_admin'
 module MiqMemcached
   def self.server_address
     if ENV["MEMCACHED_SERVICE_HOST"] && ENV["MEMCACHED_SERVICE_PORT"]
-      "#{ENV["MEMCACHED_SERVICE_HOST"]}:#{ENV["MEMCACHED_SERVICE_PORT"]}"
+      "memcached:#{ENV["MEMCACHED_SERVICE_PORT"]}"
     else
       ::Settings.session.memcache_server
     end
