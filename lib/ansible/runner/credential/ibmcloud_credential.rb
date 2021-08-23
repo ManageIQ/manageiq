acc48609ef1d8661696cc52a540ab82de575074b
Isgandar Valizada <isgandar.valizada@de.ibm.com>
2021-08-23 18:31:50 +0200

    - added extra-vars to the credential - renaming corresp. Auth. class

--- lib/ansible/runner/credential/ibmcloud_credential.rb
+++ lib/ansible/runner/credential/ibmcloud_credential.rb
@@ -2,13 +2,19 @@ module Ansible
   class Runner
     class IbmCloudCredential < Credential
       def self.auth_type
-        "ManageIQ::Providers::IbmCloud::IbmCloudCredential"
+        'ManageIQ::Providers::IbmCloud::PowerVirtualServers::CloudManager::IbmCloudCredential'
       end
 
       def env_vars
         {
           :creds_aes_key => auth.auth_key,
           :creds_aes_iv  => auth.auth_key_password
+        }
+      end
+
+      def extra_vars
+        {
+          :encr_cos_creds => auth.password,
         }
       end
     end
