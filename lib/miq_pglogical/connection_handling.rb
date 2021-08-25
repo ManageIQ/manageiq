60cf92de4e9425106d0a404a71cf3a95096c54e7
Joe Rafaniello <jrafanie@gmail.com>
2021-08-25 10:44:16 -0400

    Remove extra semicolon and useless second argument
    Both are not needed here.
    
    Followup from https://github.com/ManageIQ/manageiq/pull/21372

--- lib/miq_pglogical/connection_handling.rb
+++ lib/miq_pglogical/connection_handling.rb
@@ -10,7 +10,7 @@ class MiqPglogical
       def logical_replication_supported?
         return @logical_replication_supported if defined?(@logical_replication_supported)
 
-        is_superuser = ActiveRecord::Base.connection.select_value("SELECT usesuper FROM pg_user WHERE usename = CURRENT_USER;", "SQL")
+        is_superuser = ActiveRecord::Base.connection.select_value("SELECT usesuper FROM pg_user WHERE usename = CURRENT_USER")
         unless is_superuser
           warn_bookends = ["\e[33m", "\e[0m"]
           msg = "WARNING: Current user is NOT a superuser, logical replication will not function."
