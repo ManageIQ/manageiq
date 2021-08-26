23951fd305abde40c5f5c2a6cee3497dbc39fa50
Brandon Dunne <brandondunne@hotmail.com>
2021-08-26 16:09:48 -0400

    Ensure we're bundling release branches & tags against Gemfile.lock.release

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -5,6 +5,11 @@ if [ -n "$CI" ]; then
   git config --global user.name "ManageIQ"
   git config --global user.email "contact@manageiq.org"
 fi
+
+if [ $TRAVIS_BRANCH != "master" ]; then
+  cp $TRAVIS_BUILD_DIR/Gemfile.lock{.release,}
+fi
+
 source $TRAVIS_BUILD_DIR/bin/ci/setup_vmdb_configs.sh
 source $TRAVIS_BUILD_DIR/bin/ci/setup_ruby_env.sh
 
