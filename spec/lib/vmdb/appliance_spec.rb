25d7ab295650a3b4cc4fa7ea431f8ea3d687e751
Nick LaMuro <nicklamuro@gmail.com>
2021-08-27 15:16:23 -0500

    [Vmdb::Settings] Add password_filter
    Adds user configurable `.password_filter` so that users can define their
    own fields to be filtered via things like
    
      VMDBLogger.log_hashes($log, hash, :filter => Vmdb::Settings.password_filter)
    
    These can be configured in the Settings.log.filter, so users can add
    whatever fields make sense for them and their environment.

--- spec/lib/vmdb/appliance_spec.rb
+++ spec/lib/vmdb/appliance_spec.rb
@@ -1,6 +1,51 @@
 require 'stringio'
 
 RSpec.describe Vmdb::Appliance do
+  describe "log_config" do
+    context "logging settings" do
+      let(:logger_io)     { StringIO.new }
+      let(:secret_filter) { [] }
+      let(:fake_settings) do
+        {
+          :authentication => {
+            :username  => "foobar",
+            :password  => "12345",  # "That's the same combination I have on my luggage!"
+            :api_token => "abc123"
+          },
+          :database => {
+            :maintenance => {
+              :reindex_schedule => "1 * * * *",
+              :reindex_tables   => %w[Metric MiqQueue]
+            }
+          },
+          :log => {
+            :secret_filter => secret_filter
+          }
+        }
+      end
+
+      before do
+        stub_settings(fake_settings)
+        allow(::Settings).to receive(:to_hash).and_return(fake_settings)
+        described_class.log_config(:logger => Logger.new(logger_io))
+      end
+
+      it "filters out secrets" do
+        expect(logger_io.string).to include("password: [FILTERED]")
+        expect(logger_io.string).to include("api_token: abc123")
+      end
+
+      context "with a user configured secret_filter" do
+        let(:secret_filter) { ["api_token"] }
+
+        it "will use user configured secret_filter" do
+          expect(logger_io.string).to include("password: [FILTERED]")
+          expect(logger_io.string).to include("api_token: [FILTERED]")
+        end
+      end
+    end
+  end
+
   describe ".installed_rpms (private)" do
     it "writes the correct string" do
       file = StringIO.new
