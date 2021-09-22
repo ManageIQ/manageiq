82f48f61d9c232fc95107362d7dfd9adf25cc409
Adam Grare <adam@grare.com>
2021-09-22 13:48:54 -0400

    Merge pull request #21432 from Autosde/volume_mapping_delete
    Added ability to remove volume to host mapping
    
    (cherry picked from commit ea2cab67e7a19b921ac3ed7ebff15137f48e91cb)

--- app/models/volume_mapping.rb
+++ app/models/volume_mapping.rb
@@ -13,6 +13,7 @@ class VolumeMapping < ApplicationRecord
   belongs_to :ext_management_system, :foreign_key => :ems_id
 
   supports_not :create
+  supports_not :delete
   acts_as_miq_taggable
 
   def my_zone
@@ -24,6 +25,33 @@ class VolumeMapping < ApplicationRecord
     # TODO(lsmola) taken from Orchestration stacks, correct approach should be to have a factory on ExtManagementSystem
     # side, that would return correct class for each provider
     ext_management_system && ext_management_system.class::VolumeMapping
+  end
+
+  def raw_delete_volume_mapping
+    raise NotImplementedError, _("raw_delete_volume_mapping must be implemented in a subclass")
+  end
+
+  def delete_volume_mapping
+    raw_delete_volume_mapping
+  end
+
+  # Delete a volume mapping as a queued task and return the task id. The queue
+  # name and the queue zone are derived from the EMS, and a userid is mandatory.
+  def delete_volume_mapping_queue(userid)
+    task_opts = {
+      :action => "deleting VolumeMapping for user #{userid}",
+      :userid => userid
+    }
+    queue_opts = {
+      :class_name  => self.class.name,
+      :method_name => 'delete_volume_mapping',
+      :instance_id => id,
+      :role        => 'ems_operations',
+      :queue_name  => ext_management_system.queue_name_for_ems_operations,
+      :zone        => ext_management_system.my_zone,
+      :args        => []
+    }
+    MiqTask.generic_action_with_callback(task_opts, queue_opts)
   end
 
   def self.create_volume_mapping_queue(userid, ext_management_system, options = {})
