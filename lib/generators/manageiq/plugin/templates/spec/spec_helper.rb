685a1e4a29d1621ceccbcff5a14859764a5b781f
Adam Grare <adam@grare.com>
2021-09-29 16:24:33 -0400

    Fix rubocop issues in plugin generated code

--- lib/generators/manageiq/plugin/templates/spec/spec_helper.rb
+++ lib/generators/manageiq/plugin/templates/spec/spec_helper.rb
@@ -3,7 +3,7 @@ if ENV['CI']
   SimpleCov.start
 end
 
-Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
-Dir[File.join(__dir__, "support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/shared/**/*.rb")].sort.each { |f| require f }
+Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
 
 require "<%= plugin_name %>"
