d2083246caa865134432ea19e7e80b2b97994da0
Oleg Barenboim <chessbyte@gmail.com>
2021-10-07 14:24:39 -0400

    Merge pull request #21476 from kbrock/support_stub_helper
    Introduce stub_supports
    
    (cherry picked from commit 68decd312fffe090c1a2257047ea8e21f2b97344)

---
+++ spec/support/supports_helper.rb
@@ -0,0 +1,26 @@
+module Spec
+  module Support
+    module SupportsHelper
+      # when testing requests, ensure the model supports a certain attribute
+      def stub_supports(model, feature = :update, supported: true)
+        model = model.class unless model.kind_of?(Class)
+
+        receive_supports = receive(:supports?).with(feature).and_return(supported)
+        allow(model).to receive_supports
+        allow_any_instance_of(model).to receive_supports
+      end
+
+      def stub_supports_not(model, feature = :update, reason = nil)
+        model = model.class unless model.kind_of?(Class)
+
+        stub_supports(model, feature, :supported => false)
+
+        if reason
+          receive_reason = receive(:unsupported_reason).with(feature).and_return(reason)
+          allow(model).to(receive_reason)
+          allow_any_instance_of(model).to(receive_reason)
+        end
+      end
+    end
+  end
+end
