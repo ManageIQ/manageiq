e957d3aab20d0daeb902b456b0050310fe459012
Jason Frey <fryguy9@gmail.com>
2021-10-08 12:42:22 -0400

    Add bootsnap
    Bootsnap is installed by default in Rails 6 applications.  It improves
    boot time (on my Mac) from ~14s to ~6s (~57% faster).

--- config/boot.rb
+++ config/boot.rb
@@ -6,3 +6,5 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 # add the lib dir of the engine if we are running as a dummy app for an engine
 $LOAD_PATH.unshift(File.expand_path('../../../lib', __dir__)) if defined?(ENGINE_ROOT)
+
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
