fbe3bb21c24373297afd376d9b2096e684632c9c
Keenan Brock <keenan@thebrocks.net>
2021-10-18 15:26:50 -0400

    Introduce stubbing for params_for_
    the api uses params_for_create and params_for_update
    for many of the OPTIONS around the supported classes.
    
    Since the options are driven by the supports?, putting these into a single stub method

--- spec/support/supports_helper.rb
+++ spec/support/supports_helper.rb
@@ -2,12 +2,20 @@ module Spec
   module Support
     module SupportsHelper
       # when testing requests, ensure the model supports a certain attribute
-      def stub_supports(model, feature = :update, supported: true)
+      # @param (Symbol) feature which supports feature is supported (e.g.: :create, :update, :delete)
+      # @param (Array|Nil) fields value to stub params_for_* (default: nil don't stub)
+      def stub_supports(model, feature = :update, supported: true, fields: nil)
         model = model.class unless model.kind_of?(Class)
 
         receive_supports = receive(:supports?).with(feature).and_return(supported)
         allow(model).to receive_supports
         allow_any_instance_of(model).to receive_supports
+
+        return if fields.nil?
+
+        receive_this_message = receive("params_for_#{feature}".to_sym).and_return("fields" => fields)
+        allow(model).to(receive_this_message)                 # only useful for create
+        allow_any_instance_of(model).to(receive_this_message) # only useful for update
       end
 
       def stub_supports_not(model, feature = :update, reason = nil)
