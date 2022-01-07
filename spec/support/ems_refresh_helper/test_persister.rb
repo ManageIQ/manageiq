8d1b81102e2b06fd5860423380793430e6c09d96
Adam Grare <adam@grare.com>
2022-01-07 09:43:13 -0500

    Update specs to skip STI in TestPersister

--- spec/support/ems_refresh_helper/test_persister.rb
+++ spec/support/ems_refresh_helper/test_persister.rb
@@ -3,8 +3,8 @@ module Spec
     module EmsRefreshHelper
       class TestPersister < ManageIQ::Providers::Inventory::Persister
         def initialize_inventory_collections
-          add_collection(infra, :vms)
-          add_collection(infra, :hosts)
+          add_collection(infra, :vms, {}, :without_sti => true)
+          add_collection(infra, :hosts, {}, :without_sti => true)
         end
       end
     end
