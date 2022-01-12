30b306d28f5f62902536607e6f53d8586b87069f
Adam Grare <adam@grare.com>
2022-01-12 17:12:46 -0500

    Use parent for manager_id if it is present

--- spec/models/manageiq/providers/inventory/persister/local_db_finders_spec.rb
+++ spec/models/manageiq/providers/inventory/persister/local_db_finders_spec.rb
@@ -402,7 +402,7 @@ RSpec.describe ManageIQ::Providers::Inventory::Persister do
     it "checks primary index attributes exist" do
       expect do
         persister.add_collection(persister.send(:cloud), :vms, {:manager_ref => %i[ems_ref ems_gref]}, {:without_sti => true})
-      end.to raise_error("Invalid definition of index :manager_ref, there is no attribute :ems_gref on model Vm")
+      end.to raise_error(/Invalid definition of index :manager_ref, there is no attribute :ems_gref on model/)
     end
 
     it "checks secondary index attributes exist" do
@@ -413,7 +413,7 @@ RSpec.describe ManageIQ::Providers::Inventory::Persister do
           {:secondary_refs => {:by_uid_ems_and_name => %i[uid_emsa name]}},
           {:without_sti => true}
         )
-      end.to raise_error("Invalid definition of index :by_uid_ems_and_name, there is no attribute :uid_emsa on model Vm")
+      end.to raise_error(/Invalid definition of index :by_uid_ems_and_name, there is no attribute :uid_emsa on model/)
     end
 
     it "checks relation is allowed in index" do
@@ -428,9 +428,9 @@ RSpec.describe ManageIQ::Providers::Inventory::Persister do
     it "checks relation is on model class" do
       expect do
         persister.add_collection(persister.send(:cloud), :vms, {}, {:without_sti => true}) do |builder|
-          builder.add_properties(:secondary_refs => {:by_availability_zone_and_name => %i(availability_zone name)})
+          builder.add_properties(:secondary_refs => {:by_lan_and_name => %i[lan name]})
         end
-      end.to raise_error("Invalid definition of index :by_availability_zone_and_name, there is no attribute :availability_zone on model Vm")
+      end.to raise_error(/Invalid definition of index :by_lan_and_name, there is no attribute :lan on model/)
     end
 
     it "checks we allow any index attributes when we use custom_saving block" do
