41010025caee538255ee78cb2ee08e96115c1ecc
nasark <nasarak_786@hotmail.com>
2022-04-07 10:49:42 -0400

    add delete cloud database methods

--- app/models/cloud_database.rb
+++ app/models/cloud_database.rb
@@ -1,6 +1,7 @@
 class CloudDatabase < ApplicationRecord
   include NewWithTypeStiMixin
   include ProviderObjectMixin
+  include SupportsFeatureMixin
 
   belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::CloudManager"
   belongs_to :cloud_tenant
@@ -8,4 +9,33 @@ class CloudDatabase < ApplicationRecord
   belongs_to :resource_group
 
   serialize :extra_attributes
+
+  supports_not :delete
+
+  def delete_cloud_database_queue(userid)
+    task_opts = {
+      :action => "deleting Cloud Database for user #{userid}",
+      :userid => userid
+    }
+
+    queue_opts = {
+      :class_name  => self.class.name,
+      :method_name => 'delete_cloud_database',
+      :instance_id => id,
+      :role        => 'ems_operations',
+      :queue_name  => ext_management_system.queue_name_for_ems_operations,
+      :zone        => ext_management_system.my_zone,
+      :args        => []
+    }
+
+    MiqTask.generic_action_with_callback(task_opts, queue_opts)
+  end
+
+  def delete_cloud_database
+    raw_delete_cloud_database
+  end
+
+  def raw_delete_cloud_database
+    raise NotImplementedError, _("raw_delete_cloud_database must be implemented in a subclass")
+  end
 end
