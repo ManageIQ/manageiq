3b09b4a7a3f440b96b2e28880b7842af5de5d3bc
Gregor Berginc <gregor.berginc@xlab.si>
2022-04-22 12:50:49 +0200

    Refactor PhysicalServer operations
    The change_state method was repeated in different operations files. This
    commit moves the change_state into operations.rb and cleans things up a
    bit.

--- app/models/physical_server/operations/power.rb
+++ app/models/physical_server/operations/power.rb
@@ -26,17 +26,4 @@ module PhysicalServer::Operations::Power
   def restart_mgmt_controller
     change_state(:restart_mgmt_controller)
   end
-
-  private
-
-  def change_state(verb)
-    unless ext_management_system
-      raise _(" A Server %{server} <%{name}> with Id: <%{id}> is not associated with a provider.") %
-            {:server => self, :name => name, :id => id}
-    end
-    options = {:uuid => ems_ref}
-    _log.info("Begin #{verb} server: #{name}  with UUID: #{ems_ref}")
-    ext_management_system.send(verb, self, options)
-    _log.info("Complete #{verb} #{self}")
-  end
 end
