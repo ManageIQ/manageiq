5d665cdd06b9aa86062be67168434311f882715d
Jason Frey <fryguy9@gmail.com>
2022-04-26 11:13:15 -0400

    Merge pull request #21829 from agrare/only_capture_metrics_supported_container_images
    Only capture metrics for supported ContainerImages
    
    (cherry picked from commit 32750cf7e828ebb14f235c8470a2c8653db397bd)

--- app/models/manageiq/providers/container_manager/metrics_capture.rb
+++ app/models/manageiq/providers/container_manager/metrics_capture.rb
@@ -4,10 +4,10 @@ class ManageIQ::Providers::ContainerManager::MetricsCapture < ManageIQ::Provider
 
     MiqPreloader.preload([ems], :container_images => :tags, :container_nodes => :tags, :container_groups => [:tags, :containers => :tags])
 
-    with_archived(ems.all_container_nodes) +
-      with_archived(ems.all_container_groups) +
-      with_archived(ems.all_containers) +
-      with_archived(ems.container_images)
+    with_archived(ems.all_container_nodes.supporting(:capture)) +
+      with_archived(ems.all_container_groups.supporting(:capture)) +
+      with_archived(ems.all_containers.supporting(:capture)) +
+      with_archived(ems.container_images.supporting(:capture))
   end
 
   private
