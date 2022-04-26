5f20100b758ea1d64cb934cef8c7a690c59fc9b6
Gregor Berginc <gregor.berginc@xlab.si>
2022-04-26 09:10:40 +0200

    Add bidirectional relationship to server profiles
    This commit adds a bidirectional relationship between physical server
    profiles and physical servers.

--- app/models/physical_server_profile.rb
+++ app/models/physical_server_profile.rb
@@ -13,8 +13,8 @@ class PhysicalServerProfile < ApplicationRecord
   belongs_to :ext_management_system, :foreign_key => :ems_id, :inverse_of => :physical_server_profiles,
     :class_name => "ManageIQ::Providers::PhysicalInfraManager"
 
-  belongs_to :assigned_server, :optional => true, :class_name => "ManageIQ::Providers::PhysicalInfraManager::PhysicalServer"
-  belongs_to :associated_server, :optional => true, :class_name => "ManageIQ::Providers::PhysicalInfraManager::PhysicalServer"
+  belongs_to :assigned_server, :optional => true, :inverse_of => :assigned_server_profile, :class_name => "::PhysicalServer"
+  belongs_to :associated_server, :optional => true, :inverse_of => :associated_server_profile, :class_name => "::PhysicalServer"
 
   delegate :queue_name_for_ems_operations, :to => :ext_management_system, :allow_nil => true
 
