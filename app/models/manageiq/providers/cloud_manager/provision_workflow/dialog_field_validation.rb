505e0fa896065cefacc50d92d3a00df5bf0f76e9
Adam Grare <adam@grare.com>
2022-05-16 18:28:18 -0400

    Merge pull request #21863 from GilbertCherrie/fix_provision_form_aws
    Fix aws provision form required validation
    
    (cherry picked from commit 966ac05b476b1815d950e274843bfd8877f5c47a)

--- app/models/manageiq/providers/cloud_manager/provision_workflow/dialog_field_validation.rb
+++ app/models/manageiq/providers/cloud_manager/provision_workflow/dialog_field_validation.rb
@@ -1,9 +1,18 @@
 # These methods are available for dialog field validation, do not erase.
 module ManageIQ::Providers::CloudManager::ProvisionWorkflow::DialogFieldValidation
   def validate_cloud_subnet(field, values, dlg, fld, value)
-    return nil unless value.blank?
-    return nil if get_value(values[:cloud_network]).to_i.zero?
-    return nil unless get_value(values[field]).blank?
+    return nil if value.present?
+    return nil if get_value(values[:placement_auto])
+    return nil if get_value(values[field]).present?
+
+    "#{required_description(dlg, fld)} is required"
+  end
+
+  def validate_cloud_network(field, values, dlg, fld, value)
+    return nil if value.present?
+    return nil if get_value(values[:placement_auto])
+    return nil if get_value(values[field]).present?
+
     "#{required_description(dlg, fld)} is required"
   end
 end
