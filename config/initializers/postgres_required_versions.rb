5213bca30929fa23e83bf28d317bfc75b347d4f4
Joe Rafaniello <jrafanie@gmail.com>
2022-09-22 15:05:10 -0400

    Allow pg 10-14+ in dev, require pg 13 in production

--- config/initializers/postgres_required_versions.rb
+++ config/initializers/postgres_required_versions.rb
@@ -5,13 +5,13 @@ ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend Module.new {
   end
 
   def check_version
-    msg = "The version of PostgreSQL being connected to is incompatible with #{Vmdb::Appliance.PRODUCT_NAME} (10 required)"
+    msg = "The version of PostgreSQL being connected to is incompatible with #{Vmdb::Appliance.PRODUCT_NAME} (13 required)"
 
-    if postgresql_version < 90500
+    if postgresql_version < 100000
       raise msg
     end
 
-    if postgresql_version < 100000 || postgresql_version >= 130000
+    if postgresql_version < 130000 || postgresql_version >= 140000
       raise msg if Rails.env.production? && !ENV["UNSAFE_PG_VERSION"]
       $stderr.puts msg
     end
