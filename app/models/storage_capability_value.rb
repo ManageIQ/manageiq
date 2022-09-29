413a9ccaa6d68f7ba67ac9fbf7db2043228acd76
Or Gur <Or.Gur@ocean.ibm.com>
2022-09-29 18:05:00 +0300

    still have to fix capability and value response, probably in the jsonnet / client.
    right now they are retrieved as nil and it also seems to cause the values table to write duplicate entries on every refresh

--- app/models/storage_capability_value.rb
+++ app/models/storage_capability_value.rb
@@ -1,6 +1,6 @@
 class StorageCapabilityValue < ApplicationRecord
 
-  belongs_to :storage_capability, :foreign_key => :capability_id, :dependent => :destroy
+  belongs_to :storage_capability, :inverse_of => :storage_capability_values
   belongs_to :ext_management_system, :foreign_key => :ems_id
 
   has_many :storage_service_capabilities, :foreign_key => :capability_id
