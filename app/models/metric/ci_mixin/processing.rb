510b4d9d7248a487e5ca3177f31c3cd3354c3bef
Keenan Brock <keenan@thebrocks.net>
2022-10-27 20:36:47 -0400

    cut up metrics perf_process
    only introducing methods. not changing anything else

--- app/models/metric/ci_mixin/processing.rb
+++ app/models/metric/ci_mixin/processing.rb
@@ -18,82 +18,98 @@ module Metric::CiMixin::Processing
       rt_rows = {}
 
       counters_data.each do |resource, data|
-        counters       = data[:counters]
-        counter_values = data[:counter_values]
-
-        Benchmark.realtime_block(:process_counter_values) do
-          counter_values.each do |ts, cv|
-            ts = Metric::Helper.nearest_realtime_timestamp(ts) if interval_name == 'realtime'
-
-            col_vals = {}
-            cv.each do |counter_id, value|
-              counter = counters[counter_id]
-              next if counter.nil? || counter[:capture_interval_name] != interval_name
-
-              col = counter[:counter_key].to_sym
-              unless Metric.column_names_symbols.include?(col)
-                _log.debug("#{log_header} Column [#{col}] is not defined, skipping")
-                next
-              end
-
-              col_vals.store_path(col, counter[:instance], [value, counter])
-            end
-
-            col_vals.each do |col, values_by_instance|
-              # If there are multiple instances for a column, use the aggregate
-              #   instance, if available, otherwise roll it up ourselves.
-              value, counter = values_by_instance[""]
-              if value.nil?
-                value = 0
-                counter = nil
-                values_by_instance.each_value do |v, c|
-                  value += v
-                  counter = c
-                end
-              end
-
-              # Create hashes for the rows
-              rt = (rt_rows[[ts, resource]] ||= {
-                :capture_interval_name => interval_name,
-                :capture_interval      => counter[:capture_interval],
-                :resource              => resource,
-                :resource_name         => resource.name,
-                :timestamp             => ts
-              })
-              rt[col], message = normalize_value(value, counter)
-              _log.warn("#{log_header} #{log_target} Timestamp: [#{ts}], Column [#{col}]: '#{message}'") if message
-            end
-          end
-        end
+        process_counter_values(resource, data, interval_name, rt_rows) ## rt_rows changes
       end
 
-      parameters = if ActiveMetrics::Base.connection_config[:adapter] == "miq_postgres"
-                     # We can just pass original data to PG, with metrics grouped by timestamps, since that is how
-                     # we store to PG now. It will spare quite some memory and time to not convert it to row_per_metric
-                     # and than back to original format.
-                     transform_parameters_row_with_all_metrics(resources, interval_name, start_time, end_time, rt_rows)
-                   else
-                     transform_parameters_row_per_metric(resources, interval_name, start_time, end_time, rt_rows)
-                   end
-
-      ActiveMetrics::Base.connection.write_multiple(parameters)
-
-      resources.each do |resource|
-        resource.update_attribute(:last_perf_capture_on, end_time) if resource.last_perf_capture_on.nil? || resource.last_perf_capture_on.utc.iso8601 < end_time
-      end
-
-      # Raise <class>_perf_complete alert event if realtime so alerts can be evaluated.
-      resources.each do |resource|
-        MiqEvent.raise_evm_alert_event_queue(resource, MiqEvent.event_name_for_target(resource, "perf_complete"))
-      end
-
-      resources.each do |resource|
-        resource.perf_rollup_to_parents(interval_orig, start_time, end_time)
-      end
-
-      publish_metrics(rt_rows) if syndicate_metrics?
+      parameters = convert_metrics(resources, interval_name, start_time, end_time, rt_rows)
+      write_metrics(parameters)
+      update_and_publish_metrics(interval_orig, start_time, end_time, resources, rt_rows)
     end
     _log.info("#{log_header} Processing for #{log_specific_targets(resources)}, for range [#{start_time} - #{end_time}]...Complete - Timings: #{t.inspect}")
+  end
+
+  def process_counter_values(resource, data, interval_name, rt_rows)
+    log_header = "[#{interval_name}]"
+
+    counters       = data[:counters]
+    counter_values = data[:counter_values]
+
+    Benchmark.realtime_block(:process_counter_values) do
+      counter_values.each do |ts, cv|
+        ts = Metric::Helper.nearest_realtime_timestamp(ts) if interval_name == 'realtime'
+
+        col_vals = {}
+        cv.each do |counter_id, value|
+          counter = counters[counter_id]
+          next if counter.nil? || counter[:capture_interval_name] != interval_name
+
+          col = counter[:counter_key].to_sym
+          unless Metric.column_names_symbols.include?(col)
+            _log.debug("#{log_header} Column [#{col}] is not defined, skipping")
+            next
+          end
+
+          col_vals.store_path(col, counter[:instance], [value, counter])
+        end
+
+        col_vals.each do |col, values_by_instance|
+          # If there are multiple instances for a column, use the aggregate
+          #   instance, if available, otherwise roll it up ourselves.
+          value, counter = values_by_instance[""]
+          if value.nil?
+            value = 0
+            counter = nil
+            values_by_instance.each_value do |v, c|
+              value += v
+              counter = c
+            end
+          end
+
+          # Create hashes for the rows
+          rt = (rt_rows[[ts, resource]] ||= {
+            :capture_interval_name => interval_name,
+            :capture_interval      => counter[:capture_interval],
+            :resource              => resource,
+            :resource_name         => resource.name,
+            :timestamp             => ts
+          })
+          rt[col], message = normalize_value(value, counter)
+          _log.warn("#{log_header} #{log_target} Timestamp: [#{ts}], Column [#{col}]: '#{message}'") if message
+        end
+      end
+    end
+  end
+
+  def convert_metrics(resources, interval_name, start_time, end_time, rt_rows)
+    if ActiveMetrics::Base.connection_config[:adapter] == "miq_postgres"
+      # We can just pass original data to PG, with metrics grouped by timestamps, since that is how
+      # we store to PG now. It will spare quite some memory and time to not convert it to row_per_metric
+      # and than back to original format.
+      transform_parameters_row_with_all_metrics(resources, interval_name, start_time, end_time, rt_rows)
+    else
+      transform_parameters_row_per_metric(resources, interval_name, start_time, end_time, rt_rows)
+    end
+  end
+
+  def write_metrics(parameters)
+    ActiveMetrics::Base.connection.write_multiple(parameters)
+  end
+
+  def update_and_publish_metrics(interval_orig, start_time, end_time, resources, rt_rows)
+    resources.each do |resource|
+      resource.update_attribute(:last_perf_capture_on, end_time) if resource.last_perf_capture_on.nil? || resource.last_perf_capture_on.utc.iso8601 < end_time
+    end
+
+    # Raise <class>_perf_complete alert event if realtime so alerts can be evaluated.
+    resources.each do |resource|
+      MiqEvent.raise_evm_alert_event_queue(resource, MiqEvent.event_name_for_target(resource, "perf_complete"))
+    end
+
+    resources.each do |resource|
+      resource.perf_rollup_to_parents(interval_orig, start_time, end_time)
+    end
+
+    publish_metrics(rt_rows) if syndicate_metrics?
   end
 
   private
