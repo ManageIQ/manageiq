d62cf94796ac53bb193715f7925d61f926efe13c
eilam <eilam.elimelech@kyndryl.com>
2022-11-24 11:07:32 +0200

    add deploy_server_from_template_queue

--- app/models/physical_server_profile_template.rb
+++ app/models/physical_server_profile_template.rb
@@ -16,4 +16,22 @@ class PhysicalServerProfileTemplate < ApplicationRecord
     ems = ext_management_system
     ems ? ems.my_zone : MiqServer.my_zone
   end
+
+  def deploy_server_from_template_queue(server_id, profile_name)
+    task_opts = {
+      :action => "Deploy server from profile template",
+    }
+
+    queue_opts = {
+      :class_name  => self.class.name,
+      :method_name => 'deploy_server_from_template',
+      :instance_id => id,
+      :role        => 'ems_operations',
+      :queue_name  => ext_management_system.queue_name_for_ems_operations,
+      :zone        => ext_management_system.my_zone,
+      :args        => [server_id, profile_name]
+    }
+
+    MiqTask.generic_action_with_callback(task_opts, queue_opts)
+  end
 end
