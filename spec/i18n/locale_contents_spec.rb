ebd95765854d6b3f95bbbfbeff1f562b61562722
Adam Grare <adam@grare.com>
2023-01-03 14:49:32 -0500

    Update the locale test to ignore the exact year

--- spec/i18n/locale_contents_spec.rb
+++ spec/i18n/locale_contents_spec.rb
@@ -5,7 +5,7 @@ describe :locale_contents do
     locale_files.each do |locale_file|
       it locale_file.basename do
         yaml = YAML.load_file(locale_file)
-        expect(yaml.fetch_path(yaml.keys.first, "product", "copyright")).to start_with("Copyright (c) #{Time.zone.now.year} ManageIQ.")
+        expect(yaml.fetch_path(yaml.keys.first, "product", "copyright")).to match(/Copyright \(c\) \d{4} ManageIQ\./)
       end
     end
   end
