13eb09519756d6f098d2207b313150f6834e728b
Joe Rafaniello <jrafanie@gmail.com>
2023-01-09 16:25:22 -0500

    Cache the model_name previously constantized
    We don't need to classify and constantize a param's model name over
    and over again to see if it's valid?
    
    For this api request with 10101010101010101010k event_streams, the requests before and after:
    
    initial(cold, no prior requests): ~11 => ~9 seconds
    subsequent(previously requested): ~8 => ~5 seconds
    
    http://localhost:3000/api/event_streams?limit=100&offset=0&expand=resources&attributes=group,group_level,group_name,id,event_type,message,ems_id,type,timestamp,created_on,host.name,source,ems_id,ext_management_system.name&filter[]=type=EmsEvent&filter[]=group=other&filter[]=group_level=detail

--- lib/miq_expression/target.rb
+++ lib/miq_expression/target.rb
@@ -15,7 +15,14 @@ class MiqExpression::Target
     # {:model_name => 'User', :associations => ...}
     parsed_params = Hash[match.names.map(&:to_sym).zip(match.to_a[1..-1])]
     begin
-      parsed_params[:model_name] = parsed_params[:model_name].classify.safe_constantize
+      @cached_model_name ||= {}
+      model = @cached_model_name[parsed_params[:model_name]]
+      unless model
+        model = parsed_params[:model_name].classify.safe_constantize
+        @cached_model_name[parsed_params[:model_name]] = model
+      end
+      parsed_params[:model_name] = model
+
     rescue LoadError # issues for case sensitivity (e.g.: VM vs vm)
       parsed_params[:model_name] = nil
     end
