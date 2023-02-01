439b1686dc0fef15388362e072d35d622d9ed3af
Keenan Brock <keenan@thebrocks.net>
2023-02-01 18:06:45 -0500

    For small_environment factory, create has_many associations using after
    Per factory bot documentation, they suggest using after create.
    After create has a reference to the foreign object
    
    https://github.com/thoughtbot/factory_bot/blob/main/GETTING_STARTED.md#has_many-associations
    
    This was tweaked to use the new host :with_ref (in hopes that we could drop host_with_ref)

--- spec/factories/small_environment.rb
+++ spec/factories/small_environment.rb
@@ -2,23 +2,16 @@ FactoryBot.define do
   factory :small_environment, :parent => :zone do
     sequence(:name)         { |n| "small_environment_#{seq_padded_for_sorting(n)}" }
     sequence(:description)  { |n| "Small Environment #{seq_padded_for_sorting(n)}" }
-    ext_management_systems  { [FactoryBot.create(:ems_small_environment)] }
 
-    # Hackery: Due to ntp reload occurring on save, we need to add the servers after saving the zone.
     after(:create) do |z|
+      # Hackery: Due to ntp reload occurring on save,
+      #          we need to add the servers after saving the zone.
       EvmSpecHelper.local_miq_server(:is_master => true, :zone => z)
+      FactoryBot.create_list(:ems_vmware, 1, :zone => z) do |ems|
+        FactoryBot.create(:host, :with_ref, :vmm_product => "Workstation", :ext_management_system => ems) do |host|
+          FactoryBot.create_list(:vm_with_ref, 2, :ext_management_system => ems, :host => host)
+        end
+      end
     end
-  end
-
-  factory :ems_small_environment, :parent => :ems_vmware do
-    hosts        { [FactoryBot.create(:host_small_environment)] }
-    after(:create) do |x|
-      x.hosts.each { |h| h.vms.each { |v| v.update_attribute(:ems_id, x.id) } }
-    end
-  end
-
-  factory :host_small_environment, :parent => :host_with_ref do
-    vmm_product  { "Workstation" }
-    vms          { [FactoryBot.create(:vm_with_ref, :name => "vmtest1"), FactoryBot.create(:vm_with_ref, :name => "vmtest2")] }
   end
 end
