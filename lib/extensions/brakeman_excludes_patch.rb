5bdfa1f6b212e36e512510d26956cc9af23f332b
Joe Rafaniello <jrafanie@users.noreply.github.com>
2023-03-14 09:58:53 -0400

    Merge pull request #22403 from Fryguy/fix_brakeman_patch
    (cherry picked from commit f7b9170894c122dc1994973675db8064ff662524)

--- lib/extensions/brakeman_excludes_patch.rb
+++ lib/extensions/brakeman_excludes_patch.rb
@@ -1,22 +1,28 @@
 module BrakemanExcludesPatch
-  # Original code can be found here:
-  # https://github.com/presidentbeef/brakeman/blob/5a1eb49216b7087243f5ed0f17c90ea8ca696df1/lib/brakeman/app_tree.rb#L204-L216
+  # Override the default Brakeman in_engine_paths? method to account for running
+  # brakeman from within an engine.
   #
-  # Brakeman silently excludes the vendor directory as of 5.0 by default, see:
-  # https://github.com/presidentbeef/brakeman/commit/89e40fef82a2144aaec5f6203959df39aed22c17
-  # This exclusion occurs AFTER inclusion paths via configuration such as engine paths.
-  # The patch below overrides this behavior and only excludes vendor/ files that aren't opted-in via engine paths.
-  def reject_global_excludes(paths)
-    paths.reject do |path|
-      relative_path = path.relative
-
-      # Add last check for engine_paths
-      if @skip_vendor and relative_path.include? 'vendor/' and @engine_paths.none? { |p| path.absolute.include?(p) }
-        true
+  # When brakeman is run from within an engine, the list of engine paths will
+  # include the engine's root directory. In CI, the vendor directory would then be a
+  # child of an engine. The original code would then check "does this path include
+  # an engine", and it always will because the engine is the parent of the vendor
+  # directory.
+  #
+  # For example, if we are running brakeman from /path/to/manageiq-ui-classic,
+  # @engine_paths will include "/path/to/manageiq-ui-classic", and the vendor
+  # directory will be /path/to/manageiq-ui-classic/vendor. The original code would
+  # then check if "/path/to/manageiq-ui-classic/vendor/some/file" includes
+  # "/path/to/manageiq-ui-classic", and of course it always will.
+  #
+  # This patch takes care of that case, by honoring all engine paths _except_
+  # ENGINE_ROOT itself. As this method is only called on paths that already include
+  # "vendor/" we can safely ignore any file under ENGINE_ROOT/vendor.
+  def in_engine_paths?(path)
+    @engine_paths.any? do |p|
+      if defined?(ENGINE_ROOT) && p == ENGINE_ROOT
+        false # Ignore anything in the ENGINE_ROOT/vendor directory
       else
-        Brakeman::AppTree::EXCLUDED_PATHS.any? do |excluded|
-          relative_path.include? excluded
-        end
+        path.absolute.include?(p)
       end
     end
   end
