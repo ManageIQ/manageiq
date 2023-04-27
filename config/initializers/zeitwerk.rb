f0f7bfb074dee395b75dfcd157c9449997120966
Joe Rafaniello <jrafanie@gmail.com>
2023-04-27 13:11:14 -0400

    Workaround dev issue with nuage needing qpid_proton
    Nuage depends on qpid_proton which we can't require on mac
    
    In dev, if we eager load the application, such as with `rake zeitwerk:check`, this file blows up
    on mac because qpid_proton is an optional dependency but actively referenced in messaging_handler.rb.
    
    Only ignore loading qpid_proton here on mac.

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -2,4 +2,10 @@ if Rails.application.config.autoloader == :zeitwerk && Rails.autoloaders.main
   # These specific directories are for code organization, not namespacing:
   Rails.autoloaders.main.collapse(Rails.root.join("lib/manageiq/reporting/charting"))
   Rails.autoloaders.main.collapse(Rails.root.join("lib/ansible/runner/credential"))
+
+  # requires qpid, which is an optional dependency because LOL mac
+  if RUBY_PLATFORM.include?("darwin")
+    message_handler_path = Pathname.new(Vmdb::Plugins.paths[ManageIQ::Providers::Nuage::Engine]).join("app/models/manageiq/providers/nuage/network_manager/event_catcher/messaging_handler.rb")
+    Rails.autoloaders.main.ignore(message_handler_path)
+  end
 end
