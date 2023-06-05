85a2eb19fcfcae8fcf4fed94a82fb4f6c86b9cc2
Joe Rafaniello <jrafanie@gmail.com>
2023-06-05 14:28:35 -0400

    pdf_generator directory is for code organization only, not namespacing
    Fixes the following with zeitwerk when running the spec/lib/pdf_generator_spec.rb test:
    
    expected file /Users/joerafaniello/Code/manageiq/lib/pdf_generator/null_pdf_generator.rb to define constant PdfGenerator::NullPdfGenerator, but didn't

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -1,7 +1,10 @@
 if Rails.application.config.autoloader == :zeitwerk && Rails.autoloaders.main
   # These specific directories are for code organization, not namespacing:
+  # TODO: these should be either renamed with good names, the intermediate directory removed
+  # and/or both.
   Rails.autoloaders.main.collapse(Rails.root.join("lib/manageiq/reporting/charting"))
   Rails.autoloaders.main.collapse(Rails.root.join("lib/ansible/runner/credential"))
+  Rails.autoloaders.main.collapse(Rails.root.join("lib/pdf_generator"))
 
   # requires qpid, which is an optional dependency because LOL mac
   if RUBY_PLATFORM.include?("darwin")
