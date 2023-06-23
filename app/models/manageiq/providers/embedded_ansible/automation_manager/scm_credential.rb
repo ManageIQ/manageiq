1595ebd45dbab9060619f177014db7c4fd2b34d3
Adam Grare <adam@grare.com>
2023-06-23 13:51:31 -0400

    Fix EmbeddedAnsible ScmCredential parent class
    All EmbeddedAnisble credentials should derive from the
    `EmbeddedAnsible::AutomationManager::Credential` class.

--- app/models/manageiq/providers/embedded_ansible/automation_manager/scm_credential.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/scm_credential.rb
@@ -1,3 +1,5 @@
-class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::ScmCredential < ManageIQ::Providers::EmbeddedAutomationManager::ScmCredential
+class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::ScmCredential < ManageIQ::Providers::EmbeddedAnsible::AutomationManager::Credential
+  include ManageIQ::Providers::EmbeddedAutomationManager::ScmCredentialMixin
+
   FRIENDLY_NAME = "Embedded Ansible Credential".freeze
 end
