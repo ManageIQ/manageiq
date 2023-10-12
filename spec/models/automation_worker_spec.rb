97bbe9f693300b99d07f52e601665a8fde8eeff9
Brandon Dunne <brandondunne@hotmail.com>
2023-10-12 13:38:23 -0400

    Set AUTOMATION_JOB_SERVICE_ACCOUNT env var for the automation worker

--- spec/models/automation_worker_spec.rb
+++ spec/models/automation_worker_spec.rb
@@ -22,6 +22,8 @@ RSpec.describe AutomationWorker do
       }
     end
 
+    before { stub_const("ENV", ENV.to_h.merge("WORKER_SERVICE_ACCOUNT" => "abcdefg")) }
+
     it "#configure_worker_deplyoment adds a node selector based on the zone name" do
       EvmSpecHelper.local_miq_server
 
@@ -30,6 +32,15 @@ RSpec.describe AutomationWorker do
 
       expect(test_deployment.dig(:spec, :template, :spec, :nodeSelector)).to eq("manageiq/zone-#{MiqServer.my_zone}".tr(" ", "-") => "true")
       expect(test_deployment.dig(:spec, :template, :spec, :serviceAccountName)).to eq("manageiq-automation")
+    end
+
+    it "#configure_worker_deplyoment adds env with the service account name" do
+      EvmSpecHelper.local_miq_server
+
+      worker = described_class.new
+      worker.configure_worker_deployment(test_deployment)
+
+      expect(test_deployment.dig(:spec, :template, :spec, :containers, 0, :env)).to include({:name => "AUTOMATION_JOB_SERVICE_ACCOUNT", :value => "abcdefg"})
     end
   end
 end
