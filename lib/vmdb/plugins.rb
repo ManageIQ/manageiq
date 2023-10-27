dd3f6ad5972e3496c9e3bc04943e61531a78cb91
Adam Grare <adam@grare.com>
2023-10-27 12:03:23 -0400

    Seed workflows from plugins' content/workflows

--- lib/vmdb/plugins.rb
+++ lib/vmdb/plugins.rb
@@ -80,6 +80,14 @@ module Vmdb
       end
     end
 
+    def embedded_workflows_content
+      @embedded_workflows_content ||= begin
+        flat_map do |engine|
+          Dir.glob(engine.root.join("content", "workflows", "**", "*.asl"))
+        end
+      end
+    end
+
     def automate_domains
       @automate_domains ||= begin
         require_relative 'plugins/automate_domain'
