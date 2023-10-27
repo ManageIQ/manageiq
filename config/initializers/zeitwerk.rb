5dc641fffd3c654d9076a0c9de46d78a68b973e8
Joe Rafaniello <jrafanie@gmail.com>
2023-10-27 11:52:12 -0400

    Don't eager load a class needing qpid_proton loaded at load time
    This class can't be eager loaded because not all platforms install qpid_proton
    and we need qpid_proton's class at load time.  We can't install qpid_proton on mac
    but also don't want to install qpid's dependencies in CI in various providers, core,
    etc.

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -11,9 +11,8 @@ if Rails.application.config.autoloader == :zeitwerk && Rails.autoloaders.main
   Rails.autoloaders.main.collapse(Rails.root.join("lib/ansible/runner/credential"))
   Rails.autoloaders.main.collapse(Rails.root.join("lib/pdf_generator"))
 
-  # requires qpid, which is an optional dependency because LOL mac
-  if RUBY_PLATFORM.include?("darwin")
-    message_handler_path = Pathname.new(Vmdb::Plugins.paths[ManageIQ::Providers::Nuage::Engine]).join("app/models/manageiq/providers/nuage/network_manager/event_catcher/messaging_handler.rb")
-    Rails.autoloaders.main.ignore(message_handler_path)
-  end
+  # The following file should not be eager loaded as it depends on being installed, which isn't possible on macs and isn't done by default
+  # in ci for other plugins, core, etc.
+  message_handler_path = Pathname.new(Vmdb::Plugins.paths[ManageIQ::Providers::Nuage::Engine]).join("app/models/manageiq/providers/nuage/network_manager/event_catcher/messaging_handler.rb")
+  Rails.autoloaders.main.ignore(message_handler_path)
 end
