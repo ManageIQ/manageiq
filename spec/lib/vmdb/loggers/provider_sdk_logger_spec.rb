1b091943f25a0b7bb2abb93760c230d9a14d52c9
Jason Frey <Jason.Frey@ibm.com>
2023-11-17 12:10:42 -0500

    Merge pull request #6 from Adam-Grare/add_basic_authorization_provider_sdk_logger_formatter_filter
    Filter out Basic Authorization tokens
    
    (cherry picked from commit aaf2753e6a2387aac6188ecf58e912d56d3701b7)

--- spec/lib/vmdb/loggers/provider_sdk_logger_spec.rb
+++ spec/lib/vmdb/loggers/provider_sdk_logger_spec.rb
@@ -4,23 +4,27 @@ RSpec.describe Vmdb::Loggers::ProviderSdkLogger do
     @log = described_class.new(@log_stream)
   end
 
-  context "azure" do
-    it "filters out bearer tokens" do
-      @log.log(@log.level, 'Bearer abcd1234 "stuff"')
-      @log_stream.rewind
-      expect(@log_stream.read).to match(Regexp.quote('Bearer [FILTERED] "stuff"'))
-    end
+  it "filters out bearer tokens" do
+    @log.log(@log.level, 'Bearer abcd1234 "stuff"')
+    @log_stream.rewind
+    expect(@log_stream.read).to match(Regexp.quote('Bearer [FILTERED] "stuff"'))
+  end
 
-    it "filters out sharedkey tokens" do
-      @log.log(@log.level, 'SharedKey xxx123 "stuff"')
-      @log_stream.rewind
-      expect(@log_stream.read).to match(Regexp.quote('SharedKey [FILTERED] "stuff"'))
-    end
+  it "filters out basic tokens" do
+    @log.log(@log.level, 'Authorization: "Basic abcd1234"')
+    @log_stream.rewind
+    expect(@log_stream.read).to match(Regexp.quote('Authorization: "Basic [FILTERED] "'))
+  end
 
-    it "filters out client secret tokens" do
-      @log.log(@log.level, 'client_secret=abc123&management=yadayada')
-      @log_stream.rewind
-      expect(@log_stream.read).to match(Regexp.quote('client_secret=[FILTERED]&management=yadayada'))
-    end
+  it "filters out sharedkey tokens" do
+    @log.log(@log.level, 'SharedKey xxx123 "stuff"')
+    @log_stream.rewind
+    expect(@log_stream.read).to match(Regexp.quote('SharedKey [FILTERED] "stuff"'))
+  end
+
+  it "filters out client secret tokens" do
+    @log.log(@log.level, 'client_secret=abc123&management=yadayada')
+    @log_stream.rewind
+    expect(@log_stream.read).to match(Regexp.quote('client_secret=[FILTERED]&management=yadayada'))
   end
 end
