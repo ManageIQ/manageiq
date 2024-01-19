4370b900e4d9478afa16fb81928a6d8dc053c82b
Keenan Brock <keenan@thebrocks.net>
2024-01-19 17:45:00 -0500

    Add tenancy scoping to Switches
    Switches were not filtering on tenant
    Unfortunately, switches do not have a tenant_id
    so we needed to add a little extra code to find the tenant through the ems
    (which is through the host... sorry)

--- app/models/switch.rb
+++ app/models/switch.rb
@@ -1,6 +1,7 @@
 class Switch < ApplicationRecord
   include NewWithTypeStiMixin
   include CustomActionsMixin
+  extend TenancyCommonMixin
 
   belongs_to :host, :inverse_of => :host_virtual_switches
 
@@ -13,6 +14,22 @@ class Switch < ApplicationRecord
 
   scope :shareable, ->     { where(:shared => true) }
   scope :with_id,   ->(id) { where(:id => id) }
+
+  has_one :tenant, :through => :ext_management_system
+
+  def self.scope_by_tenant?
+    true
+  end
+
+  def self.tenant_id_clause_format(tenant_ids)
+    {:ext_management_systems => {:tenant_id => tenant_ids}}
+  end
+
+  # in a perfect world, all of this would be in tenant_clause
+  # and that would not return a hash but a scope
+  def self.tenant_join_clause
+    left_outer_joins(:tenant)
+  end
 
   acts_as_miq_taggable
 end
