235eed6c024f47df1a495b81c13ce3dc30f1f069
Keenan Brock <keenan@thebrocks.net>
2024-04-04 19:17:42 -0400

    Fix supports_helper stub_supports to handle unsupported_reason
    We are now getting away from supports?(:supported_feature).
    Instead we are calling unsupported_reason(:supported_feature)
    
    It cuts down on our calls into the supports_feature_mixin.
    
    Before
    ======
    
    When using stub_supports,
    unsupported_reason(:supported_feature) was not stubbed (and unpredictable)
    supports? was stubbed
    
    After
    =====
    
    We are stubbing at the feature definition layer
    all code in unsupported_reason is run/tested
    all code in supports? is run/tested
    
    This puts more code under test and allows flexibility for refactoring
    
    Notes
    =====
    
    Wasn't thrilled about kwarg supports, but a few tests still use that.
    I've changed them all but it wasn't too much work to keep this working
    so we can backport with minimal fuss
    
    This code better tests our features that defer to another feature

--- spec/support/supports_helper.rb
+++ spec/support/supports_helper.rb
@@ -4,7 +4,7 @@ module Spec
       # when testing a model that receives multiple supports,
       # put this down first to allow the other supports to work fine.
       def stub_supports_all_others(model)
-        allow_any_instance_of(model).to receive(:supports?).and_call_original
+        allow(model.supports_features).to receive(:[]).and_call_original
       end
 
       # when testing requests, ensure the model supports a certain attribute
@@ -12,23 +12,13 @@ module Spec
         model = model.class unless model.kind_of?(Class)
         feature = feature.to_sym
 
-        receive_supports = receive(:supports?).with(feature).and_return(supported)
-        allow(model).to receive_supports
-        allow_any_instance_of(model).to receive_supports
-
-        allow(model).to receive(:types_supporting).with(feature).and_return([nil] + model.descendants.map(&:name))
+        allow(model.supports_features).to receive(:[]).with(feature).and_return(supported)
+        # TODO: verify this stub is necessary
+        allow(model).to receive(:types_supporting).with(feature).and_return([nil] + model.descendants.map(&:name)) if supported == true
       end
 
       def stub_supports_not(model, feature = :update, reason = nil)
-        model = model.class unless model.kind_of?(Class)
-        feature = feature.to_sym
-
-        stub_supports(model, feature, :supported => false)
-
-        reason ||= SupportsFeatureMixin.default_supports_reason
-        receive_reason = receive(:unsupported_reason).with(feature).and_return(reason)
-        allow(model).to(receive_reason)
-        allow_any_instance_of(model).to(receive_reason)
+        stub_supports(model, feature, :supported => (reason || false))
       end
     end
   end
