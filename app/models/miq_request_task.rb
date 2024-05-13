d9a492e91557485a69f555b2101fa546f04aa80e
Adam Grare <adam@grare.com>
2024-05-13 14:20:32 -0400

    add configuration_script_payload_id to miq_provision

--- app/models/miq_request_task.rb
+++ app/models/miq_request_task.rb
@@ -140,12 +140,12 @@ class MiqRequestTask < ApplicationRecord
 
     _log.info("Queuing #{request_class::TASK_DESCRIPTION}: [#{description}]...")
 
-    if resource_action&.configuration_script_payload
-      workflow = resource_action.configuration_script_payload
+    workflow = ConfigurationScriptPayload.find(options[:configuration_script_payload_id]) if options[:configuration_script_payload_id]
+    if workflow
       miq_task_id = workflow.run(:inputs => dialog_values, :userid => get_user.userid, :zone => zone, :object => self)
 
       options[:miq_task_id]                     = miq_task_id
-      options[:configuration_script_payload_id] = resource_action.configuration_script_payload.id
+      options[:configuration_script_payload_id] = workflow.id
       options[:configuration_script_id]         = MiqTask.find(miq_task_id).context_data[:workflow_instance_id]
       save!
     elsif self.class::AUTOMATE_DRIVES
