2995b4b56a468a84432a84e4c36b9f33784a9840
sudhir.kelkar@in.ibm.com <sudhir.kelkar@in.ibm.com>
2024-05-29 20:52:03 +0530

    Added environment variable TERRAFORM_RUNNER_URL

--- lib/container_orchestrator/object_definition.rb
+++ lib/container_orchestrator/object_definition.rb
@@ -175,6 +175,7 @@ class ContainerOrchestrator
         {:name => "MEMCACHED_SERVICE_NAME",  :value => ENV["MEMCACHED_SERVICE_NAME"]},
         {:name => "WORKER_HEARTBEAT_FILE",   :value => Rails.root.join("tmp/worker.hb").to_s},
         {:name => "WORKER_HEARTBEAT_METHOD", :value => "file"},
+        {:name => "TERRAFORM_RUNNER_URL",    :value => "https://opentofu-runner:6000"},
       ] + database_environment + memcached_environment + messaging_environment
     end
 
