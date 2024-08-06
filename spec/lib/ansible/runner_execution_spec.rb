c55e2cceb9e291cdeb94fda2da823c51433fb0ac
Jason Frey <fryguy9@gmail.com>
2024-08-06 12:10:23 -0400

    Cleanup playbooks and specs for consistency

--- spec/lib/ansible/runner_execution_spec.rb
+++ spec/lib/ansible/runner_execution_spec.rb
@@ -3,22 +3,26 @@ RSpec.describe Ansible::Runner do
     pending("ansible-runner executable not available") unless described_class.available?
   end
 
+  DATA_DIRECTORY = Pathname.new(__dir__).join("runner/data")
+
   shared_examples_for :executing_playbooks do
     let(:method_under_test) { async ? :run_async : :run }
     let(:env_vars)   { {} }
     let(:extra_vars) { {} }
 
-    it "runs a hello-world playbook" do
-      response = Ansible::Runner.public_send(method_under_test, env_vars, extra_vars, File.join(__dir__, "runner/data/hello_world.yml"))
+    it "runs a playbook" do
+      playbook = DATA_DIRECTORY.join("hello_world.yml")
+
+      response = Ansible::Runner.public_send(method_under_test, env_vars, extra_vars, playbook)
       response = response.wait(5.seconds) if async
 
       expect(response.return_code).to eq(0), "ansible-runner failed with:\n#{response.stderr}"
-      expect(response.human_stdout).to include('"msg": "Hello, world!"')
+      expect(response.human_stdout).to include('"msg": "Hello World!"')
     end
 
-    it "runs a hello-world-vault-encrypted playbook" do
+    it "runs a playbook with vault encrypted variables" do
+      playbook   = DATA_DIRECTORY.join("hello_world_vault_encrypted_vars.yml")
       credential = FactoryBot.create(:embedded_ansible_vault_credential, :password => "vault")
-      playbook   = File.join(__dir__, "runner/data/hello_world_vault_encrypted.yml")
 
       response = Ansible::Runner.public_send(method_under_test, env_vars, extra_vars, playbook, :credentials => [credential.id])
       response = response.wait(5.seconds) if async
