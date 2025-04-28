4891ec7f52cd13de4d7683a85629ac31d1de2e09
Adam Grare <adam@grare.com>
2025-04-28 14:06:25 -0400

    Use a custom Logdev for RequestLogger

--- lib/vmdb/loggers/request_logger.rb
+++ lib/vmdb/loggers/request_logger.rb
@@ -3,30 +3,17 @@ module Vmdb::Loggers
     attr_reader :resource_id
 
     def initialize(*args, resource_id:, **kwargs)
-      @resource_id = resource_id
       super(nil, *args, **kwargs)
+
+      @resource_id = resource_id
+      @logdev      = Logdev.new(resource_id) if resource_id
+      @formatter   = Formatter.new
     end
 
-    def add(severity, message = nil, progname = nil)
-      severity ||= Logger::UNKNOWN
-      if resource_id.nil? or severity < level
-        return true
+    class Formatter
+      def call(severity, _time, _progname, msg)
+        "#{severity}:#{msg}"
       end
-      if progname.nil?
-        progname = @progname
-      end
-      if message.nil?
-        if block_given?
-          message = yield
-        else
-          message = progname
-          progname = @progname
-        end
-      end
-
-      RequestLog.create(:message => message, :severity => format_severity(severity), :resource_id => resource_id)
-
-      true
     end
   end
 end
