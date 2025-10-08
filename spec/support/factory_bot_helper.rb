028e3d07a00cfe68445dd8eb792e4a2ed8a8b294
Joe Rafaniello <jrafanie@gmail.com>
2025-10-08 12:04:27 -0400

    Use pathnames to follow factory bot rails conventions
    factory bot rails converts the default factory bot directory paths to pathnames
    based on rails root:
    https://github.com/thoughtbot/factory_bot_rails/blob/dfdef4c70a76d36058cf77fc1ccd9904a2bf0beb/lib/factory_bot_rails/railtie.rb#L50-L52
    
    * Ensure any duplicate pathnames are removed.
    * Clarify the rationale for using definition_path_files and reload.

--- spec/support/factory_bot_helper.rb
+++ spec/support/factory_bot_helper.rb
@@ -24,12 +24,17 @@ FactoryBot.definition_file_paths << Rails.root.join('spec/factories')
 
 # Also, add factories from provider gems until miq codebase does not use any provider specific factories anymore
 Rails::Engine.subclasses.select { |e| e.name.starts_with?("ManageIQ::Providers") }.each do |engine|
-  FactoryBot.definition_file_paths << File.join(engine.root, 'spec', 'factories')
+  FactoryBot.definition_file_paths << engine.root.join('spec', 'factories')
 end
 
-# find_definitions is not reentrant, so call reload to reset things before it calls find_definitions.
-# Other code, such as factory bot rails, may have already registered factories, so we can't assume we're the first to call
-# find_definitions.
+# Prefer setting the definition_file_paths and call FactoryBot.reload, which will recreate the configuration
+# and call find_definitions.
+#
+# find_definitions is not reentrant, so a second call will lead to loading already registered factories.
+# Other code, such as factory bot rails, may have already registered factories, so we can't assume
+# we're the first to call find_definitions.
+FactoryBot.definition_file_paths = FactoryBot.definition_file_paths.uniq
+
 FactoryBot.reload
 
 FactoryBot.define do
