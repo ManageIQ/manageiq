58d13b0b065c65baf2b73425114420444e238840
Keenan Brock <keenan@thebrocks.net>
2025-10-09 13:26:31 -0400

    Drop ActAsArScop duplicate methods
    Going over ActsAsArScope and ActsAsArModel to double check the Rbac.filter code
    
    aar_scope_klass
    ===============
    
    No reason to define the delegate of aar_scope_klass, when ActsAsArScope already does this
    
    `all`
    ===
    
    - We only ever call all() with no parameters.
    - Rails doesn't support parameters, it will throw an exception
    - Our code doesn't use this for AR or AAAM (if it did, it would throw an error)

--- lib/acts_as_ar_scope.rb
+++ lib/acts_as_ar_scope.rb
@@ -5,7 +5,7 @@ class ActsAsArScope < ActsAsArModel
   class << self
     # Standard query methods are delegated to the core active record classes
     delegate :includes, :references, :eager_load, :preload, :limit, :order, :offset, :select, :where, :to => :aar_scope
-    delegate :find, :first, :last, :find_by_id, :find_by, :count, :to => :aar_scope
+    delegate :find, :first, :last, :find_by_id, :find_by, :count, :all, :to => :aar_scope
 
     delegate :klass, :to => :aar_scope, :prefix => true
     delegate :table_name, :reflections, :to => :aar_scope_klass
@@ -14,15 +14,6 @@ class ActsAsArScope < ActsAsArModel
 
   def self.aar_scope
     raise NotImplementedError, _("find must be implemented in a subclass")
-  end
-
-  def self.all(*args)
-    if args.empty? || args.size == 1 && args.first.respond_to?(:empty?) && args.first.empty?
-      # avoid warnings
-      aar_scope
-    else
-      aar_scope.all(*args)
-    end
   end
 
   # TODO: goal is for this to be false
