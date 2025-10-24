03e87361e79e2ccf2a3a89275c0a41e29df61f7c
Adam Grare <adam@grare.com>
2025-10-24 11:27:59 -0400

    Change source_id to src_configuration_script_id

--- spec/models/miq_provision_configuration_script_request_spec.rb
+++ spec/models/miq_provision_configuration_script_request_spec.rb
@@ -2,11 +2,11 @@ describe MiqProvisionConfigurationScriptRequest do
   let(:admin)                { FactoryBot.create(:user) }
   let(:ems)                  { FactoryBot.create(:automation_manager, :url => "https://localhost") }
   let(:configuration_script) { FactoryBot.create(:configuration_script, :manager => ems) }
-  let(:request)              { FactoryBot.create(:miq_provision_configuration_script_request, :requester => admin, :options => {:source_id => [configuration_script.id]}) }
+  let(:request)              { FactoryBot.create(:miq_provision_configuration_script_request, :requester => admin, :options => {:src_configuration_script_id => [configuration_script.id]}) }
 
   describe ".request_task_class_from" do
     it "retrieves the request task class" do
-      options = {:source_id => configuration_script.id}
+      options = {:src_configuration_script_id => configuration_script.id}
 
       expect(described_class.request_task_class_from("options" => options)).to eq(ems.class::Provision)
     end
@@ -14,7 +14,7 @@ describe MiqProvisionConfigurationScriptRequest do
 
   describe ".new_request_task" do
     it "returns the provision task" do
-      options = {:source_id => configuration_script.id}
+      options = {:src_configuration_script_id => configuration_script.id}
 
       request_task = described_class.new_request_task("options" => options)
       expect(request_task).to have_attributes(:options => options, :state => "pending")
