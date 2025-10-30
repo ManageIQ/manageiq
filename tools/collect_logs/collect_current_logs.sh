a94dd8164f912691f604d94e705348a58f9f4844
Jason Frey <fryguy9@gmail.com>
2025-10-30 10:38:31 -0400

    Support /mnt/log_collection mount point for log collection

--- tools/collect_logs/collect_current_logs.sh
+++ tools/collect_logs/collect_current_logs.sh
@@ -3,14 +3,19 @@
 # save directory from which command is initiated
 collect_logs_directory=$(pwd)
 
-# make the vmdb/log directory the current directory
-vmdb_logs_directory="/var/www/miq/vmdb"
-pushd ${vmdb_logs_directory}
+# determine where to write the logs to
+if mountpoint /mnt/log_collection >/dev/null; then
+    base_log_dir="/mnt/log_collection"
+else
+    base_log_dir="/var/www/miq/vmdb"
+fi
+pushd ${base_log_dir}
+mkdir -p log
 
-# eliminiate any prior collected logs to make sure that only one collection is current
+# eliminate any prior collected logs to make sure that only one collection is current
 rm -f log/evm_full_archive_$(uname -n)* log/evm_current_$(uname -n)*
 
-#Source in the file so that we can call postgresql functions
+# Source in the file so that we can call postgresql functions
 source /etc/default/evm
 
 tarball="log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz"
@@ -29,4 +34,4 @@ fi
 popd
 
 # let the user know where the archive is
-echo "Archive Written To: ${vmdb_logs_directory}/${tarball}"
+echo "Archive Written To: ${base_log_dir}/${tarball}"
