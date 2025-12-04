ef73045273f691aab0ca96eed4fd0f28b05c5139
Adam Grare <adam@grare.com>
2025-12-04 11:53:04 -0500

    Implement remove_from_provider/check_removed_from_provider

--- app/models/orchestration_stack_retire_task.rb
+++ app/models/orchestration_stack_retire_task.rb
@@ -1,5 +1,11 @@
 class OrchestrationStackRetireTask < MiqRetireTask
+  include StateMachine
+
   default_value_for :request_type, "orchestration_stack_retire"
+
+  def stack
+    source
+  end
 
   def self.base_model
     OrchestrationStackRetireTask
