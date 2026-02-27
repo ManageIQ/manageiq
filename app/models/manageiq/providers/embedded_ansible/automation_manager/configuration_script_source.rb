721c7ed15ee3dc6568eb0a80ee27c8b646d4f267
Adam Grare <adam@grare.com>
2026-02-27 09:39:53 -0500

    Refactor EmbeddedAutomationManager repo sync
    Move most of the common logic into the base
    `EmbeddedAutomationManager::ConfigurationScriptSource#sync` base method

--- app/models/manageiq/providers/embedded_ansible/automation_manager/configuration_script_source.rb
+++ app/models/manageiq/providers/embedded_ansible/automation_manager/configuration_script_source.rb
@@ -14,53 +14,31 @@ class ManageIQ::Providers::EmbeddedAnsible::AutomationManager::ConfigurationScri
   end
 
   def sync
-    update!(:status => "running")
-    transaction do
-      current_payloads = configuration_script_payloads.index_by(&:name)
-      current_scripts  = manager.configuration_scripts.where(:name => current_payloads.keys).index_by(&:name)
+    super do |current_payloads, current_scripts, _tempdir, worktree|
+      worktree.blob_list.each do |filename|
+        next unless playbook_dir?(filename)
 
-      git_repository.update_repo
-      git_repository.with_worktree do |worktree|
-        worktree.ref = scm_branch
-        worktree.blob_list.each do |filename|
-          next unless playbook_dir?(filename)
+        content = worktree.read_file(filename)
+        next unless playbook?(filename, content)
 
-          content = worktree.read_file(filename)
-          next unless playbook?(filename, content)
+        found_payload = current_payloads.delete(filename) || self.class.module_parent::Playbook.new(:configuration_script_source_id => id)
 
-          found_payload = current_payloads.delete(filename) || self.class.module_parent::Playbook.new(:configuration_script_source_id => id)
-
-          attrs = {:name => filename, :manager_id => manager_id}
-          unless encrypted_playbook?(content)
-            attrs[:payload]      = content
-            attrs[:payload_type] = "yaml"
-          end
-
-          found_payload.update!(attrs)
-
-          found_script = current_scripts.delete(filename) || self.class.module_parent::ConfigurationScript.new(:configuration_script_source_id => id, :parent_id => found_payload.id)
-          found_script.update!(
-            :name       => filename,
-            :manager_id => manager_id,
-            :parent_id  => found_payload.id
-          )
+        attrs = {:name => filename, :manager_id => manager_id}
+        unless encrypted_playbook?(content)
+          attrs[:payload]      = content
+          attrs[:payload_type] = "yaml"
         end
+
+        found_payload.update!(attrs)
+
+        found_script = current_scripts.delete(filename) || self.class.module_parent::ConfigurationScript.new(:configuration_script_source_id => id, :parent_id => found_payload.id)
+        found_script.update!(
+          :name       => filename,
+          :manager_id => manager_id,
+          :parent_id  => found_payload.id
+        )
       end
-
-      current_payloads.each_value(&:destroy)
-      current_scripts.each_value(&:destroy)
-
-      configuration_script_payloads.reload
-      manager.configuration_scripts.reload
     end
-    update!(:status            => "successful",
-            :last_updated_on   => Time.zone.now,
-            :last_update_error => nil)
-  rescue => error
-    update!(:status            => "error",
-            :last_updated_on   => Time.zone.now,
-            :last_update_error => format_sync_error(error))
-    raise error
   end
 
   def playbooks_in_git_repository
