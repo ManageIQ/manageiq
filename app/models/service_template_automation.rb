5f87678bba1b733dd6cb1c0f03a1c40f7b2fa541
Adam Grare <adam@grare.com>
2026-03-19 12:13:23 -0400

    Move ServiceTemplateAutomationMixin into base class
    Now that all of the external automation managers inherit from the
    `ServiceTemplateAutomation` class we can drop this mixin

--- app/models/service_template_automation.rb
+++ app/models/service_template_automation.rb
@@ -1,3 +1,11 @@
 class ServiceTemplateAutomation < ServiceTemplate
-  include ServiceTemplateAutomationMixin
+  def self.available_managers
+    automation_manager_klass.all
+  end
+
+  def self.automation_manager_klass
+    @automation_manager_klass ||= "ManageIQ::Providers::#{name.sub("ServiceTemplate", "")}::AutomationManager".constantize
+  end
+
+  delegate :available_managers, :to => :class
 end
