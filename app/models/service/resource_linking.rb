e8e7cf9c6409fd027e727d0a4815e43e7cd0e70c
Adam Grare <adam@grare.com>
2026-03-31 14:43:25 -0400

    Add adjustable timeout to Service::LinkingWorkflow
    The default timeout for any `::Job` is 5 minutes, it is possible that
    some large `EmsRefresh` full refreshes can exceed that and there is
    currently no way to extend the `Service::LinkingWorkflow` timeout.

--- app/models/service/resource_linking.rb
+++ app/models/service/resource_linking.rb
@@ -1,7 +1,7 @@
 module Service::ResourceLinking
   extend ActiveSupport::Concern
 
-  def add_provider_vms(provider, uid_ems_array)
+  def add_provider_vms(provider, uid_ems_array, timeout: nil)
     vm_uid_array = Array(uid_ems_array).compact.uniq
     raise _("no uid_ems_array defined for linking to service") if vm_uid_array.blank?
 
@@ -13,7 +13,8 @@ module Service::ResourceLinking
       :userid        => evm_owner.userid,
       :sync_key      => guid,
       :service_id    => id,
-      :zone          => my_zone
+      :zone          => my_zone,
+      :timeout       => timeout
     }
 
     _log.info("NAME [#{options[:name]}] for user #{evm_owner.userid}")
