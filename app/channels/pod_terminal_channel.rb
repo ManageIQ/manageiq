b2579935918aa3d9c976243b67f844ec0d6115d5
ishaaq.sr <ishaaq.sr@xaasio.com>
2026-07-06 15:37:58 +0530

    fix rubocop offence

--- app/channels/pod_terminal_channel.rb
+++ app/channels/pod_terminal_channel.rb
@@ -1,12 +1,13 @@
 class PodTerminalChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "pod_terminal_#{params[:pod_id]}"
+    stream_from("pod_terminal_#{params[:pod_id]}")
   end
 
   def input(data)
     session = POD_SESSIONS[params[:pod_id].to_s]
     return unless session
+
     cg = ContainerGroup.find(params[:pod_id])
-    cg.send_terminal_input(data["data"])   # dispatches through the model, not raw pty_in.write
+    cg.send_terminal_input(data["data"])
   end
 end
