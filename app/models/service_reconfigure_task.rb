0dfa6335ddb451ef9bc43ae4caaf1a6279adcf61
MANOJ PUTHRAN <manoj.puthran@in.ibm.com>
2026-08-26 15:46:17 +0530

    Add OrchestrationStackReconfigureTask & StateMachine to run reconfigure subtasks

--- app/models/service_reconfigure_task.rb
+++ app/models/service_reconfigure_task.rb
@@ -17,6 +17,13 @@ class ServiceReconfigureTask < MiqReconfigureTask
 
   def after_request_task_create
     update(:description => get_description)
+    return if automate_drives?
+
+    # For services that drive reconfigure without Automate (e.g. ServiceEmbeddedTerraform), dispatch per-resource subtasks.
+    Service.where(:id => options[:src_id]).each do |svc|
+      _log.info("Creating reconfigure subtasks for service task <#{self.class.name}:#{id}>, service <#{svc.id}>")
+      create_reconfigure_subtasks(svc, self)
+    end
   end
 
   def deliver_to_automate(req_type = request_type, zone = nil)
@@ -76,5 +83,37 @@ class ServiceReconfigureTask < MiqReconfigureTask
                                :status  => "Error",
                                :message => "#{request_class::TASK_DESCRIPTION} failed")
     end
+  end
+
+  private
+
+  def automate_drives?
+    source.class.const_defined?(:AUTOMATE_DRIVES) ? source.class::AUTOMATE_DRIVES : true
+  end
+
+  def create_reconfigure_subtasks(parent_service, parent_task)
+    parent_service.service_resources.collect do |svc_rsc|
+      next unless svc_rsc.resource.try(:reconfigurable?)
+      next if svc_rsc.resource.respond_to?(:retired?) && svc_rsc.resource.retired?
+
+      nh = attributes.except("id", "created_on", "updated_on", "type", "state", "status", "message")
+      nh['options'] = options.except(:child_tasks)
+
+      new_task = OrchestrationStackReconfigureTask.new(nh).tap do |task|
+        task.options.merge!(
+          :src_ids             => [svc_rsc.resource.id],
+          :service_resource_id => svc_rsc.id,
+          :parent_service_id   => parent_service.id,
+          :parent_task_id      => parent_task.id
+        )
+        task.request_type = "orchestration_stack_reconfigure"
+        task.source       = svc_rsc.resource
+        parent_task.miq_request_tasks << task
+        task.save!
+      end
+
+      miq_request.miq_request_tasks << new_task
+      new_task.tap(&:deliver_queue)
+    end.compact!
   end
 end
