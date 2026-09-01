9700aee2faa6f50be8104e20c2cc3090902d188b
MANOJ PUTHRAN <manoj.puthran@in.ibm.com>
2026-09-01 19:49:02 +0530

    Use run_reconfigure, keep signal name consistent with provision & retire

--- app/models/orchestration_stack_reconfigure_task/state_machine.rb
+++ app/models/orchestration_stack_reconfigure_task/state_machine.rb
@@ -1,13 +1,13 @@
 module OrchestrationStackReconfigureTask::StateMachine
   extend ActiveSupport::Concern
 
-  def reconfigure_in_provider
+  def run_reconfigure
     _log.info("Reconfiguring stack:<#{stack.name}> in provider:<#{stack.ext_management_system&.name}>")
     stack.raw_reconfigure_stack(:dialog => options[:dialog])
     signal :poll_reconfigure_complete
   rescue => err
     _log.error("Failed to reconfigure stack:<#{stack.name}>, error: #{err}")
-    signal :reconfigure_in_provider_failed
+    signal :run_reconfigure_failed
   end
 
   def poll_reconfigure_complete
@@ -26,7 +26,7 @@ module OrchestrationStackReconfigureTask::StateMachine
     end
   end
 
-  def reconfigure_in_provider_failed
+  def run_reconfigure_failed
     update_and_notify_parent(:state => "finished", :status => "Error",
                              :message => "Stack reconfigure failed to start")
   end
